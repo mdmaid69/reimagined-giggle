dd5e4ebad4b8ecb4b32e3b39253196dc7b5cb8f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:09:03 +0000

    nature wave hand put shake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
