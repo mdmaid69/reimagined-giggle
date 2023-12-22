300da5c303ee02f18dd5c419e100a2457cbfb1b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:41 +0000

    combine approval runinto Qq natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
