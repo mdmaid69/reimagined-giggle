de105e18c0e3adaeeafd11ffbf1816b38a29266b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:23 +0000

    black finish set hide block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
