13b91ab21c87896cfa9f217c5985d09869bf1f5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:26 +0000

    Oo flat hit convinced start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
