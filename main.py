25b2cff1f3d6e459f62e5410135b0c3b1e7cd305
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:04 +0000

    stay goback(to) object address dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def divide_numbers(x, y):
+        return x / y
