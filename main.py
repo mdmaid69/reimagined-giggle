b13a1491b649f28d67cf303a107fcf8c93babf61
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:14 +0000

    lunch prove suffer indeed level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
