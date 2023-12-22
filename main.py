0e516d231e76433422aec13fc0865ac5aacd9b3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:09:07 +0000

    arrival dish declare behind trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
+  def is_odd(n):
+        return n % 2 != 0
