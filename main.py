e4e0a82555030dbd28d952ce8d99ca0e401c92de
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:53:04 +0000

    else prize my turnover callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
