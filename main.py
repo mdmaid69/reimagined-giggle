4ea05d7295b86ff7f73d6574f7bd7953b9a550c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:40 +0000

    round call anxious dream up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
