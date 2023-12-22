e11d27a04b672153c723e8a99b1218f7a2ab853e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:52:51 +0000

    estimate office maintain bury defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def exit_program():
+        sys.exit()
