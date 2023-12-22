1938e099b5743159c5a2b6f3e09e0d8ea883ece1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:22 +0000

    no or shout cope name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
