40e7dfc0d616eae00ecd80fbbd51c6cc28745bd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:05:44 +0000

    cool each since pool pronunciation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_power(work, time):
-        return work / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
