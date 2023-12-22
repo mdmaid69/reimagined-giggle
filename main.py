90a9fb116d11d5a2a5d0044246c50106fd2e5cac
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:11 +0000

    idea waste surprising ordinary turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def convert_to_binary(n):
+        return bin(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
