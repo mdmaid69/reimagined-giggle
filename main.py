ad39e328f11f8440feda80fc53398bc7982f9a22
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:27 +0000

    develop catch suit impressive compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
