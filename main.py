a829a332f200c575f9550acbb21e0fd066d611f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:05 +0000

    rush sell intelligent tape deep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
