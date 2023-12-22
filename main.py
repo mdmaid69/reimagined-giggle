078980c8dc3aa902124d9d64b6699a8a52ae9e84
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:53 +0000

    involve suddenly when such trial

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def convert_to_hex(n):
+        return hex(n)
