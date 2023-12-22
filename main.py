88973664489725b06a4cbf86a37ba71b957bee07
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:42 +0000

    aware spread scale knock case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def reverse_string(s):
-        return s[::-1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
