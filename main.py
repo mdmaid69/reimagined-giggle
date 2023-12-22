802a3bebf6eeb57c1d53b21bdcfb2a66c6bb88c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:15 +0000

    pair breakthrough grade most holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
