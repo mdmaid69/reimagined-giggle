a6452299a5342dfa061e8db1b17684d9471fa779
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:38 +0000

    happy pause best 573entertainment amount

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
