dca70ca9ab5e2e2f9d5667205a7afe002a83a2e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:50:08 +0000

    small direct table exist hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
