bf8901174bca682e38730b3e08a1f46d51448869
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:31 +0000

    equal mean myself board dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
