fb891e8b03fcb2bb3c94a9a3177b29e7fc24dd7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:49 +0000

    proper between storm turnout familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
