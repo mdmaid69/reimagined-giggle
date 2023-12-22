6214e56304189f085e13e3a96fd8206114a335d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:27 +0000

    worth road forget keepaway trip

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+from collections import Counter
+print(Counter("hello world"))
