e2cf3d9cfbf8c179e66db8039cf6c76fa99d4897
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:37 +0000

    test early tonight slight open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
