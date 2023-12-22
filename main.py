ffaeac8d2777da9e8b39cd70e16cd405a1814a99
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:21 +0000

    account positive live science week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
