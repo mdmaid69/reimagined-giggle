8773c11885d6ab126a82d11f99c1b42ef99acffc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:45 +0000

    plant tend sea handle anxious

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
