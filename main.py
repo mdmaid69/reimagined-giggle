7f499543462d0f48dbf6187448c4efe5065cfda0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:28:20 +0000

    authority hope fear for lookaround

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
