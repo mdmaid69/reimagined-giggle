99aedc85611a099bb50a8cd05e9a9236b8f07474
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:02 +0000

    food maximum shopping short design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
