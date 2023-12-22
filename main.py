c94c465390746d06e05f07e2abf11b5c74948cd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:51 +0000

    respect top tap centimetre follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
