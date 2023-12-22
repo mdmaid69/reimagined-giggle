e8fc3be1864010beb56f0b097a623998c1f74af2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:09 +0000

    attitude abroad custom myself hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
