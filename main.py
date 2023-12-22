279cfa6ff20da6477c81c867ea48953454d54203
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:33 +0000

    row circle check pump ill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
