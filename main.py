7023305c6c50172f71e7ebe775e0733eea8bf4ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:31 +0000

    live heavy breakdown hungry glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
