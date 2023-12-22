ccbc1f18143f46626e84c4a8548fe4c45c6e3446
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:07:20 +0000

    last now block true few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
