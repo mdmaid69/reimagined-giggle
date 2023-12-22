96e92acdce5173bf9170e48d8e2873bca000a5f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:30:27 +0000

    brown future shock recent dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
