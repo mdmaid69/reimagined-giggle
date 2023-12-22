354622e1c2941c85813da7268bf94734ce2db47c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:31 +0000

    fact kind handle takeback basically

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+print(re.match("h.*o", "hello world"))
