cad8e9f1e08245bf2ddf058865aac8ca70b41f3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:55 +0000

    long store minute demand about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
