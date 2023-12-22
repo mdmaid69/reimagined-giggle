0a1cdd655229dc1cf7d66de9692d5e05e5d57eb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:16 +0000

    bear trouble language takeout paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
