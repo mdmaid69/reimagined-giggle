95b2cef33ee089e14bf0bbae84253f8d95838290
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:17:15 +0000

    odd impressive disappointed pattern needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
