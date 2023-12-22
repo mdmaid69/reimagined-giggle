3300d704c64ec5c614aee840d4892bf691ea9993
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:14:36 +0000

    sister trap but all fit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
