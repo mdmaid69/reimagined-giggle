ca221742f438fbd1a2411103ee2275ba52f08950
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:38:02 +0000

    container clear temperature doubt shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
