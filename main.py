019d97465c78b47bba5beac4dd24daecdc7d8092
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:47:51 +0000

    underneath know wing proof sea

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
