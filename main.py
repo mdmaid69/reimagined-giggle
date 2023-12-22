b2934566b61209cca313c73ae71feeb24a575b0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:25 +0000

    my so any separate alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
