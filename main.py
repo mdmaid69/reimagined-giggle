abf12e67f0833e57af29b0c2b0876683f26fb511
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:27 +0000

    here advanced sharp see weak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
