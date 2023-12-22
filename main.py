64c6c565797fcde8d015657ceeaffe0fc83b9534
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:20 +0000

    on arrange beat progress left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
