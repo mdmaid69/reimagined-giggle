876d048c0b3fb8632ab055bec01ef1e8fd2edf2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:39 +0000

    free formal character comeover especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
