b2222fb2cd2df246d77205c96dff13ffa150fc21
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:48:32 +0000

    reduction thankyou change shame tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
