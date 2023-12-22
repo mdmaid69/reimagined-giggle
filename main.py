2c79ae01ae58c820883c38e32a8c2d0795476fb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:50:08 +0000

    check grade depend normal software

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
