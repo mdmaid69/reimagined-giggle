00bd9cccf2ef59604b032974228eea6e6f84e710
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:17 +0000

    seat type form instrument freedom

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
