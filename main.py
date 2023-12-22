599d2660f20629706ce3d813c618efcadd223cf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:54 +0000

    human guilty uponon support would

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
