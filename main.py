9c7b255d17e8c1b9e9df3f86e4e10ae3c326ad44
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:55 +0000

    nothing offend finish for confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import collections
+def create_user_string():
+        return collections.UserString()
