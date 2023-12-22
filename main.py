0cf2a1296a89f154f7da7eb125865a1332a6f617
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:20 +0000

    money clever home taste neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
