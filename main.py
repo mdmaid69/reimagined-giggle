43e32b61431d61ff45a4951ee8d49cd36a0173bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:16 +0000

    connection dress along since sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
