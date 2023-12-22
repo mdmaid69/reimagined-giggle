8d895d9a9fe68602dd186f4b1c40b9dcd6a7d516
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:58 +0000

    how contact loud lucky either

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
