bb708e5b77a308a424cec0659c347e3202c9d078
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:45 +0000

    clothes bread seem any goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
