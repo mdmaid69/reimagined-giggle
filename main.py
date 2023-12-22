a19fd345814b00746d782080ffc48c0f0744419a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:47 +0000

    accident home image responsibility here

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
