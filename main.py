1f488dfd04f0e2aac924f748fbb0adb47054d82d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:30:49 +0000

    too trousers accidental afternoon minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
