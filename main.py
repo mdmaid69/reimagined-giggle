89d3f74ddde3ebed71af764725f1d56d406efb8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:54:15 +0000

    but nearly lower Ll Ss

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_power(work, time):
+        return work / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
