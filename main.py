8075fc5aec61385650aa941fd748c157ca43ce69
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:34:33 +0000

    indeed smell block belief still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+print(json.dumps({"name": "John", "age": 30}))
