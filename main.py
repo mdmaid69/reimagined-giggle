da540ca5e1ed7122ef391ab34550bc7328f14866
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:17:45 +0000

    defeat manner point present convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
