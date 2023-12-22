68587747be824263853f98b30857478ae3903e76
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:05 +0000

    expression always Itself patience slice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
