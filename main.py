af435fe45d4f8f44150fcc58f16e3605a0b02c27
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:12 +0000

    card music matter stretch traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
