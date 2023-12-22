a078e18cd7fde2110ba6811cb4b3b165382cbe8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:13 +0000

    debt whom swallow put amount

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
