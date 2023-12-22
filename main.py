d64efdfe35b03686d99aad957f0be565c84af904
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:58:44 +0000

    benefit further hurt powerful careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_as_set(array):
+        return set(array)
