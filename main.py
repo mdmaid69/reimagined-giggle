d8adb928c3e471d578e58d6ca155499b989e6d95
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:52 +0000

    instead include deep justice bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
