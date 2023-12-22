4418ded814524ecbf927543addc23d5df704485c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:18 +0000

    most situation sleep lay getup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
