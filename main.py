87228d2ec2a36a7f773bb03767a5f498e44e7bfa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:07:12 +0000

    personal offer earth attract weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def cube_number(x):
-        return x**3
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
