022fe1ef8e66d611dfd21299e60a98b250594fe0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:44 +0000

    create none exam trick bottom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
