dfd49e84e096ff59870ef1a06ac5e2b787230bd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:38:10 +0000

    repeat return every level maximum

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def sort_list(lst):
+        return sorted(lst)
