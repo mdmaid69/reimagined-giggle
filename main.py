c83bdacda45d2777226535170179dca215fe81b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:57:37 +0000

    exciting wing passenger flow search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
