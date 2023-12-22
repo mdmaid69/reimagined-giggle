d83a916c384afdcf875b06a2de8577dac505dbca
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:18 +0000

    give year bother chief breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
