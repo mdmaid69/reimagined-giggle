1b32549d96e418f43a040bb43a24078b93ed7203
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:07:09 +0000

    pinch care improve crazy attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
