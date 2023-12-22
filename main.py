3f79999b558741a758a04f3bd6f30a6d3205aaf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:17 +0000

    change answer leave fit wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
