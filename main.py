e87c913da6d18b0a8774c686a67b3e2b45f9a74b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:05 +0000

    suddenly brain practise pay them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
