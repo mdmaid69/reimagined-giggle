a4bf89288d333d5be770176a68ca18635d551c50
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:53:45 +0000

    mix pretty joke sound loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
