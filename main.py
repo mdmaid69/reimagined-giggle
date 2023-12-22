4e5dff4b856dbf59d3ce64f0859260362fd2fe04
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:32:25 +0000

    object benefit strength broad people

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def calculate_area_rectangle(l, w):
+        return l * w
