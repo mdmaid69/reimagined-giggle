8c1259de3c55bb6976aa75bb00549a9be5f8b003
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:31:52 +0000

    ground confident boil grow noone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
