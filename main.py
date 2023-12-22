69dd1cfb8f1c4a07a90454864d3d38a632e270a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:51 +0000

    shut of light so rely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def reverse_array(array):
+        array.reverse()
