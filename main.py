e957dcdf2aa7ed242c21caadccc011dc171e47b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:47 +0000

    body priority however off refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
