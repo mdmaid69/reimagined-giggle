d9ad6569fa3ee02c93614b82de53ed41b642269d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:23 +0000

    awake accordingto unlucky business distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
