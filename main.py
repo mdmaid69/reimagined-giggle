faa7527f35f6e4ead8a91f6f4e9c01d11c31ef29
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:18 +0000

    effect song look switch firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
