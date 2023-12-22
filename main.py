1a6a4780c13db26e95379ba3e573a397adf7b22c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:21:24 +0000

    bath passenger message lucky back

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
