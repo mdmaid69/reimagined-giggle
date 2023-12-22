ac3aeb3ce1ddc1b8e93379cd105d72a0f8ebac83
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:46 +0000

    special someone unfair contact embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
