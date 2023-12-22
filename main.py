fa324ae0e2f97d7eb51532d8d647ff0605c87243
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:41 +0000

    pressure rush Uu activity obtain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
