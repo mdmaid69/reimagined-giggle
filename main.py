d4f9499fbc65e79443d13425026b6d61c04d268b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:20:24 +0000

    who follow careless original real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
