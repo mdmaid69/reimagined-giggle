244d9ccc8fa4ddceba7caf187f229f3f09cb9cc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:30 +0000

    advise rest announce read pause

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-name = "Python"
-print("Hello,", name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
