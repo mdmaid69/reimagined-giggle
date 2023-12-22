ad517aa891c903ce75fca06b0d46f02c2cbc227e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:51 +0000

    patience worst catch inside old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
