700632d0893e4b331a1a5e7873eea5a4211c4816
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:56 +0000

    treatment thirsty strict independent hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
