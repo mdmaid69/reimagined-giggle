4e2d399afbcadd9fb9669788fcbe43c9f2ff5e2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:25 +0000

    avoid create wish freeze nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
