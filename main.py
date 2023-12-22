57ff99008857e905ced8437dfe5ca242033d09ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:43 +0000

    combination angry face listen comment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
