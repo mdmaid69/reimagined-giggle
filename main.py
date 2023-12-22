efca4fe3cb67de82e7359f31dd7e645b21b242de
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:03 +0000

    thorough turnback familiar opponent unhappy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
