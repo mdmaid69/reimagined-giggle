29e2e0a2c49b27f6d8299ce9ba86dcd48e1efd4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:54 +0000

    Itself whichever please uncomfortable dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
