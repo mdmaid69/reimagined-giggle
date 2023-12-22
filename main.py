c766785a259f23d8b13a4d4ae41ca5e439a21dfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:50:37 +0000

    public dig 573entertainment stupid life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
