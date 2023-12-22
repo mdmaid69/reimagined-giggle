06cd28ffb1cb16e8640c6bc1712a1f1cf40d3146
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:39 +0000

    public plant touch music somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
