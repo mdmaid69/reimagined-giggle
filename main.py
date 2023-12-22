523b77c53b937362a37fc146ac2bb6d75760f2d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:23 +0000

    loan train matter right learn

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-print(sum(range(10)))
+def find_max(lst):
+        return max(lst)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
