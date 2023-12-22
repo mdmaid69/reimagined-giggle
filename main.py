363a9933a941a733ce30e4aa1398d54c224b3144
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:32 +0000

    letter oil unlucky only loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
