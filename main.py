a0eff37e5e50823630062092d719f2335fdfa106
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:57 +0000

    expect start apparently opponent wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
