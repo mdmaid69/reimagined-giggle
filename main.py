3fab8a2a45e0692881d28ce9a34cfe5ccfe33b30
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:58 +0000

    little 570enjoyable exchange top special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def exit_program():
+        sys.exit()
