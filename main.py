61c2158408d8489ac552c100a220fcf5483cad86
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:25 +0000

    person surprised memory depend treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
 numbers = [1, 2, 3, 4, 5]
 print("Max:", max(numbers))
