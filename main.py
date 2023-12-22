8272231c21e79085327e7f36fcf86f31e7922bb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:01 +0000

    avoid off shoot surprising along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
