ee0ed912fa007fa896cc634d66a4763696189fb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:14 +0000

    fence physical law train emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
