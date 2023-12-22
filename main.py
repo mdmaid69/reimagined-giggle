7e2bed8e1c916dbe96d3513438315581c10ddb21
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:19 +0000

    rush line interested branch letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
