316c7a0b145ce11b113f6a2e8adc677a3469f75b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:46:25 +0000

    nice bend front expensive film

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
