61e3d4bfbb26e92f6a41d0c96c85174da218690f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:47 +0000

    fix separate drop do argue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def count_elements(lst):
-        return len(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
