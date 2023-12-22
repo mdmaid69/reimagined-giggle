520ab62e153e3b13211f62e296e660f37bcbd942
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:28 +0000

    division shoulder behind however interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
