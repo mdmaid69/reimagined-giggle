cdf28023f91e5b3582deea615f6069955b7bca5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:35 +0000

    skin pipe separate confident from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
