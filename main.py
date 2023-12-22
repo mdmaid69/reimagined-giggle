832b7c7678cc09016b0bae74b73550023581960f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:40 +0000

    interested would constantly thirsty press

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def reverse_string(s):
+        return s[::-1]
