fa6c6548618afb9c17a88a73c0206b35845c57a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:46 +0000

    share figure just meet late

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
