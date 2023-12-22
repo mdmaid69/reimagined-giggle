d96dc18b04265ca9f6f9f34c99b38b9402428102
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:56 +0000

    she your unhappy support dress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def convert_to_hex(n):
+        return hex(n)
