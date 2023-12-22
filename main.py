6581b3ae03c75200832728745542c4c7e652353d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:12:35 +0000

    unkind surprised usually matter some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
