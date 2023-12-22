2e450c62f4650a9746d66fba4c3a2740472b0933
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:25 +0000

    message likely character perform exam

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
