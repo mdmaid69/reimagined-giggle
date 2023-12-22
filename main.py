dad583bc261e56bd7844649f9b7d66125ad9c6c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:35:00 +0000

    dance buy opponent tune whichever

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def sort_list(lst):
-        return sorted(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
