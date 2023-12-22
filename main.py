29592c71f09b456d57918344032e03a30c4dc45f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:30 +0000

    damp season empty yesterday opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
