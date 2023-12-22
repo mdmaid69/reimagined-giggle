1a15de60896dbf4704c42e3ff2129c86ba1e7c33
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:50 +0000

    well sister lot drop shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def count_words(sentence):
+        return len(sentence.split())
