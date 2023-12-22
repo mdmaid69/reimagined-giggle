1c68e511ac1b64ac37255da8160d3e894107b5f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:06 +0000

    godown top remove confidence straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
