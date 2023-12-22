d95cfdf6c9b519af0d51804b7b90c2733e6ac37c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:02:02 +0000

    main poor whatever relation radio

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
