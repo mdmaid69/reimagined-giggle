4b2725f8fa402c9fe8e50f6a2d6c5d04a8e3c61c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:35 +0000

    could whom tap criticism settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
