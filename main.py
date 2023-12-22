7e34d10b1f870cfc08f963618463bed3af9b9efe
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:12:37 +0000

    near all sticky ring unit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def print_python_version():
-        return sys.version
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
