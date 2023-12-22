1221840c29d37a7f5e9bf0f16a8b92d106b00439
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:04 +0000

    future related usual suffer strength

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-print(random.randint(0, 100))
+def find_max(numbers):
+        return max(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
