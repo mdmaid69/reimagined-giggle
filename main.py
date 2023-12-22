87653620e6341dd4fc478e04079387e6207e1aea
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:06 +0000

    clothes your turnoff pour length

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-for i in range(5):
-        print(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
