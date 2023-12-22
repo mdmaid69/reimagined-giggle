b9a78b3889a861f63865f04b4cf66b0b6f548498
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:56 +0000

    sail follow across actually someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
