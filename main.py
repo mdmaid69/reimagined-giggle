3c26e9c9ad1e778f50a43ad2f0fee6ab3bebeac6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:10 +0000

    bet ever bag only visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
