19a6cf427d51c70586cb60d2b766e829e29ad468
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:02 +0000

    such culture wave pocket turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
