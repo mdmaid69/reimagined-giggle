4ee0ef3912119bbfb91dc919693c327916a54470
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:52:11 +0000

    least his another pin generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
