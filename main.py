8bdbfdcae77d71535db29b814f8c273b0d1dbe08
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:52 +0000

    glass answer better woman impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
