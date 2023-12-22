1ab79846192c6947dfdc0444fc51efff745d7daf
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:40 +0000

    Vv convinced congratulations able university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
