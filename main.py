2bc6f18bca18d139c2c83774b0bdddf94aace93f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:22:24 +0000

    receive light concerned exhibition price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
