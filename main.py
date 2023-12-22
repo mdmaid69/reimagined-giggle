41a9987fcd33031a198f3e07e3e6f2bfd982ce82
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:47 +0000

    fond occur among fire satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
