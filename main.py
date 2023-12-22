b0ac7bf6257f8817d155e758a8316f0a0071e343
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:06 +0000

    fresh upset negative solve smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
