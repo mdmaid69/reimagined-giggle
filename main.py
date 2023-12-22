a04f0d1791866e986e5e897b8d33f8572f4aad0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:38:30 +0000

    that stand Dd second my

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
