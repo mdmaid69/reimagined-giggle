d1e8a47c49851094176d401e789627dad22dbb3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:27 +0000

    rare form by shoulder everywhere

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_max(lst):
-        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
