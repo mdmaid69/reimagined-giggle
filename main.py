c54b4061250e22201bbab8f30f397c52359341af
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:32 +0000

    period better strike calm unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
