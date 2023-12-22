e0a51f500b444026bdd7b0bad105e350a64e7092
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:24 +0000

    thinkover contents space family ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
