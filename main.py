4e01d6f761e0f6df3bcdbbc831d39ad6510cb925
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:38:06 +0000

    scrap which comedown married rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
