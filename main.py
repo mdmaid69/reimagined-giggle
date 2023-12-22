4edc141ddbc0064dcb94aa67a44ac9ab6fe4833c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:26:52 +0000

    rain bread mix conduct bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
