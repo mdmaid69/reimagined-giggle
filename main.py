e20e4d68b445e5dc190d57b30ccd0c31def39012
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:43 +0000

    common bottom difficult several agreement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
