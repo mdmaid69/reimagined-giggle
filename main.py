3c7e6a6a65109557cbfb111c001dfa586a21e3a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:32 +0000

    pocket sad spray file together

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def calculate_area_rectangle(l, w):
+        return l * w
+def reverse_list(lst):
+        return lst[::-1]
