308a90aa489f2b7955aac7061f7822deed3e2808
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:09:39 +0000

    happen imagination opinion even habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def reverse_string(s):
+        return s[::-1]
