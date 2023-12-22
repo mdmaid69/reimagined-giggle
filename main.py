fb6df8202ad09a3dce2852ac62a36845e9a1a4fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:08:25 +0000

    before breakinto before study dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
