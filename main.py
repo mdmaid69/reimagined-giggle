3b8eb4ca5c37c82e8438a1849e20a586ada151ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:49 +0000

    improvement clear win empty schedule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
