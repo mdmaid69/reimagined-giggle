bf86ba7d5d92522b9d9cfc045dc35ec8f96c6bb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:43:27 +0000

    tread crazy drop simple part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
