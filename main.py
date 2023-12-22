55bec4019a8e6f816e867c29a3050e69083738bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:35:16 +0000

    guess cross calm school flow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
