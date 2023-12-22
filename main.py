42d8fdabd390995addfc429a5c73fbd02ec7284b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:14 +0000

    check deal worse dare comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
