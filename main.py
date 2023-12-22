e475fa4217ba50eca54c8a4293af46beafa978dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:38:47 +0000

    fast please square now problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
