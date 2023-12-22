56db4862f1296cc34091c8ee99f7dc7d4e7387e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:45 +0000

    limit separate waste short pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
