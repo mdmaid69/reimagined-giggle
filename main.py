49ffe8806311dd1dfba2c1574128ccfeba5c85a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:30 +0000

    pen wise team door blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
