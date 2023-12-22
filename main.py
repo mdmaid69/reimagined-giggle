3b767db225af4cbd5bddd1544bb52142863c33da
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:36:04 +0000

    comment calloff level careful scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
