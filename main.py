bb7f0366be097c2614595579f0ace74b01ad5965
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:07 +0000

    connection trouble check prepare arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
