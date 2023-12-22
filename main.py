ccea6c4dd46815900c8a774c3ab3b057186e16b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:43 +0000

    error come manage interfere practise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
