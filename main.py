fab5b3aab3dd7e4247e8b65716a2594c90f38773
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:57 +0000

    suffer full stir sound colour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def is_odd(n):
+        return n % 2 != 0
