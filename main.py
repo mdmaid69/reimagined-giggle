5f50ac09cb4019d87e4d28191eb3a57c14264066
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:54 +0000

    waste breakin takeaway build Ss

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_cosine(x):
+        return math.cos(x)
