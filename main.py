21c9b2177075649d6baa6bef7b7db4842a239766
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:40:11 +0000

    rare safety wing dream deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+def check_if_array_contains_item(array, item):
+        return item in array
