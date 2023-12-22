e099bb649f3884ded67a188ccca3d555fad21206
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:27 +0000

    roll and imagine word key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
