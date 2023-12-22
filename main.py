55b5e40c839a03e3dcce063912000983ffd08bcb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:46:16 +0000

    bottom communicate cool sort total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
