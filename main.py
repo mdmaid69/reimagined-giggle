d61bf53251bf141a7b3895f26dc49e39126c57e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:58 +0000

    touch shame independent city serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
