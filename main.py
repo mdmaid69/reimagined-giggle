77a23e27e7c88526630401c7db17ef05b28ac21f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:25 +0000

    comefrom see furniture remark scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
