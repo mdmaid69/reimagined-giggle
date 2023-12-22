b21c1f5bee4243bb2f57d24621abb62097acc0ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:24 +0000

    survive market interest turnout hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
