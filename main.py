d86daaa906077b2476e9689453295488ea5bdbb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:37 +0000

    back generous question solve note

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
