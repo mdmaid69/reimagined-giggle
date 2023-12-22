52626fd0fa556ead2a2400d5613cba843b154e66
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:31 +0000

    share city community breath lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_even(n):
-        return n % 2 == 0
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
