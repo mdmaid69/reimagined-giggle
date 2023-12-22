0ce729d5f02064e2aaa0a67f0aa4695f662b5264
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:20:06 +0000

    break desperate organize present equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_current_time():
+        return time.ctime()
