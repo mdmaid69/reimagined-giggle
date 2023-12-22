0329c7d415b4deb4f65f2bd9e483ca11540bfa8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:00:54 +0000

    risk gentle hide me score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
