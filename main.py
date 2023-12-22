553a0dcb034a60c62e595bb1f1f0a517d2ee76ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:42 +0000

    serious gentle calmly check human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
