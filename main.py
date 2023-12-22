20f4db35bc32365dc6ae2affd4440c6924a8a389
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:50 +0000

    pour want translate century twist

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+print([x**2 for x in range(10)])
