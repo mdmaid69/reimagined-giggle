6d42fe7727904b9ad8a5a636b2eeb3743fc675ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:14 +0000

    wash boil instead intelligent young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
