370686c74348efdc1ef7b0abf6535a514b7206f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:33 +0000

    read object chin square frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
