33434c5eeeeef8eeac3acfb6e8b626734e9e3535
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:23 +0000

    ordinary constantly convincing fit front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
