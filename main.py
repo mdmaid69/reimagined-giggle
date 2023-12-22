1a99f123be804e1c27a408ea8148e501ad97b875
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:22 +0000

    precise consist regularly bite appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
