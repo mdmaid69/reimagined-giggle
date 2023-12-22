086eaf37aaf3ad9398faca6d12d841ddde23e6f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:31 +0000

    whoever sight pleasant pour underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
