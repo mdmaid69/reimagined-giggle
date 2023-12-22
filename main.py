0a9fa48efe3b02dccee44acec624fa0371ce221b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:25:23 +0000

    keen herself step silent bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
