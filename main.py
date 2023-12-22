7210eef92b23309dc59d646159f928b224d6953c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:59 +0000

    payment union thanks strong attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
