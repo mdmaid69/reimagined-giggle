70655a8deb198519db2df29601bc0a46fd45dd4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:47 +0000

    change instead definite moral keepoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
