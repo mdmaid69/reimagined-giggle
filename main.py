3c039dba9224223fc462142c596bd2a54c2e8667
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:28 +0000

    phone great boring concentrate provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_acceleration(speed, time):
+        return speed / time
