9e21584f9e4317058a97b53065d92fe84138a9d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:15 +0000

    image exploration border shelter past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
