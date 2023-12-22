ef216fb67e4b9b646cd9d8b9c0f7910b6ea074fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:35:04 +0000

    hurry tidy board without them

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
