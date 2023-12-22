70974dd6873548022241a84267f0afb77849c88c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:49:08 +0000

    expect take little pure reliable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-print(random.randint(0, 100))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_binary(n):
+        return bin(n)
