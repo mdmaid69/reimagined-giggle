e3b9857e89aa81f1da4c3f3dce8ba8c7ae559965
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:04:02 +0000

    comment turninto sticky comfort season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
