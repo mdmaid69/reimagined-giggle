2e5fae6ac988f1b2ad9118f2570875176eb74f61
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:23:36 +0000

    brain defeat dish call rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
