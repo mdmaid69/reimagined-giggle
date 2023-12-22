a38093a41b6bebd5bd58725a88f2e76da0f1d18d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:08 +0000

    careful Jj comment dish lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
+import getpass
+def get_username():
+        return getpass.getuser()
