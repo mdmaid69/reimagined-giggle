3ae9c8c544abe0532880a277c136a17260a57a28
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:39:56 +0000

    mainly emphasize freeze period smell

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def delete_file(file_name):
+        os.remove(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
