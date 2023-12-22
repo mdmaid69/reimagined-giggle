8ecdf82beddf21db330eb9ad4801449fb0bb8fd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:30:21 +0000

    turnover money reserve along rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
