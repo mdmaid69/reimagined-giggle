ca2e978a1f751db59a49c6dc2cd35cd575f7afd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:42 +0000

    stir attention bringdown nerve strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+import random
+print(random.randint(0, 100))
