c76b6ce0223f6441feca4fa34c679de285b0d42e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:17 +0000

    sell heavy takeover grow ago

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_speed(distance, time):
-        return distance / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
