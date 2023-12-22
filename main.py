dc738b6cc8088ab11424661baae77ef24409f9fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:40:21 +0000

    half over ourselves instruction tense

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
