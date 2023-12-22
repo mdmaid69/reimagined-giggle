8d3b0a64a3c4697c08e4af7b68818ebf374102b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:55 +0000

    sure importance culture glass impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def get_array_as_str(array):
+        return str(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
