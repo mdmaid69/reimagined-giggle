fe52c1cfb52c7a101ae8708568349b86aa8bf04f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:33 +0000

    tool paper simple cost goup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
