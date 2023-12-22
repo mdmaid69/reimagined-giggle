24ef95ec5d8c20394ba42c31d38cf76dd4abcb9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:13 +0000

    soft develop reduce sing 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
