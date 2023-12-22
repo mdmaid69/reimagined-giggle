5448ca0855d00769db7c05cf90b525f06e719e60
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:53 +0000

    land increase prepare behind rain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
