ce9c0f9a9eca014cf2a44e7143830f5f2da286db
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:34 +0000

    confusing spare exercise permission murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
