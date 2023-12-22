16b26c71f8989e87cea08c9886294529007fc812
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:43:11 +0000

    cry recovery Cc situation look

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_speed(distance, time):
+        return distance / time
+  def reverse_list(lst):
+        return lst[::-1]
