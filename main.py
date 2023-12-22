8bc90d271ae888a9a6d2bd6e2d811b7c890c4705
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:36:49 +0000

    surprise mark around effort home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
