a262b5f83e7da21716f865f58f846a86c8c9dc19
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:21 +0000

    put copy spray fall steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def convert_to_hex(n):
+        return hex(n)
