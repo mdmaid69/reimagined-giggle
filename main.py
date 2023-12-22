f3228f8d2b64222f095cd6217ebd28e3fc3df63a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:50:02 +0000

    loose curious goout account quick

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
