c51b3469dbe2e9161f995a7549d31fa2acf7a94f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:17:17 +0000

    foot repair protection peace this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
