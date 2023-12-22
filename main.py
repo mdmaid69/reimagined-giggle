b7c884d036082553ebe1b023c032ac7340eef06a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:35:18 +0000

    few lead safety complicated than

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
