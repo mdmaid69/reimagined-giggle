63924eb32567b81821e64bcfab3b39159e8582fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:49 +0000

    warn programme hit shoot cut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
