e4cb3899019c00d3bd7a5c1cbc779cb134fca118
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:38 +0000

    Vv react operation bunch subject

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
