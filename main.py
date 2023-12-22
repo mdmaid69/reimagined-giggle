4cf3a6f9e510a8f048032f1624399fd61390831d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:00 +0000

    complicated cut difficulty fortunate talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
