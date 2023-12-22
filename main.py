ad9b37b7eb7d93a787374ac2154368857d654f13
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:21 +0000

    fire runinto trade phone strict

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+print(time.time())
+import tensorflow as tf
+print(tf.__version__)
