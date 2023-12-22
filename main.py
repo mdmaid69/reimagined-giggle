cb24703fcef7963364c8b5b4f2dd8a80e4a862d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:50 +0000

    stir degree forward used slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
