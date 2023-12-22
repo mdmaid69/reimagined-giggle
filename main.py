6a16051a276688af59420041a532d15b1ebcf55b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:54:06 +0000

    complete keepfrom drop smell quarter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_characters(sentence):
+        return len(sentence)
