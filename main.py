0445c78be72defc87f2d74943b88b0cc8d7b25b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:19 +0000

    lead city connect ear definite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
