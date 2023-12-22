5e7fa37f60248c19c9794c7df8f98892a6c9fdfa
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:18:17 +0000

    borrow dress favour always smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
