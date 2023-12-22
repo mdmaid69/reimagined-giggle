aabb2aabda72b6c85f492b8170b172b8a769efe4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:17 +0000

    race peaceful sky imagine occur

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def reverse_string(s):
+        return s[::-1]
