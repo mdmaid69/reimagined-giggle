399483ed9d3e78c6d5dd819b19ac51c7df649873
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:15:38 +0000

    score somehow complicated world race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import time
+print(time.time())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
