ee5bca306eb5aa44f4bd07e6f6e45ea7a59d4c5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:30:14 +0000

    constantly birth vision room guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def square_number(x):
-        return x**2
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
