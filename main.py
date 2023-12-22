b04f5ed3034957ce3bd95186af7a1539c57ae51f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:55:25 +0000

    eat warning disturbing strength watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
