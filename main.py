5b95a714e0f48dfb2a1a15152b95fc22cfc52fe0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:59 +0000

    repeat smile loss underneath couple

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
