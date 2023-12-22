1b337b85574063a88c952c78f02a0fb4b2fa6dca
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:00:59 +0000

    off present knowledge more by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
