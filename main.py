111a6b034cba82a0dfe73fbfe436768230171e06
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:09 +0000

    comeback beat tone I along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
