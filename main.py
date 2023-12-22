576a8a3c4932c2585acb009ad4159e9565008e56
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:22 +0000

    perhaps left scene crazy identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
