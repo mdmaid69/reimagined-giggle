b3e4f4f893dab759bbe3860bcaf89dbf86c679a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:31:53 +0000

    thinkover modern address long raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def convert_to_octal(n):
-        return oct(n)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
