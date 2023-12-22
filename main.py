a260a7fd11e69349d3926ca06bd45bc7d3d80943
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:16 +0000

    runinto cope comeoff disgusting bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
