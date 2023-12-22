fb8db95335a222e1bea87cbfcc146f16f238e553
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:51:55 +0000

    Oo suck ordinary turnover makeinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def convert_to_hex(n):
+        return hex(n)
