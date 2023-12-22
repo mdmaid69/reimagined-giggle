fd1f6fdb72db4bfc432b59f8247d34c50e64502a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:26:35 +0000

    whistle anywayanyhow free welcome crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_density(mass, volume):
+        return mass / volume
