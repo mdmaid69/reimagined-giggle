90564f5c0b1f94296c346fb9368240be27e93d41
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:04 +0000

    getup street importance where criticism

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+x = 10
+y = 20
+print("Sum:", x + y)
