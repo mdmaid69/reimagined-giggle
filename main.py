da33c35206159afed6670f194aaf517497805ab9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:33:10 +0000

    callback place naturally tense its

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
