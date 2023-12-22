5db5e963f46cd20d3c5370f51d5d36ca0cbefb38
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:17 +0000

    available owe wrong roof here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
 def calculate_power(work, time):
         return work / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
