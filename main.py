b80cbfc0f31d8eb48aefe298ebab3ead6c37d170
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:36 +0000

    her news prize occur keepaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
