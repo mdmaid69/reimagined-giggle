08bf3b051424cebf52e1bf5a99832689c37fc40b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:31 +0000

    frame boat exploration amount sing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
