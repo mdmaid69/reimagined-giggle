4f24c66fea2cf379ba2e9dcc859447ce7993dd27
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:46 +0000

    talkback high stay once threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
