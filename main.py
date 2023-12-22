cc831837a03b6597f7467fb07a683928e08090e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:05 +0000

    exit dance sing tonight partner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_odd(n):
+        return n % 2 != 0
