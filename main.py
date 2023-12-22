2cdcc46ac30f5bd90acd202263989b6b8f55c84a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:30 +0000

    fill record affect scared stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
