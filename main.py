a06b41e5daffde89ac76e500af58eba7bf50713c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:26 +0000

    exhibition lonely neck quit sad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
