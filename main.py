b28bca2b8e7f263168761b06d6350b8b13f1063d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:04 +0000

    oil deliver company guard more

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
