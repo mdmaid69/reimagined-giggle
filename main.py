fc1a723e3764b877e9bff1dfba3433220d334962
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:14 +0000

    married quick watch minimum show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import sys
-print(sys.version)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
