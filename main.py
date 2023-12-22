682ce37c956c11d4770a0615bed200c8ec182a70
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:40 +0000

    system arrangement should frighten class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
