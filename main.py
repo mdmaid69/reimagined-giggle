ab5ad23b248ce36edf7618ceb7d0435a43e09030
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:50 +0000

    occasional dear score teach priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
