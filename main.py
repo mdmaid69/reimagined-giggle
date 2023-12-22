3001120865ea4212bc8c7c55c169d6a114d0196b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:25:22 +0000

    effect right load runinto undo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
