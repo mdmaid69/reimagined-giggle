c06e42c99e29489a0722bcc7514499d0564f14dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:33:01 +0000

    today large pleasant under dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
