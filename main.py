881feef50e34599abf4a72338201c86ff12ccc4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:01:04 +0000

    soft something once Rr takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
