6096b49c067755d746c791c711d4133765dbb84e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:51:12 +0000

    now building tidy sentence left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
