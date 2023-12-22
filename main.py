84c173b39150d48de301b240a2a8f2bd09757c37
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:16 +0000

    careless design production town law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-print(math.pi)
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_set(array):
+        return set(array)
