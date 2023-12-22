042071993085c475be6235d1a4866b0ad23a04fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:37:45 +0000

    negative level right 567enemy pot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import os
-print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
