1bcae485a99eb68123f3d2d09e48e075f69c31e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:17:49 +0000

    spend easy imagination settle tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
