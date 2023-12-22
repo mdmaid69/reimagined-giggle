d9d2e131e3d370b979666e8fe52f567012a6a29f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:40:41 +0000

    scrape marry matter off object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
