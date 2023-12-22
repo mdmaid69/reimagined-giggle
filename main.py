b220a0a28b2d5679e8faf4c1079d2b73465fcc40
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:26 +0000

    effect dry different excuse finger

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
