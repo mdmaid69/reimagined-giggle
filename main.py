5afed16d4fc23dffcfacc748a532411866982e16
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:37 +0000

    heavy watch weigh discuss exercise

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
