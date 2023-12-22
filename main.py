54ee0533268d70673008d20e62b3dbe8fb8c4a66
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:54 +0000

    square attractive 179bite single Ii

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
