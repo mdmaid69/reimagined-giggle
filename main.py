2f5813786a6d0e173f55223a6420560f1eb27001
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:43:34 +0000

    downstairs either cause flat although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import math
+print(math.pi)
