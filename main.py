4c5d2bf7e148a43eda7a4fd3b8145beab109a8d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:15:27 +0000

    busy run she convenient everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
