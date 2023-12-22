ea4c54b9ab8f0570e84e9b171f1753769d77995d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:44 +0000

    aim scare best suit trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
