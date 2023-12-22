dc00ac1d92df66a2c36bb584ad0abacd97b57e1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:31 +0000

    behind how greeting across spot

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
