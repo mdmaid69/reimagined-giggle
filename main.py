15ae3f37bd51a14acc1a7f017d1c31a78a3ed54f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:50 +0000

    zero tomorrow lay height payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
