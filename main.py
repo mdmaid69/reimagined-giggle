f587eba45f99252eb6259ad1f0c13686f0720031
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:20:19 +0000

    stir board fall hard brother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
