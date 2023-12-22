013f8eeb4c7999afa2aeffc62ccfb5dd0331e797
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:42 +0000

    worst thirsty knock open lend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
