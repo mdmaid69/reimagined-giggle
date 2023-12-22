e5375bb35a7db51da0d93163c8aec140d0a0684c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:21 +0000

    mark wipe kill turnaway damage

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
