c8d93fef470a98a74af44028cc531cd785114cf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:28:00 +0000

    tape furniture lend over frame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
