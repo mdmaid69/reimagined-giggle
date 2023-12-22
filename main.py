e646d00d1829e89fe8625a9598115a3eb9717b7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:16:51 +0000

    mistake telephone now she style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
