be3d359c11d4969df841b7b306e29804cc93ddf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:56:15 +0000

    news miss nor state declaration

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
