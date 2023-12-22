3e823703004b96ddfe6ae2fc3ad8d6c63cfdd072
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:56:38 +0000

    path empty guilty holiday OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_union(list1, list2):
+        return set(list1) | set(list2)
