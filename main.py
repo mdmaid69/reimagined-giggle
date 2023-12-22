88eb030b72b0959c1cbd8d40b607ab2f116d950f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:32 +0000

    steep prefer drop cheat beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_floor(x):
+        return math.floor(x)
