f477a5857380f392022ca9876508c042bcce6e82
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:49:13 +0000

    fill operation them major record

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def exit_program():
+        sys.exit()
