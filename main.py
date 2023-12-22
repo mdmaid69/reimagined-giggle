cef979d707bc4effd9b8983eecf5eb684c3fdf1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:14:35 +0000

    advise exchange few every visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
