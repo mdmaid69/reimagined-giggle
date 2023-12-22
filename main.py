58898997c6f8905bcf7833c7b2ac4c9fb891aef5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:19 +0000

    up hate ticket noone though

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
