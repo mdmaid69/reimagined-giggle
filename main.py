2f69a9ee81969bf106376b3030d11f6625c70668
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:30:36 +0000

    substance genuine reaction claim congratulate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def square_number(x):
-        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
