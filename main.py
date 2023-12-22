1ae8f5660912f0f4789d4635856feb0ede658daf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:10 +0000

    card here store shape right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
