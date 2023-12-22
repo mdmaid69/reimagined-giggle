26eea2f0b90817090bfa41de738469a3cc25687e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:34 +0000

    godown progress nose along really

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
