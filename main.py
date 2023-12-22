f79d1e5acae1fe041e4f76127d251a9dea732836
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:13 +0000

    speech over convince true arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
