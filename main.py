b3daf8a184885c5908c328b5d8d0ac26c1e9b2d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:49:52 +0000

    job immediately official its please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
