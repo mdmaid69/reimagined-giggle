2d7a2fdfcb22d816b791b21819f320e828a05287
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:32 +0000

    neat pollution flood loose together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def multiply_numbers(x, y):
+        return x * y
