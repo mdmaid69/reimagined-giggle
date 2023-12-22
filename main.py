4a0503c32577150123ca22714343b83643ea91cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:44 +0000

    brave file regard university Uu

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
