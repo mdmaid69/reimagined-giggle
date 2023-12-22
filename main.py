11312b7ab173f9305b32e347f9a7ca81c734f3eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:57 +0000

    feeling famous him fashion prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
