f16119d36e7837fa65aed20284771bbe5495b918
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:35 +0000

    mess rubbish theirs language turnover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
