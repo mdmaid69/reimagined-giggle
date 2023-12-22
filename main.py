c831b18efd8a4a22bc0476e74bd4810cc5e5d395
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:42:35 +0000

    work furniture attempt mainly contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_power(work, time):
+        return work / time
