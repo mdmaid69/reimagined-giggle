2f41303f10612ed5e87a268257f2a41779958fa0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:59:24 +0000

    lie skirt sudden safe defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
