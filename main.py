4402fd9089ddbb3406a463383e486071a1940c2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:43 +0000

    laugh computer tune minority height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
