4b145661535f7e25ea8b1d6f641ff39bffc81a58
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:26 +0000

    nature story last share narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
