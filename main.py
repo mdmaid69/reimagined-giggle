ae0e50f9fe1c798abe40ad57f49db8d3ab0ddbbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:26 +0000

    least deep ear scare close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
