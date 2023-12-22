dd89a419ee11432a713346bb62aa96ad7d49fa85
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:01:22 +0000

    than real total drink play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
