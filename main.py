62427d6f3afbdc04c27724abcfc418db26ebb3bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:17 +0000

    dry far roll float country

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-print("Hello, world!")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
