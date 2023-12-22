f7cbb3827479083d817ee3b55dcdfa32554071bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:48 +0000

    serve behave bridge blue grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
