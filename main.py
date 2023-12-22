f90462a65c36e6b7ccbd1e4b17ca1de71b600ef6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:52:30 +0000

    responsible payment experiment goodbye surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
