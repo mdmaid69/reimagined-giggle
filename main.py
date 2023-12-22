5f15660c1882627a486b1ab05f9314f26b3a67a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:09 +0000

    reply steer basically estimate phone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
