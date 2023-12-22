0c5bccacb325ce4a119fcdbb0cfbd666a35fe331
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:49 +0000

    getaway replace content report better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(sum(range(10)))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
