d9661c2bea561cc9ffa64e0ff0a750b2f7df1fe5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:47 +0000

    on wheel connection meeting outside

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_to_hex(n):
+        return hex(n)
