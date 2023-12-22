8d9b17cfeeac6533281ad211780d1bc570f982b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:13:50 +0000

    place whistle takeout bridge drive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
