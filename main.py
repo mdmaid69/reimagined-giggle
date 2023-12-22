2c34f39d91d2237db733ad32ed250793b3237327
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:59:02 +0000

    disturbing history bank uncomfortable attention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
