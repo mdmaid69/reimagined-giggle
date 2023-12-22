f50fcfd17d7a1a56ed638ebd9895ba5c98d45423
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:37 +0000

    house drawing cinema joke birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
