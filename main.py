d30ab937e451ab8b09dcc0e4b4f007b87652f851
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:55 +0000

    pipe original threaten crash laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
