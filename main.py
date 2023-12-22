746f20b071d18a73a9afb4c9263e012ae86ef9dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:47 +0000

    understand hate touch Vv fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
