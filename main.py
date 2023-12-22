b33362d8025e14e39ff96a44e34fdeb05ae69757
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:12 +0000

    breathe creature centre worst free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
