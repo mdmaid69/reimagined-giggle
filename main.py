efe0fb93f86563160290aad55577fc5c58400dae
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:52:09 +0000

    name grade pocket sing appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
