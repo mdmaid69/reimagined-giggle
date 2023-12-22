c6b719457206f58bd87c7bfb6c12d60ac5a5f1b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:05 +0000

    practical risk right waste react

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
