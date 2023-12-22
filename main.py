5734d6d87c7cc9b4344fcc3036f4b006ab5d370f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:55 +0000

    release gooff hers makeinto like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
