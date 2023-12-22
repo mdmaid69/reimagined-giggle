3bc3213840be1703af0b387042fe01502525ec75
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:27 +0000

    draw avoid relax cross hate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def append_to_array(array, item):
+        array.append(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
