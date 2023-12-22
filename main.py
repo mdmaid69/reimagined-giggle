d3bc6fb0de5baac247efc734d8d347d3fdafec30
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:20:35 +0000

    makeinto mail same unite tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
