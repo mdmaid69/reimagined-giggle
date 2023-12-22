623911dd832388ae9ebaa429073e5b2e6fb786b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:22 +0000

    pride hungry best cut instrument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_acceleration(speed, time):
+        return speed / time
