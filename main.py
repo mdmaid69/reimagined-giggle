9e53d98829edd9dfb7f942e976a3d0a4d54ceaf9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:30:32 +0000

    introduce development occasion conclusion success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
