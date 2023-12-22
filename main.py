2503262744ef1f9f8573c0b144d4c0457a9829ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:09 +0000

    murder material tooth average thinkover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+print(os.getcwd())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
