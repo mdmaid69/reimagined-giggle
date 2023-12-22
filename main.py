629f38ef15dce8fb4ab234939971f993852fc315
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:53 +0000

    criticize secret apart shape enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
