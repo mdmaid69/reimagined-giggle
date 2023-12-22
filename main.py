049a99bc317979ac2d61b9f63b38e74ddb876021
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:28:48 +0000

    meanwhile underneath deal period cheat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
