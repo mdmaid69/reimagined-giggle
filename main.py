3e251ed14576c3ee32c6b3017da4de674577f5cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:02 +0000

    before the question careful chase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def add_numbers(x, y):
+        return x + y
+def divide_numbers(x, y):
+        return x / y
