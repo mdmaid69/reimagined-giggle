a58241e51022af4a647f00058d168559c59d8903
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:40:36 +0000

    indeed cope type protest circumstance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
