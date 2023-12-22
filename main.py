e3d96f45cf022532d50a20902baa8454a7dab7b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:54 +0000

    shop circumstance fit sweet official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
