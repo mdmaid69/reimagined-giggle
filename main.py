9b7c68f170509dd13eefaa8a9cf81b073fe023c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:20:34 +0000

    reply card against actual prefer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_power(work, time):
+        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
