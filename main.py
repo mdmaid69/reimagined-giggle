aa735be01ecaba78b362fa33b90ac6ec0e359727
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:29 +0000

    before out silent just share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
