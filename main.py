a17b469616d745786790de36fae0e0dd15ba5c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:20:21 +0000

    home boil wind getup nowhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import sys
-  def get_python_version():
-        return sys.version
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
