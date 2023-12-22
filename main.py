6fb7eb22ab22c57f75bfa41c8d2b86eca3a45c67
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:33 +0000

    suppose sale rid branch interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
