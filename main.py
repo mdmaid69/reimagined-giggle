d15282561acc769f82220c5e255f2f8d7b54c67e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:40 +0000

    popular reliable ability past duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
