493ea169a705179a4f79311cea92519c5fa75882
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:37:23 +0000

    pollution right shut close broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+def get_current_time():
+        return time.time()
