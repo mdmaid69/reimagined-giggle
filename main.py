43f5e85428d0336c7f5a66b3dc5822eb893cba18
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:56 +0000

    message spread plain concentration now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
