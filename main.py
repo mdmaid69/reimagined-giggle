8a23d2ee2827bfa7ec8a20dcccf1daffb5f4e5fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:02:19 +0000

    man nature minority cover shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_min(numbers):
+        return min(numbers)
