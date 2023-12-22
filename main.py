2440eea1995fdbcc752a75fb44ddf49ebd576838
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:42:56 +0000

    label even character summer market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import re
+print(re.match("h.*o", "hello world"))
+  def add_numbers(x, y):
+        return x + y
