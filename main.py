0478864fcbba504c3f83f64c5c5316503e612253
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:26 +0000

    contrast kill murder cook delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
