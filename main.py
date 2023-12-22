608860c9ab366b3a2172518397f8b3619a634da3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:41 +0000

    turndown wide fresh next sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
