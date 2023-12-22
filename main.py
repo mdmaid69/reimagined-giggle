2d8d2c2d9a386c2e3c27445fa77422935c596d5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:10 +0000

    butter complete final type callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
