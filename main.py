e4cfe5cb6ba4621376bc4bd2beefb69e70a42ca1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:14 +0000

    fat weigh add thank birth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def count_elements(lst):
+        return len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
