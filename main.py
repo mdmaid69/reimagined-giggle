492ff9201261b817db0d11bb033d6401896b67aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:22 +0000

    sign chin total great goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
