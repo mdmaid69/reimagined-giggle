4b660c5e6ec488e6bd4a85b3166629e265a548aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:54 +0000

    free likely task separate extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
