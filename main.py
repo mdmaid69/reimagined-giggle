330faa2f387b5533ca24c99bdc2ddeb8e8f36e3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:16 +0000

    succeed hello come bar pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
