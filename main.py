165365db67d1486602ed03077762a5e6127a3f6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:31:15 +0000

    deliberately concerned stone glad early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
