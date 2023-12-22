a3e7ec6ca3084dc888a6c5ee28144446c9222ebb
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:51 +0000

    badly very guard help preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
