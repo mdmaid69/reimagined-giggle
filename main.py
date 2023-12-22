3d5f74e7e8ebadafce4f9cd04b80d7411d7bae4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:43 +0000

    basis gap honest compete weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
