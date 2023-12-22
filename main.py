d64ff8ff29b02e4d77425f5d7a0489291577579c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:11 +0000

    prepare Vv little neat young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
