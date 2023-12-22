d73459d8dac18044998f018e54a19ed188b2dd17
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:20:32 +0000

    clever reply wet moral physical

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
