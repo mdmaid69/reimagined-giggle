ebc66f21a8b0b04e1f2a870833a8830608bec97c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:12 +0000

    trap shine chat recovery whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
