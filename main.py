7e8065454224abb25bcc27ba2c99c14c943a2f73
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:50 +0000

    backwards classroom preparation force empty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_area_rectangle(l, w):
+        return l * w
