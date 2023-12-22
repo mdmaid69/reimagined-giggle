d6b0e624b3d6e9e30724d258dcc1edb091a14091
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:39 +0000

    either underneath price dead operate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
