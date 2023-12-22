2b8ee18d8d7f2f3665095d2d84d74e7f4dfa66d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:45:20 +0000

    its wear pick skirt practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
