a9ac9b4473ac28bb80c1eb8645ef2362a90b57dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:31 +0000

    precise top motion reserve attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_list(array):
-        return list(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
