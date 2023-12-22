4e1ac623e4170d06e6e1c44b2afb0ad22ffd91d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:17 +0000

    owe on small butter birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
