c854d0c73300cd6e759ca168d580a1da1fb1ae1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:40 +0000

    hate pronunciation thick should imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
