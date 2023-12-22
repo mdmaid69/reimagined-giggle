1317efc194a0b3f5bc3a5872cbb1db6847d64700
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:49 +0000

    hope for judge nor together

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
