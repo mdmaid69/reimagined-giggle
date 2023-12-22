50b9d81c439435610f62b678fcf29d609e2468c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:59 +0000

    cook friend blow comeround wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def is_even(n):
+        return n % 2 == 0
+def find_max(lst):
+        return max(lst)
