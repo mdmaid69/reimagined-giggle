8db36196352745cc49f17bcd7f8dd0bda549d943
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:15 +0000

    beneath purpose relief pretty ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_speed(distance, time):
+        return distance / time
