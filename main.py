5bdc5be420db69c08329c5b9e121fcde72284a32
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:50:30 +0000

    achievement steam offence test certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
