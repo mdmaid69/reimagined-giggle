5fee8e838960890739eaf612707ddbb1f24485b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:31 +0000

    dream present wind difference protection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+print(os.getcwd())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
