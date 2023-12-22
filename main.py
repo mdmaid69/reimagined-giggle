35292b70fee4f08bb529922f007ac8dfdc4bbc8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:19:31 +0000

    mistake pull choice lose plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  def is_odd(n):
+        return n % 2 != 0
