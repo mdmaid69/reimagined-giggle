a0e7073b63e818c3a869094913b380fc71c75398
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:49 +0000

    tone produce computer influence always

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
