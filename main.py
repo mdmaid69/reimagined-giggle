315558b25e3fe113175f00c84b4cfcc83cd2fbd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:31:21 +0000

    congratulate 572enter badly private star

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+print(sum(range(10)))
