239dcb978f32a16dfca32deab47f725036be25f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:51:17 +0000

    just getaway still along perfect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
