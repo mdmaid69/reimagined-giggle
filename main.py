5d400a92ebd286c703d4688169a134e5d59cc395
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:17:01 +0000

    pair punish manner minority pick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def convert_to_hex(n):
-        return hex(n)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
