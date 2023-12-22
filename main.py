5c83f87d15bc439c0a12e0df676bf2596c2d51c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:23:09 +0000

    careful rub fresh summer goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
