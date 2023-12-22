e39bfbbfe2c51eac4e6fdb73b1f279579731a002
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:00:17 +0000

    true turnon experience weekend bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-print("Hello, world!")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
