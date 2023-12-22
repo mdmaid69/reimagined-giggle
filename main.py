960b225df34035592a0c5b452e75fa7f3bda3b2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:03:07 +0000

    in sorry foot announce black

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
