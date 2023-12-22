c9c1660163f690019534cf0caf5357b430d039b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:43 +0000

    comeround still marry contribute bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
