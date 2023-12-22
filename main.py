de15f2c803c10b99bda03d74040952274cf9dfce
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:48 +0000

    take popular characteristic jump shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
