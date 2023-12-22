5511810080f8b716c61e8723fc128ba4159d432a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:43 +0000

    because delicate already exit stage

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def sort_numbers(numbers):
+        return sorted(numbers)
