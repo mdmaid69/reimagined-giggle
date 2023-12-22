859be80ed70e4e40058cdaaadb739a55122b940e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:30 +0000

    male pool switch table say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
