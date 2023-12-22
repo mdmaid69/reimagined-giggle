c808d16fc29d134b5c6271be64236ef4e37729d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:10 +0000

    getout hard beneath blue hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
