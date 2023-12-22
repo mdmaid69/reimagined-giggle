cdd76140d1bf2885bea0e8f875c26ee8c382e6c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:56 +0000

    cope work kill calmly listenfor

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
