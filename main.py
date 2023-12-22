7c24ec874455511fd209e9742bf91a158b26d690
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:45 +0000

    safe familiar noone stare perform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
