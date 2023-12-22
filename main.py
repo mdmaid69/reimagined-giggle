cd6bb2ba70829a23661977d0499dd575f4cbe2a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:26 +0000

    burn secret past edge clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import numpy as np
+print(np.array([1, 2, 3]))
