be913d3fb0d387e878f455aec069a5ff0b425fe1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:49 +0000

    care bring fortunately sticky impressed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
