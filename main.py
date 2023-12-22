b1270e575252cb5a29a1b689c3954e2a30dddda6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:10 +0000

    comein no present our summer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
