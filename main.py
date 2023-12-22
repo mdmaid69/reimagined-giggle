3ddc701e8ada5b8658e8d9e5eaeac771245f7892
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:14:49 +0000

    lead disagree pass goback(to) bound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
