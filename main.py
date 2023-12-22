67bef61e5fbacd329b357f60c6fb43a3c56dd174
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:37:26 +0000

    target activity party report difficulty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
