880a256d2b4379b23d231edb4c633f88df5f6acb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:07 +0000

    matter bush correct should approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
