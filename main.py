8bb559aa3d30217c702b08c999befb3400634f66
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:24 +0000

    attractive hate light treat shrink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
