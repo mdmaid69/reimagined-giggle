5ccef3a823a13b8a53a33fccb4a840e8416ecd17
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:22:22 +0000

    male politics attention bar knock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
