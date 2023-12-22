6eb52fb9598ad6b81a918f25ec7f0eff81a51f68
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:04 +0000

    someone pain behind course shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def sort_list(lst):
-        return sorted(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
