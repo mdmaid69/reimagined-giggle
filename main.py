5b41ae707534595b0b62e4881716f0bdbfed2512
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:23 +0000

    swing possession relationship sport brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
