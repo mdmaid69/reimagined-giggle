c47eb6fc7675289486155d1b33e94c112f28a7bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:28:33 +0000

    room whole heat comfortable Ll

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
