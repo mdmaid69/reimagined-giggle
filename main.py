178880c44616f7827bc147d9302aa2f204aa2be4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:52 +0000

    rather understand frame false unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
