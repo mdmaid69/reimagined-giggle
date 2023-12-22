a919cb8fe684193007916fbed71121b955a967f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:30:24 +0000

    amount deliver run language whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
