ceb2dced808df22bae145ce0249d47ca6c6a7c72
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:38 +0000

    stiff truth niggaacademy wealth 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
