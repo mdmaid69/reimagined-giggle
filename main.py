5891eede77759b22c68499f111a5199b64281a2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:45:34 +0000

    stamp strict temperature form person

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
