5d86d3aff9b5d65678ca7393c6f524211dac7698
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:04 +0000

    someone hospital break cause range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
