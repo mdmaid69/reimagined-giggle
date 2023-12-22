911efb980006b9e03a3b8fbc5e4ccb57608e09c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:09 +0000

    cross sister protest separate patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
