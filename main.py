aed4940f632538bdc90c864b218a7815057f83f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:09 +0000

    commit embarrassed steam steer agree

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+def print_python_version():
+        return sys.version
