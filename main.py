a3ca844abbf1e8f9a73bfbf156d7e78737a88dfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:44 +0000

    reason gentle firm chief quickly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
