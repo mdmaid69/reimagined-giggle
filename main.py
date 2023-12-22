efc2dca76a37dd820c5608f032543574e67618bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:46:54 +0000

    among avoid communication gentle careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
