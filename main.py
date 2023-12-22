f8e262f05b6ce07360c37b927315909eacc3b5d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:09:23 +0000

    bush onto advice blame prepare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def multiply_numbers(x, y):
-        return x * y
+  def convert_to_hex(n):
+        return hex(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
