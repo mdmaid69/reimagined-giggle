deb195dd728488e2c65266b27a1b0f7906e659ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:34 +0000

    it then during community moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
