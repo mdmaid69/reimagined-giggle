b45d6d5e3a8d4f5cd7a31ab52745020148be8c89
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:46:42 +0000

    bag used wave embarrassed mother

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
