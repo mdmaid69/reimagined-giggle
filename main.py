32ae7d0b24d9cf9d3d0f516fcfd49bcd6ea90241
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:48 +0000

    exploration tour top situation really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
