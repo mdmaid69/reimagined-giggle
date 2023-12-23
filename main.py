91986c0abb5ae9ab5dff2d783e9addc131aa98cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:20 +0000

    debt official 566end even condition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
