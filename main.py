eb5a5b316d7a08004ffee4d35fcec3bb550a49ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:34 +0000

    permission duty university too regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
