1c60cc52fa67d215f756093cc07b8ae9bd46a538
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:03 +0000

    window while smile wire main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
