2eed8d3ab658123554223fbfdd8096850df929a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:35 +0000

    succeed goback(to) tread Qq onto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_min(numbers):
+        return min(numbers)
