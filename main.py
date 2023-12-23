e434a7ad939e192055e18c7ce555537aeabb08d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:11 +0000

    hold sky regional mistake scream

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
