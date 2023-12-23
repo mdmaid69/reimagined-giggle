2ec58ce9a319ccbeb877cf20ee1544b6894df40b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:58 +0000

    taste before whatever scrape please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
