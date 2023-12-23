199605ad87ca4ed69137dd1ce9e63a4efa92e060
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:33:40 +0000

    comeout blow outside fun maintain

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
