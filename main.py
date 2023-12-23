a90bef8e664291ede8d7dadd4dd053400db319bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:42 +0000

    like store middle shake live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
