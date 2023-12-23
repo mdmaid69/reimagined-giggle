c2e7518a24000071e74ef3bdfa1634333b340fe2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:02:51 +0000

    man within still frequent doubt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
