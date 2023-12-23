9d36c64fc2ab9882d995c14b154a092e58607704
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:07 +0000

    mess suddenly case workon accidentally

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
