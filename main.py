ac091ec0c38c1236348b6f23c9e9c5a2c1b1dced
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:32:28 +0000

    temperature soil plenty border mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
