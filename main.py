f9b020793413b3e2fc4b904b57b8d6ee763b678b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:56 +0000

    cause interrupt face settle list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_list(array):
+        return list(array)
