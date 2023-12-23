7322a8b7c922e10e30db1033053881951d51f531
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:08 +0000

    sale sport juice bring return

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_area_rectangle(l, w):
+        return l * w
