60b832277f79bc672876144bf6cac4bc47f893b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:33 +0000

    shadow spill save undo hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
