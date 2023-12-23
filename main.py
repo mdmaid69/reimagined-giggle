fdc5c029ea89ee696ea39aaf0a17368cbd85648f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:59 +0000

    thankyou quiet available mainly lesson

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
