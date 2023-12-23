916f269e6a598904abafd3c5809aef85a30acdf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:22:11 +0000

    pair suckme organize each hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
