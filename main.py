2c9e716799667d98dfc1a3a6a0bea969a46372cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:47 +0000

    knowledge not sale speak clever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
