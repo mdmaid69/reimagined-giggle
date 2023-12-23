5b2f85b37547051267c0f30549718783e49648b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:09 +0000

    emergency squash makeup in tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
