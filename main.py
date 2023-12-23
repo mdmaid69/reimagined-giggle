1f58636e9c0f9546500585449fd48a7d50e23888
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:34 +0000

    suddenly sticky position blame everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-print(os.getcwd())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
