08723c39fbb7d298fcdd8ceb7a9e56024b1a2c56
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:07:53 +0000

    sink far whichever left throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
