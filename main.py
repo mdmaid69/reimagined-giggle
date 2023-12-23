9ced6d37b49724690acd62c994d3840f5c66adf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:41 +0000

    tradition upset slowly fast record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_speed(distance, time):
+        return distance / time
