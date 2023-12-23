db1a209d1abb1a7a830d09ddeed3538c3ca6b0ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:21:02 +0000

    crowd high only black hope

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def is_even(n):
+        return n % 2 == 0
+import os
+print(os.getcwd())
