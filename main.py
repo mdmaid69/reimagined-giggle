9f1c0f58530da1fad34f3a93ca5064c0d1471edf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:22 +0000

    bin your introduce television relation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
