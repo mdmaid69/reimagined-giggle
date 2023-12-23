d772a3ac6a193a3c8c68e53126b415c73983f60c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:43 +0000

    season try even turndown beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
