110e2d86e1caba69b968963dae63ebca0a643b71
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:11 +0000

    right feeling week next desperate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_to_octal(n):
+        return oct(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
