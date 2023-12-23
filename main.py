9633eae885034e5e3e8fbf69379c73f463026fcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:02 +0000

    grade your charge discussion quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
