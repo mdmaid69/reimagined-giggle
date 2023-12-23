6a149d767f44ce24db18452ae046b3344aec7294
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:54 +0000

    tall thinkthrough finally send earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
