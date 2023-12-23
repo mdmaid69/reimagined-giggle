e51ca382b3f31e3ef892a57c140ee8ac8c6444bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:19:00 +0000

    time display politics callout scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
