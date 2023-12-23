08cc3713ba5372486e41f7c25a17e9bb29bee7ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:10 +0000

    now steer collection pretend work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
