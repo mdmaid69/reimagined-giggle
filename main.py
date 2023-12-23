ba3c3076db1be3ef3ea5c1114976901903c11451
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:08 +0000

    culture study people fat education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
