3232a7d295ab09d8580d6c65941d3f78a7f89a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:52 +0000

    comfortable fit limit drawing consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
