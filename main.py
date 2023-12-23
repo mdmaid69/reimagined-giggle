a08401d5765b096f207d2a3803918619e6477b31
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:57 +0000

    shoulder dress brain model bang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
