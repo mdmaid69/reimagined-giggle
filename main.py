49cb95fb9b4e0e65cad603ea123872a7f5c3f854
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:50 +0000

    state getaway possession doctor judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
