8c42191496c5fbd44e58ba7b5eb78f989214a56d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:07 +0000

    smoke beside modern discuss record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
