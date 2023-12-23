4d02bc279e5c41a78b46bc1c7ffdcef48e41516c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:37 +0000

    season estimate collection dream Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
