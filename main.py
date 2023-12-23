a28b8efb315dd026417987698ec004d0ea1b88c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:44 +0000

    last score difference cope turnup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_acceleration(speed, time):
+        return speed / time
