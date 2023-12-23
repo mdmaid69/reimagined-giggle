1e0d90ccd8fc9d9b72b334e9a5af82163c21e433
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:29 +0000

    congratulations majority as hungry sensitive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
