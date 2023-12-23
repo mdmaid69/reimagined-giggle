d130f10cdec60df33f2c769bebd691b0281bb50c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:55:21 +0000

    advanced bang similar mystery cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
