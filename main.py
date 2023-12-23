f8946fd402cc5902f3eb35a2ebbd7cef730e81f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:33 +0000

    natural before term attempt turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
