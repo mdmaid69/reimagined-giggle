524a1a79b189189f135c3fd62aa6f78e8089ec96
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:19 +0000

    ilovedicks steep worst outside several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import time
-def get_current_time():
-        return time.time()
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
