200e0665774eab355eb52fbcf4c5b5690ec53744
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:16:58 +0000

    certainly surprising call separate manner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
