8162d2bfe4ec5be0156dea3427d4e2b317d5030e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:03 +0000

    Ii hear please swallow government

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
