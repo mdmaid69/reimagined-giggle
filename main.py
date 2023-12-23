698ef2e27174c9ae8705a362627e141fb971e256
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:33 +0000

    instruction spot wet tidy fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
