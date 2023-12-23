f9f36d2ac77d262a3d56d84f02dd3dec9f1a9d59
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:34 +0000

    available sing safety fun appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_string_from_array(array):
+        return array.tobytes()
