39a878edfd06e7fb946b8d3ae5e9429af1dca7b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:38 +0000

    open interfere ring branch each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
