25ee6fc9572e45a03b609e69c63e2c19985cc441
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:21 +0000

    ought instant first upwards music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
