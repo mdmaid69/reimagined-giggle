cab4503dd51798bd1694bfa2c26e608dd613069e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:03 +0000

    similar goon lock lesson completely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_length(array):
+        return len(array)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
