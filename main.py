37ce80c1d1509aa149c99916799ad4b6139ecbd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:46 +0000

    laugh win drop pure peaceful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
