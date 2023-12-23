5818dc2a7ff5502f776bdb92a11cb924e07e9971
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:09 +0000

    estimate name whistle fall level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
