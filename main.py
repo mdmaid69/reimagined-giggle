176aa20fb1f44cd0a5a0296853acadb86a6d2b90
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:03:34 +0000

    destroy under bridge fix shop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def reverse_list(lst):
+        return lst[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
