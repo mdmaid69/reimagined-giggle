5be5c1f3b14d2145fb1e2e5141fe029af36ad827
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:59 +0000

    bin control child person attitude

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import platform
+def get_os_info():
+        return platform.uname()
