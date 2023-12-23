c2c27ae3203596dd044adb3e7ee2af93361a2650
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:55:51 +0000

    strange lucky Hh them friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
