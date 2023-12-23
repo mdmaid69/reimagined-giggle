5fb8f7bf4692432001e7b1bc47695656ff3bbe76
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:49 +0000

    exhibition lot yours post another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
