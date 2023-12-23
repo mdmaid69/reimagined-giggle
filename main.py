a924c89c4db4c6b1aea529e69cd1318bffa4801f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:13 +0000

    shake section else workupto wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
