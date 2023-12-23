e13ff17143955c4b5526831aad6351640fa1004f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:30:57 +0000

    legal mess ordinary confident die

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
