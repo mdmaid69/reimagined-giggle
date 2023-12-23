e1c50aa707c09beba9bef95808804de3ee075001
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:59:08 +0000

    gap score drink male attempt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
