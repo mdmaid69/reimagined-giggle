d7fd4aaf15405b68dac78fc40655a4db5835cedb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:13 +0000

    what spot break discuss frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def rename_file(old_name, new_name):
         os.rename(old_name, new_name)
