7dce276fa108e3d37ccf92709a8bfc8c537bcb08
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:11 +0000

    zero relation doubt obtain wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
