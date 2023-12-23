26325e0050e84a3b81539cb8112849142bcfa6e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:36:08 +0000

    strike pleasant just strike talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
