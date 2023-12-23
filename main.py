fe66cb63b2af99e5b496e1f85e9e060ee37e32d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:09 +0000

    side step firm immediate middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_set(array):
+        return set(array)
