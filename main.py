ed692a13406a3c8e8020f884150500cffd8cbb62
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:47 +0000

    discussion catch worst careful miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
