d9ec77213c70505a17ba2c0c7786aecaaa2e7327
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:38:53 +0000

    line expression physical all behave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
