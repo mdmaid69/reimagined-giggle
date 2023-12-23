7bad39649f957fb8aa247abc420f75fecc0b4e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:28:55 +0000

    smile otherwise impossible miss instant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-for i in range(5):
-        print(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
