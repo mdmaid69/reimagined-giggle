be3c8163caf25a477e7afed69ebceb5adf02a834
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:40 +0000

    speak win try answer dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
