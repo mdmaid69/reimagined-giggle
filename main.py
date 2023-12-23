ecef2d258538ba8fc4ada0d2dc898c2849237378
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:02 +0000

    police support most yours coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
