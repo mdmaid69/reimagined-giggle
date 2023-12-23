47e15545fb62da79f88668ecee138b72c7577939
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:08:18 +0000

    not impression concerned strength myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
