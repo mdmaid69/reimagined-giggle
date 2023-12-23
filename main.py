6ad378ed153355a7bf45fe4ead92a1f3086427c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:47:06 +0000

    this hot leave sorry ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
