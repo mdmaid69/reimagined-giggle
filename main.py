4d03e2311561f4f40b29403bcfc7e15fbe8f1d3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:22 +0000

    approval godown much laugh temper

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+from collections import Counter
+print(Counter("hello world"))
