d3dc7495063146eaf4389480e6daecb4f3c8a170
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:44 +0000

    thinkover attractive by range gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
