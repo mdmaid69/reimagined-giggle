b5a3ac6254507fb34e4e6e6cdea0f6dac35925b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:17:25 +0000

    sure them move fork fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
