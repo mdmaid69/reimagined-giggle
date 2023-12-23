66d2f264793b6d948f9d57d18ca58bea94527d07
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:52 +0000

    short permission unless expression base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
