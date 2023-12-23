83ff1464f7ed3c18e1b49d8346efae546b9a4b50
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:15 +0000

    temperature impressive let noise once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
