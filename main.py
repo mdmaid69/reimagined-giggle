33a0313f06302e1ef4b0366cd7e58a54b72bf94e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:00 +0000

    computer closed mystery slice cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
