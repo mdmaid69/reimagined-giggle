8e4a7826a46a38820e97db816b6ec6e96415f34a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:13:06 +0000

    colour comfortable operate project temperature

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
