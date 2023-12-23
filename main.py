2a1ae351e26f6095450ffa9e83d37e1165c7321f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:09 +0000

    to 565end like together beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
