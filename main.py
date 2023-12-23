56a68deb85cfc5134cc05f5ac8f32808ad3c77b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:52:48 +0000

    keep theirs stone interest impressive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import time
+print(time.time())
