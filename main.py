6c10cc8305d2c42c0fb128b1a6491952a88dd1a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:39:30 +0000

    anxious bag cry learn start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
