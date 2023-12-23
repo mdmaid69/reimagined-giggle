6f1e2e85b9e37399d2f9ced8249df08d40b0d19e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:57 +0000

    treatment reaction swim during shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
