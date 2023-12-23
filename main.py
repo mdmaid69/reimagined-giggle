23ca0c898518d53e7624b96e4bcf66b4eff561f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:13 +0000

    they nonsense praise disappear game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
