ddf00dae1cb0ff5c7d3459ec93767e7f90c19863
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:19 +0000

    breakin regard issue also congratulations

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
