71f2c931774c5008901482720ba631912f871b49
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:47:35 +0000

    curious to breakthrough dry will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
