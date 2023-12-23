f3f21813d5e97992edab30a353ee7e6e24cb5df7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:55 +0000

    plant translate provide hold bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
