0c1e2346e1600e996ec0a7837af1d52a58311c40
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:32:11 +0000

    post extra ready group exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+i = 0
+while i < 5:
+        print(i)
+        i += 1
