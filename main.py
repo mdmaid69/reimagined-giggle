5fa9b577b1d8816e9d04a28921e251458a805ba3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:30 +0000

    fast date rule promise tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+print(os.getcwd())
+import array
+def get_array_length(array):
+        return len(array)
