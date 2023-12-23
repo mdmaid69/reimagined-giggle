4f4f60356f01fe14ea210ba70d53e5fc20f9abb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:26 +0000

    stretch prevent yesterday constantly up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
