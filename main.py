4e3790191fc4bf47bb3714cfd3ab4aa330bb05ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:32:51 +0000

    concerning Hh ocean strong warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
