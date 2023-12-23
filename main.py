6bace3692bdbe70128bec280d2b04a4065c6d685
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:01:37 +0000

    interview cap on detail union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
