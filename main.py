e48eaf47a3c0176e6a49f9d41813a0c8de6793b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:13 +0000

    pause middle change under sight

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def reverse_string(s):
+        return s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
