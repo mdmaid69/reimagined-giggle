60effe604b217623497a22afc54a479f895d11c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:30 +0000

    address trap possibly uncomfortable empty

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
-def calculate_average(lst):
-        return sum(lst) / len(lst)
