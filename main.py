cca5433fc1b21eb40e9e1229c41b026a68e3254a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:30 +0000

    wall plain practical excuse result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
