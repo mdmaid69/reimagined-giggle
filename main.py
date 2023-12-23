0372ac021a79ab5ed4fe5038ba301ff24b522e2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:54 +0000

    goback(to) crowded loan memory sensible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
