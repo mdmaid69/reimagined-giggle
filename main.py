7c9e04d86940400f525956183633381378e6d966
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:15 +0000

    pretty since company rich total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_volume(length, width, height):
+        return length * width * height
