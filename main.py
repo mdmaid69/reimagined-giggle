db77f35b5f7a56f9d95ea670e8b7c87b1f389622
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:39 +0000

    accidental spill rob pin comeback

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
