7de8d9999bcb26afd30f0555092336feb6b367cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:53 +0000

    need interest fall statement pleased

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
