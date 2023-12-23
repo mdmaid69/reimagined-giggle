2505c3c13c681fd758707469ba21759edfb0f099
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:07 +0000

    anyone straight imagination fit send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
