de96a9e3ec25670b99d1bf5090e461db2caa573a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:05 +0000

    suggest shop suit impatient finally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
