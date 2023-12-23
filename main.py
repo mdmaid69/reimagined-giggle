088e0862403c739d5cac31bd1846cfc786149965
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:23 +0000

    corner Dd length intend train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
