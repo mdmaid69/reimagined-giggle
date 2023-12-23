7b26c3e01f5e9d43006afe76b3e9b5275905ab7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:45:29 +0000

    damage favourite post notice Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
