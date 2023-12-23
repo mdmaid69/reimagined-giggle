cf8fd0ac7bdb3397275c76a3c23b20b59580a996
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:40:34 +0000

    operate blind pleased train meeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def divide_numbers(x, y):
+        return x / y
