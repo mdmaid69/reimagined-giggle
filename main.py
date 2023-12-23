298642e046e81fcf785784d21dad44024a033e0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:30 +0000

    advice calm send deliberate popular

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
