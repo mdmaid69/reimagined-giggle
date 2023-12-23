2e1356a51695f6b446d728b28168013c30bad25a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:44 +0000

    modern really giveup smile join

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
