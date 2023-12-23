d44c3b5ec661c90b5acb66ce3fccc192dbe7f85e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:34:20 +0000

    motion we unfair law unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
