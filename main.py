0273ff4b8239a8a1b8ceb21f2ad7fb294aeaddf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:10 +0000

    extra shake total fall slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
