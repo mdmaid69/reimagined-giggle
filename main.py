9a62a100267885c199d21124a28c3ccc38b48319
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:07 +0000

    own best goon offer everybody

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def convert_to_octal(n):
+        return oct(n)
