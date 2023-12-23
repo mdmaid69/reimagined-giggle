3ae5ecda54b44871c546b5b858116f6462f255e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:17 +0000

    force possible lesson bottom advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
