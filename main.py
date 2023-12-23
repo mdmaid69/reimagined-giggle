ab758911d33b87b0890c614734b10efa80d51fa7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:09 +0000

    right wrong like round mix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
