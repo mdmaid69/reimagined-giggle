c32bf7475bf9b994f69a36329662f55c47347179
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:17:45 +0000

    dark burst priority Cc popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def convert_to_hex(n):
+        return hex(n)
