54e28536f3eff8ac89fe2c54931374224ef33532
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:05 +0000

    comeoff secret hello mess regularly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
