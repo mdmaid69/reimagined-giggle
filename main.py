e1b923196ceb03741d89c732703d2e409ce1f501
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:02:51 +0000

    law sell bother invite cover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_speed(distance, time):
+        return distance / time
