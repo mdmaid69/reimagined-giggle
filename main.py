6e716c8efd277e3ef1d5f88f643144ed5a370de8
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:15 +0000

    bit morning skirt building thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perpetuity(payment, rate):
+        return payment / rate
