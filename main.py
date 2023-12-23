1f48af8155d379ec7e4728955fa350d75838ce2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:20 +0000

    still stamp could form collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
