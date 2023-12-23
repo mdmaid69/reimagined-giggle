7208c679ac560d02ea0aa43bce829d21a860ee7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:52:34 +0000

    common heart Ss cope big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
