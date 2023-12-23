c345dfbc99f5b14cfced2ba8ed644e819d54b207
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:12 +0000

    help surface tonight operation path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
