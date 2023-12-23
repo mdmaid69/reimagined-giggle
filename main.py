ec5a4e4835db4f0b637c5806ba38507f4ae4ae09
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:01 +0000

    label hit receive raw steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
