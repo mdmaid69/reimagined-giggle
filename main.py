7e1bb1e2a64b7689ba05f8047bdc5028ea588631
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:25:13 +0000

    telephone to today language reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
