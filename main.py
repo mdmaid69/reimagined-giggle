e19302c94ae3e2150e4dd553c69df1c26defb6d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:57 +0000

    arrive black alive today confusing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
