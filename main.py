4c8fd0666858fedffefb8a875c22ed6a6a44ce6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:39 +0000

    although escape unhappy mystery tooth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
