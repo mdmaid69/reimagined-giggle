ecca57bb4ef5da502a49c3742e40b31fda15923e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:18:42 +0000

    other upstairs course desperate somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
