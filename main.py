376cf57e4930b83a930a0291bac5090d9cead35d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:50:10 +0000

    correct cost possibility brief wear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
