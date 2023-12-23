058f42d017755facc7049701e89246764102874f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:48 +0000

    emphasize minor group hair related

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import os
+print(os.getcwd())
