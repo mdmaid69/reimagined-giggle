447d28dbe7c9af970c53d0d2f01f379fc3f14725
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:19:07 +0000

    ilovedicks custom benefit justice ability

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def count_words(sentence):
+        return len(sentence.split())
