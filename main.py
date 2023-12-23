d7029b6d7f304ae42b7b82f75f9ca7cc748b5b09
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:43:48 +0000

    impressed print black onto possibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
