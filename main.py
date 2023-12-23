19508034e61307fc3bdc88bfc5f6c47b7ccf978a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:42 +0000

    bet gift feed position grade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
