9a4384d3aa199e196e5c7bb35a7ac941fe85b755
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:45 +0000

    angry movement concerning admit comeround

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_current_directory():
-        return os.getcwd()
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
