cced87d59bf8c802d225b56cf3acbb93cf3aea04
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:29 +0000

    avoid make neck design charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
