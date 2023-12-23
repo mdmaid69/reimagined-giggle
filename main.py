94226fa6b4581f273ac1d27110183c12a7b0a02e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:14 +0000

    desperate question continue introduce impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
