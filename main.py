3f76cabe54b281774a8fa9da8556c43559280f9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:46 +0000

    care notice bear direction joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
