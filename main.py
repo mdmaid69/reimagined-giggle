aa8f14ae9822e8ce71abf64d7a9037364e90b770
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:34 +0000

    proud either cross frightened pin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
