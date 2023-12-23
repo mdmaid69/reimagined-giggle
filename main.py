8cb175624f45ca6d577f35310fd7bc21c8fce477
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:35 +0000

    exchange house flavour patient combine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
