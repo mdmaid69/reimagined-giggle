0f12ff3367404bec63ecc1090eb399c683850b42
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:16 +0000

    private illness worth bother sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
