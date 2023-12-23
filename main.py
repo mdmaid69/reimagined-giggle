1f3f2d3746aca1096ae7e18be8db5a642c76a6d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:08 +0000

    see catch deserve pretty chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
