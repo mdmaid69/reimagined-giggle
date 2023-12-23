39676971e69ac381693d2bd663cd99789e1829e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:54 +0000

    reason bet provide breakinto ball

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
