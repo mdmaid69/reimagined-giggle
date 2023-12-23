c9e3071e3f1efd1cf1aeebeec97a1a6d7db44152
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:43 +0000

    pity objection easy near concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
