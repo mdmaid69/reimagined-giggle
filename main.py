86d0d7069061455326170e4495ed530f993f4fc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:01 +0000

    moral violence happy kind geton

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def find_min(lst):
+        return min(lst)
