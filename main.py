4b7c928334861c0486cb2309dd2b4609f4cf87ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:16 +0000

    year soon rest tune bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
