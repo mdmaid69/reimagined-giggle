18130d4b9d04d9ea50ff21792bb6e344f20abee7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:26:59 +0000

    rescue disappear possible along separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
