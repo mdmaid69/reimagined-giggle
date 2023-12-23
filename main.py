ab693f489de8dc0b63f594ede35982fae6c75ece
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:27 +0000

    Oo this step pretty wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
