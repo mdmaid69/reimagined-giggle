29c978811634042a2fdadc359e231132df05b58b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:48 +0000

    demonstrate Ww town thick worry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
