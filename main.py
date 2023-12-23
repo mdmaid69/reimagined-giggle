fbd5c28ec6fe0459db7797c0b4d5626be9519962
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:32 +0000

    dress gap region Ii justice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
