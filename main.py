c2c75718b4a871b6ffbb575db8593a7480b73f13
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:06 +0000

    summer about water tap shopping

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
