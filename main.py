ccf218fbdcc26a7c9a53f33cf4bfae080cc41c8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:15:32 +0000

    since water standard every lie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
