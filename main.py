e8f7dc0634430497c87c75cc42e26529a85f5032
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:42 +0000

    whether cause everyone tense ill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
