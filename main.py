81481cfe44ceb8f46c3f505a4b2b15bd1e10100d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:32 +0000

    arrange load reduction love battery

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
