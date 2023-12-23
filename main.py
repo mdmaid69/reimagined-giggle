091d39c3c120bd253578a71723db8b9ea270e40d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:56 +0000

    left still hide frame so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
