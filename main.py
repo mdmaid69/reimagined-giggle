c29a557c374d664eadb6df2a6026070a8190a737
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:13 +0000

    past short prevent I expensive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-x = 10
-y = 20
-print("Sum:", x + y)
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
