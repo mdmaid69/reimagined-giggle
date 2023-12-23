76c04dab65ca974e35f385f0c048cc78498b5bad
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:29 +0000

    there colour payment comein should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
