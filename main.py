17f3e293315dfbb00fd9d86e1b3a1fd117e90d14
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:10 +0000

    country safety spoil unable pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
