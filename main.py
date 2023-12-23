e9a060d246cfdf82815c063ac1edb0414f04d6f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:27:15 +0000

    complain exit raw will bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
