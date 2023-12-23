6edbd435fe82a6c474532939777cbed6b58b5979
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:29 +0000

    appeal increase offend throw Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
