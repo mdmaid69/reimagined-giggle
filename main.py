456bf44b8a4cff137210a70c67319030626e99ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:24 +0000

    frighten study intend ability whichever

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
