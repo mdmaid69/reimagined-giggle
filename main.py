1aec5c6e332349cd9868f83aaf8f188aeeb2f437
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:21 +0000

    recommend train lunch hungry combine

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
