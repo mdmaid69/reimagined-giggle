1f3ae95fc3b4fba71b9f50d16807838c99c5de95
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:29:56 +0000

    damp advice maybe scared spite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
