de153a565cd4e8739a6174bdaa8e601ba8650468
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:54 +0000

    remain Itself ashamed care divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
