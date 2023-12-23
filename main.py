d4262657d56794e083d8e7fb60b618feca0347d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:44:47 +0000

    leg least keepoff surprising certainly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
