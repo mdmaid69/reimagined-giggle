378180159481906aed27e1532c707783f3eaa724
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:58 +0000

    comeover excuse backwards police place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def create_priority_queue():
+        return collections.deque()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
