a63b8fc9162a6e92a7ac1995b2169625ee665a7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:51 +0000

    genuine smile impatient thing mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
