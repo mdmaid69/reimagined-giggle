dbb0c978a82b2a6df0fb0323e9466bc79db23765
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:43 +0000

    fortunately message creature trouble one

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
