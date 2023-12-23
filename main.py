eebbb47dc46ed645576f59082d94847d813b562b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:10:51 +0000

    comefrom balance save narrow finally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def divide_numbers(x, y):
+        return x / y
