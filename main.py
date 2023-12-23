5d8a034d4a0fa43d95cb76e5ee03ff71b7639f78
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:49:40 +0000

    absolutely pay along start day

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
