ac5f607f079b49e54bedad005e31c1099a675393
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:30:40 +0000

    plan face so reduction post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
