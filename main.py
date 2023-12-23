f1f21fa80f0633035ff4fc25c459a991c353b2ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:34 +0000

    squeeze twist cook performance thanks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
