10aff01888f8df58bb28f23b0a3bde7a823ba734
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:48 +0000

    intelligent anyone pick no comfortable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
