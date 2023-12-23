74d0b2bbfda48a48ca06adaddda2bb3e7e8c90f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:32:11 +0000

    process survive sweet callup bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import time
+print(time.time())
