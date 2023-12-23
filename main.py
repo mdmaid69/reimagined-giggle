9dcb76d30672883542b4043c58c47b67fd9d4245
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:15 +0000

    rely somehow plate week second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
