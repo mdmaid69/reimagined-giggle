9c779cf45223e7d72681dcb1c3fb78d4db9a3621
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:30:59 +0000

    unfair complain hit whisper tour

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
