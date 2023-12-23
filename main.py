610784cc3d5c2789a1f78b890105f76e5cb44e98
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:16 +0000

    ought always compare mind official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
