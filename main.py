36382ed2d9620f2221b15a0664827da758faec9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:51 +0000

    work confused hit year trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
