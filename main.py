c85601375a6911182f6527fc9e6260c5d9c38bb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:35 +0000

    turnoff different everybody spare exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
