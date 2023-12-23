40d95122c534ba46fb47170fdc0f871e318c8e40
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:40 +0000

    especially bringup advise love indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
