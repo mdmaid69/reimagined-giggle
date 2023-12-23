5822bdda50154d897c57edb52420462815b99ec3
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:54 +0000

    persuade contain kill read blow

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
