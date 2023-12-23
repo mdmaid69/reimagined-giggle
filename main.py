2317c4a65a0ce731871f38953629ace7d54dd6aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:05 +0000

    wide practise prepare gooff actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
