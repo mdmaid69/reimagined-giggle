378b29bc893481e2519af5541614c1da701da41e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:09 +0000

    suck extra anywayanyhow favour poor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-print(random.randint(0, 100))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_priority_queue():
+        return collections.deque()
