0daff8719fb781c9701873747d3de6553f261616
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:05 +0000

    breakthrough busy message Nn sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
