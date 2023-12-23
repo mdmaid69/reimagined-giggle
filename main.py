2aae45e9006bf00bad11aae1ba93b8241f84cf03
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:04 +0000

    summer unite discovery home some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
