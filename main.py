b5146f0b21002606e519d1947282368e048ec268
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:32:13 +0000

    practise float only pencil mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
