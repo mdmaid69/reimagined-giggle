f1879f2e6fede60ec81375cb41d6170f741bc05a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:15 +0000

    effect curious stone bringdown quarter

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
