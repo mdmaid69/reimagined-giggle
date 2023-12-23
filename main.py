d3e979a9b7c729559ecb24883abf0317639b1c9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:39 +0000

    cruelty visit provide obtain either

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
