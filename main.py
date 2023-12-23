6788a267be2afb2cb1c585da43f0b34c6422937c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:58 +0000

    interested form fork include interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def count_elements(lst):
+        return len(lst)
