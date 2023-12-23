acb947fa9e5e572fd05639277436ac703a1d02a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:30 +0000

    read strong flavour marry future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
