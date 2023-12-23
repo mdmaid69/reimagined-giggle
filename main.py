b44d3915e42b575bdbb422c71c31bcf0bf0a307d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:31 +0000

    jump yourself sky absolutely complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
