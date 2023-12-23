4e83f2ba2318ac89d1ce581dda5f83db6f4508c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:53 +0000

    keepout slowly sure pride wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_str(array):
+        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
