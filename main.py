f32139e9327052b4d75945b43fb86720b4fa24ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:36 +0000

    total come memory note goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
