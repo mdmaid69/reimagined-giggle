d91f96ae113f4d940d4633b27658864dc7c8dce3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:24 +0000

    true show comeoff must Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
