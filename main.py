6cd7232fc8b231c9204dfc0baa25687f34a2d9ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:42 +0000

    curious real fierce interruption worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
