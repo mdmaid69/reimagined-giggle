83d0167d8c27c033d1ddc0b5fc06dd243df851f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:18 +0000

    empty plenty part earn callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
