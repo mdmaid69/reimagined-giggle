cad90482081bc0ae83714d89ffb77469936d2fbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:43 +0000

    plenty connected organize hot break

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
