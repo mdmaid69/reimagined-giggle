1053c464356fa6ef42b3e27c0559d131a4e72527
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:17 +0000

    air walk nonsense importance ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def clear_array(array):
-        array *= 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
