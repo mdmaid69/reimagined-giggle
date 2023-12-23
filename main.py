fa3a846de378401114c4bb789a2f64fd9eb31286
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:10 +0000

    quality gradually pretend schedule runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
