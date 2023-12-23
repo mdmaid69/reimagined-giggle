b6ac1fbae8124baa6e58459ab1863e25503f291e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:50 +0000

    escape getout innocent Cc guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
