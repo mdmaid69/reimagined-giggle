33a5615e70635d434a18c4a03a3d6b1134edf9ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:03:26 +0000

    strict production level have on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
