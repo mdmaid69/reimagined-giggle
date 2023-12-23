b78af9c654c26ec7ba6f87ca23b535a09a255368
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:54:53 +0000

    still attempt same here buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def print_python_version():
-        return sys.version
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
