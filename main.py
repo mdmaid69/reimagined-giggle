7c6ea33ed1d7c2b5344892b92a9cae697cc98185
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:06:56 +0000

    operate carry ill office basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
