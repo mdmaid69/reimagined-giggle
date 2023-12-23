6018721c63b49f28c8551e19ba30b1b413d95012
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:15 +0000

    emphasis which business only provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
