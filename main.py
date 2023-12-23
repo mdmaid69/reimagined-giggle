1f4d4c8302305a1b71a44d5bd58f197001494303
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:11:19 +0000

    persuade sore over discussion level

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_pressure(force, area):
+        return force / area
