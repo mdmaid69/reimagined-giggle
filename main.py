033ffd0e40ffe6bed55a9927335ac19c7e6326a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:00 +0000

    wake sister activity hurry repair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
