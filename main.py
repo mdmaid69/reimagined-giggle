c464e1c65aeabfd2e8ab80d85b2d0ebea1a2e873
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:57:06 +0000

    567enemy official system condition slight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
