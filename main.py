4f5fe32bb57ec2cd3447e045f776a76d25339cdb
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:44:02 +0000

    challenge beneath sweep basic damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
