2084691b438b99ad503f7a201b3a26d035541190
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:06 +0000

    kick short drive club best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
