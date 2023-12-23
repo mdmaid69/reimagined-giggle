7102b08ec5b7b5950c5e48f0d34de5ce136e2c91
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:57:19 +0000

    back callout vote year attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
