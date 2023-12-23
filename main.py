69df593a577c6fd95574b9b01aa5beb86ca5672a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:15 +0000

    front appeal admit custom rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
