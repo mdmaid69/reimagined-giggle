f542717ea0b3d3c328a1c3b14ce2b1ec97edc757
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:15 +0000

    public rob compare measure manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_max(numbers):
+        return max(numbers)
