2ee46ca9c2bd1684b373bac9bbe9063e4f913cd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:03:12 +0000

    motion run always swing get

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
