7a05bc26c37f32f8b3fa38fcc7b8273df8ce6167
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:02 +0000

    breakfast notice message twist marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
