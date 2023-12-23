40120bbc3916dfa8061dab0cf49ed04fee077434
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:31 +0000

    snow disgusting organize impossible tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_array_to_list(array):
+        return array.tolist()
+import time
+def get_current_time():
+        return time.ctime()
