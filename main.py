3de0358efee19c5819985ccdd0d30b917789783e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:27 +0000

    steam soak tough achievement within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
