47e3137c91e4751aadc451636ded97a390329eb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:44 +0000

    steam lead measure tense avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
