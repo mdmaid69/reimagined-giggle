5a7a5f7a0805c9c941440d76d6bd1b1d0a56872b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:05 +0000

    keepaway excellent your front gooff

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_area_rectangle(l, w):
+        return l * w
