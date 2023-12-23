a1c0954ae5708cc93c2d5f122adcb31497f4cad0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:21 +0000

    thick print rain next catch

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-for i in range(10): print(i)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
