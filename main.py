924b1b58902f5b5e654f0e9eae13401d42d53205
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:46 +0000

    claim interruption circumstance consist boil

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def multiply_numbers(x, y):
+        return x * y
