d43acb4924074dd0205f46a93451f0b89956c594
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:55 +0000

    control fold complain taste rich

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
