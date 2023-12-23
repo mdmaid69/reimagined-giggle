fb30e7700bae7b75f6a4dd2b5c9d45eeca1cd45e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:51 +0000

    odd trip cheat issue half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
