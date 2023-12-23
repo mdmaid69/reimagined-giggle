9db1be9682eef98a16661a1eb74b87ba6f673dc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:52:31 +0000

    clothes hole arm model please

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
