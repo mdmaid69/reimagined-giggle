0849635f451b78f75511068f9c616598cc7d7025
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:46 +0000

    paper such charge something believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
