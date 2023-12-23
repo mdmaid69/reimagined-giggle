9ffff00472c9d1b472dcb7bc48695cfee63df5f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:49:53 +0000

    comeoff desperate spray week ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
