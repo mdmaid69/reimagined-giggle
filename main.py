009c8cd75a65aa806197fbe71ae4189570773bd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:55 +0000

    everybody catch nigga runover area

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
