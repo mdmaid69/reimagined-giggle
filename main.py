b1f38adf3e62b5f20b3c1434e55cba9122f1db9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:12:51 +0000

    offence swallow shall wealth terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
