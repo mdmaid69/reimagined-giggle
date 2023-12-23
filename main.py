4051b6e83ddb36fee26375bf56dceb8ec1ac5801
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:19 +0000

    relax neither avoid crowd language

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_volume(length, width, height):
+        return length * width * height
