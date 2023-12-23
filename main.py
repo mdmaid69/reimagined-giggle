98c69acf4a2f09f1fbbd81c91f63cfa0da1afb5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:24 +0000

    crash Zz swimming disgusting size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
