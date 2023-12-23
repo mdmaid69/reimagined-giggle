5f4bcce1a43c119a429c6ab8069599b06d1b78fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:15 +0000

    carry fit rent price right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+print([x**2 for x in range(10)])
