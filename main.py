b78254412e93b113dedf0d637951ee4cb625cde7
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:45:47 +0000

    head time recently however club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
