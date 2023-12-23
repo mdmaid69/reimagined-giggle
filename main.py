c8c11f998f0e7af9637ab514111ec83ae1b4faf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:27 +0000

    law hurt loud peace anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
