88393d5c470e40f0aa80353e3c233d6f604430fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:46 +0000

    dare bringforward so comefrom roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
