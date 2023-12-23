f799afa40f0d3d2ec52b80b732afc425e23ab58f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:12 +0000

    gather famous circumstance vanish stop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
