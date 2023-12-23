c2bd1cd592569c45bd6dffb9176ae7ba53bd634f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:42 +0000

    shoot uncomfortable birthday skirt treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
