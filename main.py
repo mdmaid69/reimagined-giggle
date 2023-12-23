1fa5cfe9a6305beece95364d080c56dce726eca7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:22 +0000

    window gooff judge stir argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
