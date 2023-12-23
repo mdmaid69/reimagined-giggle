e573f47f37ff33ee4262ea5a544d11bbab762718
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:07 +0000

    need keen welcome impossible equal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def convert_to_hex(n):
-        return hex(n)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
