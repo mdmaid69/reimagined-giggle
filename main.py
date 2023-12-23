a7400eb376084795d4babe13cc481747ee223cde
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:55:23 +0000

    boil official pay spare anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
