8a944667e582bb787f2053c6450829ad219d3a26
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:52 +0000

    memory away hole admit near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_dict():
+        return collections.UserDict()
