e3a5e8058dbf4618a089b08ca794b973b361c33d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:19:00 +0000

    colour necessary if police influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
