792ebd198d83b32b61a3e03497fcf84c4d650f4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:37:39 +0000

    exact prefer swim unless use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
