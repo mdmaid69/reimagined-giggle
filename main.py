3579511bb754a4b06f39ae9a19e788e70e85c08d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:13:23 +0000

    whistle either vision invitation basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
