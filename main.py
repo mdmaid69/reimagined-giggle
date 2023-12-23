01f26918931acd0bd126f1a0bd98b4b9ad711253
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:47 +0000

    wind actually better belong experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
