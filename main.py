3d8d454be46d6b6db969fc4a07d36c21d6a52ad7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:09 +0000

    emergency complicated underneath reduce fast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import numpy as np
+print(np.array([1, 2, 3]))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
