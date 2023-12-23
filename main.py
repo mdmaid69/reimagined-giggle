d6035df4012860615f28ada1692a80b039918c68
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:40 +0000

    trip holdup more term judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
