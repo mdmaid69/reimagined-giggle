3a8ea9d92d302423262be12fade46f6b0dd3667e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:15:25 +0000

    shoot into fall comeback meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
