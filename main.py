2b337753aaa0d463810f791c1dbcac74ef996f09
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:19 +0000

    threaten shallow niggaacademy track practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
