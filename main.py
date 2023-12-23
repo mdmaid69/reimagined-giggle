927c9259361594bd73654ec0214f850570d3e8ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:54:03 +0000

    frighten town with ought dull

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-print(datetime.datetime.now())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
