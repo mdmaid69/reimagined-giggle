80951a801dbdea5f22a4a46f5b4c3664590200b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:00 +0000

    dance thinkover declare across flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
