7e7b766b9904a29a7c7026dca5a488ae0a60cff3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:58:06 +0000

    discovery permission Uu wind shop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
