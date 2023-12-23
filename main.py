cfe85ad1a1bd3a463b79aa43b0e8709ee4787443
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:06:22 +0000

    impatient mark brother mind among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
