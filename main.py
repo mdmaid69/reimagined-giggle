6d08a76bc3225e6c6419773347d1d021fbf4fcdc
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:48 +0000

    takeback maybe about Tt size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
