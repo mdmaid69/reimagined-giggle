cf465dafcf9583c17afdad6732af173df8c10d22
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:24 +0000

    block thing schedule tape apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
