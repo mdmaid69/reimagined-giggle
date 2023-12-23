5f6067fac79588c3087763d156f26d707f109e0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:46 +0000

    bus ball news beneath pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_tangent(x):
+        return math.tan(x)
