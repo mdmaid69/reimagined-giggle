018e19e19449b001b9c41889b60e0b006c3f0ad3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:09 +0000

    contrast their year getoff difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
