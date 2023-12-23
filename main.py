d009f3a5df75d2d9584f726bfbda17af625721d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:40:44 +0000

    under afternoon will negative respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  def subtract_numbers(x, y):
+        return x - y
