53e654cc90d9d7413f1877f05661ef4e0a62566a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:32 +0000

    string worry injury that list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
