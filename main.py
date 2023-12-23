ddb90975fcd32661da2b30eedc75664684293427
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:36:45 +0000

    pile squash concerning describe relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_power(work, time):
+        return work / time
