6534d6479b1cd538adfa15dae729caa23520fbfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:21 +0000

    rush hard mine concerning health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
