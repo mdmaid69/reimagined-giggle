b8888a3a5f90aceee045c2ea2d0288ab41a12274
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:54:13 +0000

    risk connect frequent instead disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
