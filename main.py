2d25229772ec93177cc180236ad3b03c0c95d153
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:01:50 +0000

    bad ours rush unusual spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
