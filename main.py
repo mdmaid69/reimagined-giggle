c8c9d7a4dcdeb0843ec819deab0147083b4dfd57
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:53:55 +0000

    poor identify keep regular soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
