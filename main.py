53a2fbe9a887579f886981d05ace13cbf8c75568
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:41:42 +0000

    whole string answer key giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
