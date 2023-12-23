09ea8a0d6b785af8db99dff5ab7328c3e32b850e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:06:32 +0000

    complaint worst organization still payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
