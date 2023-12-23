a3e331b452a9a344a4a4eaf39f6259c7a572994f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:47 +0000

    port sign ground fix inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
