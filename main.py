6f70317afcbe2882dceff0ec415ab4680931bddd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:17 +0000

    giveup plenty platform habit holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
