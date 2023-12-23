bde711ac38c6b1ff1e450b8f16a6be15c7c561a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:48 +0000

    achieve flat nearly fix most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
