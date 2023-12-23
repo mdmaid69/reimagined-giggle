87bb9604303eebf032bdd28a91c91073f75e6cbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:06 +0000

    base finger door guard far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
