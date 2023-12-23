ea069cee4850536441fe9e904c7f35d1264bc416
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:50:10 +0000

    flat feel return snow underneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
