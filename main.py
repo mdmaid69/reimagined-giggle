009153808d54e82463b26c8e31e5ef82dc120829
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:21:27 +0000

    greeting measure left improvement whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
