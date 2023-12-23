44bb5f0f1b9eb9f71d46646b81fb147e1fc6cca9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:48:18 +0000

    reasonable quit test comedown pretty

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
