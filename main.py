d21f2421ef176e145ce8d53f6d0523b38c0179e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:09 +0000

    tell copy stop treatment anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sys
+def print_python_version():
+        print(sys.version)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
