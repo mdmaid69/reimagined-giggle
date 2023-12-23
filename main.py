2bc8759b6fe226fdd20f8f22996cc7935161617c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:08:24 +0000

    whole above intend lower anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
