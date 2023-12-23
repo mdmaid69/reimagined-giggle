632fe99c78f5be0e403ddbab84e590eca62b94e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:27 +0000

    aware market related wire today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
