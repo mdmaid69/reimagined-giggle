02ddaf92d138a3489bd248a34d7ba6c66a2882d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:40:17 +0000

    deserve off spite attempt direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
