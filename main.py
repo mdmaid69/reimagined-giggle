9ad74c835a05b263e60eeada1ba8c1ebe6e31f2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:18 +0000

    want attempt demand noone violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
