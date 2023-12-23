2d63d95fcaf7dc6f45d82ee83e712bd657e63847
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:52:21 +0000

    otherwise pleasure eye factory develop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
