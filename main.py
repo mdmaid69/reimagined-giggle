655ef1956993428e11445d6a85cf74804b6fa9f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:05:31 +0000

    joke narrow afternoon become there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
