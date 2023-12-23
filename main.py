1dede2518b5bf9200d80789c675dd6eec39ee9ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:52:33 +0000

    identify roll mean plenty nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
