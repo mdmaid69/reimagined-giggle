0ee23ea292685a782f25b1e3f3afa574b99dc9ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:01:33 +0000

    eat light keepfrom bridge Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
