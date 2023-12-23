6b4a087a182b4cceec0057c89e7942300e117f7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:10 +0000

    else rid proof moral start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def log_message(message):
+        logging.info(message)
