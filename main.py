c81dede32f3d081e3012d47f76046a10000d0e5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:19:46 +0000

    blow bear private deep getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
