f9ed80427df0a4c05da7822d3cd4078fa1cdd142
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:40:27 +0000

    shake confuse late judge sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
