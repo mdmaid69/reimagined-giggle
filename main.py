150932efe664f9f9b935718661610c4258b86759
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:34 +0000

    weigh club hurt sound speech

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
