a8aa7e43a0649c7899be7997c7300925116228d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:39:34 +0000

    even least turninto shock brief

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+import math
+print(math.pi)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
