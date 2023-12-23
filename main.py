5f86b77769e15924116e7275ac4555c9eaa58d2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:51:10 +0000

    sometimes turn want cough minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
