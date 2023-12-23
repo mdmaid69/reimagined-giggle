dfad26c6f80e01e1b4d63e80e2cfb0fe1da93045
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:02:49 +0000

    rough pen school service public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
