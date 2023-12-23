606c1d4748fec2dfb054d9b5c22c0fbe72b53cac
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:32:04 +0000

    party lead press first order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
