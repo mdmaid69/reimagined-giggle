12e60e8a734cdeffc55638cb4631889205f7db62
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:20 +0000

    different kind suit hour expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_error_function(x):
+        return math.erf(x)
+def is_odd(n):
+        return n % 2 != 0
