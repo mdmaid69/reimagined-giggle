2ddb50c6794560be75874c84e917e211c6ae7a18
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:20:51 +0000

    sleep mystery interest scream bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
