9b787bc1fb6cd0216ca679a1f36e42ddc10dea18
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:43 +0000

    holdon look promise polite brother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
