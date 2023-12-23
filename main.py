248b471e740d46f8c3eee6c0d46df9cc0b9665cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:26 +0000

    face tough receive hurry if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_today_date():
+        return datetime.date.today()
