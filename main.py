9c3cd1cc0515bacf0667b8561658f97b696c23a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:29 +0000

    abroad paper honest hers front

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
