71e5415dbe074408ce0cbf70dba7abd503f596a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:28 +0000

    base reduce turninto goup from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
