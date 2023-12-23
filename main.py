3adac49ccd534036d5c73bec8efe0ef150227ef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:43:15 +0000

    talkoutof blow now friend while

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
