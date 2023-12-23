28dd5a6f50c2118b5ce745b8c74356cb17767b52
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:50 +0000

    yours brown instrument still rarely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
