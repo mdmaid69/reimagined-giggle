1e7aedefa5b670f97d32dd85717caafd4c5bae36
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:12:59 +0000

    comein love emotion turn Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
