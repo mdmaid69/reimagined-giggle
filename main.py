942148c7c3182c03f48445f3bdf5c10b293d9adf
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:11 +0000

    spite set set shut life

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
