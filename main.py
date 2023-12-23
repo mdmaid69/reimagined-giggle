11a7781bc93da04f6df042ab4b73fadc5797a0a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:35 +0000

    independent goaway turninto during size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
