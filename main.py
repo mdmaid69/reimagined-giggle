b1379b10f65f01c6ed6ead4b2f265a8b33e7ff97
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:12 +0000

    use stamp pronunciation doubt feed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
