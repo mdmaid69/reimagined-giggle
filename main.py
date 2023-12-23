0981ef9ea7067573db4102d4d2910f1ad970b5b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:51 +0000

    equal become ahead then practise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def cube_number(x):
-        return x**3
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
