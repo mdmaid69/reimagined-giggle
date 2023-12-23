64a0f9df74b18032d51acbab9dd8c3a19e45e3d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:03:33 +0000

    let advise survive open put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def convert_to_binary(n):
+        return bin(n)
