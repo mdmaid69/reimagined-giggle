e0845ac9e72b0da38c41fc9dbbd56799ec5597f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:19 +0000

    cough shelter healthy lookafter spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
