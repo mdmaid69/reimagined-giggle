9b893a638cac3ee0ff8f3cfdb53020618b2c49c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:08 +0000

    strike bank tight should violence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
