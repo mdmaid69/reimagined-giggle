5ed85d86c298703911701cd65d2329d0b25142f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:51 +0000

    mention good already comfort rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
