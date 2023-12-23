0c72f6e2a8e717c5f1ad8a112dc3ca8ff4af7dee
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:31 +0000

    book slightly class please nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def find_max(numbers):
+        return max(numbers)
