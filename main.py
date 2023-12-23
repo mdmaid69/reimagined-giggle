3169bc27860753848f5bf5b30394a92e02fbabca
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:56 +0000

    influence bury strike company high

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
