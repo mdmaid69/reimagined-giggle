5a77f006613940d30d3e471b19d22ec30106c4dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:18 +0000

    instant chat personal constantly arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
