7e92029e7e287523134510b48a7eabf7d27db1b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:24 +0000

    learn lookup rich mess hope

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
         print(a, end=" ")
         a, b = b, a+b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
