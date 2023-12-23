3357378e12f75baf1e68d362b1a8763149409141
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:45 +0000

    equal uniform environment vote everywhere

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
