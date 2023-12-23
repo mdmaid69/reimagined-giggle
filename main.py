22bc1d6d3af78b2df55a4e0bb49aeaaee1477ba6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:45 +0000

    yesterday seat along help gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
