f813ec1e5d0e4ae28840e9b8e46456308dea3069
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:10:04 +0000

    worry make damp alive band

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
