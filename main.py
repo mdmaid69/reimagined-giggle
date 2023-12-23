6ac0749e1e12b6bfa84e03fca9faf1d1599c1fa5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:26 +0000

    open manage even keepup introduction

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import datetime
+def get_today_date():
+        return datetime.date.today()
