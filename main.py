39a6dbe81fc94fadc31b3ccc2566d81b47f1a3ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:51 +0000

    now exchange before happen friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
