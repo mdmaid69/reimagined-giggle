4550651b16f0bce57896431a111df92b2b06eafe
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:28:06 +0000

    odd patience separate precise unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
