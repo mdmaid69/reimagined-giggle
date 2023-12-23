17d7dd04c53556b2821ee9bedd35009a7d94ab28
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:35 +0000

    line laugh gentle owe second

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 n = 10
 print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
