1f0a063efdf10c97584b286eb7434770604df78a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:06 +0000

    dress mark disturb average prefer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
