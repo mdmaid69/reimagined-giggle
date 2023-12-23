a167bc4eea4b16534cd679c3593d02a7dc3f452d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:21 +0000

    force as mixture common improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_index(array, item):
+        return array.index(item)
