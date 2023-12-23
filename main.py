6fed49c13ecbd6856c7d8f9a9a3ffd0bd478bafc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:10 +0000

    underneath effort everyone anxious thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_floor(x):
+        return math.floor(x)
