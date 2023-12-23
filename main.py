7151cc00a36af5ad1d194e4bfe77cd9d57c316fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:28 +0000

    comment tread thorough sensitive drink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
