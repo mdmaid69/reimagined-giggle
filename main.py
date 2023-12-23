8ce5c8098b7a047d6af478e2af3ef3540dd10bc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:34 +0000

    breakout centre open squeeze damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
