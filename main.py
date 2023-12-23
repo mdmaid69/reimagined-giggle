644cee7134248a4a52529add35b33fde41ab207d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:07:54 +0000

    nerve anywhere search rush hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
