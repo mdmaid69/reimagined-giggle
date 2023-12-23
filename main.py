619310df21e55873e980b93694daef2c503b402e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:07:57 +0000

    than battery anyone guard there

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
