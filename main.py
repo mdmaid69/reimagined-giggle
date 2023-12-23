477a75cd2785af61e4e93d43df832cb0dbf9f5e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:25:02 +0000

    indeed funny definite both stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
