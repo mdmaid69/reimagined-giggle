95d321c41d9d2d398a420bd92263f000597632b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:23 +0000

    crowd field important so visit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
