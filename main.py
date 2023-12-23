fbebbeffd4195a05796115a85bee2ee923ed684e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:22 +0000

    blame petrol opposite friendly turnup

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
