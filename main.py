14f55e6d815a1fa656716dba25c910aa9f0b58cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:28 +0000

    avoid shoot opponent whom grateful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
