678ce7e7fef7919d98f09db0b5fb14d1ca8e88b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:31 +0000

    mix float tired road train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
