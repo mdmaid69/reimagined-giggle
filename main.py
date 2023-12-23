c89386387c2363c4a529942b3d9214b7455380a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:51 +0000

    comedown complain where violence exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
