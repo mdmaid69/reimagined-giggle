7b3d927cc3df63f53e23bc50c57d5cfa5d8c79aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:05 +0000

    average calloff success cost before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def set_array_item(array, i, item):
+        array[i] = item
