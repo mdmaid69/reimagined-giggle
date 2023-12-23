a41ce5d888a34e6f3aa5e1f0b349f36d7a40c375
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:15 +0000

    let guess good along common

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
