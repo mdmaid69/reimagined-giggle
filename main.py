81622597afea868b57efed84cb10a237625b4bb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:23:40 +0000

    this far movement contrast gooff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+print("Hello, world!")
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
