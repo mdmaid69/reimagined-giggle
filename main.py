3193edd2441c2f1d38dbfffae1a94f68c3a28698
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:09 +0000

    dance dress 564encourage nature his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
