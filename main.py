04c91fa24984796cfdf4428948222dbd95690c51
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:01:10 +0000

    estimate experience Cc bottom switch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_length(array):
+        return len(array)
