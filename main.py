34ece736742ca2518a48fa2010dfc0f7706b2f79
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:09 +0000

    guess yesterday bush within brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
