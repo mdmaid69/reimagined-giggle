422040660ad021e2cb8106709139be33ed2cd79d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:51 +0000

    rid explanation scare shut scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
