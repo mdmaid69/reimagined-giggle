c212f4d3cac8387ca6188a25c0bdec8101a38e76
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:57:39 +0000

    beautiful better upstairs convince manage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-for i in range(10): print(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
