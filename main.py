b6f5f468ac9003b0dd3dde39340226e68a78a5aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:54 +0000

    emotion exam cheek find imagine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
