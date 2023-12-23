1243bfa60d6f8d7582680c8ee0d5b9a076e2e5eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:56 +0000

    waste order demand cause dance

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
