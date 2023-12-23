8a252c1113c762bfd28f3be06fbcd56ebff1dc40
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:04 +0000

    expect money business sky centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def convert_to_hex(n):
+        return hex(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
