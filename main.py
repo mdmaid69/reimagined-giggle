206b9176cbd97d34c83a851e4d7565c36ebfc9df
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:58 +0000

    copy rate birthday lonely school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
