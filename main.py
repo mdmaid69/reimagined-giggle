f538ffced90963c1143f24292b2e04f504244aaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:48 +0000

    distant matter mind dish catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
