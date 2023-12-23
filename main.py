08fad8d10280e74831b3d3bed207313aff7d3c41
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:37 +0000

    organize gentle upwards sting point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_density(mass, volume):
+        return mass / volume
