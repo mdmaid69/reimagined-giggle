61b11969c7c673a43065d4dd5dff877740eb380b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:04:58 +0000

    hole uniform lookback(on) respect unit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
