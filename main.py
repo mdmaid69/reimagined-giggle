30e0a7b1f7bec3b7afe4e6b334133682723fd098
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:17 +0000

    chase shut human impatient apparently

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
