f51fb96141ad5818f8e1882d20ce8929e18f293e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:02 +0000

    score little country goon sell

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
