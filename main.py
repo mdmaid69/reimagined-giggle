515f5e6238199ef07f5470ea254d60ec273ed4be
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:14 +0000

    visit attempt rubbish or service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
