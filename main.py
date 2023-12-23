68b17eb3f6ea6c6cf7eef89ad0ee1a711c05a714
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:03 +0000

    standard switch lift pleasant straight

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
