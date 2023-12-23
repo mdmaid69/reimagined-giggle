7224c4b5e37c78e7489567de278c919b90e23b27
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:49 +0000

    central length Pp different direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
