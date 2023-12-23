90c633b21527f8676b5cab66e2ba9d4ad783217e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:52 +0000

    sea wear foreign glass protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
