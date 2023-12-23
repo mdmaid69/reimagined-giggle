0e9b105dd7d463d20047ff7132967169a585f875
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:35 +0000

    promise punish work stupid crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
