16fe0a1365f5588e9e97576bc862322f6dcf4091
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:40:22 +0000

    public offence national lucky bye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-print(os.getcwd())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def remove_duplicates(lst):
+        return list(set(lst))
