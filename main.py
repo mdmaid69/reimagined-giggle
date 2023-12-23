21b1df2e5d2de7e2270f5ee2a60d953ecc8981fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:08:00 +0000

    smooth pick gate border change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
