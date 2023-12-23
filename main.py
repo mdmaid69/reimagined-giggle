d71c0c2e4074271c9b41ce50ab6e7b2841ba0b81
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:10:07 +0000

    wide wing command otherwise discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
