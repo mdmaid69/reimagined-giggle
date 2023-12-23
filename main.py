6b2b29dfc22b7ef76c2347834c2e85c180eb02cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:07:08 +0000

    ready laugh label nigga possibility

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
+def reverse_list(lst):
+        return lst[::-1]
