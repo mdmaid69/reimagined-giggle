2f60aa512d9dcb74078419f6148c3af3446ef8f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:54 +0000

    model pool familiar flat charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
