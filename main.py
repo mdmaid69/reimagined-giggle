08b63f77decd957bdce956a1aa95883e4e54f0ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:51:18 +0000

    exchange certainly rely nerve own

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def print_python_version():
+        return sys.version
