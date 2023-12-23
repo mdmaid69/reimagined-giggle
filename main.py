c06b8b6bbae24485b2213eb1a05bb8ffbe74ea45
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:14 +0000

    unless test juice ball spend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def square_number(x):
+        return x**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
