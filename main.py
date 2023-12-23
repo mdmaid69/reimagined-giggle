4b25cbace4ccff21601368f9e07c17175752c2a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:15:17 +0000

    fun introduction innocent pen strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
