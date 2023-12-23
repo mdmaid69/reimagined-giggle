45a5d59afd25017a2103cfe5b1d7af394406e667
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:06 +0000

    decide honest pity provide responsibility

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
