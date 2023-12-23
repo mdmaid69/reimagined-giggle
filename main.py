f50bc8ff93495230f57bcb9b2197db8108627762
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:23:44 +0000

    Jj public 572enter rest ticket

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
