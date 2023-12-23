cec94baf4323f9020226e65fa5d01ff80ac94b6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:05 +0000

    crime far male twist disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
