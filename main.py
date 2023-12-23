6577d58a93d19dd743b603a5f24155c193ca9bee
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:13:07 +0000

    Nn arrive responsible enthusiastic takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
