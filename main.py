62305f95974e06cee4f58433814ad4e4e9b1ef72
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:57:56 +0000

    play squeeze recent although another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
