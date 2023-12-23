69cc06450f7fee0ba791d7cc49bf88f9f9cda235
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:17 +0000

    bet him track giveback part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
