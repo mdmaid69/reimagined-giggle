618f2f77be3ee06a3cc918de2eb4b75363a7cf05
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:40 +0000

    so deliver goaway suppose regularly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
