326d341239067af9993b8fd3daa5f474c01562fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:46:37 +0000

    curious possibly lie number top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def count_words(sentence):
-        return len(sentence.split())
+import os
+print(os.getcwd())
+import math
+def calculate_cosine(x):
+        return math.cos(x)
