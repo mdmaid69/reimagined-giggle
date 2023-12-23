94377744886e5c2d41682d647fe3880aeec7351c
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:20:13 +0000

    compare stamp enthusiastic custom hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
