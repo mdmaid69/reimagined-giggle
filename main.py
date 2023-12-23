4fad2a134ff5a29686e576b82560f8b950e3fcb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:21:28 +0000

    heat uniform getout him fortunately

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
