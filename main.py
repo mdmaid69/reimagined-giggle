250ecfe04d43ef70e891248477cbb45fb9923315
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:17 +0000

    score average suggest complete thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
