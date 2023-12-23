4155eae75cfb8e94acfa9085dfbf9c7795f860ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:08 +0000

    ride limit lookaround justice smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
