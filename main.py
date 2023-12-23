03d3190acdfba4ba508cfc2bb8888692835a8e45
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:34 +0000

    cinema of free pain make

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
