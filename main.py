a7ba5d02598b453c78bc5c1ed77b532517aa6aa3
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:51 +0000

    shop crowd date body surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
