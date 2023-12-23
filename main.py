d83308e32cdd208e6763fa76c51b0fc2634e0c17
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:49 +0000

    familiar shame point rid thinkof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
