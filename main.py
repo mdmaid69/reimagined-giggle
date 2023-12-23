33bcd967c071ae1105d3ee16edab0a869eff45bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:54 +0000

    left twist warm sign throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
