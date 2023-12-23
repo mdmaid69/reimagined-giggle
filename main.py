e75bfd7c8ea60d12f1ae99d47d25021d6638bd85
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:58 +0000

    public value behaviour lunch train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import logging
+def log_message(message):
+        logging.info(message)
