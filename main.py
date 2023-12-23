985ec8aeeceab86b2848fd83ee077aa5a81bdb58
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:47 +0000

    disappointed female usually custom experience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-print("Hello, world!")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
