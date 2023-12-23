ab8db431607b81a82e6ec2f302dc9473ca8af091
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:09 +0000

    so too comefrom bath action

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
