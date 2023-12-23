37992dc1a3077bca0b6bb8daffedbc8df1e66278
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:05:57 +0000

    himself favour pipe attention lead

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_speed(distance, time):
+        return distance / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
