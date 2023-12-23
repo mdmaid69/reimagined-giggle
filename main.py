542f7e45da986cd2a223851cf9b6592389e75055
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:17:58 +0000

    string goout fierce gentle keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
