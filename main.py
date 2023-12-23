db9ca3a60e86fe559a28866e0b6c285e8217529c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:17 +0000

    tragedy finish copy turnoff nice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def cube_number(x):
+        return x**3
