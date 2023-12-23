9456879f46bcb9a4c46dd7f035d2b3b5787ddcd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:56:36 +0000

    computer scrap mind check tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
