74c0c4c5b6c36624da8024f51e39e10155e794ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:40 +0000

    culture communicate please everybody extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
