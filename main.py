9b81bd7093d9b5472c4ddb98975bf8915906e4e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:26:33 +0000

    content life dress offer only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
