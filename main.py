d77acc470bb834d0fa0eb4d9e2e7e60e652b939a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:48 +0000

    confidence we young warning cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def convert_to_octal(n):
-        return oct(n)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
