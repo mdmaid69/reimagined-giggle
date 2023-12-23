4fff25120a7548e278069d63d5c11a2bff614247
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:43 +0000

    plenty bill include bringback sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_area_rectangle(l, w):
+        return l * w
