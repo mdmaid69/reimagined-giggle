92b258273e346177112a63c8b030a9c4a83b39f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:03 +0000

    pity drink wake fairly key

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import collections
+def create_stack():
+        return collections.deque()
