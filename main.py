3ecd5da90e1ea6ac4160b3024b64f773bfa52dbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:08 +0000

    OKorokay gooff fine together nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
