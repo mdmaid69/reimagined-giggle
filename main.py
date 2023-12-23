eeff0b232bfe5b35c6aa12695702b41bb4baca63
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:53 +0000

    sharp error Qq everybody this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
