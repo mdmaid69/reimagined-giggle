1646d08df3ebc97e6567291216cf4bc4b4d9be7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:27 +0000

    hard set fact open influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def cube_number(x):
+        return x**3
