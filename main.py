9fdfee42e33ef1112c07bdd29f93e40fad70bf22
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:34:20 +0000

    principle disappointed colour argument into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def log_message(message):
+        logging.info(message)
