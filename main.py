00830c04a20fd8404eaf8dd5662e6f622b6192cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:20 +0000

    away hand practical roll trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
