17f0b75edc5bb3206524fbc91cf0a04462ed76d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:20 +0000

    quick all punish popular professional

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
