5d38e4c0bdbaf38030b6e2f6662d22a144149fb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:04 +0000

    underneath beside welcome squeeze much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
