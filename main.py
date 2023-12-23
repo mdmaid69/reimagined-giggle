29a32c167dd234dc8a74b22def664dea23437666
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:06:13 +0000

    turnaway clear national Itself bin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def clear_array(array):
+        array *= 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
