3f51dfd7bdc8e36ee32202cc55e0bfc4af2ee639
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:33:44 +0000

    wear oil occasion card degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
