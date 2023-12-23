a3302796ac51af8d91bfb1af59cad2bc38d3cc84
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:51 +0000

    569enjoy deliver callup abroad tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
