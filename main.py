f2d0c1ea8571ea0058f463bad1ab54049b3c3fab
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:29 +0000

    niggaacademy warn thirsty answer beautiful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
