a9f115a80c7ccd1d5816acd9e76f47ea8cce084b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:15 +0000

    ours tend makeup subject pronunciation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def count_elements(lst):
+        return len(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
