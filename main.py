645d87b6def52753051a2de8448e686450ad0c58
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:27 +0000

    accidental proper temperature pen drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import tensorflow as tf
-print(tf.__version__)
+def get_array_as_int(array):
+        return int(array[0])
+import sys
+def print_python_version():
+        print(sys.version)
