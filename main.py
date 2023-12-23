827746c36a1cfad5315e8b59bfd3eb010333abc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:30 +0000

    attempt learn computer plain before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
