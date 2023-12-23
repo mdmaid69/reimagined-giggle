ad92fecc654102fb9cf66e59e584b4a58d98b6f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:46:34 +0000

    anywayanyhow fire coat normal meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
