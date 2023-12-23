e78b37b452d81cff1693953570f87eb51c15f37c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:43 +0000

    within left bridge bet flavour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def multiply_numbers(x, y):
+        return x * y
