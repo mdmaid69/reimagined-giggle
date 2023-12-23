bc2a953d5ccef290749a78624957e74e1d9b3620
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:22 +0000

    conclusion mention callback wash drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def add_numbers(x, y):
-        return x + y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
