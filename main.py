97d31614267cdb026c1487d6ecb6c05bb77aebb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:46 +0000

    juice room slide 570enjoyable tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
