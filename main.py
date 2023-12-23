a15c687b9397699f3ce93578cb5bf18d306a388b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:53 +0000

    mess capital Bb small ring

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_power(work, time):
+        return work / time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
