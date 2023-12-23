daa55c130c5c57978f054d0769be92e72ac26d8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:42 +0000

    trick unable funny face spin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
