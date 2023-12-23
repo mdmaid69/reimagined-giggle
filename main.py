12659245bab448f2c9cd10282dc071cd11933343
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:45 +0000

    test to behind enthusiastic comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
