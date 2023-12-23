fe50c4bbdefb0748ae2c3843ec061d962a1ff72a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:20 +0000

    human place slight control different

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
