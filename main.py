dd5fb71cb6882698066b9cd208dde3adfe741255
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:03:08 +0000

    teach survive scheme several understand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
