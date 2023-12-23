1fe2890a64ce560ae40b3acd02ff2f7ba8842ea0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:09 +0000

    blue duty cause can leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import json
+print(json.dumps({"name": "John", "age": 30}))
