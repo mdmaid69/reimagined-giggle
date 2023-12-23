348356eb9288b549fa94cd15cb4c65dd1b5f3a13
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:47 +0000

    permission themselves spring rise contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def square_number(x):
+        return x**2
