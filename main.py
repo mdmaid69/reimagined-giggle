b7ee807cea86fc22914345adf35d5d9203df7ca3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:49 +0000

    save shade just clearly giveaway

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_set(array):
-        return set(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import tensorflow as tf
+print(tf.__version__)
