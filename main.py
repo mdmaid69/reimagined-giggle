9c89b87d0fb9c6f8ae999f40aeb0e2e3d76bfb2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:35:19 +0000

    guilty thought word strong best

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
