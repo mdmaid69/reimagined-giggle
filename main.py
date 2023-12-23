b057873920035ebb4d8cc71ba813880021a04cad
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:39 +0000

    here spray food poison over

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  def count_elements(lst):
-        return len(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+print(os.getcwd())
