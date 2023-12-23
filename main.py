1d6ef2d87abe67cc23b62e8643ebde6402fb5883
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:49:13 +0000

    dry behaviour decision wind track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
