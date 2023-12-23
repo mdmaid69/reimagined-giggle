57901bd1412089bea31292f37f093f2fcf2e1177
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:23:48 +0000

    organize check Kk combination study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
+  def is_odd(n):
+        return n % 2 != 0
