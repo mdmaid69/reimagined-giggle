1df1a6bfc53c18234000ceadab4ebf063e3e3547
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:42 +0000

    partner muscle crash object hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
