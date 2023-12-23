71fe0f6f89bb2a848318e541aed9f48a17eae6c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:48 +0000

    buy pleasant chin runinto formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
