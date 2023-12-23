9c167e5479a3c43d927eb89df89bd15acabbdf70
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:45:06 +0000

    goodbye section right contrast speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
