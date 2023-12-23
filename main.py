0b925ef93b92501b512f26ed870e306796cf5bc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:35 +0000

    along little dangerous authority ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
