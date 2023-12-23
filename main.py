7e88c2e9722040e0a7fae4306cca82511d0916c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:02 +0000

    squash behaviour full software basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def convert_to_hex(n):
-        return hex(n)
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
