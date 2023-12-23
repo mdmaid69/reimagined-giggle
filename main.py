8a493dbf66badf5ba4d055998752baa836caaebe
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:03 +0000

    mistake goaway recent shelter shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
