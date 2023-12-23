f1a737bcaa1367db316ea38b3980908f47754bfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:29 +0000

    by coffee want trick hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
