e57af18da5f3c7bf8acb39625bad1476fde0fdf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:43 +0000

    release distance print software noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
