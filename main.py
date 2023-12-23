7714e13d3095791d9db0dcf399aee4ef1b63393f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:08 +0000

    meal knowledge Jj 573entertainment hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
