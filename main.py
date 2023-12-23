0fe7d8d99c7d20bd31e8233db32bb6fed5725741
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:50 +0000

    throw survive through lean damp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
