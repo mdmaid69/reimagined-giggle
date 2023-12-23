1f2b749fba4f23d01a009e24fe09e337fa2c3b22
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:35 +0000

    load family worth sleep platform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
