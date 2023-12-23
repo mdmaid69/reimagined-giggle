70a5011b3740e1efe67940e4ad1c3638ddcfff05
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:13 +0000

    receive place that sing getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
