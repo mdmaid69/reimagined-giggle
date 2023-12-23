b1cddef4bf665a6332f7ce0aff6c72d9acafb6df
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:52:48 +0000

    aim new admire boat obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
