68b01fdd09eb8a46453434fe8c20e21d3ae3f078
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:51 +0000

    unlikely influence giveback brush raise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
