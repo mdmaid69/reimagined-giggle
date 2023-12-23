3f110b4d982a58aab690b93239781c085bde1512
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:23 +0000

    create warning order figure copy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import numpy as np
+print(np.array([1, 2, 3]))
