6c786d432c7bba865e97fbfdfbab79d545b34fbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:56 +0000

    experiment apologizeapologise payment immediate project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
