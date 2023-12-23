83f13d91edf11626fa8d79482905a5ffa6be3644
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:53:52 +0000

    completely hope scare wet advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
