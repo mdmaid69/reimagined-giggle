2551f1613f0ffaa11c4884d4c724511272492e62
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:16 +0000

    yesterday warm habit fall lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
