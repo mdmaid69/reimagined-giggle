441dba1d6dabc66b7700089e2b79702fd3ba5df4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:58 +0000

    fine name remember morning me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
