4198c3495e177ed45b5b9b2d0dfb06a3da933929
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:18:38 +0000

    city wave finish pass rain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_power(work, time):
+        return work / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
