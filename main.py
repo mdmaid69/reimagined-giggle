b85e7eb8710336e7e6453c245366623c5b78335a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:25 +0000

    tidy flood easy either fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
