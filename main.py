a95895956a8280e65763224f527c5ff69e219c21
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:54:05 +0000

    foreign square national interrupt bill

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import re
+print(re.match("h.*o", "hello world"))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
