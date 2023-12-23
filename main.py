924fa5420861c62270d25bbad82f8001821c427c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:09 +0000

    total feel heavy whoever odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
