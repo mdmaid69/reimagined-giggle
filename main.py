e6ba088a30e3d09faf58f99ea1d6770edd9d67d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:55 +0000

    certain forward foot place herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
