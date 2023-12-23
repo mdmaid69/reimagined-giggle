6922c994fac8cff533b38333879cda1a6a3220c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:32:14 +0000

    pretty develop guess sentence reasonable

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
