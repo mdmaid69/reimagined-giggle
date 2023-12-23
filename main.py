0ad4fdb537d0c100371c48248887e8c060b23f4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:37 +0000

    complicated tired bunch check combination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-print("Hello, world!")
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
