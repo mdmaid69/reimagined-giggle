99265fb556089b9c89c9e386eda6d6209254592a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:56:15 +0000

    criticize act lie yesterday government

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print("Hello, world!")
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
