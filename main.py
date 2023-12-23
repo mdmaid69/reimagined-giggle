0ebf6d445c8f6638b91e14eedfc6307bf39b5a1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:22 +0000

    damp jump system clear port

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import time
-def get_current_time():
-        return time.ctime()
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
