11834bbeb6fad524421b85d177fa1fa578b44b9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:59 +0000

    main reasonable convinced Hh paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
