a022a9292d9095b26382af1e440cde8a89e2d400
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:25 +0000

    health strong noisy note worth

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
