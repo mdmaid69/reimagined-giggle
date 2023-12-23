eeee47f960e44eb36c4ffb82d538a8c9ddbaca59
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:49:23 +0000

    belong meet plan determined accept

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
