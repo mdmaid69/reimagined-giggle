c46e2c30e1122a3ba68bb7e81bdc04ff3793f2ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:32 +0000

    star describe raw card all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
