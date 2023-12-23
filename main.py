10e7e00dd4b26910c7429741aed1a886789d9a37
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:56 +0000

    keepup star move land as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
