3d246661278269d2726a223b401158b5fce152dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:47:17 +0000

    stop into branch bear available

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
