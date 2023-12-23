3f4ded3bbf46fb3990a99450aa08cf4269537d33
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:06 +0000

    rather glad squash refuse goout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
