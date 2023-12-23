6aff22a2f29ebe64f70509cea0d6d24895267ed4
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:18 +0000

    control pleasant squeeze head direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
