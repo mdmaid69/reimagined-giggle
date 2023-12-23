2c081d2cf408cb2c28933ff4a6882b7f8b5684a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:28 +0000

    relation lose comment relationship glass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
