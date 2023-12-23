35620ee837fb4a0d64cd70f6accc6bb8608fdf05
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:26 +0000

    team hang lower ours off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
