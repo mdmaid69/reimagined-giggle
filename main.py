d6b642a3101681daeede0397ec10702d05e00a43
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:00 +0000

    cross criticism forget be back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_counter():
-        return collections.Counter()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
