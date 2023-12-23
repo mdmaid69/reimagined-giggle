9d10c09aa5862bb7fce2b128f50dc9b79c09d4f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:34 +0000

    so reasonable object second top

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import tensorflow as tf
+print(tf.__version__)
