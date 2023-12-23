82669ac0dddc941e0bc199ada918a37d60959a5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:38 +0000

    present once distant lunch pick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
