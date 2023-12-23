82a9844243967b8bc777488e38744aa9a029abce
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:46 +0000

    frightened impressed creature relief may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
