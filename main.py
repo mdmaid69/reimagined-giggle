28b537de6d7a162aa1b35a2c95b771d7a42c3760
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:28 +0000

    anybody trade you turn you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_index(array, item):
+        return array.index(item)
