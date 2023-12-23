2548a26eda759856c7d469dfbc381a3d75576497
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:50 +0000

    attitude part gradually rub activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
