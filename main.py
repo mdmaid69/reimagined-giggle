4ec3ec7e8d763f8e0c951f01d2c1fc7377ed77d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:42 +0000

    brown happen bet wherever cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
