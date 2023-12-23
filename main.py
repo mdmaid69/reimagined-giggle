a65d3e400042f4aed926e669bb6038a5f0e04ccd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:53 +0000

    open lookup length without sticky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
