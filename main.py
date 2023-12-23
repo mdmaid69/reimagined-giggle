38cb62d08421247c274c6ed6a60f7421e026b1a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:25 +0000

    phone use ignore reliable lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
