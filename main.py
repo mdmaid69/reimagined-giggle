645c01426a63928b7b2e16efdb54bfbe33be4223
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:53:22 +0000

    Cc border furniture chief geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
