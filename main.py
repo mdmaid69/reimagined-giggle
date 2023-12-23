73b06fbc148d7e9d93410ff50ccce1abbe9ee111
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:05 +0000

    plain heat criticize worst care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
