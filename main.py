e6f231e1bf4157f9604bc29b3a6a319ac48f26b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:32:17 +0000

    squash pretty seem that for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
