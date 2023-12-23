d6eca6ef9a74706e2b5ddc4dc93d9e7673b92d4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:43:50 +0000

    fit ago rent snow brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
