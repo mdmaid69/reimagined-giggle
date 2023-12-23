e70682da9c2884968522a605f70d72e78aee9ca6
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:16 +0000

    complete positive thinkthrough vote hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
