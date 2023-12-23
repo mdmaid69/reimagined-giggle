b246ffdfe22ee92edf798736d82fc0a0f1d437a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:33:28 +0000

    add accept across work apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
+import collections
+def create_user_string():
+        return collections.UserString()
