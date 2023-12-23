d5283e83d8af3194bc9daa604ae84536d5add419
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:43:38 +0000

    depend getup picture secret hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
