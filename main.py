9550093da8d56eddbdf889d49081b5580a2b1c06
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:05:47 +0000

    strict key sensible modern attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
