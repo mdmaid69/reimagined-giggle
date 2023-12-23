0c827d497e8a6a1c2faa9189d965562725b55732
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:36 +0000

    statement wind meaning excellent forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
