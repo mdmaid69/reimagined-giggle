d47f299ae595ae07854fec445307e51b133113a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:57:13 +0000

    runover breakfast cold either pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
