fc47c419dd51473e735c1f8152f25f0e9e7db420
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:16 +0000

    poor deep state creature powerful

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def convert_to_octal(n):
+        return oct(n)
