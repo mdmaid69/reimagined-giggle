d136aeaa7da87bda1b590b74a9ff41c2e1f6b485
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:27:42 +0000

    yet second lie meaning we

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_power(work, time):
-        return work / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
