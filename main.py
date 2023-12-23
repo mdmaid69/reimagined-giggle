221fb8729ebf631d019f1e4954ad247ffac49fda
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:18 +0000

    use wherever evening apart refuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
