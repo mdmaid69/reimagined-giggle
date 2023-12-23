905dc64e57be3e0f0f6edf0408cd747389dea700
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:03 +0000

    cry lay 565end fault match

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
