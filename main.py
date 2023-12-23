16de01f5c3db135c4f106b9718495a4e4522af40
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:38:17 +0000

    home human case them case

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def reverse_list(lst):
+        return lst[::-1]
+import sys
+print(sys.version)
