a1098f68789ebf0ec9417e93973401b34116a320
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:55:05 +0000

    loud class holiday level boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_str(array):
+        return str(array)
