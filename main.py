b478b4fc69fbc53f79d4274abeb86750f53916d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:11 +0000

    position increase turnover side neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
