73dd931b3b5526f519fef73ef1e5b52ee34e9b18
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:42 +0000

    custom keen remove kill short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_typecode(array):
+        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
