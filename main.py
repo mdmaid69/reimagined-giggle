17a5698ecaa0d4060d5588e160c28dc12f71d538
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:25 +0000

    refer pronunciation have ground effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
