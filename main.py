f2a49d1c389c6d29d1b97a217f6d89e53f137780
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:22 +0000

    lucky worried correct class month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
