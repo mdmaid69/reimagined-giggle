cbf4a59bc9b1e3bd0c9fbb08daf9f4f14db3e2ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:43:09 +0000

    freeze what anywayanyhow grateful yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
