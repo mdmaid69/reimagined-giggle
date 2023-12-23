b56cca02467b8745eabe43bbc07f57491c923d9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:57:25 +0000

    busy better speak emergency weather

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
