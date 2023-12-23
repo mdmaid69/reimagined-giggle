d4e1f157b7adf1d99b5e704f0e895f01b807b29a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:30 +0000

    used settle skirt classroom communication

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
