ae0771924dba1e17e8f7f14f37f8a21849ea5e6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:48 +0000

    Ll remove offend mistake opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
