45365f3455beff70fe657b7e67dff5ca442725e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:55 +0000

    unable bend lend idea burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def exit_program():
+        sys.exit()
