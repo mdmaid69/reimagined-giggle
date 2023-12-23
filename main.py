36701a83d8afafceec8a9a0db4b41ea39d309c05
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:58 +0000

    bottom film anywayanyhow little enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
