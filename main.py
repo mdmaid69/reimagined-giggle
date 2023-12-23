4adc443a714d7edcb0889e275d0478d03e970f3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:29 +0000

    pleased deal test way zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import platform
-def get_os_info():
-        return platform.uname()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
