0039890012ab43e64942c68d2ec34ece04e13fc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:38:16 +0000

    crime right wait bath general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_binary(n):
+        return bin(n)
