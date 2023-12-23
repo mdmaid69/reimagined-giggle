55dd55448c859afa3ba9ac99d79f5da73b41258e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:48 +0000

    along wish fork central building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
