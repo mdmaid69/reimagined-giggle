e5804c2b2cbf30cf08f07334281391c87eee429e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:28:12 +0000

    past collect that trust talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def reverse_list(lst):
-        return lst[::-1]
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def count_words(sentence):
+        return len(sentence.split())
