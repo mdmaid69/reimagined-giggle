189cefeb676c0659ab873b535a9603feae0b0b1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:08 +0000

    question unfortunate influence proof steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
