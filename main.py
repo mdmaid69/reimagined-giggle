fdfc408723069c8a6d6e1c60dc3cb2ab2f7a9a52
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:59 +0000

    heart invite damp talk ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
