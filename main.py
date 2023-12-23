7e927644d390f7c9df0851949f2252be52770954
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:30:39 +0000

    ought guide safety mother tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_odd(n):
-        return n % 2 != 0
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
