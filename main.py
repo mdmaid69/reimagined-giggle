45bf1c69e4f4e13247262a1d20ac265b57c85822
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:40 +0000

    runaway tired married thought everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
