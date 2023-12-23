95e155c2189c4c072359eb073c231cac2a7828e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:36 +0000

    cheque lie sign congratulations bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
