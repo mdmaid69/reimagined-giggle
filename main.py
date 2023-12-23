4c733648d3e153502cc9c50d0a327abc550e091b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:49 +0000

    juice wish throughout 565end equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def convert_to_binary(n):
+        return bin(n)
