b4b1a67e52181e803c2190c9f01c33edf0549bbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:52 +0000

    wake famous share drive pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
