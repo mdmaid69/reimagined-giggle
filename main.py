9992803b9ab7391bc743a9f4a28b85b3ecbc3762
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:33 +0000

    Pp rate past tongue drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+print([x**2 for x in range(10)])
