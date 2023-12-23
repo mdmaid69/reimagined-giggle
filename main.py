8127d217e9f5125cfb4ef0241f2371ce0cfa5bce
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:16:26 +0000

    worry off sign exciting practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
