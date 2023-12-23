1d2137127e65975597b17107072393afe932d87d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:48 +0000

    burn Rr manner cruelty nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def split_path(path):
+        return os.path.split(path)
+def calculate_acceleration(speed, time):
+        return speed / time
