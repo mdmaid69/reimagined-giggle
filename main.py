269554a2fd8aa7a2b97ca4979d2c85573e3fc375
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:52 +0000

    generous pleasant switch lookaround still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
