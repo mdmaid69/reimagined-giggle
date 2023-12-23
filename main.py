149f70f67025621012376a515091ca103cea22b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:40:34 +0000

    software almost disturb exercise combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
