429b82f16c6f235cd0fcaf2ca8b350efd2439841
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:26 +0000

    hold base long sport short

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
