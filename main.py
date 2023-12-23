0d4eb84feaf78c363e9dd36546d7511ec4fe1bda
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:48:30 +0000

    comeup replace system minority injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
