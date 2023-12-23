7797b84ddab3677ea91bed0bc154a7bc21e749ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:14:18 +0000

    owe convince moment right burst

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
