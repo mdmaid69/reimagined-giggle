517bc4984c21aace7ada324148093b8a3deb2867
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:11 +0000

    error represent nowhere against work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import re
+print(re.match("h.*o", "hello world"))
