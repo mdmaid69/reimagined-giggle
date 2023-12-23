728e4c258397da8784f70c5a033f0f51ba1ba893
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:44 +0000

    mix funny arrangement below gas

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+print([x**2 for x in range(10)])
