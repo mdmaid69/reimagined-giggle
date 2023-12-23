08cc1bf525e2fe6dac6f3a6d68ecb55a749c70cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:48 +0000

    basic connected leave direct expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
