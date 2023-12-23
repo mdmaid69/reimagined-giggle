a26419b11471cb171df9fd6228716f59ca12411c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:56 +0000

    argue murder impatient force scare

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
