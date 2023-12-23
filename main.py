90087edf04364e2bcb0d71ad68b15a302f536c8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:02 +0000

    good soil getaway arrival detail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
