c4d2def1ba41698a5ede344cd3ab6fd9db480a16
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:10:42 +0000

    help eat while dead pronounce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
