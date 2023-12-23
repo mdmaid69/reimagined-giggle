27529d1d502f86b8724904445603ec1fa28fef2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:09:48 +0000

    ilovedicks time charge responsibility deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
