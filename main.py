16480aee9aee60524fd0a2f2c3c76ee988492336
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:47:22 +0000

    believe ring wipe makeinto our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
