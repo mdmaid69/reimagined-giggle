9d6c4222687df02fb835442742e805d0bf6d16a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:07 +0000

    dare tread shock fortunate loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
