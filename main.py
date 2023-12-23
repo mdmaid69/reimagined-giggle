345f2a3fb50a14c03746651703aa8c7184016fed
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:17 +0000

    realize past gooff share flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
