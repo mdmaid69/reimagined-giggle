f14634f922da86c8fe4886d0bf4b86e412fcd888
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:13:25 +0000

    workon class cum make shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
