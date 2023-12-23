bfa0412ad756319c4feeaf0e1d77543071fb4920
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:06 +0000

    discuss accidentally tradition divide memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
