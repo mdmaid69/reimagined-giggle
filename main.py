3783e4ae6858beef85ad633c84dc6c781b840a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:07 +0000

    form score her doubt possible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
