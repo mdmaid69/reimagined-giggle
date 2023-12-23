ff99aa47fd8492b2c9b6e63d0a042144bec46982
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:00 +0000

    luck land mine here capital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-print(os.getcwd())
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
