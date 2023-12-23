9f2d0fd862d8f0f517c10b6f72c97e3eefaa4b85
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:06:45 +0000

    shock science emotion prepare and

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
