fe14ef21ebcbe917f4b1dd187955e823c898eda3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:41:37 +0000

    harm another complaint early through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
