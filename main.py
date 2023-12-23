c43833a00c5a120c2d506ec0b49d32213e1118e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:42:15 +0000

    impressive land tape within sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
