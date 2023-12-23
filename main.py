c56422fd4530a2481478d70a1d4386b211edb56c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:16 +0000

    holdout lost keepoff teach injury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
