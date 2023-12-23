9b43f3014e629d156f6387afe7dacae8e43cfe6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:41 +0000

    by fight top learn Yy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
