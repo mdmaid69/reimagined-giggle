29e75b64adce992053402691e3acb8770d4fe490
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:50:56 +0000

    pour Hh tune lookup hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
