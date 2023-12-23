d90f5a9098892bd821dc3a7af178f01408e52dc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:34:45 +0000

    improvement tidy justice perform precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
