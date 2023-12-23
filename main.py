c158fdfb28df0c4c5c72172da4e7eaa80e3c0649
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:03 +0000

    pressure doctor party exit convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
