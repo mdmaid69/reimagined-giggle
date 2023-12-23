7b2761a8aca9af627d34ea600a8a4011415a1688
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:48 +0000

    voice sentence various force free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
