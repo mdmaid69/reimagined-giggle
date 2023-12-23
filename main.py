ffd8d3ba7061ee3a82d901dd6f18f1c9d3f32090
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:19 +0000

    unless force desperate attach someone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
