358eaab744549aacc06cf03fc0eaa5c4917a933f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:53 +0000

    hope oil calm common contents

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_base_name(path):
+        return os.path.basename(path)
