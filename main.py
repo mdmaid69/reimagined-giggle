57080b9d51cfcf1df65fb0f610f9fa4fbc3a8a74
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:23 +0000

    thinkover waste happen right guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
