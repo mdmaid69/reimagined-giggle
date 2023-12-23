7f131c245a974ea82c45bf16eca49cd06fd36f03
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:34 +0000

    talkoutof give character fly drive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
