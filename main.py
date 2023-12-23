f838d61c7b2be7e7b2d6cb3302c6c39893933176
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:53 +0000

    loss discussion judge female before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
