c1667bffc849fc42c6e4926e54543fdce8cc0395
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:18 +0000

    everyone brush rush story settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
