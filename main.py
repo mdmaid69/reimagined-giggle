c3294f24faa506d547e874be19c085966f8127b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:36 +0000

    yours classroom add always from

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
