e06e6230a27e3b99ae44502c90cc859271f334b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:27 +0000

    increase fix careful stare 568energy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def greet(name):
+        print(f"Hello, {name}!")
