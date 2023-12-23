ea33ffe36438afbfa56692015845aad9c895ae96
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:52 +0000

    Vv test maintain lookup floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
