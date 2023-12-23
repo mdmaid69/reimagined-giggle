fb21d1704b5ebeed5ac0032956ad61265bcb8731
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:06 +0000

    anything black across different image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
