3efd3e5cf4658b9ecd2ef634cfc1625e573e8678
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:21:57 +0000

    uponon issue drop mention admit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+print(sum(range(10)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
