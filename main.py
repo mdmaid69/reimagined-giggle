12665cf39c71991239a9c6609f91c43dfebb00fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:16 +0000

    there light callup rest sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
