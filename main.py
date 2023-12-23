f1475bd461c9858b52a6fe25b82ad7fdc627ba15
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:22:21 +0000

    admire Qq voice switch speak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
