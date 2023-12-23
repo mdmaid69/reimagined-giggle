3ebaa84442436884f837f5254db55001d36ed873
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:25 +0000

    completely the fasten shoulder feeling

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def sort_list(lst):
+        return sorted(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
