de48b68e6cabd486c3229d158744928876f64093
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:25:41 +0000

    good proud mean radio container

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
