52b63c48b4f5ddfa423019c3098a60b6656dd64a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:57 +0000

    opposite small often talk sweet

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
