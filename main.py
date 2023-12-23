d52e12880c35ff075824733883c614ae994d833a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:01 +0000

    gap protest phone half pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+x = 10
+y = 20
+print("Sum:", x + y)
