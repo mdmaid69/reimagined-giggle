302eb31f0a6143c3593b3043693bbb83cdccb0cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:45:47 +0000

    set next confusing getout material

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
