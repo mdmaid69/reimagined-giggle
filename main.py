3d78f04f63e76d7c300e7a760938f77695a197f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:37 +0000

    oil accidental life job interview

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+print("Hello, world!")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
