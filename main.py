c0facff7af19132b1c247edc17b4e61670c0dcee
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:55:00 +0000

    reply round start pinch such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
