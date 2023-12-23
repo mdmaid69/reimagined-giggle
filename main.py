00076692f8479220282c543aa8b8629ae810c203
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:48 +0000

    suck born town degree bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_index(array, item):
+        return array.index(item)
