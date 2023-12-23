4ee5852786a9c22972a246b603a455cce4b339ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:22 +0000

    dinner whole 566end any female

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def print_hello():
 thread = threading.Thread(target=print_hello)
 thread.start()
 thread.join()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
