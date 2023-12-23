466d1514f86c946e7bff1ddfbcdcd0c1c6e986da
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:36 +0000

    computer pleasant community Tt single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
