d04866dd88cdaf52c30284dc3d51df0462aa0d3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:46:25 +0000

    build somewhere over far task

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
