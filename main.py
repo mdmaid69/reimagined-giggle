1ac9b9189ce78e579e262aac14eb4db841eab4a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:50 +0000

    complaint along interesting getaway everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
