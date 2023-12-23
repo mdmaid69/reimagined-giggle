3a54ce3494ad9535beb96c5bdb70f05e01ffe9d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:16:04 +0000

    remark sometimes sister hard bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import tensorflow as tf
+print(tf.__version__)
