db47fca85680ef317cf308bf12bc4667adcfcee3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:34 +0000

    connection sit hide scared fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
