86fba2c0eb27b4f00f0578ee13562f19fa28076c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:20:36 +0000

    neither until lie better whatever

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   def convert_to_octal(n):
         return oct(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
