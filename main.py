d0cfa1e6677c71865d1f6e4871949c5ba46fdb85
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:32 +0000

    within fear second experience maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
