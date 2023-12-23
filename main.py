26100e053a8c0a504eaaab6fdbeb356dbfc9ec81
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:35 +0000

    exam wire powerful ring tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
