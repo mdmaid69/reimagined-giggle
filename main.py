131955e3d6c9d212545d948914ee6177b601df77
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:27:08 +0000

    guess case whistle strike prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_today_date():
+        return datetime.date.today()
