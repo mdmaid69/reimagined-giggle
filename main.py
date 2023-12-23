ca7deb15e418e4a12f090f44759e0d9468f85ec0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:34 +0000

    message uponon blue ear programme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
