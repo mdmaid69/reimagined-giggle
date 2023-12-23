ef4bdd5c7089954a819193aa3abceb8af6ca64cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:50 +0000

    scrap add rub Ee difficulty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
