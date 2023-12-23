e2c9e330b81bdd025e901b3bde9ed5fbd5551856
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:45 +0000

    discover past bet difficult cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def is_even(n):
+        return n % 2 == 0
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
