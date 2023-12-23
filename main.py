61bc827f48d82ccd58c0cfbacc4c6febead49d70
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:27 +0000

    wire yourself seem thin rush

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
