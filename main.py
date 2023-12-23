ac3559f1ddb7b50f1c91ed648c3686366cfb8db2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:53 +0000

    out nice callback equipment tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def sort_list(lst):
+        return sorted(lst)
