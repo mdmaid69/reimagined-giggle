436fc3a74b6b51bb34bd3eefba3c94a25763b0bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:44 +0000

    dream happy gooff decide home

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import numpy as np
+print(np.array([1, 2, 3]))
