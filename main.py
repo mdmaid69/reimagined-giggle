af2bb8824247bcb33a37ac9f7df7454d9c707dc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:46 +0000

    content fresh catch stupid attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+import array
+def get_list_from_array(array):
+        return array.tolist()
