0d0c553179c53ee0cc922cdb75c7f40811298b0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:55 +0000

    Jj backwards pity game offend

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
