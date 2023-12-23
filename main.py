a8ea18e35bdfb41f82b724e3980b8c1beda7220f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:54 +0000

    believe record thick discussion talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def subtract_numbers(x, y):
+        return x - y
