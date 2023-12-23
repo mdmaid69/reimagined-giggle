9a51c466eed425e96f3ecdff98a8194c7b149f5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:50 +0000

    bush lookaround rise somehow trust

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
