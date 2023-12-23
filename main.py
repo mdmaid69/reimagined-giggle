46383936e54a2496b3fa7edf65418a7c6c6bcd0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:35:12 +0000

    exaggerate declaration warning apparently pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
