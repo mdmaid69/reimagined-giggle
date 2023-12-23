aaf3d98e671d2b910e74aaf1e14aee6e58ff1e9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:13 +0000

    type sort I label arrange

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
