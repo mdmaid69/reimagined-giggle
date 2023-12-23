2526f00fbea8290b02dbbd80755133427990d1c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:33 +0000

    extra subject remove feel giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
