2c4f76f0f637edab9c2fa65f8d604eb22f789c88
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:31 +0000

    matter front bet embarrassed interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
