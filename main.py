f62f6b28551750f7893de3c59804240f7955c5bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:20 +0000

    deep pain anything only free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_string_from_array(array):
+        return array.tobytes()
