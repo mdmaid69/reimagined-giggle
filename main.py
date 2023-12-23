ad47585f29613c270814da534bfa0f48e046c1b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:39 +0000

    well passage dress violent charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
