f8b00c39a2763cb368591f89da29e58d47f820af
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:21 +0000

    interfere callat less guard hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
