ff2d7b192324adf7330db5e6943e5375077a552f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:37:32 +0000

    demand love lookafter past number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
