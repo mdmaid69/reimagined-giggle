54534757e50e59aa329caf2bf23d5adc4de78c77
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:57 +0000

    platform settle command average necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
