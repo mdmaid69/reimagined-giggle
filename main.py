92621868a85f252c4b208075170236dd1afdb7b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:47 +0000

    print drop none body however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
