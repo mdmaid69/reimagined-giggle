582e643c27c11e10e732508eaf57ed88b6e9e518
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:01:27 +0000

    interest city theirs wing exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
