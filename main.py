c304a1d73d0dbfc91751785dbf1b0707eea6a0cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:33 +0000

    dead before job crisis through

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
