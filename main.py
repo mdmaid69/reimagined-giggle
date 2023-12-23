b8c34bf173d63631381d19b9952bc0ece623be1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:58:43 +0000

    obvious blame nearly safety slight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
