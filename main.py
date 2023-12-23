4ba09d9d79c608f74108bf63949d83419b623654
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:49 +0000

    her love contribute slowly one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
