c35d9705a396db3a172b469d3a90572f243117c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:56 +0000

    involve mad result figure pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
