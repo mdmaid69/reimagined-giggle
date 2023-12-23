ce757aa36dd909afcf8e888d29bf059bcc6ba1bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:18:36 +0000

    far attempt combine memory argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
