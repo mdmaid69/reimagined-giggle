d5c9879bea39352fca5571bcf315d695e23e15ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:58:20 +0000

    mother not hard design impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
