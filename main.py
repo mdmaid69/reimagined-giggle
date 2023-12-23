99a2b682d7783a05b42b969e2201834d1d6eaced
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:04 +0000

    affect touch callout contact mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
