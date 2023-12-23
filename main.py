624246ceb628a62fd1fa09ff920208d7344a1c53
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:33 +0000

    march laugh available meaning strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
