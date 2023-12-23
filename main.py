5c920e3f9e37d1e84776ca582f47add1258205b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:48 +0000

    turnout cost bad ear trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
