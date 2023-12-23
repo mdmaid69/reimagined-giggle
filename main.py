c8f947140ca3df7272da04644921e2b5dd976a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:07 +0000

    whatever may bush close undo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
