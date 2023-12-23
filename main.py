55a135cdfc271d51b3733c1369caae5f3ba23ec9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:21 +0000

    trousers out favourite survive peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
