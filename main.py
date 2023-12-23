77fd57f21998be2b3f700683badfda1a856a5af5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:15:50 +0000

    film underneath speak scared stupid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
