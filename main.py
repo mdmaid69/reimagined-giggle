1a2d22daa9651fe8a885719b93fd41db75603420
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:57 +0000

    hello shall basis formal past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
