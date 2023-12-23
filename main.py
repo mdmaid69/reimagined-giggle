a5b58f8c5991128e9fb7625700c0922c6b7c8d30
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:56:24 +0000

    fly mind control notice scrape

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
