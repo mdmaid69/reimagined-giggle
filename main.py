732d353ad8f60958adab12f18ebcc7c787a4465b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:30 +0000

    accidentally post sit weak exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
