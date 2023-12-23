a94e21d5086568e66092d0051e42e8097961ddd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:15:55 +0000

    after exploration mind proper getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
