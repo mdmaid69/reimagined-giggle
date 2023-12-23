fa4e394488324f4a245067ad2157ff8bcaef7e81
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:17 +0000

    half proper worst future sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
