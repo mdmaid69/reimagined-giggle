1b901b2ccf070602cf9de1e47804af6a08948653
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:45 +0000

    snow aim invitation money field

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def add_numbers(x, y):
+        return x + y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
