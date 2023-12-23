b807c8e9c1ce12595932b7a377d35fc0798f6655
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:07 +0000

    pick exist reject achievement minute

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
