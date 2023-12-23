ab248992695866c452c4a4e9b5c058fe52f7c84d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:36 +0000

    private reduce brush original minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
