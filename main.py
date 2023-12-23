33180c3ab00d17a8ba6499ff9c5851febfe383bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:02:29 +0000

    manner trust post main bang

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
