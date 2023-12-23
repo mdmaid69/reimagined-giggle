8dfa3e462e3bd0a8f15904249f57047f5734f621
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:10 +0000

    cover complete share pair knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
