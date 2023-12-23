60bb47107a14913f4a010672050217c2acabfc37
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:11 +0000

    instead promise consider reduction offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
