6d950dd3594ebfd8439429e8f9d382764aa9e759
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:46 +0000

    my late desperate peace impatient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(10): print(i)
+import logging
+def log_message(message):
+        logging.info(message)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
