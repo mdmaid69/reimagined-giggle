1a4d642dd89151606a30701962634f7890cc5363
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:35:55 +0000

    exact success trust will scare

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-print("Hello, world!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
