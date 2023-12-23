cc20676511a04ea1ebcc543f2638b3a5401a2dfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:50 +0000

    pump speed wealth glass whisper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_max(lst):
+        return max(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
