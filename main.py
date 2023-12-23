8a4dee413f8ee72a1df98550aab369684d775bb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:49 +0000

    ago continue hardly be anybody

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
