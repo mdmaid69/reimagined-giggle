e64af476e69f22f0d8d185847081d8b96e0a7437
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:53 +0000

    immediately mixture chin practice attempt

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_as_set(array):
+        return set(array)
+  def subtract_numbers(x, y):
+        return x - y
