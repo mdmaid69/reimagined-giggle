cdaf47cad204d8423e62bb32da62a6c42c88152b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:28 +0000

    offence mixture woman strike content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
