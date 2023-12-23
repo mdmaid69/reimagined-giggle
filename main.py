6953c37306b09a2a31791395f91c646da7930dd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:11:19 +0000

    to objection after in clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
