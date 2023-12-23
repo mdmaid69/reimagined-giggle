f499b5bbd84dcbb86956abf8cbc6ce2e57cba60e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:13 +0000

    principle ring calloff occur Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def split_path(path):
+        return os.path.split(path)
