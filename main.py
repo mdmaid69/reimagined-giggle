178af4394efeae9e901294eb7628ad0169e5b92a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:57:48 +0000

    twist pull person majority basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def square_number(x):
-        return x**2
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def reverse_string(s):
+        return s[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
