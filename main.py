650b268beb683e4194e6ee375b7e840eb2ccc4df
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:26:47 +0000

    rate upset introduction shoulder reliable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_pressure(force, area):
+        return force / area
