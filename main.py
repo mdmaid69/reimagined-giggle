c62731488f7e15a4ae3017cb04cf5895f21f0910
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:47 +0000

    left 572enter cruelty shout substance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
