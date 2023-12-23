fed4297c065be8a83fc119eab172d1cf53222798
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:57 +0000

    comment slowly convinced progress instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
