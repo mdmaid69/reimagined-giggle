d6f482644f515d2570819074a049023d03d826cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:44 +0000

    middle disappear comeround born flight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
