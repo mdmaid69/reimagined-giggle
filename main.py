acb68ac17c3bbf796c99d5933bbb8442e93c449d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:29 +0000

    disagree sign crowded disagree guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
