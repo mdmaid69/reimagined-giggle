ab2ce631dd9013bfc1f12341c59a4dedd9108e15
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:12 +0000

    wish address noone reduction final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
