15933f1619344f7f164ab3a75720b5e6784df013
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:04 +0000

    spray much free short will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
