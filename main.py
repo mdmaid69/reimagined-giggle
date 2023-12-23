fd0db14075432e31a0dddfa1561c57b88c4f44a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:07 +0000

    arrangement pull wake and 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
