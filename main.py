8ca3705d0c4ef092d891c042d03615a8e5cc5174
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:01 +0000

    place split suggest appear myself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
