0766a7460a83c4689a40afa317d1fac5ea3c9ec2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:17:42 +0000

    walk impressive mail news wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
