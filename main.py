e0493d7d17308809721eb0f865811c02a98db085
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:32 +0000

    hospital protection middle question getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+print(sum(range(10)))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
