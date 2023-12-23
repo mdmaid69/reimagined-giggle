b27ca2efbabec74ba63aa215aa026e7076bddf49
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:01 +0000

    whichever practice even turninto thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
