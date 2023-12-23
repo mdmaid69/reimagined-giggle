82b055898d2454d7d4d76ce76736b3a5983484e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:10 +0000

    tie unlikely tall hurt immediate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_rectangle(l, w):
+        return l * w
