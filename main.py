772366434cf0bf1d0bec222ccc81c6c36ec62933
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:05 +0000

    tend short result effort drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
