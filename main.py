fdc78d79d6c443066586bf34ed0dd98b199bb066
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:34:53 +0000

    argument dry our label straight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
