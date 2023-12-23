d207c57935944eb0a24517e2a8d6b1baea5ae57d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:45 +0000

    sense equipment cover sound exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
