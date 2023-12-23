a00e3bb6f1e3fd8854e4c71667c4c71c851fde8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:59:02 +0000

    modern family straight scene peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
