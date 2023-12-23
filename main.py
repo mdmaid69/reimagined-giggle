ac58a9ca2d250537cd707f6e80cfd4747d95df68
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:56 +0000

    dangerous walk invite trust fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
