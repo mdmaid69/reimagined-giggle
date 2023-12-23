bc2c672bb5735f8da1433b514b9dfeb918dc01c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:41 +0000

    exchange secret introduction any everyone

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
