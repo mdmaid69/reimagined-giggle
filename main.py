da732eadbaeb74e56ec657e08df1b79eaecbae17
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:03:56 +0000

    reduce realize shine prepare tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
