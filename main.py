48e46e007824638a78926b658ee62b1c2d9e5c4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:08 +0000

    chin object fortunately grand immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
