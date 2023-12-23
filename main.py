d3712f0c7ad2add2d671328aa7f3361fb5f5410b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:17 +0000

    this listenfor he dick design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
