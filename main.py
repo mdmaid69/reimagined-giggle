6ded41892e41c142f9f41934c46ba7ceb772f62e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:32 +0000

    live turnout study slow mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
