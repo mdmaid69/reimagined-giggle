b0c908f5e3514b3d0f9be7f397f001d9cdcea273
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:01:56 +0000

    guard how though attempt complain

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
