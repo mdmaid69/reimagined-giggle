cf6111b7fa1de5634738c4028b8d7c437f10217c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:07:36 +0000

    modern load neither convenient organization

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
