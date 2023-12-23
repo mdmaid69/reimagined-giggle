d0368250f4ae4fb3a4541c619365dc593c52735a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:37 +0000

    say public period favourite hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
