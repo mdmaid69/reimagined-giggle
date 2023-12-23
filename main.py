65fc091b98952f44a46e8c668d4f191459740a56
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:09:00 +0000

    build fear destroy afraid soak

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
