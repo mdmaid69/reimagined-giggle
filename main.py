392e97a10b6aee572f1b2d66fe77054e985d076e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:03 +0000

    rich again rest bridge sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
