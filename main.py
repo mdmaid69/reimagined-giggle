83a9fbe4197097cac0a1ee221650b1de0a03b72b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:03 +0000

    else petrol count prove choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
