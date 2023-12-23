91562da5d831b9fb3f4add7eb65eb0164bc78406
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:07 +0000

    consider fun hide when turnout

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
