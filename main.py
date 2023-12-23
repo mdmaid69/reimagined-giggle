e3bfd6b6409a79553fe175ee936e392789426989
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:05 +0000

    habit sweet window pretty Ll

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_speed(distance, time):
+        return distance / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
