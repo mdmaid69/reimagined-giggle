02a3b0f9c5314e69562d52eb34d34257aa0b755e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:18 +0000

    place suit bottom famous sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
