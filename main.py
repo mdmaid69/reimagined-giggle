ba6c514cfcdf78a502597477f9f02f465ec4da5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:16 +0000

    kill destroy will pile heat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
