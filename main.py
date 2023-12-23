90e2436ac0a93364ae38a5275c58ebd627a6b6e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:41 +0000

    plain inquire hello shut confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
