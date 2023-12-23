0c8f928cd51eab895eb6cc233ebcdd06940a29cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:30:36 +0000

    runaway class picture building gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_min(lst):
+        return min(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
