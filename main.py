c29d1c5a0f2d0a70725934215a6ae37e22094c0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:48 +0000

    lie usual turndown pause report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
