73f24db103f40fd6b5736a63fdd613bb054f484d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:26 +0000

    intend fortunately Ss move tone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def multiply_numbers(x, y):
+        return x * y
