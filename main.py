8b4ff2e7d8b927f4e7b2ddc9e4fd0c569d63972f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:59 +0000

    cut meaning prepare about confident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import random
+print(random.randint(0, 100))
+def sort_numbers(numbers):
+        return sorted(numbers)
