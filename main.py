4321f9eac793c4824ffcf4c0752172e814766350
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:28 +0000

    shame crash square as whom

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
