692d8071469a50b16362788682961809440078e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:21:36 +0000

    open behave spread sense lock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
