611fc383b96c044848a626a257fb1bbf3ee8c816
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:11:14 +0000

    immediately skill godown lookafter perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
