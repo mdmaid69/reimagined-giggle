db66e91871083e45d473815310bd1d09776efc23
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:23 +0000

    congratulate become used corner steam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
