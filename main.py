f1ce91760286242772ae99aa43c9154b709dc52b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:53:59 +0000

    prefer standard cope friend into

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
