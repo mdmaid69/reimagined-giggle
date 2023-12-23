8c1da8a9531a5f07b3c05e73c1a59e940884515a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:00:54 +0000

    stamp light early tidy fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def sort_list(lst):
+        return sorted(lst)
