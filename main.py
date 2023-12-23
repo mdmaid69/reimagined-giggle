df070aa8e039e78adf4765253f0ae06fca03171f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:33:44 +0000

    kind corner recent meaning Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
