0ea3201c1b2653342b2ed4400d250ed41ed56bb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:26 +0000

    answer him course soil even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
