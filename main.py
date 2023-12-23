8af720174566d16051b1308a2ba4b6c0a25ae4d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:41 +0000

    forever lean refer light wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
