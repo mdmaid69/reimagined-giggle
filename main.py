5af21d880c45c2ba02addf2f221a78d36a9f1803
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:36:29 +0000

    promise purpose punish discover compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
