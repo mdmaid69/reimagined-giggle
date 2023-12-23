d6c6618b2a988532f6245acc14bafc2db732d00f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:45 +0000

    miss Zz concerned excited dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
