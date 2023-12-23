cd555749edfa077a8def67db9ea8a7139ba04b27
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:04:55 +0000

    stupid ever during chief switch

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def sort_numbers(numbers):
+        return sorted(numbers)
