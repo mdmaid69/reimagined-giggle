7cb1ac2461201ca89cf00ea2661a173b693d3989
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:31 +0000

    suckme pile paper subject real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
