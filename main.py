8fb98bec06ed18c32b13b1629361f2cb3776b9d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:07 +0000

    square because emphasize hurt sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
