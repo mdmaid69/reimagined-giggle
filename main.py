ca21b0ed22830c954194b8eac11bc23e9b1a96bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:57 +0000

    receive slight bright various fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
