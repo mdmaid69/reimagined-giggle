a7565e13dbd1e5aba8e9fa4e88fc6b048cf25341
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:49 +0000

    unit proud right likely notice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
