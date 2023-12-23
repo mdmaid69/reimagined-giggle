cc87f31ec2fad88b0d3f7a01cca6b636df008ccb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:53 +0000

    home suppose treat switch back

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_work(force, distance):
+        return force * distance
