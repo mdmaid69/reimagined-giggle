5d6fa678eb79fadc18189cd3050e38cd4b46beaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:54 +0000

    club exchange objection rank still

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def count_elements(lst):
-        return len(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
