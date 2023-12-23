1e219d2768995b2aa1ce6880677a8bec8069f1c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:50 +0000

    beside night hurry holdup injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
