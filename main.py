2a30ab385181d8bd34be953c7a6ff4f70d56ec2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:03 +0000

    forget arm share design after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
