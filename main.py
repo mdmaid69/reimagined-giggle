2f2607f2a16603e61607b5dd38cab981f483177b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:21 +0000

    boil minimum nor top trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
