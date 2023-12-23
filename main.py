20c8b1245cf8af4d03ccd0f0fdec6b6043f2f428
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:24:29 +0000

    example label dangerous city pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
