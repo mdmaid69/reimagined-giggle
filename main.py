2f24387a53fa652a331ecae8808f07bb40f070e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:10 +0000

    takeback deep fresh profit genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
