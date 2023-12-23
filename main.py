a97f7aaa61c0536c9267989a4236a85c8f89dbb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:34:02 +0000

    last handle nor event watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
