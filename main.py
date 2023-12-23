e5b5995746efd1c3b09b8762f57369b6f2515d4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:36 +0000

    phone please awake bend criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
