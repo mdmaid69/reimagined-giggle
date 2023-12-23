dbaf4f70daed97f65c2375c45f502fcb95f760af
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:34 +0000

    wear spoil close interrupt fond

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+print([x**2 for x in range(10)])
