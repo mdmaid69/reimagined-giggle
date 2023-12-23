cc16f0468d576d6f392aec487b8d750eae6c237a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:29 +0000

    blame chase success lean search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
