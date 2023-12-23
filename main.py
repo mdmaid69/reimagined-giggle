5502fdfa5742c13c843f907f87961b821ae60ac8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:10:47 +0000

    adjective guess her successful hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+print(math.pi)
