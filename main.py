53d1401cb8e2a366ca32aec2808f92e4851d6529
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:42 +0000

    wrong ignore watch but godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
