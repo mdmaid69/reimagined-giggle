47d5efaf12b5080ee19fb113ee8b332ef6d76fc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:03 +0000

    Uu sometimes get let breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
