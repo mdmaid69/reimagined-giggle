6dd0cb2611fa76594c1adbe14b5703f269cc8ece
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:36:29 +0000

    bet ignore rarely beside form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
