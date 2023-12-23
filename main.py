e698cd119edd2513ed6c3864b2dd06fe8a4d9b6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:54 +0000

    team introduce post some post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
