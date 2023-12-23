1dbf2832134a98c6eb346c3382494e300964ca57
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:25 +0000

    occasion exchange black undo bread

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def clear_array(array):
+        array *= 0
