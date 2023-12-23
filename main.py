2d18f8df98af9271081e97836a29f3fdb0faf766
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:02 +0000

    study by bringdown detail crash

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
+print([x**2 for x in range(10)])
