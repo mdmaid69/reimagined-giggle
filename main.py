c59fca025fd01f11d60d6c7ebf872577918748d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:41 +0000

    attitude shake standard business rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
