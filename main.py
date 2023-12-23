030a5769c74c3a1670afa301ad47ddc1acb43b53
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:27 +0000

    throw protest command wherever hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
