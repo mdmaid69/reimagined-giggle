fd0025fda4ef16472478522cdaaaf64b21fbe99d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:35 +0000

    somebody sing fire practical even

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_area_rectangle(l, w):
-        return l * w
+print([x**2 for x in range(10)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
