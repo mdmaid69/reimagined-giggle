bf3663fb2041bcd44a16cedc02376cf5a07333d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:47 +0000

    anywhere either week quickly bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
