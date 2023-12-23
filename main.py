0dc8e323d8281a572969dbd40c29f8a6fd89868f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:42:06 +0000

    expression warning money set Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
