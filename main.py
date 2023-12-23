ed827052da616eaa8ffd7103e2e572c70a6f2890
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:14 +0000

    important will blue announce rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
