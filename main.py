09fe8cc03e9ea9bcdf24c1e4fec3365c065c9490
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:45 +0000

    shock goon Jj close central

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def cube_number(x):
-        return x**3
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
