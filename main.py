5895d77859d6a539cf500a0a980a8d8cf0da6aa3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:52:56 +0000

    promise force tape sink trousers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
