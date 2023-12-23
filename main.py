ed935694b78161920bcd28957b88da754b62f10b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:09 +0000

    look this tour have measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def get_current_working_directory():
+        return os.getcwd()
