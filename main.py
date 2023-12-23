70fbcd24aad4b16d078ab323d85ca8d97d059430
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:16 +0000

    hole small one reliable spill

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
