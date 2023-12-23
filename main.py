3b217a4d9d45527607bc6c5f8018a4d60a97157b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:02 +0000

    prevent path extraordinary catch suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
