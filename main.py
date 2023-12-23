3ae88a971bc5140be860105a70ba706d3722e70c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:10 +0000

    familiar clue belong mix private

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+print(os.getcwd())
