43f17660786d8eacfeaebd3fcb1d5b3d5c6ceeb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:48:51 +0000

    guess money breakfast decision rank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_hex(n):
+        return hex(n)
