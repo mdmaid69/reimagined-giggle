10a83e3ac8e2c9da5db167fafc935609df2e1936
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:21 +0000

    delight thinkthrough trust question conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
