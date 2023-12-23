a981823247f5b81d05b53a05d488b9cc5e45e699
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:28 +0000

    compare tool usually pot content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
