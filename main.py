8ab1fcf9bc5c8f2b81a09117208dcc33f30a4b48
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:01:43 +0000

    wish shame start different blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
