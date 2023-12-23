525d58d3960cf2515870453bc2bc2705f29c29ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:02 +0000

    think fear pool surprised full

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(10): print(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
