0fdb3c77094d92818ea5de390ed5f5ee195ea04d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:37 +0000

    566end slice should sweep public

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
