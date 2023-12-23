6ec06bc1dc97506095ca5a992cc208a77f4904a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:39:55 +0000

    objection tie fault continue when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
