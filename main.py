8a9435d4994d3b2387d466fe55ec30088a76d3ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:00 +0000

    fire beautiful minimum bin glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
