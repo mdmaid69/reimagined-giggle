bf0eac5b7e43985e491c77c64c08c30ded4f7449
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:21 +0000

    tidy some excuse whistle rest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
