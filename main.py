86c34e645222e2f96cb1d88e80f11760a432177b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:56:37 +0000

    load tap worst invitation argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
