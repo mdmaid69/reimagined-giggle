a77457df1a1d5998df895ef4519e162ecd5727fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:52 +0000

    produce by operation contrast either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
