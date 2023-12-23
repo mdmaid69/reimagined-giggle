38d2683a8d8d62dbb7189142510b4fee22838254
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:01 +0000

    judge thinkover injury nose picture

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_volume(length, width, height):
-        return length * width * height
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
