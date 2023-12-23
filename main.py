d0460985a82524573637580d86dd47d13bbc2cf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:19 +0000

    watch Pp instead mad exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_typecode(array):
+        return array.typecode
