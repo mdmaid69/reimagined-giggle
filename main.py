5fb3e346fbc204076e3d2a6b3bcdac9726e883db
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:06:04 +0000

    behave favour cross breakout lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
