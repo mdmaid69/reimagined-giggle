64c38af76193db1ed2870755fb69fa13781af348
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:57:22 +0000

    key pour speak shop guard

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
