a41c6001799cc4f9fe1330855132679a6159bc44
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:56 +0000

    bridge fit string entry affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
