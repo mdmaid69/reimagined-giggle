7ca9880510685951b74cfd8c01bcefa80c238c9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:42 +0000

    rescue 572enter choice piece programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
