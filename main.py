a3d7be93e4f77154c995ad987b05872ed8576995
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:57 +0000

    bear naturally cause destroy practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def exit_program():
-        sys.exit()
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
