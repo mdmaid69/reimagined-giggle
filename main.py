5a0406668a3ec509e99ce734d6f7791d7653688a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:56:29 +0000

    excited fast happy successful draw

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
