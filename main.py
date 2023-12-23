39e76da07c83773d3443c900d78c340631ad7174
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:12 +0000

    order need loss other rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
+import platform
+def get_os_info():
+        return platform.uname()
