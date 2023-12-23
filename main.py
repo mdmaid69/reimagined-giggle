f03825017524a3dd3117f4028ce1fc5cdd163032
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:51 +0000

    exactly substance spare block meet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
