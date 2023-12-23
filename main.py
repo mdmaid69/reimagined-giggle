57af14ad920afbaed8a20f5fc933ff6bdb742f9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:21:20 +0000

    adjective callout move claim suffer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_max(numbers):
-        return max(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
