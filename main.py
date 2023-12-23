5780d26b703da86815710b13d71cb6bd6c6a35bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:16 +0000

    afternoon probably whisper 570enjoyable curious

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
