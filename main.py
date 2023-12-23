142cb1ffd5983e472039d1e59d79b4fb8646775c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:23 +0000

    claim boat however ready position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
