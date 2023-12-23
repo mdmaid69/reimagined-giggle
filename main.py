0e322bad64d8d0519049951310b2b5f72a5dfe38
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:24 +0000

    Itself test callup content build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
