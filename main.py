bdeb3975a3fc4092b06a2ed2320a541c33ac2cf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:09 +0000

    wise block listen write crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
