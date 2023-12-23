1d0a18175ebddd52befe8e61cbdf669833fdcca7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:43 +0000

    Bb lesson tread move fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
