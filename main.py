c9dffafb3bfd83d94249a2c097799426ea3164e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:25:11 +0000

    coat tap comment occasion main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
