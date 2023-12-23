5a434ad52f8de02c2c915ff396f112dc750394d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:10 +0000

    bringup odd pull regularly put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
