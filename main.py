9f21028be4dad45e70664be409a5d33932eb0aa9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:08:19 +0000

    even hers immediate ask you

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
