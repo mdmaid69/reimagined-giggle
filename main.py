1a069f88e7eedd5630ec581e6840a50e06a3c640
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:16 +0000

    settle brain put activity force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def append_to_array(array, item):
-        array.append(item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_array_as_int(array):
+        return int(array[0])
