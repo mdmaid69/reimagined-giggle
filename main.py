87a48cdc38bc048cd7bd8b8bbeceb2c2d533851c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:22 +0000

    time workon reserve bright negative

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
