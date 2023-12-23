4ce5b602cfa8c767cb71e9271e815b9e167e6e58
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:57 +0000

    risk talkinto once best lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
