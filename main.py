386b15536e3bb11206d93dae56fd931c7e2f12a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:58 +0000

    persuade Nn sorry skill roof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
