df7df2150b9bcebf7e41c5368d89928a1f4547f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:30 +0000

    worth far tour last like

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import time
-def get_time_since_epoch():
-        return time.time()
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
