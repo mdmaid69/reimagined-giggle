ec23613fe4a323412e5c71f14fac6c950ce72e0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:34 +0000

    difficulty divide century reaction harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
