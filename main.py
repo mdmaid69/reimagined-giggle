6dc6be29c780af73cefa7c7dc1046b3dd608dcec
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:09:18 +0000

    record wind wash task yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
