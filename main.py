809cd8ce0b37b3bdf97479a44279eae0d06953bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:57:54 +0000

    good suit rest steer afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
