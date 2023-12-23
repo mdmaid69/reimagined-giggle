dc21bf1cc5ff3c6b32c7f54ce342e8cb2664068d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:51 +0000

    little turnback tap show welcome

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def greet(name):
-        print(f"Hello, {name}!")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
