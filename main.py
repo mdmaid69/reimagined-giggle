d286753a5c03c825dc349e2be2fba8a55aa7aa1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:28 +0000

    separate wire sticky impression you

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
