690dd7c6459ee293140810ce7657730560088a0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:27 +0000

    shine thankyou turnout spill open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
