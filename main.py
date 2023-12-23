be5a2ab1c904255f7d681d626e0fa42e51caa41b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:54:26 +0000

    quiet dinner record agree people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
