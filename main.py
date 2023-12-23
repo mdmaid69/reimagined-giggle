776489c5b9798b8868c66b38666177b508fd67e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:11 +0000

    increase bunch smart store Oo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_complex(array):
+        return complex(array[0])
