0d13ef8b1b1e2a11d0b3a687d77580a6eb0a1a7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:58 +0000

    moon friend impression behind home

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
