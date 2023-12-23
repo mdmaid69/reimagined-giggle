4f616bbe2b0d3592763baf273cce93f43a792baf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:50:40 +0000

    her Ee night fire mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
