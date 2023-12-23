ab17a57339a883999f3e21345ce4208fbd4362a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:33:35 +0000

    unfortunate intelligent squash noone defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_pressure(force, area):
+        return force / area
