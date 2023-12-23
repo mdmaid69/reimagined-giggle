e1cdb0d2bcfb04188e0a0f731fc31817669122da
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:10 +0000

    vision bright keepout opinion bit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def add_numbers(a, b):
-        return a + b
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
