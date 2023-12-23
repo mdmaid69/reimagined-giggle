b3d28ed66db2cbaa9e764a5c6b79200de80782d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:39 +0000

    advise goaway record insist music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
