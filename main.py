01a1837e3f2ead815ec1316ffed52820b12781b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:06 +0000

    towards ball pour whistle call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+print(os.getcwd())
