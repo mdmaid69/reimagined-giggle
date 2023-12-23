74b48c5b2397207522007b611654bdfd3d56419a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:02 +0000

    popular level hide heavy wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
