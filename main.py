8a36a23d87921b84200a72365de887ec051bad80
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:14:15 +0000

    through length statement cruel gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
