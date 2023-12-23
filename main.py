825d40d06f7559388502b8407ef67a6cfd6e0b60
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:38 +0000

    quit friendly communicate honest side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
