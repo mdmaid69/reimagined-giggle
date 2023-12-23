622089d8345b312af550a87fd7061d1c1ac032e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:28 +0000

    whom lay habit new spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
