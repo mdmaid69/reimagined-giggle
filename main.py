d659f415a58951f0b82da489b5ad091bdefa73e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:46:29 +0000

    bringforward touch squash cause pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
