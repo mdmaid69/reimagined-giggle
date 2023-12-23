9eca71f548b7fc8dbab68bc573f658c23b0b5e68
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:28:23 +0000

    suggestion poor relief anywhere production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
