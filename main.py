9f16787fb67edbbc9e1d3e9f6cb02b64221343b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:32:57 +0000

    unite food command rub content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
