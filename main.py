577cc4bfbc734708e29fbee594c188b863740676
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:14:06 +0000

    refuse plenty guess strange level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
