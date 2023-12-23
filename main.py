51fa32ec9307824f566691e3ff69ece5d41627a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:18:51 +0000

    condition remain explanation abroad father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
