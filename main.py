8194cdf7e9929290beaeda86f819caca24453951
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:39:00 +0000

    fear press list arrangement cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
