b99ea7c7b2248a1bc3e761abff6841738127c6d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:22 +0000

    translate rough brown discussion meal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_acceleration(speed, time):
+        return speed / time
