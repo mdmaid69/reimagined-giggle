f2d5082f89e03edcc7be4cac966727592b4b7d1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:43 +0000

    downstairs increase confidence work wish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
