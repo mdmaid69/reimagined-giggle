993f6e43edb0d832654f9bc4acf391e6aa427a3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:33:35 +0000

    please comeoff turnover return unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def subtract_numbers(x, y):
+        return x - y
