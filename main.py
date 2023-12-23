75f7849285279fd900f3f201de9ec03378102d55
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:51:27 +0000

    regularly contribute demonstrate business hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_acceleration(speed, time):
+        return speed / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
