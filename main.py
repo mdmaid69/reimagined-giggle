9d7020b57dedbfbaaaec85bc8f354c2b901b9fd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:49:43 +0000

    aware slice print improvement above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
