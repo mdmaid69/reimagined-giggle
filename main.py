464692a88d5b8c935f0b61933e5f23b4bd6c8dce
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:18 +0000

    fit advantage normal closely disapprove

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
