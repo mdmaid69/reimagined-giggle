e827195d9735e9fba3964acfbe3cfcdcc7ff8dc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:05 +0000

    flavour minimum connect own comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
