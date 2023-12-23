dffa1fd902927b7bda10fa3c4cb87e68d6e0e8d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:33 +0000

    affect burn passage commit recent

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
