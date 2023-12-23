10d7524b8a4fd48a1262843db0c43f3a0300fdc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:50:49 +0000

    loss sweet exploration snow blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sys
+print(sys.version)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
