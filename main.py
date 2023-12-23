6a13030883f6ce7ee146dc15840cb4cb35d28873
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:12 +0000

    come temperature cruelty ocean dark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
