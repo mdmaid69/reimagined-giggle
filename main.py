b737f8912d25dc67fb7c0a31f0da4fe33d96bbd9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:59 +0000

    key suddenly hurt command obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
