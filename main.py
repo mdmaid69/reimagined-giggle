7bf3e6e81668f917020abe506a158c72414ff4c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:48 +0000

    straight must accept practise offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
