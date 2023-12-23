417f7179de260dfa857c194b9c7e2e2d7da023a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:45 +0000

    radio pool properly excited play

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def count_elements(lst):
+        return len(lst)
