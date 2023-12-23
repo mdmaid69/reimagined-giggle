1be9e03ebcc41680e8f747876693a34fda6a856d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:18 +0000

    company confusing provide finish quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
