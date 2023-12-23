79eba7fc79cd690e49e53105840d20be9b5dfbe4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:51 +0000

    load rubbish lookup moon slide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
