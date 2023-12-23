632eb755d2c5114924d5fc90c3d6c2c74beb5df8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:45 +0000

    love cope 573entertainment practise state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+def exit_program():
+        sys.exit()
