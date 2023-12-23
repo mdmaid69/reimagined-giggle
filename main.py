43f02494e342e9209d764d0091106d8066e833d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:39 +0000

    outside one bar answer degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import sys
+def print_python_version():
+        return sys.version
