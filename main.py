19acf892495f5315f3ec6b5e779b8f9b8a2c4375
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:22 +0000

    star bye achieve bottom themselves

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
