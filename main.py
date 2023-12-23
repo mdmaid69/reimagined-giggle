3a21dbc63bf2b1cef9c283bd70a51d8c79107b92
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:10 +0000

    relative smooth boring compare own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
