76ad792150235e98fa5abd2ecf838ff9fd743f26
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:22 +0000

    wet waste Pp turn guard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
