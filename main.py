123c6252e635479636ece136032b30e1c575ced5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:57 +0000

    foreign least wrap used regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
