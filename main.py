e4db87fd8152c5373d794051f89a3c8c3a14d8ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:12:45 +0000

    welcome down tell method accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
