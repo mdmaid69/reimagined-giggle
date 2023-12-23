3f94107a4dd0ce702bd1a021dc9df1e38dd68115
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:43:34 +0000

    straight history absolutely treat name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
