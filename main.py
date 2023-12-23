4bab8b2c8e7fa5c47962c145932320a96de1d5a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:23 +0000

    miss rather best certainly use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
