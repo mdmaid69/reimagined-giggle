75075a3f5a319a2f901b567ff0c6d478de76b655
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:22:51 +0000

    below one fact plenty murder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
