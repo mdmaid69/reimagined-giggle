2b9055f6c0d4c792b22c078ab5cd7baa232ed706
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:57 +0000

    anywayanyhow very keepfrom false definite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def count_elements(lst):
+        return len(lst)
