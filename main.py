f412a72afb819a6e679fde8e71ab79e592d5fb07
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:09 +0000

    comfortable gas poor connected study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
