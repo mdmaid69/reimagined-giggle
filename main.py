45480e8d8a49bcb2abb8260799513c5283bb0f8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:29:15 +0000

    attract announce us group ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_str(array):
+        return str(array)
