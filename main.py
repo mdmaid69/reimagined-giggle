8abd80a4278efbd7940f8f4430f51fbe712051bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:37 +0000

    argument comedown cost event join

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
