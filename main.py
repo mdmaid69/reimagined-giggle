2d89f67ec3e61335a39bd4e866cf7252aa2d670a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:29:28 +0000

    protest be ours free kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
