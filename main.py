657a24bf3497ff8425d7f2548ae43085983756f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:05 +0000

    crisis tradition likely tap constantly

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
