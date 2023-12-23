b084eec7a617ac4c89ef248b5d6051a61bbb4734
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:32:18 +0000

    wide direct inform base far

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
