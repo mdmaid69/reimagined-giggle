156d440c257b0ba888ae218f72ce4c48bc2fd884
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:33 +0000

    accident independent take hate expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
