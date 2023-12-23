7f2782b533751c85582e6b6a1d514c21790b3ebb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:51 +0000

    getback now file greeting loss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
