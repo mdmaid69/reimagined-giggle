1d55cc5e88f1860d1db0877b6270e58471cdfbc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:42 +0000

    drawing afternoon fill speech carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
