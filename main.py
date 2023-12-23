92df7121512742acfea108215b391468203573a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:15 +0000

    age nervous turnback confused rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
