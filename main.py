9dda252bd09764f9b0118f77df04dc9dbc1cb21e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:28:19 +0000

    avoid finish crowded cinema branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
