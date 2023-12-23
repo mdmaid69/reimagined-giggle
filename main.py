4ca6ae2b0fcfdd47189a638e2dc0ad813ba5294b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:37 +0000

    trial him dangerous share patience

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
