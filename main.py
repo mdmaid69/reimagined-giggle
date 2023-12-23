468736810848846a9980436cf21e260c0faa5465
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:15:55 +0000

    fill performance world Vv guess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
