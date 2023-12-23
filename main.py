def5839c25bb9bbb4ee9c7cd2c269f72f2e5e339
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:57 +0000

    long beautiful loud fortunate blue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
