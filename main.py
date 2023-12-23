d9dafc66aa0ee8c984a2ddded1a75af1eece3655
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:05 +0000

    return spite nearly fit runaway

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
