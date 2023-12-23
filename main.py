5d8c1d075ec5dc724bbcf825024870892f821c5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:21:44 +0000

    loud total will big pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
