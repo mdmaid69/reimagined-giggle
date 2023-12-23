c2cc07fbb0004a4fe315dfd21683b9bf6b11d67c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:07:06 +0000

    train phone figure crazy definite

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
