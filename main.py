e74623a6b844d42d462ce79b17a189a3af5509f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:10 +0000

    scared offer them upwards nose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
