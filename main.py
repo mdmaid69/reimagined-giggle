18edfacdb309a96f3ab8ea6f5a937c5c526f5711
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:59 +0000

    bringdown Uu order on sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
