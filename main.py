e838a5330f3bd0e123c15ecbc2502d0fb7029510
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:23 +0000

    glad police Ii touch finally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
