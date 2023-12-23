4403a88782d638c36895cd45952081810004f6d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:53 +0000

    off describe go head steam

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
