af73ee516a5bcff00089b7a2ce8bef00c70f6b67
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:46 +0000

    mine communication shall sink guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
