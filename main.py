de6a01cedd6458b9d9558076f8646f52c7939bf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:30 +0000

    breakinto method discuss wind listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
