d01db30dd0c013241b810ddfe0ab2e39ca7e5b3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:27 +0000

    safety attractive common smooth address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
