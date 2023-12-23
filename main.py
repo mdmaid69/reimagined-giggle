d3fadc2c1dc9b867ca4ee3d8de38cbfd27392c7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:27 +0000

    love evening workupto day deep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
