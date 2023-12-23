996bffe9baaa9ffa02f84a64bdcdc24075dddbfd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:17 +0000

    acceptable check turnaway interruption suckme

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+text = "Hello, world!"
+print("Characters:", len(text))
