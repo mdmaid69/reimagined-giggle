6fc4445199c00ac1b7ea095a54c1715c76e68306
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:51:57 +0000

    central deal smoke storm stamp

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
