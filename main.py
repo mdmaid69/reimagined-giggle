5948def26518554c867ac9165b8263e94f92fca5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:58 +0000

    judge programme wing cost clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
