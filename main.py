341fffd5e53980b16c62472874d12136c9e58f4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:16 +0000

    wealth protest estimate pretty whisper

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
