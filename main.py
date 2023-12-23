aaa90f927244b8b9b746b23a1df487938bfbccf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:41 +0000

    favour wave rent science situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_volume(length, width, height):
+        return length * width * height
