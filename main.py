426beef7cc977df454889d611dabf92f8313d20f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:49:20 +0000

    game other calm risk smell

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
