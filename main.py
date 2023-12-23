fcec9c955165960e9c1dde6d37ca8d836fc5f975
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:44 +0000

    passenger store proof comeoff wise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
