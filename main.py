3a2bf86f0773423bde388c80c021ef810f74cb68
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:48 +0000

    bring expensive thumb conclusion of

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print([x**2 for x in range(10)])
+import time
+def get_current_time():
+        return time.time()
