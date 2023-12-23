d6d8995c91202bf37bb87c784cd9e6a39a1bc55a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:35 +0000

    shoulder grateful dear tragedy mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
