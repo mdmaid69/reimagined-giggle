9b052e67f8aab804d5a0acc8e0dac44ae606cb8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:41 +0000

    objection ignore turn appearance chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
