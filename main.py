3d511521d081f712f1f95917b26d405afb9a109a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:23 +0000

    estimate hope sore return leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
