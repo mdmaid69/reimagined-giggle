ce3f044c7d0ad94a2622bc602d069f2c0f850e5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:19:44 +0000

    emergency rise goodbye circle declare

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
