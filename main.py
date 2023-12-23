d8063f6119e4cf73a27bf08c5b72bd5ffe28bf87
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:52:48 +0000

    cook off serious set whenever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
