7fc4e7c11ddd07eed26c8c59d09acf6f22a2cfdd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:10:53 +0000

    flight fond runinto warning company

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
