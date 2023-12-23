3936b9b26b5fe0b9ca79fa86d5a1335e581927ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:42:14 +0000

    explanation method bath fire pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
