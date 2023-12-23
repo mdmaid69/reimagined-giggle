53b49a0f2e34353843a1a4e47fbc92e7f96ec53a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:02 +0000

    voice job what passenger normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area(radius):
+        return 3.14 * radius * radius
