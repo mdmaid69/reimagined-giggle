26d51cf5fa264086c4e3fcb27c0b0630b4e15cff
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:25 +0000

    force perhaps fire sort cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
