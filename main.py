b1297b0074a5fe945fd26e1a2699e29a0d2b4a8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:47:04 +0000

    forgive keepout worth habit pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
