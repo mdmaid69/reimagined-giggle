89e548c6cde6a97100f6435c2f8f58b47ed51692
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:00 +0000

    save tip sticky that bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import collections
+def create_queue():
+        return collections.deque()
