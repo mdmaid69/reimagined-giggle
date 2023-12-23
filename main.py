596249bf7fdefa26f5d9de9e0d16414558557d3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:14:14 +0000

    once introduce value upstairs order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
