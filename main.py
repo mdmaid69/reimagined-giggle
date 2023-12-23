54256a9e2c865be42227dc1fb9ca91db2f801d2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:33:36 +0000

    deliberately such obvious upset word

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
