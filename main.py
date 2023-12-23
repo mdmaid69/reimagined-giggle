5b2c388461b2cf31c30c1da739de07ca1e577d06
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:02 +0000

    promise none activity calloff thing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
