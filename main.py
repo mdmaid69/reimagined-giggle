0bb46e6907d13bd0387eff8d9f856bf13c4d69f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:45 +0000

    pretty without achievement lunch direction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
