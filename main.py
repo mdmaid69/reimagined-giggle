79ec730d8227651d84b428e42e8b50920179cca2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:07 +0000

    often offer fail regret flavour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_binary(n):
+        return bin(n)
