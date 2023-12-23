5234ff47d08bfba8987d4ad9d80a97cdb1a2e423
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:20 +0000

    steal order drawing murder keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
