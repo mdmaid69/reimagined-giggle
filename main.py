fbc931e9dde4c86fd0e58e4e22a709e051fe2307
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:21 +0000

    various high judge tidy relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
