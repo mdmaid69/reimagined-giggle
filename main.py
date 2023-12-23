b5db39642e335912b2f2f89ed49f95ab5ec756e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:08:26 +0000

    part creature mood our unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
