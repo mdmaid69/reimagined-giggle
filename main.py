db7526b0e33d90aee9756db10c96028fd1fea8a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:42:06 +0000

    straight involve post choose discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_counter():
-        return collections.Counter()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
