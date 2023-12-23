13a3c071df00a1a4206834920a9f687e3ec4ce93
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:33 +0000

    out whisper realize agreement paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
