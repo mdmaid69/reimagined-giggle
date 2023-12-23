76e28d0945b6f66e7388d15011acb12680cbad91
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:20 +0000

    certainly minor trade wrap estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
