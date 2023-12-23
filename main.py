5a6ef5417a22490001152e3ebd826594c839b136
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:15 +0000

    persuade consist spite collect great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
