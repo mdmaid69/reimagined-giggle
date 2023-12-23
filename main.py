c7ff7a9c05eddd70b0975ba56de293a0e9a19d26
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:34 +0000

    blow town buy when any

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
