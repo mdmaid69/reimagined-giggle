eff41213ab04c9ca062f9a2b1dc49edb225b6d75
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:43 +0000

    recommend change grateful equal copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
