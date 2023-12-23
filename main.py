7ea5c8a326b3e1aa0c079bb2553cbf651dc0105f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:24 +0000

    face odd part transport get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
