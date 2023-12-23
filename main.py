4539aa05c44f55adac0c9673520159f3c9627587
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:13 +0000

    satisfactory goon emphasize bank equal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
