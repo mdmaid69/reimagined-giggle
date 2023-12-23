e089b305633d37dbf0fded03e0e41eaace2e9ee0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:08 +0000

    mystery settle party appeal glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
