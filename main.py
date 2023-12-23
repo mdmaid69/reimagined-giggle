a0a4e6f6b3288e2fe4ea038bde8a4e5cb1951847
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:42 +0000

    undo welcome door account respect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
