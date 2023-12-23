2c0076853a12f96a4a892fae3cdd1e676531fde8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:49:30 +0000

    sort weather happen show over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
