7d2fafc3ed918973b37acdb9c928fe0357ee9aff
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:09 +0000

    key visit activity wish content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
