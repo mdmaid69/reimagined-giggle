6f7c189645ce2c633db57b86041148c43e83701a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:33 +0000

    shoot disturbing history paper around

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
