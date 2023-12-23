dce73d5fad5fd7c54eb6835ac1f9be0d818ff3db
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:20 +0000

    takeback breakinto last live crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
