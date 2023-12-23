4c696a41a87b6de8ae14e54437634b4d79a7b894
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:09 +0000

    each catch safe disappointed fresh

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def sort_list(lst):
+        return sorted(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
