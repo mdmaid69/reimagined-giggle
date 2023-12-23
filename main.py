65dad63448eb73a0e2cd8373f0af211bac4ba500
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:19:07 +0000

    male challenge break story turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
