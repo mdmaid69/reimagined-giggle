d1325a803dd4892e1871ac74bfdc33259a2bc857
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:27 +0000

    empty same promise choice prize

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_work(force, distance):
+        return force * distance
