1843386f343070a58535da74d39d3597fd46217c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:36 +0000

    protest half explain play positive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-print(datetime.datetime.now())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
