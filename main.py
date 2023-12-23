080685731b9be5d74fb9abf1bee8f4b0ebbd5614
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:35 +0000

    entry cover education state arm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 numbers = [1, 2, 3, 4, 5]
 print("Average:", sum(numbers) / len(numbers))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
