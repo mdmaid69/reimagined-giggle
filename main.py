92c99d11ef0ff519115e2f22a6378d5ee714acef
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:27:19 +0000

    love sound over to advise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_length(array):
-        return len(array)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_area(radius):
+        return 3.14 * radius * radius
