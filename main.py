4d03a89bb94185d9c2d01f9024d50398cb67c3dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:56:39 +0000

    back pupil design fun point

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_power(work, time):
+        return work / time
