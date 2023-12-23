c6e332f983502274531ddc5c7ece467175ed1e22
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:09:43 +0000

    game Ee some bar cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def count_characters(sentence):
+        return len(sentence)
