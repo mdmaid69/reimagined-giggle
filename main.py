e2d70e9d8fc1888764cf5ea94c37262dbdbf2926
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:26 +0000

    definite immediate front whistle come

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+print(os.getcwd())
