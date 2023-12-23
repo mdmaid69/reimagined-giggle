909a30754e0d0934d67c46b474e84058ed2bb08c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:25 +0000

    evening little final peaceful watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_square_root(x):
+        return math.sqrt(x)
