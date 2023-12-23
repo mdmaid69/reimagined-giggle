c916ec5fb90182c4da3de7de6aa3089bcefedbbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:39:10 +0000

    switch worth skin runover seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
