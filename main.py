866a9ec98faab448ed204098170e23968dd7261b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:33 +0000

    me get truth wealth mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
