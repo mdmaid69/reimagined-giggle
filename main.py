689ab171a34e00ee808debeffa4d0600c052abc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:59 +0000

    clever familiar everybody upstairs shake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Hello, world!")
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
