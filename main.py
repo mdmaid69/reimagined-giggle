c4484370f95904fe0195a2085dcd4e24df932d7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:19 +0000

    lose set above behaviour actual

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-print(math.pi)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
