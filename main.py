182158d5a902a077f8a6cdf082a30d38afc26547
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:35 +0000

    possible excuse though recommend popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
