092a2eda558a1e5bd13743c509aadc12b03b30dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:17:29 +0000

    pool report organization design sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
