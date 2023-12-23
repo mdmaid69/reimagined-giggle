29ba7bf979ad3f6f49d97b9874c6c31d95f7670a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:20 +0000

    outside excuse thick share sky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
