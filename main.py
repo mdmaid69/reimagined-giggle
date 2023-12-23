6b49f6d194c9e3aa98fc5686a2e222d14c7db012
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:32 +0000

    workon trip loose hit rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
