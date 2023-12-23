1997276579a39b7af0d0a950ac05be673799423b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:05:53 +0000

    while lift pressure might shock

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
