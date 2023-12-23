1df7e6adb8901ead9553be210cc5d5dd18f954a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:38 +0000

    however press wing join birth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
