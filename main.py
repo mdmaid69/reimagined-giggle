47e7c9a61eb662494d4d3890a53706739131f4a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:32 +0000

    arm image peace soon hard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def get_current_time():
-        return time.time()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def convert_to_binary(n):
+        return bin(n)
