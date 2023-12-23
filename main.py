b33b15a58d03d98e21a4ebd6c74cb5b31005171a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:24 +0000

    themselves forgive Dd excuse one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
