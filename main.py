7d9883bedda27ac8c37f0a9839484107d2cdc500
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:19 +0000

    lunch information conduct present instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_item(array, i):
+        return array[i]
