7d7d3221e511360bfe60dd7f0e9e1abbb3b9c934
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:14 +0000

    who rule probably off couple

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
