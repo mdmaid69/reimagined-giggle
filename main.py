de165f7ea272459cb7d2de25e1e29d1072ec4698
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:27:58 +0000

    degree feel pull head expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
