4eadec87357840d53c5991310d9baa304b857480
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:58:06 +0000

    talkback dream hold distant kick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
