8fa3b45031244c1878c2321f6d91e1cca61ebc34
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:49 +0000

    interrupt influence hope break rather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
