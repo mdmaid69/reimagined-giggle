275d075a938a66460e84e86cf6bad6326efdf151
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:22 +0000

    party tense purpose still eat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
