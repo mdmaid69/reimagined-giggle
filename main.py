90a4d805bbaf283cd86c099ae5dc32462b46fa47
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:18:16 +0000

    temper walk stir fetch partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
