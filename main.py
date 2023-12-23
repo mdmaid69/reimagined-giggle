35a9eba03410bf1f33384cf76ff59379a3dc409b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:14:19 +0000

    whole danger complaint nearly Dd

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
