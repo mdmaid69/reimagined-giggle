b38959bf6d9772f9b870066cf1b3ab56c917e828
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:27 +0000

    hang surface final believe formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_item_count(array, item):
+        return array.count(item)
