10aa5df735a407439322ad667db34fa31f3dd61e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:40 +0000

    ear constantly town shrink tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
