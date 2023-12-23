ad73a46d7e2f5650ec0b30dbb310e167ea166cc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:08 +0000

    sell fashion upset mark fat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
