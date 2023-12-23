10e03312eacb47a5475cb5e7c99e07873e43299e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:43 +0000

    stick obtain on closely tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
