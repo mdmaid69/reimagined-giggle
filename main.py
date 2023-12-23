abfe8cc2c222c38318c1593581a60b87afb3db96
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:33 +0000

    whichever impressed question delicate judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
