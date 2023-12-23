6200dbe50abed0453fd9c6ea6b48dcc955848cfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:50:36 +0000

    declaration case exchange holdout aim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
