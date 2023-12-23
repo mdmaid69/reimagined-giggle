e1e251a405dde02e5473eace0c03bae89acc406b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:10 +0000

    trust surprise myself busy situation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
