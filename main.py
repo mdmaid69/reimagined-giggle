bf7495ac0bba7aa746d53f2454db74525643462b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:01 +0000

    before regional put often split

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-print(time.time())
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
