b0766180d32a73c849b7285838d1b1f090c8ddbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:04 +0000

    card between forever reaction seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
