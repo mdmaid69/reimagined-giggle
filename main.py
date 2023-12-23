466150c83c0eb53db2eabd4b32ce4d7d3d4d0bb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:24 +0000

    physical spend strict getaway we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
