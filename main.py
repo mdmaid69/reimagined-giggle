c3febedfa0cae7b8cf9f9c1e73c4c746cd9a9d6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:22 +0000

    go play please doubt impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
