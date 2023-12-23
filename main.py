5ff6d254de78fae95ff61290b834fbaf1b474c39
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:39 +0000

    attractive rush introduce front by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
