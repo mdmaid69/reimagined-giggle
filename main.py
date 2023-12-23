1abebe9c99da31787217c58335e85cde02490018
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:57 +0000

    shock appeal month juice as

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
