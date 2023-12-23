f47e337b32fadf5a8e6f54d9bb9dffb207dbf95a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:31 +0000

    own giveup contrast idea then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
