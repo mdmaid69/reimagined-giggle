ac602c743c0cbc29a3894e5e76c50575abec3f04
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:41:12 +0000

    purpose fast dress comment shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
