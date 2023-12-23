9e2f9ac891d1c9d8e3fa7e88c97dcc86da3b6e97
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:45 +0000

    turnup trust top television pleasure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
