30cd58a484034f16178710a97f83e49b131d16b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:22:20 +0000

    keepout quick suffer breakup list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import sys
-print(sys.version)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
