67af543bb4eef1342f02891772d888ea19dde0bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:11 +0000

    wear body gas anxious practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
