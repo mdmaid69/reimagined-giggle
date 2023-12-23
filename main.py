1fbda60b2cfb4ea5bcdab43e7463085d7de9dd71
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:41 +0000

    hang case bring comment feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
 def get_array_length(array):
         return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
