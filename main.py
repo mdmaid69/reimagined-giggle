9545449cd822e035df3a1984e57d20b4c2c1e4cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:28 +0000

    row key face think cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
