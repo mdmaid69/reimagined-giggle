56df4ca238f1611a92c9f01df49288273f315eb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:12 +0000

    569enjoy print do goodbye fierce

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-print([x**2 for x in range(10)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+name = "Python"
+print("Hello,", name)
