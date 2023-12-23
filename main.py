c20da975dde0a2a02009fa51443fd60ae8b2394b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:48 +0000

    twist cold letter weekend soon

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
