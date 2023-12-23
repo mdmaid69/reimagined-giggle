9999a9825be916d0b0e75be87a3a27311ebbf0e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:30 +0000

    pour tear tip steep cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
