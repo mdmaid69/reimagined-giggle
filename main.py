34cd1e7fe89f604cf8baa301975d8008b159fb07
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:37 +0000

    suckme discover discussion complete dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
