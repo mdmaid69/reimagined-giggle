d1077dff63260640dd57a7e05d2701bd56a4d673
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:33 +0000

    careless contain thought Ss news

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
