becb24f17bab1efe830081224560874ea7035407
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:13 +0000

    respect swing advise receive shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
