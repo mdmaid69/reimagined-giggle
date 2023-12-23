75d529b5382772c29dd522d96be24960fa43d7a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:23 +0000

    Ee police brain then realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
