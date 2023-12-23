928b1bb75fdf486c9f2953cb0f625d8f7fdec19a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:35 +0000

    nurse better type afterwards high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
