be4c2a8ebf278557f5bc2f7f25d90c8468b08079
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:56 +0000

    light mass hurry convenient wet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
