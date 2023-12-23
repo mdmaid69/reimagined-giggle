d2964a8b7f47c7a202553066399d934e72a06072
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:18 +0000

    back promise emergency considering shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
