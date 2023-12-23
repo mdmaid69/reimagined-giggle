78ed1fe5029cbf5daf665bbbfa9d6bdcf43a64db
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:28 +0000

    surprising table miss uncomfortable season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
