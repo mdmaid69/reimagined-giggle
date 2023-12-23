bc6a44f901ddd3d16fffd12b0b24d65d10e6808a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:51 +0000

    temperature cost success Bb limit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+print(datetime.datetime.now())
