1ff2dc1133a7f32216240e35a5758983abd2673b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:05:29 +0000

    confusing divide nerve desperate complaint

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
