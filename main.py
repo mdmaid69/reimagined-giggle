b8bc68bf5e078609432136824fa241d7d41cfd9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:50 +0000

    pot loud prevent repair taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
