17a341992f8cf55f61848fc5065c0b030d6f209d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:39 +0000

    chief offence surprise charge notice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_list_from_array(array):
+        return array.tolist()
