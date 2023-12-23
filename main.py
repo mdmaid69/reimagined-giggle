609a21322302b5144c6c4ed504134109a905a7bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:37 +0000

    store rest experiment smoke birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
