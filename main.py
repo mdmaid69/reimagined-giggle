25ff913c0256cb404fb320c1b706ff30484bf77c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:08 +0000

    we finger distance land once

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
