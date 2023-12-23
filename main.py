4fd1f36348f2ae94cd26b46a667d80246aadc1ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:14 +0000

    tax couple mean like ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
