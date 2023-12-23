27a487dd4c48ef133c8f348164fa3065526e3241
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:47 +0000

    full lucky morning experience correct

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+print("Hello, world!")
