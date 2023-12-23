b72237d89f417c7fa0a22cdfb84ecf22df5f3152
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:57:48 +0000

    runover transport boat bottom cook

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
