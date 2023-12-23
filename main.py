ac0a59d3a3361a0e03a10d33d23fec6fc68badd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:44:02 +0000

    limit surprising manner comment size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
