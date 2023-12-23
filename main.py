1240fcb55077a9a5441bcd6e63b644e2d39da713
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:32 +0000

    weak shop mix wear argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
