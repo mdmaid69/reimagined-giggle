47e67ecf6a5967e942c3cb60c6782ecdf901d15e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:22:48 +0000

    lock inquire fill practise ashamed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
