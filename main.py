d2021345c45abdb6369f931e2aafd50fce42bd14
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:47:09 +0000

    wave exhibition sure laugh plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
