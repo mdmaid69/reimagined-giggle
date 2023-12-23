fbd9de0925341d9b7251f5199188b66b0f42a13b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:31 +0000

    suppose she understand sport attempt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
