bebbf06be75473ae378b67434b4e7b3f1e476bcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:11 +0000

    hurry variety delight contrast fortunate

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
