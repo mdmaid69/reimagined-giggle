46d65f8e1c540511e472d5956aede9121435f3bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:01 +0000

    nervous scream dick future file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
