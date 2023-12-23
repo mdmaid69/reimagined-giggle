9853ff367dad3b56662874dca5b4f8ce165ebdd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:50 +0000

    best branch convincing question law

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
