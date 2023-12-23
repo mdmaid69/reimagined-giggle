b9018fe26f9c599944377af243b21d0b8c6ad234
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:03 +0000

    past certain this zero Rr

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_cosine(x):
+        return math.cos(x)
