4ff666e87633a0689be93cc61c3f59aaa23ab60c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:32 +0000

    remove to influence system holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
