9c138fb1fd903b6a15877a9c90b9044200322ab1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:19 +0000

    whisper impressive rely influence term

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
