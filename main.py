515dd9d6b94bc8f94d205a2a58c4a8d28aa1129d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:16 +0000

    frighten book print common punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def reverse_array(array):
+        array.reverse()
+def find_min(numbers):
+        return min(numbers)
