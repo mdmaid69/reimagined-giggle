500ef22e7525c4908f4bf3bab60b94f284fa056f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:35 +0000

    in level journey neat bring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def add_numbers(a, b):
-        return a + b
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
