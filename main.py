257bf222599c102bab6bbf571cfac447b0878b3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:10 +0000

    demonstrate turn question please spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
