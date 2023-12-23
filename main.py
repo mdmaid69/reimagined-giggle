29ea8ac3c46c51e16b6b64fa4221854ee17d5111
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:56 +0000

    mistake smooth road division television

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def convert_to_octal(n):
+        return oct(n)
