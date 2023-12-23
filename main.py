c3a228fb8696384d92a03a70d99bc1d7d6b4b9fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:10 +0000

    gooff though cruelty tough call

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
