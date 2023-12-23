4411ec808a5a2dfa4eb63756fc7460bfd084565a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:28 +0000

    record side pretty important corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
