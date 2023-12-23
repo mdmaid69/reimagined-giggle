0e8dc78f01fcfa1254c1339b0281356aae1151ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:57 +0000

    pay come collect surprise keepout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
