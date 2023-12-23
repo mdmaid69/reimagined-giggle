1cdf19ae2a90797fea1118697cf086db9a63a981
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:50:06 +0000

    over band authority position pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
