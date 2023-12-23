bc04f095007567fcd242c6b508b4ec6435675ce1
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:30 +0000

    rain takeaway sight trial front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def convert_to_hex(n):
+        return hex(n)
