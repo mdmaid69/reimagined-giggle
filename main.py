45d76dc74a315d726b013d0941929e24505445fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:32 +0000

    lean unite wind appeal heavy

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
