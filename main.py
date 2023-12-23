55c1613a00e1b11c7ee6290c6b83da7869769848
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:11 +0000

    disgusting tongue negative spare who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
