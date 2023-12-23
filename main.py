fd8f9d6fb49c31bbca5b1379022fd409fa139624
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:02 +0000

    team charge OKorokay before bang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
