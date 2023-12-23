0c5c8ef95712553aff7a0e60d3daa35a7d2c1ec6
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:43:37 +0000

    view it onto bit damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
