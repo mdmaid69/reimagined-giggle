026cd9061ae4fc40aa44d328d7fef63da5825d30
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:55 +0000

    spring copy key connect mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
