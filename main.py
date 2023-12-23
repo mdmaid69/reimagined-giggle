5b810c8ea1dbfffced8b6c8df7ca83bc11a9b808
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:26:14 +0000

    pick 571enough that earn card

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
