d66ef44ce47186842b2a98f96b5760ae777f0ba4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:59 +0000

    raise giveup storm conclusion announce

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import re
-print(re.match("h.*o", "hello world"))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
