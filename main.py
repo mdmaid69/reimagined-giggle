b6a8b8b661cf0e277c4861c2e2a3d0618076ae96
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:29 +0000

    guilty alone plain comeoff choose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
