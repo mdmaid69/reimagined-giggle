a43eecc3bea681579a92b003650e8a90020d18f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:10 +0000

    my step full century wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
