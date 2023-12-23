97ddf50cbe44b51e7c39992befb4b1c7fe7216d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:53 +0000

    shall past sensible name radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
