c886b0c5377eff582deb8fa8ab249888e486a32f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:38 +0000

    achieve turnout receive warning pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
