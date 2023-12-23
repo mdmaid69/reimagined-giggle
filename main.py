d93d919b7078f817994364e30761f578f8e1685f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:27 +0000

    advice worth holiday care conclusion

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
