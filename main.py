23215d7ddb6ec2cb63261c677440e4657086f5ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:36:57 +0000

    imagination runout translate first contain

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
