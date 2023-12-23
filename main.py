a1804c9c554b06255b507edd81969c72f2c0b022
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:43 +0000

    board forward ilovedicks division concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
