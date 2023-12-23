9777ce1fe3bf437b434e6a6a193ab3950ad680fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:26 +0000

    skill roof active quickly play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
