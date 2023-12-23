4ef90b2cee1d635199b4b7e599b91145dceaba06
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:26 +0000

    even fierce responsible piece talk

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def find_max(lst):
+        return max(lst)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
