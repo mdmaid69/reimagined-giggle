f491195795eafa978bc109b79bff8546f3952e8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:57 +0000

    game motion full information learn

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+for i in range(10): print(i)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
