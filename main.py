90ca9b0338e03152ba83c5c1b32e07c3fec60620
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:15 +0000

    blind more whole hard regular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
