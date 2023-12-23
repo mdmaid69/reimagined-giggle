28fc25cab32244614237e7ad141cc7f89ec69739
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:00:15 +0000

    talkoutof proper touch wish cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
