62748be2d13e67be4d9f82cc9e9c31d368042e60
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:08 +0000

    drink wet nature curious moon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
