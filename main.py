945c08626b691cf85609433ed5e7abcad0912eb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:11 +0000

    bit figure satisfactory intelligent seat

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
