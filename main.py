3fe683e18884e135675c5dea45d719f38755ca30
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:20 +0000

    fight shape thin to message

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def convert_to_hex(n):
-        return hex(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
