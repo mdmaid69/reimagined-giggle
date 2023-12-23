451b4a01f1ff9fdadf8f78e9a0648d1d1d9705d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:27:49 +0000

    fat popular destroy cover stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
