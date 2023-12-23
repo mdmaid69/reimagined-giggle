a7d07346946850e221ffb9c1fb3f96749c397a49
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:02 +0000

    relative sort light edge soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
