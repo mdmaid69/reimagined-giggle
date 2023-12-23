2f1b335da19998a93db44f2fb244b0995d1b161b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:14:46 +0000

    achieve persuade exam underneath pencil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_acceleration(speed, time):
+        return speed / time
