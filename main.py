ced47536c60a1d64b2d0f8fb6a3b9440257c353d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:46 +0000

    strange pretty every factory present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
