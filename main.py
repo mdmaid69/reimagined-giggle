f4ac9d618ed55949901f1e28bd5f5b3c98bc419b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:19 +0000

    Bb range case sticky direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
