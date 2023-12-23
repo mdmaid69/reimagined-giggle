c83c944335a0cb6ba8b7a6aa7132b4f29728d49b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:58 +0000

    trial twist direction Hh border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
