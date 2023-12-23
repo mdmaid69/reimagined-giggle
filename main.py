3bd5704d3cbef427a69a1488c5686b17665daa22
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:07:58 +0000

    mainly refer pronounce yours tooth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
