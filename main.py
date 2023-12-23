d6a566b94a62b9930196145c3f39e18b181a3a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:36:48 +0000

    gate shallow trick exam improve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
