2dddcc683c51b443cb9a5da9976e03bf3a41663c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:58 +0000

    lesson run state firm drawing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+print(math.pi)
