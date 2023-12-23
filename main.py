dfd763374ca88fc71e2ffe7afd191f0f83da9dfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:05 +0000

    prevent maintain criticize family breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
