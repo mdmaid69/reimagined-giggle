26ecd800a5e79b4250fd2ad54953358b27e8817d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:03 +0000

    nothing man Zz mine sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+print("Square numbers:", [x**2 for x in range(n)])
