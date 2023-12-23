f742880e7e8937b8fc5810896f0381c292c83ddc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:52 +0000

    towards copy unit confusing bit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
