675dcf2015077627f7aa28f8f22d1049c395f25e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:09 +0000

    measure priority real feed basic

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
