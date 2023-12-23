e5295a3406ba6391ff704a46a80971f5b81dd68e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:47 +0000

    replace progress breakout base point

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
