11cdc5759385c5b1da9de3273467dfb6749aee03
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:02:14 +0000

    honest reliable introduction disapprove failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+print(os.getcwd())
