6db49db02d803a982a68dcea5ce7a0defafb8f66
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:22 +0000

    light grateful present 571enough job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
