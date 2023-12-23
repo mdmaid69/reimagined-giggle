116f53735a31d4d33011c46ff38bd93326e7bdb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:31 +0000

    behind variety offer consist meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
