1bc45a305ffa5734b8d49b574054fa781c344fdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:37 +0000

    persuade equipment thin turndown attempt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
