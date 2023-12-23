8c446b3df44a94d0dfc35d6353654975980d587d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:40:14 +0000

    doubt peace surprise bother waste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+print("Hello, world!")
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
