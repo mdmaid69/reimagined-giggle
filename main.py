096d82e7c8148f7c039e9e4c185bbf360b23fc91
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:35 +0000

    wave everywhere model equal like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import sys
 def exit_program():
         sys.exit()
+  def square_number(x):
+        return x**2
