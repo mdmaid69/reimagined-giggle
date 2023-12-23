36f6af9bc6c7b4fd3fff68e89eba5aabc9a14e28
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:55 +0000

    watch silent whether complete mistake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
