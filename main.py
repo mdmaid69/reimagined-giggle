160632d574b682e110456a86b0fb518e041cf532
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:39 +0000

    it pause yourself tough height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
