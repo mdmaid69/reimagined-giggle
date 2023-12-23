a5e461b111505c331605b2d3a3a860d3eac36ba1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:47 +0000

    leave normal regional best object

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
