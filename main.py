a03acc5eb4699e402767443786bb9ed3992c412b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:47:15 +0000

    spare beside react wealth busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
