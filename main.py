e100e2567fa255639496993b67b38800007ee078
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:43 +0000

    know start slice break thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
