f209a6152d8ae653aed9bd3640921c532b0f79a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:41 +0000

    past giveaway board hair conduct

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def reverse_string(s):
-        return s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
