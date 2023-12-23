1de3e84f4960f18a68f8da6ea420b2b717783262
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:23 +0000

    office either afford for star

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
