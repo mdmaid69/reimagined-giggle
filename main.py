b7c932516a4bb0e8c4e8d54014b1734163887133
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:14 +0000

    undo quantity off Qq example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
