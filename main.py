86ed9c4166ed5f09d4421772de0db77858cdba93
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:03 +0000

    tape impossible face mainly information

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_string_from_array(array):
+        return array.tobytes()
