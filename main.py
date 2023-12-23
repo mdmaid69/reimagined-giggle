4a204b1715e04eb11b2040cbcbbc2b296c56e943
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:53 +0000

    throughout game comeover fine bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
