a5a7eb32db19ad24eb757250b7377c8f7a8f0d0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:26:26 +0000

    amount pick advantage uncomfortable business

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def convert_to_hex(n):
-        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_current_working_directory():
+        return os.getcwd()
