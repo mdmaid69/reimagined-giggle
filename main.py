861d1fa25ed3365f6f6f87108e0f01b1b6f464aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:13 +0000

    satisfactory face present keepaway night

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
