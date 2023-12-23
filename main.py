2694d13e7823a7ae8ef389f7bc0f28ed7ad86d82
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:28 +0000

    nothing cruel takeaway touch lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
