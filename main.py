d7ac7d9bd404231bcba9eee00bd6bef2b740c409
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:04 +0000

    proper muscle rate chin balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def reverse_list(lst):
+        return lst[::-1]
