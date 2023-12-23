35db7b634de58b273e81edc4fc9e00df998197f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:40 +0000

    condition offend study call shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
