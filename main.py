b66ebd88f319b6d0be7c96a1fee358133d59ff61
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:26 +0000

    church everybody disappear force onto

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
