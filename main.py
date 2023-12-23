19fa9efb9ef0b6d7379c73543a1255af920da1cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:47:28 +0000

    improvement tip constantly become explanation

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
