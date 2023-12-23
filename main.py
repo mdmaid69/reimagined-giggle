684ce0c5e23e25f8bcf55ca2485f382470cae300
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:26 +0000

    long afternoon false honest imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
