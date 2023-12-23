8d5079f53e5bb62e58072ff31d11372e942d1845
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:27:32 +0000

    shrink write loose exciting 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_area_rectangle(l, w):
-        return l * w
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
