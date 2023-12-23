2a6b7d6085d6cabe5c481b19b539d371b1a534a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:23 +0000

    contents talkinto estimate flavour female

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
