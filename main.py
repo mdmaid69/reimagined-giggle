5c864d43399ed6a9490de7c451d3fcded99960a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:32 +0000

    ours voice expensive borrow use

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
