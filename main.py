d60decf885045ae4a5df4c359560c83b9ff5fba9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:56 +0000

    interview trick cinema flavour soil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
