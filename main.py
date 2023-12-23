e2bf8306d19b991c426422d043063fc27ca8fa02
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:29:59 +0000

    expensive see wind go famous

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_buffer_info(array):
+        return array.buffer_info()
