8efb79badf7ee57b5320339259c7a1b2a321d7dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:20 +0000

    journey comeup phone getaway inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
