5a4d110b302cbb6461a3801320ad73f81386f66b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:35 +0000

    attract fortunately usual think it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
