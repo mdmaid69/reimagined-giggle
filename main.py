7ff25e7d90796133e6b5f99d113e54b36fcfe8cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:37 +0000

    draw keepaway building only think

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
