2ac7df2b08123eff8318a93e0a73283cd83acea7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:25:24 +0000

    disturb fresh nothing blame tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
