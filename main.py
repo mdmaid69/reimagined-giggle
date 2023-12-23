9a4d9c471f6441de4fbfadfdbe52025459d295fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:21 +0000

    price stare appeal exchange soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
