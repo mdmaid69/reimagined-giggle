66302317f27e0f3148e2de9bc468376fefeaf7dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:46 +0000

    spot success responsibility rain nowhere

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
