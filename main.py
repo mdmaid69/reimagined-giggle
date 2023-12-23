0066e22c1be4a24e2d2fe6219d2471296e0278c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:48:34 +0000

    development insist damp door dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
