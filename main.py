c488577a2de72afd801fd0caaf4701a01a0a56dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:15 +0000

    comment spend crash boil difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import datetime
+def get_today_date():
+        return datetime.date.today()
