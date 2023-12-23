5a8789be1f7927ae25bd8bc420a94c1011bdb0c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:22 +0000

    round coat happen open eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
