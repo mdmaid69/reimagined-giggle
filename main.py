5a46b53b61e271ec7a6e1637ea93dbc07398c4ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:45:32 +0000

    chase die paper throughout become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
