af3bc9aa20043fe95f0dcb9ba7c302a371630214
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:23 +0000

    command neck listenfor shall callat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
