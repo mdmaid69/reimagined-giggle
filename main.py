b1017033da47c6a239beaf3c6ac85f0c9f842d21
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:57 +0000

    reaction commit please show label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
