78f3457e530a3352d3735b616f387e364a8cc7cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:45 +0000

    favourite concentrate its independent soak

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def create_numpy_array(lst):
+        return np.array(lst)
