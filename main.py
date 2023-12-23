d639f5b9ca142569de67a79ba68565925cb7c5ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:06 +0000

    each poor threaten form trip

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
