1b476457e4c55bd7e711d5876a6e20f5a861ee3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:37 +0000

    tap obvious unfortunate 572enter further

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_item(array, i):
+        return array[i]
+import random
+print(random.randint(0, 100))
