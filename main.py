e218528b039e907baab3a08f26e30ef9ed7cb544
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:09 +0000

    better Ll emphasis lookaround experiment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
