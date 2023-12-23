4f7cddaffb934f382ec2f62aded152282da3416c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:35 +0000

    narrow count crowd along Nn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+print(re.match("h.*o", "hello world"))
