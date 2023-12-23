c31e256338edb09b93d15ff3a21433d75750bf5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:34 +0000

    possession butter play cold certainly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
