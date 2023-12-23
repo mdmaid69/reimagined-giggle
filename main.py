3d351c28f0bc349f1ae51f4328175f4586cfba2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:51 +0000

    afford slide up freeze ask

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_min(numbers):
+        return min(numbers)
