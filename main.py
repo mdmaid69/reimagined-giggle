6cb33f91ea406c249f0711711794a43695524a2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:29 +0000

    breakfast spring combine house autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import datetime
+print(datetime.datetime.now())
