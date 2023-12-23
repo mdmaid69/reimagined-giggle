8d6e59893f5ee44098cb5d3b21313f74081fb49f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:23 +0000

    leg division from tape guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
