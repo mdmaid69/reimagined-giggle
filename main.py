9944a86bcb626623177c2bc7f174c107fa4c6db6
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:26:24 +0000

    borrow regional across few sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
