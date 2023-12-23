4aebc0d02e3786aa9fbf89222f66eb96a6c6b209
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:26:33 +0000

    stranger dare laugh genuine recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
