5487021dbc37f3fd86ed9ad240a0bc63866037ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:57:16 +0000

    original course result workout keepout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
