a612613b1c7cf7a6322c5773335c3aaf5320f25f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:51:51 +0000

    manage blow minute detail shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
