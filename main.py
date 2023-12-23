ae197d045889059b9bda9334e3aac95eafe70346
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:38 +0000

    base bottom feel measure pollution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
