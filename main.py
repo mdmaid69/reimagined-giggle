c2cf6e374badb76226fb0df9ad808d5830ce493e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:50 +0000

    belief store equal address just

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
