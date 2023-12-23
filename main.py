1b20daf83529e922a7fc74167fe4495a599e3eeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:28 +0000

    drive gentle ground usually bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
