70ebbb5b4dfe751d609af255156b4c10d8411262
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:10:45 +0000

    label female funny confused getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+for i in range(10): print(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
