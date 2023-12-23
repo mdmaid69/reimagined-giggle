a2757026c7b009c8f2455b74d7c71b7053790c58
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:03 +0000

    maximum valuable block computer friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
 def calculate_arc_cosine(x):
         return math.acos(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
