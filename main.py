c9cfe1db9766c777eb646f0873c47fbb01dcec68
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:45:39 +0000

    previous all I send dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
