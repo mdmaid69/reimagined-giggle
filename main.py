b560c863fa90084a391a1861b9589d56cb6cea18
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:58 +0000

    low sensitive else sting callup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
