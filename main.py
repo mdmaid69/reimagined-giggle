508b6d921ac5dd7878ad7b5b50241114a20d1d95
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:31 +0000

    friend corner force course takeaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_set(array):
+        return set(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
