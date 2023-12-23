7ca9f59e20faf4df8d7cbe98a886f58b8393ce44
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:17:07 +0000

    influence tool memory image stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import time
+def get_current_time():
+        return time.ctime()
