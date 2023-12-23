6672118fcf5e5c9687e0c2e33142daf8024e9d08
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:31 +0000

    design arrange mean upstairs now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import random
-def roll_die():
-        return random.randint(1, 6)
+def convert_to_hex(n):
+        return hex(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
