d1d15970e173c8ad143040b74cd28f30f24bd728
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:34:41 +0000

    complete side doubt buy print

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import datetime
+def get_today_date():
+        return datetime.date.today()
