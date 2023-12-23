7e20885a1c888a6497bd1996a9be490f5ea57c84
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:24 +0000

    thin attempt average everyone summer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_time_since_epoch():
+        return time.time()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
