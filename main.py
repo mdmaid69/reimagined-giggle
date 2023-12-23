af7b02c63a927b50f84ad6d5348b769671226ce2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:40 +0000

    wave woman ever actually artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
