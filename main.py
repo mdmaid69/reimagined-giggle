eacf53437b17f585c4f55361257c9fb0206ba2bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:01 +0000

    butter disappear drink news turndown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
