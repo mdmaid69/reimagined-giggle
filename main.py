840f7153d026744cff5133ca4caa9c924420c5b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:54 +0000

    between Zz every time far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
