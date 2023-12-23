7be2115ce23d5b71ac048e761580cef3419809b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:47 +0000

    thought stage attach stare method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_set(array):
+        return set(array)
