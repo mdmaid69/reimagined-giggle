7a0bc78a1b3102cde1ee181efa6f570ed1011f46
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:53 +0000

    get block tradition hit constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
