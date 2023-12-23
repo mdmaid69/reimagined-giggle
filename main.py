da445f12bff896fab3dfcf7b96f9084882d52e96
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:39:32 +0000

    up serious offer beneath distant

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
