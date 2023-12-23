0418a5eec8b2f46930ef4fb96e33583f6665049e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:28:25 +0000

    female sound whom spot string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
