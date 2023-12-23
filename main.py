7393d4b12b47ca1ca1395c0373314c09b11448c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:07:02 +0000

    point something score approval slip

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_string_from_array(array):
+        return array.tobytes()
