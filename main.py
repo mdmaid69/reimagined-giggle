c8feeac1e05d773b390b88335f536d83432d5b7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:58 +0000

    generally suit part plate war

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
