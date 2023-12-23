7cfd079a959c8b39adaa2aba7587557056e91a04
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:54 +0000

    tape exchange proper expensive cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
