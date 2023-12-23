f26cbd4ac76a09d0cdd2025a4fda4756b6d38718
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:25:19 +0000

    beautiful lot action question low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
