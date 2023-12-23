34ff3b0b3c815d9401a335160caead2b28aa0ba9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:14 +0000

    crazy record cough might slightly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
