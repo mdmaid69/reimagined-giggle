cda9f28bba5ad5f6e925260176bded370d45d82e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:43 +0000

    formal plant neither under side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
