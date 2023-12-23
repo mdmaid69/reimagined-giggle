63cfe6457b5a13b9e18dff7fd5585a134d510b6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:42:46 +0000

    spring earth turnout love turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import json
+print(json.dumps({"name": "John", "age": 30}))
