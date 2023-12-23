d76783819ed2cc438e3ff214cb2d45bb780e8d57
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:51 +0000

    edge interested mess commit uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
