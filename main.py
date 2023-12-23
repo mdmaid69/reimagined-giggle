5a3e76d622d2e44d7c36027021383b24a24ec972
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:17 +0000

    snow how stamp feeling convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
