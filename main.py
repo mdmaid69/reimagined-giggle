d166eccac0b71f1670db3f9ab0974c18d4a98d39
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:16:01 +0000

    otherwise silent soon hang uniform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+for i in range(10): print(i)
