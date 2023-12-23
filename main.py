8868dd9cb952bc8c7d136a95fb84f9130da00b1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:42 +0000

    maximum cheque importance comeover cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
