36ecf528b9f9327e9015e0abb546b90c4777a7b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:21:53 +0000

    pronounce defeat search improvement sister

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import random
+print(random.randint(0, 100))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
