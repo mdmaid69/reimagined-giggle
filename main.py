a87c3649495470a8ade2e1bdec1e61a04564c07a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:46 +0000

    arrival turndown boring describe funny

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-print("Hello, world!")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
