766ae6da329d376a4c55e2de70c6265bdd02f27d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:58 +0000

    hollow glass collection admire spot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
