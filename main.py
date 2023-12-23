aa52d8aa855bcbd4cbdbe32e4fc97a8b7e201486
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:56 +0000

    organization love free marry thinkover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
