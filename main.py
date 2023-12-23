e513fb410c6c4bec90176865a696461bffb67ec0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:45:45 +0000

    cheat Vv ought practise hit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(10): print(i)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
