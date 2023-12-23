c6907ab1601da8707320dd328c19c05e718f9f1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:43 +0000

    571enough recently schedule war hand

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import collections
 def create_counter():
         return collections.Counter()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
