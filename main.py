a40aa2ad43f72d45f52c1473778334717cd043f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:34:42 +0000

    hang mouth bin basic exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import time
-def get_time_since_epoch():
-        return time.time()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
