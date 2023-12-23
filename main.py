a16830aee54a41c55ff5f68d8697d41b94eed4ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:14:05 +0000

    exact unhappy himself town understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
