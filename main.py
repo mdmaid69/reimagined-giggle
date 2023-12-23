148b9b22f11f83f487ceb75e713bfd608d74c9a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:47 +0000

    innocent clean still wind type

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
