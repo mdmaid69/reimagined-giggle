350ba5ab25dd46753bd9ff4e3ca83227e9602955
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:10 +0000

    care heart bet attract sensible

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
