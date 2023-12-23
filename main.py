7e9654324f453b3b64a2862ce9b47e54c510d03a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:16:10 +0000

    after admire foreign turnoff negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
