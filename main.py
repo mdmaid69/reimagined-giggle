415cd8024bef2214e3437f12de06c1983118ee90
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:21 +0000

    lesson decision repair destroy simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
