3aeb97c0f98ffa22663852bc2808bbbe35ddfa6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:01:15 +0000

    undo set sad private badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
