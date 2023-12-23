d114da04b9400ef28062438e99085663c83ad04e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:16 +0000

    hit complicated brief platform escape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
