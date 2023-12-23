3dc10e6241ae5de58e37c24dda54fa25e9513417
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:02:27 +0000

    meal yes should least operate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
