09d276ac7f4d26c24a420b4d94504d90d3f0e944
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:21 +0000

    whoever underneath recover responsible advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
