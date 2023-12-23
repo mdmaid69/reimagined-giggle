477e741ace6df7e374fa38b50c5c686b6037f96f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:53 +0000

    copy rob crowd plan fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
