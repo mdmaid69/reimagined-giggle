e32282b73ce92290fbc84daffe5be1c2f09fe051
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:57 +0000

    stamp comeback what imagination this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
