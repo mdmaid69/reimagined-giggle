1621edb77ef685c563dbcaca686bdbb8e84eba99
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:40 +0000

    recommend taste but coat whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
