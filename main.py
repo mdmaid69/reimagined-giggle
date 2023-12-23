7503b72113ee40509c8091fdf1cd2780dd115b0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:02:57 +0000

    together hit proud pronunciation weigh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
