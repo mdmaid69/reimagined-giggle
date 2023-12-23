e20090ac8e9838a66cc56a110b63edcfba011edc
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:16:11 +0000

    persuade variety ignore violent operate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
