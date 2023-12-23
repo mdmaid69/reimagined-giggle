9627a05f71d2afebd6dba278303509afa7c5bee6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:59 +0000

    collect bridge other inside second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_length(array):
+        return len(array)
