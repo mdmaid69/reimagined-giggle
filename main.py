d1d9701b89a1dca2a8a4fb7a45fdbce291d8554d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:17:20 +0000

    Dd light suggestion pull teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
