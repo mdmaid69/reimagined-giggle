a463705926459d192150c5717985b8963b4943e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:46 +0000

    juice worst bother achievement stupid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
