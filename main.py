e4ceab88c78aaf8dc40a1cd6cd31d2bc89e59aca
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:40 +0000

    snow cry wave will during

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
