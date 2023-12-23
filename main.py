78a29b01b1a135877e2f8cfef53cd23a95b94d86
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:09 +0000

    stay home Dd lift temperature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
