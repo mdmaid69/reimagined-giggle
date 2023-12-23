bb7b6b2e8344d2c6e6a7b56695b17e4eadb0bacc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:44 +0000

    different recommend truth neither agreement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def square_number(x):
+        return x**2
