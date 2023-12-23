77ca2e17a4a8b7ebafb64b93bc7b8eafc0ecfbd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:44 +0000

    than beneath stranger turninto make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
