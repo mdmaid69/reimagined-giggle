61f01c31777377598af1678ce23b4460494d287c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:45 +0000

    method fit book violent fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
