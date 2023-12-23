14fd2c29404dd768f609bd14713be99b031bda04
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:29 +0000

    loss load urgent share wear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
