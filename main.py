db28ba2563e49da343a2ffc6f33f4e1d88ef5e91
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:04:56 +0000

    medium charge break cause comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
