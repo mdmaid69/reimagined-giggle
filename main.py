a38e9b0e7fcdce055f82ff161af3520944baab91
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:45:39 +0000

    describe hurt fit discussion connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
