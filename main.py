189284ed3adb6d52a96ead9a4bd80357cdc2d34b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:08 +0000

    drink kick their battery scrap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+x = 10
+y = 20
+print("Sum:", x + y)
