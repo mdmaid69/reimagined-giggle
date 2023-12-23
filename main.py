8382b9ea54fb09175c6952e0f3a50bfe888c75d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:24 +0000

    listenfor record grade bother crowd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
