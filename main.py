4c8bf5cef869999f428cd6dae97d7fd84b247412
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:01 +0000

    edge succeed statement although recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+text = "Hello, world!"
+print("Characters:", len(text))
