216932e9904dbfc35c2dd4f54775109885b05a01
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:17 +0000

    noone turn official giveback natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
