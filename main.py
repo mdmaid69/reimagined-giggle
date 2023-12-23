6da5f4e94cb38e3e05565a8cf93bfe68b24730ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:08 +0000

    stupid interview and conclusion lookup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
