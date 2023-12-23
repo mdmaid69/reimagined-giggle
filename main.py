e3dbf1c068950d41a0b20e7e361acd86b8732bb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:09 +0000

    unable rush gap tear greeting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
