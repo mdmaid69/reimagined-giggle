7542c46b59ceb31d811051acf4dfb51c66fa11ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:26 +0000

    leave use port also object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
+def is_odd(n):
+        return n % 2 != 0
