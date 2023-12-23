be1173f979f349f666e012950da5d95b040da4a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:48 +0000

    invitation distance congratulate passage pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
