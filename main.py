331926be5bfe1faf044506265a1a07de1499088f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:38:52 +0000

    turn crowded absolutely rough area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
-def find_max(lst):
-        return max(lst)
