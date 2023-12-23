168ac31f6f88fb925e0cff93a474435e63334c70
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:19 +0000

    getaway accordingto proper have emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
