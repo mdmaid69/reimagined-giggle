43075071694851a264631a7f56134bf2271493ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:03 +0000

    brief family strength children rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
