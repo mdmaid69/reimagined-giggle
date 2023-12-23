dc864f4a6bff9850d8c2ca83166027fd2bf7436e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:47 +0000

    shine exactly regularly raise cost

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
