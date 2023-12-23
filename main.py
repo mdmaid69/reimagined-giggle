dc9797ae5f030308e7ef2f9f416ab75a96b7da3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:29:52 +0000

    process pressure know duty satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
