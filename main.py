1eba6abf413e7ba2b8f4d61bd35872bb496b83e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:52 +0000

    arrival dangerous urgent past prefer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
