85e07b2aefb9ec764362216902dd33eba5df1fc7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:12 +0000

    fly picture love service breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
