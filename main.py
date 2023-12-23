dd74d0064cf58a60904ded67c37d6eb7fc574476
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:46 +0000

    sleep important air taste fat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
