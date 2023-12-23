5a1501e389b16a8a9b4905fe47bc8767d80a9d8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:03 +0000

    wrong sleep callback bill subject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
