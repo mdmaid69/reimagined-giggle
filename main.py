661abf78e7065a84489c6a8971a5687a9c713d51
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:08 +0000

    bunch subject mean lean realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
