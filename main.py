7c84264cab8a5b4cab7abeb3bfdf5b685cf8f891
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:09 +0000

    responsible rest anywhere year type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
