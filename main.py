cf42b4d8d0cb6eb06b93d955a2830529afd9988e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:21:54 +0000

    object 566end border accept line

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_str(array):
+        return str(array)
