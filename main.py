a3a8af831f2a0702c5abadeae461208d4ed6795a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:40:13 +0000

    excuse much zero memory damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
