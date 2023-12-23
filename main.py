d3663c5ad8165e20d18ee98736bdc2a07eb680d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:30 +0000

    lock cut near wild mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
