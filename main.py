e6eaeb61fa9bc04831bca735334e7db20382df41
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:35:59 +0000

    mistake image escape your smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
