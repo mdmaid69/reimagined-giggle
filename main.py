deded05b8a1ff323040605be881ad4abc6e4eb93
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:46 +0000

    meeting theirs crisis character forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+print(math.pi)
