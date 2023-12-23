44ee137b8a5a8c69927f320a3613ae23e7146501
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:10:23 +0000

    affect whether hurry have software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
