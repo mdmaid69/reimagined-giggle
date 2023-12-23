2a174ac223bd99bcc5383c206cce46c3bf95a559
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:16 +0000

    punish away level storm rain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
