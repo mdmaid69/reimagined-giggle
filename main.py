4a140f713119a2ab46ea927efbca5d79b7afac6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:14:08 +0000

    by instant free long phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import sys
+print(sys.version)
