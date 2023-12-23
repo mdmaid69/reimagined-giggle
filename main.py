26ef136ae75b71303cd01f0199baf01a6e47600b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:15:21 +0000

    combination 566end frightened tidy or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_typecode(array):
+        return array.typecode
