16d4ed3bf3f43f2b978f984f79e1a44156ddb46a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:28:36 +0000

    awake can maybe start but

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(10): print(i)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
