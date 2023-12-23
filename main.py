1dfb2528550ff5bcf97ff0533b8ec6819acc688b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:49 +0000

    idea control Jj communication remember

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
