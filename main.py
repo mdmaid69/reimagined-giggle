50f86917b8aee59817fb0affd129a7515758e3ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:11 +0000

    expect far nearly solve delicate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
