54434cdf2a2f4ebf7d79084bc0cba50ba49ccef3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:12 +0000

    shut late escape valuable hospital

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
