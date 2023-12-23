46ab7dde415b5831327df2799a6b327a2f0c263a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:28:48 +0000

    record nose worth genuine so

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
