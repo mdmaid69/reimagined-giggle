6bb8df4429e8c342e71109ad189ffdd4221cbb7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:42:09 +0000

    away law passage no absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_palindrome(s):
-        return s == s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+def get_current_time():
+        return time.ctime()
