fe3ba31b7e642ba9c99c7a6da1fff6e08d5ba013
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:33 +0000

    many birthday fat cover owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
