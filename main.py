6a36ff6689a92ab673f9ae4b6d13892fef751bd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:10:31 +0000

    like shape wild effect swimming

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
