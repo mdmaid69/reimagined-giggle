256eb642d439db605b77d7bf7d4912883b56cbca
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:46 +0000

    patience new everybody below of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_priority_queue():
+        return collections.deque()
