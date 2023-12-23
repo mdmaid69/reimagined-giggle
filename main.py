4ebb720a10174bb3f6cbc9be5e83edf1b3438cf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:27:37 +0000

    or 567enemy leave mark intelligent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_speed(distance, time):
+        return distance / time
