db259a151d662d1c9ecb99e3247fdaccbe594651
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:33:14 +0000

    attract strange well go success

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
