c5f12109bcc20a40cbec08e09643177c452c81ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:51:24 +0000

    worse recovery hungry dangerous answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
