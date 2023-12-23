2d3622e2bfd9c2381e3c276333f91466113b4af9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:19:10 +0000

    war else over correct clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
