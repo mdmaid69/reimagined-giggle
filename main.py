baef72d1d63b1c7c983d18802fb09f9ef3a6b0ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:32:48 +0000

    try build moral border mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def clear_array(array):
+        array *= 0
