f85ded95bd583c8dce01304f9866c3a8d4a4f389
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:50 +0000

    air save operation lose usual

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
