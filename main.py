408a00144170f67e96395f5d0c174a28e90ea9bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:35 +0000

    street stick too rubbish temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
