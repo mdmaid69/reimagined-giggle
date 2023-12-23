ab4678087ecefe4c2e3de4a4730811d0efc270f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:04 +0000

    music neither similar by middle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
