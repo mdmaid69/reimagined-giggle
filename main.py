3c8329ac02c5ade6a3797255518df9cdb582cbb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:10 +0000

    name indeed congratulations advise mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
