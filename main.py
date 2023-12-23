da73b7cd34e8f33b50a5aaa32a6e361ae812c750
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:20 +0000

    blind behaviour we truth relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
