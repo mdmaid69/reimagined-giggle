99f859ab2fd4ae0c2ee493b4d95b45ec81fb15fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:44 +0000

    right tooth congratulate nice any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
