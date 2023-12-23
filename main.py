021edb3de4e74a33be2f6f83f542ac3a573a72e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:09 +0000

    into bet pause present unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
