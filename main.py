a0249b2fec01f916f476dcd3ca6025c75efc7668
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:07:44 +0000

    aim actual active turnover urgent

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sys
-print(sys.version)
+def calculate_density(mass, volume):
+        return mass / volume
+print(sum(range(10)))
