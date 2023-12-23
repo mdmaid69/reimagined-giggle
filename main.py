b04aff9a2636425eb9ff1aa3389eaebc68a2d239
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:47 +0000

    full patient stop allow bringup

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_set(array):
-        return set(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
