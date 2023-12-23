e3271d5afa70f48981cf71778500b54ac883de35
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:29 +0000

    brave extremely bag crazy lookafter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
