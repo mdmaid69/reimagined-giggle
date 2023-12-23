9c0ad548b63974b4194053adf4d2ce5e878d9606
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:09 +0000

    forget intelligent hungry scared wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
