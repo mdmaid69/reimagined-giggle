7b1bbe3b209e9af31102353f7ddb401f1ca7042a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:40 +0000

    complain steer surprising convinced music

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
