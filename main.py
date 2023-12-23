8b480e319b1bd44aa335cc7844abcd4d67b4abba
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:11 +0000

    show poison month nowhere getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
