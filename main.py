d96ed0c359c9c3c23eb8cda20f758998d7e84e6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:13 +0000

    stand Vv bet wealth shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
