6c71e37a38a49160ddeee3a09571e98904fae090
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:29 +0000

    pass borrow wing into swallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
