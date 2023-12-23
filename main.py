9d571ef2269f0927c6c6e56f1560651b4ac866dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:09 +0000

    nigga variety fly politics spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
