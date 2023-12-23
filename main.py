78d1cc2215439c584c6a0af89e4f74e677ac022a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:53 +0000

    strange cheat minimum exciting count

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
