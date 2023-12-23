d479a1bcad41e6fabbeff2751453ea6abb4cfaa3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:10:36 +0000

    poison convincing hope gap winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import datetime
+def get_current_date():
+        return datetime.date.today()
