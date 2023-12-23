65d208e28a1a9b58159346af5fc310ca318d85e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:30 +0000

    positive accidentally top brain stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
