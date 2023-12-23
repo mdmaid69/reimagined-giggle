7dc423e2832bcf573dd43d655f8a5a5719f5fca9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:10 +0000

    name Ll that nervous narrow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
