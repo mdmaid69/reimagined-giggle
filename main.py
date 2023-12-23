29a6e7b3fb460b35d7abbff77a2aff521aada779
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:14 +0000

    stick place noone absolutely achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
