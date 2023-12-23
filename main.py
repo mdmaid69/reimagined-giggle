0a82cc024c190f5b30f78eec7e3c49c62d7b1e5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:08 +0000

    sell turn afraid declare study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
