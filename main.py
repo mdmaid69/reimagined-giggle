83c63aa56023e37deab724b4e1df95838c6f3b85
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:16:14 +0000

    equal grade guilty snow takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
