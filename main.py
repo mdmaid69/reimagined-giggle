32ec9ca034644b070c7095a93d04ba4cab3b417b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:59 +0000

    total border news increase equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
