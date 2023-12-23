e4520d721afff4d7443d828b81f33fe2eb207df7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:04 +0000

    earth matter view beneath offence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
