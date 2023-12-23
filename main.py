4e5f3d2c37a48fbef1bf6cb7c7a2ce075f898009
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:24 +0000

    likely such immediate deep dick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-name = "Python"
-print("Hello,", name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+print([x**2 for x in range(10)])
