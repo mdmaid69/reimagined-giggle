9aa13aed7d46945017c37804e6b7dba564df49eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:36:42 +0000

    Qq failure ask angry emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
