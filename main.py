c613ddea29dafef95278733d1a7d75a14eab3cbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:01:14 +0000

    bus as turninto picture keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
