05c2cbfad7964744b8f1e0554d8d73bd831de1f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:10:02 +0000

    well summer cause sensitive impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
