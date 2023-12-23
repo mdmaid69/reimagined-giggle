2f5a4659dc21cf76f43167c0c72b96fe5cbfdfb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:45 +0000

    deserve crowded sound left sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
