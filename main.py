3b6f777353a55ff894c198206ac768b90367cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:51:55 +0000

    crowded wish rely possession after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
