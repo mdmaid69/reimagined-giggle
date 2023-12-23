7d012140c535f148f72e20eff4511f32c188bafa
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:41:03 +0000

    road follow mistake previous noise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
