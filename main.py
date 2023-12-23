ddad065337aac63fa0a80cffee7f084ed878f7c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:46 +0000

    judge sort whether couple workon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
