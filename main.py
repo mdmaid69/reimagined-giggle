47150fa2871463f69d43a23023ea7b233353fa33
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:14 +0000

    satisfactory flood joke different snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
