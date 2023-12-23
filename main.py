19f7f21627fde2648ffba50b4486a36945a5626b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:37 +0000

    Ee turnup reaction disturb dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
