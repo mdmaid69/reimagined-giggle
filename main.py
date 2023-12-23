feabaff0c64ba1b6d86260e832d50fced8813a9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:14 +0000

    piece slight pencil section brother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
