a92274e760ff8c57eeb2023d2bd935b2cc809fa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:00 +0000

    line careless appear notice duty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
