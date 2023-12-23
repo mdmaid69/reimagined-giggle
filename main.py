8e1480b8d742bee878d42011a06140103cb1cb2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:56 +0000

    different proud condition board male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
