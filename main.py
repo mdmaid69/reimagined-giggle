efc884e0c9ab8466d2d301a0970461ef374fff32
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:44 +0000

    passage press somehow behaviour certainly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_power(work, time):
-        return work / time
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
