7e70dd5a74091ac280024d94c155478d04cf5636
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:06 +0000

    strength example usual discuss because

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
