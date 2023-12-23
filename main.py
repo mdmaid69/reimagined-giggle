1d7d158318b2f7cb67c7a628004482b4d5f859e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:51 +0000

    scare judge event straight sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
