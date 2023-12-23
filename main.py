ad1a4f568d1d61e7d4bc1da69b6424ea814c66ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:12 +0000

    surprised practice gift detail receive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
