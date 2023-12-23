94c3200d9eb54aa369fa60211521e8726d73a60d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:55:50 +0000

    fashion approve good special rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
