254b026e795bffb85a4d7aee16884a5a8d213b4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:36 +0000

    Ii leave Yy accept call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
