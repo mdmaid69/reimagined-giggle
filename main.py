265c55a7d572688060780cf14afcb60cb54f780d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:03 +0000

    bin path possible table fit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def remove_duplicates(lst):
+        return list(set(lst))
