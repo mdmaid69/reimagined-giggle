1c5f62efd490abdbee205603df21809f4cafa0df
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:44 +0000

    for realize note typical road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
