d328a77ef6760702da2ec47a044b23ae401c563b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:52 +0000

    note government it real lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_power(work, time):
+        return work / time
