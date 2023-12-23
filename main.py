9a1f2b2eaaeaa95740af8fdb59df731e7cc6e3a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:13 +0000

    explain project hospital silent tool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
