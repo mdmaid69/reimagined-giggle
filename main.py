6c07f3007da5da5b068bde5a3f5999209b066b24
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:58 +0000

    they convince account party fence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def cube_number(x):
+        return x**3
+def is_odd(n):
+        return n % 2 != 0
