4a252e9d2ed1565e529d0141214408fa08fc1f0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:27 +0000

    arrange opinion comeround earn somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
