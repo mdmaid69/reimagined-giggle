38998002424d4e783e9c077e2d305d5699819389
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:44 +0000

    weigh lookup talkback tread journey

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+print("Hello, world!")
