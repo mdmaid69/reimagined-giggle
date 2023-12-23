a931686e220d794c242f5e69338cde1e0fdfbd0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:23 +0000

    contain pocket eye key in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
