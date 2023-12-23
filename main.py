74b315154ed6518f507a277d3ed78c6b78d28c3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:58 +0000

    lunch moon handle estimate talkoutof

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
