18f8dc4c93f4cac76a929b8141f3db464ff17816
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:51:22 +0000

    yes mark able shout female

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def convert_to_hex(n):
-        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
