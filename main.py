6ff3858769c2cae1a1e277bd549b41f3a5e5755e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:20:14 +0000

    autumn effect second increase regional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import datetime
-print(datetime.datetime.now())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
