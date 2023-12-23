74228f8367d2e5ea6efbf8fc897938c519691177
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:38 +0000

    behind unfortunate science Vv boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
