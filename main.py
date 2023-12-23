9e90688f952f81f868448ae8156852409c895ba8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:41:00 +0000

    likely during fresh criticism whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
