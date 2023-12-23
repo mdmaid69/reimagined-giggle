cef852e34a2c99c9a03bab37963484c80028acd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:37 +0000

    land off exact judge steam

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print(os.getcwd())
