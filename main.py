99cbb140fc343c45af812c5f7571fa35bf5f4f8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:53:54 +0000

    lay takeout value prefer pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
