1cf9e8eb9a1ddad64a225b267690c88e99f53287
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:15 +0000

    change listen geton practise across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
