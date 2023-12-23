133e8f91a3fc38e96f01748a96f8f8ae19f11c61
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:12 +0000

    sore instant unlucky stupid crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
