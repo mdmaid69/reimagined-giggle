51ae202807f1a56dee52e8045a4073ae1950b5e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:27 +0000

    sore 572enter matter reserve fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
