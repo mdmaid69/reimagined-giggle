d10a22d8be1c9f060d878cd2dc99134b7a57bc04
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:03 +0000

    welcome sometimes apologizeapologise hurry heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
