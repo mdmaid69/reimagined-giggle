409fc8bbe4f1c203046adde884aa1924866f0b9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:38:46 +0000

    every from lean praise attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
