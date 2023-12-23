cffebeb4fe301ff8445b1f36d952402647afc1c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:07:43 +0000

    everywhere instruction here male now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import json
+print(json.dumps({"name": "John", "age": 30}))
