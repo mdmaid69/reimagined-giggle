6a8ece04d055ba04b781f00e01369730a62ce320
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:30:07 +0000

    condition Ii inquire makeup knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
