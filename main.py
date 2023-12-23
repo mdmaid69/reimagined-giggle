5fadf17c991fe18d3ee7b70cd47fcf982c5a374b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:40 +0000

    culture town concerning treat it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
