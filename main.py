d051d54c8d15e8afcb1b91fb150d3dd432498c4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:44 +0000

    badly impatient custom grateful drop

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_as_set(array):
+        return set(array)
