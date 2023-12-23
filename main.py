ab6dc34a4a5e028947f34feb82951387f7814ed3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:08 +0000

    noise regional speak run breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
