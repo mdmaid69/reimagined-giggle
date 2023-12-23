8f83fe003321a6bf475462405dbef280d4afc59f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:54:46 +0000

    benefit giveup fasten warn any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
