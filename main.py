5b1cbd1d79d988638c02b07db37d685194a41147
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:41 +0000

    perhaps discover recommend defeat sale

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def convert_to_binary(n):
+        return bin(n)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
