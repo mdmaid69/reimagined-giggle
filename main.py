d2601c254f0a288f24c5488e2a5a4bf69162bdf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:18 +0000

    over circle conduct avoid people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
