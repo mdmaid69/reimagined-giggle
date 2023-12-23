5492f9f4da1e5b6cd30319fd5132c218747e5220
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:53:55 +0000

    precise turninto content another impatient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-for i in range(5):
-        print(i)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
