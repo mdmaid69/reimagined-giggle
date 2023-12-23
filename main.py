783b95d683140d27461a1846f1b303931e4bf1c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:56 +0000

    force danger pen practise view

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
