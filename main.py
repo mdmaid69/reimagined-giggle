ff43aa60f25e4e1a6d8abeb1f992b3abb7ce5c49
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:41 +0000

    ignore like warning repeat week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
