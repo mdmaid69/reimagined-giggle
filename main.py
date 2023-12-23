19c66b9fd7437fd6c9b1f9f876c091b3dafdeb1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:50 +0000

    friend copy along drink yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
