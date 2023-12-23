d7f432cb718601b8f78bd7d8c405bccf2d0e8c1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:40:50 +0000

    unhappy schedule tall defend needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
