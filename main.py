4e9e2d9d1c37cc166975cd9769984f6954b2a9d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:11:08 +0000

    interview pleasant present by record

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
