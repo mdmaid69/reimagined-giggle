7036911da1a127cf7580cdc49aefbc170772097d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:33 +0000

    violent achieve practice makeup section

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
