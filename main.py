f8689b687ca4334a3b76b75597195de5c7d6536d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:10 +0000

    behind dish feel always sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
