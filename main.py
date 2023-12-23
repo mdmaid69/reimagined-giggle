2ccdc812c7400a1ea9f73417e16081f81bb07917
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:42 +0000

    suddenly suffer winter deep pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import time
+def get_current_time():
+        return time.ctime()
