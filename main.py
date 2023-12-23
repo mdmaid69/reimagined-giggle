3d648f3d84f29ee6f746449bc18f9cf51fc4d917
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:04 +0000

    we satisfactory 568energy disappointed possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
