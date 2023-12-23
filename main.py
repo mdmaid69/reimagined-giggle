23c1cd887ebd93777aaf1dcede3ed562a83daf88
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:59 +0000

    thinkof freedom drawing area judge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def cube_number(x):
-        return x**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
