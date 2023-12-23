cf33c13d4979a940e2442092d20f97d2714bcd27
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:18 +0000

    recognize slow stir beside string

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
