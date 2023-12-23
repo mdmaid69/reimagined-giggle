dd82e3eed1a2b7671aa85df360a9357a3bdbc649
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:47 +0000

    ask free period list back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+name = "Python"
+print("Hello,", name)
