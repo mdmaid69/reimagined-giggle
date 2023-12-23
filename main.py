e3ebb125b2c4fff65d6008cc2dbecc6c8d6e9062
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:36:47 +0000

    embarrassing sleep television generally stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
