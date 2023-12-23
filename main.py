e9edd7712b6d03df6b69b0d2569aad26931b9306
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:01:22 +0000

    neither wherever sense should entrance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
