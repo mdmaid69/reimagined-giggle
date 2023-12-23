f2c306bc604c2ead6bf2b9c12be88a9f08f85a9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:22 +0000

    perhaps smile pick fall bet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
