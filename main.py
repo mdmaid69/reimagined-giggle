de780a66b10ded084e42b615cab8a6c08ad277e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:00 +0000

    couple excuse excuse shout comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
