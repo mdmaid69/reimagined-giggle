433455da366681dcb286d60d21a8a51242b2b7b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:33 +0000

    total thorough slightly gap earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import sys
+print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
