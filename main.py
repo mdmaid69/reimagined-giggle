5809f2b2bb9a421108f4706b7f6efe0554e802a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:00 +0000

    partner how connection design splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
