465402f896a8ac436aa84e38bd5c4734dfde4fb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:47 +0000

    hardly true possession crash school

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
