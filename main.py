c554e08b1ae6956230abc0c7c244ed09a2c0aa54
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:09 +0000

    once heat show also ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
