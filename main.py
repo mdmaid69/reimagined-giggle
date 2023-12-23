2e54c29e142c03d6ceec90958e4a5e3d4e45bb0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:14 +0000

    about smooth course rather cruel

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
