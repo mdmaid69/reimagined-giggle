5fcf3152fb0f677dd6f79dcb7d6577593352c120
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:43 +0000

    invitation loan pay reply possible

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
