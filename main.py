e063ebf4e061da8ef92ce9f5a30694d0414c7f76
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:31 +0000

    last rain remove throw sound

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
