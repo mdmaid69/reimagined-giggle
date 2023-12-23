f944d394c10adf90d936015b5e6e2f720b50bf6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:48 +0000

    fine common middle bet but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import time
+print(time.time())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
