7c535059d7993a23e0efe71ea2d93c0dd0f5de53
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:16 +0000

    raise Uu early comeround judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def convert_to_octal(n):
         return oct(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
