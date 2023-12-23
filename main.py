66364c31f36b3593afdecd44780c67ff289c37fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:03:05 +0000

    crowded before not front combine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def subtract_numbers(x, y):
         return x - y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
