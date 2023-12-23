6e7281b49b59f298d1bfdbce4f7ae90d0c15be49
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:24 +0000

    breakout take corner wonder unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
