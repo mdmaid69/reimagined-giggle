fd9e0ed00bcd45ae83cbd2a17840d200fafca446
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:57 +0000

    share blue unlikely married spare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def remove_duplicates(lst):
+        return list(set(lst))
