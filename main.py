fa2fb03db4c9020754f00be5b05a119f79583d31
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:10 +0000

    also crisis consult sometimes wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
