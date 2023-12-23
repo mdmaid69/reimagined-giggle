23dfd291a6bf33930666c30d88e907ea721488c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:57 +0000

    recovery sore wait pot snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
