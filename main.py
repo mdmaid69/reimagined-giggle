3dc253991a999b2ff8bd570462f9ffa2e56448f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:33 +0000

    description company someone impatient team

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
