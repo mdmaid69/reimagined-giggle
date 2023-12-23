2c17317c8b790e1556651a8fe94865d7d383bd5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:26:20 +0000

    effect contact car disappointed fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
