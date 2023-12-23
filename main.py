cf771b0c489b1aa6221093e973426657b5991f76
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:36 +0000

    guide news tap break dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_string(s):
-        return s[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
