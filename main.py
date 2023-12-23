34d8aa68d3ec94380a821f89071bcb271c0f68b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:47 +0000

    line error visit eye permission

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
