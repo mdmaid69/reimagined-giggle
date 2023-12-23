9730d5aa8ad613dae380556c4c90ad0d0960aa0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:58 +0000

    add attitude ever plastic seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
