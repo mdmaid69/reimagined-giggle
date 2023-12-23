1686335953439fad223c47c5242ae1ab1352b2d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:00 +0000

    believe more drop interview Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
