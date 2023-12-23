a5109c80c9c04f884dbef6922f3c267bb7452fc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:20 +0000

    ground fear not consist whoever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
