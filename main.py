8e56b0e8f1fdfe3f3a2b1943741108b5df65c80a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:24 +0000

    operate train 571enough story list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
