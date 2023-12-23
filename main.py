5c79f50dca29b0a973b2bfc2876cf89d5cf4a8ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:14 +0000

    neat tool favour frequent Tt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
