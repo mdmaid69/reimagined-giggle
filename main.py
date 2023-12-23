bd3dfd2a40264d81c2549269ab0828ba08c7a585
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:21:18 +0000

    must visit during ordinary illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
