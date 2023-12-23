1c4138fc8cdacfd5971c66c33a93ac9e68feeaee
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:08 +0000

    damage prove read project tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
