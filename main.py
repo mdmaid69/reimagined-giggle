05ac15062bba04fdc30323c02c675a88ac719d73
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:43:20 +0000

    comment health handle deliberately talk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
