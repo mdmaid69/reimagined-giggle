6827229374c96472166d1bfb151af508f5f0bbff
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:24 +0000

    collection empty free hers across

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
