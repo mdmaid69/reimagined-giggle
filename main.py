7da2e540540446f7736ed342f882ae160e58b2cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:25 +0000

    everywhere outside favourite heavy open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
