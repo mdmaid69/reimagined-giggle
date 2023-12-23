570eee4cb41077607138fe8b9c9a965b57ac4558
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:44 +0000

    hit experience clear party surprising

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
