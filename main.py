26abdb999c5f8e7f57f2ff9ee0a51f20991f8d8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:09 +0000

    translate moment downstairs broad favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
