e90875d104baed9559991f4b5e31ae8d60148f1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:34 +0000

    lead takeaway ashamed fortunately last

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
