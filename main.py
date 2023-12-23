627b33dc4c59cc45d0bcc323d7dcb25026ef510b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:45 +0000

    choice release contents successful nature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
