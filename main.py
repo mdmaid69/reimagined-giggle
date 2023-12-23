4acaccc1690a8130c7a0c9e85c712a756196b3f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:58:49 +0000

    pick quit behaviour difficult pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
