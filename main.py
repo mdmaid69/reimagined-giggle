00012ce19d1d38356929b27a20d82fa71a70cec4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:57 +0000

    secret tight area thinkthrough lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
