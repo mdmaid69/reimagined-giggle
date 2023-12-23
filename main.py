3238453aa5726ef92be5d628ae635626926f6dc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:41 +0000

    stage comein brief word year

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-from collections import Counter
-print(Counter("hello world"))
+def add_numbers(x, y):
+        return x + y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
