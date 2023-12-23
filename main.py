a177fb121cf4c40e40ceaaae4cb6542f067b422f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:42 +0000

    frame silent wish affect bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_acceleration(speed, time):
+        return speed / time
