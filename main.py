1e8b62dfa8625b80aca70de5f390150f9408b11f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:19:26 +0000

    your none keep appear while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
