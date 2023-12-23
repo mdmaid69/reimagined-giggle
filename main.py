3d46073eb5abfcb893dbe1de6f539ed498827e9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:33 +0000

    add inquire stranger circle anywayanyhow

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
