18718d2174eaa888526f2e1b06f7980403a66a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:25:48 +0000

    by arm murder else identify

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(x, y):
-        return x + y
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
