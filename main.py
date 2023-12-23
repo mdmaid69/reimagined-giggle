23970ff162fa1eefed57b7605bacb8a04d8bdfd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:53:59 +0000

    either collection divide complaint mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
