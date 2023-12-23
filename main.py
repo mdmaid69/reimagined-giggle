362508326a0eace5035b13bd990357f04f57f0f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:53 +0000

    politics trip decision shut fortunate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
+for i in range(10): print(i)
