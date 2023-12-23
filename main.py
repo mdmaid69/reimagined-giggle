567e33a6ccd5661cb3b2b1716e199e7804398732
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:03:25 +0000

    image true many Ii fast

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
