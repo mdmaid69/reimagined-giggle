5d8e424ef41c5c5c4ead1dbbc705318b38a8ff79
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:26 +0000

    juice drop strength not sudden

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
