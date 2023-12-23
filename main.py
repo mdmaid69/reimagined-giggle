551da8baa81c645f1bc8974d4c42ed5f5617549a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:38:02 +0000

    treatment wave shout ground expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
