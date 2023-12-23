0d3d80bb8598aa0b2f6e07dd2b02029dda4bccbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:45 +0000

    goup ashamed also bother cum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
