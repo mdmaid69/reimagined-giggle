a3bb1ea2614624761dd06b58143145b8878e7ee2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:49 +0000

    takeout healthy indeed callat rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
