a9727a01409b715f25e0642321496549e371f937
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:46 +0000

    everybody profit list car attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
