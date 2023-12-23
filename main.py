4389ed6bbb7128838e888def2962fce84c269981
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:29 +0000

    fact month honest opposite completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
