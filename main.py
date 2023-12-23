972c678880f76051eb8a42d80bfcb9879db14e6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:53 +0000

    excellent rush underneath row raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
