d692f58f3d88ca8650f6c9954d34898f5a4a9257
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:19 +0000

    dare some television upstairs fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
