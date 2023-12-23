e73939a13dc5aa1fd9aef38911ab80b7ba472e3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:08 +0000

    change belong Dd with except

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import json
+print(json.dumps({"name": "John", "age": 30}))
