fabc44f90efcee66bdedd76db15ccde7a2e45ee5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:51 +0000

    advise from often trousers necessary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
