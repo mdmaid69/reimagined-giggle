25ea977efa831bba7c86709830897410c80886b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:52 +0000

    unlucky set yourself wheel fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_typecode(array):
+        return array.typecode
