e9420f93d254e0d378833d4311e1059759bcd041
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:37 +0000

    experience area tall water favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
