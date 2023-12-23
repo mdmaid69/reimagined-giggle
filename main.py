c729188f523a4c9e8090234a5ff981f60a78c39f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:56:50 +0000

    pretend touch emphasize protest report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_list(lst):
-        return lst[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
