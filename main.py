107f419f8e538dbb42d1598aac2487566f72b26f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:21 +0000

    sink something precise accident introduce

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def remove_duplicates(lst):
-        return list(set(lst))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
