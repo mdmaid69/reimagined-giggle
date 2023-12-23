d33ef8facae8808eafddc161229086b183975108
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:16 +0000

    sad new temperature spend squash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
