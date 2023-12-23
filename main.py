c9428e72ac7e43739649c30da60f2926fce95aab
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:30 +0000

    perhaps giveup equipment smart block

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
