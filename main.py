5e45f561d28ed545ac05b33f55720ad794101209
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:57 +0000

    other estimate flat mistake priority

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
