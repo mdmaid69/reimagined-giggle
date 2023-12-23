2037ee12580ca482bfb7eb494855729223ff3955
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:33 +0000

    female keepoff rarely sweet repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
