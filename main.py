147d4e363ba4442847908a319a12d0dc4313d52f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:06 +0000

    dinner corner mood no communication

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
