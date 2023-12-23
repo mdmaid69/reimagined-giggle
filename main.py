b388f98fed3a05019b34e0f9376444c7af1e8b68
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:13:59 +0000

    he judge shopping design fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-name = "Python"
-print("Hello,", name)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
