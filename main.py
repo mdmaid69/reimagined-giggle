3797594f26363905a5754f9b4ccd0fce4b42cf2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:16:59 +0000

    skin consist temperature mark truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
