20ba8cd68f5f5515caf43d297fd8b59409832cfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:21 +0000

    confident insist wall will story

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
