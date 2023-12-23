dc5b426f5ddc92c92f146291783e74a805a8b362
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:13 +0000

    admire phone free stamp tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
