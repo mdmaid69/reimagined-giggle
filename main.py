6381ae06152dc68ef6294b021e42d3f4ac81c9db
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:19 +0000

    from they rain sensible practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def print_python_version():
+        return sys.version
