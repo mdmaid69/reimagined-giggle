e26ed6f2b9973c98bd76eb166fe92090056d97ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:41 +0000

    office taste flat throughout ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
