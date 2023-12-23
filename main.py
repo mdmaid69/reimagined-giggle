a4d2f713ea263f5c7faaeb3443b95caeba735dae
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:49 +0000

    term full fault top circumstance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
