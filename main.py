918c9bbc17f64e83819916f9ef0e7a6c5a063021
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:02 +0000

    owe moral record building freedom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def is_palindrome(s):
-        return s == s[::-1]
+def sort_list(lst):
+        return sorted(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
