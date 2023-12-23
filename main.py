b15706843c6ae300814a5e94788ed87b403588ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:22 +0000

    try art comedown OKorokay copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
