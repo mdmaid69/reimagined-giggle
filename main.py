892fe2047ce96641cb3821be4d75ec4534a6e251
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:39 +0000

    wrap could attach tidy comedown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
