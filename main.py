4bb01e4ba4b6bcd892c51e8f4784a0a812fd3a03
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:33 +0000

    later nonsense clearly pool politics

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
