ad54ea14cb5e0bff424de74895ab6eb88cb820b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:49 +0000

    skin declaration talk edge ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-name = "Python"
-print("Hello,", name)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
