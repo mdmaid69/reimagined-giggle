325ab3154d91cea92d8352547cea67b7901691f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:51 +0000

    finally surface treatment bad buy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
