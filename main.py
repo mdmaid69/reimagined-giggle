31b746c33f562499502740b4c921c3f1a1539f27
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:52 +0000

    bringback confusing building scale commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_area_circle(r):
+        return 3.14 * r**2
