fd7d4802aacf9467f1fabdb268cc3a8e45a5c41d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:51 +0000

    fond figure where ask every

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
