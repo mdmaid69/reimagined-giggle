b78e318b21812ff22cfa3df2bc042d00fb9215d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:39 +0000

    load keepaway keepaway plan season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def convert_to_binary(n):
+        return bin(n)
