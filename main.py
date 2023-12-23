cb9ed9ddc1e3b51c3826d771a74c62c6d0006236
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:35:40 +0000

    interrupt combine health whistle good

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
