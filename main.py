ec178e33933ec064bd819cefa25fab3fb4c5d36f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:47:45 +0000

    perform scream bright lookback(on) natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def greet(name):
+        print(f"Hello, {name}!")
