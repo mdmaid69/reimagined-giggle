ced972f826f9d96bada0db09e9dcfc4193090652
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:18:17 +0000

    offer street calmly comeround respect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
