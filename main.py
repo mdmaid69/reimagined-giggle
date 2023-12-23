a17336dce459bd01c633c8a89b0a230d8712d750
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:34 +0000

    reject within mouth illness dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
