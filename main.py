15f4bfacdbf547fad1d4115a7a8c74b10e2dcf67
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:53 +0000

    shout me shame close you

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
