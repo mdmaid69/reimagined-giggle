aaa1ee0392e1857a043bec3b1bafb4b78df2d178
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:33 +0000

    take partner risk convincing phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
