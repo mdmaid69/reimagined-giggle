2915121f3211de5956898aaefb8352c8850f409d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:45 +0000

    from organization some ready workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
