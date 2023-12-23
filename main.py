381b2da1a769867960442dafcfc5bb4210a7d20e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:20 +0000

    question ill call escape scream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
