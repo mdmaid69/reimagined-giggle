89d7bee230be32e21c5bdc56a9f8d5ff4ae6776a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:15 +0000

    effect pleased strange strange quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
