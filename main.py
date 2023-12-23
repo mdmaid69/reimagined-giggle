3d3c48f634c296f8c5c2a7e8195f86c51c1929bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:20 +0000

    neither class perhaps habit tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
