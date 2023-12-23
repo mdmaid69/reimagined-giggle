c41ac3755f89d2bc67ca4dbf1f8885fc57cee9a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:21 +0000

    bottom ours risk press arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
