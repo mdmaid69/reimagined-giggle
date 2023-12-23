89b47b3e7870f00edab00cc50e9199dd5a8c5591
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:50 +0000

    talkoutof ground slice send Bb

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
