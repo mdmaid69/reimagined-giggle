4653f5c126bc5d887181f2525e38ddd82c94f1d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:21 +0000

    afterwards present feel save discussion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
