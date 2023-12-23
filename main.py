715076c83203a4b3f738e84e7bdef21bdd58e52f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:43:43 +0000

    lie debt hour badly disgusting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-print(math.pi)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+print(os.getcwd())
