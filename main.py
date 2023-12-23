ed34fdf4b5799c3f8d206c5a77dba4afd843fc7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:33:33 +0000

    radio bin risk force tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
