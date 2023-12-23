1813c9392e553abb6af7b441755a8c19dbb436a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:31 +0000

    precise hit spot switch their

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
