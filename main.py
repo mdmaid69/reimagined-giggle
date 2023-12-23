ff702c398e7d77db375bc6660557aba4fe248da6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:42:56 +0000

    turnoff evidence ourselves phone mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
