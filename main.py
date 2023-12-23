77cdacd23a5a2c015d444348ddd92dc56f2500f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:12 +0000

    give firm shine dry period

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
