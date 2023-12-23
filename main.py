15ca99f17c81b54f106662658b945790423b461e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:39 +0000

    government whichever mother around sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
