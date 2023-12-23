0b9abbb1a6ab3ab272328f19316baea2ecc8bf68
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:03 +0000

    office language challenge summer shake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
