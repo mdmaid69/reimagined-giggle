e7d09d26cd29cd107f5ad69a1c9f3a7b7c2d4f26
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:46 +0000

    and general wide may punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
