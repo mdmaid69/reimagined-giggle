f53877519e8bc6d7afa04dabc5b4a3f31f6abd35
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:37:24 +0000

    hair this board few independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
