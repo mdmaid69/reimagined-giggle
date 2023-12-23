7d0d227097aab320edff390020053d9ee18b7164
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:55:40 +0000

    blue anybody spend mail hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
