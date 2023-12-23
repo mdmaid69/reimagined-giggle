7e9ff0d882044074b561f4c744f1adc27342bb6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:00 +0000

    brush work ocean branch shout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import logging
-def log_message(message):
-        logging.info(message)
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
