d494be9c1566ab5c7fa48847245640bebd5278e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:07:20 +0000

    hot thorough wish unusual excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def add_numbers(a, b):
+        return a + b
