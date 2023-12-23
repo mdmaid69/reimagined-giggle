9740a0d74dfcc067c5f8022ca803c0022b1ec29d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:16 +0000

    minority offend near pencil car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
