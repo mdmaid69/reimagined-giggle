cddff81851440bba417473254773ed0c100bd37d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:00:36 +0000

    trust die match welcome healthy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
