4a5cfd11e8cd2d3ed06aaae4e035d7072a1ed887
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:32 +0000

    combination habit reject fortunate mean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
