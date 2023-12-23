e0861c7f941fec7dc387bb00faacca472880cd89
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:24 +0000

    popular mysterious look occur method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
