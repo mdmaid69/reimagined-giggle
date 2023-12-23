6f768a900fa4a758550a25a4cdf881fec6dbe1a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:11:59 +0000

    suddenly time could dinner cause

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
