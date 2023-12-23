b2cd49f055a0d4e17e750bc2096dc3371481469c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:17 +0000

    chase memory point kill block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
