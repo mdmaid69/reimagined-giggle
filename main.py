33068eb05e867e54743794e6638021d52f152bf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:25 +0000

    visit deep getout experiment grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
