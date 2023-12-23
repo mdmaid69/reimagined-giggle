b28232612c84424bf7c9dab86e7b75aa1caa915b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:18 +0000

    market tongue notice apologizeapologise hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
