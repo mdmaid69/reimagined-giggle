3cd73980f916edac68d33806861e40855ba48968
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:22 +0000

    opponent make judge star convincing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
