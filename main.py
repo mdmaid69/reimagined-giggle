3359b0099141879c9e76235b88f7de517cc43cb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:53 +0000

    along slide blame spring brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
+def count_elements(lst):
+        return len(lst)
