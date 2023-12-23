fe1943be352810f40b5c77870c79f480311b095b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:56 +0000

    tense father emergency regard area

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
