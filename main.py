a44f3c1aedc3265dbc9963172eb07fffe7beb356
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:44:19 +0000

    club want advice cinema keepout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
