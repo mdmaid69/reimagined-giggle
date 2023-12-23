627dd38855d2c33e6517b26fc67b9c78868e6c8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:14 +0000

    raw name direct cap visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
