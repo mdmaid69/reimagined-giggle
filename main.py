5138393573fa0ed2812642439fef07a7e2cbe188
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:16 +0000

    old child search child statement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def convert_to_binary(n):
+        return bin(n)
