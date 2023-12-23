f91237aebf88befa112ff0217592d1adaed1bbb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:35 +0000

    vote limit pleasure anywayanyhow whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
