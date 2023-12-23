b909c3e190d3057a81eb9055eba21bdc8d73cd62
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:15 +0000

    exact comefrom suckme shut down

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-print(time.time())
+import sys
+print(sys.version)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
