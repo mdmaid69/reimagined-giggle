4cbc530f575308710c1f040e85bca7b89071f613
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:37 +0000

    impressed lend shake brother neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
