f9bb1a54f0d2d768de4d900174b877d17fa5ef4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:06:55 +0000

    soil above declaration belong finish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
