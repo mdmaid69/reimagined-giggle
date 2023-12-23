4f35da43832cf1e55e370dfc1474b2bb971fdefb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:34:41 +0000

    doubt order convenient price personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
