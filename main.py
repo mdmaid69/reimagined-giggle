8ddc775a1f83badc58e981784703af403d770d7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:19 +0000

    single 569enjoy joke up possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
