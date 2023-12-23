1e6dfbd3a0018c55e21a126b6af1fb6957230a6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:16 +0000

    easy rest face write coffee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
