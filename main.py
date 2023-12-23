7f4db1f2d045126f385d7379408175afdcefad5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:39:32 +0000

    pocket amount it protect how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
