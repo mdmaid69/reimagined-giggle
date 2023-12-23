5e2e42dfcc861ee23ddc9deea32de6a22336af7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:14 +0000

    whoever mend Cc water team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
