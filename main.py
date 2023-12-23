fb2853890c6422367f667e375f66637f8c0486ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:55 +0000

    564encourage passenger tradition act block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def add_numbers(x, y):
+        return x + y
