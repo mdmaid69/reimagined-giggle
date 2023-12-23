ba4f1db16e28c15a5ed494908469a0ce44852677
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:43 +0000

    back mean seem keep forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
