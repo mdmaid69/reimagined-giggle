adf61134be6cfcc379f78a6a9536474cc9aa5e66
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:25:31 +0000

    claim wish what secret everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
