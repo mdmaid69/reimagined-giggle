001089da7aa1fd0fa535f0d89350fa4a755159d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:22:32 +0000

    offer more book slight wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
