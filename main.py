2e3452e1f33a66de5e1830561bc2162df47e41f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:50 +0000

    measure permission path deliberately trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
