4f28751da38283a6713fa48fb8454f0bf2282d91
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:22 +0000

    unite serve welcome thorough sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+x = 10
+y = 20
+print("Sum:", x + y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
