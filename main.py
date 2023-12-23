3b6bacca632ae312acb6ef1552175dd9c9930b11
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:17 +0000

    runaway fat start there wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_power(work, time):
+        return work / time
