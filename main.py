6f749ffe517d76825626b02160bff6345877874b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:53:13 +0000

    seriously return apart few summer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
