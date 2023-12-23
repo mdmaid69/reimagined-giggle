3f486b580437741003a0e4d5dd98c81f67f3568d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:16 +0000

    breathe grow split tight related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
