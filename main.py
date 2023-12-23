fa775ad0c3c1b998ab1dc87835b226fb35cc7146
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:15:01 +0000

    sound stupid hospital study simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
