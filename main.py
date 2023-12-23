f6d04015461cf970e7bdb01b73396514a7aa1fae
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:10 +0000

    talkback education nice raise meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
