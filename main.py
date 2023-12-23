0a76728bca0c666ac1d0ad00615ed15e968a2dc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:20:21 +0000

    emotion mess rent pump runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
