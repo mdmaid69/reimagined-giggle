8bb02a9e7199108a063632e661c1d5be6fa8bacd
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:28:26 +0000

    tread wall owe lend deal

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
