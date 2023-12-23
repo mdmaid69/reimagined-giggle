f6dc58cbc6d3d64e11c5b9d8a76cc2174a6131bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:49 +0000

    gooff debt edge chat forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
