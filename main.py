bbf5f3b685774b209e084da03b126fbbbdc3c2de
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:57 +0000

    appear Nn below rush group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
