884d61205595de6a88cecf5ef93236686ed0af27
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:14 +0000

    lie yet contact song hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_tangent(x):
+        return math.tan(x)
