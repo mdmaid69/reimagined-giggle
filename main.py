419e8b195234b87a94c567b321b5b5a79442419f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:02 +0000

    material rough close nonsense start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_work(force, distance):
+        return force * distance
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
