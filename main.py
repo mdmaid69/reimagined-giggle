372a522178c4fe960db084e906a61cb9d2d026a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:29 +0000

    personal star yourself ilovedicks equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
