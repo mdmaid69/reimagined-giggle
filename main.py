cf924d5fb0f380d603cd4f70943fdd1e0e0c14e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:06 +0000

    otherwise soon regularly Zz watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
