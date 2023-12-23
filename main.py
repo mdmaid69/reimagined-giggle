1eac89455ab4a1a76d8c390a0d2e72aa547b37b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:16:01 +0000

    take to store matter positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
