a21e3163dfe1fd8e81f51174a71f5093537ec2bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:25 +0000

    like during ball quick empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def subtract_numbers(x, y):
+        return x - y
