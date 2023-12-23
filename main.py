86a6221951e68fa99edf07c2795363a466d1d9e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:21 +0000

    object wide activity smile man

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
