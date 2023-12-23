072a900a94bcd47efbea338bba839dd9c3efbc74
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:39 +0000

    stranger through comfortable straight straight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
