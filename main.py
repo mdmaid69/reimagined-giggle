e468163d4c75e90bf63903f87d7b4b32925e4389
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:48:38 +0000

    badly fit class capital second

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(sum(range(10)))
