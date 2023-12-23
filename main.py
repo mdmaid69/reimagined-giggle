cf952084610e2587fc14dd05ee273a1458d67ffa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:34:57 +0000

    regular common sting yours ride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
