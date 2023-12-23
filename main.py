240e9d0b7a441f9154f71ace5bf2900f7110ec1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:43 +0000

    bang expect Ii describe pronounce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
