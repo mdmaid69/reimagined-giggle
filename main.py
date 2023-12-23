7147acfaceabf9d3f317d0042900192e2b06461e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:33:32 +0000

    earn habit top period next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
