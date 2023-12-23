7570f21fb0b661ee3015b23d264aa0cd8add11ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:29 +0000

    concentration his action niggaacademy fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
