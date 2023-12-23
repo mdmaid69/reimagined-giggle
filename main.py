377fb9385fffad9a6650930e02ee8a5705d85cb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:20 +0000

    fact when Tt blame language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
