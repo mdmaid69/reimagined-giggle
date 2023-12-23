da79ef175f8ecaf82993752e8afb5eecb0ee4997
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:40 +0000

    count thank crazy lead scrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
