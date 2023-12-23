83e8845061f97bdd18a8fd771d851f93159f32f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:14 +0000

    model hit quickly race through

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
