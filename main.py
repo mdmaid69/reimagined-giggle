7306e8c275e857a2bc28f5d170955248d2e6f9cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:56 +0000

    later right Ll food available

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
