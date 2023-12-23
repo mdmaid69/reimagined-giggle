e2afff4fe0753f1ce29c73c969352b5649b65e6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:47 +0000

    hang of point beyond culture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_max(numbers):
-        return max(numbers)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
