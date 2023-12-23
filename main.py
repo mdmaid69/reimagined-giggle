1c492aa404f1c06341099788c52d66f1fb14f87b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:31 +0000

    stay respect bury climb considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import sys
-  def get_python_version():
-        return sys.version
+def get_array_item_count(array, item):
+        return array.count(item)
