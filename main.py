ad924051276f002b3924bc0c30925ff1d0646261
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:37 +0000

    pity neck deliver feature contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
