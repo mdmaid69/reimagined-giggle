916fc6b264ef4d53a0aad88fa82d3d3f944dee33
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:37 +0000

    strange relative used lend instrument

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_error_function(x):
+        return math.erf(x)
