3dd572fc2239964df451224b0be5189bf29423ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:52:40 +0000

    left circumstance ready preparation dull

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def is_even(n):
-        return n % 2 == 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
