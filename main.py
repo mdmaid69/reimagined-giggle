9c1dbec80a6ec35646594dbec820e075b008d5ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:19:22 +0000

    keepout pick tour Ll possession

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
