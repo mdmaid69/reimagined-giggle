4c2f37dc7e2c6a526b3bb6216b5e7476ef24bbe2
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:32:38 +0000

    pattern easy search friendly arrive

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
