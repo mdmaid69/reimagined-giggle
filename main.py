f9188459927c27f75a76ded724e4a8bdcb3c13f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:22:24 +0000

    design close owe wrong around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_item(array, i):
+        return array[i]
