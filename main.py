1dfe540ee9d93cab9a9fca459b7491beca4a940a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:23 +0000

    unless sister strength convenient contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
