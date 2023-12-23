ae77be3a0901f007a41c82b4e606003c7c2747ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:47 +0000

    failure ill foreign letter steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
