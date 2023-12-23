26c90509878412c26d8848ca9cf82db05a0142b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:13:33 +0000

    yet offer carry smile next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
