5271d0c8bd38d9ed7ed15720f88599b46fa26f26
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:48 +0000

    name translate extraordinary interfere union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def sort_list(lst):
+        return sorted(lst)
