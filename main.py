160f9968db8fd868ce8e3254f02cd6257462ca20
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:59 +0000

    undo meet pure amount tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
