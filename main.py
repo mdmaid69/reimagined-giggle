9bb9536a6f30fcf0f872fa4642daea125e33d1e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:23 +0000

    hospital pain drawing weather burst

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-print("Hello, world!")
+  def convert_to_binary(n):
+        return bin(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
