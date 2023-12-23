0171bc3e721fbe068f46b819142b3e59cfe92a5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:19 +0000

    both and goout correct alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
