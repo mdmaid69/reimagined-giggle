c31d1bca08271c38e2baf0591de5e0868287d66a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:40 +0000

    left operation bringback mind desperate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-import math
-print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
