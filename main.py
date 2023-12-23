819a79f52e1b2fa9bb6f7c757a24da42bc5d7ac7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:37 +0000

    approve takeoff keen scale now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
