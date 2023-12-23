c049461ab205ed068ec8a12e72830bd9e45f7c69
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:02 +0000

    soon deal birth treat gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
