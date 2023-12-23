232ef1ee01f2778b03d6e928232e24d4e7990627
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:35:13 +0000

    bet persuade progress what spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
