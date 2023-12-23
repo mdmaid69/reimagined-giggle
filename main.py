c3ff7e342bac5c303eac52ad5c8be9e77d29795c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:47:36 +0000

    mark soak sky hard pencil

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-print(sys.version)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
