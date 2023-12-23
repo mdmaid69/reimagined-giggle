5973df4a6bc5448074df382463c2758a7863ed47
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:39:43 +0000

    provide nigga comfort surprise delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
