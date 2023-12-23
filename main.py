18859a02b1f44ee8cca812be7b934366debf604c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:06 +0000

    heart fail freedom object minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def clear_array(array):
+        array *= 0
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
