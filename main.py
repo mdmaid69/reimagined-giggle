4107db28cd92138317b6ea1880ae298448230dcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:31 +0000

    raw tense lock downstairs warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_item(array, i):
-        return array[i]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
