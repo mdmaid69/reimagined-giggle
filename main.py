c306f1374c66a25beab25d81697868556bc936a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:46 +0000

    joke hit meanwhile lie feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
