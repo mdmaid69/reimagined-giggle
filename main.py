6dfa6299b161918c832af0e50246f7d6465696a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:48:06 +0000

    underneath careless aim wish reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
