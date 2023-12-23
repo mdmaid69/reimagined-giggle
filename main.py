b4744a51d0e65089f7ec6dc8f4fd3cb25966e352
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:57:55 +0000

    before union process instant strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
