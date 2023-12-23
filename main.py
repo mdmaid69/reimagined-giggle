76b23807571a9aed303e93204cefec08cb011670
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:59:45 +0000

    fork other possible produce Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
