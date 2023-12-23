182f98a547e6446976c61d14364fad72899fa9e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:32 +0000

    job group file mind score

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
+  def sort_list(lst):
+        return sorted(lst)
