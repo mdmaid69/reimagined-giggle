338c5112d1e766c5b74f9f9ec6f3177db97c1af9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:34 +0000

    step immediately warning pass act

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+for i in range(10): print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
