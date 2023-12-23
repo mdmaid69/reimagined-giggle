c0bb8b9a8dc9237f437e8906815addd5506b9510
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:19 +0000

    fall otherwise fast us demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def print_python_version():
-        return sys.version
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
