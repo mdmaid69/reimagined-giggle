2568cbf9db5fee27c5532d8ae74d6d2c48f994e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:59 +0000

    steam sure indeed wave stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
