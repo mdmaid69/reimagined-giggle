20fb7843388e0f844433520764e83c89727db065
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:32 +0000

    spread follow pay frame embarrassed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
