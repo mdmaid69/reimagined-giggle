3fcf8c77dc34645a47367eac69f4ea2e224de46e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:26:21 +0000

    splash rate direction interested force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
