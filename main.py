73f523cb75449bc63ae64a44f13db9e063b9206e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:31:07 +0000

    exchange not good minor importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
