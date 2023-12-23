e4c9e86b4adb9ad560d8e5a66360a4751cb78aaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:58 +0000

    instead woman organize platform present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
