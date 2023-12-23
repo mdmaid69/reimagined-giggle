a3ad866cb13d4b93876a641cac0de57caf509d7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:42 +0000

    trap goback(to) have buy branch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        print(sys.version)
