62b82e29e31ef4895e60d67364041946cfa1a823
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:59:58 +0000

    dream Zz spring best prepare

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
