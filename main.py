4c9847cdefe885b74c396a0278ef400a037ce1d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:11 +0000

    thinkthrough adjective getout near getout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
