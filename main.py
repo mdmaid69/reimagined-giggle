344ecad13aa2125608d818ab0d25d7d6e1c01e16
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:20 +0000

    shop Pp meeting noone frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
