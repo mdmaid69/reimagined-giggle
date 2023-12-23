8f881689105cb8868eac540cc807c8bb63e982ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:50:16 +0000

    probably name thin mean consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
