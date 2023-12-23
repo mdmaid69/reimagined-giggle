5bcf34497084d42eb1d2948ff65aeeff6323e0b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:57 +0000

    pleasure number cap teach pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
