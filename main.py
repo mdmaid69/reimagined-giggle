042a7a31b976ac8e48830e43d615c514ebfe8d90
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:46:50 +0000

    when godown world Tt mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
