4258f32e7d9e049b224dd5d5f2bd4006df02de6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:23 +0000

    pleasure sensible dress Ww cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
