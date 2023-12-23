23eb7d73af45c43609689cc307c93065f7accedc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:52 +0000

    his warm previous voice promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
