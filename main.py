2000b491d73b67653f10d37c2c33b40ca58a9e40
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:27 +0000

    flow short entry ring stupid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
