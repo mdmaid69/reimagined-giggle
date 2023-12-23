5e42f5bd12f36cc5b38eaa728e649affd703a227
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:07 +0000

    cut guilty straight term touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
