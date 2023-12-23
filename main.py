0a078277855fd86dcd8ef3c4a2a37929beaf47fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:36:40 +0000

    best however no then challenge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
