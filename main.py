be22f2791e95fe9b400cabc7e1e42612dbcab132
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:33 +0000

    patient steal Pp about him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def divide_numbers(x, y):
-        return x / y
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
