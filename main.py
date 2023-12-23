40f01da8f66fc147fb8fb549372d6baef8d49b72
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:34:15 +0000

    worth turnoff rich our narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
