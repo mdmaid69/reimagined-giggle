3b9fec408bd295cbda1c0fca84d7ccf60e07d4f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:16 +0000

    turn disapprove catch prize shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
