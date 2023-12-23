764ebeb7dd8b51cc51e91ea51af74b062bbba075
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:50 +0000

    add popular generous bury autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
