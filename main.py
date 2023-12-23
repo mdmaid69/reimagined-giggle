22d82011eadb6e4ec5651716122b0d71325ef7c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:10:21 +0000

    bet unable pick separate comment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
