7936159c35a92cb1b6909f75ceae558d729a2729
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:12 +0000

    voice tend he judge Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
