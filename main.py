58ffc06766dae3bf51392c834a9027ebc3a775cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:17 +0000

    term bunch plastic hold takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
