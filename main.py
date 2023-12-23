2b237c8fe4042e3affbcdbc866b8008694eb03ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:45:51 +0000

    stay getoff far anything seem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
