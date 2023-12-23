f14187b5852f00fa7fab588a0832c497288a869f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:59 +0000

    giveback tread should turnon pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
