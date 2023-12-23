723775b1533ce9e2d3ac88df7f771a9cdca23a85
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:13 +0000

    generous bar usually different birthday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
