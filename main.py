178f75c07defbac63fb8c5da1dcb93f5964f9471
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:33 +0000

    571enough Dd at Yy protest

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_str(array):
+        return str(array)
