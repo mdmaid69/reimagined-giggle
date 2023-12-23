e41e4ce89f17f1808c6a732ccea9967c27c79ef6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:39 +0000

    nurse judge ours power morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
