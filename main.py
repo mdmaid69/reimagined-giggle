992648b52da7de4f11be08906902bc0eed634847
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:14 +0000

    obtain whistle where but store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_typecode(array):
+        return array.typecode
