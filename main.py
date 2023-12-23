21d9aaa82a47739722407d232ae2bfb26b7cf29c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:36:20 +0000

    aim top there people sky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
