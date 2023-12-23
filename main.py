823eef2edba8a78c3fa0460f2b332e09a3bb5cab
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:22:14 +0000

    rain worst identify night can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
+import collections
+def create_counter():
+        return collections.Counter()
