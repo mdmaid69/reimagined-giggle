d480cb5e2955b3d1b200350fecd95c301bca5628
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:47 +0000

    pleasure press colour team conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
