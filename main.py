619255acb0e06bd8f51c70df9ec42a2e7d4e0269
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:47 +0000

    OKorokay already get expression thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
