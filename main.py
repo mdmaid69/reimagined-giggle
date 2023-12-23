a4bc6eff8066fc09f27c70046d43ff3cfa098244
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:09 +0000

    detail disagree shallow handle scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
