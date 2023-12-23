6dfa215d56bf7a515150a0632b9c2ec8018481ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:15:14 +0000

    underneath bet wall influence away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def reverse_list(lst):
+        return lst[::-1]
