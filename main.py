c7dbbb2c699720869e0ba82e10fcdca686b512f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:59:38 +0000

    exam probably lay recommend Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
