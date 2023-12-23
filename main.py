7a17005eff7f9d0325497cb3bd3c11e8b31f6e1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:46 +0000

    interfere whether doubt pipe woman

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
