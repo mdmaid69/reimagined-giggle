16fbc6ef5dc41474d27426d32618bca0cc8fbc40
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:51:16 +0000

    office cause boring range passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
