d2d83f4d5709be130b259a84720ec72e3e2d019f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:33:48 +0000

    brown 564encourage across heart low

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
