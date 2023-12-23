4e2e5c9f5bf2d261949e2bb74cf2f5039df46f1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:20 +0000

    play damage drink needle find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
