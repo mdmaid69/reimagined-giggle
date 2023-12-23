811ff717a98fa892664f3b20b0f58de8216510d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:04:19 +0000

    mixture but steal flat condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
