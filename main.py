3bd712d5bc75fbd4875c139fa742b54e5354b4a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:38:00 +0000

    crowd weekend plate mysterious stamp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  def is_odd(n):
-        return n % 2 != 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_work(force, distance):
+        return force * distance
