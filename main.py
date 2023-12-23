405e80c9b791da94f7ec7123357acfd91ce4aa82
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:54 +0000

    pot chief it inform dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import platform
+def get_python_version():
+        return platform.python_version()
