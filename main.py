47804577c1357b1ea3e3655503d2d5ce4330b81b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:28 +0000

    keepup sticky make appeal steep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
