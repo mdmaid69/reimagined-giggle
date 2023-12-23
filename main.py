ee17c89b7f003779b6e8de32745dc589b60fa7f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:07:16 +0000

    chase command goup shop treatment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
