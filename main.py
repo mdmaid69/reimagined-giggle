aab42b625164ab3c00deb3afa1fb9596062eae46
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:30:13 +0000

    today uponon wide activity certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
