1c2cfe0b8994ccd9c3ff2d873edf685f7f104353
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:54 +0000

    band bottom really wealth mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
