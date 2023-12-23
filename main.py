36b942179229ad0b8e58da06b64441d86135a05c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:07 +0000

    figure single much evidence standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
