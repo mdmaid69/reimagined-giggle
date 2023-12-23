41027034796cf187cc5f125cd43fbe3c0267213a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:35 +0000

    guess negative tell doubt keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
