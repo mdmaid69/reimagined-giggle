b284c2ce66a65c353da1316005f17785c892e608
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:02 +0000

    many drive too software get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
