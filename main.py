e9cffdcc2d086c6c837d30483f23f3916c93fb0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:44 +0000

    size pencil supply laugh welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import platform
+def get_python_version():
+        return platform.python_version()
