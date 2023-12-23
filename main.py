36a70745eca774a9d5ca07eb6c8a26d5da54e01a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:26 +0000

    long there outside afford behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
