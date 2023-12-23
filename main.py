830b58e244634d28e97e0222867e33e8895f54e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:35:34 +0000

    compare splash division measure alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def reverse_list(lst):
+        return lst[::-1]
