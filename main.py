da0653cdb40bdbba0ba88b3725df741ab7b4b097
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:42 +0000

    evening middle mine pen under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
