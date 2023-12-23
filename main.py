debdbfab38a1ab885863d25c838c3f26d734e3c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:17 +0000

    practical watch exact strike mind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
