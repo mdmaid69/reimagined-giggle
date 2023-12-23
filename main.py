0c11c7fc21d21fb575021735dccb91807e1b1e6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:59 +0000

    exit trust left politics claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
