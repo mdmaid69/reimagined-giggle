9f0553bdb6578c8ea5124bcbba92a55f90dea067
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:23 +0000

    university left jump calmly spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_logarithm(base, x):
+        return math.log(x, base)
