466c87d04420ada8552d1bf85cec68e6df4292fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:59 +0000

    Itself rule job plan swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
