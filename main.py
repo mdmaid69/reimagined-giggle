df8299734673f0c8c09a495bb36f640e18377101
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:47 +0000

    height sharp bored declare early

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
