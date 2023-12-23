12835ede5d7164976eb5e9217f3d8b07b7310fde
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:45 +0000

    skin probably anywayanyhow last intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
