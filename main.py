eef6257bc3e4ef3a26d93c224d45bc5aa6eff1ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:08 +0000

    anybody occasional transport goback(to) people

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
