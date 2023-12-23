c0bc9c7d0446db24aba7026579faa7cebcbdd7b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:49:58 +0000

    rush attach fence fortunately theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
