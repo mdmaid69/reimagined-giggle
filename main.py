a18c25b8eed9bfc379cd2c47087770d5c87cf39a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:44 +0000

    doubt successful spring beat narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 def is_even(n):
         return n % 2 == 0
