1209df86596a3009fc90b65958a892b3b942bfba
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:58 +0000

    declaration away why themselves convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_as_tuple(array):
+        return tuple(array)
