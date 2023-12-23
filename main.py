d3f33ae46f43151ce286dff6031ea35c366ea0e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:52:45 +0000

    heat once upstairs such some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
