c5e5c27cd7cbaf5bdae413137a83e1143911f066
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:30 +0000

    rate mystery fork manner again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
