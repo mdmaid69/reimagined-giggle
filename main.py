7450787d1cea4609d3120a7930c4811219df5afa
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:10 +0000

    bunch score turnout check giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
