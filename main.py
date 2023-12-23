496aeae55a3bddccaa3071e95e9fc23356d1e9fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:43 +0000

    actual date space time threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
