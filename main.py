7c5ea727a20de4532b4f89c3353d5322955409ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:23 +0000

    switch somewhere after deserve no

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def clear_array(array):
+        array *= 0
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
