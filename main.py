8ddb7e5debe80af3dc88ce1908cd022349f3ecc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:51 +0000

    drop Ee comeup doubt in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
