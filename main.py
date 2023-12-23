85f1220c030d253ffa891588cd04641c04a0ced3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:01 +0000

    accept throughout the shape floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
