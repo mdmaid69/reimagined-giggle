807b4a2f6ad481717bc901762fea37957c6a54be
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:01 +0000

    test separate late single them

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_counter():
+        return collections.Counter()
