be8b899d01a698c3c3726baa47ca9128f03d3ab8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:57 +0000

    further after noise taste right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
