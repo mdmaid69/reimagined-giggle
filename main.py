3b9d28f2eff886f15804949a6a80acf6b2e24eeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:48 +0000

    brain for press building ring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
