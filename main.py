cfe3f07d0550d3f30122aa886c6f75ecbd3cfca9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:04 +0000

    creature plan post trade offence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
