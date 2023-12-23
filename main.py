d1bf58c9425e00c19b64cbde6400df70ec01466b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:47:35 +0000

    juice thinkover learn ever die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
