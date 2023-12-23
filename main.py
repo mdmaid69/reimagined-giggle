94a86e18b39a6771b439f3f08f5b1cd2cc0d4f87
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:18 +0000

    attitude light angry chat total

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
