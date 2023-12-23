eed70992bbb3ea32494f399ae4e6f1a800f83a88
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:39:57 +0000

    rent workupto pleased tight spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
