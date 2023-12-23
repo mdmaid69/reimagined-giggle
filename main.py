85b9956a8b141ba6d393db6167ac63d0f0f6b995
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:30:59 +0000

    cry peace comefrom explanation head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
