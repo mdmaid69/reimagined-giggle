109ed04a340c364c5d325a785f669f94fa5d510d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:39 +0000

    climb steal bank advise private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
