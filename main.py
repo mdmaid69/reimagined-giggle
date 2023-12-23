0fb9062f26c9e34351f30cebfa9fa970f232fdaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:16:39 +0000

    arrangement hard bread difference fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
