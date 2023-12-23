62bf83842a2d02b8a02dedb29056d3fedb6a023f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:29 +0000

    few guess emphasis dance window

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
