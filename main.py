50eb044ac1f2ae27ae86330c9cf7e50a6b5252cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:27 +0000

    noone tape careless shake knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
