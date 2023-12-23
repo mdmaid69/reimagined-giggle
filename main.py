aad65cad27899dd9ca4aa084dc4d34d990dba131
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:19:53 +0000

    hide keepfrom invitation announce plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
