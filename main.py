66142a47ed296a2b70926149c5835dcb60400f8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:13 +0000

    chin neck post bet middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 def calculate_area_triangle(b, h):
         return 0.5 * b * h
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
