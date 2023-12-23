ed999239fec87ec1dd42ed7312cdbd4661199ac0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:19 +0000

    close after expression trade unlucky

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
