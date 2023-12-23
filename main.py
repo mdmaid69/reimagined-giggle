ff000eb5a453d32fe89643a06f248c5c17a718ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:52 +0000

    recover demonstrate error muscle absolutely

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
