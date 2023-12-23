2135e89cba6ccd1c9e20e8657400ad624bb53307
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:07 +0000

    one middle getout the noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
