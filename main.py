a64780519e68127eaa9e7836921feafc48af0c1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:25 +0000

    place free sort whisper book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
