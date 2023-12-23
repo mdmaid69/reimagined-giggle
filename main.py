4543c20eceecd2c8bfd6a43dd3385f8a70befb76
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:38:24 +0000

    difficult region watch spring through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
