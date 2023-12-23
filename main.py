3b3629e8f3cef9b0fb5fb3d726203d557d482a4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:01 +0000

    fact office degree soil write

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_max(lst):
+        return max(lst)
