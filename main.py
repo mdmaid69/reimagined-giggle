0177bb25f313b2573ee8142a189d9137bf88fc4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:06 +0000

    will mood hurry weak Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def sort_list(lst):
-        return sorted(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
