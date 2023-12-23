08d750d8b1e8b1a29e96f9cd4d7d75f57a78546f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:15 +0000

    too about choose age loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_min(lst):
+        return min(lst)
