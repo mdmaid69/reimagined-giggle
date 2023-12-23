adaac468962a72c510a97996674c6628f5791fb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:43:45 +0000

    remark natural either keepoff lookafter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-print(os.getcwd())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_counter():
+        return collections.Counter()
