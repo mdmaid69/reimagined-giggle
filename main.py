30582587067392b2afffb43f9715cf147d7d7949
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:13 +0000

    sweep aware flat spoil room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
