9c23973d91679e1b12af270423c39e7eda607a58
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:21 +0000

    bringup pin ask safe tear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
