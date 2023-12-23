df3f41f57307995b64f4615393c9803786ec9a65
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:59:52 +0000

    other appeal lookback(on) understand software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
