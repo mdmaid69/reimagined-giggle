96d121cf939048a318820d7bd9ef4c064e9ffbba
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:09:13 +0000

    beginning workupto display bread pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
