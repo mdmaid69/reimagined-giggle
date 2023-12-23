bd376c7891a1399189bbbcca488ae76c7022f19c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:45:08 +0000

    degree laugh freedom impatient decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
