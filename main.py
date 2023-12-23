8ca578db4edd7a6e5cad7fe647cec3fac50ea2ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:35 +0000

    cry congratulate sink turnaway out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
