ca047fddc1e4861ef71f03988d263eb064dc5a67
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:56 +0000

    study once today cover wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
