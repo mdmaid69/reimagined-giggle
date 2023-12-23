72a81a4cee62f6a5d1e6a6e1068c5664546f290c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:56 +0000

    entry Ww hear vision strength

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_speed(distance, time):
-        return distance / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
