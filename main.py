6d19e79a2dd989962f857b01c4fe950e6eb3f985
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:41 +0000

    shop uncomfortable recommend answer better

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
