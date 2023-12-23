ebad5bd1f02b950526186fe1f81344cbc25cf07c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:19 +0000

    tap meaning consist generous demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
