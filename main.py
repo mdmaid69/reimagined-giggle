689981cd5874b1b13af8e5022272587539db4357
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:26:16 +0000

    shake new speech after performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
