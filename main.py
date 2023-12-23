10830d1ec39d400c72a7bce533fdcba7f7e9daba
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:29 +0000

    bar spread contribute base favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
