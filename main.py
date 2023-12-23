6dcaf9dfb00edc620f2ac38952dfa6ccb860a1bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:02 +0000

    condition other play cry whole

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import platform
+def get_python_version():
+        return platform.python_version()
