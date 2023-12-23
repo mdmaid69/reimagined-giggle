e590a3d78925a23770a714163a27906f7e3edcf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:04:19 +0000

    unit than well nowhere pause

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def sort_list(lst):
+        return sorted(lst)
