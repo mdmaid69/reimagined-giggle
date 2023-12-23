d63a99e7f7b3605a86f56f8a9f9056f9ce5aa6c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:17:53 +0000

    word scene store regularly classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
