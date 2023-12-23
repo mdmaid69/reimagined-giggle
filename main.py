74b3e1bb35e9d9a36f4a67a4a9b84f704e895392
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:29:44 +0000

    art block quick unlikely breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
