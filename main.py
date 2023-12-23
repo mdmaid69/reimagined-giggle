a170476e55800020e694424c1e3f7d9eadc6497d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:37 +0000

    increase violence lunch giveaway backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
