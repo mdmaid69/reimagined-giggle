e5b5c7cb5de1a29e65f088b09496ab40221cfaf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:37 +0000

    air much dig inside terrible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
