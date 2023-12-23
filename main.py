79d1bb61b5ad2a4aa7a2443fa4b0a58d96288cbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:12 +0000

    tidy home factory best regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
