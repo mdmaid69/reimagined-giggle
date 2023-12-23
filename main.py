352a66b40ba1b14ba4269931389821ad762705af
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:17 +0000

    564encourage actually soil Jj serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def append_to_array(array, item):
+        array.append(item)
