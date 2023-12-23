819a8f44841f55f9ceceff185d70c024f61c9fcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:11 +0000

    upset lunch accident visit pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
