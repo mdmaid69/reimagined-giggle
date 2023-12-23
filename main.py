bf5d3884c9bb64690f79e75f797cb201f4e38c22
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:10 +0000

    keepup opposite position book recently

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
