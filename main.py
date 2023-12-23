35218e71d76f09e49e8961afd64f8a2a0c398489
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:04 +0000

    limit 564encourage make keepout belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
