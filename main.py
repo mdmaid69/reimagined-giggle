da80ebb5e06688ff343237c5dfba907e56fc1bfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:22 +0000

    speed university use steer effort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
