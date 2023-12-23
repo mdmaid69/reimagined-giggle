894a4acc416d351dee1f0650db3dda02caa2f80c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:55:46 +0000

    apologizeapologise dark soak ear can

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
