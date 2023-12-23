e1178e87117c7cca8e761503a12c613b9ca9c709
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:03 +0000

    shock better leave giveback Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def is_odd(n):
+        return n % 2 != 0
+text = "Hello, world!"
+print("Characters:", len(text))
