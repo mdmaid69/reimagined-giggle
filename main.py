eb807184c584a5be39df878e2dbe4a7de8f537ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:34 +0000

    short slice plan soon progress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
