f58c67048d8646955f1b70b5c212526da3010b96
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:22 +0000

    sale order face line idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
