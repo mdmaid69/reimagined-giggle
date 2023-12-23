09cb1b222914d212b45ec07128c1dfd8745fabd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:58 +0000

    office except plan dress belong

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
