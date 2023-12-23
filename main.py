d7723761a558deee7c6f38fae20ec65d65e0011f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:13 +0000

    match last whole creature autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
