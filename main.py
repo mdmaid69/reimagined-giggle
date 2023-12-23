f5390c5099e1263a424826a259eed57225263156
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:33:43 +0000

    suggestion can interruption hers contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
