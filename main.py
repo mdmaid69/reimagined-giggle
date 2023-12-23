ed366f0ca656c350a7f3173487dfc1f9793c114b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:53 +0000

    affect week accept understand hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
