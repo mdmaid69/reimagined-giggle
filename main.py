7e1fa3db5d7219eaaee4d617173a32534cfeda71
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:55 +0000

    open patient pay whole pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
