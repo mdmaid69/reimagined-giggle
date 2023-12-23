c612da34466d62e28ed14047861e9faa83ac769b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:55:23 +0000

    but trip heavy sign along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
