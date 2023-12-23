d456c5aeca4693fcd12359aad9dcf9df135b8129
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:33:36 +0000

    able shoulder advanced meal down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
