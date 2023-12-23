911d80937a396b05b46b40775c360464a2efc810
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:30 +0000

    capital none prepare bang backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
