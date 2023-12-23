cebb31ffdf839802efd4afd418a232fa5dd25f52
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:20 +0000

    separate pupil rank ground attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_priority_queue():
+        return collections.deque()
