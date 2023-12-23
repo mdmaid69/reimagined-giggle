f6d2019aed62fef929e5a6bc498fb972ea60959b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:24 +0000

    friendly criticism new right inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
