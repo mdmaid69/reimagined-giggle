59e6fbdbba38ca7114ba0793b8864ea49fe15ff3
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:13:48 +0000

    empty ourselves could lookafter try

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def count_characters(sentence):
-        return len(sentence)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
