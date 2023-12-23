4354cd12d37dc8511a4541f737e6eb43214e1572
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:21 +0000

    ever world giveout piece heart

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
