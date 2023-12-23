caa7b8dd48f52b886c4f73a330e1bd89a3346935
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:27:59 +0000

    intelligent why post murder pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
