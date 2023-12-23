9af7a2968a435c92bb6b514e7cc4b79213b4dbcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:09:39 +0000

    slight split expression mother top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
