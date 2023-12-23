38b7fdaf3d353f177338745cda1987514318de9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:10 +0000

    familiar advantage wonder contrast everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
