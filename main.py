a2e6d7faccd0e96d80b81da81144362a6c0be411
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:58 +0000

    breath room hold across less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_speed(distance, time):
+        return distance / time
