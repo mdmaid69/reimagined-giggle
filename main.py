116ea95476f65dc5840c1538d2f1736118283009
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:45:33 +0000

    slight acceptable catch protect himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
