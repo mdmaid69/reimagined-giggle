664e3fa6979e1fafafeb7d42f06708b0b7a6214b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:40 +0000

    occasional thinkover pool round company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
