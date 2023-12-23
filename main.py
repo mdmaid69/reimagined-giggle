5d580b65215c67a9e02b4cb78b13f6e672e4f392
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:42 +0000

    slip maximum runinto size or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
