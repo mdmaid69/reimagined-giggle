cc72f938725fae010dd49221717f942acc2bc961
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:52 +0000

    black hair ground violent hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import time
+print(time.time())
