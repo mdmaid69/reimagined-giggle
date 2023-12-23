24585751712695544ccf94075a5cdc9838e17e6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:10 +0000

    knock intend tense cruelty shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
