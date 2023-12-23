15cd5c3b3f3a59e2aaf48f324ce5554da4222f2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:18 +0000

    black news off classroom alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
