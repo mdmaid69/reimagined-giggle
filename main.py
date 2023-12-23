0492c257c81db9f9214a06c4965ef825d712442b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:21 +0000

    positive sell sharp passage future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
