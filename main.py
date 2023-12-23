b338f6a904fad84b323d93bb8ad0a636f1ecee86
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:04 +0000

    gas seriously I love peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
