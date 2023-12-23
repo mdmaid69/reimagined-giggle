098006b4a45930676731db39b0af51841c4949fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:11:15 +0000

    damp call dangerous call firm

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import re
-print(re.match("h.*o", "hello world"))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
