a281fb18c409c48a5d20fbebfbf46fd638e10c92
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:25 +0000

    affect run black sudden challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Words:", len(text.split()))
