7f29401a97a906104f600e0924a43a35c9c7ce65
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:42 +0000

    part cap wait push holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
