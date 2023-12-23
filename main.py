aa5e6d1ee3c0cd5254740276261d06dae6297299
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:57 +0000

    Cc study work bang divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def convert_to_hex(n):
+        return hex(n)
