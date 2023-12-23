2f3fe29555bb81104c77b6a8f972b95ace3ccaf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:07 +0000

    breathe butter escape eager from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
