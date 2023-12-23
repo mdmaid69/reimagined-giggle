4169020088e4fe76c5ee3af9e3cde0844a1bfa8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:45 +0000

    duty string reduction new service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
