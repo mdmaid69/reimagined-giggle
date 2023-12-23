7c51c12233ef6352ef4490904901cc72211768dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:07 +0000

    buy prevent wear progress do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
