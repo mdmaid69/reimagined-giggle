fde0c3ef659a19585f6ca5cb10a3cf63f1be31d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:31 +0000

    unable heavy thirsty minor takeout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
