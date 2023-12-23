82cb401ba15cd07e38d61b7999fe37d57827539e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:00 +0000

    close left pause land cruel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_tangent(x):
+        return math.tan(x)
