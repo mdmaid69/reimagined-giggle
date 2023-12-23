6129a4c537adebe7cfeea08a7fe922b4f49582da
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:27 +0000

    discussion address brief travel centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
