b044013e4fd62c092e024b28df3b185003234f40
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:38 +0000

    every lie top flow listenfor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_array_to_string(array):
+        return array.tostring()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
