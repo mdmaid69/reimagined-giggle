735f963e6bf747520a7add928629ed9146a0a5f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:06 +0000

    public art bush chief 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-for i in range(5):
-        print(i)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def add_numbers(x, y):
+        return x + y
