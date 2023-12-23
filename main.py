8c54f1a94276da3ec3ff5b8593310b986b389cd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:01 +0000

    lesson crime stop dress play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
