799e975c429cb3a35522c7fcad49175346736e06
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:58:15 +0000

    spend experience grade weak ago

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
