e5ddc116011bb23e2e0e8023e83c41b06a76db57
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:15 +0000

    tense minimum stamp attract character

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
