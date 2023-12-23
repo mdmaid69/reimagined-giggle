244a6ce68d9d20fab5eb57300d96aed0eb87af66
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:05 +0000

    fold look professional mine Pp

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def is_even(n):
+        return n % 2 == 0
