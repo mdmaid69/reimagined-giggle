bea5dd4115b0e2d3201933a173e96c7e0d40fe93
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:20 +0000

    butter always whatever measure exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
