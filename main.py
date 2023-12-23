8f2a8cd2f686a3ad7817f2958f21addc1b816aec
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:11 +0000

    seat to artificial undo shake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-import random
-print(random.randint(0, 100))
+import os
+print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
