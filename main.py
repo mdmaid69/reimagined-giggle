f9dd4956e45d10f689377374f4c39d9146857cde
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:43:37 +0000

    strike smell back mad medium

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
