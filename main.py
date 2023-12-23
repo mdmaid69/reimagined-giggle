a72539a4d333edb2c9a90e83676d724f2f3eed05
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:59 +0000

    hurt offer foreign forgive worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
