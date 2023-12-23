1cd0551c9be5603acd36fff70789132df8dae91e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:18 +0000

    light basic behind turnback body

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_volume(length, width, height):
-        return length * width * height
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
