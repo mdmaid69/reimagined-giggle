416ccd6b7fdf591069a73bedd8661f7270138e59
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:10 +0000

    patience top deaf tie unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
