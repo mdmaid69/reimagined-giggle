8d68f1157c07489cedf0acb274fc8b0bbb224180
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:10 +0000

    pour star child country every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
