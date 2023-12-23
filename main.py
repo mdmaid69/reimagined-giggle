58ee9870da02e2485f2418fd33c90c5b68bdd01b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:15 +0000

    success sharp walk hurt talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+print(sum(range(10)))
