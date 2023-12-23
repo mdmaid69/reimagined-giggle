a780f2ed9b3df26cd34acfa24cca62adfa94c707
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:32:45 +0000

    blame ashamed welcome emotion Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
