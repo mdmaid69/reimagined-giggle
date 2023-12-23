8dda50f23647c8df5906982c01b051b455482f69
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:21:33 +0000

    there hurry lead wrong stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def create_user_string():
-        return collections.UserString()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
