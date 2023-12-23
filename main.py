750044ec1c8b53bdbb36086ec2ba830406ea2e2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:27 +0000

    but gas back tight trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
+def square_number(x):
+        return x**2
