38737857f25d7de74178292180d8f94bf2b2ff69
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:30:49 +0000

    message basis defend section description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
