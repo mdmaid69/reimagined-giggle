d201d9be90dabb6e2331718c470154a20f7bbfa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:34 +0000

    hide draw discuss relief takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
