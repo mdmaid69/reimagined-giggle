a1a452eeca42e36ebe04c0e5b1b81b0d32567ff1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:30 +0000

    odd talk experiment strange bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def get_array_as_list(array):
+        return list(array)
