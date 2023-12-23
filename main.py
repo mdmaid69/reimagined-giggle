7668a26196bac15c4569dda4c08878abd50925e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:37:43 +0000

    Tt organize shop lunch off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
