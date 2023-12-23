32a0e25540aa0a99bdc22a3c2e132ba8c7202a6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:30:27 +0000

    system bin hello pollution among

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-print(datetime.datetime.now())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
