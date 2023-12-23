5a9dd57b9d3e2df887d1f6523856d96e0e2c40f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:37 +0000

    transparent trap operate protest difference

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
