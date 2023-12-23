1fb533bb57224357269c3297787771520c504503
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:16:14 +0000

    shout split tread wipe waste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
