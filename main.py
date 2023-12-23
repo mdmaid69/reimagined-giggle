b7d990d87b30ed2720acad2fc53283d00939e65d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:30 +0000

    Mm left couple study necessary

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def reverse_array(array):
-        array.reverse()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+print(sys.version)
