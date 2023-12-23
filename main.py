dbea271ae3cf7b2162c194834bb69a65d499db22
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:10 +0000

    Vv congratulate preparation glass hate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
