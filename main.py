589f4746ede491520f42a0811a168d0ec36cb688
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:37 +0000

    view able subject model well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
