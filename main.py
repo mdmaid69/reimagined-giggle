828ed83427de5986828cdde9e3ceabf507b02af5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:54:13 +0000

    obvious arrive approval because steal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
