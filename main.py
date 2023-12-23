383fa7acf4fcffaba57d4eaa69a899c05e352fdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:49 +0000

    female ring slowly strike this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
