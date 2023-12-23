b5b2b60ac0a123367d8ea6e849bc2d9e215cdbe2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:20:05 +0000

    mine trust workon ball medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
