80addf94605102f2f875eb6669271963e64021d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:09 +0000

    plenty quickly size throw later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
