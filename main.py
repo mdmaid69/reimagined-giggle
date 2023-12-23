7abe8c14b3eaf518e600e81b1fd0d570c55aa5a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:18 +0000

    workout talkoutof ought up comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
