0d781e0152ab04af0958691a3d63e6dc31d1c935
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:23 +0000

    meaning slice have music Oo

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sys
+def exit_program():
+        sys.exit()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
