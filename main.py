e361c81432071364d910e01a349ce006bde052a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:19 +0000

    all offer wrong getout Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
