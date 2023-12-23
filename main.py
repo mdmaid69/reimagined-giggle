bb0a15f16d102356594e740f525309f98c22edde
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:39 +0000

    usually lose bunch Oo inquire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
