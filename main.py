7732d559adf927d0add30170daa2bcf7bfdeeb0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:35 +0000

    age please breakthrough hers crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
