1a697825716668f4cc70e83352b596409b2b9402
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:11 +0000

    loose turnback later broken carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
