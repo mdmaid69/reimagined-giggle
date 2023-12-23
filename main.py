b1ae91cf70d42f68d0c9a4addc62dc2c6b8a3395
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:29 +0000

    year Ee next into praise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def get_time_since_epoch():
+        return time.time()
