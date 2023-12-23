6e14e6bf9e31b9f1cd57d2e6a2d050444c76dbcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:27:10 +0000

    bush too through still pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_itemsize(array):
+        return array.itemsize
