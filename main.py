890ae1f762b93909c0fda3f899c76f24eeb7cfba
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:03:15 +0000

    interview small consist later dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
