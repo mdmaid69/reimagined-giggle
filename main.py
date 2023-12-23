f93eb324e0c683ef027bf0da4d687210248c95b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:47:49 +0000

    strict activity distant fork lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
