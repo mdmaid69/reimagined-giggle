be420c315837fa1627af5ec1fd250bc43b76b3a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:31 +0000

    debt dangerous delicate sink tight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
