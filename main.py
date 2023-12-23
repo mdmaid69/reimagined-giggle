764a92d59d7026d3f9b00f4859084a9fc541ca0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:55 +0000

    church loud drawing balance die

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
