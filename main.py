cb7bb29ebd574fe2dc70f8ed86801a0bf6a481fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:54:01 +0000

    copy impossible communicate greeting even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
