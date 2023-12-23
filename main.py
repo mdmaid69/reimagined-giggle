ae1a28ccd1c6b8a1ae74eb8edfd94e8e289fe43d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:24 +0000

    fire separate damage organize worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
