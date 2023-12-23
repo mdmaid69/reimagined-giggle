40b3fd294567171652aa90ef892d0a9cd377011f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:36:08 +0000

    equal anybody price separate temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
