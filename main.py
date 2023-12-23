ea839c0bac85748c3f5fe786c3b6264ebf4f264d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:59 +0000

    add side early theirs nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
