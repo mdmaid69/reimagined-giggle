ee825a32d44b48f810507dd6f19d589a3f2617c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:19 +0000

    polite gather skill hand knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
