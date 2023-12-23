2564c773cf72c00cf60a1a7875da8aa79a73c860
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:16 +0000

    advice perform stranger thought rely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
