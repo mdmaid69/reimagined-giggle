2d06ea13aff93ec212a0cdb831b1e30fb66d02f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:19:41 +0000

    hello announce operation post offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import json
 def convert_to_json(data):
         return json.dumps(data)
