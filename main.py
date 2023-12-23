d42b577753f07fb0685f7459942ebb7a328e4465
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:50 +0000

    past interview but attention advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
