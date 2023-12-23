1e1893c7b19dee3d974970c5168326744d027858
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:47 +0000

    thank runinto load shopping share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
