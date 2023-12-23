a91ebd4e8d31e28583a08c29660a1d11bd0136d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:17 +0000

    lie weather any apart belong

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+print(math.pi)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
