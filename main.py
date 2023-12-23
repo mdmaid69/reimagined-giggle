dadcbf5148fcded59b54a5060053535b82b96107
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:54 +0000

    advantage government anxious dead problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
