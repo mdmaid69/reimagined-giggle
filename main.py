66a0f24a16c759dcd5dfebc8c9dd164ffa99c999
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:18 +0000

    wide target wave although poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
