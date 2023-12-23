b7e01ef67ed1e92af6d9de22c4847cf0ce1dd94e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:23:17 +0000

    coffee help foot impatient contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
