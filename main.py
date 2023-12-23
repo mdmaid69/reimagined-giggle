b504010fee05eeadd6377206c0902508c0e59964
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:22:47 +0000

    knock school solve again easy

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
