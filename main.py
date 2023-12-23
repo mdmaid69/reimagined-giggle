ab7a5c99f342336393a6d0a1a5a04c0df07ba0cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:42:17 +0000

    niggaacademy shame fall lookup handle

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
