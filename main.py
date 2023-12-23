e2781bb1da3f2d7f665b6c51a9a9b232c95d6aa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:02 +0000

    tie comfort callat dry ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_elements(lst):
-        return len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
