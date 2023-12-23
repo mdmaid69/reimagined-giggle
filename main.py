51240cac9f2e0b84a1363fd5999ce8769e36e91a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:57 +0000

    rich second wet present ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
