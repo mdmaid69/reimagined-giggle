6e461584bee2f8dd42e2da18354f139eb52c5285
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:46 +0000

    tone comeoff bringdown splash idea

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
