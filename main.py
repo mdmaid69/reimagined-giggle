3fbef2ea4b59187dca0409ac5cd337ab633c488a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:03 +0000

    tap tip today something check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def is_even(n):
+        return n % 2 == 0
