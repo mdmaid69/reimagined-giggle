3bf668f516ff335d9934209de32d056acc242072
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:42 +0000

    Hh means smile general travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
