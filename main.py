910ad7d93ad39c44cdf73a8d65ab1d2cb0dd361f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:13 +0000

    wall right division realize ill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
