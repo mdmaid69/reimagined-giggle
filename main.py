933646e95a71da26520ed9baee65aa219fc00158
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:15 +0000

    light peace wear takeout permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
