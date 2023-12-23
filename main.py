65d924b8b5ef465c6c01c57da4404e82a42d1e17
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:49 +0000

    law thank book snow actually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_binary(n):
+        return bin(n)
