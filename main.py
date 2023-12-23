cf4dbf40489a92d502e9786b322860b3950d056b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:37:22 +0000

    create sure simple shut season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
