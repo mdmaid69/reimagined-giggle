cad7b76f46bd8fcab0119d65426b20411028d682
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:55:46 +0000

    thin rarely old sensitive state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
