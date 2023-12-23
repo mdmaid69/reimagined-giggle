8d16cca21a8ba19c981987bb8275661985ebef34
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:16 +0000

    prevent giveaway stranger will mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
