87d49b591aa83a437a7a289c7d4a7abe9c20b96e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:44 +0000

    how close contrast slice respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
