e1bff61635aae3f903a0ec1f187dca9eae1420ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:29:42 +0000

    advice recognize trousers benefit piece

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
