1af3f6dcf7c08c36ed2a0eb1695655dac86b3534
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:01 +0000

    force round shout cope sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
