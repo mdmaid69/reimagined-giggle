e38a92f930c2dd4c640df1b18f49fcb898006ee5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:35:45 +0000

    definite over introduce smile brother

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
