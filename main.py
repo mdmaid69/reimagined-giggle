6e74afe9106285159849d9508f197992576a3b30
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:58 +0000

    himself squash sit go beyond

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
