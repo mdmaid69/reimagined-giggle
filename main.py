a4af32e08424d527bdf2e518eba82a1cb72df15d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:27 +0000

    fold afterwards lookaround bored plant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_str(array):
+        return str(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
