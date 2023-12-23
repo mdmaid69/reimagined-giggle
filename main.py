3491e6d4df204f86943ce0f4a935989ca10fb8de
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:38:30 +0000

    to habit move practise public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
