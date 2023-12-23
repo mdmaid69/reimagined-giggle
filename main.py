e12212747ea570d09d7efc59734f56575f8555fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:24 +0000

    pump 572enter away effect may

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 def cube_number(x):
         return x**3
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
