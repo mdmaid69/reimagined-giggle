c6d9b78be35c19f7dc07182fca46b37dc7080951
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:11:56 +0000

    pay delight that ordinary popular

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
