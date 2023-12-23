305af2c322ee1fd8bfaae8ac8eeadbf60cd0ae34
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:48:46 +0000

    case future part correct substance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
