595b67ab3141d769704eb021b3914a9d8147d4dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:19:17 +0000

    Tt phone confusing branch sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
