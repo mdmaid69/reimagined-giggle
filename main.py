4357bc2410a2e9977407bcfd69f7bc282f7c5db8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:10 +0000

    conduct appearance book careless bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import time
+print(time.time())
