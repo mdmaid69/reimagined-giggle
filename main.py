1e6a9be0332b330fa89dabc7c90d387604f8ad9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:19 +0000

    might autumn wide fail comfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def cube_number(x):
+        return x**3
