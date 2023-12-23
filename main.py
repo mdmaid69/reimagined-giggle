92df3bb6abd46036ffb6713c81338e926ec406b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:26 +0000

    strict get table she chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_index(array, item):
+        return array.index(item)
