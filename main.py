903dc35feed9baf3a8ca95d51d91f2c0dbbf194a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:04:49 +0000

    suit marry gather deserve temperature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
