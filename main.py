4b703260fe8f1799b2529ba69ba0856eca3f44f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:18:31 +0000

    material hide morning valuable by

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
