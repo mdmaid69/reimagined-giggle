b65ff3d3b00d9ccee1f12cd523d1e8019ea0f2b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:34 +0000

    practice confidence equal relationship demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
