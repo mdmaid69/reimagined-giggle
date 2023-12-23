1f782bbc111a9839d4d3fdb268e7bb3b68e76a39
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:17 +0000

    supply workout better smart like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
