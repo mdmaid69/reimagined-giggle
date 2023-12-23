64f4320862121cc516cb7bc483424afc5a225f43
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:36:48 +0000

    answer down doctor later print

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
