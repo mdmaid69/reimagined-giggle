273c566c093857789a025d4e5dcf93875a5585fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:32 +0000

    pocket extraordinary earth play organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_max(numbers):
+        return max(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
