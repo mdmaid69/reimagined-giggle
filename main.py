fc4be4b3974d65f0ca1f79807d717fd77424480d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:52 +0000

    majority table on movement steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_queue():
-        return collections.deque()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
