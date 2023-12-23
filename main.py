0175f35b767303a154dda01e14b4cf21a845e756
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:39 +0000

    less bother bet boat underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
