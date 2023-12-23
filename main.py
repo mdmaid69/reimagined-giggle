74a484e595f012d74cc445293772893f21c4eb8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:09 +0000

    typical adjective pump deep fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
