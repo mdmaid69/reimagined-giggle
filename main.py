4c07131838147905614e8aee143ba96fe5f1b402
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:16 +0000

    height pretty pollution purpose public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
