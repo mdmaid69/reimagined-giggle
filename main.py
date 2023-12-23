40d2d4c57d4d5969ecbfc27f0f4d78a743a2b927
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:36 +0000

    temper control settle behind report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
+import platform
+def get_python_version():
+        return platform.python_version()
