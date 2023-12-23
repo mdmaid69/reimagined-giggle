c9358ab9c8aadc3027358d34ae24194c313c7ddd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:54 +0000

    escape surprising confusing delicate trip

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def get_current_working_directory():
+        return os.getcwd()
