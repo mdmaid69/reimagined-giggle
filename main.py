dcae77eabb2e8b2ace05235e22131e1ce4c6ef92
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:12:13 +0000

    impatient past feed disgusting bringdown

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_list(array):
-        return list(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def remove_duplicates(lst):
+        return list(set(lst))
