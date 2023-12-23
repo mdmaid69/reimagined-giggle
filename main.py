d56b2d658e47e528db8d5dbb180dc774d9bae6a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:19 +0000

    invitation brother pleasant room convince

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
