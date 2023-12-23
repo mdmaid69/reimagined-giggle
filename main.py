86652ebdb07c23ec3475e6142f5eba257539a6de
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:11 +0000

    slowly worried make wing music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
