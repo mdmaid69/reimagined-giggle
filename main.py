f4fe1790039340b24c2b365632b63a2b72b39e55
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:54:39 +0000

    work marry challenge send level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
