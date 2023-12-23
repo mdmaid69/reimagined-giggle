d26617b46787a71efa70c175fda68cf5119899c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:11 +0000

    tidy communication but past familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
