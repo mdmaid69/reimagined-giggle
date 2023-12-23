20bc3fb315b34b93132bb76f2ee9e3b1261768ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:02 +0000

    consult use eager keepaway this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sys
+def exit_program():
+        sys.exit()
+import random
+print(random.randint(0, 100))
