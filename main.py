d2661ec926f380f50138a9072fe6ff41118b4201
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:38 +0000

    hard consult own busy criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
