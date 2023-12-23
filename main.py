478c91e9564f7ab6db0b3ad952ac14c9d8bc550c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:23:27 +0000

    hard wish spring mark however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
