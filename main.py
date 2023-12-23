918a4add00aa78cb0e11364a98ffd1ab16d97ca0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:11 +0000

    light question description wide dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def reverse_list(lst):
+        return lst[::-1]
