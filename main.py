593eafb2e545f590d41b77d7cc8fc26e30fb628f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:02 +0000

    creature touch holdon sudden truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
