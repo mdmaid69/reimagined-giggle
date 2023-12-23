81884083ef948cfd1d61ac06943126e4084fb789
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:31:35 +0000

    new hang rise pattern admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
