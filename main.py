77874515867f040bb8a54dea90f625ac5ac15a82
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:30:13 +0000

    sink play report pause Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
