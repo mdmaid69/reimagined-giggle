e31461651f456ba6b303518fca3a01643ef4da62
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:14 +0000

    tough advantage family direct anxious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
