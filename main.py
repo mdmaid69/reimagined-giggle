e918854bb36d4b531810861f1dad2dafe6e7513c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:41:15 +0000

    together far rain nerve zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
