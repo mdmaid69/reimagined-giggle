3c3909ca248696d6cfb011ea4b006564971d394c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:51 +0000

    generally theirs spread sweep awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
