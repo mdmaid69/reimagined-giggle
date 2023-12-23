f228286c0997a5ad6c709167c0ca1863d745028f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:07:51 +0000

    whether performance card tend consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
