adbab76e1bb5665b3acfa9e5ac1973c2366d5361
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:56 +0000

    do expensive free fail pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
