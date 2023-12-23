2ab829d38660ea605560275a487ab8b53b2ae7ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:40 +0000

    politics Gg eye software maximum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
