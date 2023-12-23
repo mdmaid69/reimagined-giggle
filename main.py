ad968f5da8de4f3fd47b911e9e8b95bcdb89a822
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:09:47 +0000

    original different priority public behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def reverse_list(lst):
+        return lst[::-1]
