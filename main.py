f1e5b4c06edb8e4ee310ed46c7772fd51cbcde1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:20:11 +0000

    night whole argue winter draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
