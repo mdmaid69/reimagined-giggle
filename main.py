4792396fa30085681410d8423007dd6292d31397
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:08 +0000

    runinto respect project believe nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
