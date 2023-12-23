d202526bc64f1c6547e5935345fc6addbc5fd2b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:55 +0000

    write pipe anyone apart string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_set(array):
-        return set(array)
+  def subtract_numbers(x, y):
+        return x - y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
