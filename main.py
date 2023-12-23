b41cdb1189cb9ae04767d8f28d75d13fcf506590
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:18:37 +0000

    available promise complete children effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
