473b146dcb724c08cbe0c36eaad34a8736b2d372
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:55 +0000

    preparation difficulty empty sorry club

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-def find_min(numbers):
-        return min(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
