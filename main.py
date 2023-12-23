65b85186873b20a5d8c5138331699a54b5c84295
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:14 +0000

    beautiful throughout slip that ride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_max(numbers):
+        return max(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
