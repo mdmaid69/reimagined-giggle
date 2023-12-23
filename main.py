7fe76d8f575d706d5dcc2817597690cc2d8baff7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:28 +0000

    getout apply fire machine arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
