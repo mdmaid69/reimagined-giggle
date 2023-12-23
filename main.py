7e036ad3c6d3556b7d47c583bd10d2f87b34a80b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:59:43 +0000

    theirs total general escape ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
