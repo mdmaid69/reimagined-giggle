a0cc03c448357158f37cf7bbadb1c35251145416
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:38 +0000

    reaction wake behind discovery squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
