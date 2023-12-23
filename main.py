d2c2b183b197cb80ff81cf2f79fe72347f0aa34e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:33 +0000

    wet circle criticism dance inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
