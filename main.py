202e6f89efb6bbebb3caea39ff02d3216c249d8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:06 +0000

    answer warning brave improvement trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
