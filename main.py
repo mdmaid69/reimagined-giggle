56297d12214bebf11384fc5ddbe7aeb286b9b0cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:45:58 +0000

    best open bad pause accidental

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
