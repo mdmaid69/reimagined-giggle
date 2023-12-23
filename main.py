7598d10dc441f2bdf69b3bca339c81a1bb4fe46e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:43 +0000

    dick expression comeup hour minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
