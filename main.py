7647a2ce9395a97877b1e3c6cee1b824cd7a79ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:48 +0000

    contrast shake tight pass whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
