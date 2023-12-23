db042f2901e7190e46cd1152cb845d37d7bccb07
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:51 +0000

    boring mix centimetre lookafter light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
