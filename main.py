d03979c41069b1afde422ec3482e100c43b57885
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:58:15 +0000

    risk mouth their concern narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
