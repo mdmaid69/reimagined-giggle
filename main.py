67511128bc206a3dae5bab825c85188105b199c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:50:30 +0000

    worse prevent moral to this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
