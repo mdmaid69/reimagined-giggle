4614ea1a71589642f01d2b66b63ceb7a623195c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:45:48 +0000

    which middle night performance keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
