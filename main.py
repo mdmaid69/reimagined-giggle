08bd608b3054fcfb4c00a058dd3d3f37f2b5b1cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:24 +0000

    fit cut risk nowhere authority

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
