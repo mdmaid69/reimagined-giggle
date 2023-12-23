74eedb385675f2f0df7f21cc50e612a96ea58b97
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:56 +0000

    branch basically manage wash produce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
