13edc2c7ea44b52384d7596e770f13a7d845eeea
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:17 +0000

    record side he flight treat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item(array, i):
+        return array[i]
