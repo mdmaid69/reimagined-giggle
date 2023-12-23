a6ee24912558e0559c791b26a7f726ca411ee1e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:44 +0000

    past beside thinkthrough sticky violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
