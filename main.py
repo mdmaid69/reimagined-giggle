a31fd68099c141a4057adfa612f3718a74b44801
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:17 +0000

    shout whatever off use connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   def add_numbers(x, y):
         return x + y
