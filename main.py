d1b3093f4219ec4dd965aeb77d3656e8a6ac63f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:43 +0000

    secret birthday rarely put my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
