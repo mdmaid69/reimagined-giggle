cef5bb140b4da55334ab3a826735b9e37b23f753
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:39:20 +0000

    underneath where sister doubt minimum

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
