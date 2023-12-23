7a93d9d535195807b9d4eec3a49778de74651fe8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:14:03 +0000

    temperature glass protest clear bush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
