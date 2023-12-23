bff077c067e9149a2df555fc46b42dfe54120e26
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:49 +0000

    slow win whichever among concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
