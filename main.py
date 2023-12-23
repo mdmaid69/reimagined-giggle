75bf4799025fe80268bed2b5010a42ab1bb66c52
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:08:24 +0000

    action sale wave afraid cope

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
