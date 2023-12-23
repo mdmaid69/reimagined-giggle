ec30061489f937b7445a66796515b8b7c540fc7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:51:15 +0000

    where bill used godown about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
