5619a0350943e36e4d798fd149b8a016378ad72a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:30 +0000

    weather regularly thumb disagree hour

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def is_palindrome(s):
+        return s == s[::-1]
