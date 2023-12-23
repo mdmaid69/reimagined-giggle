1c4a11ad0ecccdee269e0020410fce0781508eef
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:07:54 +0000

    desperate tip present hard dinner

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+print(sum(range(10)))
