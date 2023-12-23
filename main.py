27cd5f2b35f84f55859b6d0cf0856b03e7226415
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:22:40 +0000

    uncertain win 570enjoyable clever height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
