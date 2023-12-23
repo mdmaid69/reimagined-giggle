e6a31186f580078c502e27c8f5105edfa772ac07
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:55 +0000

    except class giveaway number match

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
