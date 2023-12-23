9d5e7bdb5558ada163c78137eb7b17d6d29c93d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:44:22 +0000

    scare smoke cope cost middle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
