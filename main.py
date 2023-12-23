c3ff5e8b2a84fa2e9195d20f3c4ce543736a103f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:19:28 +0000

    pressure range state married raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
