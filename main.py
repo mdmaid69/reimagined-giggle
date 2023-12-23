08d7ef2b8e1edac11ccc09408819e856931c11c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:50 +0000

    breakdown geton Qq normal lock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print(sum(range(10)))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
