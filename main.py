4bfaaf27bdcfbe7363d50ee7abf31fd9e33db9e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:18 +0000

    wander project summer commit turnover

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print(time.time())
+import math
+print(math.pi)
