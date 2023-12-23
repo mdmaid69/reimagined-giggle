77c620b4a8485c2ac73740439296a387870298f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:04 +0000

    past pair round whether lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
