5f3c1f82abc9d120c3d49a6218aaed38555993c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:54 +0000

    real sore leave since repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_buffer_info(array):
+        return array.buffer_info()
