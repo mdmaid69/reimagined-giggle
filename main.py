d504c6e626e37d25bcee768ff187b17f836c50c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:35 +0000

    later satisfied weak test cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
