6ff12699009077664d0a08043723060609f90b8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:57 +0000

    fat impossible belong passenger argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
