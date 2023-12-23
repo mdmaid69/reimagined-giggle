6357b7f05b3c37103158f4ad91cb7758f0f8016d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:22:51 +0000

    tough shock day block throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
