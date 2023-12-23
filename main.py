40e3d2e75a3c1a7eadf6071a52804f132aca11fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:48 +0000

    system strike sound thick minor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
