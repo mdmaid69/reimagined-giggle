dd46519b91ab09525cc9d9aaf1bc220d12be3751
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:59:43 +0000

    government rank pause greeting hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
