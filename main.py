2f144be70c160d64d98d9769970fc0a3cafcb64b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:46 +0000

    equal convenient run demand deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
