f00253ce96d9e5e323ef00e13aea3c86ac3c29c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:14 +0000

    past else gather culture guess

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_str(array):
+        return str(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
