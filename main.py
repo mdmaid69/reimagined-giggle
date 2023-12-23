0cd87935afe790c3cc6fee5bf4bfee973e2915aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:43 +0000

    makeup bet stop final steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_length(array):
+        return len(array)
