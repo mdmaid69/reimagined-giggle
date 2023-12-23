2115b807055b2117ba4798a491541c04548ed3af
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:45 +0000

    type benefit left ago powerful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
