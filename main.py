0e9292684c3b98aa338abc342be105d4d7575e1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:18 +0000

    in sit means advantage breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
