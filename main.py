9df3284fd65b60c5087ae8ba112d6cd73a60b963
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:51 +0000

    just talk balance actual rest

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
