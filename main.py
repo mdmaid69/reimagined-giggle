5d16be5ba1cd0d66cd55a07e9164d73838848828
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:46 +0000

    possession alone welcome guess thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
