47bd3251ed84b4611f4d90f4b19b7ddd0a4dc758
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:12 +0000

    butter fact safety maintain cry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
