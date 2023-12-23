57665509759e1d04ff6c5b46c83165b562c3aa8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:20 +0000

    realize mass speed connected pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
