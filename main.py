86e0930fe7bf701d556901f504c314f07904ee8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:49 +0000

    moment now workon wander hurt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
