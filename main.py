c4962efaf78932deb32b8fef5d83305c7c600efd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:16 +0000

    direct period convinced Nn catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
