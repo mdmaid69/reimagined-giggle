db97ea3a6907f55e184927ffeb914ec10c598e8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:37 +0000

    ride across sort water tell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
