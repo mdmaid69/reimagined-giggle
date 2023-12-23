2d2a24bb45f70bc5db712f63b7ef6347bd3d9bf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:14 +0000

    half new characteristic fear book

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
