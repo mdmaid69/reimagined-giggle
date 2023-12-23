c1d4ff4a14f8fc6a48070cb91af9aa25364cbbde
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:54 +0000

    accidental consist guide smell school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_pressure(force, area):
+        return force / area
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
