2acd93f5664019d9ae16cc4d9f5439c70e07d9da
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:48:38 +0000

    trial set secret by task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def find_min(numbers):
+        return min(numbers)
