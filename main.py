5247c6699699619d1d526ca1dc9c8c3cd27b1026
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:39 +0000

    once spill count wealth instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
