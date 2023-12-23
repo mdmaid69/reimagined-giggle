450c001d80144914afa57c9dd7bcec6a459dc5b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:02 +0000

    refuse container part opportunity message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
