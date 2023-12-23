e38c70b23d5c72888b583af0de92bff80a699506
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:25:54 +0000

    cover afford mind Bb fold

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def remove_directory(path):
+        os.rmdir(path)
