f499488a0a7806eae9bade5fb17bf7ce290c5a76
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:34:58 +0000

    form drive variety set wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
