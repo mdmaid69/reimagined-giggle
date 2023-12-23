a9466118be46f431e5fd86d043443aa6f84adc42
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:35 +0000

    past butter organize threaten name

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
