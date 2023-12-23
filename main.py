6b0190fa700486aed90f84eb131e173bbcaa7dbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:11 +0000

    throw thirsty afford closed influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
