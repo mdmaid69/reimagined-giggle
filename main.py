6fcd15a99fccd727be31fbcbdd2eb53171e30c6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:00:24 +0000

    exit example side dish spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
