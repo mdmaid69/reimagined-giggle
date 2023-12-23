9c6412b7bd59a59e2854e72e9be591deefafe0bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:32:00 +0000

    test mystery who for guide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def clear_array(array):
+        array *= 0
+import time
+def get_current_time():
+        return time.ctime()
