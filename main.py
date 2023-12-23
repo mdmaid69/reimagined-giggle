20ce12c1af7b76961405f82491322d47116d94ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:58 +0000

    respect praise exhibition too through

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
