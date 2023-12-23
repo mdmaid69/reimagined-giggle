c4cddfa85b3af1350b7131af3b57ac795e01478e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:51 +0000

    sink lunch length bringforward fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
