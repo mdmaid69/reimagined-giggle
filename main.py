75dfb01267e02441d0f00cf97ca2215d0b7b57bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:12 +0000

    skin juice lookaround cheap hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
