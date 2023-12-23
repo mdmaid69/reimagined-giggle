0460590cb3ad8d7b576f15eca9fdbe2b19d4eff7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:01:27 +0000

    yes worst speech poor leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
