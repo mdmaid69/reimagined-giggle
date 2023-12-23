5a6839bf95c8c91f73d4dcfbe3d4e794e49129bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:09 +0000

    communicate aim divide politics comeover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
