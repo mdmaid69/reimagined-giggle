a10f8f48a7b7a42530847f25333de8463ae0fd1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:19 +0000

    order sharp shopping slow plate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
