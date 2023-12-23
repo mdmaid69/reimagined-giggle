697067fb1ec20b0c6abca94b31a11be67d42916d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:13 +0000

    relationship family leg detail achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
