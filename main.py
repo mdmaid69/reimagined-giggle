89687c3da080cc25acf5a5532b80e77c8cf3e629
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:25 +0000

    schedule than wide waste steal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
