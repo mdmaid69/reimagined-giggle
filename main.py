d75b65970deaff10e886f952e66720062a559a49
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:50:38 +0000

    while upset soak workon several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
