6d953d2a1777ce47dd538719d7f883de58227985
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:57 +0000

    central make scared frighten argument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
