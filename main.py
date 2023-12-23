da10f1984a40ace3f6b9815d28c97acb7004e059
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:44 +0000

    computer interruption true miss turnup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
+def convert_to_octal(n):
+        return oct(n)
