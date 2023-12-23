24d76e87b2ee512e7ddea3c23c01d9e039334fe3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:04 +0000

    expression ever allow post stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def multiply_numbers(x, y):
-        return x * y
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
