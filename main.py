b736faf6a471b4a886ed6fbf6a1a87e9cc0e422e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:12:44 +0000

    better modern rare work exit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
