7027125ca4e9e28306e39f0ccde745956fbbb061
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:50 +0000

    to face tap medium live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import sys
-print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
