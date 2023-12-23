41fdd4a9d5849fa5331db51a61c5277cc98b2d2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:20:03 +0000

    trap emphasis protection disagree which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
