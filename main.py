f5fc77a401093a7036db9fa552bf61c8549da718
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:29 +0000

    inside medium thin material any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
