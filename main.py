71309d95f67fe9e383c2e1148cdfeb657d7f3b75
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:25 +0000

    challenge fine round demonstrate ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
