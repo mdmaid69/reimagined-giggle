bf958f111ff34222c209f0b3acfee534d6170831
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:31 +0000

    if pick lean keepup near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
