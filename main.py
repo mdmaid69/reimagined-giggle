877dcdb33e0421ba5b070431077de20fa3c97781
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:24 +0000

    ignore draw emphasize surprise otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
