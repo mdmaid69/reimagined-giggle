a6820cdd2c7b57cb9cf98bb58d8e7cf90fefd38a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:15 +0000

    dress taste explain fond dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
