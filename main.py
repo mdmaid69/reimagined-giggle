b6478ac31df21d94e0f232fcb2c00eba5c972526
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:18 +0000

    contribute his will or short

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
