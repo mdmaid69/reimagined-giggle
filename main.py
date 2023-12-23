6674812c1fc431a913eb9c06c4dbeaec1dae75db
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:01 +0000

    indeed keepup game realize physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
