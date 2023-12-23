688561ce6b14dce4062131c77cb9f8826fc3bdf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:19 +0000

    stranger stranger up quarter community

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
