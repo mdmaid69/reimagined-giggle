dc6ccb316cc918ea00aa211073b6d1cd50a5f9d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:52 +0000

    strict set embarrassed vote plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
