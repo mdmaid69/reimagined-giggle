36c577bb5b62804b62e717dca5cd5d6d59cf4401
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:15 +0000

    complain wake believe maintain fine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+name = "Python"
+print("Hello,", name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
