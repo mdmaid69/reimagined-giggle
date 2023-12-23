8fab6f8d0fa511a425337e7448c1ce04f1bd96f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:36 +0000

    hurt almost badly border debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
