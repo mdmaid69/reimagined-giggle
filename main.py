8d54256d392400941d2d053f7038fcf5a38474fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:17 +0000

    advantage fortunate nearly very address

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def reverse_list(lst):
+        return lst[::-1]
