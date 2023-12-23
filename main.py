101773b01f687f9874fab9168cacef3adc21c8a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:51 +0000

    off blame imagine habit aware

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print([x**2 for x in range(10)])
