c1e07e725be7a2eccb51e5db832a7b1f88c39b1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:10:30 +0000

    basic wind level money feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
