f3586694ccd5e633cf8849e274e6e11b2d8b5bef
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:51:30 +0000

    deaf stiff happen tradition tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
