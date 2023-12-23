4f593b1fd56389a3bf2bd30d9c269f348b275487
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:19:50 +0000

    both equipment dull getout spare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
