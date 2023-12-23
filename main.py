85d728b049d99794be5c422aa9e6cac98f530979
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:07 +0000

    start ago getoff breakout wet

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
