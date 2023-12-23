b5ec3e3d1a5e4ec536197caab4640fb0c621018c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:44 +0000

    exact maintain never direct present

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
