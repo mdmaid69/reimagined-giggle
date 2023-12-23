f5707f1045605604b1c6e18ef27fcc0e940871b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:20 +0000

    range confident kill scream nor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_as_bytearray(array):
+        return bytearray(array)
