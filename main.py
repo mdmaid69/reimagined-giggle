357621497845fadd11a76a16db81f3c504406002
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:40 +0000

    bound urgent to punish flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
