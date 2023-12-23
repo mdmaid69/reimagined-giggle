159de10e0a8e6c0e1aa958da394ee5c522abf233
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:08 +0000

    hold target on shape properly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
