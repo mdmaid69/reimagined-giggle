48c38d1ee692baa50c9df95ac4ec210f779190b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:45 +0000

    difficult name hear pocket cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
