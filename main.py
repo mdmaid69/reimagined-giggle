8516673016dbe9208eeb64283c05b25f880a8130
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:50 +0000

    ready murder habit worry soak

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Union:", set(list1) | set(list2))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
