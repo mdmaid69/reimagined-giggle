f803d33bf414c687eb93ecc28120e6a2b13f139c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:44 +0000

    information worst stop getoff country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
