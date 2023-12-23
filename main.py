107d6d4a70b665f6f510fc4263b552840f251654
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:35:16 +0000

    seriously car autumn improvement friend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_interest(principal, rate, time):
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
