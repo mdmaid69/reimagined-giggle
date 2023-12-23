97ddd493d2ad3e8619e09e9af0cd87e26b90148f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:20 +0000

    flat continue cross her comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_list_from_array(array):
+        return array.tolist()
