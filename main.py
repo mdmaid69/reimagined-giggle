4bb31b126d179fded09526dc4fb63c1df5885fdd
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:30:28 +0000

    Pp somewhere drop supply distance

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
