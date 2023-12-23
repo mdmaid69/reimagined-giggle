760bf60b6eccc2216b0afe8091a3aacae660ad7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:45 +0000

    grand prove argue back judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
