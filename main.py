2e55c6755ebae450431a32c3bcbbbe9d7bf87854
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:22 +0000

    whistle fond spring tour performance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_str(array):
+        return str(array)
