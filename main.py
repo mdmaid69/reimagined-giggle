a464f01d0031d0ce46a131447ac78e97b744982f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:49 +0000

    then confuse name sink rob

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
