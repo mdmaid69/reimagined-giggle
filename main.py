2e39d465394213ee9c24d919dd3c06de23e4a8ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:25 +0000

    develop stamp geton future now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
