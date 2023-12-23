91deaf12c9b27e13700f02d31d4aff78ab65cbbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:48:17 +0000

    trouble hit thumb cope computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def square_number(x):
+        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
