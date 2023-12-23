7c6b346765be0a23211f08de4b049c170da4c571
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:35 +0000

    quickly occur beyond along pretty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
