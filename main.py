d89d8464ba865b17b0beca28c1e951c2506d04a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:39:46 +0000

    hospital force snow mail low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def clear_array(array):
+        array *= 0
+def square_number(x):
+        return x**2
