861edbb2f1af6ac3050beaae9d1ee2e4689ac5ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:52:39 +0000

    instrument nice cope admire hour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
