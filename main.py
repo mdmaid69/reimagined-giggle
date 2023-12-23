b110f7e7766596390b3c7fe5c68ab10a605a9844
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:32 +0000

    keepoff expensive advise side union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
