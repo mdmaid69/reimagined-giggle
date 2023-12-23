f3251ec91b6f44cd83fd01a264ba6c3f7015fdb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:27 +0000

    content pinch lookback(on) shut whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
