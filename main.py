f3ca9b5fc5463ada05c252f3ec228e765fe6520d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:26 +0000

    respect usually accordingto means return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+for i in range(5):
+        print(i)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
