d0bf5c3330a514efd7116701a4c66b429a18bb11
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:34 +0000

    anybody turnout long calloff drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
