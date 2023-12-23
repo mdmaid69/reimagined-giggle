3f11e29cbd91cd2dc51cd68c87fb9c7a1925db82
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:05 +0000

    bath common fail thanks sea

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
