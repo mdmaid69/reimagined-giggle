0e89d6a1f1b36bea22ff6999b39035b3aa15da33
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:36:13 +0000

    steam start touch runaway his

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def count_elements(lst):
+        return len(lst)
