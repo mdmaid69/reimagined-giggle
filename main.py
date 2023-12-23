d07be07dec0187d5cfdc416c8c29375125bb5c15
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:51 +0000

    obtain properly vanish afternoon your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
