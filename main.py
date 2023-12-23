528c1139de2acbba32747c5ebfeade12a7c985c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:32 +0000

    argument protection approve solve gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
