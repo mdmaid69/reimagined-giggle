6e1a46ac04a456730a53019dbbd8f5c616c69bf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:33 +0000

    treatment mix undo middle roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_as_float(array):
+        return float(array[0])
