36711c49d6517f9ea425ccd0f148b4cf31c2f5dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:29:38 +0000

    week teach no dance fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
