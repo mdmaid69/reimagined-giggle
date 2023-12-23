ee03bd82a2c43c97cfa6472179f1fda952e22919
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:56 +0000

    dry action false listen fall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
