16f67055bbf3a65a12f20ac040dac103e0e42a08
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:52:58 +0000

    cinema male community right finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_str(array):
+        return str(array)
