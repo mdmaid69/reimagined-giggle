114ecec3e7c9f983dbceb5347b60d5a31ba0ff69
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:53 +0000

    borrow principle track tune show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
