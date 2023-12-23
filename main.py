cc05c2a17d88bfb6747a293154945cade015e443
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:25 +0000

    buy opinion certainly noisy belong

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_factorial(n):
+        return math.factorial(n)
