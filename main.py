a8414c00b05e46c0a613c1ba5a2696d211df26d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:04 +0000

    space argue lead hollow off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
