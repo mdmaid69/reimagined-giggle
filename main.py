56b32f86db48a300e63792375dec57ea4693f5f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:26 +0000

    total authority bush price bank

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import sys
-def print_python_version():
-        print(sys.version)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
