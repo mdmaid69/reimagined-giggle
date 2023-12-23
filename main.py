67bf902e8caa549fd9cb5dd90973dab4683824b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:59:50 +0000

    trust respect concerned correct gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+print(re.match("h.*o", "hello world"))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
