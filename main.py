11e5d809029b6b8bd23c9d827eec029993b4ccee
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:02 +0000

    fasten by during ilovedicks inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-def greet(name):
-        print(f"Hello, {name}!")
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
