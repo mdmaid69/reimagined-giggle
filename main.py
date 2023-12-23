f442e50e6fbc9ebb6158ea9b2e20a1c4a088f38c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:52 +0000

    embarrassing picture turnover height destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_tuple(array):
+        return tuple(array)
