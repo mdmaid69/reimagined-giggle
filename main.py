fd09f350591161cdbd378136ba5d9c270751de90
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:19 +0000

    danger possibly cap after teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_string(array):
+        return array.tostring()
