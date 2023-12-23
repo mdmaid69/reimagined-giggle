74d921611f58d7880a502482a1e3accdc46afe8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:40 +0000

    strike unfortunate description beneath sea

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
