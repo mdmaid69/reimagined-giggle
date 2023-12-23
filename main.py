98a1f70346b58fbd61442c8adac29f0fe2f0ee4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:45 +0000

    group whisper supply comeout sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
