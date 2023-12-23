387fd30772a3444a09ee08c5df979a38fa3076eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:01 +0000

    support shop arm matter class

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
