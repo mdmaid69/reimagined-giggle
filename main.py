8e6da6c842c9a59bb66dcc5491915660ff291097
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:09 +0000

    hurry subject Hh wise block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
