f792225b21c6f0ee6f5488483c06b985f3b181c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:27:11 +0000

    deserve justice hit command sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
