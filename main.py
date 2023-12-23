21a34dd2ce69dc96d9407c9fc4f6b8eb0a2edd15
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:52:28 +0000

    large goback(to) knowledge brush slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
