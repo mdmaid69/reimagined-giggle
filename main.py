606c6cef1a0b33a628e218fd49fb74b691a6e8c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:13 +0000

    level right definite after appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def reverse_array(array):
+        array.reverse()
