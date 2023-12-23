e5837ab5c461c3cc44d7410e62096cff75105c6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:21 +0000

    apply however trap backwards suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
