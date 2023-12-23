9bb34de728b170ffe65b29a7de5bb060a5974c92
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:42 +0000

    snow divide trick uncertain more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
