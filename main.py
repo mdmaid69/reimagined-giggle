5716c86233b11b6f20fa6112a004e4aee1bf4eba
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:37 +0000

    direct comment send Hh next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
