7d73eb011eb79bca878ba8c486893869349ff716
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:56 +0000

    callat sense worth slice anything

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
