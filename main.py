aa3f8e0034d0b88c84a077881efadcd229981483
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:15 +0000

    catch now difficulty tread choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sys
+def exit_program():
+        sys.exit()
