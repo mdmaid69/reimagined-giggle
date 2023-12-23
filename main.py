0e60a3f34d3df34556092fc40acd8b153a2a6815
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:36:47 +0000

    ability even rise share greeting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+def get_time_since_epoch():
+        return time.time()
