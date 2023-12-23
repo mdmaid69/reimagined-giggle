d4890d4d246b169087f707b52f3ea053a34bbbf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:27 +0000

    exit suggestion treatment rush wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sys
-print(sys.version)
+  def sort_list(lst):
+        return sorted(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
