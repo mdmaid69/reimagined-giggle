965588367daf66dd8b5a74000d11883213d75526
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:49 +0000

    Pp wash still sale ordinary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def square_number(x):
+        return x**2
