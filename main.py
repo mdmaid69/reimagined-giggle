432a02ebfb5a2ea52a6874e7a47b778041fcbe4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:55:47 +0000

    Ee gift certainly reply several

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
