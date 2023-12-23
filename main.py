2c76ee7eccf85d4fbae915fb5740c2cc16147c1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:55 +0000

    position block shine further Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
