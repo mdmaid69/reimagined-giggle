56444a86fcc336b14d2ee1ed62e509156b73e4d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:59:14 +0000

    gooff dinner university unlucky hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
