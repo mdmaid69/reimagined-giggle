56f1fa7bd17e69e164a46bd1ea10e823e44917c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:38:55 +0000

    wish beat somebody neither after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
