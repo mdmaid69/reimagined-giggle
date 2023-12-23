7507f7378d62efc7f30e0830f8ba42cb1ef3b826
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:19:27 +0000

    excuse together insist extremely dead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def multiply_numbers(x, y):
+        return x * y
