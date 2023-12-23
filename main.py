2506f914f56dab5f418a592e9b14b374d77adfcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:09 +0000

    mark rent direction getaway present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
