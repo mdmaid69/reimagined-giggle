1557f1a8b4269fc375aa144b6fe4ea354fa8fa24
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:06 +0000

    young print mystery legal crash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
