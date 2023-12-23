e9d16ebb73194a2727ef21efff25a550d074b817
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:12:21 +0000

    boring runinto turndown successful realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
