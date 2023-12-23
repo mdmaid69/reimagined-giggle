7b87f9fbe459d70949eaa60e30fb50f5da49708f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:16:40 +0000

    different Ss remain attractive back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
