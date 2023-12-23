236cb4e9018eef7c745fbef34f5cb879156d8b16
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:51 +0000

    suddenly Tt square shake hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
