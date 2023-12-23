cdf313b8d3cc48eec6393e5d5e037c44ea0085be
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:03:20 +0000

    uniform absolutely command not afraid

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
