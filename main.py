baf76cdb704c05d3cfe25a1fad72bf2f2c36186c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:35 +0000

    material find surprising forward positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
