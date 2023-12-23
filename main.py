ddb177ed473a078e52fea77f65423f33e6fa0f14
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:55 +0000

    group opposite surprising usual few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import numpy as np
   def calculate_correlation(arr1, arr2):
         return np.corrcoef(arr1, arr2)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
