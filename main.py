b39266fc3f6ba0048a6550356dcc898adbebe8c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:04 +0000

    splash violence doubt benefit definite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
