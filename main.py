37579b439d1a24e95db6dfa8139a0f0552dc3612
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:43 +0000

    risk file failure available keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
