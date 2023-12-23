b7cb0cb3ed3aeebcedc14bc5ed2fb79cf5374015
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:06:00 +0000

    take opposite contact geton shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
