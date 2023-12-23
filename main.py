9c3e050f1aff5d1c029be425b1975a022978f7b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:34 +0000

    compete table proud persuade shape

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
