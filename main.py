23766c6e89a400b4e9da0ec6365f114e21c649bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:25 +0000

    ask eat colour major strength

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_palindrome(s):
+        return s == s[::-1]
+def add_numbers(x, y):
+        return x + y
