e9605063fad6422418e3f67ebcf2d110bdfcd5e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:57 +0000

    holiday central tongue report dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
