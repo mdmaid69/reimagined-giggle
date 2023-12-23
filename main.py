2fbadaa786f800e0cfc1d519c8a4f88f5795c642
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:19 +0000

    slide healthy bringback joke open

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_today_date():
+        return datetime.date.today()
