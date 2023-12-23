0c5458f8983ac740e333033e4a2f7d7233d4f2d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:26 +0000

    deliberate when operate unkind reaction

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
