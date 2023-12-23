6c2456346829521ff6fb1db76d6adca4ce78a0e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:35 +0000

    same practise phone go pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
