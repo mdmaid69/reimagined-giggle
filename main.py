6ca08580a522586ea1f9f95871301cc607f03675
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:39 +0000

    finally next start react but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
