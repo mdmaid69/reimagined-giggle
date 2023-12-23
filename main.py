4db8f1cebf5605828e0853cd0a086021b4114e3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:16:38 +0000

    this lesson question injury so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
