8b69f93af9592cdf7a695956a97e0ee34850704a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:02:10 +0000

    continue opposite instead school pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
