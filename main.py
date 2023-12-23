629aee4e76edec838d15b19d8eee2a43665370fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:17 +0000

    Ff exam view connect kind

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
