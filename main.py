db40bdecc4db5cf85ca7c6c98ab7567e13eb65d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:51 +0000

    my drawing Ee comedown Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
