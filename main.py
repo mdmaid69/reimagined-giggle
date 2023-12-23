b7259faad64001a9cc38704aba8d9fa9723b60d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:52:56 +0000

    pen plenty odd store run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
