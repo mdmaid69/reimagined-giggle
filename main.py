15618f18e976b17a805593acb85547e9ba878b68
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:44:17 +0000

    admit swing gas inform hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
