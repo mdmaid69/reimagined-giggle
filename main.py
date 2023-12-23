baad9f4cd4228bd2a78bd1c62b2471549f853005
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:55:54 +0000

    power basic immediately morning middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
