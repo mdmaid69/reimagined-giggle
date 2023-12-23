b2eceb791f6dd8e9868526fb287987176a8967d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:03 +0000

    prepare deep release enthusiastic change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
