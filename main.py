b3b51421926f976a7fa85e359d7ef6c9b28181cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:12:25 +0000

    basic total compete prevent empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_length(array):
+        return len(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
