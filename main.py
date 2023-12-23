dff65f7fada321781dcf5954e9d7842b8edd7b8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:41:39 +0000

    lookafter where exam eat war

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
