3c73e3136587e64d9fa7649b2c3bc795176135f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:06 +0000

    pair journey crisis trap compare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
