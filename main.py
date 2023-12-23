a7558b4c7e50630f45ca6536a1eff634bae181ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:36 +0000

    board raw he motion flow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
