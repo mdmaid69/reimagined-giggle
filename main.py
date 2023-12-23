aaff357cff50647ba6382107b57d4f42d1044d5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:08 +0000

    perform print pride interesting better

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
