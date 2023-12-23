a16e5b4765f43bbf409ac31876c9a81417be415b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:51 +0000

    cheek even wind brush trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
