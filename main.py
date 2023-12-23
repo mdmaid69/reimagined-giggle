ca837938210e5c77d098d2b8cc36e49471cbf81d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:09:00 +0000

    bye tonight condition switch bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
