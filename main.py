6df7ced2727bcd6261b5793e0910b70100fa6f85
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:14:46 +0000

    better everybody willing second uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+print(time.time())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
