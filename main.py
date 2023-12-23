418ecce3b9aaddb96deb39730b88b60c4258f3ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:20 +0000

    lookaround visit in getout idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
