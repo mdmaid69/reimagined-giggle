d2e2e36f98db983f7393b8082c5fd794df29f45d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:33 +0000

    cinema tough disappear before fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_length(array):
+        return len(array)
