bef83e1da45401fc5038dc020d8b3794b65fa99f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:49:19 +0000

    supply other operate Ww attach

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
