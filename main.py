b0f886b6cfb9847100713f56e20f4fe498d047c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:05 +0000

    soon join mainly black cry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def split_path(path):
+        return os.path.split(path)
