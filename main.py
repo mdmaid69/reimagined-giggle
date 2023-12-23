6e4ea7c7831eeba41252cd0f23d6f965add0d150
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:38:37 +0000

    funny never approve nose comfort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
