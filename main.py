87b51ba20833714eb96e0979939f09ceba9d8aaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:33 +0000

    smell Mm gate female curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
