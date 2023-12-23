bfd8178560f6a31ffb112d311c7a2891a25a96bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:41:38 +0000

    advantage wash Oo practical contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
