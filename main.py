b3f699b2163ce332bbc5e729146d74232563c77a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:53 +0000

    interested decide stranger satisfied little

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
