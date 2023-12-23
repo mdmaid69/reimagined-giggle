44b4e55f4f81001f1b4eaae5442c4283a4d28eac
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:50:55 +0000

    sharp keepfrom stranger mistake share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
