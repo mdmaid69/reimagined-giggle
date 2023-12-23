d0b079c16b65e01de432f5230b1bea5f3735d347
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:53 +0000

    hot Tt dark or same

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def subtract_numbers(x, y):
-        return x - y
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
