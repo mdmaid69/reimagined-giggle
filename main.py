e16991d6f8f63f812e75f7882b014caa1d802749
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:02:16 +0000

    agreement intend support increase takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def convert_to_octal(n):
+        return oct(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
