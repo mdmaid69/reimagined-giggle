11acc0c32c50af56a95648df697a36af7d0cf405
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:51 +0000

    ever settle platform show once

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
