35db6f2f956d9e9cbd67258220bc70c2dd578b7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:39:05 +0000

    journey guilty bang show our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
