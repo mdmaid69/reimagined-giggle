bf11cd0e0356e0a049646d20fdf197a2f841bfd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:54:31 +0000

    responsibility violence attention offer apparently

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
