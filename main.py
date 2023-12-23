e9aadce8dde44f3210999852f509c8f7499a647a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:26:35 +0000

    nonsense experiment amount kick frequent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
