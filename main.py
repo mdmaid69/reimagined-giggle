804a98edea8011ee46791ba6a7923127796cd4d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:28 +0000

    after remark up smart certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_set(array):
-        return set(array)
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
