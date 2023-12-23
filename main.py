ffacf198a2cb0d7098ffe4a7be5c6bd3d7145b5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:28:49 +0000

    building embarrassed fire 565end Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
