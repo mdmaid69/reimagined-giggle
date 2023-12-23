572ba5444c7e5423f182fb10ceccc542852364a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:20:53 +0000

    everyone much middle whistle mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
