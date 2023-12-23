454916ed59d28801a1aed78fa90098c94eed4eb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:45 +0000

    closed callat deliver sting weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
