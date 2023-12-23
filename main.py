0e5cdaf0b8516c5e29ea23707e1f2d2257ecb4ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:01:15 +0000

    empty keepup performance trust themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
