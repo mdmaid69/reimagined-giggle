58e32ad894df2ebfb835b2bdf1dbfd1f01881827
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:01 +0000

    police find false must deliver

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
