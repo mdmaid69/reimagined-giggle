223899ee590f9670500f13b9f2708ea79c53c42f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:07 +0000

    expression himself neither cum dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
