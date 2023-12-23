084b2d2465bb38d4e6312f3c74c977657488edfd
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:49:48 +0000

    walk appear where account consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def square_number(x):
+        return x**2
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
