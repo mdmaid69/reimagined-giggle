4bb611690dd7ebbdfa578c533322354b89f46ddd
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:45:05 +0000

    stare low waste turn principle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_area(radius):
+        return 3.14 * radius * radius
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
