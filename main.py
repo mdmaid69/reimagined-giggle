dcfdfbf0427b859e2a6586f8a2d907d8ef4926b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:31 +0000

    enthusiastic quickly discussion interest invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
