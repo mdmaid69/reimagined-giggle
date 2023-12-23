369ae16f4585d20a93c29cfdbb58c5c91e6e9679
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:16 +0000

    shock 570enjoyable large breakinto spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
