9846dce9419e81140c0056119fe912d8fd1ea04f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:35:12 +0000

    look check order sure workon

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+text = "Hello, world!"
+print("Words:", len(text.split()))
