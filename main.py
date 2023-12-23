3b0a3c2fb03f293e0b681b19f163c1d3a4c6cf6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:00 +0000

    basis cry steady approve everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
