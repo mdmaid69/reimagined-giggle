e55d5e231b2e67f617718a41114e612e16ffeb5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:47:53 +0000

    both kill pay suppose down

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
