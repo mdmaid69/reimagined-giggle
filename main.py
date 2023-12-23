44202e69f221d16ff6e8e119d240951e30e69a58
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:38 +0000

    pattern exchange finish past defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
