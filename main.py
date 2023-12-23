dabe8a03d5a583258e9cff121e3bc5368770e36d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:31 +0000

    danger attract dress waste art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
