23dda34bf161eb5a0d33ce2cbb1d59630e6a5e40
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:33 +0000

    wealth protest quality frightened drawing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
