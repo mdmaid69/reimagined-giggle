eb3411ad372cd6d4f19632746b7021f94546f586
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:27 +0000

    behind platform say produce protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_typecode(array):
+        return array.typecode
