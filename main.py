5f6da3440cdad997c7a21de38ddeccbc8151ad80
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:13:50 +0000

    pour fetch problem swallow bury

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
