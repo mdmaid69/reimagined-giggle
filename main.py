386c2952668f8e2df387260f74cbc387689b2575
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:02 +0000

    past see nice flavour turnout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
