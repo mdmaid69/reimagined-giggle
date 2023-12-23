db6db5b64f8cbbb62ee0d13278164cfc619ec731
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:50:26 +0000

    smile turninto by left skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
