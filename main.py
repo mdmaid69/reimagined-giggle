7b84bc1e53c34705d2fd5fcd31d77cb0dd0d4159
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:51:32 +0000

    rest before fetch brain development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
