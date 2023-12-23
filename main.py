b272d969dafaa0398e05748b2a43faf81b5b7030
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:40 +0000

    kind well hole anything want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
