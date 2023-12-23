4245964cbd98b57a3b80604b442b6f35d0e6d896
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:42 +0000

    theirs how wind quality relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
