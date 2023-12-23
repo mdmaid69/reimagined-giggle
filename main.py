00d30c133fd601d7b787cefde6acbe28b62533c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:21 +0000

    exchange foot deliberate afternoon almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
