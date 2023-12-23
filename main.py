4e5c6b7a22c16f2c8c6bf5c63546ce96c720ee3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:09:07 +0000

    help wherever hour tomorrow yet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_length(array):
+        return len(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
