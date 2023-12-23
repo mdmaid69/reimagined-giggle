ec847cc4f0fd128f4d68d777bf2b5ffe47096999
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:06:16 +0000

    form class comment getout address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
