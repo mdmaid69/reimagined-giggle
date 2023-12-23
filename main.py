4b31762f58729fb80653c1a625e817d98527120c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:41 +0000

    big regret find switch familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def square_number(x):
+        return x**2
