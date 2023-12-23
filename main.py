d7ac7f92282df149350ddf358c4f6fd67b463a61
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:40:35 +0000

    backwards matter talkinto mysterious flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_string_from_array(array):
+        return array.tobytes()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
