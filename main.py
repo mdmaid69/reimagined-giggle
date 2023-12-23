9b7f782b7b56fc34002f5392476d564c79c6d327
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:40 +0000

    mend plant today definite far

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
