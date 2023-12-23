3ce687d654ae056036463aa8297d8cd23004bb43
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:10 +0000

    punishment boil protection spin onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def square_number(x):
-        return x**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
