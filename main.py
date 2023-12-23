c534c21bcf985b8e6e25f0ca37a710d06d752dc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:49 +0000

    suddenly familiar identify snow feel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
