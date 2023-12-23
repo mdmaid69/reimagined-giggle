026e9c6673c6014a147dc620a33ed504154640de
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:58 +0000

    but friend affect quit lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
