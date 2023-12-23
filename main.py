900a91326efca39e1efb231538a7756e6b9d4512
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:42:59 +0000

    179bite declaration distant class instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def cube_number(x):
+        return x**3
