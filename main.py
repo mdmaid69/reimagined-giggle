6e906b146cbe8e696dcb590b78a7e82ab50e01de
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:57 +0000

    circle plate effort weather split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
