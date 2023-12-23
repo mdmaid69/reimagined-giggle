99d9b93dd5930dda4168d75221adf5d05fd52302
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:01 +0000

    wet achievement treat apparently unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
