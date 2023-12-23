4ea7a6f9843ac86659c64083986934be364bff1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:49 +0000

    discussion lookup accident deal shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
