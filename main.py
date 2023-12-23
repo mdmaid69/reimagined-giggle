171f98a1de34b86ab52eb1bde2ca8038f5b3c552
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:42:26 +0000

    belong load opponent stick company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
