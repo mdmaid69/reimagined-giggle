88bd5e9bc2e67179df6380e843d0a12ac17e476f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:12 +0000

    less guide getout middle woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
