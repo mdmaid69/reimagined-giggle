698dbf3d334a2a76473b6e50e43cd98edceac013
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:38 +0000

    below second switch squash open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
