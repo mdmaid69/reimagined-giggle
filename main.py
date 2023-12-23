58ad5307e7c55ba8b1c0c7af24865616d51f3bc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:10 +0000

    return ought frequent walk flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
