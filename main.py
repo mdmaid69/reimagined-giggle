46b58b897f77597cc5c6e8e6fb09b9be7e524287
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:02:17 +0000

    violence blame year flat empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
