2d7aac0876e47bf9937b0d0668f853841b2a9459
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:32 +0000

    release whistle so giveaway knowledge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
