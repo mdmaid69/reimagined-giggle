ed65baa169a9c51c517d496a31e853c5598871c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:11:31 +0000

    characteristic Tt powerful upset feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
