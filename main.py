5821fbfd4aaf5624bc32c1d7eacd5cc33f8d38a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:46 +0000

    precise month subject prevent smile

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
