df3ba14ca90cdc172658fc8308614f5636104189
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:06 +0000

    thankyou brain embarrassed mean rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
