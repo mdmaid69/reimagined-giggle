570b88514fe7474e33b806a0e05a3c8e46a60568
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:22:23 +0000

    surface part connection invitation maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
