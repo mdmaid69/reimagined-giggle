b479283d9f49ffab2544bbd087edd99e68b6105c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:32:46 +0000

    height ignore break equal stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
