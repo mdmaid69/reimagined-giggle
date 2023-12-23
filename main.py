70aa0692da34fb0b90f4d04c157b4b194a8b4634
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:00 +0000

    war authority maximum come detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
