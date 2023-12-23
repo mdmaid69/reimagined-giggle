9343ad084f45a4a11cb86d6aaf6baee84753fb86
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:31 +0000

    exercise fun adjective rarely swallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
