ee4a48c36af87381adf6164094d1f274b085766d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:49 +0000

    to season creature may bother

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import time
+def get_current_time():
+        return time.time()
