93f1af48e1ccda3bba222156d9aacdbb072da6dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:13 +0000

    test flat fun instrument left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
