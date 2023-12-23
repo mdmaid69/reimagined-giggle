b9e1334f7ec17f136cb855aff00f2cdd69f0ab9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:54 +0000

    dare treat closely cost lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
