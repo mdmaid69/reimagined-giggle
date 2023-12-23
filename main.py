8ba0d5aa20f1bbdd7121efc3d7076710fe65bfaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:51 +0000

    right destroy fair look range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
