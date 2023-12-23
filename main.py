9c6bd50d086e574f485e26faf2c8e9242ff4ef01
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:12 +0000

    lookback(on) group crash body pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
