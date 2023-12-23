69e1856ee716cb92437bf811163b9614c98248af
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:47:56 +0000

    welcome then keepoff load pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
