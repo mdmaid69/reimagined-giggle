9ae98b0743ed0fb1032ab65dd288b50498176f41
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:54 +0000

    method introduce difference easy job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
