7e358dd5ae85e11b092d9d26dec2aafe657a434b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:51 +0000

    school fresh definite cheek nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
