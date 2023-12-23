79b9ab75a6294270cadf0910e4efa95274e699ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:42 +0000

    estimate desperate full terrible around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
