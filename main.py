ba7532f7f4ec473557440ff59889e4e8a3172886
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:43 +0000

    estimate touch care lookaround up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
