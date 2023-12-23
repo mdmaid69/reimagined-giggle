269b715163c1e2123c913914546c6c9820171a87
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:10 +0000

    honest bad care contents criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
