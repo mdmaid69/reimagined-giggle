abdde3796f929670306f94b5079537c8f6a26c1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:23 +0000

    intelligent free notice drink Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
