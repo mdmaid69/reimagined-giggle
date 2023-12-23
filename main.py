f7cd2b6fea829063b74dbc44b07329fa1c5da985
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:15:16 +0000

    bet unite play insist talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
