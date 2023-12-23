0b204ce500a463cbeb394a813f181e08483a164b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:20:00 +0000

    personal seriously deserve might separate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def cube_number(x):
+        return x**3
