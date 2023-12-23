9d67d1a75c4b70c0a5f0ad4af2c2d452c683f94d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:08 +0000

    spoil luck visit frighten approve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
