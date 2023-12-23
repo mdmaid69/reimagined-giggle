424afcfca2ce501b50c0627603e94a3c3a7ec244
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:47 +0000

    care crash ready bet male

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
