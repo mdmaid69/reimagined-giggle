707037a0d5aee31cb3d671bc9f90c46bdcccccdd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:09:18 +0000

    neither practice case disturb together

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def add_numbers(a, b):
-        return a + b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
