6a80f300b2e011faa0406e3057def3919bee3d95
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:29 +0000

    hope fall wear block be

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def is_even(n):
+        return n % 2 == 0
+print([x**2 for x in range(10)])
