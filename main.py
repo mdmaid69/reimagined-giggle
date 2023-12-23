518f3115d97b5bbe9000aef5ccaa41e966cb3c09
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:28 +0000

    cry cope badly shrink able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
