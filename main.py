bfe69f7bd303c1badf323d41fe32742c27eca568
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:16 +0000

    cheat comfortable band complete mark

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+print(sys.version)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
