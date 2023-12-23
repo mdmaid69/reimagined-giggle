ba25140c30c4499730a797c153188089ac57931f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:25 +0000

    there advice waste up smooth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
