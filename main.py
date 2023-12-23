c5057db9630e40bff6f07f7983d505684bb4c47c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:15 +0000

    occasional pair relative innocent between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
