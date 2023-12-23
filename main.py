70c1e2c47503629bcd0bdab90f94c3f5e4757a15
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:48 +0000

    holiday train objection progress main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
