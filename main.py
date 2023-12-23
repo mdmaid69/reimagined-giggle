2849507187577d01a7899b1efa79ec392dbd9e69
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:47 +0000

    shadow bound morning capital dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return math.factorial(n)
