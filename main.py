b95be3352fce48adf6bbae62a70eccc31166c726
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:18 +0000

    study themselves I once scream

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
