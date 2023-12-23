becdb72887d5521ebef5b5f61a0bb65a117ed3cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:19:00 +0000

    fix makeup problem answer difference

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-print([x**2 for x in range(10)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
