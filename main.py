91414fd5190ad0c1bf937fe58700af0634e93de6
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:34:10 +0000

    upstairs factory other substance I

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
