394b6617e1cfcf6dd26cbd658d7ac413129b9228
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:51 +0000

    Hh disgusting interest space attractive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
