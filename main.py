25a864d987ca6fff3021de39d0f22cdec3d5bc74
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:03 +0000

    stop pay load match strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
