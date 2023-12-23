212496bd7cb4fffed31e0705ecfec2ccede2a3aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:27 +0000

    person professional guess length sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
