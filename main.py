0694638040aa1139911c83ef343ec8bbb629c7e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:28:46 +0000

    straight die badly degree convincing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
