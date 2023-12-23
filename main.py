b17136da6a35ba08ff2628fad08a36b655b24c01
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:57 +0000

    there bottom discover advise behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
