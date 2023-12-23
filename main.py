0986a619fad4499ca72649a83d65e6117888e7a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:13 +0000

    dance friend reduce identify lead

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
