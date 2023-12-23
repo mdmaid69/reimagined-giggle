aa1b9aca56e8b553ce1d159ea4eda9693beabe74
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:44 +0000

    close violence undo notice owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def count_characters(sentence):
+        return len(sentence)
