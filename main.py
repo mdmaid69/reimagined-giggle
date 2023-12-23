fbfa40f6ef4beae28066b844f025b17ccdea1353
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:35:55 +0000

    cinema ball pay experience cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
