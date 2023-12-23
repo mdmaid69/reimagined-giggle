756bc81dc7d742d803aa493242baa11b608e12bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:38 +0000

    hurt string kind along drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
