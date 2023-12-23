de95a3bbd86b0ccacc5bd0a5dd1dd27ea5bd9464
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:40 +0000

    amount freedom solve failure bright

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
