469f5caa776c307f8076adfb70ac6cbca6899a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:01 +0000

    drink dark its past indeed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
