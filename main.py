16cf554f09d8da2afd08894035439d755fe0d05a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:46 +0000

    soak precise nigga brown everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
