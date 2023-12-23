1e9a5e98b2ed81b326cd0128929f0d6055ad7c7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:32 +0000

    wide container attract sense takeaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
