82e2d6de0c8cb7221a6713a50060a00fd96cdec4
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:24:34 +0000

    laugh storm his hope comfortable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
