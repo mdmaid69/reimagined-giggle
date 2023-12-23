9ce56d2afe152b52ffbfea9759b22037cdabe714
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:32 +0000

    emphasis attitude or nearly 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
