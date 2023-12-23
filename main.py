cddd412db056396a270bf7ed26fc9c96c37f7ec7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:04:49 +0000

    pollution everyone smell wind remember

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_power(work, time):
+        return work / time
