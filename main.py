7edb6062bd63f47924fdb3f3d8810966dbeb0701
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:23 +0000

    late breakdown safety luck movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
