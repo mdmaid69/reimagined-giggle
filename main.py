7b3a8c751ccaa4fb95f4e227ebd59b62c0d41214
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:52 +0000

    centre matter extraordinary guilty machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
