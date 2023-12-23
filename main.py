aebb7a63b266a2036e396e2262af621445404550
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:37 +0000

    collect relative pass respect worst

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
