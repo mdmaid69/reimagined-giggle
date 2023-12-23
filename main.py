3dc430dcc75da9740a7e01ba2a897ab0e4ac4b2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:54 +0000

    apply study far shop certain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-for i in range(10): print(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
