115bfc4393edefa2d1f5110de2f15c78131a770f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:05 +0000

    shop tax block damage force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
