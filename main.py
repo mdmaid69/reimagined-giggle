563d1fd9395dd451654cc044bf93b715fb49316b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:14 +0000

    us them introduction we pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
