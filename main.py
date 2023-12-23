aa42ba0ca7b07bc4c95d0999d6dfb296f5ae4e55
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:36 +0000

    safe rush Hh Hh reach

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
