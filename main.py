de9f7607c0272ae7161389d99212bb4ad125ad47
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:52 +0000

    size tap back act year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
