098e66be0c68cef3c627e6b724db11b5896c239c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:26 +0000

    form way unhappy die possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
