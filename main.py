d5315e02405e7b02b61e860490d538dc9f422385
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:38 +0000

    display experience splash message turn

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
