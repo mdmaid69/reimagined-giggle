110738fa60c97fbccca61a65c5ff989d98a9464b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:34 +0000

    risk breathe no waste keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_repr(array):
+        return repr(array)
