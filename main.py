b8d3887e4c14ba126bdc8bfea3ebe3e38a55635f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:53 +0000

    takeoff true family advice usually

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
