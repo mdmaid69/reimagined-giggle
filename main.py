486118930a255a4fce5a7972101ec5584fedf964
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:54 +0000

    now care track even rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
