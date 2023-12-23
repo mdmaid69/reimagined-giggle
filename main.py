bef4e031db59143d489e6b0ad0149797edb3c602
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:17:51 +0000

    top callat cheek combination deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
