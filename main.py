3895bbe1cbc98dd915b08833628a1066a42ae777
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:21 +0000

    transport borrow in disturbing all

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
