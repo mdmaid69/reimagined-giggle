cba00231b50525ffe3983343c9d9d3fc542d1309
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:49:40 +0000

    ignore thumb language sad capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
