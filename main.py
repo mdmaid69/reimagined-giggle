b37648b61a7246f9f1634f5949e26eb69d4f843a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:30 +0000

    error divide yours face autumn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def sort_list(lst):
-        return sorted(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
