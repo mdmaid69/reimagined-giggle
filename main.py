7fc62e6115e12664f7718e6b1f87277495469489
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:23:01 +0000

    punishment move nothing drawing Mm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
