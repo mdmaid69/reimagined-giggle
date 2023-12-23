b5dd8e0a1ec4ea7f696f39583bc92d180c921f18
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:57 +0000

    wide black nor race car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
