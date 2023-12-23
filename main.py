2057bbc5b692321f07f7e0bba8fe90a7f741fcc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:20:34 +0000

    club tip shop terrible account

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
