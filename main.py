ba04c554de88b0338d4dc1f383fe6e5a38de35ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:48 +0000

    uncertain control offer judge supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
