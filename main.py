6a21ed84a34e2d415d53dbbf7da92641feacc62e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:15:28 +0000

    agreement such compete high whistle

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
