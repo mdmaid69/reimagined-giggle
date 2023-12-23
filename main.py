d20819e4bb18be454cc072688561882cd7df1903
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:56 +0000

    fence hit answer catch uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
