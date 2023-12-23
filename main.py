e84c57494dc67ea66e93d065a2072c13c646975f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:00:19 +0000

    afterwards walk experiment key favourite

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
