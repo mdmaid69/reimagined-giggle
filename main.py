3ff9d7c1dbba9fed5b7056515c6ed1d5be7ce0a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:19 +0000

    doubt abroad careless address ball

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
