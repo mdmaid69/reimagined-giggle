1dd86cf71eb62b805601fa6f81ed48ec55804edb
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:15:19 +0000

    equal great on operation shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
