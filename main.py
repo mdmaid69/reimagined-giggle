076a5c21a1556bf66db82b26366445a2c25f50ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:35 +0000

    urgent send already authority hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
