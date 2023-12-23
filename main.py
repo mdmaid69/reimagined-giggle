89246fd9ad58328ea446dc4693b9a1adf29a6138
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:04 +0000

    wave last drive dig dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
