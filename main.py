ee6f366ab0c2d8fc8c044446ce3989d6fef0c8e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:45 +0000

    reply flat common dig hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
