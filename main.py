ae58c5c4be88808f241b74aa55b1e97febae38c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:07 +0000

    operation collection mind after object

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
