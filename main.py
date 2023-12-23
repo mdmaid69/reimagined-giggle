43daabbb6e7ae10eac453c16bb15c378912d2705
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:33 +0000

    official over push though find

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def reverse_list(lst):
+        return lst[::-1]
