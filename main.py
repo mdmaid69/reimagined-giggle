7be83f3f12531194444b0e6a83f84f975cf904f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:30 +0000

    protect 568energy modern communication cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
