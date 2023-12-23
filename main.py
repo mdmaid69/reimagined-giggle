3b69ca95e2dbcdbf0a88ef621ed9552877744387
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:10 +0000

    cover walk owe house afraid

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
