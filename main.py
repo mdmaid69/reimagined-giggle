14734551dc43fce7d9c159d5f92728169cb5016c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:43 +0000

    pretty change waste union last

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
