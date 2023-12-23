a60cb7fbe1885d442635d675b29c5a6d93d0cf06
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:12 +0000

    confused summer whatever thankyou performance

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
+import re
+print(re.match("h.*o", "hello world"))
