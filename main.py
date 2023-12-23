171308b4b3b3a7f0c7a7903fcca2eb172ee6b1d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:49 +0000

    wipe plenty tell occasion actual

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
