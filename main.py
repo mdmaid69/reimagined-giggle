a8c41a5fe34c4b6df08f2f49c3aef52282a9d94d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:49 +0000

    violent tired relative listenfor score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
