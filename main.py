88d06440ec63ecdf89c7f61064fc20f94dea55fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:14 +0000

    pretty direct excited thanks married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
