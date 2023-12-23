2b6d133d053c5a2ac366b7d0361f75ffe39c3f5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:46 +0000

    deep Yy professional genuine shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(lst):
-        return max(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
