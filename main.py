e8cce4d2a1a4865a51cd01d12b0cdaa54df5db9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:08:57 +0000

    weak line accommodation mark dead

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
