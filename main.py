b27bc2e3e63f607fe7ecb872937468454d87538c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:58 +0000

    accept whenever shut other to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
