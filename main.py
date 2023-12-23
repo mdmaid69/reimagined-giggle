7a4dc957f1f62341b03d7b7eb8cb5170acaa1a66
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:52 +0000

    everybody zero absolutely arrangement common

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
