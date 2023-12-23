1d284809063e102ca202fba4313f645b05413455
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:16 +0000

    beat run ball stranger information

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
