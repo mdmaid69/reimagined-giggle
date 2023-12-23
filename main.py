ba80881695c346207df81a43c765e931602db3e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:45 +0000

    precise occur yours care order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
