587bc03525a73204d7cb5df00ac3fdec8ba3671a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:36:43 +0000

    ago guess thank quarter smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
