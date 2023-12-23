97006080e5d7c43dc8baa05a115b638a1865a57a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:05 +0000

    nigga fresh tired check off

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
