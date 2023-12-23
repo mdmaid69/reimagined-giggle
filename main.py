f2ba9fcf306d75f4437bac0bbf9c6caf1c0f8c66
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:52:33 +0000

    stick other concentration runinto tough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
