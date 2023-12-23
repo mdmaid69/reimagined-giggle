5ce3f0d9eacc2202a8f89f9846c8cf226b110f8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:16 +0000

    possibly sore attempt any angry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
