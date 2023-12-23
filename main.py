a5c5fd783086373f7d1530cb26704cfce74ed5d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:11 +0000

    project move upstairs Tt every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
