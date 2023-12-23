17adecf1fdfae9219d36804fd5c566db4ced4150
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:33 +0000

    rescue mention achieve back maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
