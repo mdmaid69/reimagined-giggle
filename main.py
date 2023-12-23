b416b19305c4041e8b881b86b77abe53f2125b4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:21 +0000

    few reject wave declaration often

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
