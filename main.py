53e964d7498527cc936c37f4647f6c9eaf18b33d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:12 +0000

    divide pollution whenever impossible load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import math
 def calculate_floor(x):
         return math.floor(x)
