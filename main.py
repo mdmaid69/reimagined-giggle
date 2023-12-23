38871fd678a7c520ebb32b827c659cd63c5ed90f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:34 +0000

    phone production skin reliable bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
