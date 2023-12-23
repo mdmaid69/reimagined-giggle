05c49f33ab98bc5bba2ac2ebb8eaac561f5a059d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:21 +0000

    judge whistle date so use

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def clear_array(array):
+        array *= 0
