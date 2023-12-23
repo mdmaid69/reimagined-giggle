b9902473d68dd0a897d3dddb29cf40c4c8339e4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:14:13 +0000

    effect handle please science book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
