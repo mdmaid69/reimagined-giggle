f50b84bd332a8f1805cced028926e2ab8dae17eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:33 +0000

    wet process rid everyone suck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
