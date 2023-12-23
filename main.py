ad5bae9c1bc33a2bd677b3426d2e787229dea6e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:34 +0000

    sound job appear characteristic luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
