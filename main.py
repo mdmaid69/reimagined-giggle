b61bb950b3b5f0300300657578f59f2bc687269d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:47 +0000

    admire store warning bye keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
