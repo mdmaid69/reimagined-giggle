78855ad848111d3507253911028ad68af9a18163
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:20 +0000

    central little justice basic move

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
