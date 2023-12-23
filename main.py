0e1bdcea2d52e540b35a592e8753def12b70246b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:46 +0000

    unhappy lead square down sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
