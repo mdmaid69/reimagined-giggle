9465c1c2fa113319e5316ae019c8de4e76cebe79
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:14 +0000

    strength tie lucky arrive slow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
