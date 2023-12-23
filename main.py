2c1b0a37c4737c4b09e48854cdef05dc5dad8ebf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:44 +0000

    store draw reserve Nn deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
