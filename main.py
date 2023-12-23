1e3ba92405443f043cbd6edb40e51bfbb4f95cf2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:24 +0000

    godown age cry dangerous free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
