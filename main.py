94a7e184ccf683ec5ebd6884c2fb2029acce6c99
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:13 +0000

    calloff sentence offer happen realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
