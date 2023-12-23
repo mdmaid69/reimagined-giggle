e8cb1f5430948e5f2013692b1e14ff6dbe709fdc
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:33 +0000

    share murder teach friendly still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+x = 10
+y = 20
+print("Sum:", x + y)
