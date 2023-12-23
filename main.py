768fee07e1cb2963ba42d9a6503be530faadd9af
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:36 +0000

    check suck suggestion war by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
