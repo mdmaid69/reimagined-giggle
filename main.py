f971082c61eb2f7d4615e4638f3efd1a5a5bc62d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:11 +0000

    ashamed problem principle name please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_current_directory():
+        return os.getcwd()
