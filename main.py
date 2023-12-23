21a108407f0ce550f03a4a32f6ee03810064c044
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:18 +0000

    sound spread bus surprising distant

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
