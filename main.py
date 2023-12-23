687cee98d832ab961d1413c3d2fd79bfd980a44a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:25 +0000

    least should house contrast communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def change_working_directory(path):
+        os.chdir(path)
