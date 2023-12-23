4c2ab77f27ff9ccf425c92279f412d74033d80ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:24 +0000

    ability innocent punishment relation brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
