6c163e2140e7b3d061d44e1c11cce1076a710218
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:06 +0000

    mind read grow fresh touch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_binary(n):
-        return bin(n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
