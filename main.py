20174f82d07bb5d9e3533a83951c0ba8c1e70ccc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:59:24 +0000

    their thin reply takeoff carry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def remove_duplicates(lst):
+        return list(set(lst))
