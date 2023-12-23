b59cf1e114609a4d167953cfffec279380377fd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:15 +0000

    slightly injury raise game land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_max(numbers):
+        return max(numbers)
