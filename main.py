7dabb0b7ba7122d815497413ba9ef6c48b059329
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:14:38 +0000

    smile off task switch save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_directory():
+        return os.getcwd()
