aa7a7aa7ee95e61af2ec5fab5416b723c5424a57
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:22 +0000

    round attach answer welcome record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def is_odd(n):
+        return n % 2 != 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
