f24b1f2adaf5f91fea97aba8505a4f1411f33d55
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:08 +0000

    blind none feed Qq neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
