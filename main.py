02948cc0cb4c8333afa49823c92b0ddde966c9f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:11 +0000

    sure obtain wind value order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import sys
+def exit_program():
+        sys.exit()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
