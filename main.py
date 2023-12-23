fe083334759ddc186d6bf1fab7f90446a72af3a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:43 +0000

    spare behave public concentration carry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def is_odd(n):
+        return n % 2 != 0
