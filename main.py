e8302a0d09d8781418802986cd08c0dd7fdc8c19
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:45:45 +0000

    less relief club remove bet

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
