e0e54585154fc4446f3ef8f0143c44ddfa7a68d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:12 +0000

    hope often about miss object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_area(radius):
+        return 3.14 * radius * radius
