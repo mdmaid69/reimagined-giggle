4305675b9de021dd4be10ebab88a44671baffef3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:37:47 +0000

    total visit tough relationship once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
 import math
 def calculate_cosine(x):
         return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
