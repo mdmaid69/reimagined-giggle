9d02e7f7222c445a300c18b9870c758db0139fbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:22:47 +0000

    cover pen quarter between guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
