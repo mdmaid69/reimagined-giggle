e7514513c1d58d246cd074e3420c4049f7c9e69d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:08 +0000

    next sad wipe watch once

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
