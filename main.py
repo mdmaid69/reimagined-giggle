d5b9d23985418b0a4bdac34157a6e592e213a1e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:45:45 +0000

    exact turndown order sensible several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
