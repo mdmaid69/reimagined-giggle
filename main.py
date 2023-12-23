00d5bf48da9ddff4e0699112a47ab96e2dc4c30f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:10 +0000

    path compare heat slightly pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
