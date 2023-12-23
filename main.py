92f7034777572403031e6f99fbb85b74be8356e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:54 +0000

    realize famous square office everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
