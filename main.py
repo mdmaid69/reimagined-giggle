bf24d3a2fc2dcc4416332179bf8b97ae61576550
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:44:32 +0000

    healthy either result design hurt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
