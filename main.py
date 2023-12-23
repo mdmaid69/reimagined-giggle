5d05f2cd22b48e809ef81cf635a1178d031473cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:54 +0000

    remain open major conversation scream

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
