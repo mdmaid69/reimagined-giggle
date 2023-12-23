7dae993a991742f0382853c40fcd0671bb6f7510
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:56 +0000

    somewhere transparent embarrassing main unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
