400f76fd6eb14becd3af1ca89481e1c28728f347
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:43 +0000

    famous speed before loose give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
