3905bf08412f55637672e464ad42ab11393f3cf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:06 +0000

    sign innocent add bar rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
