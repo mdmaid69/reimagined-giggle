82e678b687f1705ce633f68c83789ec64431bf43
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:29 +0000

    test pause entry listenfor meaning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
