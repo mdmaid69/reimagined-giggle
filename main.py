79e1f88f9f4f91a276673ccf284ba4a22e8ecef7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:47 +0000

    bored convenient freeze equipment human

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
