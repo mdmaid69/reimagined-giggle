4ede703b3d4298539377e5ae18abf1a04283e03e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:09 +0000

    here 568energy sale pleasure house

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
