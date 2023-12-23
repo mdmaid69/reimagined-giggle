239df26006b57711f19a0e9ca7d41ba81e84f794
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:02:13 +0000

    strange wire clear reliable everywhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
