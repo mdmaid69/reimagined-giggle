ad84ffb3abda242300287673d8e25bc26c578ce2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:28 +0000

    gentle later undo yours father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
