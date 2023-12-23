cc593638f6aaa53a685cff3e484c9a1ac0beb9da
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:29 +0000

    pour bend flow compete marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
