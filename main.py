a35e756ce767d7d445ca8bf2683d22f23d715e9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:49 +0000

    past lunch turnoff find country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
