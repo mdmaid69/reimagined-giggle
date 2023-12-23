8171c7dbbb7b92904b614beae2733d04041aed40
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:33 +0000

    autumn bring once turnup common

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
