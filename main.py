54fd1deffd7001aa9242a28902c168eb84a53764
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:11:36 +0000

    will range plenty low fall

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
