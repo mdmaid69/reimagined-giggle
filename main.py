2034d5a7fb756cf877db911550f1811b5766a21f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:31:40 +0000

    raise show welcome work care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
