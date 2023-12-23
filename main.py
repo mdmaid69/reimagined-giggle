6f662d647bbd78a346463bf119e797d96d27fafb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:36 +0000

    cheque house whisper appeal loose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
