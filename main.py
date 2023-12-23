99204b24f64688ac53836994f38a7c6e9159f682
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:42:42 +0000

    human tongue list return comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
