c05412bac7c63cfc51e5ee69a163fe0258aa7d99
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:47 +0000

    continue fire Nn trousers swing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def add_numbers(x, y):
+        return x + y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
