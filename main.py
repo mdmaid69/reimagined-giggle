4fedec3d1cd09a20bb664a44bae159ad6e0838cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:07:14 +0000

    corner congratulations beneath refuse laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
