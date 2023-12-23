abe8ee2f26e3c04f1efc1baa8aed88c6b8b908fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:49 +0000

    unpleasant tidy stand bother sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
 def get_bytes_from_array(array):
         return array.tobytes()
