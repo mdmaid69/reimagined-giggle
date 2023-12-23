3a2a96bee2d74c263bd6d075038b0d31302c4e02
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:24 +0000

    passage behind average worth city

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+def calculate_work(force, distance):
+        return force * distance
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
