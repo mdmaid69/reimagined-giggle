fa0a17febfc96a3811ad2afc5a1b029aa348eefd
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:28 +0000

    branch direction behave book bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import sys
+def exit_program():
+        sys.exit()
+import os
+def remove_directory(path):
+        os.rmdir(path)
