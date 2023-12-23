6cf3d62fe266b0d5db5070c03fe3d1607f3d0134
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:41 +0000

    punish male stay common Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
