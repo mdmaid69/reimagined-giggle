e210196e287d28a6b3ff7a9e05a180f6ff919fbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:10:33 +0000

    what state Cc whichever beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
