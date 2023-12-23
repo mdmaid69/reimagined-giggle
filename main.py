d94a5fd4f8324262d153336da6c358e3bf63f2e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:43 +0000

    boring reserve muscle disturbing path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
