99d8324853d6660520fe83dc5875e15ecf10c7dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:59:53 +0000

    negative completely leg look unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
