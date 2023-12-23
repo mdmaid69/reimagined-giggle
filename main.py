07685e9375c765512efa4ac20bae190d34606fc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:35:22 +0000

    that train wise talk low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
+  def count_elements(lst):
+        return len(lst)
