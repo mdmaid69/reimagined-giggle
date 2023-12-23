fe1b22a91f1e73f267bfa147063a5f08cca14034
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:50 +0000

    stand considering search breathe breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_max(lst):
-        return max(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
