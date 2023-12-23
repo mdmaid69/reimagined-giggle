ed2e43b42cabe02e5e34e143cc620a94d01c8eb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:17:32 +0000

    expression gather free flat design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
