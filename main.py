d9d7d7c3cf6170bf003387abf5f077828e806b8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:10 +0000

    listen breakinto black uponon remain

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def square_number(x):
-        return x**2
-def calculate_power(work, time):
-        return work / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
