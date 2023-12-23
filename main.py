da14451737ecca4bb092e8be628ac4f43d8bcc2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:53 +0000

    other beside present gooff last

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
