20847407049f374f91e62ff10c51aca975657727
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:41 +0000

    wealth separate fall develop giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
