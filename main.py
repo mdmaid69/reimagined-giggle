f23ba69bb3e68f78d108f05de9c39a94afea15bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:18 +0000

    card Gg national occasion knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
