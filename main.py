4cd43bb66e793437e1a11a821732e8d9c3d14a08
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:45 +0000

    motion build scrape century plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
