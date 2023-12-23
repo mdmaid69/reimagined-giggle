e02b858fbb55b20578753cc0d788b506a5766446
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:48 +0000

    picture beat talk evidence century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
