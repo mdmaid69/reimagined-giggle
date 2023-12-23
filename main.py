a84330040e565deaddb206c7b7ff3a3baee910e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:25:13 +0000

    cheek right escape praise pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
