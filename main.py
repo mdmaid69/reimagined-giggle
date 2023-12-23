01921450db50a6a35b5a8c32277e1296a1931d1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:46 +0000

    honest sing very it black

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
