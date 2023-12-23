cbf24172517a6d4d6a4d6d5c329c462a6ea498f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:41 +0000

    possibly out child education negative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
