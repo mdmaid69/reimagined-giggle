6ea7290b02d878ab5db484afc903d29682983f54
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:16 +0000

    separate follow body way although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
