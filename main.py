1621219251b8dc94c6b3e820ca6102df6c581b1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:28:25 +0000

    skirt risk tap compete disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def set_array_item(array, i, item):
+        array[i] = item
