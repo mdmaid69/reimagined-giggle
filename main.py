df5f325a7906138ff3d0d273576eb1b3637a5bc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:52 +0000

    birthday lay straight exchange disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
