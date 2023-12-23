04ca4d2fce3f8d1dc0f9b4780769a4c82923ecd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:58 +0000

    well more way stupid 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
