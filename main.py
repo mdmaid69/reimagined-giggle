a233ebafc9614820be54aec0ea45eaef81f79c2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:30 +0000

    stupid preparation him test knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
