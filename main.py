b844da740fb1ca9358d230308656793d91eb9546
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:23:10 +0000

    exactly runinto press past year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+print(sys.version)
