077c09bec99daa08ea5d0816c4751ec7c59b7b2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:37 +0000

    left fortunately rely reaction tidy

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(numbers):
+        return max(numbers)
