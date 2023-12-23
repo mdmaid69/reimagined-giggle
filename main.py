a7bdea3554fd5ca76dab747d4405e595d9be3648
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:08 +0000

    accordingto cool borrow conclusion open

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+print(sum(range(10)))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
