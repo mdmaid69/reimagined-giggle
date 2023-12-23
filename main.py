5c449ca46088d7383ebc74f57fe496f2ed7cf39a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:06 +0000

    service intelligent wander height turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
