b60c4961c67da9805169575250a0d86583d01b34
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:21 +0000

    steady coat thinkof this its

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def divide_numbers(x, y):
+        return x / y
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
