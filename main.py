8c16a322212b244446391a50a02d517ed24956b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:22:37 +0000

    holdon reach unit scream march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import array
-def get_array_as_str(array):
-        return str(array)
+def set_array_item(array, i, item):
+        array[i] = item
