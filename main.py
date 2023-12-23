e193417f137a4c145c3a1dc474288f6d2ab7820e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:28 +0000

    sense polite relationship disappointed twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
