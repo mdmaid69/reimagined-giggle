67220ad93784e8308118a9abb1571c78e62fdd8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:55 +0000

    flavour together crisis happen may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def remove_directory(path):
+        os.rmdir(path)
