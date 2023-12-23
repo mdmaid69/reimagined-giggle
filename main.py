d8d6f1e7a6d0afeb273dd9770707c28d3c7718f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:42:55 +0000

    happen account yours delicate space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
