1fc64138ef215b718fbea2867a175c95a2fe1f25
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:23 +0000

    mix reason tie common length

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
