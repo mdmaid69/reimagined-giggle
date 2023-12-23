b030f850db200854e0ce8308ae9d1eca3dcb5041
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:00 +0000

    should telephone computer female rarely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
