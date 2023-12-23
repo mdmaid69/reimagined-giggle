25ce13c5c54ab1fda40d909a8227ff0efa9e911b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:21:20 +0000

    bank accept television rob depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
