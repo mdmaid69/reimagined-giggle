90d9cd098a11c6da4c67e24678222f38eea7cf56
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:34:14 +0000

    giveaway deliberate at time catch

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
