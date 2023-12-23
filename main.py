e636d6a1b7df5f6ef36364f339a36c2f95ed0216
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:48 +0000

    problem somehow traffic straight achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
