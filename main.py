75da296b20023938c8bccbc43521f8034083373b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:30 +0000

    transport charge wish speak cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
