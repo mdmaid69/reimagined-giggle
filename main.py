daf0493871a11c5b13443a923b5a5dae11445197
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:37 +0000

    share dress history still serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
