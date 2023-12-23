9e12a18f887a63fc8b7bd93ffe23b4c834f4db2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:03 +0000

    sentence artificial whoever crash combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
