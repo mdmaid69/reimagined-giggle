3e216e5712e9c1397bed2eed82743931beb882de
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:58 +0000

    people light and keepaway middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def log_message(message):
+        logging.info(message)
