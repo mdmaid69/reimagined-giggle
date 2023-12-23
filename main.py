1d81c3ee83803fee45f698e7a932906b08b56a23
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:54:36 +0000

    boil care forever wish better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
