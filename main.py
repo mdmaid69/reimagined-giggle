010f176329b90f5050bb7c21ba2a21e68729c3dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:47:02 +0000

    bored start interesting now duty

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
