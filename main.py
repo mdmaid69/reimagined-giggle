4a8dcfcad71e7b7ea5a9419dd4a401cbc3ac1b5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:54 +0000

    sweep force Hh back order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
