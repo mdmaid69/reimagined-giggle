5f7ce1720fdfc2cd013beefc90c75fda3b4ccd27
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:33:23 +0000

    doubt comein genuine height awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def divide_numbers(x, y):
+        return x / y
