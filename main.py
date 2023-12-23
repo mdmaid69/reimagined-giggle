4456a8a7facc0d1581880d2d5a47b2c851a808c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:31 +0000

    outside pen field practice interested

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
