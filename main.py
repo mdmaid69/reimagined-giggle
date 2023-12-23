3d38513b309f0cabc1ef118b24bbb1e197321d86
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:38 +0000

    address guess perhaps flat smell

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
