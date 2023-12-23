5c7117f851f1237f6b832d344b5ff66f94dc8e10
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:28 +0000

    Vv 566end our see peace

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
