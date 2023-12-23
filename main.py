9b86234ad00b4228ca873e8ba50f8165ae5fe612
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:41 +0000

    beside money land hear less

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
