15d65fa219844e1677e6f982f4a9fc38b721b6ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:06 +0000

    matter payment about black disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
