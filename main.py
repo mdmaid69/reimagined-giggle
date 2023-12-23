8ae8ff24f9a2df76d51c0dfcaf8600368fb7fc01
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:52 +0000

    definite he hope bus extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 def count_elements(lst):
         return len(lst)
