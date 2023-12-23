31aa7908ba682d15a1ecd09bab7481c3971b3ace
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:08:45 +0000

    which tap in search high

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+print(sum(range(10)))
