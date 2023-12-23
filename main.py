2117c5a67c460b13cea0780143a43afa4b0f3013
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:29:13 +0000

    rank coat means unfortunate tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
