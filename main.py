aaacd71bd9644cfc6482ed47d003fb29172e3d86
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:39:59 +0000

    nose alive appeal afterwards still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
