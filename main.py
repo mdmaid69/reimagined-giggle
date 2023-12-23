6b1f1bd43c24ea8e3dadecd4ac0f4d329bdcf9e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:36 +0000

    pipe opinion especially put bright

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def subtract_numbers(x, y):
+        return x - y
+import tensorflow as tf
+print(tf.__version__)
