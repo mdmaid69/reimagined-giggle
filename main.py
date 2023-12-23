2168683e13e35ce91266814c289add59829793e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:11 +0000

    Mm tooth old turnaway breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
