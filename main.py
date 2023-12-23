50cd691f6c7c607e4d3f99532aa4ae30cdcd6919
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:41 +0000

    children gate famous rate clean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
