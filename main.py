e5546d32efcf334154311270eca941dfda19346d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:33:48 +0000

    easy moon save material position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
