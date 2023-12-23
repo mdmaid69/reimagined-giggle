9460efaad900fe08732d20f3531e795da3919391
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:35:44 +0000

    delicate mess breakin they season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
