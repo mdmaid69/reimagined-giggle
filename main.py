17232bbfdb2b924e97d3630b35b15545acbd0ea9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:07 +0000

    Jj just control software and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
