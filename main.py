23892ec49abc974fd6254674650e3a1d1f5fd11e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:13 +0000

    fact goback(to) whenever recommend guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
