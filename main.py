a28171aa956c2a5b4955b11c634015fd43499ef2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:08 +0000

    breakfast strength suckme well machine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def square_number(x):
+        return x**2
