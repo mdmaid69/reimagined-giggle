56f1f00cc69b86496fd17f5a52c8ba5b1296852b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:09:01 +0000

    formal winter beside suddenly life

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
