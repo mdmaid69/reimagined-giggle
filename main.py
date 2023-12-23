a3a40522aaa9c52b39207aa48f19170596656518
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:46 +0000

    control slow comefrom yourself underneath

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
