a6a0a507182512ef015cd447a8867d0c67d6e84b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:20 +0000

    scream sleep same fear responsible

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
