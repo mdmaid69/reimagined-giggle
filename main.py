2c1ff2db8c912463b3b11623273c2418b06d333d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:10 +0000

    score disagree but foot wild

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def convert_to_octal(n):
+        return oct(n)
