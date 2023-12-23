51c53ab5c4b5ad7b2e989100fea32985271a484a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:59 +0000

    wild awake professional runaway variety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def get_array_as_list(array):
+        return list(array)
