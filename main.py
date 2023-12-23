f306b64066a763fe49429a38c2f67f239a9ed0fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:55 +0000

    anyone intend wing about mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
