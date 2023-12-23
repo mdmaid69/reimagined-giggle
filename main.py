cce48f92509c2e7108b334ceb05af8dd3513e386
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:12 +0000

    union paper belief progress twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
