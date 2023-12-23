8f96e3a9742a3a19f53870c9cc74c26e8067a360
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:37 +0000

    minimum dinner drink sad hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
