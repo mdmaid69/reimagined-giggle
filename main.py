2a44d9174f4bfbdcb34a849033eae235fc08131c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:46:45 +0000

    nonsense experiment frequent everyone anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
