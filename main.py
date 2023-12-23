cd427f1a7d5a99d5a645fb93862b3288bf2a77ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:32:10 +0000

    plenty bite prevent catch support

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
