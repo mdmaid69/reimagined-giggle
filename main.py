92f551bda50fdd01b8250dc30d37aec69e45f4e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:14:29 +0000

    nature Ff belong behind knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
