561521d0242361f36ef8b225afc7bc867e6ededc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:26 +0000

    insist complaint regard appeal middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
