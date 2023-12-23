2b2e94e0e2414fe430bcf6e923cce818a5d41f3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:04 +0000

    pressure damage order together change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
