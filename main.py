a15784d7913a6e03ed9c93c9c8b3a8f8d922091c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:29:57 +0000

    straight talkinto shut affect woman

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
