84a2fa0908fbc4ae2e59c48b8c83c06d12f0e71c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:09:21 +0000

    act stupid fact drive accidentally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
