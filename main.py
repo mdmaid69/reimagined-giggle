2f66ba9aa8a2bbdc50693d35d6fc03b0bb98b241
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:58 +0000

    stir scrap tough live pause

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_odd(n):
+        return n % 2 != 0
