a5212801924f06a2b6583c200b224d64b28b58b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:14:40 +0000

    everything pronunciation money near emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
