f9498fdc67014ec6b4774bf0dfd2b8f91c9e0afe
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:37 +0000

    rub round OKorokay tonight gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
