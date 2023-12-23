01d80506f2167efce2515566ad450a1f023fdef1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:04 +0000

    performance science breakup care limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
