f8e60ba129074df0fa2b47a4e808fd58c306c0b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:38:44 +0000

    reliable thing powerful catch not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
