f95ce99dbf4facf3e46274e07e7abaf321493e35
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:43 +0000

    cough spare beside increase quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
