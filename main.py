8d8409c5108e70a050b6e90eea33e4d65f4772c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:14 +0000

    blame spray black lose accept

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
