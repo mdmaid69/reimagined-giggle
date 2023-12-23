b5e260479a10d35dbfd7e31a267574d61ee1c7c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:49 +0000

    instruction design young content getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
