df46a6b4908ead76e13b7e6dbd9073f85f1facf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:15:53 +0000

    stamp figure callout work present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
