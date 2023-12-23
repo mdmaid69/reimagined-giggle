af4901c3aaeb5206b9618e0e99426fb8faed6213
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:50 +0000

    forget public ignore ticket free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
