9c70a0866e8feaa2cb867994375daf6648486cee
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:53 +0000

    realize juice wish turn main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-print([x**2 for x in range(10)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
