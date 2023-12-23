6fe808a222930a6829e8e76481f2ea662d54971b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:37 +0000

    else radio hand offer back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
