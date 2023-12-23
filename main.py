4d91f01f5667a5b93ba076895f296209b87aa922
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:54 +0000

    send post move moment compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
