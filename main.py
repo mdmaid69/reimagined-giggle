678a64d547a77edfb8eec0a15c5c01f2a9973ca3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:19 +0000

    comefrom neither art mention not

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+text = "Hello, world!"
+print("Characters:", len(text))
