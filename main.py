bbcb5e3ac7daecf6e8aeed84299e325ab73345e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:32:06 +0000

    put grow supply everyone lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import tensorflow as tf
+print(tf.__version__)
