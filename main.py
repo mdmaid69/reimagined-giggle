6c7337c7b1483680da25512a1f974c60163cf9e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:07 +0000

    date keen bringback perform polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
