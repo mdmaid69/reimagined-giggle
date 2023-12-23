125c69c18bfc1ba5d602fc2449e7675faeb5afa5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:07 +0000

    clean herself protest figure tend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
