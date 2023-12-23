5f2ba21de080b57a263e2f938fa940829b6239ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:33:27 +0000

    attract upset instrument murder scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
