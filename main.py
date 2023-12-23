2c331df7fddd4623f52fcd69a7cae6a5d79e80f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:25 +0000

    many time joke collect niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-print(math.pi)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
