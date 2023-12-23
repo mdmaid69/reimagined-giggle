d8fa827e69b1fb4ca9090ce1ab2e110f975a605d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:29:24 +0000

    exam consult respect underneath instrument

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
