0dd02ca27985a460729c157942794043cfb3dde3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:48 +0000

    container closed plenty learn land

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
