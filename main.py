ae9d7edadac252ae13dd03fffc434a1b03f6e42c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:28:19 +0000

    best correct drink organization situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def convert_to_octal(n):
+        return oct(n)
