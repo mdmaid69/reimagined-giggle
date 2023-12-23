178384a8946ef5e039705239fc6081b64a20811d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:18 +0000

    fall runout pocket bang butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
