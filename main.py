31944a7da4dbd5255adf0376e4be60a2d8c72005
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:26:01 +0000

    improvement pause bye walk poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
