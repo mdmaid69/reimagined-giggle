4744283d2b6c9aec884d956b0944f3b72c05bb63
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:35:19 +0000

    workupto reject also odd mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
