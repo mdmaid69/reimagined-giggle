77ecd8d459f1baf4eeaa95fa456cdecdd993add4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:47 +0000

    each rush recovery activity sea

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
