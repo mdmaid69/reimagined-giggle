577cfa9d42eec152d05fbbab41edb4a58aac196f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:45:37 +0000

    theirs obvious where relation cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def convert_to_hex(n):
+        return hex(n)
