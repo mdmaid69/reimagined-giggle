105d30fcbbc8eadb1d9b27f0714a37c4d3b0e89b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:00 +0000

    point least beneath rest design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
