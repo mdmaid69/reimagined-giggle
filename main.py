1243540306a4989ba6eba3741eff7235019e4af0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:29 +0000

    minimum train kind worst hold

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_union(list1, list2):
+        return set(list1) | set(list2)
