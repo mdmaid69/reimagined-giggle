888e4c03f1dde358e214d23f3b559f5d9317de12
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:34 +0000

    science human flight fond on

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
