7fe61b38a008f94316372149b3af4edf50572f0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:53:41 +0000

    commit also alone murder trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_list(array):
+        return list(array)
