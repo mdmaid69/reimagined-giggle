83eaba99e4101a936ce581df5fc9568b0c9108da
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:24 +0000

    odd clue over classroom relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
