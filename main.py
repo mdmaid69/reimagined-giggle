92fa6bddc4b9751b30208afff8665b4b146bd99a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:15 +0000

    rid beautiful basis calloff bar

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
