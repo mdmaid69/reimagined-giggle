cdc28b05733cbb5e5954a174a58a7a538a1f114d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:19 +0000

    flat matter bath music many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
