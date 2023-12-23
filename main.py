391a45f36b80f65f07b74c0278e3b24e9b5f671b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:31 +0000

    discussion approve emphasis may remain

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
