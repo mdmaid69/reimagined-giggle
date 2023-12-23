2b1fb3d4b9501451e430067b8c27e8753861756b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:08 +0000

    disturb free undo stare false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def greet(name):
+        print(f"Hello, {name}!")
