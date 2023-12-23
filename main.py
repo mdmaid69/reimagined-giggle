e79ac03503c0d5b33d8fbf93c8178b5d4c719bfc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:46:15 +0000

    obtain short 566end maximum rate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
