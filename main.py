9feaa848d99f834d3e9a865af71bca8b6036a63c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:16 +0000

    then dance as temper communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
