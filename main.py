a13a60b55f03dce94315a708ffd5039291ce4c50
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:24 +0000

    confusing minimum quiet high rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+for i in range(5):
+        print(i)
+def find_max(lst):
+        return max(lst)
