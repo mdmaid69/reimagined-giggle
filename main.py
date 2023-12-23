67cae4c85820eaf29830dcb2d4d9c8ff87cd500e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:25 +0000

    equal to noisy pass female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def divide_numbers(x, y):
-        return x / y
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def reverse_array(array):
+        array.reverse()
