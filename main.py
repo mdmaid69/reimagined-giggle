fac014c8cb71018d7c957eff58fe56e79aa2379d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:54 +0000

    undo silent shout downstairs challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_odd(n):
+        return n % 2 != 0
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
