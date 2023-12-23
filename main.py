082d5399845fdc9f3b633835f5eac827395bb069
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:33:40 +0000

    who form flow right shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
