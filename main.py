ce5971c66090d9ca9e72eadc39a9d608a84312a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:35 +0000

    telephone nose lock quarter trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
