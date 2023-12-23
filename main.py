d5f57245025c477a49db72a87d6b55476cae7062
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:31 +0000

    return wise narrow yesterday fasten

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def append_to_array(array, item):
+        array.append(item)
