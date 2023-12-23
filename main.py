ba8dd50d53a39bddf991bef7b0d6aef0d36703b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:51 +0000

    general than sensible spend distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
