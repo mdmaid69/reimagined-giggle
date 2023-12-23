2ec6f0c930f539c6b01a8941dcbd8849f449fb05
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:13 +0000

    fact old stranger apply able

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
