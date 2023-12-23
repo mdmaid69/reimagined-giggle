3a57b10f5628b074907e339d47534cd9c445ffaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:27 +0000

    above method callback tend even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
