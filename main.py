0563f53075c2c366b4fdcdf376fd17b065bb397e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:10 +0000

    callup tonight worth spend drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
