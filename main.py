37363c12ef59e2ecba1042702511fb6632c7660b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:58 +0000

    meanwhile block everything suggest thorough

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print([x**2 for x in range(10)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
