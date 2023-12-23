71880569bac4f780d96dcbca07e444d00db90411
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:42 +0000

    organize half region sense knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
