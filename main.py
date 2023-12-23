6f65662daa872a1e8319d524b3b2329f315eb11c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:58 +0000

    see sign road error learn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
