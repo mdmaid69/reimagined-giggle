f848de0fa9cc70a8835d01c1f32e29598bb72ef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:53:50 +0000

    people old twist exactly callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
