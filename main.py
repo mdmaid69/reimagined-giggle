01cb65cdede5c843f5613616537351da967e6b7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:40 +0000

    hardly sort squeeze thought sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
