8cf779e77d625f7e0295b4b004146f94c53c475e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:06 +0000

    repeat deliberately word urgent skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
