b4d4bce0681386872d454634a92e52a66d2e1b6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:57 +0000

    load exist trap everybody comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
