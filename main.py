6616095d07dff3af2c07c99424aaf7b9d038b7a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:14 +0000

    cost real government past smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
