c95454f2c5ebdc6b3677698690468564f98d44b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:48 +0000

    yourself flat which gate back

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
