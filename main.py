9490548d724c3d8016b34d9d21bd9e2a309f01cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:22:56 +0000

    relative or track allow confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
