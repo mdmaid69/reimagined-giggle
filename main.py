b124551846ed18590ccb845c25a96fa56757c443
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:42 +0000

    entrance judge broad confusing drawing

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
