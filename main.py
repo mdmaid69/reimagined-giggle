08f856fb35f7d76f6df896fa917ea6ab33d202b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:43 +0000

    card insist spray congratulations colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
