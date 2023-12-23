5f2efb234c6c7bcd3fcc90263bb3853bccb53fd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:54 +0000

    disappointed offend go advise act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
