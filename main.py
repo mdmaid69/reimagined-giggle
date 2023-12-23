5f74ce36803ffd3ae645b6948dfae46a6e73949e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:16 +0000

    wrong lost least familiar listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
