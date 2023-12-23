8ff133cab8733c333fb9069d6bde79ded54f8e59
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:26:09 +0000

    search heart about boring total

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def cube_number(x):
-        return x**3
+import math
+print(math.pi)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
