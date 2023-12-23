1a54c43f588baaf4e6c0c95c4b52024add4cb675
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:58:09 +0000

    steam certain out this eat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
