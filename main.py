e65bd005196d6ebdf78741bb605d251bdd86fbcb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:53 +0000

    narrow building false turnoff tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
