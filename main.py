36470b77b16ebf356b9791260dc488c843b5423d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:58 +0000

    pay behave lead division off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
