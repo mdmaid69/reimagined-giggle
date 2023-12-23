001880da00568b87e7ed82131911b94696e8f1b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:19 +0000

    belong play brother innocent of

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def reverse_list(lst):
+        return lst[::-1]
