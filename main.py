5c31745a58ea7c9464d46d61a87010bea9316299
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:05 +0000

    shallow boat handle last niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def remove_directory(path):
+        os.rmdir(path)
