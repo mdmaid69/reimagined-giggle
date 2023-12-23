599aa9539e65cbe80a6dbe774a97894064599cfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:10:29 +0000

    praise regular firm sing usual

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
