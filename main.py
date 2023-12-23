5dfdaeade8ac030aa52497cf307807b9c614465c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:00:22 +0000

    remember account read dear speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_rectangle(l, w):
+        return l * w
