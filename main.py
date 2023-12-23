0e99f5e289b96f7fb7316a7962bd59824ebd4868
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:55 +0000

    joke let moon Mm immediate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
