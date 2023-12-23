5c114cf3448810eb16d32448b30f2a17742de114
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:42 +0000

    board welcome phone illness fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def square_number(x):
-        return x**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
