73cf43e7bc84e6bb35cc5424ee743096e2c2dd88
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:46 +0000

    convincing even stiff black too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
