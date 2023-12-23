2abae7d8ab14381b0051d034419e95c844f16abf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:51:06 +0000

    match obvious work sport error

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
