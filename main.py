310f3f5adf311e54ecd73052b68580a774600378
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:01:33 +0000

    colour neat opponent sky loan

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
