deaa911a2a178bad4106fcd2ecc23630e3da35ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:00:26 +0000

    degree announce reduction price excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
