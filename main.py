06800650f6036c7ba7c2d673af48fce301cdd5b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:08 +0000

    difficulty move congratulations principle direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
