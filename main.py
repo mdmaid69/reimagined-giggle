54dedd047aa6d988366d4cfe82ab3afed10489bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:12 +0000

    pattern possible behind workupto by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
