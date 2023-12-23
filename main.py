708a4fe5a0ad5a125005925a1ace0aea589c5057
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:11 +0000

    battery attitude right love cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-print(math.pi)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
