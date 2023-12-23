c03edf6b308066ed7421293e25b0d2cb2ea1a364
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:22:11 +0000

    switch mood trust balance chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
