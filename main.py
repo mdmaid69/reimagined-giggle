eaf52d4f6ce6ef2a628bad19a6b28b5466cccad8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:52:35 +0000

    culture evidence exactly force children

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
