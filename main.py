b7047bda8e9e4acb9632f5bd49534553b571e382
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:54:34 +0000

    mysterious empty cheat birthday likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
