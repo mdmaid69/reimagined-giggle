d2a479f71db5f01988f3f4a1c82de84d16568341
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:26 +0000

    night truth anyone reliable someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
