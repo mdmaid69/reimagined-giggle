b7b126c359debbad29a22850a87b5975808d8cee
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:10 +0000

    clear either limit deaf should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
