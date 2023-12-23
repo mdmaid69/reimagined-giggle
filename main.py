b08fc8b57ebb36846068eb5ee32781bf59d03e0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:09:26 +0000

    afternoon line turn great answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+def calculate_work(force, distance):
+        return force * distance
