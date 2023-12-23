feed0b8588bbd333b555bd640309b17fe2aa12a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:16 +0000

    delicate cope meet keepaway lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
