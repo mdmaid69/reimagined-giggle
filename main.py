27de472ea634d44c80bab3b917013e71ae7ae081
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:12:00 +0000

    hide type trap into would

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_str(array):
+        return str(array)
