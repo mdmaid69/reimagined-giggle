41c4493df5aa00cea4c8b62c5435748aec10f1b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:37:10 +0000

    takeover law slip ready minimum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
