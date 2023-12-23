88bfee495b1869d028a31d8b4a4bbc51c3da1cd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:27 +0000

    moment club usually square yesterday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def change_working_directory(path):
+        os.chdir(path)
