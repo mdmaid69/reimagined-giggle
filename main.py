7f5cc5fdb8f75f20ab618067955875384960b2bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:22 +0000

    window scale crowd embarrassing best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def split_path(path):
+        return os.path.split(path)
