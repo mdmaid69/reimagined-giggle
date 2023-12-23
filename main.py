cea12ea4cec1913dd99c81a171d8d39f32f7eb88
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:25:49 +0000

    at content advanced search shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
