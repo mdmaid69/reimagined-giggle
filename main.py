69ef391c11f6fba79cb6b646314f907267a153fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:06 +0000

    wake agreement this rare steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
