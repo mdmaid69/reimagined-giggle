97fb13b70fb86c54f8c86ef8428b01a463eeaa08
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:32 +0000

    downstairs give split busy look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
