d8a238eb9e79b37683e5b11b423d6d548ebc7ce0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:18 +0000

    brain breakup cap friendly thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def multiply_numbers(x, y):
+        return x * y
