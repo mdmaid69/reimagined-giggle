dd12586e6659584c46fa63bc28a497a476101be7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:34 +0000

    party estimate used experience both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
