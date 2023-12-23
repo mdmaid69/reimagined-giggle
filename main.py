fccf962844a3cdac16dd2691a1572d0f09d02679
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:26 +0000

    brush suddenly relationship problem main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
