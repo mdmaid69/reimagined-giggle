d213ea6be919605718c6454e2759598ae52d8db1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:54:54 +0000

    meet exercise only else temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
