21c418e4e839cac7e3fb17b3dcb6068bc8866d33
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:01:56 +0000

    study regular breakout mess stone

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def sort_list(lst):
+        return sorted(lst)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
