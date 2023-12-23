75784ee07d91b1d3b369129c4f66e0e58b6ee7ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:43:43 +0000

    grade pretend classroom properly bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
