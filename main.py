8653295f4bdcd93e1cb0a293c99a6bf4adbecdd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:53:54 +0000

    operation branch rely over Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
