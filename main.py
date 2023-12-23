241b5454275924d1d3560b6abcc43dfef1227d52
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:14:42 +0000

    convenient wire float reduction few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
