91fee43be5796177d43ec051aca5c27c63c2daa7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:25 +0000

    foreign innocent stop funny fun

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def count_words(sentence):
+        return len(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
