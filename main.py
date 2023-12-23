5e018c8baf63acb6f41388900834c37f03647a01
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:29:05 +0000

    backwards sure otherwise immediately tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
