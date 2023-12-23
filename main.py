35fc56d904e85c1ba6908717264fcc4ed3a144a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:57 +0000

    power alone quick measure fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
