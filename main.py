3ab284a0f2e7bbd52c2afa88e7bc956f40b7513f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:39 +0000

    we everything over keepup deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
