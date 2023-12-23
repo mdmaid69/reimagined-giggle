c783fc827732732197fd1a99f58559991279e1b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:21 +0000

    goup breakout start distance material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import random
-def roll_die():
-        return random.randint(1, 6)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
