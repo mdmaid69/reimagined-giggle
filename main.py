c25430dabd5f56caa4a9f85ac4996c1c8710ef84
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:54 +0000

    content influence separate consider consider

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
