0ea5ebbaef083539d52a02efe9bd51703e3b9b50
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:20 +0000

    rich relative switch production happy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
