938c210cbea3422247bccc10f9d937857389fb13
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:32 +0000

    afford path ball pity height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import time
+print(time.time())
