d61bec71cfb52996055053a18ecb8dc009348a62
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:14 +0000

    whole clean surface considering interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
