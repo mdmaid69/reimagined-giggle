4c14c7ee7deac78f963281f572f759db6e28331a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:52:02 +0000

    situation top add relief classroom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
