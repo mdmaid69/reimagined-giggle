aa7e2293ff4b3c4fd3f1291ba5a4bee66f57ae78
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:18 +0000

    comment apart loud impression 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
