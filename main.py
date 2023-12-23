21a7a5e8b1f4b00176241f5324ad843cc6c515f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:14 +0000

    limit stretch this cut idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
