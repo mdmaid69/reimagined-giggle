4dc65532c5941580d9efd0af1ec00707e6cff77b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:40 +0000

    square pair upstairs chat later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
