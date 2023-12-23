d19e806aacc28020c512a824bfc71841c704aec2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:50 +0000

    language which health world work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
