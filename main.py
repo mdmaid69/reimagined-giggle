3bc873a3858e1f1d55bea267e261453c88a62279
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:53:10 +0000

    needle way anybody protection law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
