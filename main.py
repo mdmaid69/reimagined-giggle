a2d2f1fbeb016af1c5f38734d0e3a51482852c27
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:21:48 +0000

    see operation back careful time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
