cae9b666e977efbd980a3a40ae33be4534c3492e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:21 +0000

    false proud great soft stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
