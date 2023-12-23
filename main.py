522ed25f4fcaa45708f346222dabcfa2bd972f10
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:29 +0000

    store lose drink like project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
