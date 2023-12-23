f9013d6fcda089fc53558da2179d9a4487875925
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:37 +0000

    cheap home steer hope object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
