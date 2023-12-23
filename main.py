b12b3f2192448641035097c60900b3fdb5e22e4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:18 +0000

    give knowledge she this fork

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
