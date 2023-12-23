f9d993174bc03e694dd85bd1b588fa7147a429f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:58 +0000

    less keepup lower attach set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
