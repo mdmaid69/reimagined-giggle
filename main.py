d452c425895fbd84a9feb63751c0fc335bbd0d99
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:51 +0000

    everyone science Nn relation equipment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
