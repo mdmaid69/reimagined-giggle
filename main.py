ac3d981b823b4fd7fd76194c1b2a90d27ea6dfda
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:12:10 +0000

    obvious further strict minor phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
