76e2fd3305fbce48acd7b29a5a5bd8793afad89e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:00 +0000

    breakout heavy abroad motion science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
