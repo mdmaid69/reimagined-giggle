944f98e4eb67b8c6df1097f58b26abc0282fffcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:53 +0000

    grow avoid better safe trust

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
