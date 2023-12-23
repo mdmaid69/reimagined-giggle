d681d8041ad929dc21e6f6d5c8438e8105a3046c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:14:01 +0000

    door characteristic constantly approval only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
