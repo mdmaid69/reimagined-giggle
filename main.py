4c627244bf22fd44acb6c5d79a2a22d00114529c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:17 +0000

    deliberately temper makeinto climb along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
