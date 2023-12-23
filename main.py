6da7898494c79f3ab783ad9c433518fe4ea6bea3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:20 +0000

    touch immediately squash discussion me

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 numbers = [1, 2, 3, 4, 5]
 print("Sum:", sum(numbers))
-import re
-print(re.match("h.*o", "hello world"))
