71b3cec1ca7490b331e22907454c7b429905d94b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:48 +0000

    badly company classroom sort scrape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import re
+print(re.match("h.*o", "hello world"))
+def find_max(numbers):
+        return max(numbers)
