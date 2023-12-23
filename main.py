51b81b4db65dc88f5f0d7c5b5e7483ca832d697b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:12 +0000

    praise Bb stick education next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
