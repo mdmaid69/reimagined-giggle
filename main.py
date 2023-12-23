c285b433449a5916f4934b2b63bd3627364b5fc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:04 +0000

    sore declaration of platform common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
