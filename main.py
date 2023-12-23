3a168aaa3b8f1353cc05f33ae5b1a6ef6862297a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:45 +0000

    telephone estimate blame base lead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
