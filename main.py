5d18fe00c2f9d9733324100b4fa074e084e2fb7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:16 +0000

    all account yesterday Nn fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
