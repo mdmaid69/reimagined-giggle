897c6f4712f9ca02debd959bd61d03b65e9cc537
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:26 +0000

    send aim ticket near maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
