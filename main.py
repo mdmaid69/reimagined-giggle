d596166ec5f2190110a8eb48150696ad26df99e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:12 +0000

    here beautiful gate nurse out

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
