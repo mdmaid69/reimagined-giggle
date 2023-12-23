d17315c54414a3f4117c3888a7e78b6801865ffc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:36:54 +0000

    today demonstrate curious within likely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
