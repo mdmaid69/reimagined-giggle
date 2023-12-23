fb93b162a22c0f729b33bee9f3a91d722c482882
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:02:24 +0000

    well record container example getback

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
