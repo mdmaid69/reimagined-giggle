447b8d5e74bc79ceab7ffced3fbc2640b4b3b6b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:25:08 +0000

    air Gg criticism happy spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
