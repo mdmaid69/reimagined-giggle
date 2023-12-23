a0c39e20a0200d16eee083d38ad5d8176e100b06
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:55 +0000

    trip wild breakinto fit burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
