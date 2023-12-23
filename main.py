6a7207b074cfc8c64e9baf4d363e715c10b09010
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:03 +0000

    conversation wild old song basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
