942bd53ac58764ab25c6cf07a6f4a51d90547e0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:38 +0000

    slice stone takeout before extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
