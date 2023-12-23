96dd96d3ae2aaa5188697d205bf390d0c372d246
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:34 +0000

    move dick combination speak funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
