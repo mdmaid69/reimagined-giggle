c0c561c02d293a6875d51f0808dc32edb7723bd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:59:53 +0000

    word work rise aim attractive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-import collections
-def create_queue():
-        return collections.deque()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
