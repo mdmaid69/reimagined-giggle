0de334b730d70da4553080c6609915b5b3799cf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:07 +0000

    mixture some clean getaway mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_repr(array):
+        return repr(array)
