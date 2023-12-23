be29649e2e801af9bfdd921cc9f921493f1ac456
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:10:29 +0000

    married winter immediate disagree stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
