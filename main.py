e6e22bde3613361d46f4771e2259b2087eb1976f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:27 +0000

    down breakin hit thing 566end

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
