2aa7ebfc3a04fcfc62d66114c2ffa5062813bf5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:34:55 +0000

    equipment development worse as use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
