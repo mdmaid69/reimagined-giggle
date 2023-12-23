258ad9e577b3bec79d58a3d7343d3b7f6682c16e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:20 +0000

    meanwhile instant worry temper story

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+print(os.getcwd())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
