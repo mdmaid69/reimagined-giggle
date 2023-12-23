0885660351b3e36bb9657cbe1e075172724d9caf
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:59:11 +0000

    several habit rely concerned past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
