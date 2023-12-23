0d6985ad7097067f4c785dde5544e26a9b2e829f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:39 +0000

    bother market doubt age beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_max(numbers):
+        return max(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
