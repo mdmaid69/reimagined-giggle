b230f67cd351f322e5a36160f450d861bbf62f63
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:54 +0000

    machine boil worried ability wash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
