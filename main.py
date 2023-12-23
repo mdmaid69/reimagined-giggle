bd9b31064afa154f82f04daaea402e128a482233
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:58 +0000

    dark control shrink handle fashion

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cosine(x):
+        return math.cos(x)
