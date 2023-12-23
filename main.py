88b0fca6c03b2f97f4968478de82f31510ff8fcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:29 +0000

    bar of whom both disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
