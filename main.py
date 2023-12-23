d477677363bdf0ad74207b1fe7a1f25937323c85
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:48 +0000

    for tool slight damage tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
