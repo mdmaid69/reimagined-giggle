3fab2a427fd541548258f4c3b29ef4d5d4e01bd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:24 +0000

    level slight free plate trouble

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def remove_duplicates(lst):
+        return list(set(lst))
