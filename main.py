6a5dfe15443c4f0007ddfb86143720da621d315c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:33 +0000

    maybe shape lock level complain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
