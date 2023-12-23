8aaeb9638c9b24fb78a67fd3c53d95f8227eb4c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:23:31 +0000

    environment effort thank this window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
