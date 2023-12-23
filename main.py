cbd1ff091254b096acbb5c3802df133b158e56fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:56 +0000

    method touch drawing between wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
