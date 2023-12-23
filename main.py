b5167ecd6f2e6b4b0074e09248dcc7b69c23c44d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:25 +0000

    tragedy like delicate base stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
