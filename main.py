08d485c83dee8a49c16193d06f3f7a3844959952
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:01 +0000

    replace insist flavour Cc emergency

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
