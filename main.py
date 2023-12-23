49087090e6f18fd0b6c4e768ec30b4b8c157014f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:43 +0000

    wide where proper steady trouble

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
