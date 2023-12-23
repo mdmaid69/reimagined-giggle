495404aa36f1af36f63cd897d1f97d8a86849d1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:33 +0000

    medium people best slip drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
