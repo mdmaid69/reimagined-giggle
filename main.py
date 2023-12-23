9abf2f8368e23fd240f87b77b315e4d417250ccb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:34:48 +0000

    complicated hope store hold cause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def square_number(x):
+        return x**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
