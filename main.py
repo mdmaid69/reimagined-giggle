b6ec3bc176888b38953d5aa4f4dfd18db796e236
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:21:22 +0000

    together 569enjoy slowly luck waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
