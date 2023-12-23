8955c7a0f536b987ce59096a6f9320e7119427f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:33 +0000

    ever fact quarter advice picture

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-for i in range(5):
-        print(i)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
