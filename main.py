63281017ce55ef4675da35bb5ba1cb25ee291aa1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:49 +0000

    bet refuse surface future door

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
