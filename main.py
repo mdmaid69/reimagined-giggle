19f5b6aea1109314d4c1e68307badb69c41ccae9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:10 +0000

    the behaviour sit post splash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
