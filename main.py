f14fd45a54698750697b07f1c93f46850ce0952e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:32:43 +0000

    guilty among decide front train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
