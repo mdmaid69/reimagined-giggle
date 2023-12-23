72900de3e5e103c1a6c796255e606991b574cc1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:28:16 +0000

    official impossible copy include extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
