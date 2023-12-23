25303616539f5fd1b1b4414c60a47c0e4f7ea9fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:20 +0000

    on apologizeapologise frequent flood post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def add_numbers(x, y):
+        return x + y
