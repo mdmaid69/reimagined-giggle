cfb7fba7e094fc8e895accf3e4e73a7c77f853ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:47 +0000

    win factory deal instead deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
