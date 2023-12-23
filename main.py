73f22d425cd8ad218dd70a11abb8004acbfb7bc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:02 +0000

    clearly reason tie set long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_pressure(force, area):
+        return force / area
