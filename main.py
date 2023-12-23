9608aed8d57ae7e5c9efaf817000693e0711c53d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:38 +0000

    roof bringforward fairly minimum cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
