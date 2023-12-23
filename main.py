2a8ad8fa514d65e65df6bcc988d83835d6025771
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:27 +0000

    conversation nice treat wherever turnoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_index(array, item):
+        return array.index(item)
