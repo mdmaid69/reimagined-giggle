5f3d0dfbbf0f80c22201edef175b01ffc7bc3269
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:57:01 +0000

    search moment target tired ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
