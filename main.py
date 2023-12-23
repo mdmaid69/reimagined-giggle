a4b61606d916185bb7969ba03e369a95326f7343
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:43:56 +0000

    advise course comefrom turnback correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
