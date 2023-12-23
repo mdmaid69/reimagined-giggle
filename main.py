ad6b3da9607e19c74cac4c3f2abc2a2f5b2bb70e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:14:58 +0000

    attach already share complicated right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
