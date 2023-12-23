3fc0c66656e913c496507793d05b94d13377f81d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:10:37 +0000

    572enter loan intend performance mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
