0de64516fbde5bd3c8a7a9be6f12894af6aee34f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:23 +0000

    autumn display coffee wash address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
