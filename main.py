e8b3d5672729b7f7e3a68cb954b95091a220cb62
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:48:51 +0000

    earn care major Uu place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
