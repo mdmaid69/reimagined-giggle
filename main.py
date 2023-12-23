57847824c28643aa7dc9f4771c86ee3e42365d13
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:03:14 +0000

    their common fold negative exam

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+print(time.time())
