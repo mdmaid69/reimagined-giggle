0c1d0ce9cfb04107c7a38285b04ed23093fa7898
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:10 +0000

    may unless happen wear prize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
