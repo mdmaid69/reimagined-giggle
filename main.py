53e32430bc350190f29273b49cc65154820396aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:29 +0000

    perform cum flow powerful scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
