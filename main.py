a390e797c15c3d3a1f9e4345f7985aa201aa3163
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:21:32 +0000

    wipe late zero regard key

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
+  def convert_to_hex(n):
+        return hex(n)
