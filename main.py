2978926d2938640116e0770d00b8bc4b0fada238
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:13:40 +0000

    not answer Ii up stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import collections
+def create_stack():
+        return collections.deque()
