b3329f85c13ecda7e7d0cec16b45785280161f41
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:39 +0000

    myself damage could often immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
