a30a6c8a23565e3ff99703469fc8b113bc20e527
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:03 +0000

    believe will name story pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
