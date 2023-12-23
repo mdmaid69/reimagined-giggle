35984c94057b11250327e8db0781c72518967350
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:56 +0000

    slowly general however closed tread

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tensorflow as tf
-print(tf.__version__)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
