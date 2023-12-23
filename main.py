6b41224f6daebf78ae1d3a86017f8ed23f89ad2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:32 +0000

    pin friendly loose concentrate about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
