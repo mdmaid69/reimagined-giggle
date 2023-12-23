5ac81c8655403d6e34719ce421c4400f700729ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:43 +0000

    Cc actually clear giveup organize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
