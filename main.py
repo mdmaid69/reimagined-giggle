915f7a98e7b573f4f6d9f0e9add210776bd838f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:40:46 +0000

    except trip undo Yy control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Reversed:", text[::-1])
