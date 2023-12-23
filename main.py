82ca0b95da46c67c6ecaf92b4ae03d38c58b652a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:15 +0000

    method shoulder approve innocent blow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
