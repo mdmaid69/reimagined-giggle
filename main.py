87fc1c7c5b6a8e0037d2ac7af1fc016956b718c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:52:43 +0000

    hand beginning flow cold really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_list(array):
-        return list(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def get_array_as_int(array):
+        return int(array[0])
