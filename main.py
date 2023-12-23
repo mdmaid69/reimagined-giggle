049d00c54dd47f80fc0b88c3921ba71ec7554c06
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:05:35 +0000

    whole certain chat regularly turn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def subtract_numbers(x, y):
+        return x - y
