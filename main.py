fcda8acfb797ab96a7f086b275e89313b451f9fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:39 +0000

    problem separate regular full do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
