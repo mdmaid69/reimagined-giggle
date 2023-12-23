8753a1f1ed223c4a7df334e02ac9a0d46ded425c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:44 +0000

    estimate size takeoff swimming ahead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
