a966269caf69a364c690f27cb933152cb777583c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:30 +0000

    bus front record Bb child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
