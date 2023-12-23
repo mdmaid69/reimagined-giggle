59ab35b51e1a3c9d6c8b5e1db7aa6a14d9116f78
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:03 +0000

    goaway whether grand wing single

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
