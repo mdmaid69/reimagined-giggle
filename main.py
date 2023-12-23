fc196007caa1e8ecd3002e5983025aae9c86aba2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:47 +0000

    basis compete report between soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
