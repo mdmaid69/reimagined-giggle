e20841555c40cca659c1f42ee68e29968bb2d790
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:50 +0000

    system relax together pretty shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+from collections import Counter
+print(Counter("hello world"))
+import os
+def remove_directory(path):
+        os.rmdir(path)
