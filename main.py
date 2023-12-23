3fa937b4a421667e3d13e41fba8327f918455271
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:54:42 +0000

    address certain nothing visit damage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
