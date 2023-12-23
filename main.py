2dcf509621f09f3324a86e073fcca1fbddbb454d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:12:55 +0000

    exactly look too open force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
