e32a23b360885d177a3803d8ee89559ee06ac6aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:54 +0000

    mistake wet section trip meet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def count_elements(lst):
-        return len(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_typecode(array):
+        return array.typecode
