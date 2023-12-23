759c6d3e00445db59c2cbefc0a81fb5c38d48f20
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:14 +0000

    frame relax breakdown maintain top

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def convert_to_hex(n):
-        return hex(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def cube_number(x):
+        return x**3
