1b7bb1c7101bbe6a1a1f1d969e4d4b2aa6ca23f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:59 +0000

    sweet rise opinion nice friendly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
