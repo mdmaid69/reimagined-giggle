b35964715a08acff37a5ddd765a5593ca8b69af4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:15 +0000

    matter light lose lot perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
