88f47aecce7a88fea37361e8a64ea511ae3c478d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:52 +0000

    brave exercise floor strength shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
