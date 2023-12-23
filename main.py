ff73f424db067a76ddba6fe1dba3ad34385f9aef
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:07 +0000

    performance previous naturally pocket rush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def roll_die():
+        return random.randint(1, 6)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
