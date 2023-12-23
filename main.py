ff58d3e3602c7a7331c29c06e0aa925eaa069f8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:19 +0000

    watch of hide basically hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
