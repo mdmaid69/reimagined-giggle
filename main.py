f288f059f8c6621b793cf0a5d235d8e2668400bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:16 +0000

    tone hide car fresh long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
