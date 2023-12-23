18bab1281d023307eb8c930865ba60d38fa7f609
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:51 +0000

    drink information save consist likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
