03fb269cd44db608fa6e1541ec43d6dbeb38e288
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:38 +0000

    hole help vanish belong copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def split_path(path):
+        return os.path.split(path)
