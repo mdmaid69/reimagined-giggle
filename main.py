09505227f7c6df3cae8d23d5ec8f106a0e8a004a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:15 +0000

    authority turnon fail advantage Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
