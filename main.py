84916109176cc4cfb653ef2cbf9548f0ed3bb2de
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:15 +0000

    anything model earn Jj spread

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def greet(name):
+        print(f"Hello, {name}!")
