b7c8ad19ccc53fbb8496087e87241219c3d8b8a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:01 +0000

    arrive ordinary under characteristic yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
