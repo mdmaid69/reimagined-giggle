c0efd3a2297f8508b0609fe880d809fb0dc15592
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:31 +0000

    count trip press skirt onto

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
