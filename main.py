3f0c3bd0bc81f45ec8485acc936e7b54d1a2f04c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:42 +0000

    advise smell worry water rich

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
