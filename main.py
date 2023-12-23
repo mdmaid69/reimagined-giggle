64fff3ca6f5588dd2c0d49937147fd8a2d260d8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:55 +0000

    contents offer minimum ground deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
