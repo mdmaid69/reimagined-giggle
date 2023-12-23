e91938f08132ff32f3a5e9c514288752c41e92f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:14 +0000

    recover basic dead tough onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
