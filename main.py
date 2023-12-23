1936141d9a7a4bc265a82d6ccd75785845edf1ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:48 +0000

    become block anywayanyhow left further

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
