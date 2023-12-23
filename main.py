545bc4e528a6a39f7fd7af4b93849352c0435aee
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:29:55 +0000

    interested feed pleasure else ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
