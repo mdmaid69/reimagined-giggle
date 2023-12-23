1379f5e119237a3cf529b7915114a8760b73d855
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:25 +0000

    point shallow actually legal port

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
