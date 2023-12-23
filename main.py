34c9b718cc0ad0c7a0ff437236f3e36cf0dfbdeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:29:02 +0000

    unlikely attach they advantage quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
