9ee70780f93fc82cada6679cc13fea871e3e82bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:39 +0000

    difficult today comment ilovedicks respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
