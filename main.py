4ca48b722f5f26570bc8e821433057fef8f0ca8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:23 +0000

    reserve ask state yet arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
