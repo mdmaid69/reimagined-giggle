923153a5f135cd26a2412113b04ee9b3553fa2a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:22:56 +0000

    right flat behind as pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
