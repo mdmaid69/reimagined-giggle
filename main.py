e33b6a750a5ca11ca8a987d00f3014fed9e6991b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:34 +0000

    colour they anybody with form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
