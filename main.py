a3d5945e04709d75fca2a8eea69c4ffca581cdbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:09:36 +0000

    expensive effect sport invitation taste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
