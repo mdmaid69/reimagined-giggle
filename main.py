a7fd8d818ffe323904f029fa8ea59c5af682e3bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:50 +0000

    meet such absolutely serve pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
