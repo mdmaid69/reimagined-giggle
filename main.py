9c01ad6af401c6e64bc8e99167fe30b20c5db8ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:34 +0000

    often us mainly genuine university

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
