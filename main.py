c70ffe43c7174b6ea75317ce2fbe885155e7f461
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:09 +0000

    compete rush getoff lookup practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
