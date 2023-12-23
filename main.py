e2b712ee2d79f4675829f4b4e39ae0bcc926edda
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:22 +0000

    team shopping lock object also

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
