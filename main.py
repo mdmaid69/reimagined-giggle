db4a60cb43db91d12bbf485a44635c46b37ea76c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:55 +0000

    evening female exam over point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
