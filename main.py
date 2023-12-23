51f4b42095aaf56be576cbbf4752688e52360902
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:58 +0000

    hide corner tonight environment frequent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
