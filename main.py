f3c2af1a091f9e08ed84b79ae6419e7579dc74c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:14:42 +0000

    leave base off juice object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_list_from_array(array):
+        return array.tolist()
