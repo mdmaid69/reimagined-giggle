a4e2ce04af14455f84788b1441da363113b4f493
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:12 +0000

    own sleep plan half guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
