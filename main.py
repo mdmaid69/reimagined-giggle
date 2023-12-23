5fce45036b5995a57facdb2bce2dd77438195cb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:34 +0000

    rest proper cap sign guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
