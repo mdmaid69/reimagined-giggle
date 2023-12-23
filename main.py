4a6119a8ac46ff66c6b837e083778eddd5b6cdcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:57 +0000

    centre represent arrangement dance direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
