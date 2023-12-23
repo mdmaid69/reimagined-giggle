d71d4958947b777a4162c25c00c3924c47f9437d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:44 +0000

    chin by flight medium actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
