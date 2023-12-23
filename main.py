87f3ef9ea01e7490febdd998be4b7e3781bbfee3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:03 +0000

    throughout discussion succeed expensive behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
