c2c4597e02df5e651674aa2457c497fca21deff7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:50 +0000

    recover recommend actual exchange attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+def find_min(lst):
+        return min(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
