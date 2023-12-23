d727ef5849d466030f1817ee8d643611b98f39ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:46 +0000

    cause even chief dress chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
