f3c52d87c3d1aaba80ab2e5d456696ad6de97ac0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:12:40 +0000

    boat how any regret wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import collections
+def create_queue():
+        return collections.deque()
