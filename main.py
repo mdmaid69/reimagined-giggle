4450b0ae9413bba3dc31a52d7c1abbe71fb4a794
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:02 +0000

    comeback cruel workout calmly flavour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
