aace0c8a998fd94b67586f1dc23c539683186bb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:11:34 +0000

    worse wrap natural disappointed greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
