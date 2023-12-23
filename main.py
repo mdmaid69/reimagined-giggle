3071dbb02533c58c6e2c1ee85fa0e346067c83a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:07:52 +0000

    fix too fierce run exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_current_directory():
+        return os.getcwd()
