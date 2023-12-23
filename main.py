20723cbcb030ca6df6a4ea61488a5fda81df9e01
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:50 +0000

    shape rough combine anything Oo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def subtract_numbers(x, y):
-        return x - y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
