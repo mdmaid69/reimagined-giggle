ca2c41af43459383f5ba8d3ac9980db854ff1c5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:51 +0000

    fond deliver always afterwards season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
