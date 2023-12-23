751f2f9d25eacaafc6eea9c085dcd7cc388285d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:18 +0000

    measure relative dance yesterday home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
