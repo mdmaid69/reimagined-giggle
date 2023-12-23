554d50f6217d9ba8e26dcf26eefc9bbe35bf75b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:21 +0000

    related drink occasion typical clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
