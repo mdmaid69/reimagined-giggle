372cec04b936ddc6d18350c4ec5a2f0776bd0604
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:53 +0000

    workon turnon maybe plan basically

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
