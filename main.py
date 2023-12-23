6555bc30b68272961e24669f647fd80b310dd714
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:54 +0000

    high disappointed roof shelter kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
