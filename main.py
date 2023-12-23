daee007c5eacceecd2a6d5e2b2a34a5283effd30
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:56 +0000

    comeout announce prize lesson best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_even(n):
-        return n % 2 == 0
+  def split_path(path):
+        return os.path.split(path)
