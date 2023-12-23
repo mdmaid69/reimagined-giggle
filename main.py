429daaa4092ed3318ad1ec07f908014624cb73cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:58 +0000

    declaration survive afraid importance offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
