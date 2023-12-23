9389f579f8742b193df2658dbabed989d158abea
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:37 +0000

    comeover accept turnout she effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def subtract_numbers(x, y):
+        return x - y
