7a3e5317b7c1d0845c25b4931bfb044b8d03b92d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:52 +0000

    attempt announce promise solve getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def subtract_numbers(x, y):
+        return x - y
