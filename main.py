327ee335d685eea2a0d7d5675c7ac18954f5a392
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:21 +0000

    miss guess patience accident important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_hex(n):
+        return hex(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
