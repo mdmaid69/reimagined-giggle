ef4929d4d87fd8e8aec9263c45e224774bd74216
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:10 +0000

    deal comment dream interested have

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
