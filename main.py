67eddcd7759b9a323dac16ba07b787f2b0a6af36
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:26:27 +0000

    own mass disappointed none chief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
