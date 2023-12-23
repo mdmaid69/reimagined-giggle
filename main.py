7719116899a35b269b747eb3fab845dbeae65c3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:40:13 +0000

    direct fortunately else swimming admit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
