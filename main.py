cbc1bfb00d607a926b31d3cb607eb340b2a4b2f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:39:04 +0000

    bury afternoon contain clear pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_min(numbers):
-        return min(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
