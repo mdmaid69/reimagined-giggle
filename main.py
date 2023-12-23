873dbe7134310352ba961e2ae513a0cb06c34151
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:41 +0000

    drive protection vanish only vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
