8afc263c8c81d8ff06cc3cfac95bbf50b0ecd650
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:47 +0000

    spare shout lost least only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
