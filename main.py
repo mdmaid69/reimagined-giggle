a256a03a019819ef1c344a270d53cbb3c122e94c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:26:33 +0000

    ignore there 566end present pile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
