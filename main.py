d5c66e06ffc0dfa8f94ca930a9affa7a13c6f5cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:33:51 +0000

    flood disagree possible mistake shame

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
