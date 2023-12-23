e252a4cbac29fb198195efe7075691b3ba6d431f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:05 +0000

    admire section write skirt ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-print(os.getcwd())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
