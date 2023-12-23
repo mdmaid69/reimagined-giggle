a735abfb04413f31701b497edc50dcdc7814a1aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:26 +0000

    underneath unpleasant satisfactory mess pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
