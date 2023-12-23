fe83841d1c3d6d439e0448bd1648b9486bc4ef76
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:31 +0000

    doubt ashamed only appear duty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  def is_odd(n):
-        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
