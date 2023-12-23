b37a4fbeae507687ef3f5b1e0c522406ae669a34
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:50 +0000

    along doubt believe wire study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
