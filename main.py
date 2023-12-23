48fcf80531f6dc8a2bbe9e94c115bf2ae77f575c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:47 +0000

    meal arrive by use speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
