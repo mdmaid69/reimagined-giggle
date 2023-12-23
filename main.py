a70a6d1a4159c5af706e0a234561ba26e72861f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:33 +0000

    become convincing everywhere hot godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
