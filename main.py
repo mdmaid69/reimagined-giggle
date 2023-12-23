a451e28683b9b3197b138751e2238f10390a31a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:31 +0000

    progress fail culture no outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def reverse_string(s):
+        return s[::-1]
