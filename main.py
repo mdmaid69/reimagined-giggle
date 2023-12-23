555da33082f6473b2f2964da83251501d2824284
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:29 +0000

    today on brain past attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
