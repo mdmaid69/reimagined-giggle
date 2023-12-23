4bcb3696592b73e64abf6d8a1a4c998ccb3bd69f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:17 +0000

    aim inquire mysterious cause reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
