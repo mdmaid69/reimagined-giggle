87df434040cd736787be1b888daad8a9f45b39e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:24 +0000

    neat complete eat disturbing tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
