ee02acff1ed2203930cf1bcbdfb914512a93dba6
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:20 +0000

    closed close if embarrassing tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
+  def remove_duplicates(lst):
+        return list(set(lst))
