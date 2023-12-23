cd056a2a50b8162364263d7936134ab4c121e829
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:18 +0000

    responsible tear reaction forever praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
