615f3ebd269f1432d986e434766a2f81a7175dcb
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:28 +0000

    nurse absolutely belong authority move

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
