506d15548766c7b59dd78f1b8900eb1fe6490eed
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:54 +0000

    education object refer rain but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
