24021ab7bd641194c9e8964567d9928d9d5447cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:07 +0000

    murder through wall path holdup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
