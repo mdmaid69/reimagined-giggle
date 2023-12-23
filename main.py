86a0f3490ca1c57e944ca6ead6c87369cfc96437
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:34 +0000

    where port glass exchange constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
