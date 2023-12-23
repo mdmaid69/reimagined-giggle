49ad3d23e1e68dd89e48a1ee605d96c79cdf03f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:55:12 +0000

    narrow general cry tune train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def divide_numbers(x, y):
-        return x / y
+def check_if_array_contains_item(array, item):
+        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
