4b63bd9e947d2cb249dbc65968a952ca02552a4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:27 +0000

    walk track amount description show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def cube_number(x):
+        return x**3
