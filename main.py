e483ad4e803e27b0558715d0cec65e4c67ca4698
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:13:29 +0000

    pressure generally past but file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
