8011355b56c7d8060699cd9e91c40f9f05b1a824
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:29 +0000

    Qq business run receive no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
