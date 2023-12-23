4a361d7eef840143be174d7b4db42844eeeb83c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:55:21 +0000

    seem corner special murder bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
