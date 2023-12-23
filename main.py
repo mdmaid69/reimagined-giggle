4b0d89638efd126ff489a4f8c85e7ac798ddebc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:36 +0000

    turnaway shallow settle tap breakout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
