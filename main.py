8680574cf735940611a5e2bc5c6324e4fb6a8203
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:57 +0000

    bury keep minority into back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def cube_number(x):
+        return x**3
