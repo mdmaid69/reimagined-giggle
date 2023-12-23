4738615809e28365c8ae2eb92a7a9337d7dbe4da
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:16 +0000

    inquire child goodbye exchange chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
