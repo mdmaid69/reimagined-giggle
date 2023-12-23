bfca552f09f3ab2fbfddb6300954263ed0ec8123
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:40 +0000

    thorough sound anywhere develop move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
