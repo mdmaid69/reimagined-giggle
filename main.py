12f330cfb99021f519ca4531196a0217623d67d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:41:23 +0000

    strict figure I lay negative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
