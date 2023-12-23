31c656e7d037dc952c0d6cdb92e020b705564ecc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:53 +0000

    takeoff children friend knock confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
