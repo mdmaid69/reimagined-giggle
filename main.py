a4cecb8ff28e73f452db3bf4a55b349164f962ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:13 +0000

    pain to impressed politics underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
