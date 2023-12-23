6db4d97655c0dbba1cd337f66a6fccb6464fd101
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:30:28 +0000

    left if reasonable bringback obvious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
