2b371f6997a7d9716ccab3671aa3c1aeedf1c806
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:55 +0000

    before clear heat unless something

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
