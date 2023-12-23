e352a8b29a7bb4fc49544c053679862cf5c12d8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:28 +0000

    meeting callback impossible rain off

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+def calculate_work(force, distance):
+        return force * distance
