06884c906251492ce4b37a13031c1594ebc628d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:38 +0000

    prevent especially guilty seem afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
