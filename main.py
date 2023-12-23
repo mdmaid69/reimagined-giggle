24b5e49b73571d4623dd97ffd5a1d911c6ec439d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:55:15 +0000

    fine terrible ball lookafter experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
