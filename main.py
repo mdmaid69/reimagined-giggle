b307aad6f7ab3fcf01390389b92b26bdbffbc7e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:22 +0000

    motion giveaway company desperate blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
