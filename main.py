231a89401e72c5a77fa13aebf629527d3b978874
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:33:54 +0000

    pay price worth only always

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
