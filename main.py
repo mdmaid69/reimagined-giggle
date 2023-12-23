e355dcb7416c6c5fe259ac4fbc3c04d7101d2049
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:29 +0000

    love Dd light work mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
