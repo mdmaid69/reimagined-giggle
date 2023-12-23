4ea08e1a0f344207199d5d0274d30b071c162c2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:32 +0000

    tip regular disturbing cut pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
