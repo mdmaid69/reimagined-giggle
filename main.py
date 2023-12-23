f05bb9952994d7985301d5fb6696fef171681b2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:19 +0000

    big guess shape beyond substance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
