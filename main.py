ba4903fad8b1c5b7ad1f7e41f41f73260b817644
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:46 +0000

    somebody disappear brown label runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
