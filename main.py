9ae528aa031e37645bde65a6a2a2903396b167f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:01 +0000

    wheel rise fire takeout 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
