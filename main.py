96526f0b08412789e7fae722df76a0b7340017f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:06:07 +0000

    shut mix ignore flight university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def sort_list(lst):
+        return sorted(lst)
