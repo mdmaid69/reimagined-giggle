699760e72b2ba9e406370a674fd55a1584c71aeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:58 +0000

    warm expression free average heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print(list(itertools.permutations([1, 2, 3])))
