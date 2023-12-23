eee5ddca2d02f75525bc7435399f275b7bcc2a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:54 +0000

    dangerous effect fierce face that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
