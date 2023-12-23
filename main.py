40f07e9d386bbf60734193d031331fa01a1bc807
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:56 +0000

    may Ee movement alone truth

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
