c20f906e68151bff92f98901d2e49e08006f3358
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:17 +0000

    pot secret model only vote

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
