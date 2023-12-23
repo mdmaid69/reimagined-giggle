c49c7870f86e1df12cb8ea41c4f244a46832f734
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:51 +0000

    consider unpleasant reaction concern flight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
