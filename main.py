fe80a180b481326c4bce267ce24c27afedaf4cd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:09:08 +0000

    pay professional skill though she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def multiply_numbers(x, y):
+        return x * y
