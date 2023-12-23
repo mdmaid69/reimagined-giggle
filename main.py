f3a79e5cbe24717f776b9d00fd851843057a28d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:45:08 +0000

    suckme forgive replace protest sit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_user_string():
+        return collections.UserString()
