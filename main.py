25581d6b1d9853a1a07eec71444b152ae704a3c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:04 +0000

    at mixture impatient professional anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import random
-print(random.randint(0, 100))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
