1815f1495d7078fefa6a8a05c23776ca81969778
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:37 +0000

    burst vanish someone unkind set

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(x, y):
+        return x + y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
