d765037ed6c0c43f38e200b13bd91522c0780c8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:07 +0000

    instant sure light meanwhile occasional

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def subtract_numbers(x, y):
+        return x - y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
