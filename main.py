4a27691e6761aa21eede9c847170dcb53d69b02f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:21:23 +0000

    fetch list home ride with

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-for i in range(10): print(i)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
