229a1bc17a06bf87b40b7e94810f8aeb91f152c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:56 +0000

    invitation argument march open mother

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
