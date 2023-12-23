b3dc75e5ae681c1e93d45b583e68d978897c1eeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:58 +0000

    safety round charge match type

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import time
+print(time.time())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
