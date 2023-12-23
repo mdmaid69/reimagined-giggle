09f3f60b2f3199b1e6c50e7194e1729e16a0750b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:29:45 +0000

    takeback sort wash regard early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
