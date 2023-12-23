a9de7ddc25eb0b54f7042d703be7d237b82b7047
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:55 +0000

    direction authority Tt swallow present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
