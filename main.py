71e110b79f248c5f9854e25e3c2527febd707234
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:45 +0000

    attitude today explain pile harm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_area_rectangle(l, w):
+        return l * w
