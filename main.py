27bf50b06d5c94731363b577f63744b23f6968c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:46 +0000

    hard century check during century

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def count_elements(lst):
+        return len(lst)
