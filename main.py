2985acc23bfa9e45297da663018a496b6c061b33
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:29:08 +0000

    narrow interesting measure whichever equal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
