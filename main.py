9fa9399985f631491797322120295ccf3a2a0c67
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:32 +0000

    really strike good best condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_acceleration(speed, time):
+        return speed / time
