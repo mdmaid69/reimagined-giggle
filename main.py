15d1a4242a236cfb6032682818356b2cf04f8dde
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:47 +0000

    give goout strong related this

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_index(array, item):
+        return array.index(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
