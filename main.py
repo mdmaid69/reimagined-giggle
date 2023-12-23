8a78c9f366dfff0db20c6b16acda2d5635061393
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:11 +0000

    remark marry Rr book greeting

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def cube_number(x):
-        return x**3
+def sort_list(lst):
+        return sorted(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
