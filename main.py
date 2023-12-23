341a42c7ddb99d807002a1fd65a368e4019dddb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:28 +0000

    bank supply touch wherever embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
