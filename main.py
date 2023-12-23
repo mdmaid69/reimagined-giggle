d6abd2e3c6f2a4b2d86b8b55a8eb8c4504daae48
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:37 +0000

    moral work view listen fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
