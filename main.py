b33a85c4df972d9467d1b69952b33181cc23470f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:15:55 +0000

    involve once squeeze hurt round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
