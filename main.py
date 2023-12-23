e76f6e6790f557870754c5b46683a3b74bbcf228
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:38 +0000

    include firm how surprised unusual

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
