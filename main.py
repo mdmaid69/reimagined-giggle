b36d8494c23afd031616126c498bb39f298cf011
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:54:45 +0000

    shame until communication whistle impression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
