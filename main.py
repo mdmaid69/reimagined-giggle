f95ffd2ccc71171b00bcd8caad715f5cdb978a63
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:30 +0000

    persuade scheme letter wall dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
