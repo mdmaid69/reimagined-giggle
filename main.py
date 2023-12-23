067e6764a8f33fff33b6118e7adc444dcc958f69
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:25 +0000

    dish passenger least major succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
