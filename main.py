60ba75d1c4e28f86ace203a24f22ba3ddba0ca95
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:11 +0000

    weigh present prefer neither practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
