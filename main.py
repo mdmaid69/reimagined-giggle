ac661fb23607d5b2bd49a38c49b1bac7d57ae067
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:42 +0000

    border difference after you chance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
