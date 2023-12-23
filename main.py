ede2bb340db25f3190fb87a11a65279f75fa3f00
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:29 +0000

    intelligent effect ever suggestion memory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
