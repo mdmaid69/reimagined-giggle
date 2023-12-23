c98417e6a5d75b16d505728cf197f76650785df4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:26 +0000

    large very human need claim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def is_even(n):
+        return n % 2 == 0
