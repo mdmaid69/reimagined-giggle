41c4ff082e47b3bbb16876ed4f89458a3f79590d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:54 +0000

    government them search break try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
