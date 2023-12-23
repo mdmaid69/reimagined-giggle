4ea412057966b45af9b848fafd861f68f4f4d312
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:53:20 +0000

    fun class have disturb comeoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
