7aaece2009708f7f72d236c7f09b428e50b42300
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:16 +0000

    use draw game have private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
