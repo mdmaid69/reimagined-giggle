73780b7c0f5befa35db0486bc84a576d40b12c0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:59:50 +0000

    laugh adjective contents head sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
