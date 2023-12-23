e2598dc21884c79417a4dc497d9c3441fba659d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:28 +0000

    565end takeback neither achievement herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
