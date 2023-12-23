47f5c47de1eb67b7092606b63e964ed60f6ef5fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:17 +0000

    how principle positive surprised worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
+  def is_odd(n):
+        return n % 2 != 0
