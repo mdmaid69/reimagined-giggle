fe74479ba1925f5483316b0afc3b0bb2691a3277
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:30 +0000

    copy grateful abroad glass collect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Characters:", len(text))
