c05d01f0923b13756f45ee9be94a7f3440520510
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:51 +0000

    march passage save ignore lesson

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
