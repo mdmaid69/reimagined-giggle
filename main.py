d10671932b71ac74652c36cf9e2e9625527eb912
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:36 +0000

    hold clear guard wind spray

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import platform
-def get_python_version():
-        return platform.python_version()
+print([x**2 for x in range(10)])
+import array
+def get_array_as_list(array):
+        return list(array)
