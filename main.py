6db79de34e0580340bb2e3de5dd34e0e7970fea3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:13 +0000

    wild possibly factory scrape sing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import os
-print(os.getcwd())
+def change_working_directory(path):
+        os.chdir(path)
