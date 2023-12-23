1678f40a97ad9474aacc58d114c97199d21d8fbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:45 +0000

    boat science but interview tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
