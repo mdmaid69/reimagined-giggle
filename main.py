a1ea0a2a1871112d13f4d5f34e42e894498861bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:25 +0000

    healthy unpleasant actual lost very

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-print(math.pi)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_circle(r):
+        return 3.14 * r**2
