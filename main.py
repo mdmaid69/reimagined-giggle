ef21f87c42d97f12734d91a03ee5d7942e1024f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:22 +0000

    defend fortunate whatever pleasure brave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
