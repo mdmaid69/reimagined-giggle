f19c0daba5ce1daa5e50b7aa415490c0fc929606
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:30 +0000

    turn fine Ss learn release

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
