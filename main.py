9caaa3da79ea559370e85298216aa241ac32b60c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:06:46 +0000

    comeround sail sport violent frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
