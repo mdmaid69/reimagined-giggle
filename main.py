dbd4a4360955ce9c60953dec1b0ec801c9587a1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:51 +0000

    result convenient influence prize soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
