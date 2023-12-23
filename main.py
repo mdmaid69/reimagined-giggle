2b3175cceffc2cd890a021266fee8faae9efc2c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:03 +0000

    sometimes wash borrow stir become

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
