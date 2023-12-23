5646648aa6fc3442082cae6f61960233922f464a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:05:44 +0000

    lost after moon wave receive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
