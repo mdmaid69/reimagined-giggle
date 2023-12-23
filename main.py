2639c19695eaa3f5604dab616936230d73ae849a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:34 +0000

    form attempt pronunciation land late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
