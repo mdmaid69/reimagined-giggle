08e1ea2d87b8bab2d00a0394520f22e770e13f93
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:56 +0000

    bye fork music mystery drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
