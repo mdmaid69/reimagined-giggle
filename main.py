02cccb214709714b9cdd2c158a87adb625dc55b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:16 +0000

    past wave country satisfactory moon

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def reverse_string(s):
-        return s[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+print(sys.version)
