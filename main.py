5642086e5727aa8d2acbbc4ea4fecb0c7c02dbce
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:29 +0000

    clue sweep pronounce squash mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
