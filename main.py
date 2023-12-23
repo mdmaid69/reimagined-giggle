852b4739cfcaa81bf9d064b94644b49253906879
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:58 +0000

    yes whisper safety great 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tensorflow as tf
-print(tf.__version__)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
