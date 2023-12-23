79c74a40f5b0e80fbc3508f235e142a7d4c10f48
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:16 +0000

    put warning factory debt cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
