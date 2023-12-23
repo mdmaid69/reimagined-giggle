9a909714a945a5cf9ded1d5f7da146cfb98b0fd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:22:22 +0000

    port excuse office excited along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
