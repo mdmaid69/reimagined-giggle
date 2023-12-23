758580a37a07fc0da77a51a895de2075fa45d39b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:53 +0000

    pocket win and various hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
