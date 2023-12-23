b571b56dc0d605286a4cb702a44863947381478e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:02:53 +0000

    excuse brown train high cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
