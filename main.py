22cb16ea422c0e545906e465c1929f078bdd6765
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:28:33 +0000

    clear survive gas round regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
