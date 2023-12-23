52707902ac4e39ebdb07cd91a948023fa48e8ca3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:28 +0000

    whole relief enthusiastic 565end honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
