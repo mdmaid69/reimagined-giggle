3adfcce75ecd015e21c85692e913058163a5e142
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:25 +0000

    frequent hair train boat practical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
