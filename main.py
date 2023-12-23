8ef71d1410c2ca6bdb0e23175f8b9abb036237be
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:52 +0000

    pair spin cheek test unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
