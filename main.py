13b7924e389fbd5f219ec404c45196d3299cacfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:05 +0000

    fix notice payment dish regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
