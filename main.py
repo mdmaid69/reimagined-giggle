60ee5059879f8de34e72bc6a61dcff349fa11b91
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:45 +0000

    smile deliberately previous turnoff lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
