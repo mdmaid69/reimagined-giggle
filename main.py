2e43348910143a106c053003a157726de18c5776
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:50:39 +0000

    hurt opposite offer neat film

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def is_odd(n):
-        return n % 2 != 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
