d250fb6f92546406826f2e5ed94343ca2d8c5c4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:03 +0000

    home glad smile sweet colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
