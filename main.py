e51838c97e3e13820ddafe6168e601dcfc375f57
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:51 +0000

    point teach voice narrow home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import time
+def get_current_time():
+        return time.time()
