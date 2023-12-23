3c5adbe2d682b68ac7bbb74ad512145fea2bdf88
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:49 +0000

    city warm maybe crime moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
