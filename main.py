8b5dceb400b863ac0b9ab4c61d843991f792ed32
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:44 +0000

    concerned confidence century well transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
