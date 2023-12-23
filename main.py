f87d31e11c4497111d85059077708dab141ab200
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:06 +0000

    lead promise rarely noise friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
