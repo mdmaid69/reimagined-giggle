a672e9883f84086009d2be3a35da4dbb60790e76
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:33 +0000

    perhaps exact tax cruel ours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_list(array):
+        return list(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
