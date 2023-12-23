d287dac9211a0ed6a974b1e4b5a704d531a392de
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:33 +0000

    maybe accident unlucky turnup appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
