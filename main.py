77cef868d3e5bb743a14d13abefcee211ab596ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:46 +0000

    rule means female try small

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
