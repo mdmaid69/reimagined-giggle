d5554ca950d8ed8014b3d4f24f8110a446f8cd2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:26:43 +0000

    regional throughout peaceful past ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
