1ad170084c0ef76dacfc94a852d4ff25e7908574
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:02 +0000

    boat zero over story government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
