c44bcf791c806ee6f93fd63bc84916bfec2e1f28
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:41:50 +0000

    friend evidence holdon rain chief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
