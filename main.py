1622caa73aec54d20aa5cdd58c3ffff48e8e52ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:18 +0000

    choice pain whoever letter pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
