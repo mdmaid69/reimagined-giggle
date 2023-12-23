f22f48a24489ae23c15734de516c47ae03844e2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:28 +0000

    principle style swim shame soil

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
