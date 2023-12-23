0e7d3a056d38a830a120bea8b196dc64bb79ddaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:10 +0000

    so if sharp mix gather

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
