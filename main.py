adf021000447bc5b1496a70e0eee47a9c36c81e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:03 +0000

    bring fork they wheel payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
