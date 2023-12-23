c686c3f56d6c11387b560070d6e425faf5ad8128
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:45 +0000

    move active born city train

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
