bb716f57ff29cfacf0d7df53e61189a3b037293b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:57 +0000

    soil bus culture intend ahead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
