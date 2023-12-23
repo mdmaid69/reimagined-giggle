b729b1115b75909e3b923d762983ce85b0db40a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:49 +0000

    aim respect job within tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
