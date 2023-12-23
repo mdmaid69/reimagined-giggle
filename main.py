da395d7d70b150de55b9ecaf4b14fcc46899e3f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:43 +0000

    hurt concerned print play plan

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
