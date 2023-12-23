8f1520dfafc56f2a74af217df23b182aae7cf2b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:12 +0000

    attention phone mixture enthusiastic pair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sys
-def print_python_version():
-        return sys.version
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
