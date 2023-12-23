051899a6f2c3461566b9aeb9cec0ca9517ddeff7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:56 +0000

    beneath moral mention chin warning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
