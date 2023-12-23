cfbc86303b28009722e27d9fff0fa843bc30b4fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:57 +0000

    exploration suck rush able type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_str(array):
+        return str(array)
