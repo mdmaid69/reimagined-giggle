b942f84069b4d6f578c9ca8ce165a2b6d7b65e34
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:24 +0000

    coat turnout throughout shrink crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
