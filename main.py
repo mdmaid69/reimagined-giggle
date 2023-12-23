1cace8edba77f7c98d7c4d86c3c49d671add53c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:46 +0000

    expression upwards defeat morning mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
