eaa8098fb259a2f22d3d30b72a1888fcee9cf649
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:05:29 +0000

    pick club holdup notice money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
