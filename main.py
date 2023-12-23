e54ceda53b54f674e5a9aa240c3d205d4547dba0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:09 +0000

    dark necessary smooth getaway during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
