b8d76e4ed2bef628d1b2b12b0332a8b9f572155d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:47 +0000

    smoke better goback(to) post the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
