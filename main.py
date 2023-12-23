057a735f41a5e3aa80aea542216fdd813c007a39
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:09:32 +0000

    section stare careless squeeze up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
