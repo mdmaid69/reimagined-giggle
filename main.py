44ff2ef88427b17154e8231d1ad16e70f8c5d4dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:00 +0000

    schedule patient mistake sleep cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
