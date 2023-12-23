0f85e43e2abaad9dcec1aa12d8da2cc0c86d6a2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:52 +0000

    bill murder correct this Ee

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import getpass
+def get_username():
+        return getpass.getuser()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
