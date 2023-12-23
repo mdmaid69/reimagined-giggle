86fd2342f159a33c602363c45eddf73b9749333f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:35 +0000

    learn surprised special preparation several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
