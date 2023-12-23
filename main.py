2ae40c9e86a419bd448a006370e2298741bb2e62
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:59 +0000

    indeed importance comeover solve lie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
