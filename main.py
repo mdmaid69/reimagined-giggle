c28f8a830d5916e07450b409e04b5e67114b5b15
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:53:29 +0000

    mark fat keepout lift all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
