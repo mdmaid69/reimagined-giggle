b769bc39eb6ee3483a0271be48b307f8006f7697
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:53 +0000

    advise picture characteristic check pay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def reverse_string(s):
+        return s[::-1]
