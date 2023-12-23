5e6a09a9af582b0dfb6f6c218d452286cf7acd2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:14 +0000

    take characteristic terrible exhibition 566end

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
