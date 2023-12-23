b9b56f21df06442dfa60367d815aa6688adf9615
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:08 +0000

    drink bin 565end takeout mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
