5ef80a72623d932846a551e86b9acfb3d1864184
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:12 +0000

    connected joke fill woman deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
