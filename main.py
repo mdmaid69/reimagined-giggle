4bdb77a7de4a146dc3ce0e4a1be98d03773aee64
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:28 +0000

    sensitive tap school get culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
