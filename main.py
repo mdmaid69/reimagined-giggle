46f2714405a5bd23dd05f7dce76a42ccdc891b00
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:03 +0000

    excuse band blame recently second

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def append_to_array(array, item):
+        array.append(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
