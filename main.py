827043af18b1af7e4110a1c3cdf7e24bbd4adb79
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:55 +0000

    store about leave embarrassed thick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
