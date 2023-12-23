efdfe0c9636059d7fb5cae4e4ef3d0e44d18e99b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:38:19 +0000

    cheap eye receive praise insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
