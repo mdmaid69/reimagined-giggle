caad8ab7df5f1881a47f84d89f53142cce7d8e1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:05 +0000

    translate everywhere ourselves left uponon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
