21e80e46e250dd98db148d892ccdf948fca793f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:20 +0000

    repair point hard nonsense scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
