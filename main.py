acf1404cf5a53deff098d90cecb1a1d42edbd248
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:40:05 +0000

    breakdown brief every either every

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
