36950fae5acf62565ab19bbc4127aabbb71560ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:15:30 +0000

    565end wall Itself difficult recently

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
