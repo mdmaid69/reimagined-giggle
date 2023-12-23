7ae5ecf5cca22a38c9e73c3427ca35990c4d5ec6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:12 +0000

    want model hide willing weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
