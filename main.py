9d5f96d2c257443862edae8d9efb41473de2700f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:08 +0000

    change recently little correct final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_floor(x):
+        return math.floor(x)
