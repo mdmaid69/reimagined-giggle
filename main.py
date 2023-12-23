6ad597c5043f15265ab99dd5da59664be8d242e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:11 +0000

    right sound people steam getback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
