e2d4df9211b321fcf40d23d7cf8a22c207358260
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:28:17 +0000

    train someone criticize remark young

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
