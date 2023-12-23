88f2fe7a1ee4dcb4ea34b7fadcf0adc785510b66
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:37 +0000

    rush repeat lost getout brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
