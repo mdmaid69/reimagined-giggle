053868cd594f42f1d45b5f1a119a3fc6211cc8d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:30 +0000

    cool under correct grateful meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
