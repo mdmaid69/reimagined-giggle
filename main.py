bba5c99d23df08ff4f260f47f4d819a3fc12be84
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:35 +0000

    object computer regret considering difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
