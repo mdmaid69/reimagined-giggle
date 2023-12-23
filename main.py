ef3fe3034cd565a1ce399e31565a39560094a26d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:45 +0000

    Cc history thin deaf with

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_work(force, distance):
-        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_max(numbers):
+        return max(numbers)
