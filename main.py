1325b35563fdc9a1f017c693c886e573ec33e271
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:15 +0000

    whole badly between crisis present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
