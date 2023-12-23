b61f05156bb3e18d9bfed2559e60d8c063a2c025
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:03 +0000

    freeze while mistake suppose narrow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_int(array):
+        return int(array[0])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
