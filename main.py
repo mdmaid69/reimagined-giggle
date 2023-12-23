f2ee56d5f4373b4fcb78cc78dbfa7834df877d25
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:18:30 +0000

    down band condition attitude official

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_work(force, distance):
+        return force * distance
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
