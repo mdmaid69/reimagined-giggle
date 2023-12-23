5827d78e0e541d9caf027a16743dd22e8866aea4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:08 +0000

    scale knowledge introduction feature empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
