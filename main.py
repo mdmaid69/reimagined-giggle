90c4d485d0cacc5d3e686885d02aa42954aef712
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:11 +0000

    reply exhibition shoot frighten exhibition

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
