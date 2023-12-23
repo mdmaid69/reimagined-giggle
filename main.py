832d0146781f2926399cf3df0574aec3530e3629
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:19 +0000

    keepaway exact through lookaround condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_list(array):
+        return list(array)
