d374493c3c923bbbe61d5f340412192bd8ca8796
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:39 +0000

    tip turnoff she pay break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
