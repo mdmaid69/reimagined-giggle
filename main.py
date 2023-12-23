b9379c0153f341941db2f34e37c836b31d9aff9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:35 +0000

    hurt properly steady fall prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def delete_file(file_name):
+        os.remove(file_name)
+def convert_to_octal(n):
+        return oct(n)
