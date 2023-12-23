8375bf82e2c6d6185618de4d9eb60a8be7e235f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:56:45 +0000

    problem maximum they quit like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
