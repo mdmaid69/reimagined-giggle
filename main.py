a97141c07c257e695092af75510321e16178e006
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:52 +0000

    exercise fair look borrow snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
