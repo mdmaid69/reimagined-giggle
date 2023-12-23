b934f99a53229e58979adc9694e44547602a5f99
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:46 +0000

    within collection scream common bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
