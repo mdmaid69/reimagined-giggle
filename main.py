e76bc6635598fcf1cdc4b8deffd3e4eae6cd51e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:10 +0000

    treatment Bb love book last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
