dbe6355af6bb69e1284966bf01d5833a19d491b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:19 +0000

    well always exciting possibly sticky

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import platform
+def get_python_version():
+        return platform.python_version()
