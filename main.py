792f99467fded98eff0d4a9b7f75f1c17dcfc954
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:35 +0000

    control field lesson follow handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_min(lst):
+        return min(lst)
