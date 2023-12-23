d456287487eba6cc4125ec1bbbeeae4df3ea6c6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:43 +0000

    fault sort firm smell takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
