8223ef41fcbd6d5129e87ae70f332b0372ec1866
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:38 +0000

    foreign ahead realize instrument anywhere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
