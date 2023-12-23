13b3597db3cf10937d585143526f477233e00440
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:15 +0000

    immediate forward excellent present Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
