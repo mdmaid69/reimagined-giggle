33f7af0bde6d269cc38c9e3ebf1d815a66850469
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:14 +0000

    hurry violent report other split

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
