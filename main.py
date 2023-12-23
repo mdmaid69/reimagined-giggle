0b2578f63571e8db04592d7235cf429c125b1553
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:03:08 +0000

    translate only anything back catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
