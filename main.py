628dd21faf735805dd4c698ae88c59f8d78f7b3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:01:39 +0000

    cover house hate talk no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
