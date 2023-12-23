dadd283f5c54dccb77a38237f3bacce5fd7671aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:10 +0000

    mine feel block lot pollution

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_as_repr(array):
+        return repr(array)
