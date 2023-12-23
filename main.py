4498beb79d91f0fd5efb70cc2eee06424ccd4534
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:59:56 +0000

    common breakinto it live as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
