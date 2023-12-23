49b218b1ecd9b6ff2438e20dac9bbb78cbb60bd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:13 +0000

    least shame basis equipment head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
