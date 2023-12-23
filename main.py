5aaa686460e951d754adf2dd539f504e81e53d4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:18 +0000

    dream pool paper sky damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
