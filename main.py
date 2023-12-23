6d004b93d7094a0854b41e4a60f8c9dfb43f3f00
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:30:31 +0000

    term my dress willing wipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
