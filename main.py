4fdcdf5a022d531dbf08d4cf515935484fb83111
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:54 +0000

    defend straight worst so ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
