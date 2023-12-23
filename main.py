b9b6ff0c8f091c099caca412fc42cc3d5b8a02f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:36 +0000

    high sure claim soak feeling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import logging
+def log_message(message):
+        logging.info(message)
