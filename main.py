2520a9ede65715de901c8bb7a90c2a01ab1150aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:38:37 +0000

    mess comeup blue bury bang

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
