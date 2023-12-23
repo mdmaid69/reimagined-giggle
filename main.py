1133f6afceeec4a7ca08a338a7b4082744f68979
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:18 +0000

    strike tough length small broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
