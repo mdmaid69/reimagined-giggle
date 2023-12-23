1dd107bc373ea92ba7beb27b3125f2775d1ed39b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:52 +0000

    fond even play type that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
