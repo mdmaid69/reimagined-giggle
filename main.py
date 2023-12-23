e9443bfef5bbd8a9cd170116d4cb167daf03cce0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:58 +0000

    minimum mistake fault feeling Pp

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
