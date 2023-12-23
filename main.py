6744cedfc905295d7230680b39b5a638de81c1ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:48 +0000

    clear confuse near health also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
