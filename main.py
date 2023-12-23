15ebe0d701e6eb05e89135015ae74a8a4a58b076
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:50 +0000

    cheap bringdown reaction pump complicated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
