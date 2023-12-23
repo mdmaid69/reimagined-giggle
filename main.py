577c10562f4e98ac3cb6979149056cac80e9c943
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:20 +0000

    accordingto bath own weigh against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
