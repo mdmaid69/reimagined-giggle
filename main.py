bbfb7d80b5bc41f019ba85cadca23f993ed9500f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:36:43 +0000

    supply school central off remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
