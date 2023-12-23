83c5fa63a181239c2bcfa8b66319b8c7cd06b9f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:03 +0000

    anywayanyhow matter fair advice family

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
