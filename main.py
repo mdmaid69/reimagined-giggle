d45b6207c417e936d7db51f18c07ec267b4e9e9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:46:46 +0000

    miss responsible equal goodbye machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
