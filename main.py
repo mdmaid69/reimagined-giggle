1a509aa926a20976e5bdd19a7f3ea5b617c0280b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:18 +0000

    string buy beautiful quality soak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
