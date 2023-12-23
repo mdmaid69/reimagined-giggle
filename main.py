3a18aea446bffc06433ad0a975b15fa8a09be010
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:38 +0000

    narrow the hold exaggerate satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
