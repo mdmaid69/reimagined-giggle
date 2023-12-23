2280c3377ea6993d49d283b52970e07effe1d368
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:45 +0000

    age eager represent begin slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+print(sum(range(10)))
