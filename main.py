7f0dc5e82eb22b820dbf68aec6f518ae8f720262
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:26 +0000

    him delight turn gift ocean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_density(mass, volume):
-        return mass / volume
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
