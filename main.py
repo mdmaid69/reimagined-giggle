2d872887b7fc7e00362b8724e7693f1ac661bfe5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:47 +0000

    runaway crisis broken spring upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def remove_duplicates(lst):
+        return list(set(lst))
