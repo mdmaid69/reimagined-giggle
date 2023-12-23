304ced33caab711c0fb3764acbd09e2b9ac9eade
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:35 +0000

    hope favour dead after communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_str(array):
+        return str(array)
