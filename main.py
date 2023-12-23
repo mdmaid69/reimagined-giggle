6ffa12ecdbfe43cb6be57ae6a0da73f64e1bfa2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:06 +0000

    switch achievement Vv something since

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
