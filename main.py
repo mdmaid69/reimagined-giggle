736179a0c4321efb95066ce46836ce21aecdd730
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:56:03 +0000

    anxious time by tip apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
