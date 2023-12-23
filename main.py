c907c8120ecdd95458b81813134233e373e07828
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:29 +0000

    safe raise loss sign when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
