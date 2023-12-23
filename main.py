4c3f35a03d400edd8b5c22aba2ac77e5d864450c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:09 +0000

    pretend increase image clothes pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
