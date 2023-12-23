e0c2ee9b36ed9982944f009c853170cca85f039c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:27:38 +0000

    wire prove find quick future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
