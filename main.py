162783b460dcb5eb2ae0f3c293f63b540b2cc691
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:41 +0000

    natural suck row become advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
