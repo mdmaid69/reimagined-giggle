d4b1c89873e600df08de43849da2dd8b508b84ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:44:25 +0000

    whistle nowhere niggaacademy just sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
