12205503bcb63b9c7a5a0826e9f08b92c048cb5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:38:36 +0000

    real art direction in bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-name = "Python"
-print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
