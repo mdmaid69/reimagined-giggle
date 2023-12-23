9dfc756c8fc70c783b12a3cb2501deede6e3a52f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:56 +0000

    steal regret woman thin sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
