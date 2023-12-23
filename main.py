ddb2ff10a35d96ca0d6fffd8f6d50c2ee047b476
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:05 +0000

    comeover creature play people tight

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
