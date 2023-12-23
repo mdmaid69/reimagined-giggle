29dc6fd44121d7bfcd2384cc45151fd19ffc80f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:41 +0000

    influence sure declare repair else

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import sys
+def exit_program():
+        sys.exit()
