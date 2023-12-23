7d88d69cd4a82a189ef65275850d222623044fc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:45:31 +0000

    exercise size confused smooth snow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
