ccc74dac8662125034172b7af406bde34872e9ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:21:01 +0000

    circle direction rule insist skill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
