14dcbaee19e1cf5f38a0e21986b0d2c255e0e4b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:35 +0000

    further tidy address development declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def subtract_numbers(x, y):
+        return x - y
