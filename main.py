3ecbd10bf8306e11da598964802f1e1d879e9cd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:00 +0000

    pity pretty furniture enthusiastic vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
