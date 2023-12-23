46f44bd9d8eecc770492aa60c7c6ddeeb793b94a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:12 +0000

    mean either shall computer ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def convert_to_hex(n):
+        return hex(n)
