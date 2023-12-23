d4bc87ca14c55166baf05303a3d88641b070f819
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:07:46 +0000

    exchange muscle sorry smoke birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
