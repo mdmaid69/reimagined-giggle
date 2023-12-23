51456f6b684928e69980556d18652d6cb7ba11ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:49 +0000

    ocean contact charge tape anybody

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
