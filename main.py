113b95ed74643666ff4da7c3a28cb5c2c815f3d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:49:12 +0000

    future himself course beat finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
