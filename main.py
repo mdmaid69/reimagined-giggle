63e611bcc3fdf680b264db998d2e06d544594330
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:56 +0000

    sad support recommend exact interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
