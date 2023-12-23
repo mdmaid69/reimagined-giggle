5045403ee49a9754d97ecc44dc8721cc8faa2d8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:00 +0000

    bother disgusting lead lean now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
