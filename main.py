bb2012e9236ffcde2ac9a1f397c01f1412669d07
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:41:44 +0000

    eager possible early unless warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
