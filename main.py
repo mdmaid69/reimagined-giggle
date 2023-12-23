349106a56f5a499ee4c4fddd9e3d16fe9d2f0773
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:43:43 +0000

    trick in runout direction tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_base_name(path):
+        return os.path.basename(path)
