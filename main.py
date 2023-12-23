0981032d163d2ab89ddde29cad875e30f1734ce1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:29:32 +0000

    typical cope imagine anxious shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
