3d3746c13200ed3ed6442f20959a73fdbd4d158e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:47 +0000

    block piece early spread load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
