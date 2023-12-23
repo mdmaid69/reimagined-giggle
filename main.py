e1b10b17bb27d766dc27b7e34e7ac7e158385502
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:38 +0000

    light supply bar mouth sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
