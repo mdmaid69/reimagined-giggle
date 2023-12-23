8b7b64caf3d21a194fe34233690dc2f0c59baf83
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:52 +0000

    chance separate full exactly obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
