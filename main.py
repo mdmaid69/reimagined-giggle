be7cc4d72a5b536868fb5c6ab01132bc50b1ff86
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:01 +0000

    brother fail stir lonely drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
