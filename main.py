b0383f99651d6b5411b7f042d7d76bba8ae8fa52
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:29 +0000

    disapprove lookup television convince properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+  def split_path(path):
+        return os.path.split(path)
