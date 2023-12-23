6084f8eb520707a0b2dc319ce62a6295cddda03b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:17 +0000

    relax opposite art remember side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
