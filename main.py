29bb82edda0c1110cebe0e627dd767beb6ab7b1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:26 +0000

    gift away length company without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
