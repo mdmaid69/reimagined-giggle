3d026d327f2413bc08f311ecaa677f36199ed0f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:26:45 +0000

    pronunciation watch draw dinner tidy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def square_number(x):
-        return x**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
