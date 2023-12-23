f982ee8d5f43fd7b00e0deeb236bd204caf34224
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:35:32 +0000

    love coffee anybody always dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
