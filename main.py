8749fc9f3bc4df4645de0b91d4c0b175479779f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:36 +0000

    talkinto statement frame whom protection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
