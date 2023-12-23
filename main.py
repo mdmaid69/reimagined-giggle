3888dafff73e68695214550b1ad03e30518fbbd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:16 +0000

    stupid position thumb Jj wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
