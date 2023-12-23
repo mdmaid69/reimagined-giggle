5eed63e6f86b7c2a470ffaca1e69f087d6b10f68
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:36 +0000

    knock open tip except light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def greet(name):
+        print(f"Hello, {name}!")
