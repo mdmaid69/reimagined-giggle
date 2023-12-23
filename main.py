ea3942a8e65e01351521ef3aee82686f44879bd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:29:05 +0000

    bite all eat crisis uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
