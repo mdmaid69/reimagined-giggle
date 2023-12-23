2e899ad7c5583365f1d7a7a16092e45d3856bffb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:10 +0000

    cold pay middle month runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
