85ec8cbd3e82408e2120ee157628dcf5a1bc0ad6
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:17 +0000

    about pay turnon store manner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
