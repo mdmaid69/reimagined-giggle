f43e61bad5cae233bf4029e0d76d4f33cc926da6
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:53:19 +0000

    sensitive detail minority embarrassed frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
