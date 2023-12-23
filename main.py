91273559b9fd9df362d71c0a142195c2aa1e3d21
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:46 +0000

    shame phone art beside measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
