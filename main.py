2f0c77d77d1a69524adf691952aa5472ffddf254
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:47:01 +0000

    popular test OKorokay underneath sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
