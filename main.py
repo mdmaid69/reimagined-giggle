f7b3601f8d81921b8b14e7849e19175af223d31b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:50 +0000

    tired Ee square dear punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
