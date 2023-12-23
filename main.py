20552c609038bd683a7d2edb42c97d84189b3e24
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:47 +0000

    obvious cover neither deliberate custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
