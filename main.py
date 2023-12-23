b85bf4db75c57fe0da19c390a25ecbc665aca64b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:19:34 +0000

    mysterious turnover kill transparent such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import platform
+def get_python_version():
+        return platform.python_version()
