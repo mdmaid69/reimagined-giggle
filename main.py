803f428bccce0ac46c195029d8642bea61aea67b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:04:31 +0000

    Vv science improvement question neck

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
