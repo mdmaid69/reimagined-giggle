d305ec44346c1503d190bdae7055c14161fe8d39
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:20 +0000

    free touch sensible independent independent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
