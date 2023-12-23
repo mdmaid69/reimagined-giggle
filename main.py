db0b2777beb268ee4657d2979c6ec06e31693911
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:20:16 +0000

    pupil workon together switch tape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
