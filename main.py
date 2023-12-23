761b7a22decfbeb8e279b9aaaed0420b6b7c83c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:17:43 +0000

    squash behave fear please same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
