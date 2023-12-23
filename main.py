c00a3e454af161ed71724de75cf0fc2b2948f229
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:45 +0000

    minute match rich agreement people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
