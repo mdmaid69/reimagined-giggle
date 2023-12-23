b38b2fcc0784bcda4e4b6385e2690d3ea7e50c69
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:49:48 +0000

    blue nurse piece small permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
