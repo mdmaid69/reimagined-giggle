1aac43d71d384327d8662b689b0ab93c8b5ff0f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:18 +0000

    fold performance would 565end autumn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
