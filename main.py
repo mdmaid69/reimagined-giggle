b159a7c4966f396c1364ba536893585d1abf1523
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:06 +0000

    water payment clear power club

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
+def is_odd(n):
+        return n % 2 != 0
