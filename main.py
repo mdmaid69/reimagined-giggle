346dff42d3473a45a0f03106dc9366db7df8f67e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:52:18 +0000

    mysterious rank welcome Zz official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
