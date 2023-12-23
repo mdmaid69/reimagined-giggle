83e0e3b0f487c886e9016c91ba681c4d703fb576
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:46 +0000

    remain Cc machine learn fast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def remove_duplicates(lst):
+        return list(set(lst))
