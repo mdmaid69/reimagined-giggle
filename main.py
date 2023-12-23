d550788a775c0d119f6afdc9e7d96a8b22deebbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:58:33 +0000

    ring claim comeback various brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
