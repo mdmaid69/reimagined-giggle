d9ab81c9d7bd90f330070ac87c48d442bd6faa0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:20 +0000

    century appeal whether marry thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
