bf014e85c49b0c36ec6c0814da2be0340f4d7dd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:20 +0000

    mix will exhibition interview freedom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def multiply_numbers(x, y):
+        return x * y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
