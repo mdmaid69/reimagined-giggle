2818bfb9ef22a364a018bc988a018e5c2cbcf6f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:52 +0000

    snow spot machine add responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
