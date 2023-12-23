323000885c491718210f8d8f8e4a7375bf9fe243
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:57 +0000

    cause street they sign write

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
