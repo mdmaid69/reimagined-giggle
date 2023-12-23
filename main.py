1cd4d285e9d7f1d1a88b9bd6713f9e7a98039e12
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:21 +0000

    consider wander convince correct that

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
