15329e401d1b89fb4cc75bf5ecbe6852ee0b1a12
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:01 +0000

    likely reason shine exciting base

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import time
+def get_time_since_epoch():
+        return time.time()
