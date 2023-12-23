31cac20c33faa91a4666ba69bf07d4920495e9a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:45:52 +0000

    average nervous professional breakin pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+for i in range(5):
+        print(i)
