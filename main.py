cad4f5cfccd272ddec321820b030d8398dcae92d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:58:06 +0000

    section great bottom wall all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
