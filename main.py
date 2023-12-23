01d1e043bfb2025de5a99b2617db931662c08646
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:43 +0000

    coat case can ought please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
