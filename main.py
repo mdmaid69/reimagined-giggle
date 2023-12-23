7180e634b63c2f590d85034eacfebda58c4d6131
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:16 +0000

    undo address bend replace disturbing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
