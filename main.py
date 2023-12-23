362b90df5116c6a71a5c875cdf4fc926a6f8f268
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:42 +0000

    please juice project tax cut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
