d19f5b9d64e5b65423951670c69b7023bac8e940
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:29:13 +0000

    food copy fire expensive wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
