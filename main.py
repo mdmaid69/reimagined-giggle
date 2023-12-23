731b88940db45573ea4aa49e891dcdf5e1e85566
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:56:58 +0000

    deep time hold design close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
