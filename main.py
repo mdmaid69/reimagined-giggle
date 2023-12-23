bc682a0b3fb278cebd8fe5c8d0f8d2ff965cb47f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:11 +0000

    threaten wide angry artificial smell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import tensorflow as tf
+print(tf.__version__)
