2729df23bc8678f1ebeb87af1a8f80d9757d5f0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:23 +0000

    limit content simple kick against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import array
-def clear_array(array):
-        array *= 0
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import datetime
+def get_current_date():
+        return datetime.date.today()
