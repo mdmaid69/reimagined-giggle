b321ecf0b7eb7f3b16297fbeedd13a3ec4b395f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:49:49 +0000

    ring rubbish home artificial claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
