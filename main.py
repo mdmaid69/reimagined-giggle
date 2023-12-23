99566c9e411b84e3f2fd51878e690bbcf86ef4d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:10:21 +0000

    here range instrument row forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
