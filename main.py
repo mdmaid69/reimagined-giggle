c497d335dfe2165d137cf460be629056ce58cac7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:26 +0000

    old in nurse finish question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
