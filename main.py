97e9a6e8e3307dac06ea5bfe68a02fc23b121b6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:58 +0000

    land tour soon pen light

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
+def find_max(numbers):
+        return max(numbers)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
