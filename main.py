d855447053b544cd736271889f3dc48b62391560
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:52 +0000

    considering family variety especially argue

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
