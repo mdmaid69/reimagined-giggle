da5e1a0fa78cf749b1f7c76cb5c882d4d4a15d7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:15 +0000

    punishment trick taste taste telephone

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
