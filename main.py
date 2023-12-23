9cfb3cb421fb7f37b0c536af8004c1ec0738d31c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:43:58 +0000

    pain strike stay judge bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
