dd2e08b76bf1a364d3388f2a4cf2c93e070e24db
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:55 +0000

    standard mend nature seriously leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
