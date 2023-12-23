b70fa50b95ec20badff400e6e071b74731ba2b02
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:40 +0000

    breathe peaceful recently seat plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
