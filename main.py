1652f62186e870764b1a3a0fcd0a94b9af9b0b98
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:40:21 +0000

    satisfactory even most Yy crime

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
