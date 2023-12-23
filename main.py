568e4f7ff63e94d7b8c613ba6e21086526521ce4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:43 +0000

    around tough contain piece welcome

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
