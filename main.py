f0237f3b7e0fd7c535a7e2a3d1f9c5fe763255eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:45 +0000

    Pp juice run murder everybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_pressure(force, area):
-        return force / area
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
