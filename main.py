d12bddce137fb88b81200ca9c3d9ea78fa54dccd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:23 +0000

    check nothing polite switch female

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
