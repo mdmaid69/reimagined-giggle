5a6af4a68cc16f92a84239da7fa59e4e55c4dd22
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:02 +0000

    clearly prefer distant broken holdout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
