f5c9807fea4ac20d614bfe7d81d35e642adb87b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:06 +0000

    trouble correction unkind luck below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
