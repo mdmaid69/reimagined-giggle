d99964f31826d932c4c2f1c6c02f6833f28d4890
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:34 +0000

    avoid brave somebody calm discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
