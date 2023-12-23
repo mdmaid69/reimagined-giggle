8dde5bda1819ad1fe9106e4609030163a7239896
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:21 +0000

    ability side track set risk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def is_even(n):
-        return n % 2 == 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
