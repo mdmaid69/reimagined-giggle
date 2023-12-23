e0a026f18a30ab33d55c8b3a47d8bc3bcaf8a92c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:39:10 +0000

    passenger during past later your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
