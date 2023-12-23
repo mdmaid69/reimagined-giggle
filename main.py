b151ed9306b021ec45786553901325d46937a15f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:02 +0000

    at dance wide he tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
