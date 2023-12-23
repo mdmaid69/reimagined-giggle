c1b1400f7ee722d207743553f004c3f13a59e9e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:49 +0000

    completely difference seriously stiff until

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def reverse_array(array):
+        array.reverse()
