e2861caa9c8ed9243d27cd6490f30ccb8350e17b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:43 +0000

    bottom children door wrong neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
