4886ff2e643e078c1fde2860457b0422b054fc5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:57 +0000

    arrival write burn mean correction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_item(array, i):
+        return array[i]
