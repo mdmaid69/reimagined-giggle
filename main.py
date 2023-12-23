a9b8b8f877fbe6565d7981e3a83f877734bcc3dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:25:09 +0000

    tired minor workupto limit clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def count_elements(lst):
+        return len(lst)
