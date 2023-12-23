11dfdb6d91a0c7c73654253173f200ab35bdab7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:20 +0000

    average contents kill official silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
