e06f12cd150c1624984c9922142bc815b9662a1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:36 +0000

    damage mail duty shall sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
