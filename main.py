7fced27a63d2497bee27a7c55f2d15bfce51891a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:14:36 +0000

    grade attach cinema understand left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
