b139e0ad0fd38d9aee8b5e75d038ff1129ae905f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:44 +0000

    shock letter address uniform travel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
