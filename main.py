1c70ec42dc429598a6d9c7bc48cdb48df76fd953
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:28 +0000

    plenty blame cheat little side

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
