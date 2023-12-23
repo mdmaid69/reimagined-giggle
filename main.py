3cc2286aff4b7d5d7c7447d690cbcf986c12a690
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:35 +0000

    future aim Ss long wide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
