25a587aed2576acdd4d0c7a19885345493840ef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:02 +0000

    foreign wander pollution balance dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
