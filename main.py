f8769c1b8f681028908777c82fe45ad9ebaffbee
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:55 +0000

    there anyone fence smart organization

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
