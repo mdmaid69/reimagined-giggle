c3d6b9358ccc4036121dd8859d2533e084db07b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:36 +0000

    report friend cost wealth apparently

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
