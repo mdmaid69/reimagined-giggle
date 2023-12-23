b1ea1022c8fd71ba6a50d27061bf7f4f59e01010
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:26 +0000

    building home television tie bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
