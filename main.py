33aa8500d77e982d16171cd61ef22183147ce7d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:53:42 +0000

    central shine fat beat appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
