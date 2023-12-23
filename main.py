fb5505bd94ba8456e30dad61e6d09c546fe4a12d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:28:58 +0000

    possibility base telephone common uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
