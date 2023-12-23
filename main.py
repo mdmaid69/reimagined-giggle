8c8306a37d3194af8b0db1f58eba14b12ad8fbc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:35 +0000

    middle that ashamed please scream

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def add_numbers(x, y):
+        return x + y
