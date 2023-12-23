540b23541408273e1406f179692e54774213431c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:15 +0000

    detail ability suddenly fat read

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_error_function(x):
+        return math.erf(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
