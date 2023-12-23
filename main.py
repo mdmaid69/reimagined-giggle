9c189807d35a2f6d88c59d18bd4f24f90c5758a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:42:09 +0000

    club coat spring goaway order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
