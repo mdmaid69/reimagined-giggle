3461785a44e0397363958449c52a97237b83fdc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:53 +0000

    attempt deaf motion art bill

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
