7ccf133b0bd723e76709c26258bdfad48c13a1e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:34 +0000

    bag lesson idea train instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 def calculate_speed(distance, time):
         return distance / time
