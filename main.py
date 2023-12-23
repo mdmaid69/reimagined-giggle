535feb7d74021f916397d82544f4f71fbbee0f33
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:20 +0000

    centimetre borrow myself far warning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import random
+print(random.randint(0, 100))
