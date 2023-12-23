7e904731f07fa17c57d5a73b8a8ddc040d2c5ea2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:33 +0000

    odd lead prove knock dead

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_length(array):
+        return len(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
