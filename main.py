b3e7329eb5bc7d1ccecbfa42618f830ca9f22cb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:29 +0000

    grow fond production fork problem

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
