917cf47b1ece857ff586d68676d7585b38949635
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:31 +0000

    emphasize exploration life attempt friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
