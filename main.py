7fa582a4edcfa26b5a027ea2ea8709f26ec602c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:55:01 +0000

    pride accept early cruel dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
