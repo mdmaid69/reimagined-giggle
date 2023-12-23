264acc3cb68fbb1603f700ddeaaacad7e14e804f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:47 +0000

    murder cost happy hurry minor

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-print([x**2 for x in range(10)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def split_path(path):
+        return os.path.split(path)
