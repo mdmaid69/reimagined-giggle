f52ec8568d2e7bf8fb970738f798b16f3cb1226c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:24 +0000

    free turnon relax principle study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
