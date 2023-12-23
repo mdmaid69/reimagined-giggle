b632201a5d2df43d059fc4f9988c83f92912431f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:50 +0000

    frequent however anywhere bang earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
