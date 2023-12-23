53e56be7ee6ec79f995003afee79c50a58d61262
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:54 +0000

    boat tough Nn check excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_float(array):
+        return float(array[0])
