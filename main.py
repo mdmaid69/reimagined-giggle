be60dac815d4bf123ef40aac2365859954beaf6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:57 +0000

    power arrange willing tragedy now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
