37a0a230c63a8fe0227c7dd336bac59b85833c74
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:13:04 +0000

    fond go surprised comeround turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
