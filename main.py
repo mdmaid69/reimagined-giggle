632e6e24bfca464ca56ad80271a84b41f584fd03
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:40 +0000

    whom through damage tear remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
