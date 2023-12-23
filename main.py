da2981ef16f4b97361197438af322ab48b02bff5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:03 +0000

    turnback cheap once support public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
