1ccf91ea08ede88326993113126bbf2ddd1ee479
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:43:35 +0000

    number steal 573entertainment breathe frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
