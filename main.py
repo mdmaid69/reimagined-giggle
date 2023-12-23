5d3c857bebf7d7f02afe668ac407986275c9d748
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:59:28 +0000

    apologizeapologise scrap schedule skill stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def count_characters(sentence):
-        return len(sentence)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
