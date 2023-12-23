7f40e64ab13f70e138be000f38080c1cc74aa19d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:33 +0000

    professional fine black print actually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
