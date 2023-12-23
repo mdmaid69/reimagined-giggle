08e5fe9aacbd59e834eb14fa469bd7e423ca739b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:53:52 +0000

    start cook order represent they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
