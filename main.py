3f6e174e29300d6ce632ec5e4a439e486b1059f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:40:48 +0000

    shock length careless walk dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
