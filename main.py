c51d87438244c9d0d05f78bdcdb95d7fdf932505
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:06 +0000

    Rr better father part join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
