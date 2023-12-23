6c2c318fad32fd110d01e2dbc58dcbd48c8dae46
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:40 +0000

    scare think flavour embarrassing steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
