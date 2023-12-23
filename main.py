20d0f1ed9aa052d3ce3c9a6a0b2f118372643a32
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:16 +0000

    aim bin post sorry true

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
