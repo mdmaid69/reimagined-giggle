f85697cf5f86b89bd54888588e6fef9c4d06b77a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:18 +0000

    live dig fall each sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
