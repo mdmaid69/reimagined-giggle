33599308f12fb61b951e988b74b46c70f13d0b20
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:14 +0000

    smell party guard sad error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
