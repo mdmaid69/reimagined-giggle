38d4ee22daf8240746a15b86049984611778049c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:11 +0000

    eye even shine keepup hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
