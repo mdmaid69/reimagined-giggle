06a17bdf6b1d5f304f98a0a6a57699b24b61e2d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:12:10 +0000

    Tt start successful hurry part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
