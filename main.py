8ffc74b1b17df089343af8464d57849042e4c19f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:07 +0000

    fit proud confidence somehow obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_stack():
+        return collections.deque()
+def find_unique_words(sentence):
+        return set(sentence.split())
