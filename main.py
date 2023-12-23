d75628071a631fedf93e1e76e84c7966610ac079
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:59 +0000

    talk experience negative school prevent

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
