9aa60502dc2e086a37d81394e6c8084cd4d87398
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:47 +0000

    represent group unfortunate region post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
