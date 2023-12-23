6f98b67b805f0ea5fad71f6a23b36ec63bd01087
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:59 +0000

    dry arm patient every contact

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
