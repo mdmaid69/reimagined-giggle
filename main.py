f559357637c7a64a0bcf36fa2c9d05843306cb90
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:51 +0000

    meeting replace add welcome slice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
