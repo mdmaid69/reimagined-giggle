ef2691de9a5cfb9df4f81bb067654062d2f761d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:50 +0000

    juice greeting body bringback guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
