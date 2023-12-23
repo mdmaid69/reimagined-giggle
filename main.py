9773c5f937018e8102e0a22f28ec3c03888da024
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:46 +0000

    state frighten telephone band upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
