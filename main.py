9b5cfecca7cee9880adfbf9dd40fa6daef3f7c3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:34 +0000

    taste remain convincing pattern hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
