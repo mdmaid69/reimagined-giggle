dfccf5b865150e28bb4c0fca896ac8af3c41702a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:26 +0000

    reserve blame earth patient far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
