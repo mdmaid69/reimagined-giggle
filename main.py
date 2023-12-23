435b424ba4494f09858b2181f7654e1a1f58336f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:15:59 +0000

    hair unfair recent truth shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
