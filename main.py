42dc60476a59974e7ad30a987170ff79f7c0c4fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:37 +0000

    door gentle thick performance drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
