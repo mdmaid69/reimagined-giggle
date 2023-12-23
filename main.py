2eef9eae39ca1feff0b4b2702620053dd96aa120
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:10 +0000

    protect secret side special broad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
