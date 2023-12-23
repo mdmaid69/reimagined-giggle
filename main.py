863acc2bc1feb3630aa00b992b91d46d440fb3e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:48 +0000

    tool unlikely fasten how pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
