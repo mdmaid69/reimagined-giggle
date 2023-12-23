fdaa0f27bf005f8c8972b372e4f3f4e7969b680d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:16 +0000

    play belief another learn create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def print_python_version():
+        return sys.version
