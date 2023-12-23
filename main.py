c07c18aa646d6be92ceb2a8ddecfe5dcc00befbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:09 +0000

    cap uponon consist involve worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
