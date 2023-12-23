0c27fff2ef4791365b7da62b90dfa1a22b74f4fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:32 +0000

    trip major fetch mixture month

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
