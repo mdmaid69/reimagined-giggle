9d05ac3894a1763a0f6ac443d854581610f882c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:33 +0000

    I present exact motion intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
