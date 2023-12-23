cf0fec2872864aeb863b823b77ca0294f0c6bd25
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:17:27 +0000

    arm project address watch understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
