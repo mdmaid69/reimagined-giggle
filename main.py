c54c345bfccff9cddab6dcf9b2102506ed77c9f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:09 +0000

    plenty unkind although health often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
