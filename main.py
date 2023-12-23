57d2e73dc5b10e2d3ff1d4d10990f1b2f8593f3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:01 +0000

    used tidy while badly mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
