0bccc6c9da7d30a94a494244358c880ecf262229
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:02:14 +0000

    pocket belief recent welcome science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
