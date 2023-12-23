fc58744f38611555cc3524d07a9069417ec3c751
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:37 +0000

    influence thing repeat pleasant thought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
