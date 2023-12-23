6594233b33bdbff8cdb75665e87c6aa1545f381a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:34:17 +0000

    better always defeat improve translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
