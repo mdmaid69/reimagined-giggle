5d19f617a91a751172b5c22be30258916d238754
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:37 +0000

    whisper complaint pot tax cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_array_index(array, item):
+        return array.index(item)
