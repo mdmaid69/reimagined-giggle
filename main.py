e3615874ff74653c08eb2751196422becc982130
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:12 +0000

    573entertainment common seriously still some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
