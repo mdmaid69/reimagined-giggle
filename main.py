c2eb84ee78de4efb6b3edafe091c703ba013f552
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:53 +0000

    communicate ourselves front regional sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
