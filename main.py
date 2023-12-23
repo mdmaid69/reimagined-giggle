4abceffd561a38580bd3ad5763d6cee3a2655ab4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:32 +0000

    cold centimetre mix Ff night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
