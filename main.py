59002ba14b75f7bfb2af4cafd01a99ccc876d5e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:15 +0000

    bus sick birthday straight better

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
