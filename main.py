6be2932345e65db1c3584e84ea36c1d112976f87
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:49 +0000

    unite basic personal mistake light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
