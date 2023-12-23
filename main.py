d6de5a6d7af2df0885981bf578c1bebe84826722
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:28 +0000

    matter same alone plate correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+for i in range(5):
+        print(i)
