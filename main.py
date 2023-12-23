1434416f93bfe05c326480027e2a7db94be2fbfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:29 +0000

    poor family object whom market

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def cube_number(x):
-        return x**3
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
