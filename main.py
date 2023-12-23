9a419cf7f68a50bd9ddc17141c13997a72ebb1bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:55:53 +0000

    destroy person old completely stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_item(array, i):
+        return array[i]
