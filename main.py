d81db0d3a01bd3ecb2b4afd264b8b5ba63eab8ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:10:39 +0000

    issue along connected lesson path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def subtract_numbers(x, y):
-        return x - y
+def sort_list(lst):
+        return sorted(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
