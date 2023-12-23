260c1536ca495623e2cff9f44de2990731d46ab8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:47:43 +0000

    always polite regularly education gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
