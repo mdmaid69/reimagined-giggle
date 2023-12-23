35388fdb5007c85b812e98fc826836ddcdc25bf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:41 +0000

    create theirs effort look float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
