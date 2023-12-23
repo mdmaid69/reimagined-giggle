36abbe487f02f64f9c8195f1767fdf495c7530d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:46 +0000

    keep tune declare furniture tap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
