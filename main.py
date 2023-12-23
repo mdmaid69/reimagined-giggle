829c2b69200bc7bbae944c9c59d30cf616a763cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:45 +0000

    branch buy fortunately refer relation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
