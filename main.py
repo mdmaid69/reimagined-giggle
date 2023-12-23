762d158d20a4db9cd22c90de7b99409a24f72733
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:08 +0000

    speak negative off invitation dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
