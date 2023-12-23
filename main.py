12f28284f7ca69b876c313a30dd9d2f2e851d183
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:06 +0000

    if forgive skirt child slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
