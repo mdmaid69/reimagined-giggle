c4b77707a9b9c87ba74879e4bb41a6a96964da42
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:10:39 +0000

    wrap night rely image organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
