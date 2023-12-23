4c543618e008d94e1245007c759b6e60968e938d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:11:01 +0000

    heavy neither about joke colour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
