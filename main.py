2df2bffe55a96cda0b4f443898f1ed0fcdf6ec19
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:28:37 +0000

    miss clear culture raise side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
