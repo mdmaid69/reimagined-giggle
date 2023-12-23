23376f8ddcf88467c40a19c42e6818aa24721dfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:34:47 +0000

    turnoff chin square disapprove makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
