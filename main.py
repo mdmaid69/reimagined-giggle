f3a473da971e03d3c3c82e3e739bbfee2d11dad4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:18 +0000

    urgent notice attempt collection pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
