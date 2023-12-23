6547fb0c3bb208b5660b8436db15d078341a5b8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:27 +0000

    priority after us special such

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+print(sys.version)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
