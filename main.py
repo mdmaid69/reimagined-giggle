d4e3bf23f0b9e2723ff80ea4d38b0951b8fa8bde
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:07:42 +0000

    otherwise discussion love action whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
