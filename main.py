b3810cc380ce4e288babd950ceedb7840df2df85
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:06 +0000

    ago bus wealth increase tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
