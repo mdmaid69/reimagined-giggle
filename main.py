e8ae6ea55a2e6ddd8d563228fa6148d0bd04e786
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:10 +0000

    rank kind transport collection convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
