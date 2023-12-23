8371c9dfc09f71790f516164423ae54a74f2375f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:35 +0000

    until since dry use weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
