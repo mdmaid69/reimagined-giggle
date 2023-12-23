19f38e3b37b9d1ec8949f29ae179cc007208694b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:48 +0000

    traffic congratulations sight see head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
