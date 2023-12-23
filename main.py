01ce236a7934c88b4191f19775a38ea5fbf75653
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:17 +0000

    across promise skill government part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
