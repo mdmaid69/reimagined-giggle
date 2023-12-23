4cc2691c7c7ecd22e87588c18a25cab420703ec9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:44 +0000

    bang behaviour capital machine hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
