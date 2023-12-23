dbe2e28c3e21a91da5fdb25050203e2cfcdf9621
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:04:35 +0000

    obvious sentence aware slide yesterday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_index(array, item):
+        return array.index(item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
