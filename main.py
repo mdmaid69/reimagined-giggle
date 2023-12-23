5efe746fdb2fb7f9949eaee420ebb7afa2c36634
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:48:16 +0000

    search underneath up wealth slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
