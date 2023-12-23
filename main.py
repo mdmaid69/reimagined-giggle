d617d20d9e547566bc5bd1e10af18a7f784f520f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:26 +0000

    cheap tread snow instead church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
