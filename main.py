d6c87df9e7e57cc6b6312bb1bf82f10ce6630559
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:38 +0000

    hide outside clear record mind

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
