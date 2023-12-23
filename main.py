a1def41a0284bb9e443da7005cc123f849fadc39
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:51:28 +0000

    joke shadow kind street load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
