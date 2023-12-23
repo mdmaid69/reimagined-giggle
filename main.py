0e83a98be7b7a2400b8dbb8cfd8527fb0b65968b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:32 +0000

    flood break crazy off rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def multiply_numbers(x, y):
+        return x * y
