5160fab6dfbc20afb9e6bfe751634014e26e766a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:50 +0000

    Ss past already regret head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
