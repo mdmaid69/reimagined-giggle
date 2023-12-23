d9ed4aeaa20d662716facfac1a5ebeb8ba080ef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:56:09 +0000

    press holdup bound provide medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
