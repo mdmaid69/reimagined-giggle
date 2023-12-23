f49d3b2f4c31e2d6fac892d0eccbb6625820ce9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:50 +0000

    whisper hurry laugh cover first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
