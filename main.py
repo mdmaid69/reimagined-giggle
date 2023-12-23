2eee8b60f5af97edf37986af76ace87415142674
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:24:27 +0000

    tell feed rich under excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_item(array, i):
-        return array[i]
+print([x**2 for x in range(10)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
