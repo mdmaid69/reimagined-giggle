c00c573fc3f1cd354451305541c3154f351517fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:31 +0000

    pump dry prefer it thanks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
