1bc9b3b5cfd3993d86e6a1c70efa6b89bfc56e34
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:58 +0000

    rescue mistake wherever honest himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_array_as_list(array):
+        return list(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
