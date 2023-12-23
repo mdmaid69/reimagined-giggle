1625174b2a19a18acb0103e4087c14dd291a5c81
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:32 +0000

    artificial kind field public produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
