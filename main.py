bb64096d0d3629297dcf2abe76dec72feaa9bc07
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:10:46 +0000

    identify cause share detail public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
