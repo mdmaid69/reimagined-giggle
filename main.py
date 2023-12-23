891d7009ef086a9e77bf6f2fafd080f0c463a8a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:38 +0000

    reasonable fear loss light early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
