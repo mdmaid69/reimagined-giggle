5fa17106a1dc66e45327f7748ad1928f2540533a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:22 +0000

    dish hang mainly hello likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
