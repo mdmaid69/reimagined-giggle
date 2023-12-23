0bbbe49a8afcab058249ba975e2d9b6e002280eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:25 +0000

    mix there mark drawing difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
