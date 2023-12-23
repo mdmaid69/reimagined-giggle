9778bbacc90d00ca9733700988e4d2746969420b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:18 +0000

    trial lead taste slice draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
