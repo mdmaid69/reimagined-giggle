0c979b44cbbfd7797f64909f4ceb7a31bbcc1487
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:31 +0000

    shut turnback birth correction stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
 def get_array_as_str(array):
         return str(array)
