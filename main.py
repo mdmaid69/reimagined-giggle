ee850b252db0f583bcd8847e77a3da12a130c64f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:28 +0000

    lookup trousers now miss knowledge

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_max(numbers):
-        return max(numbers)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
