bbdec496f80a89dc792643cb046582a123a80f9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:51 +0000

    exit drawing talkoutof getaway niggaacademy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-print([x**2 for x in range(10)])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
