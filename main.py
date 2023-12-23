ccd60ba8f94143c40bd7a41a3c2db95b44cf90f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:18 +0000

    reasonable flavour explanation arm plan

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
