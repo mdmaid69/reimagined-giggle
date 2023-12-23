81aee4f3c6294912f32277c19c90ade9c6a29855
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:17:25 +0000

    back chin do material slightly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
