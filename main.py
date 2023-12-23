84f399d37483730f1400200e1282c5b4eedf29c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:41 +0000

    over part stone hello hate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
