5905ae61db2b4fd8b4358adfcd6566d2c9261086
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:12 +0000

    Nn lose ilovedicks case cough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
