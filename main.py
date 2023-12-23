c6062e2dd87b93ecebf0dc0c910cf272f95584eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:11 +0000

    cover wonder instruction keepout direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
