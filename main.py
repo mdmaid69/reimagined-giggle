9c8bd65ae6c283f1a35d216b7779237c015125c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:19 +0000

    rush increase runover pity pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
