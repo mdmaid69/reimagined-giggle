eda31a55828dd51faff337f881a7b906e15626b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:06:44 +0000

    cost conduct rest successful university

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_palindrome(s):
+        return s == s[::-1]
