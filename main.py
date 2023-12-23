d0bd33c24f3e8cdbade0d68c2edb35877046bbe8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:21 +0000

    arrival 565end second stick there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
