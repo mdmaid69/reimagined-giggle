2961a8eb26dbd7abe88fb32d51562e6a66753890
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:59:07 +0000

    near willing performance adjective steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import sys
+print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
