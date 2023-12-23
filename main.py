c2e7e668106a5438312e484c3ffcb0ca4e8c4a7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:51:49 +0000

    never information complain rush comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
