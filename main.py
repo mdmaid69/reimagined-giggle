0f967e4b8e25a0d8822b38bdea79264030d23057
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:08 +0000

    sensitive plenty raw hold water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
