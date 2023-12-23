ad481456aae14aae4b9df83a01b280b3f4747188
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:50 +0000

    married common pain trap everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import tensorflow as tf
+print(tf.__version__)
