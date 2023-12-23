bc84375d772811f71a5c05c9977d69c32933a120
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:25 +0000

    close measure tone smart father

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
