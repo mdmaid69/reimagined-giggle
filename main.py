68eab1f1bdb50ed3b432f08b1e9ba1ea6ee258a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:21 +0000

    nature speech share hour reach

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
