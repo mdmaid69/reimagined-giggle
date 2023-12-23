6eb8535e3a5caecb743ed23401f56ffe475161ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:54 +0000

    quick fashion position rain plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
