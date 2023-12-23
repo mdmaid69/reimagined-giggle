8f6a8746530bd58517e7818053b57d6737d8953b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:16 +0000

    they rich in bread night

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
