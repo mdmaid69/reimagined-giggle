f73093a2793e67e99b934d902ceddff090172d74
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:32 +0000

    holiday gift interfere date petrol

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
