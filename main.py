fe270078256b6eb99190206217a1f79537fc1fcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:55 +0000

    rate arrive cum along permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
