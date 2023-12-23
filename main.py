a81b6a5de0219e6983deeb57afa569ed07d96a69
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:32 +0000

    attempt square shade build run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
