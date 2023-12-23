07daee021e1d48348604aaff54fc856323b2b16f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:15:59 +0000

    sure everywhere settle gather last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
