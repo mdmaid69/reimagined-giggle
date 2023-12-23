704cd9a9d525b2904dab24f365e2919b00385e93
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:27 +0000

    shock foreign while emotion admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
