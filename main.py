89ed9a9478be51ce53a5003d2c08b86141d75f79
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:19:32 +0000

    almost nurse unlucky stop comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
