bc426e621dcdba116db222d52a41c8c5688bb6f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:07 +0000

    most round less value task

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
