a7c570525e0031e4f1bff5ec445db5c49f63a43c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:50:12 +0000

    tired mine collect today private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
