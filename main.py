f4e3e490c334368bcc355ba83dcec9f086da8d9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:07 +0000

    unusual marry answer children stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
