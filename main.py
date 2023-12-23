628d378c3ec9d0896e66d6a93a2bdce377f7592c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:03 +0000

    depend profit thing reaction touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
