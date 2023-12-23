cee00ac102075a1845ddf8234eb85a46ecb8e884
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:34:04 +0000

    band danger attempt why neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
