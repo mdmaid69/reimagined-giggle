6cc473ba363b3ce26c41653cb84d650c81d08259
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:02 +0000

    Hh hang different talkoutof whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
