913359e88499f0f8a97c73ec28c4af8f21d85399
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:27 +0000

    round fit spend drawing touch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
