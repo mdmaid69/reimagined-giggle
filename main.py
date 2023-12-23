6e2bb6aa4f668df1437bebe64caf9479afe9be98
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:25 +0000

    lost clean undo soon typical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
