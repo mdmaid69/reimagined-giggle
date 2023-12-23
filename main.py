2e5dab566e597aaf7d5de3ce5742e696823312de
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:14 +0000

    temper order gradually 567enemy convince

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
