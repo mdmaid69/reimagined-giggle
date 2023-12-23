ea47a6d8cbd4830dd2b273dd8fba5c710ae6a0e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:57 +0000

    course immediately control throw special

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
