ad0275da7cbfe6a7fdd7453e1d163774aa95cdd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:09 +0000

    actually personal that language hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
+def add_numbers(x, y):
+        return x + y
