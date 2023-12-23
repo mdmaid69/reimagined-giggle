ec3412daf51d27a6f68fa173516db6095d871d97
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:35 +0000

    environment strike more hello upwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
