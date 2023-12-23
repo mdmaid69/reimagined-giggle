a2e8c1a188da5abf0bd03f82f92c05e900d94ed2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:47:55 +0000

    shine neck people nearly measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
