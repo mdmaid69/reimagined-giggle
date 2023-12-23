9f12c727a93487e5c6db5c17effe1c8eda19d673
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:41 +0000

    consist great rise suit at

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
