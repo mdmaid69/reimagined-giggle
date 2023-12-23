c1cd04f66d94c56f55082f0c366684215a39ecb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:02 +0000

    club away tidy patient comfort

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import getpass
-def get_username():
-        return getpass.getuser()
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
