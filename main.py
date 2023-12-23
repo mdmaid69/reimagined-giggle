5a28ce4deadfd2b0d53f3e921b9fdba9df1d52ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:14 +0000

    beside turnon exercise weekend rent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
