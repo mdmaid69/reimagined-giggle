7fcd3cbc7790007e058cbf23d6007d8c608e27a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:52:51 +0000

    smell equal embarrassing people you

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sys
+def exit_program():
+        sys.exit()
