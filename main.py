fb7576d246baf6e511ea9d203d2bccdf160af147
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:33 +0000

    mess importance provide to even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
