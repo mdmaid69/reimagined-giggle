f57a4be6318e88c7d4e1137af27ba67883f78361
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:48:14 +0000

    theirs range shut skill sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def find_max(lst):
+        return max(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
