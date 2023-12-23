49f83d754738708699e17e0d84a3bbd05f579820
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:26 +0000

    Ii 571enough fire definite story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
