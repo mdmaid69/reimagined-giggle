44732a67c407882ad2770e344b24801478b14088
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:22 +0000

    polite warn coffee big climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
