713f5a80649db92ec6d0c33e9e99ad11da4a051a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:24 +0000

    string rely before continue doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_set(array):
+        return set(array)
