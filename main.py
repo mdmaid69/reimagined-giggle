06fe85211f821ca60874cca94b15ae0bd08e4ccf
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:27 +0000

    post problem worst convincing organization

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+for i in range(5):
+        print(i)
