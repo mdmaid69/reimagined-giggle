843772abdd34500ebab1c34feefab6385008387f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:00 +0000

    division so object workout lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_pressure(force, area):
+        return force / area
