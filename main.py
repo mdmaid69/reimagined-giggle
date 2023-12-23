b97e1375907ed31855ad22898da28de0d59c83b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:48 +0000

    tie survive through announce sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
