d0a2d5c8722ef8bf85969a9e40d2e5b3753ca96d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:10 +0000

    use control come factory attract

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
