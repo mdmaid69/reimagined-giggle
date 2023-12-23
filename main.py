6b0869917d94a91471ab685b59dea7b8a5388031
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:17 +0000

    field Vv dress dull confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
