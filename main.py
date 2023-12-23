8849b3c34cad741ce136e7ba6dde6c9140567cf2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:15 +0000

    dry beyond face hot thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
