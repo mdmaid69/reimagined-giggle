5cb3232486a1ca03f2bc1ec297437e53860d675d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:42 +0000

    treat stand acceptable interruption regional

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
