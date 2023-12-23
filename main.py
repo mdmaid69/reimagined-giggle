542a518a43e7998fe7b9a7928069578395cddf87
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:26:37 +0000

    train reduction spring wild have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def is_even(n):
-        return n % 2 == 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
