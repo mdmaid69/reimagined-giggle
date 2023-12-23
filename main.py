443954675a4ea964b5ac4c777c745d1528fa5415
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:45:42 +0000

    match lost afford forward than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def sort_list(lst):
+        return sorted(lst)
 import collections
-def create_stack():
-        return collections.deque()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
