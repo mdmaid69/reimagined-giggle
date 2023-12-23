fa4543cd32e28d814d8245a74e826f3440f526b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:04 +0000

    effect past ocean tap protest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+for i in range(10): print(i)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
