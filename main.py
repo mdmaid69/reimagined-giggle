3e5ec4c18feaf3227a454189565bb4e5879f9416
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:08:06 +0000

    judge drive with Tt cut

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
