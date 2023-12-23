ced723cfdb97b21c523f4041c43fe989e381156b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:36 +0000

    really dark estimate protest main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
