70f63e1b8ece193e88707eaca7656de9446cc1af
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:08 +0000

    mother name town black Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
