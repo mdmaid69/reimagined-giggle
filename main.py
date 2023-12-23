8be8763c4080604c9155d83c04185b3b9710b1cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:33 +0000

    past off completely upstairs risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(lst):
+        return min(lst)
