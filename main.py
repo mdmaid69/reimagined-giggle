6ca16a273096e6aaecf15967c46b17b724cfe6c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:30:39 +0000

    draw priority jump hit Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
