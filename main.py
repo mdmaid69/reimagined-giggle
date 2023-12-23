3cb36180c78a7e3041ed34ed861f65e7385640ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:47 +0000

    drive bear medium bush number

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
