de11802b445bbc7abbbdf1806c50c2d6bfdf6f75
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:36 +0000

    to pen smile trade murder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_octal(n):
+        return oct(n)
