f4e55c51fc64e244bc6351d711fb8df9cf465598
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:52:20 +0000

    glass eat proud stand problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
