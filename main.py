95bd5d08542cba4ede87c5bb3dd761eb5ab3e87a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:35 +0000

    eat lost bridge scene getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
