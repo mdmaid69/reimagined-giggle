6498ec9b33bcaf476b81d1359bf7f1fc03b3aa0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:02 +0000

    Oo plate play hungry by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
