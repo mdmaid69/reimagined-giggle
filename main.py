8ddce6b44ab841bb74b3a51a92f9a6e5547bb345
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:29 +0000

    gift last quarter cut disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
