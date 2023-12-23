05f1c9e138be83073dd6b2933a8cc51a8135d700
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:45:01 +0000

    lose speed sink careless fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
