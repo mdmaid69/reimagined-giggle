4e87245d80470851880c01a0c24f1d706ab957d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:20 +0000

    talkoutof personal preparation invite table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
