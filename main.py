48d88e96b6e5c1aaa94a3b25ea95741f016575fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:11 +0000

    season necessary talkinto tense fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
