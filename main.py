db33135262d91b57a0ce3d324645719bab2fe455
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:49 +0000

    pull shrink sweet clear influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
