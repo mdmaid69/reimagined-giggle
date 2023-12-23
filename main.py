f5a79fe29c302a5190b69693dad8e81c8261d44e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:29 +0000

    exchange perfect centimetre willing cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
