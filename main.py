1eb5e6037a409aab0addd299817d7488bd68081d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:56 +0000

    ourselves arrangement command quarter speak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
