85b577a244b8944181515ebd0bfb5ae68ec9d995
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:18 +0000

    pressure if petrol laugh church

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
