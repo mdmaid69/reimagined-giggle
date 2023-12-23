fe675ec83f9c4a49d790c87424b14c5be60c26fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:19 +0000

    nonsense drop example sound ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def subtract_numbers(x, y):
-        return x - y
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
