e77ac246dd1f27246bc89889cec09efea76520c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:27 +0000

    activity direct lower police their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
