5ef2ae2b41095e2e7992ec2555efb6572bb8a174
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:57 +0000

    defeat period each complicated charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import tensorflow as tf
+print(tf.__version__)
