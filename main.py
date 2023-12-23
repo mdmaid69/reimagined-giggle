645ee61ab5fb4d4230439d8ffa7e7b3dfc195838
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:46 +0000

    harm sentence follow flow habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
