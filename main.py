027e0de39e723fd9fb16aa4a0a0b53bd2f80f24f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:52 +0000

    other major flood necessary lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
