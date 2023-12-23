22c1afca88abed1d3233af8f26d3ee2a74a3f830
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:43:18 +0000

    plate father OKorokay fresh free

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+print("Hello, world!")
