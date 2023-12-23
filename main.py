4489a3efe0f5430082d0b825281f049977f3851d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:39 +0000

    young region remove passenger tell

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
