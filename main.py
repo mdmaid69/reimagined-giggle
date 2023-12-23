3f8ebe6cebb46324bda75676c193feee9350a8a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:48 +0000

    they match explanation run report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
