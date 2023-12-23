9d06d123d0afa76c7ef5da4e48a04bbacc0229d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:06 +0000

    opposite side marry offer somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_even(n):
+        return n % 2 == 0
