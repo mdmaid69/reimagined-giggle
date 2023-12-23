74f8118815758e335d5414e49d7d0aef8bcf5085
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:52 +0000

    normal pump agreement afterwards foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
