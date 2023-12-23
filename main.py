b37afba87a567bf1196f036db03b4f1450e5c45f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:38 +0000

    fresh sleep male time gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
