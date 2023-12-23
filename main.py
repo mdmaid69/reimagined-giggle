89ac502d2066312a96f779a5b73c3d387d4583c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:15 +0000

    judge design set ought light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+def divide_numbers(x, y):
+        return x / y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
