ea93d71f6fbd5a69ded52036b1391e7db30e7f1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:29 +0000

    embarrassed geton later section rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
