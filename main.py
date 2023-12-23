e0dd7fe6e5201c9454b8a15558345f0673306b09
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:51 +0000

    feel cum scene neither I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
