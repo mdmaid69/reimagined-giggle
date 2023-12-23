70a46bbea404cf9bfb67cd4816b1015ecb0b7731
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:38:48 +0000

    strict below comfortable fact honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
