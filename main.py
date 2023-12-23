01cfaf03903b0d5fdf1f3bb093f95362c1ec7253
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:17 +0000

    priority border little task much

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-print([x**2 for x in range(10)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
