76d0ebef06b774fb55a9002168b0633fd57e7e0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:24 +0000

    geton change intelligent pen swallow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+print(math.pi)
