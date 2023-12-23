bdcc64add32f58109a3452cf025571e8d80a4afb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:47 +0000

    beyond fortunately impossible breakout ours

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
