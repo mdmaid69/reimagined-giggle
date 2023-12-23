ea8ee07c75391fc301966430158e68a64cf3f275
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:40 +0000

    other callback female need bring

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
