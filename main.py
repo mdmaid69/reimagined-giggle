bdf118753838206a1f2aec37af7377e9a964f323
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:50 +0000

    gap save exchange middle tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
