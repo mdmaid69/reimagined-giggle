ee97158fefb901d6ced2324e12e61d42e6cbec8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:05 +0000

    organization afternoon grand dish precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
