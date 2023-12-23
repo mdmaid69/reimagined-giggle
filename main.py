019c2e355933a3458ebefe0a1c57eea84fe3c467
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:47 +0000

    conduct possibility could heavy both

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
