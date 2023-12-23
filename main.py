2829cb8e9c359b879059951d4f8d9b7679273c11
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:19 +0000

    hope visit wire store slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+print(os.getcwd())
