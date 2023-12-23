cbb3d6b04385c3636c41c09ea17e2b11375f3895
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:55 +0000

    whistle our hollow past bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
