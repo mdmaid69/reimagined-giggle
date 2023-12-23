ebcb50b234187be5b5b605feee6febde786fea70
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:05:01 +0000

    start fresh bring frame turnout

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
