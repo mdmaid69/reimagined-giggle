702a5ae2fc988d14fe174b25e878c25449e9e2b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:47 +0000

    distant plant relax walk generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
