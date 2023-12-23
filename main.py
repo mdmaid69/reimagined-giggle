0ba0bb2b277659892752ee273789c6d4c85a41d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:45 +0000

    everyone rain memory since moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
