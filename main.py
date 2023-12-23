62dd41f5c25d5f0582778e43a1a85fd6a04c0446
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:56:19 +0000

    march afternoon whom tour holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
