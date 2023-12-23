265d9e225fbe12ef1b0d7ce06023dc7620aecfe3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:19 +0000

    complicated pretty ago as motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def get_current_time():
+        return time.time()
