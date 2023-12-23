9371f008cdfbdfccc5931ab3a4a6af44ed600768
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:04 +0000

    strength pride announce price my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
