7fd031bbbc6a4a44628959b6f075b1585b4748a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:19 +0000

    seriously steep prize club even

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
