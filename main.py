64e14ba194148c62e63f8d4a0ba682e0b32030ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:25:26 +0000

    offend walk form aim dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
