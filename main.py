a45b9a5080ef8d625c112d56646484521bb4b7ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:39 +0000

    talk careless rate advise strange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
