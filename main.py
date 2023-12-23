6f73ea88c0b670e3df9c5f56e5a51ca1bfc9b5c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:11 +0000

    among talkinto please hot lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
