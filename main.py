2e74104c86581bf63f30635f64acb947ab8c00a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:31 +0000

    light arrange contrast soon proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
