8980a7cafb697a3ead6467bf7776358f7445d145
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:37:20 +0000

    needle mark sick scrap blow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
