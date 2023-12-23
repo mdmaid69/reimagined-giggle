25fdb0d8f402a7301c3dcd357215e1c7f569d3a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:14 +0000

    would bring substance nice light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+def calculate_speed(distance, time):
+        return distance / time
