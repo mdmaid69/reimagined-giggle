a0aa19f7b3d1c7d9439f49d987cdd909c522fd25
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:22:25 +0000

    destroy collection spare second child

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
