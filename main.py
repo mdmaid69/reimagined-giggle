aa883ffb3f176a11cf0394066e2f07bf8a240547
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:04 +0000

    bin satisfied copy so exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def is_odd(n):
+        return n % 2 != 0
