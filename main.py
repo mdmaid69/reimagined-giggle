ee43acae99b9013b2ea37b94934bada625f689ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:03:54 +0000

    over trial heavy show mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
