87c5f70db8e93d687e16b9be2992505921b60c9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:21 +0000

    black eager fast chase ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
