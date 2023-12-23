21426f53708b32896cb659d7d0aa3eae5bc3445c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:45 +0000

    exhibition his pay wall important

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
