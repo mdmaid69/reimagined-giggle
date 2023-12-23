c3a7ed177eff89adaa6196304c266575edcfa7f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:42:08 +0000

    meaning excited push thought sale

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
