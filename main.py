6fe39d8b7237bd49ace59b5ccf772551046728fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:45 +0000

    mend rise line show hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
