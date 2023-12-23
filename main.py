44c9fbfbe76346f2453152854a2932105cc59cf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:44 +0000

    stage run blue real special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
+  def convert_to_octal(n):
+        return oct(n)
