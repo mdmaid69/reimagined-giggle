2dd3f4f61dc57b2101f38c2e4c56ea18d513e639
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:40 +0000

    blame dead respect occasion young

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+text = "Hello, world!"
+print("Characters:", len(text))
