49734bd72d23aff4031b1fce8f2636b675dd7d03
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:28 +0000

    sound lie mind attempt child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
