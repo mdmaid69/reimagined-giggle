5413d8d788ebd005f9d7c8daa859c6ad34d20b0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:57:09 +0000

    comedown Hh situation talk there

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
