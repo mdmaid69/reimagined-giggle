fbf35a6e532722b19a56e181dca006182b205f8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:56 +0000

    ashamed breath unfortunate without important

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-for i in range(5):
-        print(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
