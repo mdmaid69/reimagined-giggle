9dcec2c6b1dc9cd25bc929ab3f72c42a1b27be8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:00 +0000

    sensitive present real ocean process

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
