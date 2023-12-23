4062a21d279d963724e060201eb89dee469615c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:54 +0000

    mix no confused very temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def cube_number(x):
-        return x**3
+def get_array_typecode(array):
+        return array.typecode
+def calculate_area_rectangle(l, w):
+        return l * w
