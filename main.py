33f2163b0eb22c15f9ebb137ee44d46bff3964ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:30:56 +0000

    walk careful ear only meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
