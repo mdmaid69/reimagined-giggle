0153e131421a1ea48bff3ca940cc101a1c49b38c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:38 +0000

    tonight lucky strange wet tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
