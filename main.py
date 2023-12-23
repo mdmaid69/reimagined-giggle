ed1e4d535e8ad8674814402be06e551608bf9379
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:12 +0000

    doubt last teach family sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
