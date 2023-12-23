3a70d5da7c8c23757ec3989d6505b0bde0100163
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:46:51 +0000

    difficult sleep lunch fail debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
