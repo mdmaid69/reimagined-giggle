20f6bf181bae4a0bfbf52ba7ea743b9ce0463f3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:47 +0000

    view thing too us protection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
