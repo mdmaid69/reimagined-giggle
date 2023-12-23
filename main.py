bb95d5378634aab125bf8bd0ff0c67d59628a4b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:05 +0000

    protection apologizeapologise often custom crisis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
