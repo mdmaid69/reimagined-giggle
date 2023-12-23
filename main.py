89a6eb52a71f376f4a037bc7f26f9e1f6c071ab7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:47 +0000

    organize curious please heat tie

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
