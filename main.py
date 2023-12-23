a1615f718e8903b78f7b827c019547d0594934af
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:03:08 +0000

    soon hard false accordingto medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+print("Hello, world!")
