11c9372f65d28ff34c62d2a6215467d60697803f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:30:19 +0000

    confuse relative however brush new

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
