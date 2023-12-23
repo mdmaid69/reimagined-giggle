5926c8fdfb1fc30f9725ed1a92d3f4321e2810dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:11 +0000

    anyone bored nature base pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
