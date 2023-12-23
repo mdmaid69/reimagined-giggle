5c5f0429c7a03249e0d344fa02b39007c9583e34
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:21 +0000

    recovery tight handle few breakinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
