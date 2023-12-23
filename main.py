7628c192fad423ff9e5e377aec1048f703e2923b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:35 +0000

    wall offence set breakdown sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
