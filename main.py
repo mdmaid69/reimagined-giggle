5f48b7b71157e8b2d6042d27f59ca07ae305e456
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:49:30 +0000

    clearly appearance takeout people arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
