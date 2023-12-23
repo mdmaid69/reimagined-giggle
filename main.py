4e16af7d1fb5ed325d512c012ccdc54d2358f707
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:22 +0000

    allow admire goout come with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
