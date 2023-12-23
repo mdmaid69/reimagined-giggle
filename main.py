855794c5ffb7fe1733b4cb29501b97aa540f59d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:52:39 +0000

    vanish brave secret hole yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
