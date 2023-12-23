f7936a50e3b2745f5e68410f9986582a2a2e15d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:34 +0000

    surprising comeoff flow along direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
