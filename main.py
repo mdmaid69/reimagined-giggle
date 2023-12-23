05132081a344604236fb2d5a3a851693a8a6e23b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:34 +0000

    late bend unite aim bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
