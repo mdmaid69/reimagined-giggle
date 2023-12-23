cc2eac265c6d2d84f76263424c3a8a24f71dc10d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:02:20 +0000

    turnout skirt value smile reason

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_typecode(array):
+        return array.typecode
+print(sum(range(10)))
