520b9ea9034d0847aaae8a73eb59197d4d10790a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:38:54 +0000

    separate declaration complete interview hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
