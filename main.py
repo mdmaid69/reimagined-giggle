5b844a194faf7215d527107188833742fe5bf01a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:31:35 +0000

    check seat pause explanation rough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
