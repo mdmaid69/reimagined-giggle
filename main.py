18c00c8e80d0fc83b127f8193f97b31195334033
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:48 +0000

    either direction sure degree dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
