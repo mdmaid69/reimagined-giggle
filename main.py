f161cb6670f36239c1a434b8f3c293d70d359de7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:23 +0000

    convincing brush interruption clearly win

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
