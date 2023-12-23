cbe5703b5c4a53ed721df9b43e7d7288eee18165
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:23 +0000

    dear mistake fault very protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def square_number(x):
+        return x**2
