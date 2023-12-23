e6b730bdc3ab60c2b8310a8e65c6021b813468a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:37 +0000

    after port giveback sea last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
