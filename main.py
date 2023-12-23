4876be82562826c88d080d2c96b52d880155c8e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:01:17 +0000

    open neither exam rob final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def convert_to_binary(n):
+        return bin(n)
