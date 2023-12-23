ae0ba0a004dfab20261345d0ef01388ff31cc782
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:02 +0000

    which plain taste impatient tone

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
