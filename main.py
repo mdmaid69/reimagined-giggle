68a4dae4fdcb1c506c37046cbdba2ffa7a0b6c59
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:49:51 +0000

    better science recover treat stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
