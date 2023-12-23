8262e3f97022ecd732cb902ce66fe7fe510b2ba9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:21 +0000

    blind soft Gg emphasis deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
