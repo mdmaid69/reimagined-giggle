9a3be3d22e5986b8ba93b3b5111167fb6dd29623
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:27:24 +0000

    between family conduct everything report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
