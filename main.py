4732425023da7c862508ca5a88486736bf885a33
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:50 +0000

    failure wash lock laugh our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def roll_die():
+        return random.randint(1, 6)
