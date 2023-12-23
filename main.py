40fc0b3f6b8a98c1f57447b9ed9e9be8b00a6906
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:52:37 +0000

    curious normal trust practical concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
