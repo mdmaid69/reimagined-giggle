660312214ef08441f862e0302c96a9ba0fbb3d41
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:12 +0000

    squeeze nurse zero strong nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
