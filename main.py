4ff44823bb7eff7abf093d61f5d8907ae74bcbc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:39 +0000

    dear embarrassing man thick general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import datetime
-print(datetime.datetime.now())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
