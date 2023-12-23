56672f109882190e74d5b556dd3d8d72ad6d311b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:59 +0000

    university basically honest detail old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_as_str(array):
+        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
