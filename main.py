90092cb35fbe345955e3772079d0f015c4101a09
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:07 +0000

    rare tap she burst challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_min(numbers):
-        return min(numbers)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
