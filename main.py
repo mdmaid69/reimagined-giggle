19d4350f2a87dfa86895baced617f5201364d3c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:49:08 +0000

    maximum pull uponon talk ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
