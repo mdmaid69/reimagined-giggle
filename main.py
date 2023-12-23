d6fcab09d848fd293882afc33b0b7ba072c80884
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:35:07 +0000

    change hungry often watch exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import platform
+def get_python_version():
+        return platform.python_version()
