a684a29202b6562b8102c0ccb1c58f149dcf9ccc
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:02 +0000

    reduce government avoid catch check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
