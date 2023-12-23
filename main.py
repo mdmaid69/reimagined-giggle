4b5bfbdadc6faf24339964173df2c066d5987e9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:37 +0000

    private win pinch taste anywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
