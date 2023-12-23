778b391fd74e4331d8d1e8ecc2f1af43bfd33aed
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:36 +0000

    move thumb excited mine Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
