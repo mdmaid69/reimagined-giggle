337670d628f1712017fb01410a75f5ea0ea9600b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:22 +0000

    opinion cry name original beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
