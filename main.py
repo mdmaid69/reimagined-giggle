78121c7ff9f19751772250856294c1b3995ac35d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:45:36 +0000

    until makeinto recognize last glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
