64287ac299f16dd1fca04ce946b88cb287acbf04
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:41:23 +0000

    past attitude firm round thing

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
