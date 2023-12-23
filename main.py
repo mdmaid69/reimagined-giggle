1e52e95f0493360193583177bd21e0236a822493
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:10 +0000

    now close stop once sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
