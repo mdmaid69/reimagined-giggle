0c52dea3568381655b74bc98fbd10c8f42e14c5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:39 +0000

    bit increase address popular before

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
