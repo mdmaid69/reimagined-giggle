1ab75828176a4f9d535e575245a1877ec354a71f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:08 +0000

    capital subject sense kill rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
