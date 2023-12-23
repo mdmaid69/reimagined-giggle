825d1baff1e7cd407dc4ba12d7c7cb4a65efe064
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:52 +0000

    operation patience thick if rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+print(sum(range(10)))
