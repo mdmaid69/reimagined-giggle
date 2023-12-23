d8a34fdc20c220ef4c1e2e9a8d9151af5ab5eb1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:06 +0000

    deserve to round consider bunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def sort_list(lst):
+def sort_list(lst):
         return sorted(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
