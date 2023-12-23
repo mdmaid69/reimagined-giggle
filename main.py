2d75d65f3c3c83a575966a6ecf7f1fc8e5963ca5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:55 +0000

    failure uponon face if learn

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
