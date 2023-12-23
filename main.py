694396b6f466200d0df5bf62a92bc449375b4645
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:23 +0000

    temper pay sail allow fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
