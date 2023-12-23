07768d0e34f9dd41a982a75d3278e47350381db6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:11 +0000

    test object Mm office say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
