2a1f34d5f30b15125d5d648a35a8a9bb8c733607
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:30 +0000

    mood debt language stand whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
