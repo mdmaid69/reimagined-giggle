604902c3a73e98faa8ed4732767212f99ea19a93
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:37 +0000

    drive matter whom comedown Mm

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
