3c44b8221a57c59efc969ce9efffa10ff9d232f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:09 +0000

    pronounce finally point callout test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
