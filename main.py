611be8aed0fad06de713fbee9809b26f43fc97a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:37 +0000

    appearance recognize inform sorry sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
