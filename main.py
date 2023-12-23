2560d57796457728101c688d98d299b814800f10
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:43:24 +0000

    opportunity more best survive count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+print(math.pi)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
