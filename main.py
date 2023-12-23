209f1e4df1d91f4c43ffde6d6e7de99bd61d87e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:30 +0000

    comment criticism inside whom bet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_max(numbers):
+        return max(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
