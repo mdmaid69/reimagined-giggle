7147f41444c3e8e24df6fe9f45e77433bbc5d579
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:30 +0000

    meet reject wave use understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
