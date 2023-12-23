baacf00c302518457f294ddf350978d1fab87c7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:50:59 +0000

    otherwise couple regard emphasize interesting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
