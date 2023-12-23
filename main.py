e79cdba3d2ef4f25f6c1bd62380f951b4564773f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:19 +0000

    put include corner else convince

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import re
+print(re.match("h.*o", "hello world"))
