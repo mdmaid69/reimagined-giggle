8b318646c317770a12f518d8a18111212b2b9916
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:39:36 +0000

    round left skill breath clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def count_characters(sentence):
+        return len(sentence)
