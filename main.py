09d407e1b184706dff9c08469a05b18b7e15282b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:11 +0000

    day relax noisy inquire pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
