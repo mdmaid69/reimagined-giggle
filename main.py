f942e6586433ae82fdd334f0ca8fdc5f126ad6a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:18 +0000

    remove pride live law even

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
+def calculate_force(mass, acceleration):
+        return mass * acceleration
