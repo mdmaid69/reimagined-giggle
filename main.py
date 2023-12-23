d22b3d728c9423cace853c3e696d5c4736fbbfa1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:38:32 +0000

    drive strike pleasant immediately exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_access_time(file_name):
         return os.path.getatime(file_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
