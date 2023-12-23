6b768e270cd240cc49f08e2a8eed094f8ce5d1d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:51 +0000

    wave afternoon stop pocket never

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
