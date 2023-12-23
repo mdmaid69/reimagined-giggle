6d25500a7c28e6db75036f4d664cde6284117147
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:54 +0000

    flood usually relative upwards shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
