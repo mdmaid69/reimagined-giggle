047dc5f64cedf7791e060f0b912484a7d4b1645e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:13 +0000

    discuss somehow distance exciting charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
