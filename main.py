20bcbbf919385bd5bd4390984fdc30745620a725
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:56 +0000

    what Rr work woman feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
