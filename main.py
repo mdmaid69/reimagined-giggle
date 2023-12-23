081d2e2057d376e48091c7e3b545f985cb577c06
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:38:45 +0000

    second lose address back listenfor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
