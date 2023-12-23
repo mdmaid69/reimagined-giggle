59d970995ea9aad648f0874ce472ac81725ff0a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:32:30 +0000

    lay grateful pause above face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_min(lst):
+        return min(lst)
