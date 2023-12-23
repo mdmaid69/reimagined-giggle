860bd13563ab0400f768b53c5a71f3c22fd354a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:18 +0000

    early sticky thankyou only immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
