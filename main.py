5f1023cd2efe4ea728261a68e55ee9b3602748fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:57 +0000

    perfect picture further label strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
