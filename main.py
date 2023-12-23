b815f831f5cf86ad157f70a8f53e5b63c9d5dbf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:38 +0000

    view tidy region left as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+import collections
+def create_stack():
+        return collections.deque()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
