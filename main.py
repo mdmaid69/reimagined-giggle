abfd7b2798016d1f4490301c1cdea19e9e0e6f04
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:05 +0000

    exploration avoid waste smile gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
