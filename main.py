f51dea2fc4bbd3d08ead00b1397e622984f9f109
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:04 +0000

    write difficulty disappointed possibility least

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
