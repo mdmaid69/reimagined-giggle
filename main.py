dbf94bb80a00858adca28db053bddcace8016533
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:26 +0000

    skirt grow tone skin whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
