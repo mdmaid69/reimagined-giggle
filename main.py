c01df6c28869fee35825e3ccd4d7384b4c08c0ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:09:42 +0000

    attempt precise live show noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_repr(array):
+        return repr(array)
