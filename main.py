710dabdab758a9bcc36855d7204d02e5d2417e54
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:53 +0000

    similar fond conclusion demand disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_floor(x):
+        return math.floor(x)
