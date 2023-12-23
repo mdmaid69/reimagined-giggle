583ddc03055b5176de697d2e23384e917244be13
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:47 +0000

    shallow question earn bad every

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
