3df583b9ac9178c8ab078c3af2f4ca09fcbe547f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:01:02 +0000

    rough beneath dig judge hospital

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
