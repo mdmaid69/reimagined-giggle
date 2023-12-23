4b9c90fc378b483eeed6b32848e430b48bdb51e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:52:55 +0000

    along sea through interest charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
