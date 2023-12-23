b39aae1fcb4afd098e2d8642e1333f0cdb4c5146
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:42:34 +0000

    shock touch bin term plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
