63d5ef0e88cf7eef579b61a2f6ba5c13382aec18
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:50:16 +0000

    care lean during answer earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
