c98546fa636af4c03febd07dd19d5a78380317e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:00:55 +0000

    practise middle holdup Pp official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
+import datetime
+def get_current_date():
+        return datetime.date.today()
