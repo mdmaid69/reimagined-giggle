5eda2daacdedcbbda309c29702e147d95688c7a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:53 +0000

    deal completely pain tone communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_max(lst):
+        return max(lst)
