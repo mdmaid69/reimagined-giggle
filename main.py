10f73cb9e081b4a7167da27fa5d8f21f8de63400
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:18 +0000

    call spin yourself promise stone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
