6961d8e8d279c9fccfd3e8ee8e0243b35fbe894d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:42:40 +0000

    rob certainly least bye division

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
