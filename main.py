f4b37b4b07a0baff43a5172bdfddd8e5f3aa0517
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:57 +0000

    formal lower exercise pretend pleasure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
