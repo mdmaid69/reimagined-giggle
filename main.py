e481e24cad6d305fedb955aabe2c68a9ac821fb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:37 +0000

    comfortable whichever float test correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_floor(x):
+        return math.floor(x)
