7bc4cffb8c547e22ccaf1988b60af4b36c62f224
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:29 +0000

    past life Gg record command

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sys
+def print_python_version():
+        return sys.version
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
