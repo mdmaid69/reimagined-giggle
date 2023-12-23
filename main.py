b94b3af91e0f47c04ab33ffd8f10e98c370e29c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:51 +0000

    close stick collect design government

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
+import sys
+def print_python_version():
+        return sys.version
