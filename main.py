9b3377abc852ac80fcd059d212d8a359cc7b298f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:12:41 +0000

    chief might wash much breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
