f52e65bd0fc76843da9f1c32d20cb875292c3d70
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:53:24 +0000

    swim comeback shallow evening match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
