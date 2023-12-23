ee52495b74ac03c22973d0eb89550f96375140c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:29:14 +0000

    break bringforward use average visit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
