d2f356ae6153e01c943b1ed4934f16626326ee1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:14:38 +0000

    visit coffee defend port floor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
