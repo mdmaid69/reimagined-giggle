2ac64b5ffc01a60444d0b7f464fcc314e3311147
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:59 +0000

    scrap meanwhile callback payment rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
