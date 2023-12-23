46f8d82ccbbd1e85a111778bf75503aba134b416
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:01 +0000

    pool store side even kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def convert_to_hex(n):
-        return hex(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
