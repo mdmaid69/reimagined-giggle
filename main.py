9e127e0114ae37d89440a0a625052832f6e99083
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:06 +0000

    hope return getback past sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
