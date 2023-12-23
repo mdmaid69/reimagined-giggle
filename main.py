7a7e45fcce684f2f24c9c52ba457b81417c081b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:09 +0000

    switch getout runinto account choose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
