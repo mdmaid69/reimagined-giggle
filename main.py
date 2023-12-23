179310d711ccde82dfb77165d224a2f9c6e7f4d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:31 +0000

    concern exchange keepaway succeed whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
