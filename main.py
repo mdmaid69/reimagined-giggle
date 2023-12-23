c05f18128daba72115611685d9a9e17017f3d2cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:55 +0000

    apparently habit set half minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
