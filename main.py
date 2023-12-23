6e8ca60ab01f7b9911e53bee3ae96cc9d67588cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:21 +0000

    Nn getaway cook wash unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
