6ee8d2b07d269f3d739a482b0982f5dd5907447b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:11 +0000

    word your behave clever prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
