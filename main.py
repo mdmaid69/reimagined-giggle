bd38e2e9e7908dc247c78918abfb793b97cb5884
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:54 +0000

    report basis lesson point impatient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
