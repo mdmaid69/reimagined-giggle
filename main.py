5bd32e0b9e7a3a4036ec9fe1bae4600240e414c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:22 +0000

    action himself shock use scrape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
