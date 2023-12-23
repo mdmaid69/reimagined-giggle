d162990020cecb68ff00b838af8d4c339c014cdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:00 +0000

    heat frame winter bus vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
