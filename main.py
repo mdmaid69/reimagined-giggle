e90859dcf8e2e2c6bed573a3e4cb257c62e65d08
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:16 +0000

    shop emphasis difference practice damage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
