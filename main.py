fa42d32f92c8bec3411acd6c8f7bc0f1f27af211
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:31 +0000

    central bin line people trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
