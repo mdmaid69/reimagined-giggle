049fd2bcaba27d5964ca55ad70aadc6a8b2940c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:44:44 +0000

    sit under appear claim difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
