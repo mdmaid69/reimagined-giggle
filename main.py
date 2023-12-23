7857aab86bbb876793e4bf9ac219059b1d06b165
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:37:05 +0000

    build suckme 567enemy square star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
