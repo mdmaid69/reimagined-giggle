da56028432dcc1cf8edc0ec8bb2e54539c499a97
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:22 +0000

    yourself force cross design makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
