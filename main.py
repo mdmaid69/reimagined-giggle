01b1a58b3abe94980593ca37f0a169f79d8f348a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:11 +0000

    keepup too comfort make nowhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def square_number(x):
+        return x**2
