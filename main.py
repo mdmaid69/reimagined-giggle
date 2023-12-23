1f0f3f3b9d4e737fe6141a4ebcd858c3a9add6a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:49 +0000

    scream who used image curious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
