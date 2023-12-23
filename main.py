34c535724ce3f5d04716c5fbf0b463263b5dbf15
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:05 +0000

    healthy beat hour importance team

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
