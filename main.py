c43b2f3273cf0ef2f237d2823a2fa581de6e807c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:30:50 +0000

    similar along pen know fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
