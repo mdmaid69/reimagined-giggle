c17c046c362981ee5a6c25427673ece0aee108ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:59:44 +0000

    minimum wide burn shall key

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
