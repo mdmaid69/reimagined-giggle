518814b37695bc3d41b58d231da54c161c337415
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:48:57 +0000

    break practise impatient mistake nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
