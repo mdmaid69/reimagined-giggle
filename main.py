2b0fe6db11cff29d9bfa7c1db1ca6bbc7fbae282
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:04 +0000

    back complain fold good beginning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
