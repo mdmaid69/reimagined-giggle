51cebd4c996277888632af7fc4d1e6020e4d62ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:29:03 +0000

    body Ff nowhere across exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def convert_to_hex(n):
+        return hex(n)
