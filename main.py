62bf42f0a12896a9160921c52a079bf5fd797355
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:18 +0000

    used physical hair mark forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
