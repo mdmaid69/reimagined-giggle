4fb0a16161c2e4a706ffcf7dfe067dc7cef9a77c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:23 +0000

    ability obvious hold arrive getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
