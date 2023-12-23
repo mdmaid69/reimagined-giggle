60e2111fccdc4c77dd2e2b56a37e8bda1e50d254
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:55 +0000

    guide noisy introduce dress callat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
