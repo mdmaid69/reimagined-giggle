bdefd5eb24f0b6b2e471726351d6deca0f0bee33
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:29 +0000

    ground suppose suffer pull match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
