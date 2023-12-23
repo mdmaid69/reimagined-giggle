28f682835e74b65efe27c1f9da21c62fc34ec79f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:14 +0000

    male fear forever owe power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
