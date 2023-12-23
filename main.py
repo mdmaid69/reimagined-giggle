20d2eb9689714c1652e7a6024b939e5ab329b097
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:46 +0000

    yes somehow before several young

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+text = "Hello, world!"
+print("Words:", len(text.split()))
