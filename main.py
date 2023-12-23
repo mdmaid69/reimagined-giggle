b70be934f53823268b5ee3056e363f5b6036ab7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:07:21 +0000

    thanks let circumstance eye pipe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def square_number(x):
-        return x**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
