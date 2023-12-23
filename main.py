127f3474326a74aff0166f1dcf98626497763776
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:37 +0000

    basis door bad wave area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
