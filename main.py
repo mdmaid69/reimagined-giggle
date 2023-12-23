50099e5f0263b02132d772d8140ab5695821fc21
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:42:41 +0000

    seriously charge stare burst dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
