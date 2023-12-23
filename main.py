fb47ac6419f6ca3519527458843d126adf7f5b31
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:57 +0000

    free if either land disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
