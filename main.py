268e5c0a6d88198e4f35950ecb4951f6a027ba01
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:02 +0000

    limit except read wind niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
