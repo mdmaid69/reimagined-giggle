8be8b2ec2e546b3f5d49e3ccab72ac54d8c67420
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:24 +0000

    group bank giveout myself soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
