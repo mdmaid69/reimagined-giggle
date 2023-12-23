114d51a8f3e05e9600020fba4e00053e496b4e0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:26 +0000

    accommodation lookaround bread twist possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
