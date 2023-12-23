b6fdb1c94c1bc7fa757e833a41a7c210e71ceb39
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:19:58 +0000

    arrival recovery play reserve OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
