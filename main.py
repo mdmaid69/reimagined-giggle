1116e7a34403ae3c8ded6c2377a88017640d7550
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:17 +0000

    move back steady scheme patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
