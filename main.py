27559beb27f6fb37734868fd2ca87b2d929b961e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:22:53 +0000

    week fall concerned send professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
