ecd50142547c7055810f9f531ae086ba92c0a002
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:02 +0000

    copy left develop reach consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
