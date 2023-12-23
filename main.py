78bccc7fffd82cbd5414b6de22e3738f2fbc3345
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:30:34 +0000

    along special Ll will proper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
