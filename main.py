2e0668ccfeb754787c51072659ad615a19613ef5
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:16 +0000

    through case Qq like date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
