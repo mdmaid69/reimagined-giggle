437dee6c651a11108d36fde461247632a3fe1f19
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:44 +0000

    runover food stupid slight 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
