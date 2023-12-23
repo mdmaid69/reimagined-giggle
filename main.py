81b2480a0aa71762f2b8d870e5acc798507d0f08
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:10 +0000

    play serious count trick smell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import sys
-def exit_program():
-        sys.exit()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
