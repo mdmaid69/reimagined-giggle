863bb0e2e83a2e61c12ef4324e81fb230a196a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:43 +0000

    spring tomorrow remove joke position

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
