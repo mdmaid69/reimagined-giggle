a8b4ad837ce1d4cf1a1b9a493583dec1c80f19b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:33 +0000

    influence increase precise dig behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_work(force, distance):
+        return force * distance
