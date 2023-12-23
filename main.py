d5fc9844f9b7cf7bec1b7dbac7d3c50caeaa545c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:55 +0000

    operation pretty always line port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
