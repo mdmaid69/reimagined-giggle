ae84187e3dd92c425d31a0bf4eabda26919e4464
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:28 +0000

    soil accept ignore treat sort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
