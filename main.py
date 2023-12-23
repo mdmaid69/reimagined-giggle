0e44ecadc122aa15938d9db26ef887f500115ffc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:16:26 +0000

    conversation discuss environment exhibition supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
