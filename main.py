e4e7257568747e9d3384c40b3c85bc6432f52c2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:37 +0000

    act ring final storm mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
