87178e766a72e9ea0f63bd1373f309afe045f532
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:58:26 +0000

    dare activity health possibility any

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
