db00b187aca86a48290b296c14eab4ad1379f276
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:30 +0000

    interfere store speed breakdown section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
