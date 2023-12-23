a1428ca715b6858b7f0ce84c7547a8a311b3729f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:04:27 +0000

    previous against freedom stop spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
