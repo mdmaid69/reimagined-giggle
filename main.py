d1366f42203b27f23ff758fc32437637cfceba62
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:29:59 +0000

    mistake can back Rr apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
