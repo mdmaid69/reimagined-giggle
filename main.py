5590c418bea42821fe5c3c8663188307aff52c5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:01 +0000

    nearly crazy disapprove goout life

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def cube_number(x):
+        return x**3
