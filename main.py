6e830bc91948c8368e3721711392f58ef710ca92
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:39 +0000

    family straight mass squeeze someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
