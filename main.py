e1237929aa40009630d8cda88562b5318a07cf6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:25:44 +0000

    note position him shout clearly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
