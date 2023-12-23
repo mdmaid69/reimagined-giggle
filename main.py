7ef789a4b39004297c82a2fc989a4e57a0be46f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:15 +0000

    rough street law once thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
