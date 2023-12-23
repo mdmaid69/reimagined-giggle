fd3361fc7d1564fa91628758ecafb8c3aec743c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:01 +0000

    official exciting set among mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
