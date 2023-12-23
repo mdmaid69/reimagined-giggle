c9a0ee0fdfe03ba02b093854a9c87dc863910cfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:50:53 +0000

    display cheap ever door callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
