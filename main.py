0ed991449680036b49143547d458937a1bacffc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:58 +0000

    promise tip forward little out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
