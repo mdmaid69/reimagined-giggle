795a975e70bdb42cbf3b2c0557c19e0b7892522a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:40:32 +0000

    uncomfortable send sea night rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
