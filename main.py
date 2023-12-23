34adafcaf8dbf64dad5133cf204cfc495db8ce96
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:04 +0000

    bridge 566end obvious keepup sea

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
