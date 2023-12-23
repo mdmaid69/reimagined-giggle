109a3c78fb7a42be4c67af626681a2da9bdb1b33
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:01 +0000

    real whatever pleasure miss attitude

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def convert_to_octal(n):
+        return oct(n)
