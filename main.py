e937ec8142ab506055474be28a2b42a1016ecb23
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:39:28 +0000

    strict terrible word rest noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
