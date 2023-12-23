cabab523cfa2bc6eb87749e25da24b7de9354c0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:18 +0000

    cross judge any gradually rest

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
