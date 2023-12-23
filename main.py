ebf83d0386c4d1fb1c92949f007ab2a9eff58c71
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:51 +0000

    rare temperature sharp apply lost

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import os
+def get_current_working_directory():
+        return os.getcwd()
