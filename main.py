110527f8d9a8d246e94797c082fe94a1ca30f302
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:08 +0000

    cover visit approve fairly wing

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-print(math.pi)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
