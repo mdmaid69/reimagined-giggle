5a82e0baedae0de0bfb75454763a88fc7d616e0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:35 +0000

    us and fond wide break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
