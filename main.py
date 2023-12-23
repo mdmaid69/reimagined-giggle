6737fa7f805a9e3d04ab42a06a426b000d074753
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:09 +0000

    operation getoff themselves above tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
