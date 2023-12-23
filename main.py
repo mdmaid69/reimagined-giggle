026f4da749e08f7ff0aff6586fae212ad14afceb
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:16:37 +0000

    exaggerate comeround rent refer fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
