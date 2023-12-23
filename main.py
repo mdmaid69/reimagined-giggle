3e0b0fec27134ebc942582c30eb196ac7ad8eee0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:16:08 +0000

    art top brave peaceful favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import random
+print(random.randint(0, 100))
