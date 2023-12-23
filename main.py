ffd0d09d19a465af235783fe71dc2061571bff09
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:25 +0000

    list behaviour approve politics agree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
