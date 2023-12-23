8743a7e02b663423640f739f0d0090afeb8e31b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:44 +0000

    wheel your opposite repair coffee

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def roll_die():
+        return random.randint(1, 6)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
