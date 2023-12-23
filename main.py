ce18798c492d55cc7a76768c2f21d644918d644f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:11 +0000

    official opportunity need dark disgusting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+for i in range(10): print(i)
