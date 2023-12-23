97c47b05da4183d7183a0752e553a2e382e9b5ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:46:44 +0000

    circumstance steam general city otherwise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
