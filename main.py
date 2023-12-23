ff8c2112f92160a8f7712810e853c0df79dfe0ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:29 +0000

    within section interview complicated beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
