3050090bf6e2ad58b7f69ecc5512717223d1654b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:04 +0000

    contact us line trust trade

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
