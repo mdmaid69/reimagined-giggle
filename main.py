2cabcf7ba06a876b9791bdceb5f118c4ddf3488f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:06 +0000

    join party powerful tie temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
