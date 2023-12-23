71c06f7c423b3dd8ffdef9271790b37c1b96aad4
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:57 +0000

    prevent break sudden keepup base

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
