bb36a3b107c5079fa3de5ccd30268629b8bf7205
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:02 +0000

    even hope hers search sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
