79e9cbd7157366c7d953a6fd068e1b0961c64cf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:00 +0000

    miss organization string yet collection

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+for i in range(10): print(i)
