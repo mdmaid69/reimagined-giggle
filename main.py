94e6e172eececb13bba90a891fd98fe7bb29ba17
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:33 +0000

    principle mine getaway protection pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
