95f51d7f09fd0deefff64711b1eb9166d73deeb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:49 +0000

    heart side comeback interruption must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
