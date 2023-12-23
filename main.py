4247acc656e3011ac8b4b0ed13f75fa9bc9b78a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:19:43 +0000

    turndown contact judge cruelty phone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def square_number(x):
+        return x**2
