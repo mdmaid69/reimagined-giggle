5d05620ee043f2232e1e5bee642b348f24b508c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:23 +0000

    century stop be children production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import time
+def get_current_time():
+        return time.time()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
