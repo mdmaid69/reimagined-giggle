992494a5746cfb31084c19be340ee74e6e618cb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:45 +0000

    its dead wish commit reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
