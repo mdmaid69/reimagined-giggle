d9a80d0d4bc74caa967b4094b2bfd2da37e331e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:13:31 +0000

    think young danger person satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
