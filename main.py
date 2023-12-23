764576d23fece1811eb7989bcc1fada8a71c14d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:15 +0000

    progress big call lean form

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
