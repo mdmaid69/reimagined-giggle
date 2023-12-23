c6a98fee8df6c8be36f23b0f31012e540c04a376
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:15 +0000

    positive door year Ss occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
