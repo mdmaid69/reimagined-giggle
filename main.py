12e3bb7bbdc43af7703111f5606c0b623236b132
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:08 +0000

    go meaning getback dream coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import sys
+def exit_program():
+        sys.exit()
