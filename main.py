6d0ed8f20485a410ce004cf38607e7e78fad618f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:55:13 +0000

    goon fat damp affect story

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
