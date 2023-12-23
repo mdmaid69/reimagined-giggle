f3f93ea3076f4b62dffdd09dfee28894366bbe9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:57 +0000

    breakinto hollow judge turnon order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
