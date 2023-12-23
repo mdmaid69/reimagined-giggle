5db19a56acf3d8ba6448a73591d5d52973fc9a61
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:44:23 +0000

    he method interfere right dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
