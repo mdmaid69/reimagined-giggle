8b43ba6b26ad633c4b4bff69d5feec8329acd055
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:55:09 +0000

    prevent comeround peace bottom hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
