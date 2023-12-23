c14904f9834fed7d274e8b437ddafff7ab9a8d13
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:49 +0000

    settle slip impatient party onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
