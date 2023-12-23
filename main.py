b3e4c4df4dc0a54fcb2719df5d09d59db612e424
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:27:10 +0000

    hardly Kk safety tune pin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
