e62c3022736221c291900eb1951c9cab679ebc9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:59 +0000

    behaviour loose degree tool protest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
