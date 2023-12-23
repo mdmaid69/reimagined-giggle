2fc4f1d39cb630d0d32cf98e4782a81e39143c45
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:22:08 +0000

    rid calm sink region murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
