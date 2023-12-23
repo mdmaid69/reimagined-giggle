a6eba6cb970399f2ca50a4d3e9ed5f984bcfda23
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:31 +0000

    quiet shop height laugh business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_current_working_directory():
+        return os.getcwd()
