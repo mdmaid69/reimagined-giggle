4fe1211007c034bb46a000beb3f57eed3a340128
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:29 +0000

    difference situation scrape stiff open

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def find_max(numbers):
+        return max(numbers)
 text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Is palindrome:", text == text[::-1])
