e2144f9b2235f7a0e67d748c31c55af9fb02e899
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:38 +0000

    consult 573entertainment pause possible back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
