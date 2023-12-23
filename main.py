eee82b91b4aaf7a1671904e85b684689cfb2d716
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:51 +0000

    total rubbish warning 573entertainment understand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
