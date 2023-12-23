96e0c740d8409c6c3efa5daa5d1b64f826bda03e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:01:47 +0000

    violence represent together shadow it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
