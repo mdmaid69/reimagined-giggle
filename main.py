cf240496a6c6cc4506da4dcf7a92bef5ec880c49
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:29 +0000

    tragedy discussion along estimate event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
