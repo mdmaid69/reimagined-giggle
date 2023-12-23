24e77eea82328557b6b4cc2530be183c191dbb6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:38:31 +0000

    couple offer back worse inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
