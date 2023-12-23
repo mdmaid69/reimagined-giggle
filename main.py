79b30f8cc2f5dbe27b3861e6524e0b6254bc1db2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:57 +0000

    throw earn born convinced pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
