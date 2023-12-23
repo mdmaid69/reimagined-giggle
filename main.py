b3758649b7cd69a3f8d4bb8194f3b18e58fa3c47
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:22 +0000

    delicate unkind brown favourite perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
