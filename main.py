e5e463d88d9959c07964f39ea2d7ded6dfcf20b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:06 +0000

    this continue walk march recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import platform
 def get_os_info():
         return platform.uname()
