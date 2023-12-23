ccde0afc272481906382f0de2b11966b066568e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:41 +0000

    raise drink rather formal stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+print(sum(range(10)))
