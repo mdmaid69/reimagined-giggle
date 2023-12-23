b0b2db9077adb516e3d3baab3e8dfa65257f6c47
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:36:07 +0000

    exploration joke pride typical close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
