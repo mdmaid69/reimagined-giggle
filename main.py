6157d3e10e9031da5a22a9f6b225a211e65d1091
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:36:06 +0000

    accordingto reason off mass easy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
