fd92a6bb9d1e4875f1b57570365110fb23dfb8b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:45 +0000

    565end future tip arrange tooth

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def get_array_as_int(array):
+        return int(array[0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
