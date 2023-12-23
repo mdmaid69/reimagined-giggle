55062fc98f6ac45421514f769d0ee1f2289d3ca2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:21 +0000

    guess recommend hour high cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
