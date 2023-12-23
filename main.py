577947584eda779cde82fbd3f71a67a77f3de0dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:33 +0000

    female shout easy organize impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
