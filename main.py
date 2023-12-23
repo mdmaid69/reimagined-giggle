4fe31a7121973bd0bae87182796e3c3ce0ed3e69
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:36 +0000

    opinion wall ahead up exam

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
