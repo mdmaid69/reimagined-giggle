0557b722502d27e60032ffa4ea6364fc1ffb141f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:57 +0000

    swim check save steep minute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
