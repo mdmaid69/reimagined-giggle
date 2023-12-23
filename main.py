c9e1658082bd0b16125817250bc1e7cf398aa57a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:33:32 +0000

    food getoff game ours press

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
