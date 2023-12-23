a51823a42e794c96af7652f84e90db414b55625f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:41 +0000

    rate outside dinner compare compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
