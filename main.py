d52ba31002fe4147380bb7490a5c8b3617d28bdc
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:03:27 +0000

    past entrance lock fashion wander

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
+print(sum(range(10)))
