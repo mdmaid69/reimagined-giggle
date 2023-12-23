0234c1382ab3ada67fda4f24f2b972b2d8f19743
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:48:24 +0000

    there land part thorough possible

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_str(array):
-        return str(array)
+import random
+print(random.randint(0, 100))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
