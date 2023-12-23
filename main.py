ef34cc8db716907d6758bef3d92ab501567723cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:38 +0000

    comeup lunch kick rate possible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
