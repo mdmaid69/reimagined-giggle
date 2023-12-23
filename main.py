104da49b0d90bf496c21cde03f60d0d5a4f2174d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:14 +0000

    use turninto centimetre regular contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_work(force, distance):
+        return force * distance
