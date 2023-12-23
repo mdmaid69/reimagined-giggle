4f41c6b20c83178a2ca311764668788ff6b173cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:04:09 +0000

    well study grow talk choose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def count_characters(sentence):
+        return len(sentence)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
