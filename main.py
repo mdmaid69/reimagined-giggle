054d77e662a2d45a51cfbd5720bbd7b3e61e50b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:59 +0000

    fight remove middle prize pot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
