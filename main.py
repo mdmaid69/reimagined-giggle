0bc922aa89e632b6833643010c873e9a0efea6da
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:24 +0000

    blame total tape prevent wherever

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
