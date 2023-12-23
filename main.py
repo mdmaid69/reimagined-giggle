cef27faeb8e93700b627a73217684bd8192640dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:06 +0000

    takeaway muscle floor build performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
