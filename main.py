38cf2f41d465b4bd21763086305183d8f3a6abcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:32 +0000

    law inquire tip ring medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
