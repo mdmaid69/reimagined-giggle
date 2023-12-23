ca1228c91e3c283a9f827e9ffccf2ffe22f400bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:45 +0000

    crowd especially impressed there makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def reverse_array(array):
-        array.reverse()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
