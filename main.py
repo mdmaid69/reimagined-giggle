2be2d9675767cd9df30c39b485f23be01ad5db8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:19:05 +0000

    storm pronounce lot accident attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
