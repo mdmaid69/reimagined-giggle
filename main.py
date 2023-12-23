95e2c4c29ce77199bf56b3720d84c72a8fbf1074
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:21 +0000

    environment frame repair please study

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
