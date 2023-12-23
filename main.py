5723fd909f6ade2195f917576e1df474d216f8fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:00 +0000

    swimming frame raise foot schedule

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
