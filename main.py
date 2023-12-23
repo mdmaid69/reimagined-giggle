e2b481f6e086e0765109618cf11581d0e70ccd26
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:05:49 +0000

    Nn rely turnon trip bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
