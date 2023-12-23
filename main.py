5fedf4835f47f0f2fe9ef1c51613ed84c7ce9e02
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:53 +0000

    point Jj bad slice die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
