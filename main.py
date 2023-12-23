6c61e50bc89773cdb5263f2a54f921f29abbfb2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:00 +0000

    judge buy month passage introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
