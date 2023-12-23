79d88920a6bb3c9ab866d83e78239785b844c1fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:55 +0000

    fit open clear bury ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
