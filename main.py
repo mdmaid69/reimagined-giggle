7087925bc31b46a2ad45350bd87388d3f2e50e55
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:22:38 +0000

    emphasize square fault better kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
