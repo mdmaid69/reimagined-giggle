650c3477bb3beaa3463f83af6be451df7a29f058
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:47 +0000

    area direction care understand drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
