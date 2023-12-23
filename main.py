17b19939ef78b1c81ba03e62f124e973b15bc80e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:33 +0000

    love direct heart protection act

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
