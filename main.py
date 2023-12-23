c55cabb49320d0674c45d582211089f6c7e3267f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:32 +0000

    lock contrast fond what but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def square_number(x):
+        return x**2
+import logging
+def log_message(message):
+        logging.info(message)
