0710e8d4d607b46eb538922175e4bb2688147b62
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:56 +0000

    hurt urgent similar able science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def reverse_array(array):
+        array.reverse()
