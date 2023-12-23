5dc10aedc3f8a6f6a87b4679c0bea97b89f41b06
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:26 +0000

    tear beneath wave fork than

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def cube_number(x):
-        return x**3
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
