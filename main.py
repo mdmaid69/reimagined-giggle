c3d5c9f33666f0b764d93bbb202881bd303ccc04
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:23:10 +0000

    range live justice vote so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
