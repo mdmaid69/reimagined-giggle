58fa497a200d1a8f7cc757c40c08f9e0f20d05b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:28:48 +0000

    wire shock concern compare good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def count_elements(lst):
+        return len(lst)
