59b421f73c2ccbf15a02d94c59df8dcfd167a2cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:25 +0000

    eye smoke fine empty important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_length(array):
+        return len(array)
