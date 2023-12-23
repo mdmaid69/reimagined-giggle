573153e1f39f379618639d546213e48dceb31f35
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:38 +0000

    once judge test group fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
