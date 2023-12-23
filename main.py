922abd08e03615e44f6b7bee6d38d98300a75509
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:14 +0000

    busy scene pure telephone age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
