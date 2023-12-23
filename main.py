528822c3aea38967685f2a5c9130e47c6faaf8ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:38 +0000

    high score interfere part hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
