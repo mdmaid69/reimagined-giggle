80b6fe9ce9a786ac88270451c547ba04b3a47f04
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:53:43 +0000

    rush treat beneath ago nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
