8e342109274ea9477c19854f6024301879f09e41
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:50:08 +0000

    frame correct slightly job within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
