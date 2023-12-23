0d2750587e222b29ede1703fddb1b1c2076b90f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:14:02 +0000

    wave certainly length greeting determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
