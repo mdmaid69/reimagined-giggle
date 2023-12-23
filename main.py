7cb52d42c19b4d5baf6c1c2b6734a5a7b2a38a4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:12 +0000

    recognize comeout cough change sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
