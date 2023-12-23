50df22f71d10582d6ba2a3f38be084419cc37997
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:33 +0000

    well prevent professional together child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
