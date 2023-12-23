21d15fa417c17c6a11aa04d17ff47925baa65bda
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:53 +0000

    means course talkoutof remember thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
