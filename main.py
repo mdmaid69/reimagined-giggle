3892c48451bfb7fa7117cf12703c5721fc94e44d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:37 +0000

    new decide exaggerate ticket interest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sign(x):
+        return math.copysign(1, x)
