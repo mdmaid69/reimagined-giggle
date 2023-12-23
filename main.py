74ea327e8159403ec05d5123141191478a99e9e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:06 +0000

    deal than active murder estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
