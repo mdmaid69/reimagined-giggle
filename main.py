d49c9fab4f0e4cbe217d89a21e939939cadf4c4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:33 +0000

    Nn check high transport fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_base_name(path):
+        return os.path.basename(path)
