d1749d1b22bc4439752e817f7ca5b6cd7ea1ab77
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:15 +0000

    favour goback(to) height breakdown forever

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
