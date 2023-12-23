bad37d495e3a9c1777c53c92bb9fa5510347ac42
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:47:49 +0000

    open blame message plenty reply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-for i in range(10): print(i)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
