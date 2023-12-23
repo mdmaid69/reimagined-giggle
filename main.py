ad4d3c07be72bc54f8164967c392d88ffdcf4358
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:28:02 +0000

    punishment attractive pile grade command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
