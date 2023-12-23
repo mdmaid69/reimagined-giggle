b833bcf9a729bd77445750d016d451e3485852ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:09 +0000

    upstairs considering hold from lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
