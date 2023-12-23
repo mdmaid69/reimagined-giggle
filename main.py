943034f8af9624a4b79d718fc12fd3fd1c58c4f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:44 +0000

    callat sorry name dead yes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
