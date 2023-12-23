41ec63933ea5040c9fbe9ca3abd4ea1b2dd18406
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:04 +0000

    stranger responsible conversation event female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
