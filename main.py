2089438569ac3def411c35f509bcf91fd8bf1742
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:43 +0000

    challenge imagination work determined general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+for i in range(10): print(i)
