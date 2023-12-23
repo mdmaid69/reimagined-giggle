4fa2522ef472ff1bf0e1654a0adba87f6d970073
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:03 +0000

    region great fat neck niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def append_to_array(array, item):
+        array.append(item)
