cb43b7798eb370f190e9c7ce66aa8306658478d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:33 +0000

    murder choice anybody behind part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
