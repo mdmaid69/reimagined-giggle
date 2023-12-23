02a9f5a3cced2ce401a69b30808dc84b6f4739d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:52 +0000

    that play mind hate against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
