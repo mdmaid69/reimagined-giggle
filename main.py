7540eeb18337c2cfa2272c76829ffafb84e3b108
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:36 +0000

    special Hh rid suck appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
