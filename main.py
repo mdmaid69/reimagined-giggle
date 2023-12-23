b3c1ab08d731f404a98c1b1e0a51657765204a51
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:42 +0000

    start limit shock train view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
