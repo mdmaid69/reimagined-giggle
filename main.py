f05ee22c954116362701c14b1bcfbbabe11abd11
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:05:35 +0000

    needle formal dish roof stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
