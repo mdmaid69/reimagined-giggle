d04959065e48a3f81cd553edf69765f8df48f0ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:17 +0000

    second worth situation calmly worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
