1c65ab302922519845989a3e37e0a23eda58eb41
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:14 +0000

    nervous lock refuse burn regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_typecode(array):
+        return array.typecode
