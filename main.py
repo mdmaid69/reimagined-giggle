b62cdf8e50a7136cec70dcf15d9b370730d79564
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:28 +0000

    tight delicate sign strange relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
