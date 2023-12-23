8ca04acb15a69a506336cb4fcc0d2a6fbd088fb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:54 +0000

    opinion disgusting control pleasure exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
