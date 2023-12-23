2a46bd963a42a70843cd89ef82f98264bdd711b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:23 +0000

    fun claim style against giveout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
