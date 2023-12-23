cba629068ccd73763e6af699d546281edae0683f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:27:30 +0000

    convenient sentence cope consist week

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import collections
-def create_user_list():
-        return collections.UserList()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
