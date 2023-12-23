476228141cdace40485c4fbae317ae50728b5a8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:24 +0000

    bad schedule offer switch especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_bytes_from_array(array):
+def get_string_from_array(array):
         return array.tobytes()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
