03c62e660b3c7b15d578d4f2d2b019129054c903
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:52 +0000

    open boat sister prefer keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_min(lst):
-        return min(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
