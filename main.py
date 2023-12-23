120e5a25068c6bdd088a89de95e390d8cd115d3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:36 +0000

    unable deep instrument trust need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
