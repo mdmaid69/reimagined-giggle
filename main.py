881914460a37e5fa442a88f014f0ba6bba86393f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:45 +0000

    careful language duty their himself

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
