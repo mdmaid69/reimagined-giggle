ef1d0eda2dd37294d0bdf38dc136341b8d370087
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:47:06 +0000

    honest well might piece excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
