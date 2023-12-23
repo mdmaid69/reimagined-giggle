3b8c150f8037c8283f0e819d0901d9142811f35f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:17 +0000

    carry mine fast to noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
