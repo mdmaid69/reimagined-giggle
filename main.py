e055dbcc98772c4ddc2dedacafb22ec37ee23adc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:11 +0000

    succeed please why goback(to) often

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
