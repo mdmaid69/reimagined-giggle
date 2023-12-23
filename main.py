8d2a2d02755629ba014772182dac0955752c1d65
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:47 +0000

    expression shadow describe scene clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
