5d444d22b64be05b645f2eb94c6d1dc73ba4c079
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:18 +0000

    weather injury job cum tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def square_number(x):
+        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
