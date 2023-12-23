7e1eb55d116a22b4774e0f2a9a6e2a4944670b3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:43 +0000

    hard scheme fat main fetch

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def add_numbers(x, y):
-        return x + y
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
