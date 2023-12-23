acca3030896c6f6c7c71fc6e7d034b2790567f44
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:46 +0000

    consist black repair left about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
