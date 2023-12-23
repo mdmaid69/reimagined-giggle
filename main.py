ac16268b8ca60f46dd06ded4e7ef13a95b4b8f86
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:16 +0000

    sweep song vote honest inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
