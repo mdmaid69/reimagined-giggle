a44f39808b0e9fb2a6c0136341b4db609a50b0cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:13 +0000

    Rr hardly guide giveback alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
