84fdf97d24e119e09bb558a750cba9d6d4760d86
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:31 +0000

    away involve year smell correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
