05ad54da5c49e4fb272d9999d8d27b600066b6db
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:14 +0000

    thing listenfor group as phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
