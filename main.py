159ff5c94024236b22895a46b47e3fa73c709a8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:30 +0000

    bored goodbye cost spread fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_set(array):
+        return set(array)
+def subtract_numbers(x, y):
+        return x - y
