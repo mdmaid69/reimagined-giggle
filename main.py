b108c8a9bcf66ab00320f0ba8da1461e321b936f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:27 +0000

    connected begin promise field similar

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
