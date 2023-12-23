bf0eab72575cdeafa21988a16c4626eed534ce21
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:21:15 +0000

    quantity comfort most official gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
