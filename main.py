b8f35a94680dabc540e076b034f794a47203cb79
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:37:21 +0000

    miss acceptable hole interesting meal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
