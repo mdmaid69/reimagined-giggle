2220ce86fa76a094ce3e6da9871543e4caeccc67
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:27 +0000

    male after spill television loss

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
