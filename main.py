912c97e578d02cf501d5d1d1786707eb90700cdc
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:42 +0000

    through fire warning thankyou past

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+print([x**2 for x in range(10)])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
