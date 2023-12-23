b2bdd5e0f1f22109e6743d8f02aecf9097410bb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:39 +0000

    church agreement turninto attractive do

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
