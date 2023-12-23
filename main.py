92939822698cf85f7713d31a1fa88c5899798204
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:40 +0000

    friendly light cost bus wealth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
