fba9b8808144d5a51adbde9b55bfcce6d47cda1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:09 +0000

    outside sometimes perform dinner forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_power(work, time):
+        return work / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
