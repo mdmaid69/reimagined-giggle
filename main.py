11f3ff24631b866562ad0585433b7655243f833f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:21:40 +0000

    treat pollution workupto first happy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
