6496461c0315a9a44d1b576363c194bd1f6efe00
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:50 +0000

    goback(to) child thought print tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
