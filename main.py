1c15aa176356c8a7f1eef5966fe2f89b217cd1b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:38:26 +0000

    correct he floor recognize always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
