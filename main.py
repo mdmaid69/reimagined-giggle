fe980b40276b59181d9ec35b68427050c3002e15
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:57 +0000

    chief yourself national rank own

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
