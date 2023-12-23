6cb01fe250a68c2c98d33ed21ae66926c2bc8f44
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:47 +0000

    regional treatment shoulder become crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
