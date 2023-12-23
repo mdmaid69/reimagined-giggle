2a9c2661d78c714e4433742758807f9ea1a244f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:25 +0000

    concerned lunch soon gooff suggestion

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
