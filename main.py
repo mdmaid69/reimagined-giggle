b4b35f15ffa9021cf5ac8e68e67debef6bb414d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:41 +0000

    excuse fill behaviour remove slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
