1c3ededbb36e87eb5f9edfda02b243330f80c64b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:24 +0000

    glad face instruction chat our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
