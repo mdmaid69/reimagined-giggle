3ea1880c62093d25be75512d38be093d6b2d74d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:38 +0000

    how treatment stand tough level

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
