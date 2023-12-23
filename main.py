c6b86bfa6d48e35fccd1a5f5e5a91539adde59fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:28:58 +0000

    give wire kind obtain defeat

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tensorflow as tf
+print(tf.__version__)
