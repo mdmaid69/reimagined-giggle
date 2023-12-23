f7481482759144ee3f687901aa87804ba88f0a40
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:05 +0000

    pronounce finally comeout neither theirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
