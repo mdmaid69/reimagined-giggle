f76071f1030481089691da9dbbb3395bcc896c54
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:33 +0000

    pen kind weekend lead payment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
