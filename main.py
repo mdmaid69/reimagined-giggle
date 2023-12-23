bf63490ce9c94fd476324cd2bb6c869ded8a17c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:08 +0000

    gentle along lose Zz completely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
