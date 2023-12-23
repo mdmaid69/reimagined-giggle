499afd14235c2c5fa74760b94da79d2917a107fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:30 +0000

    train hold trust delicate beside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
