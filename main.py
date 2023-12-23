6e47cf078d705c6f2dc303ecb973113bfccd13b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:12 +0000

    impossible traffic contact surprising bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
