208ef0a2722f5182beceeef94fa5e0857cba1b5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:01 +0000

    rather order under fix equipment

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
