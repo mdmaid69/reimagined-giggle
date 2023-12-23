7abb24650aafb55e8aaf440c616b4bfdd4eabbf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:36 +0000

    contribute polite big 567enemy exercise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
