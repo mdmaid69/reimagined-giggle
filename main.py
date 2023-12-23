1d36184fc09278c8db3af6869e672294081cc83b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:34 +0000

    also perfect common whole aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
