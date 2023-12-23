6e4aa9b36b6a01d0d16195319feac00a9a2e01e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:18 +0000

    grade begin unlikely separate themselves

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
