721f8a7d2fd7faf602e98c89969dfb1f45b1b5da
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:50 +0000

    hole tough goodbye rich ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
