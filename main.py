4535e68a48fe499546a9e6e0374aaa0056ed2ae4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:10 +0000

    sort argument push tough respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
