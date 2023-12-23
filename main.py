1e6b893a1ac79b3b4a2c5a5c901caf058bd58631
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:10 +0000

    survive swim extremely silent breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
