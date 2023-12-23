2b40c5ca4e3ac05f26b919c0686aaee03adec40b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:53 +0000

    loud calloff train shop glad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-print([x**2 for x in range(10)])
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
