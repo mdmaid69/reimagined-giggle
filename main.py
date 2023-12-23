e0c178c3eef000535810b3a226cf6b2de0206152
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:16 +0000

    person file work official advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
