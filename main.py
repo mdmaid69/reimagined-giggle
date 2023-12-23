936f26e6aa7b0dbc9f0402741c816ea1f82de53c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:35:22 +0000

    none block stare mistake relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
