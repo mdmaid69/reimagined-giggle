4b3d133cacf2a25e99dacc474fc2ea65f1dc369b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:14:49 +0000

    alone body trial pronunciation completely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
