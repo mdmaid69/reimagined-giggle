0ab03a406eda7455f6a326d6e9396c55ccb53968
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:20:18 +0000

    Itself space surprised hard anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_speed(distance, time):
+        return distance / time
