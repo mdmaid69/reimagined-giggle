d647641cfce51f842e362744ad70669e1116bfc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:33 +0000

    cheque glad person interested subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
