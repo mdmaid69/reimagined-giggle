1e2a4da160bc79f04bb2fa3764264f57ffaec3f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:35:01 +0000

    soon round formal certainly different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
