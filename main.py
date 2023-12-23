1c12edd8ca69881133b036d58431ef0b741c699b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:21 +0000

    mess place trust disappointed slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
