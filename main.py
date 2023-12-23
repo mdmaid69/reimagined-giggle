689d163176b0bbd7de963306aba7895d0c1d73e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:31:15 +0000

    refuse us thinkof violent left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
