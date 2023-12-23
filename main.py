5cc7ab3d4d4054b639ef89f2a14c19f3155a359e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:47:41 +0000

    hurt achieve muscle handle classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
