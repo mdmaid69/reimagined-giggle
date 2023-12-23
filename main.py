576d171c7e956b5b5e57deb84d66e5febb38751a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:05 +0000

    passage activity declare correct scream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
