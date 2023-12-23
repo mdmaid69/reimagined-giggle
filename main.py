085633a3036eba6b4bbb9353584f6fa59b6e189a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:56 +0000

    few decide basically house turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
