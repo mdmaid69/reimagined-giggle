1dac7d48e4b8df330e5954924750bacf2e7de0d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:25 +0000

    definite track form while hello

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
