8c65ceff279426c19db0bf1c18212129bc1f73ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:22 +0000

    wonder stare guide pity lower

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
