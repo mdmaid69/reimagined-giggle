4f37ecdc63de5cfdb2d46d410ffb0126b999887e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:36:58 +0000

    bringback label by exchange birth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
