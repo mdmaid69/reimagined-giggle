d29780b4a4c4ebc7e2dd9f4b0eb510d7c2ff7f5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:53:54 +0000

    place towards sort brave dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
