afe43416027f8a255a04e5b81b572b8e16b51846
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:17:08 +0000

    plate bury room both worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
