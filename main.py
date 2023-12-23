02014b41a6632f663c574b0b99318055386c594c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:44 +0000

    opponent under reasonable mass attractive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
