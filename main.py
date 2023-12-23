cc59adc7cb7b63033f7baea0176788b1d15fde7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:20 +0000

    attention owe line name minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
