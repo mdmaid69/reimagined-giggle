2021fdfe7c50b90424016f9af3840d7499002a4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:51 +0000

    century central common possibly official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
   import time
   def wait_for_seconds(seconds):
         time.sleep(seconds)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
