994649c385a37d8e45a2624acde2ef57780d7914
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:56 +0000

    decide flat rarely still contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
