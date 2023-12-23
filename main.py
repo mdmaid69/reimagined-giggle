37db20ab9bca2c82fc5995dd5c69c693f4734a26
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:50 +0000

    near bill free floor cope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def subtract_numbers(x, y):
+        return x - y
