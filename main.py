5fe92061c94676ed1ec426671afd5224893a51b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:37 +0000

    whenever turnaway protect arrangement good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
