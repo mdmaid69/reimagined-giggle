444e3124f754489081b26454ab707f70a092dd52
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:37 +0000

    nor suddenly somehow callat meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_item(array, i):
-        return array[i]
+def sort_numbers(numbers):
+        return sorted(numbers)
+def add_numbers(x, y):
+        return x + y
