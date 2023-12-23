2aee3ae93fd366008755e6a0567230a2ad80ad49
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:12 +0000

    community suppose television deliberate safe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
