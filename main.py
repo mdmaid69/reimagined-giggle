20c6ec9728a18400b64c83ef5afb3ced6e5f6149
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:14 +0000

    rush in negative our wall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
