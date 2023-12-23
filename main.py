0537a452be0c139e2d424d725de0ca59062b7590
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:15 +0000

    company trust move building food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
