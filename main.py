c45e0d6013c9ef3d72cbfdf108a80b3a75e9c32d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:58 +0000

    shall mark perhaps benefit even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
