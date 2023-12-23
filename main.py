bc482b3242529ee9de3cd5382eea9f0ebffce9f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:53 +0000

    sell after correct easy grow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
