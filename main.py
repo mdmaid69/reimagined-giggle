7042c49e346b94bc9ea62af544abf8015187a72d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:45:17 +0000

    evidence gift borrow anyone turnover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+for i in range(10): print(i)
   import pandas as pd
   def read_excel_file(file_name):
         return pd.read_excel(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
