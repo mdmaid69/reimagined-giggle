e75b2a599a267a26252d06bbbbb7f1394b269700
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:19 +0000

    physical soon substance Uu recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
