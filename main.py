6a7f11fd833fb3a2ca154b3465e127be99e04244
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:07:44 +0000

    me disturbing minimum last swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
