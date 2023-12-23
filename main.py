0582980076217f6d15a24cb8f274cf70d788c4ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:40:16 +0000

    goback(to) eat show cut between

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_today_date():
+        return datetime.date.today()
