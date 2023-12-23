afa00ed08c5ac90fe684b275db7810a7842e146d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:28:04 +0000

    crowd buy clear bye smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
