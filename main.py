98992dcb39d454143e5425a8571552c251e95e3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:26:45 +0000

    sentence recovery pretend shut study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
