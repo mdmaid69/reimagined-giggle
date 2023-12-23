84c15db1dd9d8fe40cf646f421dc9c6b60cb6fd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:57:31 +0000

    clean target tread profit waste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
