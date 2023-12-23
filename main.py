c54413e20a31bed399518dda9e33647525e146d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:10 +0000

    creature them computer section day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_queue():
+        return collections.deque()
