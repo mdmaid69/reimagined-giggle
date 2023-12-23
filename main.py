8d26dcaa7407a385bce252346ab9d83e1e9225cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:49 +0000

    talk grade crash blame sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
