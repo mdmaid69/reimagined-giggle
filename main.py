5d0eef58b3d920509c2a0b80a6dc67945fdf166f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:56 +0000

    without trouble crime authority geton

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def greet(name):
+        print(f"Hello, {name}!")
