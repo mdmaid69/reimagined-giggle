5265c0dccc086ec5ad5883dc172cb90a0cfbd19c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:35 +0000

    later prefer occasion quit every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
