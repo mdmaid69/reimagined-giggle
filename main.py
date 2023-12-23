67b08f41fdafdd993d41e54d95f9548cc996da7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:31 +0000

    until everyone meanwhile steam push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
