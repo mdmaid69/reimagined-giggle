a045b16145699051bd73185bed01149c712b3c1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:45 +0000

    lonely apart trousers of bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
