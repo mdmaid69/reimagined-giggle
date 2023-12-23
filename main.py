97592af0093b15ea125d81306271f574a08e4e5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:32:02 +0000

    remain cry opposite especially marry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
