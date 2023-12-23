5f1b9eeabed4050c7043de7445f89ed4e3546084
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:30 +0000

    warm introduce base scare respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
