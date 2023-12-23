b175d9506a1c0acaeb3063002c10e9e5cdcddf89
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:27 +0000

    delicate represent style pain Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
