fc8c001c48f50885aebac4d78256a7316f22e5ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:35 +0000

    count lead rise field only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_typecode(array):
+        return array.typecode
+import sys
+def exit_program():
+        sys.exit()
