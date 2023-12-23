473082bf161046a0add68f90fa27a40038a5e68d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:10 +0000

    drop reduce tidy hungry reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
