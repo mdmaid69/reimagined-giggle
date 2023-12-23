20c9d71dfa70c3f1353cdb0dc2fa6dd2080f0f32
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:30:47 +0000

    hour region card Rr tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
