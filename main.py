fe60c8411a3c83a9d01f224dd0f46369b7601a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:54 +0000

    read sing another spite attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
