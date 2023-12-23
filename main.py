161f9931c4b0823fc938dfe5f1ebbd301024299b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:15 +0000

    swing phone from general divide

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
