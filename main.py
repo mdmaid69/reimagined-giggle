b3c59af354e31ec934cbb3f5d21719a53cfb45d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:38:10 +0000

    572enter come warn have wealth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
