f7b22cfee7a9908104a389bd88cfc39dea6e3d0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:05 +0000

    instrument above disgusting software perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
