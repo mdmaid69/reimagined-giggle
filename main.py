04857b7717f1513b90614e32bf713ddd0ac091a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:47:36 +0000

    OKorokay Jj wash hear switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+print("Hello, world!")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
