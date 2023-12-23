d6e7bb411b73c9b2be7840c57247744e8604f21c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:12 +0000

    danger squash describe noisy our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
