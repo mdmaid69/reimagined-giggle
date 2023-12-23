82c1b20f31de069387e59544540f282bc3d1cd24
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:31 +0000

    cheap chin story judge achieve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+for i in range(5):
+        print(i)
+import os
+print(os.getcwd())
