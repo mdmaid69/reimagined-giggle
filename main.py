4fb25e362ff9a4703279fc1961c2e3143d3d8482
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:28:35 +0000

    relative content row wet tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
