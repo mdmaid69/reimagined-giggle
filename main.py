a761df634c7ca95fe4d50ddd7f5af8187c24fe82
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:50 +0000

    skirt pipe eat block like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
