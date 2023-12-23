dbf108694c0d00fd91dd70233e1f0030ec0f7217
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:38 +0000

    first callout loud be Kk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+print(math.pi)
