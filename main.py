44977a0f19c318f6425c3fbb4eb5f489d4f32904
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:58 +0000

    commit human left pot care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
