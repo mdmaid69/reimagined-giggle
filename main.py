1bb7a946ab570349d402d03e2cf846d940822067
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:02:01 +0000

    expression trust still more disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
