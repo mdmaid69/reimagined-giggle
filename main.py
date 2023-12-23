0dc9ad29fc5206e9e1677625f4dfab6208e6191b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:23 +0000

    live scale report physical argument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
