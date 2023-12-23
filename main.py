cdbaccd4608bc0b651a7902dbaf2c3452b3e261c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:11 +0000

    key wide standard have challenge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
