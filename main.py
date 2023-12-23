a47930057479b16a8988b08759a41a8cb4accd72
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:13 +0000

    left matter miss borrow warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
