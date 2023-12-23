e0c5df9e9e0af2cbec3c3ccc988fb047caea4f71
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:19 +0000

    watch rich opportunity bath approval

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
