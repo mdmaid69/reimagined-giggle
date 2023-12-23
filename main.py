09e18add085f0b8eab59fedb33a692812c5e784c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:23 +0000

    564encourage week capital rid callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
