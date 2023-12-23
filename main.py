bf6d865e4242208e8a8574ef5bc9196e511f2a43
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:50 +0000

    scheme match ordinary as charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
