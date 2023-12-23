bb285951d38a8ef59996542bfe63237f7c2ee696
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:46 +0000

    someone exam beat tap hit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
