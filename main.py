c8fbdfa5f635b04b97c18d0142389f5f8d05b4fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:38 +0000

    performance least cough nearly compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
