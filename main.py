e111199ba665a94f08af08ab8724af66ced8ca4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:07 +0000

    takeoff closely safety move any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
