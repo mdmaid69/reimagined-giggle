d26cfd3256ecf7ff8c6b3f1af7f270d12d71a534
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:58 +0000

    total be scream desperate stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
