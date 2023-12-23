10e4996ff3a795453890fc2942fc6d91b66f7f71
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:53:50 +0000

    convenient confusing support shoulder container

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def reverse_list(lst):
+        return lst[::-1]
