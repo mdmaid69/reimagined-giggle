7c08c826f9b3970771b40e416afc7d5d7549f477
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:23:03 +0000

    really grateful that goup reaction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
