03ec3dcae360085cb3601dba52deb921c465df59
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:25:52 +0000

    floor ordinary drive survive shut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
