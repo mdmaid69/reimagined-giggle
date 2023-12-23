0811b2e092d7267df367c737befd4b7d46371223
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:48 +0000

    live giveup shoot thinkof flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
