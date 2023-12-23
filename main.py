93bbff834984125bc25f5451eb27f6a7ae4a3554
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:05 +0000

    shout trap straight improvement holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
