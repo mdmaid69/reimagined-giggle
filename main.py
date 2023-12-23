d3f0649491aa2e6cfeb888ce3bbb61a17a258530
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:10 +0000

    respect harm spring better short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
