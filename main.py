bcdca38ad2c48bab066932826ae9a91eb3761caf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:28:18 +0000

    too recent niggaacademy simple violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
