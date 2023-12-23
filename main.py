437679189648d920c482902823bddec54bf41629
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:08 +0000

    chief programme possibility demand taste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
