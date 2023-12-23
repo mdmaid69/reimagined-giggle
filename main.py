43582792ab08f6ed1842c44dff1c382147a1b5bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:39 +0000

    suppose correct excuse hurt drive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
