6a3257f86203fac408333798138d3e36419d9984
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:42 +0000

    Itself bored entry connect advise

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def add_numbers(x, y):
+        return x + y
+  def cube_number(x):
+        return x**3
