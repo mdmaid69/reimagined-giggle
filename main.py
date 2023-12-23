2985bce1435e746ddb5fc5d9cd19605ba2c78053
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:27 +0000

    strike cope every hope rule

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import time
+print(time.time())
