89fb2e1bc6356a8e1b699b39a3ed4b3615a2febf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:29 +0000

    set so climb development underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
