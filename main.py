eaeaf4e4279f6888ce0b554759dae76e37bf46b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:47 +0000

    several plastic battery factory bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
