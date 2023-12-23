27eccae4ee636461fc1ce2e8c4fe245a7981f12d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:15:20 +0000

    callup crime important exist shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_min(lst):
-        return min(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
