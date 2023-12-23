a05677849f6ca90d60ff1e4795d651074db36b2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:22 +0000

    holdup point disappointed slice brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
+def remove_duplicates(lst):
         return list(set(lst))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
