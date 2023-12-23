6af8c12dcb108278b6972aabe67a3ac573edb254
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:08 +0000

    delight left beside fill left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
