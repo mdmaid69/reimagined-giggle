ae642c683fb5e90b1a8ec4559bc1bf66d0b785e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:32 +0000

    separate obvious square bored wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
