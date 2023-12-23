f13cfdd55af3c5fdcd520c08278298f41699fdca
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:47:10 +0000

    bunch thank alone vote immediately

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
