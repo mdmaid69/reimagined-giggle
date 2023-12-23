ee2584641e747b88c6a0beaeb5f066ef9503d653
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:10 +0000

    further neat record chin rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
