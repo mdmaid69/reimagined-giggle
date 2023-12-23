398f4e9467aba77cd7711cbdb54c24a6af586742
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:40 +0000

    569enjoy well press shut cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
