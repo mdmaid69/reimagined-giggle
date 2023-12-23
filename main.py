05cdb22bb3f1fe343bcc607df3e2c2a5e38c06b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:07 +0000

    morning fasten smell lunch otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
