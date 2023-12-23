25da8545b878ce7d5fd9bf02fe83e91841fe45dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:32 +0000

    wait wall off we ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def is_odd(n):
+        return n % 2 != 0
