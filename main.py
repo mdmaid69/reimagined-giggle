baeff87c957c62e65a6fdaacea9e424749e11cf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:46 +0000

    peace mark congratulations live comeoff

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
