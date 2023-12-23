3965fb90a24931c539996f0639379ef48a70bcac
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:24 +0000

    neck relation tell sting bath

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
