7f99e6c5338f83ab23a53cd6151e1d28503e328a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:21 +0000

    step Bb rob breakdown inside

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def subtract_numbers(x, y):
+        return x - y
