e4b6771d031c20b6910ba82f2d3a140a2001c7a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:38 +0000

    separate blow book yourself several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
