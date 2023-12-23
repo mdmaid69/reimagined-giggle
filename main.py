2b5a5be940ec53f45d1bb4f3e52490cf7c9606a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:56 +0000

    cost owe group negative wild

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+for i in range(10): print(i)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
