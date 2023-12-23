fec7e50d8555e977bcda3fb5a6bdba0c16192a8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:03 +0000

    scrape feel zero union any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
