66d4ae44c8f37fbdb376649ada681f9f6e1807d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:58:39 +0000

    giveback inform scheme key situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
