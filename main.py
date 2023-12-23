7a2a78781a8b89b35e7093e6f4ddcf0b2a12c907
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:33 +0000

    whom relax objection giveback each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
