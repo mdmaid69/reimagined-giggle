78b61b5be531003fae7433a49f2b36c1d2d316e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:39 +0000

    admire ashamed guilty suit pipe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
