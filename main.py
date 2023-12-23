dcf54df44f0ae11da4f88bb254b26d745be90ff9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:58 +0000

    odd solve let seriously save

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
