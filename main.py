c837db117614f5683dd36caf177c17af6f29ad68
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:55 +0000

    hope prevent morning fault dark

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
