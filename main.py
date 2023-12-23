8841a0d0a70bc91e26d3e4c1f7dd162aa3a41835
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:00 +0000

    first reduce skin serious exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
