8e0dc33e85c372f44ea5ff6177bf958577b842a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:13 +0000

    preparation street feeling success disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
