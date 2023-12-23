ce271dc2cd9ed31e953b1dea2afad5bda19614f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:05 +0000

    dare night come extraordinary somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
