e53ba321eb36d7c70fa9cfb615f51f4a96d1e4e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:24 +0000

    protection vision connected heart old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
