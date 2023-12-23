f4ddd909ef5f300d1924c5efddced6c93b29e024
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:09 +0000

    shout somebody emergency far slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 def calculate_force(mass, acceleration):
         return mass * acceleration
