ee9f5ed9d634835a13bc9844342bc9c2317dd88c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:29 +0000

    goback(to) collection contrast shape sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
