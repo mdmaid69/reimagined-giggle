a3163065054daf51a008c5aae967054e77e1f2ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:16 +0000

    news recognize in acceptable land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
