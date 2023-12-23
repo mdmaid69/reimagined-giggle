4b76094ff3dfd456abe54c388e153ade6afbb0c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:24 +0000

    makeup strict what quantity grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
