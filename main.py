557ed594bdb72ef2383a70905c532f57f173bbab
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:55:26 +0000

    snow track complete bringforward frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
