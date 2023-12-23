a9bdb58e060158e5b3bdfc11a2d91e37defcb6b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:06 +0000

    date sweet fall wide gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
