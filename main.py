f21d934e7f8dc35bc4749ea07db850b190248e22
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:23 +0000

    bored to manner world steam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+for i in range(5):
+        print(i)
