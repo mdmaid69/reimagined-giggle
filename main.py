e1b0405d8c47ad3c9fa9790612f9a98eea539074
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:02 +0000

    fond neither will provide fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
