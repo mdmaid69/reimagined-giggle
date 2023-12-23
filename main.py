c8e1dfaf22bba28ef51a67b989a2ab158c17e85d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:28:08 +0000

    point achieve house half regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
