633804511e8e5a33fb7a5c9284b39407a6755bd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:06:37 +0000

    hers rush them build pretty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def reverse_array(array):
+        array.reverse()
