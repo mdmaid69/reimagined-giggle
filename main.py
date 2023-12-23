70f5e11c5a414fdc996538d208cb1efd281827a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:49:27 +0000

    under responsibility movement yes fear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
