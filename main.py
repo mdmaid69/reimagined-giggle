cc357fd55a7d4e4d2f567e50808a3e3870c132bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:27 +0000

    design fold goodbye remain feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
