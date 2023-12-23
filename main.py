4d55316987a60d8e20c067c3ddb815b501a45b54
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:30 +0000

    unlikely whole pot succeed contents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def is_odd(n):
-        return n % 2 != 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
