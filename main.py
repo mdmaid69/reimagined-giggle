cdf04a247513c2d0df4fe5be2341e467a10e7942
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:01 +0000

    return sharp disagree mind ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def get_array_as_float(array):
+        return float(array[0])
+print(sum(range(10)))
