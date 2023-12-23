bfb4f76a2160dbbedadd3b0f1f1f07727d0b119f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:41 +0000

    form long trick foreign under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
