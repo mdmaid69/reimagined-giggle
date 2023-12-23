da75efdc0c0196bf3a8f97f5fb2244d5f3cf02c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:07 +0000

    beautiful office separate sleep objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import datetime
+print(datetime.datetime.now())
