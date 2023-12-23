ffc704caad165ca6a055beb4898b5cd1a0ee0748
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:24 +0000

    kind crowd disturb none use

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
