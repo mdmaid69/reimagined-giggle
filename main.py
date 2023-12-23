5775b648d5e0f45f4832494cfe96e252cf795f1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:07 +0000

    flow remove than ready ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
