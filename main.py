e03d95782e3b317e6a059fb441959eef751a6349
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:49 +0000

    catch sticky lean town relax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import re
+print(re.match("h.*o", "hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
