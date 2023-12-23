c6f52f60b1560869f492464f9c71c790e09bcefe
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:43 +0000

    or bag sweet description swing

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+import tensorflow as tf
+print(tf.__version__)
+import re
+print(re.match("h.*o", "hello world"))
