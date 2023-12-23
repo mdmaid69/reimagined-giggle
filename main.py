5db8563921ec98ee4362f9e38a3f018db07d5097
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:08 +0000

    honest exit away sail pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
