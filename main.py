e88130af71327fa23c8b5ac4f60859c73858f638
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:33 +0000

    deep length welcome announce nonsense

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
