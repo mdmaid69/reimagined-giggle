22e5dee635d754f1da9e54c6d29037007da3f7ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:15 +0000

    anything account tap contents impatient

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
