039fbc5586bcc833ce1e928e44f83da182b3400b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:45 +0000

    possibility Vv excuse tidy rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
