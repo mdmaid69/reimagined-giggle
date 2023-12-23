a19aefb9471044b589ae06ca298264cc5ffb8c3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:49 +0000

    shall translate favourite paper prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
