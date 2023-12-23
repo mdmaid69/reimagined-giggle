7fd13e35e0cfb9ff5b36185eb33ca41617169083
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:23 +0000

    chief war star secret perfect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
