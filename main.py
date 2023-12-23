abd3dc7de16ecb8e4ab040d5db04f7e196738216
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:40 +0000

    by change film clean bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
