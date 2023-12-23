73307b07d24bc801c0f30d98ea20ca03e32e2717
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:47 +0000

    promise 568energy cheat ever actual

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
