f02da85db2cb546105cb10db70ff56709c347172
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:59:38 +0000

    bill brain fold boat wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def change_working_directory(path):
+        os.chdir(path)
