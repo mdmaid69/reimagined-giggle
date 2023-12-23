f942ca545fc8aedcd89360db82e2bf2e61551788
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:19:04 +0000

    more Yy too clue lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
