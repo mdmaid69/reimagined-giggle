448724798e1b765c3e0d3432a2ae2e9b0c440f5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:34 +0000

    date hers below disapprove club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def get_array_index(array, item):
+        return array.index(item)
