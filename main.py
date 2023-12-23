a32a43687c03dfb6f55071a18589343f31647303
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:18 +0000

    bus niggaacademy now table knowledge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-print(sys.version)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
