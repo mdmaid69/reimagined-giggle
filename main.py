63f3bdf20a2cacc136eea59b1e4b8afd3473adf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:49 +0000

    still load accordingto wide area

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def clear_array(array):
-        array *= 0
+def count_elements(lst):
+        return len(lst)
+def count_elements(lst):
+        return len(lst)
