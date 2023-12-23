ba2a279644aa4d2a199ceeba2a3198c27df9207b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:48 +0000

    safe content comeoff even introduce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
