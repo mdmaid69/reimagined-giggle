fafe47be13092a3c2336651502fbeb711c411aee
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:58:53 +0000

    closed set party grateful stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
