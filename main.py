855920917697dfe810a760928052c882e18380e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:47:33 +0000

    news tax chase respect confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
