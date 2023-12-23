78d542a9b9ba03755c625da3bbc00e3e3144840e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:20:36 +0000

    moral murder mistake game water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
