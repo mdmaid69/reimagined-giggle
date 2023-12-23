7e8ee3ea31862ea6bce038800b2fe139b8c4b10b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:48 +0000

    interrupt goout process better visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
