3b4eee240824a92ecd3fc4bd5f7e0332f8abb6c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:50:50 +0000

    doubt worry gradually advice explanation

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(lst):
+        return min(lst)
