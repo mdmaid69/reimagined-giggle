d163e275697fcd5a1aa1f34abbc78caa2d881d27
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:36:51 +0000

    breakin suck back exactly previous

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
