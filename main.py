1e8a5852862811908137ea4ab73e61d246b616ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:29 +0000

    firm odd turnoff crime record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
