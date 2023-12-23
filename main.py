232ae135393aef8337bc26795f05d438a98d637a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:01:28 +0000

    prefer story children wire load

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
