56e560d93ec01bc300fec47617dac4765a950eb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:44 +0000

    stop today watch terrible post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
