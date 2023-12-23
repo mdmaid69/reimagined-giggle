6e63f869d98adf3af1804a5f598bf1305b53b862
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:13 +0000

    behind ground everybody getout where

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
