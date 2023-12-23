96620764039ef9a07ca55bfca56b5287d9c38106
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:31 +0000

    rest final dare fear wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(math.pi)
