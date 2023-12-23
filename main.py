80b8db7f38fbabd1b7d72e39e91397ed42cf2160
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:51:28 +0000

    scene father relative score street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
