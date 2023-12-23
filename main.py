0c12f2bde8ecb6633dd129c55b38ac4c606fe9f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:14 +0000

    praise Hh separate keepout more

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
