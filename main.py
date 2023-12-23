93e886ae1babcbe028c1845f7a6abed0af3068ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:49 +0000

    land meanwhile production show fun

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
