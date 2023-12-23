7d43852e8d2602b33b095e05b0b631f7a7a9a7a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:24 +0000

    stare praise apply aim keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
