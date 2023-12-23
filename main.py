046bf69b78c370f194714c243076e195c11f7cc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:03 +0000

    hear store correction tight considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
