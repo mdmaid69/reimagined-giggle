d20658e856d4c7262d17a77aacb5ca8f8d7aab5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:15:11 +0000

    offend belong interview smooth nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
