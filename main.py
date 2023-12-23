db7e440c3f11c14ca41d46a97d8484112ca948d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:54:54 +0000

    face turnout shadow stranger charge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
