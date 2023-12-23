5298877a5771f86476aa5aefc84887d9bfd1800d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:14:18 +0000

    relative fail moral turnout claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
