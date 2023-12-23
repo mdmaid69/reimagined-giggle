cd3f9c0256c67bd3f0fb24fe5dbc4d6076e12e90
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:24 +0000

    peace sign discussion fight visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
