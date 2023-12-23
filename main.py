3b757eb89c30d6f4937abe617e3cbc59780ea0e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:00 +0000

    fail whichever sudden method reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
