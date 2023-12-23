1aafab3b4a525333194212e9edf2e2bab125a31a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:06 +0000

    gate pick keepfrom whoever telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
