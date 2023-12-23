513fb93a056f4d89d3c3ec4ca3b5936e163263b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:02 +0000

    professional dress quiet slide because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def check_if_file_exists(file_name):
         return os.path.isfile(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
