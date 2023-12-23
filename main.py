716fe792ecf9f2684e5f813edb22ba03a66187b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:59 +0000

    use cost direct show present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_work(force, distance):
+        return force * distance
