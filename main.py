1bb07cf3a7913864e999115986874e34bf0e9134
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:16 +0000

    thinkover related repeat use exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
