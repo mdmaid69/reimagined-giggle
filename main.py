b9519cd3ae2f1cee4f43586583717de60869f41d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:41 +0000

    up action everyone increase content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
