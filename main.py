7e980a7e8c1e368f74da4d5637be16e3158968e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:39 +0000

    interrupt unpleasant poor interested fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
