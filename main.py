eb277d2418173e3efbbae2a01fa36a630c22a59c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:26 +0000

    study comein why near left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
