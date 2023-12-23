35c1cde77e24c22dddf982972d42b5a19b58b10d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:00 +0000

    top lookup grateful connect police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
