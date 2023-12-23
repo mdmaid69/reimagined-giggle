9c0d7188994d809d90c3069c0bc841cc7402224f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:57:15 +0000

    instant marry start dish thanks

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Words:", len(text.split()))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
