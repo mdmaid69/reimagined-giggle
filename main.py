a0fa6ae758304d8e6202ed029ea0218c0ba629b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:02 +0000

    order impression fear combination climb

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
