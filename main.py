4636e259cd6a48c8c8dc45705fbbd4b9b227b64d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:55 +0000

    regard fit able talk figure

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
