8081e22b884ad2e43edb14ef2bd6eb632ae173a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:22 +0000

    nervous on head please love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
