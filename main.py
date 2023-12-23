073aef7eff468f147018c1a4ebba945f75e9f87d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:43 +0000

    unite perform exactly ask operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
