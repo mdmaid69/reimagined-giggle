f257b846d262c5d33329ff0934435dcae55b937f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:55:33 +0000

    slide lean slide offer tie

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
