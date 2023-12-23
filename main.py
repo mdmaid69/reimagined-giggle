b68feef2e8c930c0dddabb6759993a06daf03783
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:38 +0000

    concerned hope lookup artificial over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(10): print(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
