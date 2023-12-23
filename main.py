ac9e3e5d49687942cb53baec17b1804b133a3aa5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:15:30 +0000

    slight main yours squash consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
