4f69d7b0ca60286d041b659f8fa930d1a6cd196a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:13:56 +0000

    they score tight steal rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
