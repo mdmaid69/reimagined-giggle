6b711cd95070e51ac52ca8b449b7e60ffb00ef9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:33:06 +0000

    medium than vote welcome report

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
