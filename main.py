6faa8ae8e000c0e16d590c65c66fa895ffb7967c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:40 +0000

    with face owe pipe level

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
