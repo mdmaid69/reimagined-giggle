f65a15373ce7e44a7dbe47d847f810ffa48090ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:15 +0000

    its best around distant mouth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
