11f3a32d33ec2c6db95655a479f62ccb7763ad7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:38:54 +0000

    yourself shop finish nowhere peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
