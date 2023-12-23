da9527f502e5ee8164e5028fe9f163ff0ca3f3b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:26 +0000

    past wherever include rare follow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
