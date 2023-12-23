ec951f29e1d8edcf59efa7c8a006b957bb9d1d71
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:06:55 +0000

    mysterious getup proof ours physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
