04965726a136648eae49eb6c3e2cace951f8c43b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:32 +0000

    grade money what advanced attach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
