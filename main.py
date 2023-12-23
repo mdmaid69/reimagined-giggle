92ebed8c4deee03754ea28262cb6dca4f2a203aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:31 +0000

    accident seat tax season impossible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
