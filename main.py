5d32bb925fa2aa4585930c5177cae1f3a6f20230
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:28 +0000

    impatient getoff description subject half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
