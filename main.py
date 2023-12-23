63069c7e6a82779d89f25b2a57029917291d2482
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:15 +0000

    journey accidental actual state claim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
