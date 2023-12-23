f07e3612109c405e016f4010845aa9101dbacd50
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:06 +0000

    sharp share Mm limit hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import sys
+  def get_python_version():
+        return sys.version
