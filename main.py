b02a4578cc418c7358a006911146a41506418be0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:35 +0000

    such sore whom discovery inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
