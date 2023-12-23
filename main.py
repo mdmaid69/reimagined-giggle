2900ce34996594ff726082d635e48c47d954d4f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:28:49 +0000

    sink switch few pick OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
