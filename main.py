7a7a63645de25ecaf1a2d94dec36fd59b9c4eaee
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:06 +0000

    wrap claim pupil harm unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
