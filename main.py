0c0418772c7084ed31d7dbfef747b9db7829bd2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:37:53 +0000

    comein comedown which pull after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
