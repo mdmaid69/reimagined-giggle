4de28303ecbd7258072ea8f26f49c5bed86773af
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:17 +0000

    happen show fire month history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
