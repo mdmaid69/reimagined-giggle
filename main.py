ab16a69302269c669d367778a6f94649861635d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:01:52 +0000

    deep border judge fear go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_length(array):
+        return len(array)
