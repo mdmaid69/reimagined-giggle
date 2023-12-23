d2ecce7be75fe56fa41b4430f17e217ce773caa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:28 +0000

    belong tragedy connection approve belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
