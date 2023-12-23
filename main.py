9bda5c3bef6360a5b7ab6f1271525671641c261b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:11 +0000

    offer disagree blame difficulty also

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
