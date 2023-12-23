51c4629c9b5bf15aa45a3dc8373ecac69e146749
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:21:55 +0000

    neat post door dish over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
