9a50aa6c3943f654b55d16ba159c5a62e77c6f03
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:11 +0000

    mess boil finally listenfor innocent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
