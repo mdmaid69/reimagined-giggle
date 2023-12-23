a8c8390c504be335e5c0c78c5c4b76305973fb71
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:57:36 +0000

    square police cost earn but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def is_even(n):
-        return n % 2 == 0
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def is_odd(n):
+        return n % 2 != 0
