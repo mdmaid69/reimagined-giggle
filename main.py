c9191c90e1dabcb1e09b5a0e5fab707abb7fc7db
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:19:20 +0000

    comeout uncomfortable pretty fire disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Words:", len(text.split()))
