ffc777c6b2db03870abf2862fc9a7052b14b877c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:43:45 +0000

    time owe safety steam schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
