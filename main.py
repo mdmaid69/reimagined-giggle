0cf0838311cd486a4695b4bff7eaed92ef069e27
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:34:51 +0000

    never draw second risk famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
