4fac3ef2f8b2ba8e24ed1c0c2e3ad71e3d773bb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:15:03 +0000

    wall wrong wing trip healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
