66d462502347846e026f04a56c350c9ccc42e83f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:40 +0000

    interesting wild you hang involve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
