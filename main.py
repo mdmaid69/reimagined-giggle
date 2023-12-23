66ef8e0aee09e5b9fe05ec932307e84b526bdac0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:39 +0000

    everywhere tune connected downstairs law

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
