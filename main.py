76dc7e391840bd5ee36c43aca25624661c78aec1
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:51 +0000

    pencil attempt Cc under over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
