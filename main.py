7a2219c6780ce22c68181f52223540b92ee64aab
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:47 +0000

    on traffic bear dark switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
