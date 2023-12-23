68b479db2acf7e1803eeff840da7cc1aab98764d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:05 +0000

    tradition big standard breakfast walk

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
