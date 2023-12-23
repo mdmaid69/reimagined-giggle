211265d85cdf7eb900e8214da35db37f8e4532c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:28 +0000

    entrance themselves sick left surface

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
