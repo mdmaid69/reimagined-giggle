772e750b501711f2430093cf2abb5d2b31f30944
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:36 +0000

    amount importance book joke dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
