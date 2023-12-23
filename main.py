2a6ce3d478bd92c95f11e12a10adeaca27f38cea
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:45:00 +0000

    star exercise mention healthy swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
