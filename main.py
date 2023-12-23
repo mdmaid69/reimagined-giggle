d674c628a0615ff7e2b501433d058bf388057e0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:10 +0000

    float pattern effect once attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_str(array):
+        return str(array)
