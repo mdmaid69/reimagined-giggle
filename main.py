98717675e47f8329a776c82e9e7b78024fcb0a27
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:21:21 +0000

    wind take quarter silent flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
