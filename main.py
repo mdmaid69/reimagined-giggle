a65d9622ed7a3fc228f8cb104c69a729d73b3c5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:00 +0000

    load really confuse nervous light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+print(os.getcwd())
