5917bf44a09beb1396694abbaa60c43230c3e1ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:28 +0000

    expression Gg morning conversation face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
