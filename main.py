de8bf90bef0d15282554ddb403a3a4d65d2b4e0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:48 +0000

    leg wherever cost ourselves callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
