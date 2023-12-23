ca0a3c2e7b8b1318c229e00af4b25c2276857879
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:08 +0000

    laugh size care fail then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
