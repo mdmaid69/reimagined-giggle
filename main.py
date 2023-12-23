da2e69dbf1b21f4d44a465e96ba50ecd773b06dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:52 +0000

    conclusion bit post means pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_current_directory():
+        return os.getcwd()
