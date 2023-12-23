203dc8d6ac9e6cacc8176c11f7a50e2233542b13
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:24 +0000

    major study exchange spring dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
