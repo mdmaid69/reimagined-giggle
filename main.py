816a14b84671e8e63cd70521de371e582e69e776
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:14:36 +0000

    comeround through flat cross little

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
