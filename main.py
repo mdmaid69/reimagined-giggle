16a03611da1ac5b1abbd2dbc5aecf9cc92b9badc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:51 +0000

    near mistake phone introduce urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
