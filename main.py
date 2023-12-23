7092dd51d642fc454ea3b495e561265166116002
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:47 +0000

    blind test any factory bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
