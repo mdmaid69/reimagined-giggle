11a870bfe09972b42142f6c68f61534698154486
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:53 +0000

    however meeting heavy scream expect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
