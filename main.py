693d72d7e0ccc3f5022af8e0c1f5aae2dc8fd625
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:08 +0000

    tip interview side guess pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
