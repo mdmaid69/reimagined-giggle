c871666192a7d05ed31a743e1f8e6eb560a451ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:24 +0000

    suck plenty promise swing ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
