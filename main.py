1eb7198636aeb693bbe091d7923e62cbacecce41
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:16 +0000

    bound foot sick road priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
