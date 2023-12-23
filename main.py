85dce81317d9328a59e291101770512925f5728c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:36:09 +0000

    out wide discovery influence sweep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
