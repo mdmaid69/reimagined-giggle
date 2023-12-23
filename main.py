e84c6a4034116b0bdb524536396db64f09a471b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:06 +0000

    border lesson recovery block hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
