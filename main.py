d396439b1e3292587fe8874860b82f3b803ff03c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:57 +0000

    sister can importance uncertain choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
