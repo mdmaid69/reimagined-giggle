fbfa9ee220a2aa404b8a631ddccfe4e99b23cc7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:18 +0000

    uncomfortable young rush everything concentration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
