35adea6e4f107a19ef4a00171a48a82f8f6c63c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:59:41 +0000

    today confused speech catch bag

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-text = "Hello, world!"
-print("Characters:", len(text))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
