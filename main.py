8666aa10bec23c57daed7709969084c30607a3b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:46:20 +0000

    shout damage earth turn empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
