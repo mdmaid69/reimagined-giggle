650415a1575c0c9ec07bb0be11b4dfbc2b6c59fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:06:08 +0000

    bad hang pollution and still

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
