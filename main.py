d1c58e1670364cea91cb11187b44cc2e86441297
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:58:01 +0000

    wrap responsible meaning 571enough go

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
