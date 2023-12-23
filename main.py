6173581649f1f5cb818c6811753a63f7e9e2e1c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:44:03 +0000

    afraid hurt design run treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
