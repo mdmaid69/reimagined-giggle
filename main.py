283460b2ea4240ac2c6f64eb4bd646117b01857a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:49 +0000

    hold scrap previous schedule feed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
