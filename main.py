dce31b1cc1f7f6953a42fb98ecc3a5e4afb73948
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:21 +0000

    discussion ilovedicks bread Jj mark

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
