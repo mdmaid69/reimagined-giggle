330e889d89c657078202e3c9a4dead60bbe92639
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:13:07 +0000

    break their nice when discuss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
