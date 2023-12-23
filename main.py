ae90b93d35c72a6215aa0c387ceea6be711a18c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:21 +0000

    operation trade broad sail hot

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
