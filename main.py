3cf62bee936be8939b289e5de3dbb5adab3f0a0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:44 +0000

    embarrassing hard straight breakout doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
