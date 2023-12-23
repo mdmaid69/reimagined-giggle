6678dd4733d4b28e9de72471b5e8a5442645aa18
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:03 +0000

    best wherever Tt week bin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
