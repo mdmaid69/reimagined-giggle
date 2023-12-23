d5043b4d4db229af4b56e87745132bb84546936d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:02 +0000

    quit turnout storm serious far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
