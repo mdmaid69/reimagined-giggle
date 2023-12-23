c5c430dbc128c9240f2cf102ca83c7dc7e5d711e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:12 +0000

    pupil plain cause generally somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
