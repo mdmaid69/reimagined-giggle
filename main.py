a6001b7beb544eb48c68a24263055d9eb04be4d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:49 +0000

    exit touch low crowd sky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
