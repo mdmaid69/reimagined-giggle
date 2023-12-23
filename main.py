3ee3b07e7e42afd526c0b82befe16e0947f08a31
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:46:24 +0000

    callout sure round objection shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def multiply_numbers(x, y):
-        return x * y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
