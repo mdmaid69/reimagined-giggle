d940bd159d875b2024359e74471966925244d73b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:16 +0000

    brother silent recovery to direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
