c8978da621108e8f537c4dd0f026cc4a45722e04
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:03 +0000

    tape well themselves breakup bush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_list_from_array(array):
+        return array.tolist()
