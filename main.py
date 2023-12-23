f7f7d877649a95608619819f2dd95f9afb6acec2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:03:27 +0000

    produce search argue mouth past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
