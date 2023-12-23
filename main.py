66b5ff80949d1479d823f9e8247133e2a2f0d3bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:21 +0000

    benefit ashamed also simple direct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
