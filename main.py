cad838efd91b52d92a60dc90c766dc65fd4db5df
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:26 +0000

    godown lift sport bet physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
