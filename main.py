e27935b8f79733d628bfd39cbf2f18630f9b3810
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:09 +0000

    Ee hour weekend interesting head

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
