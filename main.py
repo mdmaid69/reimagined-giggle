b15a88208d9d0126f0a8a79d312c0897e427f27e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:02 +0000

    finger getback left straight poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
