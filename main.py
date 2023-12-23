8ceb86d28fd9a5a1aff6d4cbd37fc222acc485e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:30:51 +0000

    comeover spill call choice party

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
