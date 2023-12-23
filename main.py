f962aaaf52f87205deb665e787b203d854fb8f91
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:52 +0000

    wind warn laugh put Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
