aed727ae7e671fe2707cb408962edb6ff92b8db8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:03 +0000

    authority sail Gg afraid control

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def add_numbers(a, b):
+        return a + b
