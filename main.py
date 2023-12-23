c22c4024b808e11cfacbc87fe945dba43cd3113c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:35 +0000

    book die night persuade pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
