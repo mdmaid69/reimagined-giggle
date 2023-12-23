c407caa6ed86d3c3f11e49d69ddbe8cc0f4dc42c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:06 +0000

    comefrom natural arrange also grow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
