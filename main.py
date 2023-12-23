4bd59ca9a96af80b10f056d04db49e7723217165
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:13:09 +0000

    protect complete myself anything onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_tangent(x):
+        return math.tan(x)
