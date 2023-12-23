97923e18ec029043e69e4ea30413dcdda4121d99
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:13 +0000

    community sign afford sorry scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
