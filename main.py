3a49cafb5a5152ea95d1a9118f5731615240faf2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:10 +0000

    difficult supply dear challenge just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
