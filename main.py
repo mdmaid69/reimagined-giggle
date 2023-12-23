929582d094f755b503bba7f92fdb18dfc048dafe
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:50 +0000

    relation surface continue cross date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
