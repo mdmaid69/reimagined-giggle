c48c7ee9b9f28a3fd0111ad2307f62b3ae14ba97
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:45:17 +0000

    ticket sense brush lost excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
