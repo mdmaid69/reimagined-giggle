d58424380a13f278aa4d535e49086cc0b72ceb57
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:20 +0000

    comfortable our except size fortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
