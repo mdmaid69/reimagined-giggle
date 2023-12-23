747dec1124b96aaa83f1d401f36f0bf3b28a6812
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:29:02 +0000

    conclusion low help swallow brave

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
