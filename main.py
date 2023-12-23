3313a9e03312853ed2ae6c975762fc8ba5b3efe1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:11 +0000

    turnon study part comfort obtain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def count_characters(sentence):
+        return len(sentence)
