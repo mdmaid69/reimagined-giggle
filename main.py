96532654470dceeffe643c13aabaad4302477845
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:22 +0000

    interview quality boil group ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
