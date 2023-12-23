fc11a724ece094d8e0cbc58bcda6d04803eb1fc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:26 +0000

    miss show message various right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
