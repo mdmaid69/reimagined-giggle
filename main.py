d83498352400ecc35526fd45b76b3edaf2dad611
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:29:00 +0000

    final thanks early since along

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sys
+def print_python_version():
+        return sys.version
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
