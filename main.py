1d3c5d6b75150846a3655d9d3c3fd5800f0e44b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:04:02 +0000

    moment minimum kill shame autumn

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
