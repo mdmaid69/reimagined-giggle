780b4e2c8bbf4f9c858d9e0403c76fa9b7543ea3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:37 +0000

    soft rank lay smooth concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def find_max(numbers):
-        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
