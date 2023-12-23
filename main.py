32a1f28b16552fabf5e64c77581014f2149c6edc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:44 +0000

    rub truth solve pain gooff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
