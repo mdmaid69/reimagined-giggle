75a19583f9ab401d3d8496dea5691e3d564e22fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:15 +0000

    release laugh temperature bus wonder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
