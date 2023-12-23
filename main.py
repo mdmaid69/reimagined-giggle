06f95fd7ff61bc29e76ebc198c175bad275b1569
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:11 +0000

    squash object father Mm waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
