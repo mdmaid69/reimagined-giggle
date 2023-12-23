0e1c9a5587c0eb9a77e20b006d491668c75f5cf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:56:07 +0000

    morning discovery brave realize chief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
