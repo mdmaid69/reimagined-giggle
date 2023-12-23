274ae5e9ef03080791c77e664fdf223c52204833
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:04 +0000

    foot yes side bar sweep

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
