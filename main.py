33a2d034bcf6b7bad9001c93d468f7ce2576c634
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:32 +0000

    except loss reserve when criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
