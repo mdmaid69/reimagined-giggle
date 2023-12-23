c790b95fbd96e1561d459dafa146066127fb8b18
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:50 +0000

    across sick our turn himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
