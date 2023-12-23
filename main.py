371070e22e9bbf8b0b0dd1e244889052c459eaa9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:18 +0000

    tonight I support ilovedicks protest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
