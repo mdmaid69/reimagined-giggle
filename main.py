82e8a6aa59b7a9b1fa3f36079c2eb9d6505b0c40
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:13 +0000

    help say whistle burn last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import tensorflow as tf
-print(tf.__version__)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
