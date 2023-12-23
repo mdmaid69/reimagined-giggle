dfb5ea085958a503bb74d669b65fe2d653220416
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:00:05 +0000

    watch lead train basic lay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
