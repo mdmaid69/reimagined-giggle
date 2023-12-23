2c85705b03523116e7c301db585928950cb95c74
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:19 +0000

    still ashamed Itself Gg cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
