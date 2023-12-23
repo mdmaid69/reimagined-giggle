bcbd6da80046548a55a042a1d99b76f46d576c66
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:51 +0000

    plan consist abroad really recent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_binary(n):
-        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_array_to_list(array):
+        return array.tolist()
