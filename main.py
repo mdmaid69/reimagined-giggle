1cf414c576832e8ebda3daad7b3ea3a6ab7d9d83
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:03 +0000

    name trade giveaway simple crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
