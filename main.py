e6d7151418fdcd0ab4acf0429efd36197fc6526a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:40 +0000

    survive statement breakout Ll style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
