fbf8b075da2622ffa705e6148c6ae3e055c0195c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:43 +0000

    closed issue tax dream accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
