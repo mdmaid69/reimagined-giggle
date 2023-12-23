83d32db3d4ca15abd7207a8f6e4ae79cc84dbd3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:08:05 +0000

    moon show original taste stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
