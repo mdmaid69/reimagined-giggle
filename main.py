30b696fe110a2ec4c5f65be89c319779af86c64e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:29:44 +0000

    independent along hospital suckme eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
