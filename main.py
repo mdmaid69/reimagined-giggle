1a2b17dd07783cf4100f475f6ea5e064f8f9fa46
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:54 +0000

    relation already violent underneath exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
