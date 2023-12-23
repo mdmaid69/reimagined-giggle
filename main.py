e4f047f647a36dd4ec28234554097a9ee8dc4193
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:10 +0000

    nigga love surprise nothing air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_set(array):
+        return set(array)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
