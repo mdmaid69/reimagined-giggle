63d76deccf62d8b8ca54a55b54ca0feb8d559729
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:43 +0000

    convince travel advice Rr coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
