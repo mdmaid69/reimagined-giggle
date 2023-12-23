b3a62d7c97fe34de8b321dc4e9da3cc14fb19c6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:13:25 +0000

    damage right winter criticism study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
