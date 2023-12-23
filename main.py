8158a226d76c3f63a5d2ae9e42942a68f3e70c18
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:06 +0000

    strange thinkover other switch chance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-def exit_program():
-        sys.exit()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
