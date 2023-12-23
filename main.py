7ccda662bfafb96afb2b29f427956a81355f91e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:54 +0000

    comedown sister floor zero similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
