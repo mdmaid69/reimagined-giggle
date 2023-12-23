adfe37eb752b15fd1163c68d8d50e09f90470aa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:42 +0000

    create breakout picture bend thing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
