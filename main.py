f3d15153ec2cf1b84f8efde4d48c085c08662106
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:23 +0000

    list until progress Ee doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
