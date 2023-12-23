d2ec9d48eec30190dbfaad3c1dbbc50ee2832e83
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:34 +0000

    funny correct next towards skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
