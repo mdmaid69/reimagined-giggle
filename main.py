74133bf3709842851ba86f3dcc16109da8807bde
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:01 +0000

    keepout radio challenge outside area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
