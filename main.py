7fd199defffa7966bc76f0942038407f0ef31e0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:13 +0000

    breakdown appear unit well turn

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
