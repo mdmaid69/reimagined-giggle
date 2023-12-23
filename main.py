550b983d13e9b8d630a48a554e040da7777ff04b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:40 +0000

    attractive add threaten invitation stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
