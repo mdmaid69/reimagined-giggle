4420ed23da61ccf23d0d8a269ea65c05d6490410
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:55:07 +0000

    pick scream until minute goaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_item(array, i):
+        return array[i]
