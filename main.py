bc43667269eeaa5e64e91d52ed331a5cb6fbb24a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:54 +0000

    intelligent greeting or error difference

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-print(sum(range(10)))
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
