da107479c5b56ac78bad3faeb8a2e31c1ba2d0ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:06:52 +0000

    copy arrange takeout rid sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
