059337cc7b421656534254ee6756e326386e9e3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:40:45 +0000

    sign nowhere usually completely possibly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
