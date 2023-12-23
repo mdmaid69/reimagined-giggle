e746467af956904b2aa6be51fef5e6d3a1704702
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:47:36 +0000

    through wire important generous software

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
