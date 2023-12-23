36ecf58fa67d0d441893590808f6472b02b497ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:57 +0000

    strict light getback minor Gg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def convert_to_hex(n):
-        return hex(n)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
