ab2046b36820bb8f1cdb1581c5ef106fc7ab5d99
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:05 +0000

    keen load cost temper turnover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
