7b1a838d886d931cb85928d38f4e913db6b0b622
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:57 +0000

    connection keep average probably lie

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
