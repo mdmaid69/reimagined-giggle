813abca37e81cc0bedc4ddda55c0a80de3df2352
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:03:14 +0000

    from very treat part nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
