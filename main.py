361b3fdf703e4df81803fa7176227987d6db7714
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:58:05 +0000

    unable bill connected whatever effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
