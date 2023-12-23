d445401631d178853896b9956a5a2ea18de443f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:21 +0000

    agreement acceptable little cool range

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
