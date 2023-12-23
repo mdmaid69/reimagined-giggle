f0a7e735d957ee662e516910a447b284f10ac058
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:30 +0000

    generous bunch protest slow crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
