eb4c621eb11bfa30d53e1b5b93718d4a711e73fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:21 +0000

    physical exact announce wonder object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
