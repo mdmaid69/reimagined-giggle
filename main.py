4354a939bd2604f2e9acccd4024a2ef2b29dcd8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:53:05 +0000

    effect comeback related practical either

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
