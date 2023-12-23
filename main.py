592116a59b571d99179d3d6aa2f362b667854f2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:38 +0000

    drink bored wish chat thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
