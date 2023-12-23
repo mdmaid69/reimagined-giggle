be9fcec5a176f90a9f3af95189482d98359eb32c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:04 +0000

    still find price able male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
