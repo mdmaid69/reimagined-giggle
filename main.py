cd85e28651dae0a8244953323d285d67abfe7df4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:55 +0000

    succeed voice repair better until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
