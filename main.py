3c670ff9e8c4bbaaff710fd224b88b1ede7f3378
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:59 +0000

    dance kind combine port shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
