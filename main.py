aecb4ea4439f66c364791f0411347fcdfad0cafd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:58 +0000

    skirt wherever ever respect fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
