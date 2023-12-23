d71b494761b66ccb7708ea2e0515306ffd4e06c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:59:41 +0000

    top nothing previous surprised me

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
