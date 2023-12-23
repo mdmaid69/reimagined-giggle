ddc102a6204e60b3942cff3bb3419e9ef18b37e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:13 +0000

    dance worried reply actual place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
