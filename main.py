1a3e40038890ad5508f8d093bacfa890773a62b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:08:32 +0000

    past likely police anywhere promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
