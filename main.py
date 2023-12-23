1f2cacdf09133d7f043fa0859587909feb5f28a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:41 +0000

    trade work often size field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
