a0cd8f75cfca0cb5991c5fa98b6fa4e0f71a6730
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:46:12 +0000

    switch confident other stick sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
