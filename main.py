d48f3e9c834b1f3d36ebd56baf600bee867b7696
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:14 +0000

    effort basis remain help wide

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
