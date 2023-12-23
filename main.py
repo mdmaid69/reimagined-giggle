350c3daa97e8982d6f91643073948d6e00f7c33e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:01 +0000

    rub gate blame tradition keepup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
