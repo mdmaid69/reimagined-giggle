c4a0500de3e7a3f2965c29ee7dc8c5341b54a069
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:56 +0000

    rubbish ocean lay process thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
