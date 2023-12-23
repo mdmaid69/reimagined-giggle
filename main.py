4b003952495cbdd588c7a60b3bc56a29e8adbdd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:27 +0000

    takeback refer meal switch sell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
