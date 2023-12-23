b0a94395ac6fd53e2c451eff5022088b4340ef9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:24 +0000

    gate thinkover steal future nose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
