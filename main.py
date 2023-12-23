800d013d93de7831203122dcaf00d80dff14df66
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:51 +0000

    concentration common attempt principle careless

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
