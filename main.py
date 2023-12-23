82bfadfea0cf9293dc5d2a163be7fddfc042167c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:53 +0000

    sort control sale treat receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_median(arr):
+        return np.median(arr)
