b2a0edb36ba1c3633e1c4cf1f02488589604f762
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:18 +0000

    Nn watch doubt room away

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
