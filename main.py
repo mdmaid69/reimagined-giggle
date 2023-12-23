5a1720703c47e3dcfc34aef046dedbb250352280
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:20 +0000

    water course call slightly in

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
