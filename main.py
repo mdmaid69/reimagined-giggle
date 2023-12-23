bee2f8d2573e11dbda71a5c9117751f8d8e9375d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:37 +0000

    gentle modern defeat 567enemy reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
