e0e0d7a3f75a5997bcad174f749d4f796e1e7d81
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:42 +0000

    I definite public determined empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
