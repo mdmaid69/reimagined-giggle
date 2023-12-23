bf1388d6e3a094404fde5e752f04e8bc10adc8a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:18 +0000

    recognize split thick please central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+  def convert_to_binary(n):
+        return bin(n)
