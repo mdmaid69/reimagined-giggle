558a4398220aebafc69fe80522aa16e339e09949
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:59 +0000

    famous high age set progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_power(work, time):
-        return work / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
