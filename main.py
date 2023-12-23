0ea295cec065c82293231a158950ea91c8b13b1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:09 +0000

    chief sink brush Ee break

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
