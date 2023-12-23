e7c7bfa703c005559f9fe28656d6eda89c4c796a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:01:53 +0000

    basic play thick help hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
