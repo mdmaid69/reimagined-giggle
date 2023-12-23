795dc2df8b6c6a96399d90f4148ecd42f2bc65f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:17 +0000

    lot furniture bang definite willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
