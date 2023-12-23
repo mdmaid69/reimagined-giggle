fcd3b1d087f8defe7572197709adb87d8235f7ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:43 +0000

    busy rate town patience murder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
