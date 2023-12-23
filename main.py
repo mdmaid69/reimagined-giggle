a653b9f6b1b36275436f5dbb367f805b39f94f9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:31:15 +0000

    together group reduce hair right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
