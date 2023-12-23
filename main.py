f71def2ca2edf394c2505ad050fe97f86dc1ab7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:17 +0000

    we I ignore grand trip

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_elements(lst):
-        return len(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_string_from_array(array):
+        return array.tobytes()
