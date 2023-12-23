a84ed4ccfb2c107c07e1304638fbc0c9d19efbc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:37:38 +0000

    gas scared crisis study voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_hex(n):
+        return hex(n)
