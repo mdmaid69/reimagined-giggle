d50b87819e622af91ea5b18624f718d8d22fa6d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:06 +0000

    seem appeal right programme reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
