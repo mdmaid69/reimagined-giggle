29dd385dc774977987b343d5d9e06a95129a72d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:31 +0000

    description expensive steal rich murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
