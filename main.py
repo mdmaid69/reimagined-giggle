d00415543c55ec59597bf12a93b8ed4c54540a1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:32 +0000

    furniture noone common arrival hollow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import sys
+def print_python_version():
+        print(sys.version)
