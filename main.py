c7a3102aee9e5584a60c2fccd788718e985f55b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:37 +0000

    anyone equal occasional where treat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
