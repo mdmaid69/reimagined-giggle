7b7744ab71e3a7360e3953aba40a3049e2cf9f3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:46 +0000

    attract tongue approval hit this

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
