c00492b942ad48f1507a50bb29f544b89e83e257
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:17 +0000

    system alone body listen proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def add_numbers(x, y):
+        return x + y
