359145390a6d37103a2767fd81863324bb8c8263
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:42 +0000

    hair blame flat pause light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
