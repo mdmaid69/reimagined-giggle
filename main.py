b360275c75fa0bb4a031696997b64a1188b17128
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:17 +0000

    badly affect flow noise crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_work(force, distance):
+        return force * distance
