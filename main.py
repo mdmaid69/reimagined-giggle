76f026e795239263c3d2c7cbdfca85afbda59a25
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:58 +0000

    remark entry call affect stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
