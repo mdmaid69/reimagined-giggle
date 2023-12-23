a5d0dc5aee6bbacd072f0a8bb580deadee12cb29
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:06 +0000

    balance neither wealth minimum married

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bool(array):
+        return bool(array)
