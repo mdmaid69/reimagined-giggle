c220550870d9fb4b257694d0876ef19ab314259b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:43 +0000

    culture father whom because brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
