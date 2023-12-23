25a02bc25e1b41a80042434c2654295f3a8cf755
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:40 +0000

    bury water event brown opportunity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
