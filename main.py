9491de8b7000ea896c4b03ae5409fbd3d8d614ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:25 +0000

    suffer takeover total clear press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
