a09c147881f7282da276656f0eaf1fb7dfbff6b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:33 +0000

    nowhere favour broken country innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
