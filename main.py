4f9ff9fe6a683dce81374d1ba8ac87ae1ff7654f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:23 +0000

    fill kind poor cause no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
