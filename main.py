feba10e05236d0533b862d06668f58d7ef713738
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:15 +0000

    precise distant runaway concentration disappointed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
