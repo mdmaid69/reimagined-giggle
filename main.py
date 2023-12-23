dd0a236332f53ffa4184c83a6d2ee8c9cd78a114
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:02 +0000

    identify loan unpleasant birth repeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_area_circle(r):
+        return 3.14 * r**2
