61e210c29ae298ca789aec9942b549fc9cca3527
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:17:13 +0000

    union clever pretty threaten bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
