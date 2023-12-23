1e7b6c5446750e7de8dbcceedd3b0d2cb0e91a24
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:16:14 +0000

    tax lookafter fortunate black recently

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
