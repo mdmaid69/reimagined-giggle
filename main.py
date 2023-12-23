b9d05b7fe32987297774fad0fd027b93fdbf4a44
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:27 +0000

    runout mistake block complain table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
