baece01de6fab70110b949345544b41d85373746
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:01 +0000

    mean scare manage shape getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
