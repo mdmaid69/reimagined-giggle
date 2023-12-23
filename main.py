bab8c04293b7bfaec5b0b37a15f4d8866b7c574f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:56 +0000

    apply compare combination spin yours

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
