d1bf54dcd18ffac00e84a503929b8c29c7657776
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:40 +0000

    Pp burst especially tidy whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def reverse_list(lst):
+        return lst[::-1]
