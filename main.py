d5a0ff78b464ea13c9760aa1dd1358c8d2a2fa0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:08:20 +0000

    fair pity public trip suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
