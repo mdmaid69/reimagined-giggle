842af1eb5cb9da7030a5b08e04dc08ca07fe643c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:07 +0000

    stranger her refuse flat record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def split_path(path):
+        return os.path.split(path)
