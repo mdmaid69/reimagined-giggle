a6123d9ff8801139731986d66d9b01d2eba0a053
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:29:59 +0000

    certainly start pile settle lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
