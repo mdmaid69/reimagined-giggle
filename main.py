d1321a402fd99e78d9ba5342d0d601ea46797d72
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:16 +0000

    excited gooff comfortable punishment forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def sort_numbers(numbers):
+        return sorted(numbers)
