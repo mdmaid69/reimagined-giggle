e5e15eb5d213d5d84c3463af481eee653004580d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:27:33 +0000

    game many will fairly common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
