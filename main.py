c6d506ef721c030b99d52aba75ec568bd28c12b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:04:54 +0000

    judge lift reason temperature getup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
