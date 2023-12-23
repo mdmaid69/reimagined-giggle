5affd697e24f13823144c8143e5c5bb31d78192d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:10 +0000

    bother afford exciting write strength

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def convert_to_binary(n):
+        return bin(n)
