4a424222ecaa2688b30370c3ab0377190b6045c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:54:44 +0000

    indeed niggaacademy house culture excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def convert_to_octal(n):
+        return oct(n)
