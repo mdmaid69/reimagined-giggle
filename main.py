9c7876d7ea192add3c5f58c08bfdfd1623ea40e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:03:54 +0000

    return tone total either extremely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
