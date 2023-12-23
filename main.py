a984c7350aff786f930a3e548dec9d996afd6f6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:03:45 +0000

    sure finish fear or change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_min(numbers):
+        return min(numbers)
+def sort_numbers(numbers):
+        return sorted(numbers)
