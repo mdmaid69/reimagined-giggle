46e45154e490d704fb582f975cb0896971940ea5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:20 +0000

    that dry unhappy mix comeup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_hex(n):
+        return hex(n)
