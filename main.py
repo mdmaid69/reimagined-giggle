04df4820f1e5fec12404f3099e0326201db5e3ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:31 +0000

    trade come of season stage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-def calculate_work(force, distance):
-        return force * distance
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
