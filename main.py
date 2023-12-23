5b866a99f79f94ce2397a483d69bea2087035f05
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:40 +0000

    heat emotion less when steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
