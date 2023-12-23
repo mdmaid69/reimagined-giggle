968d987e28b26170010bb16d265b30ea871bda14
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:58 +0000

    mother great spill practical try

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
