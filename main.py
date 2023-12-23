8a02d9a19ba028369f05b6755fbaeba70e08c1f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:32:47 +0000

    569enjoy belong lead any spoil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
