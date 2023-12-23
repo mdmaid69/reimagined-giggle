61fa9d97ee3fe247f8ca998876ca284893167ee2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:14 +0000

    reasonable badly breath notice entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
