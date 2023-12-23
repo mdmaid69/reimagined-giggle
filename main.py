48ffd10199c3352da7182e2b1dfacdb341e3ccf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:22 +0000

    love hold convenient give runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
