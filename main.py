fe1c73ed095ab5d656795d3f5de8a5113efba5f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:57 +0000

    pupil kind fat wipe thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
