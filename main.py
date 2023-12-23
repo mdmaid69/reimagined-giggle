fe6423ed6a5c5a8748557fe76d1b46aa569cb10f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:00 +0000

    single worth invite transport somebody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
