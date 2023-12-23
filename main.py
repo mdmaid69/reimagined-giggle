388fd69113460d386b1a4ac8afd2fb0bb03197b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:59:54 +0000

    why already dear test sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
