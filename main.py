43e4cf5cd73191009f4862154fadbf1ae65896a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:04:28 +0000

    hour chin children brush dry

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_list(lst):
-        return lst[::-1]
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
