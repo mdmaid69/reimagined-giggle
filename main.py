ed9b2c15d861564c8ef26f5ac25d1eaa887d81ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:46 +0000

    issue lunch fill make water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
