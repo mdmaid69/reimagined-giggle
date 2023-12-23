5b7b5954b276e07f15c4e64c37cffdb50dbaa92c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:46 +0000

    wave agree 572enter really healthy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
