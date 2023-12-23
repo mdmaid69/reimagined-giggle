5225f0325e026c1a1f87d7ce5cce9971d403a62f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:19 +0000

    he wave view trust failure

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
