c154f2e64a21e8237b892764a06730e70eac5eed
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:26:30 +0000

    wheel sound wonder force backwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+name = "Python"
+print("Hello,", name)
