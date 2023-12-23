275ba9cd0a9a712873e6990e5cfe90a12d1d173d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:47 +0000

    hospital morning hurt birthday suffer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
