b9ed42534d7ff80e8132974934a31627c27ab118
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:22 +0000

    impossible agree mixture slow congratulate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
