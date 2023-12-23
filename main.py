c66f26c677376b6cb39a35b33da2107ade4954cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:19 +0000

    pass miss comeoff prefer state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
