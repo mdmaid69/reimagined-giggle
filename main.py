1792d796992f7c838f712d9c4a942864ca91b635
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:48 +0000

    open natural event high shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def count_words(sentence):
+        return len(sentence.split())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
