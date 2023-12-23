778087b3b8c1e5b9527f908af46e7662a07d535b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:15 +0000

    can before know sweep acceptable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
