5c1b2af5f8ec4940aa0e7a92f3133275856af9b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:04 +0000

    interesting responsible blame love keepaway

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_pythagorean_theorem(a, b):
         return math.sqrt(a**2 + b**2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
