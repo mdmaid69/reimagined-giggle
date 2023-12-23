9c4678b74f176a8201d11bf7acaeaa3bae0bc95d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:43 +0000

    suit patience win control all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
