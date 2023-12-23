1a738f077bc83c5c5f029a662f48cdebecc462d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:42:36 +0000

    closely build right thinkof comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
