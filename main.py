4111254d792e963ee4d25137414d2e632aefd9cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:51 +0000

    effort well summer tax average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
