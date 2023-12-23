9c21842ed8dccb290b4ea78f2effee8d92f2312a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:21 +0000

    do try slow spread than

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
