60861d0a619aa0162a0b4793a27754000de7a79a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:15 +0000

    voice agree stone run draw

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_min(lst):
+        return min(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
