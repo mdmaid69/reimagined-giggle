f0132bd8786b096bbe8f72ac01bcb08a06577e24
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:08 +0000

    shame demand influence disappointed window

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-for i in range(10): print(i)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
