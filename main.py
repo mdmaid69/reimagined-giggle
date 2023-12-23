5cbc029dedb920e91fa7500fdb79da4017cfb899
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:05:25 +0000

    stupid tradition sort agreement measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
