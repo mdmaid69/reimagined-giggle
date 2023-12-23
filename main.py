12cb8a39124f7540f818feaadd3a414216553ac1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:44 +0000

    fashion radio bring comedown fasten

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-def print_python_version():
-        print(sys.version)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
