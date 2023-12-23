bc0cc5e61d46ef0c3833451e4d52a91b21cf0d69
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:46:20 +0000

    slowly certain forgive knowledge slip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
