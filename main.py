2d6d0522129270c3ee6b9850b7737c2a16187c75
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:40 +0000

    violence happen education sing deserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
