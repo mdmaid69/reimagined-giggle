55339120893294b5d45ec1194ce2ce685396a310
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:27 +0000

    detail day slip look ball

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+print([x**2 for x in range(10)])
