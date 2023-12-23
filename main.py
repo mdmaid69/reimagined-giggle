29a59edbf5fd68ad61a834ef123a43714c2819b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:15 +0000

    acceptable opposite activity fall cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
