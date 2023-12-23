8b5203ba577b7c5db6899fca9df997a95bf93747
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:20 +0000

    underneath stone sit something future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
