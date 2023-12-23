fc43eecf050e15a1614b78d787f12ad747aa3462
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:50 +0000

    goout fly repeat middle eye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 def calculate_hyperbolic_arc_tangent(x):
         return math.atanh(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
