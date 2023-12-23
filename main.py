039ab25929b4349e322e3281505a870ef67754e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:03:39 +0000

    improvement her when fast watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def count_words(sentence):
+        return len(sentence.split())
