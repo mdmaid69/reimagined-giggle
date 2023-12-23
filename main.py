cd4b33bdb8339a18fc365cad96ca4d9f94e7cc48
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:49 +0000

    throw trousers this base way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
