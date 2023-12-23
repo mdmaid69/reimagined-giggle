281b46ccf1f76c3bd7c93eeb7c697e629654a37b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:06 +0000

    murder kind position chase ourselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_unique_words(sentence):
-        return set(sentence.split())
+import re
+print(re.match("h.*o", "hello world"))
+  def subtract_numbers(x, y):
+        return x - y
