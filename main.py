6551e0e1cc5367cb0bcd214ad74e843d69bcdc2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:01 +0000

    off nose certain goon path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
