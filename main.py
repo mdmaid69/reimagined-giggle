84c24e44ca5fb0cc8aeb62310ae5a58a8ca7c063
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:40 +0000

    comfortable tradition separate connect lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
