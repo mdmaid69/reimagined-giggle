5a8304f4bb8cf2bcba352abde70d3ffa6e6acd40
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:08:57 +0000

    include attempt feeling sign keepfrom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
