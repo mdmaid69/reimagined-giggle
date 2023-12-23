b5f0b3ef6ed87ee828d3957a953539b103e926d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:54:15 +0000

    prevent guard separate suddenly comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
