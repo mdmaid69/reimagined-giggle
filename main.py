ea831d6478b4a973668557028d5ef16c9dddb965
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:26 +0000

    anywhere of better dear murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def reverse_string(s):
+        return s[::-1]
