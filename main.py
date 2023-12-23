be24908fd0a88fb7e30de36313f27eb56b9f605c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:36:41 +0000

    sometimes have weigh raw afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
