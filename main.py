3b622c5fd27f7d520b89c291113b773a0deeddcb
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:28 +0000

    lead stay post main new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
