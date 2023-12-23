e1986c1794bd382e029d13f6b69f7fdd027001ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:45 +0000

    so getaway feed hurry tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
