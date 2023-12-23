61c4b85eb45498cd9159fa642a8451be9288d7f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:21 +0000

    prove congratulate union emergency popular

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_from_file(filename, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
