c5e2e67ea75f5e83e0741ce14c9500086ec34c52
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:23 +0000

    shade street force land comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
