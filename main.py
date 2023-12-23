fdda8fb5f275de4e78dfef92878bf8c792ba20cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:53:46 +0000

    nice bear bite expensive foot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
