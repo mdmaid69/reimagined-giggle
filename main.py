2b58cad487e36393c9891cd7cb6cc4617aa7e47e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:38 +0000

    tall taste will by although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def square_number(x):
+        return x**2
