cfc3639ad24a05ee98e1a2fe7936e7256fdd53b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:12:41 +0000

    lucky coat gift rob part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
