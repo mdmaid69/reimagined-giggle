3a4b45d4896136eeb6ba82e982e9349b0fc19115
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:42 +0000

    scream protest inform principle run

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
