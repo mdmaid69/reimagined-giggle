6e5b5e61f543c1c9520ed026d531bac5925ab51e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:18 +0000

    spite hour impatient swimming case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
