03f5598844b3349414d397b758d0835738afff15
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:05 +0000

    otherwise bin butter lay contrast

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_density(mass, volume):
+        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
