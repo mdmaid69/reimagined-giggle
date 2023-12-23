ac588c660d073479849b38ecb34c31abafc72ed8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:40 +0000

    part group store pipe science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_float(array):
+        return float(array[0])
