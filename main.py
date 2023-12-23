e57c7e93059bd1d6b35337abf75600bb108f5ca3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:36 +0000

    lock kind surface schedule little

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
