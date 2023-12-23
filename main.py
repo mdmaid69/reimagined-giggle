afbf42588be51e969fe2f2b21fb8b997aacfb4a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:33 +0000

    sick hate against impossible confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
