16bc514352cd1665bd014f9f2283994c745065d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:34:07 +0000

    however show change reply copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
