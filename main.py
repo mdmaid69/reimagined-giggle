2b99fc12726a6c536e1fd9c61d51c75523263e91
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:38:25 +0000

    right petrol completely play concerned

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
