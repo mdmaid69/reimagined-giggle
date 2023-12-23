120699c0311065074a851808d5707116961ea486
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:47 +0000

    run mail length towards thinkover

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
