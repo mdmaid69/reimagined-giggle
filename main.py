2158c29ae120b81b3074e4ebc3ea578467ec1bcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:40:32 +0000

    declare danger benefit lean port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def split_path(path):
+        return os.path.split(path)
