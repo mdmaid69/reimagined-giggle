f263000e8e6128c5b67e991bd6fd7cf1c6f517c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:10 +0000

    better disturbing introduction ring order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
