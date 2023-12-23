62b9cc9c34fab2cb6d7320399b862afe03339209
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:36 +0000

    think building disapprove reason year

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
