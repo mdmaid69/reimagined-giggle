471be47d7b0e270d4c08927d3702e9ba58e2de26
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:59:17 +0000

    clothes live swallow shopping touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
