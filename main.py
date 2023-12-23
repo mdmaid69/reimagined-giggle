d0f2c6d572d910f51977bdd8b31966974bb719f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:52 +0000

    address loss storm same scale

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
