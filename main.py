51df2547dcd1d9e480d109fdace68bd6645734f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:45:35 +0000

    something high just whisper solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
