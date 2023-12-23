2d6a9023a1be4aaa32675b3416379d3a178bf59a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:47 +0000

    win set confuse even plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_set(array):
+        return set(array)
