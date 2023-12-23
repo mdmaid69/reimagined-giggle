0c046260cc739fa35a6d8e33bbfeee22c42148db
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:15 +0000

    cost finish follow slightly direct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def divide_numbers(x, y):
+        return x / y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
