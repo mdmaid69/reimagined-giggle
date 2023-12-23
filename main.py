66b6fc1353823ce6725ad82a57f5adf034bf303c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:16 +0000

    exaggerate point violent spoil protect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
