e430f5d3ab4b0640c016695d3164020b4a334d85
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:59:00 +0000

    life Yy transport maintain amount

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def split_path(path):
+        return os.path.split(path)
