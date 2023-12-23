0ec9e0694a3020b13139ab51049233d2d37ace4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:06 +0000

    organization Gg interview certain study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import numpy as np
+print(np.array([1, 2, 3]))
