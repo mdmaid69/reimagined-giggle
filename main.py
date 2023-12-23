084d72077b33fa3f7e0f9c1ceb36905bc01e4035
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:12 +0000

    move talk town high safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
