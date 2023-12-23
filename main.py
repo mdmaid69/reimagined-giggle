a5f0927f300c5ba45f328dfea2d996a171038db0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:29:50 +0000

    separate you instant tough platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_base_name(path):
+        return os.path.basename(path)
+def convert_to_octal(n):
+        return oct(n)
