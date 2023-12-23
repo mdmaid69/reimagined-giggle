32eec3cdf13183f76f1c0424bdc3d05ef19c2740
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:21 +0000

    first conduct need back cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
