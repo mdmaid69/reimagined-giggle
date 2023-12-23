bec7b8860cb94afcc4846133c4515e7886771c4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:25 +0000

    spray want positive frightened dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import time
+def get_current_time():
+        return time.ctime()
