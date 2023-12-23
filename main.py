9f7284fd9046abd6a77aced70f74edc668ece088
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:38:50 +0000

    free Cc scale measure thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
