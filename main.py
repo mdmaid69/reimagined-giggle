69b58b19cc19e8e6cefd69dd72e022c0187e9708
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:21 +0000

    confuse getup transport dig today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
