617bf74af0e0f3022bcdee724684b4005aa6c69d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:58 +0000

    visit branch meeting plant on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
 def calculate_logarithm(base, x):
         return math.log(x, base)
