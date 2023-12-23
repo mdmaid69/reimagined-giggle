79c05d488c905b3b983f4ff8cb518eab7bfa104e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:37 +0000

    air content proud recognize hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def convert_to_octal(n):
-        return oct(n)
+import tensorflow as tf
+print(tf.__version__)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
