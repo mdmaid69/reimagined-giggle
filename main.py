88beba91add3ccdc98b785b6d7246beeffbd1ae2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:55 +0000

    label worth month tired model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_permutations(n, k):
+        return math.perm(n, k)
