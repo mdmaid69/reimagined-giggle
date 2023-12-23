cbac7f563b728ba1170a4f15c76f6cb2c934879e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:54 +0000

    hard otherwise preparation block lookafter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def square_number(x):
-        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_force(mass, acceleration):
+        return mass * acceleration
