e605e7d4b5dbca1b02760ddd9da01387137b26ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:58 +0000

    speed tradition luck fear her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
