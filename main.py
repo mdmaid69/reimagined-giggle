61f448a40ded04f3afe5a7955682273c7211bb1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:27 +0000

    butter calmly trade responsible cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
