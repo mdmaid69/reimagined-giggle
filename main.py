effbfa16df62802aceb17c1f9e8197633760db1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:26:44 +0000

    judge its on slice tall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
