60d84d082be43847666af06f1ef11d936bcb10c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:09:57 +0000

    travel course problem willing save

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
