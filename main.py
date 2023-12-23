925427c9797e7474a9437fdaaa86ce87f860726b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:46 +0000

    below unite decide example tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
