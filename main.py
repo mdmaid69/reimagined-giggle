a71e61e5153f2e0555c5b4f1afb5325bcac05c9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:36 +0000

    separate message model fire comefrom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
