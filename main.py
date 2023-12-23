e97350f7c4b0e44c7c7c3e06e74eb7a3afa805c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:03 +0000

    butter miss medium straight beginning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
