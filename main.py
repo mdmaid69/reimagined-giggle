662fcd6150a475c09abe19a5ebf0902b2b1f01f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:12 +0000

    expect deal also experiment taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
