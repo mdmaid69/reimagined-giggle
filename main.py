5985a663f51911435a38ff7e2bc54ed8e337301f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:35 +0000

    related chase shoulder noone rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
