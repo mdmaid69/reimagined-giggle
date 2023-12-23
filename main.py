8d6b49e8ba0e1c1b20fcb28e984c3fd1852eb591
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:55 +0000

    vision dark refuse tense lookaround

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
