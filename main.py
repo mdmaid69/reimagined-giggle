f4a1907b6c88e50cc54c98eb40fb0b2fe3ffbae0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:11 +0000

    of friend science keep holdout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
