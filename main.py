080be4c8bd071c189f29ab6b9c188b9beb18ebc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:28:33 +0000

    respect area worth legal free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_area_circle(r):
+        return 3.14 * r**2
