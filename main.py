a2e5d8f720eef47add29b8a3aeafafd76f5fbab3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:35:14 +0000

    release quick worry person professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
