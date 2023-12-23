e940b3989885e5bc4642a643c4cbc74ef0d9c46c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:33 +0000

    soak unable please sensitive admire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
