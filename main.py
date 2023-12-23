66e06f27e9f6888ecc9360a5eb3b8411bf1c1559
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:27:41 +0000

    little morning thing worry speech

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
