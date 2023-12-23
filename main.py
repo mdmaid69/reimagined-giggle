50b4dc559252a434b6ee6b366041b4b2b6cf90df
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:50 +0000

    slice minority material Qq roll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print(sum(range(10)))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
