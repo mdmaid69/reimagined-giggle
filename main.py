ce6c7d8dbc62bd57803b46730f579203ed24641a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:26 +0000

    Cc set cheat container order

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
