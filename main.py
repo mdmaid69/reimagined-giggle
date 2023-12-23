2bbaec4c691a6e97f7a638f13831f9e93985d4d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:00 +0000

    cover fresh long takeoff natural

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
