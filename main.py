1ea1ebb4d4c48613f95dbee70485dcb2759e3e0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:22 +0000

    somehow carry though equal compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
