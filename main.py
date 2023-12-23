7122072127902ed93208c9437474a573ebe5b850
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:39 +0000

    still Rr skill crisis ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
