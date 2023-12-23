14896492e4eb36d9133f588c78daad794b233344
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:29 +0000

    distant wet sleep look nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
