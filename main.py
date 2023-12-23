d755849b60d4bf9fc14e82328ba7ad6b2d193784
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:29 +0000

    hurt bye along transport total

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def cube_number(x):
-        return x**3
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
