e682aad8e9766e9d12ad7b8a5562147116186bec
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:13 +0000

    hard make appearance comedown listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_speed(distance, time):
-        return distance / time
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
