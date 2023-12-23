6e6313188b23896a30c520a77361263edecb81a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:59 +0000

    behind punish deliver information describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import platform
-def get_python_version():
-        return platform.python_version()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
