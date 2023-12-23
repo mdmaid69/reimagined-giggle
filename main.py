01333258511b637749fccaadd8d62e8fdf6fd271
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:48 +0000

    radio fortunate avoid week plan

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
