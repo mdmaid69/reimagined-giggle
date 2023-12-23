358aeed3d6c1b6bea87e7988f9a1a7cdf7f36e9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:40 +0000

    broad disagree uponon underneath serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
