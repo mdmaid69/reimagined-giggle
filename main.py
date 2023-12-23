94bba771da1e8669d8cf87fec2ee4e35fadf163b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:28 +0000

    deliver sensitive down she contain

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
