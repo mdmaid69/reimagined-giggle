39938914046d83d1cf884b3a80b273849195da02
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:43 +0000

    breakinto shallow call town loss

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
