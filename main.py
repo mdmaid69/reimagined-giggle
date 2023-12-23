da85eb3449a093e1f7bf978db4f33fa5e3d20a26
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:00 +0000

    way below workout safety familiar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
