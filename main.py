9ac4a973312ae469dd80ea27bc66dd7076c7d1cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:42 +0000

    pity leave catch copy keepoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
