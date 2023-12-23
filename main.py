55257292b8cd945265ad06226b8e070913976efd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:49:14 +0000

    fat woman support put 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
