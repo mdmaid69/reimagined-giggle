ff37e9f9b787e561b9ab267f31a3b5ab44c2b241
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:21 +0000

    job disturb curious workupto look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
