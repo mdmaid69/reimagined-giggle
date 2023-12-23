3b044b9a0db01ccbd8c912e6e8827507bbd4c4cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:45:42 +0000

    score in miss miss ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
