2adf94312afc52c88213d2386a0725797f3ba48e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:56 +0000

    lay rain fierce convinced admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def square_number(x):
-        return x**2
+  def add_numbers(x, y):
+        return x + y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
