2360abfd20973c89571922c37b24d344472803ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:11 +0000

    later report last pin strike

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def add_numbers(x, y):
-        return x + y
-def convert_to_hex(n):
-        return hex(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
