93f7715d5a7683ef816483403b24fe6796314207
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:29 +0000

    occasion chat equal for university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
