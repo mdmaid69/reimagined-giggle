3372889a37fc6ff0f847e9e7158954f763f6bbe9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:21:09 +0000

    them reasonable odd drive majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
