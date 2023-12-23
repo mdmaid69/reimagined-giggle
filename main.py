25b76ab2defe34a9580736a078be1fa2cadd31ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:43 +0000

    fear sensible left embarrassed time

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+print([x**2 for x in range(10)])
