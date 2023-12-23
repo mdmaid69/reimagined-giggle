808de5cb9b645487282d089571bdffe646bb655b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:35 +0000

    scrape live carry account how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
