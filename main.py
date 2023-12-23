e52b6bda0753f850a9aa54d0f96a1731fe142998
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:59 +0000

    wet plant runaway miss slight

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
