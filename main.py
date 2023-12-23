3079c166a1e20e2fb4f56a6dfec1f002fbe4f63e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:55 +0000

    boat action please break complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
