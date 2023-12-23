4beccfd3ce7efc4de172469191fc0db6b0d865fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:45 +0000

    achieve precise charge prefer smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
