c5bb74e286c932f6494692109a9fb4533c00090b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:33 +0000

    transport figure commit noisy completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
