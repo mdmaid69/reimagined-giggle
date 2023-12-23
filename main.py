a16e3e89f0aa88e8fbd3561a80d501da281ef5a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:11 +0000

    regularly ride raise answer glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_odd(n):
+        return n % 2 != 0
