9015c87459f5bfde4924a8f6369f165cf89d4d81
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:36 +0000

    music accident organize aware argue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_float(array):
+        return float(array[0])
