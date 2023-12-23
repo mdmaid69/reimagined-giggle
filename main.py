5e5d5f6800af3c3b5569bdcc6a36d64a14b1b3c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:40:33 +0000

    practical trick loose evidence react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
