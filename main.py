bcc6551012e936eb450815efc392a6a1ede7698b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:26 +0000

    concerned complain powerful she threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
