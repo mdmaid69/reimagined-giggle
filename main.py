8c74a5a20474b0aeeda4f9af88bf167ae5bffaa1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:57:04 +0000

    mess high cheque immediately catch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
