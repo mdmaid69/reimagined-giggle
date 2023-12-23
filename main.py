7266e38a3c2c155440db1171e86da0d9bde817d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:35 +0000

    hardly block middle bend anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
