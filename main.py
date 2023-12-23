fb0e8b60221a55d259ab7a7657542649fb3f7eb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:13 +0000

    wild finish likely eye move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
