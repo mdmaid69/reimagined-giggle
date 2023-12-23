6c71071d5d36f788cffe382e8013d9883c481b04
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:48:25 +0000

    laugh prevent peace worse off

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def cube_number(x):
+        return x**3
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
