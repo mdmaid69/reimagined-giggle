592bc64ef016cb31e6d31dbff42818811a843c77
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:53 +0000

    attention dress wish safe thing

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
