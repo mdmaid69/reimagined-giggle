09bf74f1d1551106103021e2228cb61ba1c4b9b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:04:17 +0000

    upset himself corner before approve

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
