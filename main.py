2f6c019c73ca9a721187da7d7971794eb1587b3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:19 +0000

    region will favourite here smooth

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
