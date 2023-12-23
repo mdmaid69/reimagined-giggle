e95db566f8fb2cdf87d77eaee8e15fed568c0deb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:58 +0000

    concerned figure account space national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
