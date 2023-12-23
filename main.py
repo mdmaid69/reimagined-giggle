baddf8ffc37a2ad5313a4728d31855618689f298
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:37 +0000

    bag discussion match fetch lost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
