a46d44fbb1f82c377dbfe4f5aa134be87ac66926
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:35 +0000

    start further danger true scene

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
