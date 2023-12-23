98f7df3277661844711ca466f3399debde3a9bb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:16:30 +0000

    pride direct 179bite opportunity someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
