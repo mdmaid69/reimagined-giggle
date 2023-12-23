892710736fb0fd36c727bd27adc109e33ca74e01
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:43 +0000

    identify claim acceptable start your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
