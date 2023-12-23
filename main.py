9ddaa65dad154baddc7f3f7396221369881a26cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:36 +0000

    eat dress describe admit compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
