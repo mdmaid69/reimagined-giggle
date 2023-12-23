40a8ee89e5789a7602c3fd8851de6e2e5537401f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:33:19 +0000

    set speed pity since break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
