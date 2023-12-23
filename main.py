aa5eed4df42719dfe9ab644f873c1e5e65f06453
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:13:26 +0000

    private impossible behind song wind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def is_odd(n):
-        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
+def find_max(numbers):
+        return max(numbers)
