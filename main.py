4452c1c2e16ae0219cea63b853ad5b40e987889c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:45 +0000

    sound before sick callat agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
