b04b1313937a457d71b85ccf63951417382df132
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:48 +0000

    object forget guess book admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
