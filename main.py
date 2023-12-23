ac20ded814692d9966b2015c26fdbcc245747cbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:41 +0000

    Jj takeout lesson contrast table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
+for i in range(5):
+        print(i)
