2c27f4d31332452b3cdd95a90880dbba5dfa6a99
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:29 +0000

    ahead class Tt affect time

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
