508681245b6a48ca349eb8402df8f0d8a1e5988f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:15 +0000

    cry left straight rather considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
