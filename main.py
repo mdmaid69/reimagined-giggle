26f4620c7f9da88e29f240ff60672b27ebb7c6ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:15:51 +0000

    dress fall whichever book order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
