c285f28dcb0c36cca15672dfd2b005fa4dea2884
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:21 +0000

    share confuse occasional march off

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
