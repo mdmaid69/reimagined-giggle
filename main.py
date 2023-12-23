bda3191b89e382ce4a7c4412964b28a690f97cf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:50 +0000

    food union wind illness before

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
