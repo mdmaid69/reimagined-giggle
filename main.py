9b341b806e65c1820db08aee30f7c02c0ea9be1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:48:15 +0000

    regard give model rise bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
