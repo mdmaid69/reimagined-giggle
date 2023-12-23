5d52dd1f1ae542c9cb881e6eb4d5eeeba59915c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:27 +0000

    supply unless who straight praise

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import time
-def get_current_time():
-        return time.ctime()
-def count_words(sentence):
-        return len(sentence.split())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
