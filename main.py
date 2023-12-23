92a13ec0bb617f4ccdc0e20f980d665a601e2a31
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:46 +0000

    father claim impression familiar properly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
