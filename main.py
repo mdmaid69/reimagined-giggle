c103bb2e685d5bdef16566e8bf04c3db636c85ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:42:47 +0000

    of seem summer cheat usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
