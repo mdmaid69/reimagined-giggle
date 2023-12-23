fc6566659bd884d26b9751994d3789c8ae1d2aff
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:27 +0000

    charge pull part independent mean

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
