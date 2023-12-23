a3b0dc3b6bea55f9cab0231ca58c149f3411bba3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:52 +0000

    back should cover behaviour affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
