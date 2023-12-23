dac20f0ba2012142f06d077fabe6a59015369ebe
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:20 +0000

    Bb talk today tell ours

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
