1a6cb99285086f86688009e84f716a8b731b40ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:35 +0000

    reserve wonder tonight freedom pity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
