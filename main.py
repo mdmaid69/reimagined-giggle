e0936d79a35c35d1ac8dee45d11db7ef1da3af84
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:54 +0000

    body previous exact tie breakout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_max(numbers):
+        return max(numbers)
