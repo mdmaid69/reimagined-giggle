2b81e5596d18daf7c5b998611c06f1416e4c8f29
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:57 +0000

    balance Qq wealth curious follow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
