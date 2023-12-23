3070264f104872a1a4a7ade98f274e02c6005873
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:13 +0000

    everyone conclusion hour second teach

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
