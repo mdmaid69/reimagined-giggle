11e09ef2ee3be4a57e5239037818a84a10372957
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:46 +0000

    judge cinema ground proper prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
