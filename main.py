8304972e866e935a9d98eff847a4dbd391c89077
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:41:41 +0000

    attractive minimum across drop offence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_length(array):
+        return len(array)
