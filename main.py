8870da99c447d761d5398b9be3648ebb18282d57
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:08 +0000

    attitude drive field bet takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
