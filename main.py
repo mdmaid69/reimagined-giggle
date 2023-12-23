9a8cc00e2c99071ef58803757c966972150641d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:06 +0000

    copy dream answer listen brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
