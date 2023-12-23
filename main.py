e4fb4c76a2180049f8beb6ffc493ce91c884e80f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:14 +0000

    authority in schedule partner responsible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+print(sum(range(10)))
