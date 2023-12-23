480e9e0045b48ddcc5fd2d22340b211f45df6679
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:16 +0000

    somewhere spin present possibly break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
