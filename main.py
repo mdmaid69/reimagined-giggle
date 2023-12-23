664b38cab22ce482204a46aaef982c7270c35e45
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:40:34 +0000

    out better throughout from happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
