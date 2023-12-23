3bea98a2b82b9b8f93d8668349aba6f5c1f49827
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:15 +0000

    switch soon load such hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
