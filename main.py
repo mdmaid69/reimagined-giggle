b7a48146c876620f120ffb0b2046b2c5fe7c712c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:08 +0000

    perform act sell tight mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def clear_array(array):
+        array *= 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
