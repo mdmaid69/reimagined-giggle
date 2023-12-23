7f3fbf63507e14256ae0e62bd06ce5a2841865b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:25 +0000

    comein thank teach justice place

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
