a29909d0a7a1f8f81e35a8bbe72d6eef8025050e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:18:03 +0000

    truth increase total communicate goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_power(work, time):
+        return work / time
