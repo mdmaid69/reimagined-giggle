abc3e7251995dbcea3cfc92bcf941c52f60315b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:27 +0000

    bet include apply computer subject

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_max(lst):
+        return max(lst)
+def sort_list(lst):
+        return sorted(lst)
