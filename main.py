2cde5095331cc9e73e245c1b4a8a522b23a132fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:36 +0000

    where level precise unit aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
