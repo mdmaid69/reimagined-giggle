8c83e0e79cd4317bd57146c28063cf31b5d6c569
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:00 +0000

    worse roll soil trousers search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
