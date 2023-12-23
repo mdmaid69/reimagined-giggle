fed04deb2246c73c1f7d1add9fb62ba949d61efe
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:46 +0000

    clever child survive proof getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
