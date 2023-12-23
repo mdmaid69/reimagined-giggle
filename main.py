2d59006f1702b885a1e79ed8e73ac681d92a9872
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:02 +0000

    noone thank shout sensitive development

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
