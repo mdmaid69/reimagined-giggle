d9fb8adf57aa742a7ff4192a98c6aa484587a5b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:23 +0000

    feel any match stay range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def reverse_list(lst):
+        return lst[::-1]
