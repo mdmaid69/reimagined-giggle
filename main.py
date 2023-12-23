447050cf9f0aef47eb5fc890583f30c93d3c228f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:16 +0000

    woman geton damp software loud

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_priority_queue():
+        return collections.deque()
