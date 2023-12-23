22f479840dcd1498d314f44141b6e81d12a0beec
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:57:20 +0000

    similar his vision wave challenge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import random
-def roll_die():
-        return random.randint(1, 6)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
