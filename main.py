d7616888e446dccb10533f28578a8a4d0089b9fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:51:21 +0000

    offer intelligent future let wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
