b45adfce6a349391497f0c2f84ad0a8889e21c08
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:18:16 +0000

    open cost let wrong charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_sine(x):
+        return math.sin(x)
