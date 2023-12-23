7734a7ede7be72e4c1cca22e80939a5789c7ecd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:58:22 +0000

    laugh gooff generally whichever used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
