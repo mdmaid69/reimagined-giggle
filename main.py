f6cafb0fc92a18262ae0bde84b0a572d08207e9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:34 +0000

    570enjoyable fond respect busy admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
