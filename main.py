bf68b8867a524ed28f4a4b3b1fdcaebbc40c5b93
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:15 +0000

    sure healthy little successful innocent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def calculate_area_rectangle(l, w):
+        return l * w
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
