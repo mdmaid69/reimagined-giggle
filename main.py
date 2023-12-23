d913854a7a6daf4003f215c7161b141be53e7228
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:39:53 +0000

    disappointed regional consider from provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
