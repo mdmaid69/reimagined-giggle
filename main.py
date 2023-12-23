7a2499814a43945ced46283626cab305657efe22
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:42 +0000

    turnup view brush height guess

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
