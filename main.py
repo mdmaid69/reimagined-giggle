a53c53895b431b2937a7b03c9e16c301e399d316
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:46 +0000

    count risk especially length foot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
