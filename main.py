4691d01c5b2f183723a50d32925a5225a464b45c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:38:50 +0000

    tell belong style train gradually

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_min(numbers):
+        return min(numbers)
