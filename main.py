1901c6925b19a87601e825e79065592d96f845d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:12:25 +0000

    accidental objection tread wrong whatever

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def remove_duplicates(lst):
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def remove_duplicates(lst):
         return list(set(lst))
