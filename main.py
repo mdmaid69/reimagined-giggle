f6eb9b305151697d5cc331d17c34efc74b7a75d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:55 +0000

    prize please exactly broken notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
