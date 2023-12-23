a5eae01aeac53d8a477847d62bc804d29fe8224a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:54 +0000

    upset store give desperate frightened

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
