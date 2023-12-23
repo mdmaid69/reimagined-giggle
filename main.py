3f1f10604f2acf09b438038eac8ef27e82928412
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:11 +0000

    day believe agree sure fail

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
