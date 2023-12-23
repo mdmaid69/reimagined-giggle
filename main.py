3fcbd435ec58b03f37acb244d06c09571456cea6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:24 +0000

    again transport rank divide circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
