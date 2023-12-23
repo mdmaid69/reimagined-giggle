eeb49c0994f2e67cb51ac140bf9f32dc3b876276
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:38:01 +0000

    could emergency wrap runout connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
