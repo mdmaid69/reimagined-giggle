4310bd2813bec794bb9e2927e2a95745dafe16ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:05:57 +0000

    design relation pretty sea tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
