563e761c1749da1c23f52aa502a1acf6c5d48bbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:41 +0000

    flavour law land sit tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_item_count(array, item):
+        return array.count(item)
