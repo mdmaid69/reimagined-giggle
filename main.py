df41bcd80361936db331d598dbc3542553880563
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:17:22 +0000

    tie task regularly strict should

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+def get_time_since_epoch():
+        return time.time()
