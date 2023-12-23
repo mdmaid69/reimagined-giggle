e2f3a1289f0117746697ca6901139c8676027e12
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:53 +0000

    equal appear smoke tie Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
