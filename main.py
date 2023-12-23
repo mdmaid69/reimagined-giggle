e90ba275e50767275059f614b63d1fafddfb28db
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:42 +0000

    condition because sort shame authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+for i in range(5):
+        print(i)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
