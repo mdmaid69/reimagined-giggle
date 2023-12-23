dd94254fe294b1b21ba5202fa1586dd7338874bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:06 +0000

    talkinto exam glad habit they

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
