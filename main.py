e1444ab87158c9b9f2d30c03be2a061baa812ae5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:17 +0000

    dark judge or travel freedom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
