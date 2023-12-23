b8cd4c4f098dd5f1ba17e262d3a91d102f58cabd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:26:22 +0000

    force light measure that fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
