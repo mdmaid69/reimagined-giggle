5658de9248f26d318e633480ccf9cd216e9d2e4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:21 +0000

    beyond married watch risk hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
