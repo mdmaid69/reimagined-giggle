7118cc9ed7545d69f04f6569d7d1af675e8d22fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:27 +0000

    alive lie lead defend tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_average(lst):
+        return sum(lst) / len(lst)
