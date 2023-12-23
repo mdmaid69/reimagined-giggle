18503a357d1c1e6e144505660b00b6fd7f5c0810
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:08:06 +0000

    Itself protection difficult tip remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
