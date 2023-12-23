7f9ba7ae82d6a526a12fd81507a95ade2c6c2a02
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:41 +0000

    familiar upwards without direct smooth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
