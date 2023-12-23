cb63335227713d850d51eedb8a0095a684d4904c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:20 +0000

    demand hardly wise reduction hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
