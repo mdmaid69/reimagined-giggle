2e3307b8d9bac9c5cccd55fe1a17c76f9a25a802
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:48 +0000

    drawing show snow borrow move

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print(sum(range(10)))
