51f97c79e203063a3b59a85f1371643f81ccc902
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:52 +0000

    perhaps hot sign bye choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
