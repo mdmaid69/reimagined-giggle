9baa4de9ec053d885ea0ade29d4eaf97aa6ffb58
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:06 +0000

    well turnover off rate key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
