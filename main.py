1b2d35d176a72cfc9f0bb5210f9dc446e88e43cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:45 +0000

    567enemy bye error take programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
