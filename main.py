6594b1257ebd907f8d43ed369268cda4be95e29b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:00:28 +0000

    everywhere concentrate expensive sore gift

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
