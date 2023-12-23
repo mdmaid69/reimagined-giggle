ed8baeca7e02ffdb222dda0aa11f0c959b0424c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:12:49 +0000

    sing comeover still watch foreign

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import os
+print(os.getcwd())
