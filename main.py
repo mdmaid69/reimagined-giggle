2fd7ddf0e06ed33dac982bad747fe49c06a7af0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:06:41 +0000

    mainly regular money gentle exchange

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ doc = ET.SubElement(root, "doc")
 ET.SubElement(doc, "field1", name="blah").text = "some value1"
 ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
 tree = ET.ElementTree(root)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
