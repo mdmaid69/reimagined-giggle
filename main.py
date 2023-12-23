8ccf130e1b5651d309ef3e031d8ea25b7ba92097
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:29 +0000

    mother allow makeinto possession list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
