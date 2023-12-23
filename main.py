54e29ca98af7c3dd9fc9e4206a835d3da94b3364
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:19 +0000

    bottom punish disapprove 572enter drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
