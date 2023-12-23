9c9448429e5804168826437819f80fd2c46f1b46
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:49 +0000

    ours any plan spray train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
