18aaab1a919ee22bd5dfa6c7fbe0e4c0c51f6780
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:42 +0000

    view allow his wrong wide

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
