1aa2524e45be43b43924e5abe09162929cd4b717
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:41 +0000

    reject store temperature damp heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def add_numbers(x, y):
+        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
