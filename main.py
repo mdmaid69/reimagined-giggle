53d7084a0019caaf86a6e11e661f8fa602ae9367
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:05 +0000

    tax priority fetch home matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_area_rectangle(l, w):
-        return l * w
+print("Hello, world!")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
