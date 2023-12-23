82825ffae75fcc8c027f9e0588030ee36d645a82
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:21 +0000

    main every spend discovery someone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
