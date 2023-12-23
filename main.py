05262f18058066dfeff1acb327c135b8160d2699
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:54 +0000

    well question remove dark burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
