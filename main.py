3443b90437a683bb3446b22033682aeeaf6b3346
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:38 +0000

    than nurse rub think nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
