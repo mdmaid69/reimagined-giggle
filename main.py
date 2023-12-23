52653a598fbe8e486a6f287b2628af992f61f6fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:58 +0000

    rain shadow active set excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
