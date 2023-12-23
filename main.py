ee8f3c09c82126b596e2a9baa2e3b4dc72e53535
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:25:25 +0000

    talkinto follow peace bunch Pp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
