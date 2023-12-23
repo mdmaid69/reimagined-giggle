511ecc9503eff2323520ea4a3593f224b79c1142
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:01 +0000

    grow between arrival tough plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def convert_to_octal(n):
-        return oct(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
