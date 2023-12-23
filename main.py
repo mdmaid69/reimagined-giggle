767a8b42888e0e9f96bff62f644e1ed8b4175a8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:46 +0000

    myself past concerned book sticky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
