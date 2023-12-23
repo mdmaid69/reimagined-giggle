6d741275eb62a0baf65587cbaa8f4f4be1780c53
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:02:17 +0000

    refuse shop threaten greeting deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
