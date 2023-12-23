5c16d2a9b0eb279a31140cdbb3e592364f43a0ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:23:15 +0000

    scrap issue answer check mine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
