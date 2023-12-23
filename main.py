92f9c6e70c87c459fe71c2768cafb1996257dded
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:10:21 +0000

    compete hers male sensible drawing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
