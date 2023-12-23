cc3f3d407d2f0c00de0cc9af771bcd7aa87c41fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:56 +0000

    noisy however complete seem drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
