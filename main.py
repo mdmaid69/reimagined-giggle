d448de6f2e7c7f2eec8743250f65422a7d5f14a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:21 +0000

    stupid anybody lower real company

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def reverse_array(array):
+        array.reverse()
