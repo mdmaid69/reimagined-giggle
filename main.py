54dc7f0f6174d1c4ffdcfc785a14edb816881a50
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:00:25 +0000

    listen platform polite war ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
