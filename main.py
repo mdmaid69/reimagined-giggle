ff380be48fb4c2b5f9d7f2f75979e5ead39707b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:58:17 +0000

    mess row important organization where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
