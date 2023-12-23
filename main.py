819bec7816fcff3370a6c7df425ae2c0d7e615f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:08:54 +0000

    571enough trial fold feel negative

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_palindrome(s):
+        return s == s[::-1]
