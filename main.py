1aa274eef7982ecf9a83ce06bfa719a58a8fd7ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:09 +0000

    relief tie evidence under avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
