04fcae318568cea2e5f69c14434c6e99b49b4370
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:05:20 +0000

    complete during see there failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
