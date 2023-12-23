88611b414393d26279a11d695c6d9c4eea6c8280
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:24 +0000

    table crisis defend secret aware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
