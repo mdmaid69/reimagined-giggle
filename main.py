656efbd5b609e4c52b7feded4bc471c7523d416d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:02:42 +0000

    very flow small whether discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
