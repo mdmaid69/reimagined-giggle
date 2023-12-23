94ba744621883d2c5c9cdf269b6d0c8aba43f490
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:24 +0000

    worried 572enter takeoff plan choice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
