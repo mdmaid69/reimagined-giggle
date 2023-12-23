c8387c9084c2f1ba9b4788b8e79654e8e1b757e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:58:52 +0000

    understand stiff develop programme grow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
