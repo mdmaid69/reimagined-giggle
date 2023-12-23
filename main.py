d5df9f60c1c6aa97f5d0c6d213ce2da5463a030f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:43:15 +0000

    private hot pride after crowd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
