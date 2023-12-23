033fcda42d77482f92fad1aacee37cd59d08e4a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:41:23 +0000

    good now regional myself accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
