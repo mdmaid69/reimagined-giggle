b721563dc5fb593a0bd476b02ef9752fef5d587f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:21:04 +0000

    past nowhere to send she

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
