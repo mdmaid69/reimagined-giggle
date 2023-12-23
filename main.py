9714348fc8ba12c0f5fa56b7df20d7ff162b65c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:42 +0000

    shape machine bunch meet word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
