449d678eb207667c53c7e83a89da0be5fb367b8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:31:08 +0000

    opinion keepfrom congratulate can over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
