bd516ce25d1d15c21b0e5d0120049d083647f23e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:33:13 +0000

    harm its separate trial quantity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
