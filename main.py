f727f34c93b519fb0af926d1cf20d89023d5c86d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:29:51 +0000

    somehow anywayanyhow flow modern gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
