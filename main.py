37a55afd455addde351a9dbec716e154a461fae5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:18 +0000

    accident flow turnon healthy set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 def find_max(numbers):
         return max(numbers)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
