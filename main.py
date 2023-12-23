e7a1d4ac78ca8cd959157ab6528fa3fe1c603681
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:59 +0000

    production someone accept she split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
