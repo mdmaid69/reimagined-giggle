ebf55c651931887088cb4096e34558b267720dd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:35 +0000

    wall responsible far argue blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
