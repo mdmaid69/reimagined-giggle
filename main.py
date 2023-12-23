c7ea72ed5802d310e27eccd19fe20761bf61bac2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:24 +0000

    cheat tear year patience party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
