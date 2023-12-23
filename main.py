a50c8f2c9eb2a48cf1d6eaa82a82267e853ec66f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:09:22 +0000

    that spare why sting worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
