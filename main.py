3cbdf7ce8f4f1262e4d26dd7f57bc02da7cd5a68
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:20 +0000

    visit opponent sell top break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import sys
+def exit_program():
+        sys.exit()
