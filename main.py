3aab59bd80ed7d26f41ce01e1b355bd1ba76b9b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:25 +0000

    turnover ring way type serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
