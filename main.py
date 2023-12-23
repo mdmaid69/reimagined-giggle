dcfb15807636f8d78a9b8914c0929b87547a41b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:16 +0000

    wait sore force perhaps bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
