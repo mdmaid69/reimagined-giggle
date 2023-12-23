90b92782d0e66db71a1a8df87edd93ea608767fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:51 +0000

    post hurt square before create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
