aac0fbb8093ef1afafdfe5e63727f4403f72260b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:47 +0000

    afterwards holiday generous spend closed

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
