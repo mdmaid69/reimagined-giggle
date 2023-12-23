28ff6c41c3787ac221971eb3f026c3e8f76cb121
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:43 +0000

    since calmly steer view similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def add_numbers(a, b):
+        return a + b
