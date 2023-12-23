98d53504d11b04dc0c929f40a84a2924b372fcbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:02 +0000

    medium nearly smile trade though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
