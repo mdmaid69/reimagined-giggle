a734392874db9153168496c2e88e63a5752a3226
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:01 +0000

    runover now base fall correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_tangent(x):
+        return math.tan(x)
