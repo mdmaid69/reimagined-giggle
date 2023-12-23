981b46a9641f2fcfc550e4bccc65e651c8467290
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:00 +0000

    accommodation recent exam on various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
