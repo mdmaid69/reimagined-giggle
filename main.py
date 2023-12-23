95d73ca303d4532b65b6a7f1760268f5608a3483
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:46 +0000

    declare 571enough invite think suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_float(array):
+        return float(array[0])
