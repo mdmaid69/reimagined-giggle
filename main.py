0aaa833ca806ba838815fb3cfefab66597a0765c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:36 +0000

    blind thought pleased world freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_string(array):
+        return array.tostring()
