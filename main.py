f367a7522ab80ac563dccd29caed5b0b70608acd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:15 +0000

    action air gradually recovery keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
