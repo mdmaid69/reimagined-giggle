005609b103e0a14f89e30c5aa148846cc5672d5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:46 +0000

    cheque you communication claim move

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import time
+print(time.time())
