728d32185a0366077063097cb7ab9e25fad4478f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:06:01 +0000

    dance lose relative past corner

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sys
+print(sys.version)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
