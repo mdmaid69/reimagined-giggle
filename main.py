b6c57a4983d8a7e70619837211c8b598777e6afc
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:01:25 +0000

    move believe sit Dd arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
