8d0462e9d217bc99c6ab2966439b44f03de34818
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:19 +0000

    choose write outside judge rough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
