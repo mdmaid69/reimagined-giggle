74aae37ed6a4093cfe07d51ff60216eb144fc89a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:29 +0000

    Ii respect people pain who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
