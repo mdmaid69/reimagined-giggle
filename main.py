21b3ad312da9e2d8ad70d6e88ef1d35ab1243158
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:08:39 +0000

    statement mean occur middle comfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def cube_number(x):
+        return x**3
