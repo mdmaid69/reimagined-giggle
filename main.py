83b45018b3a212b4da0a199f2bba884e6850007c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:27 +0000

    herself worth idea early transparent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
