e06713994c3af71175a05a86fe11bb6b5ea9e9b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:20 +0000

    crisis afford repair which reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
