90a9c64bbc1f653df7e4cc57a833229ca8ceaefc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:50 +0000

    pupil there dress check proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def count_characters(sentence):
+        return len(sentence)
