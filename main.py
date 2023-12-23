4748524f23cd7767b5c82934bf67fc2b3b91691b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:03 +0000

    search immediate easy apologizeapologise same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import collections
+def create_stack():
+        return collections.deque()
