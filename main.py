05c7d4972ffeaa600d99132b46e432226333e0a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:16:50 +0000

    bend bath actually round split

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
