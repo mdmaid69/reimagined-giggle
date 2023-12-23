497b9a5113a3e41b6702c034dcca6f177b84ea2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:47 +0000

    slight basic sink in exit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
