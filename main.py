fe0b6bfc849dbf5f4e9559ef3cf66a158b6151fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:38 +0000

    awake performance scare her workout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def clear_array(array):
+        array *= 0
+print("Hello, world!")
