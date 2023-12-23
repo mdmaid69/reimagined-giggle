a8ec87c55c54c5b4fee95eb0449d4ec57c9ff031
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:51 +0000

    light pocket wish runover count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
