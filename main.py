6c5f48fcc59675128fddee2d4c19b80eaba75100
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:12 +0000

    eat close inside present dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
