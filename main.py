e6b365a2541ac95a011dcb09951bca2126e2f11c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:19:32 +0000

    close notice age area crash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+        return os.stat(file_name).st_size
