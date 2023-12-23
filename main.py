6ffff04792a3dea95ea2fc928e3cf1686f823572
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:49 +0000

    beginning contrast unlikely morning comment

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
