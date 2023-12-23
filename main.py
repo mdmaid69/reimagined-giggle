592c62ed26499c12cf9e0422ff499d8daac99b11
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:04 +0000

    attempt film gap sink regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
