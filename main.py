aac8171efdf02b5c8acce12a3c79da065552ba1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:42:59 +0000

    impressed invitation corner find table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
