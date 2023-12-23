f523c6dd0792dbfc531c978283765d2cb2fa7419
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:05 +0000

    Ee course conversation crazy count

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
