78af421ce5c4b532512ad3d2dfbb5faa13f5509a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:12:00 +0000

    deep easy height space while

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(5):
+        print(i)
