95ecba2bcb627008f9f86f3f5c3dd9e4812ef31c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:15 +0000

    workupto closed check give experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
