1de953bf3f37dc052b957fb747a4d621037eeaab
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:19 +0000

    avoid spot put similar breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
