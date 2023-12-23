854e821adfa56183930122bd65310e3148edb8f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:34:05 +0000

    question big deserve reason tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
