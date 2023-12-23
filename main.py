cd86f0387edf271d78a058a3737996c822a956ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:04 +0000

    crash sentence play makeup journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
