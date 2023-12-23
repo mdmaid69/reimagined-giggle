51461ccc7f3a92c967e144835cd5c8eeb002e824
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:43 +0000

    pay schedule inform bin choice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
