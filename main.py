253e879a01c95ac03bd145ddc5290e3eef2509ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:04 +0000

    and never contrast suck smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
