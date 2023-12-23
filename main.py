48ab5e04269ad4fadeba52bd393d38f0f2376d73
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:50 +0000

    afraid swim decision reliable uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
