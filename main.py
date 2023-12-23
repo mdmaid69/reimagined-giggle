dd6f2c79b2a64c74fb6ec80ea5c2010499604d3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:25 +0000

    secret during wet obvious whenever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import datetime
+print(datetime.datetime.now())
