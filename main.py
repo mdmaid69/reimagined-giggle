3df51a0f6625a0888027c30a4632cf06470cdf59
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:25 +0000

    thin minute offer independent television

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-print(math.pi)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
