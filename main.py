ec539076e9c5946de04b2073b294761de8d0d1c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:20 +0000

    subject subject breakinto still carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
