3f0f04ee3823d4b309f3db1d45e423df7f4ff73b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:21 +0000

    slide standard determined cry blow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+print([x**2 for x in range(10)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
