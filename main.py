123a468892bf0ad4cd9c8f3f176151e7fd88fdc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:33 +0000

    furniture mood factory alone trust

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
