e9c65a6bd9d4471788cf95ddb72c75c4795e19c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:13:53 +0000

    inform otherwise equal movement head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
