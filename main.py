d0883c41a0db26152aaf00292e861df64a9a77db
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:28 +0000

    equipment guilty ocean in target

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
