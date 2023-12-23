f33f3a7d9d907f8b48ad378aaf37920b8ac8991f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:58 +0000

    command crash sort behind glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
