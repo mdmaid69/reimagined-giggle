285c200cc4cc47785d069dab5c9182d9fe64ab4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:40:49 +0000

    game underneath share business rubbish

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-for i in range(10): print(i)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
