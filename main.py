4b9158201a79e7ea567615f7925419b337bb56ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:03 +0000

    birthday development exciting prevent breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
