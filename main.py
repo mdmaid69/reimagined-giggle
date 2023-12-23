38133853b3bcb8a0e601e586d23602f9acfe3969
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:02 +0000

    plant excuse determined interview break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
