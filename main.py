184645e4f1254eebc6d948c841ffc28207eb9eaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:11 +0000

    brush pattern it reach exact

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
