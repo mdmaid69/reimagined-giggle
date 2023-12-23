8d6db2685957040ec5c30c6f576610bbc715da55
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:52 +0000

    ready draw the quiet wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
