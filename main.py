52f2db95230b0b81905bb7d5be16298625772dbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:46 +0000

    advantage takeaway something if although

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
