11da7e50d94ddd67ce76b39ae61aa6a873e28b39
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:56 +0000

    type question approval new or

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-print(os.getcwd())
+  def convert_to_octal(n):
+        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
