1db9e12a6303664e5ad09efde6cedc88a3ad301b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:05 +0000

    further bringup classroom scared smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
