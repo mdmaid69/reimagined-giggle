1a291689635acebdecf5a0c1749f56a37e6b0d32
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:49:30 +0000

    the sit my abroad school

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def subtract_numbers(x, y):
+        return x - y
