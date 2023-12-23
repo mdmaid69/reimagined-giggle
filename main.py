e69503ee84a2e93274df31e360d0b6c7838b8ab0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:32 +0000

    trap hurt contrast cost receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
