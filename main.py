44f66767a479dcc2f8bbfedd68f0e81f99ddc1f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:43:25 +0000

    raise edge furniture sometimes calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_density(mass, volume):
-        return mass / volume
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
