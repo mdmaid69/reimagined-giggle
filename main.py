5378970fb21e54959328027bdd0b40a8875a54ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:41:03 +0000

    hit keep pour trip guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
