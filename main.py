1c5ed0d145200259baffdfc97a5187cf9022d51f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:49 +0000

    test serious pool adjective cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
