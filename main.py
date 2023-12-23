2c7e0885c76e01b92702e71bfddde79b6993bd18
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:37:40 +0000

    put steal matter probably explain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+print("Hello, world!")
