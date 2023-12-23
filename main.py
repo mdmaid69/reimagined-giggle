b3a7644e059baa0b7e2748aeb468948f7add66eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:48 +0000

    light turnoff idea gas talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
