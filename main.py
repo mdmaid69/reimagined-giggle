3358c1f2ad00da0c956303f10fee49f896bfc880
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:27:07 +0000

    worried concentrate offer turnaway only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
