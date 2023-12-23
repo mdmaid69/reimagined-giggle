0d828c857fefe8d95afe50922eccc9c7cb2d4200
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:38 +0000

    write tape lend form getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
