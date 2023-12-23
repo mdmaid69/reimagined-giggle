e2c3d4d02528fc26ca124ffa2f5c04b063bb2eab
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:31 +0000

    abroad dry true quantity type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
