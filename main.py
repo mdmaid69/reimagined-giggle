ff70ca6962c6a0ca17112333cdb9e5e259921527
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:16:51 +0000

    avoid unfortunate shock oil recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
