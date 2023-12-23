c99f8e09466e9152765a8f1c80032ce2c5d4da0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:05 +0000

    control line seriously job claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
