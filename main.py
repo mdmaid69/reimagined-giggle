23f210a7f5a2561a64eb0b7329dfcc2b59f33009
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:35:26 +0000

    fork secret image better fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
