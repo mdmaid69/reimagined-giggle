3038218d8d4639c3e1a0634a844f99de4188b4c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:49:38 +0000

    tight avoid deliver fresh wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
