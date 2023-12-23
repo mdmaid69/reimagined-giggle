01b175ea90c9174c8b695ae7d0d94f609200c0af
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:59:49 +0000

    legal have original praise home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
