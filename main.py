3008f954c9bae53640c7db7c3278f40cabf182f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:37 +0000

    approve getout only lucky born

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
