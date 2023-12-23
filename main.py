c9df9d46301781136fe3a0873f7ae13757658159
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:44 +0000

    cruelty she rise raw improvement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
