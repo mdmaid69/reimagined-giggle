5b306f91884022a6e038b8c206c26ea8c0adde79
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:08:42 +0000

    sit effort Itself comfortable sticky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_area_circle(r):
+        return 3.14 * r**2
