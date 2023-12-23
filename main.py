7bfc2bedefe4e11616013aebefe3a890bb0339c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:37 +0000

    Yy indeed need politics hang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
