ffd2598b8acd85a4b7f3ca4d1f3f09de670de076
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:45 +0000

    so correct limit getoff fire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_min(lst):
+        return min(lst)
