11667778d829c190b9cda297d886a9e37c4ce5aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:48:17 +0000

    cum his thing wrap nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
