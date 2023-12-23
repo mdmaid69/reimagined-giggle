a74fb9fa327857f87b86d4d4d85451e763c1dbc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:35 +0000

    stiff direct left morning translate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
