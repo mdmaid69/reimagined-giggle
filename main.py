1604db7e87dc1723f4a29fcd66a49706d77203f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:43:06 +0000

    involve nurse to conduct dig

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
 import datetime
 def get_days_until_next_year():
         next_year = datetime.date.today().year + 1
         next_new_year = datetime.date(next_year, 1, 1)
         return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
