21a637905d207ca1ac4d740f70afbc7e6105984c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:35:03 +0000

    look arrival talk Gg uncertain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-print("Hello, world!")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
