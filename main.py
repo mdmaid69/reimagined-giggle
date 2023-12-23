4073e699361b70dc5b9a8cd480175ef919956d5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:46 +0000

    where study high song bringup

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
