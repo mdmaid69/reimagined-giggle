02163bf9c99c109a919e1077aafd5dd31684e7d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:39:51 +0000

    crash unless cross score breathe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
