96f3c3184c664c8477389adb4033163ec0a8d9bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:44:40 +0000

    prevent contrast improvement thirsty trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
