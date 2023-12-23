0cc80c252b9e5e336781550887e0b22279f5c99d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:18 +0000

    final less collect space first

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def subtract_numbers(x, y):
+        return x - y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
