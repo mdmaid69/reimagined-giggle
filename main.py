e55717cea523b0431969f9fb33b788bd4eacfd69
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:45:48 +0000

    unfair spring bar over true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
