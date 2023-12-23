6ca47e3d8a650c32f7b8e2798964178c0f90da22
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:23 +0000

    impressive decision exciting tidy again

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
