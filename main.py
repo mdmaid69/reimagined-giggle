38258255268486918ac2f66db8b1978834bcf124
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:35:05 +0000

    direct pupil Jj fond announce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
