db06baa837110f84638007be04ea70ff437cfb15
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:57 +0000

    failure anywayanyhow where Yy change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
