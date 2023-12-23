0915dff5d960b22f567312462b92bdf31f56865a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:01 +0000

    nearly operation concern coat serious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
