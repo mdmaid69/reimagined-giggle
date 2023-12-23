cf402964a005d26914db772410434f02527b7e8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:02 +0000

    private path telephone punish cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
