b8575330a31e27d1fdd945b5e6b29eb084a3c487
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:15 +0000

    freeze chief must time beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
