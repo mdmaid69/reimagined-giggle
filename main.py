b40f489fc37f872bc3be6b39bb84c6a6a90b5580
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:48 +0000

    mood aware book tone negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
