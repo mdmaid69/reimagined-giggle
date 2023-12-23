285cc2f6d41dfa7efae639ef93a7b666ff27556b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:05 +0000

    organize action Yy unit trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
