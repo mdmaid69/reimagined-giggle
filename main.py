352953ac51d9425f598acb1fdd48ef08671329ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:42 +0000

    alive consider family moral lesson

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
