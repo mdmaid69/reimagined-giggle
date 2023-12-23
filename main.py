ab9851cb98d4650bd6cdeef9d932bcfe75a62026
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:41 +0000

    in chat anywhere little across

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
