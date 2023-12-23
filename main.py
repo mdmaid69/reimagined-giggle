752ce778249b34f140ce65f8c1d099bf959c5fbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:43 +0000

    what each exaggerate leave thinkover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
