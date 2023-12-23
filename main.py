46e29cea23b66ae25badd8ef75adeefb99ee1796
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:37:05 +0000

    steady long Yy government picture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
