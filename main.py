f5b2b52ca78a9983a3946660fb4f47105d90d92c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:54 +0000

    or estimate immediate most sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-print(os.getcwd())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
