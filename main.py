53697ac4633ed41aa9ace4f341182a95c097df18
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:47 +0000

    switch avoid traffic centimetre loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
