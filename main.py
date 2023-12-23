c591b782ba5eaff9fd01abbe34f3c10d8fdf926d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:25 +0000

    worth funny lock moral among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_item(array, i):
+        return array[i]
