df403c883e4553a0716ec868f167a49a56d11581
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:35:40 +0000

    middle comeround use only live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
