ce7cdc3edcbe5eb2b5956895f394b60c1d160786
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:55 +0000

    grade this unkind cool position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_even(n):
+        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
