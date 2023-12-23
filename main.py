cb6c8acc9d25406ca24fcae58f8cb82dddab0de1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:04:45 +0000

    to light goback(to) yourself equipment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def count_characters(sentence):
-        return len(sentence)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
