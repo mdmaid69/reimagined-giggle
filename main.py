097db54a3985def18702b0fa68e9f8af2f63aa9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:05 +0000

    this worry dull train instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def greet(name):
+        print(f"Hello, {name}!")
