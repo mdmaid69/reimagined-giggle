b151dc0bf2522a7eef8e0fc816ac114eb9f1a733
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:25 +0000

    person bottom rain raw various

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def cube_number(x):
+        return x**3
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
