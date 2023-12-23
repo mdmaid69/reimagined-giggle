d83bad5f45a269116eaacdbaebc0c6042f0274d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:15 +0000

    follow lookaround brush appear before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
