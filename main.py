7d2deae818421821b7c35ad9bb09eeac5f9ff1e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:19 +0000

    organization street Kk snow complete

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
