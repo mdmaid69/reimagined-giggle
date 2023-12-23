b20286c5ca159e9a40a907ce48bc58d8ccca6fc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:21 +0000

    treat interruption bringforward concentration backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
