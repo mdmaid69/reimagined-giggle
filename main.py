437d1e53ad05add67010c27f506354c4a5ff3c7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:42 +0000

    variety exciting pleasure bringback keepoff

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_max(numbers):
+        return max(numbers)
