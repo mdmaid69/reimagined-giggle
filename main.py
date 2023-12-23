fc116fbcab0af9b2b96d19efa28a4190580f0a1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:28:07 +0000

    means well rich backwards slow

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
