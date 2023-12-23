6cfc7f336140cf62d4b8acee7bfc56eb1739d3e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:35 +0000

    please science reserve bath leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
