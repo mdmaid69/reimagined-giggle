baf14cda312bb02c5fc71c23a7179439aadd490b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:04 +0000

    this would Kk accordingto uponon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 def calculate_area_rectangle(l, w):
         return l * w
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
