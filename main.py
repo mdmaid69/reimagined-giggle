ab6dbccec78427a02cfb02b9a2c441e187bb7d34
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:14:47 +0000

    degree disturbing accident food compare

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_work(force, distance):
+        return force * distance
