100c7a96633dc29eedba40c47657a6508dc79a83
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:33:12 +0000

    unhappy either cold interfere brother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
