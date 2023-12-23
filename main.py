4154d4b061ab9f398e45990c8659a846830a705e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:38 +0000

    solve or pen price dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
