a0098d63524bbf7726e84768d96e45b5f38fe62e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:09:28 +0000

    like escape below frame hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
