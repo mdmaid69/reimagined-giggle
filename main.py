006d7c1d5537cad775e5e5b81e8fcbdb03c2bc12
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:26:48 +0000

    long off reject takeout excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
