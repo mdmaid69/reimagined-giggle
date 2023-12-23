21e4ee1889f63cbffda75e3f2e85f810937e305a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:58 +0000

    frame possibly Mm touch frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
