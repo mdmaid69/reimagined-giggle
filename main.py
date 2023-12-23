2a90c4b65b6741a9a894e1af895e72604359f4c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:41 +0000

    crazy inquire name actual work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
