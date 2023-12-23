596aad0abe09ba9f0e9703b0ff390e275ca58b7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:03 +0000

    hurry leg really bye arm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
