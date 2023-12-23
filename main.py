191f46c20bcc423f1cc4e84ddde918417b639862
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:40 +0000

    attractive sting turn moon care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_current_working_directory():
+        return os.getcwd()
