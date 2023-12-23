1e0587e82f2a02628333ff0b15754567326f8c45
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:13 +0000

    blue satisfactory second fetch nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
