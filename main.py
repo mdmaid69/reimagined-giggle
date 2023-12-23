faeb534562f7c5949c1c84b85ba99db88c857ca6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:45:52 +0000

    scrap fire same excellent breakup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
