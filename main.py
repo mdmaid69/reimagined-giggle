ab6f6f7c141c9f4fd4203ef17df8906dd8c4f711
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:19 +0000

    similar hear used hope flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
