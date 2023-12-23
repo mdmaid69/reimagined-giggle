2473c6664e494c7b9f441d2f488591e4c3a2f43d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:46 +0000

    believe closely task exchange style

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello, world!")
-print([x**2 for x in range(10)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
