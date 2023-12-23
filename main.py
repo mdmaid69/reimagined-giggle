89c741c469ecab2afe8a81a89296cf3ad90416af
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:51 +0000

    picture dream unfair below woman

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
