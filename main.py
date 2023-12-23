5d42d9fe3b4f5f5c592fde3be3b977c4a739917b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:37:03 +0000

    grow question ago female force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
