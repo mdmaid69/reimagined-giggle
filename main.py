42fb284f2f44b979c9f7ecac0d3623c7d86ec7ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:08:01 +0000

    manage definite left point improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
