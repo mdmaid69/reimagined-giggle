b5cb0d9ebd09c5c79003eb32a9218be2422678ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:19:25 +0000

    size ticket build later nowhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-for i in range(5):
-        print(i)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
