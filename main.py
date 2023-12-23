c49834833d07f6d33f19f5bb7197b83a09c7ad66
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:08 +0000

    mark keepup fond nerve now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
