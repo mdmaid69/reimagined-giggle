29e71402ddfa7a18804912d0dc780c401adeefb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:49 +0000

    back reaction land above heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def roll_die():
+        return random.randint(1, 6)
