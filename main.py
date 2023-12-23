105b782bca2bef1774c90695e6730f0b6efcc461
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:54 +0000

    brush money urgent month blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
