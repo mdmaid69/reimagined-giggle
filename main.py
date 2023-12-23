5548a5ffd8b8e19fe63e737351c1408d7bc2317c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:24:05 +0000

    sink store impression regular embarrassing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-print([x**2 for x in range(10)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
