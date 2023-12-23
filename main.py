87506d5cfd2a8eb3632ad5268c7fc26c456f676a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:06 +0000

    Ee over involve yes term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
