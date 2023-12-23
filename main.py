8a073b5f72856f7a4384e578e7b74d94464d4489
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:19 +0000

    food passage return cover present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
