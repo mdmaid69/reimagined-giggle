aa326de1f50075919e7680e9bef662fb7e46aa91
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:07 +0000

    Kk manage disgusting adjective surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
