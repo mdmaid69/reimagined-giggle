ee7a3bab2cc5340a911f96abbe1312242cfc10ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:18 +0000

    disgusting cross intend belief bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
