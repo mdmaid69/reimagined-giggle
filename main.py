c18a4b5aaae745fff4716a9965542180243d5c49
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:06 +0000

    break hole though bend giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
