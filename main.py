965eedc070df76e17c5ef597e5129303b09836b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:30:27 +0000

    shout nothing ago dry basic

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import platform
+def get_os_info():
+        return platform.uname()
+for i in range(10): print(i)
