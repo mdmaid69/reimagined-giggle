e9e5850e54bc0e6ae900932982be84b8bec5c33c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:06 +0000

    arrival exchange hers trade fall

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
