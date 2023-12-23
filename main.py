0a4957eba8953001a098007b2fd82f724cb94f73
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:56 +0000

    polite ring here identify relation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
