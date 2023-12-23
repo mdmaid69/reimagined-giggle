3dc70925c913119870093ba3e33032fa8903c050
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:51 +0000

    count poor demand catch then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 x = 10
 y = 20
 print("Sum:", x + y)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
