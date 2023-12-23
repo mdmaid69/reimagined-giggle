d0756861287dd596bed8ecb6a62c5f9ec8153fb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:57:04 +0000

    Cc imagine mood should ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def check_if_array_contains_item(array, item):
+        return item in array
