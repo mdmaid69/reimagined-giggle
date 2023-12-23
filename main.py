97ba03618a5fcb6c5cc7a0cb4c94abe7f9a264ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:52 +0000

    variety good wander fortunate clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
