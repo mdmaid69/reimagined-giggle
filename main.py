97114b3eb97e0ac53aa508169183f9df315a5438
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:40 +0000

    quiet dig attempt stamp interview

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_unique_words(sentence):
+        return set(sentence.split())
