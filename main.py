65d7161dead9269459f60bc88938851b17821be8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:05:15 +0000

    how bound burst neither attractive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import tensorflow as tf
+print(tf.__version__)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
