77436023dbc0000d70cfaaa7fe1f737208bde4ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:34:07 +0000

    open avoid clear everybody waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
