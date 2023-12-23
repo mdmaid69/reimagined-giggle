e0a5f7145e4f23aa4594f6284a474fe12246116b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:58:13 +0000

    temper swimming communication bye common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
 def calculate_inverse_hyperbolic_cosine(x):
         return math.acosh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
