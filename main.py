c3ebc69f4a406dafe5714a7488a589d766fe8bcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:40 +0000

    part problem loss reasonable yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import sys
+def print_python_version():
+        print(sys.version)
