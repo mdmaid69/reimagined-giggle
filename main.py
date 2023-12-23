20ca1c3726a91c0d25e2b4163c84cd9b518e1de0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:33 +0000

    match model high motion later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
