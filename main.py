5ee902acad9a44e7eed4bfdf68235f96da690e09
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:58 +0000

    job world proper concentration content

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
