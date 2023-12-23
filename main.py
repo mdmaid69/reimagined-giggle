a7c5b9b9a7b332a55bd15cf35ff6d88e4b4a03c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:42 +0000

    thing read main translate back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
