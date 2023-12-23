1610a34f1787d7f97362a10fdfcce9d0051251ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:28 +0000

    stone front back somebody unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
