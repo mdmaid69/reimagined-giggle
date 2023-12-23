6d10086d7a53cf3073469c4282ec3a51168f1f33
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:54 +0000

    print Itself mouth reject since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
