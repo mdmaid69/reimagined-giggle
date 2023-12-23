e6574c9a60eadbb270c0e4ec60fa5d352e7948df
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:40:25 +0000

    habit aim rush excellent division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
