ba57e736b529703a835fc96b53b665a0ceac5c92
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:37 +0000

    high justice knowledge design slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def reverse_string(s):
+        return s[::-1]
