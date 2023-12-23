32047140a43bc07adbe7d6139a0844f927ec1e1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:25:11 +0000

    later figure that convincing stamp

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def count_elements(lst):
-        return len(lst)
+print(sum(range(10)))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
