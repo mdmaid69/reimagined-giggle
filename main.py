ea0f8fc459b65678a17c64eec1b49aac65cd76d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:00:05 +0000

    idea mention entrance may mainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+print(json.dumps({"name": "John", "age": 30}))
