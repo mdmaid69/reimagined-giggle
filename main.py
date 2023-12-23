8be2687ea5be50b77eebc181944f9d0dcb9706b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:09 +0000

    object comeoff town pay final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_area_rectangle(l, w):
+        return l * w
