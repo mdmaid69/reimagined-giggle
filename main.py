f9952641c1e699768a3876c567889e84e9b59fd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:00 +0000

    remain exit education anybody pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def square_number(x):
+        return x**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
