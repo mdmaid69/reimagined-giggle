20bcb2cdbd0e3214de720f0a3861b787dd58651b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:27 +0000

    deaf description burn breakin ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
