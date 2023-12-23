d8286ba0b660a41961244abe83a6faf540eaa854
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:55 +0000

    reply single callout skirt danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
