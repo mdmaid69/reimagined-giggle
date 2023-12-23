e36f99c67d258fec5a8bce69d9fcf466126b1001
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:44 +0000

    copy carry ticket 570enjoyable cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
