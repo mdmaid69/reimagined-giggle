39b8b6a5ad5d19453ae1c26fd1b1ce503513b61c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:18 +0000

    pleasure least play receive myself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
