be4d7ea12d00ca06259ae00a0a6bc5ce82a4fb09
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:48 +0000

    comment holdup choice window long

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
