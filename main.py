6fea47d198bde509d30d9e928b0f86b8c8130d8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:39 +0000

    whoever appeal waste before opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
