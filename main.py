aac99f261bd6b41ba20980c0d01783f7c08688f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:34 +0000

    trade left fly appeal choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_odd(n):
+        return n % 2 != 0
+  def multiply_numbers(x, y):
+        return x * y
