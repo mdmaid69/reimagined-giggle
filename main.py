d7e4d930359de1597659b0d5a9d65965c22c07e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:44 +0000

    unfair environment father preparation flat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
