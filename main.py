da97b34140c994ee4cf741a5a6d4e5e43f7d2b3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:26 +0000

    definite noise clean system post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
