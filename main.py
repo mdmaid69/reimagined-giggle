2e34145a22fcde0935799a2c6350498b5a86a810
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:15 +0000

    table geton mean programme blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+print(time.time())
