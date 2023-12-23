7d15c0733b56be886d92af4bd7def2e1707bdc1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:46 +0000

    language attract equal divide uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
