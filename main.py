606b7f2e1f3cd697845c9721b0d2265e220e9c66
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:02:49 +0000

    address hot praise apologizeapologise act

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
