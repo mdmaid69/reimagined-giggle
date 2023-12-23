9d8eb83552ecfaa999a77a2e5af8e5e7471acb37
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:50 +0000

    disagree front mistake bill patience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
