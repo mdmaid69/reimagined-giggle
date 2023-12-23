eccbd6072b1591e55af3bafee9bdc5d69f5b46c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:26 +0000

    mystery play consult belief drop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
