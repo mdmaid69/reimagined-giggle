8df04c8cd42c27dbdae92e7ae26deab8f1ae0c55
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:21 +0000

    keepfrom close mean fine best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
+def calculate_area_circle(r):
+        return 3.14 * r**2
