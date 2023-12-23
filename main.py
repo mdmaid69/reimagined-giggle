2c2ad57e749b9f4e4267ba6aae1b2156e4aec2a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:17 +0000

    raise cry guess culture wear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
