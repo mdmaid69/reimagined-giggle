72037e262e6d13ff501b5d887e6bbf3277df4bb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:25 +0000

    sell value view turnon alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
