ab22a294c8362121519ec9c929c081b75bccb192
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:25 +0000

    worth seat blame noise private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_user_list():
-        return collections.UserList()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
