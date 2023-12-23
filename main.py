96740ddd39298588ff7bd6c7a9de882eed46a260
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:47 +0000

    lead piece trouble other its

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
