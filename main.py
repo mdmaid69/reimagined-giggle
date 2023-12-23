8ec730a411aeed15ad4dab02441e7bd483719880
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:36:06 +0000

    wave thick giveup shelter day

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
