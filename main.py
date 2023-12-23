728140b048cef1e1b62ae3faab9363f1281a9191
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:14 +0000

    responsible total own terrible pour

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import random
+def roll_die():
+        return random.randint(1, 6)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
