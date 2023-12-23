f8f859f4263c9090b8b9ab48ad5e381b91a2e3cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:08:21 +0000

    suggestion up remove snow even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
