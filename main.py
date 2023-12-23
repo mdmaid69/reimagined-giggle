b31090b849cd70b93cb4f26853de47fddd9579de
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:28:06 +0000

    rough already cheque course cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
