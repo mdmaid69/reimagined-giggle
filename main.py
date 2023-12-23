86dcfb32764864152b45985cc67d7e3c9a46a0df
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:53 +0000

    listenfor concentration try meal mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
