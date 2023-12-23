4eacfb5b51edb7237fcb4d26043a90ee61476a85
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:15 +0000

    exchange show hope public relative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
