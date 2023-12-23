3ac759fa0b6d97af0d35e4dba783695740f4293b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:28 +0000

    connection generally expression them pair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
