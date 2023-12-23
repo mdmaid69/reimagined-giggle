b16b7b779805390d2af99d7319245b2c01bad073
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:24 +0000

    forget phone taste bill silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
