3d2cea3206c9108107537c0bc5be81f139ebf638
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:39 +0000

    label dare land middle burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
