72f7d608ec3aad540fb14a6661b1bc78bc8aff81
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:04 +0000

    pinch interest bridge direct this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
