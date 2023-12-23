a114abaa21aac4113aae7e339ea68486f0d766b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:29:11 +0000

    past centre basically stand advice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def multiply_numbers(x, y):
-        return x * y
+import math
+print(math.pi)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
