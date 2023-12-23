a083a40c7b602dc3509a1c52d4787c8a02b08f3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:23 +0000

    unable shallow its city sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
