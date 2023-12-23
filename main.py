09cfca3afb10221c8a53a6f0992b58616ca739fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:11 +0000

    comfortable bill comeup control town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
