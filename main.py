eac4c8db6e90ae4f6144e69d89dd4c901055abb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:30 +0000

    command raw float bit evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
