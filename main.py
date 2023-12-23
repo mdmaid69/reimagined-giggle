eea58a93c4dbe497c70d60d4070ec01689b37133
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:19 +0000

    nonsense project sort depend turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
