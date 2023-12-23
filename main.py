ba3590271218d2d65fc234e7773571fa00e51558
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:09 +0000

    please your weekend fortunate shame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
