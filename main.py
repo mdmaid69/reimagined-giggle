1f671626253e17e8ba3350ac5ba7b5c34d892c26
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:34:33 +0000

    welcome workout sound hard less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
