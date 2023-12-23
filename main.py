f09a0013c2a76231389f892e062fa4e8019a224f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:57:38 +0000

    home draw rare home common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
