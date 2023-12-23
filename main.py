b93594f00d1699f50b09bda7221cdc5e17103bd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:55:52 +0000

    release direct but term contain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
