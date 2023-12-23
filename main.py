e44ae33d6d98280dfd9b95216e35d5fa39b52ea5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:47 +0000

    frightened backwards shut post lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
