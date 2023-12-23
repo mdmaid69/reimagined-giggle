e4e33b5aefc0431ca18506e322fb49ed4445c7e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:21 +0000

    plenty couple uncomfortable concentration breakdown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
