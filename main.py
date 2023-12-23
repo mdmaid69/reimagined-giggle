299d09f5781f6ab6678e412512423ce38662caa3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:05 +0000

    minimum half rare father explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
