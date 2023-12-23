033c9c633d101e9b1a6a144decf5aa15c12ac6f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:44:13 +0000

    say contrast passage frightened extraordinary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
