a6d6706d36668fcc61159885d287cccb01974aa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:32 +0000

    book pity area flat rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def cube_number(x):
+        return x**3
+import array
+def get_array_length(array):
+        return len(array)
