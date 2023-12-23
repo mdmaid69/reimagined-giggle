92586cefa1106368f006d1a9dfdfee75261f90d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:29:16 +0000

    Jj dead comment modern temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_today_date():
+        return datetime.date.today()
