6f259f7a68d62abf00952296d522c54e12cbff11
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:19 +0000

    pull include used fresh computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
