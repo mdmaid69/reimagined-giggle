29e06fbd6f1739fb3ee454035cef64443866018b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:21 +0000

    ring left maintain holdup possible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
