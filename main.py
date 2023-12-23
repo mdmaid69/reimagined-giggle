c612694b16efe3dd4b434f77edb84e772d66d9ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:53 +0000

    find food demonstrate myself boat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
