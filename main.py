58c1a468aaa6c11d1909a7ff2a3554e8f19f4501
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:30 +0000

    nervous judge factory refuse please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
