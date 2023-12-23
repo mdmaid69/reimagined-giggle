28ec5ae89298a0d1c9401a299e8cdd3f960b7a66
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:53:08 +0000

    harm phone petrol accidental arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
