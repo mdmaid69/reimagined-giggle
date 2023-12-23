443f5947c44f8004381003c16a0713eecff95e24
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:58:21 +0000

    chase keepfrom surprised advantage anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
