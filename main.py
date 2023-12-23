463324ce9bd142f754269768798c4120099698ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:25 +0000

    really achievement attitude of play

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def count_elements(lst):
+        return len(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
