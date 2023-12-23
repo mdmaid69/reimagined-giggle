757dbc58fbe843d5d1d0350ed8907edee53df342
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:53 +0000

    unhappy where cheat scale reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
