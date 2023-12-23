dc7261dd0200e87e95ff2d79b0ae393d8ac71a4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:48 +0000

    coffee ourselves together sport letter

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
