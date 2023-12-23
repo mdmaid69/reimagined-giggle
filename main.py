727efe3a4e977a93458728ab7f46a8ecacbea8e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:21 +0000

    describe black fortunately few respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
