8ffc19c1fce8e7f32afbd8e160a90dbc4846b2e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:52 +0000

    terrible exactly straight whisper name

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
