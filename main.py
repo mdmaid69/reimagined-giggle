45bdadb840de75063d065029dc27d0fadff83c41
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:00 +0000

    neither near thorough limit previous

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
