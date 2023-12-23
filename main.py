11bfd52242a6c173436a62216fae82f2ccacfd26
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:02 +0000

    mark boat hard mind touch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
