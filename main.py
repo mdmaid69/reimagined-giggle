12321e09e34370e94f8121220b9ad5c1729abd15
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:24 +0000

    fork runaway Qq badly fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
