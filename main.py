0a16bff59b0857c01d57f172d96e1c57b04dedf2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:50 +0000

    arrange getoff fall for exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
