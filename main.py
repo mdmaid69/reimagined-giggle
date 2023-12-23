1c0cc6cc773d9fe1833e547db9478853c2d2431e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:19 +0000

    comeround weak exercise teach official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
