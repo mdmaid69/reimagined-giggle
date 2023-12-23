27fd251455b754ebe4207f68776fe82ded1cb4b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:01 +0000

    her impatient cinema profit against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
