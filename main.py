c7c230cc1d703310169e5c0feef873816e66f8d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:41 +0000

    dear your pleasure regular system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
