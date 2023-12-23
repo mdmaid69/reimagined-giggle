fca3b30465fd2f8ae42ab9b7ae49b64403c4595a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:55 +0000

    increase Hh mend soil ago

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
