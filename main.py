fdbde44a46b288e4a0814875365b559e06936775
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:55 +0000

    nerve Nn gentle he star

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_current_working_directory():
+        return os.getcwd()
