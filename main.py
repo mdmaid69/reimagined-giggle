42024dec559708d488d6647373bb9f1d0ecfd26e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:30 +0000

    workout account pleasant defend trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
