b5e34bf6cad6c5a53ebd77c73e8a9ee5e38906bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:32 +0000

    shall chief learn land hold

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
