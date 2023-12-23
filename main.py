5329751b76f384f12f6512ea10c80492a47a3ead
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:12 +0000

    combine affect slowly 573entertainment rub

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
