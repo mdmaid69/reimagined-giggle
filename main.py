88e0cd3ab88c0f356b4a764a191840c3a3d71c1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:36:20 +0000

    display stare hear aim noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
