c1ff72511e4a1b27cf1e3ffa8661f9b0ab3c2e25
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:00:54 +0000

    flavour profit little store wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_area(radius):
+        return 3.14 * radius * radius
