912e094a5111c4b8ba38cda4e5d46244770fa78e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:29 +0000

    fit anyone run zero leave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
