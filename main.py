19262d2957d3070b0150e7844f857638bfb9e343
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:54 +0000

    wave now lower steady recommend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_set(array):
+        return set(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
