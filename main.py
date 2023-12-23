209dd63fe4c3706714b649879ff099d2d634bf94
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:37:39 +0000

    copy bother its today attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
