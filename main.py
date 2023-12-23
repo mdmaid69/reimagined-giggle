a3d413696b5eea72627fb65fc448d26f10074cb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:56 +0000

    off getout getup discussion over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
