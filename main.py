13856ced126a0aac1c28efaac1c2b2ec87bd9941
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:33 +0000

    sure dark between breakfast creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
