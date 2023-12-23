8abdd52f000a9162de8c8905a98a67907eda5bbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:23:34 +0000

    something unkind after possession discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def square_number(x):
+        return x**2
