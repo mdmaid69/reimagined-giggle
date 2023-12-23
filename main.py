2f44a8f76aded0cbc0b8ab932133ed0ef72125e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:03 +0000

    active have wind stay centimetre

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
