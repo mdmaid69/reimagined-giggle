96039472ede4884bd03e326704aa8a5be44c698a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:47 +0000

    Dd colour burn Hh container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
