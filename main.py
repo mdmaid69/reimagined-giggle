926cbe2b2e9732b3ce7e173f7e7602ba82bc0b4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:49:21 +0000

    murder cover wear declare legal

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
