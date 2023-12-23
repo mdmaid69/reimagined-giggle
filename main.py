a0c975c0e664bef79ac957afcac91a2e10552581
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:31 +0000

    position such sky world last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
