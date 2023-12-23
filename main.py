e5c6224fd33d089cd577989041b4fb53ddab5208
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:44 +0000

    what broken shall hear treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
