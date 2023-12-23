7271dd796ecb182970daca742cc70df190867e5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:48 +0000

    turninto cross hour chance declare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
