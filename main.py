fd16733880bcfd10d34ea18b7f343d43806b0222
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:59:13 +0000

    whole afraid miss problem jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
