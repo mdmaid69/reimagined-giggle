cab79d08a5ba6f2ea4216e441588ec8af9b8aa86
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:33:49 +0000

    meaning opposite smooth little bet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
