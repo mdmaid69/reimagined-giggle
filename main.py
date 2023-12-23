3376651a5568838cb0fa8fc02a453de0421af9e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:53:46 +0000

    programme strike happen thorough Pp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
