6a476c27860dba85676c813ec7c648342c8bf2cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:03:18 +0000

    single lonely wash ago declare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_floor(x):
+        return math.floor(x)
