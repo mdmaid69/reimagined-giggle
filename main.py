1db5bc128df81ff3f93f1b8390e500e0d1584c11
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:37:09 +0000

    scrap rid best achieve old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def is_even(n):
+        return n % 2 == 0
