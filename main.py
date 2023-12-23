ab7089df24a0594452a41608e92361a6045bdc56
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:23:43 +0000

    nature dress complaint careful silent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
