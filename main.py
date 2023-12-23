cb675db061be556b95ea5dade2390b6e45aaf464
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:34:24 +0000

    content show chase artificial 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
