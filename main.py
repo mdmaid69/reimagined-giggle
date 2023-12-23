44979ae64d36b4fd075d7c1e639303dcb8b37b4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:07 +0000

    inside label police exploration all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_rectangle(l, w):
+        return l * w
