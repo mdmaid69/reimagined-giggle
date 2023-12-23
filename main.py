b70f9a754205f8b5d07918a8b33100d893d2348f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:52:57 +0000

    desperate very attempt ring disappointed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
