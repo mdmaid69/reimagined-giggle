96a363cf1a1bff020bafe02e0f2a7920a105f72d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:05 +0000

    question we breathe definite moment

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
