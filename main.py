d4f9103e64466b1219444d306319ba1a1791fecf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:06 +0000

    exploration about loan naturally bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
