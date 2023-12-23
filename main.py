31ea76e31a7c63331bd8be868d684dee96cb394b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:07 +0000

    purpose step complain acceptable base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
