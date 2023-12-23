8c273e3fd7868f02e4f18edcd35493305eb66413
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:35 +0000

    height hope thought song yours

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
