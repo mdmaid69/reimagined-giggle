190de61a92dbdba7913525d5cb4241b72483d69d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:29 +0000

    everybody special choose high accidental

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import time
-print(time.time())
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_sine(x):
+        return math.sin(x)
