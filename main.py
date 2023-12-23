cea9ae1f1ea9e8d0dc814099c50a9fb8e5376fb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:42 +0000

    custom model before control priority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
