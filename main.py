42912f6a7dc689d67b8bcaf1ef63edba051b8e34
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:45 +0000

    question creature unlucky wipe occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
