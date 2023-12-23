14338af686dd80adb2cea44aaa3cc08ea14a8f3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:17 +0000

    address goback(to) solve mass close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
