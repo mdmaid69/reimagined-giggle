2ba0e1d70551df8f1beb50713caad18dcc45a893
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:24 +0000

    culture spin raw along birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+print(time.time())
+import array
+def get_array_as_list(array):
+        return list(array)
