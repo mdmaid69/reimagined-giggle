733507d9ddde5256472a0650e11a602a4850831a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:47 +0000

    worse flavour couple common all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
