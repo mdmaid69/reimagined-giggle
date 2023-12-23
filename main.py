24c4faedf39178133a59eb45e5db1a0ce11fa6fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:52 +0000

    cheque Pp wing practice well

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def get_array_typecode(array):
+        return array.typecode
