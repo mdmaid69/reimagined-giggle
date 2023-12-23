9990cc5f560a8e26f79473b33db2882bbf5fb218
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:15 +0000

    middle thanks classroom sound successful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
