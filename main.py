ff38329e6cb004a0264d7a9e4a7bd3f10f00e863
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:25:40 +0000

    think however card check upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
