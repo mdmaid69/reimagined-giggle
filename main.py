1e3d60b3f708326c6442a41ba9d72e77efa1d7fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:52:51 +0000

    but keepaway discussion free second

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
