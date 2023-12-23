cbe79f4478db655b796c77a8b722ecffa97109e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:21 +0000

    spot behave successful problem branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
