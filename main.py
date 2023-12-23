1d5f090e7d07f085a0fec830a8451219fcfdc216
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:32 +0000

    result object sometimes know politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
