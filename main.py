3f67bc213e407d2ae0f2e78225e159be7a5a01b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:20 +0000

    satisfactory exchange terrible floor description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
