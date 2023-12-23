6f472863e85070aea6d96dad7cf55f9163499187
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:46 +0000

    standard squash nearly good speech

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
