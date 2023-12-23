5a42a44e184c1ba9e6549befaed04e52e8f7df88
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:43:16 +0000

    put reliable dress lose 571enough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
