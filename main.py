5e0db7bac028b3389e44b944afdfabb6f2a967b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:30 +0000

    clear meeting write recovery catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
