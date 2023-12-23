ac406171548e11fd40074e1b0e0d1b1f8a97881a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:51 +0000

    fond apart innocent niggaacademy persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
