10fd442cc7e0ac4bc68c48e933535d305fa049b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:44 +0000

    pen steer alone OKorokay surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
