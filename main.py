e443eeec9b0dee21d8bde2367a01f8458b7fb456
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:17 +0000

    difficult properly scared patient peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
