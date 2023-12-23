6e07b38793b93bddbf5204b50388ca2483a275b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:34 +0000

    water talkback unkind interrupt failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
