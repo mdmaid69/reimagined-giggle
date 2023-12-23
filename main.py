acc525605da3c309d6c8a1774cb61f52db9e07ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:33:12 +0000

    still somewhere closely twist callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
