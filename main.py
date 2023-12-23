f33a0ed0f5d13d6efff936cc1471048f3c492877
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:25 +0000

    also acceptable better myself murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
