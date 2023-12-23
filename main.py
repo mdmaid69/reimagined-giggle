75bf00f02a8e00dddb8076d6714bef0c2e14d715
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:46 +0000

    goback(to) introduce make week contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
