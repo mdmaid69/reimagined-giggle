50b45384b30045b2bedb3fa092e4332d7e9029e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:29 +0000

    temper large late interruption suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
