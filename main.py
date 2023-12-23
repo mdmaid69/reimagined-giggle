789895fd2e14c6a805724b2ef9fef7ea83919d6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:04 +0000

    pull reject embarrassed owe happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
