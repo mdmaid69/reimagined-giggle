3a66c82e0d0ba8b12fdde7a824c54f0777a795e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:39 +0000

    line heat public beyond active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
