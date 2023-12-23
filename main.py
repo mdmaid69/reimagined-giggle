fed70415ceb626aa80f9cc2159f5488dc7540b48
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:29 +0000

    runinto invitation emergency valuable answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
