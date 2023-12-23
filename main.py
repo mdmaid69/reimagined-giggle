b91dd83e36f862bac642d45bb96e4ff659622c30
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:35 +0000

    Yy fashion breakin laugh shade

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+i = 0
+while i < 5:
+        print(i)
+        i += 1
