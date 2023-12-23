def27b3c723117e68dd981f46f009014def6a501
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:50 +0000

    task bag grateful introduce vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
