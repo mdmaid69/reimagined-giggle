086188ba150e5a433180ce6eac94f3cae8f712b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:54 +0000

    provide terrible length professional callback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
