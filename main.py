fb8bd4a71122bd8efa48a6b494e0249ffc58d8ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:46 +0000

    lower building there nurse certainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
