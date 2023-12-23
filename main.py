0dc06aa42d80e038721d6c6d45c0fa2cfb9b732f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:57:02 +0000

    well dear congratulations brother only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
