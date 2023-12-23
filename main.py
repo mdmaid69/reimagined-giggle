170c0183f676759b204d7e534ff7b50ffacc6114
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:43 +0000

    show get arrange involve shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
