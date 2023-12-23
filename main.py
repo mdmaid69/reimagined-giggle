f1cdb753e8c7cfbebccc613958609aba3927ef33
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:40:29 +0000

    recommend operate address opinion eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
