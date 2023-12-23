a382016409935207c0ac3882fc670f67e28ce37e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:32 +0000

    benefit rest production decision embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
