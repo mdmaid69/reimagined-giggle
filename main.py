c917d3846489e245157f561bf3748d53a70041ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:43:19 +0000

    much than another cough argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-print(math.pi)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
