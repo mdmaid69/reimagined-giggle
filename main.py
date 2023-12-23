08de5c5d5b5e4292abbd1a7b39ca849f9c938fcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:28:21 +0000

    sign sale stamp involve yours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def subtract_numbers(x, y):
-        return x - y
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_length(array):
+        return len(array)
