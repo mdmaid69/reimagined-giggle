d90f3a576b904b7ca0d5aae9bd6707c23e788419
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:57:15 +0000

    lucky water winter may tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
