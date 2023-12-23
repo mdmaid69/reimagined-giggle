6b1a7c1e57dada0a5e9414e91eb70c8f7579eea1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:15 +0000

    or anxious compare fun only

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def add_numbers(a, b):
+        return a + b
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
