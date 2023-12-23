7bab059ca5b979e366b16ece0355dd7452d16328
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:59 +0000

    however cough share shall today

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
