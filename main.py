c98be95a545632cc0aa946a8938ee7e862df0c43
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:36 +0000

    curious equipment air train prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
