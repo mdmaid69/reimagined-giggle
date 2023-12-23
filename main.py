7fc8e421b2ea5d0fb1aee0733cb3963af889e124
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:45 +0000

    neither word excited poor long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
