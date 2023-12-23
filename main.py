dfdc78a4d11c54c9163d5d0d9411112a9c0651c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:33:38 +0000

    empty might poison this exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
