3a3da2c55f3e3696c681e13af9a82ae0b0643d30
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:39 +0000

    injury hold offer takeaway talk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
