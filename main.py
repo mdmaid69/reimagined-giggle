488ff0036f6276e7173b77d09d499119a1fbb708
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:04 +0000

    best uncomfortable niggaacademy acceptable confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
