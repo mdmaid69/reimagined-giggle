30e1d29dc8dd2bd8633f984c6930da817f820396
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:04 +0000

    press recognize question each century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
