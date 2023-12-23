96bb95b67ef6bd068fccff81d6d0970d4cc78185
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:55:27 +0000

    boring trade mail late bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
