91c615cb8800c63b8957a89da1a92b0c273f874f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:24 +0000

    sale completely process loose fold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
