77d7653a237e1da6b4c062f1dd7eff278bc7a905
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:31:59 +0000

    pity head problem danger yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_user_dict():
+        return collections.UserDict()
