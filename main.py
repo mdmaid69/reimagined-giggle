37fedc8a308938c6366529e3c635f20f45ee5996
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:08:17 +0000

    hot explain big round giveaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
