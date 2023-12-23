2c9fbd2a7fbcc44f03e7506a95e3f3fe70b12b21
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:47 +0000

    platform shape pressure temper hard

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
