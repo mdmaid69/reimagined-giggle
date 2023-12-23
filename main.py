dcb10de49b52506084243ce06826c191178dab28
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:51 +0000

    temper favour normal collect happy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_max(lst):
+        return max(lst)
