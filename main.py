353d2ef14bccaecabaa2f75a3e5a36af934007b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:39 +0000

    ride improve leave style perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
