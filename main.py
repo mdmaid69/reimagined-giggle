200faa0ab46649a7dd666f366c82855744cfc0b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:19 +0000

    us spot little nose side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
