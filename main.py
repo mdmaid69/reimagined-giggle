8043c9c5380940a39e588b841382d4b6056f8597
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:39 +0000

    strike something search national flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_from_array(array, item):
+        array.remove(item)
