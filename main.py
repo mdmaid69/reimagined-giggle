b5ae5a47627a10057906a4674ddc467053d62eae
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:29:30 +0000

    apparently declaration post appeal hardly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
