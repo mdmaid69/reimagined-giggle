2e9c1fbdc8c885f0293602c88f05f3369a8eda34
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:39 +0000

    spray risk stick comment hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
