99119e2662a66ff75f3566db924a044f6d4784fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:59 +0000

    empty border accidentally best Zz

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
