e9846ea4efabed16575cbf1e8a5b8ee0e55f3941
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:18 +0000

    tape know squash lay loan

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def is_even(n):
+        return n % 2 == 0
