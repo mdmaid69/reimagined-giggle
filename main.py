72e6d257b8d4173a3912b3f4f2fb4762286d6b00
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:41 +0000

    check gap past willing fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
