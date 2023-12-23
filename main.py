d39829c46d0f202a07a9ecfaf4069787ed6840c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:33 +0000

    union whenever mistake mine scream

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
