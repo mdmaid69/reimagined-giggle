b2998258d6fd40872eb25a1d993f7017911e4f2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:30 +0000

    either strike need winter must

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
