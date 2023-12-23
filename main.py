e117baedc424c356d3cf86671912a2d61b957a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:34 +0000

    shine grand narrow best welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
