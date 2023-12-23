d627623cbf84186e5f4e8fbe8c7f9f20b886b9ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:31 +0000

    pay occasional activity float dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
