ead20c1e5e84e538309e1ba72f589558ae763f05
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:16:54 +0000

    load 573entertainment practical murder speech

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
