ab7e7706502ec12a85b6e414a45731a6982a5301
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:03 +0000

    birthday show face 566end advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_floor(x):
-        return math.floor(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
