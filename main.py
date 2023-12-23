b8df38296f9e13049bba5ca7dc7eba4167b7e1e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:33 +0000

    bill fence so throughout basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
