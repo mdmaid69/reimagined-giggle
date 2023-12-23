8094db94c6af108b05900712b5e233021fa9e2fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:34 +0000

    hang voice our affect seem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+print([x**2 for x in range(10)])
