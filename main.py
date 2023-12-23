5ded2ecba2fa66327bfa4d3f262a025820ae00f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:17 +0000

    circumstance plenty tense room tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
