f44f913dba3ff4850df2b8d8379d3374edd4a8c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:20 +0000

    smell cry steep rubbish shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
