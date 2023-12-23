3eea745a350ecdede5c7bf8cfe6062ad1826bc74
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:27 +0000

    temper excuse illness attention price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
