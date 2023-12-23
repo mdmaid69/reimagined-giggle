b3ae4bfc8fa90dbb02476c826d675526182232c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:27:31 +0000

    switch house rain keep giveback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
+def sort_list(lst):
+        return sorted(lst)
