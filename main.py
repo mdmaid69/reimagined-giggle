8154c28b5ad6c2c267283bbe46ad7dd91fa85491
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:44 +0000

    how serve share wander safe

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
