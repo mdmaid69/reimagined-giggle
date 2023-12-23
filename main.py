351fe7b771dd3614aae79a51484813b9f54ba04b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:21 +0000

    unpleasant boring embarrassed cry open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
