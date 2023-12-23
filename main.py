54c727afcec9815c449d5088e4810469f3f2ca38
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:19 +0000

    strict simple Pp another wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
