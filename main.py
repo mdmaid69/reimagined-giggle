70f4355a74930bf77f4f8fd7e274e07517528c8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:34:12 +0000

    key man transparent instead environment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
