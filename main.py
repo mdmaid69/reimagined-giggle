1e5381dd727d23ae909b7ed2d50aa0eaacebaf3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:32 +0000

    undo before place pretty claim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
