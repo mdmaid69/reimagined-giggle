b7a88032706c145f4db5ed8e81866b9bd43b4f5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:58 +0000

    exaggerate judge classroom confuse radio

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
