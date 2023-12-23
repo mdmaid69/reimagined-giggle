d6f50fb68960b18a07426c0055d47112ecc4704d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:02:37 +0000

    challenge us previous word worry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
