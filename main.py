19d92224d0a828ff6425333785e811915a73106a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:17:01 +0000

    better please form stupid edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
