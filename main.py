412ae76e73bfff76ee631641c6c297328ccdd6e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:55:21 +0000

    maybe voice reply concerning relationship

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
