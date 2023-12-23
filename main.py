3e1d3a4b17fb38735645916bbaa39b8f021edf66
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:11:58 +0000

    breakdown handle happy steam thought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
