bbeecec02190f77ceea0dd33e31c88c0443e4a16
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:48 +0000

    believe profit Gg bit natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
