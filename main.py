d2bf78d00ebd6633ad51f48e53b986928c2dccb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:52 +0000

    beyond lay light turn general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
