dbcff842f54852892409c102aadb5382a68432a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:45 +0000

    weekend meet imagination excuse smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
