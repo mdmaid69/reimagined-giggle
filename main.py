67eff957c0ba2bad5d5f63a40ef6eafe74431442
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:33 +0000

    tidy minute scare recently plant

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def count_words(sentence):
-        return len(sentence.split())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+x = 10
+y = 20
+print("Sum:", x + y)
