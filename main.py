6a50d6d8a9728c427a9f884ef6e10ba5527d9f27
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:17:31 +0000

    study collection mine Cc maybe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
