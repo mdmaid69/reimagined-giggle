50ca4da8f674c3db7417e9e07d5c0151526d19ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:13 +0000

    bottom score off top opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
