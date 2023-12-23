7a9dd54ae969cabd8493e5c1fae30efae8a58489
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:55 +0000

    bath runout speech then return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
