f02639081cec317e6e8ecc96f6a401ee3e63b394
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:12:13 +0000

    ocean common rain design glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
