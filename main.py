7346a0e2703f09e4de2dad3ac709933ba5c5e87f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:57:54 +0000

    offence quickly mine surprise car

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
