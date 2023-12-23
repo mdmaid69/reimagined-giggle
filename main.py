4223b7f2a5ee5e6dbb84db92ce0999dc1247f10c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:21:36 +0000

    issue cruelty patience label sometimes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
