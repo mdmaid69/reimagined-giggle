747d9e7736da3819a685064ef24f20e1ea8285b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:33 +0000

    use blow such hurry no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def add_numbers(a, b):
+        return a + b
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
