7dded212ff7703eaddf8113119146eea4d8449ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:22 +0000

    ourselves weather long dark criticism

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
