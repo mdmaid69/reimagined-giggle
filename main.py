bc999495d6393315a09fa1ec859efcf3f9836fa8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:44 +0000

    shop thinkthrough port as tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
