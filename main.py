ddbf3ffb63dfbed52c127ee95f2bebb75edad780
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:54 +0000

    find command everybody worth shout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
