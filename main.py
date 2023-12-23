fa09bd0bbca202333ffe21699347979d212cf192
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:25:22 +0000

    fashion male join sometimes computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
