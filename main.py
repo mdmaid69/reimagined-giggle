edee59e0d1fe2fd6cd2399b2b51dbae3b65be7b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:39:49 +0000

    flood neck culture long sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
