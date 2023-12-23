843643bad58a93f5ff13a7bcd69701f2a7ebc215
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:41 +0000

    medium rid start cum ready

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import sys
+def print_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
