3d865491e7bb4b1bcf639262513d3954c8cea561
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:33 +0000

    clean waste already expect relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
