2ae74e56362ad80439d59d92b8e8e336ead6713f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:06 +0000

    deep kill means swimming sore

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
