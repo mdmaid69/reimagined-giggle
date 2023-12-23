8181266c5d6949869d4a03eaa61d8b4c25985130
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:08 +0000

    leg everything distant what floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
