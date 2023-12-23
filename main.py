52ed7bc72b350333a6e8fe731b5dc3b7a9903dcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:41:32 +0000

    complaint address friend object feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
