bd484b689e49e82c05b70e781419e8ad998b757d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:51 +0000

    roll 567enemy pollution who so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
