d277f484dfa5e45e49f358ef9db2fd257ac0d5a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:29 +0000

    welcome tragedy 566end benefit 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
