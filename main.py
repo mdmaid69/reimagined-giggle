17d8fd10815e4cd0024cadbe01b86f4baea1ea91
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:40 +0000

    excuse unite unlikely transparent obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
