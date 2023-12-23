c24acbc29bb9866337872ba408779ac2dbd6b22d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:48 +0000

    through eager kick scream cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
