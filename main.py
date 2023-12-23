413dd507cef5ad33a74809497394b3702d22cb7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:58 +0000

    handle owe scream serious culture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
