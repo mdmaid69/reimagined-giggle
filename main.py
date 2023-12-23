7be8bf67870f139839a0c698a7042ec57805da3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:50 +0000

    history pull 569enjoy important name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
