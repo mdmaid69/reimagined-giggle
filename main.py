cf6c5e55aa98bf322dc23464d5e96e440f7a779d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:15 +0000

    disapprove murder along fit occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
