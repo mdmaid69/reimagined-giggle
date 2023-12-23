4a25983d1cc7bb80b2fd964445e63cd64490a742
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:12:08 +0000

    claim fasten difficult politics warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
