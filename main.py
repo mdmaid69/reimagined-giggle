f8510514ae62b47b8e062a1587ea9de0a804472f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:44 +0000

    strike save rush modern hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
