6f14a5f06992f801694b49fba42e087d2b597152
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:34 +0000

    permission tonight valuable content meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
