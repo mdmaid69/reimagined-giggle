b19afb50080042d92c2e61b3abc6cbc421ee82d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:35:28 +0000

    various common cost minimum fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
