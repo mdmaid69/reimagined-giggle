89cf26f40cc34c81122db0fcaa5295714fa9ffa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:10:40 +0000

    stop believe today compete immediately

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
