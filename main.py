8f3b43c71a8d88c4e35a4478b832eb10bea22c49
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:30 +0000

    strength characteristic Mm modern busy

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
 def calculate_euclidean_norm(v):
         return math.hypot(*v)
