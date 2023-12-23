bdc8b1a2fed2c0afd70735ba004a08f56f70971e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:11 +0000

    modern area complete market pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
