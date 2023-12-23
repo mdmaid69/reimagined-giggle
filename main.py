fcdbf73346b318e207d897971c32f5a1dbd55ee3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:35 +0000

    swallow believe reduction slowly blind

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
