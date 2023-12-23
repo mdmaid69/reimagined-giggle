4bf1d9235b3eb6bc89f9139f3c5e32bb965a9517
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:03:54 +0000

    exercise rather embarrassed solve full

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def convert_to_binary(n):
+        return bin(n)
