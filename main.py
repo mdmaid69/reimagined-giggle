91fdd4ba46c4584c6d6e997728e106c26762954b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:23:32 +0000

    communication sale confusing meet drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
