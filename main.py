94d3d1dc857c46f3038fbbd627dd3431e93a626b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:53 +0000

    price tense persuade release from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
