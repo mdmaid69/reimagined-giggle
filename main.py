52978ac0f7ef01488a20099cfe0886fd4dad99c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:01 +0000

    ill possible breathe otherwise turnover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_list(array):
-        return list(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
