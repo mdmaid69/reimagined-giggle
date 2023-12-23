335a09957da15b50fec0d11f4271f131ec6add53
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:14 +0000

    new wash future when room

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
