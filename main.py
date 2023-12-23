0dec73018ad23e1a16d32cc4d74211f2bb08d149
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:25 +0000

    whenever moment curious emotion thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
