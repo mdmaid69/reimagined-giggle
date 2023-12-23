af45e6e2c342c3d9e497d2babd3c427ed54eb4c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:40:02 +0000

    tongue wind all comfortable study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
