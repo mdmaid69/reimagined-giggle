528ed42d6709af97d7d8649a3a9b546078ba44fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:04:51 +0000

    squeeze dream allow among by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
