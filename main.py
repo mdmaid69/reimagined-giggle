bdcc36a51055e8842e55284731b2aea040fbe75f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:33 +0000

    takeoff precise satisfactory person rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
