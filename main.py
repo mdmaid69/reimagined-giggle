27c038d73dd3792b6b86b3f845eb1309f2dada42
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:09:17 +0000

    bill nothing choice meanwhile him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
