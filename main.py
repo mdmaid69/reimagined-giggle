16e61ab1b37f24329affafef1ccb5f5d7557e0d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:48 +0000

    closely lean about frightened fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_bytes_from_array(array):
+        return array.tobytes()
