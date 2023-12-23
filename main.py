ea69a672c5d8458a47a5c7d8e2a505da239429e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:04 +0000

    and fault relief why come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
