5439f25483f11cf4f27190c1e7211b88cc348264
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:36 +0000

    further could opportunity record enthusiastic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
