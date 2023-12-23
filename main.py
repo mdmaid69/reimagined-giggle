0a3792bb8be6f91bd5435196adec3129998d25da
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:22 +0000

    rich stamp destroy crazy people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
