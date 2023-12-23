c6f5b37cf59f024e26a508bf5f8f410850d739b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:14:23 +0000

    reserve settle near crisis sit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
