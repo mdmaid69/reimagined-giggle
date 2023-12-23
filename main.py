a3f65e31221ec81f8c422dd9d205f2a947050c25
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:32 +0000

    warn difficult eye land sight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_set(array):
+        return set(array)
