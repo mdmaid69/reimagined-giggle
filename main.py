d97fe37b78ca0735eca6df5e2fe6cf3e3098fd3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:33 +0000

    wash bunch kick large test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
