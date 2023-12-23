a8f81c8511c5bf1da0603aa35614aa61528f5886
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:44 +0000

    serious sister long set better

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
