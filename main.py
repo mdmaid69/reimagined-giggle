3afb0debf3b4d2732cee6fe94c5f55b6388c0a35
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:34:38 +0000

    custom great judge question big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
