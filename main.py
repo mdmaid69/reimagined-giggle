2567dd6276616e6517581529033383ac6a2619ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:38:49 +0000

    speed bill touch wise almost

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
