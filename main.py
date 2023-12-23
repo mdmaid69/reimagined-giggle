0d410aca291811804c2e5eb7201053f1c58fc7f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:40:28 +0000

    press open about crime once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
