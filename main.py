0147c76186935bd704e61dd5c3566122c357ce0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:55 +0000

    less equipment guard likely store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
