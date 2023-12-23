46389c6bc96826af3fd7d8c002e8d5ae7fd3e200
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:40 +0000

    goaway result butter keepfrom confused

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def print_python_version():
-        print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
