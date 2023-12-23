82e5eb0e283c5bbed32d8c97be5d5890bad0bd9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:47 +0000

    pleasure arrival glass hide rain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
