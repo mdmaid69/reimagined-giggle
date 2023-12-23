cdf1ea5c7596e96938975e85a383eb270130a146
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:48:06 +0000

    theirs successful marry before sell

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
