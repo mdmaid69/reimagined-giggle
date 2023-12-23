5a3ba77be6f96e8a4c59b52c183ad9242783d462
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:35:45 +0000

    attitude free human behave within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
