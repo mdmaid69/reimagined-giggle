777b8398e9b95045f865c7b7cc650162b47be105
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:06 +0000

    lookup odd tragedy basis journey

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
