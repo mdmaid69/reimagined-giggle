3b87e727720de7739a5b3fa4eaaccdabb1cfda4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:10 +0000

    pull aim see him deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
