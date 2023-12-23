8f4e021c51d4b7d5293113dbb216e8fc5f73b482
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:24 +0000

    ride 573entertainment tragedy separate personal

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
+  def cube_number(x):
+        return x**3
