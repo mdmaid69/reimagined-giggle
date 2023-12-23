0a41844f71b73c64f0e797e75e61de9d01df0321
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:28 +0000

    middle telephone tongue free suck

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
