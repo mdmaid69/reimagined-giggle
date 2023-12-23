915d09b5ed84cd6438cc1c5039f254778831cd87
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:55:36 +0000

    fashion flow breakin custom over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_directory():
+        return os.getcwd()
