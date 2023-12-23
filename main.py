70dc41762424f708dc1ee17277eb9f00d6c2f5a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:24 +0000

    sound disturbing human book actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_error_function(x):
+        return math.erf(x)
