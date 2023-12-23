d35896a443933d20052219cea963173aa4afc4bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:36 +0000

    class confused callat start return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
