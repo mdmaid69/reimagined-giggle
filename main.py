1b695a6841182e786998bc610de940223f13300c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:21 +0000

    club rarely tape sad experiment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
