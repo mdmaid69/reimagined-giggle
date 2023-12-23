3140f7aa0c6782db46566bd07fb17e1077e42f6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:53 +0000

    happy last sticky dinner advice

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
