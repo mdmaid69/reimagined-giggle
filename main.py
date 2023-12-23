180fd1cfa7d147710d916f5c425192b4e70b34cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:45 +0000

    waste fly press most visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
