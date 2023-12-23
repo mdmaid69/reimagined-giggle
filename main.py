dc9adcafefaac4558bf04bfe586571ac3d7c795b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:01:41 +0000

    run pour present hear almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
 def get_array_as_bytearray(array):
         return bytearray(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
