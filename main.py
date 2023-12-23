abf4e5cc57cef0481a4a371d7851dfe95947a44d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:47 +0000

    mind no fine everywhere cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_even(n):
+        return n % 2 == 0
