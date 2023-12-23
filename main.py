2437c09d0c012248ad966406268874a985d8ff76
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:46 +0000

    Ww sink hers buy glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
