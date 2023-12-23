deb8e8673005aece15bd5b5799b8893019067a34
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:55 +0000

    violence takeoff job during good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+from collections import Counter
+print(Counter("hello world"))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
