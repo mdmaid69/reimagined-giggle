01f47ec08c4e756e6837e31405f0a784352f3676
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:13 +0000

    exit few letter nigga scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
