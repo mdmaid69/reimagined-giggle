f3b2a37229ff670713bb27b93202e5ec8e514c49
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:20:55 +0000

    related try likely front curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
