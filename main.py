7103b7935039bf428c1cd73339f81f6259a12c73
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:00 +0000

    dark birth excuse important cut

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
