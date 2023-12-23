0aee99f06eeeaaed689f1131a40c982d9db4863b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:14 +0000

    share compare mention crazy connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
