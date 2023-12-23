dc4cf8d60ec90c455a73881e3673f817dbcfd614
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:15 +0000

    clear attempt defend seat head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
