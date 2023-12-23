d8dd1fe13b21616e317da3530d06742a010c9c5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:07 +0000

    demonstrate doubt always degree form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
