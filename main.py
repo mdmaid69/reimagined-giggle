2fa78cb1b029e8f6965b1ffe88cdea2c922de538
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:50:59 +0000

    hot Cc under confident decision

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def convert_to_octal(n):
+        return oct(n)
