8fc322adf23deed69664a3dd7adec24b206d19a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:44 +0000

    snow comment child recover case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-print(math.pi)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
