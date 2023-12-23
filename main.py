d46c970239883123851010c589316a42b533d896
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:43 +0000

    practise push treat beginning suckme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def count_words(sentence):
+        return len(sentence.split())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
