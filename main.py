da8a3b957cbc2d82dfb09e11fd68d8e12375e830
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:55 +0000

    many file justice praise yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
