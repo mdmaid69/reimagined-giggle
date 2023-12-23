ce779904e9d62a04cacb2af4823bd5d7042f55c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:38 +0000

    unit underneath over light need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
