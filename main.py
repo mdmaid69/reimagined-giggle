f9d52842464b10e6d29bdb4a629e0e76a0f940ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:00:19 +0000

    worry holdup responsibility change turnout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+for i in range(5):
+        print(i)
