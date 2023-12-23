9727c3ae9c7380bb200b0c1c236c9991071c8b9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:55 +0000

    avoid agree actual accordingto unite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
