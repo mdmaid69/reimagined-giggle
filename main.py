82ad2fcccc4355f65f1a4418a42de5cb6c3f1dca
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:50 +0000

    closed contrast attract nearly next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
