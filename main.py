15c60526af7e55dfd797ec7cce8cf7c543854431
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:24 +0000

    dare worry right post cut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
