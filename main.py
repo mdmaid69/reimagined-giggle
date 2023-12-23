c91f2399588f980f0266c3508c27ac041f18f8f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:02 +0000

    roof eat fall large copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def convert_to_json(data):
+        return json.dumps(data)
