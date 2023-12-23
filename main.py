48501c1c7d7b7a73a537a22efebc84401e3ad2fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:15 +0000

    mad store school programme further

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
