171e67a77d43a71e6c21969a2de286399b32bfdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:37 +0000

    thinkthrough sound anyone criticize wind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
