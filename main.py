1b05387a639ee169d8bac46cb3dbdb906891b765
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:00 +0000

    cheap difference price behind check

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        return sys.version
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
