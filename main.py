2e145c4f6d90baf83ac4def8797fab43a78191a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:15 +0000

    to push flat star error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_acceleration(speed, time):
+        return speed / time
