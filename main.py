302c012c8929d51bd4d0f8415adec4b5e4b495dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:29 +0000

    callat dear judge advanced reach

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
