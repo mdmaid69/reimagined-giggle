3e859057e638e484f9b16ad74158e9de226aae04
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:38 +0000

    neat bin muscle wander compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def square_number(x):
-        return x**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def print_python_version():
+        print(sys.version)
