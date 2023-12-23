a5741a4eab2986fc5bcdfacc8d77fb7781992c3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:43:16 +0000

    try central hard whichever crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
