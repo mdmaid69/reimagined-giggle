4f9512abab9c13fc6d495f13c0cf0bb6f8e1c073
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:54 +0000

    warn afford keepaway limit reason

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def sort_list(lst):
+        return sorted(lst)
