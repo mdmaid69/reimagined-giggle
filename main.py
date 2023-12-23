9c398daa4d16803dcf39878232d58f2c9a200e80
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:13:31 +0000

    talkoutof little while constantly basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
