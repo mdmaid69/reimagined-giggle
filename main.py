0587fe23c5d38d80b1474c604fd49934349cf071
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:18 +0000

    adjective somebody show drop key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
