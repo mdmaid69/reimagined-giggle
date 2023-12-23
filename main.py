cf36e4a10012a73f9e22a9e07261a339b2900ebc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:58 +0000

    impression oil vote emergency fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
