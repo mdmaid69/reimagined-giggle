06562db870b908363cee8d7ac53d164f0b140c57
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:07 +0000

    mark wish reaction confusing long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
