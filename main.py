53e06ba44489d60393981ec00b2559c21f81ec65
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:29 +0000

    steer turndown powerful pronounce less

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
