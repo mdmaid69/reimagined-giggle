28a20bda71f96fe6854637958cb7f9b27eec4030
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:50:36 +0000

    himself there lookup possibly welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
+import sys
+def exit_program():
+        sys.exit()
