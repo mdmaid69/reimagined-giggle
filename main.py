01e6c61ec30ce622327247e85fae913a53dbfef1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:57:14 +0000

    meal pump herself tip almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
