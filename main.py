9046d0af80c3c9c58a458c3e740495b8d8012eec
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:46 +0000

    similar past reply tread best

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
