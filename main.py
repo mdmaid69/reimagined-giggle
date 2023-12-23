46a47823c9ed62e22aba96e860c858df7a34d4e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:32 +0000

    breathe spare search search section

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
