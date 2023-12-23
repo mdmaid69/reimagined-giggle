d8b2d83201604b79e94c82d061732c5c4548a4e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:07 +0000

    niggaacademy bit design talkinto getaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
