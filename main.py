f7c05779ec0341026f2cb27f26015a541f9c4b5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:55 +0000

    exercise past laugh line inform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def is_odd(n):
+        return n % 2 != 0
+text = "Hello, world!"
+print("Uppercase:", text.upper())
