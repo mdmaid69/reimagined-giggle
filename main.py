a446acc55d35480d062d565c5247dbc11f6adc16
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:56 +0000

    custom chief above Ii care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
