78441cdd4a075b7a0c41ca3408254cd7e6474fb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:31 +0000

    fact post choice disturb equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
