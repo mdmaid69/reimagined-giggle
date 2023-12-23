058db62bf964257009476b7b7fc45dbbdf365d3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:33 +0000

    boat honest foreign certainly public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_power(work, time):
+        return work / time
