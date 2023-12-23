355e18d22742591f910b971154136203fb727e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:45 +0000

    walk squash disgusting soak national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_min(lst):
-        return min(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
