1457245a5a274be14c9c948b0f43967518001485
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:08 +0000

    willing bite surprise spill bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
