8f17eddea3aed6fb127d9d74816b67d1acf4ee35
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:08 +0000

    burst list impossible emphasis hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def reverse_list(lst):
+        return lst[::-1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
