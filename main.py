ecb173084148de72ac45d221b08b9834e571f60d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:37 +0000

    moment age offer fine sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
