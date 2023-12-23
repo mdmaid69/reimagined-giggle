20db7e840a5c4cf13f9a078c3b9334a01f085b9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:54 +0000

    big answer situation afternoon hope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_volume(length, width, height):
+        return length * width * height
