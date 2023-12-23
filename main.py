79c312da5c834a342d1ac6fca3f48449280af272
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:19:31 +0000

    female identify choice total Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
