ecd32641da1e5d2cccf4b625d317a50f4bb57e8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:09 +0000

    majority score branch smell thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
