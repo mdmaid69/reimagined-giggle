f6075b27daa2205c8e59b4763f839a9979491dec
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:15 +0000

    summer gentle home copy forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
