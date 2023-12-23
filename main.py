554fb52865f1ce66c1f584bc269f3722e293f0b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:35:58 +0000

    keepoff once admire can forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
