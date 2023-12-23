c42982a7437b36c50bbe92be8fd78a079b11974c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:40:07 +0000

    warn dress confident keepfrom medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-print(random.randint(0, 100))
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
