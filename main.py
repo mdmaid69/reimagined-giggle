d25da635f11efccd140e4e97d0ecfb5f6f006e21
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:48 +0000

    classroom detail goup building traffic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
