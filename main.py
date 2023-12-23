c147d42d2034a426e90f4e34f3dd98a8a7517641
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:12 +0000

    takeback play sweep need glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
