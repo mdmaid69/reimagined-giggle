eee54d4ef9a429fbd84626a38b050226bdb109ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:34 +0000

    standard people mind holiday tip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
