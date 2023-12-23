3301aff5b23c92e908225c50bfdf1f1ca5bca975
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:23 +0000

    will book remember clear proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
