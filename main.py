f3b3a198ffeb8e1b540c1b6cdbb584b408709eb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:32 +0000

    description soft proper brush easy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-print("Hello, world!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
