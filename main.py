4659c60875e2c360651fbe6d3d3d64cfd5ff3dfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:27:21 +0000

    visit ilovedicks many exist tone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
