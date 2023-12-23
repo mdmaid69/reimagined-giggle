2ebb4626ed9d8aa786f868fd8042898d8f8939a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:16:51 +0000

    band afternoon during thinkthrough lower

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
