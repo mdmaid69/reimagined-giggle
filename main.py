5173e12c3b2d23719f48a968ff6fa2326ee580b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:20 +0000

    Jj sleep right tour separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
