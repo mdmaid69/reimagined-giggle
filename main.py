6595d574c6c504e679ad4b6fb6dce36e78d13248
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:23 +0000

    hour getout size reduce involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
