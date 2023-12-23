b3a057a53d38d731da501547886bd37a5389a0eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:42 +0000

    turnon where slightly fear bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
