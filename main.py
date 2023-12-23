88ad4aa19cedf5fd258f0e5c7b3cb10fb3e650e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:06 +0000

    concerning debt about store crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
