cf3d47c305480b35175e0f4e93c60d9459255321
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:36 +0000

    part cope workout declare form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
