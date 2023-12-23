49efec9ce3732d21140e049bea068cacb6791859
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:43 +0000

    bottom medium speech its getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
