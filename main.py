9696aa0cc8665d21db2e5a29db6e12b20b3d14ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:26 +0000

    callat shoot cry Ss whether

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
