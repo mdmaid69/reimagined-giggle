09e32600721da5de950e08a1bbfaa6d7ebc0810c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:09 +0000

    cold design breakfast aware clothes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
