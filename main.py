d660f1168979a50dfa95c1db5ee027e30f8ce4df
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:59 +0000

    real greeting Ww completely lay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
