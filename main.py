ac9f3458981ef994b2c0a1a2ed86d6603f33a39b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:32 +0000

    allow hurt bringback bang injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
