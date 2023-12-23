e61fe3e11bf2fe040ac9651cd5b54a83578b8625
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:34 +0000

    cry present breakinto exaggerate load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
