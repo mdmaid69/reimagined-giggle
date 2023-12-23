aea48059496783f96f523a63e63c311540d0e520
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:55 +0000

    thought eye lonely official rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
