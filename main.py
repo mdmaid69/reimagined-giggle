9909a26c6770737f212d0479f43b56b5448b38d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:47 +0000

    blame goout damage question entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
