d31f28b4e673ee244da9bb4ccd1b4480b0eee5cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:48 +0000

    live use blame crisis believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
