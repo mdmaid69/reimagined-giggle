6281b01518b9665e3227b1102c7bdb59f45dc7c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:29:22 +0000

    thank recent mind feed likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
