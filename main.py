a1bd721a3277951505b2fd09fac08152263316cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:08:40 +0000

    load human content tour shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import getpass
+def get_username():
+        return getpass.getuser()
