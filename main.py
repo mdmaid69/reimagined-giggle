6132faaf6a17e585c573e0b0e257ec2825eeccec
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:16 +0000

    blame judge communication talk bother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import time
+print(time.time())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
