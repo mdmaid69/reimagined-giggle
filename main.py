3800389d44e7ba8bf18f879b84c512b1fa27eab5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:58 +0000

    occur failure above giveback legal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
