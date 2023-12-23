86099145c24b29f8a6587c96fd05a8d72140315b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:53 +0000

    turn anywhere tooth stretch idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
