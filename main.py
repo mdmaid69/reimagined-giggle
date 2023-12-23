c2486e5e3db13d57afc369255fa3691e79f066d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:23:01 +0000

    independent Gg determined dark pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
