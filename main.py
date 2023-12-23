5dca4c460577cf7d4af33d9bb24c5a26c14e2c6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:42 +0000

    broken stage plan runinto official

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
