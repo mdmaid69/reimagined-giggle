a056d371e6003fca1ac5d607041454796e42b0c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:42 +0000

    whisper boil busy you sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
