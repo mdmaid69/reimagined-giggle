13d1ab4f30b82d8051addd2ac4f85556dd4b2fa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:06 +0000

    brown exaggerate plate which study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
