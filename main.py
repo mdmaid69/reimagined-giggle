62633047dc2865b81d1cd1c4dd353f98bb814e55
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:36 +0000

    turnover word miss chat charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
