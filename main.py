7dc3a0c7ce3abddcc2b4e3081c2be3c2ff061fda
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:16 +0000

    turninto storm recognize health mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
