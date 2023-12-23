77f464d6291396f02e34e42be88a867efeb4a1ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:04 +0000

    snow ring example absolutely Ss

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_sine(x):
+        return math.sin(x)
