6530db1f10d1a23af07a916d5762636916f377c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:11:51 +0000

    ilovedicks practise join report wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
