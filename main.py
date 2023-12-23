6f9b676106e71a0e5facdcd99fe357053f3f4f4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:11 +0000

    dick you press invitation error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
