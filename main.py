91e18ffe39e78ce05075830e579caa7206e390bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:48 +0000

    test unite care help test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
