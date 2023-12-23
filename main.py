fad8430e6d0d64c3460868b6883c71aeb6c1d724
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:05 +0000

    offend accommodation turninto relax now

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
