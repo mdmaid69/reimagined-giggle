e3936179653a47288bc7b04134302fe5695b252f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:40:22 +0000

    male arrive fat juice square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
