3962d56ca79ee7ca219f23e18015d1a942aec86f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:48 +0000

    winter so instrument system woman

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
