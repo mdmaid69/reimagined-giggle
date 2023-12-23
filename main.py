942103e5fc12a1eac56e2465bc379c8955991e47
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:11 +0000

    cost easy else start holiday

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_power(work, time):
-        return work / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_area_rectangle(l, w):
+        return l * w
