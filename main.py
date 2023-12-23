eddfd80939a4c285c1537d603729968682eb2167
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:35 +0000

    your split swimming behind adjective

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
