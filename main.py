394ccb2cd6cb27ca93ebcae9d57b520bf9e41f45
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:13:42 +0000

    round lay possibility past prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
