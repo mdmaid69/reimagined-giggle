6d642e443c1a7200c0c619f0f5729df21818322f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:34 +0000

    seat immediately boat turn tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
