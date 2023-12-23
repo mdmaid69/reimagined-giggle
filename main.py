132e9dd155a168842423bd114f87226456fd681c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:20 +0000

    single survive extra feature no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
