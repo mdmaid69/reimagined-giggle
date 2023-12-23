89554de6d97c5a7d3dcb5d830aa5b1409baca047
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:40 +0000

    style bush beautiful chief sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
