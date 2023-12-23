fbf9b1fb3ed317e31a40d5642bdd1a493e835ddf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:38:46 +0000

    lower 179bite plain thing slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
