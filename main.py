771057a2bc46a4fe6d5fa987bcdab2e731168962
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:49 +0000

    official though promise no comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
