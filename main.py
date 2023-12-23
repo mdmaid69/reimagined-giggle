c8c8bfe065c27e0acb27a2edc63954c45c612f80
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:43 +0000

    wish tooth previous far satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
