6051817ecf75b2cae677984ba50ee6b0c9c7543f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:32 +0000

    narrow welcome cruel fast wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def reverse_array(array):
+        array.reverse()
+def find_unique_words(sentence):
+        return set(sentence.split())
