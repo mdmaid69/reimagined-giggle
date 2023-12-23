b7ab0bb1938b3e834c6fd020ee25ac393a899b89
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:27 +0000

    flavour push lie mood word

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
