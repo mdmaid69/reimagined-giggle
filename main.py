20bf844207c140fe2ff524dc046060e5e917fa70
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:31 +0000

    reply long belong friend understand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
