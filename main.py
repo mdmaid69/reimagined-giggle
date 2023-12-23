8e6cb54d4287609675fbf26dfa28a7c5c96241aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:10 +0000

    makeup date regional keep head

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
