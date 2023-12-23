a17a972e6f0261a4b69bc604ef2ff76123bc1ba0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:10 +0000

    flow steer claim tour equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
