be0f0505806cdb6dd300df6bab6be9ddfb2b5474
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:03 +0000

    beyond harm perhaps bottom draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
