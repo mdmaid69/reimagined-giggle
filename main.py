0c48ed5e07389a98f065fe0f090bdcbe5d0253f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:09 +0000

    flavour normal motion art clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
