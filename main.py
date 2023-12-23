e3249dfc58ffcbf90dec566112eb3ba389b0ffa3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:47:23 +0000

    machine gooff clear fast question

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def reverse_list(lst):
+        return lst[::-1]
