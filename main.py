8391d504f040cce251c0876ffbecf28e38fdb191
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:06 +0000

    meet tidy whether inform bus

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_hex(n):
-        return hex(n)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
