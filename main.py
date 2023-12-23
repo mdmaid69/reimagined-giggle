3410bf1a3875cac062576a98f479d5b94a9ff0ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:37 +0000

    estimate unusual circumstance tour cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
