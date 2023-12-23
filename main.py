abf2c4621828bc7746c2320b6fb3df2ef7923220
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:29 +0000

    winter bunch close keepout surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def find_max(lst):
+        return max(lst)
+  def is_odd(n):
+        return n % 2 != 0
