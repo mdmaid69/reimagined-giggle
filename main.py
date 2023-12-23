c6eaa6096756d7c232f877bb7803d59c0fb0e49c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:13 +0000

    mine pretend wish division same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
