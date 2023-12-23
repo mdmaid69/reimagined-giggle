2a2ce0aae754682362f9d8d483573071f4eae3c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:07 +0000

    exhibition free control radio peaceful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def get_array_as_set(array):
+        return set(array)
