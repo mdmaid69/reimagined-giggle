d9ee005ed6beb819c659f2f51ff529e7aa9ee563
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:41 +0000

    fast bit run set official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def clear_array(array):
+        array *= 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
