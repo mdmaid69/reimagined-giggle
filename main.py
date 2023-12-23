fd86e5d8d6f1171038a3f8c75ac1aa29ea273fc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:36 +0000

    convenient sting from ordinary about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
