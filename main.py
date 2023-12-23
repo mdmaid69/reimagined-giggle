89a4d7b56225faf1c23c56946d2679971d7bdd23
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:18 +0000

    broad disgusting match false narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
