823989ef010b5193ea8b55956ceb5939216b0251
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:46 +0000

    passenger determined second please fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
