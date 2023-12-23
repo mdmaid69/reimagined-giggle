b5c15bd8eb56234b9cf0e002997526a2553a89a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:09 +0000

    increase treatment pronunciation lost hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
