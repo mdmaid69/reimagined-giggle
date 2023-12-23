6fe4ade445a2e08ce3fb07d2289a142ff3087c1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:23:08 +0000

    mother broken from getout suggest

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def convert_to_binary(n):
+        return bin(n)
