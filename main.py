6662006703fb63a1571596ac0f8c32cff6520222
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:46 +0000

    mess wet my could stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
