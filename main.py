8aa5d51405fa96d8b95a3b363c4a707439f8209c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:40:13 +0000

    event course band friendly for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_floor(x):
+        return math.floor(x)
