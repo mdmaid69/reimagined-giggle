1588ef38c839f96f514de95eef186e1e04a13496
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:35 +0000

    moral opposite nerve together basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
