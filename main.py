df13ca1e4a0feb39b5df6473bc84d564ac4a779c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:05 +0000

    throughout whenever Dd afternoon off

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
