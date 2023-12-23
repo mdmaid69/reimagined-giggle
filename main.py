5cbd16229118ef38e335e032cffc7101733b90c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:34:53 +0000

    than contrast innocent month imagine

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
