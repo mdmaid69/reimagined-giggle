d6c0c92599bd94823b62ae1d1af3bf25fae3380b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:45 +0000

    brown enthusiastic neither as tax

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_square_root(x):
+        return math.sqrt(x)
