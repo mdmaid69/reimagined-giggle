66d0eda2b8dedffe2f2fc5fb397fe5f9d4833761
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:29 +0000

    accommodation stamp opposite reject mysterious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
