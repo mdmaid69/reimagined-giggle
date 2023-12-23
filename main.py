3bfc5e4d7628719b5de95d3a370de48d6559b4d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:43 +0000

    spring wherever exciting right value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
