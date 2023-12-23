2a3010ca1460187000b9df635ee28d797520bf37
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:24:28 +0000

    later past comfort under confidence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
