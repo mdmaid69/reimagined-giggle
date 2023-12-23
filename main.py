06e808167fe1633f8aa70473452581f7255a5ac8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:33:07 +0000

    prize violence just succeed consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
