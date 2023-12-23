4c7b62d6d6cea1cd544afc2e8b11ea5b6372b82a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:38 +0000

    terrible advice trust band trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
