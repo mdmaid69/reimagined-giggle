91d84b6be93c46d9031d43e6cceca4147c870573
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:09 +0000

    case belief trick hospital pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
