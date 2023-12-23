c2b74da766e4f6c421a0cb62988676d3a8fe5b90
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:17:05 +0000

    somebody false Nn grade dance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
