05fc1cdf03aeb20f6d39b9f558f646d445a4ff16
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:11:04 +0000

    off morning turn improvement beat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_rectangle(l, w):
+        return l * w
