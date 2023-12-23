32076e9451e03550b55f02365ae91c831f640b8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:05:39 +0000

    brief ocean determined air arm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-for i in range(5):
-        print(i)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
