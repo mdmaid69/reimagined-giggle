47fe8413c5d741339eb8815b994944ab1055c939
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:55 +0000

    church blow decision same attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
