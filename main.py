b3c61873a8259e51323a329c4083eb1333702a2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:11 +0000

    mystery fall one push help

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+print(re.match("h.*o", "hello world"))
