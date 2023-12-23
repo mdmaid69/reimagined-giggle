5ed71d565e2c7df9b7d88dceb6f53ae88b26c1fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:24:32 +0000

    news exactly upset desperate turnup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
