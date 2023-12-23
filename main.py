93a6aa69daab4c49318774c9c1154c77a097276b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:56 +0000

    shake direct loud club method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
