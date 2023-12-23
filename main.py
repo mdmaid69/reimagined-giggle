b939285a5125bc75c679571b2a7f5323fada579e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:02:37 +0000

    impression lucky sense turninto extremely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
