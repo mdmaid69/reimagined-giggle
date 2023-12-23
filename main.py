278d87fb2b4586eb7bf4522a35ad835bf3ed6e69
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:59 +0000

    private border interesting level remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_error_function(x):
+        return math.erf(x)
