87a09034f465c71c92a8139be01188faa6f59232
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:03:37 +0000

    sometimes bill rob sense any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
