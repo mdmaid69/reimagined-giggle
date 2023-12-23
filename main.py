e2110dc7a90cfbb0fe9727f8dabcb1b3bcea2083
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:34:37 +0000

    below university talk comefrom tough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
