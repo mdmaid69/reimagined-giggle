4f1ce443e9816d03458028edbbc84785bd175d15
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:41:29 +0000

    operate holdup tomorrow rain step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def get_array_item_count(array, item):
+        return array.count(item)
+import numpy as np
+print(np.array([1, 2, 3]))
