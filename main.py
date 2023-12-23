83871140f8777a3f13036cbd248541d3101344f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:47:09 +0000

    drive compare immediate replace no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
