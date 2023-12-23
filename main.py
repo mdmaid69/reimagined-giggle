387c6bed8e48d48d0ed26804c6517834b3b37975
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:42 +0000

    dry accept beautiful breakthrough mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
