1a3b71e8c9ef81728e37f316b9cc5299676ab77f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:50 +0000

    beneath scare zero meaning press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
