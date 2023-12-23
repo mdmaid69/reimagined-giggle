fd670bc785805b7ace37358002b7d96f62283f58
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:55 +0000

    correct shallow field noise error

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_hex(n):
+        return hex(n)
