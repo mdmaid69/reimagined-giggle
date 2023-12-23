097ef612b382534d4b6d3f0f1a314fccf71cd678
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:12 +0000

    complete backwards their terrible worried

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
