1c33dbad95b2bebf60943b6e5d234427e8333551
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:20 +0000

    pollution deep 564encourage back snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
