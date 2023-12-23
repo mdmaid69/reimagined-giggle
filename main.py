f77a484aaeee20dd503beba37365f7f8e109ad46
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:04 +0000

    begin cheek blame region active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
