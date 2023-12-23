f8de939c048e8765b3aac77b3becb5a40ba4e086
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:10 +0000

    preparation kind below waste better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
