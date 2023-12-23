51e57aaa9ed9614fbb975e5923c02e404678f397
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:32 +0000

    develop rule offence unable exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
