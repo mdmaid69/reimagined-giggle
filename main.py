ea920c63c4434f340260171cb4468fd994e57037
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:34 +0000

    surprised rank scare quantity risk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_min(numbers):
-        return min(numbers)
+def reverse_array(array):
+        array.reverse()
+for i in range(10): print(i)
