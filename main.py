87ab76324e662c08e922157944c74065d94fbde4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:29:59 +0000

    between marry personal bury worst

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_min(numbers):
+        return min(numbers)
