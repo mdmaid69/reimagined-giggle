f66493bba669ab2e182d75ffb06ed76a5db9955d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:19 +0000

    throughout read completely official set

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
