b830224e71b31086e29e7d2c433a859ab6e673dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:35:55 +0000

    yet plant environment 564encourage load

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
