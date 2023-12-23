c1322243000208b6ecd9992bf88b7cc77fb18162
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:03:45 +0000

    programme wave pass eager operation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import collections
+def create_user_list():
+        return collections.UserList()
