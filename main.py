4d5a02a37f1c3a752a8ca6c7904c56b8965f6fe3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:04 +0000

    hurt goback(to) majority say live

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
