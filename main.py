dfd5f4ae2842481777fa661dff3d3f450c65a267
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:44 +0000

    complaint train operation circumstance study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import random
+def roll_die():
+        return random.randint(1, 6)
