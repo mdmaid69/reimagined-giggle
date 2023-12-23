3dcdcc8cbee79bc1fd4f3e7fa73f199266d8bfbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:08 +0000

    pour keepup warning music better

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
