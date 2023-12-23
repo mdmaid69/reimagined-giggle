112ff74b91c1ce3b8867503da281eccc08dc3751
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:25 +0000

    calm different story equal why

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+print("Hello, world!")
