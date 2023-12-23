531aa87654f5c21f47a33113200ee7a075f61584
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:58:58 +0000

    people judge natural sink ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_stack():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
