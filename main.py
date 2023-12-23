14ace94289b386157cbd3963dad764bf422bcb52
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:09:16 +0000

    worry high generally shoot arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def cube_number(x):
-        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
