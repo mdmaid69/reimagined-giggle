5132bc3bfffc8a3e693e9aeefaa8fa638765298c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:26 +0000

    thumb offer move sweep uncertain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
