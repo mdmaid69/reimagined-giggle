5335ea0371b9e663ec433092043695ccb9ca3d23
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:37:19 +0000

    no vision some look see

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
