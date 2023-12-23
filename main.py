41be2a57d18e595306b049d72178d70c935a2eb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:48 +0000

    dangerous report even tread idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_min(numbers):
+        return min(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
