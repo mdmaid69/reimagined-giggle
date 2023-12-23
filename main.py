706085cdc6538dc11331f24eb2de6b3a230a87a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:46 +0000

    heavy approve closed bridge occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def convert_to_octal(n):
+        return oct(n)
