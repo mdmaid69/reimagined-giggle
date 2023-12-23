987d36f177cc9c460b8961545ce6a980130485fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:19 +0000

    thick progress thinkover comeoff safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def sort_list(lst):
+        return sorted(lst)
