522eac6b75bf84226f9da1e62f63748187a33497
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:07 +0000

    card development pretty no land

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_max(numbers):
+        return max(numbers)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
