e8bddd1ac8cfe1bdad4bca5aeb73935ffff1ea0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:46:03 +0000

    learn trouble goaway once complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
