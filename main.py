fa0a10d6764ef6f1e414ebc667729a619a3f69fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:05 +0000

    responsibility lucky around eat true

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import time
-def get_current_time():
-        return time.time()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
