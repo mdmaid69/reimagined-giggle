761ab98f4100dce181148a7585bc75ed4369096e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:29 +0000

    strike fortunately look wish safety

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
