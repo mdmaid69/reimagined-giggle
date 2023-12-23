d1b8e0128cd48c090ade9648d234502299b9d591
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:38 +0000

    steal consider makeinto ticket action

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from collections import Counter
-print(Counter("hello world"))
+for i in range(10): print(i)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
