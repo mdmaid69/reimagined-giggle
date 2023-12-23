c32275d21b782f27de17448c72fbdc5a54d31753
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:09 +0000

    nonsense attention reasonable dish bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
