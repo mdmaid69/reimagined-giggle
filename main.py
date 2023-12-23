dd44e6888e61d50259c07ae5657a81644cfce10e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:10 +0000

    error result runinto major nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
