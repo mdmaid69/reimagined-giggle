8a457a07d065b6965c680eaa35c4d24e1656f8b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:36 +0000

    pen quality him easy except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
