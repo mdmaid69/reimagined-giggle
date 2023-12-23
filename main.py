bfba377117fff8e35f30562a8854c526a512bd84
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:23 +0000

    visit ticket worse high shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
