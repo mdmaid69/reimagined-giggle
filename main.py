d9b56d5b6cec10fdbb7c85fbe02254d7a5473c30
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:25 +0000

    couple measure introduce medium confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
