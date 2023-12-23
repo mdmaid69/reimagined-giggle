f32f4d3302e7a559b7d3ec740b9455bcb7ec8e87
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:33 +0000

    tidy autumn continue branch breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
