91f255628d4a475b7af83ebe11952d590f4cde95
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:57 +0000

    vanish destroy shop lucky insist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def convert_to_binary(n):
+        return bin(n)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
