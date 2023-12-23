38fb55de2889e3595a2ef44c479ae214527301d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:39 +0000

    term suggestion imagine wake might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
