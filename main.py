b45fad31a11b61456283619f046f74cf8c41e72f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:30 +0000

    doctor frighten behind aim store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
