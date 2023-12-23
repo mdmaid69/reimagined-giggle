71699cc60bdac8484956638aa4fd0a9fa0fa4545
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:19 +0000

    discussion single chat him mood

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+  def convert_to_hex(n):
+        return hex(n)
+def sort_list(lst):
+        return sorted(lst)
