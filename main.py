11e279b0773214d1a319cc59ee25e86ac5811043
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:41:53 +0000

    cut foreign win man bend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
