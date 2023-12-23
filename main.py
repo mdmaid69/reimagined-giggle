9acd012ce041373a28aef2c4d4d1f6cd70dc3df6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:56 +0000

    search handle light tip turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
