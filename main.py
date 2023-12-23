3e9fe3b0c73291cd380b3681031c7cea56973de6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:14 +0000

    safety stir community several run

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
