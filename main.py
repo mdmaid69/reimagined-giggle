97e7b4917a70f27ce67ec160189fc446ef296091
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:21 +0000

    team exist amount search keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
