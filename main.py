f1e377e1900161117cd875cea5317172ad51071f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:56 +0000

    bet not trip early do

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
