96f59484e847f01d18ac81f3f74b6b9a7efa9310
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:42 +0000

    runinto experience long miss small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
