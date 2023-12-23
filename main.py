f90ac1f2ce3fd526019b639a2be46d3cf202bb83
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:09 +0000

    comeover child foreign very low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
