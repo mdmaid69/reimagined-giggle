968059bef30904b53671800180e8a87d2c77c570
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:12 +0000

    slightly direct Qq sorry bite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+x = 10
+y = 20
+print("Sum:", x + y)
