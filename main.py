208a5dae5b11bc18c7331df88080dd61342d65aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:40 +0000

    contents disappointed bag invite collection

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
