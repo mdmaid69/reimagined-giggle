2d721b324ee375cf7ae5a2d17fbbb439f8cec70c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:56 +0000

    male successful type choose worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def reverse_list(lst):
-        return lst[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
