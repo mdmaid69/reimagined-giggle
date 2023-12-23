d1882119def635da1db57074e3df664e66ee4deb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:30 +0000

    both window cope giveback giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
