d088d1fd5057ff8a357949350bb5dc9a6ff49bf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:22 +0000

    unable thinkthrough great unpleasant century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
-import sys
-def exit_program():
-        sys.exit()
