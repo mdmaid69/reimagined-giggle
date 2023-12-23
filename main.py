54c436f1aaafe83f38e09fceb1508adac342267a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:21:01 +0000

    forget goodbye central follow our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+for i in range(10): print(i)
