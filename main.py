403f857acce44a2524f36fd709c5608b9b16e9f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:18:07 +0000

    before before first object century

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
