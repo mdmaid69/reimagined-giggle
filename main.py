377a22f29b7fc14acc7ca2691f80386702692aa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:17 +0000

    will cause repair undo pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
