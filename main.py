2bcfaf0d8ea3351c69337af32f788b21731c45e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:29 +0000

    run town example married free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
