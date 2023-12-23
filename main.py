b35bd1c581ced2b51700be522de4180313e0360c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:30:14 +0000

    tax insist hate attract goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
