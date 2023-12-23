55c68723cce5b74afb1074109680e3d50e117417
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:08 +0000

    taste dinner note ours space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
