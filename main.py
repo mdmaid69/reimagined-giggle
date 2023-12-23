e86e99802e75bba0b21cbda034df2ce0b6a9e7a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:20 +0000

    spot solve advanced dream comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+print(os.getcwd())
