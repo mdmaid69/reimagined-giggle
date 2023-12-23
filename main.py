116efc94cd4c9c5db8a05197389a106f48e1d77c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:41:54 +0000

    but popular throw next money

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Hello, world!")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
