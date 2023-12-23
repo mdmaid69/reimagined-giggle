38bb7522f9d9a21ecabfafb12bbb160293976cc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:58:19 +0000

    rise move voice seriously guilty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
