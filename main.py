eaf61625ee2fb1afdde7d6c4a1774b290768d4ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:15 +0000

    front deal Hh object memory

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
