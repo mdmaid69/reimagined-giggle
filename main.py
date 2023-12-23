7bac50f62508e2a2052fa62148ab11f6527cf400
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:29 +0000

    between fault stop there instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-for i in range(5):
-        print(i)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
