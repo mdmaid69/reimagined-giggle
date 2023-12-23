cb6bad7183c9bdba9badc0c73d327f079a92f451
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:22 +0000

    clean quick getout just problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
