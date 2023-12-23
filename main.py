8940d0aeb7d38d9a3a18d6c0388264f1d6e1f878
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:30 +0000

    clean cruel careful switch female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
