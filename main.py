1359809195d0ea42e33acb2e3b2ad7ba9b6f1613
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:04 +0000

    wash nigga nothing far next

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+def reverse_string(s):
+        return s[::-1]
