6dd7f5bfb93ffd22b4a8528151a4c0ad7d7ee2dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:57 +0000

    like shelter we under give

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_stack():
+        return collections.deque()
