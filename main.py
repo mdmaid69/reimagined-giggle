305a92b23c9f24c7f9cddbd25fd7bfd7816224b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:17:18 +0000

    afternoon either performance start price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
