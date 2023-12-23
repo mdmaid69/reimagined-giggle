f244956a6694a666d6a2f906a2919f8cc2e57501
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:45:25 +0000

    development and 179bite want choice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
