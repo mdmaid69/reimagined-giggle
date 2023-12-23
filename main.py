280e4847bfdeb8d1f7dc16525b502e9edf843f32
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:01 +0000

    who Nn tape test either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+name = "Python"
+print("Hello,", name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
