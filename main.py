53889ae0f01d45979f33b31dad5fdff38f57bec8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:35 +0000

    right reduce reduction shine refuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
