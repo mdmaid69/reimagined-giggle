e46cc7c8263fb87789ad023884d31c19295ebce9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:32:19 +0000

    price opposite rarely tie chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
