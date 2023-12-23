62720ff7cf4203135059ac3ea988cc86458a30ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:27:47 +0000

    honest press progress number big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
