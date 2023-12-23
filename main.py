7c301d1a1b408f10c870f6461953942aa4a71782
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:50 +0000

    fast exciting discuss tall herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-print(os.getcwd())
+import re
+print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
