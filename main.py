a47cf73b9e2b3b754e23fb43257051bd02b0b8cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:09 +0000

    priority object splash port mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import tensorflow as tf
-print(tf.__version__)
