97986cfa8b929b4065d9532d68ee2f511c39242d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:15:27 +0000

    favourite trap hope only defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
