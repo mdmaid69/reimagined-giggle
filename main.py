cec2fe407d666b16cc3e7d577867f1e5dd448d88
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:06 +0000

    her practice fetch Hh embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
