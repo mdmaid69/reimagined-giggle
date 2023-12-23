411998b13361ece8348c51c35b52f4d9166dc7da
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:53 +0000

    rare become sink refuse few

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import time
-def get_current_time():
-        return time.ctime()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
