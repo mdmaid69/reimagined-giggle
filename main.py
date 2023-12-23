4651dd5172f016769d2a1becfc5710b55962c384
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:50:47 +0000

    near border could strict knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def convert_to_octal(n):
+        return oct(n)
