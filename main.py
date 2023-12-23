81d9bbd16cbfdb7354afdf893d6e483059185e03
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:33 +0000

    unlikely turnover mail dead spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
