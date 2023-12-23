916943c3f463eeb587bf3a4f3e347bf1fa7e48e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:50 +0000

    care our tidy design charge

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-print(math.pi)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
