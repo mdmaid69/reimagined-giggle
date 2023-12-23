b4dfbfe60f94cf6c48f1f566b5a94f317837919d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:04 +0000

    whether improvement watch getout interfere

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def square_number(x):
-        return x**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
