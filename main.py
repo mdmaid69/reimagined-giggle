75c99ce1ae83a445d74362332899d9b544f2b027
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:00 +0000

    unhappy male rid period rescue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
