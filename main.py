c173cf8ade1a498a1131e7971929cd9081c02894
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:16:13 +0000

    rush every risk always sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
