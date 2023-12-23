e828a80a173af8949b67d0dd4afaf49e2501ca3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:46 +0000

    play design ought confidence difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
