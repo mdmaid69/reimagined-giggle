c42d14e4c859d1ced99bcdfcd8a272a875a255aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:29:47 +0000

    common like cross nice limit

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
