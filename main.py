c14456c855806cd1d15354b7519552885e5a1069
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:47:58 +0000

    sharp valuable mother summer attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import logging
-def log_message(message):
-        logging.info(message)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
