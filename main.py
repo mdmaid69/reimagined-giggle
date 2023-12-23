94b91003712ef92a4ddef34c35d052307ca9fd8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:52:39 +0000

    swimming open their spill cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
