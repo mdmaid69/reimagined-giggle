9079ab9d50fccfb387b75cb1df08d14867ac9fe1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:12 +0000

    willing turndown similar famous comfortable

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
