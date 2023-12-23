b92d45f7a1c90ecd1d989ba579515bdf139166c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:34 +0000

    dead heat rather somewhere well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
