b4e6d6971be25cccd829ca6bdb1d848a8877f719
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:08:52 +0000

    happen difficulty broad left runaway

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
