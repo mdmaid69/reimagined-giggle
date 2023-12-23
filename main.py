1cdf89df79f92d390bb9146daf761eb263cb6d54
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:21 +0000

    learn high concentrate dig hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
