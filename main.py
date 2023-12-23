0fe21b2813168a595222313a1eedacbc81c3eafb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:43:11 +0000

    further course responsibility off agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
