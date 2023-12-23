a93352642562c820929f05324fb7326aa3b7f586
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:01 +0000

    meet motion wander manner difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
