96182b11b807ef756f03560ed232b225137b9c19
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:27 +0000

    along occasional land waste miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
