eee58cb88f38f6297509c73116e9547ae23ddf4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:22 +0000

    proper much advise bush hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
