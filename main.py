7554e94514d88da550e001e355c22e0c3eb33252
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:57:33 +0000

    gather loud mix walk simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-print(os.getcwd())
+def get_array_as_repr(array):
+        return repr(array)
