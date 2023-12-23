3e154cfaef88db2430a36f28556d306e4663e732
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:48:31 +0000

    pleasure spot swim union runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_power(work, time):
-        return work / time
+import sys
+def print_python_version():
+        return sys.version
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
