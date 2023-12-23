f61247fee95e1ad11786e5ee61dcdb3fea53e0b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:44 +0000

    thumb tape post hot valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
