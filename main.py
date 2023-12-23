3a2e19c52720d15814257de8249041b0e545c0c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:13 +0000

    tape yet behaviour generally worry

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def convert_to_binary(n):
+        return bin(n)
