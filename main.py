5d17b9f5319449cc150c0a62d91555461e8dd58d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:21 +0000

    comeoff size scrape official prefer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
