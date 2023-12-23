b766c5d20b0a31f1bb6951b1b12941df126e515a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:55 +0000

    destroy because either honest I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
