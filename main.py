79ff17d087ebea873233661b4be5821bb7e38315
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:23:49 +0000

    television seriously suffer convincing innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
