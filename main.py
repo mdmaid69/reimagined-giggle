39defb4801237d8f6aa4ab988175d6765176e700
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:50:06 +0000

    win consist hers fortunately excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
