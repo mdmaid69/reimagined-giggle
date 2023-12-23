11ca5723a1a5223af6ea5f64637f9a02fc578663
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:04:37 +0000

    correct return complete listenfor mainly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
