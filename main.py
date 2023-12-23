c9164357c3922ba80d0020592e3451048b536b3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:40 +0000

    blame well money impossible room

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import random
+print(random.randint(0, 100))
