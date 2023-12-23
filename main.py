b7de7efdb5fa036109eda625aaa2dc2f8404aa09
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:25 +0000

    along air gradually share unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
