a38686d2222cb789f76e30d97c1a32c18610c3e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:41 +0000

    ask Vv borrow Tt workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
