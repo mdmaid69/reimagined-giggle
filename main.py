2dad7df422def97533c4d5625c2361413b74bc1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:15 +0000

    beside wing combine pay gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
