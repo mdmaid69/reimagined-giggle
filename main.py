c7df4b277963566be35b9f4bd3192b2202509744
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:05 +0000

    cook male arrive bad band

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
