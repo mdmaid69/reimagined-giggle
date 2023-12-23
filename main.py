3f75361dc3a5bb0cac95fbfe9aa887a308b797bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:31 +0000

    memory takeback offer back laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
