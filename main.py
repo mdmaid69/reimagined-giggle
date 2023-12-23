0b1adefae5592e571f26538e10fcae288859c6af
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:13 +0000

    intelligent convenient track hollow proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
