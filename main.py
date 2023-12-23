e73c089c22d3aa33a6dc32ed690812a5d05cc1e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:42 +0000

    very decide young near noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+def get_time_since_epoch():
+        return time.time()
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_floor(x):
+        return math.floor(x)
