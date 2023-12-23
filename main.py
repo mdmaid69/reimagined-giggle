7890e8f189a6c834ed0bdce209456a2be7065b40
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:40 +0000

    everyone flat pass statement complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
