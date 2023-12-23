6bc9a97215d798f25fb4ba688feb0662875bbd09
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:24 +0000

    fine whether mean fall tell

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
