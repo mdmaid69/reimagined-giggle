1d12bd95564d9e671895040b7c14d87fdb8a0966
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:05 +0000

    ordinary exchange beside bringforward separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
