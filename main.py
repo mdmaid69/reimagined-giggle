225cf32db0bcbaf1c77e22cc9b8373da7a2d7806
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:25 +0000

    test sure later steer shallow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
