e4621b1b7fa6598b8e6de78de157fb368cbd1ce3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:13:44 +0000

    please taste offer who disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def divide_numbers(x, y):
-        return x / y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
