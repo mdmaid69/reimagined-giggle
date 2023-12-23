b1ad87963ead30eeeba83be3bb1f85a86fcaa32e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:54 +0000

    minute success admit form drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
