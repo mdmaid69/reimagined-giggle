8eb8f665936806d1102c4df175fd4d57468ea0c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:51 +0000

    its as impossible new convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
