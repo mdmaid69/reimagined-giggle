70193616aaa1b61a58e7d383c92d46a99261a0c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:34 +0000

    high letter runaway hurt run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
