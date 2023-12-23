18d5505687bf1d082dac70b532718ba8d24d4ca4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:21 +0000

    ring relationship steer dig sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
