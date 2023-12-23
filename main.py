b60f1845b6f86be5e56818379c725c78985640a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:25 +0000

    dick bury pride adjective find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
