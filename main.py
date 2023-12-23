305a8b7b160ad555ec946341b6ea0cb46c80a36c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:23 +0000

    shoulder bite message pocket watch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
