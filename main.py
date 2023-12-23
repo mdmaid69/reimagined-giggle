2a5cdfaa7b9dde103bde7df7ecec4000e30f232e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:58:10 +0000

    door consist cheap lose partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
