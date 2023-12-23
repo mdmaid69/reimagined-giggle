85ddae45fb698876483946ddadfdbf5a5768548b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:54 +0000

    warm healthy goaway switch bye

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
