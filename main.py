ca2b980ae78fea2065c62ac5cc77e4e4080467fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:17 +0000

    paper history but pot connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
