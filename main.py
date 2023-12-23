a86b935ef6ed396787778adbfc8d707e5ee991db
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:29 +0000

    rob news cause lucky job

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
