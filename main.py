c5b136d715bcb9ec7967f47373fc6eb212721af2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:32 +0000

    float official present treatment mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
