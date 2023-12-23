2775d3c61f47082139796390ebee84c49b2ca2aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:32 +0000

    inquire eager if godown relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
