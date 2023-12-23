32330c9771004f161bde5994100a5d52c3792334
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:38 +0000

    convince slice swimming fall needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
