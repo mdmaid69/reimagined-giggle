2d23d39cbc0473ee943ec934f0bcacba06d87571
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:46:20 +0000

    match well waste opposite shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
