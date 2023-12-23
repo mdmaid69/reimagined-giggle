e41f2ab98f35d20e8b0b8e99119840694e5706a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:35:14 +0000

    joke dinner read female disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def get_current_time():
+        return time.time()
