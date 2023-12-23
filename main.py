d434cc2edebc61bc8a0113512fc11479a2f3a09a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:01 +0000

    hang along repair safe pool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
