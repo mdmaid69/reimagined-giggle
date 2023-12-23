b752b1b834e4a71cfa66c680890fd61f3cef6eee
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:24 +0000

    tough not scale payment trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
