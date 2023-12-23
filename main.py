438a3913828cb2ad60df5f463b591a85b1eefc34
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:02 +0000

    lock rest near closed total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
