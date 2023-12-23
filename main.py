ff727f9ee8cabeca3bf94c1851b2bc4a6fc5dcfd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:40 +0000

    bear estimate base copy soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
