ffa60ab6cd5c9126415938edc42ed5a99371e2cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:16 +0000

    feeling frightened immediate trust knowledge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-print(sum(range(10)))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
