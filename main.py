3ecb51843578670e7aa92b23acf43f62f6b3bd8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:38 +0000

    someone unfair steady secret prove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
