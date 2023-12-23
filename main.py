34a4d89111f3e0c93835d85d5270e69b7b4584e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:29 +0000

    recently special relation branch hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
