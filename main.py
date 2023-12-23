957ebaa7e47a8099789d6e67dc509f3367445dd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:32 +0000

    favourite nervous service bunch union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
