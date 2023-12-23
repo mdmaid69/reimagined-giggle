c750bd1da33d2e0b84c68058712dca266680c433
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:13 +0000

    Ss hurt true neck fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
