d3baa594430d3527fd8668eeb18066ce32232cd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:53 +0000

    sound whom job scheme blue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
