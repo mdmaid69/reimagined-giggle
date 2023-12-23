374dd0e5f02f6cc69c0f64fa1cc67c611cbefe19
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:26 +0000

    by port something language surprising

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
