4ba354270a8a9d492cc800cf2adc8f6af8dfd67f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:55:45 +0000

    close this uniform mine kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
