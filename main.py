17f5dc2c5c8d5d7cfc575718ba27496205febda6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:15 +0000

    rule fun goup maintain back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_area_rectangle(l, w):
+        return l * w
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
