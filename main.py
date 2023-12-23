b93fdff8501ae1b80ab5a126617afaa5963a5e8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:46 +0000

    band clue fashion hour like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
