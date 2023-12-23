c9b1a8eeec5590fc42e3a74f4e27c66714a23ab0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:37:08 +0000

    grade pronunciation settle public variety

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
+  def sort_list(lst):
+        return sorted(lst)
