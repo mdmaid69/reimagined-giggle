6b2ddd2f39047d47fba70b05dc13335d79093f9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:12 +0000

    file recognize suit on piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+def reverse_string(s):
+        return s[::-1]
