c5d00eec7b7c8ee726cb315a0b08d965b9ca1ca9
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:21 +0000

    blame original gather pressure science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
