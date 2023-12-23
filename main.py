dbfcfba86116438b6ef723a9828cb0ef4cebd9c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:09:27 +0000

    wind transport unusual subject tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
