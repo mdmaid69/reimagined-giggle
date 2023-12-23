4d9ea5d945b193649ae62045698956ad4cc5c389
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:59 +0000

    without quit shut feature alone

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-  def cube_number(x):
-        return x**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_list(array):
+        return list(array)
