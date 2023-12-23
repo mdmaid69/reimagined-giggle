af315ad6ab6702aeaaaff22650f7feab7d0ce249
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:14:31 +0000

    peaceful wall include such high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
