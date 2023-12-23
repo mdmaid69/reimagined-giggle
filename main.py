ceb2a49dc48e5fceba7fac554d979393d5b05eb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:44 +0000

    wide bring valuable push smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
