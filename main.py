a3e4513de35ffea571d7517799bc5fd0a580025e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:21:05 +0000

    contribute content post back provide

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
