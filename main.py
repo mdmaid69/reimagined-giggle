f105f3aed491710516093e085b3788e65b4073cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:15:31 +0000

    development instrument young less unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
