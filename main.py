83b4fb46b81d96b55dd200072a986f2a4c188ba7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:16 +0000

    upstairs song pretty unlikely where

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
