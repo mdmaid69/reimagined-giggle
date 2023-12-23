ee7a50cd2caa20f83c569c46a0f6f312623890bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:33 +0000

    place their person improve work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
