9cc38be3ca9c47686cb0b48ecbe89fa7b1cbfb74
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:29 +0000

    mystery hurt arrival soil sticky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
