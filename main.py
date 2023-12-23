85048ebee0d4c0688d7a4014e3f3569c0aee76ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:19:39 +0000

    benefit thanks pretty well 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def convert_to_hex(n):
+        return hex(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
