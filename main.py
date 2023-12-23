368361f4463fef224742b7dffe2e3ae1c9f9f411
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:50 +0000

    runinto office deliberate account neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_sign(x):
+        return math.copysign(1, x)
