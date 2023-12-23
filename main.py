8f0126890345d7a20f4e6adbd8c062000109782d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:28 +0000

    software workon song snow several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
