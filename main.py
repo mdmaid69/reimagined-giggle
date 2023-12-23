4fcbc1ec0ee5890f5934355f86d59b8be1d8d79a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:02 +0000

    needle argue niggaacademy calmly push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
