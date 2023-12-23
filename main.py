09188210cc0bbd004000392683f4e6a403d1292e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:23 +0000

    fight place hers sing result

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
