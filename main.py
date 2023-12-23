bd12b17fbba7f8cabbb471cd0e3ba10b501ab729
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:45:38 +0000

    note Ll careless drawing mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
