bcd7309742fd66f95ac99437cc1b597045526065
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:35 +0000

    prove off tooth within takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
