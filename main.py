d7b5473d0868a00716764bb143509f683d3258ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:20 +0000

    relation complain beneath exact opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
