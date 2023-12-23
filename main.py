664f21b7344945e352d1608b14bc5e5ebe8733e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:53 +0000

    careful copy swallow slide evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
