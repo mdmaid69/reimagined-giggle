d8e2d1900c4c63cad9b82f884a69df3a334be3f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:07 +0000

    approval determined duty power sale

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
