1c6124d3b8af2e7d025c6060e78358f3538f662d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:39 +0000

    pick 565end fold far upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
