59e1046abb5d21baddf27519e14a8da66d009d78
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:37 +0000

    seem stamp answer until street

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+print([x**2 for x in range(10)])
