7e1c2d7d28b7eb8a65bc76bf4c6ab03b808737c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:55 +0000

    beginning remove stone century beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
