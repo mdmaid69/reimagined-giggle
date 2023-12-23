c779775120c3db91e5532ade4fba3e3a99e4a6ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:42 +0000

    large confused total then cheap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
