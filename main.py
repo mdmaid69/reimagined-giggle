9cc410e004fc1bfcb7b22550fbeeba97c2d97260
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:46 +0000

    order hurry bright wait imagination

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
