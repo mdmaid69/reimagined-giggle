634baec6f10af4687907bd469fb7053989990f9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:18 +0000

    shut afternoon increase drive science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
