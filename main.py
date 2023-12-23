ac993410b853dab5f4db0020df7d18f4c0841e93
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:32:40 +0000

    look among protest ignore wind

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
