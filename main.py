bbffe5884b8fc465728fa204673e65f3e12e0e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:15 +0000

    kick attach purpose share book

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
