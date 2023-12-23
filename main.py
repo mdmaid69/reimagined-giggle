8782d2dcecde619ade4eefc0f7959730a796aed9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:58:17 +0000

    or like love store entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
