cb84cae41ffc0944e9a27c7c330804c960236d4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:21 +0000

    list prize direction finish copy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def sort_list(lst):
+        return sorted(lst)
