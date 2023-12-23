23dff8fddfeaa92a396c7cb111e2373306c1c22b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:49 +0000

    spot allow soft game abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
