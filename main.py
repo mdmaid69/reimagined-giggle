8e88995cc3eec7fe2479189f58f7bf578119abeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:37 +0000

    prove various quarter condition comein

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
