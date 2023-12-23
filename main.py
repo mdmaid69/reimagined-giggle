13e2d95a3f173de79b956491025dc87d63238c81
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:05:01 +0000

    uncomfortable behaviour break contain tough

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
