ca7fa5d028cf83f0cea6008f2077e2f4935a963b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:20 +0000

    fortunately print reduce old possibly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def count_elements(lst):
+        return len(lst)
+def calculate_speed(distance, time):
+        return distance / time
