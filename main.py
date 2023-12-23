ec9a1952b545d3d9c1ebe622f91789db0d8c7ca7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:47 +0000

    bother whichever stretch bunch comeout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def split_path(path):
+        return os.path.split(path)
