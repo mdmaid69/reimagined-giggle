a1912c3e31a05718db610bcfe235ac378e69e955
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:28:51 +0000

    mean concern small mend play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
