c47b1f5b8d3581c9eb3370a3b659d75f6b01d255
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:47 +0000

    attach price pupil standard frequent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
