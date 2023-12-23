6cd87d1c54587add08a1b12863cde87019c3a9b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:40 +0000

    except relationship push means reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+for i in range(5):
+        print(i)
