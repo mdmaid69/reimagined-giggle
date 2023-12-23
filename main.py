89d00f9f83c2fa2533ec9ce39e04c4c5da670ccb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:27 +0000

    card first crazy country cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
