c7c1959f159b5c3e69aaad4ae9c4863c7e782641
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:43 +0000

    admire male land comeround loose

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def is_even(n):
-        return n % 2 == 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
