fc1c1ee8180ae8a695caf74160904719b08587f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:35 +0000

    impressed blind bus swing false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
