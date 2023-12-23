068ecd40a7c4b100fb13f8aad0e90105335d4e07
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:06 +0000

    clear cry buy about unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
