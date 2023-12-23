ce0dbbea2e27c28225babdfcba621a418eb11351
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:52 +0000

    pity worry everywhere drive centre

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
