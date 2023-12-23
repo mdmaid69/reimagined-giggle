c978d5c50bbeef1dc451901c8867de1c04e3af86
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:55 +0000

    estimate deep beat through sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
