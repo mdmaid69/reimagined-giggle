b70128995ce1794890f8960b8f5556d24ca2db58
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:04 +0000

    mind listenfor container law nigga

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_max(lst):
-        return max(lst)
+print([x**2 for x in range(10)])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
