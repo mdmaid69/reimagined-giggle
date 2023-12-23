124cbfd68e24c50ecf71869e8cb5e59951d1000b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:55:50 +0000

    sink turnout boat seriously concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
