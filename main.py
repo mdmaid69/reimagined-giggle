5ea3c0f98ba3678c1023d420970347a4e3bab9f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:43 +0000

    bringforward climb increase bet left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
