75fd6a0aa635542b5cf5cbf176e777d9eded4ca0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:01:15 +0000

    breakup image he trouble port

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
