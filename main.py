899376dc5495c39c8636b676ba6883a6e3a5efed
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:46 +0000

    system boat ilovedicks Hh main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
