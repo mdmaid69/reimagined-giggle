038d6535ddaf522589227b5967d40add2baab515
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:16 +0000

    fresh crash frequent scale yourself

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
