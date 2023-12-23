1c1175798bc9b827f69872e000b06db0bec88ec2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:52 +0000

    major either shame decide drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_counter():
+        return collections.Counter()
