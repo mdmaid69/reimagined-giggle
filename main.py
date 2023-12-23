5c40cddc268e1b2eace31ad09225c07fc7684e45
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:06:01 +0000

    gather floor important wild scrape

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
