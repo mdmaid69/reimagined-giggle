fb30b66ea2081ce989d68d5b2ca55651b6711a65
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:58:37 +0000

    pupil start trial square free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
