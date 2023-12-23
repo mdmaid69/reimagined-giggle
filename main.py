14534aa1dbd9cab13806274697d69ac6d66866aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:21 +0000

    sudden defeat plant shake mainly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
