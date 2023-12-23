5c6980ad49636b2ec7e4ef17c248e0920b6be2c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:55 +0000

    give callback recommend road add

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
