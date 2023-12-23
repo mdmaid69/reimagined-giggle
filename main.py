6d0516859c8b5c901fbd5b1ba0d8d1a87c3d72ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:05 +0000

    previous top coat feature after

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
