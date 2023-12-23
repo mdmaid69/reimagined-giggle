e957c13fc5986cc950540a6418bfffe08a652a4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:26:51 +0000

    ground fit goon field lunch

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
