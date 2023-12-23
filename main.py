3fd2f4b544b338b4b856057240b8b18e90633501
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:13 +0000

    check theirs agree strength model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import sys
-def print_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
