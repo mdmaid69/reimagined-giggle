9131108191aac6d885e27a7b7f3086dcf1fdbde4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:22 +0000

    feature vote wave little equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
