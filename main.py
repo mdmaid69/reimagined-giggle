2ea3dcb096ff392b6b148b6522919a2aa1294317
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:09 +0000

    crowd Uu delicate keepaway debt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
