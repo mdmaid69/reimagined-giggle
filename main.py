baba52add1ff0bbdfa55f2f084932ee996be22c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:22:24 +0000

    within choice piece ill basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
