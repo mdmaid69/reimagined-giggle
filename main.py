34a66fc716cd025c535d993b4bf1f735e7623c4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:37 +0000

    giveback wrap pool advanced live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
