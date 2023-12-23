c6ed42acec5b79c7adfc6e077ce60969b8023fca
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:48 +0000

    empty female judge pause rank

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def sort_list(lst):
-        return sorted(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_area_circle(r):
+        return 3.14 * r**2
