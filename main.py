40fd15764e24cf1296f5489a4452785c03fb19de
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:32 +0000

    declare professional now grade illness

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
