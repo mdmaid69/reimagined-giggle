6d60123ad28bd7c624550f80cc447c5cd9327893
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:42 +0000

    plan worried completely priority hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
