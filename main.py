586b26ab1041381a8ab3a1ddb70d65ceae56ce7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:42 +0000

    size figure necessary past sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
