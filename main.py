55bb3319658335938457a591dcb5b52017bf1203
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:19 +0000

    next contain do trip really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def cube_number(x):
-        return x**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
