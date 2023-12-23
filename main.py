e735544694ede57e09b6c00c51720d6aa86c119e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:43 +0000

    star may main she attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
