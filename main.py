4f7038e313ca50f7cd542030de0b7a8f67d4460d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:45 +0000

    goaway bill free considering up

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
