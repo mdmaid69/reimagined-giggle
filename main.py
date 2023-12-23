1c88bcbd4ed0f19d6aea163df3e29ebaf3de68e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:11 +0000

    meeting talk relative please over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
