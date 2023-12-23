62da49f2705a6570fd637c2b155230c6f46762aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:10 +0000

    suckme case change callout suppose

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_str(array):
-        return str(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
