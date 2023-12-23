c0fe64d57971abb3bcbb73c295bf539e33dae0a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:13:21 +0000

    harm light miss middle join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
