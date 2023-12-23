b1d84b8182d6482828d9549ef8d9b50f031431eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:03 +0000

    wing wealth number post sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
