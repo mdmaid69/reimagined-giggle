dea56199b25c2d9fa9231982687bce5bcd350b5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:27 +0000

    every change calm dull search

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_max(lst):
+        return max(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
