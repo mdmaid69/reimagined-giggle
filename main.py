9dda00cc7d4ff7733714591778ec136c176a5169
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:14 +0000

    feel answer make sick lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
