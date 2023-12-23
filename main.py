2d5db3154ade6aa547bdde05ca3e6c980a113c00
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:04 +0000

    sail neck repeat 179bite exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
