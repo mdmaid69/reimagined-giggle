742ccb3d3e0493f6dc59f110830e86cf817ed434
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:56 +0000

    present workupto charge excellent store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
