9b80b90eb1f86f329f2e3b4cdeb2182e9a6b9256
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:51 +0000

    willing its underneath discovery interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def is_palindrome(s):
+        return s == s[::-1]
 text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Characters:", len(text))
