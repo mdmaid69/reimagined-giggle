086b44689991268ef19a6c2481cbd29f47987e60
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:00 +0000

    ignore touch matter constantly risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def cube_number(x):
+        return x**3
