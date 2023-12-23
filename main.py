788713f0e561d7dc7879e248611fb79343cdd60d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:11:44 +0000

    stage do chance poor question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_typecode(array):
+        return array.typecode
