d2d200cdbb89105886d46f096bc93da89eb65905
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:14 +0000

    convinced love taste occur mention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
