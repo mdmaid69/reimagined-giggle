f7271c86a012bbe984f182c3b84b60e7487b0af6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:53 +0000

    adjective frame poor confuse surprising

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
