50bb9b2daa9b07808a9af6ee250df8260ead5cea
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:50 +0000

    vanish land press suit know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
