d6bf7c4426e87f31b9775000b15a0df0d3592f49
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:50:26 +0000

    almost spin land lucky force

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
