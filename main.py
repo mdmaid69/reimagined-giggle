34794111d2435977292f578d5cd55cc053b42098
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:22 +0000

    season each copy Nn everywhere

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def is_even(n):
+        return n % 2 == 0
