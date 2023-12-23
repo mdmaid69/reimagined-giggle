50e1e3e1313005bdbd9dfd65735fafdd6817ad15
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:34 +0000

    test cry please scrap clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import time
-def get_current_time():
-        return time.time()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
