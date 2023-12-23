51e74e3d03ce36a57301ba8840382f9b41801d92
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:18 +0000

    address stranger shine put but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def multiply_numbers(x, y):
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def multiply_numbers(x, y):
         return x * y
