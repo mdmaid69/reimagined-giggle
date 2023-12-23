61f139fb9f69c68ba0eada7ff1803e95d3a8fa3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:58 +0000

    brain platform excuse zero cold

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-print(sum(range(10)))
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
