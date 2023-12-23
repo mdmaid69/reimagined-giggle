c92ef45e9f4dfd7a8cd848243d004de880d72f5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:36:27 +0000

    smell represent opposite unhappy well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
