7385a91a301290bda093b9d6a8db9cac2366decc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:37 +0000

    achieve progress young closed most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
