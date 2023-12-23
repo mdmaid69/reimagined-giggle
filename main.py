905e15ca09649122e839ea3a7368f5bc06ccb2d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:28 +0000

    pretty somehow quit difference temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
