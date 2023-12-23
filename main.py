2cf82db6f14ba8dc839d70432d5c67748b6e7f9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:37 +0000

    entry turnon or they petrol

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
