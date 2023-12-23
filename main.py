7c7ce8c0796b9285a2b6037657ae708bfeb28058
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:36 +0000

    soil wake pool letter quickly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def is_even(n):
+        return n % 2 == 0
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
