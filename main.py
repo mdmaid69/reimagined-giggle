5d47c5cc89c08a91dfe3e52b9a8bfb8d38f9d791
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:40:19 +0000

    squash gather spring bin standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
