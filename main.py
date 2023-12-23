dd6832fc8f7af6e19f64381975df668d8a4322f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:37 +0000

    let share dress patience relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
