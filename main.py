38f5a667484dcdf5f4e6b1dfba2c38a27271b53e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:25 +0000

    the habit wrap spite terrible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def convert_to_binary(n):
+        return bin(n)
