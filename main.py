2276bc1b85ad2ec7985220d127336b5c29563178
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:25 +0000

    quit clothes someone truth seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
