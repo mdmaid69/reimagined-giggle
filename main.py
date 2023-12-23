a9fbd3d8f41979448f883f87700234d988a8c0bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:55:10 +0000

    keen level boil little object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
