d78b36340237a36cab8cd8cb9b5ee0c852c0b4f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:05 +0000

    also fight consult hour fun

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
