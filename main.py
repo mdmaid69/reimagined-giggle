bffd5b04f256b488f1d6e70daefd9e6f7baa8a90
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:20 +0000

    impression drop bother declaration nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
