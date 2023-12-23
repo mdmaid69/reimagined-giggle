201e5882c0c9ac384571b017fd39970865497a0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:39 +0000

    badly couple broken stand effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
