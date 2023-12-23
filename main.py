3fac8f791bbae9f3ee18bc27ef797289ba148875
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:59:38 +0000

    nice this insist speed stand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
