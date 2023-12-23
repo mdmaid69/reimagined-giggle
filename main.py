d704a4b3f95880a5b4db0602f4ab7bb515f51be6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:58 +0000

    apart mention slowly cruel pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
