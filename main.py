6ac8f77e4af09237c918cfc20e4d2bfb96ef6be2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:24 +0000

    lead odd many opposite minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def is_odd(n):
+        return n % 2 != 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
