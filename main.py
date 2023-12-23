625f343b562eec8b782a8d8a2631cd7d2066718e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:10 +0000

    uponon job admit agree shock

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-print("Hello, world!")
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
