57aabab0d8ecd176775de6d2235dc671d0bd949c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:33 +0000

    service check problem leave sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
