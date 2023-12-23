0b6fd7c0b20e3e1c91b0463573e89b089cf95e8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:23 +0000

    sound threaten you serious Itself

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import time
-def get_current_time():
-        return time.time()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
