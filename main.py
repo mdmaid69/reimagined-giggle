25f8651fffeea602d43b1e374a2945f53f8d5046
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:08 +0000

    blue custom after store tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
