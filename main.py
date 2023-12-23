ceddfa8c192a7236e9b955d1b7bf34bf5f9e5ffa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:44:28 +0000

    phone joke game much flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
