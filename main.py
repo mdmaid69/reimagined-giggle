4a7c009dc119a4ff1fec741e9aa6a6beefff8a47
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:54:27 +0000

    bank occur breakthrough track attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
