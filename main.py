26908e11f4dc8e80f47419fa61b761ad2650b6e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:03 +0000

    defeat everybody claim land subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
