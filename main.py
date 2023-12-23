0e18551b18499f248292f220630444c0d8e45786
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:08 +0000

    argue anywayanyhow children hospital scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
