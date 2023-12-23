dc8580ebee1e9da06f76131ae6cd958160818cf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:58 +0000

    woman insist young responsibility refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
