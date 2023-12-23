5fce7a862a6113f0f63454831de4e751140bfe0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:25 +0000

    cruelty town point blind pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
