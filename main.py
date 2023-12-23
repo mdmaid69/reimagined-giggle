a5b6887d3510e42393b812b9e5e5426638ac3852
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:57 +0000

    disturb combination borrow measure acceptable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
