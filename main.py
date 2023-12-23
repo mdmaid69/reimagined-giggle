1b3389dab417f10351f8f41b08b511322fbb3521
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:56 +0000

    cover floor exploration order separate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def convert_to_hex(n):
+        return hex(n)
