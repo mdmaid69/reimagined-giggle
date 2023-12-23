b72804e7320758f6bce41482e353a09a039dad4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:19:23 +0000

    contrast waste capital treat wash

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
