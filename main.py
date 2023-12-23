5d2392f7d429f6d60cbc66714f74c94421b8f909
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:22 +0000

    from action only interruption getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
