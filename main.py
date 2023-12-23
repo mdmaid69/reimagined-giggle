d7dc64c4e5d1260b09b9ae99c9337e72d4d3deff
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:30 +0000

    check amount fall many twist

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import sys
+def print_python_version():
+        print(sys.version)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
