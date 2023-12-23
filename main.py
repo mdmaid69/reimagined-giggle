eb0c37dcdb19427f2a13e163215abc0457ed9095
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:09:24 +0000

    best name talk shut tidy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def divide_numbers(x, y):
+        return x / y
+def add_numbers(x, y):
+        return x + y
