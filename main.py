3a7d123a0115b3d417ac807a6f438c6ffd4735bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:33 +0000

    basic dress warning warn dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
