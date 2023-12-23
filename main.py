20af62d29b8cbbf22e225c449198981a83577f88
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:03 +0000

    release sink row character determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
