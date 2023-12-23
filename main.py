753a3b641a3c3d79d78c2465686073e665cf6af3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:10 +0000

    whole sink nice communication say

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
