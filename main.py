e22c1251498a51d83e81d70b75f41878e323cf87
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:05 +0000

    party among company stop pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
