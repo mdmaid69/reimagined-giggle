80969666ac376a65c6e6d8f99932991bc8b3c464
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:26 +0000

    polite lonely makeinto left pollution

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
