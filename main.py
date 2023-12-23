ae110cb99be56bd598ba43f3174000c4d2757c4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:40 +0000

    accidentally during beyond uponon tired

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-for i in range(5):
-        print(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
