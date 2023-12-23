1bba49e78068d074f49e3147049d8603deedccec
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:42:10 +0000

    from off flow Uu hold

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
