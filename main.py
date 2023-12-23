aff7b560b935a2484abe78868737b3283a116a15
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:19 +0000

    Vv Ff brush operate fold

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
