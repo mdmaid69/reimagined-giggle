2c51f4b549642b3b0e4530bbc796b63fb22bc44c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:18 +0000

    squash mess comeover bin all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
