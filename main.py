2a257f71fe771d40c5c2d7171b0ed5f5f738bba6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:01:06 +0000

    punish before afternoon mine dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
