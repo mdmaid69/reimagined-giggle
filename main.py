11a87074703ad6b22224d96013475447ca5e58f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:42 +0000

    giveback past bend manner callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
