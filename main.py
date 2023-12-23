17e9d244c5a7837eefe03393708cf28247615b18
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:42 +0000

    satisfactory wake clever find trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
