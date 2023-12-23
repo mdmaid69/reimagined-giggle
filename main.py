b2eee7a767a3ba9ade3cf9991969f8196e7ff2b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:37 +0000

    concern father runout outside upset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
