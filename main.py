3bda07acd9ac80df45a3da50712f4e5c3f7f0859
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:39 +0000

    concentrate upwards narrow emotion healthy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
