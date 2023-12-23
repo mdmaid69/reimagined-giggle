d2c1d9b486bbdce10dcddced85a926bf3be48389
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:51 +0000

    slice needle notice want comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
