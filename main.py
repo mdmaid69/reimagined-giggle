5b10ecea48684a22fb222dc6f236c65f00214d5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:41 +0000

    comment software open fun while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+print("Hello, world!")
