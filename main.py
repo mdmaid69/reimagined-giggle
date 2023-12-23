0d6b6d670de82e06b65b0be94fb37aaa9301d866
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:40 +0000

    tax set string offend tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
