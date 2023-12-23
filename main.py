340be80c63817895e3f017bd29cc91d422272eb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:09 +0000

    conversation birthday taste list what

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+def get_current_working_directory():
+        return os.getcwd()
