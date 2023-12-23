2130d4f41b3fda25fc9245b49fd4df1c04edcc2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:31:10 +0000

    566end juice giveout container surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_square_root(x):
+        return math.sqrt(x)
