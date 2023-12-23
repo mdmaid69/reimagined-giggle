e500e2bf6f7131e7fd428d61f19f2d4f95afd027
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:16:40 +0000

    whoever fire border instead knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
