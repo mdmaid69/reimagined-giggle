98facedb3bae38998c00c9f9070855695d795394
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:14 +0000

    calloff save today distance prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
