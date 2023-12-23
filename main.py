eb199a9c674dc8df1ceabc41e4489175856f6d65
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:30 +0000

    command split justice wire fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
