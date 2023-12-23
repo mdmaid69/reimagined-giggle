397b9229ab0ebacfeefd7bf961275eadd44e7e06
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:11 +0000

    spite rest instead roof upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_speed(distance, time):
+        return distance / time
