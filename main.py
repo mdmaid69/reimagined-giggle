a85bd48b6ad73ffbfc3cb083f0362445a93e16bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:34:48 +0000

    cost throw one die wild

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
