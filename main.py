2e3210f9c87b03d0aee68024e5be995846ad147c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:24 +0000

    combine memory avoid breath follow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
