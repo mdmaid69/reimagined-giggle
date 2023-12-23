11074da4796fef0c1387ee5b24882fa0ed2ad153
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:32:51 +0000

    back few vanish freeze deserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
