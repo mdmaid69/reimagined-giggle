0924e6f543cd3deaef641590644ed07a803e13b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:17 +0000

    takeaway go rain excuse what

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
