ca8b11a46b1182e5d5f1bff9b7cd4ee8e0d6e818
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:56 +0000

    show against expect might young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
