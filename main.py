962901dcdccd62a8ff68011494a2e9762342785d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:07:45 +0000

    indeed sticky practice possible record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
