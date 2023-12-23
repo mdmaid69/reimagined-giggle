006b70538c4562545af352dc7a1f3bd0ebb2b8be
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:03 +0000

    himself whoever lookup urgent sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
