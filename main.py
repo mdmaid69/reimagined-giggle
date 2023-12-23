f91702a7021a002e984ba6a5b99c7cebadb2c39b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:31:57 +0000

    who behaviour point environment left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import collections
+def create_user_string():
+        return collections.UserString()
