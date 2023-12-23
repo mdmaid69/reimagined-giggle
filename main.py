66a2c905fe5ce0de2e3a00975277911616109964
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:44:05 +0000

    fill large persuade crash play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
