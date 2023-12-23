ab6b4fb66167c864d6487fc99254254463c7fd99
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:07 +0000

    pressure book show constantly game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
