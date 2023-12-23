88412ef9556371c9ee3bb43a44265137adaf7c23
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:57 +0000

    holiday platform label accommodation judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
