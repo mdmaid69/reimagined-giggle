c893dd2d307b252349d57b95b792c0e1831ac222
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:32 +0000

    reaction often somehow lucky trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
