eb4c689aa9e24e65488bfaa96d4ea46215274585
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:52 +0000

    success influence occasional indeed workon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
