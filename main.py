680f64c84c0bd8a21aee24e5634f51e3009b6292
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:35 +0000

    criticize charge explain wash adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
