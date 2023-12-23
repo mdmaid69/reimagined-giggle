e57e6b0caf1e5a55d840ccedf1e468aaacb85e9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:14 +0000

    put interesting send except winter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
