c5ac0b2a7ad642aa458e180cd59fbd311c2298d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:00:45 +0000

    workon my confident sensible control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
