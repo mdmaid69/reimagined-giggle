b092a03905702e46ffcad12aba0539706f14f1fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:10 +0000

    wash rarely as conclusion beginning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
