02472be6cd7c8384573e44f196f2354821b66305
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:33 +0000

    workout on luck ability little

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def count_words(sentence):
+        return len(sentence.split())
