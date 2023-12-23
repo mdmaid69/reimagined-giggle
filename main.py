9eae723bec57f20ba6510e2538981d63422fd3c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:18:43 +0000

    away include sweep consult suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
