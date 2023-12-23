3081d1d22f467382cd8e292c59aa0959c996920a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:05 +0000

    children control week minor march

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
