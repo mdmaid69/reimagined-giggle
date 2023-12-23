bc9529d619f6280640c57d7993a080849bd261eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:24 +0000

    label nor refuse book closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
