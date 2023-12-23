0f5b4c95822c9d781055efbb09872680c1b94350
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:03 +0000

    favour actually news degree delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
