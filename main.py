3909953c7d8ccdf2087d5c88a4ac6974b3627cbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:48 +0000

    task reply join track mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
