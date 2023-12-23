22ae8cd39c0be624bd6a651c3dff43001c114eef
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:44:24 +0000

    blame knock middle consist report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
