03a423c9763a3b0d6348238b2db245d39dc90272
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:55 +0000

    benefit acceptable valuable proof weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
