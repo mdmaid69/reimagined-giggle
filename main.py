aef35d52abba57fc069380df9288689ed4561e5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:32:59 +0000

    figure pleasure introduce convince treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
