269320caaca0af7b380fb06bc8ca5700c4085310
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:50 +0000

    advice make unpleasant band centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
