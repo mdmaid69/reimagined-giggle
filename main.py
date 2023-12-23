b107efa84cc01564637803846b6471d8fcd2caf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:24 +0000

    finally thick remove Rr warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_min(numbers):
+        return min(numbers)
