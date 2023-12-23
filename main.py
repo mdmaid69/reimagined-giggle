3cd0e84f12049185fdc3d82c966c6c7d00704964
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:29 +0000

    print comment pull upstairs practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def is_odd(n):
+        return n % 2 != 0
