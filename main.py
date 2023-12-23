7ba6700b7e4f7f7f1bc5b8d408c4b91efd8423e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:35 +0000

    disappointed regularly wave tone freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
