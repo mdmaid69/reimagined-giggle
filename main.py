17678cf3608b1ff58eaa707f56a907b88d8fc190
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:33 +0000

    any behind action might spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
