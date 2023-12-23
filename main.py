3ce577f7f1d3c54249935528d6fab4753c6ad2ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:50:56 +0000

    lead best bang state interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
