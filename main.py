030039734375a689aa76c97935a55f4068359af9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:12 +0000

    arrangement deserve agree line cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
