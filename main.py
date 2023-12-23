66147c3a7659366e4afd9d336074492ad2821f67
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:31 +0000

    remark drawing here vision accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
