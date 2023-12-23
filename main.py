300c20425c0c063a92e560f864ab90afe54e37e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:31 +0000

    spot press begin bottom error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
