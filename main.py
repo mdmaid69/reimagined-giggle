bb54ac4ae52535267c35b49afd5fb2fdaa7390b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:02 +0000

    accommodation morning getoff film door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
