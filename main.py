6e03de53ec3d20475f50e868514c7deed4e5d1ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:13 +0000

    not truth most breakdown closely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
