1f1a22f6014ec63e193471d3a991eb52fadc0c9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:12 +0000

    or smell quarter yet group

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def cube_number(x):
+        return x**3
