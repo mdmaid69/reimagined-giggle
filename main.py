7427011e17673cb27b7bfcd918a4e5215bf3319f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:03:24 +0000

    female estimate steep 571enough getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
