eea4c99ca63f0974a0987960f5ee571227161c50
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:14 +0000

    tap wish listenfor sell on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
