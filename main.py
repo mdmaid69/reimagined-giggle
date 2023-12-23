a199abaae92eac3c93dbdecb5ce75ad92bad2403
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:11 +0000

    react take boring duty gate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_numbers(numbers):
+        return sorted(numbers)
