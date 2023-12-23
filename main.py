ff44fc1f164c91d26a614260c7e42e8b46732293
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:04:29 +0000

    Uu bus vote path means

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
