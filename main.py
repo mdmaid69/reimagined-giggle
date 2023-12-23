80ac0c84a10c973d2b2dd9ec6afb32866d205338
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:29 +0000

    exhibition rescue get refer upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
