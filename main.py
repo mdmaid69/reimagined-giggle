889cb90114ae5e16ad95e2810ed3a8f98c28a430
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:13 +0000

    really idea foreign cry problem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
