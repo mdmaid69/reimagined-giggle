b2652c81f51fddf5241e30945b597dc05d505449
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:17 +0000

    beginning soak unkind collect anywhere

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def add_numbers(x, y):
+        return x + y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
