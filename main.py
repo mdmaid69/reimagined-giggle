d04d51ba3a48ef46acf7d14118d07065b14a459e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:50 +0000

    join attention every Pp scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
