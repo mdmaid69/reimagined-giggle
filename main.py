b7c3cbefbc8fe755a844098a77360e6d4d3f6785
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:18:51 +0000

    win somewhere lookaround shake burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
