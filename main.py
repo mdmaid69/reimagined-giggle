9680c05f26078de57c499c09ed16e7ae2cca21c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:14 +0000

    quantity answer freedom once people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
