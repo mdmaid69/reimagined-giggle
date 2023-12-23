e62c67be35e4a71ea3dde5b8901d17a7dc04026a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:38 +0000

    active pure print appearance Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
