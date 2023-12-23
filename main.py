f381cb9a6fb09e76168adde1588122147923aa7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:41 +0000

    organize season within stay 571enough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
