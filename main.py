3e03c8801d0d5ab61f21f263a5455a8dd74692af
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:58 +0000

    deserve damage delicate height spring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
