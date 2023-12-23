f71aba8f3e00200ef8d038fe70f89b98e2f824a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:46 +0000

    fold beautiful circle she lend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
