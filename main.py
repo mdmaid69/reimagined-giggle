50c047ce398450252f904ae7f11f332d5ec65063
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:50 +0000

    curious forgive mood area produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
