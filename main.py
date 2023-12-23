4ce3f36dc5444b05c497692993fff787e8ea2b1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:01:54 +0000

    say silent quit smart measure

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
