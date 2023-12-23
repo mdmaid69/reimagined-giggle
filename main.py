19ef6896292f3cf6d7d573be6835c1203e7ecd7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:33 +0000

    street rely crowd grateful perhaps

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
