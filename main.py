f60717b5685882bcbc11f68d6b0582def81d3cd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:16 +0000

    often quality thanks underneath steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
