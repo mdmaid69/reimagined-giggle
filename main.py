bcdc4cef75800b18fabfbe92e489704898490442
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:19:46 +0000

    light definite pretend order delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def reverse_array(array):
+        array.reverse()
