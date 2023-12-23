380194734ba33cf56f271790a74b6b4a8bf0c619
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:36 +0000

    although exact boring stupid godown

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import datetime
+print(datetime.datetime.now())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
