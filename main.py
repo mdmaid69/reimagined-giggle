1231b1fae0cf9d980fbbd4c4e02b74c5b1fecf96
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:40 +0000

    unit black any introduce talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+text = "Hello, world!"
+print("Words:", len(text.split()))
