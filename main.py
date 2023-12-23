b782eb510111fc7d2f3199f10a22297b34010034
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:27 +0000

    recovery my confuse choice less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
