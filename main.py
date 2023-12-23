b97f028ea6102e7ed72cbaad7f48808006560eca
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:36 +0000

    powerful herself traffic shame temper

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-for i in range(10): print(i)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
