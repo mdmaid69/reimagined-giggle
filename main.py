60e001871781c5270ffde9ac11df6885a1667a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:18 +0000

    dinner order nerve grow kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
