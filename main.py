3e111920d1494536377bcfeb3c78c626ed7c120e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:29 +0000

    collection temper example easy shock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
