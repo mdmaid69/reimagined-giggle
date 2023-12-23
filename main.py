e97193b96365657e53190a4fc653a6fd3858b395
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:28:00 +0000

    squeeze breathe almost food pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
