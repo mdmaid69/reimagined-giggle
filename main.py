47f1c0c6a47306c4c2b2c74df82afab47430d1db
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:41 +0000

    phone path practise mess value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
