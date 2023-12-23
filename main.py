b465ba7f4298bf9f282fc56f4ae9041ea2a87d21
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:03 +0000

    normal sign command computer true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
