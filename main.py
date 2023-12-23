e8e876bded253fc2169d0e53f3baee9600a31e4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:27:11 +0000

    since pure giveaway stiff miss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
