20729a5f325afd399749417508c9db35a0e9cd1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:56 +0000

    minority air angry phone disagree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_current_directory():
+        return os.getcwd()
