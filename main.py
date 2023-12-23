1db44f58a457bedaeae529a0d87efcc6b967c35a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:46 +0000

    so correct professional feature recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
