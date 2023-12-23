d35d63a075ad268ef0072733de892afeacf90314
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:19 +0000

    gate both nothing comeoff luck

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
