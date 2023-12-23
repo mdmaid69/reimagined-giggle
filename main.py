c7bdf8d91c548e5563b018a5b3f6a2ebff5970c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:45:20 +0000

    smile Ii future goodbye gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
