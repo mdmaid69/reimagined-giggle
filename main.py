22434175c00a65243c3bfa87bf713196fb91491f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:41:34 +0000

    interested cover slightly ring favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
