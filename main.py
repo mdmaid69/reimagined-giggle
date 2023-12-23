1d8860c25522e813a1be04f8a335476515e8f688
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:35 +0000

    top bother consist care confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
