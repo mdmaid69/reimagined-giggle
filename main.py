599ba5564d2bbf6d3ec85904d78b85c1a5b8e0d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:49:47 +0000

    string truth nothing attract star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
