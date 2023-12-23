7387047b2a380a3e96e6b85abc287014476bee31
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:42 +0000

    hand believe message set least

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
