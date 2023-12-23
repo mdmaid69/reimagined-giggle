9931c05d608e279e43c0c0227d682ce1bae439c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:36 +0000

    dry leg realize sensible giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
