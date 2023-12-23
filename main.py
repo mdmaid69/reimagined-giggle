d71f05b57e0b185e7b89c0e521ef9b8ee83d9f9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:48:57 +0000

    off team similar return brave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
