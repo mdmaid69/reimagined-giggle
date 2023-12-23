bc1167109a0fd0c75eb90ea25af0f026d542af76
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:22 +0000

    wave thinkover call black pretend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
