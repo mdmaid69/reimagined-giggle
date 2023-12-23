753cd8745d5025f9935b90f8af46402c90497876
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:32:22 +0000

    lean trouble want difference project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_work(force, distance):
+        return force * distance
