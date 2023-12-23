a257b1d5489d8242bc65e8860a9db0b8932b12ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:34:26 +0000

    funny spite tax open tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
