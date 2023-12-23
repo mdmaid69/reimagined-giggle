6c71b48919728ecd5810feecd9d7cef4d157d6bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:08 +0000

    bread hope noisy joke above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_queue():
+        return collections.deque()
