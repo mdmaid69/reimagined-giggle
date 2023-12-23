f07b38bf6b037de87a10e0ab68501b20b68d8c3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:26 +0000

    water neat yesterday past worst

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
