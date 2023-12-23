d95723ac56b5e1c4634638b5662697a9e41a404b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:33 +0000

    unpleasant ocean trap total will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def reverse_array(array):
+        array.reverse()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
