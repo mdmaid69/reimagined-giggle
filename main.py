9521eb16f28becb128a4522124190b57221bbcfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:03 +0000

    real last add here remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
