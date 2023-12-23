1e3318f11cbdc2cc5501e2141f90f8fed152d7e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:17 +0000

    stop hungry born leg no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  def convert_to_binary(n):
+        return bin(n)
