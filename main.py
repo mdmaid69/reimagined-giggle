90a2496b4a3be970e3af5893d1fcac3c8a80a507
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:38 +0000

    undo point comeback share match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_list():
-        return collections.UserList()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
