cc15ee091aaac8a00aaf658e33a3b5544f8547b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:49 +0000

    runinto century relative computer surface

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+for i in range(5):
+        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
