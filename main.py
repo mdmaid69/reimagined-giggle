fe2037403eefc4e9f03478cf2b36062f0cff427b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:21 +0000

    above collect capital rush bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
