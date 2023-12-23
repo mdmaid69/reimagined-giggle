05a277c441cc0bfdd8bdf5fa9d6d099edc197cec
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:54 +0000

    prefer whoever until calmly true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
