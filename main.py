52828919d4197c2aec35f3c9e0ea3182bcc1bb28
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:20 +0000

    unusual pair Bb country respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
