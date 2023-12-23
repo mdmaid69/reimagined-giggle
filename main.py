30f17e4b484816d07ec6cab15b57df559a515e83
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:51:34 +0000

    block start rush fasten pencil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
