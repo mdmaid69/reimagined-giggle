85fe66996f502fa63dab6005ba72e4b5d7149a90
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:04 +0000

    field fit brown dig throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
