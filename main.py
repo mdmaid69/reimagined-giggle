c9ce96f9ccdd3e3a6da697c5d576a48d6dcf436d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:58:17 +0000

    clear shame watch please intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def divide_numbers(x, y):
-        return x / y
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
