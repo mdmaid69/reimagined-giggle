55a96b88b481c65afde6974314a25f8a2cd0879d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:28 +0000

    replace change her relax sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_list_from_array(array):
+        return array.tolist()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
