df7e2a841aa208dc0d3fc42515a1ec38efa5fe3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:00 +0000

    about concentration shop together form

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_density(mass, volume):
+        return mass / volume
+def remove_duplicates(lst):
+        return list(set(lst))
