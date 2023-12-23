04778feffab9cdfa38b0d968687d53cfd52ec33c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:37 +0000

    conclusion interview centre praise see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
