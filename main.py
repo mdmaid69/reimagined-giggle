aec25024e6432a8193a56ce28a0c01a48079f0e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:23 +0000

    deserve nowhere correction rent run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
