e0356fd0eb9e94a9d639e16a195aada29f1b66ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:09 +0000

    nonsense square beyond unit hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
