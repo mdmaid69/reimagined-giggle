a7a3a3cc2e3e4d94aa514ce642b4920a49df5f5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:32 +0000

    protest Mm part pause cheat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
