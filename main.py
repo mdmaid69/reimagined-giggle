adc448a00425f9a18f54f38aaa5cd72d9491dd04
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:32:32 +0000

    splash battery hurt brown time

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
