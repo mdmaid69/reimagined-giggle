066a42ec1c5c21ef4c257073968911088bf607d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:48 +0000

    else correct bring answer just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
