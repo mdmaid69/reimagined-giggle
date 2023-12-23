f6e4f1c878961a8b6441f2f309702a44c9f58d3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:49:55 +0000

    purpose any turnoff rid breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import tensorflow as tf
+print(tf.__version__)
