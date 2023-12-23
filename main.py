a183b115987d5ff884d019f7f62ad92b6574a654
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:33 +0000

    maximum lot busy even successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
