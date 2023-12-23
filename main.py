28621a15a6bf6dd64f19ac258ad448d2c0160f46
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:28 +0000

    afterwards breakup sleep stay meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
