a89bc42ba243cf3e0a1d7739cabcfdebcd1b34ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:02:07 +0000

    getoff exaggerate best cook before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_list_from_array(array):
+        return array.tolist()
