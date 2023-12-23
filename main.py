3f3bfb48bce44986bf20e88b073d4f9856664821
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:18 +0000

    turnout release callback platform murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
