079bc622bf662314aa90ce30fe2b8e5397d880fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:49 +0000

    Cc pair over runinto start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
