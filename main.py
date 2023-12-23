4aa349ad85178f2b840098bfcaee42aa7e122423
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:05 +0000

    consider welcome against soft active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
