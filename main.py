a0a8ffbc6ec3eb89e3b570328117477caf441f08
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:35 +0000

    shoot simple Ff copy scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
