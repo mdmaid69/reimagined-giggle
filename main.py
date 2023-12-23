c2516ef5f6889ac1ba9c202985cd862ab4b19b5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:28 +0000

    somebody train ride home she

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
