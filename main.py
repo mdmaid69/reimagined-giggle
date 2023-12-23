d54b8d64669d98d35ddfefbc1454c9f150f55bed
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:10:46 +0000

    attempt round total because drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import sys
-  def get_python_version():
-        return sys.version
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
