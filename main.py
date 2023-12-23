89512afc090ae6d8b26b55652f77bed99e3e35e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:29 +0000

    hospital bear agree calloff war

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
