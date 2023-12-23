e1a4c72dfc1488ac3961a831a7bef60332086b5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:10 +0000

    dream no score greeting treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def find_min(lst):
+        return min(lst)
