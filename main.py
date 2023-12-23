ff321a56f17d1e74804adc51e4d3a7300816982e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:45 +0000

    treat time table phone type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
