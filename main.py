7b5277363fe6a242dcff1408d08ed7cefee92856
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:54:01 +0000

    official lucky mess information plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
