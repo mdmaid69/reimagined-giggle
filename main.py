da0fe340df63d020d2320d0514f56fe9009c0683
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:58 +0000

    neck listenfor partner cause telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
