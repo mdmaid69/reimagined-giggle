d83de4ff1b8ad14a7cb85bfe6b4c86cc0c54b1e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:21 +0000

    stamp heat in basic content

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
