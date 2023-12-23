3d13f622161268b2a4ca1471c212ffaee740b9d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:56 +0000

    reasonable stage tongue patience deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
