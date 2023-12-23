bd481452ae38f1b0b27be0394eeecaed01eba560
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:33 +0000

    comment bar wild rest back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
