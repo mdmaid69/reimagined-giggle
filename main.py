5a132d572fc5381b1d6429027330ce36c646ec4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:25:00 +0000

    accommodation nice contain prefer into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
