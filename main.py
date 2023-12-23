ae4a2aad6cfe8ef685a4fc71ec275f4bbf1d188d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:29 +0000

    Itself rank pool takeoff gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
