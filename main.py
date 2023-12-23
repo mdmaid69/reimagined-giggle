0ab25450b94b9c673fc51d53fec4f89ba8e59736
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:56 +0000

    step hard soak 568energy lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
