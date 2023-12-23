37efed5afdedb9cb81f1f4adaf9e78af60f004e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:42 +0000

    proof spend Bb sail use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def remove_from_array(array, item):
+        array.remove(item)
