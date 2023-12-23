e68941651c3c69e3ef7043065104d4f63c9dc5b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:08 +0000

    right pupil outside himself guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
