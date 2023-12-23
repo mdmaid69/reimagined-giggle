e856cbede2d5d1f38f73ead0afe2f3bd1b2fc9e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:41:19 +0000

    design hope dinner search continue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
