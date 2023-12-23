b1c16a7a1f56b2c5c2506d87de392dff71ec0f33
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:02 +0000

    earn shut accordingto may team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import matplotlib.pyplot as plt
   def plot_graph(x, y):
         plt.plot(x, y)
