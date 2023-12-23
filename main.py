9021f72e8cb358304af4b52bb6cfee4097f132f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:52 +0000

    sting noone rescue similar behind

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
