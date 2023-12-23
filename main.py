7137705c7ecefec3d96b97decbae60808d9c0e7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:58:45 +0000

    fortunate careful none test beneath

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_ceiling(x):
+        return math.ceil(x)
