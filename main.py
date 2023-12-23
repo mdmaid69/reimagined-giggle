9233944004106e958c64fa5ea6013c37957c24ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:09:45 +0000

    fit commit approval fall somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
