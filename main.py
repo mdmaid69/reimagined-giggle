4b7d5ae884475251f6a409e720029473ba876117
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:14 +0000

    what valuable left too capital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
