7d0f427d9b46fe7e5f7702f24b40102620cb5491
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:18 +0000

    know face apologizeapologise high trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
