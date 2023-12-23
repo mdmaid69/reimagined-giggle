2cbe26077019cc521bf3695f122d42c9ef8e26f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:04:33 +0000

    hollow blame even less software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
