ed340ade7456f816b12eb3d5defae253931f5425
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:56 +0000

    home round zero show every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
