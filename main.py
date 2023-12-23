4c61305510dbb4abb9b4b077959c26d72440fccc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:02:11 +0000

    bury hour should prove yours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
