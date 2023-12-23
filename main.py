b899afda12ab95fcf56eec0f59b5b68a0f6bed4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:42:45 +0000

    speed read steer weekend score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
