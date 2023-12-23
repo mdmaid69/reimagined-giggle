0c3f149120745ce5b33e9ba93e674b5e32ec2dc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:23 +0000

    expression play agreement sticky bringdown

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
