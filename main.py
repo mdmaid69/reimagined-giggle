75af55a2d94e47dcf313507b165957f3206cb89d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:23:08 +0000

    soak machine enthusiastic bet calloff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
