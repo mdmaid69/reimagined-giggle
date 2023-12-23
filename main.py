cf694e07f0aaf1ca330d3e39833d4220bb7fd001
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:04 +0000

    either direct meal crash suppose

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
