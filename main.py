68a1fba47f9e8581b41467b8d2f030fbe28adc3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:05 +0000

    occasional interested like raise survive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def divide_numbers(x, y):
+        return x / y
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
