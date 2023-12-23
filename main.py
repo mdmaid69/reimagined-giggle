82526cb23b4b17660fb66fc9d613081d140388f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:29 +0000

    replace borrow holdon division finger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
