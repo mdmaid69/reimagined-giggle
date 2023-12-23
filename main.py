00e0ac0c4a62922bfa55af948c9479375e81a72e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:40:57 +0000

    giveaway fault mad cap meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
