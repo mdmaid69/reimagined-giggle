a1245f08828e1480e1ff769a629725c5ca714277
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:08:18 +0000

    bang wait heart excuse persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
