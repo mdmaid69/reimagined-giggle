d59d15e59eb58410e66bf1bd8b3b2a03f3e98814
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:52:27 +0000

    road open only Mm level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
