5bad1e87cd604698ce25631cc9c0e51e87de41c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:13 +0000

    during job middle loose represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
