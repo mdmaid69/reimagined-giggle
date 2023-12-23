d91c854dccfe979dd2d06d058a415feef92bdec1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:40:34 +0000

    afford pause keepoff gap level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
