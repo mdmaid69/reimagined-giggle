9473b8703c7f90147d0d63bc09a2007c4328e2e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:57 +0000

    pipe breakinto godown tragedy official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
