6e2b3729e1dd84f963b40ad107fc1491e46f2392
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:32 +0000

    evening create claim direction disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+print(os.getcwd())
