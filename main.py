cd7a81585c7e1a50ff3ea3eb934e164b5436371f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:03 +0000

    substance cut drive claim glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
