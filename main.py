b78d595dcb03f6f6ba2c9a411fd442e34c8ce936
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:35 +0000

    sweet damp everywhere practise window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
