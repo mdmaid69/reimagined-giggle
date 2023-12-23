300a9c93218c25a90795f331b4743ae13f8c1c90
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:41:25 +0000

    oil glass determined prove race

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
