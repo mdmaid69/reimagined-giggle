7d1de036b63449ad7b0e23e5237b35a741f015a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:32:14 +0000

    spot minor operate open lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
