aed13242d09be869d25240dcb2cd71f0176b1044
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:40 +0000

    warm catch exploration shoulder today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
