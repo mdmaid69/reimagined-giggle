a631b5ff608f0fdf999537dcf2aca69a6258814c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:43 +0000

    pause disturb upwards underneath lift

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import time
-def get_current_time():
-        return time.ctime()
+def reverse_array(array):
+        array.reverse()
