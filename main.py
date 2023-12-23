7375ec80c8eb600396a591f6eadb17d6980bf49e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:15 +0000

    bye wet hurt rule size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
