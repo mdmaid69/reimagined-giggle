fb47e626450516439628dedf6d51fbd551c988fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:34 +0000

    risk treat theirs name nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
