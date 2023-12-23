29750a06bdbdeeffc514da68d637ea4e260e0c8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:17 +0000

    lose safe frightened point ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
