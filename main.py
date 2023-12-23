1a0fbe91851d0cfecf3d6792dcc22206e65f6023
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:06 +0000

    snow Uu rent stamp pick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_base_name(path):
+        return os.path.basename(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
