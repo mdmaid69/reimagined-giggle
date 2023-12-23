28eba7df79bd4b61aaee05c4e0f3bfdb5a6ef415
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:47:06 +0000

    true press away drink of

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
