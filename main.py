f948539f0c6fd45bce5f47c5d587d5a8aeb767ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:37 +0000

    share her back recent Vv

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
