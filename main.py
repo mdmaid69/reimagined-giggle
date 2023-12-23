62ac929f2080c40daa1db9a0fd4f253572466e55
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:02:28 +0000

    something place nowhere sleep takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
