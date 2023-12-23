684248fb4fe4b448f7e320bcef7898107cad28a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:28 +0000

    here under today home mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
