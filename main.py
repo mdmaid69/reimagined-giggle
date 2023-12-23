ee2a84500e12f55105f076e2076a2872c08298c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:07 +0000

    often meaning alive skill spend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
