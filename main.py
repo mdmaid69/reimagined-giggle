1eb830e0bd4580ba26890b7bcfed2dec222db875
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:41:49 +0000

    broken cover thankyou above convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
