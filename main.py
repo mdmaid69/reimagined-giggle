9007055a87dfbefb3c2ab56411c7d000e1c68ef7
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:16 +0000

    Yy search development talkinto each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def count_elements(lst):
+        return len(lst)
