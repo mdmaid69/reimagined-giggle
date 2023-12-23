cbb6dddec3e5d2b241a17cfe8d17e073a7839f8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:09 +0000

    treat scream across extra shop

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
