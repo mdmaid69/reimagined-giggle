172d5fc1e06d51d8426cd48a14787a0c9a581ef3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:10 +0000

    OKorokay lesson nor earn freedom

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
