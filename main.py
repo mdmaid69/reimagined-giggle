234867b601260e39a607ce0627b61de9bc15e5c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:39 +0000

    plenty contents left unfair cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
