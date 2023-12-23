3ebbd83b4d3b67036278ac888edb610f8bf84b28
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:42 +0000

    badly bother rule switch secret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
