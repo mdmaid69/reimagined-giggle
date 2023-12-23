b28fce6c0e9b27777c08e8aad5c7f5805410a1b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:25 +0000

    lunch rich whoever confuse careless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
