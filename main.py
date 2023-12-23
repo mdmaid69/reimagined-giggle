360cd42417a91c7fe2179373d91add97eb83e281
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:20 +0000

    rare dry little miss present

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
