64a845990121334eecada681bf87bb3a6de7ba85
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:33 +0000

    hungry exploration doubt rich wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
