83c39b834693c5f26b2cf5815913a2647fda4ba0
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:29 +0000

    question Dd fix sudden exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def count_elements(lst):
+        return len(lst)
