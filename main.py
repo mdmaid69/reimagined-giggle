7aed205fa29f518dbeeb7639b5e0c404b3fb4176
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:07:07 +0000

    world possibility show week tear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
