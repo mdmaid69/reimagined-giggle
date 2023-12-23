cd544724869d47dd77971952f4a475da1192ff77
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:09 +0000

    add mine work ocean load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
