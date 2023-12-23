b8b11c326d60c0f7ca68c2e4c38a2291b711762e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:00:11 +0000

    he swim answer communication cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
