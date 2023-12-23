aeec3edfb79c38f58ac31a582c6198fd1daa0125
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:40:46 +0000

    present consist town accident path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
