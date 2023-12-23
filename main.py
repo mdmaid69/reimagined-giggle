489ad1cb38a3e8de11e167882f45037c4ac45d7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:11 +0000

    Ii runover thankyou produce downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
