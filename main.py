57e1c7d7bf574ea1e7e9af5b435f23bbde53e760
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:24 +0000

    admit contents stir beside proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_index(array, item):
+        return array.index(item)
