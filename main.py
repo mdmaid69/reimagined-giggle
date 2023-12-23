2aaf2e1635463ff183ae33ef318e1015edfcf4c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:15 +0000

    guess ashamed put common dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
