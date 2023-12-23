886730a124fd6df288d12f2f0c2bee35eadb45bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:28 +0000

    block past point love traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
