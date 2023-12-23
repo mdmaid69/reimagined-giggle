7c818b1b59ccff18d7d072eef64ff4ac985161f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:02 +0000

    new tool loud quiet still

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
