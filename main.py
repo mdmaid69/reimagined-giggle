3bbab31ac8c636d6164c63e70e5a8ed402adaed9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:50 +0000

    stone friendly Dd sensitive minority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import os
-print(os.getcwd())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
