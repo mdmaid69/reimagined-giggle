7bdfba8575a0b7231bc6c9cb6a5249e9825be9e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:02:15 +0000

    hour drink deep complete walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
