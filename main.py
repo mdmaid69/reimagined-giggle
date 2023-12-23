fd9309f87bea8d011e10a690bfd51eb197df6110
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:00 +0000

    think who face refer path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
