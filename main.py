57c8504c36043fd311dfe4bff2db9eff5d9374c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:39 +0000

    hungry mainly learn at wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_min(lst):
+        return min(lst)
