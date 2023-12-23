441810ef9f17b0fa1f39b5085bb0ec30808c9647
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:32 +0000

    yet steer convincing arrival quantity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print("Hello, world!")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
