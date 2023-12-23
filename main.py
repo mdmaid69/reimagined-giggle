3fb73fbd55fcf03b746145cde0f6dfd5717e9134
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:36 +0000

    belong apply everybody regular feed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import platform
-def get_os_info():
-        return platform.uname()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def square_number(x):
+        return x**2
