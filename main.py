d558df8b69736416e62c05d1b42b0786fb820ab9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:49 +0000

    breakup strong display arrival sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
