452aec3cfc7a895ccf406b95f157fe41f82dc7c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:07 +0000

    involve plenty reliable share damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
