087240c40c1832b4f409f60fe5f5b892957b9b14
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:43 +0000

    shop speed principle forever blow

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
