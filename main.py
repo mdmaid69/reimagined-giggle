77b5775fe5d78fcb9d22411b7cb456db8c5c2670
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:26:11 +0000

    getup fit fix none put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
