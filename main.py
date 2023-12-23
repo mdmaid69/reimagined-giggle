c93f6ffcefe1bdbf1c4d46830f6c0b75c7cbb4ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:10 +0000

    style correct care choice takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
