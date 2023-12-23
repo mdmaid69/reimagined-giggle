77647a40cad60a69081252e951b089c6627efa1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:21 +0000

    activity call turnoff adjective bother

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
