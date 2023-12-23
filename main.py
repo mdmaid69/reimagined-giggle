4a11d11d944a52acd9421f849f38f4dfba4da0f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:46:18 +0000

    bend everything might frighten decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
