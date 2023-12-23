350a1117b00baeaa9942f2e56452bdd9a6cbec42
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:32 +0000

    think bottom watch case lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_min(numbers):
+        return min(numbers)
