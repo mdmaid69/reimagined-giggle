6c13586bb592a37bdc6c13267321150f54441d1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:01 +0000

    that custom relax row watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
