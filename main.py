2d518573c93ca51f1b9ccc74fee99b09e05102c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:52:29 +0000

    principle fairly find pull travel

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
