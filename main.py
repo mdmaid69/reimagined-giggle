e603c74c9674530e3c55707c0b462c5ca5fc5e26
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:45 +0000

    if skin anyone turnout fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
