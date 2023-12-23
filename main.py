1a88d081c699aa31fc6ece26f15fbf3a8a249137
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:41 +0000

    mark march listen set reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
