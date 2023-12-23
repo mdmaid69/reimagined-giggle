a626a7943d768bb157f0aa4295d70483d738a70a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:59 +0000

    but natural pattern trouble education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
