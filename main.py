5586684a408ae224e40332ec7061d6ba692a2fd9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:57 +0000

    another sight say pure dance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
