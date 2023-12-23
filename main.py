e9ab56679b224865823acca6c4ff5c2f74393fae
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:17 +0000

    Rr shame wind warm mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
