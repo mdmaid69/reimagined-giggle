c5e863da9a9a0c6570a0d3c2f2eb586714d46c3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:28 +0000

    male strict dull remove ready

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
