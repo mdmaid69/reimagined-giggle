7d234545d500f1c8baff827392c6644cd5b64ac2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:43:00 +0000

    its face pass claim female

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
