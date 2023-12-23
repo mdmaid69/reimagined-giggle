07ebbb46a5d6ee32f6a3322c3c055f335c687eef
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:16:31 +0000

    herself fault even custom chance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
