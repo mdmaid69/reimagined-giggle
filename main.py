82e941a4e5b28233e4ce4d1d7da429c7a0ecebd9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:51 +0000

    advanced pride plastic immediately belong

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
