20327ffbb482bad71b05de8daf84d9e7d7e753a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:21:54 +0000

    brave public study zero mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
