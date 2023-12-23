39fbd9c179e651f117b8f6a48f4f30d899a76fbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:53 +0000

    sink maybe object content tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
+def greet(name):
+        print(f"Hello, {name}!")
