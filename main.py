1921f6b0ccc4fff58109e094f65cf1769700e7f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:21 +0000

    forget difficulty tap shopping information

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
