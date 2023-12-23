5779cfa1d98f056d59bbeb2c4afb4db76eb0420a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:04:21 +0000

    truth woman bottom original convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
