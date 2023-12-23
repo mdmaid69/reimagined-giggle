e6627d28e7fde2cf94378b2f05849fc27d120eb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:16 +0000

    treatment match train pronounce otherwise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
