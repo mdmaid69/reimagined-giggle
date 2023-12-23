7f7c2d9f69532c5ff174ce406d0e6b2a6aa75fb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:50 +0000

    glad circumstance gradually flight meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
