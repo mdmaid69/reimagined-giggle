01bec932d5554ece496c6657463f74fb1f233867
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:57 +0000

    however cold harm freeze attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
