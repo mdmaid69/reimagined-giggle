4363ef3062c1e37f1a46114f2adfce8e9bace6cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:54:07 +0000

    new capital too send join

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
