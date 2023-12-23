53f8b3dcadeed0313837559d9fa7ee4fd01f941d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:25 +0000

    left bath challenge mistake towards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
