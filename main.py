9a26f57eda028273ade66e57421814529bfca7ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:53:32 +0000

    clue collection fear whistle effect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
