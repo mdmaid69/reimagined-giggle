e44014e00db898fb197724b9659ef9c8a58167e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:58 +0000

    interest free advanced extremely wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_index(array, item):
+        return array.index(item)
