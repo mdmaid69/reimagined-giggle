da7317cf623551faf621faec5b3c15870d146653
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:23 +0000

    bye like necessary comfortable unlikely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
