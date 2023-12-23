7321769dcd716c8d42db9151373414ecd859b222
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:23:46 +0000

    perfect time improvement reasonable manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
