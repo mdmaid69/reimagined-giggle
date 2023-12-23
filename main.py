e4bafd99f8c546d0606240e7d2d2c527d5d4b6fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:38 +0000

    butter fill squash limit tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
