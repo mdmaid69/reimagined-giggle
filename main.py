6b2e3ded3e73d3d4b4db4232d8f88c7c8fce0672
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:56 +0000

    chief injury find keepup skirt

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
