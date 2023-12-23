f9d364f125fe414cac7b68687a19a248b56e6f73
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:28 +0000

    develop complicated fat argument performance

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
