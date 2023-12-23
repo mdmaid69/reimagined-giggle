ca2256496b2df6d956053afed5ab9cfb40cce9a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:48 +0000

    uniform compete against terrible lost

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
