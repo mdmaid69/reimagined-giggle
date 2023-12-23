30e03cf39f453ec6896cdcb74a523c7b2270b4e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:28 +0000

    software form pump collection high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def convert_to_octal(n):
-        return oct(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import sys
+def exit_program():
+        sys.exit()
