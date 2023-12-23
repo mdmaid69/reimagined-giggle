688d1d2da2b150db0acf0b1ff8ccb542c8360134
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:39 +0000

    566end stand constantly record acceptable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_typecode(array):
-        return array.typecode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def convert_to_octal(n):
+        return oct(n)
