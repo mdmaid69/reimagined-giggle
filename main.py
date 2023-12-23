6b7cc1ab0f71e35c41bc000d61cc7dbfa4042196
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:39 +0000

    calm usually search previous plain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
