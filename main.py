c0ec2594d3481e6181a1be290c568a443aeaa98f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:18 +0000

    beside wild flow preparation many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
