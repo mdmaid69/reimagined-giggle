a7872408f5ca3a20f341b9ac74e0fb52e8ee5be7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:17:59 +0000

    dick division afford blind cause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area_rectangle(l, w):
+        return l * w
