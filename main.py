d7f9fd8b5e4f476aa379c6cf22f8768f473716d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:50 +0000

    slow clear spoil system score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
