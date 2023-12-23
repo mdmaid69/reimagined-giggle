ea35b8fb235eec30b53459528e3f7412c62c80d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:14 +0000

    roll worse many 571enough again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
