a10f34c20a2430e96ea016f9ce2066cff62e7fdc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:33:54 +0000

    tone sort increase danger slight

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def convert_to_hex(n):
-        return hex(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
