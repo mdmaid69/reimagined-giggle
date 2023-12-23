bbb09b9782f3f73ddaab390c3de9f38f184d165d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:53 +0000

    if mark yes rest trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
