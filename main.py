eed2f660092550addd5c0890cb3ff41e99caccaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:50 +0000

    whichever birth that section accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_power(work, time):
+        return work / time
