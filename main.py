77d3956e39a86babd23569611d312ddfbe2157a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:32 +0000

    test once taste stage opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
