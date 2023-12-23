32b7885a4226227ff72d31725f795cb7178e1f34
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:32 +0000

    workon shock appear now embarrassed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
