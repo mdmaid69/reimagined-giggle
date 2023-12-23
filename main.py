0b8ea1effd70310ff4f744333466b44989ac62f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:26 +0000

    suit year pass correct skill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def reverse_list(lst):
+        return lst[::-1]
