4be9712f488c0b04c7b73bea9d32964ba053b10d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:41 +0000

    murder tone week sick absolutely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
