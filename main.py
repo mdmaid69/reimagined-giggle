2b19a84dbe686b99dd63e8c9e148203184d1c664
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:42 +0000

    debt nerve likely best evidence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
