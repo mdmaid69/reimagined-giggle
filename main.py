cc1f19ee41383e70729ed60a5eff7c4abeb71724
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:30 +0000

    exercise pass swim post important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
