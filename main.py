c988243182eacdb5b23efd83099ad7af8992cff5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:20:07 +0000

    example children rise talkoutof action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
