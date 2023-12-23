d978e39b3fe819a5d03b3b6db1bb5e2bcc54f392
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:30:24 +0000

    exactly condition callback birthday hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
