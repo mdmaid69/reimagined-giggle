0041a1acb6bbb8de746e11502e0f50633c7eaa9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:57 +0000

    prize walk recently who Ww

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
