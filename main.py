aa605247f20f6c03a13480705a2b1d0f430b7453
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:01 +0000

    lucky either share 571enough attitude

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
