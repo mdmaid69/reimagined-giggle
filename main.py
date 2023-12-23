e183fe0a1e5179f94e1905c763aaa78adf2bf192
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:30 +0000

    software not careful happen sort

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
