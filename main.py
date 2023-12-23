7056b97bcddc812a75a35d14f3c3f2cd80108ba1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:31 +0000

    team sometimes fly pattern fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
