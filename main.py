1800a9c78173b70eeafef2463d805e4dbc8372d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:40 +0000

    block exact good worse off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
