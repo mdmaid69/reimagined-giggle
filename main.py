f89fe0841aa1e62afb48240bf087e9cde395c31b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:19 +0000

    full he consist please measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
