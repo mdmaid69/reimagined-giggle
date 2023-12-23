1600934a7662b06763eb935345d55e6a88cc8c9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:30:56 +0000

    holdup know project pronunciation class

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
