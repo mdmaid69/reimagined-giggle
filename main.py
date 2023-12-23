c6a0719a7dc7674ce5947c5968f1179f848c8030
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:37 +0000

    separate contrast usual instant she

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import sys
-def print_python_version():
-        return sys.version
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
