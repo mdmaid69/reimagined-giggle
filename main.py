eecf58cc6ed38efea95ed29ed1566608a1774356
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:33 +0000

    giveback schedule pay and want

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
