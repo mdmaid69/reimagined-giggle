d340a1076b846ce2aa4c3f3b78e1bbb4bdd084d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:05 +0000

    in runover off tap impatient

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import tensorflow as tf
+print(tf.__version__)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
