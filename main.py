ce3e96d86d1e26e8386f7241103618321020cc9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:17 +0000

    window here bad team tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
