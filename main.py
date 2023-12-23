94e3a485cc4d74f1599d32480bc5fb91534a864b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:58 +0000

    regular work space interview Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_queue():
+        return collections.deque()
