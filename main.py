58755ccd27def2a201a05632bec74551d8621921
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:25 +0000

    fix suit house flavour niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_power(work, time):
+        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
