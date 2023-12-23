76dbacf35b2b68e7496d12c9496038cab0c94b63
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:00 +0000

    smoke leave strict swallow runaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
