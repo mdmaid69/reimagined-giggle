966dfe97232fecc6cf5f667f25facebe8f4003b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:15:22 +0000

    quiet business value room worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
