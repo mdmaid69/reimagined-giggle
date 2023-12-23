fb0d30dadcffc6ab6c2f96f2cda4cceda5b99170
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:24 +0000

    upwards view cost consult cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
