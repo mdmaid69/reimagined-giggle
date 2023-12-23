1465032c7c76c0cee0ebbf14de8d35d9e529651d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:57:43 +0000

    mind interest mysterious fasten connect

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+i = 0
+while i < 5:
+        print(i)
+        i += 1
