1eb7f571fae67d80efb365e35852decf3f849443
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:05 +0000

    familiar capital relative morning mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_typecode(array):
+        return array.typecode
