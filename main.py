65172ec2043b67d818d12ff4da05ce7435a68eab
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:41:17 +0000

    lend thirsty top offer and

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
