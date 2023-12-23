42a05598c4fb8654d63a729aa3dd7f1c4081e1f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:21 +0000

    path ashamed neck careful soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import re
-print(re.match("h.*o", "hello world"))
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
