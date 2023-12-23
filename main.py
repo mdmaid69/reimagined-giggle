d00573e2d7d1f25a26625768b276382ab15dded1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:54:10 +0000

    its pocket contrast burn recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
