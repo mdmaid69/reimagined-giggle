d34ff74115aee1de42e2ab126a5283bf7353d4e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:06:01 +0000

    reduce before rob Mm unfair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
