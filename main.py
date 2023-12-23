ac47db96162aa2bf87193244d285ab6bf463dfcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:46:31 +0000

    stone judge interview anxious team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import datetime
+print(datetime.datetime.now())
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
