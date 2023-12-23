9d923bee08fe9cd966d3cda56e300300b18b0f1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:28:54 +0000

    compete Ee break foot tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
