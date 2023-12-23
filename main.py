672588e4c87164af6f5568d5a8e9d9e4958ea22c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:52 +0000

    close length shine score total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_list(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Words:", len(text.split()))
