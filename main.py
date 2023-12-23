66c9617f61dd3e12222219de48250e5dcd0c992f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:23 +0000

    straight achievement agree open trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def square_number(x):
+        return x**2
