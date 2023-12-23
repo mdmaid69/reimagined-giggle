c46b61fcfc0bba55ac53ea744f0bf22b4455d0bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:54 +0000

    kind ask outside issue everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
