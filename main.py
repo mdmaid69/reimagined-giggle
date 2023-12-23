9dc23489b18edcda7c51e0e9c8851785e2064cd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:12 +0000

    sister discovery ago process make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def add_numbers(x, y):
-        return x + y
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
