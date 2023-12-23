0ac51cc5ec47198e68bcf5844d2c410c5f70fd1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:35:05 +0000

    pump luck it use upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
