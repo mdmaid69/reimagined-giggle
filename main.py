c4ccd8dc2a14cfcdcc6a92d440f5525365e341a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:50:35 +0000

    later branch forgive play journey

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def find_max(numbers):
+        return max(numbers)
