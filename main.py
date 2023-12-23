e1e757130d6a553747515908cdbc0359bce074f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:48:57 +0000

    entrance question attempt community top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def remove_from_array(array, item):
+        array.remove(item)
