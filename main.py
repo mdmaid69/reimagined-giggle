eacc08556f3e12777389a8042f7fd234264e6459
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:34 +0000

    expression dress need congratulate different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
