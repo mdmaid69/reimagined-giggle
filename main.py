62ddd270aeb2df547ed652e7c38e76054d7e9ac2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:35 +0000

    harm anywhere takeout frighten harm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
