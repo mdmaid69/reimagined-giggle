6939d88438415f62bd7774750e0d3a688ad9b16b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:31:22 +0000

    any charge record communicate worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
