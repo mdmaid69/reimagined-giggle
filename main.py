e8952b5ac38ecddbfcf86d26922079391c47ddba
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:21 +0000

    blow main weather community within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
