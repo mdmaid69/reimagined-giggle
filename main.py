2d4df834f06208422ae1382f2780b3746cfeac33
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:06 +0000

    far pile single move short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
