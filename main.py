a5f0844c5aeeffbc397911c9d7faabe51f608a06
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:35 +0000

    567enemy put sister solve centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
