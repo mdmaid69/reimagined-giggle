1977e348796b15b6bac5d2da23e17d0efafc9915
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:36 +0000

    attempt shop regard balance conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
