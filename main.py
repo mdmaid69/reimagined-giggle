441ab1f27d939890dffc044b978552cdc29eb1a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:27:52 +0000

    luck trouble tidy sensible mad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
