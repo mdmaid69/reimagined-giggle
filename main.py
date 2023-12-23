96566cad028d78f117e452476e8a3ab02978bdd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:50 +0000

    help arrange appearance against light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
