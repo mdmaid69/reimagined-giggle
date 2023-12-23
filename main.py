855c5b36e52806a972644362f677da62a3cc4699
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:54:57 +0000

    attitude that improvement want frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
