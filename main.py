d76c28080dcf74bade6f5a02ed54bea7e6b8c04b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:50:20 +0000

    environment fine but large tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_list(array):
+        return list(array)
