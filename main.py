51bce94027d0c9e0d2cc82a70ff21a979f0a558d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:02 +0000

    frightened frightened turn how window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
