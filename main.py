c56325c6bf0186df0798cb3759f9e1cdd1e01540
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:11 +0000

    loss whenever need closed convenient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
