f56cd90e6abe0859d5e2f9dacd8532fa133ebf2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:06:33 +0000

    across float print bound catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
