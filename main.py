2507c5477f01f3076f9b8837757e01593da17f3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:04 +0000

    roll separate lookback(on) abroad somewhere

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
