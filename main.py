c1ab3d280bfb094fd53395d3f0dfc16c595d7ae7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:56 +0000

    grateful move ought female nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
