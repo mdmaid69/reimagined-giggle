17669752fabe3a370e1a05c8970453d4fbe4d8d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:45 +0000

    music lose run smart chief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
