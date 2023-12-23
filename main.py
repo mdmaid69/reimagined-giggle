a441ad704555bc2b3dc4995a87c29a2405615494
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:03 +0000

    spend load choose unpleasant think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
