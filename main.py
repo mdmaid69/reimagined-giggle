a663743d3bb0b78903bef85fde7e937069fb215c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:55 +0000

    first squeeze bet boil prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
