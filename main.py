eb9300a168c7c2c0d260dd236f1e81ad1a7ca969
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:34 +0000

    ago proud pain confused bite

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
