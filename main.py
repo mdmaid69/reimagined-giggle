485d9390c38140319699d545ebeaff993f664c0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:11 +0000

    dream preparation bit stamp face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
