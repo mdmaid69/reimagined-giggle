ebdadb7585f0c68ed2064a14557f6d8a8d60b332
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:55:15 +0000

    any check consider rent mind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_counter():
+        return collections.Counter()
