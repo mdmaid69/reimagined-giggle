89e2743fa22e332264e723c0e52191ad4a015da4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:39 +0000

    patient turnout book Tt 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
