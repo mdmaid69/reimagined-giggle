a8b402d60447e63241131a639d560765507a71a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:40 +0000

    suggest pity snow supply original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def is_even(n):
+        return n % 2 == 0
