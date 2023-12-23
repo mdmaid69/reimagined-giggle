97372454de75750196f608a50b261d8836eb3f00
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:09:52 +0000

    lucky passenger check shape high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
