fde2c906663d62cbd9e8d21e9f339a5f9c5a42af
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:14:53 +0000

    neither answer fall uniform communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
