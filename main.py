3121c68d9fc6d09b5d8024516a0218b0104e9074
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:39 +0000

    care point sort lookup uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
