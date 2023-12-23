bd00f951d9cba33735026956003fdd9e94ca20eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:36:34 +0000

    Vv turnoff lookafter more responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
