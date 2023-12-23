22a888c646a21c6774d03f5b1c383bd7c7db23ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:40:03 +0000

    attach world release split ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
