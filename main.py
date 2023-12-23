09a64fdbdef16eb7cbb5c8206e8367d2198422cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:02 +0000

    fortunate team report blame satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
