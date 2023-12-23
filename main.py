edb84c2a3990d57fefd48913eb1c4ddc08b175cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:40 +0000

    embarrassed storm value independent convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
