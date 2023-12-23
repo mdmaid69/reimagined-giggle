d79d171372d0d98eb2cb2ff3f0cf99af1c230bd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:58:55 +0000

    over outside cover scared difference

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
