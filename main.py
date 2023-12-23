867cdd62e10d40b9e38fc8d36498c3e014b2a9e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:39 +0000

    various defend conduct mistake television

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print(sum(range(10)))
