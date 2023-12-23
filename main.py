16f117c352e1caacc5dd01fd812b3fc8c27d7644
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:59:20 +0000

    sink study fill frighten ours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
