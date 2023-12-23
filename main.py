80bd79619e366a9e15009e962fded538ab683596
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:42 +0000

    comeup behind in great unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import numpy as np
+print(np.array([1, 2, 3]))
