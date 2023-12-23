a20377d353335dcf15daa5aa529db42a9f4831ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:02 +0000

    570enjoyable score full interested backwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
