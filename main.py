8320d33adb2b262392f349f09ed5ceb4934204f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:52 +0000

    near ball shelter mess ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
