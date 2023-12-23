569bb52a8934a8dd37122c0d8c158b45720532e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:41:39 +0000

    rent treat watch shopping at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
