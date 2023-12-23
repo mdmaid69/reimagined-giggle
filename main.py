6d827b24a68ffa378e3d5bddffdcc2ab890cbee2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:52 +0000

    certainly rob scream shop gather

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
+def convert_to_binary(n):
+        return bin(n)
