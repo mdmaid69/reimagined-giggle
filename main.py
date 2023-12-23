437f2d75dedce7efa5447ef255d13dd6ba57ee54
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:30 +0000

    complicated month sea after delicate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
