ba9d6e51742e9bee176aba6003ac7fd29b780f01
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:19 +0000

    serve unkind example score solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
