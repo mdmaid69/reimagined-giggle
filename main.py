80b8c17c9e1e2dcad4dcaa027f6af7592e478708
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:57:07 +0000

    sorry draw store cruel national

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
