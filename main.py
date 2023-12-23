47b318829e22f72a32bf09d6583983d29e39b099
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:02 +0000

    service warning properly include unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def convert_to_octal(n):
+        return oct(n)
