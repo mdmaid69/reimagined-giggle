00f9df38c2b086c9302d2d1cb16fde69c6c6b589
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:49 +0000

    positive again 565end will recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
