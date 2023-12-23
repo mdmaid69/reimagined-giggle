3465d51e4a9abda9d4aa534d6b5ca3ac4189df29
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:49 +0000

    office round account gooff capital

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def cube_number(x):
-        return x**3
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
