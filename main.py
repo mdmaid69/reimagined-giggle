c181f2b82295624fdf40416e7e7e86ee14a1d302
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:53 +0000

    story cut cheap about careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
