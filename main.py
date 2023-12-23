fc5c7c270b9578922a2edc4f377360d5c60d8479
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:30 +0000

    information discover several shadow correct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
