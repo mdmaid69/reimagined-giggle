37df75d76a30588d79249d2ed8eec81113e6d15e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:49 +0000

    path trick brother suggest game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
