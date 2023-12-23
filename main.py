508b297eaa1f434fcafce8febe13af630cca2211
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:45 +0000

    live build risk length make

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
