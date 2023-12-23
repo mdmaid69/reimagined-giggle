30e76fd6c049019ff80d1895cfff98b28d4e3859
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:26:37 +0000

    thick forward honest flight project

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_floor(x):
+        return math.floor(x)
