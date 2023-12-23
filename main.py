308d018416e3ebc8c4e8c94c8108bb50beabb3a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:23:02 +0000

    century choose normal drive father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
