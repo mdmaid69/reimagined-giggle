3c77ebf06b513b9ab44493c5a3af25c85a9796f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:45 +0000

    mine trust trip even introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(numbers):
-        return min(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
