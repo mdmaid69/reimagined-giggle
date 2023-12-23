2788df1293531d7c482f9e15ce924fe5327f0fd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:54:28 +0000

    hear steep high certainly previous

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
