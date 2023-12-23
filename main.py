75f052a8793ce2cbc9047b28b8366825b770fb8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:06:52 +0000

    takeback minor cut happy touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
