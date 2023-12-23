b309088beb20878c8dd56281212e1134fe514033
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:26 +0000

    stand careful fetch tune Tt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-x = 10
-y = 20
-print("Sum:", x + y)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def convert_to_octal(n):
+        return oct(n)
