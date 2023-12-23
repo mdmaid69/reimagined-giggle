a40f83f7b7a9dfb76aafb8206d88cb7c93a128ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:40:25 +0000

    minor point careful win passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
