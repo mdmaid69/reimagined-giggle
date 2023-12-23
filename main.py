f2875d0686d301be4627164460afd82eea006fa4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:58:04 +0000

    turnback serious instruction only fold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
