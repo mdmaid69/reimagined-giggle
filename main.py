d7beb79dc76431f8b0c56034b245577f4ce1fa3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:42 +0000

    give pay long clever sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
