4f775d05ec1759b2a1d0ed6e05c275e3ffaab7c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:31 +0000

    present plenty risk successful rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def square_number(x):
+        return x**2
