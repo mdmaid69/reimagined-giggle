5d1f4778a726bc805950dac2c5e4498005169fb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:58 +0000

    sweep pity opportunity convince front

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
