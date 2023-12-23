aefbe2d763c28341c15f241bd33c3dbb20a6d18e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:07:35 +0000

    soak true entrance by heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
 def iterate_over_array(array):
         for item in array:
