4be194c26c1a9daeaf6959682e75aa8d83e93427
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:51 +0000

    calm worse pool rise agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
