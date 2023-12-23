b6b7e44ef9c468c12701e6baaa561484543308f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:53 +0000

    draw middle succeed another equipment

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
