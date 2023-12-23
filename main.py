dcec1cd3fb3cd75d77bfd9e2fe8f0b21fb405b55
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:32:32 +0000

    uncertain late freeze free reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
