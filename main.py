f8a89c3edc9e92c03a8190af486ebbdbe9d33672
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:29:11 +0000

    pipe lay concentrate respect unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
