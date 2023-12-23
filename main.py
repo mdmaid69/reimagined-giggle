3fc79d7876cc0cdf37809a804bee55f771a6278d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:49 +0000

    load care date upstairs charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_repr(array):
+        return repr(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
