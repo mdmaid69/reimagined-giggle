58e7ae82a19dfc9cf30149db2e59d1ce100761c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:25 +0000

    never test ride underneath square

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pressure(force, area):
+        return force / area
