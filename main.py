ab53d73f5752be99b69bd73191c5cd36b0676a55
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:17:40 +0000

    telephone nothing offer over goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
