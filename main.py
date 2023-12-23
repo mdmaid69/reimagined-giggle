7cea424bd40875aac34b25a2c611047de598bae2
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:43 +0000

    tough flat snow whistle miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
