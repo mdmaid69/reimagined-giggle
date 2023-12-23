1119b3cebf9b3ca552f2ff61512102b90a51874e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:01:25 +0000

    air reason formal yourself comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
