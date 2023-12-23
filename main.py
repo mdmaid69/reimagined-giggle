217a876baaa708bb455491ebea977756fbd042e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:39 +0000

    surprising ignore plan ours them

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
