9a9b1f87d7466d1d79a779724be845e155e7da47
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:33:51 +0000

    border 179bite operation cook mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import os
 def get_current_working_directory():
         return os.getcwd()
-  def calculate_area_rectangle(l, w):
-        return l * w
