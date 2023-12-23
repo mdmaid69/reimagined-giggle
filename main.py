b5f9d346e3ba54f9ce8cc1b1e175b83e7c461b89
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:21 +0000

    minor quality recover the sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import datetime
+def get_current_date():
+        return datetime.date.today()
