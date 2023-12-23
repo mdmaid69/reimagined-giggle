fee4c6ce1e1f471b007da0dbe85685dd7fb8e9bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:50 +0000

    uniform yourself runout put feeling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
