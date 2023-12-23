a8767d58cb2c6e531be79f4f0697fc31cf6b9ac9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:30 +0000

    opportunity abroad lunch seem various

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
