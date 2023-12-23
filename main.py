e50b21cac182b311c2e89110f7fcca8e8775f0eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:23 +0000

    most receive other pupil while

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Hello, world!")
+import collections
+def create_queue():
+        return collections.deque()
