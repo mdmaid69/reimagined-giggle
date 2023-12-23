f3afea360b50c30dd206330e7a64c1cdb1004529
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:34:36 +0000

    hurt once date really child

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def convert_to_hex(n):
-        return hex(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
