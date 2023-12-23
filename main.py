927d5b8e1e3b8f6f73401092b8a95da541be36fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:39:55 +0000

    save mine trousers memory moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
