e20e707061a97206684e96b0c74531f88d09cbf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:19 +0000

    describe factory meet protest responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
