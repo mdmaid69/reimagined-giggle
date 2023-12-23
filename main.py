47383a446980c65a4c1d968f333074255f4404d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:11:47 +0000

    order bite flight letter trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
