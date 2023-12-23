86d51bfe0c10388eb7dad09bb1166252584cb939
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:04 +0000

    behaviour decision wave bin hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
