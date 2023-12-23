194afc1be7ea9952df377daa04803960e0dee37f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:32:02 +0000

    funny steady switch learn long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
