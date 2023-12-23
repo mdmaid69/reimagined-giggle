e37a694b7654edcf4d2fabf22605d17a033877e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:44 +0000

    bringdown protection feeling their amount

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
