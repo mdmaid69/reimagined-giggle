fb3bc6fcb2bcdb65fa44135c9b7854ea67f3dc70
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:28 +0000

    tidy century reply report gradually

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import socket
 
-{body}")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
