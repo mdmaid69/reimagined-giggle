e3adce9f08f1ee3b82f99946b60636f6d70da95d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:24 +0000

    anxious represent react hope interesting

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
