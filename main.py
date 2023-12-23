f78c92261d894541eea9e60d9df7d2b9bb9e1db7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:15 +0000

    them section along lose possibility

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import collections
+def create_priority_queue():
+        return collections.deque()
