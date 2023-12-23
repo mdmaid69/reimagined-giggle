334d7f237251fb4bcdbf070fe2bfcd6ae97549e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:15:08 +0000

    swim tired straight swim like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
