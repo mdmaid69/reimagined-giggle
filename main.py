7d88da457d4b19b822e9f99f8bbe03bd42562204
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:05 +0000

    seriously Ww stand match getback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
