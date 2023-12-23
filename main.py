d1468de651ea9c964ac048cd5053aba4671c831c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:24:04 +0000

    listenfor frightened in month Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
