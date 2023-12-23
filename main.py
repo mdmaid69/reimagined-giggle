f1b4350c0c8e996a8f6626b28aa78b616cfa301a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:46:04 +0000

    chase spin further turn private

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
