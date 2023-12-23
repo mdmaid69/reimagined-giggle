c85d3dce7fd046e7f1a10b49f31b0f508e165681
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:43 +0000

    temperature appearance matter turnout position

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
