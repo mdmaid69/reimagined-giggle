ec5909fcf833e81a4c983265a8271e4477bd1aea
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:35:41 +0000

    spoil save gas sort unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
