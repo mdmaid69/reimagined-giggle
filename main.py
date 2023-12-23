4bfc9cafb3462084b66c09cdf0f1183a51efa8de
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:42 +0000

    learn terrible basic concentration problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def find_max(numbers):
-        return max(numbers)
