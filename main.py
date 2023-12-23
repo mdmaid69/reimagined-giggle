15cab931f7e5671b01997e357866bf93d2ceac12
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:42 +0000

    promise sister fasten quit unit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def reverse_string(s):
-        return s[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
