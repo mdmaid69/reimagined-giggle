d93cf92630578c2da04ae717f79d07e92a97cc6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:15 +0000

    coffee protest taste thin second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
