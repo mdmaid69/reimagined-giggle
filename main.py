8dc600058314bf21722d6a134948ad55ba9203ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:06 +0000

    innocent flow arrangement pause tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
