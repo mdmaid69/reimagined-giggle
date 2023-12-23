ddc808bc1bdaed8912f82ed1e694524a4661d047
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:28 +0000

    fire lend before consider mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
