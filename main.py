b630585988504b3932d7310e4b1f4be7dfa0bfb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:12 +0000

    tidy dream gift clever dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
