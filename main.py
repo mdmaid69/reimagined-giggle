61b22f274cfa86d11024bf570db459f4b397daad
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:43:29 +0000

    left level sit muscle ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
