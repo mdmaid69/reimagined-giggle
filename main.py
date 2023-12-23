a611c08e2c9dfb75193799832b72d120da419183
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:36 +0000

    test give push direct type

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def clear_array(array):
-        array *= 0
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def multiply_numbers(x, y):
+        return x * y
