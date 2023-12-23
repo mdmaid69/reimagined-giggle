ddbd6d318b4aa852681af5bc631a4e90c13057f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:09 +0000

    whether achieve bottom consult entrance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
