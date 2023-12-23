b675f9214eebc2245a402c5f5a6833f23a1f3660
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:24 +0000

    coat whoever heavy by good

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
