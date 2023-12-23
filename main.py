44d53ef267cc5911f3bc8024ace448a5de11cd67
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:59:43 +0000

    hope shadow practice powerful doctor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
