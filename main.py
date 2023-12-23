5d0d135573f0d45259ce567131e8bd7494696ac0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:54 +0000

    everybody study offer score various

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_current_working_directory():
+        return os.getcwd()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
