bb38bcb783702afbf41d992bff14885453acbd95
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:28:58 +0000

    bang before worth funny general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
