4bdf695ba89210d8ad4f16015fb76f43c09863a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:38 +0000

    shall soft odd tap pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
