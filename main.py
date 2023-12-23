12a6ba65279b4789c9b59dd8273514c146216c6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:09 +0000

    power left borrow lift on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_directory():
+        return os.getcwd()
