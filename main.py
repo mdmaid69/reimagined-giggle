377258186cd34ee21897a3d20e4873e6b281afb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:56 +0000

    over wear whether whom admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
