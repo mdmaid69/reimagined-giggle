e80107cbf63afea72cc859a4cc6ce3689da5bace
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:19 +0000

    contrast read set surprise congratulations

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
