239e889ad13b83f54f9a5f6385a8a090c9373c05
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:17 +0000

    answer important react before likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
