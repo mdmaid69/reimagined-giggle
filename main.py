9ff850eec14dfad734551cb5fd2b6d46220afd7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:28:00 +0000

    freedom point Yy attach form

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+for i in range(10): print(i)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
