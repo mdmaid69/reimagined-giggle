8c9ddb75b861caed21a2568567f6aa1a2f687fd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:37:49 +0000

    time direct runinto sense object

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
