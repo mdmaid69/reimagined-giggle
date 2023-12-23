6072318fdf871e37fd77de00910b8ed74f53ed6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:17 +0000

    stretch mysterious brother object sensible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import getpass
+def get_username():
+        return getpass.getuser()
