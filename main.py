aa70c420ceaafa90f4e2647f506f7768ffe4bed4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:29:07 +0000

    rely culture pencil bringforward confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
