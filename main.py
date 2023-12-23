365061678a6ed5b030de3fd5b2ce0633634f46ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:57 +0000

    skill control rise distance whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def multiply_numbers(x, y):
+        return x * y
