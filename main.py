b28ecfb0d6f19b72030a7f2b8e1a816cc82ba700
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:20 +0000

    weak shop wash fair negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
