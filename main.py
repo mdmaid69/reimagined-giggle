b1cca94ee59fe282378410d3aab2339cfa4d7f6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:07 +0000

    notice join instrument together produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
