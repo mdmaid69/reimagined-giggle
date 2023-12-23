bc6ef74dfcc612869a96cd3f13a44ccfff334c38
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:24 +0000

    vanish choose card morning statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
