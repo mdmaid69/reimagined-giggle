cba0fdfdf2232d744a4d9a75f86083c077656c66
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:10 +0000

    takeout pot how backwards accommodation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
