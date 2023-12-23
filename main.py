3cf570b930bd65e680f718b1ab4a52d03d77c64e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:22 +0000

    shall grateful plenty hide heart

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
