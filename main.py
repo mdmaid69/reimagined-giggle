5a916f7c7b1783d52328bd331e03325e8d1aa584
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:44:55 +0000

    risk serve nervous and neat

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
