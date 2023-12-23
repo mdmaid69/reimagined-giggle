d11763e50392f64333059a1645aeba727cc70eae
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:02 +0000

    pretend violence best radio tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_list(array):
-        return list(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
