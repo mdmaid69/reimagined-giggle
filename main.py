af4a13817c8d16a503c890fe0b44691c6a39bd78
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:40 +0000

    respect patient rely letter length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
