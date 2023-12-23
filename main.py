26500c83ad3d606c214731223d7d34374126b02f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:46 +0000

    Zz regional empty duty stranger

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
