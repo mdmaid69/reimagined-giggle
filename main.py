fd548b23ac75ee7a39e108c1fcf7c31921afb46f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:49 +0000

    inquire rest unlikely tend matter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
