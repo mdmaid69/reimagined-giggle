2615e8c7f03303b4e7599d9b384f4d234c4e83e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:59:36 +0000

    near extra remove print say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
