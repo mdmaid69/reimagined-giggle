9a5b5aabafe11f34380aa9fcf6d0ae6da93afc7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:41:23 +0000

    distance convince 566end estimate definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_array_as_memoryview(array):
+        return memoryview(array)
