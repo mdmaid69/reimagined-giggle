3df2633f05b9bb4bb6f6578a16ba6cd4250c8fba
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:32 +0000

    flat 564encourage destroy unhappy contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(numbers):
+        return max(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
