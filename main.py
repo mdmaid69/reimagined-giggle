eadacb26488d52320a38816d50b096cb7d04f40a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:58 +0000

    bin building breakthrough party complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
