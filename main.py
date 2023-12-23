aa7fa25d10fbd3752acc35e44b7c9c34e08e72b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:29 +0000

    something fix alone catch poor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import datetime
+def get_current_date():
+        return datetime.date.today()
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
