e852350c2e09007ccd746cb76a4e3ec621a6f1be
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:40:28 +0000

    spread cruelty comeup hang shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
