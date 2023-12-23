2604bf01e35b954f760ab60700e5a27b9c909149
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:45:29 +0000

    late unpleasant breakfast female matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
