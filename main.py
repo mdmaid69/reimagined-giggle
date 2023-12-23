328f6d5764d44f869007c123a633c3e63d54acae
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:37:44 +0000

    meet acceptable weather translate bath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def count_elements(lst):
-        return len(lst)
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
