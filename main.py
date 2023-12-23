14ab62eaf1d3f9d98ad0514c94b0cfe473fdbbd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:54 +0000

    crowd familiar set risk spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
