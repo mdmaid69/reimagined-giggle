aa8c83eade6a37690be332564c9597772c03dd27
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:02:34 +0000

    last change both relationship cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
