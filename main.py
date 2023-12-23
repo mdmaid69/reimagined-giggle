bf5412cc534ea877251f7696bce6b41750246d57
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:28:11 +0000

    over treat march punish exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
