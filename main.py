67029caad523fa0f6d2375e68228ae6a0087f651
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:00:17 +0000

    creature someone print instruction nowhere

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
