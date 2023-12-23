ff870345992000b5e5fa59a17a34ecbf2f1e8f38
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:47 +0000

    pile visit turnup remember soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
