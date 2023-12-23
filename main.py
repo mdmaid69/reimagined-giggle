1e2e30def33874695c2dc3f3fa4266513282f2cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:15 +0000

    correct programme deal left react

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
