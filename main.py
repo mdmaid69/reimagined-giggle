502e8f481a65f84b6d431c6d87878a183a65309e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:53:53 +0000

    feed reply forever general advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_length(array):
+        return len(array)
