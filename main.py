7ae11e2a1e4532742d267f09dd0cea948ec75a0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:38 +0000

    earn risk stop print lookback(on)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
