162b8d212372ac8045fcf25e2d6cecc8492d71b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:45:37 +0000

    declare try clothes replace soil

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
