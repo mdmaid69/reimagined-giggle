a4920d30f4c1337d213d5c71f55bb5a49879e574
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:34:47 +0000

    patience star behind home gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_file_size(filename):
+        return os.path.getsize(filename)
