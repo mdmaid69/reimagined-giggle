ec30747c7ee5f30d169bdce6f3778410e8102ca8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:19:53 +0000

    sweep stay tax turnaway everywhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
