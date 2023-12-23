d587532072d724917e2e5878e058f642df7dc3f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:11 +0000

    rule left makeup general basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
