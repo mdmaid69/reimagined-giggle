b730f4186f37a07d94d73502d7cfbaf840eac1a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:35:39 +0000

    type life gentle swing contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
