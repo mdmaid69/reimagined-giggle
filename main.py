d4bf9c925061c0161723fe6e69d14a49ac9739ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:36 +0000

    lookafter challenge neither fight sensitive

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
