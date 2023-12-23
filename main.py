d41a29834495a5c7047b6c1cb6d4a81736304eef
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:44 +0000

    compare breakthrough refer thankyou punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_sign(x):
+        return math.copysign(1, x)
