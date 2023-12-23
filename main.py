bece44bfea16eeb44806099a9211a54fd92bfca1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:29:44 +0000

    leg dance somebody pity division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_string(s):
-        return s[::-1]
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
