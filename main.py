81c0646c236bed5fd097baf9c69f4191c29a8c03
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:42 +0000

    comeoff own home container instrument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
 def get_array_as_bytes(array):
         return bytes(array)
