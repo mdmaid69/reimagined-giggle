c0c0887af320d8b82e98b3c8ab39cceb3ad0e964
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:33 +0000

    much cheek interview hurt goodbye

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
