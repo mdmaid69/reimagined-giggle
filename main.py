8eda01723a6e48fdea3c67290782522fe95ff54b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:44 +0000

    flat getup nonsense shame 179bite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
