d52f654b04d9e86528d3b98ae7df49f611e4fabc
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:36 +0000

    tight workout bread mark area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
