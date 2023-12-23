2342fedafc4fd15bd3a46fe886217d1fe75b27f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:04 +0000

    cheat cry downstairs uncertain waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
