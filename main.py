44aa71391a3d1dd68657d66f20a82d0fd2f3932a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:25:26 +0000

    vision available accidentally dig cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
