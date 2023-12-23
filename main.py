4d62134ff4c2e4d5cd78dd9bdbcc7c94f2ec2b51
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:29 +0000

    claim emergency makeup begin freeze

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
