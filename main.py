ccf129f3b708aef91e81ddf6e0172ac7b2d4ca00
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:30:38 +0000

    ready opponent breakin safe burn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
