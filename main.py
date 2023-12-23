580e5b372c7a478a702f0c296a127753188fea0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:25 +0000

    rule tough compete movement explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
