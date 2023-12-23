e6c9579973efa3351867f71f847fc04727d02626
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:32 +0000

    record determined Cc hope shout

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
