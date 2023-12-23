0ccd370847182d5d2534a1ff1fbb64d0e1cbac50
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:17:33 +0000

    branch undo Ss interview whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
