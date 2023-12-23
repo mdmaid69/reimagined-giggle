9184236d4de68e8ee4d248560a8a0730f4bb62f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:20 +0000

    sea comedown fly uncomfortable mistake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
