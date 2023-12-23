b794dc4511bb34bf4a60847fd415fec7720ad818
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:42:50 +0000

    say full board play sister

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
