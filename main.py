c9b4509659192ca38d4d273712f2098e78637a71
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:02 +0000

    human back tidy century day

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-print(os.getcwd())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
