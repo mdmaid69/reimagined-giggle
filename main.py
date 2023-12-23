2abea21492f1b4b01d33724a3c86765c4d63c3bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:02 +0000

    light pay to far Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
