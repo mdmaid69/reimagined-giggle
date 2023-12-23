71b13630fbe8a869f6d18c659e2600e5a22750ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:51 +0000

    consult empty challenge attempt advice

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
