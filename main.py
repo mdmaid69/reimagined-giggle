45bb75a585286836256caaa03f2e12a020ceeb76
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:51 +0000

    say practise happen emergency injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
