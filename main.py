d7ca6426cadb38fc67359223887f218b17e16a5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:00 +0000

    plan behind takeout mystery anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
