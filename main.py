4ffa1d85ccfad39656dddb6eab7c82eb0b7aa5d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:03 +0000

    complete demonstrate giveup speech nervous

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
