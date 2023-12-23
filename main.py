a6153ea17f723454be3f6e91f4ee044c56fe2791
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:53 +0000

    future guide about rid adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
