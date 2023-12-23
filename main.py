75b68dc442ce65f73b21f6be25bfccbe655696a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:41:15 +0000

    fall pay Yy pleased rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
