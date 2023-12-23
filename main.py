275c07ede10639ddbf3b064f128af490289ec334
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:08:58 +0000

    direct tired change manner probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
