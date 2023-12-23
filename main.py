3c67aae9800c26ce9ad33668845c3de23f566194
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:39 +0000

    Dd imagine clearly experiment Ss

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
