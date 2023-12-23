b7396cec2bafeb13cf153a45c1a50dbf3732cef4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:47 +0000

    offer cruel mention sing object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
