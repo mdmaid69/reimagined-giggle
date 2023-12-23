241ff059203e6da3d85e2136c69ae56e4f6fc460
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:55 +0000

    travel sell like equipment news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
