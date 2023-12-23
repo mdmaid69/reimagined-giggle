74dcd42d80b2fd9b7d1bc75ed9d09f20ed8103b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:49 +0000

    regular only loose repeat company

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
