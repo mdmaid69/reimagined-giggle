0cccc91840b8021735153e121620612d9a90fe7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:22:59 +0000

    dream production remain pressure union

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
