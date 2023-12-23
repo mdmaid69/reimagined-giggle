5968ececfa0723151998a02e8dfd6d800cd3e4d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:20 +0000

    demand settle by interest shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
