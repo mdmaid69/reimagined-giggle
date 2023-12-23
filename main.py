abe13aee119ee4f2bd6cee04a1a8f552c51696ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:36:08 +0000

    often desperate behind star takeaway

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
