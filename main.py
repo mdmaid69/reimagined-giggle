482f8c19a720c9a87c855e2545d1544accb102fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:45 +0000

    compare sign just settle object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
