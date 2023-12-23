c8c0796f0e956df74cecb3472ddfada8c60ea39c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:12:43 +0000

    clean determined 568energy comeoff bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
