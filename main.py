1a5d9cbe6378c51ea9497383cdf60bd848372daa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:30 +0000

    Cc also comedown consist trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
