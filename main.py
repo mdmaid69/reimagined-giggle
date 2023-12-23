91974f7fb03d2f122e77489459fd35a6c499bcc7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:37:03 +0000

    day load big classroom will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
