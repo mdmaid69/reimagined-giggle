b1cfa64dee2d63dcc64c8971c049f6ea54b56ce8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:47 +0000

    apparently fact lucky pot round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
