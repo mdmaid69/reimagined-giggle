888e09ae61847801a715de4d2975489b12f18522
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:37:10 +0000

    division bottom beautiful trade paper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def sort_list(lst):
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
         return sorted(lst)
