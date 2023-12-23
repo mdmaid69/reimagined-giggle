2586c5e4f0067983fa83ca3b9095868c6794b954
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:34 +0000

    wind off you cheat fairly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def multiply_numbers(x, y):
-        return x * y
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
