1900a101d7dac0fefd0533fc7f689d78eac16288
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:38 +0000

    hungry there behave treat honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
