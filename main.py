aef012558114141645bf3a410c34342874536a97
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:02 +0000

    independent undo situation flow hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_set(array):
+        return set(array)
+def count_elements(lst):
+        return len(lst)
