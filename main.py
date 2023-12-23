341419c5b6e7013202c50c5150f353a52780d82b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:28:50 +0000

    dish below mess table radio

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
