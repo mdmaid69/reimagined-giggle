d31cc45a77da5fc32edbfc19bd0cf1be62352ae6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:03 +0000

    present exchange land evidence shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
