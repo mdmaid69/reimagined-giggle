2b1a4da9c4f380d18e1ed50ebbec3315e75518e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:55 +0000

    anywhere gift theirs low noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
