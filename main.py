46897de1ec388aee66e0abda3bc95bf635b39146
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:35 +0000

    continue system base yours keep

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
