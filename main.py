b07044d917ffe7933abecff2f3da67e54f2bf714
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:16 +0000

    waste exciting watch general explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
