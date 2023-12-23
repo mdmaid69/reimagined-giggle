f34e650db55e44a0c91dbd5e722deb586506f86b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:17:03 +0000

    brush call pleased relief crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
