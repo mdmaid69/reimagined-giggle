29c66b9475a0ba44f40269f64cca74cd27f35edc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:03:31 +0000

    yes further measure like too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
