7cc02e1e09c7c026fc6b3c7039f8c85d1819e894
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:54:40 +0000

    tell suggest rubbish urgent exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import numpy as np
-print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_stack():
+        return collections.deque()
