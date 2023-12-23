bd4e23137c9cce4f99d306db8ca56052966ef3f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:11 +0000

    hit goaway will circle square

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
