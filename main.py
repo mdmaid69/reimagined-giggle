35eb7ae3c19c4e552460345582ebaf4e0c76b588
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:37:35 +0000

    Cc depend cheap emphasis list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
