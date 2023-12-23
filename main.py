37691de4df719e27dafec2bd480813e9625447b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:18 +0000

    attach national friend crowd sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
