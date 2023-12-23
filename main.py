10d14ad98c30b59680c7c2fc1d3e22e16b02b58f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:23 +0000

    unlikely dish shallow mine ticket

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
