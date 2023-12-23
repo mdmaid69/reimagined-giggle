0ec2c15bc7f65d1eec48ed7f05dc6e51a91068ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:33 +0000

    below corner shop branch not

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def is_palindrome(s):
+        return s == s[::-1]
+def is_palindrome(s):
+        return s == s[::-1]
