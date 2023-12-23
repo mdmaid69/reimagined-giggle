72ee424ae84b2e6a0e4c7bae4aa56be9fbfa94d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:43 +0000

    brown create steady mess expect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 def calculate_volume(length, width, height):
         return length * width * height
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
