0efd6c68b111e50b79ce5a01daf773f2c97a3ea8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:15 +0000

    much practise development grateful question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
