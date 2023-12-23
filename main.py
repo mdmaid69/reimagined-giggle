73b680cf1acf5166b66d7e504a54138f4d0c5192
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:47 +0000

    all help judge nurse bite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def count_elements(lst):
+        return len(lst)
