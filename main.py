137ae570bed9104a0b4ef86557437459ebce351e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:08 +0000

    class ilovedicks quality state action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print(os.getcwd())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
