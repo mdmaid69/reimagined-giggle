928365a6615e45196bf7d637cbf2ab78bd01d8b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:02 +0000

    tool measure not traffic Oo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
