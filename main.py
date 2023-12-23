16d5f52df4c816fb9be9634ae41e841cd2748b15
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:10:02 +0000

    finger deep natural week naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
