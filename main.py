036bf77cefe6858cab1a0b8da697e257a9b66411
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:24 +0000

    bang confused warning information label

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def multiply_numbers(x, y):
-        return x * y
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
