10985b6fe6801841720b510e42326aef27d44249
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:07 +0000

    far fence worried share permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
