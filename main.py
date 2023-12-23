83d3d2f7d2eb252d3ff474a642b1a6cb782d5c4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:04:30 +0000

    loose plant manner relief cry

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def convert_to_hex(n):
+        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
