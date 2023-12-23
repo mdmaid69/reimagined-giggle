c2c7aa300bba40db32b03023a938333a9690d4db
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:21 +0000

    motion cry indeed bring production

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
