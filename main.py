09789cd1ac205f8b83978c2926aabf5b70f92a89
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:28 +0000

    whistle record middle experiment pressure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def divide_numbers(x, y):
+        return x / y
