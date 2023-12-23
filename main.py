1c150d7f6965ec1e98793d98fa0406c97d8465f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:56 +0000

    cold amount high harm minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
