d4f76f9a0eefb0800965633349ff8ec6e9ec7414
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:58:19 +0000

    awake defeat thirsty water address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
