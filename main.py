1df856f7cc3f4438382174efabb820bc17ac5f3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:12:45 +0000

    temperature Ss cost unfair compare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_power(work, time):
+        return work / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
