d6e7f31b0a4226536712023c4d0656d354f91a60
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:45 +0000

    them strength wait decide offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def is_odd(n):
-        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
