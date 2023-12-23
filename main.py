bf4ed45bd9b24ffc633f9aa1bf0fb6cdd08fe8cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:05 +0000

    convenient delicate consider breakin fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def is_odd(n):
-        return n % 2 != 0
+def get_array_item(array, i):
+        return array[i]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
