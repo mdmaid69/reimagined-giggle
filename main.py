ae97e1d12df572c90c523335b568c8e14fd459fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:55:47 +0000

    polite floor deserve lookafter outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def is_even(n):
+        return n % 2 == 0
