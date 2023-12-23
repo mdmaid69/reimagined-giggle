e773ebe8ecda6e191b4fd8bab6220d6aa9dd9868
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:20 +0000

    meanwhile sky rely dangerous avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
