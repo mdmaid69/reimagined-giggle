902e1099d13c8691a55a5c6cc31e03db73bc1c82
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:34:37 +0000

    actually remove ours turnover wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
