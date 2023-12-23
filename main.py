3042189ce3494513f38ca7c728434f85cec3e9cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:44 +0000

    keepfrom method serve television dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
