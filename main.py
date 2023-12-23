be9113d49026495f3b451a0ad899e924c6dea278
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:27 +0000

    approve pride seriously quit obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
