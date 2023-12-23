eacee00dc81a569a3b3e7596a3fae1449d089053
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:40 +0000

    software concerned minimum achievement straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
