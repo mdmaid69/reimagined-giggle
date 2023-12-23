8d35c2e34633007bf8256463f75f4cf8adc928c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:34:37 +0000

    school sleep send pool pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
