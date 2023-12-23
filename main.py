f208652ee65948b99b157c8e40f991c676400036
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:59 +0000

    challenge begin test turnon ride

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def cube_number(x):
-        return x**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+print("Hello, world!")
