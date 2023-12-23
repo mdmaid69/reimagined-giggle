72cd4712ef36c86824f7a8db347ea91e31c70bb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:04 +0000

    judge train touch bye print

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import unittest
 
-{body}")
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+def convert_to_json(data):
+        return json.dumps(data)
