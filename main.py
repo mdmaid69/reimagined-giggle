1edf639b870c3f056c4cb339bf23d58bd57864b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:45 +0000

    search discussion turnout lesson shape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
