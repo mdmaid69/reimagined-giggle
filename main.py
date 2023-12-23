3bea22494e2b8c526bd55391898c6813bcd7b400
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:29 +0000

    today thankyou trust Nn near

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
+def is_odd(n):
+        return n % 2 != 0
