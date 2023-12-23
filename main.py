ab037d910f3eba1c99cc562fa6ca27bbb80ed804
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:20 +0000

    later cover score pleased moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
