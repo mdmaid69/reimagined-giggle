cf74e90881a841f1145fd500da43f7d32bcc3fbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:39 +0000

    improve rescue everywhere loose train

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
