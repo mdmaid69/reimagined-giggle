d213c580d83b411412c4f01ac2919b72547110cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:32:34 +0000

    even party pick hello there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import tensorflow as tf
-print(tf.__version__)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+for i in range(5):
+        print(i)
