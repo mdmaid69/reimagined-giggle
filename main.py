7207a552459dd340de12cfbdbf195241c6b1e76e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:32 +0000

    trick worth pass speech past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
