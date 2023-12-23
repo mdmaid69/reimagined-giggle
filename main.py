0f1f4a1f3ff1a0ca31ea13bbb4b4b1af75cc6402
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:17:55 +0000

    shop notice method left unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
