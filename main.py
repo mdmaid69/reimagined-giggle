3847c507d4c8079cc429c7ebcd36d64cd737a898
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:23 +0000

    answer attempt distance separate smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
