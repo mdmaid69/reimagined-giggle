9d6e07ba302c6a28929098f073d7fcef62b5bf3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:38 +0000

    window sale performance into shape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
+def sort_list(lst):
+        return sorted(lst)
