ea93f07d3ef7506c788b333431cac038220bd08d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:24 +0000

    miss down justice maybe theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
