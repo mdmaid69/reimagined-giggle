fcb63486319a2050baabb29aeea8e30645a6076a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:07:45 +0000

    shelter let mend rush soft

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
