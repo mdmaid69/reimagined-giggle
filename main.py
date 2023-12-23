ed1e8895280594ddda93dee1c709a1593cf754d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:56 +0000

    deliver sorry somebody height callout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
