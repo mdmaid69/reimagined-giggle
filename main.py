1a73683b3df0e802681ee46207a60b1475450b21
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:01 +0000

    surprising takeout board pity like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
