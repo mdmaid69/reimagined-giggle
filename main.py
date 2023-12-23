f291e267905f67202bb2f09c4bbc4de3863f6945
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:42 +0000

    tell sink name software mess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
