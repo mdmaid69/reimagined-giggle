d7470a4e02ef8cf48c39c86657d6f5a27c90b366
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:41 +0000

    suit temperature half hope pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
