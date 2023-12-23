86e1d379b77bc398970f63562c32aacec9ad4ef3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:03 +0000

    office ask narrow delight environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
