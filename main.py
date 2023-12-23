6b33588fe0598b7bb7f4962cb6b58130174213eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:47 +0000

    nigga exist able own delight

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
