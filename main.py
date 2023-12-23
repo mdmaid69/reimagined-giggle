e95f0fd72723a0c5501d0ff2439c22f3488facdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:15 +0000

    range model land praise complaint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
