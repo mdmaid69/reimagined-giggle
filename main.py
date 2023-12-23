de927bdf1a4e632eb916f5c1be047209ab7b279f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:25 +0000

    sentence share remember choice enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_density(mass, volume):
+        return mass / volume
