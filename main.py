a8602d39b703775b1afb338cc4f5878e7c398b3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:01 +0000

    between violence course compare watch

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
