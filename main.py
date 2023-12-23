88e1657f64642a67b39c3ca772bf9b1bfc5d9679
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:12 +0000

    drawing clothes by flow rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
