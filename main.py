3904103980abf8309e5bd61fbe8ac01641a58416
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:10 +0000

    well invitation crowded needle confidence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def square_number(x):
-        return x**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
