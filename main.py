90ff118375651b08b0bab1c099a595cf0effc57f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:17 +0000

    shrink sort company circle rank

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 def factorial(n):
         if n == 0:
         return 1
         else:
         return n * factorial(n-1)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
