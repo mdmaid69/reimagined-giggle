558ffa1933a39f66e6fba58ea919b6f5b2b61628
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:58 +0000

    Kk complain powerful proud steady

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+name = "Python"
+print("Hello,", name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
