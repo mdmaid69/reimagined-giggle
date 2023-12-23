f4b9909defb78c127beab546ecde2345bb345e4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:22 +0000

    fault tend insist strange turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
