762059af3023ae9cfbb1f1d831d9f8bf4e1a905a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:04 +0000

    criticism split him bank odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
