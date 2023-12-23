f2a65c26ffe1df37e5348818191d65131cde4321
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:54 +0000

    vision team explain long short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import tensorflow as tf
+print(tf.__version__)
