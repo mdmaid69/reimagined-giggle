d7ec0c3b0270b471e195cea0731d7e527b1c7c8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:40:08 +0000

    guess storm bath above cruelty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
