b975b8efb53a3f34c82ca2867261827659b4e18a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:43 +0000

    prepare damp storm regard continue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
