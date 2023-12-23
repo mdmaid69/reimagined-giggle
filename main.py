142418119547b5cf2c4b63b43b4975717209509d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:46 +0000

    moment sign extremely present his

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
