42bc33ba7cee3facde05a530fa16af6685754ac1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:49 +0000

    debt fold surprised hers instant

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
