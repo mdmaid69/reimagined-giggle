ca86a2434b15b824ea0c9932e54def25d66ba90e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:45 +0000

    attitude house store low unhappy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
