3247c0b187752e5b671966885efbeb9347a9b40b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:34 +0000

    month favourite cost sentence used

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
