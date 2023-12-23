4a3adae37e01e3c6965f721f39833fcc8be25bfc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:39 +0000

    performance oil possibility forward holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
