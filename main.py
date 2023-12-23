0f952096d9482bd88e5c1e7d3e8d641e18b65429
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:09:38 +0000

    laugh outside container broad broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
