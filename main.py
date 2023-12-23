e7c25b084ad478480d58440349ce462174fc87a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:27:07 +0000

    design yours bringforward development pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
