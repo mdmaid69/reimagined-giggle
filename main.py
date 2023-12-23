f4477ca8e231669d5488d30767161cb8f41ba181
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:26 +0000

    description plan regularly refuse term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def square_number(x):
+        return x**2
