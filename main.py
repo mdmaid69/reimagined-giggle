f4caeb49c304fed44aadb3fbf43c5767845eff2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:47:48 +0000

    downstairs broad Vv average tool

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
