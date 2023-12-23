1c3ec9a194c7b6d98c5dc6dc17d83b1da6ac4e13
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:04 +0000

    circle other pollution spray aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
