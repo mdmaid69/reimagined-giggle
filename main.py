31d97b78a0ace28d4ff1d2a5f57955aab585d332
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:18 +0000

    embarrassing mess shut strength heart

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_elements(lst):
-        return len(lst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
