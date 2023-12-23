fe55b13b319d0c4a255e123f17342fa27a523ece
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:56 +0000

    deliberately exchange fire spin war

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
