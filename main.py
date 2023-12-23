c21da83d5c417a1c91de60c92b762008dbc701b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:23:25 +0000

    big turndown deal reply Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
