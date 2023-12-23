10fe6fbd5ae06ec50b00043d493833c8c05356a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:30 +0000

    water warm generally actual land

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print(sum(range(10)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
