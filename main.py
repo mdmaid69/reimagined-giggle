f759fbfd68fcf154c316119d479f631ae02facf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:08 +0000

    lucky read takeover live cut

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
