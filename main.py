18ae7212f62c9d14402f2b4ef13d0cbfb30585be
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:44 +0000

    fresh exam voice spread comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
