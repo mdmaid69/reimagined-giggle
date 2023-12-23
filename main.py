237f9b3f0ad9187b7144d54ee7dfef015f394d01
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:07 +0000

    push cost generally stand Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
