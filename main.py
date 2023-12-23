b98dee1e496eb9d8be89b7a180e6cf4e972e262b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:59 +0000

    sense push trouble feature OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
