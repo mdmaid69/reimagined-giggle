605a8c6caf0b25183e5161c240e6c0d02d09fdd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:48 +0000

    slice cum emphasize succeed store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
