974755f6fc8f5a1774d668ff67f4274d39dfc160
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:56 +0000

    pause would disgusting team university

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
