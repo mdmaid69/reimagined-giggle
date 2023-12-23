1da9da2e2dd9ec25761fb6d43ea64617a1fe9463
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:33 +0000

    moral surprised tooth instrument trust

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
