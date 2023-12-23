953f6801f33f39bb4a07e1fa411f23a0a6f08ae6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:07:04 +0000

    rough surprised wherever object success

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_min(numbers):
+        return min(numbers)
