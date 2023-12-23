0e85b5b8c2a0f7ff18092350e39f8fcfd2c32947
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:25 +0000

    improve sense sleep any edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_length(array):
+        return len(array)
