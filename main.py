9dd6f80436cdf4cc2d35b51272c9505e28e8f81e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:12 +0000

    attempt check goup collect moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
