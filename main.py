c820151305466609d0c5a0f9bf81e084251c923e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:54:17 +0000

    smooth disapprove Kk activity dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
