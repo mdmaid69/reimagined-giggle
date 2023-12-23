653a42b859b6379afb778332bdf7c817aaf3f408
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:52:12 +0000

    comedown rare very belief crowd

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def add_numbers(x, y):
+        return x + y
