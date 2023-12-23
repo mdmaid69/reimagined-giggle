90a70fc3b101cf5de947f0c20613c0c4d2167a53
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:13 +0000

    total still with alone combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
