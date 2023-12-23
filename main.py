ebef7916ce0e253526d6d379d8bc042e3cf4eb43
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:25 +0000

    copy complete after pronounce scare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 def count_characters(sentence):
         return len(sentence)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
