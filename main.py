a204c9a98169ea3278b6a7cb4bcf61200e752ddc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:07 +0000

    one suggestion terrible meal over

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
