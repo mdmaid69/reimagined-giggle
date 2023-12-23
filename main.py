9e3c3643c29f9c81f78b149841933827d0e6278f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:21 +0000

    brother probably damage suit possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
