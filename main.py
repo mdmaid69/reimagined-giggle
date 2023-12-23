757d424e180b3e2218d5cbb2aa09e9c5a8258df0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:46:47 +0000

    forward criticize connected disapprove body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
