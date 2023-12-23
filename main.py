2e67955eba65bd6d55c87cffd54ae97277ce1ef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:08:22 +0000

    suit health who exist good

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import os
 def create_directory(path):
         os.makedirs(path, exist_ok=True)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
