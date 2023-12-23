1b23587955845c849e719ec7752ab7144ab821ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:01 +0000

    purpose touch switch company play

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
