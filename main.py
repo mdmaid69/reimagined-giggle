4c30a9d1d8241719756f0cb1aceee395fb99a4b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:58 +0000

    comment factory forget print poor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
