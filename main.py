3a37bc9860c2901f1751f39254ff051ebeb6a936
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:46 +0000

    none about different listen conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
