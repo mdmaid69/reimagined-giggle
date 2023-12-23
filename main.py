cee58f4d2fee20f37daad0592226b515a96939d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:25 +0000

    also independent description busy peace

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
