d13c622db8c21d87f9d1c6dc11d13e2105b0f869
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:20 +0000

    for confused deliberately behind failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
