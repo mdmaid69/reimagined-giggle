104ffff80ded6e1816f870ff3807566403ebc89e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:22:22 +0000

    clean push terrible machine sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
