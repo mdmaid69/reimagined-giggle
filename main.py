f32527bf8ba91066e2eab7579e7f07d7a5f4c046
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:51:00 +0000

    remain early race field help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def cube_number(x):
+        return x**3
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
