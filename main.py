4edf1a2be5a90ae18e6e214c837ae23caca45e94
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:39 +0000

    talkinto simple plenty scheme 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import sys
-def exit_program():
-        sys.exit()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
