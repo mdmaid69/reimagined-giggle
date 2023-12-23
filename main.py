2eedac46730b0365284cf32c0e2a66c63abb7146
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:04 +0000

    tour year smoke trouble join

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
