45245cad651aa57ccbdae21561d0874f8478f966
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:02 +0000

    move model building arm section

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
