00e597a0ddcd0b0d89c3e994a96c5f98c682e308
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:33 +0000

    some violence immediately basically turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
