0051e1f183824460db8cb1dec3f593e9c9234d12
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:17 +0000

    dry nervous runover lookaround unlucky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
