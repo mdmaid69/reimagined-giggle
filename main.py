81cf3e8f5f76f12708603b64399cc0d47d7f3c2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:51:30 +0000

    season disagree glass comein mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
