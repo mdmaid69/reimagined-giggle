b9f4c8018f62cfaa8a802be390431634f70e4d47
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:28 +0000

    trap record listen use clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import datetime
+def get_today_date():
+        return datetime.date.today()
