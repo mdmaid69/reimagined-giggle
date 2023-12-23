a8544f703a6ea3ff771c6c800d4f374aadad21b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:35 +0000

    wall warn supply develop evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
