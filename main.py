9048e7bba566d2940b420d3e87a76e057a527819
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:46 +0000

    bridge contain telephone murder welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
