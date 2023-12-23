5e6681c9b2a1d5772db8e639d71de67336286f32
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:44:20 +0000

    alone right pleased though visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def reverse_string(s):
+        return s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
