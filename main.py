e38bdc7299404ce67ffc05e8201a32ddc9f125c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:22 +0000

    fold bet town mystery clean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def add_numbers(a, b):
+        return a + b
+def convert_to_octal(n):
+        return oct(n)
