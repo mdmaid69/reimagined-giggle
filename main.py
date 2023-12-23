8fa3f20f0a9768bbb37e63b67aaa9a355e43a0b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:32 +0000

    deaf store occasional bound sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
