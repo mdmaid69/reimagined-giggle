43df22148e0897fb4cf98da5ce356eac80bfcb88
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:19 +0000

    scrape custom file hand hers

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_volume(length, width, height):
+        return length * width * height
