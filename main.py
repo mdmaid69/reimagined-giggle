e878d7d1b220caa7dd52ea42c3c2e29b1840a4e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:58 +0000

    female appear world describe bright

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
