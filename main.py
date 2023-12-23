20238cdb3525dd7425ced978094bd1a2303b1f72
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:54 +0000

    life everybody him bite wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
