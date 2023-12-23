c4d6003f5faba8c1fe903e153fe8f086f237a735
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:51 +0000

    limit reply we protection slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
