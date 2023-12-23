6d54cd3fd5483455c148e7882abef8ce33d40176
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:47:41 +0000

    frightened chin block use whichever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
