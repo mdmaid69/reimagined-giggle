9daf3e49c838dde7c94778e7d6bba47505e7ae79
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:03 +0000

    patient government art politics pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
