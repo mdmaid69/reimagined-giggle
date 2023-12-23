3a058dacf40460bc6193f3ff154257bff24d098c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:38 +0000

    takeback splash scream dry play

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import numpy as np
+print(np.array([1, 2, 3]))
