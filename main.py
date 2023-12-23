8ff8897a64c0ecb99a3e66b15b98e7c9d5d9193a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:47 +0000

    positive brush improve sport translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
