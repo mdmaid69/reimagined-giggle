b4ef7703d23daca711eba32db9a81d3771b3c734
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:27 +0000

    relax taste pen polite used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
