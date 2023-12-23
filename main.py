929d03a6693e84a417328b50af4dae41a286957a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:28 +0000

    sad land naturally boring fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
