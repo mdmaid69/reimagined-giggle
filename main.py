4c6dd4a425539adf261954b3184712f68aa9659f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:16:18 +0000

    balance dick deliberately home birthday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_max(lst):
+        return max(lst)
