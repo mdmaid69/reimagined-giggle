9a4a31b94c50e598181ed07c57ea387b8c071811
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:48:30 +0000

    slice breakup man to message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
