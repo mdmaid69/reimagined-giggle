01dfdf92a826c7198a4132b7498998368ae55778
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:32 +0000

    judge him separate power among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
