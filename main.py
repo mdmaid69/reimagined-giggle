8ce7cafe63bb220394a4bbfc72dd7b19b7b3ab0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:08:06 +0000

    morning pupil brush priority guard

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
