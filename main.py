812e611bbe5f8b062ef059c1a6b37a7a183f75fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:37 +0000

    mix fast top bar I

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
