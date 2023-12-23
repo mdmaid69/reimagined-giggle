fe43637d44e45eb6f7c73c3ff74cd72b3ffa0436
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:38 +0000

    butter birth remark onto private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
