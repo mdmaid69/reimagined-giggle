e6487d75510eccea84833f4c7a6b2cc62ab9bf71
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:49 +0000

    question platform centre brave callback

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
