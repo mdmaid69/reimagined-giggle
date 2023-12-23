44bfd94745d60846eba3d52c4e84ba589d192c0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:25 +0000

    surprise bear past provide stay

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
