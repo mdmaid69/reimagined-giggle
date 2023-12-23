0771fffd792a379459fd78eb4c8ce9d1207182a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:20 +0000

    foreign respect fault tall common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
