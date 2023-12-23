b1ddaa2dded90be727fb97771e7dddb5c1e9ddfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:52 +0000

    many strong smell Ll smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
