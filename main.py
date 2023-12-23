ed4295c5664c506eafee85c41be06caf0e86f407
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:20 +0000

    pot rent behave full Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+for i in range(5):
+        print(i)
