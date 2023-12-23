7e57b8dc58361927d43b7e8a0ccb26f01e80b320
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:59:57 +0000

    mine learn string occur characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
