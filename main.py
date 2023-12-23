269d2cbf4e2b8a938fed334ff2dce448e89373bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:24 +0000

    breakin glad like doubt squash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
