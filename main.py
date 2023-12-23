a50d33a7d655c138f9493ed90ae32a1bcf3bae3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:33 +0000

    damp point punish tax wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-print(time.time())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
