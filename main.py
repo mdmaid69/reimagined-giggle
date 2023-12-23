b21704b32b7775f7a68f8620bb87e61299589b4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:20 +0000

    run block sweep set health

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-print([x**2 for x in range(10)])
+def calculate_speed(distance, time):
+        return distance / time
+def sort_list(lst):
+        return sorted(lst)
