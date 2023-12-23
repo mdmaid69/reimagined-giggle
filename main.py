547bcd73446b64d950e9cad01f796a097a6ca8d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:27:20 +0000

    present information tomorrow fast jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
