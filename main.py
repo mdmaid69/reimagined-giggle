889234c2bc88718fef4addc54b304e79fce4be16
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:14 +0000

    welcome excellent distant indeed equal

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
