7bf3bd92101575d8b79e20a2f2f74498b6c10f6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:33 +0000

    satisfied Kk strength greeting wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
