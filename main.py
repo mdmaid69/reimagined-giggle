3ae4ec4bc9cd7232d8763b7f544cbf933f874013
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:45 +0000

    offer Ee front voice skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
