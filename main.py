a880de6f4f7f8a0afff78b411428f2de260e0a36
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:29 +0000

    handle murder vote convince contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
