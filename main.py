a61099b56cf70f5cfcc787d8ef648f7460c84571
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:43 +0000

    same form sore plate better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
