f161e3b976f137fe79b48015c4d567d71377a880
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:32:59 +0000

    tap crash sea tragedy material

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
