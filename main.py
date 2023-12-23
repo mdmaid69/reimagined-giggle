876cc252b3e134a672502bd8ef72fb818ed56e33
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:07 +0000

    ticket below live mysterious smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
