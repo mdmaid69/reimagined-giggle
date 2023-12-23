8c9a6a7f157c7cd8e91c0067b3f5a0f227b7441e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:03:33 +0000

    wander Jj reliable Dd answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
