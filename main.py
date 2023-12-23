89927a877163c03b714d3ea71568178e297077c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:13 +0000

    dead open take impressive creature

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
