be6a07f0ebe1b9b96e2f242cadeca921e23ad6b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:21 +0000

    open he people thick friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_list(array):
+        return list(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
