bc35d31f38d30f8ccf6d41f36eecfbd395c0f82c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:52 +0000

    sure thick border sudden fat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
