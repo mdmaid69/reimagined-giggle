629805fbdf5a0cb3c7047270026771352bcc782c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:54:11 +0000

    fall Ww performance before especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
