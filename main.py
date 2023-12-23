0480babe7073a8eba06cc81f969fc9f6297348c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:28 +0000

    pupil last 565end everything boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import sys
+def print_python_version():
+        print(sys.version)
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
