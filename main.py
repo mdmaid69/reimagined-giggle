43ea2d69ecde1b279f4ce9765506d7865eba9315
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:15 +0000

    roll day interest gradually there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
