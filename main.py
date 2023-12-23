0bc23c6413dbc70402f4896951507bad96bde78a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:31 +0000

    project benefit principle eye include

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def is_odd(n):
+        return n % 2 != 0
+import os
+print(os.getcwd())
