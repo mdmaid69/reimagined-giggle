fb855f8cd1924f0c07501cb26b48ac7c5d665df4
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:48:44 +0000

    poor law let fierce meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
