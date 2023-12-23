c7cb35a6512d3510e61a594d6a1490e1e43260f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:41:36 +0000

    rate 573entertainment real practise keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
