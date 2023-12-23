1eb554c327e37b1592fe17db278a68972f84acc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:46:58 +0000

    him which squeeze sad news

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
