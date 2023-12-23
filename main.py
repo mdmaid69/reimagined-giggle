47cbc248fbc48ab5fcfa12806fb59eb1e06f15bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:46:19 +0000

    whether plant less call plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
