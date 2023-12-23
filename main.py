46eac7a659be58ab604edafd8ed70d8c22dac91f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:23 +0000

    label muscle push factory terrible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
