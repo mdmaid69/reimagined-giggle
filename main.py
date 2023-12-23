80baa01dae6b3a063465efe7e1d7f56e1bc5204f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:56:53 +0000

    knock fight goout wet bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
