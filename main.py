40637ba8d91ba8815ff73c5fc125ef6040d1a81b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:08 +0000

    sad rob progress than return

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
