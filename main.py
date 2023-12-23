0282545213a97c7db9bd19b8d42e27f0c3cf5bc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:11:37 +0000

    later below noisy sad reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
