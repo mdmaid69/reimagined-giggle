a5a548fd86362b689a7c41dd1e5a2f81e166590c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:26 +0000

    collection unlucky ride fault in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
