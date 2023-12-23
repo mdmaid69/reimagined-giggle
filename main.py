6791769cd94f8329e2139068e98edbe39a889469
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:00 +0000

    translate error aim careful tie

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
