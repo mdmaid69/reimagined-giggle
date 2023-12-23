5cc8dc526a105cf3c48d0d0647d370f06d3a9101
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:50 +0000

    rubbish slight comeover thank form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
