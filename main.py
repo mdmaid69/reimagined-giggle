3eb73414bdf224c35caea26bebec348ff73cf33b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:51 +0000

    pride aim vanish still appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
