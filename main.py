f0a0a07c91b6bd3528bd44dab4653659a8cc192a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:51:20 +0000

    level raw shallow by respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
