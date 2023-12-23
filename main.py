a7dc1fcf00e1c8b9cddd6c85a90958181c895902
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:00 +0000

    border convenient content hollow needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
