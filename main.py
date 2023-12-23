d6ee1deb47d16531ba078bc9b6359c205c5c0c08
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:47 +0000

    getout plain yet mainly principle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
