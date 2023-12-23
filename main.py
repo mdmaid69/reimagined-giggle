a2b6242e5e9cf1c3236193ad7d46bce5eba5a5cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:40 +0000

    cough warn own reduce mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
