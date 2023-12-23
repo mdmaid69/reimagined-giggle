a0c12deb026944020b413e042250f570c70397ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:26 +0000

    taste left key charge section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_string(s):
-        return s[::-1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
