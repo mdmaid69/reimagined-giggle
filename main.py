20fed9b4b688a9ab95f3e303b650ac1f022f9378
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:16 +0000

    content past shame sore downstairs

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
