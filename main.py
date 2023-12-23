0612b9ac888db8b09a92d1fbcaca3bcae21c5663
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:04 +0000

    open wake effect firm school

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
