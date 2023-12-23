7d7a7daf296faa100546014fec50b121bbb595ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:57 +0000

    undo dry trade act fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
