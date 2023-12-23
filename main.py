0f51ae7aa4bbec60d011531ab72f3c6c30b4d30a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:34 +0000

    vote birth shrink professional allow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
