4006e4ff6d95a3ceacaec5631dc35452c0f3e210
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:47 +0000

    full square letter form bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
