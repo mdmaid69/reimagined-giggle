a06470e88bd2abaf4ea0005b4b8812000783053a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:00 +0000

    ask put dark dinner check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
