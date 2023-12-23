a47705596bbecb08ca1659126cb523e50d28fb86
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:49 +0000

    she step trade nearly wipe

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
