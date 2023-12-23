18231b053e6b8c3d8b1d64f4a83e48ef2eb37154
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:56 +0000

    follow rather personal believe cap

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
