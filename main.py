8c38f4453a0fcaf030c5831a1a3c72ab0d48efce
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:08 +0000

    pin production whatever occur everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
