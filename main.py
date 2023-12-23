dee36f70b69d7884c15d9f86532f6c2cbcbc603c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:24 +0000

    scheme talkinto band eat half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
