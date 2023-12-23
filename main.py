c9548ddef82e1048dd732bf893eeab14b72ff901
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:56 +0000

    clothes translate similar practice closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
