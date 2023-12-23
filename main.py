78436d695eecb8421cb56621e2d5d58beeac22db
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:42:08 +0000

    sale port face help land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
