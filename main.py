754f59ba1e1bf65fd2562e72b8ccbe064868c443
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:09:40 +0000

    anything possibility square himself arm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
