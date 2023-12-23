c2c05d97d83a642be2d0b13b1f1266d6d873596a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:57 +0000

    proper quick remember runover town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def get_current_working_directory():
+        return os.getcwd()
