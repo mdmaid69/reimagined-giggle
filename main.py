50e86ba93338ea6de003c01e6b82dfc92482751b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:14 +0000

    your stop hair process brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
