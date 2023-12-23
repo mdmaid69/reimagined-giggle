82e293abf6664fb93c548edc4d582de3b278a472
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:33:47 +0000

    frequent pupil delight possibly niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
