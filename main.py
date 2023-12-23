9594c6b2467098de658258da791a60868c38d558
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:22:31 +0000

    connect trick cover upset edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
