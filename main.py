2fb6ef999e1009e10377f18a12aef817538bb0d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:28 +0000

    pair low estimate warn comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
