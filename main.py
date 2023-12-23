434bb761a6b13d914affbd57adc7fce83fa270f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:21:53 +0000

    try central runaway hide decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
