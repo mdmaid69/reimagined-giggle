9d1047cfb1864ec646ba0ac6457fc2e7d1c160ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:27 +0000

    score Tt reach well loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
