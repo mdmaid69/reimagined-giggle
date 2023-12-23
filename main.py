d63f093d8c9e0a71fcc0b856ef74639bd3eeccfd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:57 +0000

    callat complete confuse raise or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import datetime
+def get_today_date():
+        return datetime.date.today()
