1f66256374fb16efeddd4d15255aac606a322fa8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:52 +0000

    concentrate hold environment experiment hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def get_string_from_array(array):
+        return array.tobytes()
