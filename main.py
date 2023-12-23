d5a4841199ed6424b679952b23e6252f991cf84f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:32:02 +0000

    manner deliver section male cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
