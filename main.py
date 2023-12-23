0b8a5f9a8e27412abb6150d840d8dfe831cea9af
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:13 +0000

    tend tour hers pull any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
