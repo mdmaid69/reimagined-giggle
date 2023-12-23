8771b434926cd3a8657f419defd3e92f25692008
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:25 +0000

    near touch fashion makeup grateful

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
