a67e80e2f39ecc56c6a52c8ab3724cb9c51bb6f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:15 +0000

    calmly tap duty 564encourage government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
