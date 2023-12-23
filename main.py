ad18ba5cadd8b466f627baacdc1bf8f194d875e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:49:42 +0000

    young wise smell offence this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
