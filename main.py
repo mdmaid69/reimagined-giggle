f1bf4cd257c923ad3b0032e61cdfceead1584aa9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:11 +0000

    have wherever limit once takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
