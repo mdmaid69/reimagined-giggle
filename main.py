1ca8bdd85440abc4445193d26475150dc746f098
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:59:46 +0000

    smooth sort after calloff stupid

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
