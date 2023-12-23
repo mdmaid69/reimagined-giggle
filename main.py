b723911b3842172043e85000ebc67a250c4b1337
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:32 +0000

    motion control press as wide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def sort_list(lst):
+        return sorted(lst)
