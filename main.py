3549c831f8c54c9ec58196c45d0e2ba22f99d742
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:11 +0000

    advantage ago state word level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import collections
-def create_priority_queue():
-        return collections.deque()
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
