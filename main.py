edce7f13ef6c27e023454620011bc4df71a77a78
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:33 +0000

    unable major classroom breakthrough left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
