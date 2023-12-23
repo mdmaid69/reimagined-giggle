3342a251811188f6d013b969cafa76aa47fb3757
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:42 +0000

    profit prove area but container

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def convert_to_binary(n):
+        return bin(n)
