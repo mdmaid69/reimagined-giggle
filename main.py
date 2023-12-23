8e2af73642f214f6061df36d2e86741a9f14ffc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:48 +0000

    watch appeal Qq past discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
