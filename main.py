179f720e7bab1461066a53f1fae818c3415d4d37
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:42:43 +0000

    completely tap lot experiment result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+name = "Python"
+print("Hello,", name)
