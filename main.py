c040652bd818039412c30cb68736a311461aa0d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:34 +0000

    lost obtain bridge shout brain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
