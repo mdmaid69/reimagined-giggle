d7d8bb5c1b076bc22d383ee350b47c04f642316b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:18 +0000

    by tip power arm owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
