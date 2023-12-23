004ac7aa6fedebf2dce783e15ece5e52ff3add7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:29 +0000

    scale variety watch contrast public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
