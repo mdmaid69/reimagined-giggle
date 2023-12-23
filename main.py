6f5e9fb38da215fffaceef890350069d461837d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:55 +0000

    repair chief wear scene food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_itemsize(array):
+        return array.itemsize
