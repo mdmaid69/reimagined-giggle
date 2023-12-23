02250d71f95518cdef4a08b9679e5f79bf89093f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:40:19 +0000

    somebody must shelter child thin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_string():
+        return collections.UserString()
