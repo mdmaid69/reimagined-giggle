0f71392b9ada5af478b4c4989a0dcadc59ce7e89
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:00 +0000

    respect second carry vision comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
