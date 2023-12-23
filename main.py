d36080e45bb220a6f44fc0222353dc69b42283b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:45 +0000

    keepoff minimum sign peaceful admire

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
