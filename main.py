70d50fdec95b82c91fdb30ffa356bb6f1d513dcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:06:54 +0000

    cheque shallow use soak town

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
