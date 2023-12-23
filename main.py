42423d573512bcf04abdd34eaa1cd68a44be69d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:40:36 +0000

    certainly crash factory here pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
