f7e414ebf7ef80d412b75a4761b657f282eec64d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:36 +0000

    they difficult save equal godown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
