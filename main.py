7d5438b0d2669358c254a4dc9991ec1c625bbe30
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:56 +0000

    promise mainly 179bite bet introduction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
