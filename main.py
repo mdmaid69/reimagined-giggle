4b33a5e8f7746c6c138be46c63fc94d7672d96d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:53 +0000

    OKorokay copy talkoutof mind marry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def divide_numbers(x, y):
+        return x / y
+def remove_duplicates(lst):
+        return list(set(lst))
