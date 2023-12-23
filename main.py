33fbea3a9d7e62243553bbc53f5af9d73343f476
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:50 +0000

    yet much impressed to many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
