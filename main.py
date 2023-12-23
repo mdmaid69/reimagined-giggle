ea20b1455234eda1ddd6276abd7d53216b8ebb88
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:03 +0000

    regional sink advantage right touch

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(10): print(i)
