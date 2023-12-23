e0a05b14a385465ab2e401a59b1f2975399e0283
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:57:52 +0000

    little wild rent frighten pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
