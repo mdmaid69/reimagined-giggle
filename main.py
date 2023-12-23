1e35ac0238eccec97c189141bf11ce40ae0d3870
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:10 +0000

    bread instruction both slip high

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+def print_python_version():
+        print(sys.version)
