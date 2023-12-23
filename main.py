bf5fb17ec750cf4c3291674db739943c62d6bcd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:38 +0000

    shopping backwards cool shoot entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
