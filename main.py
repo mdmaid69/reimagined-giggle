68617570d02e07dc01af9ccd38c4a7b2b21c610a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:33:05 +0000

    well emphasize buy drop turn

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
