a66286b36308513205e9ffd7a3051d2db02a62a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:05 +0000

    upwards good certain your pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
