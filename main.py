3fe84d6ba862974f416f32569e2cc061943a82ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:42 +0000

    realize holdup impossible satisfactory catch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_list_from_array(array):
+        return array.tolist()
