7b5bfd91a0ee88564689cc21addf269238049c4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:38:43 +0000

    female unfortunate cold OKorokay Pp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
