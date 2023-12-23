8f983a4cd9e757551154e7ad3a94417e83156872
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:35:26 +0000

    scream report any either exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
