43281a8d3cba382f54e2495cbb7ed40f6de0b058
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:06:41 +0000

    hit refer charge stamp accept

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
