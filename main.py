3397357d4a15267524aa8d4e9161f68c7ee7040f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:08:03 +0000

    lucky satisfactory scared join friend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
