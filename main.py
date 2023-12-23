02c73cdd71ea2dc67daec57333c579e2b6f318e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:57:56 +0000

    without just important opposite translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
