942bf5297ad0155a37891fac97baca02698c4c27
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:35:09 +0000

    whoever create goout lost 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
