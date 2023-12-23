90a17e5d91beaa1f941fd9c58dac1a6889329991
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:41 +0000

    reserve lean far telephone female

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
