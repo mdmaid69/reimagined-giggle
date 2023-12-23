850c8959570234ff10da9174ef48983dc145842b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:30:05 +0000

    tall friend circle me breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
