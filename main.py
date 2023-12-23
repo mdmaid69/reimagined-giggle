8264252ef0ab2cd2839a42a1e01dd41d8a730f3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:31 +0000

    object holiday way treatment building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+print(time.time())
