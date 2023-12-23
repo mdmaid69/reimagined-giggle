f6962c845b74c4d47695991abcede54c1309cb12
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:13:19 +0000

    name bother hello mess twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def convert_to_octal(n):
+        return oct(n)
