9ce914b19d5fb553e91347680153229d55fb3678
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:06 +0000

    fat snow glass dream gap

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
 def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
         a.fromunicode(unicode)
         return a
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
