94a642f65011fb495d3827e5959735fd3b551550
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:21 +0000

    great peaceful factory breathe poor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_array_to_string(array):
+        return array.tostring()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
