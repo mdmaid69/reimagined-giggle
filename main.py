e7a5ab5e25cdcee519468a66c6f53d0f01af6a2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:11:53 +0000

    drawing accordingto still build drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
