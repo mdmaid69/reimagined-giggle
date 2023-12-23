d90c01cd4ca1e8efd206938a261752c6c0e7a6ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:55 +0000

    warm my here ear 571enough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
