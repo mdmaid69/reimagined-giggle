c1a5a9de17b7ac522a4c8adf60617b1f2c76252a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:31:52 +0000

    discovery answer steady lookup brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
