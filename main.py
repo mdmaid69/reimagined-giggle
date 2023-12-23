050b2df2b7d015337b53ca2f347f98bf54ba545a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:31:23 +0000

    speech age fear lie figure

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def add_numbers(a, b):
-        return a + b
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def convert_array_to_list(array):
+        return array.tolist()
