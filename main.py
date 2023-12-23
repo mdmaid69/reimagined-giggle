c1dfef8cc1e0d285816d79c5c00800e94717baca
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:02:48 +0000

    wide excellent between slice play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
