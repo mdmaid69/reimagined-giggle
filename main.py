d665b5d6e31bed4e257d46feec706612f4162c43
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:46:01 +0000

    basic exaggerate swim heart tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
