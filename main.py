2fe939ccf435bbd22d62c073d336cedefa115031
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:08 +0000

    central recovery pattern club exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
