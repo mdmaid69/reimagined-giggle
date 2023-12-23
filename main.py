da737a3a8acc68e158d9a440f664815dbf0af308
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:07:13 +0000

    remember simple message giveout sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
