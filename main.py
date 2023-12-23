c19e449461aa216f01b040c072cdb8313b787128
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:29 +0000

    exciting stretch possibly both wet

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_work(force, distance):
-        return force * distance
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
