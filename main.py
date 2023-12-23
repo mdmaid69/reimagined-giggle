c54d6e1132093a5de35ec9d1a21eeaccf08930e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:53 +0000

    divide curious front wild blame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
