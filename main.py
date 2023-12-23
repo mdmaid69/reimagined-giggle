c38b66d7e5a188e223ac42330f4fe0d94d83e6fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:21 +0000

    standard before post comeback plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
