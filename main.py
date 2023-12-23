4aea3568fce798c419634ec44988f3bc30cd4ad3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:30 +0000

    understand poison class high recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
