488e5509ec3322e6ad482734c032fb134f7aa41d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:55:13 +0000

    effect tall anything busy all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_area_rectangle(l, w):
+        return l * w
