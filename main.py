5cd72fd397e34dd1a76c5420f6a6ab0dab513d19
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:37 +0000

    pleasant neck scheme computer kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  def square_number(x):
-        return x**2
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
