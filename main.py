bcc091a8daec8208d2ba9f9ee7351af264859ddb
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:45 +0000

    tip accidental dear improve model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
