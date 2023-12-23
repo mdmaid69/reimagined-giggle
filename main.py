438884895b80cdd537926f0971ca75e8efe78693
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:34 +0000

    Ww cheat shoulder either cry

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
