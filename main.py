4c2a2bdb3d0cada026dc2290eea81ef8d11ddde5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:43:58 +0000

    press level bringforward woman guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
