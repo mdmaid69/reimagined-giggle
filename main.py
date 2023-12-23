92cde56b4bd27a4560413a0e2365d220249edd54
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:38 +0000

    surface come carry stamp fault

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_numbers(numbers):
+        return sorted(numbers)
