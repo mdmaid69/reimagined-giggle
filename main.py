fe337674b8be63eaabb92b327b26711ca34f2e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:56 +0000

    to dear record own pattern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_max(lst):
+        return max(lst)
