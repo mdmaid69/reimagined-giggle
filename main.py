c24c23afe7d19f6014a8096856225cf13c434d2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:03 +0000

    kind child steal pure other

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
