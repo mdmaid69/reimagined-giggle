6344a82e380d625928d40d261c7015493b514a40
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:11 +0000

    form previous sale scare 568energy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
