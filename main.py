86652bdd297f310048e5a5784a9fb5a15f0fc663
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:48 +0000

    blue Oo pair them 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
