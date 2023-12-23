9b5a72d21588dd16edf11d676332085e9e998f62
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:29 +0000

    show wrong machine keepaway capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
