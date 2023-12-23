e34580feefa0cf46a10b44930e9090438f34fa8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:17 +0000

    angry fashion near usual march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
