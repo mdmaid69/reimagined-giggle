ba4c44ca42912c296641153805cf35570b2e621e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:53 +0000

    equal use violence suit chief

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-print(os.getcwd())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
