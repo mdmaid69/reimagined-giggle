39c695ae3f514a01cdab6005feee008ad4cc1aa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:38:00 +0000

    rest with welcome accordingto blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
