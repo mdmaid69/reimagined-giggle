91ed198258027d6916cf1029184ad3ddd7b2f000
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:55 +0000

    remove solve behaviour settle first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
