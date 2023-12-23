b74f900eae91bb34580f08f68119019dd5142222
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:52 +0000

    bus around measure show pin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
