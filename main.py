89cbcf9d2d6fd19231ca84747f4b3079879c8381
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:40:48 +0000

    possibly secret idea lead interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
