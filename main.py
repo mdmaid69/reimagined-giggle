364e09837f95af379f43be9736a1e5bab46261aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:44 +0000

    pool plastic breakinto meanwhile else

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
