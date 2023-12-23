d54577b399be64458fdc6eb7918a76fb5178be72
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:00:26 +0000

    fix name tall service trade

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def cube_number(x):
-        return x**3
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
