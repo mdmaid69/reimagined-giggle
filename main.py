158268ab3058ee0c97b958570881a13a249ff624
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:51 +0000

    return relief head rain already

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
