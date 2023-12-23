ac9b96d8b7293da7796ba9d4956c7b5d61b1b7c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:06 +0000

    Nn sky comment news inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
