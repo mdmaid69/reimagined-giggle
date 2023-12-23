442cc953f909c0e16ef575eb759aa6fbf8ff8949
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:49:06 +0000

    raw cook piece soft help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
