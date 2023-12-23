593be1dca79dc7facb2a4fdf07af8e72cc48b12e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:18 +0000

    anywhere roll concentrate importance habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_current_directory():
+        return os.getcwd()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
