e13c4c6bbe86efee7e2582c284931cc1465fd456
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:30 +0000

    still regular ignore anybody statement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
