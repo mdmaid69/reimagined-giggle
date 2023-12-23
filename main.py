4b3265e0e9f4edccf2cdda69c4e31159fd8b6b03
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:11 +0000

    block either improvement train plenty

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import time
-print(time.time())
-def convert_to_hex(n):
-        return hex(n)
+def get_current_time():
+        return time.time()
