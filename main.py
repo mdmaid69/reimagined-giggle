85eb02d544b698df7ffa9cfd30ccc34b0224ddb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:26:12 +0000

    coffee lunch this much long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def multiply_numbers(x, y):
+        return x * y
