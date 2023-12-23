235e3a439b341f64c4385db133018d1cbd87a094
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:37 +0000

    upset vanish life high night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
