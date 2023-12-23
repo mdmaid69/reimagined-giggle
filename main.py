733fb575b994ac090f0c29fbed77c3ca2e14a285
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:04:29 +0000

    no point guess Vv experience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_max(numbers):
-        return max(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
