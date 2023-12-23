b724cefd8310eecdc797316b37f8620ddd9d6036
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:33:41 +0000

    corner thinkof eat smell female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
