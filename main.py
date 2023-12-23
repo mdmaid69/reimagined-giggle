030e063cfbc6f6a0b483b6167b1040cc3b948aea
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:22 +0000

    abroad earn himself expensive sink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
