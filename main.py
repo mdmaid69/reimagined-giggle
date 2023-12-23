04f28a49fcebcee70e9f5f2a1a9b5eba2593dfbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:07:53 +0000

    walk pull that herself teach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
