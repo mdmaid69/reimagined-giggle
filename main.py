5927530e1db59ea01d5606519e6d77997aa1be0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:47 +0000

    leg level sign common pot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
