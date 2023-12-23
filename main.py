64d3e15b07ce6b56ae616112e754b8f0fcab86f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:55 +0000

    play thought colour lean downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
