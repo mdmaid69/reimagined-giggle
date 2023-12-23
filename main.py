6c007bdbbd80e119e70501519349b03e7bb3980d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:33:43 +0000

    fortunately disappear information strike breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def split_path(path):
+        return os.path.split(path)
