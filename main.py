2d20319b0ea95fe6028e1bb27a5b37f53d6e8107
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:01:12 +0000

    question either flow opportunity occur

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
