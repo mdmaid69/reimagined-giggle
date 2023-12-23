8f78342de4bc0686b25154a78161f4552190d704
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:03 +0000

    wish responsibility completely ground develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
