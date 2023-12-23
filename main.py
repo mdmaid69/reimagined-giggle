0bdd65522c40eb7c1071d3866a9b8e8239f957cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:49:17 +0000

    there himself close congratulate judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
