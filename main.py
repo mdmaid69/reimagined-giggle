a7baf1fcead934ab4d81f8a702fb780487851f2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:04:48 +0000

    human difference may study goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
