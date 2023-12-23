576b9174379c57dbd96eab78a0350b49b53722f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:53 +0000

    scared market change divide exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
