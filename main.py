5d2eef1575483199609dd878c7700bcc155e593e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:59 +0000

    clean each expect rescue sweet

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
