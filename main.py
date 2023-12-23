6dc68fc6532327b3e9b7c6c02fa0c93d4f1433c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:56 +0000

    comeout event although last reduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def square_number(x):
+        return x**2
+def is_even(n):
+        return n % 2 == 0
