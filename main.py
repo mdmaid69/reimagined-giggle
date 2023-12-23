52678c101c36bfb171d981b5b07c3a89c299196d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:31 +0000

    power hour related complete generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
