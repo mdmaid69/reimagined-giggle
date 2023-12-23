d7ffe0c173ac10d55abac8edb9d9b7f4b99d1faa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:59 +0000

    where right tongue grow desperate

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
