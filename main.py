49bbe62044c5fb6d910eaece618b43c63aa7df71
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:10 +0000

    physical cry holdup cold freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
