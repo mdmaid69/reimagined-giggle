1b5f1186ffb9426ddb4d3b5b95358f7c8a824ab4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:17:28 +0000

    somehow swing class talkback pocket

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
