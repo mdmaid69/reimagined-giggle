fae4835c4bdbb2a1143bca03c740e46182a832c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:49 +0000

    rather now recommend stop support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
