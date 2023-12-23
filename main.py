6c6a0ee0e911007f82c0ed1214770c83b35a18ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:51:22 +0000

    shout hour swallow joke book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
