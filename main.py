1b5a792e80f8543b9047de4c2455fe1b82559b7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:37 +0000

    properly suppose sky act object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
