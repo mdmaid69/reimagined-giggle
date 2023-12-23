c3323f9f3e1dd33b8c8455ef524efd46d8791e6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:52:12 +0000

    help turn base course uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
