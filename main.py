e7d35d5e3fac217925df60d16c6c1687b0aed5e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:45 +0000

    estimate fact game up comeround

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
