fa26fe3f6322ddc53c02b279c8ba78419c4d5071
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:19:22 +0000

    cruel pile congratulations runinto wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
