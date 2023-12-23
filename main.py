c5a55a26fdd7cb556940c59ab4d1d53b19f77260
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:43 +0000

    cheat wherever tough general unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
