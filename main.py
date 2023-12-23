cb4346b4f5c917f8cf516a7a2a1d7820b23d2f41
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:01 +0000

    shoulder boring decision home container

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
