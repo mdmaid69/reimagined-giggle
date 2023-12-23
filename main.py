a56304a855787170ca3031f8c019d3ef2e425a83
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:41:46 +0000

    risk pool turnup neither agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
