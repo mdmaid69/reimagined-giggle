1f914623e77b3b44807dc940276defb4e1e01f85
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:50 +0000

    trial unite need port sick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
