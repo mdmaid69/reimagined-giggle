988f1332e45b10ea3293ed6e25630e517e0d3b80
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:33:29 +0000

    pile even consist knock among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
