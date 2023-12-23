2b457e35edc24594906c2c02dc0921e448b740b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:17:42 +0000

    danger confuse book certainly manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
