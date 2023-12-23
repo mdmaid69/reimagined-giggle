51374d3f50fe54dab899a43cd71b3f24b073631a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:58 +0000

    down pleasant grateful maybe example

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def square_number(x):
+        return x**2
