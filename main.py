187b8870f87e915de9e3b42f90c5a0211689ae32
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:04:41 +0000

    spin personal part towards slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_max(lst):
+        return max(lst)
