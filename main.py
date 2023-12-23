d02b47eff85fe8baba07720b03493bb1b65d834d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:36 +0000

    view release print watch upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def reverse_list(lst):
+        return lst[::-1]
