11b58ac75a074235198738cb0e0cb2d5a110744e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:20 +0000

    body friendly drive Nn steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
