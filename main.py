25dd30967846027484da0bedeb5ae3bd4a062d7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:17 +0000

    accommodation book friend flight total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
