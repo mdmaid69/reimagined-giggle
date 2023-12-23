1b055290e96f3053c7cb8724f799a07787225d37
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:39:41 +0000

    completely hit spread tell there

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_power(work, time):
+        return work / time
