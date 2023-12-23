55a003f23377debc48e55ada044828666722b544
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:14 +0000

    type long ago sure deliberately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
