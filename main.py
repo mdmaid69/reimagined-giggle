d9b5a47f8a9df47e6c966f1c2408a289c2d7f513
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:28 +0000

    deaf dear 179bite answer invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
