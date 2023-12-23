04f84b2a926e3403dac8a0e00caa9bfa6a1b05b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:45:19 +0000

    giveup straight team set gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
