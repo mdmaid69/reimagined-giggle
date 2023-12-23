55717a57a443c236b2e78b5bf883c96e590e2ba7
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:41:47 +0000

    brown sweep advise Tt height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
