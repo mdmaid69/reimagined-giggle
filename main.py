17d41786bdb2788c46aab381e5512eca66683d71
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:44 +0000

    lose whoever country bringdown string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
