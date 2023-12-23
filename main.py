31ae16e4caf254d6359cd4576e7853e3f36dc427
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:12 +0000

    build hand like none stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
+  def convert_to_hex(n):
+        return hex(n)
