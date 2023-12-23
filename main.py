1f4b203085374b585b10ebc9224c567d71593327
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:16:38 +0000

    total opponent either maximum fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_index(array, item):
+        return array.index(item)
