eccc368b2bd11b51ad39254eef41250518fb82e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:56 +0000

    tired giveout message skirt left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
