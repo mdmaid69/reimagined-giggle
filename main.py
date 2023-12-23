3e6b6a1b176fe5b17340cbd6aac7d34a858073eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:18:39 +0000

    command please Ss swallow mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import time
+print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
