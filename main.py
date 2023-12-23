309ab581606cf90a29c7aaa8503b1a31f1954e14
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:08 +0000

    arrive star answer through modern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
