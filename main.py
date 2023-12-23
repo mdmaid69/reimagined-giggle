57357a2c5dd425e66884921a69fa9c4b6694d68b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:41 +0000

    form store show sweet proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+print(re.match("h.*o", "hello world"))
