ae6a9a023d9a02891e89387c7e9a9a01c2cd9c97
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:49 +0000

    support cum trap workupto destroy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def square_number(x):
+        return x**2
