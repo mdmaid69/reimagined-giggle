c2c1356263d089d0dd958b534f7f36962b89a71b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:59 +0000

    note turnoff ignore alone stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
