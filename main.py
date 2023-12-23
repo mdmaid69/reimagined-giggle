1d03d0f8ead2da71c5936839216c17a2ba1945e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:14:03 +0000

    swing scream one sensitive road

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
