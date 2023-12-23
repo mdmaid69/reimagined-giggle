5114bac66d15b3217febd9a0da3783a391667d53
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:34:07 +0000

    sure suit train treat decide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
