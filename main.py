f5c50ebbb79d4b7a70fb25bf810be65fe43cc394
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:05 +0000

    probably patient getup put class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
