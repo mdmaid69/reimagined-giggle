45bd7429b66d7d5938bf08fd41785dd01fd1237f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:13 +0000

    comfortable pleasant himself dangerous shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+print(math.pi)
