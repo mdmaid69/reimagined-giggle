f6c090950205e9a81ea590d5a3abf682e61aa22f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:57 +0000

    thin inform sort that closely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
