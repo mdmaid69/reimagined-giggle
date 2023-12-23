1b188bac169e2e6b86381ce2bdad13480d685b1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:52:11 +0000

    569enjoy creature turn demand change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+print(math.pi)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
