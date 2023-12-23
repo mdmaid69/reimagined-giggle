a0368ba91191557372c50a6dc0ccee0e3e69764b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:40 +0000

    fire responsibility strike forever of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
