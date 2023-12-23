5fa2b21042cb0f927aecc8d3a58926615d568382
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:02 +0000

    corner powerful congratulations recognize of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
