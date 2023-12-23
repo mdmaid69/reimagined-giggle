8ec271ba89994cb53c3f119308ad7fe4099b780d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:43 +0000

    workout alone write best early

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
