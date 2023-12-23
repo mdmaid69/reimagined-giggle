1991fbbc03b443a05533f6e861cd99a0133b25a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:13 +0000

    spill behind along argument comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
