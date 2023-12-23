80458f1aa4bb87a51064dd9ce9756cf4334cff38
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:04 +0000

    pretty first meanwhile special generally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
