2b016ffaf19c3fe561ecd819b76de0595db530a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:29 +0000

    autumn dead judge sea improve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
