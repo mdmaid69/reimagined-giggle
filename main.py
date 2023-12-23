153d0d6cab9f084253842e63f84b68051ca2a418
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:34 +0000

    discussion idea national passage replace

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
