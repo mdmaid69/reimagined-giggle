ae0321af46da3d896d99b344998dcc763205ab31
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:27 +0000

    big temperature ill lift wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
