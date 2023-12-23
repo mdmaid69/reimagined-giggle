93c90370354b872ab5f515094e4e6f8165d2da70
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:09:31 +0000

    little union memory mad fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
