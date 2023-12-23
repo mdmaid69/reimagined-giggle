8f4d62b9ce792849ccbe72fd6f79ce82fa4a0fcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:25:32 +0000

    ready principle suit weekend short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
