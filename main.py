7a57c6036b4a5cc09f3b6e2a7b66762fd4c7d923
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:41 +0000

    freedom interest rent boring direction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(sum(range(10)))
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_typecode(array):
+        return array.typecode
