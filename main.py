0585d23864d85f7a8dc463feafde957c9b325590
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:56:10 +0000

    wish joke no dry makeinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
