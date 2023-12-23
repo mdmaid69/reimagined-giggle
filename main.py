b33747f399d6632b24bebd614c24a963e5dcffec
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:11 +0000

    main stop repair word favour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
