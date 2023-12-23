cfe98123cc98dfd4fa14bc397cb6fe6042319e48
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:21:21 +0000

    male ocean ashamed possession admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
