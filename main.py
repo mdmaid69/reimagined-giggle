696b0a7e8592e027fba6d473c62956017ab20929
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:04 +0000

    prefer wonder present combine cheek

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
