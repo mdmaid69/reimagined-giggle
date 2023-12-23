e93ab79d141a075d7534f093f2850f7a90c5accb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:47 +0000

    trust bringdown list forget eager

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
