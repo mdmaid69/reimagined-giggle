e4810523adc8abb14dfe7d4172e3ac89c2f60edd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:33 +0000

    actually feeling hold autumn sleep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
