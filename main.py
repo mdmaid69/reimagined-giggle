7d26a0bb2b73bf7cd0436d1fe0dd671536e410eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:09 +0000

    square embarrassing job no small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
