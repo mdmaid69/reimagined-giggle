0bb8bea134938e3ee1635d11100d5f417f5587e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:18 +0000

    authority comein comeback suffer out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
