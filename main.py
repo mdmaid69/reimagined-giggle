3336153661b34af5a775601f07bf851eae2fd582
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:04:45 +0000

    day slide travel account use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
