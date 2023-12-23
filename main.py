b614bcd482f9d5680faf5613ed12136767de40d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:49:10 +0000

    bringdown suddenly holiday frame factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
