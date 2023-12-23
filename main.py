0667c63fa063fac135514602d9c59c69794f8763
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:45 +0000

    Bb like command beneath empty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
