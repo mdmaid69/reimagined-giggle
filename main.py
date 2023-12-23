f2b6147bbce76f28ac540f2dee548624858620fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:43:18 +0000

    experience slowly criticize far behind

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
