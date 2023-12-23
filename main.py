3d3d48b047df709ce20f008d1759369ad17dfea4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:26:23 +0000

    attract little person used comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
