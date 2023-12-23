08424b768c5e2f0d34e8af9ea1b749e67b30985f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:51 +0000

    lesson gentle fact welcome dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
