669df18caaa0db0d4d7f51e7c65f8c166d8e589a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:28 +0000

    correct distance block cool meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
