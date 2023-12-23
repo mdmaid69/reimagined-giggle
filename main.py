0984d69418517874b71eb26f0471adc8368aded7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:15:59 +0000

    now measure match thumb false

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
