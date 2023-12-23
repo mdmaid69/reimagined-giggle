1b268a045dd15c99ec9ddfeec9d4ab60d083cc4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:06 +0000

    bringdown shoulder left glass row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
