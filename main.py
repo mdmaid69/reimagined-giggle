e020316c9da6ff5e65468a98c286c48b7220b8b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:38:45 +0000

    afternoon finger much seriously lead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
