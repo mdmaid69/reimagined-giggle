d266ce8ccc1728dc53ab14d812f5213d43b24b65
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:28 +0000

    burn replace move bush closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
