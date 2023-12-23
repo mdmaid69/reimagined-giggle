dcd9b92ae8b1a5ae6e62654d9c7ec7c54cc422d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:09:21 +0000

    past blue entry introduce comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
