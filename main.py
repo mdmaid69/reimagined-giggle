f9bd6acf6466ebb21f45e1fa456d7d01d1ba2e99
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:53 +0000

    hole kind equipment gap goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
