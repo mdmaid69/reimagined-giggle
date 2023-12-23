eb4c310d96997c3ae680e4e2e5e4574285542699
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:44:42 +0000

    hair wash mine quit progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
