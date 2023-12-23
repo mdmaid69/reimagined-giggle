f9443688a2300fbfabc45ba5df2a5e1131fb6b5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:50 +0000

    achievement occasional whatever murder lend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
