2a8370b69407e7418bf160ea610bde60f01cff81
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:19 +0000

    gooff ago tend begin train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_length(array):
+        return len(array)
