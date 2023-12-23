34400913249f32984e5e2acc7afc388a7fe96ead
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:53 +0000

    level light turnback fear supply

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print([x**2 for x in range(10)])
+  def square_number(x):
+        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
