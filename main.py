5c7719127930d38336fe8ab2111cf593bacd16e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:08 +0000

    happy squeeze you transparent range

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
