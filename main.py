8cd5fd38080870bde3191e340d416e0ba47f7bad
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:25 +0000

    description draw winter subject distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
