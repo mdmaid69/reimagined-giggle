dff4440e816db0e9817b2584eb50a10e03e8930e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:10:45 +0000

    main bit delight 568energy runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
