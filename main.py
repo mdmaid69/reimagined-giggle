56dc827259ee5c5f8ea630be956cdb7500cee2e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:57 +0000

    material for study comfortable breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
