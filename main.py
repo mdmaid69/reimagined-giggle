92d54c5467f08243353f880dd133cd87b467d971
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:52 +0000

    up turnon form build should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
