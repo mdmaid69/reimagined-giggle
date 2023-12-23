c2aabb958d62bb7aed8f37ff7fecac33cf6fdaba
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:33:42 +0000

    connect you form niggaacademy thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
