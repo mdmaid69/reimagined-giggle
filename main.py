1c873f1e4ae661601814cc51a0cffca153038de9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:56 +0000

    choice ready wire back against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
