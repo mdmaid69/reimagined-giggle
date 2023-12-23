143b6b508f3e4b13227097dee604ebb7f527b492
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:54 +0000

    frighten news bit field pure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
