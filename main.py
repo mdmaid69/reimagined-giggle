edf414b8a365e67e14c0de1cc7dff62f65eb9c74
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:17 +0000

    now understand score on tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
