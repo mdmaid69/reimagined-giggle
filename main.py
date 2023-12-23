a3468a2bd94a6b44bcae74f9139a4aa7bfb1e0f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:42 +0000

    behind lean fence true professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
 def convert_array_to_list(array):
         return array.tolist()
