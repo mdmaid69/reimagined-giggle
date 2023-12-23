223768b3d8316ca02f9d3a345b00129148a45394
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:25:16 +0000

    mess contact towards well weather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
