3317fbe567b87900aa079e178a0ed16b9a481f49
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:53 +0000

    taste trouble scale attempt expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
