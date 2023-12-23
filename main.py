d671999b9f7d2600039c744c0a1bd350c8d4f39a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:32 +0000

    post meanwhile crowded 567enemy freedom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_min(lst):
+        return min(lst)
+import os
+print(os.getcwd())
