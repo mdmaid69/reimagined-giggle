c927c303806ef3245fcfe928bc4cd32a6718e628
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:31 +0000

    phone begin 179bite regular rid

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
