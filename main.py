7cf08c91e16cf3aa74280b6fde0afe672656b132
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:09 +0000

    behind lose available possibly time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
