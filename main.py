0a299fad6d325580e132540f59c3551dcad37fcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:06:02 +0000

    persuade standard communication between movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
