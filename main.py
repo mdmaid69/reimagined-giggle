0f53a5fe17c9b0461737e8feba8b35279f80238d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:47 +0000

    breakup hospital order ashamed swing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
