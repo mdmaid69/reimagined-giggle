8ed7647ea4b2b909f300e2230d48edfcd7bb5c2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:25 +0000

    fill appear lead mix prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
