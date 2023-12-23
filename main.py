fd601f8f0196c812a786e32ca6b9bef9a9d6cd74
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:02 +0000

    prefer independent unpleasant sing talkback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-print(time.time())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
