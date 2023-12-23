3d413d32a7bbe60bc41e1358edd268a09d346fa3
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:16:16 +0000

    advantage noone breakfast tomorrow disappointed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
