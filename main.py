7b1ef0fd06db8e8d26bc632527d270e0bf8b0c5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:59 +0000

    season average fetch frighten relation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
