a85a751f50d5ed20005641c5d95708723db23eaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:56 +0000

    passenger politics time wall comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
