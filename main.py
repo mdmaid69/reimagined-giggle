0e07ce4d7018cefdbe5a94dd57583ef3c13bcd46
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:57 +0000

    born opportunity hour Qq search

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
