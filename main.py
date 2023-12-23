452096ec274908d683c30c9f0db61c30ef8ca4ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:33:23 +0000

    cause satisfactory receive dry judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
 import time
 def wait_for_seconds(seconds):
         time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
