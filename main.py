3d7f72435c750bf850f571c9a2160df6eed18de8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:19:57 +0000

    emphasize run prove anywayanyhow fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
