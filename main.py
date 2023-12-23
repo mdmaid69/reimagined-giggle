6ed0b31d70011beac6eb2436402f6f09b9bdcf4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:10 +0000

    bye centimetre shall 571enough connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
