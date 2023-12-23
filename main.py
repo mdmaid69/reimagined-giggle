c3dd9c0205d7384442fad5190ad07f78090b2ce9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:43:10 +0000

    handle mark return take receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
