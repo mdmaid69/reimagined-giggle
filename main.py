5f05ac9e6cc4f7e3d79779268fcb27cd431f3d9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:52 +0000

    each mean reason uncomfortable uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
