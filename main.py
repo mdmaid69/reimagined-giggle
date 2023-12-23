d140a972c7d763deaeddd16442e92e80c8a753db
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:07:45 +0000

    drink along often scare bringdown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
