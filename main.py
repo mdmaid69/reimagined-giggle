9e923e52ecfdbc28474bcf405cf3a5eba23bcc88
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:07 +0000

    sensible contain murder female concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
