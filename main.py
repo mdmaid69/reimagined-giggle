4c5ca42d6b65975dc580c63053e513c730081d8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:02:59 +0000

    goback(to) stamp sky ignore thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
