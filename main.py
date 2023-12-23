7ac5ae3ade54b9b09b2631e7ff6f703026cf6f01
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:24 +0000

    surface invitation left rush past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
