d82858310931bc666c9b39d0cb53f57812026bf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:33 +0000

    system create answer pollution thanks

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
