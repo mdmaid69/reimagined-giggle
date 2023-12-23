d97cc71e09acfc17bfcb7ebfefbc2b7187862247
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:34 +0000

    course go expect joke responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
