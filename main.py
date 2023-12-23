72005c68857ea44bd40160abec6075a023b2ce00
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:49:45 +0000

    frighten die legal transport work

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
