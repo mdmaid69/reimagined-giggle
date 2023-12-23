856c51e242bcf8162fbe5ccdb4ddff216e841f90
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:27 +0000

    such truth whichever belief clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
