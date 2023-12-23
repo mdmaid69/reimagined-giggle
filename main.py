fa933f64e03c0ca74260fdd362c8413ff19fe01d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:33:14 +0000

    size bringdown basically speak important

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
