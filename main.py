91dcf4b7f772e1497bac736769e2e8fbc27bc3c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:58 +0000

    copy thin national unfortunate through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
