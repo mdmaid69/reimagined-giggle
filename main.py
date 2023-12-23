607c96aeeef7f1293f077548ceab43e6f818fbe5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:57:26 +0000

    unable look union explanation bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
