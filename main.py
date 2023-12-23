634c23e37c69d96c30bab9139befead7ec8594e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:08:02 +0000

    brown physical flow journey failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_list(array):
+        return list(array)
