301b2e4805bca19cc895d514d732f25b8dfe46a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:47:49 +0000

    steady slide serve comfort trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
