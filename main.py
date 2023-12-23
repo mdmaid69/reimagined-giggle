bec9ac94eef46008d540e3e8bffe4f7f468cd534
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:24 +0000

    respect its image thankyou switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
