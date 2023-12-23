d58daef876b4f6ee8b3d396227399619f2acb8ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:18 +0000

    pattern appeal firm work manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
