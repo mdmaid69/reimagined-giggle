c004b5d47d9de39edde899d8aa8520da2ee65d94
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:45 +0000

    comedown goup listen turn style

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def reverse_string(s):
-        return s[::-1]
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+print([x**2 for x in range(10)])
