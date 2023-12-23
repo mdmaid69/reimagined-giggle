cfb0b8730cf017ad4c0617159e3fdaeb39157370
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:40 +0000

    considering correct surprise have clear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
