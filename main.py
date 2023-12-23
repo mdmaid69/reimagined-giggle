fe4a7cbb4cf2d2e8702d5c06963e36146dd2f2dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:34:36 +0000

    secret gooff work early flavour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Characters:", len(text))
