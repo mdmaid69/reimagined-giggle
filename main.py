4e0964d3a45d52b8dc596e6a23df3598af6230c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:20:49 +0000

    rather goout anybody report danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def reverse_list(lst):
+        return lst[::-1]
