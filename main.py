8a9fbdf2f9cbfc45ab405f40ed1bdf0094198b0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:21 +0000

    thinkover spill Ii think strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_item(array, i):
-        return array[i]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
