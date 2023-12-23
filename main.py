7d0e538e69ef551145de65c10e4b362a7c51068d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:18 +0000

    geton tap unusual talkoutof me

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
