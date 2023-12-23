c9c834fc1557932b59cf3db8b0e465e6248a88f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:21:26 +0000

    display hurt able rain worse

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
