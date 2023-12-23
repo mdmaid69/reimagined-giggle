5dca912ba98d371334b4e0ce7ef28d683f89f464
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:08 +0000

    practice modern uniform give contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
