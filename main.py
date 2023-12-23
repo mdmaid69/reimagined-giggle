de4cbc82ccebc72aa2c3786b3c9140ed19b86a20
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:01 +0000

    talk music operate worried suck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_power(work, time):
+        return work / time
