c714834c6ca1b07bbb27f3bb536708f9a4808cf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:19 +0000

    flat future or slowly exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
+import os
+def remove_directory(path):
+        os.rmdir(path)
