08edc995ffc3b70522f05b5b8003d8486d264bc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:36 +0000

    objection pause rent juice allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
