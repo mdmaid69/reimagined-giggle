877bdea1c6f85dc831963dc632116ba7fb540375
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:00 +0000

    keepaway careful nor combine team

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
