264dce21b812b4d77011ac75d395e53690527d2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:47:03 +0000

    shadow further production reliable cinema

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+print(re.match("h.*o", "hello world"))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
