7d52e79d4002ee9a0b5d2d938a3f7bc064e9e9fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:40 +0000

    criticism skirt direction minimum problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
