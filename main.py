69de80c8fe99739732fc319644e13c6e0d00e6bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:06:22 +0000

    type mess suit team table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
