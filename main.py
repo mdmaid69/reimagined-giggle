6189cdfae065617b60acac7cbf4bf7a902bd0359
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:18:34 +0000

    slightly conclusion wet responsibility air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
