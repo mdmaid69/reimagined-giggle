0fe82fc477d831a3a4d21a2a6e05b788e14bf12e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:00 +0000

    possible general respect advise drop

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
