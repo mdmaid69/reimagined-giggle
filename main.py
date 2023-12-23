54e8ecb10d311af74939e27d1019d7e05c9b76bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:16 +0000

    false slip us century make

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(numbers):
-        return min(numbers)
+def calculate_work(force, distance):
+        return force * distance
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
