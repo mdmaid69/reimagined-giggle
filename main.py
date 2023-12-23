bb58dbca418c79574d7c389d175713b4d1bd38dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:11 +0000

    value habit right runaway understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_speed(distance, time):
+        return distance / time
