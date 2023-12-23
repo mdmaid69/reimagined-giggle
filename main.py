f03c6d2e09b934af5d755c492195c6f56fae4389
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:42 +0000

    female winter pinch your pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
