329c1bf561fd4568e64746b31fd8ef657e27813f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:35 +0000

    rise raw previous exploration guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_array(array):
+        array.reverse()
