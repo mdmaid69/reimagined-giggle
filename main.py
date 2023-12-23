4b3659b2682e24673b6781c58c90c7f9b9f1869c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:08 +0000

    understand convinced laugh pleasure onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_item(array, i):
+        return array[i]
