2aae95dcb4059a22f4fb3093b6812602b2a85e50
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:29 +0000

    many ordinary area base movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
