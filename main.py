0d2b81aea551bf5a00a2cd78f3c85752401dbc4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:07 +0000

    proud confident every sometimes pump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
