8df68d122e5cf14d9ff1a47ab51f87a45b557f8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:29 +0000

    scared hit luck medium medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
