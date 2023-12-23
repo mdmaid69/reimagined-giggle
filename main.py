fec2de439b5b25b38293a9d07a650233780821d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:29:44 +0000

    best dinner 564encourage rain what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def list_files_in_directory(path):
+        return os.listdir(path)
