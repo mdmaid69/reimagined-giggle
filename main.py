8d3fa1d077b958652c8368801e564b5c40d3a85b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:22 +0000

    clue declare run keepout brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
+def find_unique_words(sentence):
+        return set(sentence.split())
