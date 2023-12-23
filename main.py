be7192d265339b7be6c786230e586bcfb035ce86
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:03 +0000

    beautiful pronunciation count run afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
