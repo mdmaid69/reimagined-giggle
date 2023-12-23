41a574ce13f51978a78bf828d4eb488bf2a8dd8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:58:25 +0000

    knock priority possible left 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import time
-print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
