9320993f807290c9eb2618fd2d1b8297d33690fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:04 +0000

    punishment house first tape fresh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
