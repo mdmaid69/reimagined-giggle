44fa26b0425076b96cf4790cf44efe0ffe742a73
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:45 +0000

    Ww picture hers uniform shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
