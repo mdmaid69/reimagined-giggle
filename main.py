4f19e5c7fa8af7939312da9ad2a8dd02f8aaa566
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:05 +0000

    film either old copy delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
