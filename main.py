fb0da64fab449f26c0ad9436d0b1d9c48e7b1401
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:14 +0000

    regular likely crazy measure adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
