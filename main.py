a6d417dca799280515d46aa8f0c75d8081730abc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:17:56 +0000

    land choice close judge squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
