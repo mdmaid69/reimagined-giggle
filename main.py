5316a3c5ef4ca8a2f0b9ea3fa0028cbe07a23a9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:29 +0000

    suggestion real bar describe breakin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import random
-print(random.randint(0, 100))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_sine(x):
+        return math.sin(x)
