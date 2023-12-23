44ce97bde9665fd6d5be1c30a183ed86bbf7dd46
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:09:02 +0000

    print unit floor use callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def square_number(x):
+        return x**2
