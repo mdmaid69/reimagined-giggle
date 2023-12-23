edeb597cf2912ce2687cb14604253ec97cc4525b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:39 +0000

    cheek difficulty major rich history

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
