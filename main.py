66c53d654895f85529eef6699b623f4c23aa5ed3
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:46 +0000

    wing paper scared clean whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
