692c5bef10d74b4e56bddf0ab44e1c1e6bc45add
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:53 +0000

    than job field down add

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import json
+print(json.dumps({"name": "John", "age": 30}))
