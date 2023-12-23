9333f7f26127157dacdd2c8f33bc1b5398b74583
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:26 +0000

    consult trap listenfor name wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
