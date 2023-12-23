96908caa45c1f4937952aee52b40ad78aae65a45
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:28 +0000

    bring total rough nervous reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
