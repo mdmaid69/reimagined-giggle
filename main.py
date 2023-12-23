3da41c61fe72a4695a27d49a7537461b27e71b5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:20:02 +0000

    calm whether material satisfactory on

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
