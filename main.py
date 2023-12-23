2efbc8332e05c5df21ebc00b19e29f882911dfbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:06 +0000

    pen when confused disagree year

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
