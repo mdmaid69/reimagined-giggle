6974f525e2c7f658d9df7f5d9144a5292f04a5ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:31 +0000

    load fight cook allow really

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
