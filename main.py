c0f2e8a40a5b2c27b584a2dbd6a3d3fae6e1f765
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:59 +0000

    basis light dry transparent skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
