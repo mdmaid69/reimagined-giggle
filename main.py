e78203889ba79e0a61e31754e1c511a78e98cd3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:09 +0000

    reasonable provide own Mm take

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
