a4d37af199d04b1e5f271982ee1b82866e2ce063
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:50 +0000

    business them anywayanyhow form herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
