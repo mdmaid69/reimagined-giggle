d4efe3a6fdbcf399ee9e57ad2958f0259666dc0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:17 +0000

    glass fight front switch day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
