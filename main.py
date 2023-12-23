688f109b688829d177482ab06488d73fccc2a0f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:31 +0000

    direct continue aim private frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
