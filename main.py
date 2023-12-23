22ac4a721b27beaa9a6aa99b8819dfadecbd882f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:25:36 +0000

    follow doubt load nor it

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
