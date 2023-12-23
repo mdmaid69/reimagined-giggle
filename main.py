6b32f77e68f93465aefd36b15064a2e3ca2c56b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:59 +0000

    impressed set brush attach comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
