fd89b218f40141a93bc128e40caa2d8dc7920b42
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:35 +0000

    power strict score middle hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
