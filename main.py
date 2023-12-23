2fe5c57b2c13fa7dee8296f1271be216ec29b8af
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:27 +0000

    smell lookaround dig bye straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
