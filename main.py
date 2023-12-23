d43f1390ab2667903aa925cbaffa4dd766579a9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:22 +0000

    accordingto in exhibition accept free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
