c9868aa226d8983da34eb541ccc47a146b3edfc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:35 +0000

    around later adjective sorry aware

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
