16a4e21425b9d502b180eca1d8e965dbb2bf78da
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:06 +0000

    public language treat cook appeal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
