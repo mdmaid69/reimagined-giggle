088b402452db5afc2ecec8e9e741054711e0dde9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:38 +0000

    smooth operate typical that Gg

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
