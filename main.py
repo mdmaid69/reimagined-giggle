9fe2365b146d996b3e30b373c5b8573a7189c23f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:50 +0000

    future course name through break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
