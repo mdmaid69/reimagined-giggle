13a4718b45bde72f4e126d7729471cf8e2911294
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:35 +0000

    thumb this relief butter funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
