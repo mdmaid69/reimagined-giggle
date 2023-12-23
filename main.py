5d0ec5e38b702898c125f55d87528ba143ccc19e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:58 +0000

    demonstrate emotion receive emotion Tt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
