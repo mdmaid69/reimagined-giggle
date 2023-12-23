e6cb6105eb9586f350c1bf50766ff0f2d8d309df
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:40 +0000

    wealth them awake buy pain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def add_numbers(a, b):
+        return a + b
