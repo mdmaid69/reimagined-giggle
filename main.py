c3c14208f74a6d06f3cd39c26ebcedf67090f5bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:49 +0000

    tidy poor legal cut block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_binary(n):
+        return bin(n)
