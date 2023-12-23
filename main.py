25e07ef6bda3415b20f501acedbb1b6bc425974b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:17 +0000

    genuine remain classroom bad stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
