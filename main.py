8437aa764291fbf4fe1bc585ddaf07bd553f3036
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:17 +0000

    frighten impossible pattern entrance keepup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
