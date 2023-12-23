4d1d0338368360f8336de11abef4207511793a71
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:55 +0000

    opportunity quit fall day polite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+print(math.pi)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
