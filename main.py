1fa3b9458d64f496101ee5fc0de7192200cdf7b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:19 +0000

    trust form introduce keepup need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
