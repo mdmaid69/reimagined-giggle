0bbbe61872a7f14d96264737996013f6f222b841
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:48 +0000

    exam block rent hospital comeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
