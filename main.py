4b24a651f6c0ef6eda975c51d98f734e89c9e3cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:03:34 +0000

    along country allow see rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
