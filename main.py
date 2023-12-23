835ab83b1c2f65799b2d285dbe4c5e55a59d5730
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:55 +0000

    point work prevent squeeze over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
