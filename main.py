ad3f58150e13cf97e76a434bcbd87defec20fdac
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:09 +0000

    professional attempt bin perform excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def convert_to_hex(n):
+        return hex(n)
