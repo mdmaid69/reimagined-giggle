5b4280b74e85bb4061d494d0c442e7a4e9da499f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:55 +0000

    distant future declare calmly up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
