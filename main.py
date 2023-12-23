6cb9d5901f99b5a6a6f8b2cbf212f98c5a0c60ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:14:24 +0000

    mine wash anybody together minor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
