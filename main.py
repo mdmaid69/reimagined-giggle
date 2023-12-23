b5f8f1226dbbd5baef7893482811b065f478d1e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:04 +0000

    fashion desperate spill holdup afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def remove_directory(path):
+        os.rmdir(path)
