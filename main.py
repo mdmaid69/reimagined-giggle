22166a42d5f814a7ae315ff92c76ef4cb41ff518
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:48:55 +0000

    makeinto receive classroom pool nothing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
