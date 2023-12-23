07e61377458870577b33eb2f55e04afdfadf4b50
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:59 +0000

    neither tired that follow inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
