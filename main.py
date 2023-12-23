c9e2a6d55eb25c671981d138bfa5927fe2c858ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:25 +0000

    set keepfrom towards knowledge more

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
