04f2aa379233eb97bffb1b7fac9c9b3469f5c3ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:45 +0000

    prove affect polite coffee hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
