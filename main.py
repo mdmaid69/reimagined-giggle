5d66ac97c2877494d828e2407a0ad3ab20be240d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:38:21 +0000

    loud geton between shake discuss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
