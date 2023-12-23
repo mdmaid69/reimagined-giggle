09cb7de0ff347fc251c0deda7e207f33cd4600ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:32 +0000

    remark interesting seem shock add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_list(array):
+        return list(array)
