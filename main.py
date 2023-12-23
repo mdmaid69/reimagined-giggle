c92f67fffed8669ef6b07106eb9121940703a713
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:06 +0000

    choice Ss rare careless port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
