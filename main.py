d97d73c50caa78685b8a575bebe3461245ab2bda
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:58 +0000

    like pile hungry consider trade

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-print(sum(range(10)))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
