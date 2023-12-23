dca3a670203975b4d5a53a76803fd31414d0a81a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:09 +0000

    understand region equipment snow prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
