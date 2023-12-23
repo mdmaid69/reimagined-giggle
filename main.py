628346e85306573c8ce1a099434da598dc2d6433
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:22 +0000

    copy common throughout emotion yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
