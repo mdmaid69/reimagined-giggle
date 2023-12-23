c4a7b5fe2127025eb18ad6f51fe6877d12358e1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:38 +0000

    no direct busy future central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+print(math.pi)
