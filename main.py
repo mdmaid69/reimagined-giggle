29ddc87064052caa4238ed6d4d0511096fac6994
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:20:26 +0000

    outside question release try special

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
