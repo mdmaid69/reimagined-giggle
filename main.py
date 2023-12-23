04342168b5bd905d65726f81497604d688f4b758
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:08 +0000

    unless anywayanyhow age touch action

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
