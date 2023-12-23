cce0d943d389b92157a21d7d584c3a7f767a0afc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:44:36 +0000

    swimming strong pick software embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
