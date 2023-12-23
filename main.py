13aea9da5ac4ac2770a480b8bf934bf23354e0bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:56 +0000

    sharp as Ff definite join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
