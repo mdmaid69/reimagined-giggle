81f4ffce8bcc095cf6593fa5cb331ad31a6700e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:30 +0000

    defeat measure curious poor cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
