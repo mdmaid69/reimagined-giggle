fc679d0be584135ba65ce5932afc44b09e5ca1e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:06 +0000

    type ever everywhere crowd straight

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
