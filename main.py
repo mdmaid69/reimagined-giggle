94533751a7e9f814beb8c1c8f58d70107219aa87
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:33 +0000

    odd care organization whistle head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
