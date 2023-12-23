632611c9246914e17fcf46e32bc999487a3b2aa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:30 +0000

    criticize suggest noise lonely rare

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
