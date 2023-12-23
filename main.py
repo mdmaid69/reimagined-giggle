c9f72573ba3bcab6f70603e04750ff94662f880d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:17:33 +0000

    medium land advice bet shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
