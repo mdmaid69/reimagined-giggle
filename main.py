25b2782bafd9f67fbec5511e9608e4bdc79308d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:00 +0000

    pity radio sound radio promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-for i in range(5):
-        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
