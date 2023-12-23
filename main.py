7a6df139c1311773c0d9eb86ca00e173ba334424
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:01:59 +0000

    yours pocket unfortunate hurry treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
