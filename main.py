6f765f420ec757b41d470885acb12204eed8f86b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:49 +0000

    sit above fix surprise allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def find_max(lst):
+        return max(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
