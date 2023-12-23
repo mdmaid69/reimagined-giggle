3c84271b35135ce56d9a40b110064c92313fed3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:12 +0000

    comment late shrink impatient throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
