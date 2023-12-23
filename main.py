73723b1353e21a956c0eb76720b013c60d26fbf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:40 +0000

    fact off mouth chin pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def reverse_list(lst):
+        return lst[::-1]
