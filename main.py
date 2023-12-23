b2bb05f879f799516c239a3791cb45eec019f481
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:36 +0000

    let somebody weak kick Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
