3d64ab7243d74a92c0ae8920d2ad7f0a1e0c3831
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:40 +0000

    weigh Dd talk laugh related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
