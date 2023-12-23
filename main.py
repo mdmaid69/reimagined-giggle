d13c41b89078fd0cad4dbd35729cd0554495d659
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:25:51 +0000

    evidence exchange firm culture loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def cube_number(x):
-        return x**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
