b32d75bc534728221912ede8be1dc4970388c4eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:22 +0000

    regularly disturbing anything turninto safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
