e36c5e77d6755b85699fd507d0fa8f6a770f7b87
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:13 +0000

    scrap spill there getoff afraid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
