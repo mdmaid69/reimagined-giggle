7a0e1fd8caebc6c78a49c1d5b6dec8f74b8952aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:18 +0000

    means label boat Dd apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
