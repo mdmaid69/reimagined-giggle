5134d4c7f8e04899a1dd43c844466a09fc3ba413
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:26 +0000

    turn comeback knock professional date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
