dee17212c93dd79050dd947a7d519e3777372aad
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:38:49 +0000

    getup pause loan smile guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
