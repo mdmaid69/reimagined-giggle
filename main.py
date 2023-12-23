63d95d21ad14d941b0a2296d42096c48b5896ea4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:46 +0000

    image deserve temper room paper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
