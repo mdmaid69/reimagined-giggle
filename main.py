a164b6b442dfc49e2e6f99cf6cf50d2b86aa0108
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:09 +0000

    sort earth limit direction either

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import csv
 with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
