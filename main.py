5b91c04557cadc9c05c1ba8bbe946f728db61e11
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:01 +0000

    none tune undo Ii usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
