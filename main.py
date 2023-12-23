dc5fc59296cc387c1deb0700230e763b787edd1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:16:25 +0000

    history 565end constantly properly safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
