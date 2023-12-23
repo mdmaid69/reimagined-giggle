3bb6d2e682111fc128b09eb515588e04afd77088
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:12:27 +0000

    turnaway ocean themselves aim spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_rectangle(l, w):
+        return l * w
