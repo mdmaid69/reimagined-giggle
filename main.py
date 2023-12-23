9c63ed9c464e2c8b73f26bfaea880b347bd1a36c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:46 +0000

    original bar cool professional regularly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_current_directory():
-        return os.getcwd()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
