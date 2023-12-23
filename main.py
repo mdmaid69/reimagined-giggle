915b9efcabb6e2f7c2e5f3fc9b611b2b0036cbdc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:43 +0000

    please drawing offend him certainly

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
