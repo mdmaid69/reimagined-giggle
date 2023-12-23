e01181aa5133c308eb826f74afdaded7bf27e8ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:02 +0000

    confuse even possession argue fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
