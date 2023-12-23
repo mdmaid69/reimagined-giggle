3c29d68a743bfe67b9f93e9615bf05ef06a19f33
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:01 +0000

    impression really record class use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_cosine(x):
+        return math.cos(x)
