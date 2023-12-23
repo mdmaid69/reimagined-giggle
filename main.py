a0781a9b7ac2366f9b6a4e7b055d5375d5faaa06
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:35 +0000

    tidy bunch divide party instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
