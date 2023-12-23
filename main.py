22f66b4d3d562a9a1304f016c3f66db5f814a2a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:53:45 +0000

    person fire reply sort eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
