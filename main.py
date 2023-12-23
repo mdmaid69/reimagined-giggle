d0e80431b8571f8716282679c732ed87b9873632
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:39 +0000

    please unfortunate judge but means

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
