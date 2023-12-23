611bae9b71731842b20fa48c31092e5ee3ea0b31
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:54 +0000

    feel total hard reserve situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
