a7e31e06a3b27eb438a1f44ef4fda59c7089d089
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:28 +0000

    rush side marry second system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
