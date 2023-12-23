0902f87867424d560aad8e99b78ccff5cfb26885
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:52 +0000

    nurse hang healthy strict permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
