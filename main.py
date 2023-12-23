62eb5c326746bfcd4905aa18041960851e1fa119
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:07 +0000

    cap miss rain involve alive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
