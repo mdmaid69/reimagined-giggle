f8a253b54ae74ab2add55416300ca454b3ff0655
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:04:05 +0000

    broad catch straight full food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
