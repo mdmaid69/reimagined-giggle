607af1cf1fb6347067ac22d091d004cfffa3ba45
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:39 +0000

    period hot small discover loose

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
