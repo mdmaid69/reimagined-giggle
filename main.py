2f18b0f90c8119d850e748673da4f06b70336e51
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:49 +0000

    upstairs collect entry pain themselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
