c279bb46b91332844df162130cadf44784465ee9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:21:19 +0000

    something smile that public makeup

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
