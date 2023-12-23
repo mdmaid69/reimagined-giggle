ddd9525c340f6c9d743b11200ee60c931014009c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:53 +0000

    centre excited turnover mine hope

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
