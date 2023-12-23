bf319f8711781875258c58424fdbe6e7a37c1a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:02 +0000

    back before holdup genuine other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
