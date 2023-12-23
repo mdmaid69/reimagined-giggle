521cb8fd4aa14dbd87466c2c97d38a08f01f7322
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:01:26 +0000

    scare receive air fetch build

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
