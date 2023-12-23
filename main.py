f9e5dd7a01cab8dbbc510d1e57a16c0861da5305
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:58 +0000

    noise smoke will plastic group

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
