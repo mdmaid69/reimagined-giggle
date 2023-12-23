431cd3acda74c8f19c0594fd278311688f1c1e6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:14 +0000

    rough chat past surprising summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
