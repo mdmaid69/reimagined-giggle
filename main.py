91473eb4a38e51265d6dc03ae5b5fe46310da8f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:36:44 +0000

    contact first time few stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
