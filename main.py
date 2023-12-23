c094cfa7354b68461bcb3e994f86475fd45f503c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:45 +0000

    usual last even entrance because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
