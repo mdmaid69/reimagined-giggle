e2e692c1c266fcb6062a2e8608a5e7291beef1c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:00 +0000

    passenger contents plan married undo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
