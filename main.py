549f97a026b1db569aa4ab6c4c3083d6d54cdaee
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:35:38 +0000

    near whistle gap seriously suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
