02352b61bd615df7f0ce0c62d3c0e51641e1b374
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:27 +0000

    middle burn soon murder successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
