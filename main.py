59c0c35e9dd5b9b5d5ef236d1fd8fcd11c38c0ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:04 +0000

    hold priority recent switch slow

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
