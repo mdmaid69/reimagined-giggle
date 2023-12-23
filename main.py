80480657e139a69f6c6e1732c751808297628211
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:46 +0000

    land thinkthrough detail trap content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
