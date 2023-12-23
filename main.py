47ac6d09230db5a18e0be90f1f3acc6f3cf637d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:31 +0000

    tidy past copy present foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
+import collections
+def create_priority_queue():
+        return collections.deque()
