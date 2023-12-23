ba4e0008cc7806fb65eed22e85fad66f91341d28
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:57:20 +0000

    hope argue control play though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
