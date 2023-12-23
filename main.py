ea213547370490da217f13d0c5de961f5116d272
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:17:04 +0000

    follow pronunciation might long rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_int(array):
+        return int(array[0])
