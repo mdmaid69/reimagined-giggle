4be59cfc618bfce3fcc50975924b8a03d4adc3a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:51 +0000

    only sit ocean official fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
