37d32e2d1b3bc618191ee88eb8e49eefc4bfaed9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:25 +0000

    dance expect tonight brief face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
