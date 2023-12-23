808ab3447a69020847092dd5b09f589ea61bd9fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:09 +0000

    nose fork threaten write trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
