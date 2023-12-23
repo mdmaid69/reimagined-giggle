b636aa67e7dd734b88264a551815abc080c2ca3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:31 +0000

    deaf expect culture content suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_sign(x):
+        return math.copysign(1, x)
