cbd8eac73b538ef2f6b8fdd37498b1dd177d75ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:55:18 +0000

    declare tap majority pollution official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def remove_duplicates(lst):
+        return list(set(lst))
