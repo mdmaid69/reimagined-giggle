f405b6b47aa2eb8c4ab9e072a6d09c434fa2ea99
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:48 +0000

    cost sentence relax mean bang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def subtract_numbers(x, y):
+        return x - y
