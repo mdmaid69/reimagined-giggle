1f4ead4c0b168e9ee74f130739c2dcb48248c828
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:57 +0000

    bet team broken tomorrow design

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def convert_to_octal(n):
+        return oct(n)
