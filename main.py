28eba6ad5d3015e6910dccb102dc6ceb9054a020
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:20 +0000

    light present excellent able seem

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
