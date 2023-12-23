d18528a4c695b9063947cad3602d8b64cd742a6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:34:23 +0000

    tour cause rough dark news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+text = "Hello, world!"
+print("Words:", len(text.split()))
