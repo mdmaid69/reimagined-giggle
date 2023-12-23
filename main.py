c46b8ea848a26d166b0cd1fa9acdb2e5b25c38eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:00 +0000

    more beautiful underneath can responsibility

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
