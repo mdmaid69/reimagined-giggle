d0c86b42911dd317bebfe6da7c0c6c5f93f435be
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:37 +0000

    towards proud giveup wide responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
