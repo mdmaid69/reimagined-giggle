fecba5da79b28a9c221cb15a85f5105879cb75c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:20 +0000

    waste radio afford number rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
