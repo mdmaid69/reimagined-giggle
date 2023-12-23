ed18d1a4b5597eb1f866fe82ef4af8df535b13fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:51:57 +0000

    always radio chief equipment slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
