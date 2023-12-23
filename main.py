ff4af80929fd63fc8f9d16541303edcbb25c4949
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:20 +0000

    serious exploration challenge decide shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def reverse_string(s):
+        return s[::-1]
