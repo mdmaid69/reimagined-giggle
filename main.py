f8c18e6e0739158ce70a59ea32aa970cc550f7f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:29:07 +0000

    back weekend kick design against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
