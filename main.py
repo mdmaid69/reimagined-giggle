47e9339e57a600b795708845eb1c8d20a5cd01d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:50:08 +0000

    every drop unhappy shout ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
