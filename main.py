9914f8ac00e648550371c43502018c609ec19ea1
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:37:53 +0000

    disappear calloff immediate badly slip

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
