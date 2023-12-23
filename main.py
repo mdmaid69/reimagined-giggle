7aa56a47929a530a369760efb111fcf01a0511c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:57 +0000

    certainly cost often hit negative

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def subtract_numbers(x, y):
-        return x - y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import sys
+def print_python_version():
+        print(sys.version)
