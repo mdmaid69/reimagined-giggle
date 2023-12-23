049fb4ae9929e6eeeb9f6d9449aab3925974577f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:09 +0000

    factory order speed dry high

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
