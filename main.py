9a47ee302998eb1de6fe8dda10ff540f457ffbdd
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:41 +0000

    honest discovery so luck contribute

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
