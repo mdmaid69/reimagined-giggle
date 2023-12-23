22fe700049b90372bc50e8bcd205157d8c1ea1a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:49 +0000

    seem object shop loose means

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
