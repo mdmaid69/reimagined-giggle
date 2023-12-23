fd7cc1b3493790af67d024835963b3f346d6f25c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:26 +0000

    safe suffer believe pain square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
