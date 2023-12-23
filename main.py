7a77c69a1598db0b9e1181ceba267beda1eebdb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:35 +0000

    escape injury tell late though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
