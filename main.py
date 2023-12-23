b87655cf5b4401169d517370b76c56bd5ce7bf0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:31:17 +0000

    declaration match generous willing pin

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
