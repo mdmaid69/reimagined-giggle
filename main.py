a309b2cc28a899c3d83e50d0c2a32bd9d7dac35a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:56:28 +0000

    wrap later myself truth pattern

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
