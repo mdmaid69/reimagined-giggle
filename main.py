1dde98a6d48664523461839129176af0840eb946
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:54:40 +0000

    man repeat sting building fine

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def count_elements(lst):
+        return len(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
