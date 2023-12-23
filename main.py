1b6e926b69124c289c8d2aa6f7758c2e381e0a83
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:49 +0000

    wipe share corner tough recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
