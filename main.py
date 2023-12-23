29672e945af8350272610333035f004a7e70d57b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:55 +0000

    office fall exam repeat public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def get_array_as_tuple(array):
+        return tuple(array)
