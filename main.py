b640974310ef8cd74bbaeee209c1a6411d9326d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:28 +0000

    quarter mad may miss minute

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area(radius):
+        return 3.14 * radius * radius
