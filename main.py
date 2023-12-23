7f556e85cbcc97a31dccc51199acb2660e7b6469
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:16 +0000

    law hold software any comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
