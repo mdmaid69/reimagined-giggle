7bc5c59435fbdfb3166917cae8199206491d6744
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:19:51 +0000

    needle soak when top convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+def exit_program():
+        sys.exit()
