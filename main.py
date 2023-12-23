56573324a5da88ea0b94368b19f952584d978d18
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:25 +0000

    cost Bb uncertain reason definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
