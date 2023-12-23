5480ece334eb2aa1f1c64268b8939aed83a416b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:51:26 +0000

    practical prepare rain morning pin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
