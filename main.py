226c4b321ec534534ba13d18905c7f5aff1c1c2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:50 +0000

    operation calloff affect congratulations slide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
