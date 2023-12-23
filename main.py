7bf9e3c3cbeeb6ae33a7b1683f6f4eb913ee0088
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:18 +0000

    pretty butter interfere home mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_density(mass, volume):
+        return mass / volume
