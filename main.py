e8780d84126b8df196c8adf078c3c94d4c631fb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:40 +0000

    their complete professional raise narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
