a2bfb8027830fede540af00ad7fee7956e8064f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:01 +0000

    here him reasonable address getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
