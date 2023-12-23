7aece97e600ce86a10a04248556f049cebe980ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:02 +0000

    movement sensitive 572enter tell mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
