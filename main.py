ce4b1b0c7b9fbc04ecbb129079ed3e9d49cd8c64
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:43 +0000

    special law clear breakthrough visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
 def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+i = 0
+while i < 5:
+        print(i)
+        i += 1
