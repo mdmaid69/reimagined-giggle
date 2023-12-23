61e89938742ac52761a515cfe553033524d5d1fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:05 +0000

    load shrink ought badly spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
