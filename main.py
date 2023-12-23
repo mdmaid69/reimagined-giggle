95a8a0595ef7b234d0d8ed378d3ade80e5315d56
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:44 +0000

    fit minute flat listenfor reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
