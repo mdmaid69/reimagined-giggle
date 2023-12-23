f4106bb3779bdfcfd896391b7847ab7c1755abc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:54 +0000

    drive achieve hold comeup too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
