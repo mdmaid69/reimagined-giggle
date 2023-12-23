6615b7d97821cee0578776dc82f52f3739f45884
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:30:52 +0000

    your this false valuable meaning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
