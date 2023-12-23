a4bc11557a3962a7d97253423d136bcc5b5434c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:53:14 +0000

    goup school minimum argue wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
