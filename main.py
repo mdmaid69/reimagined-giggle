9c4c34cf2ca694c50d9830e6473afbddb7373066
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:10 +0000

    only fun holiday between foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def change_working_directory(path):
+        os.chdir(path)
