60fd8059db7fce879627f23309bec0b8603ffda6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:55 +0000

    harm temperature snow arrival total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
