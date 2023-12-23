82db8373169b252d572068fda9e0346b2f3faeb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:46 +0000

    same breathe base finish port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
