b1a94764f54de6c43589fdc9e18f0c3986320701
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:38:42 +0000

    fill exhibition move prevent disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
