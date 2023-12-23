d252036a7d163df7f58c976fae9d384115a0bdd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:47 +0000

    long position determined able clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
