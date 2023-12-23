d2881e8a45ad0a91971c5ab8e9db32fc491d2e9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:26 +0000

    escape suggestion safety worry with

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def find_max(lst):
-        return max(lst)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
