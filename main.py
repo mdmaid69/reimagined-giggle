709df3de1d7de320d4a0b1ebac24547a54d2cffb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:47:32 +0000

    behind mass sight benefit willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
