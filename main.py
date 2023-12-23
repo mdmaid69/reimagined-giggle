d5e089a25cb0327ac216690d17c964d91253c23b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:38:04 +0000

    message fear emphasis shrink figure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def square_number(x):
+        return x**2
