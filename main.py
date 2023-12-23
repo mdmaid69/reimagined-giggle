fd2323ad3235668d147c8a30d980310acda6670b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:01 +0000

    steam discovery about very wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
