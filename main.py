783861b0f1c6079e145da2d7894c9ec520e8e847
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:44:36 +0000

    flood giveup correction lay proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
