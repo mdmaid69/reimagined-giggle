f910bf3fc49d8d670959d384f255bc2d98c3bd09
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:27:51 +0000

    succeed turn culture answer his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
