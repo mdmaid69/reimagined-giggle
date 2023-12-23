91a90a8dd2506c4dabd03e54cd74f3e3377d2bbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:47 +0000

    frequent repair recommend form upstairs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
