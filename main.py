59e9d667b4bddb9646bdfdc8fa2a2679baf982a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:31 +0000

    beside offence hard talkoutof general

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
 import math
 def calculate_cartesian_to_polar_coordinates(x, y):
         return math.rect(x, y)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
