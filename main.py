2d7be08e102667f5dc7264dea96580085eb37139
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:01:21 +0000

    colour possible label impression authority

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
