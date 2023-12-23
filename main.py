03b793881c42f69754ad1b851af2844d99fb48db
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:06 +0000

    money us out place geton

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
