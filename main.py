07152b0ed6adac3aa1256db1ff259afddd97402d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:37:17 +0000

    unfortunate pronunciation sort well outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
