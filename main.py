3ae4f61c147fa6db2e2e625ee2bffbbb925f6452
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:24 +0000

    react my dish path upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
