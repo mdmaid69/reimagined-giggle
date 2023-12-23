4d740bd70ec2a0d5607cbae1fab7a21e34629e37
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:58 +0000

    tip strike list pattern notice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
