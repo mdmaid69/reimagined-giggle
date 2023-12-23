88a7f9d807d4054c8f67a446897dfa2b0c7c4f5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:08:18 +0000

    bank waste mad mainly passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
