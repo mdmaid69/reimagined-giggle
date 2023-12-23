faa6ab316d42ff25b24aa920679b8f605ca598dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:11 +0000

    extremely common left skirt regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def greet(name):
+        print(f"Hello, {name}!")
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
