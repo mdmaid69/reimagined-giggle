6a1b42810d6a3fda68c7cc58e3ccd5a6eaa809cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:02 +0000

    before suit jump valuable window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
