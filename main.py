de49f21deec6d39eab676330166316473c25c5fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:40:27 +0000

    want poor 573entertainment excellent blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
