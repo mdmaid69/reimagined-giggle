33521857640ddbf3e9e943d51fe83922976d98b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:35 +0000

    together start ground why bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
