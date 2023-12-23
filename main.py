dbbde86502af6f7ebcf5b25cba87a52c831dc07a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:41 +0000

    bend keep stir frightened central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
