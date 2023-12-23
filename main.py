ab39ada49baa7c105e04c15f6c81aecfd5f9ee8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:20 +0000

    give turnover mysterious plant interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def convert_to_hex(n):
+        return hex(n)
