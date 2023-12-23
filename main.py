84ec8f314791d7e3ff2cf1f80590d155f1354536
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:10:39 +0000

    offer listen getout rely suddenly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
