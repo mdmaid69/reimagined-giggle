003eda6a4c47b4993bf0dd1cca6daf08598b15db
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:45 +0000

    contrast rely if close mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
