dd29e055fb076127e31412b64da684081c2d266e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:41 +0000

    scheme twist succeed scene several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
