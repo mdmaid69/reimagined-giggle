33fd558d8aaea1f7d10580e8ba79256646a3cc9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:01:32 +0000

    firm act would trouble activity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+def count_elements(lst):
+        return len(lst)
