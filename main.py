ebc5bbcd159dbd40bf84ef1907cf0e7e8b1dcf5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:53:12 +0000

    regret fit nature range right

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_from_string(string, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
