da3d7d4b9d693efa7d892f4b2573e073994e8475
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:53 +0000

    worth date against man keepaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
