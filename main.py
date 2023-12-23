17327efb327bd8316d499a670fadc7d6788b3c74
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:36:57 +0000

    fit giveout development rain honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
