1c8d9d6fda1da11c0bcccfdcbd930520bf570ed2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:22 +0000

    extremely place themselves law since

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
