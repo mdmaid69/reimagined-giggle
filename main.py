c5094cde62aaf3006fb5d7e9e9194f6d5345d82e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:08:40 +0000

    violence owe doubt callback collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
