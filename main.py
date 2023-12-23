1a55bc1b0f65bf57b562be3736526f5075183fbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:45 +0000

    position apply drive gooff trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
