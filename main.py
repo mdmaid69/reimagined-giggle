0be6be4116b8315271b7f17879f0ef5e2cdc3fb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:04:03 +0000

    principle objection change sticky power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
