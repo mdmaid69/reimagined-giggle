905db924c537e9d6a359f3fd078f759dd7d077ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:45:34 +0000

    wash mine confuse phone female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
