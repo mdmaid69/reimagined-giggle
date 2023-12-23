3b1c8990ec83c587d33c29ad2e6b92a95cd9992c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:30 +0000

    friendly whenever loss stranger slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
