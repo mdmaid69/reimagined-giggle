ad07d87ffc13cdc505791854335d4c903bfdc203
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:17:39 +0000

    degree morning regret favour style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def cube_number(x):
+        return x**3
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
