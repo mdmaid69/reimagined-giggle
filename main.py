0f51591f78be508249d4a76b1c819f8fbaa3fb3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:28 +0000

    exercise share anywhere sick impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
