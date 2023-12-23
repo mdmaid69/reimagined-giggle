2ffc5c1137daf84b6249ada8a30b5a8d27b85f95
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:39 +0000

    step base shake hang score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
