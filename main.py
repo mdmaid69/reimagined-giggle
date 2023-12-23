14ab93b2b5e0bc02a8baa468932c05e34a7c74a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:15 +0000

    programme achieve interfere fire able

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
