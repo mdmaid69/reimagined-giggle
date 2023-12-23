2cfae9098662bf50119dcec8557ef31a07e378cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:29 +0000

    shoot thinkover thank count more

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
