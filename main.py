e391ecabc80e33ee9891e029b6ae286174a65f2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:14:32 +0000

    trust deaf example innocent shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
