7b6969d3de442308957a7c5c1c23d0df1ca7668b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:50 +0000

    unfair strange possession rescue fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
