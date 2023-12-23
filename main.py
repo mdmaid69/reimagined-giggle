8186e62a5cd2523e40a376aaa17bf79d0cdc49ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:04 +0000

    defend borrow tell weekend reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def change_working_directory(path):
+        os.chdir(path)
