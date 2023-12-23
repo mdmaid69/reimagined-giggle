c6951c4b3f96acb66ce59f4d62537e4b8bf91ae5
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:21 +0000

    lookafter strike rich shine turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
