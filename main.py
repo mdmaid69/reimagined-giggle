41d3712a1460dda74df632898b2f4a95c557f46a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:55:09 +0000

    form neat appeal night occur

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
