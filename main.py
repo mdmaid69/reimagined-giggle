6fa807dd8f0513c4414b5f3960d8cf3f6a952bb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:47 +0000

    search cut breakup direct pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
