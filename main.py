927c287a1abb9b37133624d6627722f86da4fb0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:14 +0000

    plastic model weather correct comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
