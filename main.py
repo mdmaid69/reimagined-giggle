1a6d672314f78911087c75575941054a557a2c8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:21 +0000

    worst fast difference another difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
