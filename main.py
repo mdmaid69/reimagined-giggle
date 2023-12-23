40925109191cc842f75c96bffcaedee6e581579b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:54 +0000

    mistake rest usually mistake basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def clear_array(array):
+        array *= 0
+def calculate_work(force, distance):
+        return force * distance
