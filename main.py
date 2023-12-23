26ec98542ac2cb0b6196db80ec60f77ad380b181
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:44:39 +0000

    wear centre 565end thankyou tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
