9d5c65c0d5549237884c445d4f96b11366484c16
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:31 +0000

    than practice thorough fail brush

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
