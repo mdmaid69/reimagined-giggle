4d3b3e64c6442dafc44bafde680f50c6c5d9ff2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:50 +0000

    region beneath schedule breakthrough contain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def find_max(lst):
-        return max(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
