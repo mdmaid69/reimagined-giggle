c25616f5dca4006f8f1770b877ab8b5bb94df415
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:55:02 +0000

    father manner approve lock turndown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
