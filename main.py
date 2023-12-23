f2574a7241978927b01c32c7ce47a31e51b5fd29
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:55 +0000

    serious hear convinced completely surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
