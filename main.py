955a9264be4a7906d5eab63cde062629db3bead4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:00:01 +0000

    pick nature prefer spread wrong

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
