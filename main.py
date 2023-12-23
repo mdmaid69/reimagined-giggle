a2c856bb3602a7a79ea6e3a25fe013bccafb101a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:59 +0000

    back period blow throughout hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_min(lst):
+        return min(lst)
