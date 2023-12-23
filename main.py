fc17f14f93a4daf3d499dbb424fc653b30108257
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:57 +0000

    mixture direct total earth characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
