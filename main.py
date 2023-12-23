005961423e1bbfefd0c390be1a2aaac5a48c6334
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:57 +0000

    temper whisper slice excited right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
