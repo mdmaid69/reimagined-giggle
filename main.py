b4a357a7a42625ca3cb7b71e5c2cc387b40803a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:12 +0000

    lead wave previous sad noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
