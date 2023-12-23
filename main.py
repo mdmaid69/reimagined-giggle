302b0810b927959dbbc7bbc6029f35e17f1ff63d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:58 +0000

    vision sensitive afterwards uncomfortable sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
