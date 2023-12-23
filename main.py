03397f08e297f7b649fa959e48ba4786f9cb1d5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:17 +0000

    right rough though reason judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def cube_number(x):
+        return x**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
