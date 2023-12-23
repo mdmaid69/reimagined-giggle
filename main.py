8be85988940882fb2e97e532c4f22e50ed54a972
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:05:09 +0000

    sting amount rest notice runover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
