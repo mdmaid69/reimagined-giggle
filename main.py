addbdf28d6e028228cff5288111234c24e885148
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:22 +0000

    basically activity eat even behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
