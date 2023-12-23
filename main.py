5d220e75784d0decee82ec9e302d3e389f3df71f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:52 +0000

    strike forever match again slide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 def find_union(list1, list2):
         return set(list1) | set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
