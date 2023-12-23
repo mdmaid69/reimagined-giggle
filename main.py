6a3aab8e8fc976c8bd4c2d418097af9b95f8e478
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:19 +0000

    comment fashion business offer touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+print(os.getcwd())
