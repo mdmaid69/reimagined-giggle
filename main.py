e3eaed0ffe0ae07ee37b6f9f797e43acb793fbaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:59 +0000

    train moment sort sink although

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
