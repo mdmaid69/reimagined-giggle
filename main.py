3001d6205ffcddd1e167ffab5e0d97b7d3e146ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:11:44 +0000

    area after part left turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
