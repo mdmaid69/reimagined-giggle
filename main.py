f56683b5d63b52eb8cf45a2b51c72be5b829c60c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:54:50 +0000

    put lock major keepaway occasional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
