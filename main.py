4b7dcf70c5721a9694bfd5c33199cee53fb0ab60
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:55 +0000

    weigh getback powerful team circle

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
