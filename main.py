f6b1c639c61438ae452e287fbefc30e55722ccb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:11:44 +0000

    behind behave education shade loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
