0c7a5e2c8def83670917206575f4f9f23ece2a5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:19:11 +0000

    bye reserve have mouth travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
