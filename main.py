fd977acdb06128b999b4b039c0c7edc847e87115
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:02 +0000

    travel region excuse force market

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import platform
+def get_python_version():
+        return platform.python_version()
