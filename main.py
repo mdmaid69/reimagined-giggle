0ec3f90307f48cf6e8932393f953816bbc13bed9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:44:06 +0000

    under regard swim hot interested

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
