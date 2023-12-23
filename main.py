3e1e1b421791305c508308e57bc4a005408c3f93
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:56 +0000

    strange instruction bite get notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
