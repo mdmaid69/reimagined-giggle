7c3faddf007e708cb189a3aa72a254bd966ea7c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:18:38 +0000

    burst 570enjoyable card pause mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        return sys.version
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
