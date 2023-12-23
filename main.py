eb86c0576a2dda19daa8cfd87e6c58b78b04282d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:36 +0000

    he damage path increase one

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
