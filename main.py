5ccfa91d0bc67c1f4183b098bb6f82bfd5e92669
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:52:46 +0000

    add meeting charge so unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
