2d6647b5537201fafccb462bfeec283b6d28562c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:48:25 +0000

    winter act best mine belief

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
