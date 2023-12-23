c8e6baa01cca08977f14baebf139e99ad80b2d48
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:30 +0000

    centimetre may division principle charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
