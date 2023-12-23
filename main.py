018c7a8db373a7986fd22e722500c41771ce8814
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:41 +0000

    raise explain she solve explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
