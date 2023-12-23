256ccf9099fcf1fb86da0134910b1700ced7fb7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:05 +0000

    again centimetre sport gentle lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
