60732813ebdbf5f5595c9a520d293c24dd42ceac
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:59:02 +0000

    minimum aim attempt recommend force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(lst):
-        return max(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
