7fc932001b0bcd69d0dc870af565f6f984f91df5
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:16 +0000

    head evidence deaf team demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
