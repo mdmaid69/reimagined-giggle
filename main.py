edc35785f22385eeef69be98cfe2c52ce5c2b335
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:26:55 +0000

    seat comeout tired Dd strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
