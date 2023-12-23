71dae45aa7857803237503740339b83a1ac4ec41
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:56 +0000

    tap good sell our patient

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
