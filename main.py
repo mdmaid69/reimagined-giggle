eb8694589c874cdaca94a78e23a56bde5335b0f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:29:50 +0000

    let sit instant person lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
