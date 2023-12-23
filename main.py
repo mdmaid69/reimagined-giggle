474d4da87857b78703e7aba517f6ce7e70f36359
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:11 +0000

    dance near learn ilovedicks part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
