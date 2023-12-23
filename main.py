da5d3cdcf9c4b8857bc3fb82408bf70a5041ff22
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:02 +0000

    partner mysterious reduce dream play

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_max(numbers):
-        return max(numbers)
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
