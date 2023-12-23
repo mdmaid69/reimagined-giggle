bc16cf792164da4bbf19854004d40cb372008f71
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:13 +0000

    whenever without sticky oil tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
