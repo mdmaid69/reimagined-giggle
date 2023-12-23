39d3cc718d551598c396cc09ff7f10ca926c628a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:29 +0000

    maximum maximum wake while runinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_even(n):
-        return n % 2 == 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
