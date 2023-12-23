1211558d808df9db6203793144add09e0a60f111
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:40:05 +0000

    music suit undo always or

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+  def count_elements(lst):
+        return len(lst)
