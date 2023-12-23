8e835e7744c70e29ae7556f80eb1b81a3ddbfeb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:24 +0000

    cut neither loose slight various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
