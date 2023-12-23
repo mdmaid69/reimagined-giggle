c2b5a8513e008997558ef210a60d119f84824178
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:03 +0000

    brief hair unite matter object

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import sys
-def print_python_version():
-        print(sys.version)
+def exit_program():
+        sys.exit()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
