74b16bc4b620854f9972a67f1515432ad68470c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:48:53 +0000

    cut guard keepup succeed balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
