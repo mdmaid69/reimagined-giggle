38006464f7f9eae56fc3f783f8e6536a81e2bc04
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:42 +0000

    environment unable law lock pause

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
