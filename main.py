15a797a280209642f74ec0dc4bd92ba738f85faf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:28 +0000

    soak container kick love bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
