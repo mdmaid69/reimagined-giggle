b651bc8b66b8cd33612a98ed2095e68853fa7fe3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:19 +0000

    unite giveaway this enthusiastic regret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+name = "Python"
+print("Hello,", name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
