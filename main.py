99efa78532ee925b27349dfc81610fb0e8bcebbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:24:21 +0000

    nothing sport impressed slice shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
