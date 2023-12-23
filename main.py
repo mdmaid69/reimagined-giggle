4cc8266351b8a71ddbd5794666492da7d536fafb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:14 +0000

    over effect command deep you

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
