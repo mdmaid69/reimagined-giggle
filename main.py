f50490b530d31c48ef37de2c698ae10e1f36e294
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:33 +0000

    round concerned positive you top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
