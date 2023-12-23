96bd975677ebc7e0cd512437abd5257e80f03ed4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:54:01 +0000

    that suckme sense then plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_string():
+        return collections.UserString()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
