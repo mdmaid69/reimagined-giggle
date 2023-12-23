59b149c8093a8d69052931a8929c9ad4be6418a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:53 +0000

    modern none relax start capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def find_min(lst):
+        return min(lst)
