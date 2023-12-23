293f6bda8252af200c35aeb7a5c6d7c73ace6444
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:37:28 +0000

    action turndown damage bound surprise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
