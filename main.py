c3813078ec6fcc8a5eefc9cc5942aa3aee73322e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:15:00 +0000

    transport reply when squeeze church

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
