e2ea5f914ebb1f25aaa084e20197cd1ddf453006
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:24 +0000

    fasten mail cruel table point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def get_current_working_directory():
+        return os.getcwd()
