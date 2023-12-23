484cfbf38ced24ef727864f15c20a7d9331919d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:36:26 +0000

    reliable by spoil fat university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
