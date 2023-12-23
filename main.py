1803ec608f1274b15fddcd9a83f4fb8f7d80819f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:29 +0000

    excuse exactly definite health warning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
