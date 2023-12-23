093a239b8d6d469c4320466d3669600788672722
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:21 +0000

    accept fast work match stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
