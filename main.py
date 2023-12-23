b82a67e30b969e46e1b4efb7c4b7539872858b16
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:14:54 +0000

    plan she convincing even he

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+print(os.getcwd())
