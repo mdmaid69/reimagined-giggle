0a94e76861814aca2eeb2808e2b89ad553299ae4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:39 +0000

    amount doubt smart whoever appeal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
