7a37ac8b0ad8f8a98cc146540e8196db4af5cf55
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:08:59 +0000

    prevent century count matter stop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
