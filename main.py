d9100a4a4a3d04b051f28220b3b2e7768e0c6cd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:25 +0000

    appeal serve test crisis fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
