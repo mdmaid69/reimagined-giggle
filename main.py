9744d2b1f4aa4c6e3902db4f6b0b6b573e72d9e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:08 +0000

    serve persuade follow now spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
