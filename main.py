9e4f442f993ca026fdfdac25574c2eb68edcbb2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:46 +0000

    fairly eat towards call pocket

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_pressure(force, area):
+        return force / area
