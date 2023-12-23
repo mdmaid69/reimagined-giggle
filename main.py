a7a662ee1dd4774322b836f257fdb8d52b209f9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:08:53 +0000

    bite face birth letter lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def remove_directory(path):
+        os.rmdir(path)
