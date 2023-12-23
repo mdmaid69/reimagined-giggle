afa00ed4a18a91340489bda29c9e0cd65a493732
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:46 +0000

    positive happen borrow advise advantage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
