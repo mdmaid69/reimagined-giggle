df76d80bdff5d4663879a96a2d6a9c9ca18fff24
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:43 +0000

    lucky sure any afraid no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 def calculate_perimeter_triangle(a, b, c):
         return a + b + c
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
