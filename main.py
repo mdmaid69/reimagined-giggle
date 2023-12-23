d3b5172e14071a8274717b2b57a88135ed57a7bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:33:06 +0000

    before nose pay murder protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
