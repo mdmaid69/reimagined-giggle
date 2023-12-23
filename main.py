f63a3e787f5675b0a6a36f6b23feae99a6674085
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:56 +0000

    permission equal rank real no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
