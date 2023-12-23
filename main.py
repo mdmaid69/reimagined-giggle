dab2e08a646eb5bd52624f0d7ff35ded00739d76
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:39 +0000

    achieve translate with little level

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
