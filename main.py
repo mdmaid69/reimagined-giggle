8b384bea77824ead6f95aa164d91fd79bbd4f08a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:56 +0000

    upwards hard continue word spray

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
