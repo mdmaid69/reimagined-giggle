d07746c97e2cdb0f829f799f71df21fdae2d773f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:08 +0000

    finish colour mail less problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
