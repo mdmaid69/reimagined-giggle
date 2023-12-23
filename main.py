de2bc3eb89a60bbe0cee1b6227c6a2ea8399e7ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:35:45 +0000

    middle mean stamp skirt take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
