0f9129e9820fb53aef0ca25cc33e67a49ce392d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:48 +0000

    inform reason collection still combine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
