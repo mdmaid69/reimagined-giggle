69aaede97d35c2f06cf6264bccd97f9932d84c61
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:18 +0000

    sleep number even pleasure danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
