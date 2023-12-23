89ed9f6f5d1292c831b9cfe8be520198ae97d66e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:21:27 +0000

    live tax create record getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
