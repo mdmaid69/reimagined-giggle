8627d965b3991ed3f736e87c65cf35a613c1f1bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:12 +0000

    include actual colour train roll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+print(sum(range(10)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
