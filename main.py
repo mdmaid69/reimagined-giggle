3678cb1ab04d1d72dc094d9ebf0d1968bd231b81
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:17:14 +0000

    luck now even speak mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
