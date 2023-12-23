1802bab5610f5323f7989282bb22dbf583a27d7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:46 +0000

    anybody watch activity fond transport

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
