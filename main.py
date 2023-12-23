b661dc4dadaafccf57c7904523df6ddceeb18105
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:43:30 +0000

    live home side confusing break

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
