b3034f5431b4af4d46193e5649e76b1411056821
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:45 +0000

    also mess scare borrow maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def reverse_array(array):
+        array.reverse()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
