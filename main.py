f8c64661666c93ea3f0663cea33f8d24610916a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:19:01 +0000

    mind fail government memory ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
