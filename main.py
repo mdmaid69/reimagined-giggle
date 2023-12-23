1bd7914b81834fe65603d946394d0224c79241a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:09 +0000

    fun come wave someone order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
