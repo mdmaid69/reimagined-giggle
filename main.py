90f8dfe88a38dfb81fa368a23725e6807db1942b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:51:02 +0000

    expect lock classroom forever dead

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
