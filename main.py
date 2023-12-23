f8a626ef6c905843e57f9c3fd8d4d9794037a567
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:47 +0000

    shut file listenfor used borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
