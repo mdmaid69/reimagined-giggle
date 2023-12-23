d985d75aa972ec46db81d29e8773f567f2cc9b80
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:59 +0000

    your home memory Oo pressure

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
