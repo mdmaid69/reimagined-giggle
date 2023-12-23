4426f883b05004c76713bd100dc227bdddd3ef04
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:53 +0000

    mistake and plant tomorrow disapprove

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
