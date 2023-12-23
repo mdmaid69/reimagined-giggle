cc538f5fbbda6d461aed22b3af5a7971f9762245
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:25 +0000

    approve equal come Bb underneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
