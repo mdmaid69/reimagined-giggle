6f7588b9664df3bf03b065682b52c66241bce770
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:05:59 +0000

    anywayanyhow consider spring claim pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
