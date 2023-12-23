163ade18bb6b1eecf2dfb82454af9dad1d43aa9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:44 +0000

    wave deliberate escape many best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
