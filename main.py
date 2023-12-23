4b2e84785f3e0e784113e44c748dd1b35d40353d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:19 +0000

    reject quantity bye clear lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
