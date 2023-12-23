e1c4cea0afc1d4f1764352b57f1c89f38640e9b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:08:14 +0000

    negative pull total confusing keepout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
