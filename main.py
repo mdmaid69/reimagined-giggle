97d185f38619ec6af1030fca5e47d4e701400af8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:35:42 +0000

    mistake available handle float rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
