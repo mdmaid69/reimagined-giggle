cd19198cfecad7032bdc5f580e35eb03f609292d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:54 +0000

    nearly hurt children throw poison

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
