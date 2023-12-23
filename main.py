be28e72da2e7a4b49281227a3430f99f0cbcf8d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:21:34 +0000

    secret crime movement than right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
