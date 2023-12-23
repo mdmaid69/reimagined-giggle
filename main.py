f9073c7465661a413408be18a53d97ff53b8d70d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:06 +0000

    duty sudden wealth go perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
