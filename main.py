7afb538e8072617a5e0a6af2668e6a9da761af2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:56 +0000

    us centre insist wet OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
