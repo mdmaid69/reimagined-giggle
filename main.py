778c6be59169814d02aec7f71d5840230f653508
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:26 +0000

    contain kill declare thumb see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
