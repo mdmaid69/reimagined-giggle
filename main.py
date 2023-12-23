891c359c2a61621e4f82132e1e1c6de5acf7d3c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:19:19 +0000

    rush problem past pride influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
