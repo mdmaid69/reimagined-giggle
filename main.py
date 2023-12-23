9e66b370985fbf4d0601ea82ea80dd8a7b9e9b52
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:42 +0000

    emphasize shame contrast repeat legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
