249bf9b817289b8174a2dbfdf5b2e9d1b2ef634d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:26:55 +0000

    have pleasant original already roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
