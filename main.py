8b9cdf16c1feaa9befdd6f4a186b7c302fda8599
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:31 +0000

    basically muscle rate dish makeinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+from collections import Counter
+print(Counter("hello world"))
