8dd5575acde42a06c7f5c2316ff1cade194a7dc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:46 +0000

    again splash new section frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
