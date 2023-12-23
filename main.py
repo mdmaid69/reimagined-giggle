64931eb1167d63783b6a3aa284b58edd73fa56ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:45 +0000

    crash range exaggerate appear church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
