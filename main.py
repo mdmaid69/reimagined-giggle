91cab0664ce32662b5e32ed600249f4700a27350
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:43 +0000

    spring repair little lay strength

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
