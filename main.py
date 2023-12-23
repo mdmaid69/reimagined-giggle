04ec9f8050b2450f370b873ee67f6fcd7b2338e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:01 +0000

    such study keepfrom love genuine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
