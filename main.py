9721b9f87cc4517a126060d62494e5aee08031dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:39 +0000

    thirsty firm around near little

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
