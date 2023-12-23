5e75912085bd190509393944db4dee2af52c8829
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:45 +0000

    grand save lonely involve drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
