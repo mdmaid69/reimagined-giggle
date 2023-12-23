7152ab84e1af7e834911bb051a1b71f883507235
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:50 +0000

    criticism soil among decide extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+from collections import Counter
+print(Counter("hello world"))
