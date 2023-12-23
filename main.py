c3e2957fde3183828c0edacf80e537f846dc0d86
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:14 +0000

    full foreign exact memory lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+from collections import Counter
+print(Counter("hello world"))
