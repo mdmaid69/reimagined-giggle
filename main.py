c16446a77a8ee9955609b33166dcecc97870c0bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:11 +0000

    programme callback town 571enough reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
