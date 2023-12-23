cbc4acb7e6456731f5c985bb1ea00010ebf707e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:04 +0000

    damage attempt Uu old fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
