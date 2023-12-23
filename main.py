0bfd26f1abb9a818708d1b2a9acdc979abaf62fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:49:58 +0000

    hurry school dead skirt accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
