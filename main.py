03f1c9693a27f4aa73b14d2fc65626c8c60d23c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:26 +0000

    right somewhere possibly sick pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
