a3175c325ae577022aee906271378a46c37d20bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:28:50 +0000

    comefrom may careful bored pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def count_elements(lst):
-        return len(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_index(array, item):
+        return array.index(item)
