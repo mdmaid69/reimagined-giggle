3fce297e5282cca81aa215b3a57197d4fc67973f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:21 +0000

    danger capital divide truth debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
