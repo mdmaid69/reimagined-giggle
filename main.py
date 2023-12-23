2e0e1242acbc6bdff8e2253b9a618963585a5d9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:43 +0000

    sudden except spread stone often

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
