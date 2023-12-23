6692e0899c69be3960f2662e3b8c79d52a9652bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:22:38 +0000

    funny body walk makeinto roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
