8b0cf38ad7e8e9f111574fe5e581c2757b1bdbd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:45:12 +0000

    intelligent away target cover unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
