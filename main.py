043f134fadfded1ff35eafed95655c0de3613fa5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:27:21 +0000

    pattern listenfor of anything emotion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
