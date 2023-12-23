b0bed5e9c668419fa30ef986a392f2cc4a1a7ee5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:14 +0000

    muscle store yesterday unfair immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
