53505714c9f2465a2650a9966753c52ad6e2ceb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:15 +0000

    at use tired meaning transparent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_min(numbers):
-        return min(numbers)
+def count_elements(lst):
+        return len(lst)
+def convert_to_binary(n):
+        return bin(n)
