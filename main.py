96b586899232b5237b8d2eb319c5b5b07a1bb7a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:50 +0000

    busy perfect copy rush violent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def reverse_list(lst):
-        return lst[::-1]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
