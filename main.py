d1ceac51bd75373ea2930a0e311efc48b37e279b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:05 +0000

    Ff tired offend impossible dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
