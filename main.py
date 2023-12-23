779486af9e154fb436cfb269136df9cbef28a358
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:15 +0000

    school accommodation address accidentally up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
