88e1d6945dc3d680b90796e976895d933ac01e46
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:38:04 +0000

    within seem push everybody forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
