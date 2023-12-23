d534ab2f9b90a7e5e4c65b1e014efa1ded466ab4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:30 +0000

    hardly firm connect several too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
