70d4460bba7f2f5a0de78264be4fb5706170f8e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:20:55 +0000

    big eager demand border preparation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
