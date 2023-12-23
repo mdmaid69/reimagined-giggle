197c0763c5afa5c25dd7dfd11c0785116139dd89
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:37 +0000

    sister keen Ee where emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
 import array
 def check_if_array_does_not_contain_item(array, item):
         return item not in array
