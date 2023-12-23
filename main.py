e13cfc12a63eb35c71c08c5ae8c619615d1baf50
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:47 +0000

    you cold bend part become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.ctime()
+def subtract_numbers(x, y):
+        return x - y
