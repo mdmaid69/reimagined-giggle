969dac3425bd82faf861e4bc87bd819af4d763ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:08 +0000

    anywayanyhow lower hurt section side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def square_number(x):
-        return x**2
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
