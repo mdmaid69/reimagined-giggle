75e55800f3720f50e15d3410c7e88f13b5418f99
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:29 +0000

    shock service building tool naturally

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
