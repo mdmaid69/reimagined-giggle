930f4be7f68f636efb0cd6099007c0cce9a7ab47
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:40 +0000

    visit previous open thing decide

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
