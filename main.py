093e8566ea76c6506830726ff3698b29199befa4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:10 +0000

    look beyond tape love power

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
