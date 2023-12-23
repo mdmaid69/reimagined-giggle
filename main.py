222216f0e844b83b8744dc25208215dc2370964c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:52 +0000

    drop switch over talkoutof natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
