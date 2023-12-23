8552985d554491bce62efcd869e7ddae9dc5519a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:54 +0000

    family memory few forever comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
