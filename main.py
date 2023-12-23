05c81d64aceb644f0c89952dd05e759cd13e8a7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:25:40 +0000

    pupil string priority throughout listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
