da88ebf43725cc4e679d86d1a6d90a37112c789e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:39:01 +0000

    furniture congratulations middle usually autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
