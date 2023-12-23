ea19d2c48d573149a3599aadb09e76832842e563
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:39 +0000

    learn recovery film bet combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
