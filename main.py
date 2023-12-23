44901674aca419e5558b9ffbc684032e61431004
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:30:47 +0000

    share factory hide kick house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
