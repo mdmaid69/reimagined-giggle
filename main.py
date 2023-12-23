342de8b7f410d5dae425cb2dab7161dc73a82c57
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:02 +0000

    pencil equal full busy Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
