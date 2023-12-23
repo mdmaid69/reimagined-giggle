80caebfad7a5ea23117a854c64ed7042270e987d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:49:12 +0000

    runaway feeling sink move either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
