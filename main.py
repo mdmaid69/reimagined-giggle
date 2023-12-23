adbf86b8bbf075ff47f3c913e677d01d76fda691
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:58 +0000

    nearly business clear dig discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
