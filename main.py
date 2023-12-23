3b8e0460efeae6ce7f1836f19051df9329ad7085
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:04 +0000

    snow mess wander congratulations final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
