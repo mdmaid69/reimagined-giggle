db360d8755aba5d452ea690fbae20736c52c4fa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:44:07 +0000

    close knowledge repair pupil burn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
