38922f37e19a16507e488118fe587e9313e7d5cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:52 +0000

    story raise talk future suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
