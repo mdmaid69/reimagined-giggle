a55e63e16fe5d5f80816da73ccf48ed1ec219d72
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:47 +0000

    base demand effect poor skin

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
