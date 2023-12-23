1d51dea0a217ea2108cad6a25825f5febc5a92fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:41 +0000

    news common theirs pen colour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
