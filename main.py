4d5f536766107caae62f8f93459ca76b3e80cdf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:37 +0000

    today sorry bang objection shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
