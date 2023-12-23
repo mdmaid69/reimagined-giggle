affb4e5fe526dc4fff2f744c84895b3005564460
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:00 +0000

    tough wide over glass crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
