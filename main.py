92adecdf49dad8c1b8336a0b88188893de9ca876
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:15 +0000

    sorry square say tongue treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
