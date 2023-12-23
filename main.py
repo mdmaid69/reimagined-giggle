f67cb1ae09d725900489bc1fcee57ffd039a3953
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:21:00 +0000

    drawing giveaway deep ball little

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
