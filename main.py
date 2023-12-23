cbdb38c8215cc73d6e1c74f7e453e0c3c0105be7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:22:01 +0000

    immediately alive every who man

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
