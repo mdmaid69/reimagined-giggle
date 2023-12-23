7b7841eaaa22f2184ec8c8d22efbd3f3d2b0064d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:41 +0000

    minor fashion suddenly major trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
