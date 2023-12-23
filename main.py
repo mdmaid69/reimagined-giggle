81b74b5cacaf9574fddd7c1a045cb7443e76d90a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:27 +0000

    hear reliable familiar priority outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
