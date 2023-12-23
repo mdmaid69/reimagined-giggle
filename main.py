ddd8d00c9f6857a7d2153e48f540c6473b4940f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:40 +0000

    dangerous opinion chase thanks profit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
