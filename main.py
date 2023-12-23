963f284371d194d9ab5717232ab5a0fc1b474205
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:57 +0000

    road comedown crisis turnback way

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
