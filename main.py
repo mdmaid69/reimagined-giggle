36549caff42b264f18f11f51ddce8689fa56492e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:34 +0000

    embarrassing famous tooth train interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_length(array):
+        return len(array)
