11286299285ee6304665d9bebd37bbcd9457a831
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:01 +0000

    boring negative help scheme ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
