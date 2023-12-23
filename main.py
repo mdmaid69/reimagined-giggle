fe14e7f2fe9e566c295862465e4f0545befe3973
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:47 +0000

    punishment balance basic before question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
