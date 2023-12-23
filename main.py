1610fc547b236dafb1b59db82d2605bf583ed983
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:17 +0000

    maximum post distance certain sweep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
