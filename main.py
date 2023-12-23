5963761fe228f36d6ac91a3aacac95d9b069576e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:16:30 +0000

    bridge our tradition whenever question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
