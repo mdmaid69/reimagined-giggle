a5ed39564ddf8bb977819082210ef070b57d4b41
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:13 +0000

    impatient takeback once failure pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
