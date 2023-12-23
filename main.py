84356315e76169cac2501f9b73c8340ae7e9c74e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:48:41 +0000

    government mystery arrangement shout matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
