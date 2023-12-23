318edc23e2ba1a585fd444a9dfede370ba273971
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:31 +0000

    remain about repair find desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
