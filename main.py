737eefb8e9394ebc6594a52184116258346fe24a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:29:31 +0000

    they move try ball finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
