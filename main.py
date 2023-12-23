2e4e335a3a475d5664648202028bd32dd6c81741
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:53:10 +0000

    snow in dry doctor somewhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
