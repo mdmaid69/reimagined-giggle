4eb880f4bb9919a2f12e1c987dabf3977e4f2173
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:22 +0000

    home press stir share pocket

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
