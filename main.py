cdd239c9858e30948c6b29afa0a896e9ba6eda27
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:51 +0000

    shade pity both ill slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+import numpy as np
+print(np.array([1, 2, 3]))
