f5a2c186beaead0250d0ee448ebb1c7423f43f62
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:18 +0000

    fear can harm another lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
