d89bd9f6feecdf3ebdaaeb4cf64ff91fbb5d9e83
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:22 +0000

    eager cool goout left second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
