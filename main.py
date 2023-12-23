59acf7b9c1df98bb20c0812dc537dcaf22cb39b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:22 +0000

    begin week brother I pretty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
