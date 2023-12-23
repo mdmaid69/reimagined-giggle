277e28994398d31b94696d17f587d54860a0e938
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:13 +0000

    early storm search secret post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import sys
 def print_python_version():
-        print(sys.version)
+        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
