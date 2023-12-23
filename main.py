632632450fe24cfcc3a25ad30a817e811928203a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:35 +0000

    cheque fetch amount aware decide

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def greet(name):
+        print(f"Hello, {name}!")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
