9e2eaf2f29455f1514afc580ad2c7bbee5454b41
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:42:31 +0000

    match list exciting soil slightly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
