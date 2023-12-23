0088012e03f125a2b355fe2cc2448f4891542d99
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:04:12 +0000

    even only true sound imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_pressure(force, area):
+        return force / area
