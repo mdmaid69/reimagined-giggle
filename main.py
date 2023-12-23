e38d6fd574b983771cc5f28c0f7abfb5db20ced5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:00 +0000

    butter limit makeup fast ought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
