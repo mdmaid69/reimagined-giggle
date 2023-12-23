42243d986b052517b8eaa4c60c13d6ebb10eb0ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:22 +0000

    available reason yourself correct behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
