6cb701b075d5f7d8d1eee2bb31bf55090ee048c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:05 +0000

    brother 569enjoy separate feed confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
