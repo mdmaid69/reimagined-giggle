3359df2220168fd76d09cde955442016ade86f51
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:21 +0000

    little switch later along regional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
