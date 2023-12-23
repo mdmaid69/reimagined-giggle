69cea46103fb8bd70e31816d1936759b1c03bafc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:31 +0000

    dry piece crisis lookafter business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_palindrome(s):
+        return s == s[::-1]
