05e33b1aa9306d3c97820ac19b41ec7b0558a2ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:12:37 +0000

    quarter used beautiful Kk breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
