8a7845580cb497fd579ed0ad882d9d5d6559f27a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:36 +0000

    more immediately add pause Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
