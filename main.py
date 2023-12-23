d4e7042449889469f4077eb58fb46c3b0ea0d4eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:23 +0000

    value wave release ilovedicks position

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_octal(n):
-        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
