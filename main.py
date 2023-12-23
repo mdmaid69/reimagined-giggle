c14b87172af032b6c0d77e58fb45b7cda68298b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:39:03 +0000

    sail your godown period old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
