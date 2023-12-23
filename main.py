fafdae5f2497e1f932527ed1a8c7f69c88618d92
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:43:44 +0000

    loan birthday discovery thing priority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
