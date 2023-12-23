09bb9086d1bfc43874bfc89925a668e6beb0e9fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:49 +0000

    declaration talkback pure soil make

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import time
-print(time.time())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
