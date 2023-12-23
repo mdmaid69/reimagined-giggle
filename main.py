fe9a49c8be8a888cb303d7a61b20b8de65bafc54
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:44:45 +0000

    themselves face swimming world unit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def convert_to_octal(n):
-        return oct(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
