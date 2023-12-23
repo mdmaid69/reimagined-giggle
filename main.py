be753149399eb592fb2f59c4f557969a1de0285e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:53 +0000

    exaggerate think constantly hair see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
