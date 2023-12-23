e3aba31cab69cb2c2ae49fb532ede295c0154623
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:33:55 +0000

    opinion beside sensible suppose region

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
