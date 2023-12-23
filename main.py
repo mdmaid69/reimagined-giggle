322fab2e9b25234799053033dc280c7c1a6a0b63
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:13 +0000

    wash medium step waste transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
