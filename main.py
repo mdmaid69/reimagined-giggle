2c71ef279a187ae147579c8f6f902e489d791bf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:28 +0000

    any lucky reasonable bit reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_sign(x):
+        return math.copysign(1, x)
