752f202c8e19a5df21380b9facee97d854dc8d80
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:47 +0000

    giveup top exist priority closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
