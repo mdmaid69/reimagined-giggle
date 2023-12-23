c3c6455739daa0212e0bba2ef18fab89e6d7def2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:38 +0000

    needle last win tidy information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
