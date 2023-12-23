cab93049475973ef449538d2215b906d2f952845
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:00:01 +0000

    before roll attractive beat fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
