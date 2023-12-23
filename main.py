1072578fd07b0df12e77cfc291ec738b619f0d3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:12:02 +0000

    talk takeout approve shut taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
