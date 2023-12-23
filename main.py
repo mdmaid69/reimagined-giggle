cb39ea553aad44a278229e040603d78a030cfbb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:53 +0000

    healthy carry land fall while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
