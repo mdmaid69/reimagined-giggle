ebc1776bc348c7570560605d2544be1a65f5e031
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:19 +0000

    soft young comefrom getback say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
