81497214188ae028bd798ee3e80ba75c979d14e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:43:33 +0000

    awake smell put cook test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
