039c0a4c5e3c743429230e1de36993d78f389c1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:52:16 +0000

    learn open whistle there eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
