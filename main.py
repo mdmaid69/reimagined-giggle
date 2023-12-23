338b1527e752dd6db216b5f907ed6375d27ec1ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:58:47 +0000

    friend walk breakfast step breakfast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_rectangle(l, w):
+        return l * w
