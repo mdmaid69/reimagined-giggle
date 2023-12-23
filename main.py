5636381664e0058c5a81438c72ab8332cb18a3a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:47 +0000

    direct think thinkof strict plenty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def count_elements(lst):
+        return len(lst)
