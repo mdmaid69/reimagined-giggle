9faa099f97bc4567791d9e80fc0ceae7e3143774
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:43:14 +0000

    mistake raise advantage proud wash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
