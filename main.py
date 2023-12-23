d2324c5c38208f06e57f95e73c4e7825bc4c7388
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:27 +0000

    reach doubt excited square beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import random
+print(random.randint(0, 100))
