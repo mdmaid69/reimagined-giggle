1cf3fb5fdd70456d8d74c1b49ec62804f77b5a47
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:04 +0000

    conversation laugh terrible have unkind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import tensorflow as tf
+print(tf.__version__)
