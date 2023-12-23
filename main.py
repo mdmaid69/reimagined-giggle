ab9241ef4d14ee85fbaa67646f9a2382fa6eb78c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:39:24 +0000

    classroom excuse move become bound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
