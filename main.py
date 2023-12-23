f77757799ce892e8cc56c5970bc3de5040b8796f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:37 +0000

    great scale afford natural fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
