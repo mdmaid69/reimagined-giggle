af1c78594dba3f78d30acc62810bebc154a1ca8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:41 +0000

    progress treat repeat exit justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
