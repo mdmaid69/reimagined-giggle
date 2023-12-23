d2977fbd00a62a01ad23fd1798882bbdfffa3896
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:38 +0000

    instant punish brush anyone hospital

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
