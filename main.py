4b9091c41fab1f55ca33d3f8e0d8c3ca138e9dc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:07 +0000

    memory flat test skill hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
