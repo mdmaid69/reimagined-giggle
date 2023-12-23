e268d170c78a7a2e0b0636c799bd72e22d32d05c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:50 +0000

    in petrol fall attempt freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
