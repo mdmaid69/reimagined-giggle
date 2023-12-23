cb1e3f1a8ad46a6156010ca3d715fa47b004328f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:36:46 +0000

    later expensive cover strange weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
