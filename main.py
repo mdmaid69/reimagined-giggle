d4ddfb6bd8a971cff9375468babd94b87feecc38
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:42:13 +0000

    tax love principle suppose that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
