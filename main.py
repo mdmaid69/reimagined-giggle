2978591ac820adfccc03d54aab1fbf141e3744c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:35 +0000

    Pp attitude shade stamp goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
