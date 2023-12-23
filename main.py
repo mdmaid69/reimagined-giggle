cf5dea0836082ada5f1a6696a7909b2141600034
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:15:40 +0000

    suffer read nurse far ability

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
