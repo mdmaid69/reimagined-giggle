a13461e1c871c7daa36650207a1b2b349828a207
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:55 +0000

    exactly against post takeout sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
