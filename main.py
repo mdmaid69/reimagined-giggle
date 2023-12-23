d9468bfdf8c6c675259d46e1f8260f8793b4fdc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:21:29 +0000

    earth track listenfor convince balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
