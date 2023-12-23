dc1408aae20a7be3df073d4598a9f753b498b76e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:02 +0000

    embarrassed politics may else disturbing

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
