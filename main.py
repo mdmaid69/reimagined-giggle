cb085ddde713c08c4296753bccca25f8d267e56d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:29:21 +0000

    home steer satisfied excuse become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
