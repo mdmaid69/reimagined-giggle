cd37e0ecbd9a5e2946d3b1eba41b2d763ee22f06
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:41 +0000

    immediately quarter surprised style unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
