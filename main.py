db63c73904cd984914cced51e8f937919fe83a78
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:21 +0000

    attitude kind snow plastic shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
