90f09c3bc99893ff569c61e472954cf83afb54ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:29 +0000

    female physical tip visit adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
