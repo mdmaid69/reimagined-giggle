09f0317454659292b51bca5e65205bc5475dba23
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:29 +0000

    favourite ring live majority getup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
