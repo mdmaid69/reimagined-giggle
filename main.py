02c6bb7ab988a693022f3845ecbb2b3e313fcdfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:21 +0000

    pipe car closed however ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
