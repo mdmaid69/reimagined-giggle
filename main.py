16ffcacefff9c4c73176c0a78db65a7fec10591c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:07 +0000

    bush bang interested nerve left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
