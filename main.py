81296538161100bcc679e3113340cfe87f94fdd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:14 +0000

    they well 179bite breakout work

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import os
-print(os.getcwd())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
