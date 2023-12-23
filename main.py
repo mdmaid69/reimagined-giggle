a3a5cc5458fcadd81b82b5b8703f1bf7412581e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:15 +0000

    last gather poison pencil happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
