077c9a4678c14deee0d6cc4926f266df19691fc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:15:38 +0000

    swallow stare prize warm smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
