52a292a92075b4a6699fd05f64a2166b3a1e89f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:11 +0000

    seem sink abroad Uu ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
