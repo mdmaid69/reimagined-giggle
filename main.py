e3ba90c904e8d22c37b07298162a9a54494883e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:24 +0000

    deserve proud sing firm what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
