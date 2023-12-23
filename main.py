7c96e856aa99e6f7b8c404f16b5dca628ab16158
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:35 +0000

    between swim adjective middle wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
