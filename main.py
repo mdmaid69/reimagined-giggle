51a0323f4a8fbdc5735a030544739b012f065532
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:47 +0000

    shadow guilty nor shape motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
