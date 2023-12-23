b59e0ef943fd332a33786fdba63dcf53b6678e51
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:10:50 +0000

    slip line push against view

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_length(array):
+        return len(array)
