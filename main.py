08869712de9be59c44dd69436119d64b8f3be85c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:55:02 +0000

    why reduce holdon nose then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def is_odd(n):
+        return n % 2 != 0
