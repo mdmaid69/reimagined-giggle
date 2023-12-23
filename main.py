55543598a5da347cbf2d592d5940222ca251b761
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:32 +0000

    backwards wait somewhere new book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
