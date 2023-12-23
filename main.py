80c9877a9180c3a7db541cf2713d84ebd9d1a5cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:42 +0000

    heavy size ashamed heat cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
