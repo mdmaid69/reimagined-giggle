5c149293ddd1741f37e86baccbf822a34e685e68
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:22 +0000

    573entertainment various image bread lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
