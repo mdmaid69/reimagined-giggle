3caf2a6587a63f7118db859b92630b5230a6ea65
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:00 +0000

    demonstrate debt outside commit secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
-        return len(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
