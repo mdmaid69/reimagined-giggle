23e3ca9494e5f80b196d1e843fdb92daea10a18f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:00 +0000

    old unite raw wonder light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
