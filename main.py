7fc6e9c32c15ed163fd6b51d9a47e512ef188e30
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:49:26 +0000

    broad minimum turnup win interrupt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
