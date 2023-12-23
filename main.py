c55f8906c464dc278fa78060af0a79bf649ea7b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:56 +0000

    row reserve thumb friend receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
