40cd06e3df15c5dfb0c867206181a92644c3f6cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:34 +0000

    bit foreign apart office translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
