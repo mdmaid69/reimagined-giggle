d5efddb23ea71916b7113d3069a2caaa54e8868e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:37 +0000

    rule piece clue ground people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-print(datetime.datetime.now())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
