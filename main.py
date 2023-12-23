2a388e62b1cc5476ffe256b3789745938cbe03d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:06 +0000

    interview accidentally star memory to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
