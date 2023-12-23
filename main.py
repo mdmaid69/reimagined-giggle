4e0f39a9db60872228aa45936ecf260be4c1eaae
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:33 +0000

    cool back frightened finish chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import time
-def get_current_time():
-        return time.ctime()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
