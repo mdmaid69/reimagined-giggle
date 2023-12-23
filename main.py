064e2e7c8087cbea43704452639d416286cd94d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:45:02 +0000

    production off flat lonely mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_str(array):
+        return str(array)
