ffdac0733307b8d5824ce7add624b0efba6f86cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:40 +0000

    moon modern takeoff transport disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
