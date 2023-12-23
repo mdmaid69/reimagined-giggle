5722c89cac33af241d78fea8a6ee98743cdee874
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:09:01 +0000

    organize eat hour worth breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def find_unique_words(sentence):
+        return set(sentence.split())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
