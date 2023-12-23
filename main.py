7cdacae8707c515c5cb808c227b60e880f8cad20
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:52 +0000

    body once level disgusting represent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+for i in range(10): print(i)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
