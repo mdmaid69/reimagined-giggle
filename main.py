a3aa32e49c71bb5ee2a7dd012a08dfc218f9cdbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:12 +0000

    patience clean ours woman set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
