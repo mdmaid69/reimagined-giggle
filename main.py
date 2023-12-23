a0a4f443b2916815b2523e81d96bc31821a1b064
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:57 +0000

    belong actual wrong line emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
