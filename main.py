e912d2f0b36fbc06fa55675dedfeb8ec92b52a5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:49 +0000

    task low heavy pressure average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
