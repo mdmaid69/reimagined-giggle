119d8f58c73a3ec545500d3ac2f16d094cb573ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:04 +0000

    owe loss clear sound floor

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
