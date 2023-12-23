9b52364c79d70112d41f7beaab7313d909e95f81
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:17:15 +0000

    operate finally effect public catch

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
