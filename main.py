ffe153c78d601b181cac2537ac9fd3f9670ad9d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:14 +0000

    niggaacademy star clothes soft book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
