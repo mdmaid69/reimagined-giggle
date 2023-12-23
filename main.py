415dc57f44a7520240b7bb48c9c3d0ea4f2c59ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:38 +0000

    thinkover rarely rest another shop

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
