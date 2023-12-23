f29a4713dfea1de5fcde3ed035229c11825cfc16
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:16:01 +0000

    bringforward press compete key day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_today_date():
+        return datetime.date.today()
