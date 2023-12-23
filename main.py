a36eb18ecf1e48cee2fba0defdcd9e98935fcc87
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:29:49 +0000

    regularly obtain telephone contribute tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
