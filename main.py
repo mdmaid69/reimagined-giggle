57d19e50425605fbb86c1264f0fe2c252fff9184
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:47 +0000

    wave nowhere successful another or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
