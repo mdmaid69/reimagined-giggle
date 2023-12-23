43ed8e952af0d818f2ef6c15d59d07fa814eb873
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:27:21 +0000

    Ll company rent height tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
