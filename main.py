ad0dc654d3935bb9432f7bb295346fd96725e9ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:12 +0000

    own furniture introduce pretty bet

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-for i in range(5):
-        print(i)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+name = "Python"
+print("Hello,", name)
