70ef8e402d88efba46362749bda41282b5bf5522
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:54:11 +0000

    drop comefrom arrival lunch shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def is_even(n):
+        return n % 2 == 0
