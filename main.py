e6c2add548e6c3d2b2763661cb5400adbfcdbdad
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:52 +0000

    stranger crime breakin pay high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
