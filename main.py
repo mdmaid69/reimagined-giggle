58ec2d2d6b9dbf98b43aabfd75e6a128bbbec1e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:19 +0000

    shop dish ought order means

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
