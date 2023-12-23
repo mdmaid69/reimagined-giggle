d58fdcb89ac05e7f1287fb1a138725b867dd4a19
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:21 +0000

    rush contrast chat this spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
