aade99f88d7983e9baeb80ec1143887348278cb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:49 +0000

    odd neither deliver lock increase

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
