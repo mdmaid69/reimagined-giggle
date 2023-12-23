d70940fbb21bf653c3dd6e9a004bbafd8bd2cddc
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:13 +0000

    breathe will cut giveout movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
