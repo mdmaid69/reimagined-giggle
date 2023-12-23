fd7e821823f84fbc741f3f13e1faea425f28d59d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:06 +0000

    pronunciation last thankyou identify harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
