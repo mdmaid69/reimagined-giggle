fddc688112f16019dd6cb523b4a96be1fd1b3afe
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:14 +0000

    cover claim praise 570enjoyable moment

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
