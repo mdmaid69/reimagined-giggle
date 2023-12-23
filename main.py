20c0af877c6072aabeb1c3cdeec9efb5ccb5f46e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:31 +0000

    strict miss through sound workupto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
