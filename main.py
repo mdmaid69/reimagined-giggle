26d2c72cc7fa3f8be195f99e86c359ecf99c147f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:04:29 +0000

    copy wet smell settle like

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def reverse_list(lst):
-        return lst[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+print(re.match("h.*o", "hello world"))
