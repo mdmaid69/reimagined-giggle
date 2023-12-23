b50319f23813da07ef0def3f1e0983d606c99489
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:28:09 +0000

    conclusion cover before discover sure

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
