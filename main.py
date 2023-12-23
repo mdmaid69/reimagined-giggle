fdd7545f9ee3b5097ce084cbb64635e430b30059
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:27:27 +0000

    wrong big not offend unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+for i in range(5):
+        print(i)
