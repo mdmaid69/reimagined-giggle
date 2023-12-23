e35e716ea0736674c553dbefb116d0ab77fd343d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:49 +0000

    pretend backwards degree holdup hold

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
