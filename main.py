db1d90b5454482e45153683feded337c12192ca4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:45 +0000

    control angry rush compete exactly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
