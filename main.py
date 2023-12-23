6c5e142c1823a6f1ae5ec85df94d44b46baec2d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:17 +0000

    quarter away to Cc closed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
