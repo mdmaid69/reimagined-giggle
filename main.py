eedbcf43e8172fb8d6fa075899771188855dc210
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:57:14 +0000

    all yours furniture opponent fence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_arc_tangent(x):
+        return math.atan(x)
