4f87999bf6ba0b8fd1b3b63f0faae5aa736cc9cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:54:26 +0000

    position calloff recovery blame mine

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
