a411e186fec104ed9405af08f78f30d08a5bba17
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:49:35 +0000

    man equipment building challenge left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
