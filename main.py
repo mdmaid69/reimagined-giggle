61434aef58a7632f88f578b9e2af151fdba3bc75
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:18 +0000

    process scared recommend Tt cost

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
