d58fbc64b8d21f9c91cf3c3cf7539433dec51276
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:07 +0000

    mood course service tape spare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
