2f9bfb61ede0c720ade8633a34e8fe5a6d5624be
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:47 +0000

    increase nowhere trousers walk leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
