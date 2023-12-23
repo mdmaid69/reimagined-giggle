50b407554a391520bb14dcd757aa76668e8fa1b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:57:05 +0000

    turnaway occasion bored uponon jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_list(array):
+        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
