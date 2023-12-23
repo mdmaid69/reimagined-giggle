4ac4cbb1b6b80e2cab5d67992053dd143bb2d6af
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:03:58 +0000

    extraordinary sentence rely lead come

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+for i in range(10): print(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
