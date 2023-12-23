64c86301de3b9018cb6eb33be3f89f2527492260
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:30 +0000

    pour mainly manner handle this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
