eeb22f6ac316afa939741c78f101bac715a36e03
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:28 +0000

    keepaway sticky mixture test mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
