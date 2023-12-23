e90c64659f0cb8be539a6c8f2407cfdbe49d3fa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:35 +0000

    every feature proof neat breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
