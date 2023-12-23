7f46aace615ce10adedf7c6a525793f38cfcb866
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:04 +0000

    dream concentration there opportunity friend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
