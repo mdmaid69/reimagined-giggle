f17353ba57312ba023578b4b83d27cd6aaf6ef29
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:46 +0000

    anyone well shadow report material

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
