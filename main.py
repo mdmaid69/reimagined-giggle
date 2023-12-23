744d471f4ca4af7a82e52087c5caecc2772aec64
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:13:09 +0000

    easy damage report trousers agreement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
