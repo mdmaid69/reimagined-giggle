a2538019ff4accb051ca04dfdccbd20418b4ffba
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:20 +0000

    interview nearly regular ball yet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
