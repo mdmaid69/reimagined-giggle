92fde1a0b06802ac1d7a8e8cbb3c3ba0d4387da3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:19 +0000

    advantage right hardly send ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
