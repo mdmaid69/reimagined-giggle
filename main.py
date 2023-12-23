e7b77708f009315c226e892af2f9df2688621516
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:07:42 +0000

    must total name foreign in

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_density(mass, volume):
-        return mass / volume
+print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
