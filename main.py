6487566a7c491323cc40294215ccac7b974d82bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:07:31 +0000

    cheek feel clean university waste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
