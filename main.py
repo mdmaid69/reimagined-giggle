4962680403c38d06f0f624155f82227ab14e9e21
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:04 +0000

    through ticket appeal towards trouble

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print(sum(range(10)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
