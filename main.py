9a98c80ad40b570694ecc22eb535a9ae120b0aea
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:35 +0000

    himself almost recent Jj knowledge

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-print(time.time())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import numpy as np
+print(np.array([1, 2, 3]))
