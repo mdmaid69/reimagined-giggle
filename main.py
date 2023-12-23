61b3aaec96be17a954a8d4313372d44f5581ef35
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:09 +0000

    person special interesting corner teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_directory():
+        return os.getcwd()
