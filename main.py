042f35b7ac17eb4cec3a59618a0d0834131f1125
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:11 +0000

    approval thanks certain lean lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
