f6bc1182076d4b26a915caa4700d97c19780f927
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:35 +0000

    easy couple bar complain pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
