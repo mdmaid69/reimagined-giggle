0141cacd6ed29642101b2a9a4b5b69fd37d075c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:25 +0000

    comeup holdout once present minimum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_work(force, distance):
+        return force * distance
