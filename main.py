f678df5e7ca89c520ec7b0f682aca48c6925f1b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:09:59 +0000

    unless generous project food exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
