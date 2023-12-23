2e77f3881a6ea00e4d8e90ca6e41f9e6f321123a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:10:40 +0000

    deep recover off curious switch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
