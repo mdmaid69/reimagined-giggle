5ca63f204a3875741b7e72d8c35ad1f0bdd85ef8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:17:33 +0000

    cost cross stranger serve desperate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
