fd6c977ae5d0d5dd1f2046ceef96095d0d52b24a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:32 +0000

    develop me lay 565end responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
