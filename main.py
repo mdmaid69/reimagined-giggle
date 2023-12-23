92cdf8a019e9f5e47797c1d5826aa4dbfc633abc
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:06 +0000

    floor finger meal press true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
