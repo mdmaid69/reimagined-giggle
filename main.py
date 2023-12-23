b6fe5f4a47ee749932d791f566a15ac62e7bfd5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:21:25 +0000

    rush man better contents relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
