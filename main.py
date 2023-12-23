29d148f4d44e58234f709fb5acf9f6c379ae8f4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:51:45 +0000

    raise cold later union attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def set_array_item(array, i, item):
+        array[i] = item
