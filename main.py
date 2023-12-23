679b894eba06b6cef07c8f44db4ecf69b67e5193
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:06 +0000

    exam recovery lean survive description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
