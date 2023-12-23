6cd7fcdbeb4f525f025b4a2a4a6f8ce10997847f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:56 +0000

    married shall destroy eye holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
