03cdf2bf6b7b62cecc9f167e371635fe9c08a914
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:59:09 +0000

    window tomorrow wind port flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
