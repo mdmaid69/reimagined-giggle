e751af3c189be640ec541945f846f5e66ae8dcb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:37:19 +0000

    culture artificial separate practical once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
