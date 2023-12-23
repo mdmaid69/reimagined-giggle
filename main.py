278244a1b2171c49162a391292acfa23c314f679
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:26 +0000

    shut sink at set offence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
