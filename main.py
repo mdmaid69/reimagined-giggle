5935be63e5a8df40abc4a5192dd1cef93fe8ef42
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:35 +0000

    test now his reduce share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
