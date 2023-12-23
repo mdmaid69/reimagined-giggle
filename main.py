53ed70a576597a5ebb79c96ac99d53aba95d5941
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:04 +0000

    doubt place considering promise Ss

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
