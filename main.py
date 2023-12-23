b939f3ec9fd5fd6b0c1014afd99bd985e8d80786
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:35 +0000

    hang divide quickly pause weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
