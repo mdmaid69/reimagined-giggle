98f43978a6c90227a2826fe09e6a16d651b47804
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:19 +0000

    petrol turndown land wrap to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import time
-def get_current_time():
-        return time.time()
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
