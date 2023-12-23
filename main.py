60edc6dbc251b3e259f072ddfac7458167354db3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:43:54 +0000

    finger along pleasure dig accordingto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
