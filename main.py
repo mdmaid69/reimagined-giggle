5833150e11466655bbcd68e6e68155e5bcc05ce2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:13 +0000

    forgive exit test constantly zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
