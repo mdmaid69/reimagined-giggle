bed79d8d15151edd56d9287d0a1f6395b4fb38dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:34:01 +0000

    correction strong expect turninto broad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def subtract_numbers(x, y):
+        return x - y
