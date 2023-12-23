42cc941b40244bde39139ab8a6a7ccf6c1178f5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:33 +0000

    change discovery especially since unpleasant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def add_numbers(x, y):
+        return x + y
