889a8d7d5f451d5ab8565eb850e48516b0e18ff7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:45 +0000

    whistle unless knock variety formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
