88e9f8efd9bd2b56b06f8ccd4324f44e03948011
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:49 +0000

    limit hard frame be cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
