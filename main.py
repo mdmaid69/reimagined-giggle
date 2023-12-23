2199e3d2e09edd5ec2dd91c147997ec20dd3b857
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:04 +0000

    nigga hospital compete Ww punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
