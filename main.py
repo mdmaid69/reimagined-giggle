8565c218eae8d53415fed419bb1faef7dee1945a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:14 +0000

    glad cinema own nice knock

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
