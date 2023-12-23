53ecdb097bd3caa6fe686941cfc49dd85cd27604
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:16:55 +0000

    your calm behind of cover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
