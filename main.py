b5daeec8ac461a09c48a02ebc4a19504de0f5aac
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:29 +0000

    situation wish hit stage mix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
