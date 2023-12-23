36f96020c545f1b54fbe96ce6cba75f399e00187
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:07 +0000

    person characteristic record problem base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
