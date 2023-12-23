2881c90ddd84d74df4f8bb9ad771be3da0d88c7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:37 +0000

    pure future ashamed develop support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
