6aabf018471eba2eaf4e9f596de4be8b236cffff
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:01 +0000

    medium plastic middle present instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import logging
+def log_message(message):
+        logging.info(message)
