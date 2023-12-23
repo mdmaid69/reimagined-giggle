5c556c1b1aa65b8135b9700a19a51df7fe481f98
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:33 +0000

    blame train someone matter trip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
