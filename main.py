4ec1f0e2fe557ff69c71768da216bcf801eea87b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:41 +0000

    reply turn happen skirt compete

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+from collections import Counter
+print(Counter("hello world"))
