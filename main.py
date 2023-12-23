8bfb7405400b1d4f480c37d32d900ff7a2b20ef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:17 +0000

    regular drop reject possibly plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
