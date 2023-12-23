a638ffc6255279e5648b29d6bbad5b68f0cafea5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:01:17 +0000

    display journey steady short afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
