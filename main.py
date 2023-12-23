a52524bba2fc29d84aaf0094d9c58ae93bb01617
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:35 +0000

    cross usual shopping impatient train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
