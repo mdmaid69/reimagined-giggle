bc9c75d747c34ce868f71742294aac620449dcdb
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:27:40 +0000

    appearance improve marry hope catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
