b62d9ff2d01d6227f2ba991e67ea067a336aeec4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:49 +0000

    home warn father kind variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
