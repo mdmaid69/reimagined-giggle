5468ec154846f701f6b0624f03c7e31997bd5fc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:38 +0000

    advise music human fear beat

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
