e28c8b61dfd4a2e548208cb7fcadbb9f89c3fa9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:55 +0000

    ought nigga road snow set

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
