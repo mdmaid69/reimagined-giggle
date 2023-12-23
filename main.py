54c7c638f7e97af0134d639b0c253820a0249d98
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:13 +0000

    study degree cum sight embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
