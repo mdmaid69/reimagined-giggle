09ba709e1c172954e64aa48399e8f41ce2ebcf93
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:53 +0000

    duty fill successful line level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def square_number(x):
-        return x**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_str(array):
+        return str(array)
