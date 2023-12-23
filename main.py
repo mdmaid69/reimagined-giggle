0eb1b43b12934a22a1975ef3d042967fe5dc0aa8
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:22:05 +0000

    else deep sure basis operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+def calculate_speed(distance, time):
+        return distance / time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
