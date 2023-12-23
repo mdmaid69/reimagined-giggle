716758187ca800e458d2d9253a511cd228c3b3d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:35:38 +0000

    left intelligent deserve now since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
