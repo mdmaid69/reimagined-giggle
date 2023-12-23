e2e5bd015473448fcaa8270288e041ead3d24bf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:39 +0000

    valuable place smell division upset

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
