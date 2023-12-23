da7a556f3eabc89c26aa4919b5c034fc48825f51
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:00 +0000

    risk position bringdown takeaway trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
