005300965328b4e6fc7d593f26dbbf912e8ba072
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:32 +0000

    they in convenient anyone manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import re
+print(re.match("h.*o", "hello world"))
