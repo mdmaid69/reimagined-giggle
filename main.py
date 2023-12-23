be01e01f611f0249caf74e8c0199068529404b7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:28:34 +0000

    workupto scrap journey behave pump

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def sort_numbers(numbers):
+        return sorted(numbers)
