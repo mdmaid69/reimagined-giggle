73019d9da147a8ec3d2b2d05c5a246cd7371dfbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:04:29 +0000

    confidence disappointed holdout remain thanks

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
