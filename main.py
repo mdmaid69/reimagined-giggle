d3c9f44f64959de55b2f4b5aff4b6012647ad426
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:55 +0000

    clear last much up issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
