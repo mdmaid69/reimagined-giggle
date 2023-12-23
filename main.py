069ef698722fcf3ffa133f41dab244409fbe10fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:38 +0000

    criticize place lookup performance sweet

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_acceleration(speed, time):
-        return speed / time
+print(sum(range(10)))
+import math
+print(math.pi)
