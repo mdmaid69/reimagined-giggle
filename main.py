276863a7bbe96a57f45c843d8803a73cc742ac24
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:25 +0000

    impressed land ignore real cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
