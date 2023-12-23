039be37f46e51a99a7fb7edac9898943eee1bb26
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:28 +0000

    use private bringup smile ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
