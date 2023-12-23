2a4ea04b4e78a31a850f4a4a827043ff7936418a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:38 +0000

    public lock open flat border

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
