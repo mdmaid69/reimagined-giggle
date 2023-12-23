6e32f32903df70493ac9dc895e0e641e51ceb1d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:36 +0000

    world at hate game base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
