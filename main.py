e5b3a31d8e49c3dd721ba3244a5826de71da2661
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:27:55 +0000

    concerned spin place schedule while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
