b737bca1998d5891c86e317a235743bd1f8c7d01
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:12:13 +0000

    error valuable possibly deal noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
