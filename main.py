8321d0e77b3795a827284f86dec4b3b6c609e822
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:44:49 +0000

    wide naturally black balance accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
