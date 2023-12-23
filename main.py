5edc146f63618afbf2fba87901b66344150f3526
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:48 +0000

    unlucky fall ride town inside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
