bffe710fae98ecbf377c9a95a8d4287d3863201a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:51:55 +0000

    at method could bank arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
