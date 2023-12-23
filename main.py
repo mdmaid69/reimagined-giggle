78a1c282cfe55ecb1e4e642bc4873c56609ad907
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:35 +0000

    flood autumn black nervous market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
