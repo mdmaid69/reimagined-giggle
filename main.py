c3cfeba49146a3f6bc1bc507b48b887a12b6d496
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:16 +0000

    question single doctor vision prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
