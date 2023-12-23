692791106cb4ef9ac2ac0053457754fab00f9877
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:02 +0000

    ride nose complicated official compare

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
