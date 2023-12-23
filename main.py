f8fc82868fcb61b568a298149cb774f619bbc8ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:35 +0000

    standard accidental nose dear proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
