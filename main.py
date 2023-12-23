917e77aa9c1ae6e7dd8ee49265249d58c2b95d74
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:37:49 +0000

    juice shock name brush hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
