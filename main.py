3474092fa2c294e7f1b306582ffe3f83ef98070d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:53:08 +0000

    alive brown burn brain error

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
