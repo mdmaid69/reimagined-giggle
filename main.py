67498506cdc91cedde14389de142451ec487ed76
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:26:16 +0000

    admit wonder score wind lost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+print(re.match("h.*o", "hello world"))
