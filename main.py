b53c53ff0ce3f0319247a046081adf276b277a3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:59 +0000

    juice whenever later violence rent

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
