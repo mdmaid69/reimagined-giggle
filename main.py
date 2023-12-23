747d1ede451ab1f534cdddc823c6950fa87c4ec2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:36 +0000

    black movement near about illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
