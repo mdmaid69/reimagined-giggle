1b84234fbe6758a86896c46b21cf0bc3cfb8c2ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:00 +0000

    marry expensive job reject complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
