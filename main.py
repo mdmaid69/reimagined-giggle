3bc1981715711b051081dceeac3cd402d575fbd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:56 +0000

    debt model his frighten glad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
