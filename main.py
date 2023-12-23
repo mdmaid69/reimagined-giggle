764acd31e149a251cbb8e10f7fe7535717823b54
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:50:01 +0000

    also add pronunciation difficulty ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
