b9489ac0985dc398efdaec1ac69d21740fc7463d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:44 +0000

    connect last aware decide opinion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
