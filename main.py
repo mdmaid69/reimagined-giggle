b96536a0489cbde32feae7f4a56571db7683ec05
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:29 +0000

    high needle emphasize delight 572enter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+print(time.time())
