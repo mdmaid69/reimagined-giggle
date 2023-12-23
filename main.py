600d31e080d6ce02acc8e6a3127603e8a251c3d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:24:11 +0000

    race himself follow promise flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
