868a5550e458c0c7f32c235f9cab42a4209f832a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:27 +0000

    impressive begin seem still punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
