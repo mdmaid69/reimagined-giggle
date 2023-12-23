563c747ef95eef5614df1f7480e49c8495fd24d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:52 +0000

    about release rare actual willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+name = "Python"
+print("Hello,", name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
