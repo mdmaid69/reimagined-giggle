856e79ef7659b10904d7d277934a32f1d2345efe
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:54 +0000

    muscle getoff sort check alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
