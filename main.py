7ad33211caf04973811e5e05ac59460fb0c4ac47
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:58 +0000

    forget sensible future laugh patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
