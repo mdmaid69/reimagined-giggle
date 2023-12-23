735c132d7874986910d7a40de21570941d8ecbe3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:08:04 +0000

    store call Jj love sail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
