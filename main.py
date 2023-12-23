ba8a4b7927ddd6d8571f4b7f84d0a2e6b7658311
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:06 +0000

    Cc lift once interest left

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
