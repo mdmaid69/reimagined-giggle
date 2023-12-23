a4bc0abbfb2f64df47cf380745bf84b3d78cdf91
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:48 +0000

    moment only scrap attitude computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
