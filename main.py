6bb8a942e0e3ed7e7dd65441188cff76512570d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:41 +0000

    finish dark guess fix possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
