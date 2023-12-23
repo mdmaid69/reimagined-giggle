2dc2611a640c1757c3c9e6dd2a2b83a3bfeecb75
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:11 +0000

    freeze pride smile runinto use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
