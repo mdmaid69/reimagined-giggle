8895fb487ae95d643f71ff3c12cc6d5619453992
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:16 +0000

    love visit greeting kind damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
