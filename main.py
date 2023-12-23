c48cb3a9eb105e67f1d86d4b8520789c785be90b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:55:29 +0000

    whoever bet manage gather prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
+import math
+def calculate_floor(x):
+        return math.floor(x)
