25a3d25b6dd3423ea99450aede113757e85903e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:59 +0000

    cinema family foreign song muscle

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
