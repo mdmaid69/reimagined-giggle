b241b861f21e04aa7d6dfe6aafe3d66d3f4900b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:33 +0000

    surprising could culture remark book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
