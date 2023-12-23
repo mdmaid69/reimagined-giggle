53e194e88d9f60119fc3d84af0e81f46bef36423
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:26 +0000

    even programme loud summer centre

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
