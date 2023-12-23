f178f02326f4ec6da24ac3775badcd6c7fdd61e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:17 +0000

    manner try pressure style giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
