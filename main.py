2a839f8e1c1f2226b737e61190270308750557ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:43:48 +0000

    way combination city fix guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
