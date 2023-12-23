b5a8a67138a2d71ec3f5a03ed152f143015ad4b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:16:40 +0000

    talkoutof else left deaf rank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
