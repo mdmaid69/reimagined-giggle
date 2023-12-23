d50438c21300a967c6f0396b22b5ad4cc0df1b8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:33 +0000

    case comeround sea should snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
