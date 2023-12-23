3080c5f5876f129419d38e4d1d2396c7c5f6a1be
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:59:09 +0000

    confused glad concerning culture however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
