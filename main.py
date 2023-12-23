51f493da909d4fefb05de43d405e386655462942
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:35:38 +0000

    frequent telephone pass sea should

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
