f5dfcec83cb06c9687491ab043098ab2024a7d29
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:44 +0000

    bring juice mean takeback fair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_speed(distance, time):
+        return distance / time
