c042d5f5d3092d86335f0b046b72409d31bf63cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:47 +0000

    march afterwards wet appeal name

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
