372065e123400f8d23a33b42baa506d59f14aa75
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:24 +0000

    gap broad Jj best major

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def sort_list(lst):
-        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
