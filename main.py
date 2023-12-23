389e8686ec7ef4df7d6ddfc4e80358c806284400
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:47:35 +0000

    people hungry stamp reaction hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
