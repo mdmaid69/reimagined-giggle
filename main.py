e10b41bb9f359b85eb75043460639539a17ff11b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:19 +0000

    tie flow creature winter silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
