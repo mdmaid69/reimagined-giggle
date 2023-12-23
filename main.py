3326cbc9b472e4289141ad86d333ef394c535c74
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:08 +0000

    be runaway yesterday muscle government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import random
 def roll_die():
         return random.randint(1, 6)
