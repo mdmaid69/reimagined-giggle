ecf2b7d9424b99d520bf0dc0d8c7684d67c730e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:27 +0000

    could independent pile Mm suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
