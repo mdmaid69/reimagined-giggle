e88996adcd7e5eb1c74b24031a7e94711b06e1d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:28 +0000

    mood bring lunch unit repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
