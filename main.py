d18acca689493cb4b21637020478b180b85b942c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:03 +0000

    instant broad leg straight work

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
