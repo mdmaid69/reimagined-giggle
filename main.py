c80d7267674872e38c5d05a3eb5c06b44ec429b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:00 +0000

    sensitive boil sharp earn judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
