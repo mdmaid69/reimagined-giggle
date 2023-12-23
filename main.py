2965e51d92331303e14bbf0d09561276921e1519
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:35:11 +0000

    programme exam gas happy refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-print(math.pi)
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
