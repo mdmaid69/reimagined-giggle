2d27f26af9b6587d64fb28133e9e9c4fb7007511
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:48 +0000

    cum health personal gate discussion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
