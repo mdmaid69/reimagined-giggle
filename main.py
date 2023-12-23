ab02c5b3ec1e9a509e1bf8f6be38cc5ea437df2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:22 +0000

    size trial freeze meeting programme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
