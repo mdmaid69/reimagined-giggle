9ef478a9babc99f24436dc3d1883bd412e351fcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:59 +0000

    burn welcome weather score concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
