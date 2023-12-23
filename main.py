b1c0a4f2243a56f5c730c92e6ce696bba6af1f02
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:12:39 +0000

    spare refuse open recovery obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
