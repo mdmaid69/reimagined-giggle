7c4fa1b5730229e80e1f8f09a3ab1080dc59a054
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:53 +0000

    doubt search bear hurt strike

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def subtract_numbers(x, y):
+        return x - y
