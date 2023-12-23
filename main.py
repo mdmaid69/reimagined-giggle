bff96b3ce66b469d20ec7aa11fc8cb4349814265
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:13 +0000

    beside somehow life who rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
