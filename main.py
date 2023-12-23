6171f8e7b1572d7badbdd0ad30a436680c4523fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:43:04 +0000

    as unkind service achieve content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_pressure(force, area):
+        return force / area
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
