bde3322ccbfa55a5bdff349af4e34080e3f8ef4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:45 +0000

    sudden ground dance spin official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_length(array):
+        return len(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
