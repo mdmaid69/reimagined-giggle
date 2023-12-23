ab7386b65f9caf356b26e856604b1f79393e1d2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:10 +0000

    compete keepaway father temper church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def convert_to_octal(n):
+        return oct(n)
