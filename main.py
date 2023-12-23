d40cd4c0c7d08cc64e09f735274e57cf468897b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:37 +0000

    instruction satisfied previous fit holdup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_ceiling(x):
+        return math.ceil(x)
