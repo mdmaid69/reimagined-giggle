5405849df328eb84aa144aba99546eca2261a1e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:49 +0000

    about roll woman yes piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
