1811d42d111f118b5f3aa11f9228e194b5c8adc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:21:23 +0000

    shine bound worth brain scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
