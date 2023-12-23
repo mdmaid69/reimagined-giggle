e94b461bef50a8a30461231c9677d640120aa083
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:11 +0000

    draw search never thick definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
