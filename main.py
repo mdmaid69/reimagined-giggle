5b019205c63d4ae81e9746a835e89904ce578532
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:00 +0000

    glass reduction Vv hour mine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
