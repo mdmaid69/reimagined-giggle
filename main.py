b33be841f58707e52c3d877af36980daac6c96d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:42:10 +0000

    touch contents earth again marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
