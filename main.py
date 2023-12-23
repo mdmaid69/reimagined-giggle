8de56c53d5eeeeb9fbf75b6a7f692f95a688bbb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:58 +0000

    music meanwhile announce solve truth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
