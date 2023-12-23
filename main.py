bcd42cd0d2681bb4258408a56fbcaf3484b28213
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:09 +0000

    call because ticket insist previous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_max(numbers):
-        return max(numbers)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
