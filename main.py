a6ab1d460e3d4fb50739c8da1efd1a26ce331774
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:44:47 +0000

    pollution fetch wish result not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
