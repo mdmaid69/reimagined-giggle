e37850dd929793b7e7d1a0835a73d54df50d8612
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:53:21 +0000

    at getback feed sensitive young

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
