b13761990464d8be8cd4e96309ae62e0cb29a92a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:10:23 +0000

    as tomorrow afterwards accidentally matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
