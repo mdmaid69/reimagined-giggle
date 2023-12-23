2b2cefc9ab195ef29a1ff7038a81a5024d5b29d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:28:49 +0000

    circle nice dead slowly public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
