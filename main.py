46b9e80b88a59be1eb0b09959241d79a497d81a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:41 +0000

    issue wrong wide mistake prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def add_numbers(x, y):
-        return x + y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
