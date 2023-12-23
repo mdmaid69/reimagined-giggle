c1ee1639a582d0cb34103858520dd43fda8dce08
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:21 +0000

    flat round fix worth closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
