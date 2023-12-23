dbe7d3e1ec9ff7dcd72a991d0e5fc6950a6bcb85
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:57 +0000

    hardly sentence note height protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
