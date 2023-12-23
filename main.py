1b9dc69bb3aed1c79f9bc8dc863c3f96e9385636
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:57:06 +0000

    quit illness in attitude would

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
