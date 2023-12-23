6190bc0a3fc57184d41e7391002f0fab19a13398
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:47:12 +0000

    hate bath eye perform attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
