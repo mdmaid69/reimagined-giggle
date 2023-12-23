1f8cf1c041e25d2035951d2abc0e0fb8afc1fc51
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:20 +0000

    birth meal public charge each

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
