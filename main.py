1d87fa0975b873a57555e34aa15661de90481ee0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:01:19 +0000

    serious pride square further lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
