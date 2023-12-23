1072f4cb988eea35231b61a7de9e39644b9321b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:55 +0000

    though police anywayanyhow repeat scare

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
