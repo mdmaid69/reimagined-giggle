9c897d665a1792cfed1692f5c5d5d7adb204dddd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:14 +0000

    length frame store stop naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
