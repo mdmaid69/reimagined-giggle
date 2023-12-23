dff70780ada0c96ccb2d04534f82a82f594693f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:35:51 +0000

    bridge surprised twist same all

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
