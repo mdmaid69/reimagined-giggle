5899a0022a09523e6b71863e9da58e6633b5f84f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:12 +0000

    organize sale otherwise minority friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
