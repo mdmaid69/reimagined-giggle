649fd41dce3d7739f481e136e873596bf44e79d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:26 +0000

    powerful passenger settle intend between

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def split_path(path):
+        return os.path.split(path)
