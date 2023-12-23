8752188344af28ff96ebd1944824c426f8f026e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:13:33 +0000

    belong see run rank on

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  def sort_list(lst):
+        return sorted(lst)
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
