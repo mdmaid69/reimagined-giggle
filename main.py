0fe143dccb82f429d9681443209526a6e4e36c37
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:35 +0000

    force error account both answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def count_elements(lst):
+        return len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
