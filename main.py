7ac18ebd92a0a81f06384484a8396c454ace8d15
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:30 +0000

    wet welcome past partner fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
