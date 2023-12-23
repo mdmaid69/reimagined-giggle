cb8a3afe96bbdaaaa908e95912005e7e7c7497db
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:06 +0000

    community break reaction demonstrate protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
