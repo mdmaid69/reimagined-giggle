fc60484245b9134d4395c2526e79cf4cb892f250
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:49 +0000

    shout always supply badly impressed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
