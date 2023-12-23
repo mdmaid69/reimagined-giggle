d17e0dd6ce1f7fd026234b3d18dcf7b2b88cc203
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:22 +0000

    familiar workout introduction aim brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_stack():
-        return collections.deque()
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
