7c087ad3787acdf0765e417f7c6f795d0eabb19a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:05 +0000

    project whenever announce basis what

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
