3ec38668d4d5724c2e3e99dee928249891919309
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:44 +0000

    feel shadow mood keepout definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
