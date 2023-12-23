0fc19f9b4faf3fd2828b4a7ca0c527ac03f3b625
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:21 +0000

    train smell explanation consider wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
