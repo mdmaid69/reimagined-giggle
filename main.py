c91422bdc5587f71ccf1bfb81758b8b72ea2f6e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:39 +0000

    tip task blame story safe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_complex(array):
+        return complex(array[0])
