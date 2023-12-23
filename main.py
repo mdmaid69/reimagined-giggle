adaef1c0a8773a2371c8a3238578cc088a374f7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:33:28 +0000

    workout machine neither notice wind

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
