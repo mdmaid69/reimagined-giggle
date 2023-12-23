b8ca012683e1eef8b457eafa147257d57f7055ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:32 +0000

    pocket terrible down include makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def cube_number(x):
-        return x**3
+def convert_array_to_string(array):
+        return array.tostring()
