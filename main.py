9ad635855f6cc31b1625c5cb3c622f510f58e8ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:37:34 +0000

    giveback support on middle expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
