4a60e0974c59c4246fd8d1782d0399058e118c2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:09 +0000

    rate outside slight tidy disturb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_acceleration(speed, time):
+        return speed / time
