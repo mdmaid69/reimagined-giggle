5ec62584e311bfc347b04ad0b5e36974f60c8986
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:53:12 +0000

    interrupt spend street pretend man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def cube_number(x):
+        return x**3
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
