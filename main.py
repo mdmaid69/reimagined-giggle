ba5a7add6567f7275e27313e596fd58589f506c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:12:50 +0000

    break well measure mess opportunity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
