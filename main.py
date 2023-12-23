a95a456fbaaf5c87289345cb570a7a1bd27013e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:10:50 +0000

    autumn miss friend situation present

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-print("Hello, world!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_list_from_array(array):
+        return array.tolist()
