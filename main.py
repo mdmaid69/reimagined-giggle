18cb3a4f3eabb43dfbcc0ce4341d66b701890412
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:10 +0000

    save noisy feeling noisy temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
