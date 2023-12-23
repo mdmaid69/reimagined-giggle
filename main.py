3b1ec6026113a8059b025bda7aedcb14f669ef8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:00 +0000

    well unfortunate suddenly yes runaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
