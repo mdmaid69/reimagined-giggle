f09913a0bc03cf79a159f369ca8d9504e1247e0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:55 +0000

    getback turn tax table to

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
