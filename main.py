89fd798b34c2bb3508929d3e89f38ac1f8587267
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:33 +0000

    back bang head across picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
