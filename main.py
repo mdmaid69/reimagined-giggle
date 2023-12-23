541cd63ac8db144ffb2f40588a981aaaf93ad86e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:03:26 +0000

    feel worse could film remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
