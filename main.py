a658b35031ce2e123812380cfd83725b133b95a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:06:40 +0000

    complicated positive general arrive neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
