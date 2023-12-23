9e4c6299261377a9ce3b9f49fe6d07d591bec6ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:06 +0000

    set under approval company delight

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print(sum(range(10)))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
