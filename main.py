9214e59fc6cebebf2c34165e0da9c9be5218503c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:36 +0000

    develop relation criticize 567enemy should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
