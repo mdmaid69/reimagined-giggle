e1a62d212a38d6c46b5681965c540387f220f498
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:22 +0000

    takeoff shall debt pump window

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
