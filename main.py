d8e0217a0947229162578499715912eeefdc3598
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:17 +0000

    luck healthy quiet uniform callup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def convert_to_hex(n):
-        return hex(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+print(sum(range(10)))
