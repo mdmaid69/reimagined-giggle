f34c8093b41738158075ff411eddbfb429819252
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:24 +0000

    over laugh moral convinced means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
