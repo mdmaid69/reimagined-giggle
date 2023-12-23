426d1b97f13d8867768e42debe9a730622df9e89
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:46 +0000

    nervous jump can so apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def convert_to_octal(n):
+        return oct(n)
