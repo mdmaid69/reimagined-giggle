6b1a75e3e2f69e1b771181d695f5bd5633122e63
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:07:43 +0000

    turnoff fit front exact drive

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
