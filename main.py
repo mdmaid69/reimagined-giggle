fa2613d6271f081c815ef7e760ac9bdab8edf338
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:50 +0000

    uncertain regularly backwards emotion birth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
