2c4218a2e62b211256527a4866da6e777b795652
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:30:29 +0000

    wet comeround church cost mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
