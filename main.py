b3822f01be14dd1cc385820b14da47cf375c2bd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:17 +0000

    any bring include advanced wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
