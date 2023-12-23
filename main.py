dfe7786da419b45d271150648be8f6c0a7f356c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:45 +0000

    lead characteristic payment factory dance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
