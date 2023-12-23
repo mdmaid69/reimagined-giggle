bc1818af67b50849e39bbcab1f7b72425f9d8c5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:26:32 +0000

    giveaway switch tragedy whatever wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_min(numbers):
+        return min(numbers)
