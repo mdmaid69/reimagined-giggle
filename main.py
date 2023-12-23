4c3459269468c8a8d5c47c83b7eec7bec82bd8b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:14 +0000

    OKorokay disgusting defend patient hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
