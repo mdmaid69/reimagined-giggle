4c2ad7ce9ee484b2066ca533ecbaf405fdd5ec53
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:30 +0000

    project scrape friendly paper runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
