96edf80e0ef3fd25a08f7ca0d3f4c98025357f1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:43:56 +0000

    apply would drink wrong laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-print(sum(range(10)))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
