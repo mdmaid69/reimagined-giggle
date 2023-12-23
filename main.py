8187aaa00cd070a32ba881bfd3adc1e5a747bd10
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:18 +0000

    wish star but conduct creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_base_name(path):
+        return os.path.basename(path)
