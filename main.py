54bb88f8e29eed53054340deb20d6512f8088b3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:22 +0000

    bring so excuse tense storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import random
-print(random.randint(0, 100))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
