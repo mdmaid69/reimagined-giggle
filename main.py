a0a8ee6da684862913cd573eaf423bb15e76a5dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:03 +0000

    middle wall deserve choice sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
