59ad1122c75b3d47bf16db2039e94e6c4cc9a559
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:02 +0000

    silent threaten thanks middle effort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
