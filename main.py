5b43f5d1095087dd08eece9a694eb9725256bd40
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:13 +0000

    shout string something nurse complicated

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area(radius):
+        return 3.14 * radius * radius
