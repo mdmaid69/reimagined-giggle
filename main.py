ae9fe587eb2e0dc0b565df77eab217a2ee207132
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:21:25 +0000

    chase usually pool indeed long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
