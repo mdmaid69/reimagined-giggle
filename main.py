736ab210a63e72ab2502c8a4daf3f10b533e3df6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:33 +0000

    colour roll possibility shoulder seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
