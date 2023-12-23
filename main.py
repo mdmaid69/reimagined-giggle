65d26e8cfa653a7fee4def7f2ae4b81e9fb00bde
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:02:51 +0000

    miss against about custom roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
