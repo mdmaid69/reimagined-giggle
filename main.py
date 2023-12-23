e03a3df3ee85244b250e4d5b3a64a780bfd9aa02
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:50 +0000

    can surprising match patient confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
