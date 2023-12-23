ef0ced24c89c00dc61b0b6d65142e8e3e540053a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:57:24 +0000

    later admit clean press fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
