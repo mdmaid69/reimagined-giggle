f4c681708ff678d5c35f8b2b118b059aaeb0b323
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:20:45 +0000

    buy bill expensive clearly start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
