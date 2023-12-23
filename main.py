8d7d6deaefc3c0240b6a4c6d34e709030a4643fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:10 +0000

    permission spoil scare basic quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import heapq
 def create_heap(iterable):
         h = list(iterable)
