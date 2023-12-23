f8799236f6ce0cb97a14088ec447aa7aab25fb1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:59 +0000

    hardly neat poison name experience

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
+def convert_to_hex(n):
+        return hex(n)
