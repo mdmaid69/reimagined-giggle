b0779f45e8f90d9d81c01ee6f5aa2ecd8f92169c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:23 +0000

    slow blow shall old press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
