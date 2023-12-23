deae00624de2de86e3d6f79f2524f9f9b7596f83
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:43 +0000

    distance across occasion front sky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def clear_array(array):
+        array *= 0
