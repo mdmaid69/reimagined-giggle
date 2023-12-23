fce2e14c7ba112e99b039853a9675f2f396e2136
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:32 +0000

    trial similar turninto report order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
