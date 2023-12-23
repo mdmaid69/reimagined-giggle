1aa86bc6633e1bb7da26f8f9e201726aac067bb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:40 +0000

    accommodation murder upset getback repair

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
