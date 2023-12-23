6a8cf1c714f90d180819022215930918caa4dd32
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:32 +0000

    should reasonable combination somewhere manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
