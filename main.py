2b85dcd6f7f461b7d08c3cb8500a6240db8332c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:36 +0000

    year second deep than for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
