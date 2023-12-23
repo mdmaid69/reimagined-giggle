62f85d726f23cb75f2fc54a1a216fc141e3d621c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:23 +0000

    protection Ll ask visit me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
