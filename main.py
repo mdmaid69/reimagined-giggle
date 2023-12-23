87f93e600a19b98148c04faacb272232dba1004d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:00 +0000

    sit beneath her punish table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
 def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
