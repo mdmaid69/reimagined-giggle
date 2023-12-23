1ca7eee97cb602cc664b5f8a8f313666fac294e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:34 +0000

    fashion bother broad display able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
