974cbe2a93628e44fff1c920b5530c65aa4a9011
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:32 +0000

    laugh blind away weak strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
