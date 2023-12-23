3b0d0c5eb223e12a50d8dd8b8640e694df757906
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:55 +0000

    bus repeat gas through mind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_pressure(force, area):
+        return force / area
