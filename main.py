bc73d2cbb1db589c659442f76dffd79dccd3b0d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:22:07 +0000

    rush field mainly should climb

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
