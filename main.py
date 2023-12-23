41b8647a6343f721a89c3f9ff18a97505443723b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:43:58 +0000

    system stamp either fat charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
