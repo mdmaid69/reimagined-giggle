21302883d06b4beda24ddea2196314e32e148cf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:04:20 +0000

    contact worried state unless neck

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
