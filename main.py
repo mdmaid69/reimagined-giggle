0535eb071fd35636243e36de1d9c38d07e9af6d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:09 +0000

    invite present mistake 564encourage finger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
