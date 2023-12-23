38003c08d09e832a0b08453268fb38b981cc28bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:04 +0000

    bear them godown bet care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
