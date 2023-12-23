6e0d905a771c79f7c295ab9e885f6c4948b6c57b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:26:41 +0000

    poor art zero sense guess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
