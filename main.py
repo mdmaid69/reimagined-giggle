62a44b1a7828ad506e0d38c6848ed9528b86ce1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:27 +0000

    method nigga brief really practise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def cube_number(x):
+        return x**3
