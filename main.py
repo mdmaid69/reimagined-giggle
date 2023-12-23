c9a74991acaa5391b22e6c8ad6a10868c565373d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:08 +0000

    exchange 566end thinkover cool mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
