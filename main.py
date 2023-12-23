b1d08b608e886ba35eba4f3b3a817d769604c99b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:44 +0000

    feed second can certain sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
