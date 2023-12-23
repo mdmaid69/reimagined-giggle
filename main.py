5f24f14c96175e5a56ebd30215d772a0e9e2078f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:13:17 +0000

    sky wash tongue stranger obtain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
