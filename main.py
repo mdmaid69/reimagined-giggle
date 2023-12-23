791e1821f7d2afdfaafa7877445a1080393608a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:51 +0000

    goout mixture breakinto wrong peace

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def cube_number(x):
-        return x**3
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
