54f5506158679d4daf8fa2833010eaf4b32c6901
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:41 +0000

    treatment appeal pleasant Itself write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
