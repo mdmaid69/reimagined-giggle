b253172d559d00908ed0760d9eb4e64570ac532e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:25 +0000

    black walk scream mine freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
