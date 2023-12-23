6b38dbc37959d43a55f472b59c214d1b0a3433ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:38 +0000

    certain could male important chief

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
