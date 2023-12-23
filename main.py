901ab7de8be477fcfd58e3d348cdd831244ce0cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:49 +0000

    ilovedicks backwards community one duty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
