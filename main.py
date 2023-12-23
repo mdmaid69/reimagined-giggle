ab258406cb71dbc2d90a7c45319e38f1fa7e7b3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:22 +0000

    recent rescue drive accidental turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
