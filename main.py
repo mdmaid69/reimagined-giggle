69670f5d99c33b76e6bd60ba85c4e6e8b4e938fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:53:59 +0000

    people coat remark accept describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
