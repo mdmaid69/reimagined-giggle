d727dfb659a3561f3a6ea41ade2c34b19f8e6446
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:40 +0000

    relax although reply judge thirsty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def split_path(path):
+        return os.path.split(path)
