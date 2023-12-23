d7383b2de99725e9e60795ae999ec89fcdcecf37
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:09 +0000

    main holdout still form curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
