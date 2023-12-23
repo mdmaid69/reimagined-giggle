5a32eb334074e0e6fd98dcbcfe9f8e93f74e1d4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:25:36 +0000

    change similar quit contents once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item(array, i):
-        return array[i]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
