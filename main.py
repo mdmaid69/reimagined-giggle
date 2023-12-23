e3dc447ffca2a5955045cc63cf65d642ae9ea88d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:04 +0000

    whistle free arm ignore proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
