47a0229e90f2f900154f817ade5bd3f45bf907de
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:13:55 +0000

    say task completely concern march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
