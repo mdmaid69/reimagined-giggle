925b0a1935d1566b807b83c30d131bd3efd0e7ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:35:14 +0000

    thankyou doubt lean bar proof

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
