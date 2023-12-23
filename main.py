5651afef1c972cd63b50fd82888a4403ea53db63
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:09:09 +0000

    control care mark town divide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
