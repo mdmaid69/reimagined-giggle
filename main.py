edc982e6b304e6664e6cd65ca746849574eea49b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:59 +0000

    point cry getoff content callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
