dfe813c8ba470f7e40b7b517a615ec57b74e91c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:22 +0000

    bet generally sometimes coffee opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
