e1ead4cc5da4befb2aa9abe53fd805d1a7d61cde
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:42 +0000

    reduce their authority experiment human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
