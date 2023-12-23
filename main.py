d884479496454c349f15d542e008688a3a260989
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:06 +0000

    comeround move town they country

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_power(work, time):
-        return work / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import platform
+def get_python_version():
+        return platform.python_version()
