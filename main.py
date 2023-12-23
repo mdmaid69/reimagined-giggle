cd3c156c2c8673eec8b047a5cd9ef53a17d03c0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:13 +0000

    favourite rubbish recent book store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
