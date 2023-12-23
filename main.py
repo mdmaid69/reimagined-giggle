9c976f19b4b19f19d346f17ceb7e09381c3588be
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:16:57 +0000

    refer fat note glass bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
