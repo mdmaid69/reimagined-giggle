f45de11d88538b923cdf6f7f784172e9c87467db
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:38 +0000

    dinner pause stick breath 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
