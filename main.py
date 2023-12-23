3cabc710623b7a56b99544d701494929e1a51e4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:01 +0000

    to burn estimate raise operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_combinations(n, k):
+        return math.comb(n, k)
