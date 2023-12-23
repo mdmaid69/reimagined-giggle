79c78f858344e9323e06651d4dfc600ce33fe891
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:51 +0000

    notice Yy equipment excited walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print("Hello, world!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
