d7dcb3bc53caf1f41b2065e69487c79bb522a710
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:59:57 +0000

    less left burn who think

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
