b6383eecfad53c7542023c9fd5f91fb77d4f5095
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:33:23 +0000

    sink sign ride react dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def find_min(lst):
+        return min(lst)
