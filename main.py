6f975543b08bf91b808a0efde8a648d87601988f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:53 +0000

    block cut fasten violence roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(lst):
-        return max(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
