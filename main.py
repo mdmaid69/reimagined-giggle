06ab9852dc387108f8c831adf4dd8f5e01d700b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:43 +0000

    hope true sport dick summer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def cube_number(x):
+        return x**3
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
