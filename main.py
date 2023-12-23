6accc0e336f57b60ce6beda5bb9b88b20833de80
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:51 +0000

    error chat coffee admire apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Words:", len(text.split()))
