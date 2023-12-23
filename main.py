eb48dca76b7e6ddcd11bbd3ead541ec7cdf6637e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:43:06 +0000

    team dream public accordingto although

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_float(array):
+        return float(array[0])
