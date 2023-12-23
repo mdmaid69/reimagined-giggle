6fa7b1c6167403ad1127e1e813350e6da53995db
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:49 +0000

    seriously government pretend effort offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
