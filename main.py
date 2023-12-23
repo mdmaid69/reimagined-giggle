e9af9b9e13e88e8c80894d8a848d513b21013413
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:10 +0000

    day mass break call journey

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+print([x**2 for x in range(10)])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
