7407c89d5c1fcd7ebda7064aed466ec6265a50c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:00 +0000

    gentle convenient bound leg finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
