91c09d2ecc55e5e5f6a1b6e183595410ce7b2117
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:51:20 +0000

    pass Bb pupil accommodation year

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
