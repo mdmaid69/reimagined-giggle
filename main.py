564e124cbde94a858bccd8998d7d7236eab62f92
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:37 +0000

    block bury spoil operation hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
