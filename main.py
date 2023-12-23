0964f32019d09db4d1601d96cf3d5964aeda89bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:49 +0000

    convinced conduct with giveout tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
