3e1845e08c0306484341ee6458a2a4228f6acda3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:16 +0000

    offer operation copy whichever back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
