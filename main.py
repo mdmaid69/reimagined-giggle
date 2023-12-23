0c393a123548c7b9730244f3fe729cf08642133f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:14 +0000

    awake unkind chance honest stiff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(numbers):
-        return min(numbers)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import datetime
+def get_today_date():
+        return datetime.date.today()
