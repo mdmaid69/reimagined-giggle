312f2d5920caeee233541d8ed078b77e77285d69
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:23 +0000

    fire breakthrough treatment sort tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
