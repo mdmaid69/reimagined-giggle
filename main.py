b1657bead406e18dfa568b89272de23218b52da2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:00:59 +0000

    book brush famous bother make

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
