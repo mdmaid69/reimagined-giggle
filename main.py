a568647d922caf49e3d21334c55c20c03818ef95
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:13 +0000

    tight flow cause course Bb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
