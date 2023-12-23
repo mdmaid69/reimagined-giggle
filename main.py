7d15c791a34e2b39a274a9caef49656e6cb25ea9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:02 +0000

    equal importance determined will occasional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
+def remove_duplicates(lst):
+        return list(set(lst))
