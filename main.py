adf896ee67e18aa626cea7d02530e7eebb014850
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:00:25 +0000

    wish hide touch introduction flavour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def convert_to_hex(n):
+        return hex(n)
