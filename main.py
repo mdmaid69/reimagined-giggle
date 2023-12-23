c8b250197e8e307c72d513e5b2af242885afb2d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:53 +0000

    how hard until mention bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
