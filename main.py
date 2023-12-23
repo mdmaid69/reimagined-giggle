4a41d8b95e7dcad7001ff40a12150a168d26e5bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:00 +0000

    let exaggerate pull food regular

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_counter():
+        return collections.Counter()
