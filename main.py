76c0aefc10058885829d05aaf6efb96f21d6abff
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:11 +0000

    turnup grow breathe steer less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def get_list_from_array(array):
+        return array.tolist()
