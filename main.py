cf6b55e8d68363fea7d2ef959ab87bb9ee7793d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:07:53 +0000

    closed near several tough live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sine(x):
+        return math.sin(x)
