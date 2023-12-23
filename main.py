b512d517badb7c2b1dd42269b4b78c11168b245c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:42 +0000

    must form difficulty now telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
