da69eb1d9d239e167031ada221db0e4e0c18e465
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:29 +0000

    steam that name model total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
