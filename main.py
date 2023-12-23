a3a2e4093fde00a2dae4854a00749d606d2e0b98
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:14 +0000

    related over flow bear test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
