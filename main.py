5bfe5b43528d581ee506ddda83ef46ff70f5aa10
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:11 +0000

    stranger time sudden along giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def count_elements(lst):
+        return len(lst)
