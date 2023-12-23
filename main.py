1c844582cb62dcf57d89135cbe0c92f9ddad13f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:07 +0000

    similar afternoon legal way comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
