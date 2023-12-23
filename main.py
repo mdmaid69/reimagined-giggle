b27de3983a9bfd466701cafa8a9253d346963eb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:30 +0000

    uniform hurry board treat nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
