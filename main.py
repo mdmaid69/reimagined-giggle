952ee7c5e038fa34a7b1ec130b9dc93f0553d3bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:12 +0000

    least great high embarrassed glad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def convert_to_octal(n):
+        return oct(n)
