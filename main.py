75bc4324d008e2030e4c9a60c51cae37ab413c17
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:04 +0000

    apparently capital neck laugh regard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
