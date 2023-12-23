864f04a2dce0ae3f59d676f608a1545ca6ae8aa9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:09 +0000

    successful goback(to) sentence mistake Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
