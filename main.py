e5e5fe8f5bf848ad977004b07a63f74badba9dcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:36 +0000

    Itself practical us still serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sys
+def exit_program():
+        sys.exit()
