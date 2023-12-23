196ecc89c4f6de129515316784bb1b1b1d72a355
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:40 +0000

    only back record holdup trial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
