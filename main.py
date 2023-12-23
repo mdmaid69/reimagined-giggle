2082fbce4066c1bc40852b5d3796c5e20583902d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:54 +0000

    hang exhibition check border their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
