6115fdb83722dd7354bf74212f9636984186840c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:09 +0000

    process debt freeze cook unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
