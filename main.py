5182aad94ec96f4485cad728696bc1cb1b9ea7bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:44 +0000

    tidy tape scream hear music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_item(array, i):
+        return array[i]
