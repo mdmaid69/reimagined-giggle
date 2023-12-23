b11e87bac676dc9847584799c2c849d1128fdc04
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:14 +0000

    nowhere serious be thumb bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
