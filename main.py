0be2b45365008a31af509490cfd351e06b3db77f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:53 +0000

    accordingto above union interview recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
