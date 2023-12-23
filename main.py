bb8c377e79a7df121428459c9ce53513da048007
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:11 +0000

    drop truth bath wave careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
