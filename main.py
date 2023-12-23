5dd2c580a0103959bba474c0b8ff5e76914c0b1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:15:47 +0000

    character play book congratulations punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def exit_program():
+        sys.exit()
