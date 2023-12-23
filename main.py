64726eadcf6a281c151eb334239541959ee828de
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:55 +0000

    shop swim arrange be interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
