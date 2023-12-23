29eb532d2dd6d80dc6099cb3ce7fcfb10bb0119f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:59:36 +0000

    block film success practical field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
