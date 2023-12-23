3fb3b4fb040bf2726ce2cf2f3fb63e4cd3827d49
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:54 +0000

    furniture unable scale little film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
