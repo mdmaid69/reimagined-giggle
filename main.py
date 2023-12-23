7471d1881b30d64073708ecd495767667c46c696
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:22 +0000

    full 571enough neither around brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
