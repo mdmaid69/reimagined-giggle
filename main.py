558711bc6505fda38bce6acc141d5e8ef0854828
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:27 +0000

    bringdown both grow give confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
