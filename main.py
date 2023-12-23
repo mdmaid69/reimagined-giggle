f95265de33ca751fbba577712ce3fdd452e1e726
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:18 +0000

    stick any entry congratulate education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
   def calculate_standard_deviation(arr):
         return np.std(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def cube_number(x):
+        return x**3
