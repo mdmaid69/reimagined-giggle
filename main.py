098c404c33cd6a9c81357a6f34722d5f9e7b586e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:36:03 +0000

    during mysterious concerned people reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
