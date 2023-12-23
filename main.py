9c85f1b0fe79f99a6e4c7699222a02e15a4f1cce
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:10 +0000

    572enter rid surface sell motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
