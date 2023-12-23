85a2019a2769bafc6f0037692c39bccd656dee31
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:45:29 +0000

    damage yours account leave once

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
