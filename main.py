5856f0cf5722d0a036dfd4886ab399979b1b99eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:32:41 +0000

    stay moral whoever human appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_area_rectangle(l, w):
+        return l * w
