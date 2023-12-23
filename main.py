6c889219f6485ef8c454f2d54775c013cb7a3b82
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:10:39 +0000

    ago rank violent extra hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
