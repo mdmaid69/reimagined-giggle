56256feefc4e04177704be4d995d68921b154618
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:49:31 +0000

    ill cover probably trust notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
