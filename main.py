f237a8cdd8b1bffd657c750562bb9f678f3eb9cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:04 +0000

    rubbish female think together refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
