88bb316a1f95c11744222884abe7309b32949728
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:43 +0000

    570enjoyable fortunate recognize scale trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
