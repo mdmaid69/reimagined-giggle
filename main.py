ca8132939ff90d873c169f0ee90b9376f9954a54
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:09 +0000

    frame yours petrol slip eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
