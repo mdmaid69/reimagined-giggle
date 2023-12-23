2c1a7f27a917042bfd6d8ccbee89a1c47ff21a94
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:54 +0000

    reach score related separate part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
