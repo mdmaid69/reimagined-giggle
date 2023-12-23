ed0d798f6fe9de8433e7b9ce36fc1f67fb4abdd9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:00 +0000

    dull spread flight more print

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
