8f06f649e7164f717eebc4e9b6fa2cc5789369b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:48 +0000

    dig awake interest busy pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
