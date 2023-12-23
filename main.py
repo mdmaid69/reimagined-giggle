ba3ec8296da0b128cc3501039b9d461312273996
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:11 +0000

    total government mean 565end pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
