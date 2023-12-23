18a282004c6091b2072f5bf77d0f81fce5c2ff4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:09 +0000

    action side uniform community damage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_priority_queue():
+        return collections.deque()
