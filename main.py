f4354bf12a21e6d974472c48b6d413a2c5591afa
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:14 +0000

    mention stupid breakup indeed cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
