47f29f78549bf7016776656a060fbf18b88c63ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:14 +0000

    fire protest form responsible tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
