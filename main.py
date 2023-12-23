cec9f6217799d980bcb9767664e5144e904d20e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:31 +0000

    concentrate thinkthrough than grow apparently

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
