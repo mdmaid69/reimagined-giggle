d7bac0506273bd76566236e30a1d6f881946b471
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:06 +0000

    yours too much confidence cover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_max(numbers):
+        return max(numbers)
