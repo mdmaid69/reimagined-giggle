5656e31f8400e9fd44a1639c2d7813ca47cc8e9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:42 +0000

    lookaround lead unfair turn life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
