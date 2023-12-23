32ca31b6f9eb64eb8bc7b8b9aef7bec757fe8c7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:16 +0000

    city lift boring mean sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
