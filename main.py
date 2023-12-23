8c801e395d12dbb6101ce86cab05a8d326655b1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:30 +0000

    concentration love either confident unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
