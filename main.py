3303ab3cc1181070b0d8229402eccbb228838b82
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:38:25 +0000

    hole money idea accident damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
