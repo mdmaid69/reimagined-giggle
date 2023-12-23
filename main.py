4c558696628b30929de9c26298166cc01389b43a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:44:51 +0000

    come account make that ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
