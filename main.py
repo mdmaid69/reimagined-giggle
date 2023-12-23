7e28f034a19f505a491c7a3143b7bb41d1b13bc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:52 +0000

    now beginning do Cc neat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
