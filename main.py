6b05fb11efe6c56548443d68cd805e25e9fd4643
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:17:56 +0000

    surprising skin kill both whether

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
