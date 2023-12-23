b87b1ed1029e60bccd624828a534a0f29cc14dc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:33 +0000

    path target convenient half shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
