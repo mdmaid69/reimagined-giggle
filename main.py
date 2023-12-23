9e064cd5e39d6d911d333f60af897d3441297364
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:21 +0000

    single nigga support grow scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
