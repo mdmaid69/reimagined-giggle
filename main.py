5d9003f6b301046cd3557a7c028b251c562b2744
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:14:29 +0000

    whether make touch unkind quick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+print("Hello, world!")
+import math
+def calculate_error_function(x):
+        return math.erf(x)
