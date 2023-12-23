8e9d67b545f4bd0742630b48d707360585191953
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:33 +0000

    quantity anyone world illness feed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
