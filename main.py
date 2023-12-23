90859599b5ce45a4a41e22ae4695d6a0ae1c205a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:01 +0000

    congratulate steal slowly on passage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def count_elements(lst):
+        return len(lst)
