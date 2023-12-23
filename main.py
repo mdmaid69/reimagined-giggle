447053026efe33df05ef590204bc57b8c6b35ac6
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:29:42 +0000

    demonstrate indeed advantage bit marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
