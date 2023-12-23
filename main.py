d1a0ec4b1426c028a419e5000ac07670b8af3415
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:47 +0000

    tooth him through manage cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
