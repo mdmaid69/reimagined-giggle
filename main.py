0a23675ece495619d859a44a3adb725fb2da9410
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:04:42 +0000

    use nature lower dream wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
