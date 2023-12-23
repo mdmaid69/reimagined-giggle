128a8326b3cc5eae2b88a83032fff4a3e6019a7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:38:39 +0000

    nature impression number finger above

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
