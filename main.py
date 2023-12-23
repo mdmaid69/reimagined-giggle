d6d758b4925b9ba84fe34f3cc9160532729739f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:56 +0000

    demonstrate tall score ring content

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_length(array):
+        return len(array)
