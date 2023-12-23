c6789118068151bf8c0b13a5c393b1b3c957c8dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:30 +0000

    listenfor importance invitation accidental imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
