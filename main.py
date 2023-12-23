4076f904a2687b3b50f0696351a72d7bc22b1c57
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:58 +0000

    who feel young picture news

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
