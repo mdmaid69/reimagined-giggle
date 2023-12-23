d41142ff0a519b85ec2f342d7c30c4474cfe709e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:40 +0000

    frequent Qq and represent secret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
