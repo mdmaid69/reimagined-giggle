b00e4a0244b3e526a59795ded8205134a4485146
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:45:53 +0000

    rough gas lot will mass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
