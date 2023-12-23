00ef04201d5a93f958497de5e6e27731b2c056cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:19 +0000

    they travel live block proof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def greet(name):
+        print(f"Hello, {name}!")
