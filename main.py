1874cdc44ae32aef5638a2cb14bacb0468b5be8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:57:08 +0000

    smile key personal emotion it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+import math
+print(math.pi)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
