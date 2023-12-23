333659ea57104cb7a2a70240e0bb467de90f7e26
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:01 +0000

    record measure once street extra

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def reverse_string(s):
+        return s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
