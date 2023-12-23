f39f0f4cc1a8b68bc858900cd163a7d967f53e6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:45 +0000

    spring I himself slight tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
