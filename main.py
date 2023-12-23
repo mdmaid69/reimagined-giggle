cb7c557415ff9142f73f039864a17caa555f36d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:02 +0000

    past no clean lookaround wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return math.factorial(n)
