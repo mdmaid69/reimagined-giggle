c7f218159030863086c3900dd86cbf3178a1f074
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:41 +0000

    guess holdup damp noone visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
