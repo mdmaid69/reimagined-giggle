773b092e112bc483e1f0bd55fb88a6cac2ac3a11
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:17 +0000

    also favourite mind false centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-print(math.pi)
+  def split_path(path):
+        return os.path.split(path)
