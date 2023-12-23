29361c17e0ad46a86c703798a2d8b24d6ebdbdd9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:20 +0000

    uncertain he improve like argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
