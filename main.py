d5243326b813ba45393555841f1e51110fa361fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:52 +0000

    burst near blue steam case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
