a73e0af71a6cc7725d861f043f454369ec67a4c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:43 +0000

    nor father home gift tip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
