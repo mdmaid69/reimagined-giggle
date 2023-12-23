d8b077c4a0ad9c9a8715e7a751b243e97b0dff54
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:57:17 +0000

    weather boat people affect future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
