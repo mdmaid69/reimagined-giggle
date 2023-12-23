790cfa00ed6fb96c48dedb39cdd349551f55723c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:49 +0000

    shoot room unfair pleased reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
