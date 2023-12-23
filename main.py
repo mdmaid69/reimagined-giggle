27aa31463573df6473de76885576796c47d9f46a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:42 +0000

    lift mysterious fashion stiff recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
