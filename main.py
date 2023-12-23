8a182611de87d3b73d9b5dc21144df0214956788
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:22:49 +0000

    transparent takeover shake fortunately tidy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
