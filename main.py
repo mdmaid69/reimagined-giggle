6ebf946404f4d5c280bf47ae9ad7353d47374799
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:53 +0000

    reduction belong smoke build indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
