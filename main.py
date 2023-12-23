5d9dca76d064f7279166bcfd3069c0e8cefb45ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:23:41 +0000

    stir patient talk arm rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
