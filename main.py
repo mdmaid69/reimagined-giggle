17c20e668d533b94705bc792d7f1702c38ba6bd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:04 +0000

    thinkover clearly care taste smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
