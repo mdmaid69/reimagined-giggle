85ed6b0cdfb199d5449ad0e532dec592706bc58a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:38 +0000

    tax break relief clean cost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
