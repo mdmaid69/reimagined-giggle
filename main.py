3380b80c376e2b3e5a8e6c04beb0b1cc5b274964
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:07:33 +0000

    argue off shade average view

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import datetime
-def get_current_date():
-        return datetime.date.today()
+x = 10
+y = 20
+print("Sum:", x + y)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
