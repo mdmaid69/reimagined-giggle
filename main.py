73d0d52bec38e9e91c9a0f4bb785bfcadb82a6eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:39:36 +0000

    order geton cough place cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
