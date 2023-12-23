f64fed29433ce546c32d424111bfa45b5665c7d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:42 +0000

    spray realize upstairs operate stand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-for i in range(5):
-        print(i)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
