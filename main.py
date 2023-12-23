2d1a61f6cb022f20e3d9e9c9392d694c116610e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:00:54 +0000

    scared grow justice with spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.time()
