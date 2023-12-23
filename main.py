d7ee7d8ed9630b4725fd20f3315b3d541926d8f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:18:42 +0000

    more distance comeup demonstrate careful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
