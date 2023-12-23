8cd5d1983f4e93bb06af7f32633b6bf42ede7a1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:29:45 +0000

    service destroy fast manner rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
