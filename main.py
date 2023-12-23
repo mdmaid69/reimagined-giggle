b6451157428baa604c03a056ce73c9f9c45edfc7
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:04:47 +0000

    shrink mail trouble spring draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
