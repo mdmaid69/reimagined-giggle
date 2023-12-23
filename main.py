0669dc28eef31236886ee7f53da56efba6048e94
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:34 +0000

    company hope stick crisis sister

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def get_array_as_bytearray(array):
+        return bytearray(array)
