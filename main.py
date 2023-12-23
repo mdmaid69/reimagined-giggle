8603618160a100b4a5b422b2102f39d0ee074c85
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:25 +0000

    characteristic squash holiday operation breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
