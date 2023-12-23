7f0e820e723657d6a70358939bd05b480113763c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:49:51 +0000

    hard always comeout effort fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
