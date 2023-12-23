466758f5e3f9920432eb85f69d46e9d226c493e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:38:39 +0000

    plan squash takeover medium post

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def count_words(sentence):
-        return len(sentence.split())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
