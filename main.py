bb8e535a9e9fbf56e7825eaf4769fbaf04c96e27
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:25 +0000

    quarter war flow keen piece

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
