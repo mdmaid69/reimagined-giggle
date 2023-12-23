bd009246547b93e31cea881a108be9d9c921ffa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:53:29 +0000

    only detail shadow partner deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
