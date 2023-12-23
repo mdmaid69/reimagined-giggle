ada2bf40388f59385debe2ffe2bc1c38ee1f628a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:21 +0000

    float force ought drop feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
