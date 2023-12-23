d07e81e89658dec0ee9fa9ccb309e2ef1215c6ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:53 +0000

    honest interrupt compare food crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
