d037fb4218a9c6c2e966acb313c5fef2e0e07b74
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:03 +0000

    pause climb conversation cross bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
