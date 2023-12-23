b5ece425a91a41bd6bfb59fd2fe054484dc84000
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:04 +0000

    birth tip may talkback backwards

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_area(radius):
+        return 3.14 * radius * radius
