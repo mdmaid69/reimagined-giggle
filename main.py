2a1a95b0d14ef870a9dd6dbbbcec8f6fd7c913ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:40 +0000

    description violence also variety favourite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
+  def multiply_numbers(x, y):
+        return x * y
