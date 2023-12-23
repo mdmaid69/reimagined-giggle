5236ee20c9d64927769a1bc1256eeda6556f0e91
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:15 +0000

    list survive shallow roll storm

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
