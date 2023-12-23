0dd4f90872bca1f0a75b6caa79e1a0444acc44d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:25 +0000

    dead under combination score knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
