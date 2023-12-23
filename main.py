238fa01533ee497963f41e2bad7a6c7421a32027
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:54:36 +0000

    come control bag way case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
