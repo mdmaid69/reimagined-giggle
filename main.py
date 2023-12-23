7ca065961fd39aa039107df4eb225a0e06a4a8ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:34 +0000

    week fasten press runout put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import collections
+def create_counter():
+        return collections.Counter()
 import collections
 def create_priority_queue():
         return collections.deque()
