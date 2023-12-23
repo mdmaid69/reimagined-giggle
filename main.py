e0975810c7c28c5c6bfeb20b99fce713efc3a3a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:09:15 +0000

    learn imagine fond offer rid

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+print(math.pi)
+def count_words(sentence):
+        return len(sentence.split())
