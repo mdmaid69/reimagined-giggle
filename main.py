210ca4e3b607bbb6eee79fce182dd078c32260bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:08:19 +0000

    bread legal area relationship cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
