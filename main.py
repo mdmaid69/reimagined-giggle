2b1369aa33b827f6c1f0f6006fc4da2f92ceee38
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:47 +0000

    breath stage any possession nor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
