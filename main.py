59b7fa9985dfe3e69d243c6da251be52e490e665
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:30:50 +0000

    callback power time deliberate urgent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
