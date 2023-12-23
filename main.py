e89f96d19a2925aad7efbd99999b6a8170429bb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:45 +0000

    on forward still early hole

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
