c95e3600a36aff94556026d0d29f07d2fef93597
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:58 +0000

    shrink difficulty pause water regional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
