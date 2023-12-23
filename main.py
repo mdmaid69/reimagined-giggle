2685c16d87ac9ee493178c7c43f25bef475e72cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:41 +0000

    trick bread away class fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import time
+def get_current_time():
+        return time.ctime()
