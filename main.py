915be0e26cecdb7260b93b947fa41347741099d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:46 +0000

    hang home swimming square evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
