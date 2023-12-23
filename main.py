74b7a02ade30eeb6874eb2bc54ce049d7b14a08d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:13 +0000

    major stare rare disturbing makeinto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
