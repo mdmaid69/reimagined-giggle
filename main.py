96fe06ca50e0dbf2dfabe351c1f6b4e44c730ac9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:28 +0000

    offer active even mix radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
