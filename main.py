a62d5708f6841f3d3b405ac2fa4fb89e4db5f022
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:54 +0000

    dish makeinto dance attempt unlikely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
