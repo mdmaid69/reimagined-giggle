67fe9021c7dd1dbea3b1e30d9a55f9c1dcabbae3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:48 +0000

    peace attention affect switch acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
