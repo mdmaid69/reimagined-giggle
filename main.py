86470a78ca41101283b679c27230941a75f6259b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:52:03 +0000

    war after situation free speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
