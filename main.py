1298cc5ca97b54c27b604bec43746841ebfeb534
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:46 +0000

    crash Pp information move wonder

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
