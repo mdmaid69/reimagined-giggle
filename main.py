37b3c580b2a6f19deb1c8d97bfafd96fbf2d1bea
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:31:39 +0000

    her maybe meet keepfrom goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
