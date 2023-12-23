11cbd5b45c7ce04f953a54cddd69d88c9b3c4074
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:27:49 +0000

    lie total at contain simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
