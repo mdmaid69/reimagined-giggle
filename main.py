dc111353b429e8ee4364e1bea2503869551fbd4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:35 +0000

    hardly hurry much play takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
