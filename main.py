09971f2a3270c9a6d9d51764bae5eae189d12225
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:56:41 +0000

    valuable trade region section thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
