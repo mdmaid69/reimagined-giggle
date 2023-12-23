e846c7e154fd0dd663ccd4a6bd148fb62ffe5fa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:34 +0000

    wait read appearance turnoff middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
