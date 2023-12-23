354bed9b58bbb8b46cd99e1b1e7114ce8b59ea8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:35:43 +0000

    concentrate dig skill lookaround ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
