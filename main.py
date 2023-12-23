3191af592af4935cd2eb4e4e97a63b719adc184b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:07 +0000

    basis home care introduction offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
