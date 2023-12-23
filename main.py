4a2b0168d749e63435e2717876e542f75495ef7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:20 +0000

    yet wing detail pay where

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print([x**2 for x in range(10)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
