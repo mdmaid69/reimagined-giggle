dfc2ed04f9c61af1ba2ce3a3e2e475a4350940fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:25 +0000

    bother love nowhere field off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
