ae9b4b2430716eb36a463ae218b31209925c9cd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:58 +0000

    before wear communicate join exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def add_numbers(x, y):
-        return x + y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
