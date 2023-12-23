3d3b2251b6fceff5c649a801c5b1e8a4fd890e30
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:24 +0000

    meet concerning cry talk mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
