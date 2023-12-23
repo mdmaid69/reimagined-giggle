31fd5a8ef2f15bf1108e894ff2acca67ecbb9e23
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:24 +0000

    consist mean cough serve study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
