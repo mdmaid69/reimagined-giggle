7828e2fe31f54ca986963093d762794881028eaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:12:29 +0000

    centimetre comeback party statement comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
