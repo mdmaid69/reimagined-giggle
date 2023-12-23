8e388748c5ca79eb3b8f539bd364732bc16fe615
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:05 +0000

    wrap interview sad temper insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
