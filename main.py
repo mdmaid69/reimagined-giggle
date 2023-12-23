c45eb3da700732f9eb3303b5e5a29f2caf778abe
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:23:08 +0000

    neck count calmly replace communicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
