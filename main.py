134e8837c53b782acaefd562c3998e9d9e649c42
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:00:09 +0000

    me onto view serve Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
