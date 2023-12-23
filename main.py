c2a8d989d936387848f53fc3338041a4b2494de1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:17 +0000

    reject entrance ticket increase giveaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_volume(length, width, height):
+        return length * width * height
