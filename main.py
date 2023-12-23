2b37c0127fba1b7ee37870bb78b769226a9438d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:03 +0000

    wide them present desperate invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
