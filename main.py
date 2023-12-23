73358ad3802be2cfeba34fe5b0cbf5d788a010bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:12 +0000

    better avoid anybody tense mouth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def subtract_numbers(x, y):
+        return x - y
