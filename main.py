3577238b5fecfa5d5d9e7e300ba02d5c5ca59ffa
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:21:13 +0000

    forgive tend contrast threaten command

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
