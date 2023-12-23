d59d2d5e8d6cfebdd5314d99f5ed0d32a84cbaa5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:17 +0000

    ask market represent who reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
