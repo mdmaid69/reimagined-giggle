e100a52ed4169a903df1a0ed1516d2505fd5aabe
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:01 +0000

    maximum bringup unpleasant recovery swing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def split_path(path):
+        return os.path.split(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
