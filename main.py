fda045af145c3cd9d1dbd9ede062f470248d2dde
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:49 +0000

    pleased getup waste year build

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
