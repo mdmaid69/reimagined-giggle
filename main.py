515b8b89cc2b66b8e476ff6877bbe90d1c5d1661
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:51 +0000

    wherever will benefit healthy declaration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def cube_number(x):
+        return x**3
