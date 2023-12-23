ef4be1eb8a8b329c6fe3c9cccbb754ff673500df
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:04 +0000

    connect congratulations embarrassed size force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def count_words(sentence):
+        return len(sentence.split())
+def reverse_string(s):
+        return s[::-1]
