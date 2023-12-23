ccd6a3e79a6f7833f702ce2b81151df346b54bf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:07 +0000

    lift road damage comeoff later

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
