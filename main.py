854777a220365692f92f3ccd95a169268c8b049b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:39 +0000

    spring argue Dd sail movement

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
