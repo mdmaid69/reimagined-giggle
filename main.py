94aa4f0705042a51311f83c0d0aa560145507498
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:53 +0000

    service wherever move length keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
