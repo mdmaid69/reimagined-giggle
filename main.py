aeaba9e3e6f20bdd96d8fa2ee028e22bc861f7e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:21 +0000

    impressed trip equal OKorokay callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
