843220d1039273b88ef526419d389d1a9007900b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:55 +0000

    free ground thanks bag university

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
