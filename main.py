3703b529f7a79e7a58708393ae1a6e5dd0e6e372
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:50 +0000

    right wing ground lose less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
