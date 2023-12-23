3cfff2d699e0529a349ac621d5236f325fb80857
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:20 +0000

    wild even one refuse waste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
