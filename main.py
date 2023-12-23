4424fa6acf28cdc9e2b2d9462aa92bb60b6b4986
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:10 +0000

    building taste ask lean method

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
