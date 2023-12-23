619ed35690f2fb02ecc71c0973e18f79d1520863
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:48 +0000

    complete remember miss ourselves show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def is_odd(n):
+        return n % 2 != 0
