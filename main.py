6ffbd25ca5768230fb7186a12063a2136fe156e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:52:44 +0000

    backwards wire 566end early thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
