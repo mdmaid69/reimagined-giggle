19523f987c7cab3aadc642f00cae53927d945d04
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:01:25 +0000

    date bet ask ahead result

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_volume(length, width, height):
+        return length * width * height
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
