16dd6b66a1d306c3302af91cf4867872d06ee46c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:37:12 +0000

    reason blue tonight game least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
