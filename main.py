128c06ce0ed6d044b11659e73d743ab3d732d245
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:21:09 +0000

    call partner visit answer treatment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
