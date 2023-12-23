394e2f7e01f43a33b12ba76f4c0fd886c13c136c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:04:05 +0000

    become thankyou maybe exist roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
