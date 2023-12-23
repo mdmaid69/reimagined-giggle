1dbee6973e7fb2599ac895445bbf3720f750e61a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:04 +0000

    defend complain boat custom include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
