f571460b19757772f6ba084f551d7ba5d3f6a2a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:08 +0000

    so type prize and noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import platform
+def get_os_info():
+        return platform.uname()
