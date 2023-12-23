1ebcd00f9bcdc7d5529ebc36d598bf8c904726c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:34 +0000

    visit crime band keepup nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
