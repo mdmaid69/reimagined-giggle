2cb2b898be035ead237bfe96fcdf2704eaebb6fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:45 +0000

    explanation unite murder state thing

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
