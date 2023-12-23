7192631879ad32fc106c0ca1cdb954fa57655e73
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:08 +0000

    prize meet might border free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
