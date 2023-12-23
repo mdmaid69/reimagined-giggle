0e04d2d11f682d7dc42a11816f6653501c426438
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:22 +0000

    little serious outside line side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  def remove_duplicates(lst):
-        return list(set(lst))
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
