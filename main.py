5f614a2af1cd29ff5f945640eeb24c3d325f59cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:01 +0000

    praise decide return shock mixture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_work(force, distance):
-        return force * distance
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
