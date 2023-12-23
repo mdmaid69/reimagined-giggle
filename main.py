bb58122f454d1739310efe34177a01d80aa08963
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:40 +0000

    rich schedule centre mystery noise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
