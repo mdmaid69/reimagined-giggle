f9b121c23e70ffa53fffe72218c17117262cdcd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:56 +0000

    keep natural disturbing pause appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
