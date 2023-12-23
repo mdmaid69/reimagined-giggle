58b8d8c1bef893727db31fd54ad6bfe54e4ff4ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:12 +0000

    for Ss between hello previous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
