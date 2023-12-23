a775cffe71188db4fb71265cb58a0a1fc6974a75
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:45 +0000

    invite crime flow principle week

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def count_words(sentence):
+        return len(sentence.split())
