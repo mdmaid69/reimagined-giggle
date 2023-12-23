886909e1b22571a26502276f8400ff58d008486f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:22 +0000

    married important consider sweep central

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_area(radius):
+        return 3.14 * radius * radius
