63929e5cdcd73ab5af5f66af5eaebaca19f5981e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:48 +0000

    sleep across appeal news help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
