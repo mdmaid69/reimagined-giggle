655c5a2e225512ca88ca614892796d257441b666
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:52 +0000

    tape visit myself report score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
