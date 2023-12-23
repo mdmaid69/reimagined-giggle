0dde1a400faaa39b855bbd7ea3d259eb645155bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:39:19 +0000

    range length walk indeed dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
