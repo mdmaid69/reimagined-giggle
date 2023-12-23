d6aad568c580882b4d9cf0f78f17903fd70a9ac3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:32:34 +0000

    dangerous begin similar quit cold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import logging
+def log_message(message):
+        logging.info(message)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
