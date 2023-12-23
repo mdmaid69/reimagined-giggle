9a695eb00b69ee39a96e5fcff0a0d3f0ec7cf139
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:25 +0000

    yourself powerful image steal pen

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def square_number(x):
+        return x**2
