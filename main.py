c8fb17e6534730aed38a6d0cb0805df8df447a1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:29:53 +0000

    finally control care matter face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
