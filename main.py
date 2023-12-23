d6f813c36d452ee045a52342dfd4e029b66df19d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:16:20 +0000

    top keepup immediate deserve cum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def is_even(n):
-        return n % 2 == 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
