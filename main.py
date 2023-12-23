644997d759099337f72d5a1405a507a1fb83356a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:45:36 +0000

    ourselves where error container task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+print(os.getcwd())
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
