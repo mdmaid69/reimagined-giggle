81cc0a5b3413b5ac91f94fa1058bda469902ed7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:51 +0000

    plan on would issue morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
