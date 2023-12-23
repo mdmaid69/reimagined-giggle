ab455a00cc6f1640d81a6a5f775b6efda5c8cdcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:14:33 +0000

    part criticism takeback punish object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
