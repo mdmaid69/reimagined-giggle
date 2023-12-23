ea9b3ef5480d5ff46dd3710ddd13b5077bc66ebb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:09:00 +0000

    choose likely shade past understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
