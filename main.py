86504d7818ba0c00fe3b43079635aa451c112fdb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:20 +0000

    cut section out bottom squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
