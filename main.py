4f7b5ce769cbe9cdb82c1710ce16659b42257e3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:17 +0000

    bringdown introduce terrible learn careless

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
