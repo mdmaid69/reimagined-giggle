6b527fd4702ce699e6968299928ea80382d62cf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:08:53 +0000

    information complain fork improve soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import numpy as np
+print(np.array([1, 2, 3]))
