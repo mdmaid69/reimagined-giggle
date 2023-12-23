53827c097fecbf541185d6e121de90e807576c03
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:22 +0000

    determined should rate complaint freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
