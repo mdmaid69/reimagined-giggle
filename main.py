c49be41fc344b1d53b510c580d86668f9d36d003
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:16:59 +0000

    storm thin though comein store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
