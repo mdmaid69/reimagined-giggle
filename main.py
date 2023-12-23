9299836f3c1a51e7087c096873c1274c702cd2bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:19 +0000

    wet treat plan protest sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
