d81a1c2a573f04c16c91d607f6bad1c4432af67b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:11:30 +0000

    anywayanyhow date disgusting suit temper

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
