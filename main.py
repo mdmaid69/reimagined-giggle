26424a6a2040c3026271bce86268491c79bb42ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:02 +0000

    suddenly instead deep bound capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
