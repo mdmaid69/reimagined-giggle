b4029ca69a9170f1e03a797dfa8778e1e6fc6544
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:52:47 +0000

    apparently trick trust ignore influence

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
