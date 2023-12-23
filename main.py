3cd22e4f6bc157bfc2f464ec5c67a97727699e40
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:21:46 +0000

    ring art find similar vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
