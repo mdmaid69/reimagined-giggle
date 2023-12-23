f671f71c5e296b4289dc6bf5e31f51586ec2afbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:34:49 +0000

    win art onto favourite joke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
