e9c220b893fe0e59cea386d68ecec65472dd8faa
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:18 +0000

    apply string table dress steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_list(array):
+        return list(array)
