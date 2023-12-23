7e068cadb485f47ba49bd62ce065ae51e85d61fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:27 +0000

    divide support impression whenever describe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
