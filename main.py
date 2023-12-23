e8874e4f4c7be58c5572bb3eb6884a9935aeb990
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:19 +0000

    dear careful fit claim draw

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_priority_queue():
+        return collections.deque()
