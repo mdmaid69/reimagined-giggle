60b5df277255c54bace609b8523ebf3a35a684e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:46:08 +0000

    have turnon wet still late

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
