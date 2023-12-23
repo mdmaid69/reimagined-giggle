9bd3759d654868bb8f8b16044355c56821d1bdf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:12 +0000

    spread great variety piece badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
