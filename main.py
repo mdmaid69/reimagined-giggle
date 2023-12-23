7c7fa1ff0d4f3c624b117619e00a9aeadc59aed6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:27:01 +0000

    just body pay when check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
