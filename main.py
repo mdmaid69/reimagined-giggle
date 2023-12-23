9d097f0d149521bc70a7a0b89531390e86b7ee24
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:59 +0000

    period impressed loss sure by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def square_number(x):
+        return x**2
