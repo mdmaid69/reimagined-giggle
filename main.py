1ab07f71b6ebd1ec930fd00e2d767543d15d5e37
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:10 +0000

    sink sentence print month preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
