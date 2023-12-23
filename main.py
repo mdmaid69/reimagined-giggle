ba2b67fe291f3d0f7852bd350787cc4143d4ff75
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:55 +0000

    thirsty identify closed face fork

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+print(sys.version)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
