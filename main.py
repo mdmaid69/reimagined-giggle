4df79107408b543cc1f2c89414490640353bc5ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:24:53 +0000

    bottom score trust morning congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_power(work, time):
+        return work / time
