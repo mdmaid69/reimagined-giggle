317ab0a7b2645d5c99976355b6a1e3da59fe526e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:23 +0000

    accordingto contain off small unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
