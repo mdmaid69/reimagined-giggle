055ff094f55cf08b6e13e45048c3cb92411f384f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:33 +0000

    burn talkinto open over decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+print("Hello, world!")
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
