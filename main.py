c7e90f395db8b8c99b71487e0beff1d2979804e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:58:50 +0000

    definite persuade scream find giveout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
