fc0eb30374e355f9279ae270a43c109496192722
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:52:46 +0000

    special patience no whom getout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_max(numbers):
+        return max(numbers)
