7f4ef05ad71a527e0cd1111decf9cdba3db0a405
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:23 +0000

    label comeback too persuade perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_memoryview(array):
+        return memoryview(array)
