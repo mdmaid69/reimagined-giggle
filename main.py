50f4e37fc38642a8f6f403ddefd812318a2d7e67
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:09:19 +0000

    open collect position control repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
