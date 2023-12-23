676e75382ab1da0f0b7b4518df4501804ab7d165
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:31:24 +0000

    let body vision really arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
