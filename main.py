5688803faaa227d901f80956f725a32e986368cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:09 +0000

    near answer uniform properly know

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
