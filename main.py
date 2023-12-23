1bd98cd9f7f342a32e67901e8e8eb3d9d135305d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:21:21 +0000

    hit light independent country telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
