92e11e064a1e90b32b42d10b216da8ff71cd8e8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:17 +0000

    politics free disturbing up smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
