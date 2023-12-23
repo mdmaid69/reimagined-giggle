5040753ebcb55e8eadbd7f1ba63b7cdaf5546a1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:49 +0000

    father character report cheque factory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+print([x**2 for x in range(10)])
