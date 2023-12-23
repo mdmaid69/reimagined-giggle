49075f290475247657d5e5e6a734816e89f91b4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:25:44 +0000

    trip turnout slightly fail project

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
