c4990cfa32ee894c6bcf19d662885acfcd602397
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:55:12 +0000

    wipe capital push attitude day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
