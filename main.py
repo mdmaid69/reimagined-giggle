38b99b1d1dd5d2eee65b89bced67c0f49f614207
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:36 +0000

    perhaps butter name lead conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
