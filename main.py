f706fa306de8a4393b6d0046209acd18a7bb0ca1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:49 +0000

    accident turndown whenever seem stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
