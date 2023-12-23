1423744d208b3ad19c7f0f84adb82365704aafc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:48:51 +0000

    children too common pinch probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
