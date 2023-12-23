be2d271690f45fde6f739240f290d53985153d65
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:11 +0000

    important keepout disappointed increase workout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
