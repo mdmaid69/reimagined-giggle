05fc4a3032365df6a4a2d11219b4a8e521cfe8e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:53:50 +0000

    bet cruel same key noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
