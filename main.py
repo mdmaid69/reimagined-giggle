b2a423ccd8101084b0bef5bdedb4d07a5e735c57
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:02 +0000

    ilovedicks create tool declaration regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
