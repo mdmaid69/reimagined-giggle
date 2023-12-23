d79dd31ef6e814538bb430cfc9da682291b9b55f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:05 +0000

    zero introduction hole expensive most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_stack():
-        return collections.deque()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
