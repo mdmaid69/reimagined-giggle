8e43f1b613ffb0820c727367180eab995a8986e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:15 +0000

    onto pity down public pollution

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
