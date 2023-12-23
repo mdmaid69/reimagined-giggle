74b9daeec8bb2e2d5c5fd5913a9559f694a00729
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:20 +0000

    offer will such home invite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
