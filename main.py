91a2ab5d48cca4c8e7fda32d743d7ff60435a9ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:32:05 +0000

    murder doubt unfair trip OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import time
+def get_time_since_epoch():
+        return time.time()
