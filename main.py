6f5526353033fb011fbd23e7b782cc604db45fa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:03 +0000

    polite holdup modern culture step

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
