56d3fcc47dd148e452e14296bbcbfc77a2fe6857
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:19 +0000

    keep book shape proper progress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
