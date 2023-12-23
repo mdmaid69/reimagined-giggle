7b65bad3431e45c663e49584ac2294e38757f129
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:50 +0000

    bet trust print port common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
