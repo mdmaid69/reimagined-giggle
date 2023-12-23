2f1f118e90b4601acf6de76d2f7e6b6360050a3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:35 +0000

    clue trade suit Gg whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-x = 10
-y = 20
-print("Sum:", x + y)
+name = "Python"
+print("Hello,", name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
