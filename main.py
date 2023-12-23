711d90b97a2e717956fd5cc7eee359a526130606
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:07:57 +0000

    at suckme above view message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
