73911b0b3ff1983c260c1b09019d457bf8a59c0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:22:53 +0000

    bag powerful season inform division

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
