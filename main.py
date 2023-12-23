cc1bc656a889f276b72019c64303621b54d04e7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:34 +0000

    picture later word combine Oo

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
