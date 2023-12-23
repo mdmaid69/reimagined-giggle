48cd033f31e1b0e5ea2d92db9652e188dc348c85
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:53 +0000

    pronunciation occasion hungry build pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
