8ddd49b109f5f6bfca6eb9be146b05b479e2a190
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:44 +0000

    class mess friendly certainly why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
