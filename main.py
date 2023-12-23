e097bb2b96e9ce1e577094b2af0b709ee4caa3ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:19 +0000

    mention once trap rush delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
