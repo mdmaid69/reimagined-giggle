fcb758fbce70256ae3a7b691730d8dee660fad85
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:27 +0000

    take catch stare lower embarrassing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
