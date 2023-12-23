ff1d4171f4c64c7415f08b3fb9113f00291e4edc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:00 +0000

    report date considering scene sensible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
