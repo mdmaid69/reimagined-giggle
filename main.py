c61fadf1b5690d63106658d1bc8355a3ed35893e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:31 +0000

    funny charge tool health provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
