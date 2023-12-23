215627f2cd38231bb8d641b43a9a93dec3d46818
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:56 +0000

    start surface balance from getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
