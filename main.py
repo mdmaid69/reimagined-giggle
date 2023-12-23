83194ac2f1100a5d6e85549197fb21cee9655956
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:11 +0000

    practical thirsty emphasis development evening

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
