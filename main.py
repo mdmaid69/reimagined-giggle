cbad1e6993aec038b172fea463bddf7871fbca85
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:55 +0000

    available Vv at side claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
