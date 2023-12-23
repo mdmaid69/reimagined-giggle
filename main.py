882ec89a8cadd8f24dcfbcb14e81e83e70806f7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:34 +0000

    put Pp no steal honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
