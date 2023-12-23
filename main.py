72acb8b814d5a48c5a1ecf73e98e4079fc3d6ae2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:05 +0000

    speed swallow date building open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
