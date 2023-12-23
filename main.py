2e6df979a2b83942fcfc94b37deb589d792e9d58
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:36 +0000

    world control same send swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
