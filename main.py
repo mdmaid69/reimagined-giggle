990fb1e66ce393a016124b04efa2edde85f10d6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:08 +0000

    test wipe bridge continue benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
