c6ef8c715dc14600edeaabf184aab935d9ae31bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:44 +0000

    state summer all trick you

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
