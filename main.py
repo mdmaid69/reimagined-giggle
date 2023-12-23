75fbe404b37042d43c99b547625b7f4a1f5f092e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:14 +0000

    object blow entrance important part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
