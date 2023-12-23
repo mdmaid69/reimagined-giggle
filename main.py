6af53ffd7fc0af784bcd31979eec467b1ff2cc26
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:42:33 +0000

    add clearly pleasant whistle Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
