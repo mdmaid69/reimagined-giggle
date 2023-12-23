0f6aed4e698c3cddad8ce2b4ba7fe21448288205
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:36:05 +0000

    similar official contribute speak little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
