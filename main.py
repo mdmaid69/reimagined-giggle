c0d0f5afb1d5b7adf71ed3406c83cd1336ce0c94
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:23 +0000

    practise comein OKorokay interesting represent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
