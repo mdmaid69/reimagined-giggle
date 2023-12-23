859e00a1a5236f28373c5444a22503558daf7128
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:53:34 +0000

    knowledge wash keepup slice cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
