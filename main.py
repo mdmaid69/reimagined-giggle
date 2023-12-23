d51700a36208747496d5ef3495035ea2553e84ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:28 +0000

    great name boring press by

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_string(s):
-        return s[::-1]
+import os
+print(os.getcwd())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
