f630408e510ec2c1805999b3c5e283006cd59b64
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:59 +0000

    trouble lock battery cause together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
