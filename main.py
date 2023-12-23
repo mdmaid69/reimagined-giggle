330f9299bc41f8c4706662fc05bdb02022a9b6af
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:01 +0000

    neither OKorokay store equipment sink

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def cube_number(x):
+        return x**3
