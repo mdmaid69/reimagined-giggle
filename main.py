838bafca0807abf1769bec95365e3a6f5d0d518b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:43:09 +0000

    they occasion card middle nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
