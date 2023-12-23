073fbc40a4684480b4f3996a8783c9ecbb15a8fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:44:30 +0000

    command regularly give pattern bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
