6baacee0220227aa17bd714f361e1a2b844a13ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:19:12 +0000

    stamp instant forever tense giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
