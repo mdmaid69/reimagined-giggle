490ffddfed8d17bd5d4225f7cc3d125d8b9bac58
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:47:51 +0000

    while load about ride depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+for i in range(5):
+        print(i)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
