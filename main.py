6b7e416e187f4903e1400cb612940abc25c15756
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:57 +0000

    jump strict somewhere boat gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
