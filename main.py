00dfc82e5d1a5b2f151cb2dd228da688665ec079
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:53 +0000

    whoever talkback praise turnup surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import os
+print(os.getcwd())
