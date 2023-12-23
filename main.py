5ee13be6dfa7be663d8a0bb95252e813e4d3cf1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:07 +0000

    memory Nn cry slowly bus

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area_rectangle(l, w):
+        return l * w
