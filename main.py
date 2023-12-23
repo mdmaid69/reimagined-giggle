688181462e25e3b1d549e024c99d647f7b7e31d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:26:39 +0000

    behind sound at separate ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
