343a66a3010a9b7b7cb2f1565ae7e82bf1512cc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:29 +0000

    today plenty goodbye formal unless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
