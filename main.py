9c0b4560686545f1432202749607a879b811e54b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:58 +0000

    sick meanwhile strict train unless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
