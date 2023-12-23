fd971dd0051df062270c97f48ef5c199fe75bbcb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:03:08 +0000

    us especially ground since idea

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
-        print(i)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
