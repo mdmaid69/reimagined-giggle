973d6eb0a48f3e5a6fe4f123645ec9751dafd602
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:51 +0000

    be be bringdown this entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
