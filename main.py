13cee3ff86bf696034b7c9ec7348d3493dac17ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:03:03 +0000

    education speak protest deliver Mm

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def subtract_numbers(x, y):
+        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
