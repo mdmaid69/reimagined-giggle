57d9b3162735dfa94d0024c8912d77aef68579f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:30 +0000

    noone strict mix slight outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
