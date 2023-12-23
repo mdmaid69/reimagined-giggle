bb24165a58557dc3a3a6dc83e82e048b101466ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:19 +0000

    afraid blame book weak pinch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
