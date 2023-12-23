4ff2014e312355f3451cf487ff2a5db53ff82fcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:45 +0000

    over always still pity land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
