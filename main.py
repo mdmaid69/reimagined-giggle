4dcdf688d0dba9f92539836fb536babf954fcac9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:21 +0000

    please needle worth everywhere inquire

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
