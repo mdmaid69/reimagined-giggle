e13c281e0b7b21c2161022ab5430c2c4ca02e246
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:58 +0000

    fork embarrassing intelligent expression increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
