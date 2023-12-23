0febe595131b5c0b268f6821c85d5a37817d719d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:54 +0000

    tall worse workon ever the

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 def calculate_area_circle(r):
         return 3.14 * r**2
