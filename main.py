2b1f009f12c307c77895c805ebe50518bae0bba6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:26:45 +0000

    themselves recommend substance year nice

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
