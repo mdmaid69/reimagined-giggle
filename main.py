a0dc134f7762059df6aa01c0bba5a0a96093ae8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:09 +0000

    call sort afraid well Kk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
