28478810de998421f61376207b1b455b79a0a016
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:03 +0000

    game so whole contain comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
