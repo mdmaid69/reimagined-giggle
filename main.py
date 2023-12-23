19c5afd34a9a4ebd60f80c85f752195dbcf97b5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:02:47 +0000

    space push pen wash space

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import json
-print(json.dumps({"name": "John", "age": 30}))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
