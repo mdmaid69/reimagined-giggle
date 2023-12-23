1889a616ecc06f6e8ded7dc72fa984c33bbcad69
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:21 +0000

    right build 567enemy arm keepaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
