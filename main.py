a016265195bfb4647a76aa5e9be4029821dc03aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:52 +0000

    prefer wind position worth shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
