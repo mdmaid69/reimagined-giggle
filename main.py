dbc2bfd21b28e95abb82add6c509135f95896a44
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:11 +0000

    actual consult clever truth related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
