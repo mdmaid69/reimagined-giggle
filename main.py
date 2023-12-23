0da6668ccd3875bff8171df5e1aa9c1c59831a1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:53 +0000

    get might edge off book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
