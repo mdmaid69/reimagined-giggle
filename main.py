b065cde1affd1cfbf1c01f2aaca734985e681357
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:48:57 +0000

    release calm accidentally 567enemy swing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
