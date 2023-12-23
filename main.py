1b375e365ba6ea94b44ac863a3008953725b4dad
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:07 +0000

    its treat finish speech suit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
