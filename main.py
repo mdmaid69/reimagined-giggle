c9ac798d045bfcdf7d1775aff37fecefff074a21
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:30 +0000

    tape shake popular pen violence

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def change_working_directory(path):
+        os.chdir(path)
