bd53eb36a15211b7cfb78cc173aacc542679caac
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:02 +0000

    speed invite lonely wander fork

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_min(numbers):
+        return min(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
