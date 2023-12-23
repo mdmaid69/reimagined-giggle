fd75e0cffdf7ee3e462e9cf6894fba47792669e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:34 +0000

    ought well aware valuable wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
