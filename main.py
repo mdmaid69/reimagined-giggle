ad2bd6e3898ee128e22cff7fe17cfd2e0ccfd23a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:47 +0000

    flavour interested plan better delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_max(lst):
+        return max(lst)
