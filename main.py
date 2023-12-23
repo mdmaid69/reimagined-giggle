790f482ba73893780e082c5541a413e8dd012c36
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:32 +0000

    unit getback believe whom embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def get_array_as_repr(array):
+        return repr(array)
