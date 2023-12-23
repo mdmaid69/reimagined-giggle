08eb06c04dceb02c9dfcfbbd23472b4a05cde9be
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:24 +0000

    form healthy set we protect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
