bb72d5917aa0e95a98f7413a8d798bb640c168ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:45 +0000

    correction black claim bye meal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
