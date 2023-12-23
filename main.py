c72c0e68e3b5821732770fbb96f03f69b16f498d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:03 +0000

    close somewhere majority back clear

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
