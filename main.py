8b7818f261ea049ff67025d826389e6301f4ebd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:30 +0000

    respect muscle roll persuade complain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
