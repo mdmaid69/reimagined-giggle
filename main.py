e75fb53120b713541f83d00ad905d3d12a77b056
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:37 +0000

    treat pinch education sort generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
