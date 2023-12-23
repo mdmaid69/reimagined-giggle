20d87831040e03c98db57c5de429cd5a9ada65f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:57 +0000

    although laugh hate home relation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
