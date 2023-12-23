cb344a98b99f41ffa653ad5e23e39ee7619eef6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:29:00 +0000

    defend rich reserve size sleep

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
