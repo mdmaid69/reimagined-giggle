06f6179b4318d2c18c6787edeb28a1b5a824f0f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:00:52 +0000

    straight something fix idea strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
