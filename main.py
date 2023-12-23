545ade8f89618fdf914565d137971b5a56366068
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:29:59 +0000

    train comedown wide hang no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
