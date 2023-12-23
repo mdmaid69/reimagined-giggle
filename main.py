9cc22fece581828882b86a121ff668a88a4b6be2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:55 +0000

    blame hole smooth pupil estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
