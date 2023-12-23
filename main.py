6550aae673641de381d9129b192c0bf7886569a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:57:04 +0000

    once comment question air thanks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def convert_to_octal(n):
+        return oct(n)
