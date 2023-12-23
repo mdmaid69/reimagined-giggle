eeea2a19d3f64ab1ff0d31e8a900de45cae85587
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:13:25 +0000

    intelligent city failure complaint pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
