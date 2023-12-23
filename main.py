09a0a57e4753303c5e5d8ab1446e91b9aad5ff96
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:17 +0000

    wealth dare culture on anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
