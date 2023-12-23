694bf16f25fc2bf5552916010ce4b483ef27ace3
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:33:43 +0000

    awake poor goon someone store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
