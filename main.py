0cc2dfb5b3dd89dec9045a896c2474f54f14413b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:48 +0000

    comment defeat consult remember about

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
