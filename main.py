6c0332b835b34d3041cda94a526bd9e9202ec4bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:27 +0000

    cover fault right thinkof thinkof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
