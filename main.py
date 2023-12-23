8f740b0440b2978e0f40c23b0e63aa6f3df06c1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:29:08 +0000

    uncertain meet afraid kill completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
