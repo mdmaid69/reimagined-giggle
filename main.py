43ac605d02fc98c8a265c0c2c37af9b8fda69d7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:07:15 +0000

    ignore see crash offence protect

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
