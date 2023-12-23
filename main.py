92b1a69a632ddf16567086197b12fc9eb428b3ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:19 +0000

    feel official surprise crowd Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
