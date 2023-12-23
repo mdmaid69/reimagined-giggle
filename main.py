77837e1427701d35e953b9c56d11e732ef87f637
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:10 +0000

    area affect her nigga profit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+for i in range(5):
         print(i)
-        i += 1
