6d3539ae2b0f7c4904ac6fcbcb9149991d3153ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:06 +0000

    fill argument shine expect them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
