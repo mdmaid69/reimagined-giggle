50bd8d29aeed53ee9c8ad4643ea17b5a0384c604
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:48 +0000

    meaning complain attitude check affect

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-print(sys.version)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
