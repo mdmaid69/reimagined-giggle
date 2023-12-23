73e15d8f8251106145ff0a498b1476c2bfef4f37
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:29:43 +0000

    comeoff beat exit brown wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
