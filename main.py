9f398b133695e2c1efadabd2e59fbaeee17a0cbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:43 +0000

    switch cover Tt grow turnon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
