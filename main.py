0eac6b26a97f56d043fb7647e9d7873adb39779b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:29 +0000

    dare forget report disapprove godown

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_variance(arr):
+        return np.var(arr)
