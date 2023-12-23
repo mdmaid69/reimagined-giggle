7ef45e2195b4c258bdc76817dbd66d6193067e76
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:34 +0000

    cheap comefrom imagination bottom exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
