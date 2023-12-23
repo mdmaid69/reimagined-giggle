c2d72badcf9d82cf0bc428821b4a9e2099485a74
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:40 +0000

    ear combine style flight theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
