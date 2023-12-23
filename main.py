b86834eed80648a3c5f2852a6f3b48827233c325
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:59 +0000

    attract desperate keen abroad final

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
