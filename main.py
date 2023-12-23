689b11267fdd2609430aeaa2580a4a02635add29
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:34:11 +0000

    drawing say free show glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
