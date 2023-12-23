2b008d6c847a7c905c5e4d495bcb45f37768366e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:52:27 +0000

    squash test escape may advantage

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(numbers):
+        return max(numbers)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
