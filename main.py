f5a9a7ae2850bd8f3eecb962b1866f2a785e402c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:17 +0000

    determined old so bang unlucky

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
