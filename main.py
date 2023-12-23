cf595c9bf23ad1d47c2b5aa3666cfabf05193eca
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:00 +0000

    actually big project fat yourself

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
