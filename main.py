974b10e7c492f0022d21e2d9961c667e5c4923cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:16 +0000

    respect concerning quit warning quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
