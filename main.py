0acdeea7c6a233576bd48c0268894ebebbd345eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:28:50 +0000

    number without cause address set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
