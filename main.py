32ca4e7b3ee0e352dafd585a39f02e6e7ca5758d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:43:41 +0000

    refer giveup mysterious holdout generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
