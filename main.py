c6bd58ec70851487f16a4b4a7bf8b513582a61d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:03 +0000

    uponon dream law only advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_palindrome(s):
+        return s == s[::-1]
