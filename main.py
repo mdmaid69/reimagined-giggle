00cb76e1e19a608976b467e6f748eac28ff24503
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:33:20 +0000

    accident ill attempt try measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_pressure(force, area):
+        return force / area
