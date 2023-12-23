e4c43374ca84c215a96575302802ff0f019f7c70
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:16 +0000

    win opinion fierce responsible yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
