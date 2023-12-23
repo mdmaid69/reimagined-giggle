ac90b2daa3cdd7f4069e030c00687c88e68874af
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:05 +0000

    stupid treatment nonsense behaviour passage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
