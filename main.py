f0290fd65e51fc13f8fef40ac3bef212cdbbda77
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:20 +0000

    lot by interrupt sort ask

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_even(n):
+        return n % 2 == 0
