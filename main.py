4414509102cceb2902113d242bb61d564cd728eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:54:46 +0000

    figure contribute relative possibly brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
