97217c8225195763f30de6867046cc6149051ef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:35 +0000

    plan radio ignore breakdown rarely

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_pressure(force, area):
+        return force / area
