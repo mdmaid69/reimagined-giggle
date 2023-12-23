e0589c4d78ab2c7252104145fb5c6f9fd2a7d6e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:40 +0000

    introduction section open disappointed live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
