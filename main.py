26e610c6fd58dd86f9c52d1315cd84eed1c38309
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:57:08 +0000

    search usually delight they ride

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
