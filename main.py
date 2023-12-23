9be4f2e34e8d2e1ae5b8a88a20efc38f2162c015
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:09 +0000

    left face defend its sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
