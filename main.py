126d3e5a260a64d1cc6fc7ee09b3fabb0afdeb6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:59 +0000

    sure question regret fresh man

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def log_message(message):
+        logging.info(message)
