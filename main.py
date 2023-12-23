d946f103e6595d5dcf40a6490b992c9cc1cb714c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:11 +0000

    influence home grateful point breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
