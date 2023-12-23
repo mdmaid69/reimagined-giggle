4fbe6f8beb0908a4d6ad9dfd5205d0af5faf5173
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:32 +0000

    cope however hand who write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
