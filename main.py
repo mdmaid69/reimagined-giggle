a520f1385524421c7048f90aac49d3c408fea699
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:45 +0000

    179bite joke holdup calmly middle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_speed(distance, time):
-        return distance / time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
