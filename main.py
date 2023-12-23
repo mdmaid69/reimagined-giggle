ddffd919b2d8c1ae2c9935321275f08a9b327b9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:09:31 +0000

    plenty considering set greeting direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_floor(x):
+        return math.floor(x)
