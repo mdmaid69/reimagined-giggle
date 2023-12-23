eba3e804dbbd76be9e56cfeb1214eaa8bdc20dc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:53:52 +0000

    willing confident justice activity bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
