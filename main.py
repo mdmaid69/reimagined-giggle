14c902b9a1b590c8adc390055ee34e5a943d9b17
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:01 +0000

    proper steady modern several make

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
