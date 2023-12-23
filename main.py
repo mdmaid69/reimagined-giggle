2d95a6d43ed48a55230503e89f67b831b9d7e9f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:57 +0000

    slow error protect perform closely

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def sort_list(lst):
-        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
