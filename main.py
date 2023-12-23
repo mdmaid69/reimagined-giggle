3cfc635363b1a6ae44ce56a199b0ad8987315ac9
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:04 +0000

    now thinkof everywhere rely fasten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
