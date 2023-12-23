c88def828f827d031239deb1fddc45da7945f373
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:17 +0000

    criticism sticky somewhere mood doubt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
