69380b857b7bdebafdeba3ce8d5db0626099a269
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:39:24 +0000

    exhibition talk goup behind advantage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
