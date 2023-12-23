a94c6c5ec72e5198f4df1f228ad324a738fc95d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:21 +0000

    special disappear sometimes shape bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
