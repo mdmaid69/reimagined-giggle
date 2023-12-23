859117e2b5cb501f5255d93728e87ba48e3e48ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:19:57 +0000

    flavour rest behind steal post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
