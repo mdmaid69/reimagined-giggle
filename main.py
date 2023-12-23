000c78467e9385d55c3e112ed470da8eae694d76
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:54:40 +0000

    handle storm lost before point

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
