8662a7ed07c405123802b5eaa3847835e7e3aec4
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:57 +0000

    seat show event life season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def sort_list(lst):
+        return sorted(lst)
