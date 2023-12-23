99349ad683594ab2d3a4c1f5b17f4b622b923ae8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:15:42 +0000

    except again scream clever deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import tensorflow as tf
+print(tf.__version__)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
