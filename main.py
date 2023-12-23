4085f88645599f2a47de8697525d85b91a5d0dc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:22 +0000

    workon disturbing murder post poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
