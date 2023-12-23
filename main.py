8149f9ae2a06f70d11ff374d6cb633b020dd659d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:15:05 +0000

    yet upwards bad lower belief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
