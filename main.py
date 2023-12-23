adc1157ede3c3b6bd6edd156226d84b3f7c8c253
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:28 +0000

    software few at strike programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def reverse_array(array):
+        array.reverse()
