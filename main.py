f49a1352d2dc551466f737118a4c09837f1bf58d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:10 +0000

    weekend gather fear punishment refuse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
