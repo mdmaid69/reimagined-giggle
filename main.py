253627686ed192fd4921bdadc1c52e09371e3b69
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:04 +0000

    568energy radio life worry leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
