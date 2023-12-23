4707d039588e22a65a15931035c2c46096cbeff3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:08:51 +0000

    after average emergency actually left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_unique_words(sentence):
+        return set(sentence.split())
