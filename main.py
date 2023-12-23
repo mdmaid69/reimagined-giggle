c71d4ba73d9bb2f55b355d44101246d5f68b3bb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:02:41 +0000

    board trust feature remove underneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
