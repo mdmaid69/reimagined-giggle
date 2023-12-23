cb1b98d88c873e2af1057c70c4e07918d778531f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:52 +0000

    truth set bet sleep up

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def reverse_string(s):
+        return s[::-1]
