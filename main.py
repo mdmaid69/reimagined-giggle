ec097fc57a7fb4dae926ea541d1c51c467bb01a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:31 +0000

    partner clear view quick related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
