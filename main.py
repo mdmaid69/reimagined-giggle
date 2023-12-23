a5641cac66d22de98d4765603051f2f7ec460b28
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:47 +0000

    low sing silent support shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
