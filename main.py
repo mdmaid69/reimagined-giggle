ff34cfc8a938115101e2480b3711b56286d9dde9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:42 +0000

    guess step calloff runover now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def change_working_directory(path):
+        os.chdir(path)
