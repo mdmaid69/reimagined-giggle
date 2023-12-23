782b00f253beadce0afe66657c64b654c42fda96
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:45:54 +0000

    upset price city mess common

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
