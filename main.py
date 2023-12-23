67eb27e70528a14aee4b6b45027a813ced2a6caf
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:29:32 +0000

    few food discuss scream question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
