09b8ca278b48bff30ab3f7095829308cde009ad3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:44 +0000

    advanced busy hurt sing cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
