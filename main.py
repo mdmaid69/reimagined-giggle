79c79970f7f4156ea5a432305710b78a57bbeff4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:10:42 +0000

    quit basically sensible mess fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
