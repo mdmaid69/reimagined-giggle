4dfd26382cb87cfe2237a577ab3bccc142842125
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:10 +0000

    big Qq Jj die half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
