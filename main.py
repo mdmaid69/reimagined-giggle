98065fab71f53a7259fc00c4c6b7345faf00600a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:42 +0000

    careless might slightly question doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
