fe855ac804c46ee7ac1d9e64cc71f2222191d877
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:01:33 +0000

    police pass every cry certainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
+print(sum(range(10)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
