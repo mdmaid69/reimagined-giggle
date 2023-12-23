345941abb42142fbd5b17b8953b632643e9a1370
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:06 +0000

    majority pencil force eat people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
