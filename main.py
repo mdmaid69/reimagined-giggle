9d8294e0ecd02ddeb3255630bea3d571306b3016
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:26:35 +0000

    opinion provide bus advantage total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_even(n):
+        return n % 2 == 0
