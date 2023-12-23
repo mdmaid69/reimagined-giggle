0bb5a1a3065837d4c6a381f92e007ab48a06bb90
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:09 +0000

    work walk shoot bottom poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
