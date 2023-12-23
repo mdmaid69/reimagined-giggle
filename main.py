2709ef540441f8cd90266609ed0a38eee8cfb979
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:00 +0000

    successful rent late sound wash

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+for i in range(10): print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
