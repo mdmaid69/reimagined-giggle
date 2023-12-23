28c4e698576d8e34f55f26cff9566d7c2a203d24
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:01 +0000

    572enter bunch treatment object exercise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-print([x**2 for x in range(10)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
