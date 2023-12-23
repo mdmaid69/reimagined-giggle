56555bd48e587306d983de8f5a8d20e05aabc671
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:55 +0000

    away season thankyou blind undo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
