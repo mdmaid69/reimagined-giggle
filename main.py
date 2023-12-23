fa7bdf0c6fb994dd252725bb8976eeb343ac84ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:00 +0000

    see handle ahead emergency back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
