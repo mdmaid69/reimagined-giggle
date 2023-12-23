b796188452008e062d0a4f91295a00baeacf76d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:31 +0000

    way across store giveout because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+import array
+def reverse_array(array):
+        array.reverse()
+def count_words(sentence):
+        return len(sentence.split())
