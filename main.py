1f7b20ad68c7e2b5347be08a80f083d79b9634cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:29 +0000

    beginning Gg closed star whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
