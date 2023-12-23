1f8f71040fa5174f5bdb9c8e4f58120b46084d37
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:05 +0000

    laugh turnup display year Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
