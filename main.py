9dd0859e968e598ffdc68cc83d6840d50e397816
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:14 +0000

    dare movement uponon somebody influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
