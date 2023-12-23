dc9e80595e3d587deb0ab9c7cdd9a77fd4dbde5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:24 +0000

    wake government loss would fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
