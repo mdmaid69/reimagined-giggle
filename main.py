87a5a4e44dc296e2d28db4ff50300b7b8138dc5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:17:11 +0000

    bag bye pattern politics Gg

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
