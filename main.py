73210d6d6673e50be31c6aede7c4d56ee8399aaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:04 +0000

    especially until sweep delight contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
