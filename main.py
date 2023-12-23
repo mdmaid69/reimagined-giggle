f923998498b8e124ee5a6f3003a53fd9f52bfcb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:36:12 +0000

    opportunity contain disturb birth travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
