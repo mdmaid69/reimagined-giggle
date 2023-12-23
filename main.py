b63065600f0edf7bfb7cf392ee0a600075b883a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:03 +0000

    too spend careful male weather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
