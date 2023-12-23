59b736461b5f49f4fac8f2163b3d02f85e9d7e0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:24 +0000

    imagine address make clearly board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
