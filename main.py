16cad9d56923fca998a42541274e2fa30b705d31
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:50:49 +0000

    worth wander thought turnon OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
