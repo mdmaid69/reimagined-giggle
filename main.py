41e795aa6342b9a919ea43e00b36e82669ddfbeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:22 +0000

    Ll concern talk impression along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
