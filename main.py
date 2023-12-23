f6baee4d7e3db1a9f837b25acc5e855f54a62ecc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:20 +0000

    freedom which computer deaf tend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
