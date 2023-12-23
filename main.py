92c893770adb3f2de76e6567aa8451e30d4494e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:15:45 +0000

    bread advantage fashion urgent realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
