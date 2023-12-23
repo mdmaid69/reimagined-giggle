278ca9c2ce24f3528e27c1076168e23f901119d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:05 +0000

    shame aware mistake steer past

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
