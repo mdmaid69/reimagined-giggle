59f3af6a6b74bc0d02bee8aa15c854435656a205
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:27 +0000

    circle tongue even serious through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
