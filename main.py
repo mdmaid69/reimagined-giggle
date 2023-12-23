7768c0f61dbe8eb97c43b3a748b10193fc154d9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:58 +0000

    natural centimetre create obvious trip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
