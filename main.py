274bf127eaf174ae315579b418f185c8dbcb702e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:46 +0000

    prevent boring approve sensitive joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
