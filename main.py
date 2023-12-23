6e6c501671446301086e766a5c6aa7de658a29dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:27 +0000

    concerning belong intend beat sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
