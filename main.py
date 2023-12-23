d9fe1a220283ab78a516b963edad818e98055060
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:41:15 +0000

    stir trust careless justice pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import random
-print(random.randint(0, 100))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
