20e029eaa9c55eac091e77076182b3268c8d6fa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:23 +0000

    Yy opponent office complete last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
