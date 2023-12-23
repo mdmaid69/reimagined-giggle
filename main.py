cffc44e1367c641a92125db6a63d84e72edb9be0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:07:27 +0000

    talk break wise else form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
