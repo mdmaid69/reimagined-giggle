ed7434aa13a9680767d0fde597d886a67bb23c42
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:57 +0000

    door meet getoff man list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
