44c3da4f311b89f976cfa735f4eb85895bec40c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:55 +0000

    accidentally declaration total rate sticky

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def reverse_string(s):
-        return s[::-1]
+import datetime
+print(datetime.datetime.now())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
