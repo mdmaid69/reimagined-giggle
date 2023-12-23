20532ab2b93b1b2743551bb1dbac8c656d7aabb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:09:44 +0000

    live getaway tidy wash survive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
