ec0273b11eeab902bd7a35adfc07cb92cf08299b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:19 +0000

    allow communication store difficult crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
