d96a25b5be0c7e2b9b3e29ec95adbeb4ca5fc69e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:53:00 +0000

    compare distance means previous family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
