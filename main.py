e5cb73543f9424790e833d43bd9963d921cadaa7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:09 +0000

    university past formal while kind

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import collections
+def create_stack():
+        return collections.deque()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
