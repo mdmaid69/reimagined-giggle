29952b3a7be81ca44574555c5e073cbdd3b27244
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:22 +0000

    bet key brush basic race

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
