862ccc6231c8438170840eb4812d459030f86960
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:56 +0000

    full score listenfor present various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
