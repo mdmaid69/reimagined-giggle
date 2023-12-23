46fbf877c91125b07f7dde573e5f48594ac6bb52
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:26:33 +0000

    opposite between recent trouble runout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
