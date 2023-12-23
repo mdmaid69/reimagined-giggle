b17d7edff9459ecc2a59dbbd784db4c1b5945745
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:40:33 +0000

    leave century fall prevent century

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_index(array, item):
+        return array.index(item)
