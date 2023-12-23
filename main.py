2637946d91925194a4bf8efbe42ded4636366afe
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:58 +0000

    pollution sign goout entrance tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
