b6e1bd56e8d304f8e55641a91441f4e1b80aa17e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:36 +0000

    experience 179bite holdon cover gentle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
