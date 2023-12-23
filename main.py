d4a8f376bf39edbdf93f71868c612e5c9efd44de
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:57 +0000

    study amount success suggestion goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
