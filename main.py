114b9fc19184fcf6f71d36e7ea3ed15a0a798f62
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:20 +0000

    muscle secret various some chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
