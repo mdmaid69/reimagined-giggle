294889f4fbdd47547dbec7ab7a222ba048fa224f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:24 +0000

    street service pleased phone university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
