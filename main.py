ebcb60ebf312eb76c7ef0b4d875cbbaaa316a614
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:03:25 +0000

    deep clue nerve quit friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
