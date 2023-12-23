52c2f50953fdae550ae8cec76cf75f88411cb398
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:44:19 +0000

    big interruption dangerous wild wherever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
