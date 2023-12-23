ab4c2175a0bf5830423a5d7caf5b19f0f90fd7ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:34 +0000

    exact regret holdup trial run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
