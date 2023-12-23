360c83f43a8815cb4796c7d45779633a1b0bb6cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:30 +0000

    cross out strike several accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
