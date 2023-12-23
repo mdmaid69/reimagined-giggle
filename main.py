021180f6985ad35f929ea3a5eb934f77fbda84d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:26 +0000

    hair calmly estimate calm become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
