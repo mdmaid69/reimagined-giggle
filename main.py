a711ded97d5da4a4f0405624b627b5966fbbdd31
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:47:07 +0000

    emergency rarely false loud sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
