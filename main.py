0a594fd3ad25bf979be9f683ca74df3bc04e6042
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:41:17 +0000

    rub bang copy scream side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
