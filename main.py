c79dfdc47a8537e509f6a7c94c279c9b26dcf306
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:55:37 +0000

    occasional suffer share whom whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
