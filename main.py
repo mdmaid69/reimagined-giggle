02fa96e1b49851480806ebe584d6a253a251e6bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:40 +0000

    book official must let idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_exponential(x):
+        return math.exp(x)
