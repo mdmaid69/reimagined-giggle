5fd85606d7b5584dfb86272de1ff4b7f805f2b10
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:57 +0000

    tough impossible completely damp getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-print(math.pi)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
