42c85a7242b81dbe089b24ab805c199be88cfc09
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:38 +0000

    operation holdout closed book fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
