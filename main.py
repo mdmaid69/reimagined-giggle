7232a2ec3cb58668aa0a376403a842ae9a7525e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:53 +0000

    freeze among rent fall pen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-print(sum(range(10)))
+import os
+print(os.getcwd())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
