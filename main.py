80136d07d9aa8dc8dc789dc032334ceb736d6a20
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:47:32 +0000

    correct dangerous play every deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
