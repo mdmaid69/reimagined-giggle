c111d07bdcde22427ccc57299b15c84cb903c55d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:18:06 +0000

    skin try tip floor sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def remove_from_array(array, item):
+        array.remove(item)
