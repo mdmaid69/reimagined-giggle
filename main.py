b2958cfa0e5eb79344511249a8c5f122caa00766
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:21 +0000

    plenty keepfrom type reduction voice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def convert_to_binary(n):
+        return bin(n)
