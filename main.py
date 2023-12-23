56452c1151960246b4609d285c46859c1f2551a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:59 +0000

    eat pass workon lift finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
