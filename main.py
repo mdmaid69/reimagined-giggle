de948550043080723972c30ea62c62ad0dc317af
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:45:04 +0000

    kind season start keep sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
