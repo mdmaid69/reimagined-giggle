e4e22ff96c15648ee5ec78446c4caf55b1fe2013
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:17 +0000

    term offence repair term doubt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
