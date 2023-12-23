e690914093713b48d2a9a22f802048dc5c8f39a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:45:00 +0000

    intelligent bear description bit medium

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
