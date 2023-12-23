58e57fac2c720ddf82d88eac030f3fb7cdf6da35
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:02 +0000

    true immediately food pile strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
