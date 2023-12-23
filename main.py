defba9ede08184ba69654ecc0a070e42ff13dd56
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:02 +0000

    reply warm study close success

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
