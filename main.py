8b63abc3f7d5d3c1f5406df67db78ac581b530ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:19 +0000

    punish combine rescue range miss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
