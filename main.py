4109587a949bd001faeee011f9d156e96575bd54
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:22:44 +0000

    bet object reaction maximum problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
