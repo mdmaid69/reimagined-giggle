733b8afcea5491a7befd012f7bf5564b97d43b19
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:34 +0000

    buy far music their loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
