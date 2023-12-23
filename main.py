0f6189539565a109f6d3c61d15dbfda3ca3c6119
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:46 +0000

    public certain kill bridge hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
