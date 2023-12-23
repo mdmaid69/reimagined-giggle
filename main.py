e0b0e3870df9fd9fd899db478aa876bd8030898b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:54 +0000

    produce drink finish actually light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
