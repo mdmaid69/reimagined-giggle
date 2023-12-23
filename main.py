37fdda94a0e7eb576cb4b887a8a2e99ac3769e83
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:50 +0000

    piece instrument authority education exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
