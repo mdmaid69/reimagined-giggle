8d2da96dc6d9ea31bd084f3cf843f2e7ac77311d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:59:22 +0000

    congratulations community pretty risk tight

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
