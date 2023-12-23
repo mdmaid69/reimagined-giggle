e0add0ace069a312998e2c311d5dc37fb250809f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:44 +0000

    takeover ride Mm education reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
