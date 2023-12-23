40be4044646438d9298a0b6fcb75b18022029d43
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:45:10 +0000

    something delicate politics case power

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
