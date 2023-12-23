8851574a4b3d694f2cae83cd202cfbba8e5c6d84
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:34:01 +0000

    colour dangerous fortunately clean sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+print(math.pi)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
