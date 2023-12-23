4a038228aca475ad9203f576b13bf6976ee9c1e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:28 +0000

    mark second under start Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def add_numbers(x, y):
-        return x + y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
