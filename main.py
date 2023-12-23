baf94595564fe7e206c118ecf64d960b4d98ba21
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:17 +0000

    thankyou cross home equal fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
