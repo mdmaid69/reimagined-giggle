ff713cd80384a571162f1d9f9ef166f830fc4238
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:21 +0000

    understand government correct circle twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def square_number(x):
+        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
