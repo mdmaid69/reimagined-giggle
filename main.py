50f2ad824629ea264e645fe26d94397e0fa2c05d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:59 +0000

    approve slice unable swing fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
