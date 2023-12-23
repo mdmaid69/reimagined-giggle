7421102ad6e51cd307398ea4d946500cfae57225
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:49:54 +0000

    risk either road blind fit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
