ea5370684935a162a17ddf001aa014443de783e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:30 +0000

    it our back plastic petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
