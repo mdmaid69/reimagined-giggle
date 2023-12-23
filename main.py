deeb9de73cb32e5791fa9239b791c6d81bb98d06
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:33:59 +0000

    no favourite different skirt cause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def find_max(lst):
+        return max(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
