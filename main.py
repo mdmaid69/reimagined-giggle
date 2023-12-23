448432758c4fe91f4b6e49c0391fa90c2176f483
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:47 +0000

    aware drawing question previous other

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
