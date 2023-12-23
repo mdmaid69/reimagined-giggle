36e06c56bb40c39542f57e1af4b50d03b6436d07
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:52 +0000

    fine school something favourite while

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
