595437f8cce995e30a5842d6510784bc7ebfd5f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:12 +0000

    doubt dig save attractive supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
