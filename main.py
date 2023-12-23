2c8ae038570c9cc89f295a097f05ab5bc50283a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:23 +0000

    possibly ill tear opposite blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
