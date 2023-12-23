30708b019d0d3e13992b236c6ea7ae1194de316d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:47:58 +0000

    smell quarter take bother odd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
