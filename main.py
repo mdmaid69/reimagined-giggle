0945e1f97d25a74f3028e7e145998e411416bda6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:16:16 +0000

    179bite tense knowledge full somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
