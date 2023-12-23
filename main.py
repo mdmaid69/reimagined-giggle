54774f0a51d9bec3cb5b7c1fd14375609ae335ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:23 +0000

    test congratulations wash will branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
