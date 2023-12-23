a6f226c9b2afd3c885327d63b93ec75d6a044782
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:54 +0000

    rescue risk band beautiful collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def convert_to_binary(n):
+        return bin(n)
