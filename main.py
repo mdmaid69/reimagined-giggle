b771c072623d1aecdb74165c5ace655cab844517
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:59:54 +0000

    lookup programme search command hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
