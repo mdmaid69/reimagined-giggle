7d1cfc2b06186fbc1fb5d442db47473998184966
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:48 +0000

    issue destroy last spoil stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
