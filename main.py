37c669d7363ccb89691ef7e94e787f70f24e72dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:34 +0000

    unlikely talk afternoon far call

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
