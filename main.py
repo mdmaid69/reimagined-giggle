8caaf9afa14d94bafcf00bf9174c56a822f82c33
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:32 +0000

    mood light no contents anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
