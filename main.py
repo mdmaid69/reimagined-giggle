4fcbf670972f68b5ad8ace7cec2e7a778003441b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:42 +0000

    likely hit wish action refuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
