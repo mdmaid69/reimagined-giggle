64ff742a9471daa345e821d7da158d1e73396249
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:56:48 +0000

    surprising come undo exactly wet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
