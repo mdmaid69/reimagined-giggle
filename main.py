9fb4ad817566698a17076be011634a74bbd308b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:54 +0000

    pinch ready fasten beginning through

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
