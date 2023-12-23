f5d614a74329aeee96162ebbac4f7ce724f34c97
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:45 +0000

    office then unlikely luck means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def clear_array(array):
+        array *= 0
