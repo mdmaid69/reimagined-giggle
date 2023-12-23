fbe7a95840bb59624495207eeaa3ead71c10dd9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:23 +0000

    hope soon wind convincing ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_unique_words(sentence):
+        return set(sentence.split())
