8d8389637a637e1c3edd699a94e4ae11596f6912
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:22:57 +0000

    address brush disgusting fit check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
