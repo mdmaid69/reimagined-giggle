72bb76c4fd517ce30cff45c3d54de13c01367687
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:01:33 +0000

    mad region release score beside

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
