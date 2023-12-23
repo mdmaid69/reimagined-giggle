591b54c14cf2bf295368c8699facd3a05a61424e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:22 +0000

    dry office patient no official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
