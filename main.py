aeb16b900f4481e650f2412f1ea3459baac0fc57
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:40 +0000

    second influence pour show high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
