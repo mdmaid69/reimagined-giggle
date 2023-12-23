f673d184e7d690caf5527db7d417fe73dd397a8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:00 +0000

    season comeround plenty free travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-print(os.getcwd())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def square_number(x):
+        return x**2
