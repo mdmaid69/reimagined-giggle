384603caf8441a8b2d129d5bd691af615f059739
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:29 +0000

    recovery score treat reply fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
