63ce2041a4cfe0263151069441cda017967bb6b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:07 +0000

    approval quit office release improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
