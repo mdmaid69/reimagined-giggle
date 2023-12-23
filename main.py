7b9ad8379d4e6f092b3616d066a640765b8a132f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:04:50 +0000

    command rough leg certainly narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_set(array):
+        return set(array)
