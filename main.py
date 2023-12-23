1d6a4c46d33e5942f01a8db1fe232308d8519cf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:43:00 +0000

    team tone bet accept declare

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def convert_to_octal(n):
-        return oct(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
