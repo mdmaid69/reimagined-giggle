14c14278d25eb01d71d6e6268d04baf5bbf78470
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:49 +0000

    notice bother workout read stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
