171be2b05768cc2e5d5fc5ababc26de45f047c74
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:07 +0000

    direct dress tragedy dry gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
