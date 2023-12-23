0c0d363d96b246ce6484c291a2dd8ed936b3518d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:34:55 +0000

    dry convincing round taste wherever

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
