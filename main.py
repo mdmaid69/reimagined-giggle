16f3b302f0e7022cb8c5aed98b9b812a6b15f0a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:47 +0000

    organize sudden interested edge often

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
