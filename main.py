04f3fb703f8993256163368db14ef6e8607a07bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:29:38 +0000

    claim scrap respect equal warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def subtract_numbers(x, y):
+        return x - y
