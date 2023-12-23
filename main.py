a6c5dd064d4d6652ca56ba39beb096b550dceadc
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:10:29 +0000

    big afternoon yesterday give reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
