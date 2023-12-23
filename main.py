aa31b69e62a20c9f6c2c26c0abc0646f6a9ed99c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:44:30 +0000

    push peace cross suppose Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
