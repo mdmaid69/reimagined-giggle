45f6f3d39d02698831d9f61b05628d3a3a9ea1b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:21:04 +0000

    favourite education write down matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def convert_to_octal(n):
-        return oct(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
