5fa7af4a0385804225b7fefc950ad8040df25151
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:20 +0000

    minimum string round second distant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
