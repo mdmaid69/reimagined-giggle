dca28345611db54daea1fe87437be1bf64505776
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:23 +0000

    something project my arrangement storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import collections
-def create_stack():
-        return collections.deque()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
