a1d8f9f5c9dbaf68f92f55b91169bd71cb198aba
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:24:48 +0000

    firm vanish notice insist live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
