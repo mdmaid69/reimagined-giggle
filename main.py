34102d96bba25cfe2451bd77c5e9a7f88dd82aee
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:44 +0000

    side promise Ee identify none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
