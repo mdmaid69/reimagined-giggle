96ef45d7b463e953bf44c547a7b32410224b136a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:34 +0000

    regional butter reply gooff priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
