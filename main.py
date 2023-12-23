ac343194b840ea374fc4dfd3c83fdd2101282829
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:05 +0000

    level storm breakdown private distant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_array(array):
+        array.reverse()
+import time
+print(time.time())
