5d98fc2321dab21c5de4a523832267d811ae074a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:11:27 +0000

    habit prepare run change fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
