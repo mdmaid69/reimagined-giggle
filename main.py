469fbe7b761c888e6d5b494e741e01f1b124808e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:26 +0000

    fault conclusion curious file address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
