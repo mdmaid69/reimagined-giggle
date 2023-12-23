080bb42700adf68194aa74f33ab1ff1ba8618b33
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:06:01 +0000

    closed neck aim grateful material

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
