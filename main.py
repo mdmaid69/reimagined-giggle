21f5f05f61ff3362163adb2ef2dbf85c8d45de65
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:48 +0000

    there delicate face light opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
