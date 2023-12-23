9dc7cae9f63270b0cb597685ffab501735cea1af
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:22:29 +0000

    warm against slow straight new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
