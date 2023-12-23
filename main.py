0411147a37f4b8c47b29dbfe4ebf48224b8b75d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:32 +0000

    add increase acceptable earth large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
