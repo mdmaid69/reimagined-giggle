1597b22bd08f82f9aee78f24ab2ee10979f18cb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:33 +0000

    familiar characteristic never nowhere across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
