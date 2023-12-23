a30a4e9d44b494f23c1969bcf6fe25821f5812ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:59:37 +0000

    flavour touch thing reply bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_repr(array):
+        return repr(array)
