bdd23200ed7e7f4f32c8385f19f835ba832ec237
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:06 +0000

    act sign accidentally excited emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
