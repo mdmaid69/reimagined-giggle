54b648050832c9e43f67837fb8fbcdca37892222
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:32 +0000

    refuse drink cheque fetch statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
