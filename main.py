c544a8c799a0ef96ed4d8c8a8a3795f7ca30853f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:13 +0000

    sweep introduce heart estimate factory

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
