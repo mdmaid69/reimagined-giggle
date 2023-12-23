366ae0832eec5c1b2946e3838dd8397fb401dfd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:47 +0000

    secret frightened yours let news

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-name = "Python"
-print("Hello,", name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
