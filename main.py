f04b975793c92671a8aefe3f9cca8c5b85310a15
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:01:22 +0000

    base capital rub thinkthrough I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
