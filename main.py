efb479531043824108bea986ab5a4c6a72a80bba
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:28 +0000

    complete mean rub tonight prize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
