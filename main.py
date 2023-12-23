aaf0b0ecfa4da39a47b2c9120503c41e9a838edd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:40:36 +0000

    welcome however favour whisper spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
