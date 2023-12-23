614075e03a9383d321bc8239ff3c4317effe17e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:00:31 +0000

    callat mainly could police centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
