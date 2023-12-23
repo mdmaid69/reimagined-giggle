d5732dff27a8860b8a7ffd5d48fe7ca100234599
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:46:48 +0000

    laugh chase file grand come

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
