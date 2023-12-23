30024994b276205a5fa13fd39d9b225f2df36986
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:48 +0000

    gas wrong last sensible person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def exit_program():
-        sys.exit()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
