e082e66d5f77ee8b345a95db8095e8af036445eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:58 +0000

    whisper warm offence steer popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Characters:", len(text))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_set(array):
+        return set(array)
