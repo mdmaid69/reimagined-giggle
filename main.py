49dc0774d75a86efc8e1bc89492cd713f8c5f1ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:46 +0000

    excited mine murder natural apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
