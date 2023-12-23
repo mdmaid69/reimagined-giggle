091eb95926003361bc29b558c87df41f73736158
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:02:35 +0000

    post effort blow last special

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
