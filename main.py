447994279803ef4f1b9133724071b8e169b43556
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:02:00 +0000

    close anywayanyhow sorry none impatient

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
