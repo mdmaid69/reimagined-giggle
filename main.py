1005cc8f23252948bfcf20eb346e38d3ac96fbf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:44:37 +0000

    method raw damp interview claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_min(lst):
+        return min(lst)
