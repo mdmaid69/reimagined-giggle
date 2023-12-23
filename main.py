9e0a85548f70e2b1532e39d68c21677178f146b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:03:04 +0000

    tour rubbish knowledge art front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
