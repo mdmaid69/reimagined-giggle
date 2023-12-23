e794a5c0c2e212181da8f1b2cdbe58c5b8131cce
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:20 +0000

    sign distance keep plate pocket

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_user_string():
+        return collections.UserString()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
