99118676d0a005e704868292bfd230de75853abd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:42:28 +0000

    noisy 570enjoyable soil gift through

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
