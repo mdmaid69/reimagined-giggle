40df9555d4066c1d3cc99db26a408689bd31ebd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:21 +0000

    cost announce friendly move exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
