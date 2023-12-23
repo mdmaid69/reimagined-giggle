dec2532e298265cba2516c2ad323f2d624369f32
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:42 +0000

    tidy recommend waste priority blind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        print(sys.version)
+print(sum(range(10)))
+import array
+def get_array_as_repr(array):
+        return repr(array)
