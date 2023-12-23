d92c578b71452b0e6ce019d05943caf643f5897f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:01 +0000

    stop ilovedicks approval change surprising

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
