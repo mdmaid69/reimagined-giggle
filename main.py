9989de2458c54d73b29149ad9ea9802fa6ecff94
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:40:58 +0000

    plain ear word spend flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
+  def convert_to_binary(n):
+        return bin(n)
