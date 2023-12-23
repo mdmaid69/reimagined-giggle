2895c2b44b35019e857539af31f9c67647481027
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:04:27 +0000

    regard majority shake why basically

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
