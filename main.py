1508ecfcb81bd0aecff850e705faa25f1b4a1582
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:14 +0000

    interview recovery apart familiar whoever

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import re
+print(re.match("h.*o", "hello world"))
+print("Hello, world!")
