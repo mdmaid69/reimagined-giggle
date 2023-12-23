62e7034dfd1bdb75a13bfdab88d5a72831ab9cb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:25 +0000

    equal ever storm fun callback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+def find_min(numbers):
+        return min(numbers)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
