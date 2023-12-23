9890964e4ccc308f2e14067831fb4e3933c12581
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:56 +0000

    friendly owe actual hit practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
