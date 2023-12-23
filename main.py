68c29d15f13a0b5789de33e450451f798993cf2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:54 +0000

    bottom interest year breath shout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
