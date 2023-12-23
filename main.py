295e7eaba105bb24054b091fe70dafbefafdb97b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:59 +0000

    match tragedy would professional with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
