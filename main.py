89a583a27a98b718aed9b0bd2753cf4277c911fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:11:17 +0000

    desperate damage mood flat ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
