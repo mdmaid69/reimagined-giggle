e96af5d447bcdfcb22a1e8674099922645d1da61
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:15:36 +0000

    clear uncertain whole note have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
