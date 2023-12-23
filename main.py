65c5c8748d617a5df3c24c2aedfd14c18f0e32bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:22 +0000

    wonder tread takeback whenever expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
