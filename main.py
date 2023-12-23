e505b4abbb1cd1e54e70f85e55cb5e8e77045914
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:14 +0000

    closely somewhere her culture slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
