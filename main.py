3154fe42647bd6633072021fdd4d8530c6bd8892
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:57 +0000

    tune wild base mysterious sign

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def convert_to_binary(n):
+        return bin(n)
