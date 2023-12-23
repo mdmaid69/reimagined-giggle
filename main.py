fc526e189a0ee2fe9a8b0cdf5bf661ca0a4cad17
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:12 +0000

    lunch how boring town strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
