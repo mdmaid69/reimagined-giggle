3dd909a8b59b1857b15866365e92533f327b4414
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:51 +0000

    feed dark trust wind plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
