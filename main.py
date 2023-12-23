21699c29da53fe0ceb34a674ce83c95c698e2973
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:12 +0000

    willing mine laugh winter recognize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
