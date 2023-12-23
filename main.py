b8f121a80f341bad0a12f08490d838233ff0b345
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:32:36 +0000

    politics valuable steer public weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
