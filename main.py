ebd7b3df3b9496504ec06d57eaa67637d55c9f40
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:14 +0000

    accordingto pocket land objection show

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def get_list_from_array(array):
+        return array.tolist()
