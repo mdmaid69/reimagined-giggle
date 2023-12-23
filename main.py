6b362a1c6b15bc19f187d622f11cdb8012114012
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:22:10 +0000

    hand but swing might win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
