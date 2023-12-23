675239ba6b37731709a269e6cedbbbc8b55900bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:37 +0000

    bear contact concerning drive splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
 def create_default_dict(default_type):
         return collections.defaultdict(default_type)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
