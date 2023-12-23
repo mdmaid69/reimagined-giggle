97f5ed2486a48fe2bdf03db34e9c000ac162ec25
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:06 +0000

    scared bright 573entertainment report bunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
