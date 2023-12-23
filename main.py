4f103490f8b4aeb65c3b9ec31c5c20cffebeba97
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:57:40 +0000

    slow suit fight main trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
