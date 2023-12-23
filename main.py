44f48717825ff1e837bb656904bff1109cb054df
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:56 +0000

    supply 571enough among record brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
