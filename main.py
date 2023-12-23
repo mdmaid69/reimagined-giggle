438a7b338bc9b3f9256cae85c453b47f1f7d0ea4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:57 +0000

    loan tour notice bringforward content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
