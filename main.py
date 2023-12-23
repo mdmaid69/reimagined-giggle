f72e960643235611405e66fce5dd50d1aad55a10
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:46 +0000

    certainly neither in lunch pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def square_number(x):
-        return x**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
