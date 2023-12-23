d8c32e87e611c9b1ce41147f57a1ea13d1528728
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:59 +0000

    on getout hurry tour where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def append_to_array(array, item):
+        array.append(item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
