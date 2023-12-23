dc286c4adad9d5513d8612d37f660da71caf017f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:16:28 +0000

    use idea immediate all means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
