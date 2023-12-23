ab49f6e85da16a417d415368ec5d2c68fb93fd52
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:35 +0000

    course sometimes rush tired upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def clear_array(array):
-        array *= 0
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_density(mass, volume):
+        return mass / volume
