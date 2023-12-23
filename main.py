33e28ce4d21ad8b1215049fbd073f7e5a181aa4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:15 +0000

    force decision case flow worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
