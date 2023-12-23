3d67eb44d4f17d0086c6e8f7d6ab69f879a9daf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:57 +0000

    forever better lesson issue afraid

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_rectangle(l, w):
-        return l * w
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
