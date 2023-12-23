69bc06e0498bae3ede98bcadcb59012e94c9eba4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:39 +0000

    zero they judge middle petrol

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def convert_to_octal(n):
+        return oct(n)
