4f65d122af5ad706179bc771420aee145c9b5a6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:41 +0000

    man neither show Mm 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
