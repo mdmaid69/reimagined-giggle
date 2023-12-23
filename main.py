64657014b04ecb67be2594d324a956770110e8ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:16 +0000

    old suppose cool 565end natural

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
