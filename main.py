6dfa53c49b44954d7d85f954c5305417b9403051
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:33 +0000

    furniture survive news badly Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def cube_number(x):
+        return x**3
