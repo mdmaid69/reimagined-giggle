3bdd2ff91a7b66e4bc46ea5bcfd10088b9e13873
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:39 +0000

    quantity ourselves blue pretty somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def count_elements(lst):
+        return len(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
