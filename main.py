5235f045474b07b095fe7c12dc06fc1c1d0ffb3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:42 +0000

    smile form dinner pretend goon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
