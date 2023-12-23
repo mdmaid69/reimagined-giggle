6dd4671922aec6d69a549c4a3e9b483478cd4692
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:39 +0000

    lucky giveback part shop hide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
