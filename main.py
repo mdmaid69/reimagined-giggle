6adf752be6bde94cade5f905c16b7a94fac7cf71
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:57 +0000

    wet protest factory bored accordingto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-def calculate_power(work, time):
-        return work / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
