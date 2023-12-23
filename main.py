0122869e70c2b3125815f8deabf03415a5b26971
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:16:42 +0000

    consult scrap vanish between doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
