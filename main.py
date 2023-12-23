e5bbe8693cea5376d37884e95cb362c1c8af4bde
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:25 +0000

    fill woman reliable show fun

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_list(lst):
-        return lst[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
