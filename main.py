8cbd1ba047d445a47b88f21b8558fe58e3325e8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:20 +0000

    probably play prize good language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
+  def calculate_area_rectangle(l, w):
+        return l * w
