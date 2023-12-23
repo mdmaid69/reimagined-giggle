98e7ef6279a12bee0bc6dad2254fabc957424fde
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:52:35 +0000

    return interrupt arrangement honest equipment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
