4a82d25b1fad5dc9d5f5aa161d79b39a24cbdc4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:29:48 +0000

    worst bread under send police

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_repr(array):
+        return repr(array)
