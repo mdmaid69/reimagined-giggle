e40d1838ef971bec3f84cdac7f091a2d521951d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:32:45 +0000

    time makeinto best now expression

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
