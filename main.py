49839066ae1fc6b6565e440f4dd4e3dc9ad9b5d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:50:04 +0000

    computer warning appeal error art

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
