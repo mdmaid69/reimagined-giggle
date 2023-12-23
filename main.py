b5b214153a133487216b07f4842667c52fd0c3bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:13:01 +0000

    power neither confusing respect negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
