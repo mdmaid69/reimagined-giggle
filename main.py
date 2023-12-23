f1261b1a08c87d38c179f3aca288b35f196bd105
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:31 +0000

    temper also safety progress breathe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
