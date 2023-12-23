735e906ef489065d554572a70975529183cfab9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:08 +0000

    speed comeup attitude love car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
