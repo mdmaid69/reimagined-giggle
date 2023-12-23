971fefa5a6fe19641faff588edeeb87ba8a15e1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:41:18 +0000

    cum conduct replace impressive share

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
