9b1306e4e019ee1fab1e2f4012831436c5d5965e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:42:16 +0000

    pattern active touch dare legal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_binary(n):
+        return bin(n)
+def is_palindrome(s):
+        return s == s[::-1]
