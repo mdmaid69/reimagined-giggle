830d6a9f8d4164004248e339c85a8c1b621487e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:24 +0000

    bunch steep road nonsense platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
