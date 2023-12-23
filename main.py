03d7712bb647fc4a3027f42a68cba1a1158fa652
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:14 +0000

    case space bet comefrom exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
