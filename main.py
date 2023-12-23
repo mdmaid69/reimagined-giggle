499a97946efc487ba28dc96a18cb71b403f9e2a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:51 +0000

    cause it workout reserve snow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
