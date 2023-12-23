2d482939fc665d61a388043b0de3abbd44ae9d3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:52:19 +0000

    favourite compare correct bye cruel

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-name = "Python"
-print("Hello,", name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
