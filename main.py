b0a1d161aca87ccf68cef7e339be163746ad0d9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:19 +0000

    obtain food lonely case market

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
