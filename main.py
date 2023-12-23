f524c96fdeccabe94c1f9dfdae0de25de5df9e28
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:27:12 +0000

    heart breakin simple usual worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
