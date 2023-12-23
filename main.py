51fac7bf3d842d2e5fa77fa1d9b049a8d5beaf62
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:39 +0000

    error Jj form walk cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
