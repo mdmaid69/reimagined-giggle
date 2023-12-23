d28475f234b5dd8e69b15e72211e56dbc3f68e85
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:34 +0000

    serve ask coat on over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
