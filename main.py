9593dfdde699b23e4326db3e0d6cebc4207b66d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:18:15 +0000

    part yesterday vote spoil return

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def create_user_list():
+        return collections.UserList()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
