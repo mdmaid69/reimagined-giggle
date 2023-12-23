b26be6eac9d40c320f7ca9c41ca01996b47dd305
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:54 +0000

    neat police rain conclusion such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
