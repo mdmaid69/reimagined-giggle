72ff34e9c93a81290eb001447454846316d501d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:26:21 +0000

    opposite straight explain regional detail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
