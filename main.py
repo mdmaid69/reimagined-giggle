919fef2251f6c06e1a30c275ca7af7338ecf5e5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:13 +0000

    warning through correct Ww shallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
