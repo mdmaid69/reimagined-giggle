ad9488e31c874b61a45217f6f8941cfbd4946b17
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:54:47 +0000

    comeup anyone snow into game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
