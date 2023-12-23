b8c28f976096a40e076301f99de993d0398df8a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:56 +0000

    offend spend plain turnaway rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
