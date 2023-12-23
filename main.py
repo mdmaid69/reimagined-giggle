9d93f7df211d37e4d08534cebbc26d4e16c2c8f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:02 +0000

    care file war let geton

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
