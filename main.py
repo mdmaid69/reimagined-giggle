f6fb58ff075e36772c9c95ff76d8b5118c45ff34
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:17 +0000

    repeat absolutely square stone fence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+for i in range(5):
+        print(i)
