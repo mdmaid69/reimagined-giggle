0121b081a14935d8306507d8b0d27546520b3a2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:21:58 +0000

    bear black disagree turnover your

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def is_even(n):
+        return n % 2 == 0
