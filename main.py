8e4f514a73c0ea8223267aa5f97c6255df5bcb38
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:17 +0000

    sure quality circumstance few field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def set_array_item(array, i, item):
+        array[i] = item
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
