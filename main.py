403e6536ed3710ca457b0f78b006a1ae97287d7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:46:34 +0000

    weather open steal pay line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import array
-def get_array_as_list(array):
-        return list(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def count_words(sentence):
+        return len(sentence.split())
