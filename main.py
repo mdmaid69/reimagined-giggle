757242cc6a62370d82d2a5c24cd4e48fae795202
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:16 +0000

    group lesson treatment pleased reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def square_number(x):
+        return x**2
