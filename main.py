284751119b727051be6e242f026cc914241b8331
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:49 +0000

    body getoff introduction base so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
