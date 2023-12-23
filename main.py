015f3fa43cb4d1c85785ca6a4b1248f3812f418d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:44 +0000

    within callout commit contrast behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
