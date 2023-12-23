6abfa712dc7b01f7324406f70fb0951689e2f6f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:41 +0000

    sting table blind of attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
