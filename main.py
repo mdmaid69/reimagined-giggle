fce74e2b2accf1d80e21b216639b100afdec4cc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:49:02 +0000

    fall mass stick deliberately neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
