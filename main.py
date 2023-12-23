e11ae011bdd36fa5f705eaccf783ed94dad91524
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:49 +0000

    further mystery course blame control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def square_number(x):
-        return x**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
