fd756ce8bdb4a5789b43e040c1321cce3adeffcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:08 +0000

    congratulations keepaway instant ask nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
