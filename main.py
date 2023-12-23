8d97adc5727c8514b9e45edaf634636924028c7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:32 +0000

    refuse vision limit sensible swing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
