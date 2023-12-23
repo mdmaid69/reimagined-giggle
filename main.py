dbd08874378acc91c3ab9fb3368c20ed07957430
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:23 +0000

    poison wander earn quality Pp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
