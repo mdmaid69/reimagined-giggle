18af15a61f8322fd61d15e1ba0cb802c4ffa2457
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:24 +0000

    extremely at meet gate translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
