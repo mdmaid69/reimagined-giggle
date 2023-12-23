1540c2193eb034600618668f0f98cfef5ea23c66
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:43 +0000

    fortunate equipment nerve small birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
