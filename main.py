648c7fed1a698b5c96d73fb65641a6c190a8ed2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:12 +0000

    declare giveaway name promise Bb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
