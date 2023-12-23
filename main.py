c67b8169034eb3c08976142a0c645b89b888eefc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:47 +0000

    taste breath murder callat class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
