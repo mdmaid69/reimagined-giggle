ad1b5a7994687c51d6567c3a3ea06339d56e82a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:49 +0000

    school deep second now right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
