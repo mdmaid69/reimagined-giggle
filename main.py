77e7acbaed4c14f25a73642168a9b6b17be9667e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:27 +0000

    against experiment direct study comeround

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
