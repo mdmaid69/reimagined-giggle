44735b7ec06c23748323e9a56ff7a1278ed1b1f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:20 +0000

    for leave enthusiastic disapprove treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
