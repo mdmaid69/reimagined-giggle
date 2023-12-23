a13b1985523336a367b55936565931b65d7f3767
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:45 +0000

    trade of with Jj wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
