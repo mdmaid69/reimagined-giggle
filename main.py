a1e4a54cc780f32f56beb16de9aa9a0b631c1a50
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:39 +0000

    Cc ours takeout bath punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
