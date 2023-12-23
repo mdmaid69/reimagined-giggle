42c2ce6dee777c8ee142c5e7f1d80a963185c2dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:10 +0000

    throw attitude oil cough comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+print(sum(range(10)))
