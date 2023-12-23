ba11f6ee611e0d61b63c66d93821828b0e19c1ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:27 +0000

    lock debt proper talkoutof description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
