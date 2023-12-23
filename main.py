a97980cd4d816e0592ed898182cad6a037709c40
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:26 +0000

    entry block comein hold our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
