97a86b8b5573d5b445e49faec7444f4fb391cbfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:53:26 +0000

    like joke payment me mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
