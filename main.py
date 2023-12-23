a1d46bb3582d8da7b0147c18cc3260697c332085
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:07 +0000

    bang may impressed walk usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_string():
+        return collections.UserString()
