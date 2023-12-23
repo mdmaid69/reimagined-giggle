544f90d4558692060e03726569c9a33ed0ba8f4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:16:27 +0000

    present art over spray taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
