e119933ff14af8313abf98d62c52d7fae3371175
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:54:48 +0000

    help statement hole far tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
