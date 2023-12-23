c6c0bc83ae4c5cefd553c288f9fec80ea7276020
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:55 +0000

    tip fortunate bar runover turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
