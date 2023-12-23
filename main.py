e66e9dcedb1ed61e5079cb2eef1c052cd5411995
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:15 +0000

    scared sink sing recommend arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
