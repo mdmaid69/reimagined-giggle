e3ee53c6e6fb9866590321ba3e3225a05796ec18
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:23 +0000

    straight goup responsibility personal worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
