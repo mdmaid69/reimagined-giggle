28bacee4046594c45e478138836c6a3bd58cb6e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:40 +0000

    soak same argument run shock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
