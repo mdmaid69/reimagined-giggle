b13ca60424a052478ad50207af6ec7ffada0ea29
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:23 +0000

    show mess invite table hit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
