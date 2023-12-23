595f6eb7c7c0e0bd7da20029bed263b7f5e2daf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:08:59 +0000

    recover announce choice level health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
