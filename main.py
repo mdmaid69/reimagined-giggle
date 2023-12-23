04a15eace7dcf1766fa1017a184d47139bb116b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:13:20 +0000

    pleasure crowded maximum achievement swimming

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
