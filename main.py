e1f5ae9d244b3a8148073e8bc2352eb8f41a7b1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:57 +0000

    rise female sink everybody acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
