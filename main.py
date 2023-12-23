bf17c49920d74170eed8c39bc0ee48b92c72adfc
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:26 +0000

    runaway insist fast married refer

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_perpetuity(payment, rate):
+        return payment / rate
