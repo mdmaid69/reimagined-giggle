e642076eb2e1cf2b3ce0032536eb378de22b6966
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:21 +0000

    of book opinion sort favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
