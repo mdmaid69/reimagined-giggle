6bbb2e88e20c3eaec800b6ce8acf9b8750094781
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:23 +0000

    Jj neat exercise catch story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
