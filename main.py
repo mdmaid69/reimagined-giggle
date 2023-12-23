c5b03640f5ef3b42992b65424d8a06028e7aa73c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:04:58 +0000

    score announce quit makeup number

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def remove_duplicates(lst):
+        return list(set(lst))
