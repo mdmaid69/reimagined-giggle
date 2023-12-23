81044116bcf6f24b53b0003864186b41e017ad58
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:58 +0000

    union immediately prevent major awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def create_queue():
+        return collections.deque()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
