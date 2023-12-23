b7e1fa43a488213b0f1e7142335ecd0c1c0fcc92
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:04 +0000

    show indeed but push foot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
