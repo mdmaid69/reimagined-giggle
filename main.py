f4dead41e64a1e4ede2c6645d2ec37032e0f683c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:28 +0000

    peace foreign pay immediately amount

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 def count_words(sentence):
         return len(sentence.split())
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
