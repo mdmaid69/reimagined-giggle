c2e8dad4f9ccc0c13346c02494f02cd50c88e8ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:19 +0000

    weak part favourite easy window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
