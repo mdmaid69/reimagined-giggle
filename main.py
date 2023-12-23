ba3841d6a1a5a57bfbbc4636d6f9e4b9f9c32c78
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:27:36 +0000

    off believe activity deliver may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
