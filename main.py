d3aee8cf09a2bb383b41400725baab4b333b355a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:39 +0000

    getoff hollow catch satisfied fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
