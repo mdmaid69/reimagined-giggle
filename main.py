d146d2674f4f39264ffa1ec2c0dfd53030e36dff
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:16 +0000

    whom classroom after contents seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def clear_array(array):
+        array *= 0
