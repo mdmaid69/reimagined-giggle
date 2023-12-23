ca355e88fe8f8130841efb4750f6fee9e9fb4ca1
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:19 +0000

    uncertain survive danger answer blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
