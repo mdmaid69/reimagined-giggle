3c92cd863f9c29de5a1a17eaa5164c07afe96961
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:43:35 +0000

    mess niggaacademy Gg tidy godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_list(array):
-        return list(array)
+import os
+print(os.getcwd())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
