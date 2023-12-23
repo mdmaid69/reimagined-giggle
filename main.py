9767b14d4a2867340ca3fcafd3a3bebd5d9af0d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:51:42 +0000

    succeed past deliver Yy getback

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
