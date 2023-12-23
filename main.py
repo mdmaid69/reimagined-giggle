53bff2096c0ffc44ce85306399304eda6ffa137f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:49:42 +0000

    furniture keepout to connection sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def subtract_numbers(x, y):
+        return x - y
