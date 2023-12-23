08d3aa0fd50f494ade8f5ea961b31a74ce8b2706
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:16 +0000

    sting pure sting trust always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
