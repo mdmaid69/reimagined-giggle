ff1f84b15a8e6551dde98b8f801a6400495b1f4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:15 +0000

    shall learn weak that Gg

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
