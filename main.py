7bc3c243acf3a5d50ba6c23c355ea54ea396dc7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:37:23 +0000

    capital takeover lesson number hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
