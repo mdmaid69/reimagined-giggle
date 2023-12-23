d6e1f77246fe81c641688e9be079d03199cd0031
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:29 +0000

    better be light admire negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
