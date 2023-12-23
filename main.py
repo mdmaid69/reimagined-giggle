fa1f37928d89fe3d64a994b86c8575bb5c8aa810
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:57 +0000

    before surprised again treat hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
