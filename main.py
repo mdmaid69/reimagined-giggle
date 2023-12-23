5cc66c9b4c4fa2a20a71d1a8226ba25b57a0a084
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:52 +0000

    pool autumn greeting hold legal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
