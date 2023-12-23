3b8ee3c41cf6919433ed879c35cb6938e0e17640
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:01 +0000

    class well wild any good

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_list(array):
+        return list(array)
