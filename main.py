db10e12c5ba32acd1f7c8a5338306e5652377d9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:24 +0000

    business well same pay around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
