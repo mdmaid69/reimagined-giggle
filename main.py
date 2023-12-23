2f00ce012ba8c8666d3635a4f71fafeaf849b029
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:56:21 +0000

    mind shut perform friend sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
