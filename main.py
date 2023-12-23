434d1e3cab02614de62e602abc409e95794e264b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:24 +0000

    loose high much second fire

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
