c642abfbcebda6dcc6009e9d04373f56861eea7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:31:19 +0000

    family force also turn pin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def convert_to_octal(n):
-        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
