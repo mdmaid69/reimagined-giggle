b0a2acbb72b5e820136c8d2e19a00d5580ec4643
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:20 +0000

    reason stand relax straight region

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
