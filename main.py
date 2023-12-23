aaab74637bc0aa68911da04ef9e505eb72770bef
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:30 +0000

    avoid hello swallow low or

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
