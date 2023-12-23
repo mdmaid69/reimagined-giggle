73f14835df2c9f67c8786b7254b7c75c35c20408
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:14 +0000

    future worth hand tight cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
