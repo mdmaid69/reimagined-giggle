0bb2a992719693213c8634aa20ecd14c1920580a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:53 +0000

    bus trap everything whistle exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
