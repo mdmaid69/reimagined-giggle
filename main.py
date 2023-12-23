f22a735411060216df4ceff19a47b5796966e6ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:48 +0000

    behind takeout could obtain method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
