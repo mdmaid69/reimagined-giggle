7168766070686d1302b6fd3a144178510d68c026
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:51:23 +0000

    still port later dare sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
