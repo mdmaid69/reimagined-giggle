93de399e2b6f8cbb6717aba3ebadc39c0e71fdce
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:45 +0000

    print usual hour tour discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
