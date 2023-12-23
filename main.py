52aa0ab4c6a67dd994fbd4df336e414314a6395d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:40:52 +0000

    pretty comeup police trade apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
