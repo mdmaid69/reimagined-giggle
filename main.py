bfca51079d2bbf1679d114e9670791b6b27d59a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:44:47 +0000

    pinch apologizeapologise goup shout pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
