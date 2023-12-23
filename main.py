2c1e5b2c78376318f770f3c09079a4836321d92d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:26:25 +0000

    church repeat easy production chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
