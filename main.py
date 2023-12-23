7d59037911d93e7653b32783402b078caedbe8ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:31 +0000

    sort unfair freedom keepaway anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
