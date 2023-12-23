2d83dc4aeb3d6705d022bb276927dde06c0044a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:28:01 +0000

    present sorry sign summer holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def square_number(x):
-        return x**2
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
