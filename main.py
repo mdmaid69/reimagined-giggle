aede655a793a7af39b79540577d6d883cc93bd73
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:22 +0000

    performance drive contact this pride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
