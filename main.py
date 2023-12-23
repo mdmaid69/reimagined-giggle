d9ca807d34e5414b547cc6cdf3673f54557dd68d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:50 +0000

    treat direct lie final rush

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+text = "Hello, world!"
+print("Words:", len(text.split()))
