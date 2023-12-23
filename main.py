8597eeaa6bf7ce2b2aeaa4329558195fd87a1f67
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:35 +0000

    claim last national cruelty extremely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
