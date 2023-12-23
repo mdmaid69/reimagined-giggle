c488141725d58be1a44dfef83c535569cb461915
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:51 +0000

    bright tap ask rank such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def subtract_numbers(x, y):
+        return x - y
