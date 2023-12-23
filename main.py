5c2be7cb975a2b421e9061444d445fec99bd28a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:46:43 +0000

    lookaround either national exercise comfort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
