b6bebe77f9ceb285f3e29df95bd89cb40405d664
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:09 +0000

    squash beyond concerning man command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
