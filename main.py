b96eddb84c18032c039c0f0b0e3b43e42495b6c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:11 +0000

    different home warning through blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import sys
+  def get_python_version():
+        return sys.version
