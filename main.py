3d6855e4e0156142b24419b23702d52188f1ac0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:19:21 +0000

    certain surprised union bread plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
