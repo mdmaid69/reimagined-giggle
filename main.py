63907f8970043000b2097b50ffcd226291fccf32
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:53:26 +0000

    support whole destroy exit stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
