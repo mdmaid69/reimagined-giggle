964f758411acadba198798d039cc44ddf073ecc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:37 +0000

    sad among fail mark bend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
