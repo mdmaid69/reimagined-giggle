6a13cdb4206953832e869f04dfdf63eec57466ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:53 +0000

    have owe strict listenfor frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
