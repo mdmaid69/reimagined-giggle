7c90b90fbbd0f6aa5091c79105e725969880d005
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:55:27 +0000

    light dangerous brain half tend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
