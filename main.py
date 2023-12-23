0c04dafe6a9e57ba75e54a7e90089b0849518ad5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:10:35 +0000

    rush position Ss passenger prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
