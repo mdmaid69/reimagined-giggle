5c6520e20d43a977c2a3b0ea7061096d8acb072e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:39 +0000

    woman word swallow nurse left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
