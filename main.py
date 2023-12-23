3f6f12fb7e03a3e635e662df8194500c35479281
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:07 +0000

    unit turnon drawing him description

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
