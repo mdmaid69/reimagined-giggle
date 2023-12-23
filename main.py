7ad39c9e199c0f741f35ae19d55b3176ec7b24c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:49:00 +0000

    invite break watch ball uncomfortable

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
