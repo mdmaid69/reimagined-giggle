d9b5652167c8e8f11f617e2fcb3c29078ceee957
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:05 +0000

    mess about search motion cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
