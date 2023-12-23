5632fa6de767147086db3ad36e8bfa2d8c600e1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:07 +0000

    check permission market me picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
