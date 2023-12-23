0c235d5834596544990e2fc3a0eef2feb1f4c48e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:18 +0000

    by from form lie teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
