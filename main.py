2e96a5233667720e63cc2805a3a46aa8a59e946f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:05 +0000

    separate lead ground frame convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
