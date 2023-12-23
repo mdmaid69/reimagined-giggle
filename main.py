a4636a262304c2f8e5eab4826e7501f90c72bb48
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:49 +0000

    cold tongue store else star

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
