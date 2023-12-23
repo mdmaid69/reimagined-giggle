e783deee3ce18e6c01ec1e3c5c7dbd88b938ae02
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:32 +0000

    pair over beautiful store flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
