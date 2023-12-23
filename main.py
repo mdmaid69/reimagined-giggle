ec4220edbcce1f6c622ed50be4456ae89f99220f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:44 +0000

    either young harm truth dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
-        return x + y
+  def get_base_name(path):
+        return os.path.basename(path)
