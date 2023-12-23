d850c58452a73aeb1fe4fadf3145a9d29762c8c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:44 +0000

    late neat extra pain club

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
