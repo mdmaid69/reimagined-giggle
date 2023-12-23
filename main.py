5c7fdaa29c1ee51d7c73c2eaa562ed719a50115f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:49:43 +0000

    cross nice raw law dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-print(math.pi)
+import sys
+print(sys.version)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
