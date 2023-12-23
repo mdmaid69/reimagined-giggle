7883a62a7dc7cab0d8f59f2dc50d3d7805661380
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:36 +0000

    unite build office thirsty satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
