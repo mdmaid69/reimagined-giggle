c742f2324f83d5d6cac52e2e308bb8e866f2e965
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:48 +0000

    relax minor print occasional Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
