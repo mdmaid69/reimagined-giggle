c7d5fb1cf71477615938decf9f1dc214f8315937
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:51 +0000

    important silent relax needle the

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
