decd97d7a9cd978a1fa364e0908893ba0d431170
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:04 +0000

    positive surprising strange path great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def multiply_numbers(x, y):
+        return x * y
