ee9149a380a8de8d341e1a0af908be383018f655
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:39:48 +0000

    several them thumb turn tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
