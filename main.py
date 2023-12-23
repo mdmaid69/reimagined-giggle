13e7a6c6c65fa3c63a68db3c1572eb25f9d4e6d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:44:53 +0000

    closed until aim base dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
