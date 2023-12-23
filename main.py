bf340e02de4a24527448e9ed42b5e613ee0e00ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:39 +0000

    pollution health fold about help

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
