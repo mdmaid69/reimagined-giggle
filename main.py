9dd37a8b3c4c5f9c9b1d63809fb4bfa98a87d152
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:08 +0000

    welcome air experiment team attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def find_max(numbers):
-        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
