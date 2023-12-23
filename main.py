9d804d983e0e72167a43a30106f01f6e408fbcbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:57 +0000

    impression lose game holiday whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
