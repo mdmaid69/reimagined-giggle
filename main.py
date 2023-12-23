cddf1677b36a4c94a64c0da598e2b35a84b5aaa1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:49 +0000

    Cc interview exactly hang maybe

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+print([x**2 for x in range(10)])
