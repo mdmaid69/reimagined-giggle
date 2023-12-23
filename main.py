bf09d68eb2d63bdaf7a93c71cdd7483d6bb7ace4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:55:42 +0000

    short spoil sure everyone around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
