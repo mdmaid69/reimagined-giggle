9aa94fb2922de8f80bce01ccedbbb16511726dbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:51:53 +0000

    clue clearly tradition yes clear

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
