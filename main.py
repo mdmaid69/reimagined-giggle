fbd1f6a3fdaf47f6e7c354daea343fe43b07a0cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:23:34 +0000

    their scrap back now runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def convert_to_octal(n):
+        return oct(n)
