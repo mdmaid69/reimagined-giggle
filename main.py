be339d9853e416512135816445d3d883b8f871cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:13 +0000

    it stop direct claim keepup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-print(math.pi)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
