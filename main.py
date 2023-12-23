6f8c248843a475936894d9fc2ec550eab084a2d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:08 +0000

    rub computer aim tidy minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
