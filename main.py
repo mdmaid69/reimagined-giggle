13d27a8467f2075033d2b663c37d855c7f7ebb9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:10 +0000

    pair emphasis smell about frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
