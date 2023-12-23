42b992074db85e03ccce0372f7f63388847884a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:26:31 +0000

    squash step point weigh off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
