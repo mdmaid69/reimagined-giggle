77bd08fd0de3ffea27b86380cc9705928c42e134
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:46:55 +0000

    back welcome steal proud fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
