52ee9dc2d1152a1bd82f10eca25143f7202ffa2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:27:31 +0000

    through just buy birth sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
