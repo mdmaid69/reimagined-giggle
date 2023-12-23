840381e659ec8f712adb8ca1e3e48f0086b788bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:31:29 +0000

    advice slight getup suckme train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
