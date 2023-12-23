ece0f6fc80924217eb86e1d0fd86f1ab81aa97e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:50 +0000

    sudden brain open mistake sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
