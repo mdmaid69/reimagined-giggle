f2f8ff52b84cc932ee84a3a809d7e2efd77009b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:40 +0000

    attract quickly clue rarely ordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
