22cc3f80facb0e43c3ab8aee326d5c449c47ea05
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:42 +0000

    manner yours shine health appear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
