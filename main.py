18f9901496590e4f39de75d800613e48ad8d668e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:43 +0000

    stay careless total their interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
