41af6c7f9bc7fe7fb8e537103aaae840e460547a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:19:15 +0000

    nice deep free different concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_array(array):
+        array.reverse()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
