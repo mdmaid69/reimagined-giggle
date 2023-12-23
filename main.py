09f88f3e11255a34009cbb5a9c59786252afb347
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:53:10 +0000

    ill car tap getout careless

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
