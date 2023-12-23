5e4cd9cdcba329aeed757d4e9f47ad64ce4c8d14
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:08:47 +0000

    strong father autumn declare partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
