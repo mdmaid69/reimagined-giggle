b6736fcdfc80f950bfe3d1c230eee7832473380e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:30 +0000

    interrupt disappointed 571enough wet see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
