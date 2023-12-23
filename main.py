1a0f0c5c02c1cd3e6e24146af35e0ea2cc64c2cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:09:46 +0000

    meaning loose just exchange comeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
