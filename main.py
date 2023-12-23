568819ea68c646354b1309445acfa81f14abf1f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:48:24 +0000

    lookafter it bother healthy silent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import sys
+print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
