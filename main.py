1df772d13a0d1fe46527b3ae70893441051e6fce
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:53 +0000

    fasten concerning hurry manner film

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import time
-def get_current_time():
-        return time.ctime()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
