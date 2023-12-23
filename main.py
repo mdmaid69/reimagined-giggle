4cdad2d79a317b6de931aca0f9a8e03055f26a93
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:38:12 +0000

    condition listen everywhere awake getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
