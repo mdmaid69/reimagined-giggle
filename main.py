b4f012a01fdae396bf4441b82fafde9bf3d516a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:31 +0000

    invitation increase smooth force man

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
