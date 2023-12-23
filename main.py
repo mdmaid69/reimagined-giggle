e4de820304ddc6718203586257f50506df85dc78
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:13 +0000

    lead blame arm cost store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_power(work, time):
+        return work / time
