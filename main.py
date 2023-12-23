7c60c83031600e07b08e9cff5ac05a7b3a2a1b1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:47:44 +0000

    polite school switch keepoff taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def remove_duplicates(lst):
-        return list(set(lst))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
