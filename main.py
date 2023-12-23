42b6154d5f1639e6e2d19fe6a73f152c2d309aa1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:53 +0000

    neat example fence key word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
