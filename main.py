fdb6140d45573159559c9ee1c42ba893fb53d2f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:57:14 +0000

    loan brush themselves physical apply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
