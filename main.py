b35c07a877da9ac9ee122f59b07822532a3d25fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:19:47 +0000

    house intend shine past gap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def add_numbers(x, y):
+        return x + y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
