08c1b41c572bdc78d7d77e1df4c3ea057a8f72f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:50:30 +0000

    report copy either bar soak

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+print(math.pi)
+def is_odd(n):
+        return n % 2 != 0
