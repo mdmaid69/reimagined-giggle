85cea4123d1e116997bdf5350b8c9e801ddeb730
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:57 +0000

    exist unable burn vision plant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
