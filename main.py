964e3daba77bf51117e3e2a647ab00e14ee87ac4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:15 +0000

    estimate command woman think arm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
