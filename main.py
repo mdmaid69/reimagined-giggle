5a027490d05b812b7e544c53c44d5ca3cf127f37
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:08:21 +0000

    sky operation somehow crash over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
