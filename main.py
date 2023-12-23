bceead6fed9325692ee7e989f85aa00e2964f392
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:30 +0000

    breakfast equal avoid progress myself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+x = 10
+y = 20
+print("Sum:", x + y)
