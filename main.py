289fe310fa9a41532c1c42352d7a70c82b61f292
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:00:37 +0000

    recent responsible of solve switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def count_elements(lst):
+        return len(lst)
