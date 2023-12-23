7a85db211569dc271003a57a7f42f877197ad774
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:04 +0000

    century minority pain able everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
