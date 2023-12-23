b1dd66d4876cdf8d3c6c05b9fcea88be08dd9a43
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:19 +0000

    rich hurry physical thing beautiful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
