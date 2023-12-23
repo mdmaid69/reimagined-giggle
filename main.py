f57149026e1d858ed61d745b333610e61addc866
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:45 +0000

    568energy recommend deep several scared

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
