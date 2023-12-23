5e04a08d0530652a1e7d538305290ce6db7139f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:47:39 +0000

    keepaway underneath off certainly nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import json
+print(json.dumps({"name": "John", "age": 30}))
