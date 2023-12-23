4f379dedddd41aba4d05c6e49322e26a69e087e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:31 +0000

    trap affect ocean watch deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-print(datetime.datetime.now())
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
