dc12424f1ad22644ba42648c43ee9b6321ed4213
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:30 +0000

    thing interruption 566end whatever spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def remove_duplicates(lst):
+        return list(set(lst))
