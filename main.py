820bc780e2ef343596ac17a56f23972f0929f323
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:44 +0000

    worth effort maintain left crash

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+print("Hello, world!")
