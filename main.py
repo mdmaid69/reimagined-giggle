28da2dee9e9a5e60daaafce9b7ee3c935538269b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:07 +0000

    keepfrom Mm error Mm brief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
