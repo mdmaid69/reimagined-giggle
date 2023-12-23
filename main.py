3a7317f0071099052995fb145ada4023ea69728a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:02:02 +0000

    ground sleep call estimate afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
