1c912264950e3237b9a8b3c246a5919b5c3d6815
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:53 +0000

    whisper fire squash deserve treat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
