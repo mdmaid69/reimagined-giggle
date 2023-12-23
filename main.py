947d2f3ef1511b93786d7855a98a82dd07b5d3e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:08:46 +0000

    measure claim hers well creature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
