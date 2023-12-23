81eda03c2316fed5bdf0e89222dd89fcbf4427c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:16:58 +0000

    under bread nowhere street skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
