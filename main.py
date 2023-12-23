e75490c86ad2bc35e95eee9d3250d91c6eeaac94
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:29 +0000

    storm please refuse keep moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
