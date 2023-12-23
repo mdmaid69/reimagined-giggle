e06adae53dbca8cdb5a44735aba1ef882aa9fcf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:27 +0000

    part rare backwards inform actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
