0bf88ea3e98634550f077a569294fd7ef7bf0656
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:06 +0000

    meeting row nurse original favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
