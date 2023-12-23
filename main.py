04fee8d701edb0e37f3dd0e0d48e6e6303cd5f73
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:04:46 +0000

    patience disturb length official responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
