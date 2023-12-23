58a6473846f7347b8fe99e040b462d96cd44459c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:31 +0000

    busy great frightened full bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(math.pi)
