4182709dbfe9f7e88465ef887913121453bdcdfc
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:50:57 +0000

    disappear cook long speed just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
