48ae6d11135166af239d193ebe6ae92e384cea4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:13 +0000

    temper whole show afford sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
