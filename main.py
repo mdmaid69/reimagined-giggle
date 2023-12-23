abe556f0e2d5370b95824014513de111c7a46eab
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:34 +0000

    somebody country serve issue sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
