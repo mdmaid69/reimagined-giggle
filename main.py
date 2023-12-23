255606422be43b3d1eb8f1386c5abfcbd1ec9dae
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:03 +0000

    lucky Zz number closely mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_list_from_array(array):
+        return array.tolist()
