73abe994e2a9d6ea4459b5c5fc0c151f4cb3e09c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:31:10 +0000

    difference expensive valuable payment turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
