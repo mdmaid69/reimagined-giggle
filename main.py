4470135cc976074255a6b8a69d9c8ec830616597
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:39:29 +0000

    somebody warn hide platform pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import datetime
-def get_current_date():
-        return datetime.date.today()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print(datetime.datetime.now())
