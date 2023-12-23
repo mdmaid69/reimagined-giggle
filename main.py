cdbdf4d7ea97a97ad79e8f917d4ccd26272e6cc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:47 +0000

    answer pleasant event clearly fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
