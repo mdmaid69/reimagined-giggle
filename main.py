cccb487a5da44c15a0f0c375aeb4e194439fb9cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:50 +0000

    large handle getaway wise substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
