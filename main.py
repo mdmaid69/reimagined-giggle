0fd88501c00685e01d20384d985f6470c49d69ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:42 +0000

    defeat slowly state little 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+print(os.getcwd())
+print([x**2 for x in range(10)])
