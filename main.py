9232dd0050eae643a8706e76f87d62bb35eb81b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:30 +0000

    convince distance them sound murder

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
