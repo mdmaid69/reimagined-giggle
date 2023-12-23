6b1636b23997cdaa90e282413edf1e840071f500
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:56 +0000

    past slightly no hurry form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
