7ea5a1a2cca46fedd9e977190c2c75aab4bc4d1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:44 +0000

    realize stay opportunity reject passage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
