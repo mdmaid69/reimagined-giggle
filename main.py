fac96942dbb585bac7fb47a61d1acdfa83c0abb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:39 +0000

    moral bend error moon help

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
