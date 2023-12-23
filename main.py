c43cb7528ef7eb26516e0353e95914d169ad1db7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:58:31 +0000

    blame stop achieve between flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
