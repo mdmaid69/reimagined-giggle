fafc391f6b14f558cc63524355260cf160b6be15
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:15:40 +0000

    basic home about punish cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
