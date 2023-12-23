a49b615120c997ae5cd1cf799c83f152daa711ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:00:07 +0000

    moment bag information terrible afford

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
