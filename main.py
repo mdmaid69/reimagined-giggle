aeef132aa1d889fc156b119601cd1823f7f413a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:32:47 +0000

    568energy burn rely dead fire

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
