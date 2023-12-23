541d41df8a9659ce450d873d2a951877de0414c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:35 +0000

    brief thing mess stare reply

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def subtract_numbers(x, y):
+        return x - y
