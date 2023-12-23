ee6f660d44758ea4eca22041daffe54cc73b469a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:02:01 +0000

    snow translate sting conduct ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
