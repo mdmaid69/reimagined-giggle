c7e0b951b5053156d823f63abc5f9279aeed39cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:45 +0000

    only interrupt throw person use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
