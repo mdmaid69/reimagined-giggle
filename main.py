3ffd3ecf1c458305fc55b5219f007a6b69d50ab0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:20 +0000

    admire deserve nurse fat month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
