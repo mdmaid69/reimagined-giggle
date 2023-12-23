a3e9e054a6bec4295055af954b11c2a54cb23220
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:28 +0000

    age any minor speed relax

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_int(array):
+        return int(array[0])
