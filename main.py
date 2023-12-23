4fed0c2917095e8b4a9290abc679bb00135ca774
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:42 +0000

    prepare smile value violence by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
