c47d44ef0657fc47e942b2b5b3b0709918538c3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:07 +0000

    mix influence temperature bend turnover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
