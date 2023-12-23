2f2814fe3a5fb590c2c461d22dcc66fc7b6c3a88
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:44:22 +0000

    pay experiment of spite slightly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
