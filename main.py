3c475fc5e8dae598f5235275c794c5baf006cb95
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:42 +0000

    occur prove regret fat dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
