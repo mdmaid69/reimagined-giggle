a8c815df317872f8f08c4d1ddd08ea10979d0692
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:38 +0000

    table he off treat climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
