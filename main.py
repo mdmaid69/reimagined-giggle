7064fc7d2ab0295d1a8da13ef949900c5e8a3d6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:20 +0000

    our why cruelty divide in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def remove_duplicates(lst):
+        return list(set(lst))
