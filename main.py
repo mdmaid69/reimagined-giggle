5a8cd1df766bc2fd3634b2cad86a3f22fbb4503c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:04 +0000

    comeback complete little closed brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
