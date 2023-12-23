32ee7365d43bb6eb30c338654c639f02f9349ca3
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:20 +0000

    busy summer work again contrast

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+print("Hello, world!")
