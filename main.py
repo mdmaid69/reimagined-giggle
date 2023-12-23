ae555ee338a60cc51da5663978e85d8de0074952
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:04 +0000

    final reason typical dangerous Mm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def cube_number(x):
-        return x**3
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
