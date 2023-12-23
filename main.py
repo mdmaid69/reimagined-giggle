bcb2f84585b8af121da7661ad7203c87ea14e7e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:07:29 +0000

    upset pay amount fortunate floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
