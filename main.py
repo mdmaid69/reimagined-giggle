e075a50bbe41d3b068a6c739e2ed4bf400d84e18
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:47 +0000

    bringback real border later plain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
