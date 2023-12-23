558abaed96857ccd58bde2db3358894a7c277342
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:57 +0000

    beneath list prefer lock basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_square_root(x):
+        return math.sqrt(x)
