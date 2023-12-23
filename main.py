0625b9076f18e9cb40473db808c1279209a8b41a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:54:48 +0000

    rise raw tend everything geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
