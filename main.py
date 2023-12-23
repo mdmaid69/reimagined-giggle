f58fd9db61a8c371a50e075aafda5fc114b2205f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:37:05 +0000

    long send mysterious minute rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
