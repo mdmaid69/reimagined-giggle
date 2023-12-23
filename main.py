9b72b3866bfe69b8dbbba3fe3cd9031e5a6fd116
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:59 +0000

    achievement large depend steal illness

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
