ab3f861a6abdfb05df6c69211744409ccc1fa557
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:08:24 +0000

    law floor unkind score suit

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
