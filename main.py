ade400eb2c697028312da96a582ced7b86c3a008
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:48 +0000

    seem maintain imagination after stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
