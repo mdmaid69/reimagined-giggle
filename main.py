995114b56eb286d97771e394cd4ee59f9e5b9fdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:06 +0000

    suit peaceful estimate game deliberately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
