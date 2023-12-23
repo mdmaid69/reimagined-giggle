d3cbdb2deeece3663c538c1ce6e63abdea914a80
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:29:30 +0000

    similar acceptable ought ever callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
