dc9594b09f9dc32c4cb4ee0e528a1774c40cecb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:06 +0000

    further Yy suggest thought reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
