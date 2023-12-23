2f92b6382d7da4d620ae03f88ceb3960d73e0d61
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:15 +0000

    search more scream not high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
