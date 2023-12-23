3862fe9b58507ad0b0867eab8a3067713f4c0a18
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:04:05 +0000

    somehow hang short own bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_min(lst):
+        return min(lst)
