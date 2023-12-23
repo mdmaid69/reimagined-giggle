049ef50f4b799e15dd7dd7d128bae3e2fad4abb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:00:27 +0000

    eye offence finger breakfast dress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
