96ea6b4c187ee7e3219b8c90e90a247f5a0df2ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:54 +0000

    person godown fit present principle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-def convert_to_binary(n):
-        return bin(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
