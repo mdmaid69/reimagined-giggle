19e51b2f127e5cb70032379d336b47fab74ef292
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:43 +0000

    recommend Ll professional like neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
