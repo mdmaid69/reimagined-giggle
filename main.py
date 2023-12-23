87ddb7adc672a53929ae8d6484f4d6911129ca18
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:40:20 +0000

    star sore genuine bunch correct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
