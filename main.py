3d8163e85f42d8bda9ef4a35cea683812806f846
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:27:42 +0000

    force several pleased well manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
