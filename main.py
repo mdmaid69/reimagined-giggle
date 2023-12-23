e28fc3ce408663f2d8b11c05ff28c85f34e880df
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:25:49 +0000

    steep touch still regularly creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
