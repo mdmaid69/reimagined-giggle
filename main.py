ca978663778254186be25716b144687970d18520
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:31:09 +0000

    respect basis fun scrap uniform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
