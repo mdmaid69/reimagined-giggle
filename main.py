255c0546ec983429fcc7ecb3e11c979002bf5093
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:47 +0000

    sure control shade cope under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
