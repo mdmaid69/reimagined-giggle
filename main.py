2c120c9c561a2062f6dc1ddfbf35565ff8796d1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:41 +0000

    getout water personal hollow cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
