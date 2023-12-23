4461edb3d5505b0ff8977fbdb8ea622b086c95ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:25 +0000

    pen desperate reply unable slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
