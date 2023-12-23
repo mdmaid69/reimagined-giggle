8bcfe67d9a256edd411872b8237bcca582df721f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:15 +0000

    fresh bread flat table better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
