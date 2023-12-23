8ec9d5f69bdffe26ecf4da775f2c9b7c64b4b815
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:26 +0000

    payment danger protest introduction pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
