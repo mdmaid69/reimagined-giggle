4ca2fc555aaba173460c2445503faa3fa26918f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:18 +0000

    spoil hard matter worried shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
