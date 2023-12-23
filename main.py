40c0a5825f04bbf651083d920726a65c2d4172db
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:50 +0000

    treat move scared shop impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
