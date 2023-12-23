dce0e4aec51c9c3ed8c8fcf8f832d0f06e573bf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:46:55 +0000

    comment boring serious agree return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
