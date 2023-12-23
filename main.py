5d65bd1f178107520db89446329772f64cb79fc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:25 +0000

    muscle explanation chance pupil pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
