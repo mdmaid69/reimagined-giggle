6629cf665b1d7526e37e0151f180d7054bb2703e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:27 +0000

    wide bad dance exactly important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+def exit_program():
+        sys.exit()
