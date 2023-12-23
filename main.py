f483ccc94069603ab5be2ff637345748e0a221bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:21 +0000

    skirt dinner apply callat remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
