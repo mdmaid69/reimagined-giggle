acfe342614b3110c8c12f2fbd728a8545e486164
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:04 +0000

    learn round play cheque instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
