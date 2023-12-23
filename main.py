24d58ca9dc166efea09f04298e6963bc065f63c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:29:45 +0000

    cut exaggerate open pronounce tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import re
 def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
