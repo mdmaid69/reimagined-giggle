ef11e6f62ceaf89867d5108db4a755ed7a32ea1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:01 +0000

    away force breakin so without

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
