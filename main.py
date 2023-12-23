302d797fbf78b33af1de455dc2819ea287a4e051
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:00 +0000

    solve takeaway beside breakup polite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
