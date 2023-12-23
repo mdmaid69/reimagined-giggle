881bdae539319eb76642f83195002b41ee788c0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:59 +0000

    off develop guilty hers tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
