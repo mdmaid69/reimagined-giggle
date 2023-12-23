4519798b798c34e965f84112a13ae0e44bf609ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:15 +0000

    roll trouble unfortunate standard frame

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
