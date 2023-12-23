2d7af273cfb9ae71cd0f609bc5e0939cd6b4bc98
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:25:45 +0000

    succeed contain appear spend sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_time_since_epoch():
+        return time.time()
+print(sum(range(10)))
