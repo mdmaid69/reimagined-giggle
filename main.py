6903aa76d605c4b9f36f6d86a4f70973ee95cb22
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:49 +0000

    turninto anywayanyhow grow broken geton

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import numpy as np
-print(np.array([1, 2, 3]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def append_to_array(array, item):
+        array.append(item)
