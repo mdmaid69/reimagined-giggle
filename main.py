f0a97e1ac70ea776a1fc61f94ab652aba36ff963
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:33 +0000

    flight free hurry apologizeapologise know

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import collections
-def create_counter():
-        return collections.Counter()
+def count_elements(iterable):
+        return collections.Counter(iterable)
