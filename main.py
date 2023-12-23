73a700dcf5a7f745de04f79b57c33578d6bceb43
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:25:02 +0000

    tight embarrassing smooth circumstance tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
