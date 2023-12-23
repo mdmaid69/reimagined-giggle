605b827c0853266b746a461dff87d7d8c2fd001b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:04 +0000

    emphasis better once science include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
