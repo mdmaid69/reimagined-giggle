71a397db30577c38c20361d27cfca94128c205c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:24 +0000

    mess phone arrival trade light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
