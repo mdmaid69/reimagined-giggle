4732c09a298462c92c56052c1e39cbb6eaeffad0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:46 +0000

    birth forget answer Pp national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
