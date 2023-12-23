9e3c59bf2af7e40086cd99205ede0bebd3991510
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:04 +0000

    make guard behaviour pretty flood

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def clear_array(array):
+        array *= 0
