53cf5c88b9d610d9dc805dda47d993f3a46fd480
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:39 +0000

    possibly remove exist example apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def sort_list(lst):
+        return sorted(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
