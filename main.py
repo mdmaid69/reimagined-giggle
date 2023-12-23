d83a1cbf60d99735d04797e61661a8bfa42729d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:19:09 +0000

    cross adjective consult active up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import time
+def get_current_time():
+        return time.ctime()
