5c6f2cc68bca684de6fb3fb86acb216a5dec9f8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:54 +0000

    slide able sport raw luck

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
