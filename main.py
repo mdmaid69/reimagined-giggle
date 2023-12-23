b86551e7ab9f7ecd34f1ab7ddbda6ee37c45bb35
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:49 +0000

    offer better inside chin office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
