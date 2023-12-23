8c8ac209370237bf596d4036497bb7b7e713725e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:20 +0000

    break sentence care as 571enough

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_length(array):
+        return len(array)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
