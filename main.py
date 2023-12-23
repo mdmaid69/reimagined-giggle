7139608cf27eeb8bd25a1bb3b6bebef81ad3b732
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:34 +0000

    thought twist undo earth free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
