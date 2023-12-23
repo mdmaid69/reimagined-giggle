bf52675ee915be1895dd30d6b527b147b10b1ad9
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:19 +0000

    dry sudden duty opinion steam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
