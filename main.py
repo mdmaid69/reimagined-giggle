6680363516977536cdc86d083e06f3678f0b6aa1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:46 +0000

    definite 573entertainment journey battery kind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
