97438c885ebd4440aced88a8b9ab59e6247f3df5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:06:37 +0000

    comein precise important truth himself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
