0fd39e84521e280077bfda0c2e1fda1e36a6f741
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:58 +0000

    suggest expression weigh innocent matter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def square_number(x):
-        return x**2
+print(math.pi)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
