86e84973e1fce4e41e88d6ba29f3f947034d0213
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:01 +0000

    cruelty offer organize decision possibility

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
