3c115ef70ea7e256d2d8d5d12677e9ad0d8aceae
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:46 +0000

    service shock this bringup relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
