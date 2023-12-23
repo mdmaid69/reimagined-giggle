8817f448ac998fb80f00783e14a18c6224ab5559
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:13 +0000

    rush detail term another declaration

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
