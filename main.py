1e0aece174db64fa2c9f7cebdbf5bb70536331ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:50 +0000

    difficulty performance Ww discover during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_min(lst):
+        return min(lst)
