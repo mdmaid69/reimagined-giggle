d032b985cd1e5322a77e0ac174bfde362c2b22d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:33 +0000

    design transparent slowly to relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
