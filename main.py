f113c8f6361bfc6ad60d4c8831069fe01e9558ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:52:53 +0000

    anywhere war evidence debt female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
