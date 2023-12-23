0a862328a9a0abf9711d349f57a6c49723eb671e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:36 +0000

    night month thin than through

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
