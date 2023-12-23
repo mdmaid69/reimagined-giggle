670c6ab8d994d24dfcc33d57e793df4efaf58cef
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:31 +0000

    strict yesterday march support either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def convert_to_hex(n):
+        return hex(n)
