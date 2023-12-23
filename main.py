5be4b8084f4264dae200ef3cf8426b6d9e6333e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:52 +0000

    care quarter breakin plan what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
