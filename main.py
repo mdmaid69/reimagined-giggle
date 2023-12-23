3089199181bfb08d56ed004b804ab52da2a0f70f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:17 +0000

    line float attractive blue wish

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_repr(array):
+        return repr(array)
