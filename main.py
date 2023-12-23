f485f928a2c0852daee937a39b298d484e2d0a2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:40 +0000

    exam would divide store reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
