a6f0dea49a797944aeddfe96637f2e6fa177eea0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:55:52 +0000

    matter persuade news unable crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
