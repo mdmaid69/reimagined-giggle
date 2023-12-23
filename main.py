d49ff0a962b339db2dd3f332e22f9cf21df326a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:08 +0000

    surprise tune bored ride if

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
