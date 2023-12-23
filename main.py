002a85a349a04ce67710f84e9738067ae5bc3b45
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:27 +0000

    pleasant Mm getout harm knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_date():
+        return datetime.date.today()
