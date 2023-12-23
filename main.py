6ad0129a5204f92526777fcc90f3558530df8721
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:22 +0000

    thought care action alone claim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
