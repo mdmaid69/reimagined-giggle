980079f30e699665dadbe0f73d4c163a0099168c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:58 +0000

    reliable track inform bringforward method

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+print(math.pi)
