5d80bcbf55bd5db53ae3d5c0f1cb3fe0cf21f232
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:05 +0000

    fix possession wind wing criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+print(math.pi)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
