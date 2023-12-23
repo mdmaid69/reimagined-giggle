fbe3a5718ba8390acfaff3c8153e10ed774b5265
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:41:27 +0000

    any down sink mention heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
