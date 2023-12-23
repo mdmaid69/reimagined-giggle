f974d6a987835e3c97a541b0f363b8c2ba145493
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:16 +0000

    drive store sister act ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
