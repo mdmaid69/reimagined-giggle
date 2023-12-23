5b2bee5eeb4c3bce0e0f779fd3789dbd9a23b347
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:30:12 +0000

    herself block develop press it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
