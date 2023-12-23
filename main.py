e7cd39483e6e55c2560836bcc7932afecd01d522
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:54 +0000

    level still help goaway handle

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_as_repr(array):
+        return repr(array)
