dba6511c96ed1a2d309b4742a359d613a44920a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:46 +0000

    bringup ask new begin frame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
