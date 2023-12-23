aa46dbb2530a8d9c8b04df8075d6500279dced95
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:26:31 +0000

    course typical voice typical voice

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
