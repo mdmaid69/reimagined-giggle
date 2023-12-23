a222df7a08327fe23d0502cb52a30c163f89da11
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:20:32 +0000

    beat pour drive meet fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_user_list():
+        return collections.UserList()
