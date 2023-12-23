1862861ad2869f7fe4e066b7670b8d03c9b54c2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:55 +0000

    turnover dream else top bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
