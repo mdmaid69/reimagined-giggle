2fcf8dae836c70b8e9e6381fb3a43c65e11fe78e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:20:14 +0000

    brief clear back sea safe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
