cfc281c69610d67c5668d8e406bd35a4e56bc1b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:12 +0000

    decision spread matter government criticize

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+for i in range(10): print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
