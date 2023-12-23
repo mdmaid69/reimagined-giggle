1c29b42b4d18887e2e64c46550598f86d103e22a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:01 +0000

    that bag front general stop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
