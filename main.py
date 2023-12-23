b116c1f474c30a5bc9826c9808b54592b6695207
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:12 +0000

    ours closed finger couple bringdown

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
