aa57fafff0c79a33c354222751f50a809c04bd9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:58:13 +0000

    awake big interfere grow trick

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
