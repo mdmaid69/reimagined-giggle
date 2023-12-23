5fc090c5e16ab538d9a1c39215cb27964456f7b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:42 +0000

    row wave dress great stranger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def square_number(x):
+        return x**2
