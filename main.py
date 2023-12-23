7f09247289f274dafc0733683ce52946ccfdb5a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:36 +0000

    before entry damage black 570enjoyable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
