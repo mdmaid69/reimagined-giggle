2a63eceb2862f822359af2bd657d3322a547dbd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:39:45 +0000

    contrast step storm section convenient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+print(math.pi)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
