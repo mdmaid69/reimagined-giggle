e01711ef3d5a042a17743016e77fe8cf561075a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:50 +0000

    pass runover illness official shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
