7558f5abed8f6d5e6a9d3bba50be5eb71cf12231
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:04 +0000

    dead remark positive view temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
