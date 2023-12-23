2e1990b4f449ee5eafa3a249a8656d261f563413
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:36 +0000

    firm fire decision story be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
