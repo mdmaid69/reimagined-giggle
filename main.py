42ea1ad3fbd9d4f848d89f203d77122a04d2f2d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:15:39 +0000

    shade doctor serious slightly wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
