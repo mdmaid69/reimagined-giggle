58e029d0a24b574bf15d62da5530c9200c1d3991
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:46 +0000

    night get secret indeed sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
