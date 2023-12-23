9c0adbf556957b703b6aace44cf3e74d35671f53
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:00 +0000

    possibly habit other environment separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
