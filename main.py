f858ce6d34ecf63fd02cfa77cbeb49a5b4b67ca1
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:15 +0000

    badly tap loud ball circumstance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
