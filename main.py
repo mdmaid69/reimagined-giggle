50140a15655365c4fbcae63c8aad9cae34769749
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:28 +0000

    own survive spring trick area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def is_odd(n):
+        return n % 2 != 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
