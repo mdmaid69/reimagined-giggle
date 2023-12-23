207f09a9e22823df77c54b3bb4f2fa7dfe4daf86
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:42 +0000

    possession office love spread innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
