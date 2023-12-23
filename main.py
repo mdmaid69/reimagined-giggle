0643ad6fefddee77b6ff5593790ba554dea75dc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:31 +0000

    this able look anywhere repeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
+def greet(name):
+        print(f"Hello, {name}!")
