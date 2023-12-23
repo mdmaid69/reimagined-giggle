73a221151e2c8cf7ba5e892ef8cb5ce60808ebc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:33 +0000

    class turnout control adjective advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
