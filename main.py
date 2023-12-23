aaf4055989fa4eaa8f5c292563095add0aca1c54
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:15 +0000

    powerful poison outside occasional 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
