9909ea78cd223050e4853f42a8b076a253da229d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:58:19 +0000

    be repair wish television print

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
