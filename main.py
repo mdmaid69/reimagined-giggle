43242a88afe36cd8cd250dd7d2a0a56837f4f2fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:49:14 +0000

    address accidentally call correct behaviour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
