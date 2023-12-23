4e7a1f42ba584f45ebbdd3629207c1696f7cf5af
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:39 +0000

    hole tell action before recently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def convert_to_hex(n):
-        return hex(n)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
