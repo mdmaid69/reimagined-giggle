bc41605ff9dc07d916c3080ada723c222c11c99e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:12 +0000

    wild thinkof necessary phone matter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
+def calculate_speed(distance, time):
+        return distance / time
