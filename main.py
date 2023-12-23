ae520931cec936676349290c39c7f441facaa0e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:28:14 +0000

    basically comeoff walk juice degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
