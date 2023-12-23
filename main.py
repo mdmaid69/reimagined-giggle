c8e74c5ecf0a2713a3500942e0e75e64431f8e97
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:02 +0000

    person over yes fire food

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
