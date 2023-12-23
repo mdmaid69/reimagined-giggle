2d2bfba12831187ea7cee969702c6ca1e01c1136
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:48 +0000

    hope confusing waste thirsty dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_max(lst):
+        return max(lst)
