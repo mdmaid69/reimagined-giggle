84d7cd566dd9f0e4bfada2339b39fa9c6fdf9618
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:30 +0000

    design unite lie respect pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
