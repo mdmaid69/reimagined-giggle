f05be77180fa944c5df026da1d7de2ab73bfa484
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:43:03 +0000

    hand even which come describe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_palindrome(s):
-        return s == s[::-1]
+def convert_to_octal(n):
+        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
