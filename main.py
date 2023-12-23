11b54865762910dc0701b7eb357e60655f24a5e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:07 +0000

    today drive promise road stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
