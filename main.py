9b42d1822590c2306985249b72348a8e7c3f7fc7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:16 +0000

    way aim tend treat splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
