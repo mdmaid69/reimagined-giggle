8ef64c89699fa92e9ef97f0833b93cdf13ab1cbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:38 +0000

    unite capital neither line angry

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import time
 def get_formatted_time():
         return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
