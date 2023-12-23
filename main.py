8cdd8555084c776c94aab27dfbaafd2b20bbbd23
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:25 +0000

    soak pay frequent appearance responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
