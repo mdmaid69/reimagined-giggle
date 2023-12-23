123e774a641ce39c42297cc732af2f13b75e2a31
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:45:32 +0000

    surprise moon behaviour scream leave

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
