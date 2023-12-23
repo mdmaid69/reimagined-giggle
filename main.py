3a568e32fc4040c904039464d9b4a55c88276cc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:27 +0000

    entry bringback couple track relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
