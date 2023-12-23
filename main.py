945ad4aaf2b002fa41d3af6c3b78e5f8ac290ed0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:42:37 +0000

    spare charge your official comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
