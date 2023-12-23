31a36668af9c4da0bae5f9d7525f38117efed482
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:43:28 +0000

    find breakout live father support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
