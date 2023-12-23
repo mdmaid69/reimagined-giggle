fba8311d830761460fecb5cea8990cd98ed094d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:57 +0000

    drawing charge much number road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
