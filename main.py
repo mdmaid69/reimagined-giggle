6dced1977694dd5269507d7533c2d069e3b6add5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:48 +0000

    kill towards pronounce school Ll

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_repr(array):
+        return repr(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
