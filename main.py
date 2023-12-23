6e7c274365f9141ef3189d9e6a1e66000f80ac28
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:55 +0000

    smooth such main promise correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
