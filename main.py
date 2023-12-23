8b261d1060806a95f69da1b9f25db5ee59b1ed74
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:12 +0000

    control rarely round thirsty punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
