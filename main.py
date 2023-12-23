60543b05aac474523fec53253878187f872e60b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:55 +0000

    recent strange bend shock guilty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
