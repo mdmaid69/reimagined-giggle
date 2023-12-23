96fe908d627e746da7fb8374c351684152599d7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:03 +0000

    angry shout watch declaration naturally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
