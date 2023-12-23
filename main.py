3050c14cf9908d3ffded5109b51e1e6cac220239
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:20:48 +0000

    tonight probably party view pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
