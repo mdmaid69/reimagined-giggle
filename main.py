82aff0f638d77503bd092cddcab510d04be76505
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:24 +0000

    thirsty forgive chief disturb best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
