0ce9d065523f72c81d5c9e714f52576f75433382
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:07 +0000

    there entry all wealth public

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
 import json
 def convert_to_json(data):
         return json.dumps(data)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
