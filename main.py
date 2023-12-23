9825aa29d4c3dc218a6ef2646771528bf99f0278
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:19 +0000

    weekend like nowhere yours violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
