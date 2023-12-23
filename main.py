4c803942b86cea4158077562a806045bb8dad557
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:29:17 +0000

    spite any cover view taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
