1b45d7b9d2f41f649fcc7a30f41eebebcbbda738
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:32 +0000

    turnover suckme broad variety mail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
