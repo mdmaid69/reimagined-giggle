ae0e1ed67279c60696ada384acd3da1db86a5c2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:16 +0000

    build so mine fight neat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-print(datetime.datetime.now())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
