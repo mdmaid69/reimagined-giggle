e19aba7d4a77c2ed3be92596236daafe46655d5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:21:07 +0000

    well disagree preparation emphasize absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
