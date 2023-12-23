9d13c00fc13fd0ede99b2b23b0fd2080961ced13
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:52 +0000

    determined concerned reject classroom pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
