b83e013b713d3e36a6cdfa7337132d373b0c2784
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:41:27 +0000

    benefit dull class deal juice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
