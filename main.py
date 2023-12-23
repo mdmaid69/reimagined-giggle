09162b0ff9af8ac79f3464d678a03eccbb5f8fa9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:23 +0000

    true depend defend brief demand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def convert_to_binary(n):
+        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
