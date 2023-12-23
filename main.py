093ed431383fd60415387f4cb9ca338dc28657a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:11:41 +0000

    bet gradually figure smile row

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
