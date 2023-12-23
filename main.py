4e03b23eec351c6d108674e814490cf796aab701
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:55:54 +0000

    spot cheat slice long way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
