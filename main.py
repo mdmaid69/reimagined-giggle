49b5d562f5e221ce1fe4f7cc550bdcc20634f9b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:57 +0000

    way smile second often sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
