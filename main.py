7a6a999aa06a8679ce096102e3f1678df5603234
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:22 +0000

    mind road wonder risk complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
