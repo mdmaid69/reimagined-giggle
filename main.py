e7f415d02c53a4185c345a112cb799f72cee9eb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:13 +0000

    contact nearly appearance Yy fault

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
