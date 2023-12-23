c06f68993b5807800414665e255d88fdb371b07d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:40:10 +0000

    aim finish arm yesterday range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
