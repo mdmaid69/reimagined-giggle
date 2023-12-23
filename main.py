24efdb9235e6f24d2ef7c3d7819d4b600c29c25d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:28:23 +0000

    rare sell judge confuse poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
