b3320c06c5e6dfc23a273f80fcce3f448859074e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:22:12 +0000

    number limit needle reason slow

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
