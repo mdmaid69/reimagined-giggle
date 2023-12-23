73e1fe38d47ea92d8ab65ecc9ca0bf31e34715a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:12:58 +0000

    occur flat birth gather owe

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
