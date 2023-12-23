3b53bdb533b91f1f700e01080ddb339ee3752bc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:12 +0000

    arrange treat dream life fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
