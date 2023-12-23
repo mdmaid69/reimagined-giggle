dacc15fdc31c2385899817d872c643474f00ab90
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:57 +0000

    neither scale process house correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
