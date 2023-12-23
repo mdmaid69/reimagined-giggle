51809da6cc28fd97e9ccac051148d94dfe00456d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:13 +0000

    fair attractive nice likely bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
