5670eca8a52652a66b8b6f5d6ac60e7cb4eb5b24
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:25 +0000

    past drawing speak comein busy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+print(math.pi)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
