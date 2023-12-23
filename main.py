53b02ed1d68762990177a6986271d7a53ffa3a95
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:03 +0000

    telephone usual mean physical suddenly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
