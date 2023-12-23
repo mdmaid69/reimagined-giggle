bbd5a97425e90d525b5a7aaf935b124354e87480
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:17 +0000

    window beautiful afraid empty meeting

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
