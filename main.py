e6c7150fe699997e3679eed709fe1fdd0dbeedc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:52 +0000

    distance woman software coat turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
