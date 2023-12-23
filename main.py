f29415efb27a89c10ba7db74459d2678d6856b2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:56 +0000

    break shock inform behaviour health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
