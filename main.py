1605e2a6a29d780700e1963fab9c9a83e82bd524
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:20 +0000

    pencil no please head action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
