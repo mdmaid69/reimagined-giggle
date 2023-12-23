f8c8b9da2e90ea974c4f928ab92a183c6def7878
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:59:45 +0000

    anyone present tired argue difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_speed(distance, time):
+        return distance / time
