1caaaa5a35a34556fd9918d83b587d006744a6b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:12 +0000

    callback fold sleep breath lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
