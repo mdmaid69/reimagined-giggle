beafa4370790f5dabaf147d427d93632823ff956
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:48 +0000

    reasonable mail flavour pattern show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
