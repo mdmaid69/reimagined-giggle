814054254d6c74c48dca35180aef1e326ecf3048
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:08 +0000

    getoff ignore sort cause rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
