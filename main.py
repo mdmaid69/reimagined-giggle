2eaf5b41d50d1145c8f82d533138facb6644e46e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:38 +0000

    shout build have criticize show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
