b07e3822c09e1559b7fe9c95ae2195110b879524
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:23 +0000

    show job remember Uu drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def square_number(x):
+        return x**2
