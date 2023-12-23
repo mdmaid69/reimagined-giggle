d0580a6729453406f61bdf20071ddffbc89c93fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:36:18 +0000

    to check inform measure entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
