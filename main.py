af837cb2629a81a7cea6ffe66987d780e6689ca3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:00 +0000

    exchange hot trip wake gap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
