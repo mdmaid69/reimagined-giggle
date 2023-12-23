4ce8eee6797d1074ed689a118b711d839e2c78f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:38 +0000

    bend stiff properly rather buy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def reverse_array(array):
+        array.reverse()
