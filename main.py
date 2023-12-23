82a9eebaf0abda7d70b75f0e092c5fee018eb425
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:45:57 +0000

    stage creature jump neither please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
