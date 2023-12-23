05f0c85858a554562b1cb7fbb0e010b6c4518221
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:33:41 +0000

    giveback question future takeoff Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
