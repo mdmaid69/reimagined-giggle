e3dbf21109eedbb80a162e5d9aded64e80d5e704
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:27:05 +0000

    this hang 572enter lead refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
