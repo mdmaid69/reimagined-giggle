e04e1c35663be8357601234147fac072165c0de8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:05 +0000

    twist tragedy note shallow scare

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
