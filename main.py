44371665e80733f60bea63474a383691f9f8aceb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:58 +0000

    finish hard subject general gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
