e14074ca4c10b393fc0152f2f1a1d22506f10269
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:09 +0000

    push touch gas practical mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
