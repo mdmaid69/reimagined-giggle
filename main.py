fb036833078e58dbb3faaf26c1de247b52de0a40
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:01 +0000

    pupil snow polite exam separate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def divide_numbers(x, y):
-        return x / y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_current_directory():
+        return os.getcwd()
