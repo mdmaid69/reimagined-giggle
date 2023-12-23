d966eb170f20b1bb2754dba8ab890b9fbe241758
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:46 +0000

    suck floor hard display too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
