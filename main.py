50e68f3a8aa26fc5e4f404c76c57ff338186490b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:09 +0000

    class settle lie fit punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def cube_number(x):
+        return x**3
+  import os
+  def get_current_directory():
+        return os.getcwd()
