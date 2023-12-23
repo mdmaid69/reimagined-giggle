55a266bec7f7f5916f34a14c93db1109c10d8e20
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:45:21 +0000

    whistle push reduce gooff touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
