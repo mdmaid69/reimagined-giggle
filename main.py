46b2bc0222d9d2426868821c3e6d081454e850a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:23 +0000

    side guilty depend weak company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_length(array):
+        return len(array)
