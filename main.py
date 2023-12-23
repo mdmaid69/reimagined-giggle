cf0a84eb18079dca2bec3dd857e3f96ff32b9b5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:45:12 +0000

    country real let dress habit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
