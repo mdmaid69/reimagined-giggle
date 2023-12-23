3aebc147a854ec962eb15cd694ab020be19a8d94
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:45:45 +0000

    area lie artificial inform shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
