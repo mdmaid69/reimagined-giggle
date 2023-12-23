e7854cdc384b5a1454d25f59bd99b5a8345b055a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:27:46 +0000

    eye colour comeover drive reach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
