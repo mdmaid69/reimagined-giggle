814f78c201f351a7b6e8ce6dc7d6870f8fbe698e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:04 +0000

    translate careful refer land flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
