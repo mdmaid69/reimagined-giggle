d54ab2ebbba79de1d2a8d5addd2445e90eb2ed00
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:10 +0000

    recommend take separate welcome any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
