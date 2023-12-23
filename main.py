6909668041c3ec97713da80a4931f68f9781505c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:42:20 +0000

    sound downstairs sight nose work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
