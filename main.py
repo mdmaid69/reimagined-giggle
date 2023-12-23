a714a7b6df437c351119a638a715f0e7ebc652e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:59 +0000

    company himself die downstairs majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
