0c21ce5a6a0ee2665491ec79663cd524b36238c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:41 +0000

    drive which patient dress conclusion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
