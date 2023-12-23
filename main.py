1bbb9fd35cae64d2373eeffda2a3a44f1098a37e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:21:03 +0000

    respect switch depend classroom yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def convert_to_binary(n):
+        return bin(n)
