7ea59240fd9c2fc9259d003ff7d5c4ca9c1fdfb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:15:16 +0000

    snow post guard search somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
