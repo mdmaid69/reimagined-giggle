cc6ae2435b58f3b932508936b4a1b2adeeaadb76
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:01 +0000

    clear final squash somebody illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
