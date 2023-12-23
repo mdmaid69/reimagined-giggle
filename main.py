c6c5620bfc79dee18beb2189e38985e72f4d0918
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:05 +0000

    stamp accordingto row unfair pronunciation

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
