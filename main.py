ce438db4deece1bf674a83a4c6456b9fcfb6c13a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:54:08 +0000

    runover giveup trick maybe Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
