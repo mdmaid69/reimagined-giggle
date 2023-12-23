54f3ce076d9cb8a09b86dbd90b602bdad1061e98
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:54:09 +0000

    someone complete pattern return have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
