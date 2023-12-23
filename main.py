e005ca2e1245f5081c72d372d60a6402bbefe62b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:45 +0000

    behind design figure Itself history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
