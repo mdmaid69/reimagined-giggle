c2da3c4f73757a838f1c14a2fed6f51b952bcea7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:11 +0000

    surprising generally draw spread fly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
