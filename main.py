014eaeba6fddb2720ed1895b51afe2d7af7b32e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:20 +0000

    agree lock over away correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
