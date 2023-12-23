b296f97faf3735d62d45876168d635e7dbdfdef2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:08 +0000

    suckme confuse purpose maintain bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
 def calculate_error_function(x):
         return math.erf(x)
