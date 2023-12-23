cbbb9fd91a874b6a390e93fa2372d1a43a3401f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:45 +0000

    sleep world mark whistle fly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
