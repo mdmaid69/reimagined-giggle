a16e351335d0951d23b89065638c8052e35feba6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:14 +0000

    discussion awake edge please oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
