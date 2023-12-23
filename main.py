56080926379f0202169ebb7803d9c5c165120e5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:17 +0000

    than music meal press hurt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
