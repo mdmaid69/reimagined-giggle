e9fea4073978f33441a77cdb3cf745c734f3c20b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:11 +0000

    damp development tomorrow compete old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
