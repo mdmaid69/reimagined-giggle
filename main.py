3e9ac93c755a0c08afb91e9dacf26cbfa419d301
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:29:09 +0000

    go under push reserve even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+def get_current_time():
+        return time.time()
