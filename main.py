51397f06989418c8cd3333702cfa4e51b2f0d9d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:53 +0000

    sound close nowhere steer complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
