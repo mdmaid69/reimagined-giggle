207ff711658b68bb4980c2c4750463c51098caa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:02 +0000

    workon share willing clothes grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
