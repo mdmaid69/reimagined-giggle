8c7e0ff657aed5ca1480b653eb1c3f517efbd964
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:23 +0000

    command ground busy upset heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_item_count(array, item):
+        return array.count(item)
