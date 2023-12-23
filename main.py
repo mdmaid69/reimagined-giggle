bd376058cdf14463459a9807f142bdd15c64a061
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:25 +0000

    fight day colour switch rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
