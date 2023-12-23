4d6aba8904074b7ab615a0ac716df336efe49d70
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:51:14 +0000

    bank 569enjoy importance wealth taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
