ca13433a0592e5ba30a59f2154c823ea6341e818
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:03 +0000

    star divide damp disappear choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
