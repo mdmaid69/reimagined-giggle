4495b3aefaa01604c32e9b1ec0c690a61ad32b34
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:23:20 +0000

    unfair blame flat negative pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
