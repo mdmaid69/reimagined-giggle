78bd0c24b8832596b8acd48a17bd36c28f04f475
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:55 +0000

    report object pity certain row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
