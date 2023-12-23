f323dd1ef58ed1091212df078d9bdda8f984f60f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:13 +0000

    guess tooth occasional file partner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
