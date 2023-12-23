64eb740d3e764deeb7412237384458a17b00c4ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:25 +0000

    class waste only need tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def append_to_array(array, item):
+        array.append(item)
