a23f16d2d5a01f89169e9fbfa10178c2e86c8745
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:18 +0000

    partner period nothing strike project

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def count_words(sentence):
+        return len(sentence.split())
