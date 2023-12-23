cda0403a94200958705a514d624f6383b8c52bcb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:15 +0000

    clothes quick thinkover impressed offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
