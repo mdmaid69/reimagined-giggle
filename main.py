893f52f07f728ab2d74975800a9ed638a0ee96d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:23 +0000

    sweep about smoke appeal handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
