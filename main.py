4e7f4b2555288054fae3a102e513a890b81dd267
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:00:36 +0000

    distance old time combine carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
