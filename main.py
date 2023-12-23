596eb4f25d532578719b58cf0d1657944077b264
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:11 +0000

    science order mind creature hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
