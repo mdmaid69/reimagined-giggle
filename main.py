162b8b2f9f76aa427f35b133fee69d9f621c2109
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:19 +0000

    workout precise decide boat serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
