50cdf3cfcda45d247f8006529a5bf4076273560a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:27 +0000

    set single different freeze spite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
