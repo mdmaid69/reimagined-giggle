a4bc908c014153e895856fcb5206ae979dcd93b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:22 +0000

    still gradually involve satisfied skill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
