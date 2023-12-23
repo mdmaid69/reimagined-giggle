1d4e91fe687c7bd7a6bf56c90cec1530e4299ac0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:23 +0000

    until repeat exact discussion car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
