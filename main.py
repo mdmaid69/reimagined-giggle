6d036cf43cac51e49a81504ae96ed412673647fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:11:50 +0000

    slightly turnaway well lost protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
