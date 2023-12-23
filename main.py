f9461994079a5b2b78e8351dff892f2003615f7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:49 +0000

    slowly workon news purpose no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
