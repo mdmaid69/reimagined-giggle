b3c1769151f3afe92a6557e28da670a8d1e10c6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:57 +0000

    OKorokay separate mainly find reject

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
