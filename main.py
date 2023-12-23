f998ba8b30ec768cbd8229d4c69cef20b95e2202
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:57 +0000

    radio could ordinary crazy about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
