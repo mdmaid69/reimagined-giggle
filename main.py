6cfc7aca2c978d8acb7e4f5aada4cb51d46a6560
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:42:09 +0000

    flat along rise better file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
