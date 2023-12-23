38c74b665e89505f6347ae0e6886f9826c779c3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:54 +0000

    game might depend sore deep

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-for i in range(10): print(i)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
