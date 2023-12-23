ec33b58b29956c468f53d7abcf2e1986413547f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:12 +0000

    guide ring various science fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
