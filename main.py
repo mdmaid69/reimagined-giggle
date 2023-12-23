9730138a73e0facf7003e99aca7508e016ef18bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:00 +0000

    after meeting respect tax charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def square_number(x):
+        return x**2
