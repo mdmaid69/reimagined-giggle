d22e4a75da5a0bf572d2beb93886f8699acc4e17
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:54:09 +0000

    I trade noisy concern clear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def count_words(sentence):
+        return len(sentence.split())
