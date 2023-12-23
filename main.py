87563953033e5f8d73f569dce74151e751fa010e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:51 +0000

    church danger use no mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
