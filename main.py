7ac301269091a220641fe3e6a37db69735bae636
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:59:26 +0000

    reach position watch wide storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
