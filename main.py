457e87a4f6dc430fc63c50628be39dcda2223cea
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:30:27 +0000

    computer call frighten noisy holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
