ea2f789177c30b5d5f3dcbddfbf65c4d2579d636
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:47:04 +0000

    friendly no she steady unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def get_array_as_tuple(array):
+        return tuple(array)
