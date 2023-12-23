2a924ccc21b39586648d3787f9b87cd0ea334bf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:30:49 +0000

    follow cold above trip remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
