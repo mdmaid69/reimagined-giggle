772a3a67887c07e9c2455a3f48c1e9c1b5620ff8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:50 +0000

    behind lonely put sign fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
