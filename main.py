68141ed63b56d63307b8ece3ef9aaf9e99f6df16
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:59 +0000

    best evidence pump skill tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
