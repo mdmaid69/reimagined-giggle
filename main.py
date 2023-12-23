450a8a175db312e58d4534368fd703a1a4f65729
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:11 +0000

    perhaps skirt several project job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_max(lst):
-        return max(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
