6fd165695189670def46c466adce4dd35eef2f54
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:02:07 +0000

    taste seat load emphasize machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def convert_to_binary(n):
+        return bin(n)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
