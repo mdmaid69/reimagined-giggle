46a070dea5f095f394b4437426131d502b8b238d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:32:06 +0000

    excuse slow sort again height

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_acceleration(speed, time):
-        return speed / time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
