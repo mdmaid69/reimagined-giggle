6c5898bd7c86e24aea6687f5251dfb3553bef4e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:38 +0000

    close advice speak prove spare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
