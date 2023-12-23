9fe60727e77b4df1c281b537fcad28210f52c1ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:51 +0000

    whom period say no physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
