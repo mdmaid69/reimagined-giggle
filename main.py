8dbabfb2cc1fe328b0925baf50cf4545f4ad31cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:34:30 +0000

    goback(to) lay quickly polite chase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
