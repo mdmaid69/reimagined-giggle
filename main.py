8583db3ff151b38df1daaf265721ebc893b5f237
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:23:31 +0000

    shake belong yours post demonstrate

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
