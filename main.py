eb3c20b5fb485e646b53c66e9481f629c8c36f3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:49 +0000

    explanation look consult medium full

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
