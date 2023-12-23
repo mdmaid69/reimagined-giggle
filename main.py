893881d780ae6a9725a86c665efb9db20279b4f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:28 +0000

    flavour either power if would

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def add_numbers(x, y):
+        return x + y
