47094b13ea0334089811fb059502190d991ee792
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:55 +0000

    depend issue area spray wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
