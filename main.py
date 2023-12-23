15c6c51a5c087200ffb3681532849fa3cd34ad79
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:55 +0000

    drop inquire me trial seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
