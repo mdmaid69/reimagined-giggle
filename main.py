9ad8ef400082706bebc3a8a90d5f167e7327fdbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:47 +0000

    message legal goup aim happy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
