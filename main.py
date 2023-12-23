dda8a92ed9aa21328fda216d81df8a2b2011e32d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:56 +0000

    through within I takeback rely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
