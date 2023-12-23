0c99cc570a6efde6f2206533224df8764c7bc174
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:06 +0000

    scrape recovery improve afterwards send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
