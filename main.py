9a039827b3b505c3c8883dec386d50778162974b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:07 +0000

    really material ill clothes curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
