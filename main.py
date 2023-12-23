c6ceeabaf32d65257818bc2b2814b26dcb5bcf28
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:32 +0000

    waste goout temper makeinto thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
