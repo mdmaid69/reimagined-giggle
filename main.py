b43cb487b4650bca09e4ad0bc19526a9a5f03fb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:46:04 +0000

    receive tense above enthusiastic feeling

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
