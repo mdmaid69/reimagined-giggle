d127e00e24b6a2a25c935a63cba52ac74a2bd3a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:03 +0000

    above children zero edge time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
