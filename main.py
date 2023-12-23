a2e0ee6091895c36728f001e6beff6c8b46cfa4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:13 +0000

    less Rr weekend tooth forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
