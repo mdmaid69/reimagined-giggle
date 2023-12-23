2eb7458f7e9875bd6182d7532451848fd83cb3cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:23 +0000

    fall taste path tight trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def count_words(sentence):
+        return len(sentence.split())
