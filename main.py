7a87046413e1b4e3dfdb9fd3245499beb2960a11
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:55:17 +0000

    dry replace song tragedy address

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
