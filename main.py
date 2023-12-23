490a66230ae97a75ad577f0fa8c5ff8fe5d8b80c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:42 +0000

    wind stare trousers appeal trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def sort_list(lst):
+        return sorted(lst)
+def convert_to_octal(n):
+        return oct(n)
