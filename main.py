0b2754b90ba29d9ee8ff565ff9b9fda9b49e2441
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:43 +0000

    nature deliver knock explain computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
