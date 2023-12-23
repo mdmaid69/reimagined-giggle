adaf2060e7a012f160f221479c42bb7ddfdaa26f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:19 +0000

    each polite amount sight getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
