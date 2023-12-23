b6e73f5e60aa981669b107cbddec14a33b6e4572
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:33 +0000

    represent car hide pile married

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_string(s):
+        return s[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
