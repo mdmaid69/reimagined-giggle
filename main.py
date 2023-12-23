1444686f08e5b329583a693cd27733bb52a9c31e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:02:08 +0000

    available fall uncomfortable far write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_work(force, distance):
+        return force * distance
