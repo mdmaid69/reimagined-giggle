fc56aa129e0212a04d7f5044169e9eb3ff529866
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:07 +0000

    language large organize load give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
