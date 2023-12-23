78a54996556b57e0f8b7825aebd319607f3f97dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:16 +0000

    skill path professional have practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
