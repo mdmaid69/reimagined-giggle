138bbf01eed8ec90205439c1031d317feff99943
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:34 +0000

    Qq fit hold find question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
