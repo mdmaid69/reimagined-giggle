fc9bff74230f0ef6b895f939c43e0ac5bdb56c53
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:43 +0000

    closely nurse violence friend expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
