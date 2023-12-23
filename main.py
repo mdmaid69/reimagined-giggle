758e70727af2ed6af8bd5baba2db1b901d37f317
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:58 +0000

    terrible mine mistake getoff table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
+import re
+print(re.match("h.*o", "hello world"))
