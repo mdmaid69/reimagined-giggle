aadeda806068a6598e65eb51a372764d219d6659
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:29 +0000

    improvement lead beautiful rich sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
