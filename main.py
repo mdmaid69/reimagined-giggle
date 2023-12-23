8e11abcd26065ab074daba6c2f026a44f395fc9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:16 +0000

    consult state have wish exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
