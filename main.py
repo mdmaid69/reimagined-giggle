638bdaf41f0ce27d824cd9df93141a5794eb13de
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:09 +0000

    thinkover right to divide church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
