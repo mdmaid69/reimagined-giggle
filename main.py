6654adce9694a40cb36470be71d650b7fdbc28bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:59 +0000

    otherwise before number report wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
