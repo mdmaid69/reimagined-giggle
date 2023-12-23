ace6b10b5968babb7f3fc7f309a5d479ce27a9fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:18 +0000

    forgive respect deal study even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
