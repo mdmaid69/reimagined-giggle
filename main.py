2dc7aa32f7c9b97f8a5e19ef94903df04a4cabe3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:53:17 +0000

    turnout down choose genuine on

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def cube_number(x):
+        return x**3
+def reverse_string(s):
+        return s[::-1]
