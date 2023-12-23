2d63611ddddafc1f84c26ffe675f3ee63d985826
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:48 +0000

    excited share bread load throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_current_working_directory():
+        return os.getcwd()
