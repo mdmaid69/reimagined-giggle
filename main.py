ffffc45331e5d244233a35db2e11370b71ced2e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:33 +0000

    person turnout rely belief stare

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
