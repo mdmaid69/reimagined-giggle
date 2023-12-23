f96df03913503cd0ea558df86685908a3661c6bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:08 +0000

    transparent say art threaten strange

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
