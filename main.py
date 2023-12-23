ed13991169f5b59a7f4b2b28f909a73d3e431904
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:19 +0000

    lookafter among true instant gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
