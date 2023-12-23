a83640644e5d3e59aacc57d89cbd4163713cd89c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:36 +0000

    uncertain ahead nowhere never die

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
