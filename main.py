ec9dd94182c1246e4e5fba3bc5600b461f9c5432
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:33 +0000

    emergency office crazy keepaway brown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_exponential(x):
+        return math.exp(x)
+for i in range(10): print(i)
