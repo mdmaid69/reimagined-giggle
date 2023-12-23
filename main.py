498c11030b358c2565fc8b3747784f84d199a1ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:03 +0000

    shopping sight scene bath shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
