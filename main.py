6e83015667e07906e25b8e3cf45aa1046ef10555
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:06 +0000

    square proper sleep child relief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
