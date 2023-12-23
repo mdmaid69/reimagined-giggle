2e614e5660576e1845219a3c21fb76f37a2c281c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:36 +0000

    hard taste greeting married combination

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
