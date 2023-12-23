8d88cc5bee8b5773b667e61c269fd942ac1f9af9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:18 +0000

    considering hungry smile refuse guide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
