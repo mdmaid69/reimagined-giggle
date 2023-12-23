7018a3e2b3dde2bdb8b5779e56d6d00b1d73b85a
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:45:49 +0000

    exhibition secret control still goup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
