47f37bfca509f39b591fddb2b7719d6500a67520
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:02 +0000

    side runover drawing grand wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
