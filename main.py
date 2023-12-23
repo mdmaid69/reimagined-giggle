18cdba3a1dcae10e23017132efe02b8e71222e1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:08 +0000

    for shallow variety everyone score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def is_even(n):
-        return n % 2 == 0
+def create_queue():
+        return collections.deque()
+def count_words(sentence):
+        return len(sentence.split())
