42c2423bee6bd62713ee76550224ad9371cf53f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:39 +0000

    get stage match music calloff

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_user_list():
-        return collections.UserList()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
