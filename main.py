dfca4f60f07930e92ebf447b8fe063fb622daa90
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:16:28 +0000

    necessary talk shut explanation murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
