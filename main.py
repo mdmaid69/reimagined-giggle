ac4b1f03ae581163c49ad2950fa04144e116073e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:37 +0000

    plain vote awake factory period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_pressure(force, area):
+        return force / area
