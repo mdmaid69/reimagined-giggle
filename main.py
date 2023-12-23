46f203885370ce85f76b2f882ee7f7e1f694dfc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:55 +0000

    anyone remark march operate favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
