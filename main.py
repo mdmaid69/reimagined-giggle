b0f347a9be04a1892b8e99db879280b028417632
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:53:18 +0000

    road wear urgent justice progress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def cube_number(x):
-        return x**3
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
