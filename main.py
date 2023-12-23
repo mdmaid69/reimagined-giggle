d87eb58ef66d7dd058f6351dfc97a35f6ac498ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:17 +0000

    someone spot advanced ill delicate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
