b611c6ebd8a7ee4aa65da85c1b82364a645f8254
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:10 +0000

    neck goodbye least chance pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
