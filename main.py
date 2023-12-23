368f8d40823e17c859be5394582f28022ff0954a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:47:26 +0000

    whistle thanks wrap favourite reasonable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
