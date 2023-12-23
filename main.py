b26eb583d45edf64a8e447f2e345c0715db09ae4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:36:12 +0000

    exhibition similar bottom medium swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
