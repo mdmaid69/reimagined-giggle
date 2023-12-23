395b2e968e17f16ecb9c2885667e7e00ac82b757
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:47 +0000

    combination everyone independent fine between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
