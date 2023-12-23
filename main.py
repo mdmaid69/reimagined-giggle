e6547eb3d14232ef97a87297d9af11741b21602b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:46 +0000

    sore face out wave run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
