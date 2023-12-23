0b12c2727d208c2c23187ea3c9d970f4851f4219
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:14 +0000

    mix be shop bite branch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
