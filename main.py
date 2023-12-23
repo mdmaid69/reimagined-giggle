e373e110fa4db1987d1df6f417ece023e504cc94
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:07 +0000

    talk proud peaceful whenever simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
