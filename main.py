3f17cd95d88b26cc05eae56fe4bf18899ed0cd5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:21 +0000

    grow display bad war turninto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
