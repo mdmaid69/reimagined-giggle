592112ab7ca914638413a5863f8b281f3a9f489b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:51 +0000

    upwards decide 564encourage its taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
