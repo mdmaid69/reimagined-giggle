b0abc6afe200f97e586a56aab213f9dbd62db9ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:51 +0000

    without smile correct hour deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
