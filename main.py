c0e697582c3692b240ce43c49553a7eea8499020
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:52 +0000

    absolutely closely maybe sick kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
