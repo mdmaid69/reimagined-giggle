12c8a32cc2630821b44efe36bfcfe53a7b525a13
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:48 +0000

    pipe sign approval herself guilty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
