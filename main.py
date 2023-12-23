18666eccfa7834573a9662dde84ca3acd43bc60f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:19:45 +0000

    comeup church after influence miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_list_from_array(array):
+        return array.tolist()
