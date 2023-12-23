2f9a45c95ef21b9062cee1cab1cd55b76b932478
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:06:42 +0000

    mainly score least pile contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def get_time_since_epoch():
+        return time.time()
