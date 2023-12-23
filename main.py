20f074960c0596343a30500740c4d4ba1e044a14
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:08 +0000

    pause expect cost set black

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
