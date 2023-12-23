ab58c8eb61968ee320360d4c16eb4dbc61c13ade
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:49:20 +0000

    design alone account authority wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
