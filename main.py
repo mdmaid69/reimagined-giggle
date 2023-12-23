20598323257f987f0c7e744fa46ef48161bc91ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:11 +0000

    guilty rush important nose love

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
