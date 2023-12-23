2c1560e9956dfd16cdf7ee4808b527feeb4606c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:57:59 +0000

    side journey stone story underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
