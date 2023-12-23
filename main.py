c97a23fe6e66da00e3bdc7eed5ec8b5d603d61df
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:08 +0000

    ilovedicks view own Yy characteristic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
