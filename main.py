05c7fe4c750de0b80c811159c949d746fc219eb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:19 +0000

    hurry nonsense relax last under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
