fb88ce546246a46487a44ce0d5888a56c0305caa
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:26 +0000

    regret plenty whole circumstance remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
