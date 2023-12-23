3700b99d305fa63a74da16954f5eb809d733a5f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:11 +0000

    careful suit whisper patient cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
