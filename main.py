1a09d253e4e168648572a40ad9b6f54b5dd454c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:58 +0000

    bet underneath cut equipment cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_typecode(array):
+        return array.typecode
