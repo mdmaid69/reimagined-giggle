6a1bd2496136d4795938b7bcfbb4e16c7b49472c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:55 +0000

    concerned their only dear glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
