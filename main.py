c33595767efbd29c8dce4b42728c35c7c33508f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:04:45 +0000

    plastic Zz quit play pattern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
