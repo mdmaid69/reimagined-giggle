e28cb4d9152c2c503f17425c161217f92b4bdbd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:54 +0000

    whenever apologizeapologise sink for getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def is_palindrome(s):
+        return s == s[::-1]
