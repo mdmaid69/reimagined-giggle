e5cee48d3f609600128458f036f2b675e8a705e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:37 +0000

    risk occasion temper invitation smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
