4ed19c6d87fc45bae165594abc17070faaf56799
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:32 +0000

    win big calm declare Ff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
