45cad644b7e2651eb36c2ee96834d400fa38c937
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:17 +0000

    be bear medium holdup main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
