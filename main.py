2ad11d9dd82a884232230884ba8004853abee95e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:28 +0000

    still complete circumstance radio ought

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
