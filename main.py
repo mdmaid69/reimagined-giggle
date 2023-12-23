35f81d14e1211de6caf9dfa0fe159402d534051f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:41 +0000

    mainly sensible force fetch extraordinary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
