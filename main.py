9f021cac54778d2f2514fca4c12321c4816659bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:54:50 +0000

    excellent space check aim strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
