37fe05be63167e7513daaeaf60a0fac24085cd5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:07 +0000

    goback(to) future priority perhaps see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
