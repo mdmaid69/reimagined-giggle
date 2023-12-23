325fe28daa2dda320d854fba473599fb9a2243fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:00:43 +0000

    angry picture feel hate my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
