309aba78bcd977ed1f6bc472c977eceb53dcdeeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:45:29 +0000

    still convinced add band free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
