791ac730b4b672771b4235002d40360656e692d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:01:00 +0000

    nurse dress recent stamp smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_current_directory():
+        return os.getcwd()
