222fad96828efec4e66bf67a2eef5a21d3152ffe
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:09 +0000

    mixture account cross turnover authority

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
