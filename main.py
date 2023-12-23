c9f162b4e1df8d19123d59534ee76adada9ae02b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:32 +0000

    deal big tone mark pressure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
