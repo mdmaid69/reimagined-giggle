b4af369c2c831e43d97f0b52d28541004e75032f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:23:13 +0000

    possibility tear guess protect mouth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+print(sys.version)
