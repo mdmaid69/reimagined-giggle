7a1a5fa6192cf4015e6b14514e19d0b4c88a21ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:35 +0000

    grow careful come break quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def get_array_as_str(array):
+        return str(array)
