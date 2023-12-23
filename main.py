f0680eafb76b2e1df56f56750626591eaaff5046
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:35:49 +0000

    hot wealth waste top protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
