2070cba0160df27db759f23316f21cd41f35ed99
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:42 +0000

    bringback control difficult argument die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
