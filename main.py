d10f849f34f78db5b07ce6ecd546049390f070fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:51 +0000

    leg anywhere record nor constantly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
