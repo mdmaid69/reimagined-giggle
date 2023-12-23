1e1d01e18dedab386d7290650507f538020a1fdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:10:14 +0000

    forgive back pressure declare reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
