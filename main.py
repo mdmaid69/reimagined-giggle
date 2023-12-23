adb7bad4baa80588a72bf41c7a9f548177815609
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:06 +0000

    mess professional protection comeover but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
