cf3375414e63d32b11cdb599e3e6fa6b35daac88
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:42 +0000

    patience include disagree direction exciting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def exit_program():
+        sys.exit()
