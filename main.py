89b47120fdd414fb7593ef71dec4ad760a498320
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:13 +0000

    keepfrom country mistake takeaway Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
