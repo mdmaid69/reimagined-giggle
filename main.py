d2231a3678af48b16119cee7a1ef2c1762de8d17
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:39:27 +0000

    tool ever fire team reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
