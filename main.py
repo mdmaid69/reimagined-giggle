bfd2128fc964ce0faab8f2e6d622db60fcf595b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:39 +0000

    television colour pronounce consider survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
