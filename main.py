dc1c58ac4fe907dcf91fb8c2d1e6dd57bfdbc8c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:04 +0000

    unfair immediate group long clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_repr(array):
+        return repr(array)
