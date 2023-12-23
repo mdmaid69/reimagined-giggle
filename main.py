3bfe4cf92cf1c467cd58b71602477d14058928ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:09:36 +0000

    570enjoyable job gate university question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
