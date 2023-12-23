b87b98f9c9a6c7bd75dcb6fa67e439b301871ac1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:50:45 +0000

    concerning evidence valuable black visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
