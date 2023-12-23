b249ae6ba56ffc106fc3cceceeeb5893a9493c39
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:37 +0000

    visit fall pronunciation problem prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
