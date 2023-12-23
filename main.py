68de6c496ca3b7334d8ba724e194823e1b5e253e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:06 +0000

    abroad nose ocean light world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
