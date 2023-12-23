a03bea0d1471f544d19982b38db955a4c2f2bced
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:28 +0000

    stamp favour grade door agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
