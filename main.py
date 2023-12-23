dc3b7c4c3c813a9187e7cfb7766f6f1e58acacbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:47 +0000

    boil entry accept substance share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
