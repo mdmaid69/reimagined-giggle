cafe3c7b261150ef754a51b46bedf60b23c6c6a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:07:50 +0000

    somewhere mother nowhere rich label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
