54ca626a734048e9f408411696b73d27c8295b2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:52 +0000

    dull seriously pleasant busy welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
