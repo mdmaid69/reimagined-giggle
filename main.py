0e1bcb31f7b49f30308bf50cdd68edbae1dcd014
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:40 +0000

    runaway active whole gather deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
