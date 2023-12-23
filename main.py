963dea2e9cd985aac2bead5f6650f4452112fd2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:44 +0000

    communicate while share wave switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
