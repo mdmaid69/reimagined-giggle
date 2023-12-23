714d7845a79385de4afce863b50b60e44d5f1af6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:58 +0000

    arrival allow argument store legal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
