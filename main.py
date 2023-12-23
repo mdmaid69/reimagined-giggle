ca859f1efd1b553723b50685a5344fe30a503b41
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:46 +0000

    that he interrupt attractive skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
