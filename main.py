3a9d6db08728554c2ec063d62fd719088e418427
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:00 +0000

    aware press dream noone nonsense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
