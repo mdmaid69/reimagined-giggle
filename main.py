591c1d35f389918fd7063843b25ca3a90844d1fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:52 +0000

    equal tip tap slow Zz

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
