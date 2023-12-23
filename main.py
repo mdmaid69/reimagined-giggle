cb1d10fa8e9b3ad05f313548ad2972525fe99299
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:59 +0000

    whenever Ii detail money lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
