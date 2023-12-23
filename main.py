2f52890ac6c6fa234ca1b838b2e64b921258a0b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:40 +0000

    exploration shock challenge dangerous prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import platform
+def get_python_version():
+        return platform.python_version()
