2850def43d6db77d2f89e5f370f3ba39e88029bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:34 +0000

    start dare several cause interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
