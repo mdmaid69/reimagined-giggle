9829b8ed4094c4221ec5421340077d83c77b9f92
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:43:24 +0000

    thinkof holiday bear tread get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
