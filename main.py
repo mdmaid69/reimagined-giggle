10d1e7dde4ac144f2e336d34c532188f19881238
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:18:00 +0000

    ring praise chat bring nurse

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
