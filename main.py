f38b71e639994bc49dad16547f922ae25fae2dda
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:29 +0000

    any runover temper store prevent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
