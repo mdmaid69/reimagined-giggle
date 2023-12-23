0eaa7bf738cc4471baab4e813dc219f562bda5c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:35:53 +0000

    here winter behaviour there stare

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+print("Hello, world!")
