60e36c581a983d9924707deb1248cdd3d291e285
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:41 +0000

    sign rule reach memory wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def list_files_in_directory(path):
+        return os.listdir(path)
