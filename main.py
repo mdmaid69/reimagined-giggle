49a6c64aab94874d8b136bc4985a26a0103e45ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:04 +0000

    could our town inquire beautiful

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
