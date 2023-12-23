7e46ce1e004b3835f30696b9ab720960f98615e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:41 +0000

    limit not possibly visit talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
