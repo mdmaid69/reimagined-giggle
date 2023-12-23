6314e329d582b3e1bd901316393c1e1ef71e2729
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:08 +0000

    without snow state woman gas

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area_rectangle(l, w):
+        return l * w
