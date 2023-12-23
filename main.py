bf1849e8b5455e5503cb9eab332b06e2da605925
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:33 +0000

    boring identify factory machine fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
