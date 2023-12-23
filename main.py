7c0c3083cc01736b882c7b8fb2bf5224fad02665
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:24 +0000

    mind persuade include explanation scale

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
