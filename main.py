11ee1fa9029d7fa0c32b509899d01f7498a75240
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:16:07 +0000

    really refuse name they storm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
