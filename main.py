e22b1bfe4ee4bb178e591941037b33082e0cee63
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:32 +0000

    567enemy path finish talkinto pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
