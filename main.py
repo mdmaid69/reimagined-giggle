8b04e9409f5ba99f143ef0dd88891275bbd445c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:37:48 +0000

    takeback left flow base manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
