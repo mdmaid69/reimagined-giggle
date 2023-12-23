2ee121f6f210640cf1818fec201e4f48df633996
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:49 +0000

    improve answer computer improvement slowly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_current_directory():
+        return os.getcwd()
 def calculate_compound_interest(principal, rate, time):
         return principal * (1 + rate)**time - principal
