827b3ac41ae768f566a0ad49d87e4ae35f7589a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:21 +0000

    read stamp dick trap else

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
