4f0d90cf9c8f9696b1da7241a30d054399ff3483
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:23 +0000

    strange along proper throw great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
