1a12673c53b8b27c54373cb7f864ced3336272bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:23 +0000

    tax sink brush muscle seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
