a8a7209de9751415d6de5014816e89c2d7d39828
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:49 +0000

    hurt funny test deserve strike

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
