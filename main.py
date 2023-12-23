d7f97046cf730108efa19a9676aeb9a4805cd574
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:34 +0000

    many unable state dress tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_palindrome(s):
+        return s == s[::-1]
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
