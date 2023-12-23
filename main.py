1ba30155bb173bb7ee240a8f087016d74acaf766
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:04:45 +0000

    thing sensitive voice bite turnback

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
