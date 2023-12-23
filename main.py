35ae0100e44037308029b60ce44f929802fbb890
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:39 +0000

    sort thought convenient respect giveout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
