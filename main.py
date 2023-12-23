045fa36f99b942e21b493602814ed397d6f5f391
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:39:37 +0000

    address computer shelter word mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
