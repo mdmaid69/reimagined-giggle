65a1aa53c040413c35c83f6ceba2b94496f99f3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:37 +0000

    switch drop answer hide game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
