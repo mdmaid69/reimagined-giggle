15cb65b58f8cbbf6e954e3800d8e5db58ba0dda1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:56 +0000

    keen relation support careless squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def cube_number(x):
-        return x**3
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
