f7289700982b375fd091b881d69a375a9e9f6c42
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:43 +0000

    act trust closely classroom repair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_factorial(n):
+        return math.factorial(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
