005fe4715b82395aae5a98871d5dcee8ee0e9ff8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:07:09 +0000

    bank walk sorry mistake thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
