6b9897d5a5e81e1b11dd4b9365158ce0d06d637b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:42 +0000

    single special spot after wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
