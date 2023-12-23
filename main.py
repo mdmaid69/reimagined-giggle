ca297bc5321eec86fadd58695d2c64894c72fed1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:47:46 +0000

    lend night under know relief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import logging
-def log_message(message):
-        logging.info(message)
+def find_min(lst):
+        return min(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
