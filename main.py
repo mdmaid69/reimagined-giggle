fb0afdf9af7667611a4bc91cdcc98e9650c07e4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:59 +0000

    characteristic keepout objection crowd attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
