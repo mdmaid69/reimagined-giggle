e6f3dc6b5717100856133d6074219be43edd090c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:50 +0000

    free near sell spoil mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
