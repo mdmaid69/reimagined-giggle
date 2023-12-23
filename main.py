6bb861bcc86025780dc0e2ddcb47c1d658c14e59
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:04 +0000

    genuine only wall benefit translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
