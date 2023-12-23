1b61ec1c89893690562aca5e8d42cd44b0f08245
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:19 +0000

    forever accidental lot straight travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
