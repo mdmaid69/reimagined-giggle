e13869d3415f690f73e7169fc0421b477caf303a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:54 +0000

    under murder guilty snow school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
