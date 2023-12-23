16da9454300ae5325dfdadc23420bd2993f77f53
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:48 +0000

    shall feeling though generous question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def convert_to_binary(n):
+        return bin(n)
