5dd3cddaa607f48ab4f755f78a34dce58c02f4d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:57:22 +0000

    now pen hello bend state

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
