7d218fe1f8a37dcc254a588f5742c0ac0c27820a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:03 +0000

    spin want block hard disturb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
