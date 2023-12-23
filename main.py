0cad588d5e25847343e29f59116738a82acd7019
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:04 +0000

    coat dinner prove refer justice

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
