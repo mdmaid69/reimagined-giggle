5efb9a556857e8f70341f2ce69bb29a9cdd82d97
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:35:09 +0000

    home success match speech luck

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
