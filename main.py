016f925ff89fe9d0d0a8f47bb5ec42a89acabcae
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:10 +0000

    month pause tonight everything gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
