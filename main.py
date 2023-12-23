8789396245fd990a58c5f75c345bfe0e0fd11f71
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:46 +0000

    snow wherever ever please usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_current_working_directory():
+        return os.getcwd()
