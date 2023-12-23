3b0dfa8dfc1599da5bc70b5610e82da4996dc88f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:31 +0000

    account would question often force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
