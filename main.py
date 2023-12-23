af8b33d428d080820dcdf22f9e13835c96e70f30
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:14:11 +0000

    report story mistake offer calm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
