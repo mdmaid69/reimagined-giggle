2d683d1da6c12123fb23ac2ec3705a0d2ece75ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:54 +0000

    underneath lead crowded bridge rain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def print_python_version():
+        return sys.version
