e9b86ecbb9b915b1dc6f1386dfba28381cea2ade
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:02 +0000

    illness birth thinkover excellent test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
