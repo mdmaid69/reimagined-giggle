d89d8e2e4cc450b9e1dfcc9f3b18f143057bc731
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:00 +0000

    do at but true tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
