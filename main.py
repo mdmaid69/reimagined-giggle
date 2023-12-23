2bda13abd9a391abb7bb94ca4fa1301666379548
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:29 +0000

    luck by fear priority try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
