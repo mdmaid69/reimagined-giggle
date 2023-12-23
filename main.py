f628fc5cacde2e94e14ce00db666962f63b43d42
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:27 +0000

    treat Ii book vote straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
