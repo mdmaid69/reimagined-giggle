9a4d37c2cc57e1e15d06c7ef325a7329dd0f3d5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:33:27 +0000

    advice environment project responsible report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
