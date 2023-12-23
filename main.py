48d17b05fab49198594599f8e04786f4e1b3706b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:25 +0000

    visit private respect owe pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
