8710b9d851d4c4b615326af87f1ee3a52865690c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:39 +0000

    score figure lose lucky carry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
