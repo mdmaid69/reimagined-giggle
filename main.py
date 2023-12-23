13ce32055c3c14d2c42e53aaf468b99fc1cb58d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:51 +0000

    plan beside relation vote string

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
