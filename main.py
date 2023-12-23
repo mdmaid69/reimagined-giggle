1c1a237a986ce75e0db04dd474622fb4debe6718
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:33 +0000

    short fair cool bread piece

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
