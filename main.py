a192351927f5b07e41d901f86eced9e42fb6c4d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:10 +0000

    yours sharp worst apply talkinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
