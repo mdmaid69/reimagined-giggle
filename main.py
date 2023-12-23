9a41fa10071809d08154d4d68f3a3e382548c666
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:17:19 +0000

    passenger until waste burst doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
