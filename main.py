d351d2a897033b668154bf6e171f362c8f76a374
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:07:27 +0000

    nice Mm sting phone environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import time
+def get_current_time():
+        return time.time()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
