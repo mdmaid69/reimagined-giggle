974020230662205b5bc96f4ade70a3c6d55d9049
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:37 +0000

    I side let put offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
