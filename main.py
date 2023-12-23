74729cfd2a9bd1b6323347d181b1377ab5fa5e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:06 +0000

    sorry delicate declare string still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+print(os.getcwd())
+  def multiply_numbers(x, y):
+        return x * y
