a3bb762035404c0312ae145b626b3781d339c75d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:50 +0000

    thank bath hurry your anything

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
