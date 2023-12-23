f78ec4e6a428ac753452f95ce7d41258d13ee1f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:14 +0000

    few fire autumn trust sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
