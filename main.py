d4e9f5decca99c7fc0ed5c935d8e117ed515a8e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:10 +0000

    ill sky big change fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
