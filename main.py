392c8170618365003fd26bcdb7ebee78c48e0cd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:47 +0000

    view separate across upset collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
