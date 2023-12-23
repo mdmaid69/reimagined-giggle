df2c1dec19ffe3fc490e1389cec5447aa1b21857
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:14 +0000

    powerful phone exactly tooth goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
