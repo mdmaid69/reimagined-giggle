35b62221dfa4e8a5cb5fca8bfab2efae35b9fda8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:48 +0000

    raw sail ever high over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+for i in range(5):
+        print(i)
