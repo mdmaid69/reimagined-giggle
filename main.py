97fb41a53e3f55544386e4c45fcd14babb5cb9c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:48:51 +0000

    better store underneath custom exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+  def multiply_numbers(x, y):
+        return x * y
