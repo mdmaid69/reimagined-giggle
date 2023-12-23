98ce72ddb5cfa2ef6eabc1ca796fd2b1149a99c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:36 +0000

    hope dream pour size system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
