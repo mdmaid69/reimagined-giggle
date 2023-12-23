5dcbff7c48fdb7236820fe7e38d86383b2f970fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:15 +0000

    maintain protest wet benefit creature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
