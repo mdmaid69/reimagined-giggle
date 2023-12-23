b26ffd5ab367c1af5a48853eba99f9c6ba3838e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:40 +0000

    destroy lot rain ashamed block

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_min(lst):
+        return min(lst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
