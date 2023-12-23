d31aaee0b09ef1187d03c66004721818f77098f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:19 +0000

    path issue pity greeting complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def reverse_list(lst):
+        return lst[::-1]
