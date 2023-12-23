d5d81472c29f3b812e6dd616c980227b5ec62c49
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:15:37 +0000

    interest content study row whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
