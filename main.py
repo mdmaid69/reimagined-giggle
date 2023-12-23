c9ad1ded354b024cf7c158471797b185944a6c32
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:16:49 +0000

    clean high last Itself pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
