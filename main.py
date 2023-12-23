394c3d687b5b3f96f25246c04048df335edb65e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:26 +0000

    Ll taste key city butter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
