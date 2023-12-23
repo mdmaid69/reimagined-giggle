bd7c9c761423a58c330863fb2fb1ad3646abf88e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:10:32 +0000

    present defend away better wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
