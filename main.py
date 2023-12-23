b1826e4ecd1e04970482818911e50d031acfe8e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:04 +0000

    rough many except grade punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_array_as_set(array):
+        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
