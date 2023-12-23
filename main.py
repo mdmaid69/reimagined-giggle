7e80c5282e4d74562b610ab427e6c44e19e9ee1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:18 +0000

    giveout store by takeoff close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
