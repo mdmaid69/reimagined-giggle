2520386c179e4a5e7a952eae77ac8d083da80ce8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:28 +0000

    earth repair pour still pain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
