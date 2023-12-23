a173240981b6312185ded0a96a8d3606d31b2df4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:15 +0000

    unit rather sign call but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
