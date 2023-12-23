147c46dc8e3addda8cc814f657cde5aaaf9431d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:02:20 +0000

    size blame left mine passenger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
