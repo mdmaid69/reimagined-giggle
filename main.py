09db5e886342e6507c92c2c071f942e360f4f514
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:22 +0000

    behaviour join zero next confusing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
