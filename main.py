17c7358460e2b6ed3f052de40130ff58bb54317a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:45 +0000

    feeling serious progress phone treat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_work(force, distance):
+        return force * distance
