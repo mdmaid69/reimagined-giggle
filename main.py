dbbb25a592dd6095f8437bf9802391b1867b20a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:59 +0000

    good arrival environment supply spend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
