63f7cd1080bda41208f1bc5c2372c520448b8ce2
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:51:51 +0000

    both respect what day oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
