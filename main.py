75b400f9d4430286d3a77ae242bcfa4d89e0f3b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:26:54 +0000

    wet draw meaning minimum everywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
