0bd66df0e78a8095113ff1007dbee7eacabf4342
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:02:43 +0000

    communication drink apply people basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
