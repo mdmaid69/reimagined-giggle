2b4b26d831ab8cae0b6a542a201f70d9ff1d6dc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:29 +0000

    trap platform getout party suddenly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_density(mass, volume):
+        return mass / volume
