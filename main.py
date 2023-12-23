f8725bacf06080057d2246f5f12dc4400db465e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:12:57 +0000

    fly care expect power blind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
