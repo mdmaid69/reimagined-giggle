7af8e99efbda37d79010720d7c8da13277a2787d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:43 +0000

    finally suddenly Nn narrow turn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
