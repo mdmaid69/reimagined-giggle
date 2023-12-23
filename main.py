206a944c47cf475cb3b0b63c49824c4809877864
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:03 +0000

    air anywayanyhow to steer opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-for i in range(10): print(i)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
