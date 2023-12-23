2fcdcd6c35ad52773939d257f3f11d326cc70b5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:25:58 +0000

    treat goup workupto wash grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def is_odd(n):
-        return n % 2 != 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+for i in range(5):
+        print(i)
