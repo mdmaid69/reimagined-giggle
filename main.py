8b5f6057cb10a2229f27179ca819a91de8c375f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:09 +0000

    doubt man related head why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
+import time
+def get_current_time():
+        return time.time()
