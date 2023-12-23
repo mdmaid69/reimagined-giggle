7fc0084bb1ba5a99f66bb506336b8431e69b4518
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:45 +0000

    surprise callback method period carry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_area(radius):
+        return 3.14 * radius * radius
