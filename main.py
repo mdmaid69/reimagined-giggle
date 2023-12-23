99effe78a4271cede8729474ba9436f215173508
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:52 +0000

    she flat return distance answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
