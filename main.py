e39511b48457444160b58ba61bfaed5b84833d4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:54 +0000

    rain empty quiet accommodation afternoon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
