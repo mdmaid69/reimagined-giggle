ee2d80a92fb2f189be34baf9b58f782172c026bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:07 +0000

    keepfrom major by unpleasant cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
