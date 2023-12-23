12e3961f8c8dde82d07b718e75fc96f38f8494c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:09:48 +0000

    press regularly top problem near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
