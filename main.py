19b2f927839592b6d74e8fdfef3ece56fd5a0db4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:35 +0000

    we easy window trap question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
