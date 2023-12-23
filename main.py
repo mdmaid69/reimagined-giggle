47674e5bc1ed44de347075163ecbc1a7cc5d8715
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:30:47 +0000

    instead class 568energy grand disappear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def sort_numbers(numbers):
+        return sorted(numbers)
