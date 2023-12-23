9c26468f1caf91a6fb851c68ecc093a5afc563e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:49:38 +0000

    money valuable print fly full

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_stack():
-        return collections.deque()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
