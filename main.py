38a9048b0e5cb721a532891ad6d232e8a47c5ddb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:28:13 +0000

    promise middle under takeaway sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
