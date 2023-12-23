6cc439abf4bad06c8e5e6e451d6c491369c0c95d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:36 +0000

    possession gift action hot emphasis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_queue():
-        return collections.deque()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
