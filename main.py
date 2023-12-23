72207f80ae3423293bef57aefcc938609e7e4353
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:44 +0000

    surprising capital disturbing square home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
