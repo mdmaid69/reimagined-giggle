293a5db1acc94322b3fa4a14d465ff69bb612864
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:50 +0000

    miss bite sad whatever respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
