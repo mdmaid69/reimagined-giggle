948e22cd538dbd7930b3d12c327e66ba071254a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:11 +0000

    want drive mind straight method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
