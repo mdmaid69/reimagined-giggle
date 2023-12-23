f372d0c8fbe32338b3bd783cd0fb0c8e38eee519
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:10 +0000

    maximum interest neat pretty fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
