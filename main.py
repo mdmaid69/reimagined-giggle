493edf4e7bbe9fc16a0f509bbd1f75d1aff1d8bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:38:13 +0000

    curious occur stir thorough such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
