072e097da38a9d39ac350df40f333e6cf64d3e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:48 +0000

    ought variety join keepfrom owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
