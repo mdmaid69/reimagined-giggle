5d4ab512050043bc35d7a4c1403493ac87abfeb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:56 +0000

    sound blame near angry payment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
