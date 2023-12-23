c75952087a7c1b8b99434224b85c2ba3f24e7539
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:39 +0000

    pour wall illness exploration independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
