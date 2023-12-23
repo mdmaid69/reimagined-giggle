5016dc16a7959b7de8fc9af116d27b65a08e2b28
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:40 +0000

    performance short present organization play

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
