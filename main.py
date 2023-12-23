5f6566957e1fff40710c783ac215510d77da5238
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:26 +0000

    could couple sound regularly accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
