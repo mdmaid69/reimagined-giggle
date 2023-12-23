a2c643e6a02f51388717ee25ec18d8e878c5b320
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:07:17 +0000

    left expect block keepaway chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
