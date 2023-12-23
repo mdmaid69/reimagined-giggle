6e406c4f9c452e7a1d7b8d213d405f776c3b9d44
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:33 +0000

    shopping play base job swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
