cd89bceb79b1bb2e1150139f66a5d8114aa81818
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:05 +0000

    goaway tomorrow sorry run free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
