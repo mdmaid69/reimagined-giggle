fe5f5e34d68ac0cdaf6ccd8b5681653276fa31fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:47 +0000

    loud cruelty level waste row

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
