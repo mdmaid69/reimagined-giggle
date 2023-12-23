712d363b7d4ddf3c0049e916dfd2d989927c4356
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:34 +0000

    various account section surface group

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
