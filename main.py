e514bff7e5de78f4bbf1f1aa492fed6319cb0c7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:42 +0000

    reaction wrap somewhere rain take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
