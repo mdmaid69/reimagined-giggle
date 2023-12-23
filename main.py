149860c2aca731e65c15d1a964531fb16fced370
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:09:55 +0000

    be tonight expression unkind winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
