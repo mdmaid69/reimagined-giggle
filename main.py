7584a542d3f62e876be98ca0bc4e14d35b343705
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:02 +0000

    getout steer either unusual switch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
