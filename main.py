4a87dd73a8fc453af77d0cae0019247b9cf64b2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:21 +0000

    come firm ticket arrive pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
