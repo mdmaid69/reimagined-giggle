8f559825b8ad933e32f3a5f96ce7c93b2c10db78
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:21 +0000

    so loud doubt whole environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
