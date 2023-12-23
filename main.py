660670170f4516c11c84256bc990161ebcf406ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:29 +0000

    from there sweep happy cheque

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import platform
+def get_python_version():
+        return platform.python_version()
