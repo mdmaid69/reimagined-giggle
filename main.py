4b4c76e960ad29acf583b415bc9eac9b672c27cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:12:02 +0000

    over goback(to) cry dear getout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
