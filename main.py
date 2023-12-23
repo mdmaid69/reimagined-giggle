a5bf2469e4ec38823606ac833fbce198d5530ce9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:44 +0000

    fortunate undo side off under

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
