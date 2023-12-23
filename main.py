9296d77f6988b09e51a84699f13094267107f500
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:41 +0000

    where load winter order grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
