d19ccb4f44316e8537d42605927dbcf35d0e5e59
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:21:22 +0000

    desperate absolutely opinion above fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
