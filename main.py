40b9c14dd317369e7cd35344b3593dd3206f34c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:42:40 +0000

    kind by responsibility pool fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
