40c52dac0c3941973b9b6ffe53d43741edb2cca5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:35 +0000

    to contact comefrom attract scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
