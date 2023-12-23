28357a21c10ed6f0bf5b7762bcb22c4f1a8a7bd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:51 +0000

    left machine sad deliberately though

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
