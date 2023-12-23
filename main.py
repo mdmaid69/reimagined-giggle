34ffc2585c5b2adeffb9ee2f89d2e73fce8aa86f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:39 +0000

    loose invite who fairly answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
