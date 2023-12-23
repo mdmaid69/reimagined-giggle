7fe8ec7d1e95af1d02b251935b11d1483b445cec
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:54 +0000

    calloff coat several lead occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
