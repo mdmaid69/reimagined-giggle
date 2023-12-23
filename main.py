36b4b5506ee7ba594f1ce6c6e3a18da36bfe20c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:46 +0000

    limit money fortunately around usual

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def is_even(n):
+        return n % 2 == 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
