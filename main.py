b7826949b4c8ef7b840ce6a6a694b6af7b095d4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:46 +0000

    nothing reject sleep back personal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
