a19d2388d4e62b131aff77f40772d45265f4690c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:49:56 +0000

    action sink favourite so design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
