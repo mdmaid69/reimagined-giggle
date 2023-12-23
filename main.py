f57fcfd4c8713344187b7e6a79c617de2afdcf3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:40:09 +0000

    ready discover although treat difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
