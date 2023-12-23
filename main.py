3ef4741e222b2e0fe00003707dacc2b48a130a8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:57 +0000

    check draw absolutely person permission

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def add_numbers(a, b):
+        return a + b
