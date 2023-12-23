1fa97048bc875c44a260c34ddeb85adb7b74415a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:08 +0000

    Itself stupid stage cheek house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
