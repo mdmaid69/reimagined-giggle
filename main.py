1f633b1d64f10fc937e7370120ddc253b5054238
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:54:15 +0000

    roof whatever off Mm while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_today_date():
+        return datetime.date.today()
