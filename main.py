f667343439fdd71f4f80d2425e876c8d38469d4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:54:05 +0000

    pronounce machine tour remark thanks

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
