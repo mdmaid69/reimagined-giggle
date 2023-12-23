5cd7adec3e724c5a0a67573cb072ae08325fcb4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:18 +0000

    disappear naturally heart tomorrow moral

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
