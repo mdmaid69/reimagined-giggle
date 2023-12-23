61df7555126f4df39753f84f7a7b2aec72dae248
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:11 +0000

    finish completely maintain press worth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
