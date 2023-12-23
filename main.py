b126c2f729795ac38cedb769ea49da85ecf91914
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:09 +0000

    speed lunch record will famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
