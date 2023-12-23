aaf6664d7c42f146b16536d6f1ab46189538a81f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:43 +0000

    sky runaway invite him would

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
