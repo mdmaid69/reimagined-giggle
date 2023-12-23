5b1716a5f48cd5bafb3fa1e2c49a633ad928f591
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:59 +0000

    Yy various mother tip machine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
