350ccf0bc3260bf3f1bda3651c218eb8ff6fdd80
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:39 +0000

    off sound report talkoutof first

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
