da154a2a54f9bf75b9d3fd86d2491b94984c43ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:06:26 +0000

    professional various comeup manner prepare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
