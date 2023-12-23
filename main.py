d0b8c6c355ceb0de0150925b7f4bae55178cb584
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:55 +0000

    better pupil handle speech spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_user_list():
+        return collections.UserList()
