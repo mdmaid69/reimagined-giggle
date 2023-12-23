2e2caa53d93ae3fa78cfe30ab597f120a6924697
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:19 +0000

    new list speed first unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def count_elements(lst):
-        return len(lst)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
