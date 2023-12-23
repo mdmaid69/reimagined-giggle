904a347939223ac187a24aee95282d12cae3a58e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:35 +0000

    danger important thought head stiff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
