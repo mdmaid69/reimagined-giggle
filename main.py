ff5272aff1270ec448db8f1e360ead096110a705
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:35 +0000

    rest shut wise murder object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
