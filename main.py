30a28e5911eed960501f9a3f786d1767458a8f0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:48 +0000

    angry scream section slice so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def square_number(x):
-        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
