7439cba012310dfd69e7fec10cfb0483b092f082
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:01 +0000

    much comein among else terrible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
