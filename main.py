ef50a74bd5dca124daffe26f6321ca090c7f37dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:07 +0000

    kind wet today tie emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
