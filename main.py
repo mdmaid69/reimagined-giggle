e2517f8c1661c2ff1c16960b539eb61ebba03720
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:25 +0000

    Zz round relationship comeover sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
