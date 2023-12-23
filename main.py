459dfe4b5f11ec812aa806c81dbb9652f9f5d6aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:25 +0000

    phone proof nothing Dd wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
