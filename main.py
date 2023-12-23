3baf2df7febeaafe4f7a1fe155a482d3ae5b74e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:37 +0000

    must boring round hope flavour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def convert_to_binary(n):
-        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
