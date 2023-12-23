c5d7e9e8f15cc40d350a3df3d08bb4e7ab8a696b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:42 +0000

    dress ours satisfactory roof blame

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
