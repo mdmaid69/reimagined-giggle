4fa37ac15789fd3a1ec736ed7173792df36912f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:49 +0000

    after offer attractive home hard

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
