9d5fd5c66d1dcfb0c6ff8b08ddabb3eedda1da5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:44 +0000

    scrape choice basic surface minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
