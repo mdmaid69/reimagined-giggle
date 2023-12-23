d07f6aa0df00a825443997209509edaecff0dae3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:24 +0000

    now either chin theirs lower

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_palindrome(s):
+        return s == s[::-1]
