e5ec68976fa7517f6cc40c105c0ab81b65b162d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:44:34 +0000

    beautiful train opponent meet woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
