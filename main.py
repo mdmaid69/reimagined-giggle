a078086cec8d862219296d03274bb8e9066256c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:20 +0000

    breakinto invitation fierce favourite accept

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
