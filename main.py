d9c1a7bf251c6f6195115e10bfdb28f1669af02f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:45:43 +0000

    paper dress check rest house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
