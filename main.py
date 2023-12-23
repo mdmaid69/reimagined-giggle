c914f61865cab9720b560f2a90846cefc89f75f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:26 +0000

    dry fill organization drop book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
