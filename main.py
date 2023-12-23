a67eb1f85fb951fc28754e306b4b63acff271417
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:00:57 +0000

    answer birth excuse contrast forever

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
