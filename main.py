07c71d5d7d752fcc2b68438c739314b8a5e82373
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:28 +0000

    yourself detail ready punishment keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
