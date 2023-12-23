108c0b02767227872929d0759a6b02a48ffe80d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:52 +0000

    finally exciting bath buy thumb

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
