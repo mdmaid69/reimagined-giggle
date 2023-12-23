90614bcee10a84c6723d4cbf59948700263e5e60
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:36 +0000

    scare children surprised expression keepoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
