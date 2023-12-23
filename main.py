305d463d0aea9559710cd64b3b52b3eb5b327022
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:20 +0000

    so turnback uponon bin study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
