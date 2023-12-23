5f87a5ce7e0ed08c1492dfb32c1e62290679f5e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:51 +0000

    square table lay roof that

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import sys
+def exit_program():
+        sys.exit()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
