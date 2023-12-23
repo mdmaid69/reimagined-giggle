bb88c8a61c2d7d6d874832451810a2e1c82899a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:46 +0000

    art least separate ill admire

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
