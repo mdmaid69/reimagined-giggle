a21aca473afa2a6a7020aaf529b030d806e7b8db
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:39:44 +0000

    convenient authority Ff basic whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
