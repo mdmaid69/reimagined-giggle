57ba412a071719b8daca29c5f48efb9498cbfa65
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:45 +0000

    getout great rest disapprove splash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
