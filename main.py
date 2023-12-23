99220ee6196bc68f422990b44b62e1aa72d3282d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:07 +0000

    pride control unlucky week tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
