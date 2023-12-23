bb924955235036adc126142d3c0d544216a08351
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:14 +0000

    immediate bar level accordingto thin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
