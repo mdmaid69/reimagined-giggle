19d52ebf8347f0f971702c67a4747d9674db0f6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:37 +0000

    protest Ii uponon flood 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+name = "Python"
+print("Hello,", name)
