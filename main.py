be25fd97bc2f2fe3ba3899f35b42b4d9adf1c582
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:15:51 +0000

    afternoon breakout absolutely change man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
