a4705addc6cb3204eb7037847488a03446c8487c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:16 +0000

    lesson fairly development callout skill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
