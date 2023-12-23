a8d858e01d0aaf5708c9f57ed484a15bc5ae31b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:31 +0000

    bite rare try rank experience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
