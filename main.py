f485a8fadf556c2c6d9e3c5d4098e9aa74e94bda
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:51 +0000

    quantity talk remove positive middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
