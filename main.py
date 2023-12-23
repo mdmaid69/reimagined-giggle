ca34482db50f9b8489139f8c0a5f62b127ae71f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:31 +0000

    whistle peace block join recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def is_palindrome(s):
-        return s == s[::-1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
