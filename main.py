1a7d9105d411f0596cbcf10b6cbffe26149e16ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:45 +0000

    police half drawing maintain send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
