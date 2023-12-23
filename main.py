0bc21a1349881bc7975d1d325db05bb173b1dcab
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:10 +0000

    tool crisis oil wide brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
