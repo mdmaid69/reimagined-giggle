8261d0a6c4c488c0e1ae3ccf7b3579c7c74b34fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:05 +0000

    fly news great cause anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_error_function(x):
+        return math.erf(x)
