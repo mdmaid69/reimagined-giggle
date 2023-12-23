40b821717c73fd69c7d4fb0fbf6368aa3491cd2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:15:13 +0000

    turn valuable dish contrast performance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
