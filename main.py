275124f30f4b3ed23d5b552a2d36cf6abb9b93d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:03 +0000

    rescue Ll reasonable keepaway right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
