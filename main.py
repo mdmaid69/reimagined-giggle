12087823f53a68971bd1929052d7e648e78f3b9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:42 +0000

    argue now brush earn evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
