0e6201b0e7a9ae600dd81c12267a03f843ea728f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:26 +0000

    able group point boat top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
