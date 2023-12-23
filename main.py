a9b26457160a16e39ad752acb7a83269a0b8d2e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:17:54 +0000

    ocean laugh debt distant lookup

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_pressure(force, area):
+        return force / area
