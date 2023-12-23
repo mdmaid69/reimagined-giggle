5a8bae5883bca9d0293b6221742954e12876bef8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:17:43 +0000

    important apologizeapologise shelter yes connected

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
