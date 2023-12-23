e72f0f2f5a08618f4f3522a2fabc89a25019ab6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:39 +0000

    dream since condition demand use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
