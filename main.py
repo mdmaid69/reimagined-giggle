887b705c35113bd095ecc35b873b3489eb9056fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:50:55 +0000

    closed cold organize know independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
