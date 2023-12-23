4adb9813f3fbe9d72bf5952cef0f788c67f196e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:16 +0000

    thinkthrough birth lean check light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
