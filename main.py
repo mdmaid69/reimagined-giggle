2bb730bbb79a41969764bd3f87f68960271f1d1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:42:17 +0000

    solve cheek direct even ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def check_if_array_contains_item(array, item):
+        return item in array
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
