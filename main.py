3596d2f228bfc834d9148d2b4cc99d3dd450767a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:46 +0000

    ask key last after example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_floor(x):
+        return math.floor(x)
