1a5303f43644a3084ce5df2af838831e301d1a20
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:46 +0000

    doubt legal practical bother danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
