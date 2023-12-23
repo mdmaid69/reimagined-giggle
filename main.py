8f7128fd6f9b76a400b83a285a1109bd684fe656
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:08 +0000

    think tap knowledge authority bury

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_stack():
-        return collections.deque()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def add_numbers(a, b):
+        return a + b
