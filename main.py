170026ac591591828d8252346630397f8ece926b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:28 +0000

    shade almost quick dry female

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
