2faac8d3435042b2970bdcc0b44dfec9dc02eec9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:43 +0000

    season learn pain plan sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
