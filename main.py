8ed1d41a9f2b4c36cd999a874bfe7aa0166599c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:48 +0000

    pump far turn speed claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
