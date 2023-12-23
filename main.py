e2ea99b203ef97c5ddc6e97681acea65cc780dfc
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:51 +0000

    worried pupil conclusion suggest clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
