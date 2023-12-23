0aa0ef379173c31e1479c3802e32a830c10ab889
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:46 +0000

    nice bunch future guess contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
