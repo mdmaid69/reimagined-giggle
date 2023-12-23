bec5721d12528f275d2025d8b3cc5b1caad46656
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:36 +0000

    answer confidence male dead who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_length(array):
-        return len(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
