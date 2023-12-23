ccf3064b00eea287b0fe46c0e472f025a452aeec
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:02 +0000

    appear rid film dish them

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_pressure(force, area):
+        return force / area
