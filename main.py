8cf94897761cf386d1cabc81717a11ed7a9ad236
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:47 +0000

    power afford develop terrible scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
