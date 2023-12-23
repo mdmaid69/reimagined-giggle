fae737a3b53478fbfe9a344db96696ca207efd07
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:22 +0000

    pump lock hungry people sign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import array
-def clear_array(array):
-        array *= 0
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+print(os.getcwd())
