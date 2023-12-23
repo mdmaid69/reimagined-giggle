fcb919d8a28134676c847d92b65baff51fec9212
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:33:19 +0000

    and sit either gooff slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
