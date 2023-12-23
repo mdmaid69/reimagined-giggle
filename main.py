d65789f7ac99b7b3e98dba54eac19b23e72fecb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:23 +0000

    pressure address back frightened him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
