6e40d2d8b2e28b4b8c7b14e76328c7b96c5b1db8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:36:46 +0000

    question rely afraid serious promise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
