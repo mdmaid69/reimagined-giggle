2163ea7b6c9971c18b2953cc4497cd9c3e17ae2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:48 +0000

    judge pocket now comedown neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
