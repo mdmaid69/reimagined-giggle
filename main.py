394bc28c9eb8ae93f44d78633a1685d45c64416d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:45 +0000

    aim instrument also computer slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
