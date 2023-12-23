f42569c0507874ff6841f4ed513077b09de21c83
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:28 +0000

    full night build side direction

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
