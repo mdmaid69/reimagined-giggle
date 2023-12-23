0d184305ef406b335e096e39ee13b509a47369ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:36 +0000

    gas produce please rarely beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
