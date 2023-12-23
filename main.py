6861a3d11a61416575661fc75b7c34cf9992b505
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:05:47 +0000

    reduce of violence reasonable on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import logging
-def log_message(message):
-        logging.info(message)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def convert_to_octal(n):
+        return oct(n)
