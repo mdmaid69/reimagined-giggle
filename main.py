4574c36f1a28f88020cb93334c3a7cffcd0f8978
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:24 +0000

    heart turnup gentle Uu each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-print(os.getcwd())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
