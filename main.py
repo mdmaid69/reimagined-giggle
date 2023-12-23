277fc5ecc42560132f02cdeae9fd356446b6341b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:30 +0000

    new although treat godown raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_words(sentence):
+        return len(sentence.split())
