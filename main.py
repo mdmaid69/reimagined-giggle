e59d77ee43edb726649b4361dc24fd52b9fdbae1
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:33 +0000

    dear train pretty geton but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
