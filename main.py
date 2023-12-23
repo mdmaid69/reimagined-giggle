fa0853a484817458b017ef00bf26595e2d86564f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:41 +0000

    boat grateful could he downstairs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def print_python_version():
+        print(sys.version)
