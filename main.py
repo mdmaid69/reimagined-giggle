7c3c1ad39393655e0de837849bc64a143681d1a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:13 +0000

    lot chin finish onto probably

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print(math.pi)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
