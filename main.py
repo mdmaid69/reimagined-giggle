634d868c5184f23776615936d6ca266696df7588
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:27 +0000

    active welcome object them pull

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
