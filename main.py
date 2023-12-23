62d2e3586c525babe406f06684ef64115c09425a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:29 +0000

    best present nurse storm after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
