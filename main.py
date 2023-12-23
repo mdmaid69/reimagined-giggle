421e12715a2ced8e483f66189f7f521ac108eb21
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:35 +0000

    operate performance left though lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
