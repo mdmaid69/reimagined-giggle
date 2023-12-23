fb360a5766b9055205f2313e791de5b56ff90999
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:48:15 +0000

    sweet interrupt shade performance exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
