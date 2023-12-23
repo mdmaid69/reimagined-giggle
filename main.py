9a35e5ec50c504e275e9a7606da4f2ec8cce5837
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:32 +0000

    whole call break tip unite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
