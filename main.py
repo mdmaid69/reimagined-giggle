943e8bb21eacd71af041256c249c466d9ccfbfae
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:56 +0000

    introduce round temper fit shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
