6fcf94633821f9da05f9169203b527e2d2cea62f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:47:37 +0000

    help clear before business note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import numpy as np
+print(np.array([1, 2, 3]))
