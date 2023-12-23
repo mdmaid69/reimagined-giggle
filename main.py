4f853696eec75f8e872641dd5e0564e21f574476
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:17 +0000

    worse urgent apologizeapologise bite confident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
+  def remove_duplicates(lst):
+        return list(set(lst))
