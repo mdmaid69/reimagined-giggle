13d556c55f724b48539ec11e9a748a443fa599a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:46:27 +0000

    thumb description they discussion confidence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def count_elements(lst):
-        return len(lst)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
