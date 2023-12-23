24a3c5ea2854974b0ae55c085d3a14e4ce075ef4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:38 +0000

    shut solve unfair at arrive

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def add_numbers(x, y):
+        return x + y
