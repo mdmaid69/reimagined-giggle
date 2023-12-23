5f6e00d23a291a11146928fd4af182f32d57255f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:38 +0000

    store force usual habit chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
