70a2eade862b82c3afe45464fffcad406624e5ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:35 +0000

    straight meanwhile recover use compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
