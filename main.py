5cd5042ca486133bd99559b6357f46a2da9295f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:11:04 +0000

    involve settle command everywhere accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+for i in range(5):
+        print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
