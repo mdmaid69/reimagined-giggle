a2f57fc08f2c70ebfd7d089de7c52ba887f5ebd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:41:28 +0000

    own minimum man patience breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def sort_list(lst):
+        return sorted(lst)
