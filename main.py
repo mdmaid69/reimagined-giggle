44e62a21983628c70f6ce7e2c34de989b83face8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:48 +0000

    big relative language such even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
