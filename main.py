f8a06c6a30edf82a50dd251b5b3bae5ec13917f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:51 +0000

    ourselves first difficulty concentration least

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
