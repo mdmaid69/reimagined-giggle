2dbaffbba62ba2f978392b10159abe703c1e418b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:29 +0000

    impression order weak weather say

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
