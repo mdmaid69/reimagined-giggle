87e66550f5e1808cbc3f87dde4012d6560a74386
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:50:24 +0000

    drive immediately kind murder mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def reverse_string(s):
+        return s[::-1]
