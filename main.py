3d382c23b36b4321d0a2b85bbc2cd06dc48e9d97
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:11 +0000

    crime measure sit dare callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
