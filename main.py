1a5ea79ae5435b7f399078c33aba0c46f4483aa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:38:40 +0000

    believe might message calloff spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
