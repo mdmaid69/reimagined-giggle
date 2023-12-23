7e48e95b7d13e08ad87e3d445864bbaaddbf74ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:27 +0000

    pupil herself front mother include

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def is_odd(n):
+        return n % 2 != 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
