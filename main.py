1252a0e502b83c9857296d030ef09f4db6074dee
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:10 +0000

    event rich male lend regret

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
