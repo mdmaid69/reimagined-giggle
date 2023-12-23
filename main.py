8cf9692bc3a195ecaa701a16165d4625fcbb2d91
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:59 +0000

    climb keepaway rare depend land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
