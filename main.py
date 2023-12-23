94b57a5ec41078176969ea5eb45de78a3f50531b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:50:16 +0000

    worst remark getoff new sink

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import logging
 def setup_logging(level):
         logging.basicConfig(level=level)
