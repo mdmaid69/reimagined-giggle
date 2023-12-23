31dc3953062588a8acc80d0475a2d08100dae911
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:07 +0000

    software artificial argue destroy follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
