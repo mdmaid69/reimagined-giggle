3f03573edd454f3cb5952cc7d10ff8012226aa9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:04:13 +0000

    target flood its professional fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
