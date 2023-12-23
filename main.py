cb10273d629f57abc5535f8c5ae941c852f47939
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:43 +0000

    flow like surprise discuss challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
