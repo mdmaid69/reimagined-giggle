cb706e2d602e71f8baa5053e32b2414e1b4110d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:14 +0000

    blind event refuse estimate declaration

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
