2c1d6ad70f585dabb2550d22ee5623bd5c6664f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:02 +0000

    fire Ee question interesting contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sys
-print(sys.version)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def square_number(x):
+        return x**2
