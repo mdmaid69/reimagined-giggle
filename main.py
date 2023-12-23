2d47aadfd0547f6dcf622adb525bae16fbc8d2a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:05 +0000

    effort base duty everywhere positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_min(numbers):
+        return min(numbers)
