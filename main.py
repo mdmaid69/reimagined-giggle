c9df1a7659d3c34dc44b65f8fb9ac5ccbe112468
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:17:56 +0000

    Ii complaint neither accident storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
