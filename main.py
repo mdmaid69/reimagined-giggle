03faaae8c1047f5d1b3234012a38e5d62a860386
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:14 +0000

    performance disagree object crash thorough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
