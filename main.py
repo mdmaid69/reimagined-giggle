141f529d59994a62b411c745533a9187fea5175e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:36 +0000

    thinkover perhaps another fun snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
