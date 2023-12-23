3ee6aea4537c9fecc73d90cd52840b3146a69d3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:49 +0000

    grateful workupto room perhaps true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
