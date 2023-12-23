09d0f5b2fb78eef264fa140ade38404ef0eb4eff
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:44 +0000

    bother fresh mysterious show workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
