08b760c61caaafd5b1b9ac58b9a636feec7eaae2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:23 +0000

    right content ordinary free machine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
