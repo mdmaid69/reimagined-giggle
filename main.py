90fcb72dd21ff0aa065bf12f00fb0e7ee9e013c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:17:45 +0000

    production accommodation combine that go

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
