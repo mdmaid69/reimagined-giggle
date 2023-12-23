46a1e44a2ec5d3e5c5a4da2e1af21bd413518042
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:19:04 +0000

    finally wise fond punishment you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
