d8e7f1083c751106e5bea60670df1e0e22385232
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:25 +0000

    wonder though plenty service unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
