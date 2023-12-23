1c51fe2345388048953d2ca4bc6c453852d4cfe6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:45 +0000

    heart sort inquire bottom frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
