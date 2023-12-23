b6972ac5675d0301b1a529603edced405a839c9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:05 +0000

    offer ring pleasant floor anything

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
