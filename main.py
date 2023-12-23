2d832b038041c04e5e6b79d0299c762cd87f410c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:45 +0000

    fight list smart makeinto happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
