22264b5b8b610c2b14b78de12373dca05c4abfd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:20:30 +0000

    nigga hurt culture figure opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
