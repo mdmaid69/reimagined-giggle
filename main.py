362e7f8c80170c0be45158e257213c02b710b7a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:40:42 +0000

    trap about private shape phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
