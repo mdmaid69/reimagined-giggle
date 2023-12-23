991c16bd34944ae14264cd5d63371b6db25bdd4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:04 +0000

    child emotion comment 571enough software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
