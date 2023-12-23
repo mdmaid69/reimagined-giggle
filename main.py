08c0204157026dce8037c8747ec591c1b1f619d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:48 +0000

    loud club chance point on

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_force(mass, acceleration):
+        return mass * acceleration
