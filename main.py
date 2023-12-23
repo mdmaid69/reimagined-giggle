050fbaf500415608c8fcac06e0a4ca56eb198d51
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:33 +0000

    calm tune but harm fresh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
