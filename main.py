a201e866368e72582f09602dba63c27bd8d75a1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:03 +0000

    will swim release turninto tonight

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import logging
-def log_message(message):
-        logging.info(message)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
