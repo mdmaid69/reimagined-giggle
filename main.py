ae70002248b53f6656c946eff75ec99c07b7dc50
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:38:31 +0000

    band quick official mind plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
