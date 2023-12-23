3cf825207e544c1bbddff632c4a85512f1a8d53c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:01 +0000

    even pay flat leg still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
