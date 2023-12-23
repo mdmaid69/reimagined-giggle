6f06773bdcb9d89dfe16dbcd8bca3ebda23355bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:46:11 +0000

    level deep left brush in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
