738bddb2be287f612a50ed05ccc86f2d4a8f4794
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:27:56 +0000

    sell describe Ee best ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def convert_to_binary(n):
+        return bin(n)
