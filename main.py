cf447aa1deaa3cbfefb5a13e5cd1ec55f9329095
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:25 +0000

    before getout production storm bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
