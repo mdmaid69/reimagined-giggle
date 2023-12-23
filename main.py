4fcf767e6263929a8f68b59fdbd70e5f74395e4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:20 +0000

    exam company innocent substance fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
