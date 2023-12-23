700e88d8103fddcb3bc8debb81c5c875a6b35851
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:53:54 +0000

    account feature there prefer callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
