49b1e086f80a31a3ae9e5f001308a3f58809e79e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:19 +0000

    anybody scared certainly brave spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def convert_to_binary(n):
+        return bin(n)
