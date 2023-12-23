4a270046eaf523d8583fa0dace7cd5457ad2e5af
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:40 +0000

    direct knowledge damage along various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
