5b8b378faa3add290d565ba58f8a9ed7801f691e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:38 +0000

    physical desperate impatient wrong appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
