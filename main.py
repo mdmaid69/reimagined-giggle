b3740ccab7ce6b157c420160c6bbe56ab5b3922f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:50 +0000

    scale fork number crowded case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
