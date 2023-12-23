df388e954706d23b11078b705ae90de8c4a9eb8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:49 +0000

    connect unfair lie recognize dig

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_power(work, time):
+        return work / time
