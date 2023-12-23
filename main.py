f1886cb82fea09c33cfd06381d7990c6dd8013ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:11:23 +0000

    Qq convince appearance culture workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
