5f7ad990f74bd199205d3340d8690afae8118361
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:42 +0000

    burst area opportunity demonstrate score

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
