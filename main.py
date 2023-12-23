1d129e1dd426793a0f37a86958d6d5ce25ccc24a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:57:12 +0000

    cover behind average level half

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
