9818af1160a712d664529b0b75d2d0c93e68ea8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:21:05 +0000

    building smell range 568energy treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+print(os.getcwd())
