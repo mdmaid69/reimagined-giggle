c1bf77e9e734bb3264d2a88a26727d2813d588c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:17 +0000

    food curious course loose blow

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
