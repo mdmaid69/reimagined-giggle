6c0a809a8aee9cc8ce13457af9395b8e5f4b06a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:13:12 +0000

    too fact strike tradition meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
