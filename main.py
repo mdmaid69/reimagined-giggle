fec0e3c3504c3b5343bdd88a2d6caf528030f452
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:52 +0000

    fix keep Yy finish control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
