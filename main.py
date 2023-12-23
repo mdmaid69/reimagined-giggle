80c2d458c2b5dcd497a10d84f78313466cdce002
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:00:09 +0000

    health immediate discovery admire your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
