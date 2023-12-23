637ca90135accfea3c4bc6f23b8b146db1c66091
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:04 +0000

    common rich willing Qq damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
