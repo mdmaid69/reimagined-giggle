c0e33869187cd9b432071635ea219c5659e4c658
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:28 +0000

    dead field science official sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
