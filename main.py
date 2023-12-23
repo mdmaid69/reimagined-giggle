9d5c62cf7a39d99fa1c9ffaad70986813686232b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:59 +0000

    foreign cause climb juice evidence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
