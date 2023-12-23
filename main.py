e4f7f1ac5c66101f33721c6de236b182ae95e66a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:14 +0000

    centre beginning wish none block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import sys
-print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
