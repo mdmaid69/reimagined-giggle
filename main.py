15785a739ae9d26ba6dae50c8c3f0683589afbad
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:16 +0000

    point shoulder preparation value traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
