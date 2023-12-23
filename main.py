dd5a807a6b523b6890a358f0ee90feb3d29aa632
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:29:45 +0000

    me past sure bottom so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
