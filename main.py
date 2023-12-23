10c53fbed3cf666c82beb3a1d674b4606e95c950
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:25 +0000

    visit anybody own forgive port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_current_directory():
+        return os.getcwd()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
