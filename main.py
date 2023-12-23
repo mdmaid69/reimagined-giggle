6aabdad10cccb2976eb0c4f7ecdec3cedf7487e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:39 +0000

    on light neither usually thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_volume(length, width, height):
+        return length * width * height
