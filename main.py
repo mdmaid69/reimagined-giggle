701ee8b80b19895bedb018a7c21cf7e41f9fbfa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:22 +0000

    shall behave improve course practical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def sort_list(lst):
+        return sorted(lst)
