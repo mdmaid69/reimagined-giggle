c61db2df2fe9dee81d6026e57ff0f727a684cdc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:54 +0000

    injury future deliver Uu help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
