84384dc276457eb583d236c9e405f58478f1aea6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:41 +0000

    chat ground announce damage level

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print(sum(range(10)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def add_numbers(x, y):
+        return x + y
