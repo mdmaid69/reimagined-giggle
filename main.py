d9b9a1e17a18c5b89eeb4a3c3dff517692bd20b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:58 +0000

    create corner false his disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
