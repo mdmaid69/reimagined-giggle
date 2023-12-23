f5b9158a856a8ecc7ac508fcc669b4c2a9a288e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:54:59 +0000

    sore run pool just fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_str(array):
+        return str(array)
