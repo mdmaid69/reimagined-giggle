4d77c5dc06622e2f9de6a303ac4563289d75d1eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:41 +0000

    war truth rather unlikely plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def count_elements(lst):
+        return len(lst)
