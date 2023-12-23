2678cf6fba9da8c8e1355b6982d9bf78637fe03a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:30 +0000

    lay undo square no limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
