3af7b45d1f82a210b6ead779271c8e21e4d0b10a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:30 +0000

    upset hand address capital birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_as_str(array):
+        return str(array)
