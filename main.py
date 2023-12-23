0a1635ecb314c08d4a1378f43d05c5be6fe737d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:33 +0000

    judge connection live comment deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
