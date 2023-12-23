5a0db141b8fc8d0fa246c59c0e287789f2c839d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:47 +0000

    progress broad float put face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def cube_number(x):
+        return x**3
