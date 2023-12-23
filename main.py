fa12665b599439c3973ff8ddd6dc4fef419229db
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:19 +0000

    owe characteristic appearance criticism learn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
