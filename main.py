24ee2dfbae1cdaced68bf86af2cef01f7e898603
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:46 +0000

    worst off tall shout sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
