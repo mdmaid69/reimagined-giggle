87fb5d7ed28e092ec6e47833fd9d2164c528882a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:32:10 +0000

    warm argument in level gentle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_pressure(force, area):
-        return force / area
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
