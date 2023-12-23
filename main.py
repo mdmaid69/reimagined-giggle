4c203f9af969eb9ce1c4540b7aa429eeb442685a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:19:49 +0000

    ability both message needle feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
