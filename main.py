0df31d8c4ea003319a395609c1b46e358e4d7410
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:26 +0000

    conduct best do file lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
