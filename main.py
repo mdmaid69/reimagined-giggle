33ac32b09908049b5f0b6089e8f500807b390b10
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:26:43 +0000

    wind judge cross post Dd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
