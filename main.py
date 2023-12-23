a32600369933df04ffcb41f5cc5994fca7641cf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:25 +0000

    bottom beneath public fierce letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def find_min(numbers):
+        return min(numbers)
