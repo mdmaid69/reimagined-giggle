72ebe0070151f1a225977e7f478bef4fbfc7711c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:13 +0000

    result used persuade even able

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def greet(name):
+        print(f"Hello, {name}!")
+text = "Hello, world!"
+print("Words:", len(text.split()))
