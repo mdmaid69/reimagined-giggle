254072b6ec3143ad5485c3be0aefa9c4fe10a082
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:31 +0000

    miss delicate spray guard cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
