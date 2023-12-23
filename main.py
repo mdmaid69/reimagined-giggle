b8a0bffc5c4b3ef319998165c7172f039f379442
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:41 +0000

    substance scrap realize smoke turn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
