32826d2a2e2378485e7a1a92bf4caf510c747cd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:52 +0000

    see picture once bottom bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def multiply_numbers(x, y):
+        return x * y
