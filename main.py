b3f2a9521d76b3254b9598a3ccca5b5253783e43
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:44 +0000

    invite delicate someone downstairs please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def convert_to_octal(n):
-        return oct(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
