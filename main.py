e14dd96c9d16c318693aaece7d68b67ca83641c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:41 +0000

    again heavy stage getup offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
