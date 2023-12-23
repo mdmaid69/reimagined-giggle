9374436eb24b0e20cc9f7033a7bf5323aa0cfb9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:36 +0000

    short work realize activity note

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-print(time.time())
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
