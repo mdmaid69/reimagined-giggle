1eefbbf8389d62e122ae90e98ac36818fa1b339a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:26:20 +0000

    give speech point callback belong

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def convert_to_binary(n):
+        return bin(n)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
