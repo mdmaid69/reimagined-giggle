52bc99138d4bde4e69f4f3f530245f4b72d2ea54
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:50:11 +0000

    sight receive damp remove comeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def reverse_string(s):
+        return s[::-1]
