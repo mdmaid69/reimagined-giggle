04d5945569d21bc0eb4cda63b173cd1a67920453
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:45:04 +0000

    information narrow great trousers skill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
