699678989244abdd345242f854d12138b21c3d2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:19:31 +0000

    build sale glass across sign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
