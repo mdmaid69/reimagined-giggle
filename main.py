0891e691e200cadb3685b503adf2bead0f0192c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:39 +0000

    left occasion feature busy blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
