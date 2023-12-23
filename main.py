19b31d2c1fdb1cd57cf5d929c07fbfd59d310407
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:26 +0000

    they divide we good attitude

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_hex(n):
+        return hex(n)
