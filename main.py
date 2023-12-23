001aa4e54f939e4dbc3ef4d38bd1a344ced70166
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:51:07 +0000

    maybe back manner alone exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
