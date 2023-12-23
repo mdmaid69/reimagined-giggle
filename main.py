67049a8a64ed73ab0abea6b803f081535ca5c24d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:11:54 +0000

    heat freedom used sink rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
