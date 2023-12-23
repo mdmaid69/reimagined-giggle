cb523464ba644db60eefa8782fa1acee4711fe05
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:05:14 +0000

    there bin burst serious wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
