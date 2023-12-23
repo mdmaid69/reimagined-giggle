1372268ca12899e445ed52294a69f7671472cf44
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:01:47 +0000

    comfortable rather exit correct failure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
