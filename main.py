e99258ffe0a7b8c9c8d33a0c90f6f3ba5790a2cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:34 +0000

    Dd environment used evening quiet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item(array, i):
+        return array[i]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
