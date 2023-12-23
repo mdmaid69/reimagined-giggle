1f389ab0917a19ea9891f87c322a9f1a27e9f66e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:35:16 +0000

    become surprised freeze exist opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
