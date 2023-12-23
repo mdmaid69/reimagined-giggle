a310aaa9bbc9048997745b34cd2e8d6904d33ecc
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:01 +0000

    sing hair successful field offence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
