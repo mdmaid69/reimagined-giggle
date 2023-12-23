6602131092b12b80a737f91e745fd52b70f29674
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:11 +0000

    offer passage independent classroom rather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
