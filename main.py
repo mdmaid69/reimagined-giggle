2a04f5cb4e0dac58bb33ca5f2a3c19bfa51a5fb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:50:27 +0000

    level giveaway vanish cry design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
