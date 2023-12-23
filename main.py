fffc072a9822c6b575755480aa904e7308c6dde6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:52 +0000

    wheel bang bad ours wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
