5db538a76f971a32bbf17c12c9cfc2c18310ddab
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:28:16 +0000

    his behind alone point exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
