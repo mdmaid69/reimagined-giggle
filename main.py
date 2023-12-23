e4de383e80d2c94a5fefd0ab9fd6d79da669a590
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:02 +0000

    history careful card represent else

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def cube_number(x):
+        return x**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
