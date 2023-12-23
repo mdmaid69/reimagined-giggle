da2d10b8c42508a8f6d3bbc0cd80d2e15e4785cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:04 +0000

    take come taste warm substance

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
