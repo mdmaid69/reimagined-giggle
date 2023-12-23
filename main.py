7b87fc4f75baaf77f9e52739cc6012d5da6fd089
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:30 +0000

    correction contact capital the create

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
