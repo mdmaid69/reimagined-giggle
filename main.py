038a44d9fd86853ce69048bcc2f832ea0d517b82
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:10 +0000

    quick classroom personal level comeover

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def find_min(lst):
-        return min(lst)
-print([x**2 for x in range(10)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_current_directory():
+        return os.getcwd()
