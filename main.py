c49f2bdc60da4aca36b699602337cad7d92b858d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:53 +0000

    influence change regret clean normal

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
