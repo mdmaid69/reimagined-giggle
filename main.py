6df74177ef71b98ad6442aae4cd49ebc1abe38f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:40:44 +0000

    yesterday house really meanwhile abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def clear_array(array):
-        array *= 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
