ccfc38ebc91601f065b25c5e498874a97855668d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:22 +0000

    project fast exit sit warm

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
