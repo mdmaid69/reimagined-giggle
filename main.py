fe99980375924f0d4b6c0a1451a41470d604f6fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:58 +0000

    over active reject bang Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
