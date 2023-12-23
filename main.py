19ed46326b9d430f83ae657aec314cbc6c13184e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:04 +0000

    pretty except absolutely confuse combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
