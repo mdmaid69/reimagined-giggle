183ff405eb04904d9437b7b46bb93245cf371806
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:31:25 +0000

    lookaround arrange roll block dry

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
