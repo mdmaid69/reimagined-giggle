47432f473fa97ccdc4734cc2b337c8ad0ad16209
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:57:58 +0000

    extraordinary approve impression last pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
