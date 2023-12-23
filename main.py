0a46ccc5f103ce76d25b2eee149e280350acfc07
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:05 +0000

    female large worried pass row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
