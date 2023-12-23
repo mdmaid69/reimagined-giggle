e7e5ce1866d321a3257c3c958c21abca7dcb4130
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:47 +0000

    question load acceptable modern split

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
