9da887777b17c5aad6204c3621e2777fb261e78e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:33 +0000

    be genuine his interesting write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
