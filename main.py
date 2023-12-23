f738e0353460c64d188f5536098e858bc573efee
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:59:59 +0000

    tired door thumb uponon concerning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
