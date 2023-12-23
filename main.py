c1a4a1019d1d285ce13b6da5df5723c6b1fc3264
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:25 +0000

    advice quit insist difficulty feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
