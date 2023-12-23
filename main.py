637256e8bd74bf2e1c88d581e3ee088b9016037d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:01:14 +0000

    emphasize skill your perfect comfortable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
