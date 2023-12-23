9363f466962a366a751996dda80e9c8072778b45
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:00:36 +0000

    bit range shape difficult tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_rectangle(l, w):
+        return l * w
