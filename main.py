88d12ad2fa21ad6ebcfc2fd5b7b3ae5501b731e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:19 +0000

    height several length step possession

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
