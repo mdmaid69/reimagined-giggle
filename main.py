46ce8c2bd62dbdf454edb09fe4e924bc8b491552
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:14 +0000

    unit impatient should protection back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
