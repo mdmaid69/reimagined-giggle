c71022a1af7046bfb8326513626088f5633d0971
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:24:38 +0000

    alone obvious risk conclusion journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
