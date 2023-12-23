4e3ebe4335b0c08323db5747b6966280d8807701
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:40:27 +0000

    fall hide power fact wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
