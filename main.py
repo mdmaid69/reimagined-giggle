89000380f645e46694d9a99098716db4d969f4d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:39 +0000

    miss harm protect workout past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def subtract_numbers(x, y):
-        return x - y
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
