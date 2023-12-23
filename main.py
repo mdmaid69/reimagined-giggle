5ee6b2c08837489f6402d177d5a2423ab2fe2c62
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:59 +0000

    wall spend pair terrible row

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_max(lst):
+        return max(lst)
