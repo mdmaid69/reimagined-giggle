2a1463d2111bc29b21e0261f8848176c5f838b9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:03:50 +0000

    create hold since wander correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_work(force, distance):
+        return force * distance
