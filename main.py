da956f253422d300ff571dda15ea2cd2e4dd823b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:13 +0000

    takeover model town suck poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def reverse_string(s):
-        return s[::-1]
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
