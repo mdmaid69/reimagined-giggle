6a822945293bbbab71a3c980381c51b53c41406b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:45 +0000

    afford more healthy ill prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
