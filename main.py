9e098ecfcbfd1797fffdf648ee06ba1a14736211
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:20:47 +0000

    tour influence congratulate activity detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
