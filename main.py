4778a1107aa368424e3bb8778cca85ac1f177ded
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:13:44 +0000

    degree health trade war positive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
