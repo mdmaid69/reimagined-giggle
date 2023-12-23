33506c41e611d89f140e4424f833d8f97ddf207a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:45:59 +0000

    paper boil Yy limit level

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
