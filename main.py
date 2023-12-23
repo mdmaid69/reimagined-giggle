1807b8c681d01ed8a5e820e460cbf90aaff8c373
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:12:56 +0000

    breath tell before forget matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
