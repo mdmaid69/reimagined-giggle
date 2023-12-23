7aeac3ca3fb007ae7a99936a3719b6c1870e91b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:00 +0000

    persuade pencil fail bin prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
