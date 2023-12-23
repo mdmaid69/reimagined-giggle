ecdbf714a62a078b5634b7e7fb3aae154ec14836
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:34 +0000

    week cruelty dance hurt confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def reverse_string(s):
+        return s[::-1]
