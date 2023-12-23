d64ef1fbda78deb8743204f0c77a851a1de550dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:11:25 +0000

    aim interrupt recovery bear risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
