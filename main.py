af0da47cb67456da1ce095a532568c76ba79deb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:23 +0000

    table cap pronunciation female attempt

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
