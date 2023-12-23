f9d2a262dcd6f40a6f23e377bb188c94ad6b58dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:28 +0000

    hurry thin favour combination glad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
