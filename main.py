5395dbaebfb7c3d78868c08af43645daa7a3ef6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:06 +0000

    Vv lost travel help exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import platform
+def get_python_version():
+        return platform.python_version()
