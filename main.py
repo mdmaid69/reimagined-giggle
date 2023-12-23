b878daf52467ede201a492755f5c6c6adc996c56
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:57 +0000

    beside plenty luck thank every

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
