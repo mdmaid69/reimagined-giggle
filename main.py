f01e300d4db8dd9b49d43be1e491a2c6753df54d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:39:54 +0000

    should genuine possible voice expression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
