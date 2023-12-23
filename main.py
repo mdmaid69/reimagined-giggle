187f1addee93ae03b76ca50d56b756ad5f3ffa90
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:42:03 +0000

    sale rest to contribute adjective

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
