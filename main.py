d412b55cd09fb88f3e05954d1fef0f648ecf3707
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:43 +0000

    winter key seat behind term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
