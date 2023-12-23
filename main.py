24a3e0cc59f686e95d6c9a6f45af76fc68d6c5b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:36:53 +0000

    exam confusing ago goon wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
