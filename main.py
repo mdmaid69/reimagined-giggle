abd17d154ae50d03e73ad403fd8d7c512505d8b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:12 +0000

    arrange above whisper fresh corner

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
