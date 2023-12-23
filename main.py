e11aa84dcf5c6f3e32b08550158d87f8cc787c9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:01 +0000

    average hold squash leg immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
