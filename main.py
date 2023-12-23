8fe2e212fa5521d5bfcfddcd64928d7f73b42802
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:58:21 +0000

    value chief goon freeze help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
