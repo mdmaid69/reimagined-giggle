3c5478056d1fab35717b82a24f6882e9ac8fadc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:41 +0000

    modern opponent stick stranger true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
