94a06b6b2bfb3c6e554233acadd300e2d64e94dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:06 +0000

    nose back Pp workout cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
