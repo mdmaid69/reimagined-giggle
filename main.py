b3d913fe048753fd2c6496805ca4ba3ef7f97651
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:56 +0000

    sting call tend doubt shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import os
+def get_current_working_directory():
+        return os.getcwd()
