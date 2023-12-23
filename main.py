7397d508261f17e53067559e5d7d282f1bc51a44
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:39 +0000

    grand boat concerned squeeze adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
