2ebc98cad4c36ae5b256530471f1395f93299356
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:26 +0000

    past drawing grade dry development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
