472300caf40e376a2acd05c776658a993dee3eee
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:15 +0000

    trip nose length collect experience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
