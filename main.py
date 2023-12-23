950199bcef72848c094f3330cc5e55be163edeb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:51:57 +0000

    trap shade thinkthrough wave keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
