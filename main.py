cc64baeb93a5dca857447383c1ddaed224239a1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:58 +0000

    brief wherever later unit guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+print(os.getcwd())
