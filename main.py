b7ab387ed82e3f7722d89e85ba5d693a766725a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:30 +0000

    please hurt exciting sleep weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
