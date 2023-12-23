cdaacb734fbbc52a976863e9b859493146c30f67
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:06:44 +0000

    sound breakinto bet contents muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
