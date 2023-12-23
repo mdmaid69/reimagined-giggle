e537f0a24cca8c7b604a891906b8c4de9fe49faa
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:22 +0000

    equal official speed sweep round

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
