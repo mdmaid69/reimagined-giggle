8a87aeb28364115c5c7512abd673a13e4922eaa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:29 +0000

    fortunate pipe sick how pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
