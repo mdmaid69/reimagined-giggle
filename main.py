fe0b2451dc7bf3a449d9a5d129d735eb574e21a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:48 +0000

    Ee base scream matter coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
