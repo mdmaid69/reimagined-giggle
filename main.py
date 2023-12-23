6065224fd5b0ddd29dc3ef9505250d2f8fe63a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:49 +0000

    home show light turn clearly

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-print("Hello, world!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
