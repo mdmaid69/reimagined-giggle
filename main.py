7d97f9c4bfb621ea3f41e6a908ed4b83bd520903
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:57 +0000

    mass concentration plastic arm pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import random
 def flip_coin():
         return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
