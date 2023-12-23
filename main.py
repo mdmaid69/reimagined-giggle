86e639d0bac1bc6471c5434bd56c1223cd81f102
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:04:05 +0000

    flow around ear card natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
