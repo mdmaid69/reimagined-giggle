0737f46db426d28f15f7cf954ba197b59af9a5ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:38 +0000

    nothing influence equipment estimate satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-print(os.getcwd())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
