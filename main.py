404b44cdc618c579f899674cc9dcc1a6a1af473f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:46:16 +0000

    sail special pronunciation accidental share

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
