bd985315551c3fe1e38901508e0ccb4839deb4ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:47 +0000

    hand value future risk wide

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-print(datetime.datetime.now())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
