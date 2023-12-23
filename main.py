50a6ab84b17142fc9ce9b063d9947a9a4d999b15
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:10:19 +0000

    hungry miss mark interesting advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
