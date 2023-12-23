0c0c371bb4dddfaed5b8129cc6db0c8c4971ee53
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:33 +0000

    decision Ii search recent sticky

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-def print_python_version():
-        print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
