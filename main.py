37cd55bb3faf935b1898d0d3f9722b48f87b6276
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:36 +0000

    destroy pencil fat history blow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
