ff639688c05f9922e0cb102fc68638f960548b8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:36 +0000

    tidy choose provide bush otherwise

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
