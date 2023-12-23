81cb898c80b97306c03983ba1d8315b401e250ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:24 +0000

    565end wonder may I crisis

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_volume(length, width, height):
+        return length * width * height
