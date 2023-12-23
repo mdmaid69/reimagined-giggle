44d2a14e4cfc84e4649a2a9141595f60f6478681
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:11 +0000

    giveup bear who spring goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
