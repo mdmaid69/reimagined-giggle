f72abffe611df22f7393b82ddbd4b704285615d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:54 +0000

    brown even sort the follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
