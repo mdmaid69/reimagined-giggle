50b04744d1748ec8b21bfebc46817d732d07dc8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:01 +0000

    colour natural open middle carry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
