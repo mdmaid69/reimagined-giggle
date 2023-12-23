ba107b58757a9b3cb824a213de343df0b6fb8f18
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:36:43 +0000

    dead Mm eye wander both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
