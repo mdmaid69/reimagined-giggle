36738814b69bd16226b0d3100de0cf2937d5de61
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:10 +0000

    he by identify no damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
