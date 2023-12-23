85de1b1bf00290a5117ce2ad09dd3935674ca6e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:19:36 +0000

    union divide support gap visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
