a718fe2d90e9e0b2b06a2d4ef5c77c05a886b297
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:36 +0000

    course immediate do hurt want

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_pressure(force, area):
+        return force / area
