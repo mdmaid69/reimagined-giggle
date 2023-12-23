acd981814b38c7a2e985ae3541197a89b2c36944
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:43 +0000

    certainly blow fortunate her indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import re
-print(re.match("h.*o", "hello world"))
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
