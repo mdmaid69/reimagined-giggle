506ac905a83a2accb760f8fc97e960ba00de2244
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:44:57 +0000

    develop people preparation recently put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
