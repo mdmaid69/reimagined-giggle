290d56ef118cff065ea46d4a01b052e0ac9d4309
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:21 +0000

    project case fresh especially wing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
