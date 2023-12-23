fced6a0ca9b241667b072ddf6728bf3f71a67917
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:22 +0000

    callout downstairs back board sound

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
