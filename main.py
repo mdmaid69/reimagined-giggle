fa87f46f78d4b2816d041d569d4a547ec98033ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:42 +0000

    project scared alive game goon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def exit_program():
-        sys.exit()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
