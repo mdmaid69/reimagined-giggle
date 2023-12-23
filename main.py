a0ebfa2476aaf64a92b8a6b52c622e4ff9be6098
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:57:11 +0000

    hard admire different laugh warn

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
