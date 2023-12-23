d815b2ecf81c71f7e8e0154659a61a24b8000c32
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:28 +0000

    float rain how never connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
