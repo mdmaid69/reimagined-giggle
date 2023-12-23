e7ad3606f7011f4a03318db217f4cd6c74d59847
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:53 +0000

    top wake effort alone reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
