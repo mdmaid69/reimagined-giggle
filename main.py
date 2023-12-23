873164ff620fd9f7dcf6d8c9e1a95977db8b70a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:29 +0000

    goback(to) politics convincing lock mistake

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
