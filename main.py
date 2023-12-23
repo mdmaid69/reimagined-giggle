398828fa0cfba28657f0165a1a6955c4b65295a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:16 +0000

    design plain complete face real

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
