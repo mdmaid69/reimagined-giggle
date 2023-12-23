1016f9d2b1ad52cba44329a72ce8615445f5ff25
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:25 +0000

    movement perfect compare picture approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
