96e9e1658e1fac9dd93b8c5d60920765d3377d3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:32 +0000

    field foot moment supply still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
