fbdf9ccc2cc0b706a02251b1f5643458f00b96d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:11 +0000

    apparently before disapprove moon Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
