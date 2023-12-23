2c780a6fe8e46e492b472faf37ddedf3b946a3e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:48 +0000

    check contents worried not performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
