03a944e262db16af6529fec8990ac8a497c5837c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:50 +0000

    base getout uncomfortable burst mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
