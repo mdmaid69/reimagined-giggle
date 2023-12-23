437e907885b83f39a2228793bbe5cc5742c5d151
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:11 +0000

    before explain 569enjoy female uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
