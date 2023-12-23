6f242bf10885796b52d34f15a08fc3d0c2e08d21
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:20:23 +0000

    Oo what each year relax

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
