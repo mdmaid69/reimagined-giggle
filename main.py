8d76f8d8ca4fe0e92f6066730936bf48ba953096
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:35:48 +0000

    control callout interrupt opponent certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
