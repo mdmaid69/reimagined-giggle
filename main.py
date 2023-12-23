8532aef0f9fa1ff0e62cc861f4834e0fee7e6ace
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:05:32 +0000

    hope frame birth negative cost

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
