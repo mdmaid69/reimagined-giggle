89645f5a72b4b33633eab3c32e95bba90f7dad52
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:19 +0000

    record workout obtain piece stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-print(datetime.datetime.now())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
