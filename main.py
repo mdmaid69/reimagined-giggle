c449704075203536329d0039c906308b42454c52
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:56 +0000

    match special operation foot interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
