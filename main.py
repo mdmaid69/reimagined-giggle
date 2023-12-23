5bd84cdfe9b5754ea421d3bbea939887ad36a588
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:09:04 +0000

    definite ill Ee appeal back

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
