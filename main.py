83130eb51a3b440153debda7acf8913aea4150d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:49:57 +0000

    earn blame name lead authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def add_numbers(x, y):
+        return x + y
