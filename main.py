75be8e0eeee3fbd1d7da98aac505ff8e5c1242a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:04 +0000

    part tone statement practise replace

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
