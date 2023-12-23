96499dd193fc300fca2a07bb53e6189d1efd1408
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:36 +0000

    polite among ticket soak along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
