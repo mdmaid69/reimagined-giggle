4672f40e636796e607b05b89e487decdb694ec8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:06 +0000

    little finger unkind upwards fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
