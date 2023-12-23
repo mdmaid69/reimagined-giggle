a56a24dabff511918b051b5a8a4367c9b352e460
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:20 +0000

    minute trousers will fashion gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
