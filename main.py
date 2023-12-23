ae7d1461348140861fb4a8d07f58a875c2ca3369
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:06:15 +0000

    soak level original breakfast chase

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def is_odd(n):
+        return n % 2 != 0
