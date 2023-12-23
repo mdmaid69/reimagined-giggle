826125527b4691061ce1b0e048cc69c1f6258af2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:20 +0000

    tool transparent such tear keep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
