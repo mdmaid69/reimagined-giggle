b562bb36c882740dc34dd407a02636b5c8e87c43
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:39:55 +0000

    conclusion treat side police smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
