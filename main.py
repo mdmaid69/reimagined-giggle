136196201bc25b205d0a1a29816d3ba1b8186fa4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:39 +0000

    common heavy undo street wise

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+print("Hello, world!")
