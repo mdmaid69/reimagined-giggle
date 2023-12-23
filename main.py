fa27fac1785a7e9eecc9af8d9fc6416117e676fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:07 +0000

    exchange present this loan accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
