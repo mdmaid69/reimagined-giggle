f022b13f0e8136ba406ac6f80093ead9e697ec71
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:50 +0000

    failure slowly tap convincing look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
