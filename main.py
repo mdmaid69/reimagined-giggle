2a051ef7e21097c6245e00853216cb9a4464e279
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:41 +0000

    increase still somehow smell unless

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
