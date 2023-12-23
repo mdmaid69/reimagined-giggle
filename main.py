7bd21344dd2d106c1f6a16560ea7d90bda4a2236
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:42 +0000

    preparation some trick point comein

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
