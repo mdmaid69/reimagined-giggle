7ca3167372e5558a441879495bdbed648360ca69
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:46 +0000

    passenger speak combination temperature success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
