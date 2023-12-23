3d7d2f7e334d16bf2d4bfbb97c73713c9a1a613e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:30 +0000

    form shop mess ourselves purpose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
