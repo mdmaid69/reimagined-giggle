3d9e2af7040ac36aa9886e55e7c6918fe43ada03
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:52 +0000

    dare else guess everything splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
