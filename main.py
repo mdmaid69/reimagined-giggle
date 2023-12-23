040ad82e07e6bc4e214b0eb58ab6a967fa6c2a3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:39 +0000

    necessary expensive wrap contact sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
