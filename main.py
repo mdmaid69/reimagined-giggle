4bdde4da78ec16d3c8f905c38a7fc80cb34c5e82
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:56 +0000

    ago cold majority deep on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
