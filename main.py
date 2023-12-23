2b7510302c3536b6ec826c4ae61a468cc6ea07af
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:54:33 +0000

    name mention broad keep ago

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
