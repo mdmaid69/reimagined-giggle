965cc8c4a51eb291e6723825fe3f82d115cfc7a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:43 +0000

    flavour left middle any pen

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_binary(n):
+        return bin(n)
+def sort_numbers(numbers):
+        return sorted(numbers)
