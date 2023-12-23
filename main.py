7e5b17d526145ceeea6c5672ed13eb215649c3bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:59 +0000

    wheel guilty getup near measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
