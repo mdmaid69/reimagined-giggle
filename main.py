1bcecff9a0c90429ec2d35e8009fcc2a1c512892
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:43 +0000

    talkinto belong before edge amount

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def log_message(message):
+        logging.info(message)
