2e25945d2810438d7b10e610140002e256a9b7d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:08 +0000

    distance whole price near ring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
