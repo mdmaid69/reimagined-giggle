a79e6409914afb11f8234f29c86965edeff7e23b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:26 +0000

    rush behaviour relationship stop welcome

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
