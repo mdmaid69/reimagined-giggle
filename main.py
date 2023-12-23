62c332be7c9b26e20fa4caf10133567d2bfc4989
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:42 +0000

    like sick cover dear sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
