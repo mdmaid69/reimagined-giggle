ebe9d62f51d6295d4a001cab2053aec980ac6c06
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:09 +0000

    learn slight evening run partner

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+i = 0
+while i < 5:
+        print(i)
+        i += 1
