224e34fa1ba62e662df9bccdc78c09873d62e0d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:00 +0000

    consult nerve fly swing comeup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
