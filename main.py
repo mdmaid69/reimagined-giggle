4c5f6de505fe7847ac3e05a342b34d2dc6ae5ebd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:55 +0000

    politics partner street flow turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
