cdc4e9f6a0707a081120a8cbb16db1edbc364361
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:22:52 +0000

    realize each limit word pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
