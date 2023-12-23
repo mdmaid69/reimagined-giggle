ecf1467f66aab73cbb9ff7a220e4162ea2e9947e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:20 +0000

    machine back second carry sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
