4ae80d2f7426a338bd651d4227edad8374fae4ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:17:47 +0000

    explain us keepup shoulder freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_user_list():
+        return collections.UserList()
