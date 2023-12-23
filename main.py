24b04d1035ebe21809ab1c55936bb998e71d5676
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:27 +0000

    after land godown exciting opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
