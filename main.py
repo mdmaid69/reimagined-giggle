4642618d8b28849f54e1cec567c8d31ba787a105
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:19 +0000

    base live combine score female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
