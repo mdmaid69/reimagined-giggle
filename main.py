94aa069c3d5db8b16ee9f2e67286cd33de2c9f44
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:53 +0000

    reject suffer yet arm read

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
