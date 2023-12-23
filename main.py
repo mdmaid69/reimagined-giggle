728e59bacb8b11d03a704f43f30dd2d0475ebe8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:37:48 +0000

    base form Bb underneath related

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_acceleration(speed, time):
+        return speed / time
