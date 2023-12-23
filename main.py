858ec745543bbf1dfd57aad51174581c3eaa4826
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:47:30 +0000

    operate proud tour guess pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
