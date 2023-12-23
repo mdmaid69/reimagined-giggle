628c372d96218e7865e116074b6b8a81eb8d6c7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:11 +0000

    to breakdown early food eat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
