907f540925d479119764ce00537c9b6186adcd0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:13 +0000

    knock appearance careful talk shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
