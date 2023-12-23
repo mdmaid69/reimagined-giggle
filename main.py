20078ff128dd5307ba9952e21bfac52208d2094d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:49:05 +0000

    charge interest contact unhappy wonder

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_item(array, i):
+        return array[i]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
