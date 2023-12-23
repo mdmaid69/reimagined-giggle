60c502b695375da2ddab03eccdf288c792f884d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:42 +0000

    embarrassing dangerous someone pay prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
