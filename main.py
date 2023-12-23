882c7807ee86b58461c14099d049e735a061cdd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:28 +0000

    stir intelligent concentrate recognize separate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
