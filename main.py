4887a224138814f391b30953ae338c35516a0ac1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:13:35 +0000

    remove ear there radio quiet

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
