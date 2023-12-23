6566ae5fe5442687353169e271658558f08f548f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:25 +0000

    roof bet long old thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
