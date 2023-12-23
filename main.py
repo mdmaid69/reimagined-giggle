fb8bae5618a84015c416c1df773d52137c76fdab
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:08:28 +0000

    operate noone movement independent tap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_volume(length, width, height):
-        return length * width * height
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_list(array):
+        return list(array)
