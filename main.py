574ee76153e68c61ed1d6b1f4d9eb95746e0e401
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:02 +0000

    bright Ll suit at various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
