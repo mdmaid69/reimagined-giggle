5f9b9955a314782d853e999e44b7f4e163b7e7a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:33 +0000

    memory introduce obtain better terrible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-from collections import Counter
-print(Counter("hello world"))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
