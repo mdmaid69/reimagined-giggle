480c7f6e5cebbf784905df10dde1e9aaffb9d9eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:06 +0000

    jump wish explanation quantity flow

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
