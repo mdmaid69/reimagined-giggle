d741635ef34cbb45c32e89b4b03a38f18861b244
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:20:01 +0000

    satisfactory unfair like blow Zz

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
