fc6e4ddae610d7f28cd48c94cd9756949a3c1e40
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:58 +0000

    cough behind minute yours manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def clear_array(array):
-        array *= 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_speed(distance, time):
+        return distance / time
