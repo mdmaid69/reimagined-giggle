c3f24c6cce4102f55d071c6ab13607c998fae981
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:49:26 +0000

    race however stick Uu stage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
