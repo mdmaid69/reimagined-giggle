afaa5897e0c17bd910b0ab36bc26731ec19ce469
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:49:06 +0000

    wash move crime run tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
