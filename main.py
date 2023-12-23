5d0c73cb29bac89bbabb9943169dc5d7236fdd03
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:17 +0000

    lost statement market hers goodbye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
