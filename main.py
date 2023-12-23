201d0975c15bb21de801972a276711896b87e3fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:26 +0000

    somebody shop block opponent both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
