9de45ae0a5456f3b2da5fab477d7ef8c9d99aadb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:30 +0000

    reduction opportunity such object doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
