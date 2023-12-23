dcfb441e801d0941233365cdf4df8da856c9f08b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:02:44 +0000

    pin Hh battery welcome well

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
