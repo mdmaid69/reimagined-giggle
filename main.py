ebeba6a0ffc12fb5093cb9881c811b3b969ec06c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:25 +0000

    several answer part angry reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
