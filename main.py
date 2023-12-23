ba26f995e05b41b4f280b31fa38b5880b678ba4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:08 +0000

    any imagination minimum production human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
