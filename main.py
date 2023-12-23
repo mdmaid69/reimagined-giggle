d030fae190ebecb5a097c2f083931f8db7314c3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:21 +0000

    already survive getout sweep risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import sys
-def print_python_version():
-        return sys.version
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
