62313588274d083b9a2ae9b8d2046d25daf03cbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:17:45 +0000

    fire drive that tip terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def greet(name):
-        print(f"Hello, {name}!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
