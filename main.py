db4933dba6fdc3250dd78fb535a3173037a42f1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:28 +0000

    open ourselves brush smoke part

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
