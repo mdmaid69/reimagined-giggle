01321e341c33f919ae7e9ed8d94619dd6f1b4381
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:27 +0000

    block giveaway second than tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def convert_to_hex(n):
+        return hex(n)
