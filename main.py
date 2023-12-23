2a42ae2a0495f6885a5cffaa2a45f41f8bc1b0cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:51 +0000

    basic lookaround trap this true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
