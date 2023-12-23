1049cca30225173fd9fa80e2c82b58a645b0a3c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:37 +0000

    my over form block remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
