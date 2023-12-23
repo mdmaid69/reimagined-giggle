3ed65eb0246fb195c53f31463d1e75dfd5752e41
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:03 +0000

    translate praise order soil responsibility

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
