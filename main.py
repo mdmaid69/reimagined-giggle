298890c9c56627430cd43742f61029bb177a4e72
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:13 +0000

    say thick production Pp getup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
