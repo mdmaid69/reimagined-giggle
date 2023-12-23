92c1ad67d38b31905b8d83eed6fd196ae6c07f78
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:08 +0000

    bother strange guess combine myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
