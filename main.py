65f7f2dfc80952ff7158e9bccb5bb73a9aa0b8e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:56 +0000

    act properly state sign although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
