0f3ca47d2c547e579f4d73d496b09d88690eed8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:29 +0000

    by case soil give equal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_area_rectangle(l, w):
-        return l * w
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
