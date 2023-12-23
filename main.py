ea9339d78226244ea4ecb8807160adf3ba5ac9ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:40 +0000

    gate grade connect business declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def reverse_string(s):
-        return s[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
