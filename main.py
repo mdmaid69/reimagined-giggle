91cdb43c50d7690da9dbc71f0b2f711f13a18f60
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:29:42 +0000

    which pot juice themselves lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_string(s):
+        return s[::-1]
