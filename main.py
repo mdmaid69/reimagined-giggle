3153e9cc602165da782d26ce990fb258e9f86103
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:06 +0000

    interfere enthusiastic turn easy do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
