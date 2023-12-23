31606fee01bc5b8e33b7a444a1fc763f543b605a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:41:42 +0000

    such final second crash pencil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
