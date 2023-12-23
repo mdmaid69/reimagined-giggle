ff25bf82220406641378ab58a657d23dda280187
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:39 +0000

    tie smart shopping getaway support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
