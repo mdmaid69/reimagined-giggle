aba3dde0be9574de8f4d0d865526c9fafef285bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:25 +0000

    cook later compete they without

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_string_to_array(string, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
