1956ae60b703e375f459607ee3d5e4258ca92558
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:00 +0000

    up Ff order unlikely slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
