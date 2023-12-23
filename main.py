c96cf988bd3197f32cc94dc24428cac51a2f9336
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:40 +0000

    watch convincing out spare who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
