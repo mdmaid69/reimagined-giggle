d5682de7eaef047bba2d438be2b66bdec593bef4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:38:43 +0000

    spring previous wonder judge delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
