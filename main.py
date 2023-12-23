554a06acd076f41f358253720f1c20c9ba9a120e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:35:30 +0000

    letter child advice recover call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
