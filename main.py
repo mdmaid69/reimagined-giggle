eace31489b6281980e24b9808964902eb28b20da
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:14:04 +0000

    now equal soft breathe check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
