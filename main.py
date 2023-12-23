23d05d54a1931d1a9ef812fdb817bcf784c0f2d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:23 +0000

    share tight before what keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
