8b3c221a11a67b5057a91f733197efae7b961c65
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:08 +0000

    usual embarrassed bridge circle increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
