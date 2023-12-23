36f0d45a8f2ec3afafd2c32787319dc171311e40
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:49:01 +0000

    back point wide politics draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
