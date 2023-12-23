3d7f3752a05f5179968d50f6c74bd193538ea60a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:01:39 +0000

    activity fall complete exam pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_list(lst):
-        return lst[::-1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def reverse_array(array):
+        array.reverse()
