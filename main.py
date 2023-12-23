6ebe90c55dc030adf9729bd684c78881049712aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:43 +0000

    convinced myself seriously music limit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-print(sum(range(10)))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
