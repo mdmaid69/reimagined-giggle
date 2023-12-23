073b5b7859e17425600be923b7945ca75ef23e59
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:51:59 +0000

    taste whistle beginning eye artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_pressure(force, area):
+        return force / area
