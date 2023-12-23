86988b62def1183893ba18e1b3daa5f018312e26
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:08 +0000

    noise typical greeting inquire building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
