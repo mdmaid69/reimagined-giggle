dd98314a703b8b3df7e3d2aed3a99c26bfd9a19f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:06 +0000

    continue guard hit dry respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
