a3cc08b44dface30a7e0c8d2f32752db597cc70f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:50 +0000

    shine steady properly appeal innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
