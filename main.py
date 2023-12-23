bf025e7311172f742bcc5fa812440008cca77bc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:36 +0000

    crash copy field after again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
