7f4d7e22f88532447bfa95c7c0d0fc1f03128d90
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:14:50 +0000

    limit solve back deliberately frighten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
