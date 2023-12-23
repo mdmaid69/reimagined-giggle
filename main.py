3c3978b82f37cddb4817bca2dbb1d9f42c2963f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:43:54 +0000

    fork fun warning involve loss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
