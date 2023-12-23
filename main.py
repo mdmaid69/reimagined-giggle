4c1ece98326f3b0810e8496a4bfaa140a23af465
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:20 +0000

    gas grade regular warning gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
