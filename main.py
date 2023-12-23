4b0804898017867ad92168f138cb884820ace97d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:04:59 +0000

    destroy period visit exist watch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def clear_array(array):
+        array *= 0
+import math
+print(math.pi)
