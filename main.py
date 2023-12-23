de735b5d5adcb083d542d8d10c7c90240d5adc83
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:03 +0000

    talkinto comeoff identify thing though

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def sort_list(lst):
+        return sorted(lst)
