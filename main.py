ff0b8f07789a82e94b24d62a3de910f98b864cbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:35 +0000

    cheat common advanced too common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
