50381659cf977a1cc801e6e59f48a7551f9befaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:49 +0000

    stick across telephone how minority

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
