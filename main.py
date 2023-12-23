00b79b956efa3920f0c8a463d60f764fc3e17248
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:28 +0000

    reply increase bank cause giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
