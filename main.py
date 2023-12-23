d4fd656e56504af369355cb1e1ad2395512b4758
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:18 +0000

    permission body error claim pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
