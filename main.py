6ef7edb4bad90d11be3f594ef8610cf6c23f9b34
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:46:03 +0000

    intend frame look beside post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Sum:", sum(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
