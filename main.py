ff52aa00d66bbdb6032ea0e7468a6002cf062f72
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:45:00 +0000

    border criticize side possibly ordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_density(mass, volume):
+        return mass / volume
+def reverse_string(s):
+        return s[::-1]
