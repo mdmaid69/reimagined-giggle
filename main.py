6d36ad6bdca383da82430b69ca2f7e8653913b5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:19 +0000

    design form machine turnback pretty

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
