0b00076681b682bf0268a61b1d4efbb4250ab77b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:18 +0000

    close comeback shade legal smell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+print([x**2 for x in range(10)])
