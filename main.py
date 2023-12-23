bd6a51e4483660cbf98d80ac36d60f6c1c8b7bdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:28:22 +0000

    only print progress care towards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import logging
+def log_message(message):
+        logging.info(message)
