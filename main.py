36b2e45ac5d2ffe753927026136b2b5f98026d8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:37:45 +0000

    meal reduce spot short stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
