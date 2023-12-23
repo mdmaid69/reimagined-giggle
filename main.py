1725f5700b11c5f1e5f9316a1c9dc143bbab6d29
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:47 +0000

    raise prize estimate unhappy appearance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
