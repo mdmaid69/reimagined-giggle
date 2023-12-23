5e753161cd8b75650a96a8f1d63a7ddf1c182d02
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:58 +0000

    exploration performance point steam detail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import logging
+def log_message(message):
+        logging.info(message)
