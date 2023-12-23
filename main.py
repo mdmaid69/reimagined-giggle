07eea74806129b4b0df0ee3cf2e7a7320b8b4433
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:48:17 +0000

    action score force safe godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
