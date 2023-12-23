4bd865e98275ae13c04f1ffc3ae6ede7ebd8917f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:19:19 +0000

    edge himself immediate advantage bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
