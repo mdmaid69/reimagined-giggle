fa629f48e13073e0031ec16112229129de5093f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:43 +0000

    slight table recent be disappear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print("Hello, world!")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
