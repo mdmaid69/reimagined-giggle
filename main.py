8f1a595100f85cc18bd5e87c1746b3c12b5585cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:58 +0000

    holiday typical visit tight distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
