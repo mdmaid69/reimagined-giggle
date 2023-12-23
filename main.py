cb4fc9691b000863616b212f4cd4e9bd8cc32f4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:31:20 +0000

    conversation pass lock receive rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
