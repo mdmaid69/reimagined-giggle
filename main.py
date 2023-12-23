4055b807400017f05b39935fbb859b368ec465c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:18 +0000

    drop bar style spare worth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
