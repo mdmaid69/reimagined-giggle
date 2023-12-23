c272d1e662e5311259bb6efe29b6281330058563
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:13 +0000

    often sentence thing 179bite violence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-for i in range(10): print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
