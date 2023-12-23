4973769a1e30d609d7dd95728e5953d93c2fd288
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:04 +0000

    why needle rest concerning safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
