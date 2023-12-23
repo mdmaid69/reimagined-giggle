2d2b2c9769ff9534f17e1ba152a935050cc07f68
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:55 +0000

    keepfrom responsible view reasonable wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
