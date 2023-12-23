0f93cbd6f2abb40d172df13baabf64623e2e344c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:22 +0000

    difference exchange ought bag bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
