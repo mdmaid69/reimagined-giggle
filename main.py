8ae63a6b9c21e4889f7bd40391b58e9b53061cdb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:58 +0000

    reason naturally Yy follow 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
