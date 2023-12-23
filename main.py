247c0854616cfdfbf2ef53e630a382a6acb0ce64
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:21:29 +0000

    brush carry before nurse imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
