1754b6404ce5d184be808db556f4efe9f615d823
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:37 +0000

    about offer same design judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
