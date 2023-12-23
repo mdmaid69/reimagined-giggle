716b363f9a17687b813b8782fe9c8f3f9d24f219
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:45:27 +0000

    pleasure turn breakin comeround Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.time()
