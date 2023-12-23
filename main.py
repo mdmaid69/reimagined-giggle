c69da1fa9fce98b8dc05f97756ab6ce82173f093
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:50 +0000

    release total law by home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
