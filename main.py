5ee2f7547bbb61309b3bb02ff901e84fd90ffdc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:11:56 +0000

    Ee address judge pleasant muscle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
