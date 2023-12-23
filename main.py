bd3e681afe2c093abd72757a1cb3e8c1f6df8e61
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:12 +0000

    mistake uniform invitation most defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
