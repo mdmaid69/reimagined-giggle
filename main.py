ab5d928d1b5559680922733d3a27887d378be078
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:10 +0000

    affect share grow several sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
