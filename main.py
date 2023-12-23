006322856e4e1de6de8093d991ee9887569af783
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:36 +0000

    remove Jj difference doubt operation

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_list(array):
+        return list(array)
