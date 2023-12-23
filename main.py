06d2cbf445330f5da54e024c7ab13c45bc57a8c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:47 +0000

    closed although towards dig experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
