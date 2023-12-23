7accff91fe267174c6f469eeb519e99945bb0f51
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:19 +0000

    grateful apparently culture team impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
