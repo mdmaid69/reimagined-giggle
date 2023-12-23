47e0bf20f2a4c4b2394da5a938b35050c61d0467
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:31 +0000

    say to expect lookback(on) estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
