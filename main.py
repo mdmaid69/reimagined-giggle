1a3b04e1e5255d2fd45fcab8cbe2daca5a2963b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:33 +0000

    explain action should cut friendly

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_power(work, time):
-        return work / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
