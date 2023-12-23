4d729a710e3d7d2f1dafa432d7f683b5b79baf71
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:16 +0000

    memory left feed Cc emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
