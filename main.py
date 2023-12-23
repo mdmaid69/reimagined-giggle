26c459ad94d918ca2d9f6f0f59c0721236051197
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:50:06 +0000

    already occasional comfort breakinto runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
