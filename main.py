0e1681ac4712b2d6ca54bef9e21a37d762bf8d9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:33 +0000

    shout dull swallow both grateful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import random
-print(random.randint(0, 100))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+print("Hello, world!")
