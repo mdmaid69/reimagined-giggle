232d70916e14f18fe50a08f467be0af0f05b5b4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:06:17 +0000

    strong stretch command turnaway pull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
