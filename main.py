12d18cc96ea87922bde8370a06cdd3b5bbadf162
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:32 +0000

    gap suit typical sing actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
