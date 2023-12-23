1cc323bb054760c54da0488dfeee3e2ed5667fc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:13:23 +0000

    hold average turn machine little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
