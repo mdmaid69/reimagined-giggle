c17918bfc7cce2fe53b596e17ecb3b4e17937516
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:18:38 +0000

    relationship late perfect ground embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+print([x**2 for x in range(10)])
