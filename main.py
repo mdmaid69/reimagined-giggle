31c1bd809bf28739a15cb804a13766738327f46b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:25 +0000

    absolutely noisy reduction emergency confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_power(work, time):
-        return work / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
