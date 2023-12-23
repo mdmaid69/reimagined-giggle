b4064b2ad6fb8ecffbca9e0b6c545bdf713872b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:59:43 +0000

    extraordinary this drawing touch acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
