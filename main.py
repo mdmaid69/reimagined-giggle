13a660a8fe00825f2f628c4a87d31b02473e49f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:54 +0000

    turnover Gg any anyone passage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+print(os.getcwd())
