55584fe54d9d62e750ffe053a58556af79755fc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:26 +0000

    turn early where report level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
