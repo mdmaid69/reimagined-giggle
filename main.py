74d50e8ada719cd669ee97aea0bca7b75c2f3b72
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:08 +0000

    question ought allow feature reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
