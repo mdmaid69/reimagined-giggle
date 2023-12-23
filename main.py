0a5050aa8dc1975288d0966672310e397a932818
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:46 +0000

    Jj trust takeaway nigga report

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
