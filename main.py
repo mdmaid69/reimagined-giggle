1dbe99e1427e1df11317cfa70e3caba813cc24a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:54 +0000

    emphasize course matter tight mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
