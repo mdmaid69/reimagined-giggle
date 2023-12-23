b97b8a626baad07b2012e32e8748fdb7ad95a200
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:37 +0000

    uponon light watch their size

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
