269935beb707ac94be84315fd4d2f8fbd1c62979
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:53:19 +0000

    every sign beneath perhaps variety

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
