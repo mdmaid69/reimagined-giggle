5ef07d39131ecfbcd3852445a5f0ba9ca549f42f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:25:45 +0000

    respect rather accommodation bush breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
