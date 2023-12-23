08946fa953f7a7556d97bd84896f8db6cbe4830e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:46:09 +0000

    comeback but anxious final responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
