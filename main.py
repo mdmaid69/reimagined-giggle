6a9d519f036e356b70a7978d58bf60af5f152665
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:11 +0000

    tidy temper style police offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
