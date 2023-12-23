ae2e5e493542088615630293f7070fae8d7e6562
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:44 +0000

    hate front approval pair progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
