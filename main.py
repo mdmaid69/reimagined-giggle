c83e95019bf44a38b9b12d933a2f5b0e43fce54f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:15 +0000

    see wait dead authority visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_tangent(x):
+        return math.tan(x)
