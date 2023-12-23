c617060206560f2fe71b16aa2ce3b0a1be0b09e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:30 +0000

    play stop concerned far proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def divide_numbers(x, y):
+        return x / y
