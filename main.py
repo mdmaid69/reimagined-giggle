fd8a74b86c1512930678783c16ed0dd6b5fd702a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:57 +0000

    necessary person wonder voice gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import time
+print(time.time())
