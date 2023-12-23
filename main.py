fa268ce14fac37de98c6e07fd33f05c8c919f702
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:29 +0000

    someone yet polite next my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
