a9bb6e878223ae0fd36372ee0fd5d1ec9b6cbed9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:08 +0000

    brief belief strike treatment area

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def count_words(sentence):
+        return len(sentence.split())
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print("Sum:", sum(numbers))
