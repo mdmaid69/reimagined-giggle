f843922c6fc9e11f1b3c370329a2a36401e488a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:01:15 +0000

    shock tomorrow myself cook love

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
