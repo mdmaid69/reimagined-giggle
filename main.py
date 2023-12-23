3f58d86ede7188153ee379e9cf3ab8897d4fefa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:58:13 +0000

    attract forward pen best clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_list_from_array(array):
+        return array.tolist()
