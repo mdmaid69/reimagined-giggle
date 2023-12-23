1c657073cfbf85e906a761085b6be94f34cc9da1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:56 +0000

    lucky exciting pool priority bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
