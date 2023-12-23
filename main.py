cfec393d6d64ac37d5998355d6172f44fe429180
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:06:27 +0000

    fasten estimate survive sort fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
