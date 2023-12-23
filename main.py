7c19051128fa198661b40c82b57a005855e32c82
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:23 +0000

    wave so brown bet eager

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
