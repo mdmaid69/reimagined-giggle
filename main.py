46b8749ba3a6b513ea94253c02cddfcb0d26095f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:50:18 +0000

    crazy present ignore return minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def convert_to_hex(n):
+        return hex(n)
