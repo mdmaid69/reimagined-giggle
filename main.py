9e9cc0b65b2c90a55f79456b643e62bc09c922a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:47 +0000

    holdout influence set cost dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
