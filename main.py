439279e5d1514e59eb484bbb73e96fa4cefe8c1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:47:52 +0000

    duty land expression concentration prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def append_to_array(array, item):
-        array.append(item)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
