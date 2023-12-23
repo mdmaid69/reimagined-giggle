a0e8c930cd6c418daa9be8ec237eca78d27638e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:13 +0000

    drawing pleased blame accident always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_max(numbers):
+        return max(numbers)
