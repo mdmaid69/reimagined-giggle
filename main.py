ef561a935691fda06ff28418100cd4cd7d647470
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:02 +0000

    convenient help pile sea owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
