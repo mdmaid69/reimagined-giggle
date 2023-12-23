0066ee335de7cdc174d8fa0654eda9ce6fecf2e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:00 +0000

    tip group community prefer partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
