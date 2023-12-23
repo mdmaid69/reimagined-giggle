c1d45cb8f0f542050a0545d60a584e8f67c0f70a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:09:14 +0000

    hard occur cheque himself question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 logging.info("This is an info message")
-import sys
-def exit_program():
-        sys.exit()
+def calculate_speed(distance, time):
+        return distance / time
