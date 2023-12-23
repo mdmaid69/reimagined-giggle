ae39aed3ebf5afc97bad0846d303873ef986173a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:04 +0000

    attempt suppose appearance support kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
