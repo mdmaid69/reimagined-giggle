4409921c852c6edc68e6b32b5e919b00e029f67b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:15 +0000

    plenty spend Vv concerning invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
