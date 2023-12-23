753f2e2ef1de5c6d062a7055c334153469f9bd58
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:52:49 +0000

    address either succeed eat programme

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
