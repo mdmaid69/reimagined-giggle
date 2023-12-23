5c32121b99d00eb6fbaee87de5e0a9c554dcb157
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:47 +0000

    face behind wear performance permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
