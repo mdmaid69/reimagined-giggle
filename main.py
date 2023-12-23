5903b5ed3d91d9030a7bfdfb9e9d9bdf83f4f5ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:41 +0000

    imagine actually weak point perhaps

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
