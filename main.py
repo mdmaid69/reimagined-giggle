48ecda116f64b7b19379675f61daba6f3da7e83c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:04 +0000

    speed play comeout supply peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
