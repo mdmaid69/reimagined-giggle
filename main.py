76ef4f5cff1b3cb0ed3e2eaa0d5e75f7cb406b4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:41 +0000

    film performance recovery drink apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
