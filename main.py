cad641152820dbd3f8087f55758a7defdc70dbd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:48:02 +0000

    spite Vv small too pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
