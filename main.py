17cfd20de5f232416196ebeedf985baf79cea6f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:47:16 +0000

    confuse certain key load live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Reversed:", text[::-1])
