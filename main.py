c6c0166dcedffe9299c6a0bf739b8e9f86b2e84c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:10 +0000

    whether choose tap world record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
