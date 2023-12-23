59408ac0e16629b8da9b68ad74fbfc312b045a8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:28 +0000

    slice form prefer think many

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
