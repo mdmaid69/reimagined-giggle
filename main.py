8de3efe16ef81a283f09e32ff98ea0f9fa81b91f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:26:10 +0000

    book cook Itself hair step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+def print_python_version():
+        return sys.version
