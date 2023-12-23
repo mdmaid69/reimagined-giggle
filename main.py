0afb41805f0744f8a08d39d1a198fe75b0534780
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:53 +0000

    store piece week like clothes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
