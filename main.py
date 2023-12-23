cd5a5e736bb13fa5a205debcf09250fc6d67969c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:56 +0000

    trust want ball brush circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
