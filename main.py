98ae1d69b7b47d111a23d58e88ee799bcb2ca1a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:07 +0000

    century swing translate myself near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
