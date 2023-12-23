9735553c644e16c8fd1a6fc52044f829ffba631b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:15 +0000

    climb apply battery shock prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
