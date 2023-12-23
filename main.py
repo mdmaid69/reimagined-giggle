070f5723de99e3adfea07cd60d7a6a03544091e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:46 +0000

    gooff main contribute onto touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
