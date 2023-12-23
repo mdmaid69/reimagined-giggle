3bc3d329360cbccef2a40dcc8d151f9f9dd64d38
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:05 +0000

    proper custom read fairly mail

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def is_palindrome(s):
-        return s == s[::-1]
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
