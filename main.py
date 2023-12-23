d143d70052bbaf49052524a9a63b1cae6c239c29
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:14 +0000

    slightly lost main comfortable over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
