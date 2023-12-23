6cbe3338544ef0dc46630bac32842ffc7c5a9fbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:56:33 +0000

    flow hit carry late final

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
