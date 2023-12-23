91900ddf24103e3baa34381f5e385fd8e4e95cac
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:39:18 +0000

    important lookback(on) certain fit bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
