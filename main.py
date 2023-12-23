2096fa154254af6d3730828e69da7edcad47fdc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:32 +0000

    needle contrast rid life content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
