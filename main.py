4492d6acacf4fec3b0358ff8c7d36065dc3b989c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:50:10 +0000

    area judge conduct rate sing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
