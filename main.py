30f8763beae407db3bc72fe35d40a984a23881bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:30 +0000

    freeze dance say identify put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
