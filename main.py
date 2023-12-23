e0922848bb8a8b11434004a23221ca50bfac6b3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:31:00 +0000

    dick kind turnon bringdown guard

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
