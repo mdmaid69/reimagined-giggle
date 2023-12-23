ecd425568864f84b6c1fa059d1d448796b114df5
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:10 +0000

    throw owe reaction uncomfortable possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
