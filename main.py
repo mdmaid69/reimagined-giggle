fe11f08effa2705e4cbd2b4d09b8e919bddc0b3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:23 +0000

    school nothing her description sick

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
