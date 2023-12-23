957f1cc84e93261418b2d9394818f01b8da54b41
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:53:35 +0000

    certainly hungry city fly butter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
