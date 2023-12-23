e80d367fd4a05cbb9786a5d39369f47d19d894b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:07 +0000

    secret eager nor limit suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
