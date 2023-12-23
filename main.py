702432a97dec72baf75854f782b79b023de1cf75
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:17 +0000

    value improvement acceptable open broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_length(array):
+        return len(array)
+for i in range(5):
+        print(i)
