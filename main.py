60e25e453ffd9a885ba1ef9f216da3c355817101
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:30 +0000

    equal glass hollow sound seriously

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_sine(x):
+        return math.sin(x)
