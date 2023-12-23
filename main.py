607474561d75b8ce75e3552988dfa10bdf00209e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:25:34 +0000

    ours independent quarter amount display

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
