8a3427bfb0f5049ac4e1b4b5fe71a0d866475085
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:20 +0000

    too such pocket ground total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def multiply_numbers(x, y):
+        return x * y
+import os
+print(os.getcwd())
