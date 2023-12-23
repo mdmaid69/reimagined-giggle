8925563fe7765a4fa4c0a27fe2e4b9f4d4d59836
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:31 +0000

    school hello combination thank part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
