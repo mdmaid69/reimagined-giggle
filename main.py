7b0f12542e4fd75f5022b767ddb0b98b8da10055
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:39 +0000

    thanks popular height hour address

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
