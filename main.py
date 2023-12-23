fedc45e069e87c69b775135b7485f84b2c37e4be
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:19 +0000

    inside careful equal settle often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
