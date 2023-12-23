76b7a781c40a26a857fd53da269f5558acdd45a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:31:35 +0000

    region collect familiar recent old

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
