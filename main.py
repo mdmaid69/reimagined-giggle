ce984bfa434cb6dd4941d12380c7e2d49895a961
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:28:27 +0000

    little your place what sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
