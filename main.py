100b1af811d517e7c29b4a7cc006d5420b17084f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:08:35 +0000

    branch mix workupto might length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
