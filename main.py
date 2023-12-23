cb3f5c9113ab82c9175b7dad9f3a5f99563b9372
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:02 +0000

    cross almost sharp excuse whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
