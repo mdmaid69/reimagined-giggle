5a06693d0fd451b4933c0a3c91ec13451b284d76
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:59:47 +0000

    crisis rely stay adjective some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
