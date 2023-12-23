993984ba3490cdf907b0a29cdc2a8d81ee5e85d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:47:24 +0000

    wire perhaps turninto expression attempt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
