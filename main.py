4f614ff6aba2eead018d40a271c4cdc3f1a47ce9
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:53 +0000

    argue on run report Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
