fe05fda56ee000c47683c3072f22331d31a02d3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:53 +0000

    separate birthday confuse suggestion vision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
