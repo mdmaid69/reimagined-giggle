1978ac3402e1e2491ba1fda825482b99193ee8cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:33 +0000

    convincing face education in aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def multiply_numbers(x, y):
+        return x * y
