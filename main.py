80d5761794b0adc9a2745a73bca2bb825f345073
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:19:50 +0000

    open area intelligent place callout

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
