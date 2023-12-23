7a6039e940670e9fcfc281977dbdddf5fc689e81
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:15 +0000

    appeal mass keen write medium

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
