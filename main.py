fca2482fc131c79e351bdc3f0fd758990ff8bdc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:40 +0000

    speed miss claim 571enough health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
