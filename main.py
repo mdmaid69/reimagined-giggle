9c8fd2c2f1f7a697c8ba8fa9864220944f786e0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:28 +0000

    practice task increase cheque on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
