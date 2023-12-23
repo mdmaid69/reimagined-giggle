aff321a52006e63ad56904d3ccf824397a5b28cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:09 +0000

    precise method slice black away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Words:", len(text.split()))
