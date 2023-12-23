d866bf9e6bc8aa68ef857040f4e0f0d6c9fa1341
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:21:59 +0000

    spare transport stop lift move

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
