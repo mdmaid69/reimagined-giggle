28a261e8f923653248f4c930d52b196f5255b144
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:40:25 +0000

    middle I scrap year lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
