27cbf23543c702adfe581bdf38e36722c1b75c59
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:22:40 +0000

    uniform niggaacademy basis ill rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
