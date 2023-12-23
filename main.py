2cf3f21becf6fe852b5196baefc2cee7724715db
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:13 +0000

    nurse cook negative sport kick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+print(time.time())
