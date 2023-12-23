29d4ba30dc2da0a41646998601355608730da69f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:02 +0000

    accommodation backwards range splash 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
