da7ac4cc32ff7c9ca65dc8fb8de4997a557ef84d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:43:53 +0000

    squash hungry simple use ground

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
