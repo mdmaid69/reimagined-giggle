976c0ecbc6378ac961b8bbd19b24606686c6fc80
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:17:24 +0000

    Jj quiet keepfrom effort pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
