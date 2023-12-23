7e5239a8aa379832e8960dc1f98b708533b29ed1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:48 +0000

    big country war fine comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_float(array):
+        return float(array[0])
