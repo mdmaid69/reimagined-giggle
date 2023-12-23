518936a331df7b15b7cdb1e9f49f6d34b4f1e6da
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:56:49 +0000

    private exit follow shoot estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
