baec4e9158d71e922cba65db90e416d6c9a4c799
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:48 +0000

    down draw public sensitive swimming

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
+def calculate_pressure(force, area):
+        return force / area
