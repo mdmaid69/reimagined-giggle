69c2677346e95286c5d694976e35b82d31faa607
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:51 +0000

    admit less flat office meal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def remove_duplicates(lst):
-        return list(set(lst))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+print(os.getcwd())
