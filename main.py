febf069158ba199ba74c0b852abbadbe5563f962
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:05 +0000

    round factory professional secret welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
