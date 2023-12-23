d8ae1057575d724291e783b5fac03cd3a47c07f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:58:25 +0000

    get importance free but compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_complex(array):
+        return complex(array[0])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
