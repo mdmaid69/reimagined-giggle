c6c58a7f072ce5dfe83114be0428f2db3e766eeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:35:33 +0000

    preparation you convincing record criticism

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+print(re.match("h.*o", "hello world"))
