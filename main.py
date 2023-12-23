f1a2fa1900c0a08fb231b057a3df5298161cb753
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:18 +0000

    Jj tone usual people wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
