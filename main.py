38b6be045948c217cb077d9240a24c1f05669d39
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:25 +0000

    coffee ashamed if possibly mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def count_elements(lst):
+        return len(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
