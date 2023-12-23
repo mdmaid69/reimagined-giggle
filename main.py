de87c8388f4b765d55b7c94aa0b1162f7a7f9fac
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:14:52 +0000

    generous smile apologizeapologise question march

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
