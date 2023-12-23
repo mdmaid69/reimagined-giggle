f2c2813a58738333cb9f24f07d4c98ba9201cd93
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:35 +0000

    down new difficult light drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
