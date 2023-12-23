1f48082b9735d21a7533579c4f0593f32eb0bc69
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:20 +0000

    noisy surface throw truth separate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
