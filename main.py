3cadce7bc074f9319d9eac84e41b62f748596021
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:51:20 +0000

    loan unlucky regret none solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
