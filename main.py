5b486cc47c4360b1984688679771ab8103c12fe1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:20 +0000

    niggaacademy squash unfair business class

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_queue():
-        return collections.deque()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
