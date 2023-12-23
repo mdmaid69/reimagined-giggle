7a501fb15cfdaacdb612b7802adbdcd479aea562
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:06 +0000

    than pour kind bound odd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
