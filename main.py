b4524adb1cba2cfcc508025a0e82e9d3f7a77ad9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:15 +0000

    star picture base communication turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
