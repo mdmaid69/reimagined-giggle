ef578907652e0f1569c3795c9fb3c757c86a468e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:25:06 +0000

    foot object woman loss occur

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def sort_list(lst):
+        return sorted(lst)
