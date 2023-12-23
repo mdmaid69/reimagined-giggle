f68312f8b014ac4fc48c34a40bb5952b35af6e04
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:54 +0000

    Pp pool downstairs train wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
