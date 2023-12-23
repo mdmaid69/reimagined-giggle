3fd0275d0bb20eb08209007cf551966b7b4e4933
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:27:50 +0000

    ready awake its convenient note

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
