e1e99b650ad611755f28d408e8c0235a78c2ce8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:04:31 +0000

    greeting solve choice since takeover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+for i in range(10): print(i)
   import re
   def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
