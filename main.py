348224c2874814459761ca078973e06b2cad555e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:34 +0000

    collect early walk deliberate across

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
