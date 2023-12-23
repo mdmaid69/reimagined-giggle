a9fbeda265a88d5ca1b32f68d0f4f4f33247917f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:19:26 +0000

    cross aware minor minority wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_string_from_array(array):
+def get_bytes_from_array(array):
         return array.tobytes()
-import sys
-def exit_program():
-        sys.exit()
