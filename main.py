3f8f42258509c97af207cdd6c0fe616d54408f96
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:36:03 +0000

    perfect back explain design seat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  def is_even(n):
-        return n % 2 == 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+print(os.getcwd())
