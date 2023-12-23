647a2b4cb43e3a3170d076c931cecc912c87af40
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:49 +0000

    threaten coat hang partner culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
