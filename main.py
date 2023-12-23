485f49e85738a5d07f950f9cc8a1a00f832adaef
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:34:02 +0000

    succeed reduce aim birth stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
