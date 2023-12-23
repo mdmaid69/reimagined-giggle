f88b30d98661eca5ec5434426957cfb27616331a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:24 +0000

    interview otherwise excuse one big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
