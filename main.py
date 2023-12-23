b72fa54908d578798581b489a771a9a96190fc38
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:12 +0000

    round guard fear swim opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
