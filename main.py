0a0ab7f37e4747e09fbfb2f63bdf407ffdeb7a7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:36:46 +0000

    meaning custom method issue represent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_list(array):
-        return list(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
