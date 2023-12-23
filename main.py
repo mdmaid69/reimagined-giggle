f558a205155953297de681a79031ce55fbdb1f02
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:15 +0000

    neither need blue best share

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
