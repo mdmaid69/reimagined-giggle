623128539e493266f13c8e01c74d87bca337d766
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:49 +0000

    stamp address write tool food

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
