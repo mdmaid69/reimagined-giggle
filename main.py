e628ed76a108061eb627200cf985ffc7c5867a99
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:53:58 +0000

    claim rough accommodation represent opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_current_directory():
+        return os.getcwd()
