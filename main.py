f56c60d36a3c26fbe7d6dcdd9aac583cc9d398ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:10:03 +0000

    flood their stop formal together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
