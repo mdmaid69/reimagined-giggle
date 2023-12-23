fb8fba3eed199771a9d653535bd488b6a02b1e3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:52 +0000

    complaint constantly waste afford shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
