a2835671f9aab35e7a19221dc3caadd40b6d5d36
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:28:44 +0000

    promise much dare describe scene

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
