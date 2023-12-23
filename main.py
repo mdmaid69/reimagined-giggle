e1467253f6dbda20081577a5537568dd89d6130d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:21 +0000

    rubbish telephone excuse direction piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
