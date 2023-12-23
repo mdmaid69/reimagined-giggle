e61ad61e31c14a8518095341fa3ac436b27bf82a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:46 +0000

    area come together else keep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
