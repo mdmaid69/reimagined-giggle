89c5826e5fac1bfad2870a07254a7c4814c238bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:19 +0000

    recover day port mind birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
