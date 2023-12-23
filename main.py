3cfe057b52fe8461771be696253802808e5c310e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:20:27 +0000

    wind visit race cheque declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
