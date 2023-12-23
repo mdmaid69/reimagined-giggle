35c028ba6bda88fc5e7636db5ca8f4e45bf17279
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:30 +0000

    comfortable pinch church occur case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
