e7fec186d4b375c285b4fd1abf5e6169e31f5b5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:28 +0000

    swim getback label decide separate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_density(mass, volume):
+        return mass / volume
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
