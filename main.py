4152e5dc1f3c89a80d08bdd9052e4ce2ba072dc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:55 +0000

    consist radio hungry building regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
