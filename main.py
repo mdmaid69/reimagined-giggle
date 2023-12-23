47f882d4657b23ea0bcca5c85b65ad73738a16fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:57 +0000

    it standard height passage sting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def convert_to_hex(n):
+        return hex(n)
+def square_number(x):
+        return x**2
