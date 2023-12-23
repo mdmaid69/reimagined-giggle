e2a4d83aa63ac855270186c94e82660632625c10
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:40:42 +0000

    be gift software tonight yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
