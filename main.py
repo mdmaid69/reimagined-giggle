09c9d37338c8f1f7c50bdc06a519004404392c04
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:38 +0000

    combination everything lock pay underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
