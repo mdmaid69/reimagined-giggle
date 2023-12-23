1d39b5b909154bb5beba1ce88a55d1e17f039751
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:33:15 +0000

    hate broad month sight noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
