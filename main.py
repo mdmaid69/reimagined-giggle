df82fb1f3b7cb78b724fa6463a76d411c8f0e509
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:28:13 +0000

    sad wing practical fear small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
