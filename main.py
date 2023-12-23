27d297c67a0feb6c5fba5f7573d6734aa319a908
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:01 +0000

    woman often hold compete finger

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
