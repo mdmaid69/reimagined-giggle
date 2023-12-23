ca30873334e416850790d431806160c29b208e4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:24:11 +0000

    track week zero mention certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
