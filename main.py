7172c77e801fe97ec88a462880323c8489a6140e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:51:34 +0000

    mood tread pick light dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
