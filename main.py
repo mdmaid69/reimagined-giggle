31aae6774c7040e318ee0834257f0b8c1b3bd9a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:21:06 +0000

    shadow would street glad getback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
