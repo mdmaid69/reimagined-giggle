de0887a196283409944fec8322bc2c8497b97ecd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:09:03 +0000

    whenever major above ordinary 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
