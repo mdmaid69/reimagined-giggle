68d4c1732586bb270e15f237e260a1dbe1b04abb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:12 +0000

    judge basis help please recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
