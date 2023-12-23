3eaa24f0c74b28df570d4c5df294a99d390a348c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:38 +0000

    blue bang wire show agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
