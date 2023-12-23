fb1bca1db6f2aa2fff49199011815618232ac4ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:18:00 +0000

    week where slow stop closely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+  def square_number(x):
+        return x**2
