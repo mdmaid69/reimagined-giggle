c796b04b1ad02e92c408428aece875141cc81802
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:05:30 +0000

    welcome 573entertainment shut address settle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_work(force, distance):
-        return force * distance
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
