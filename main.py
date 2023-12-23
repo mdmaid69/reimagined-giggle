67344d996dcbbb733f54ebab5d7317b9556534b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:50 +0000

    copy judge blind attract healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
