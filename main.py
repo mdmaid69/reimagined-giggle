7f93ac8040497a43f9c88f79a1b4097c12f24968
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:11:32 +0000

    flow move appear container clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
