386f0fd562b6225ec8c4ad79266bd4002e71c38a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:22 +0000

    include obtain apart immediate lucky

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
