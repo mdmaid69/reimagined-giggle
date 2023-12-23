19eb66ade3a70f934f6476f1f7e0a2fa75067fab
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:29:18 +0000

    exhibition night suit bath unable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
