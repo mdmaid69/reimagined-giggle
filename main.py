143ce32ab728fa45e326c16f8a4e4c3734ece7ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:54:19 +0000

    challenge attitude tool male holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
