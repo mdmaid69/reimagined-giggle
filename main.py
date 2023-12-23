f946fc3efb3441edccc3a503372c2ee2734f2981
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:40 +0000

    poison shop equal shrink building

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
