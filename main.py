248e87b722c1c74e22e0fee02a5e44fa4abaf180
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:10 +0000

    bad people smooth react violent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+print(sum(range(10)))
