7dcc50add00747ee84f015666a7c8734b8eba0f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:34 +0000

    other miss coffee drawing crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
