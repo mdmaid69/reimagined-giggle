83c23c8ef11d70a767f9348ecd3b72479dc7b5ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:40:50 +0000

    memory split politics field bit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
