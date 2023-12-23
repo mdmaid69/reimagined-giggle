844dfa797ba11fe0b43f23931eb2c1534a2ce160
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:40 +0000

    boring give common its capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
