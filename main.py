66d1ae8e66461506c03e5e7cf396c9d5176f3601
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:08:18 +0000

    favourite attempt will exactly load

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
