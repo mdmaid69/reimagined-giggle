c3aa0d33d9eb5601a562f83d53f32b3dc0253ecd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:24 +0000

    practice geton one subject tone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
