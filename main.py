54a8cdb27a753da47de14378a28876ead177cc3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:38 +0000

    store disturb tight busy ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
