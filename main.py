b6b826fe8ca568f9d0b655aa96e1e4f4090ec77c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:34:31 +0000

    valuable 565end nonsense round shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
