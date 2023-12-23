c22a2e96b272bb0f36c1ee45cda4f3279233e935
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:49:20 +0000

    fall call empty change lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
