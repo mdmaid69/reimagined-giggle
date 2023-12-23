2d1bf43a094b99ab0972749c7d83120cb9e499a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:57 +0000

    snow anybody crazy during natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
