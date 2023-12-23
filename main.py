fc0bb761ea27e3d0b780285b056055c393df7ac2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:09 +0000

    memory long influence finally scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
