9504e0b76ce0e66ccb7ac17b56e2f634a88a600f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:49:07 +0000

    confident swim pain your kill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
