cdd9c9744d0e586121406d66ab0dd07885cb83ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:06 +0000

    personal position regional person fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
