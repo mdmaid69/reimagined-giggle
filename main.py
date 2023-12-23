8c4ff3f650e059b87f60b759a30d1494be256118
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:13 +0000

    responsibility bad bound act keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
