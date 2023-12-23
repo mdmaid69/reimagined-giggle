f30e3068921246c6b8eafa33bcb44b1646d75c3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:46:46 +0000

    sign plate godown secret telephone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
