f88c8fe00370eb61f5011c43fe72b01bf7f1e78e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:44 +0000

    concentration minimum moment date decide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
