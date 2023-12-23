a85d03d68d003f98969c2a61aec00871510d90ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:01 +0000

    clean crazy exact someone scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
