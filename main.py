06d8d12075843c3af3d70489be66adf46e79e0b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:29:42 +0000

    past oil single forward mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
