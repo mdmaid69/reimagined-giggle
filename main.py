f73082ab1ec4072f15ac027415cbc981f0e792c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:16:25 +0000

    comment mistake mention bet respect

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
