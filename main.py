4c215d3d0c989de983b2843785bfc24635c03900
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:55 +0000

    kind callout part valuable culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
