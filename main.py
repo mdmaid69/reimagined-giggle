a788fcf70afd973e374c0bfa1005f4dcedad643f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:55:05 +0000

    win need describe direct deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
