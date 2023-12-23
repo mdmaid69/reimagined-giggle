c15c0fa0aa76082665e63db40d9cf7926b1d2778
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:50:08 +0000

    attempt boil scream label expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
