18a5345046311b19077d7a7eba35d7b76c9d0128
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:43 +0000

    goback(to) block wish dull sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
