788eed94a427f0653b61d052a7be141e693ba6b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:01 +0000

    hurt stare successful switch awake

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
