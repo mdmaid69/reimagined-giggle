06e1311a1fa0318c6b9f424bec9b5731bc6b5ba4
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:23 +0000

    earn respect interview call trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
