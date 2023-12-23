2ca84d1dec5c7bf14845590ed888c1f4a78eb62b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:44 +0000

    reply bite hold consult ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
