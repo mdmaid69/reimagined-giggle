69c69199e9061cae82984dc4ec6615eca1b1d4c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:06:46 +0000

    design central attractive increase visit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
