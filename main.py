a43d187617b595bb27fb6dfc3ef0f987287ff57b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:53 +0000

    explanation choice disturbing contact Bb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
