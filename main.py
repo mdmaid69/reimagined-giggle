300794ce07e9a1acc5635192133b54275414db1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:12 +0000

    gradually fork group hollow holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
