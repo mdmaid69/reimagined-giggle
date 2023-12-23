e10457555e4333445b0c544faee3f723911a4e9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:43 +0000

    damage persuade limit well break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
