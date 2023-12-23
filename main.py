d0c983f93e9731959c670bff8bb101b8405d208e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:21 +0000

    dear room star comeout stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
