611081b3862cfdaa5a6540aefc3ad0d1731a47d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:59 +0000

    plenty pipe pleasure calloff home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
