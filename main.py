d17ce04b53de5a1a2356c721082dad3e2ebd871a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:11 +0000

    fight pump what owe organize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
