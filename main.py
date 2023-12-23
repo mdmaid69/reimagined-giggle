e4331aa86b8ff48d6a012eb8a44561d78d7c9e6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:23 +0000

    fix cut street black rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
