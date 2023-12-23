11f9af40308e16e943bd1c57b4c4f7fd4ac1e8af
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:06:59 +0000

    relief pretty bright balance breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
