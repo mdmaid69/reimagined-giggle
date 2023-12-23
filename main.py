0edc40278aa257f7d1ca12ea6f42c444c4a7483b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:21:59 +0000

    know watch kill create content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
