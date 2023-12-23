2dfe03e2a90f70409399156d2978d46aff1d6ca7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:53 +0000

    miss flavour past back medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
