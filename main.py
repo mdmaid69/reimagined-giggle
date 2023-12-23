d96095bb176ce71214122e8b0e38be73ec3ff609
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:25 +0000

    instant hand speech ago say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
