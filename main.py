f80cfd0c8c9cec2e4ea164259e58b114fe77fc2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:46 +0000

    port risk Jj support pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
