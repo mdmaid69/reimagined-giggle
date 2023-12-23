440d5634c7aed53c2b292a29ae91e15242a61db4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:54 +0000

    ignore plain bringback well rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
