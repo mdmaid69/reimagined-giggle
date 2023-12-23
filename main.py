3e71686d7d68739e24b913a5add6155600ac185c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:08 +0000

    behaviour cheap gap defend uncertain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
