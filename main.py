bb6d59e6f1593cb92a5344dec7eabb0e22974397
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:05 +0000

    traffic uniform commit needle mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
