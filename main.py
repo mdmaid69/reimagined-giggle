51615d59593ea612898457e3c9efa33e38c0706a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:43 +0000

    remove clue grade uponon separate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
