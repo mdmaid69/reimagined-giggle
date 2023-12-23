a7919d2245bb5f75f2aa7f7518d979a52a72c544
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:43:36 +0000

    brush wind that treat bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
