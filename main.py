535e95cacabbeafb912b10da91195aafa9dd2fef
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:54 +0000

    wide will contact time name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
