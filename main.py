77a5399d54bcf03796d40e4fd3df3bc8c51eedc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:06:54 +0000

    would ear Yy car takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
