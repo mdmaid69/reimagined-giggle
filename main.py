3b305079d5d1b80afcdc9db0d5fe608ce82617b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:37 +0000

    owe shock break butter talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
