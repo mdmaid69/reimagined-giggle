9e5ce701f7cb6388ae0e6c9fca4ce8c380e144dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:09 +0000

    left disagree blue spoil tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
