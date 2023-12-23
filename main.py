0353752b13b57182615b8e579dd06a89f5fc838d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:25 +0000

    forget thick read country blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
