b909971c060b069b541cd0e0cd68f7165588de2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:11 +0000

    contrast keepaway only off speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+name = "Python"
+print("Hello,", name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
