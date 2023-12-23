030b65aa2a1bba63f2db0bc68c6922097de46500
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:55:03 +0000

    pick Gg work pick wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
