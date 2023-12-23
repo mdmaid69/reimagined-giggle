5baaacb76538ea90af789dca9958c31aeec3e791
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:47 +0000

    group prize doubt plain tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
