c3457dd53816a9321d20b596c91340b5b9ecf508
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:04 +0000

    ill develop duty confusing succeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
