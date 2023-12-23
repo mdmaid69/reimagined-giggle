9a72a7d68014c004308ad3fac7fa624be3f99215
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:16:14 +0000

    wide callout actual turninto public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
