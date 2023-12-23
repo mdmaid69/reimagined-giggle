6b60817c136c84730be39fdbc558d567bba70236
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:19 +0000

    breakin they within present runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
