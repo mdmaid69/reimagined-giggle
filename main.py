5178ac72e2ced4934fc37f84d476339419560c8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:22:49 +0000

    edge punish shallow start maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
