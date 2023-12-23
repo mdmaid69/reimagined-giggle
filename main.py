009ea68d24796a10ee7b5404c6f8b96c3eac3b9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:28 +0000

    tired powerful excited see face

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
