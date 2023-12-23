af29c3e53c97531ed5c6ad375f03cf140abca52d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:38:41 +0000

    prefer characteristic record passenger sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+x = 10
+y = 20
+print("Sum:", x + y)
