718b23d35c757567be7da52829eb905251764b92
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:34 +0000

    tidy prevent tone basically preparation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
