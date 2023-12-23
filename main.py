08430dfd059c1ad68443b2528ed2f3904b7762d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:09 +0000

    fashion deal sting find kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
