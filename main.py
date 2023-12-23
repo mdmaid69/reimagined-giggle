4dd61cb6bc97fdd423fc69f692a4964b8dd7c15f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:55 +0000

    definite flavour behaviour embarrassing far

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
