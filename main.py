31852a7ad3e52ef5e33d2fd8f67d8b221d5b12ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:22 +0000

    minute view put city getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_dict():
+        return collections.UserDict()
