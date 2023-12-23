26ae306b002fb6c74eb4ace1e48423b7e25e4971
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:30 +0000

    indeed content scene sharp priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
