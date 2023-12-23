d121ffc2f484ce84c0daf5f247dcfde377abdeb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:00:15 +0000

    open winter dry wrap disagree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
