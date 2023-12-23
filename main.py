1d405e5ccf4db50684cbd9b2b8bf0f0b4fd757a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:26 +0000

    welcome over hope child power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
