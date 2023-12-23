d1588997b6499006e99b0e0d05bdd9e714fdb49a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:14:59 +0000

    prize fly measure plant pure

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
