b512bb35db7ee2b5fede62148e809e18f61076ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:51 +0000

    home direct exactly complete relief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
 def calculate_work(force, distance):
         return force * distance
