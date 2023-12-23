c2e377c41163543b176f5e39b61bba6dcbaa6b01
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:10 +0000

    gap passage scream such effect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
