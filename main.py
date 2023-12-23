dd9f0b390f92cef733541f3a502cf2ea03d092c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:04 +0000

    spend fat false tear pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_cosine(x):
+        return math.cos(x)
