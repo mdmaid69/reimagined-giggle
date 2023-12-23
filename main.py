88c65ccef1b161ff06e11c91746c5309eaf7d201
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:54 +0000

    matter 566end exaggerate however offend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
