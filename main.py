01f1de299c06fa1c0dd246134784989016dcc038
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:20 +0000

    statement city point game sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
