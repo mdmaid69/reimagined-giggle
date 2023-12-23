6077bb8ac3c83df46791515e02f30b7111328ea3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:09 +0000

    congratulations brush respect brush combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_directory():
+        return os.getcwd()
