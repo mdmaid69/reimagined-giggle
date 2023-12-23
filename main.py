4f58aebb706792999f5a2dfa31fc64f07c22c610
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:44:20 +0000

    anxious small stiff substance comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
