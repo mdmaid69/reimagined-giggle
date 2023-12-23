978e06cf700d66ea11ed885a95c4e9f836509362
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:58 +0000

    considering read keepout condition brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
