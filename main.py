0c5bfe5a3efe1f8cb3d1ac7ec70a629a3cb31c1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:12 +0000

    belong turnon greeting shopping address

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
