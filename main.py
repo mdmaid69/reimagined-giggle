ad440fd36550255d3b2b8dbedd03622b3385e9a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:10 +0000

    sentence hold exploration spring pleasure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
