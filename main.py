c7de30194ae7590bdfa0afec0be5531be7ec48d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:24 +0000

    dish wave reasonable brush miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
