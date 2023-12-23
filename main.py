bb514f3ff78eebc37ca7663f741bce83348d4804
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:20 +0000

    image collection behind sleep taste

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def add_numbers(x, y):
-        return x + y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
