8bc946a24f8488902850b3ce4367c1d9d848c776
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:37 +0000

    taste cheque shade crowded copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def multiply_numbers(x, y):
-        return x * y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
