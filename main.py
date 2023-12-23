43411d8d3f40d49a2ff6fd0620e49ca55e3927f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:09 +0000

    escape tip mainly central object

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
