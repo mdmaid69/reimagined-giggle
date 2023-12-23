4e49ccbde7c61b932799154b4b2a36432dc6b172
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:45:27 +0000

    deaf demonstrate welcome temperature could

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
