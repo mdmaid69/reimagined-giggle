09e38e557e4a57e17d14e5f8c3d969d8dc5de542
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:27 +0000

    whistle accident put street tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
