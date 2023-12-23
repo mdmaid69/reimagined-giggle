ce9169a2f0e6931982fb75ec117b243f7f16a551
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:43 +0000

    lend from tap content quality

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
