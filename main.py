994b7ab25120550622a0fdac0c8081355d12d51d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:13 +0000

    breath hard mean production scared

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
