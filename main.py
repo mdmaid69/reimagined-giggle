9c51810560e25781d7d150b49a34b127c8e1aa84
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:22:18 +0000

    awake 565end convincing bury breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-print(os.getcwd())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
