6ac73c6c3612ab9147c90643298d206271b3dddf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:34 +0000

    central minor expect bread part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
