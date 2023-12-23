a8205f6db19762fbac0730f4d0b38178c8fbdb75
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:46:08 +0000

    juice goaway callup doctor cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
