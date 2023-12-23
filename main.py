f5af0afce2cc7f223a3f09ebeeb0da9ff76099ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:03 +0000

    wear godown Ff otherwise surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-print(os.getcwd())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
