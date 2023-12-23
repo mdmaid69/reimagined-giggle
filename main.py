df2c2d4241ea2c870aa4bc89b41302007f8b966c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:40 +0000

    definite throughout display careless collection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_item(array, i):
+        return array[i]
