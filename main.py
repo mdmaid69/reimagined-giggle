11f9553f0b26b326c206da9b40bd47c68eb70ad1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:39 +0000

    phone decision many claim contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
