fd5496f8fbe1fabac29353179632615eb8d9ab17
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:33:18 +0000

    switch tap habit remain bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
