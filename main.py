0cd2221efa78e1825ee61a08b6cfb437e97b9340
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:41:14 +0000

    course innocent children stir march

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
