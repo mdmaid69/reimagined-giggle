e41d1914daffc74fb25de0613128c84aa1bc427f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:43:09 +0000

    slowly interrupt waste obvious wander

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def square_number(x):
+        return x**2
