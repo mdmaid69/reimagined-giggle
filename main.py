f07ce6176a5deed28e6ebd2f787e6f6586a5f182
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:17:48 +0000

    dear life check concentration discovery

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
