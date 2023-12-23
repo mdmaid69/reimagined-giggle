7bc2563e3a330de4800a1715fa83a0da3df01ca5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:21 +0000

    prove need rest along copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
