4c6daaeb4df892e3179aabda117986def828976d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:32 +0000

    fear satisfied dress furniture pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
