6a80b04f999e0d09541745472350316840b31b73
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:06 +0000

    purpose cruelty worried worry because

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
