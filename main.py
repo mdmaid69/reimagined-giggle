765c89f5c5e308fdb3a34ff49ca2fbd34c57476a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:07 +0000

    but market set used yourself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
