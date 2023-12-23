3bab76c366bd310d01730f963a6d30d548b84adb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:13 +0000

    further race Ee ashamed off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
