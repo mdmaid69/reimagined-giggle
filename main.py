5f51c96c6aabb2b3a2ee5dd8c8e2718d30d6a60c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:23 +0000

    hurry cool doctor light make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
