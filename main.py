cc672eea5f707ca3d52d084ac208498930fd37a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:58 +0000

    decision slight whatever respect charge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
