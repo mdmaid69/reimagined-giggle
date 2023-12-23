232375bae6f5c93797c6bdd4931d74ab56652770
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:54 +0000

    case 567enemy keepaway string takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
