0c104b4973848a2691b33189a6a3299973e257c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:11 +0000

    put giveup steep unkind come

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
