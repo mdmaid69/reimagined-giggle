6e99caa7fcaa7691b37c1384b2580bcda84b2856
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:33 +0000

    satisfactory holdon factory commit effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
