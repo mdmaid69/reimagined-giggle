a2b14b423887194bdc7d763200a9c0f2ef4bade7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:05:06 +0000

    dry entrance will us spill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
