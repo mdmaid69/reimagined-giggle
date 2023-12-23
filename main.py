0d433f140b1f0c313f28f8bf684bb7d05ed0d9f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:52:31 +0000

    birth make church entrance scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
