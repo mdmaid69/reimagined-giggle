141247794ef3c655dde2e921cef8bd3648ffee26
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:15 +0000

    activity free invite dance family

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def convert_to_binary(n):
+        return bin(n)
+  def sort_list(lst):
+        return sorted(lst)
