3519b55398cf49af20c04fe041055e22e9d65985
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:40 +0000

    light land feature unlucky offence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_bytes(array):
+        return bytes(array)
