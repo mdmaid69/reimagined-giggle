6d1000cfe90f12476a87a64848feadf319d22ab6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:15 +0000

    stop wrong mark poor terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
