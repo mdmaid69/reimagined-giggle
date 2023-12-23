b6f0d94511561fff06e1d95f06facccc40904d4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:05:31 +0000

    would escape shop love joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
