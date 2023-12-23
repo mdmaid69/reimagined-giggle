8e30c123fa55809b10c814db0ec5dc80d5176dd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:06 +0000

    settle label artificial turnon system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
