16a6b5099468ef410cfec51c24311c84ac555394
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:48:04 +0000

    Jj within report completely sense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
