7b29e0572e6adc5dc03fc571b29a1dfa06277d2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:19:03 +0000

    scream cope education near government

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
