cb1da4d54865a22572c6b885f78cde160c2d174e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:36:17 +0000

    smooth sign warn or impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
