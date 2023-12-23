194c18d559ed7ac72726b704ee261db3d58272e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:33:33 +0000

    high pass mess slow yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
