e77f288a929657c9443de980b9dbfa27dff68a37
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:18:30 +0000

    personal tragedy combination activity that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
