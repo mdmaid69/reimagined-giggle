d809232f4ad829fd81aa5b12cb5cc8dc5f8d39da
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:01:31 +0000

    thinkof proud rule brush concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
