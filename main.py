19ed5b7c43788b04c53db10c089bebdb282569a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:30 +0000

    now manage argue hang scale

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
