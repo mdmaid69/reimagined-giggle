aaa0909cf75142efc06c6bc4ef1018f95a426572
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:40 +0000

    bread consist yet run crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
