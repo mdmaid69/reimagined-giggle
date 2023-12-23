b2266cef276601d79e85cf6295216d7c18f57fa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:45 +0000

    rare please direct call place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
