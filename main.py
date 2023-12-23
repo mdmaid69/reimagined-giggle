82315d28fee7e6b828a8000993bba900ceef86b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:41:35 +0000

    morning charge makeup tragedy advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def is_palindrome(s):
+        return s == s[::-1]
