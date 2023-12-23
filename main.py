734ae3710cef7544d0dd8cc05324d753511f3cee
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:35 +0000

    Uu land height trousers 568energy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
