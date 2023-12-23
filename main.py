aefff407fe30cd63d2183d959d985a09e1a5c40e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:46 +0000

    impression before joke strength food

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
