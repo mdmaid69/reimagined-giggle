fdc10e0fe1efd8b611f8147ac006483442797f03
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:31 +0000

    someone sudden brother introduce complete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
