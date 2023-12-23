97bcdccbbb85cdb3bfef05d9eb0351099fa06e3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:21 +0000

    plenty meanwhile wait however brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
