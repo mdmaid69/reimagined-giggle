1122b905f8eee767b33c6242ef6c60db1c1897f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:52 +0000

    learn aim principle book couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
