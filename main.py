54f67ab3febb973e481011b384530de0947e9872
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:53 +0000

    indeed risk delight when lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
