26299875b06d82d505e8fbeae83a9020e7cfcbc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:13 +0000

    left shock file show progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+text = "Hello, world!"
+print("Characters:", len(text))
