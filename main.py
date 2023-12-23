497a445c1fa209e37f1d5c437083cc1934683210
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:11 +0000

    emergency dish refuse broad by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
