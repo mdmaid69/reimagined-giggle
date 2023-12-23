23b71becfcdf24cbb6946faefc3f8b522c5f7d40
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:26:16 +0000

    mention mystery everybody clue hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
