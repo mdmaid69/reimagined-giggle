0b09f5ae98551bc79195fd6b6b7649c9837715e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:46 +0000

    572enter breakinto exchange none habit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+print(json.dumps({"name": "John", "age": 30}))
