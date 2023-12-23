dde125c6b65f3998f84bdec91ddae10365abab3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:44:47 +0000

    talkinto region underneath money second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
