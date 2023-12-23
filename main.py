c8aec81fc1262e1c130e2bfe8394c1e6fd164dc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:18 +0000

    turnaway model giveup train police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
