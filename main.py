0c9961fab199101982f4cbf1ae677f4a17d045d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:01 +0000

    Mm whisper risk set concerning

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_even(n):
-        return n % 2 == 0
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
