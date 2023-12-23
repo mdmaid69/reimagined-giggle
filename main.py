d3ae0d7cb6db2b3218ee6a73c3bfe2e34eab1039
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:58:33 +0000

    something within trip match godown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
