960e6329342431344bd2e65e8ff99e1423a9c009
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:22 +0000

    right many call another stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-for i in range(5):
-        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import time
+def get_current_time():
+        return time.time()
