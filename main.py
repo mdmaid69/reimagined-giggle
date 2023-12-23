a8ee3ae9c74ffd1cd575075a657275afc2db1fea
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:09:14 +0000

    exam rarely preparation accident black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
