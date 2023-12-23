0cbb967835a35ac299622000c6bac0884831d1f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:22 +0000

    consider level stare along accordingto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
