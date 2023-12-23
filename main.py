c2304eaaab0c583379e57ebcfe21976bcbb6dca0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:35:25 +0000

    cruel kill mail music interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
