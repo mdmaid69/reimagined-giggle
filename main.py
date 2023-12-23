611677f4220c83aaf008b1c956b0390474da3666
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:19 +0000

    walk reaction needle rule no

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
