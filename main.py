cf6fb05722e9ccfab78fa00844b201514cf89c66
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:03 +0000

    attitude organize satisfactory lean afraid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+print("Hello, world!")
