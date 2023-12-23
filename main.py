a9a8eef1a69c64b8a99721437d7bc1db714356cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:04 +0000

    worth mark butter responsible winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_bytes_from_array(array):
+        return array.tobytes()
