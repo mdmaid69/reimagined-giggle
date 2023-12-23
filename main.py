9844932ac949dbdf399bef94f7dc460d2508c05a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:00:11 +0000

    model cold yours clever leave

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+print(json.dumps({"name": "John", "age": 30}))
