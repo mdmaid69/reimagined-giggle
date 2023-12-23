312ddcc14453fde29514c8a591d62f91eb41d830
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:58:26 +0000

    official creature belong charge trap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
