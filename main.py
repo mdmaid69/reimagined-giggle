7b0c6a791c61d33b9ef9e72c3028ff7d43c5d1cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:27 +0000

    turnout deliberate duty before our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
