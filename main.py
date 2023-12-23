f211416b448ff454888a5e9587d3594d67a45481
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:23:47 +0000

    relation hollow bang heart family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
