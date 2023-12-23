60fb47c45ea5cee3a32c6a514727c0ea5e3b2ae9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:28 +0000

    in drink callat buy turnon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
