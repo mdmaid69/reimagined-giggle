0b7f0d75c90a0f6d283b75e54bd195f22a1b649e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:32 +0000

    attractive callup degree avoid wait

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
