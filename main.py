2418bf574bc2e3a967301c3d4cefacfdf7ce2496
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:23 +0000

    close less second permission turnoff

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_acceleration(speed, time):
+        return speed / time
