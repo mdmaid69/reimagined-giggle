fcb6481cf41c2e356768fe0508482d6a212dfe8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:13 +0000

    communicate dance around row mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def count_elements(lst):
-        return len(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+print(math.pi)
