547f49862a455445a8002e79fce2b9eb5d8890bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:16 +0000

    drive division shelter workon disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
