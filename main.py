abc832c4800501e679e5791fbb7bdb916c7c2cb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:25 +0000

    mend spill belief rich it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
