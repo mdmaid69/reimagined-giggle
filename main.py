83ed37fb59eb98c4eaaff732a63d7efc5ef57a43
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:45 +0000

    destroy spread must bag interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
