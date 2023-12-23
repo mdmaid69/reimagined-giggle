68c76a9d2233e5373dc13f338ef8b78d3bd3257f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:00 +0000

    spend shock how 569enjoy act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
