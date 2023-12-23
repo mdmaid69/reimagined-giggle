a9c6d1ecc479882ba8bf446c54669af0e0b5d3c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:02 +0000

    once read negative complicated back

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
