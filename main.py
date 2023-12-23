36e3b396dcce82a16943b9b7bad0e4cd097f8b2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:49:41 +0000

    bringforward taste view dress trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+print(math.pi)
