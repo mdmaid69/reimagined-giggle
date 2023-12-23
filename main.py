fc5010d8c0a2c169419ed117810ffc757b87c4f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:24:54 +0000

    branch mine unit cruel beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
