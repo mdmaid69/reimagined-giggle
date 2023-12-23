aee2765cb522b08aa9b84d49d9e7986c507ee3ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:29:00 +0000

    point deep refer water earth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
