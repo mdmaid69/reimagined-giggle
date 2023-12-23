e188fe6ef9bb95c3abe7700f8842140bab4ea456
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:30 +0000

    front guilty responsible exam debt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
