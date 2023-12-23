573531a3ad9849cead2275d1a36a05a6e727ed41
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:18:09 +0000

    to dick transparent offence wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+import array
+def get_array_as_str(array):
+        return str(array)
