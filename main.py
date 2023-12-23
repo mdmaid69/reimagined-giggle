3935d034aaca20cb12e9151f0b5de2137fa9682c
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:44:51 +0000

    cost well excited vanish become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def reverse_list(lst):
-        return lst[::-1]
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
