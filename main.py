3619564f8af0912602cbd64260d208ab07dbc064
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:08:00 +0000

    steep arrival long division attempt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
