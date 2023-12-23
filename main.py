1c28a6260d926ffa11868ffa829ec05de27f6a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:47:27 +0000

    beautiful discussion runover several bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
