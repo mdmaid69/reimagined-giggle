dc8185bc73968fced4725afb963c7a2b3350dd6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:22 +0000

    doubt centre freeze principle turnon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
