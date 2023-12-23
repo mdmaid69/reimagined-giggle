45f71f7e4e17db5acc2ea66fe398ca057f86f718
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:44 +0000

    interview trick flavour upset please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
