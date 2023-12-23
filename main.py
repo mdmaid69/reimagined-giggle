c45b2da93469cf2b39a325a9e342970e3f69d171
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:27 +0000

    family translate stand chin opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
