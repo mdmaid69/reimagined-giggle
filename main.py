77469d9e77e508854962fb29b50faff3f2e1422d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:07:51 +0000

    waste secret walk grow past

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+print(math.pi)
