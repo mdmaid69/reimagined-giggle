d3e4ec5080378b09cab6be97257b310a9826dfd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:34 +0000

    goout breakthrough frame win skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print([x**2 for x in range(10)])
