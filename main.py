7e04c5965e3518e666d850feb3c9ef92e9523986
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:07 +0000

    tear save university note bad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
