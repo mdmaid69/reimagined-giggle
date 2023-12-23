69cb696ae33f42d6931bdb801e7f4159df49de1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:29:55 +0000

    keepaway hold live relax second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def exit_program():
+        sys.exit()
