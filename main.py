0a7a8f0d57b42414091f62a691d939e72c3518bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:48 +0000

    everyone trap laugh person beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
