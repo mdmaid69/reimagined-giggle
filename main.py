bb889fea6f97508e3559408382808db9b341322f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:44 +0000

    like level practical away speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
