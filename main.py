adfb7d4c3cdc3f84c459395f5856ff7be770b2a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:57 +0000

    work survive film afford climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
