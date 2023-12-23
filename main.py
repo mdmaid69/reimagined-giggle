41a8d701b353efd2fb1a8ad35f14eb6389f71701
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:10 +0000

    moment even relation confidence sound

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sys
+def print_python_version():
+        return sys.version
