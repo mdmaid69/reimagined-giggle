1c8f8d5a26f12ef33954cba96e839bc7b05bcda4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:54:42 +0000

    explain large eat spend ground

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
