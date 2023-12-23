856a36ad458c0ab60fe01cf3705d4942424c0555
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:44:08 +0000

    silent hungry chat material block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
