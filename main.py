93c97ebe44f6e9c308cd16cfcc06e704f4c75e05
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:16 +0000

    programme adjective simple various previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import collections
+def create_stack():
+        return collections.deque()
