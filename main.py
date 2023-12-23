eb30d6130d0a77a4b4b3c439c49d4d6439d8dc6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:23:16 +0000

    cost keepfrom necessary last vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
