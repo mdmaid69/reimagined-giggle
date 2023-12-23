6008a4c4582539eb60da67814e74214e51f62557
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:05 +0000

    fall spot strange wave load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
