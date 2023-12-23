9217a4c27991dbb59a71069b03f9ec4d163a5e70
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:58 +0000

    always brush purpose comein just

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def count_elements(lst):
-        return len(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
