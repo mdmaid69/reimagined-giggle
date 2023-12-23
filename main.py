2102f0fd37ac8be271ee2ad8239ac76918493e9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:09:25 +0000

    repair behind nerve tonight cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
