8b33f29dea952c1ff0ce5bfb875792ccafbb952c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:23 +0000

    cap control most hot attractive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
