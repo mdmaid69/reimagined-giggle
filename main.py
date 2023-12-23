5ad4b055a4a848fbc8f38d41792f98214c00ae3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:05 +0000

    team common past flow perform

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
