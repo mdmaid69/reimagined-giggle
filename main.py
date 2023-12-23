a5a4de13859c7b458954617adc26b8284cee4acd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:16 +0000

    anybody pause compete almost thought

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_elements(lst):
+        return len(lst)
