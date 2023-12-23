860f95b6d49d8616086d0677b670954332a566ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:26:53 +0000

    poor second mine share design

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
