f3a672dac038210e71443aa94fce8be8e77f69ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:59 +0000

    call general emphasize sound attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def square_number(x):
-        return x**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
