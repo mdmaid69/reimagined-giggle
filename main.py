44cef61bbf2034d7fb040cdae2bed57ec60c85b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:13 +0000

    land limit or view ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_binary(n):
-        return bin(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
