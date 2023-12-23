93f2a80aec41aca7bb8a450928f0ba4699043e1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:34 +0000

    company exciting empty never empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
