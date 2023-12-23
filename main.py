4bdce2b06fa28541196e17458b9a3e5501559e66
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:11 +0000

    both include dick steer exit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
