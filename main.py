d437288e41c4ed044b643a19e5a115072249cac7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:21 +0000

    excuse familiar cold do most

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
