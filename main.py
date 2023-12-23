e173e3c3dd22d7171d2e8e1f0d559a8f7af06d14
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:28 +0000

    want theirs uncomfortable vision spin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
