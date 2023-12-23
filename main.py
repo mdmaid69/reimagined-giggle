832728e960c7c7a0290feea2a328c129730552b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:24 +0000

    me flight spoil break cry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
