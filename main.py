f0c40881249a3773aaa31bb370059dbd3ed9b416
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:03 +0000

    interview mess familiar calmly injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
