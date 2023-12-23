15d98ce21a7624ea2c2ea75f1ac432072df52c61
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:33 +0000

    lot anywhere goout around centimetre

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def count_elements(lst):
+        return len(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
