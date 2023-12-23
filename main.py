4aa892814d3441a2301cdecad7578d0c1ac79f7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:03 +0000

    press shrink what deaf beginning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
