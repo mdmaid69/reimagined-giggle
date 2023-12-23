52f95a3961c88fd9035888d9a8d465c877063177
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:17 +0000

    base last important build rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
