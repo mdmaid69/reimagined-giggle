7f10b9843210cea498986e901c27370ce3d87f88
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:00:02 +0000

    well reason second loan skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
