ba5f8bcd0cda5b8ee42310e906cd0deb3e0457ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:10 +0000

    neither direct face ticket Nn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def convert_to_binary(n):
+        return bin(n)
