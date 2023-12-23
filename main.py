057c9229b05900c4786f0ddd01f9c9963e7c0178
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:48 +0000

    negative judge create rescue release

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_min(lst):
-        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
