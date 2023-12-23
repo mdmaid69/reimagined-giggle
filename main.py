cf0118512f1158ee595e0faefdbe18186b09b8bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:52 +0000

    shall insist comfort say sudden

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
