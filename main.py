0c1bd0160afdae3c7aeb8e75fe8d15e5867414bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:33 +0000

    children underneath Ii left floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
