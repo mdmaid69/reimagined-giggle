ab7739c0b09d8323fd323079a50a9f1db93d8edd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:14 +0000

    thin pot used middle limit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
