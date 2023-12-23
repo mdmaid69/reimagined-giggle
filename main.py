94d10020dbe1f609311a832f7a8b3d8759d958f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:55 +0000

    importance empty relative unusual face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
