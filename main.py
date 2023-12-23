620a10c6b1567f14427aeb75c639b345015a3943
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:47 +0000

    ground neither once bin meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import time
+print(time.time())
