2f139090bde016337b0e6e36f69e47dec3e1bbd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:14 +0000

    lookaround machine bit frighten separate

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
