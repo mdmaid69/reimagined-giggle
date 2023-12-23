6f6616d2f4b9de92b6729226f193a755a28f8fa7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:41 +0000

    later unable during workout chief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
