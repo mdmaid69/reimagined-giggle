61b0508ccf8642949a6d4a8e7f4f9784af75e615
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:58 +0000

    forget concern worry rarely down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
