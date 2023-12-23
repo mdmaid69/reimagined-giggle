a9cc0f6165daf789bbeede505b6d18c557192052
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:12 +0000

    die quantity unless ignore quick

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_queue():
-        return collections.deque()
+def subtract_numbers(x, y):
+        return x - y
+import tensorflow as tf
+print(tf.__version__)
