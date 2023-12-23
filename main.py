3d608fcf0d11b94897255474b590042d5b854d51
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:58:44 +0000

    personal pot instant free return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
