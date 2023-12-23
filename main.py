ca16460744c19a79d083dc42d9a1316250057967
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:16 +0000

    scare drawing rid quiet wild

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
