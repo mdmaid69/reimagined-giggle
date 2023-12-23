04a5798882116e8063f9a1d730f1cab3a48a64ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:52 +0000

    ours compete reserve letter loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
