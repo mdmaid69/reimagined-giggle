e0827db06fc2b3f46386548100589f4d5b491718
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:22 +0000

    sky organize dark injury travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
