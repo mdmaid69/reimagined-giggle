3594e0eaa0367ef0d3a93d6c46de1fe1052ef652
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:54 +0000

    large about offer turnaway excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
