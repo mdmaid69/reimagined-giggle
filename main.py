b4eb8fa76e10d7e577bd115764984e4a194744f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:28 +0000

    variety keepup hurt calloff answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
