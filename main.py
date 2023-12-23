0376f39cde8624a50700ee142f19bc510533d302
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:30 +0000

    yet study children change one

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
