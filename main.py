5a0e94ab9c0fd4bcd6f2eb99c46b0c58f90b8aa4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:28:03 +0000

    instant thumb press something shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def reverse_list(lst):
-        return lst[::-1]
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
