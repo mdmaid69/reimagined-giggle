fdccabdec9f73f8c86b0f00c0e521b4bde3e052f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:26 +0000

    guess become cruel talkinto strike

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
