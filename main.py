fe7aa382889f4da8af31b6535762b52ed3876642
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:50 +0000

    listenfor name improvement strict shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
