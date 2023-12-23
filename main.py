d8fabe27eb4a77e906278fea0852e805970626e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:54 +0000

    ask comfortable negative after in

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
