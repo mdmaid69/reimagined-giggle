1cc66ec01c52307cddbbdac6f019ce6d1c208618
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:01 +0000

    scheme comeoff breakin period only

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
