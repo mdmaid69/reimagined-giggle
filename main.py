da7b5efa62cdbaa3a81f1455c2eb13ab92961b0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:30 +0000

    sensible takeaway miss snow peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
