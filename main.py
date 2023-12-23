7e3e14b4c6828bbcf74783c3fb8df8b2f3247579
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:15 +0000

    used embarrassed guilty mine 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
