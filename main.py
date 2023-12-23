9fadc65910d6943790441902406a711cb1260c5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:19:26 +0000

    centimetre dance early operation computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
