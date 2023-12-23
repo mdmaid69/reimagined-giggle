74af907f7c02a3c27119481bdd7006082c4aef13
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:15 +0000

    excited turnup list minimum offend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
