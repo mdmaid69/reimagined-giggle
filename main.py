14834f9de2ccfb89b764710816d906ccaee3fddf
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:58 +0000

    cool if afford circle lie

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
