fcb66354db0c6d7b07cc925eedf146fb2efd9e5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:03:59 +0000

    loud kind name unable generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
