312345bf5375ad804915594b15317e446e946eea
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:06 +0000

    stiff Gg follow above judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def reverse_string(s):
-        return s[::-1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
