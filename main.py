1efbf39d5b9c9488d3e6649c3684f767cb7b80b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:06:02 +0000

    fall final test case answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
