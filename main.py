e4079bf42e53be1926567d021dfbeab2bda1ec65
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:07 +0000

    let comeover used apply vanish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
