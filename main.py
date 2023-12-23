0a40d5dff7cb6e01ef64cf41e3a6964eeadf9403
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:42:18 +0000

    stranger design power delight bang

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
