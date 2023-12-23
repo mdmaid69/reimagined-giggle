c97203d9f7b5c68801c17df6b3347e6973a80d1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:08:42 +0000

    space government freedom now admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
