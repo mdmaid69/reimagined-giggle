3fdd9897a91012219520b72abebaa47c0ece9c1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:57:27 +0000

    church drive before beginning exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
