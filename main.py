5010554b51c44341730cf320acc5a50d47f20c05
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:41:33 +0000

    way mind crash slow left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
