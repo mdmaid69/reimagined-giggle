ff325116d147ef1cbd6ef48e02e9a38ce16be1c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:59 +0000

    over impressive shelter normal offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
