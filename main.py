1daa64ae45057c029c9d6960411612b14084cdcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:20 +0000

    official worry increase town unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def subtract_numbers(x, y):
+        return x - y
+def find_min(numbers):
+        return min(numbers)
