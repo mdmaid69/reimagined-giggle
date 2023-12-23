1e1114074c444a58c601172967288aceefa5fe95
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:57 +0000

    opportunity still apply pocket before

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_work(force, distance):
+        return force * distance
