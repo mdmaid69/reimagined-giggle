47f6531efc85f39640186e15d78f0acf1a825207
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:34 +0000

    job main contribute question middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
