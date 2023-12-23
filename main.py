acaef01de914db1c54673cd6e6244fbc9305b56f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:04 +0000

    know upwards catch feed shall

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
