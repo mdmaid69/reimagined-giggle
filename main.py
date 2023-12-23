750a01ad7e51ed84039a33be3ab0182fb3101a8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:06:03 +0000

    to keepoff relax sound art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
