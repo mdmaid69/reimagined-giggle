dcf0020d65ff52b8a06daf579f85363917b3bf4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:31 +0000

    some standard stamp together even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
