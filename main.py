a31a56621046685b5d32506da2de943ef769a9b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:13:45 +0000

    fat clothes laugh content bother

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-print(math.pi)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
