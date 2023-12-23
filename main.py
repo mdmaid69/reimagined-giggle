c9dd1ea192d45eb0c21dfc9f80ffaeeb9294d9f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:01 +0000

    except whatever direct interfere wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  def get_directory_name(path):
+        return os.path.dirname(path)
