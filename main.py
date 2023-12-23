883342eee1df0891720e93c00653c9a57d7cabd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:54 +0000

    regional behave him 565end emotion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tensorflow as tf
-print(tf.__version__)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
