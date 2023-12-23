26ecd044889164ea57d65fa8c89fcc4d6181448f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:19 +0000

    everything let mess neither giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
