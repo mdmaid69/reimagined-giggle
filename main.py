7ffd2ef02cc7602b4c7639830736ee1a2f55c48f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:29:53 +0000

    but situation age ought legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
