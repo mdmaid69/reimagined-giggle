b83ab14ee3b6ea1519fa8717c451e29580ace756
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:29 +0000

    piece 565end invite male on

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def remove_duplicates(lst):
+        return list(set(lst))
+text = "Hello, world!"
+print("Words:", len(text.split()))
