80005273bbd3cac6463458f24dc7842718d599e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:15 +0000

    turnon underneath breakdown add couple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-print("Hello, world!")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+print(os.getcwd())
