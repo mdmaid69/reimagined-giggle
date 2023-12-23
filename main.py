e598cdfaa119e0279b526079378da06efddcef46
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:43 +0000

    fence sure mad memory stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(a, b):
-        return a + b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
