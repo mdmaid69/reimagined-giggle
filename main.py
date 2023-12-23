6acfef966f7f89f80741fd0a561789babaf4584a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:59:15 +0000

    building at effect sale pronunciation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_work(force, distance):
-        return force * distance
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
