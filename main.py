cc95833d1a84f69053c12979e3c230696a78f100
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:47 +0000

    excited hide peace breakin bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
