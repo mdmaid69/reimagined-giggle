ee979c5295b02c650ebefdda4201840f4cc1474e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:24 +0000

    nature of switch in comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_work(force, distance):
-        return force * distance
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
