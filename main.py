b6e84ef260cce5c7a19b40eac4dba83c025faccf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:23:24 +0000

    nearly boring rent responsible shallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
