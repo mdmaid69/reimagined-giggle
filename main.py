f7626b96c3232db4f7ebc7308ab1a1df5685994a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:39:19 +0000

    love you down slide arm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
