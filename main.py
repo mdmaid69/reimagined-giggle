e0f04d73452091783525c9f2ae8e93d1061ad107
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:23:55 +0000

    needle morning load find get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
