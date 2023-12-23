93ddd9db6097f7eee6f036c8f1a9b17eb355d49e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:30 +0000

    then flow early or snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
