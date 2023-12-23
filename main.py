9eaaf9b5fec18aa511d61a04e30efb19d5fe9b93
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:16 +0000

    national bear entry beneath soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
