70534e75c0bc7073a432d35c0c044b51b5eab2a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:42 +0000

    tough hospital bunch pause grand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
