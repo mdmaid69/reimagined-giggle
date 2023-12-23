2b43f8e74a3e7733b15c77406bab2cec916f2084
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:13 +0000

    across total time tip edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
