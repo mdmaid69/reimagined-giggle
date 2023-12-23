963caca29abe690ad063c633afee6acc868d32ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:38 +0000

    couple any which talkinto either

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_user_string():
-        return collections.UserString()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
