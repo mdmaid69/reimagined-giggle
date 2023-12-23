5a26589d6b84cc198e3f0a753d5af7ff28f7ef89
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:00 +0000

    find remain plain certainly grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+print(sys.version)
