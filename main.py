03ae48da2aef3120123e45bdd801c0e3730faade
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:27 +0000

    Gg anyone repair Vv far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def add_numbers(a, b):
+        return a + b
