bf4901d177e254176adf658744ca615b8f87194d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:21:11 +0000

    smart add survive together loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_unique_words(sentence):
+        return set(sentence.split())
