6311b0f8961fae2f153de3d77cba4f77770af4e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:18 +0000

    brush Zz fix reduce OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
