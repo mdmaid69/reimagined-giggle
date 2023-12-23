a2101a1367f85e2e1fb42f3fdc76732a509c9a08
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:31:27 +0000

    anywhere will value minimum collection

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def find_min(numbers):
+        return min(numbers)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
