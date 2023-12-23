1aec8a56b5a9608d74549f1b496e17a71f25057e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:09 +0000

    sense trap slice permission responsibility

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
