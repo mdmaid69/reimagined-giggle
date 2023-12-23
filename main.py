485534cdd4a172b4918c69ed14c2aa899fafde0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:36:45 +0000

    trap long mention door 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
