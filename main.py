6caddf753f7a08b46be685a866db295631fdc33f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:40 +0000

    market centre only compare formal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
