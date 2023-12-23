f345fca9fd65e710a13d2b5263b5143fbe123a0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:58 +0000

    want straight performance kind earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
