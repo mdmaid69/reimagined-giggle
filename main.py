e992c992835df0d40d6f8566203a1cf14f08a3a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:35:00 +0000

    game hand stamp punish crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
