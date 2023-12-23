f951967249f4af4b1d5353618991b9dda53cfc72
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:40 +0000

    shock Zz climb abroad lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
