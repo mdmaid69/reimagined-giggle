eed1858fbf5e4a2ecd734c10ac6b5cfb996a8b57
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:40 +0000

    precise character poor print talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def square_number(x):
-        return x**2
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
