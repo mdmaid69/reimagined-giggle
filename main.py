887bc31b48ec5188b8dd0943c0cfe938eae4b6e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:10:08 +0000

    boring butter finally idea hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
