33de71e2c826bc98d749d2a04fd387030d841b55
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:10 +0000

    mix word alone effect think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
