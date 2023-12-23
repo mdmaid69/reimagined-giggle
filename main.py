1451ab33ea9288ebfbc9ae12dbfb445a1fa1791b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:29:39 +0000

    whisper herself it top yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from collections import Counter
+print(Counter("hello world"))
