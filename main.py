6aa77e1c321d552ec0a56b538b76ccb01f66b6de
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:32:39 +0000

    stamp temper before bus tall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
