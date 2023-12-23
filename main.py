0b56127a839de4ffa63572edb59ee4674f19de0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:18 +0000

    shall themselves Ww image used

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
