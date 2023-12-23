bbd2b744cecd2386e437df22e41f069e69d3a169
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:06:38 +0000

    agreement something basis trouble neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
