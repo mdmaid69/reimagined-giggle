3e1c491eef6b36afb1fb279e435148b5c6a27af8
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:59 +0000

    consider platform passage I accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
