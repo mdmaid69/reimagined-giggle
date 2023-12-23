731d6823e469c1a560f41ab2c326df984d3ad5db
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:03 +0000

    visit case crowd brother point

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def count_elements(lst):
+        return len(lst)
