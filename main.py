c93476fd9f89ee245318ba90396959f6441160f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:27:56 +0000

    rate public community present hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
