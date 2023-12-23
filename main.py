96a72f45038e01a4145a408c1138d6ae93c5a12e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:53:57 +0000

    572enter bill smart Pp goaway

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
