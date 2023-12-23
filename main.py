413c9866b1a4ac81b44c2f15b67fcc65b9fddd8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:26 +0000

    model destroy mine fond couple

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
