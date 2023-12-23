d24466b123442943891357938e44d78f23cdb0be
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:16:40 +0000

    contribute regular obtain brief 566end

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
