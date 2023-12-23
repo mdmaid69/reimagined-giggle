7d23f130f49ee16e9b968c6fccb7b440331f6d66
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:46 +0000

    seat frame concentrate central turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
