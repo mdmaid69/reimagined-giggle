0aae688ce75bd623adf0b1f4da112eb875ba8e6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:39:36 +0000

    tour number phone closed which

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-print(math.pi)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import re
+print(re.match("h.*o", "hello world"))
