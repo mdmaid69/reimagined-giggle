a27c5f00c159df96855be4059240a7efcdea7deb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:27 +0000

    press although late plan dance

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def convert_to_hex(n):
+        return hex(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
