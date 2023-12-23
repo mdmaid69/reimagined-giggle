73741c59c097c42cb6fad9fd525805932778d5c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:26 +0000

    wave mouth strict suck hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
