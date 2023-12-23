a2bd3c00f9e5260e008ed6159e247e1e3ffb92dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:16 +0000

    shape famous track deliver note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
