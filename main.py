ecf9f02c344662f5b05cb2758ff52ef4a2bf3ff3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:50 +0000

    572enter refuse around hot interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
