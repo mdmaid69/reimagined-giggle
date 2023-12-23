cbdb57f2c8bbd054e7196c65f76c30ed901540c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:57:10 +0000

    patience right characteristic around activity

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_octal(n):
+        return oct(n)
