7f8afad64e8e5238431c910d540a44c1d7e4055e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:44:39 +0000

    flavour war light serious market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
