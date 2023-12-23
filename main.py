6c8a8e73c983e957ef55d38989ef34b2d03bcd5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:57 +0000

    connect plan near themselves sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
