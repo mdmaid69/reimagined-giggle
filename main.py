6be4304edbbf9877ae77293af420563bd7243bf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:02:22 +0000

    treatment deserve some difficult open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
