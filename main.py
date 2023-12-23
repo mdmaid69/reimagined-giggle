25dd760366a1d6ab10e68d06def8bc65712e9fe4
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:32:31 +0000

    lock ear study want brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def convert_to_octal(n):
+        return oct(n)
+import time
+def get_current_time():
+        return time.ctime()
