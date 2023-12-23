8e3c6405ae87564af86014c1f0fd920ad7cc3976
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:48 +0000

    clean Zz positive impressed party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
