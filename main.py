54ca787dc40c62f647c672c9754a8bd51fc05ccc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:04 +0000

    public coat fix smile press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
