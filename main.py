eb0d180d33926e615a22b3d3ddc695af077f579c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:11:57 +0000

    sweep shame second me temper

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
