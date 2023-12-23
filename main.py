068b3ab485ed6b5dc8b96565de10416f6633338e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:58 +0000

    they hurt meanwhile move talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import collections
 def create_user_list():
         return collections.UserList()
