2d32a4424a67b9003f0940b6e85e387ffd0f5ec8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:54 +0000

    copy press continue advanced control

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
