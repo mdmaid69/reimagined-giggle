ca3e0d23d1dc646ab6f6b2fff106c9f4578b72a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:34 +0000

    under inquire mix central themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
