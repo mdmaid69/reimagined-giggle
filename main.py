a2e6dab9164c4ee9c70477a8dba7a852f3ba8517
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:14:59 +0000

    report out runaway key basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
