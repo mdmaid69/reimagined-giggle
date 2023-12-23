faabed762eae2910bd5987bce44a20b93dd39f6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:11 +0000

    injury fear ago wander watch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def is_even(n):
-        return n % 2 == 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
