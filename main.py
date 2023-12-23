99dc6e2f87ccc85cfe2c5bb3b2dcc7298cffbd50
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:51:39 +0000

    broad neat ill perfect worry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
