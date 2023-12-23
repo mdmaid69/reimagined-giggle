7d04ec148e5908bdce673ac0ed34836643228aac
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:12:34 +0000

    today someone statement before form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import numpy as np
+print(np.array([1, 2, 3]))
