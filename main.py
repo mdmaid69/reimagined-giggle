6301f887f5b0fb24b778c828708a9690f1ffa12d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:52 +0000

    tool schedule command decision treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
