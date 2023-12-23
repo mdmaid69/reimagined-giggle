5b99bc78144eb8214cda1af2a51a24f2291a4820
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:24 +0000

    fire control rent once nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
