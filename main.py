2f58e74431f4254052b76589456dd67c9e6adaf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:32 +0000

    fairly string doubt wave strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
