c32c8cdd513c203952bd209ff3dddfcda752e70b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:16 +0000

    project drive thirsty left below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
