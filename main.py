155b80dcc9746ac9fec305cdde09ddb0f333ffa9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:59:01 +0000

    mark kill then appeal lend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
