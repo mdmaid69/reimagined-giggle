d0cfd46092b583a7a1c4020fb3e05356e9a03d9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:44 +0000

    pain wise comfort bin complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
