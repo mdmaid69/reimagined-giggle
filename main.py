4d4c30283fb64074b22bbbb69766fe1daedf4ec9
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:56 +0000

    round shape workout none hollow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
