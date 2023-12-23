69ce4e9eb60d71dd46c7780380e7f8683ba4a187
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:53 +0000

    makeinto godown block distant base

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_float(array):
+        return float(array[0])
