09829185a85ddea2876aecf067285a462ffd6c62
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:05 +0000

    disagree tax mess operation block

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sys
+def exit_program():
+        sys.exit()
