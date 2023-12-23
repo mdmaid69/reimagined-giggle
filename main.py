fa23a016c545e30bbd395e829e8f1c61ff57e928
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:27:08 +0000

    prepare chin suffer tired drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
