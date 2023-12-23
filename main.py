d3d12b1249ca0ead7e46f67bfd681221b1a36296
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:20 +0000

    mood between petrol relief sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
