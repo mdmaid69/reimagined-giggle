aab97b29a540f5149ae3c964529c2874fd3b022c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:54:34 +0000

    embarrassed motion average section young

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
