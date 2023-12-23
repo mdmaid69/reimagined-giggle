0784c303013ef94abfe83393cbc06ce88da2ba7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:06:27 +0000

    new reply try congratulate complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-print(random.randint(0, 100))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
