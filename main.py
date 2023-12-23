db0cfb3bb8548ae30bb9c2cf64cd410253da57c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:51:37 +0000

    one damage situation feed plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import getpass
+def get_username():
+        return getpass.getuser()
