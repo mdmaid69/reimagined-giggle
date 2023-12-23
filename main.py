e2262328f97c5046aa2f5aa7a68af2923f4c0296
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:55 +0000

    talk slight seat possible divide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def sort_list(lst):
+        return sorted(lst)
