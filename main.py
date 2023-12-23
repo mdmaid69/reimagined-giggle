0022c391fa179eea83424698f121daf69010c7ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:43 +0000

    runinto dry takeover alone holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
