3d520eeeb9ca6cd55e62c5708e398a59c477c621
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:22 +0000

    grateful claim boat bit score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
