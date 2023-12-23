ac6bbb5979eed3fe0aa87c3e8cc4b72c1f387378
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:37 +0000

    check art tap herself press

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-print("Hello, world!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_repr(array):
+        return repr(array)
