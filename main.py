f32cf8fe0abf6d93a744674a2aa633170b7e3a5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:08 +0000

    slightly around everything Uu Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
