e6bb8352ec149ac5b4d8104c6971812cab042aa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:10 +0000

    destroy use nurse since star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
