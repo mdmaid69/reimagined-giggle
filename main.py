8d2f06edaf03d4583e7570e44d14d21fbd33b213
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:44:07 +0000

    advice move trip go themselves

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
