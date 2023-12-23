7f3c51c840f9610eda42917a1ecd8fcd6c19a2b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:52 +0000

    hit translate hers us bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_work(force, distance):
+        return force * distance
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
