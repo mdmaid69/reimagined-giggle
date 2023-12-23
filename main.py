cf2f48d8fcb27d4f34aea0283c741ca2af4f51d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:58:37 +0000

    divide fun goup but replace

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
