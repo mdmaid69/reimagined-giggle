941d1830daa2b05bed9a3e9d91ae0806935d22cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:15:38 +0000

    computer organize quality colour bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(a, b):
-        return a + b
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
