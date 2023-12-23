565a3066dab458d3c75ac309e4c9df38a08e3e5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:15 +0000

    someone certainly pain information among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_list_from_array(array):
+        return array.tolist()
