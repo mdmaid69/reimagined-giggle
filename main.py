a77b8d7227938254fe83680d03a174ca2401f764
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:56:50 +0000

    create hers dear left except

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
