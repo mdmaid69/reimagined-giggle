640ee5de3c66ef2ed8695281d1a26496809e451a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:43 +0000

    harm impatient flat pity best

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
