edd666762189fe7010b91d895ddb119a368fc304
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:09 +0000

    down inform goon blue talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
