d899764c92a00c7b2c6d086ca90319168029eabc
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:19 +0000

    formal will develop ago lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def add_numbers(x, y):
+        return x + y
