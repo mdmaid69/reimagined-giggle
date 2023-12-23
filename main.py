b304a90cadebd4a33b8728300148fc0eb2937444
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:29:23 +0000

    organization concentration account discuss bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
