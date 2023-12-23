c82ae348431123b437c70e9516ec9e8b1b1a9d3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:20 +0000

    cope give know for front

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
