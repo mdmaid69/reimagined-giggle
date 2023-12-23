9874af48a2fa58b95c2c9154c99971e5dd843ea7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:07 +0000

    there shout throw form typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
