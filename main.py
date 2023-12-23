0e749e750f9d62ee77aa417913f8858658fcc215
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:01:51 +0000

    acceptable reduction base sticky success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
