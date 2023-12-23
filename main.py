662831870b8052060b7a9a69536882bf9de700fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:40:55 +0000

    build copy mail tense Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
