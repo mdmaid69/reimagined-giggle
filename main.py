167a40110ad208f947655361d275d114d738e9bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:48 +0000

    difficulty waste lost cum control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
