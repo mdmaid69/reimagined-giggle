8d516efdd512e4cb09446473f8d95058b52fa716
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:08 +0000

    anywhere past compare form hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
