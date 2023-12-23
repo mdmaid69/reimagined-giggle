c8386a21d21e32769f48ff153aef1d69736cdb52
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:46 +0000

    fear cause small pain noone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
