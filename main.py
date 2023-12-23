e0d384441341a8945973292a6266f0ce0b164ea4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:01:20 +0000

    inside cheat seriously harm conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
