f1d6be4ad375691b3fda9685449ef26071c4846d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:54 +0000

    onto little major dead shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
