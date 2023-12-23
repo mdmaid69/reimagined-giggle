5223f84ef6590b518f9d300b43982d5a1ac46fe6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:53:00 +0000

    only sticky my pencil exciting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
