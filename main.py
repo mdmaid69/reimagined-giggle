c567a66780f5199f60ad8739da8c94f0687432ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:57 +0000

    sign lookup runaway service scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
