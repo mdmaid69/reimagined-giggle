62fcc35686d706738dc427a1329e2079fe22b801
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:29:53 +0000

    store sure invitation which treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
