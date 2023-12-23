3a491841e7392add0c5eaf0a9699764617b5bc1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:01 +0000

    cinema sell spare fetch claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
