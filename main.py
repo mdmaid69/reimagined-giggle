bb9b96dd09561a389fa61a54625ab4e30690aa3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:03 +0000

    traffic material 564encourage trap fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_list(array):
+        return list(array)
