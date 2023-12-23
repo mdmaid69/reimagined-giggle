49919bd8b41b7419e459712d5fab878325c6abd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:42:37 +0000

    disapprove my picture climb dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
