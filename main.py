e7b41b58cbcdccc026a8e4d55138e2153026a0bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:41 +0000

    show programme guard your usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
