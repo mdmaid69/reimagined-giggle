d47e0353daaed13f17da73658ed7ff5700e655b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:53:51 +0000

    relax scream discuss out open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_arc_cosine(x):
+        return math.acos(x)
