91fd02d71a68082ad69c48ce819bf6e8daa76158
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:30 +0000

    heart collect company cruelty reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
