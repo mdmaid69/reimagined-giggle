19970674a3ef55729053b8cd588b0400edcfaadf
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:23 +0000

    impressed I either any drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
