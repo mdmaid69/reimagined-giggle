34b520a6bfc682300c30aea9664c59cf4414b67c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:12:52 +0000

    holdout remember lower own lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
