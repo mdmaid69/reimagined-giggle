7532d12349e88298865581db4a4994e48eae36c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:35:32 +0000

    carry soil at cover laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def remove_duplicates(lst):
+        return list(set(lst))
