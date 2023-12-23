dec37898b80038e13ddaf38ac2cd86a334780a1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:05 +0000

    peaceful open apart thin may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
