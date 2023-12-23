6b030078a11a2be3ec5cf5a9e9ff73f4322d2104
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:13 +0000

    personal closed task see makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
