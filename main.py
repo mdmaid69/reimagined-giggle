93c018e4ee93a361cb4b3a73a4a8a03fa39ee51a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:39:07 +0000

    poison traffic angry pull dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
