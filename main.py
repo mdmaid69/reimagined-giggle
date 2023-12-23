d8e2c12f3b4541c96d9260c2ade66e8d001f254f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:10 +0000

    undo cry basic free out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_index(array, item):
+        return array.index(item)
