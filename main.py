37bbda45ce3a8da43dfecdd59fcab66b9896ac8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:27:22 +0000

    job talkback apparently opinion positive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
