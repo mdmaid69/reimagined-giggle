afa67b86fe05370950494793155da07b20264452
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:42:45 +0000

    get needle bread cheque middle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
