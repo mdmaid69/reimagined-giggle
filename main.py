0612668308797772c3347d8fac15d7c3b1050329
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:53 +0000

    rent male nearly operate 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
