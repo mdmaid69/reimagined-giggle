8234b83e5d7540143f4279bce51848927c0e9913
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:38 +0000

    march Yy succeed danger probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
