278e6418f8a06d4d9a7fb8cfdf6718a440f63e5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:04:31 +0000

    task turnoff geton talkinto scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import re
-print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
