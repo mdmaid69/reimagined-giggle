390c6b3be17a9f12281820842317b93069bd9e92
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:17 +0000

    excellent push change Pp cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_min(lst):
-        return min(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def count_elements(lst):
+        return len(lst)
