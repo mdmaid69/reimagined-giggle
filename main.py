ee3c4dc6f7a740c5e157e36087b8d99e3b908621
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:12 +0000

    promise event favourite during rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
