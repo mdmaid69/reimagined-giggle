33f763e7a07f8dcb309fad40a94d3db37727cead
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:45 +0000

    intend trial too frame settle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+import time
+print(time.time())
