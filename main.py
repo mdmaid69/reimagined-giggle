397bc130b9361c9f938453dcfc76f55e0708da8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:47 +0000

    suddenly through quantity direct likely

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
