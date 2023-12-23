7cca738f0e8db93d468293a1c0a35ea0238fb7b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:51 +0000

    concentration undo recovery loose ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
