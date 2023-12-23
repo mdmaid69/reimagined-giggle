fd2897cdff8da617e904e2a1244e7a0ceb721db7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:45 +0000

    care satisfied cover complaint switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def split_path(path):
+        return os.path.split(path)
