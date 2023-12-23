409a7129fcedc9c5b63dc4d69cf22bb2e095c403
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:45 +0000

    disagree surprise comment situation contact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
