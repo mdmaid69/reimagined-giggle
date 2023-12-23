fdf3d6747b0f5dc330e14844a7f70c2218329cb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:52 +0000

    reduction achievement kind save different

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
