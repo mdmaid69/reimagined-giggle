0eaf57cc3fc917b97aef5d23062a55213e9f7045
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:27:44 +0000

    shade base upwards air pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
