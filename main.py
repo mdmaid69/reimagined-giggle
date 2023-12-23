37e8aabc4ecefa5127428159ae163236d92fe703
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:29 +0000

    break cruelty load usual handle

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_factorial(n):
+        return math.factorial(n)
