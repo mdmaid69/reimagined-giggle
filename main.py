8a9c5a592bc53dc94bf80ba16ae19dc5fcefb2f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:36 +0000

    word fit brush programme regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
