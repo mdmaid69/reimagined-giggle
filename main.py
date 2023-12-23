47fe1332c2022e31bc3c5a091372a9386e08ca88
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:14 +0000

    impressive something pump profit vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
