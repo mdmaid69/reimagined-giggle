aaca344b290a30af7e6f133e499505a8838f4eee
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:59 +0000

    sure public waste basically takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
