3c18cb257a51cb966f0f150a90f19648aa711597
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:32 +0000

    wrap crash dare remember takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
