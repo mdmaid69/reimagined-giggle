0e67ba26222acd0d62b7a46e35e7fc3e6203b2b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:47:51 +0000

    flavour prevent compete operate maximum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_power(work, time):
+        return work / time
