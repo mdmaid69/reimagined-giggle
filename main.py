3e582831e54995d07103366d4144f7e41577f7c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:31 +0000

    impression above waste face warn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
