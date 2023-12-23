0ec5659732b49894fa70b34c3dd8a1c3809c75aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:01 +0000

    straight zero prize female on

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
