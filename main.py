4729ad812b2b507e63c7ca809a08cb90a91bcd80
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:03 +0000

    Ll tradition why constantly profit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
