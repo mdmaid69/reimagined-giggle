01d31a579b30a8009cdd6aebac2a8afce3a89310
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:13 +0000

    switch concentrate influence tone consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
