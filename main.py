8e21bed16bf3ad4b77ef3138e47319c20563fd86
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:04 +0000

    correct aware loss correction quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
