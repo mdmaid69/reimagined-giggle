3a47ee606b1b6f9cd7c17ce895cb7d71bd416dfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:58 +0000

    sense travel holiday way copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
