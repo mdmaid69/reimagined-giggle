436922367386bb1a95766721bc6f74f8f3f41b00
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:09 +0000

    bored tomorrow ahead thinkthrough anything

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
