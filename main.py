863c6e6ebe156d28dfb672d0d5a7318a23fcb48f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:46:54 +0000

    skin taste 565end flood spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
