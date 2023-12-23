23966d5984e4091723b753cd6f4da2d3b750bd27
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:43:55 +0000

    spite win ground Tt confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
