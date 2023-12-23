283affce8047d505d48e21d35d4bb57df796784f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:59:22 +0000

    buy left short car national

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
