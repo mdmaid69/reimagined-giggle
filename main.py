0cdae4ec0eec18552d41aa258040335991ccf6e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:13 +0000

    care fit computer reasonable stamp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_max(numbers):
+        return max(numbers)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
