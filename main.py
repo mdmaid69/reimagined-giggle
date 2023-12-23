68c917f485aca9ed044ac1179d4d773e8e426135
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:27:47 +0000

    aim sleep authority give possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_factorial(n):
+        return math.factorial(n)
