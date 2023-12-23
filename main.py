49483ca479451df60285f8d33b6ce485b1dd1e48
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:39 +0000

    disagree immediately disagree degree detail

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
