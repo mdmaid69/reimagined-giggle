cd85758b99e98a522c2b6c9038b0a0805bd28270
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:44 +0000

    used rather relation cross police

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def is_odd(n):
-        return n % 2 != 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
