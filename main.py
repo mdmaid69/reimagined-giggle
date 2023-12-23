bbdc525dbf693d64fc65ebfaa9e998c96b7a41f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:23 +0000

    excuse obvious branch afford takeover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
