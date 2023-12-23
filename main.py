db1e99f87eccbaacb4070bcf5ae8c926a44d86c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:38:41 +0000

    race nonsense share around geton

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
