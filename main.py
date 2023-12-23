8d77412828e6ddc23f0a50b9a0b85b66200c0db1
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:31 +0000

    march sweet minimum attractive off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import platform
-def get_python_version():
-        return platform.python_version()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
