7866486b8c5c0525fb3e2b93d7856d081cbef013
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:58 +0000

    innocent ear accordingto open over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_min(numbers):
+        return min(numbers)
