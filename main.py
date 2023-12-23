3172f8445effbd5dfe8b3a2f0c6933204356e110
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:16 +0000

    line receive about aware powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
