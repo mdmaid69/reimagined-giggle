11e54bd9620c947faa2ab7d8094cffab1deb863f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:22:46 +0000

    lie him completely create cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
