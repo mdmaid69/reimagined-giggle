8a611b73c197b54aa886e357aa928aa2d68e1286
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:15 +0000

    spring level greeting snow slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
