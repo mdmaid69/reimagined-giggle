6c860f7abe12b8c38e878cf04b5fec7653d96898
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:48 +0000

    call during test increase high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
