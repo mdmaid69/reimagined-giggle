91dccf36736a2e5ea4b5d0f0ea0986c530872c33
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:08:35 +0000

    address yours consist shock keepoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
