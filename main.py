20d79eb2bc8cba8e7ca43b2cb435d43994a13f53
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:52 +0000

    until hard base nearly interfere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_binary(n):
+        return bin(n)
