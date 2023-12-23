efcfa8bf94f3921f74d54c63d9156ae8ab024caa
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:23 +0000

    press choose form original home

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
