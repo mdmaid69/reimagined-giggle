cef5e9647c79f0e9d98f10f9ba954e9c96a26a9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:39:31 +0000

    identify development best Vv exam

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
