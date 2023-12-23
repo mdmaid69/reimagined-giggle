ebc1d8ad3a9434a8c6a254b64a215c8254ed389d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:36 +0000

    rate once tear squeeze either

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
