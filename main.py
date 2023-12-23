33cb259bc10569f51f5518eb4b2ef44fc10213f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:14:05 +0000

    possibly limit blame heat entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
