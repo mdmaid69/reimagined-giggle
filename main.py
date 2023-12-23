7668278b9d49506a73f1345306e1fb44a9898f12
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:33:22 +0000

    glass round effect war thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
