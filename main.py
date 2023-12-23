470a0566f697147b4b819207f17ceb0904f03a17
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:47:18 +0000

    survive position hour line Oo

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def convert_to_hex(n):
+        return hex(n)
+  def count_elements(lst):
+        return len(lst)
