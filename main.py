254834803d697600dd322b6b90710cc6bea80832
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:08 +0000

    harm mood impression meet autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def append_to_array(array, item):
+        array.append(item)
