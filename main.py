b52f49fcfba04e7539f4f5ffb02cb2a14cae13f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:56 +0000

    year hurry ours telephone secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
