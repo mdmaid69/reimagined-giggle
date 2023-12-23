c9efb11c178e26bfb40eab7f57653751e9ff607b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:47:56 +0000

    influence port choice succeed Bb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
