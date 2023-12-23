751a7982da05a7ddc43b248aa01f0af2d3e994b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:04 +0000

    pile slip field appeal stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
