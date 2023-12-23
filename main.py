ce5124b2e678db9ca5fe5ea536514f9137130b95
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:42 +0000

    skill getback dream kind beyond

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import sys
+print(sys.version)
+def add_numbers(x, y):
+        return x + y
