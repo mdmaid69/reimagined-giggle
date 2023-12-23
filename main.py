4a05f17a57af2013e12e6f1798db582036b86096
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:22 +0000

    pupil brief promise organization difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
