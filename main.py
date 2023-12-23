ba06d084421de0d251500f6dc55dba1ed346e838
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:13:24 +0000

    claim love fit tense so

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def is_odd(n):
+        return n % 2 != 0
