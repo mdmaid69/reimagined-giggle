f8ed148c8682b9e3c2299940703e4313881a3b95
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:33 +0000

    empty exam mark best middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
