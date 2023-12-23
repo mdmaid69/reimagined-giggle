15536e88493cf829d438c40a6957b48f8c2ba0d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:46 +0000

    bringup relax whether show ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
