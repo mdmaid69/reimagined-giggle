10bc2c8037204270f759b15b3cfaab9138d6a778
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:32 +0000

    extremely dry failure point convince

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
