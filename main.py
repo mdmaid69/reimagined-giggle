7159b14e778b348a8d82e9c022bdcdeb4f5cbcec
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:15 +0000

    suggest comment sad no vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
