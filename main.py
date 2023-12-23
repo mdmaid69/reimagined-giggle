cff443deca866a3056e8cb54b9a743cbafb2c17b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:12 +0000

    dry earth gradually fairly affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
