42daaf6dccf96fbc91e9448751b8f29b6a15a0b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:40:46 +0000

    too hate general rarely use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
