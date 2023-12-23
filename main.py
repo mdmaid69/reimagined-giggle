af96eb04678169841a08d5bae0100de10345065d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:29:07 +0000

    undo soil Ww yesterday admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
