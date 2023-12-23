5ef3a9d7cefaf19736523babe2564c386884881b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:25 +0000

    pupil brave upstairs lie activity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
