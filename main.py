bbf813a341fea1ec69b7aa6fe3a21ef76787bf9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:54 +0000

    hold new grateful add change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
