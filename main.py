86815d8bd375b41e977471236a28ab9f1eea1972
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:24 +0000

    pay remember sleep agree law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-print(os.getcwd())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
