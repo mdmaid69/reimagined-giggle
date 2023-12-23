fec0c04839d9676ea9ab39acf915dee26b51095c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:15 +0000

    him trap tax bound ear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
