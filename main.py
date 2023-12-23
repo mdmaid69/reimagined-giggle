7c8ea465bd56b13f29ad3350395604958d2b5137
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:28 +0000

    swimming completely scream our loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
