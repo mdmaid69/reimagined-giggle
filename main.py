8b77c3070b04d7e311222831c53a3a4364f12494
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:53 +0000

    wave up with dance official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
