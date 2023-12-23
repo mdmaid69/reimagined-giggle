c16bdfa3f095da8f43db324ed99bbcd136b03be1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:11 +0000

    justice search picture pot memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
