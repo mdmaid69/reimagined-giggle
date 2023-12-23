d542fa07eb65b4d00fda9d12ce406947b7f248f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:37 +0000

    add solve brave sell lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
