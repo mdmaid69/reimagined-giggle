daf6d5a725434f906e4dcfbf9f1e12d9f60fc9d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:05:49 +0000

    care pass dry Nn object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
