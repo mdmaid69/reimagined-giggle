20b37f1fc487ff176a1a9a9d14d26d3a9285efbd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:41 +0000

    reliable almost song shall morning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
