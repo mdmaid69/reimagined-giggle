fe2826c7a48d8c71acbd148b9edc6b63dfa8c4eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:52:30 +0000

    Vv question land forever slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
