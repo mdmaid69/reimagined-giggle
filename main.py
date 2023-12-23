0ab5583ba053ebcddd8bde8dc51a4bc24ce24544
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:09 +0000

    always pile pay who create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
