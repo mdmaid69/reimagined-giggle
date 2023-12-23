2a09046be134a0f010971583d9d2647c2c6d982b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:23 +0000

    create simple ask tape repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
