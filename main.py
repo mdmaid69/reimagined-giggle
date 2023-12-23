2f2db56cc33391a1e3cd4d9389d02a196ac9760c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:04:32 +0000

    valuable still tooth work bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
