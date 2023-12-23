7ddc78c604f7cd100f0666ed9c8552a9acc95b00
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:31 +0000

    illness touch mind curious past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
