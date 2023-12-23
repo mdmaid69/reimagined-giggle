20b196768dffe992e340948cefbd4fb7a97f3235
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:38 +0000

    past down trade concentrate callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
