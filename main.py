73cd38abe87a9b1cd97ef0d62272b5d61a7c674b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:09 +0000

    each find address break shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
