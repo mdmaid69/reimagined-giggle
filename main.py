c587f3e1f11e3990fc56c6ad9bbc32139c006606
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:11 +0000

    breathe pinch lot goon model

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
