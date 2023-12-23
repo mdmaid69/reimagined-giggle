6f8177e2793171687b7e945373f4b4e713d10d37
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:19 +0000

    flow exact blame cross hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
