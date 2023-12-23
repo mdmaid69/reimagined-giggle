083b3f651031a4ec8a4f936d9633bb248bf19d93
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:14 +0000

    mean we brush central top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
