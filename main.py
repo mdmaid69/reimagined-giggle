be49255f8aea8f9c1d3d3c9923fe77abbb21fa55
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:33 +0000

    the report quickly burst hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
