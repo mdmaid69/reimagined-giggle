370081407a0f027ddfbf3cb6f18f4f87b5bde45b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:52:00 +0000

    store or original recent above

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
