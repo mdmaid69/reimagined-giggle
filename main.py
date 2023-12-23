bde42c3bb87a96c55c5f9851da672e000f401ba0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:23 +0000

    far limit education note refer

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
