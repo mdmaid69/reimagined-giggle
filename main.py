7940c4f64023315e6f2ca5fd1f571fca64595ae1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:52 +0000

    dress content shoot godown produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
