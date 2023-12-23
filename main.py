03e2df896ee9c534525552e2e0ea79424ec36155
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:42:48 +0000

    start science plan we score

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
