1286e20726468b42c8df03c96e93fede653c7e6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:42 +0000

    season challenge respect soft split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
