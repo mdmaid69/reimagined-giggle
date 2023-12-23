1377a8272ca7859e701721177b6e537255ac14fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:41 +0000

    sweet work smooth fine pile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
