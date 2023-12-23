da728833962c8733bfa3c9c0cc83d946f26e7842
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:42:02 +0000

    guide doubt shade limit bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
