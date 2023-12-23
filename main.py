50c4371a6335fc6fa702d73f098f724fb29c46b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:24 +0000

    get medium clean your allow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def calculate_work(force, distance):
         return force * distance
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
