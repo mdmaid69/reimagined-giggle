98cd2ed97c62738141f7fa5f9d84f97a17ff5eb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:49 +0000

    last word special unite able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_min(numbers):
+        return min(numbers)
