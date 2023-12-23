23bd16be7013e1a4db17d8301f5d598685fdbecc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:35 +0000

    politics copy reserve information journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
