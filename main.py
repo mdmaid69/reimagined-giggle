e74b8c09adc44c472ac28df4ca523451b8885a54
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:26 +0000

    not free fail cry embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def create_ordered_dict():
+        return collections.OrderedDict()
