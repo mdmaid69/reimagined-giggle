1efcf6f9217766804969490c9e99e1e78a8c8e86
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:28 +0000

    probably comment his wash job

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import os
+def remove_directory(path):
+        os.rmdir(path)
+name = "Python"
+print("Hello,", name)
