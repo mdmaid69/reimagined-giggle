2cfb8e935d792a09268b49432baf550ca79efd5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:24 +0000

    stupid accordingto food comment goaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-def find_max(lst):
-        return max(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_min(numbers):
+        return min(numbers)
