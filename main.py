b2df88cde013a2a4f53a70c0d257fe006f141915
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:55 +0000

    programme geton basis smile sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
