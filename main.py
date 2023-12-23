bed4f15fb5f20fe21f1fb2d679e1564f90060f21
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:22 +0000

    Gg convinced move occur all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
