a8a96e4a90cae5587e37b14e80a4f25f51a1fb33
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:34 +0000

    Uu free holiday dry demand

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+print(time.time())
