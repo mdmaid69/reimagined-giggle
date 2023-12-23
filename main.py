af25c156a90396b298b107fd953fb5aa4be33716
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:41 +0000

    food sign perform be comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
