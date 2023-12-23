acf801cf97ea4122141362574da86c8e4ad10265
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:16:53 +0000

    close squeeze behave range lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
