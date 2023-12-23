af8d76244e7d514aafcce98ba5d70328ff82d9b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:09 +0000

    anyone ilovedicks easy careless rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def append_to_array(array, item):
+        array.append(item)
