742a1cf9dabfe0f22e84e2bd3b8021d138b27a6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:45 +0000

    height sea achievement turnoff shout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
