985653a1cb4f4dab50f98fd3f06491b9a8aabd2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:30 +0000

    ready little university chase already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
