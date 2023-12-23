a72cf621981737587fc4aecd8eeeb01f45c481e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:48 +0000

    bad feature coat ought birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
