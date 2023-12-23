1b1c0ad6e6d0e1d4d6e9364281c7a06efc055b6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:31:30 +0000

    stiff match actually sleep giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
