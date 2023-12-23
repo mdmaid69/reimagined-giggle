1787bc220dc662f43492df7785e772a525eab3c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:10 +0000

    excellent comment forgive skirt set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+def convert_to_hex(n):
+        return hex(n)
