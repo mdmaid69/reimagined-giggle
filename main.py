e0de8b1d1e2990b00229c106b5f8361053241868
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:55 +0000

    later bath improvement interfere plate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_complex(array):
+        return complex(array[0])
