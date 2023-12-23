4513085260ab84b707cf0f4c7fdf6b5d62a41a5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:07:46 +0000

    hide Jj follow lookaround year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
