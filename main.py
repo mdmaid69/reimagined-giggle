57f9b9406eb410c3cf1643424ce9ca8b866bb3f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:57 +0000

    object protection wind operate sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
