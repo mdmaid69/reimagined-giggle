cdbb782fbae6d18a283259a0b9bd289d792ec25c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:05 +0000

    shake anything mark geton swallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def count_elements(lst):
+        return len(lst)
