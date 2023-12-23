c2d80bd4278faa08ea85b2aade40b739fe58a18e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:06 +0000

    interruption grow risk cold touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
