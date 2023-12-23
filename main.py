68133744c34e21777d42bc8a33aa86c8fa28c844
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:37 +0000

    professional rain black gather less

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
