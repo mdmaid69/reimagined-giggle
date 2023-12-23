834b42f7b87d2f119e2df51b609b22b00c299c60
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:27:40 +0000

    everybody else moon push loan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_speed(distance, time):
-        return distance / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
