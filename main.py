d57677189f71fc4b1e9adc9f0b4dcd45ba65c842
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:19 +0000

    split fun set treatment disgusting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_even(n):
+        return n % 2 == 0
