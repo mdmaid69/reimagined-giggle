764a1f9e6dbad3c1b7ea502a4fa1f63abf4a6079
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:21 +0000

    hello with perfect bend rest

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_current_working_directory():
+        return os.getcwd()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
