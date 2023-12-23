51f338c9a19bf3c79493327f1fb9b02f88c78701
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:17 +0000

    little accept problem travel explanation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def count_elements(lst):
+        return len(lst)
