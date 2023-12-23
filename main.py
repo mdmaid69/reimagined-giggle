ae8b21561a87bcab2a84c306af23205f76f4bb6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:58:02 +0000

    trial 567enemy anything almost business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_int(array):
+        return int(array[0])
