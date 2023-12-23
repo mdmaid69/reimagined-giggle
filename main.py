b57f8c648d741535e44a4dc0562f3c8b20ec5cef
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:26 +0000

    creature raise sky interest getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_work(force, distance):
+        return force * distance
