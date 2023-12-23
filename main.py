9aad4e06118170198602f9fd223f26a714406e2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:55 +0000

    someone comeup increase road read

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
