9794f40e6e4edb0db33c11cc4433edc0f6166a24
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:39:10 +0000

    fail anyone turnover regard bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
