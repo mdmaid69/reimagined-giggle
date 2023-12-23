dc70a966368b0af0f399db81b2667002e62d07fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:20:33 +0000

    present brown lay scream fair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(numbers):
+        return max(numbers)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
