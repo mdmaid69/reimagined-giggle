d93d8bd3930ec4d553aa26860b864f033a3bee06
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:46 +0000

    zero gather aim suppose talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import collections
-def create_stack():
-        return collections.deque()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def create_user_list():
+        return collections.UserList()
