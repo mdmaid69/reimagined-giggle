dcf3b7ad9c97b310e67dca285a7cdd1e842947e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:36:17 +0000

    provide beautiful desperate television declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
