d0efb3b48d2db0b3e17201650f2de4c13acc2b08
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:53:39 +0000

    appeal idea performance tense small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
