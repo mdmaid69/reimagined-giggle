2e5985b912fbeb40c6b67ed83c4146c661096c17
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:38:34 +0000

    although cause about second far

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def greet(name):
+        print(f"Hello, {name}!")
