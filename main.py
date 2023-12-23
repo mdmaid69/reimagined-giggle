84c3d252688d60cb00e88138e6984c011fbd60f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:49 +0000

    secret not wind hear position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
