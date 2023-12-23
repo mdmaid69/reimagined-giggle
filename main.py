2db4ae1d5c9ddac872265fdff713d8717f9bdb81
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:59:03 +0000

    post key move spot contact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_length(array):
+        return len(array)
