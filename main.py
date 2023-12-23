957f4307bfd8cf2e68a556b3c30e58d4c84269c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:03:19 +0000

    sick thick something hurt secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
