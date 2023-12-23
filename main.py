b6852a40fab0982d8d4d5b2922488a0305fa102b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:38 +0000

    boil probably noisy serious second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_queue():
+        return collections.deque()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
