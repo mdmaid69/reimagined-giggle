561ea0067037a030b5afb96cc41b8f22fe3bd8e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:53 +0000

    pretty afford bring wet ring

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
