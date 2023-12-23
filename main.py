89ab1198cab0b21fe1ee7a4ef47e4ba50b738c8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:33:22 +0000

    shop tear sink match holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_pressure(force, area):
-        return force / area
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
