958171cf875aca94440a67b59bf9eb7d96e9e1bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:33:08 +0000

    comeout stretch vote disapprove responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
