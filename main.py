9f986e238ce8b50e3ff3957b5f925038c0a6be42
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:49:05 +0000

    model tear desperate apart everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
