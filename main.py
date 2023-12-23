f8bd5f445115696d1dfb99f6c0bbd948314340e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:03 +0000

    later already cinema fly success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
