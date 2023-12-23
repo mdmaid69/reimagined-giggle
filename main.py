66a2d86d5746ebefb2a197fb03680cbc09a8f67b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:59 +0000

    aware convenient principle become appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
