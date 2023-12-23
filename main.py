f89cd0ddd65046cce3a85afb098944df0ff2d688
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:20 +0000

    breakdown through perfect successful improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
