5cb5be4e775637c196ef4ab3f868616b2ec1cea9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:40 +0000

    spin sport drawing false development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
