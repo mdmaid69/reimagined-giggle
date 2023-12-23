eb96fac16b65bfdc5bc04a499899b1f12b4a3fff
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:29 +0000

    ready offer attract want prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def count_characters(sentence):
+        return len(sentence)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
