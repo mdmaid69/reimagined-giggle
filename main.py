f15fbe9af043ff70167c7a16f1fa4638cc566a7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:51 +0000

    someone always practise hot teach

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def square_number(x):
+        return x**2
