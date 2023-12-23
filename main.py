b73cc95ac03d0caff2b265b2868d38dba0a114dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:51 +0000

    solve distance murder rob basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
