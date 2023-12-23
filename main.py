0049b5a2672345cc2dd78d74e3b4267484e772bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:46 +0000

    fasten tax noone bother cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
