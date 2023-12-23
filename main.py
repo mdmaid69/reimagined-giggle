476df61f87fa0fd68cff69385ca4eaec005485e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:18 +0000

    pencil general noise for accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def reverse_list(lst):
+        return lst[::-1]
