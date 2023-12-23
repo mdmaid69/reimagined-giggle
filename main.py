4665585e9fc907e162fe4a1a723c4cd4028f2c36
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:04 +0000

    climb repair exam thought cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
