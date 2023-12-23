b475eec5d701aa6397d491267f046095b21bc883
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:13:08 +0000

    estimate cost especially dance science

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
