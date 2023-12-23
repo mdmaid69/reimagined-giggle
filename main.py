6faf986ca04a1506a6c0d4cb6181f98c75a14ffa
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:58 +0000

    very target world standard genuine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
