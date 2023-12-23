d6e99eba1f003c53494b119162717ef2188dca72
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:21 +0000

    dream crash tongue alive trade

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
