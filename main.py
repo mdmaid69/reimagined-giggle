a483ac84f48acad16ca1221f156b167c5e2884f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:28 +0000

    touch pity serious bang cool

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-print([x**2 for x in range(10)])
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
