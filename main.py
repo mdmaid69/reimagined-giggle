1da1435945d97be13df7e9a73ab13a5e8c9fe15c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:10 +0000

    issue picture the system small

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
