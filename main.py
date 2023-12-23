59031c58691437e4c630d6114a8542d505d4d3d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:14 +0000

    fair shame collect pleasure against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
