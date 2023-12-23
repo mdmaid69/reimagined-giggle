19a2c577dad044ec694bfb8921240771207dab61
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:00 +0000

    Qq little many window expensive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
