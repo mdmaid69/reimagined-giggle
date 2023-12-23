153b895890012fd785e3b65be0a3e5094f3dd741
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:44 +0000

    pretty too formal matter tape

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
