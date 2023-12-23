cdaa438db2ead7385a01273d9d6f57e60a6fdb0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:36 +0000

    relation speak awake little sound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_elements(lst):
+        return len(lst)
