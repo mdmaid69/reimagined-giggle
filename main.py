fe930fcdbc49bb55a356fb92546f21aa86d486a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:24 +0000

    related blame pull special chance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
