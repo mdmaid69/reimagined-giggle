b0a4ea2dca3acf724d81df06cdd2ca7b45eb0c91
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:08 +0000

    search radio tread complicated nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
