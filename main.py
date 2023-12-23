bb443543cd9288cb4589700278542e27a0af3808
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:39 +0000

    seem spill apart phone test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_speed(distance, time):
+        return distance / time
+  def subtract_numbers(x, y):
+        return x - y
