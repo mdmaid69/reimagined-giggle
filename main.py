76d2e38700b3edd9eff1cd39bd100f8fc8fbd8fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:27:01 +0000

    damage natural willing skill sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
