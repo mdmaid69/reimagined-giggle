904654676178b1cdd6ce10444e7cdcc00005e078
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:58 +0000

    flood reach remark rarely debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
