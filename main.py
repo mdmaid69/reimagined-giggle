d3abda5b4a682950fec136850594f0d58045eddf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:12:35 +0000

    size doctor mass smell force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
