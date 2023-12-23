f30e42426259f42209d6792c27a8653841c5f15c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:04 +0000

    exit contents birthday cost medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cosine(x):
+        return math.cos(x)
