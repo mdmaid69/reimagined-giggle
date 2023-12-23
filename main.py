32a325ad2c2b601df3530d8d9af71114bef362c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:55 +0000

    anxious eager excuse fence table

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
