bae8627c45b576a13b5cdd93f606f4b1af582855
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:20 +0000

    near minor like walk level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_odd(n):
+        return n % 2 != 0
