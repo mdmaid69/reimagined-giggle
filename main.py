5c5873994091eb1fd75b285fa2c23dc48743b2eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:33 +0000

    money everybody wrap vote below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
