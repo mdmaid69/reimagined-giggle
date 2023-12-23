db16119111d17a4af71c4353fd06f16ad82d73fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:53:55 +0000

    onto original mind by natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
