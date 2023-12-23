3caa3e748877d56bd8d05cee889b36459abbc874
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:03:34 +0000

    theirs build shop software dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
