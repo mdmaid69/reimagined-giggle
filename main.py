edc442a17dab6b4ab0be0930d2c05f18cda6398f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:04:01 +0000

    love lookup through nature stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
