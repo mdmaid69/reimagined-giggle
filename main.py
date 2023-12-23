b28108e86252084b369ae4537ce55c5314c2ce61
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:17:35 +0000

    throw most concerned include here

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
