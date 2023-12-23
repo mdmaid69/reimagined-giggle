8a29cfd89f2eb823dff5bdbf7c089a47b3ce6c21
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:05 +0000

    unite somehow goup declaration priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def is_palindrome(s):
+        return s == s[::-1]
