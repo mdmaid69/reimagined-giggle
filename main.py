b163256eae4a0e1cbf71cdb97be689cc215b1b49
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:12 +0000

    place cost surprised around payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import tensorflow as tf
+print(tf.__version__)
