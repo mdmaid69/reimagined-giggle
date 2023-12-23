bd54f61bfd56d96b59205b413cf417cf6dcee752
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:04 +0000

    wide question true pollution turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def clear_array(array):
-        array *= 0
-  def convert_to_octal(n):
-        return oct(n)
+def get_array_buffer_info(array):
+        return array.buffer_info()
