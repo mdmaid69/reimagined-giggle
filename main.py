810cff6d43bcec39117aedfabffc78a810c97368
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:00 +0000

    touch certainly narrow passage interruption

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print("Max:", max(numbers))
