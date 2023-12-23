12ea3922bc120c5b3692705a619407e39d6f6999
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:16 +0000

    injury be know imagination high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
