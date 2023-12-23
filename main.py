86211d0a6a8842c941d39cbbfc0d95c599722884
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:58:29 +0000

    spray single should recommend keepaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
