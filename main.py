d05ff7c37e9bd67c8c888e9a99a36c54d79bee5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:13 +0000

    wise the authority Ss clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_work(force, distance):
+        return force * distance
