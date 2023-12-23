65eb9621727b001380e9eeaa35c8339ac1666131
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:09:24 +0000

    behind very cause hit thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
