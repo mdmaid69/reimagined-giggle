33c57f423bf3f1eef6fe47f4bc6dde6ecba699cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:53:58 +0000

    damp still fond string mother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+for i in range(10): print(i)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
