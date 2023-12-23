92f9bd79647300a86bd497b66cb7849ff8314d56
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:37 +0000

    wind health report purpose film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
