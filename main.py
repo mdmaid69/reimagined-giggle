9a50fd1ac640c3b1b6491ba21006b5bc1568cb14
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:18:21 +0000

    write admit compare next safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
