9bcdb5d76f7aa04ed05aa444f08b69c883de69e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:18 +0000

    565end suit crime intelligent regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+for i in range(5):
+        print(i)
+import math
+def calculate_floor(x):
+        return math.floor(x)
