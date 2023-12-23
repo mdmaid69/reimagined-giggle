f4171e57485a63c777fc524fa7b1c5fd23dce8d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:24 +0000

    away upstairs load separate apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
