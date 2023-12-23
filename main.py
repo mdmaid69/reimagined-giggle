4ad30eaddc6f833e60fb32019bb7ae37a0783a1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:09:35 +0000

    nonsense skin part practice used

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
