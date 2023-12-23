1f9777f7a6b0fd72e39d34f23d494645278b935c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:40:51 +0000

    soon thorough she load breath

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
