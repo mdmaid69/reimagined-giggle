b6ba5334e648ee43356b3be524fba18746c35c69
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:43 +0000

    responsible confuse cough disturbing aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
