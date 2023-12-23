606d508b6ad2c937aca218080797de973b8107d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:46 +0000

    organization lookback(on) suddenly Pp break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
