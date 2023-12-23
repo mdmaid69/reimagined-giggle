61acdaee80c00996c64a0b12d1e43aebf1b24df5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:31:26 +0000

    565end hit into keep fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
