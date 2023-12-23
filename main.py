ce90dec818c887d80690c3908a273590846fc552
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:38 +0000

    influence occasional loan handle constantly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+print(sys.version)
