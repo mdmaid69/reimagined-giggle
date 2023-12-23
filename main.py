7c08943994a779e3202d2bae25b351c4c0164df8
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:20 +0000

    home total lookback(on) able content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
