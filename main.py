39633763b4283920bff4f5837ae72363c0bf8fb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:36 +0000

    excited disturb previous lunch safety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
