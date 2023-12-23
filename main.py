99a82ff076bef438629499e74df0f965fe669bc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:18 +0000

    whisper flavour environment mail own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
