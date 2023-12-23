a50b9e5db8c649a8d51399d5dbb8a87b2c7837fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:04 +0000

    touch uncomfortable sign regret of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
