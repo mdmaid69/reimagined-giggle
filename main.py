c8808ca927050313b981be3172028bc5daf331f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:21:37 +0000

    child official door lucky address

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
