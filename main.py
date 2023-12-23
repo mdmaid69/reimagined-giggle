119c4434c622e25f22b2e0060a8efe81552a284d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:46:48 +0000

    block opposite breakdown which careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
