268ab2fdb0acb4b6f3ff7268721b42fe7f6b03a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:47 +0000

    city return name rise bus

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
