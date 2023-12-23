02e5ec1a297028271341ac5193c78016a96f74b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:20:42 +0000

    old direction 568energy during start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_str(array):
+        return str(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
