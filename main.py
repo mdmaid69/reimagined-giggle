1932ca8e785e5a0f04ef3cfdcfa58b2e19abfad4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:30 +0000

    some match feeling century demand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_pressure(force, area):
+        return force / area
