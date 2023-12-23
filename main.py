d20f93685a5facdbdf4620a3efa6f611f9782d45
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:28:41 +0000

    performance success broken offend mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
