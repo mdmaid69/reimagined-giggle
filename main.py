5880ed9c7c33e8e2e0914937ffb7d2c5245bbca8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:02 +0000

    control scared create shoulder law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
