36735e7d43fbaba993f24990d6dfb08acdea4ca9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:22 +0000

    ordinary pick likely comein impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
