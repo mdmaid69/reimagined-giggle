e4b7b86563806102e3491e61e0be0594ce70ce92
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:55:25 +0000

    mouth disappointed pay people moral

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
