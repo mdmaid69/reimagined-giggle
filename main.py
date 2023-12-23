ce77fd5329e16c3c41d3e93e5bc701927bc21a5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:48 +0000

    build again Yy build grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
