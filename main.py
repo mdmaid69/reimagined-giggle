34366004d0a19c782895e4a3fb561727f974c45f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:06:40 +0000

    arrangement risk miss blame react

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
