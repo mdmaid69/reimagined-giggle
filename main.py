cddacd19b29652981130d2ee2e36647b15f82c45
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:10:19 +0000

    shop journey satisfactory fence shame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
