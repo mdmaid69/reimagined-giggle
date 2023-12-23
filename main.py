5e0216e36f1b8632c0bfcaef85becafa6139658b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:47 +0000

    base limit not that plan

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
