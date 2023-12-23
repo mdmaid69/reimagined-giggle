0fb3a40cffeae8b671af7db79e8e5ca6242b6095
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:47 +0000

    safe obvious deserve 567enemy serve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
