0773967973ed007f4cd8f9965891017174f50c56
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:45:52 +0000

    win bunch plan inquire action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
