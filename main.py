bb7088762fd3c55643583bcb1889b6af1841bc5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:04:56 +0000

    surprised muscle pause pupil dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def is_odd(n):
+        return n % 2 != 0
