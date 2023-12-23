8841c783a3913c555e38da34dfadfe9fb8b03874
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:43:15 +0000

    very debt onto smile window

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_power(work, time):
-        return work / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
