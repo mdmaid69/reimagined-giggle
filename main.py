79a36557981dff3dc5149f2dbb75c1acf0632dee
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:29 +0000

    break settle sink stop crowd

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
