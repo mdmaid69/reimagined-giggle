05e000d196efb987f7a60fe159039045e7de019b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:07:19 +0000

    season minute fly here attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
