92b2a375bfab72574568e46737bb9843bc54812d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:42:20 +0000

    recovery task honest statement stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
