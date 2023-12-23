56119ccc6452a175cd69570635e4677f1ca7218c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:57 +0000

    cope calm lean sudden tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
