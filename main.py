d7dcec369c47ec29b353c7fcb79fcead1afce809
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:07 +0000

    confusing imagination clever Dd survive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
