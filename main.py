8701f6e6b816573600925dec4690f2e1a7e51e06
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:58 +0000

    twist agree yourself get stamp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
