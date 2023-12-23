8ee6e08e4da3c778c19f7defef34fc89c6a29934
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:12 +0000

    only mess hit beat burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
