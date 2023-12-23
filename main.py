2afb93838e3625f27321b234d374bd799b73ef18
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:07 +0000

    good talkback shine excellent communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
