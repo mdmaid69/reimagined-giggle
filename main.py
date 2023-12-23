9cf184fecb53fd2c0d140ee7737339dedc4620d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:46:37 +0000

    priority continue fact anywayanyhow giveout

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+for i in range(10): print(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
