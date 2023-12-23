cfb4eec0e9e83f03c50260c14e82d8e6f79b040d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:30:41 +0000

    travel determined sister in freedom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import random
+print(random.randint(0, 100))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_array(array):
+        array.reverse()
