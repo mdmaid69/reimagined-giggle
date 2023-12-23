3fe64ed16542ce3eb0c6c93b9110e2a4de3ba920
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:41 +0000

    cum valuable secret makeinto tell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
