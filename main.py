1a27402b47dbd33ebb329755a609a9d253e026ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:30 +0000

    weekend pull error share broken

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
