23e4e1681f413bfe5adf4634b1d407ff52cc78a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:39:01 +0000

    within rush do wish influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
