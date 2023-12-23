e8601eb01944c08a95ef2effad8f9f7b18fdc381
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:42 +0000

    home arm fire table wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
