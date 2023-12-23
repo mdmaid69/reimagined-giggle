a8a0cfc8002688fdce07fb1b9bda69e7dcfd3edd
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:24:30 +0000

    stranger goon this transport drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
