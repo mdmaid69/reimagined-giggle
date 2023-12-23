5fcf91a1152e291bc01bf76331cbcabce3035dcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:31 +0000

    join bite count date normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
