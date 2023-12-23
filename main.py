55969e201f520c9ff40f90413846878929393e69
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:49 +0000

    nor takeaway appeal flight far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
