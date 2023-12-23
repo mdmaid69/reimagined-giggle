b3db31c623e371a7db43546dac4301d31c425f3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:52 +0000

    contact floor nice drop spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def reverse_array(array):
+        array.reverse()
