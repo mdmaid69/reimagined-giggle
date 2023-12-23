496c1344001c7b6b875366beede365c51491a771
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:33:26 +0000

    second compare survive rarely comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_characters(sentence):
-        return len(sentence)
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
