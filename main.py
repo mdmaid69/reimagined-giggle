db6da305ee066f9533a4ddbee266950f1352efcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:41 +0000

    natural family warn no close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
