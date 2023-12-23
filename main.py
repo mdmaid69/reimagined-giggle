088f3080898666f89c2112e83aad0385859da732
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:43:33 +0000

    real impression defend tip compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
