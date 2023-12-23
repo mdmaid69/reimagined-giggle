fbb38a322942c08b3e1e631aff8196d453dd1292
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:09:11 +0000

    funny stir settle worried job

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
