470f1a9bdd2deea4529c3627fecb9530b301f4b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:00 +0000

    shallow couple class keen copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
