3481d72803ef143b9f8ea37aeb254e5030218bd8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:14:57 +0000

    load convinced abroad positive introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
