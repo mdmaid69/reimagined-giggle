30b8ade8a989d58e72e18139d7c958d1cc748494
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:27 +0000

    blow children sweep phone glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
