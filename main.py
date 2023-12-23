d0834046a331b1f70ea69577db66147c09190798
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:40 +0000

    quit exciting comeround trade callout

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
