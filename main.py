232ef30125fa0d28044a1e792fe6b334af7e8af7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:11:21 +0000

    accept Itself occur university scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
