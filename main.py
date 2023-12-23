0b9ce5586feb7ff86791963a45facf5b7f303c46
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:58:20 +0000

    splash clear line sight Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
