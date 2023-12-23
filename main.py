0387b6aba0bed13c2198b6a58e1a2942a4f64b76
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:13:39 +0000

    callback poor test turnback but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
