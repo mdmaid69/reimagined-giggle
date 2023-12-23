e148c48839fab96c341454f8d4ca3f7729b50a04
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:12:47 +0000

    error bang load would fasten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
+import collections
+def create_priority_queue():
+        return collections.deque()
