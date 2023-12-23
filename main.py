5704b414cde81c8be8dad4b391b77b7daa735b14
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:55 +0000

    left town shame somebody blame

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+print(sum(range(10)))
