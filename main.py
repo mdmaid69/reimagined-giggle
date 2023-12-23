3532ff2de1265037543cf7a9846137395c07d8e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:16:20 +0000

    confusing read doctor extremely obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
