2bbc2c3daa721ca6ae7ecf357507c5536764784b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:57 +0000

    if fall turnover comeround failure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
