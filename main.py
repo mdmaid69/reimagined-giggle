98ab84c83fc61eb911090513bc448e62e9d4ec9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:30:34 +0000

    nonsense grow glad contrast deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
