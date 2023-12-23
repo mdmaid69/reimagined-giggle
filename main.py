faa340699a5b75922d57790ba07d3fef6a353bfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:09 +0000

    why form problem press load

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
