b0802b7d79a733413efbd479e76b2e2a48eaacf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:11 +0000

    turnoff time cheek interview theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
