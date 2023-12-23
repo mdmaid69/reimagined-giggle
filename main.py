a7313b7b4f11318d48f0645af4569d3bda6d4dc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:00 +0000

    conversation favourite coffee level to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
