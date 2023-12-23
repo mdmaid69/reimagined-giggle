41b1af784d3afd12c686f70800566696af5e33df
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:27 +0000

    effect car issue worried eat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
