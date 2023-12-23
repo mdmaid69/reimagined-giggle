b5b8f1bea2b4795a35dc9e316f9c61141c0bbbf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:52:37 +0000

    middle put uncomfortable age hurry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import numpy as np
-print(np.array([1, 2, 3]))
+def clear_array(array):
+        array *= 0
