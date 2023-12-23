196682ea3d00a8082d74b53234ef9e747a4b7ae8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:15:50 +0000

    can keepout lookaround result apparently

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
