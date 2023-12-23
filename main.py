f2dc1c35f3de90821032ae42f51fbd74395b844e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:23 +0000

    talkoutof negative battery natural first

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def count_words(sentence):
+        return len(sentence.split())
