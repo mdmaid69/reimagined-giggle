7441286732b905d11330692d2623367ad7edfb1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:29:48 +0000

    height proper meeting pot last

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
