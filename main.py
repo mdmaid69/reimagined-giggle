e0db350c18c61b3b2f395157a73cbf24b94155be
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:21 +0000

    release long ability mend film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
