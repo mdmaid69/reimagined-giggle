9617624059008d8abb6b2fc60cb38fe9108a02ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:17:37 +0000

    record content holdup process when

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
