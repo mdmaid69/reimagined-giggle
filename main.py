b2a8766d7b5d540aeedcb4d2daa6beed044c88ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:39 +0000

    apart fall test unite tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_user_dict():
+        return collections.UserDict()
