e713c0d35bc62af7a01e1ff3298c58e8dedbf2c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:20 +0000

    like sticky lookafter help consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
