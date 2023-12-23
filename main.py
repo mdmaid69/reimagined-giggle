855a0665496e5fc6af89df47ffea5d80261ffc58
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:32 +0000

    circumstance warn secret sister worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-name = "Python"
-print("Hello,", name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
