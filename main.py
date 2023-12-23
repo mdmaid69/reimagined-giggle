0f98ab3c29db192d3ee2fcffa192da1899ccd243
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:35 +0000

    surprise sight pen prevent spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
