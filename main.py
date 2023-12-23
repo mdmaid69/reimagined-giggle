940faeaf1bea57d79cbf936c074fb0755608eeb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:31:02 +0000

    off exploration perform male expect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+print(json.dumps({"name": "John", "age": 30}))
