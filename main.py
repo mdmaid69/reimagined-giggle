b403d1082b8b1078abde6adfe383204489161081
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:52 +0000

    total development aware smell single

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
