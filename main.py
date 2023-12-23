fb34b12c681fd5b4a797e073fbbac12a2d6f6aee
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:19 +0000

    stir lean exhibition move opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
