14ae18eb7fe2c7055404df30bfa48b6f817fb3ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:53:25 +0000

    gradually forever shock period gas

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_average(lst):
+        return sum(lst) / len(lst)
