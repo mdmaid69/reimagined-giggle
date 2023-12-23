5cbc460dbdb3caee043e9a58a335328dd36fbda3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:29 +0000

    constantly switch explain else perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
