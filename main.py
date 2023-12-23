c87c252d57b6f2000725089f6c532abfc897d387
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:35:11 +0000

    balance dinner when breathe pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_item(array, i):
+        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
