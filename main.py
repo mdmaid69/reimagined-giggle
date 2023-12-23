724a66c41ace4c876fe7e2afb31a50724e7f0de6
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:01 +0000

    explanation building boring difficulty interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
