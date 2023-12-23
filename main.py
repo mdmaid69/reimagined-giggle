4f810b3e5190bcd9324b7c5b7ebbf1e5cf8f6328
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:30:38 +0000

    getout unable lookaround comein immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
