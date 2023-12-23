2f03647925aa79029f44529ebaa6c26c7947da5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:00 +0000

    number record wait heavy insist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def square_number(x):
+        return x**2
