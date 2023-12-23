409b32925f221c4d1fa5f56589c24e416a682ffb
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:58:12 +0000

    behaviour lunch gentle throughout beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
