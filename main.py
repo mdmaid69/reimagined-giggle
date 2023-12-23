d4f17509bbacba770f29f1f4f9250c5f860527a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:29 +0000

    characteristic for failure close character

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
