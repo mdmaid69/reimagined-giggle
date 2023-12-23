4355ea698f90e521d3e4c6c62ec21e97b621466f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:37 +0000

    maintain still lucky fear command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
