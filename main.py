dc2f4a7fbbad2f105da80a5d946716f493e05df9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:10 +0000

    loss smell honest either by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
