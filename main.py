453aa6b252a1acbbd31c280d4d86ff75733ead69
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:04 +0000

    true proper top sometimes good

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
