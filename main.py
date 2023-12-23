7d7e06a1576c96ae4e30c9e79ed4279516076d76
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:47:13 +0000

    into character tense visit view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
