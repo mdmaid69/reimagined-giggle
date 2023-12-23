a5a60d6e3ba3e02e60b43fe89b4430519b68f46f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:09 +0000

    flood near shrink common money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
