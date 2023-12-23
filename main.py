a115191cb1bd969bab4f2d86ae7acfae594c16f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:15:44 +0000

    goout unusual throughout cross left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_density(mass, volume):
+        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
