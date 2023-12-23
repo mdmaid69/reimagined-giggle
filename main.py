d1e5f9b2be1454a3130bc11c0cdf35e8d9134ebb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:24 +0000

    division represent custom balance form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
