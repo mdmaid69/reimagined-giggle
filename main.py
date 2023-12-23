c91c57a9dc029d1b6d296bf34cc57fe53bb97f23
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:18:20 +0000

    send deserve giveaway congratulate goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
