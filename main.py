edb8d8243c4a35e3788314908315c3f1258a00c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:49 +0000

    chat deserve sick damage account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
