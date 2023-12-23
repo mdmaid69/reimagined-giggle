1bcce411ce533340355f500309d4e6320b9505e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:56 +0000

    plain shall effect win question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_base_name(path):
+        return os.path.basename(path)
