55968e297a858f1558db85f6e69b26739ba1bb79
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:38 +0000

    familiar move direct away compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
