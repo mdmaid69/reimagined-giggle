6a2e96f30f58800e7ce202d2721f72e41a1bf69b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:12 +0000

    lead even lead image motion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
