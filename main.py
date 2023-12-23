a077dad36f3b03295f5c4690e618e695d19e6669
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:55 +0000

    benefit boat stranger sister promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
