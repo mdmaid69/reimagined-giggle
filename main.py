725c7896b52d42d2e90e254b6a68c893cc8fb8c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:22:27 +0000

    basically score see not science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
