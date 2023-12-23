09a2fe092faa788967aff82136c8b7920539b08b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:03:18 +0000

    crowded Bb share pretty tap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
