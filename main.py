f7bf7e4065fb281ea0470d1db7438933bf334ce2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:25 +0000

    closed him term explanation share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+print(random.randint(0, 100))
