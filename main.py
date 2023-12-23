aa5a2d51d042889239bf38fd3dddd49ff8eae24b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:40 +0000

    post complete speak mad classroom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
