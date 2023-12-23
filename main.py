77029966063eaabe29cb62020b8fcf41cfa3becc
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:48 +0000

    Yy pile woman classroom care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+print(sum(range(10)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
