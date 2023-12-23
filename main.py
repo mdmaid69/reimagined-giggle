a9c33dbd7697dbe305fe93144bbb815f443af4a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:10 +0000

    listen comeup rest unlikely introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def divide_numbers(x, y):
-        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
