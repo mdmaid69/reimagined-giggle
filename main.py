ee3a2bdaad51d126064a5a4809715effc48478e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:42:29 +0000

    factory reaction run join any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
