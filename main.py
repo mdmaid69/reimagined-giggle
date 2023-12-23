eb84d2fe720345ac24cf327efbfea3f3a8ff31a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:08 +0000

    minute throw discuss pure I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
