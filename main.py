3bdb75d004e7af7f8d69d943fd3e9286a3645609
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:23:23 +0000

    knock careless minute mystery scene

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
