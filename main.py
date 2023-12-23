a574041b1ae7bc77d9b45c387029f23cf3ea9cf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:06 +0000

    image exactly runover responsible mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
