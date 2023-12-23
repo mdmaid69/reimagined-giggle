75bd3f0111217a718e7a04494650465498712c84
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:27:09 +0000

    goout wander increase tool bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
