ac1c320a65abb63a168370f15f00dac5ed8702c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:46:07 +0000

    plenty something disgusting affect allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
