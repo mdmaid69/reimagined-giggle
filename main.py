bda37f997efc980ce3a5dc4ba71ce511863d33dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:02 +0000

    wish taste different exam forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
