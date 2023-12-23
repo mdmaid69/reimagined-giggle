67ace9387918bc9a9a2461da79f3117a6d28102d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:14 +0000

    callup twist base battery several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
