2937a1beb1731b0d0499bf772e4eb1b880718a2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:15 +0000

    light call delight computer callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
