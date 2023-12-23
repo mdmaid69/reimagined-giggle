286e487381cf8c61d91909abceecdc7d97b481f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:36 +0000

    scale serious run exchange admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
