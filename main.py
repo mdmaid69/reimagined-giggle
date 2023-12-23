715dee5fa014c3c9e97cec48c4b42f760bd85029
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:03 +0000

    branch combine slice everything contact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
