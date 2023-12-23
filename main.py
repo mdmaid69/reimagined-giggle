be05353489804e7f40315d2f34a03c3c8278d537
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:01 +0000

    workout unite probably high thing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
