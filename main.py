904cb41b7735ca338acddd88440346ca040ee920
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:32:36 +0000

    shelter laugh choose factory control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
