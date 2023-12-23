557e43e06d06d1f21e823ff0c784cbb3edf5030a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:28:56 +0000

    spoil raise undo pass skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
