1bf344df3ea25dc0dd17fd72761f25cae33dac9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:50 +0000

    determined scheme connected accident play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_binary(n):
+        return bin(n)
