f91eeeffead1fb34625facc096dd9da831434996
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:23 +0000

    depend pure represent walk keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def divide_numbers(x, y):
-        return x / y
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
