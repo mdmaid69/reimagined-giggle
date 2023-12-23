fbff50232fa2b8f931a1e4798d9a2cfc44933412
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:14 +0000

    cool report wash same gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
