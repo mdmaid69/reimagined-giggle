0349616d880870b4107e5a5cbef09ef491bfcc67
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:22 +0000

    avoid common pull boring impressive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
