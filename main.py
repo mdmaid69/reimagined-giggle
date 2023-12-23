9ad009c49474b704d5d89ff4b3b4b70a79db3aac
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:00:03 +0000

    name again owe appearance thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
