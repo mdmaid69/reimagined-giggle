1fe785ba76decf4adc8541f5f69e602f19664b43
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:44 +0000

    square use lot dig mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
