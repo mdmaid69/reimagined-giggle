d72d5c812ad9a3842236d1211fb4a6528661edb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:50:06 +0000

    possession command load fight arrangement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
