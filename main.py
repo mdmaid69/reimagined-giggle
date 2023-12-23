a28a3a3a4bf36048596c607269ca74910e6b6398
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:12 +0000

    mention damp than design patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import platform
-def get_os_info():
-        return platform.uname()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_odd(n):
+        return n % 2 != 0
