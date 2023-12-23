eddfb9eff6520cf90d0c8bd6ca682fbea8fc0c8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:17 +0000

    sit present commit bath view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
