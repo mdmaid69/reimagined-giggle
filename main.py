8f3c20b84892bfb6df1c16e31a04b89ed8699ac3
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:55:39 +0000

    plenty prevent place something dish

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import sys
+  def get_python_version():
+        return sys.version
