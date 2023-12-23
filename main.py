ffe72820d41e75fa2ff012174fc069172d77c1c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:23 +0000

    explain far main official aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
