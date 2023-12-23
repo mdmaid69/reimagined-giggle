4296c92da66cea590545c19e33d207677034c986
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:54 +0000

    later communication quarter knowledge report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
