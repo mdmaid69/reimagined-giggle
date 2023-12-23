4bf49a9fe0fe96b1643b5a0b314094f7388da6ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:08:26 +0000

    amount month from under upwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
