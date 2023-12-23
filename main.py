b78753a5fa86b20fae159c361af0367a63c70d81
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:05 +0000

    whichever climb powerful basically machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
