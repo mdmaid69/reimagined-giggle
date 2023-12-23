7e053e98867880a000fffcb66d5eea343a88d01b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:34:58 +0000

    star intend earth trip race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
