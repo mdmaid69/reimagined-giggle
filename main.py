51170f220c0191bc09a2f4167e64da2eb449da49
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:35 +0000

    show laugh task steer takeaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
