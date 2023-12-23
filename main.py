f9d0b23cc90560c5bf2cbc17dfe278ebff73fd2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:16 +0000

    wish foot area away thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
