3aa7daef3c639309fe2c2f8743d0fbfb463dd837
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:22:30 +0000

    sign of difference uponon basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
