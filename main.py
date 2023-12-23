c558c2d685d610b1300bfe0655e2d5028a85f214
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:04:23 +0000

    score feed usual course offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
