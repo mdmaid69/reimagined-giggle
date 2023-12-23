02ec8d23702be8e9b8ad1060f3e6ac0bd0684448
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:30 +0000

    boil immediately pleased float achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
