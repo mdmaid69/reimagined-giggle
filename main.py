fcc3f9809bbaa258f5f6d73f098c05c5b87a5ffb
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:48 +0000

    win call quit meet 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
