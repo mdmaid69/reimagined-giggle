df64672088be283febc8e7111b66765f5d851e05
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:20 +0000

    software film reply movement male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
