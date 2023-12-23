4c8a4dad652a9cc2b0360896bc94bdd4e5b4f746
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:41 +0000

    bad centre grade ignore stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
