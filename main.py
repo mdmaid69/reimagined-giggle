a2c2a7601f2c903d2d3e7f155cb842654a25c2ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:06 +0000

    large turnaway value university lead

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
