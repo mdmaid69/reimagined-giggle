ba075a4d595a8d6931de7c4750f0fd6c9c66ffcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:38:12 +0000

    burst muscle keen term breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import json
 def save_json(data, filename):
         with open(filename, "w") as f:
