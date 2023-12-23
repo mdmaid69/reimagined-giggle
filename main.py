a04c6ad1ffdcc063f54b6c096226f6dfbbe92e45
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:08:33 +0000

    minority object address bottom quarter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_as_list(array):
-        return list(array)
-text = "Hello, world!"
-print("Characters:", len(text))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
