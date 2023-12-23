b8eb5ad47c7547c24380d8dc0bea00abf810484d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:41 +0000

    command confuse regret whistle lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
