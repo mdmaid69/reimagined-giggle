3c923678c061ef3cc2157422772ccb17b822dbbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:51:14 +0000

    bunch sing middle solve approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
