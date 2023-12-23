ca38fb2d03ed52d6099434113566e625224c9179
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:29 +0000

    use hole agree certain cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
