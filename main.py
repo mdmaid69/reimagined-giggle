37c37217952072e5b534c14a04ca07a48b49ae69
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:54 +0000

    now Tt report properly tread

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
