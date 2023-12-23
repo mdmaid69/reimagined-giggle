cb4090c255a3dba25d906d77c26a6a8abe90b620
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:45:55 +0000

    listenfor turnover rubbish load cruel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
