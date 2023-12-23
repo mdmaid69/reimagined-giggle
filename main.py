39621cd84d0257ed2a9adb4feb25b53eceb613dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:41 +0000

    communicate trick press comfort trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
         return os.getcwd()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
