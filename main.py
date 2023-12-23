ef3b46d782c5dc757b408abdb9884bd9a5172ffd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:22 +0000

    easy last university thankyou when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
