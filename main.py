3f03b2fc147ef22898ad4eec14a988e9783127dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:51:56 +0000

    shut century which protection geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
