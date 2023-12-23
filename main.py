58cc6acfb17fc9921c679e21c1e0de0fc764ec05
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:46:57 +0000

    dress permission shock environment concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
