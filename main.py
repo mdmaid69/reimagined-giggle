37162cdbaaa5493bca6cc7d56b4432f5993378bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:17 +0000

    awake behave basic carry system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import sys
+print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
