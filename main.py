31e5950945211a7d58e508b20aa043396eb0f73a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:48:51 +0000

    soft urgent join somebody Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
