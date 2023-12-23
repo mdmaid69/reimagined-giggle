dbdb4b40dc5e1fce393a40bf26a6fdabfd2da1a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:13 +0000

    pay possession perhaps pull wherever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
