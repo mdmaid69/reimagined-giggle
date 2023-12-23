eb6fec24cfb1f537048e9fedc17a72be6203a632
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:46 +0000

    sweep height prepare bath make

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def find_min(lst):
+        return min(lst)
