7312a3be56e7a37fb09dc760f9113b962b41eaa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:53 +0000

    like through estimate priority quality

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def reverse_list(lst):
+        return lst[::-1]
