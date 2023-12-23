9b50aa135369663a54e785707976e53d250b8259
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:11 +0000

    difference side sleep weigh glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
