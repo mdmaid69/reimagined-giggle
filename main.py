a8c50a1403ed0e8d1f0b2b74ddb0bcf8c4d995a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:52 +0000

    method to future straight fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
