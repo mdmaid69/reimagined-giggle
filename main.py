70c9985118442d1e4db3064d07746db3cde40e2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:17 +0000

    refuse legal long wish across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
