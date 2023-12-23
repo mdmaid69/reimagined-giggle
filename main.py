8ced2307e25712ff7c655dd78acc72d295157fb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:46:57 +0000

    little frightened that fire sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
