3e29b95af6a9a53b8a74ab5199956d44b59c3eaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:02 +0000

    exactly keepoff clean distant represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
