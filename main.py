7d5c5183fb720b5cabf317ab41dc0a06c1aaf5a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:15 +0000

    basis false completely shelter design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
