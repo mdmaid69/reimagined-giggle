2de645e4aad91d47b80ae1b6bc984049270b6b4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:28:25 +0000

    intelligent wise split best surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def count_words(sentence):
+        return len(sentence.split())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
