1897d4c8b84bd886188bcbff9f38b8e5fe08f293
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:46 +0000

    freeze exciting dish minimum height

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
