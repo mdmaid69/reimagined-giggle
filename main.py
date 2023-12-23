de1d17528f17ba0418e1c3ca943c427b09592032
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:38 +0000

    need light disappear actual press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.time()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
