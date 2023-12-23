121b62022f6dfcef001095cc3cfd1154e3428a4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:58 +0000

    discussion skill float fight satisfied

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_power(work, time):
+        return work / time
