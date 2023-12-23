abcc187f44086f8a0e16fa576da6c150e00b73f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:39 +0000

    thanks wish prepare divide cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
