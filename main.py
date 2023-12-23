1e07f73515f66e4660535f7bd292947329dbbefc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:00:25 +0000

    chase summer world bottom use

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
