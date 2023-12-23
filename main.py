d2207cb2307822579c0286326cc16f45f6cf9a8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:41 +0000

    fit we serve expensive mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
