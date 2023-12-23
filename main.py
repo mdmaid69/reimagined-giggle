871c8712e8d6ddf545ee52a357deb9ecc4f21202
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:34:24 +0000

    getout neck criticize compete so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def get_array_as_frozenset(array):
+        return frozenset(array)
