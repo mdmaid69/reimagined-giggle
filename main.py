266d68b34c418f05716e34283776a9cb378f0cc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:30 +0000

    coat who lead hit weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_float(array):
+        return float(array[0])
