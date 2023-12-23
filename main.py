8697fe493eb78a1607a03a6141111c1c87415dbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:17 +0000

    eager point tense count listen

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
