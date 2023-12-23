9866f4d961f8915e729356d7b8ecde232cfd44bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:30:52 +0000

    each Cc welcome hate strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
