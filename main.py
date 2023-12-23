673aaeb0e9486e5650164f16ac2feda45865bff3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:57 +0000

    experience not mind depend frame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def convert_to_octal(n):
+        return oct(n)
