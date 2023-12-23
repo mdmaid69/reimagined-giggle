0df0175c9390e1a632cfe22cd8ea7416f77f926d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:15 +0000

    dark loose hurt through watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import os
+print(os.getcwd())
+import platform
+def get_os_info():
+        return platform.uname()
