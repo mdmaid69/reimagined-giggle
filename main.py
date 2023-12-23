47815c4bab6d1a1403c6bafd080d1e1c6caa4003
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:03 +0000

    holdout quality genuine man mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
