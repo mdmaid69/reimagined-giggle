d7a016ea34006fda243646fc200f18fdaa2b6f7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:31:10 +0000

    substance complain come past finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
