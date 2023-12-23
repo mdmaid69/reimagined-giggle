f0fc456f4ea1fd584a68bdee5f17635e7c83b5a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:25 +0000

    call unite example to herself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
