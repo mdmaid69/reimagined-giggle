4bfe9f01b661227aec62aa28b84c8e5ba6a54e2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:47 +0000

    mainly react shopping appeal noise

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
