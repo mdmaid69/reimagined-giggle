31434f0b410dc5100d913fb0604abbb1de03059a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:17 +0000

    responsibility life touch personal equal

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
