c322d2baf3242aa16f945a7835024d19b0091167
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:48 +0000

    happy control range people flood

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
