010dc48d4a222901cc1a41ad65a989efea56e73c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:34 +0000

    worth confused slight fear usually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def sort_list(lst):
-        return sorted(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
