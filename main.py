e9dba34d991a3319ef8efd572aa78cc626e6e354
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:00:01 +0000

    show bet so every needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
