93437bc6500e95afff140c8a9e5a7c4645823c9d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:48 +0000

    wave check top learn tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
