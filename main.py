f007a98a822e79ff8836b9f76bad20962d0bb553
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:57 +0000

    defend question connected length poison

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
