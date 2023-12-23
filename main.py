691b5fbf0e6a187be721a2434ad7b91cbd15694c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:23:41 +0000

    check change like 179bite forever

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-print("Hello, world!")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
