3d636258e976ad86a5f81de6b926bc8672d0b2a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:07:29 +0000

    live there direction breakdown loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
