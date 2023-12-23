e89cfdd7a8ca83b306da52ce2906058f00af1624
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:05 +0000

    heat deep then best geton

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
