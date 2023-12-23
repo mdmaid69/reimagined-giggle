fad2b85e8fe7a39346d75b3dc6fd8ffaeec84e55
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:24 +0000

    Yy there present either vanish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
