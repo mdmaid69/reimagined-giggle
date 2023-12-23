b8f4692fd17bfe035eea9b6e0789d47b649f018b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:14 +0000

    sense please along table lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
