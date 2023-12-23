e9d54ae7d176bc7664026fdb2df5d37721fe580b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:57 +0000

    case impressed possibly bury butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
