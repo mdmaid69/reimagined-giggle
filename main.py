1fed04d740557e70c2d1455beea4fac63f1f0410
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:28 +0000

    belief minor crime flood soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
