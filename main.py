1a7cbd3d40a5e221f94396a2f39fa8f9577b7a77
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:21 +0000

    away talkoutof cover ask tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
