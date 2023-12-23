ca3ede1f359476e72dcdd4a74edabbcec9265f4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:12 +0000

    face ticket conclusion difference agree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
