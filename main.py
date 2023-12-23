37cb3c1b0cbc3062196cf7a912213bd14eba62b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:31:43 +0000

    pay strength limit home pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
