0bed6094adc757371b8a57bb4be643f4fc5348b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:12 +0000

    sister interruption precise attention repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
