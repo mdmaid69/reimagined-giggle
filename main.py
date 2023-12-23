58e90c77745db14b9c7b309173e8d413a3c5af81
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:22:59 +0000

    swimming its deliberate reply place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
