bf34797b5db9d90173d630a9c2807c2af3af699c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:40:30 +0000

    cover past car tone me

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
