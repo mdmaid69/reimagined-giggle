ea75e70811791cb8b5bd3a85e20a6974bcac14a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:36 +0000

    left deep identify priority so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_speed(distance, time):
-        return distance / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
