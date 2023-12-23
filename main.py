4af6a2c972c06b62e1f6fe74a75c1a7285242770
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:53:05 +0000

    bored crisis thinkover pinch Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_area_rectangle(l, w):
+        return l * w
