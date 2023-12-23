3567e7b5a52bd5a34310abdd4eaf5436f26fd682
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:34:14 +0000

    reason conduct calm without certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def square_number(x):
-        return x**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
