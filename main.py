6d071452591cd70801c81815c6540f166dfd4046
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:04 +0000

    kick physical detail appearance organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
 def calculate_logarithm_base_10(x):
         return math.log10(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
