28fab92df3f6f46437afea1cb9f441d56812703c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:17:16 +0000

    towards excuse over pot off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
