28318df62875d313d9e7b071131669364a17d608
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:47 +0000

    plain fear 572enter 565end tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
