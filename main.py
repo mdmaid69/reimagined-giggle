856b1fb2d1e9842abf41607c120b9ef3cd5f39fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:51 +0000

    strong ilovedicks do distance love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
