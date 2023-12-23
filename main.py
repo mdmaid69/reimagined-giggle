6fd8dc00452c68800c3c8b0053c685163015d182
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:48:07 +0000

    ignore petrol person drop pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def find_unique_words(sentence):
+        return set(sentence.split())
