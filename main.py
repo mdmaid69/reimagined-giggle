078273f71cd0b1184d0c1d47828c81f55734eb29
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:28:39 +0000

    shallow story reason hide fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sys
+  def get_python_version():
+        return sys.version
