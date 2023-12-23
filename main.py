1010cf134f1d03829b8f6a53474bf9f65f025bac
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:15 +0000

    share minimum correct turnover car

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_elements(lst):
+        return len(lst)
