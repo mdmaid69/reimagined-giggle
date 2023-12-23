da4faec20c45feb8481f280d47be5240f1104a70
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:20 +0000

    disappointed coffee key demand relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
