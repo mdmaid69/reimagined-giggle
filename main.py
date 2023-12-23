811cf6e93939b3a32b1655e2ba841e5c0d9569e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:26 +0000

    fear uncomfortable keepout entry tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
