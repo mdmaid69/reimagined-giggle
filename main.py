56ced8c3b6d41be63e3187f629095e7be6aa5488
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:37 +0000

    during themselves permission agreement naturally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import logging
+def log_message(message):
+        logging.info(message)
