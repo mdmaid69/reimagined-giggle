65d6c815ff245085b35698d06b93c85977005e58
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:36 +0000

    style accident country personal surprised

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
