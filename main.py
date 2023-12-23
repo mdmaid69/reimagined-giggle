5651fb34204d2dacf5a0ba4220407b2dd6bfbf3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:11 +0000

    godown trap with terrible offence

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
