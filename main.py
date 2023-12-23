b0606f8457243bd0fbc89a738ee04d19e47398d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:29 +0000

    join callout wait zero flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
