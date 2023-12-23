fb471dc03b171633e5477d997c3aefe61d915c6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:50 +0000

    goup patience fail not combine

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
