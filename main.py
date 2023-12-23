ebb90d2360a6562f907ca912a72332e3f2a70575
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:50 +0000

    badly hour hold exchange race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cosine(x):
+        return math.cos(x)
