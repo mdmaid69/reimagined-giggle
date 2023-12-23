8afcea54c3c3baa0b314e7d30d1d7582557ce19c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:30 +0000

    evening punish exploration 569enjoy represent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-for i in range(5):
-        print(i)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
