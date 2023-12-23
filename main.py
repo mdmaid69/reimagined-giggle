cab2e99d577af977015828e35b106b5d783b739d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:29 +0000

    again paper explain rare depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
