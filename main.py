92dfcceae4e7163f3238d6e7dd9bf083e0dbba18
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:13 +0000

    break cry style tear grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
