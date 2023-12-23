a6f2b312d689ffff59a9751994dcd03730a5b5a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:37 +0000

    wheel maximum bring hurry otherwise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
