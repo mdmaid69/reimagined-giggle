0307d0ef0d050a95fc4da6312e9ee19edfcab002
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:40 +0000

    nose regard education accident wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
