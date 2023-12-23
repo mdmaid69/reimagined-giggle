ade6e5b14c26537c06db3c224d3cd6acfef0086b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:42:10 +0000

    stand trade lookback(on) land count

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
