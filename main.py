3018a8608dbfa743417e96dd30e6b6e82f9ece08
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:33:56 +0000

    club suit entry pride shop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
