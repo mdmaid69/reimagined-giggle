a8e33217c0484b9054e1a50c73c0fe25ed5c728b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:27:01 +0000

    congratulate improve left mainly entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
