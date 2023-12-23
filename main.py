0f43051a21c0b10be0f2fe4a90772a0eaca03656
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:18 +0000

    keepaway female division out prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
