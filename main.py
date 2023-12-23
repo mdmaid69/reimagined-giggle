4a075cf48197d8b1fdcc116d87ea18c2085419ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:20:22 +0000

    fix getup tour typical estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_binary(n):
-        return bin(n)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
