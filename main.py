866873fb824c37ffa91daa39182ba8b890782589
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:36 +0000

    bringup pipe report ear future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
