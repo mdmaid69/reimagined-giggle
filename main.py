972937abe914ab6732bfe979fd225f13d7b2b444
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:17 +0000

    search press quarter study design

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
