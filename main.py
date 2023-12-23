f085ab4c42e57e42cd728f2267d32c9e42c2ff98
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:43 +0000

    last shake workout bringdown regard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
