b8137a8951fd0b3823826b613911a0962314e808
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:32 +0000

    independent discussion field bad fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_typecode(array):
+        return array.typecode
