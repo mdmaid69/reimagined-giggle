98cc63be5760f96b74abd7698f331e8fa44620d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:17 +0000

    speak comeup cry doctor form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
