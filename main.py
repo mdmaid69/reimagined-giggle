4bc700f0cfb22729826a14f17e84f6ba7765e5b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:35 +0000

    capital period heat performance programme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_work(force, distance):
+        return force * distance
