217e1336bfa598a96704b201309673a550aca03a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:49:26 +0000

    whatever mess squash holiday be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def cube_number(x):
+        return x**3
