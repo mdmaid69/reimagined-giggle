2bbe4609e4cba8c7f7aed3c393acfb9391799b62
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:21:21 +0000

    across border clever performance back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
