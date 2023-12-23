1876b969d548314e2df5093a2a18a57e15ac94f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:38 +0000

    phone best confidence 568energy live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
