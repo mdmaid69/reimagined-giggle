ce235efa2fda518254584c134ac9424f3dd28cae
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:51 +0000

    the laugh fine wall politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
