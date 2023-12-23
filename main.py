30c81854f454f1b9bb9e05747c94b2f0d0c0ecf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:28 +0000

    well design operate cross agreement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(5):
+        print(i)
+def is_odd(n):
+        return n % 2 != 0
