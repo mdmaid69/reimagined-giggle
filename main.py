1c507faff4b0dce8e5ef58cc2cf67bd21e2627a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:47:51 +0000

    invitation any ourselves may better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
