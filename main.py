f4808d3cfbbd1615695ecb24e40af7f591f53598
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:53 +0000

    dear appearance official favour needle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def divide_numbers(x, y):
+        return x / y
+def find_unique_words(sentence):
+        return set(sentence.split())
