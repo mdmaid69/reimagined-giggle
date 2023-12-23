7430a9369b58f2835cbafda20362a7de7b48c91f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:06 +0000

    male hospital tooth black gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
