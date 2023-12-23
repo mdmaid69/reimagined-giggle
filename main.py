48508d48d1371f9150b656918bf5341bc589cb57
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:43 +0000

    takeover wrong relation score turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
