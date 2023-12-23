78a384555f8b100a37aed62509f796096127d9cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:55 +0000

    separate separate cross regularly frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
