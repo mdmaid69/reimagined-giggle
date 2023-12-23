9a25c854c72d9de775a369343b7c2e99b4dd3c86
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:20 +0000

    calloff runout stamp complaint silent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
