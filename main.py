0d39b086ce9e75d95f1bbc33e0f22b8ff8da690d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:36:53 +0000

    maximum place belong fresh word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
