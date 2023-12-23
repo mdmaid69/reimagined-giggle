89920a9c56aada6723ffe7666ad81a9a06714443
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:08 +0000

    awake city whisper share transport

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
