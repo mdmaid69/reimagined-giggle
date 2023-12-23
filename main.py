91965528e568bb5c7feb6ce03c366865a4106d1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:29:11 +0000

    suppose complicated convenient still famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
