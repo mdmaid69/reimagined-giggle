8398123ac3a1c69961399572e7a477391645c5db
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:57:51 +0000

    plant form before bored official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
