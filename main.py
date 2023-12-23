be6c101e308bb8864b125becde0446164c156cf2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:18:39 +0000

    tear himself responsibility spot wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
