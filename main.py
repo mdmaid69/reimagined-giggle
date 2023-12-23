bca73e6c16ecef52d862434ed5a1e8ce6304e3e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:00 +0000

    available explanation female estimate permission

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
