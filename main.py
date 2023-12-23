4a66380d0260bd57a575bf4454b0c105872e2bfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:30:40 +0000

    used rather move attitude true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
