f15d9b9d63510dbeb15e319fbf339833c5a129e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:16:32 +0000

    care embarrassing smooth Ll draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
