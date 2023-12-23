8adf149fa71e5397c6e8babe7c8b0808898fa94b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:30:19 +0000

    slip number public Nn have

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
