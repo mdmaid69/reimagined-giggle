9537f818ea7919bfc2557d90551b211cc9b1c5d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:43:08 +0000

    part house frighten interruption lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
