c10d869793daab128c122cbd0732c43da59b4b5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:36:30 +0000

    worth everything towards have stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
