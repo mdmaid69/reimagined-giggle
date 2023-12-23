cfc0e110accc60c357036534384b26f8d80ebed7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:12:57 +0000

    short few without knowledge slight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def sort_list(lst):
+        return sorted(lst)
