e5578a3729b46fced98cc9d46e3d78d05c3c6616
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:23 +0000

    hold win contents accident date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
