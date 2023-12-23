7a736f5d8cb1290b39c38423c0ca4dccffefe0ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:45 +0000

    strike onto rid compete trade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
