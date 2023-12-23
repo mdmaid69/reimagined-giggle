8c7075ea4690560f368b6f11640e8941ae7dbf8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:58:58 +0000

    whisper make miss entrance set

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
