da26be96a8d06372ea09ea6cd17161e8114fd998
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:09 +0000

    allow hide great hurry fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
