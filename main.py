5494689931b23388bbc01280b1f5bbb4d1e037ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:58 +0000

    bank put woman chin pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
