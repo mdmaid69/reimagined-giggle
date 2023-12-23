ee455bcfc58b77f70c5b43393a95d059a6311f2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:56 +0000

    contrast jump last near shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_union(list1, list2):
+        return set(list1) | set(list2)
