a9ba81a1e2155997bdbfba3433fca190a6b622b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:52:10 +0000

    fire block shout activity tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
