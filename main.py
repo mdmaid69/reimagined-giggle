c34db78f4dfcdd703925b4ba460ac12149c21cf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:49:43 +0000

    system speak error high trouble

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
