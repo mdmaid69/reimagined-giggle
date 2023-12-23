cea3c47e76612638f9b1ff18e6a7e0eccd417633
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:59:32 +0000

    can divide range favourite ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
