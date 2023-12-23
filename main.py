5422ff099f01cccdfb76a844e1339be9a59991c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:16 +0000

    church often lost thick cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
+def find_max(lst):
+        return max(lst)
