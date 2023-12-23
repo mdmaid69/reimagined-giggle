9a9fcd15cf14f952f1ecb9ff02d9cd529c6b08ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:56 +0000

    ours road noise pay ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import math
 def calculate_euclidean_distance(p, q):
         return math.dist(p, q)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
