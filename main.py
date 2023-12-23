391f77d53ea0a97af5f0f31f2f1878b7828cb05b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:36 +0000

    match winter take giveback comment

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
