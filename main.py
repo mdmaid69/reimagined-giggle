a6556e4fe641153a0dcb2cc7a75c5a52431e181c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:27 +0000

    cry nurse shake good now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
