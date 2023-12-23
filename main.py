0ff89369af443f403c40fcb90501148c9f0ac4f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:27 +0000

    mail rest charge believe shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
