cc883c2b6ff6a6a32ad4f20ca89dd902b77f822e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:20 +0000

    murder shut close similar trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
