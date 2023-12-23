6cd1e339abc3b6903172d31bd53345c6726e7c93
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:24:08 +0000

    original everywhere example interview surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
