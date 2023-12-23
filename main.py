56998fd2aff0968ab5b0ae28806bc5b278e7ca1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:03 +0000

    bar wrong high equal order

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def find_min(lst):
+        return min(lst)
