1dd56287e34964a5dab2b1d55993e6d5b2a5eb01
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:42 +0000

    thing solve accidental left godown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
