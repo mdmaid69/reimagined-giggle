9957120f082b652899aa33236c0db886328b156d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:47 +0000

    thirsty drink last control react

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+print(os.getcwd())
