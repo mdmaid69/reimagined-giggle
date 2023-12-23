e8f1df298f1e83fb42782b62073c12e1411d28db
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:53 +0000

    keep kick freeze office rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
