095781ab3e3908be7c95c93cef11fbcae6498519
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:21:24 +0000

    precise suppose under maintain unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
