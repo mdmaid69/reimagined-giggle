e7e7f3885cbb08dd3f0d52531da6a712be3d16b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:08 +0000

    safe rub knock stamp illness

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
