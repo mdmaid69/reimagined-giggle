818906f58891deea67c551dcba1aac5c2b415473
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:56:13 +0000

    kind stamp bright load operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import getpass
+def get_username():
+        return getpass.getuser()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
