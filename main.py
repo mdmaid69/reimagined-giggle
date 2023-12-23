616d936ca7d647df096bf986f9fa7ff516305c44
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:41 +0000

    direction thank direct perhaps knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
