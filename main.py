e791edfa0f412c09d3f1450b3583a382ac835f48
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:46:05 +0000

    cry independent chat soon getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
