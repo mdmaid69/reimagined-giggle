eb1d6a096d5d8fc45e71ebaf099363f1d5d3fdc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:54 +0000

    favour ask however join provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
