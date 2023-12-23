89e33dbd4136ef0537f7832d57977073afb2bcca
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:00:41 +0000

    lean flow hers black free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
