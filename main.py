60cea5379609b90b67aac7304d5b5a041bc2db28
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:06 +0000

    calmly cover positive interested break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def count_elements(lst):
+        return len(lst)
