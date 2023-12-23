da823afc2105865ac6943ce9bd21fd0fad915e8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:28 +0000

    top on limit public key

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
