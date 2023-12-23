cb4509293a484a0af867568a2f274d321b8e575c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:52:59 +0000

    finger person roll apologizeapologise front

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
