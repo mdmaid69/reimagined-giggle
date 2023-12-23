834f3f1f90247d200413f059c700dc0bbaac8533
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:10:59 +0000

    radio length emphasis decide list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
