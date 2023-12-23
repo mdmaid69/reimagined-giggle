880f50bd003eb465245d45becb4c28385a1eddad
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:00:48 +0000

    back fall your holdup tongue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
