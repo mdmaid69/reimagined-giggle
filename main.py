5cce0221af3953f6544f3d1d5a89abced606006a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:38:01 +0000

    hollow search freeze load collection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def reverse_list(lst):
+        return lst[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
