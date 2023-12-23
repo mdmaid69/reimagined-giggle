2213099f25707074fd896911d37a0c111375011e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:40 +0000

    angry nose female check hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
