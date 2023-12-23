77d6faf8096dfe8d545f6b3146446a6ad3491758
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:43:36 +0000

    anywayanyhow ring sweet pick coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
