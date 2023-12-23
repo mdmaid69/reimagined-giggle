9001521f20f8a4421105fa4ab6ca298ad611ef21
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:33 +0000

    remember count powerful perfect accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
