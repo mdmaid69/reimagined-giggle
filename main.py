b5e61bf33799070903e339c1121b6573cb405447
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:45 +0000

    born order before promise flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
