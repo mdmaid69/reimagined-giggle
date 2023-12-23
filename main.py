1c418686011f7477306ad2f8997ddf8471e7e8ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:10 +0000

    573entertainment hurt unless 568energy bunch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
