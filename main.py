755943490187ec9b472c8e54e587e78188e15486
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:16:58 +0000

    instrument nothing less damp hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
