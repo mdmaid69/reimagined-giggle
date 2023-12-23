1f4e7279dafdd2b66c92f735bf2e0a44074767f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:56 +0000

    tongue gooff lose build failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
