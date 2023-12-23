34705a9a5328e6e15cf2d65e3ab633e680a04b54
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:18 +0000

    clean spoil purpose promise better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
