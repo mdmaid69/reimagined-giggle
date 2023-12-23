ac441e95e509e2b3a59b9c456f597190b7a6f630
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:38:04 +0000

    person medium cut cost public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
