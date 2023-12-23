210428ed04d8956da96b7d7071f33230a22184d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:37 +0000

    honest tragedy pocket mind guess

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
