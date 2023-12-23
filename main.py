e73e48b27e4757ec799a677ecb11f7dffeb98d40
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:29 +0000

    bar habit school act fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def cube_number(x):
-        return x**3
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
