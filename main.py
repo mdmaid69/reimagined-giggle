9ac1fe1778fd45a061e09f3209258ba4b3930996
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:59 +0000

    equal advanced body nerve smile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
