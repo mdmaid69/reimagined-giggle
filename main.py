3986aac7c6ad92b0d8b814fd35e101dc7409457a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:26:18 +0000

    hole recently street actual exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
