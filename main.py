94aafea76ab202f16d50a7598602f7a9095898b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:08:26 +0000

    priority crowd to down but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
