57bb691856463c0081f08a9fc501d00ee76b972f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:11 +0000

    him concerned interrupt in hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_base_name(path):
+        return os.path.basename(path)
