427dcf8f39a05cebd72e2ce7503b9ae0f72200f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:40:12 +0000

    voice around minority little character

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-print(sys.version)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
