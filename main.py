9aff396bd7714a804e41507a531c189d722c6fc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:20:05 +0000

    refuse thick leave later rest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
