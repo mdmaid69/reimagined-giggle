254e30b3aa230fd9cbfb422eb9378a81da766a4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:29 +0000

    catch frightened escape level represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
