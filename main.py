6fba22142d82a31a875670509b6604bf8c60dabe
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:09 +0000

    comfort shout ignore weekend public

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
