7ef62ae2a7f8bd31bde3846e92d2cb0d9f7938e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:25 +0000

    substance create train collection scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
