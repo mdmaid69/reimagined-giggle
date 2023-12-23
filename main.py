b05dfb6c396ad7cc39e6742f8ebabc802127f898
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:27:34 +0000

    remain manner most say foot

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def clear_array(array):
+        array *= 0
