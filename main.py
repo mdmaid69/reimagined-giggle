3d21c436e66163024e0851ae0471a3ed72c789fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:36 +0000

    save into callup sign piece

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import tensorflow as tf
-print(tf.__version__)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
