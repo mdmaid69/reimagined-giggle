86f5c15508fec3fd4f2e5e102a0f395dd4531e36
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:56 +0000

    clever rely section comment surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
