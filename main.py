b5639a0f2598d0481c104cce337701762bb74eb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:36:38 +0000

    make keepup deliberately regularly comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
