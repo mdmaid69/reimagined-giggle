62324033f8651b85c64056b8536947a74bc79a1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:04 +0000

    undo land but organize challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
