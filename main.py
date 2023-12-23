db44aa4a4740f0f04553ef5237b7e957f301c2b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:49 +0000

    home plant another far bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def greet(name):
+        print(f"Hello, {name}!")
