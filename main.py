2c9ee4dc0df0b6b2ebe99ad6d507d6298240fd91
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:13 +0000

    tired quantity grateful shadow common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
