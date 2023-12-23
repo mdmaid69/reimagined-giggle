74946d386427fd1d5a53ecc10a6658b9bbd0b441
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:56 +0000

    office straight goback(to) discovery live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def multiply_numbers(x, y):
+        return x * y
