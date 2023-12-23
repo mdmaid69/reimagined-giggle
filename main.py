7affdb9128e5fc32e0960353efdae9fce6b9937b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:25:37 +0000

    trap cross deep service term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
