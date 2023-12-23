fb0c7b40eba90a837f0df4700fd806f503d6125c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:29 +0000

    prize point past chance warning

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
