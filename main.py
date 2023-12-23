6ef7429df217534ecd02d2fc4e4b25379b50cd87
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:18:30 +0000

    pronunciation skirt acceptable safety finally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
