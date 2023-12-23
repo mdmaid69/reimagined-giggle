3b7bd2be933ddc97e50d4e6c759c02ce29cc4a73
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:22:40 +0000

    later warm section base sky

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
