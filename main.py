555aacdd3dc410cfc9d1b76becc10dcede5decd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:08 +0000

    may cry hurt boat gap

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import sys
-print(sys.version)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_min(lst):
+        return min(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
