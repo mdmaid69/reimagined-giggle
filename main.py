f2733dd7c69788451689450b1cdbedcd32bebaaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:41:05 +0000

    please turn evening set effort

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
