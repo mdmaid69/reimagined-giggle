55fa95788dfe05e88f167946b14b8c941d30ee7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:49:47 +0000

    star brother judge bringback everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_bytes_from_array(array):
+        return array.tobytes()
