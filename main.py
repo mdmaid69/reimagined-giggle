f9c7f70f7bb7dc33831027e4e7f1947ea70aa72b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:25 +0000

    door emphasis hurry shade start

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_array_typecode(array):
+        return array.typecode
