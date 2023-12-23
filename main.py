2efcc4c3548710c806f479f9839259259a47cfc7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:39 +0000

    wheel tradition tie improve complaint

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
