127820df37d60485dcb7f92823b8e033a627fac0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:06 +0000

    somewhere tough crowd declaration why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def subtract_numbers(x, y):
+        return x - y
