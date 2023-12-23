89fcd4cc0d4dfb08008e30b77d6cafb355fd6d59
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:49:03 +0000

    balance pot corner attempt giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
