f5bedf6e7c75b5bfb85db1110dbb5ac473e6f4f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:21 +0000

    Cc could never bringforward minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
