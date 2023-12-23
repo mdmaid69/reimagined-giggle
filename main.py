7658e19a519a097c30aa134312351829c3f3315d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:50 +0000

    bound force improvement Ll switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def create_user_dict():
+        return collections.UserDict()
