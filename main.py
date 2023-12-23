3ea70c8a19959f45fc788a9428ca8eb73d3a675e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:19 +0000

    comefrom list imagination friend luck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
