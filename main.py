866855ae255f7e219ea8b4c5dfb578844cd5031b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:01:29 +0000

    Qq mass side exaggerate minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_typecode(array):
+        return array.typecode
