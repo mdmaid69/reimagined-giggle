d6588138b965b51e8f6d295e57481e57deee1f75
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:10:50 +0000

    hang judge next brown generally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
