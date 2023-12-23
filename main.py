3c10ac5e0dabf47c434fa56cd826f866aecb28fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:56 +0000

    picture workupto profit production the

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_list(array):
+        return list(array)
