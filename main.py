67d8a636d92297572b64e6c85b697557b92fa611
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:53 +0000

    round progress over shut pure

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
