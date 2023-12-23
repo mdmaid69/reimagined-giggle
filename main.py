adfee31d3d2f7cbdc7519a0483df2b2f77babf9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:05 +0000

    fold key disagree reduction deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
