766f84859349fb87ee6a53eb918bbf9093bd9fc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:08 +0000

    congratulate pleased runover under else

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
