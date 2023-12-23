c046b1882968810bcb5caadb17dfb1b90b8d9b4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:23:41 +0000

    nigga change scene picture forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
