857c78c12640efeb7d22917b0ba98eabd2a8881b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:06 +0000

    total concentration important ready part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_float(array):
+        return float(array[0])
