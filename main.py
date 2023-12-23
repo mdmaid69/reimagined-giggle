cddfd04d4ba12a8d13a3f55337079036fbaa667f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:20 +0000

    breakfast comeround table have tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import getpass
+def get_username():
+        return getpass.getuser()
