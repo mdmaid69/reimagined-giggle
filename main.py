a8b62fee11835b347f3a59c588c1da283048646e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:17 +0000

    direct time block importance somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
