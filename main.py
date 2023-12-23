87a7dcb30e99efe765a1b6aeed83f7146e59bc1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:41:25 +0000

    put telephone keepoff comfortable rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def convert_to_binary(n):
+        return bin(n)
