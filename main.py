a12b961eceaee5fc37eae6553488222c712837e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:13 +0000

    contrast method stiff us shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
