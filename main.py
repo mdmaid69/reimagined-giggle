0c9f0ab44eab73984cb85144c23ee08237f6d42f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:59 +0000

    release pronounce excited risk join

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
