20c16fd11421ae0f01b0e514bf6cc4f6e71259be
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:44:08 +0000

    pile usual backwards breakup stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
