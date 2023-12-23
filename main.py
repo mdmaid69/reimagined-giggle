d51151d1356d07e44aea67fa9321bd0945fb4bf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:30 +0000

    healthy game off black whom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
-print(os.getcwd())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
