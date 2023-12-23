a9a23636718d0d3ea6b2dbff93641e1b25394f5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:56:03 +0000

    worth 566end zero hollow treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sys
-print(sys.version)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
