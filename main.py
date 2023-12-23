f902c27c3ded98b5a10a911b4dbab1fbd4730f61
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:23 +0000

    radio second past cause pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
