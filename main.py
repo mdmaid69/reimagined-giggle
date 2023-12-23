30f795cf8a14c7746478c5f6f8e8670887dfa55b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:43:20 +0000

    room build complete transparent practise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-for i in range(5):
-        print(i)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
