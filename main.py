f0e42f7bea2addd5a3fe4c2c1f847bdb9d8c3f9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:47 +0000

    567enemy space delicate side solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
