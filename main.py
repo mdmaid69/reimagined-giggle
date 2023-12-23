03cfbe51c0859686ca17593a9ffa7a497b602111
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:37 +0000

    goout promise we major enthusiastic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-print("Hello, world!")
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
