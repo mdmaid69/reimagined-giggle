546d27bf5fe376995f2065aa42add8e656ebdfbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:11 +0000

    miss very force lock future

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
