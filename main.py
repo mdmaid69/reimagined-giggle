051c9358bd1cf095ecc4b00f6475db19a48c0c8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:10:46 +0000

    immediate undo blue whatever scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
