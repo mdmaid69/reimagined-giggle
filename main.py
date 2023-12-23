5a9c96bee21cd00a12ac4878e8355e31999b118e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:14 +0000

    comefrom temperature ordinary bag supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
