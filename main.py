f459b907159d70795c8194c2cdb51123370d0825
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:45:45 +0000

    say loose manage official about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
