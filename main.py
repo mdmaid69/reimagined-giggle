b979413a2a1b8969c68875d5bb9f2e79f59ae658
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:19 +0000

    her thing reason task relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
