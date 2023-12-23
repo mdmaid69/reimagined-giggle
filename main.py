fc8b839d14edecc633ad18dddcf65e4f1bcaa9c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:37 +0000

    carry nothing defend mix suckme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def reverse_list(lst):
+        return lst[::-1]
+def count_characters(sentence):
+        return len(sentence)
