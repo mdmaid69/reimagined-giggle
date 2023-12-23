c93d6e753b9d8b690e70a757bd9922309cbabff5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:29 +0000

    attitude against imagination entrance or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
