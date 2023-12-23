f6b92307ddb4634d4cdaee1c0a6f1fc7f4d6c6c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:58:07 +0000

    talkinto bag about mess basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
