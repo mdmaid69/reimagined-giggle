cbb8a641511792f78d3880d8671ba0610d38cde3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:07 +0000

    accident choice dream move teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+print(time.time())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
