cdb0f21a1d18a11d40eab8b03e3d0cbae02eed7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:21 +0000

    past win makeup tool dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
