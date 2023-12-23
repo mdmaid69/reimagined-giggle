9bfb274e3b3e76eb271fc8b395a08d4535f39cc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:13 +0000

    radio worried shut pump mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
