6c94ca578321c2362578d03e39e264913a300d7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:43:20 +0000

    flood straight black pen show

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
