fedc393d3de7b7b2336392990b72bb6eedb32a7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:06:34 +0000

    please treatment except makeup tell

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
