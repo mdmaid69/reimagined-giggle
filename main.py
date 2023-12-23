9195b79f62b4f6688dbe273a52d15cf2dc79ce6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:58 +0000

    not slightly that last gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
