f1d925ece1290345e61fbe4e1b134f1ee50159b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:20:37 +0000

    hate swim beneath intelligent none

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
