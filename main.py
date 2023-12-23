ca00fbec68a0fc89547dc828604757e66dbfe017
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:37:54 +0000

    risk runout cough nerve guard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
