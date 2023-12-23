16ea4c1e66e23d60e48675a07ba404e03a1e9041
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:47 +0000

    decision juice section involve organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
