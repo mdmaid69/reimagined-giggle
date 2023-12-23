3c5792c3af8f351d3bcb5a7504ce52e1a183d61d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:08:21 +0000

    middle handle apparently later load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
