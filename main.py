8762cf055dc1b08cd029a9a862bba1f4ad4bf7b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:04:25 +0000

    only little cheek to put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
