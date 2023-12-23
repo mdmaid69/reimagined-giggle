bcdce3bf9cc49386709005043d979916473c2fca
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:09 +0000

    mine succeed their bank bread

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
