0050ab73b2d66ed8c9f9f5a8bf4bb7e5ef512f0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:22 +0000

    foreign married his grand flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
