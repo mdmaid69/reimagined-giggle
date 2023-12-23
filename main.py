73d50dfef0151eb035577f6e5d673d3d2839061b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:48 +0000

    upstairs difficulty lost him scene

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
