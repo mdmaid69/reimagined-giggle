5ff4c7fe54f74c45b4180074c77cf7533a4e1cf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:11 +0000

    flight awake too speed wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_set(array):
-        return set(array)
+def count_words(sentence):
+        return len(sentence.split())
+def reverse_string(s):
+        return s[::-1]
