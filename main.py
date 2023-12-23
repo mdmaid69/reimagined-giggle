163ed37ced3f37d9496862d3a7682b0780c02b23
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:24:23 +0000

    while clean closed every wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
