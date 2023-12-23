bdcad6f94f5c6a95360f559ca40d52609a45f83d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:51:28 +0000

    emotion order regularly memory one

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
