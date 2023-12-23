2a3d42e50a01dc7cd339c77f479c8524ea4f638c
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:48:34 +0000

    school separate ours open pot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
