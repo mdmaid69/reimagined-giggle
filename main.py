0b38b8de90ebeaf48a3edfcdc97aa3647b972f33
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:23:06 +0000

    off unable service translate comeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
