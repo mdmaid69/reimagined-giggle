6147dc32fdff78836be7af21666bfd0dcda4bc4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:13:12 +0000

    this slice file reserve exchange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
