623ad9ed10b273f1bc309be3ccc8cdcb2062ba5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:18 +0000

    mother edge excited breakfast breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
