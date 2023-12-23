73a587da42a656040ed0f6b08e74f65bc2bda09c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:43 +0000

    thorough event centimetre and except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-print("Hello, world!")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
