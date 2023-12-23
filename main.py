2b404c7e26f0eab93c4e4f5f0727fbf94adfbefb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:39 +0000

    with I squeeze worried congratulations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
