927cbfc8c28a08d04b18606907670591c2e09484
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:16 +0000

    workupto broken always degree release

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
