8cb879f04fca48cf72587a9be5d0ad3fe018c4e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:31 +0000

    succeed recommend settle disappear vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
 import array
 def remove_from_array(array, item):
         array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
