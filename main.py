1becfb489ed10b5f67fbe99254b72c6a7233ac09
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:06:55 +0000

    contrast 179bite mood year false

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+print(sys.version)
