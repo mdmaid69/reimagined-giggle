47ab59fea9114aeb02b7d8c49bf3e4fa508a35b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:38:28 +0000

    recover Zz concern regular replace

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
