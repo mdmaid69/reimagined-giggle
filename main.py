718b51b84c27b027b5b1cfffc0b6eab60c938fb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:22 +0000

    suppose distant there sense good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def multiply_numbers(x, y):
-        return x * y
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
