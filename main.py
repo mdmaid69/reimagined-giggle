177cdd9d8ad149e33c561168932bda9d74ff0b8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:44 +0000

    somebody short suit replace shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
