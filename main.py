010b02d3e3cea1509c76a4f007a40bd9b9f7d976
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:41 +0000

    interruption side see classroom experiment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
