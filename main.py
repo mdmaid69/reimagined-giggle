cc5cb5ed1c2edd89fcb8e5017d4c93ee950293a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:13:54 +0000

    post variety sensible put so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
