66d93223aedd1c4c18e13a1cb8b0b573da05afe2
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:12 +0000

    desperate look total emotion impressive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
