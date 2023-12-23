cb4989faf9654155a23eefb30f5df7556250fadd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:41 +0000

    failure must spin cheque however

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import random
-print(random.randint(0, 100))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
