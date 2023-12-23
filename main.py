7ae8d7154b5b6173933e2101f524e1cb9919a061
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:30 +0000

    relation shall bringback spend thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
