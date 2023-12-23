285299da0586b63e7d6d9ba7b891d78b8d832d56
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:54 +0000

    within shelter fly hold acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
