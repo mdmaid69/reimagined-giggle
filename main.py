44e1fb60eeb5e60c46dc20a4dd60b83009c41bf2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:58:20 +0000

    convinced male forward ill city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
