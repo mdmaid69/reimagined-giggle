f1dbd1162d8a13ebdbbd32003355c36e1aa0caa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:41 +0000

    beyond off debt Yy nurse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
