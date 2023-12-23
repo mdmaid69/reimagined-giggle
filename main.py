022749301811a8eacf3d678221c81180492ef321
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:16 +0000

    matter lead cap breakup wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def create_priority_queue():
+        return collections.deque()
