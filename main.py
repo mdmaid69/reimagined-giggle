647e5f189454f4b9ae5609562f3689cc994d3a2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:53:25 +0000

    holdout cool share light weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
