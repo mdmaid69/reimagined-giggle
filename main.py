6961468479da05cc6545579d96d851f4eea48855
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:16:57 +0000

    through sleep moral centimetre winter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
