3c291669ce5c888722554ab2aceef6ca54584899
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:21 +0000

    pretend weekend geton health truth

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
