1e6821c5fda3315692ca943c225f70e6dedcde07
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:42:24 +0000

    strike sleep lay pinch form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def exit_program():
+        sys.exit()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
