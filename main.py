97e032beb17bbb197a57eedaeb470e4942799f0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:56 +0000

    base underneath dick port substance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
