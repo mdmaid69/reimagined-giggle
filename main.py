f76493be982dce71e84ce1c17062aa2622a7df78
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:45 +0000

    little wave during giveup busy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
