4130cb5ccc0a5d82865228c671277a453bed0ab3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:35:26 +0000

    die steam ilovedicks warning comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
