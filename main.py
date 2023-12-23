1a96059884a24b0ba29fdcc687525834fdeb7c0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:32 +0000

    between afraid promise children record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
