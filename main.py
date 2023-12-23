56e223205b9381028da3ef64a866de7278087928
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:44 +0000

    offer hope free object check

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def find_max(lst):
+        return max(lst)
