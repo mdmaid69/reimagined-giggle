45a1d6346ac647018dab144d9707bc4148e2e3b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:31 +0000

    check quality environment sensitive game

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
