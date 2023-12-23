43bdb11d3ebf9d9316b5cf79b6d0a31c5766ab4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:47 +0000

    Itself scrap contribute exact war

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
