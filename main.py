367cf5566c82249eff89fb163e43683ae4770399
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:11:38 +0000

    damage unfortunate angry exaggerate fit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
