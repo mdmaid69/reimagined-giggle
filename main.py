d1b5fc11110a9f39e3d167468f8178ecd6b41ec3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:28:10 +0000

    thin vote direct thinkof illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
