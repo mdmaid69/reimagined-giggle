bb8b2c59158a6a0f09fed8fc5d07a2ea1628f770
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:36:00 +0000

    complete disappear likely 569enjoy none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
 def set_array_slice(array, i, j, iterable):
         array[i:j] = iterable
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
