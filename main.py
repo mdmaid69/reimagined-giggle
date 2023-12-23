135d2e4916a4dd979fe24ff6a19e7feab574f9cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:31 +0000

    respect mark funny freeze want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
