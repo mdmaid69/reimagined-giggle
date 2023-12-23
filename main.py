4ad656a01247153fd505814ddedb48c37407546a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:04 +0000

    comefrom makeup tall centre indeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
