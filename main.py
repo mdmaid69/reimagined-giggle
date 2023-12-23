20f4dd6d6bf342f27229e8bb5a3964b191ec1470
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:01:46 +0000

    shallow people steer situation line

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
