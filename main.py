907d38eedaeb44502ff9bc79824d6e67eba83aec
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:49:36 +0000

    year listen order tight along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
