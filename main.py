0982f1c77608405261a2f4f0e5143026043262ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:50:25 +0000

    display ill song emotion pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def reverse_list(lst):
+        return lst[::-1]
