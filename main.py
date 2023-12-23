7ad906ff1da9fcdf39b9efb29394e3db7bdb347e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:31 +0000

    dull branch lookback(on) direct stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
