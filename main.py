5827e7069e19f7af03ec6b52fcf0b3dc5fd60bf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:49 +0000

    tear especially ought tight state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
