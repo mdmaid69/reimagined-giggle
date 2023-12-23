ed031f7e8ecc1dc5d256bea503749febcd5f79d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:33:07 +0000

    whistle famous no exercise practise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
