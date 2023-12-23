b966e995f95c9de50e52c89e63b241b55c668747
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:03 +0000

    along getaway load group argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
