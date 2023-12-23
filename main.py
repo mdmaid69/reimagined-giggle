ebf11e5a3ce114fcb7f7127ba331000949ecfd80
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:16 +0000

    back steal hide hope chief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
