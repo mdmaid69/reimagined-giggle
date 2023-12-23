a4ef9e43597a79247c5fa587694fe9e5315c4b4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:30:01 +0000

    gap sky fun connected admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
