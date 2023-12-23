5f47e89dc0dc87f84f902884e11db626789138ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:21 +0000

    blow goback(to) favour accidental vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_even(n):
+        return n % 2 == 0
