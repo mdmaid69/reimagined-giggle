0b66a5c5dae42780c4bf5092281768abb23dc13e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:55 +0000

    today he top piece present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
