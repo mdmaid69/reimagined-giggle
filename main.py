1ed9b4ee95a5ced4ac0876e7bf9b183b04a75e82
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:25 +0000

    discovery yourself floor own Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
