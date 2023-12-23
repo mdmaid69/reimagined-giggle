d503296bbcd4addf28dddf5de76b40a44ab159d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:28 +0000

    rob less interested wear injury

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def multiply_numbers(x, y):
-        return x * y
+def calculate_pressure(force, area):
+        return force / area
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
