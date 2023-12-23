662ba95509fd609deaad15ff3f1253a8fa4cfe32
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:12 +0000

    better food hang sleep meaning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
