fcb89298dd572b9a94df17b0a572cea59121514d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:28 +0000

    hole good turnout theirs cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import json
 print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
