739ce2fb25eaeab94fc14a66fac74702a7cbdb73
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:37 +0000

    stamp consider carry surface anywhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
