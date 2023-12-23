1684d8d0c9c4ca5cf04a3d811a7f487b1773fedd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:21 +0000

    Qq 569enjoy fast breakinto brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
