8fa873e08c927c20b6fbbe2da912e887c4c51e26
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:42:29 +0000

    design would error again full

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
