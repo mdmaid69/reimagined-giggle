3ab8944b6b8f42b5dd6385586ca350ea3e488259
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:44:55 +0000

    usually major abroad decision ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
