8e3b89ae176ba38c40ac55a984a2b1a08011d94e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:34:49 +0000

    necessary quickly result equipment whisper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
