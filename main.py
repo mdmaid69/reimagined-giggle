1856aa9a2cd7de0b5f27a42382c0bd8bb17e5cce
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:27 +0000

    bad the space woman risk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
