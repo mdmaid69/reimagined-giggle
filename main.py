7275c76a64d877a67b98653b97d8388277e92114
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:55:19 +0000

    challenge cool tour up warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
