f6948eb443fd0dbbc94af35a82e3b925ba4a8ad3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:43 +0000

    equipment worth before operation concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
