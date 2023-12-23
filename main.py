ef391a84d32abde5b94389502ac8ed6dfdc185e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:53 +0000

    truth across stage Ee stranger

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
