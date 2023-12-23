53bc5013627f2cf2159487d9c1a9f87c4c0ad679
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:09 +0000

    turnup lookup contribute contribute unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
