8751521be5e821209ac5c7524b9904756cc97e13
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:53 +0000

    takeback work your music wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
