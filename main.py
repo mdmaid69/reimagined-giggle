9cecc76d3e87ce1a966d8800c53344f81557e23c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:36 +0000

    door increase fierce mistake century

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
