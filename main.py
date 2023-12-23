1ce9e278f684be647fe6c6a7a94d7b0308b5a077
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:33 +0000

    rare interrupt sell sweep error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import time
+print(time.time())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
