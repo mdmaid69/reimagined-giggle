c4dc0614b6106f04feeb7eea8e88ca6287b0718f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:26:32 +0000

    principle whistle experience each concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
