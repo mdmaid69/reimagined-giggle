7ef2a9756a7dd40a111227880267c95828844a35
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:47:24 +0000

    dare turndown formal 572enter run

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
