064a6ac6db93e9bc4fa297e699eee23679fe4780
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:47:56 +0000

    comment disturb thinkthrough comfortable throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import time
+print(time.time())
