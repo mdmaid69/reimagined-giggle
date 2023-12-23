70a48fb846c246de7fe2c06aafabc11218d264c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:34 +0000

    aim exist punish will see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
