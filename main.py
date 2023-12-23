e212392c6bd45dcc53b109ec9b0f659df292827a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:10 +0000

    truth snow pretend admit comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_even(n):
+        return n % 2 == 0
