4e952a1a9756118486b1656ccfdfa46875f21781
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:40 +0000

    would poor straight attempt foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
