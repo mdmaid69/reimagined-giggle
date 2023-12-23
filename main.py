5bde5a68b52ea2f00f61aaf8e00abf104ca8868d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:03:24 +0000

    year direct lay same night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
