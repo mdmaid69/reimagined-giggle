d53069d72fa97ec457a04316671b6953c3fe5c40
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:48 +0000

    wire press cheap keepoff prevent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
