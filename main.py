0b68d112f9c8a74be0c6a20e9f01e885d83b964a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:25:05 +0000

    fact introduce try hold sweet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
+def count_words(sentence):
+        return len(sentence.split())
