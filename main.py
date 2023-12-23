1a18928c7c0d65aa0574f7a7594013dea2a3cc1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:05 +0000

    warn payment bad test pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
