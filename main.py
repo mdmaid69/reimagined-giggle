feb5765efc740eccea77232b3ed90e4c7bdc891f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:17 +0000

    fall 570enjoyable disturbing walk whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_unique_words(sentence):
+        return set(sentence.split())
