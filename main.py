230efe5ea608af19036621ac4ad24ca6742f4e87
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:39:02 +0000

    score target bound wander branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
