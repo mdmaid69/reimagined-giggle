c0fd401ff9c353391a2f3a3bf4f3982c6aa4f11e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:22 +0000

    hang supply machine feeling alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_float(array):
+        return float(array[0])
