9457844d73f753670798185a199682c7e2b7901f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:14 +0000

    legal sit aim child recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_current_directory():
+        return os.getcwd()
