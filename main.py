7396b963f457621800a3b5e8ad64e44de9a5a62e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:18:05 +0000

    normal soft present dark remain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_power(work, time):
-        return work / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
