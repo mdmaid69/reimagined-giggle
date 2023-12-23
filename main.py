747f8cb78756d42005aedb985ddced8ffba70359
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:57 +0000

    scene freedom fortunately leg exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_python_version():
+        return platform.python_version()
