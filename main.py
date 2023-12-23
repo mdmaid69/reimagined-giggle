585f03110868d54556c3503b39b9fad8fd054c1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:23 +0000

    mark use far soak consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import platform
-def get_os_info():
-        return platform.uname()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
