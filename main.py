4d35aaf2b6234f3cdefe8beee9d633cf25a995eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:40:01 +0000

    true evening land run stiff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
