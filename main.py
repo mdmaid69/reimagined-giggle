79238f3b083d757a4ba35aa73b06d7b2d347e868
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:05 +0000

    pull successful main shall crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
