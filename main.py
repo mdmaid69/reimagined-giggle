404600d69537eb3586549aac84b871885b7c6f64
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:36 +0000

    steer temper contents project regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
