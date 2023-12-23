d2d61f655cf3e0003224ed14dc846ba4373133e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:13 +0000

    geton miss limit up payment

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
