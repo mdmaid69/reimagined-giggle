2d248c9598b71df5ef4da86642e187a7cecc15d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:14:02 +0000

    not bit mention Qq female

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def create_counter():
-        return collections.Counter()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
