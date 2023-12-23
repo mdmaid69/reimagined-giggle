9ea90b93c721a07c6b6d94b0df03425451220d70
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:51:57 +0000

    spare record exploration convincing alive

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-print(sum(range(10)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
