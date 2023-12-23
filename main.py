2fe1cac6aaa86a5051c6c5eec05041868ecbd4b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:03:08 +0000

    really most stiff pipe copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
