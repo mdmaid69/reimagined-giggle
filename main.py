9dd18ce6551f942d1bbb7549978a542a0189710e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:47:11 +0000

    his position trousers colour whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
