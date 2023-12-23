899e710efe2280de191f326eabe5b7d1de91eae1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:24 +0000

    cool behind national hide clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
