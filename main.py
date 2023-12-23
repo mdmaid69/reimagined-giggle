d1028addfc9c2ea2ec6cfa21b5652152d5687a79
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:52 +0000

    bottom separate tread pity possibility

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
