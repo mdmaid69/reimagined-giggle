56e4f4c4ebd270777bcb45f60ae56df30b0be702
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:41 +0000

    smell fierce sign separate combination

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
