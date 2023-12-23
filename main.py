1093624fb8eb7334a4f39c47e54f08fb64ce9f59
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:54 +0000

    crazy and period unpleasant protect

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
