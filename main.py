c3708a528a592ed48b6f8e654b3a8b074dd29459
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:02 +0000

    tap gap much attach stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def cube_number(x):
-        return x**3
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
