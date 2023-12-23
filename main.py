3acb836c7c729ba6353598cd465dfd337fb0ad21
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:43:20 +0000

    high influence sense pass depend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
