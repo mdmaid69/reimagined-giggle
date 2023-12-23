ab4eeba849f481a63bb29c91b6aa81a3d5ac2a44
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:02 +0000

    permission crash standard form funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
