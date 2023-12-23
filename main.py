f35d49cca1b5567ea4c3141171abd89ecd7390d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:54 +0000

    fill tone Oo up rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
