f388874060913afb4508a11a9c0b7d88cb16a95c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:02 +0000

    complicated hard official score treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
