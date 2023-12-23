e026be298df111ea18461363360092e6d94371a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:05:27 +0000

    getoff bath dark find appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_item(array, i):
+        return array[i]
