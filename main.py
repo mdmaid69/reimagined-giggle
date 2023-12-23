6b0e344543d80fc0743f2fa6f770df6ba268f48d
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:27 +0000

    responsible round person dance tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
