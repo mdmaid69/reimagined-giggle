1ebd231eb1001f95d6420a04f41c9e82f0f82a00
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:16 +0000

    bridge deliberate gentle study kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_list(array):
+        return list(array)
