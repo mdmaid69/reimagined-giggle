f918270a4ac6d1ddd47439ae1015da5d7456c453
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:18 +0000

    comedown for healthy bring street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
