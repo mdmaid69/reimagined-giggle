6ac66e3cf6d53ab3afb359937de86feacd64fd5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:53 +0000

    send nothing operate pretty schedule

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
