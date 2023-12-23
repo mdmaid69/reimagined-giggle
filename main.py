da05a6e5102a51e47fd4ac6ee3f4323242c36722
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:06:21 +0000

    pile means excellent crazy failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
