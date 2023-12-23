5c3674c8c3880a4320491ed329288bf5e7966411
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:13 +0000

    university division prepare want Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
