41ca15d1fac998487356626457d133d5d0d9d6f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:50 +0000

    near bored innocent congratulate uponon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
