6490df56f5919f1a47f5a2420cefc428b2d40a47
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:31 +0000

    her leave breakin congratulate later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def is_odd(n):
-        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
