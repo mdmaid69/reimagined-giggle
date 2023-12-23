3223e917e2d831181d20b328c6f00bdd57b1a050
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:42 +0000

    minimum on design wild minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
