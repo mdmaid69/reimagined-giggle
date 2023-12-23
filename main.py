16f6ac23a20de0f187cbd68ed86ac6146b9e1bdd
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:36 +0000

    dish factory behind issue office

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
