4c9069b9ce532f0bdd5842e7938fe434b1740e58
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:46 +0000

    bother attention circumstance chase promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
