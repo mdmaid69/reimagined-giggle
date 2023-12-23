c996c038187ab2334474599e17f8836a1a430f18
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:57 +0000

    Ff involve off smell cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
