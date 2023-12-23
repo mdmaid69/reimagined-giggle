a32fc999b8447d4d83a2d649e55417d4c6bab1a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:42 +0000

    relief famous profit exit gift

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_rectangle(l, w):
+        return l * w
