0512c84231cab39ac9b38cfd01b7542f6859c8bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:54 +0000

    murder correct rough development thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
