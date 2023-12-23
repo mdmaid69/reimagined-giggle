6de8d4bd1f6bf172a7b2048de2a59d389afae3f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:05 +0000

    after back government must show

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
