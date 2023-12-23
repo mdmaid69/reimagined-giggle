d1b08aa278c73883dc89b5fa2c73f246bd42863b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:35 +0000

    constantly method information foot tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-print(os.getcwd())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
