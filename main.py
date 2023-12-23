8dcaf48afa9fb8632c488bdcce79b4e6fbd11c5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:00 +0000

    discuss mainly store birth complaint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_list(array):
+        return list(array)
