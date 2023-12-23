68aab5808993c05cb190820d18025f194d1862bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:11 +0000

    praise press warning dangerous forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
