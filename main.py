a66ec1459e7c82fa4231ed4c5093aa048725e867
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:59 +0000

    careless hand my row absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def sort_list(lst):
-        return sorted(lst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
