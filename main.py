2c9979e4dcb652f16e177c426a4adce97d7d9eaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:26 +0000

    let achievement admit become often

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
