4ca46562881970b42eb8aa6f9d92164c54c47604
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:26 +0000

    trap national light shut except

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-print(math.pi)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
