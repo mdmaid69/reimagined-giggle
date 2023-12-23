1276a0ccd02aeef5b8c30dcf3dbb20f2f4b2ac6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:56 +0000

    bet greeting fair obtain answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
