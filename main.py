18e5de9d0adb1ad30290c721b4c0b3b820bbcac6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:46 +0000

    waste fairly choice square relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
