62bcd567141314e274c73fd6f6c3f0ceb753a919
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:38 +0000

    book complaint then bunch health

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
