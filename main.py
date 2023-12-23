8c48259089eb74b79afa0de7e9977297efc8f277
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:44 +0000

    disappointed period oil tough uniform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
