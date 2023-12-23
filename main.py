c86483d2935b3c9ea26c0c1a32e1bdea0f288d70
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:23 +0000

    sure foot exist operate wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def set_array_item(array, i, item):
+        array[i] = item
