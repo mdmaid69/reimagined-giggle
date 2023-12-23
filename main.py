d79ab28ac8a26990f5ec3c2bf8b18d5ecc0d3820
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:20 +0000

    rise regard Gg light quarter

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
