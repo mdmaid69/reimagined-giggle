03fe3b77e1013f5f12f3b1fcdd0d281b84d9cd2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:23 +0000

    market description count body print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
