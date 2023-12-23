96b8141cf048c0b924f9ddeaaf7e72c40418c319
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:28 +0000

    slice over rely conclusion to

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
