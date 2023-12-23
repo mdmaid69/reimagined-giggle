effd435eb49ce68294557e5cb452f370496af906
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:51 +0000

    touch ashamed move although Uu

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
