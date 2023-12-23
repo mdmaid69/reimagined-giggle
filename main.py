601c0d078d5346db3a0bc9a9d852d3f940753344
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:57 +0000

    transport win help recently alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
