45023a50cbde390c46a9482d27e7587c7620ad30
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:51 +0000

    thank come trade married use

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
