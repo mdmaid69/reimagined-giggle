e9f8fcc54f89c480d3338eafd8bb58fcfbdbd33c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:28 +0000

    reply callout reaction pinch keepoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
