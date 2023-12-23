ed16c0387cbd7302f336531fa987329a429389fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:23 +0000

    getup positive rob preparation ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
