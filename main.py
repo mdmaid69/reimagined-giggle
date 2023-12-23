9086e3c74351522bf02b0c7ac70649dc1759339b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:51 +0000

    both great responsible stupid advanced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
