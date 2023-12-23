dd71843b73d4300c6aa40789ec5b149465d18101
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:20:36 +0000

    whatever water balance quickly welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+for i in range(5):
+        print(i)
