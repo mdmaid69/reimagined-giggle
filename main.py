2a0d3ecace3a14cb754d5ef59d811e2277f44538
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:30:11 +0000

    emergency suckme goon around contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
