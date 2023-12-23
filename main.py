870f81bddc50affcfc70d409dbb1b88e06c93f9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:10:56 +0000

    have think plate interrupt pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import platform
+def get_os_info():
+        return platform.uname()
