72a3adce27720353dd03b3cc7323232a78a06de1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:21 +0000

    hole unfortunate many cost forget

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
