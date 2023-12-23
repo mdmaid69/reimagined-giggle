57002c07fef9dcda599e1a501e11d88821b748c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:00 +0000

    news lost event they type

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
