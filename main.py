79380613b470594554ae9f5877ac75a01cf19957
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:09 +0000

    shoot habit cough stop respect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
