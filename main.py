75fcf7906f2bd923cdacf9ff14e1ed715c23a41e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:35 +0000

    list clothes violence powerful distance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
