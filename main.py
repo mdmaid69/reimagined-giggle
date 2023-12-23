d79d56eadbac15a3cc7189f3d6d210a39fe4fe83
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:55:55 +0000

    beat suppose loose shopping search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+def count_characters(sentence):
+        return len(sentence)
