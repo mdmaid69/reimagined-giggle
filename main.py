b763ebc77d74aab66399141d9fcc5bd855736061
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:52:28 +0000

    news worse any feed church

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
