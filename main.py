2ab01e0fd7959d06647603a9030e7f20afd349c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:51 +0000

    familiar up unpleasant then gate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
