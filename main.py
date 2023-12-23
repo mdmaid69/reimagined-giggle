bc9afba51914fc48cfa1fe3709ae70bc41c27199
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:00:07 +0000

    order use beyond frequent occasion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
