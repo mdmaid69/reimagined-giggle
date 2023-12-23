58b37d3b4d0503b4f91a78719cae11c370f902cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:07 +0000

    through consider mess right holiday

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
