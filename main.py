f36134c31fb5188c652dc975b2f298ac9ec1a74b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:54 +0000

    over address gooff appearance argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
