796e1b42687e297c59d4d192dbe59bf36fc57c62
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:08:22 +0000

    somewhere spot wind pick port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
