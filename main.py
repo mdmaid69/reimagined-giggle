b6efcec8ccfc46f500ba3dba8f2a89fed50be069
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:35:42 +0000

    still cause operate dead likely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
