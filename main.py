3bd4976802afded24a372da097f8b099de99e626
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:06:27 +0000

    common switch quiet unfortunate afraid

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
