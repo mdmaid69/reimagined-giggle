e1e95715028252cda2ffc9be863fb5544d42c165
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:07:06 +0000

    family possibly reaction invitation uncertain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
