d3e3e6c63b6a2583b4dbb18ae45a08c31f49f26b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:58 +0000

    Ii little report pull reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
