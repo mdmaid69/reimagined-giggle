dd8d0b89f4bba3f3b65057ca58711534d1039af8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:07:40 +0000

    any tend plan anybody yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
