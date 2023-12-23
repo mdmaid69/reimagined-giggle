2e2ffd5a71f3f0a76ca0256d6271b1819b2a8905
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:06 +0000

    narrow divide independent considering identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
