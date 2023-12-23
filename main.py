40d414040cbd60782651f3ed45e6981a0ac1828b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:32 +0000

    test damage ability sorry what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def divide_numbers(x, y):
-        return x / y
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
