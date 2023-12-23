119f88d2b16f1b9110ba964b895ad7844529362a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:22:06 +0000

    blue fun majority they bin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def convert_to_binary(n):
+        return bin(n)
