9d4f83067665164c425662c0a3b9a998012d9f01
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:47 +0000

    hello forward name trouble approval

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
