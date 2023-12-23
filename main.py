677d4887deffd48f65a8398b045fc423e4bbae1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:43 +0000

    gas would muscle demonstrate expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
