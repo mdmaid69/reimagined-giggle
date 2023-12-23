1c51f4d6ce5a5f43db3974285129e8cfb5b0cef9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:39 +0000

    next shelter pour fall greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
