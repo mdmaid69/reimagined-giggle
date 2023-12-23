2f52ff61369740d3993e06610310eb0e2655de41
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:55 +0000

    likely fly compare neat expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
