e48d7b6c5f6db5c2721cd7d8fbe9acdaa547a99b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:10 +0000

    leave recommend broken object design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_itemsize(array):
+        return array.itemsize
