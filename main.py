e515c74856eb870024e0bc3846711e2d4ec12e24
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:19 +0000

    noone fight shall calloff runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_typecode(array):
+        return array.typecode
