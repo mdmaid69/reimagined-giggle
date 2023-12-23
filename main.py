2d7537d1235e48de7d0c1e9395edfb131b027bd9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:29 +0000

    goup fast later number accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
