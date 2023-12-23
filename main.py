12acd6748fec57cb952c441d47887b7c78c7c385
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:55:17 +0000

    study thumb performance since movement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
