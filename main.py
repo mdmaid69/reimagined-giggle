dfa84e35a413a726447268b7bc4cdb38e4a5a28c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:24:03 +0000

    notice abroad wide report secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
