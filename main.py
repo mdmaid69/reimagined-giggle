6b7054e4038beb1745cf1de4b0c3a7dd2a8d581f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:13:58 +0000

    goodbye firm rather hello so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
