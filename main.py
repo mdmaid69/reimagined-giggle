f185981dded8fdba81c302e8c917baf99c7a0708
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:33 +0000

    science negative nose relationship shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def convert_to_octal(n):
-        return oct(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
