22db98a7b48750faeeca973c06edb8cbeead8805
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:28:58 +0000

    closely side keepout dish wear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
