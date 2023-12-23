9815611fd640ad6557ef57095f6289fb71a1065f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:08:53 +0000

    comeround fit operation ball entry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
