1c9dd8f5bc7f0cadd3150925dc961f361728204c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:39 +0000

    family dinner read perform bin

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def sort_list(lst):
+        return sorted(lst)
+import sys
+print(sys.version)
