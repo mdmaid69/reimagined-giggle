f7020ba8c16f350c1b4028d25fa558fe52206d25
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:05 +0000

    but approve high fire night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
