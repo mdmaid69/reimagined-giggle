16a36419db8fbdfba9e6d1cffac9c7aeac82aa79
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:09:08 +0000

    place to later complaint eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import random
+print(random.randint(0, 100))
