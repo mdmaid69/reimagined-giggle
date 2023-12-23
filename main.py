c705cfcdbe2c73fbd494cc2b2f6dfc010ef1a50d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:57:20 +0000

    drawing properly tend accordingto awake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
