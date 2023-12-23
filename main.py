afbc54caf9bd95c58f6ce734a983a84b3bbd1af6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:57 +0000

    hurt shop temperature agree weak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
