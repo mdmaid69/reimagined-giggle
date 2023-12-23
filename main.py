c76585f4f020acc24746115d66da9b73138a4b51
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:59 +0000

    behaviour set escape sound opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
