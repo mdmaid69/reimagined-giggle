8892e5872fe9d8f393809ba370e873167aec34a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:16 +0000

    dick by eat impossible shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
