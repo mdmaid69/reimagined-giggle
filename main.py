74d84683444842fb074024e71cb3fde475fc5592
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:38 +0000

    possession constantly anyone section prove

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
