dc9f7a11882cf1d960373308316d719c4d281fe3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:22 +0000

    bush emergency have reliable dig

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
