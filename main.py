a7bf61868701a74f9159f0b70c9c8a960b2bb4d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:40 +0000

    confuse boring arrangement wall race

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
