7e8ac0f30e453330d916e1b8e254cf43e7a51159
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:48 +0000

    blame mail pressure neither shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def find_min(lst):
-        return min(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
