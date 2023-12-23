9a72f31769b7add1b65b5ca96ce12e95292314ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:08 +0000

    business great wire Kk fire

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
