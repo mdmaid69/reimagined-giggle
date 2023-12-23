4526b38853da41e39938e4bf5ca4f85871f15380
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:03 +0000

    behind send ilovedicks holdup vanish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
+import getpass
+def get_username():
+        return getpass.getuser()
