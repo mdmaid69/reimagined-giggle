3c32fdd2b30923ab3148b66e3b34879d76000352
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:33:30 +0000

    hot wide murder war like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
