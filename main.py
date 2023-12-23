d00ea4caa7d71f161747dbd67bff5519a06cba8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:45 +0000

    left well usual yes owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
