39a05f66a3ce71a220404bdc8b39b3df91a2c527
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:12 +0000

    then cheat mark though callback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
