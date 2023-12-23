b3adc8393e21b80d5273a47146800d9c31c74fbf
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:51 +0000

    rank convincing practise price evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
