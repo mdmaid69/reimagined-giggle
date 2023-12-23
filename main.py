71f86702538236e6faad7656da8446a5d24ad4f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:33 +0000

    past involve bury good his

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
