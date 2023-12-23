935b34b757b9644eb548d5a9a73889b295c6d368
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:25:33 +0000

    shop off crash however stretch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def greet(name):
-        print(f"Hello, {name}!")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
