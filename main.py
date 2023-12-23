2aeaccccd9c18ed0b9e018dd8d78f33438b4365e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:44 +0000

    plenty speed stiff general ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
