4d8947b8309ca400108b57a58f4e3e5fea8290d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:57 +0000

    little slide life ticket impossible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
