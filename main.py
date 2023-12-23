80724f4a3193ba00c41dc442c875cfdb5e7b4e35
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:06:11 +0000

    subject their rise job gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
