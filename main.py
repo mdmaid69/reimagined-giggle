52f84911f46954abb8714607eae70af3e68a9ebb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:53 +0000

    each swallow tape risk manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def cube_number(x):
-        return x**3
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def convert_to_binary(n):
+        return bin(n)
