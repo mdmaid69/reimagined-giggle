36e776886d9129c492c0f5610fb931317839d863
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:30:58 +0000

    cheque become wealth Vv tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
