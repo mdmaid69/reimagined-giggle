8639bdf793f6aad150b11f8db32aed70f7bb7922
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:04:34 +0000

    worried 179bite wet sort attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
