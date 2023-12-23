954213aac4167b13d029931d9f9094bfb87ec233
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:56 +0000

    never join keepout press branch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
