f901c3c3529e4a2729b68181014daec168d26103
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:46 +0000

    before murder autumn shop maybe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+i = 0
+while i < 5:
+        print(i)
+        i += 1
