164187f5e305cb05593b68cc002869a62d2726d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:26 +0000

    flavour divide move naturally slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
