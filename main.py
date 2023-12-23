344d6b800825e268ffe5cb391603bd77699f09bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:06:07 +0000

    most us underneath level value

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
