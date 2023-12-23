47210d468f4f2f8124c2f9b5f5ac6f3b768c9025
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:42 +0000

    lose wing pleasant cover class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_current_working_directory():
+        return os.getcwd()
