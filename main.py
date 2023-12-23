905e5684359ff125c8dfaea8e252b40a32a2c1fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:42 +0000

    thorough escape plastic fasten crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
