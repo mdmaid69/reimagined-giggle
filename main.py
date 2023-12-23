b72e9c78e19538cf023c19fefc40fe480e742221
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:22 +0000

    Tt argue stir favourite think

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def count_words(sentence):
+        return len(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
