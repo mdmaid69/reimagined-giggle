b5f31ea1ff0f3688302ffa4da956e2917b024be4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:42:29 +0000

    none exit distant chin square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
