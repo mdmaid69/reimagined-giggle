982c36d4e5ad3a95ff17d2801cfe23f060b6b3d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:42 +0000

    front satisfied giveup perform comeback

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
