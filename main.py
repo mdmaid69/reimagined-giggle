2b5a89533866bc83906709ef1f12e722dddef7dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:39:53 +0000

    excited when spill without relief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_volume(length, width, height):
+        return length * width * height
