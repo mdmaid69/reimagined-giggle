1cc258b43d154131fca7853f887a25e35d8f043c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:33:55 +0000

    by understand less careless wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
