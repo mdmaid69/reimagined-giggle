8fde9bb76a5fffdd12253c552a7e9248fe0c9e34
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:45:06 +0000

    distance 572enter interview union demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print(list(itertools.permutations([1, 2, 3])))
