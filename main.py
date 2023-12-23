d252d51ccc4e2eb9a2f1d005b086dbc7e4f769db
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:34:12 +0000

    law ear concern but return

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
