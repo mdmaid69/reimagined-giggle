0bcd2c245bc40f9245c55a86e4bff6f66907e12f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:54 +0000

    letter exploration lesson ground cool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
