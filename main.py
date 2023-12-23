8a44f25c1adad6bfb7e2d60a559e5e3033691c1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:08 +0000

    correct tired repeat our length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
