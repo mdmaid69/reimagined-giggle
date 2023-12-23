914decc6fe971a8dd4c044074cd3dea600a59a00
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:38 +0000

    belong late heat mess contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
