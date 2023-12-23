bfaebcf1cf0c53f978d6628a16cf94343f9f4073
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:47:26 +0000

    modern patient perform weigh increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_bytes(array):
+        return bytes(array)
