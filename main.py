5e1cb22b1a0bcb67ed7322058711c1cc03a421b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:39:34 +0000

    blame delicate benefit rescue tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
