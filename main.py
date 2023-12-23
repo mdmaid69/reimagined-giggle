7ecd61a9e2d0902c1535105c8c83e20335322325
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:34 +0000

    stay borrow section accidental but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
