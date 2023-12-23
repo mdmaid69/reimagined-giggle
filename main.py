48837a08db6443eb0cb99d717c782f6edb34a65d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:19:26 +0000

    me home insist direct goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
