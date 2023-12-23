74b20c1e99375f0086a798e4c28ef5b89b060179
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:26:24 +0000

    level week design extraordinary quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def convert_to_hex(n):
+        return hex(n)
