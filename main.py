ed12e5d72472502316d98deaf2802a9e66e68c1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:11:03 +0000

    criticism worst inquire take sense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
