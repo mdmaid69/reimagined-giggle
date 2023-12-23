60b650f488276a451f81cc7ab62d50329ead3c4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:48 +0000

    unfair divide harm usual opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
