844aeb26760c50c0d4bef37f65a3c16b4ed7c606
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:02:55 +0000

    feeling advise skill doubt exaggerate

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def divide_numbers(x, y):
+        return x / y
