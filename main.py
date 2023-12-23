50b7e006517dd50dc4596e9de455c94d71a6676a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:44 +0000

    convinced jump brain about wake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
