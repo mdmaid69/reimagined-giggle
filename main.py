99a6e152137b3442873e6d373d142ea477a51574
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:32 +0000

    full cost point degree today

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
