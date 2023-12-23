dc3f4ea742a85b3207e50b9204a20c51ea40d109
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:43 +0000

    since proper tie mysterious now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
