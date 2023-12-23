9b348bf91cb3b7ee5b2ba0bfafcfc48ec3f17dc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:13 +0000

    remove fasten light desperate another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_str(array):
+        return str(array)
