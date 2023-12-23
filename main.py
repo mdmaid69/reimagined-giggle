18e896f27eda946b6e4652f4882da7f1b06ff5d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:49 +0000

    spring part considering noisy announce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
