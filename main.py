c9929ec18256ae91c29e47f83ea57e7b0121bbe4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:34 +0000

    sign pause fat compete say

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_density(mass, volume):
+        return mass / volume
