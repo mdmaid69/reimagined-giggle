cd66170a0fb0bb483d24c3440f2effa659a1cf40
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:48:23 +0000

    course welcome old hit flow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
