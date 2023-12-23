1920c0e7f2cf73467b20c60730b041f2e627d09f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:47:12 +0000

    view ourselves contents support badly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def greet(name):
+        print(f"Hello, {name}!")
+def remove_duplicates(lst):
+        return list(set(lst))
