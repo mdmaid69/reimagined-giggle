38769ef4b0d7c8b3c06ff5890d578655399e25e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:22:55 +0000

    surprising react foreign comfort until

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
