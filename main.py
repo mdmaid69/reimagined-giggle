1fd15d213f154a28a2ae13b5448d1b7b2574909b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:06:08 +0000

    raise generally exchange gather warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
