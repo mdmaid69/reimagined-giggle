586c36d816dc7fa1ab18b41fe9615c9c762a2bb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:54 +0000

    at thumb turn human nearly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
