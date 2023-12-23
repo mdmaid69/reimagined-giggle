d3fe57e58e36ff8faf3e747691c52d1c13acd6dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:40:16 +0000

    later nothing authority jump 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
