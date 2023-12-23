ffc889483c596e3cb0430e141157198550144d03
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:01 +0000

    hurt otherwise ago talkback this

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
