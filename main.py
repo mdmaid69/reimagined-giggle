e02cdf659a1ca2f04fa39392136a9ff34fbc2d8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:55 +0000

    sit powerful hard catch turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
