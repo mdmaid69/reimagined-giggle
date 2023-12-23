505cf49c31da56982752ba0c5a71ebf741dac251
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:06:34 +0000

    answer enthusiastic common claim feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
