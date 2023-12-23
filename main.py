8b5c0269d050f8abd830c90014d16081ac6110b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:03 +0000

    custom fix convince money beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
