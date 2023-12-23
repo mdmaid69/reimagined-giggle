efb1290d8a9c40f5b7c259336725e1995d25b40a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:12 +0000

    yes tour scrape load public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
