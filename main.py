fa03137de3c21dc48c74041461a447ab73f20910
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:05:24 +0000

    memory or other plant exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
