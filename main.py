f0e996501fdf3e27af29da18992dfec9c1d7cabb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:42 +0000

    juice characteristic level floor thin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
