312038591ebde247a5b214cfb22f0ee01f297460
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:46 +0000

    lean office flight 565end needle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
