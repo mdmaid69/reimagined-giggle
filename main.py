040d4eec21def70fa30d74c0a7b681b6a60ea171
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:17 +0000

    close familiar together satisfied interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
