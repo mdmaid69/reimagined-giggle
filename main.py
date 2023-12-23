4b3520b4517760feb091bd47762cbd8032eb4e26
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:26 +0000

    place affect classroom prize keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def convert_to_octal(n):
+        return oct(n)
