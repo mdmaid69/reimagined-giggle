0218cb060fa0b03c9cc761f33784d6a0223c1c4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:22 +0000

    occur friendly feeling hurry goup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
