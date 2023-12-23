43a8079204a6051705ec4fbfb8d092933f525108
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:00 +0000

    plan shoulder trip cover mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
