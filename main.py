1df02d56da65b3eccc8bbbf35312240d944b89c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:28:51 +0000

    afternoon error surprise shock passenger

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import logging
-def log_message(message):
-        logging.info(message)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
