7a0f08520c15c5fb41a01ed60b66e299c28501ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:31 +0000

    proof coat fill final block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
