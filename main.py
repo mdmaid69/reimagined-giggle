83f9b2ec6db8c079811b19211180495d76840a9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:46 +0000

    honest rise brave bend should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
