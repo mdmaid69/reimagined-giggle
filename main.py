11d8d17983799cab06c122f9cc9f8e641895b002
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:37:41 +0000

    no top cruel where block

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def add_numbers(a, b):
+        return a + b
