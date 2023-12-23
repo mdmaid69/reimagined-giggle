36f2efa794445b03336d6a0252ca2077ec799b8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:43 +0000

    deep mine exchange breath back

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
