51cedb27b8262ef4495705e9c5cdf6d724fd9573
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:26 +0000

    rub further choose your morning

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def is_odd(n):
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def is_odd(n):
         return n % 2 != 0
