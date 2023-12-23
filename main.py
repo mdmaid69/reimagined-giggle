f963aa079dfa2826384400a9be1005ae13b72994
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:32 +0000

    surprise pot opportunity attractive Rr

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+for i in range(10): print(i)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
