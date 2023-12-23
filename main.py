f5edf29e4a42043a1bb218c370fcb103f7233873
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:10:11 +0000

    shut design comeover far trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+from collections import Counter
+print(Counter("hello world"))
+import getpass
+def get_username():
+        return getpass.getuser()
