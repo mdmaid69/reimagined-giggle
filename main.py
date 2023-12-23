1c2e351e105ec39ee5bcef0bd5a8cb13285f6f15
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:42 +0000

    Nn bottom sad afterwards meeting

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
