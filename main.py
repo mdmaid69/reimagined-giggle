a085715a82fcfc4210a24256c499b7bec879c5cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:25 +0000

    any obtain ever plan study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sine(x):
+        return math.sin(x)
