ef0b9de74305c02ddd81828dfafd4eb2bed6df99
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:08 +0000

    mad our charge reserve about

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
