da65e62d1b0fb6c70ec168deb7cbf7ba7d0ad197
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:03:45 +0000

    theirs attention whistle together trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
