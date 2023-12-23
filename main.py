612d90a7048261b950e42c2bedbefadaa7df1887
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:14 +0000

    voice soil shout bet fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_set(array):
-        return set(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
