836bd803296f485586f52869e62e4a5f11331287
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:31:17 +0000

    have shame fall broad increase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_int(array):
+        return int(array[0])
