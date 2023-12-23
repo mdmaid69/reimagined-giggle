6dfb68fa244a422cbb424b13836720abe93dfc73
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:13 +0000

    tell hold repair wake close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import collections
-def create_counter():
-        return collections.Counter()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
