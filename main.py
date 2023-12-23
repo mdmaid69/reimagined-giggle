53723fad2d41f8d0dcb413f06f04dea97a5d30eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:32:42 +0000

    together you truth tired unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
