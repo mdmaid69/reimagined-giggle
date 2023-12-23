7749006dcc559bd489015540e677e866d4c4bde8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:44 +0000

    tap move form low approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
