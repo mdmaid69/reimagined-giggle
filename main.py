b6f19b07e032c964f876d3ea1f117490230b91e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:28 +0000

    not smile than worst treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
