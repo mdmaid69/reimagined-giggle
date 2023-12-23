f95155c36d591286f0dc1df6e20e05af519d3b11
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:57 +0000

    please rush bridge artificial sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
