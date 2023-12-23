c7c6b38e0b19c7b4eeccbea3b43f0f49c3a143c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:56:07 +0000

    miss contain interview lunch reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
 def get_array_typecode(array):
         return array.typecode
