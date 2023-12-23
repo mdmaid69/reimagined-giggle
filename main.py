ef6fb14a804ceba64a41367385e2051d022b7b54
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:04:03 +0000

    bend careless never judge almost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
