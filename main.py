d64d8a75b21728cccda8f68e845a5ce6049c3957
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:17 +0000

    comfortable agreement influence order polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
