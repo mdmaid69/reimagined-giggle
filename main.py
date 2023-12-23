7e118e6075a21ad7c8e655226c71cbe15f2acd24
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:56:54 +0000

    payment why once hers exhibition

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
