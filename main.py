d963a758819318f8bc8174e766dc5954d7c31fd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:14 +0000

    rain honest keepoff deaf hers

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def is_palindrome(s):
-        return s == s[::-1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
