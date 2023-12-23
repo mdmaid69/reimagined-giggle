c30dc66b25a52b39dd3a050f55894e34d7fb6af8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:53 +0000

    confusing accidentally solve unfair born

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
