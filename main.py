0f47dc16275985d6e1d471fe25a036c52d9a4eca
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:37:18 +0000

    sister fence delight brush bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def count_elements(lst):
-        return len(lst)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
