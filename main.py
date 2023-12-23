9243384af4d573f33354736628a6324e50a8b108
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:24 +0000

    print tune regard spin possible

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
