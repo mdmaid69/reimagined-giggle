1dd231464d2acb1c63c37280545c952c5b64234a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:24 +0000

    correct care ignore mother describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
