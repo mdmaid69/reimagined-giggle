32f00a9a033f38eae763adad1de7deb9f6b4774f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:08 +0000

    kick away least exactly temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def convert_to_binary(n):
+        return bin(n)
