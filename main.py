c2d0386ef6941403faca47d86179b29f4be48906
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:41 +0000

    bet safe affect content runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
