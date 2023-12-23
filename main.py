5471f1463b8693a4c872d30c83ec07b3de8a482c
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:31 +0000

    build bother geton doctor under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
