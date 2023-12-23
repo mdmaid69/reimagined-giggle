8a99c776621a3564c91d5cdcc423b857baaad7b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:15 +0000

    study radio follow pure total

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_str(array):
+        return str(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
