1cd1cee349e28f91c3f8fde893a2b8430dbe6814
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:33 +0000

    breakfast side kill welcome sky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
