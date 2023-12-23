6bebc11f8b0c4af6ed730f06392ac9f7a29a67e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:17:49 +0000

    number university sister distant holiday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
