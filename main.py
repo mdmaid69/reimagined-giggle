30c9f312b1d7b03c2d94b7c755e27c5ef8d804df
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:53 +0000

    smooth skill pressure label skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
