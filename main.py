4aaeb95731d424eefbcd8052796f8f4bec9e5352
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:44 +0000

    room yourself line Kk strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
