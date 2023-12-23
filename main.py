4fb5ea9dc7f566c908ecfbf1eb7ce7124c3156b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:00 +0000

    again glass proof disappointed priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
