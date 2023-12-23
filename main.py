8f6ab48d3e1a7f350c788d6bc62d0a55f181d338
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:24 +0000

    make chat when away next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
