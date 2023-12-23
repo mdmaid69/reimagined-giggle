056b82296f83a048eff05581c4a0a3bc96e17ef3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:43 +0000

    rarely eat forward traffic stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def convert_to_octal(n):
+        return oct(n)
