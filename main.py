ec97e46e792e894b39d78d4e322e8fbeb53b16ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:56:26 +0000

    spring chat town perhaps minor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
