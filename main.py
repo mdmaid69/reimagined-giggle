9f897cbbb15df9f03c257e57c3d2605da56f7f39
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:41 +0000

    future dress description bring programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
