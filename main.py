7b2ce1a2c2ba8fe5a86f87802ff25389a0efd3d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:24 +0000

    flavour control call relax knowledge

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
