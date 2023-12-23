f3e03289e9792884b30b10dc30fcd4b283791c7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:45 +0000

    catch direct port other certain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
