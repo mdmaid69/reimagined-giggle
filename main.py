57e17da40b2526036d14b9d3eec8959d30a0ecd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:59 +0000

    lost complete happy laugh temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
