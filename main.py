2131769bec2b385dd98756f3a142fc30eec4d6b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:32 +0000

    sometimes goaway height rub apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
