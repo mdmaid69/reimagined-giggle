4999cb14355403134fd1cbf59152a00521dbad4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:49 +0000

    uniform right 569enjoy minor worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
