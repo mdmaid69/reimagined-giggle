6f957c63cfdaf2fc8d6035d0d6c5f014414f4735
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:18 +0000

    ill active smoke get valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
