9c930ee3a24c7e5a73940f0e15a85bb7c48b31ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:36:26 +0000

    score keepup attitude tend poison

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
