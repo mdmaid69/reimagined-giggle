69a2fe921d4a25b77f683ef957b7fb2bd427e5cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:25:04 +0000

    stamp exhibition snow stretch closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
