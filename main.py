59cbb967b70b57f1fac6fdacb4e41612e7529d06
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:41 +0000

    passenger marry tear promise software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
