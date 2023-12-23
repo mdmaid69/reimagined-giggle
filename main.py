82c5eba5c35f3c040d76aa5ac4b9211edbffe5be
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:01:37 +0000

    more bath impressed natural cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-print([x**2 for x in range(10)])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_power(work, time):
+        return work / time
