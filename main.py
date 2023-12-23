51ceaee88614ca1c8b17271a271eb09027d3bc03
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:46:38 +0000

    eager display same congratulate content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
