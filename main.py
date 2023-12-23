25d7c77e4bdfb03455d6820685133e0300c24ca7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:37:59 +0000

    floor 572enter deep transparent substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def square_number(x):
+        return x**2
