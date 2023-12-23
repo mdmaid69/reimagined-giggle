67b735668e47673c1a9bb1c7a486cfbb5104a827
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:22:03 +0000

    number turnoff date circumstance will

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def convert_to_octal(n):
-        return oct(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
