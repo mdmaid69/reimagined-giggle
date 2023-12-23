8f5fe6cb16bbe79d6878e82b91a3cff1e169cd49
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:00 +0000

    Yy cheap book team beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
