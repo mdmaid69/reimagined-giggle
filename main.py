6fb7d4735b7fcccb0789607a9923dfb43dea3117
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:33:26 +0000

    learn chin pleasure between mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_max(lst):
+        return max(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
