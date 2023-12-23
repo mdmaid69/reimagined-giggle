caa80cd3c24d46a38857c1e5c20a91c6a1e1a8ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:08:29 +0000

    apart comment translate goout lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
