828a708b44d6a6c9a9983013f80ddff6b0e03c49
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:54 +0000

    seem traffic fear possibility comedown

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_even(n):
+        return n % 2 == 0
