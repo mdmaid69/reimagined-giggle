ccf64070052fe58c72ea5058c66bed5ba6f45531
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:34:44 +0000

    opportunity learn plain comefrom rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
