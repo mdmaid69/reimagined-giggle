e680a9f9b53b178a68c1b0863555e47914fc66b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:18 +0000

    price explain warm dare failure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
