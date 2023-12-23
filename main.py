384e4871cffdc610e8de54ed439cc3a38db0aa38
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:32:28 +0000

    lesson action dream appear proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
