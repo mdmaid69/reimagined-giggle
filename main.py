0f4ec7a846ede06d6191c1629b7d15dac1d3147c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:43 +0000

    each makeup worth justice anything

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
