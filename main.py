2df2afce5a18987fee998e3c42b3534075009759
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:48 +0000

    fight through after thin fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
