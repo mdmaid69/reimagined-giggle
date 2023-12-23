ac8f4cdeee2dbbc04058e39629b0fd00c6df4a3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:33 +0000

    respect movement complaint work OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
