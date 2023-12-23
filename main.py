f08920a5fac4bcedaf79d9720b044cf720a886da
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:26:28 +0000

    backwards stick whole air direct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
