283fd953c1a776df5098a7a5108c99883c951a4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:32:59 +0000

    price tap open area nice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
