279548f790951c1828bde71bceed76ac2d787e28
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:23:50 +0000

    ourselves tomorrow walk by brain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
