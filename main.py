2fe5a55ba25ca8701f12cae9873f4cc228f05ec6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:58 +0000

    private under gap seem cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
