005b3eb22759a5abf27a19f7503be6c0c8b94808
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:04 +0000

    month general moment worth congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+for i in range(5):
+        print(i)
