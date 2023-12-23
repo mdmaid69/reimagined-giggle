97c167562d9d85be637587e682ee470bbdba3876
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:17:39 +0000

    jump trap woman taste everywhere

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
