248ff6fde874f7c19920aa2ac07cb02d085f1370
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:23:50 +0000

    bringdown mark provide smooth closed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
