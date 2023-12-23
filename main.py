55ececa6418ca54e516a0086637fb166727c1a95
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:09 +0000

    hand generous software fast tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+print(datetime.datetime.now())
