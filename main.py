90106fc39bf9d407c4876613527ef27bb10c2dee
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:04 +0000

    upstairs difficulty destroy surprise Ww

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-print(random.randint(0, 100))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
