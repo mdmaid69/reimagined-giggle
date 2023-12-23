b5dd1b8bedb6249e3dc519efd0d807c0087bbf6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:56:20 +0000

    peace workupto whisper drop speech

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
