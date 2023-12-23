607fd4822961c16d295d14bcaa3721d226b87411
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:38 +0000

    goodbye sea through lean pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
