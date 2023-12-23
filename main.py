d322ba65354cbdc99bee6a0b1780fdcfcb098ebe
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:34 +0000

    dream content any keen emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
