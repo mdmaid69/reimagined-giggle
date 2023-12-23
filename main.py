17aaa647938b29273f1b01ff036834e62c399151
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:26:35 +0000

    pay tread fortunately battery ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
