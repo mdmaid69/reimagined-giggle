b95bf16cbf3de978d398a5aca539a21a56a93de1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:52 +0000

    ought describe lesson present themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
