9fa606eca4b0118b7194c893ffffcd7daf5e4f51
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:49 +0000

    stay calloff goback(to) visit adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
