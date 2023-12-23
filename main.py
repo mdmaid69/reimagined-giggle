35243c51d90f5c5584ea98175af06c6210907f0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:51:43 +0000

    damage sister drive transparent run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_work(force, distance):
-        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
