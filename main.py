44a0479ad92f0df2b99e70028c754e607b302157
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:31 +0000

    thanks surprise roof pretend bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
