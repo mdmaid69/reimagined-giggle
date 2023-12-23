6e19a47f7cc9fe50653966a6e319916fd4d83e15
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:47:44 +0000

    separate visit niggaacademy later face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
