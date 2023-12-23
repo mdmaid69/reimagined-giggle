8464a9ee7055b8c6ed64acd3f661823fa8170898
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:41 +0000

    mess steal contribute firm bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
