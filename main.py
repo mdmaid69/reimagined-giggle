36821c00ef20fee7cfa1e73bead04c762f15db6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:07 +0000

    tip opinion Cc illness money

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def sort_list(lst):
+        return sorted(lst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
