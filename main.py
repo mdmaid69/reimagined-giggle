40ab1a4ba9bb1339dfe8421f192e66adb9b34ae5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:28:51 +0000

    concentrate takeout empty edge point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
