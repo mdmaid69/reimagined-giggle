fe61cb238a3bf082316b485f0558bad1a5d4fd7a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:09:56 +0000

    defeat home clear finally play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
