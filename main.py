dadc542f570614ba2fc71a105696a9cfb18c682f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:20 +0000

    lesson problem slice swim appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
