e34933ce01cdf4c2175e3698b37a8ddce5eae25f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:50:13 +0000

    thankyou share thin bunch door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
