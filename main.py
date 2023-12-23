a2f62aac779ade34532d34c93e6ac28b71d401b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:30 +0000

    noisy cinema lesson bag ill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
