14f4af0fb043480a43661a249a470a81a87ca0ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:39 +0000

    medium allow the appear press

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def reverse_list(lst):
+        return lst[::-1]
