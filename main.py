6ebda11e429da49d785066067b3c1cb266c5bbcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:10:40 +0000

    recovery start object press street

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
