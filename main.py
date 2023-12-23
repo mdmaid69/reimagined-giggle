8731c5fd910b0c6b9f8cb31c50e19ecb38ca6d00
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:35 +0000

    clothes produce light each entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
