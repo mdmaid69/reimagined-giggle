dd0e0af0860c531c6f30de2d909e883dd39bf5f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:30 +0000

    furniture condition frightened lucky benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
