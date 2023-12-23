8e6e110d7fd82f1e3adf9c6e8bbedd26f97e1881
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:06:39 +0000

    word nearly physical store day

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
