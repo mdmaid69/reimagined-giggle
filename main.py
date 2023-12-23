c27d5a8b9587d813f0e513a57503b9003d827e43
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:04:46 +0000

    fault instruction loose fight meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
