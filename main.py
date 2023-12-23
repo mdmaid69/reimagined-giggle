552db77aa594badf9bb55c82894c21f2bef13de2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:01 +0000

    wire money system custom afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
