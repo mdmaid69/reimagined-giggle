c566e2623ad2f5e0072a88d673d92998561cb215
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:43 +0000

    belong jump persuade bring suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_item(array, i):
+        return array[i]
