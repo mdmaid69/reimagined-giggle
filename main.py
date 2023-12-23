0bdb91980a618d69b8f038c48b3d70e8bdab2fd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:56 +0000

    shock track expression opinion butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
