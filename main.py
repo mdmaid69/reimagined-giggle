b858d8ea16db9697c5772175e04585f59cf2ccf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:25 +0000

    ourselves appeal possible learn roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
