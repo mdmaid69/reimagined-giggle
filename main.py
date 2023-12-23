1c65ff682c6ef7a0c5d21e80bfd76d7b1c5fc02b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:51 +0000

    closed flat imagine you proper

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
