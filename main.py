b5bc63f04a07ab1bf5903c2d38f892e264d2049b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:09:26 +0000

    how wake protection consider exactly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
