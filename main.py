5da5f98fe2d6a10d6e7298bb04cf150f8c3f484e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:22 +0000

    behind connected touch tend free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
