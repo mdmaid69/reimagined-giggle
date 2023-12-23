8cbe84b4f3fb671de8933bb1e81c405a6323cbea
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:27 +0000

    ear OKorokay stick unlikely length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
