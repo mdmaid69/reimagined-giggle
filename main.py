80c550def2aeb160c2da2419343fe54ff7637bde
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:55 +0000

    clear tell service creature needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
