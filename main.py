48e46704d87c7ec9c4e94f355c22f8912f56da3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:49:12 +0000

    further tip possibility workupto body

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import collections
+def create_counter():
+        return collections.Counter()
+import collections
+def create_queue():
+        return collections.deque()
