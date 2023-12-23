666a8c446fdc295935022b4ca0c48e92497ad4a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:46 +0000

    niggaacademy tape vote answer pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
