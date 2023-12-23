2f90593c21b235ec6d488b8d5c4b169ff74821b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:57:29 +0000

    prefer pair eager chat this

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def convert_to_octal(n):
-        return oct(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
