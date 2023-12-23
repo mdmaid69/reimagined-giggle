54a2079d866693f7b4998a58c3d7aecb867b3520
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:00 +0000

    club delicate die deliberate main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
