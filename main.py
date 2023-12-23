5842992a003e47c64abf354c58d5127c666ee770
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:42:05 +0000

    switch reply would there bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
