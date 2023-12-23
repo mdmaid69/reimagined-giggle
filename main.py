6f7034bb7abec73ca7d462041c766aa56d52b969
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:52 +0000

    makeup division convince future attract

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_area(radius):
+        return 3.14 * radius * radius
