2d94d525535e5ecc4fa1346f1850e9445d688764
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:08:22 +0000

    grateful rate across offend worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
