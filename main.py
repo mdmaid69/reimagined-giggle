f2caea8fde8d7a2874041cc06fa511bad800bb31
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:38 +0000

    lend development organization onto feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
