d315237375b282a114741c0408ee461ca236f151
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:37:02 +0000

    go small cause thankyou life

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_power(work, time):
+        return work / time
