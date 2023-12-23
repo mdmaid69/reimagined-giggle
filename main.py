2f68b7cc60f9d7347941be29f41cad6133ad43f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:17 +0000

    direct survive stir bill bush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
