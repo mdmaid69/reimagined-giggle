1af7106e4fd47b36bd797d236aeec3ed82d5bc94
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:37 +0000

    trial mistake reject Rr confused

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
