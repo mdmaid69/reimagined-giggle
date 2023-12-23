7c20d46615ecea95b1739efe45bdbe18a8010ca4
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:05 +0000

    hair cover black hurry sit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
