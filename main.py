cbf3660062481485862fea5a26e59041b84c193c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:32 +0000

    anywhere order doubt heart if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
