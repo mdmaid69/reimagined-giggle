cee0ed8866b3001c6fc388406b6ba2501af7c500
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:56 +0000

    attention friendly OKorokay principle provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
