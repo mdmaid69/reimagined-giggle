c7a78898dfea6fa37b62831e8d683221bc4a3a9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:48 +0000

    wish steer path time pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
