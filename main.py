160f6b14c1144cbba2205ff4b680a97971737a66
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:18 +0000

    report reach uniform sail succeed

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def square_number(x):
+        return x**2
