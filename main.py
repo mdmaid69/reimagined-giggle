59cb03515732b84588da33b6286fe93220ef4866
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:23:16 +0000

    pinch surprised frequent clean nowhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
