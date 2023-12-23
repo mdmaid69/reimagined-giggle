4ab6b647a3f64a534e963331b23df99d774a7b18
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:21 +0000

    indeed terrible may everyone flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
