ee67830f5cb9de9d09f7466c9e21f62cd5d1d38a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:31 +0000

    value glad effect joke combine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-print(os.getcwd())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
