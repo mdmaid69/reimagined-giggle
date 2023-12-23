774d331141f187548cdf260f5dd219fb9e98bb2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:24 +0000

    opportunity fact breakup back recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
