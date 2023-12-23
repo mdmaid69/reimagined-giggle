9f6ee44e16c97453f58290e5f37ce88bced8f4ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:32 +0000

    address which thanks information over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
