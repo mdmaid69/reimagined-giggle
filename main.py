ac2169c94e5b6bf308e65e09db7b1c948a7595f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:21 +0000

    confusing habit care she slide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
+def add_numbers(a, b):
+        return a + b
