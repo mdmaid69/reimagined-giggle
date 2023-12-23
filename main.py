dd0245d135ff6632467af430cd80b83f42d32f98
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:58 +0000

    answer production tidy false consider

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_area_rectangle(l, w):
+        return l * w
