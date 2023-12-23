52545a3b63f666a22f7d46c1045619e697054e2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:30:11 +0000

    ahead best afraid quiet just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
