3564f68628de2ab2b97b872e91bf5a5bd90fa6fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:41:49 +0000

    fire upset challenge telephone window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
