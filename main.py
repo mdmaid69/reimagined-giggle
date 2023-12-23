52c3926db65ea6f806658cbb1f004d2b55c0a831
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:55:24 +0000

    entry serious score experiment winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
