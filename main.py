c42aed7a2664bd61418000c5251e90ce349244fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:31:06 +0000

    last wear passage taste probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
