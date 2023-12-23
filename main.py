ff0cb13ce4d93c1283f7531e8690217b96f5820d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:56 +0000

    image come although hole original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_stack():
-        return collections.deque()
+def clear_array(array):
+        array *= 0
