8c4db7631af2d7fc6bdcb0895e4299a48a5ca50e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:05 +0000

    present steady plan deliver taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
