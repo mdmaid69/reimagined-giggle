8953a274fcc7b14f922c7b2296c674ed4eecce40
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:05 +0000

    several guide interfere content else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import tensorflow as tf
+print(tf.__version__)
