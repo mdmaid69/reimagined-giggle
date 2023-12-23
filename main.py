7c213948b3973cc67776a3be1ee07aef343debe2
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:18 +0000

    Dd rest next somewhere clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
