856334cf1882776e8cd2e06833ec45c7fd10e336
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:29 +0000

    accidentally that invite or across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
