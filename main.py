85d047d929929aa313514b47861ba709ffbcbd42
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:20 +0000

    tomorrow wipe emotion cool usual

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
