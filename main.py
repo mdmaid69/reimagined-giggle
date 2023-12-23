bc1e406cb92d789de93a0d5c0d12f35d88ec3dfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:11:15 +0000

    protest remain battery pen bear

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
