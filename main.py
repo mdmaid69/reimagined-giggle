dce0ff037080665a69e57a6f3c08e0a57b42e3fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:59:58 +0000

    problem wash rescue favourite all

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+  def delete_file(file_name):
+        os.remove(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
