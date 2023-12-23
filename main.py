03ef6c6376016a4f3f854b7ee6795a4f283dbb25
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:47 +0000

    attitude down judge everyone flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
