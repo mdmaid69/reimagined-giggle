352cb74481780da8206df3cd531cb3bc863df93d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:39 +0000

    show back shadow neither find

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def subtract_numbers(x, y):
+        return x - y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
