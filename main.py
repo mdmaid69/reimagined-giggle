7226eecd05679a87a404b188a352e8e9c6c66e49
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:39 +0000

    design themselves mention bin Uu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def remove_duplicates(lst):
+        return list(set(lst))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
