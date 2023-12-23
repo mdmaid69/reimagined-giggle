86099671c14ff5e29b768d35b4a51aec826e6880
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:17 +0000

    whom trick conclusion normal hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
