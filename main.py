283f64b5f4e699c2fbe2b2caa7ce40e856863750
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:06:53 +0000

    regard afraid equipment case basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
