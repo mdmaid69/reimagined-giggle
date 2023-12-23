fc264df60f70bc16d79807c859b109b7b695c79b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:56 +0000

    steer regular cheek wide trick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area(radius):
+        return 3.14 * radius * radius
