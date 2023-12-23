b7fde76caae153189793ed151ca648a70e37a168
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:13:27 +0000

    smile preparation undo balance who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
