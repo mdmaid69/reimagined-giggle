8b771f51f4c326d8fc80e84f2ecb06bace51280a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:51 +0000

    operate destroy tread operate mine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
