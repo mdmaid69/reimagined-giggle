17495cff36e4c293a34fe53ef8358f7832ec158e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:40:56 +0000

    callout nearly around recently miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import numpy as np
+print(np.array([1, 2, 3]))
