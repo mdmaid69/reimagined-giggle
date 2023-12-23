36611b7b5fffb23ec54a80d6a2eae667f6e54d59
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:22 +0000

    turninto scared pour inquire comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
