9e41d39a43ab6037acec42dbc17e76cfe2839059
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:24 +0000

    bottom line possible call crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
