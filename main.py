941ef8bc8628b259204f3c22ec32ff9fdfe33a01
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:09 +0000

    must harm rich intelligent weather

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+for i in range(10): print(i)
