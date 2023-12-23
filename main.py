2c0fabe0808b643913fe93c6822a4258f1a84a22
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:34 +0000

    refer rush go rain hurry

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
