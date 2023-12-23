d6147d1d6db810d2b73adfd956bd170c6b7a046d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:18:34 +0000

    wall land nice someone progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
