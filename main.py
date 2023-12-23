0468753cf360956422282c17dbb47a0f6c3127e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:27 +0000

    spin pleasure afternoon art clothes

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import time
+print(time.time())
