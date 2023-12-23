8ea6aaca255de16c7a2a1f13d6af257d78fe0cc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:13 +0000

    bath cross collection worst especially

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
