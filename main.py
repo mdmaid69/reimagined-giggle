ef2138616d1a64bbd6b3bb70ddb63c9b19647dfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:38:17 +0000

    corner entry exit avoid think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
