b3eaeec1597fc5f197a757b53d3691ea9cc5693e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:08:06 +0000

    getout crisis curious cheque past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
