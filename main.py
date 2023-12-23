a1f1727fdea1bed6567810593a12fb52d6b625b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:50:54 +0000

    first leg friendly impression enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import json
+print(json.dumps({"name": "John", "age": 30}))
