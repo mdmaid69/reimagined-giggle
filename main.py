264b91bf38c5f5abcc307e6f8917767a3db0e307
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:39 +0000

    where sharp minor goup news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
