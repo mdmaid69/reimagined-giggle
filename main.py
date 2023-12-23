b01c09e3a2d44f4793bb17d26046b439604be5b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:45:56 +0000

    wire many switch but every

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
