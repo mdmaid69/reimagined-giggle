5652a8b9c43bc871f80089ebdbd638b3ef408d06
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:39:30 +0000

    wing punishment turnup evidence post

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
