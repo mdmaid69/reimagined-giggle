cce08383fb618b0d8cad3d6a4c95fe4ffe9437f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:50:03 +0000

    choose rely firm prefer bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
