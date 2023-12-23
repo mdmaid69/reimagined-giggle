cb9bad9f73292df82a855ab5f868abec553bac11
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:17:45 +0000

    under summer afraid entrance country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_today_date():
+        return datetime.date.today()
