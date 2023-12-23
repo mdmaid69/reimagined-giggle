b474b9adcbb837fffdbc1a65bc54e7e3a4eb138d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:52 +0000

    unless trust immediately cost violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
