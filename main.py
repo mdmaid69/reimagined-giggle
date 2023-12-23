ab534b000a440e89cde0d7c7f3488aaab55974d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:15 +0000

    police exactly swallow generous welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
