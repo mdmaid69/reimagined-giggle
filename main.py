647f5633a7bb071fe3d5f2898ab5e755cf1a68fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:47 +0000

    language mixture ago answer appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
