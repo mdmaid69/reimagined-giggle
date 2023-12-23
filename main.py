583f8ab6073039b0be02a05a85d31b76516ebe88
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:10 +0000

    strike scrap calmly rent after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
