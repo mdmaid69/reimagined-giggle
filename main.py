cbebe5c3dd3759a30f8d28e996f457ba6bea9eb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:42:55 +0000

    book burst favour thin mysterious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
