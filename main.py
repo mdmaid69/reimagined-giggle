fd5e028cc8bb33f0116a1f0d2858e5f2bdace62e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:49:43 +0000

    flow intend watch deliberate dinner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
