b386472c42749fea4a6300702efe1acc97da20a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:50:08 +0000

    niggaacademy priority ever enthusiastic level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+  def get_directory_name(path):
+        return os.path.dirname(path)
