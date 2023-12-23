51a4f5016399dd367138b5567e7a6489ad2c18cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:26 +0000

    kind people trick number temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
