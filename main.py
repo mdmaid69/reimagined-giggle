36919074524b7e5d6b0ed00f214490d4aa242b08
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:50:26 +0000

    jump door spot school cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-name = "Python"
-print("Hello,", name)
+print(datetime.datetime.now())
