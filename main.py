a93fe522ea4e8ff7db48338d44d9f933bc616f05
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:16 +0000

    invite empty comein offence fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
