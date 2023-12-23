f687f17e5114ead7b78eace6b0e03e32f5f3b385
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:44:17 +0000

    572enter loss ignore represent concerned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
