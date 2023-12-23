78c4d2aab30eb969fb5108cf3c277762bba20953
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:30:05 +0000

    section height freeze escape sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
