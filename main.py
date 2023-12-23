336edced37e2ba79a28b35d5bc4a8f576f70ab19
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:32 +0000

    Jj but satisfactory container negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+for i in range(10): print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
