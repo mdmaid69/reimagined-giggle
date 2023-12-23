b5fca9a3db230b01bbdaa7259d1201f9f7f13f70
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:49:08 +0000

    Ll supply ignore owe fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def square_number(x):
+        return x**2
