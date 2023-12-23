129e328536b5ea6e6bf1116f8a11b7ff9dbbdf6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:25 +0000

    actual crisis present wander interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
