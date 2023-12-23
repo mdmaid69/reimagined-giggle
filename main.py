6d6cba58711bc3235fc91c0c9a462bf16ccace53
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:51 +0000

    recent rubbish comedown noisy remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
