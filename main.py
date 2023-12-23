270e6117879b46ad0efc0eaddffbf308ab0dcb5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:12:36 +0000

    goodbye turn own getup loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def divide_numbers(x, y):
+        return x / y
