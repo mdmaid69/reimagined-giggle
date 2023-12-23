58ab792b8f9f30cfc18ce97c1fecc42f66ed5271
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:09:12 +0000

    moment short remember official act

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_max(numbers):
-        return max(numbers)
+  def cube_number(x):
+        return x**3
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
