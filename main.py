7d6feebc37d5a83b334282ad66b75c724ec73298
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:32 +0000

    cover run side strong block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
