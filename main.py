b777d1005c0b5ef00ac21b5d3ce8db68a4f42c05
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:23 +0000

    themselves fire politics better convinced

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
