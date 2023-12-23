cd768050a938a1638bd3fbfd8d434ce87038a1a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:51 +0000

    organization reserve may appear giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def reverse_list(lst):
+        return lst[::-1]
