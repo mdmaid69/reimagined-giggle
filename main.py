68cbb75a9236b2c2b0d6fdfea0b07e8c493d14fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:34 +0000

    bottom knowledge platform know petrol

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
