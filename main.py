0d3ccde5fdb025062581ec2d7bde1ad0f6bcb1b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:59 +0000

    crime add contribute with guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
