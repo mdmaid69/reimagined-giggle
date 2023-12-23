97178fe570f1111ac1ac0f62fbc3ce229a041698
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:19 +0000

    worth care someone quick pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_max(lst):
-        return max(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
