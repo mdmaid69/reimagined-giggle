da5395b4530c7b8bcbe0ca3e6342188ba0e18110
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:00:45 +0000

    confident play autumn shelter lie

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
