3550219db3bd92b6e7689d1650f54dfdbdb4c84a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:47 +0000

    acceptable none failure listen large

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
