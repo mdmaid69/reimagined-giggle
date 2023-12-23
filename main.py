160f6fc6589cd981d77dd45f43bdd2447d7cdd6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:08 +0000

    threaten makeinto agree spray sensible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
