11c794bcda071b5bb09233892c485bde5dfeb86c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:01 +0000

    clue cut temperature this perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
