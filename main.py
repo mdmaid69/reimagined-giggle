bffd8b1527b38dce98f7da058e570bfd7db4a996
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:35 +0000

    explain method loose burn comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def is_odd(n):
+        return n % 2 != 0
