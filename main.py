f25f9edabc47462e04016ebcbdcc228e507904ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:29:11 +0000

    bread extremely unusual team it

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def convert_to_octal(n):
-        return oct(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
