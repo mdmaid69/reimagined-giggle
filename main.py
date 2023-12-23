2cdc960f90053af1bc61ebbf4296d655a6b8dc59
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:54 +0000

    protest belong demand cut you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_counter():
+        return collections.Counter()
+import collections
+def create_priority_queue():
+        return collections.deque()
