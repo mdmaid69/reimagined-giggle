4a0813552ebad7feaeb12af87564c43bf196d11c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:27:03 +0000

    true innocent instant hope recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
