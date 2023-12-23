a513fa99eacba571ef70a8768de24a669917f812
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:33:53 +0000

    guess substance underneath temper entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
