16e86f047abcc893776aaf4d3532dee10db61f9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:16 +0000

    I suckme music dangerous cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
