4a4dcd6c8e02a67e08c561c24948f9c2f75d5005
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:59:42 +0000

    occasion scream base cut shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import sys
-print(sys.version)
+for i in range(5):
+        print(i)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
