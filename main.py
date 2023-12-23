0fbaa17a64db489aaaf516933c18a68a9d5f3556
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:46 +0000

    walk hate describe thinkover state

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
