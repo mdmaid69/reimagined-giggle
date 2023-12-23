af3965a1a54b7a321b5f8ea8278a9f9c8f8917aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:41 +0000

    but excuse answer floor work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
