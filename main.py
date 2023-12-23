20a1812c8310ac8b8b8ab63c6e29fb80fa967c46
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:08:37 +0000

    no scheme instead funny demand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+print("Hello, world!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
