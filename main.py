9367cf4d761c031ce6d6aec95ec36eccaef568eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:38:56 +0000

    slight regret weekend seriously share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
