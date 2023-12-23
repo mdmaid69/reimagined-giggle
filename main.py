76c9dbb37661bcc6f79ce60e017a74363ae0f379
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:12 +0000

    comfort simple minimum long rob

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
