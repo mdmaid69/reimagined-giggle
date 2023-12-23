ef3d2441610d021c8cd79f533da4cdc5b17da09b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:45:34 +0000

    contain politics cap treat warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
