eb403e4bc97d29dad9735e9fe9978a507593b94e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:59:21 +0000

    preparation Ww comeup discussion responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
