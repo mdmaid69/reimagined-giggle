660ad6d8d456e5b208a1361c401d073bea33903f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:25 +0000

    push art target round mood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-print("Hello, world!")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
