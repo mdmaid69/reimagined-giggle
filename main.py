82aeaeba8001d2ed553a3a3030b78a07fef86903
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:13 +0000

    under matter best I track

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
