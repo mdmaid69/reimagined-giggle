e43c75a31f6ee8862ec0d0fa34e423248116e058
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:40:18 +0000

    vote typical disapprove children fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def find_max(numbers):
+        return max(numbers)
+  def is_even(n):
+        return n % 2 == 0
