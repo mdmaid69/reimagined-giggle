3bc4118f994501bc9d825b30f3f21b8697a45ecc
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:07 +0000

    usually reliable subject report less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
