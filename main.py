044fabcf666a42c7a042035e2cf568f9bd478eb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:28 +0000

    fit shopping actual slide seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
