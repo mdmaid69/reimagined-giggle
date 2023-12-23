f394d670a5006de120f66908c01f37b341c9514a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:02:09 +0000

    private slice OKorokay scream person

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
