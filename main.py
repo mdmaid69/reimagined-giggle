e6942fad36291586b5c32ab721cad5641fabbb70
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:11 +0000

    in pencil increase bringback under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
