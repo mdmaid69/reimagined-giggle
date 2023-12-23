84303b6521c96ce23fa6558f83066c968e6625ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:46 +0000

    describe task evidence dangerous certainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def convert_to_hex(n):
+        return hex(n)
