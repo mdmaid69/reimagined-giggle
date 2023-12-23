baa2bc4a99d10ebc43c1903629e4d3edbe4a81b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:31 +0000

    sharp heart model noisy earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
