03e4fd22034baa72bc49b25dd63485ebc5dbd1cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:34 +0000

    turnaway long include either recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import math
-def calculate_neper_number_to_power_x(x):
+def calculate_exponential(x):
         return math.exp(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
