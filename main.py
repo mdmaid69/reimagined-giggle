66b303c2979853f8abb7f288ee9842d57c568da3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:53 +0000

    laugh also expensive common so

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_int(array):
+        return int(array[0])
