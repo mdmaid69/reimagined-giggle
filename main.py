0693a889709f06e396539597e45099e11f32ee94
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:33:38 +0000

    safety art offer custom deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
