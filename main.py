3615551a9677bb6d6a8a1ec203c685ae869757b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:52:54 +0000

    plastic admire trap tough effect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
