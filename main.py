15e66c333e9aa84654ba41b41344b39b2b469df7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:20 +0000

    without protect shout turninto crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
