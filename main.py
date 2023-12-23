e8820380700cd1d53379af309387617d7d645920
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:33 +0000

    would approval complain unite your

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def convert_to_binary(n):
+        return bin(n)
