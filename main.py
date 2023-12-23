8f2d638a901d389916bf5cf588069597099eb5dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:34:08 +0000

    university several broad callback blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
