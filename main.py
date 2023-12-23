32eec5159936dee0b94ba0b7daf8aeecd57492c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:06 +0000

    process original imagination delicate damage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def square_number(x):
+        return x**2
