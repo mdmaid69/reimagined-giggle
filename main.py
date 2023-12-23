3677650aa651968f7f1b3ad875c5901393594942
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:45 +0000

    matter surprised loose needle appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
