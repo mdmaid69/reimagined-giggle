8af27685b1500baa285d419d82d1a62ee3f8eb4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:22:41 +0000

    say cause term question even

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
