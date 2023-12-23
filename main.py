35636fc460e7b9f43758af3f49d7f519718adecf
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:44:46 +0000

    recent where best teach sort

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
