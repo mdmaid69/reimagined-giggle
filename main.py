0f20af48166c4aa63e1761f328348b8710d6cece
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:26 +0000

    quick Rr throw emotion cut

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
