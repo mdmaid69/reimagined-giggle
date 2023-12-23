040ed7303e09c1c4693498533307c3274587b68e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:14:44 +0000

    rain authority funny could once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
