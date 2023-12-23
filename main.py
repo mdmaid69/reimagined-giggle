d1a9c6ad745aa1f8416b9103c347d7e6b1729c3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:26:10 +0000

    bang paper bit rich tidy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
