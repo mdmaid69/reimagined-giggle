fc9d07fa044996f791999ccc278b41ec035f559e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:15 +0000

    formal breakthrough change post depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
