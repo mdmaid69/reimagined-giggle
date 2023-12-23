4e391e45bb392568c2d1fa4ef8d975d8b1812489
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:02:28 +0000

    unable accidental free adjective mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
