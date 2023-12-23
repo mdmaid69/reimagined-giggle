16463be748b5d7b11f4ee06ec8401e7570df5486
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:46:21 +0000

    reasonable between even real able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
