9b842daa8454ed6469c3d7918343a8d47a1b4288
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:54 +0000

    door careless combination valuable information

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
