5d8c2ebdf3411e3a9496c3e6cfc16fd54316d3fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:52 +0000

    but race follow pool sentence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
