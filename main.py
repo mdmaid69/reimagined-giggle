6e64b5a3027dceb4d5428e3988560aeb83aa69de
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:13 +0000

    debt life genuine win standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
