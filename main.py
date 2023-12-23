667c630507c0c3e68aa469e0186872941485c939
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:57 +0000

    pride attention scream accidental on

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
