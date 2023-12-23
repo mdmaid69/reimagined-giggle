5f1a96c53b692e77af0a97119eed43a165da50fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:14 +0000

    description climb fair circumstance now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
