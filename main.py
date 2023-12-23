900a0a5fef65aabf2d6c673e8b817b60648f4872
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:03:42 +0000

    burst him communicate waste steady

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
