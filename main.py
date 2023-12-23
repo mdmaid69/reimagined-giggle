6c9f2d6edda16b9a378ef1fe60d393fccff5a7bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:33 +0000

    raw opposite replace receive risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def cube_number(x):
-        return x**3
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
