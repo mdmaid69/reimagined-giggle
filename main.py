0c9edbda0b9cc29024f1bec65c6cfe940bbb7452
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:22:13 +0000

    runinto slight worried study store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+print(os.getcwd())
