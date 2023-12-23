70497ed114b013bc5936a3cc82bbc4180da30d6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:31 +0000

    skirt journey calm issue but

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
