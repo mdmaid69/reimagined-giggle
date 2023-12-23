897354be477569d0e5d836d4c713ab1ae4931450
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:32:56 +0000

    middle drop soil impression goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
