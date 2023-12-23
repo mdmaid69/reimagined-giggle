617482af05d1eb7f3181c5a80d1572b4d847d444
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:28 +0000

    joke contact mistake right damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
