7a1845c6b06faee1282d10ea698bfb17fa604ec9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:13 +0000

    course contact solve pin show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
