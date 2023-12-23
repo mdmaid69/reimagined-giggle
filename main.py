afc5cff1d1f1e7ec2ae0b90e4182ddb29f1c3237
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:16:53 +0000

    Hh by forever unable risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
