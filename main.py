fdfbbfc55c59cbf61f0a945c90a6d2a1e2b97980
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:48:28 +0000

    fast before sick wind meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
