81c3491221a0fe6d1e42b4bf7d8ae85ccf7ec230
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:03 +0000

    underneath likely mention Vv view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+import sys
+print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
