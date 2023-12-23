e70f0e75b10ff2eed625bdb48a6f53d7a0ce253a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:27:16 +0000

    knock maintain doubt cut slight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import os
+def change_working_directory(path):
+        os.chdir(path)
