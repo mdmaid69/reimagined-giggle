07e5136895c905863b8ec25da106c6459b23b31d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:18 +0000

    style whistle order nerve discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_item(array, i):
+        return array[i]
