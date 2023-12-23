049f06e883f922d6756b5bc0a947f0fccb0c15ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:07:01 +0000

    takeaway me method turnup pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_list(array):
-        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
