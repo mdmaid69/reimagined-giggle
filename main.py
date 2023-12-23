2b8ce80b92304a184254222d96fce245be638922
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:52 +0000

    holiday smell blame slow crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
