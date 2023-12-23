f5e0be275695e26998f0a02039bb5f4465a64a08
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:50 +0000

    too custom introduction back skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
