58adb84e5531745d0a69446bfccada6b4b37cb17
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:33:47 +0000

    satisfied trial different turn ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
