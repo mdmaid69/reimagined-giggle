d5b6908416ee5882e5a14eadb0dfd6e8958ed4c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:48:13 +0000

    nurse pick except wise embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
