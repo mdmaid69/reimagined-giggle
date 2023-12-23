06475b02664ddefd40a5b5b86fc90f08843e5597
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:17 +0000

    complete clothes break since trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_rectangle(l, w):
+        return l * w
