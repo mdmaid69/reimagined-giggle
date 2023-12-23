e5402e6cd0971cc0bcaa9f3a5428d2fc0e52a1d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:06:11 +0000

    opportunity hang wash across brief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
