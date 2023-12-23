7b60760893b3ebb88117f680c1406cff8b11ccb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:26:18 +0000

    Rr reach exam destroy bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Sum:", sum(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
