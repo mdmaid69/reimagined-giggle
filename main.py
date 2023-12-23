7747a17260510aaabd9b3037f801f71475b931ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:27:38 +0000

    limit city interview against care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
