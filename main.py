54f2dc1c02eb084c2e05e196109aa3a347210f03
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:15:01 +0000

    tense stamp strong well programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
