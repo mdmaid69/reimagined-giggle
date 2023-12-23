75ab006872c90d0ac8f7593f37c7e6196aab240b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:23:25 +0000

    middle themselves rate circle rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
