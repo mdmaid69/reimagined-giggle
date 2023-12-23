a009faf7fca8187b44a29fb96f1907a66c68abc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:27 +0000

    winter design bury pause break

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
