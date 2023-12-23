10fba2292aeea2497c8b4f1211099884a9b0d35a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:15:40 +0000

    snow safety underneath Ss direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
