441e619999df614b9912e588ee2cdb9292a773f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:05 +0000

    dead worry absolutely develop zero

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-print("Hello, world!")
+def reverse_array(array):
+        array.reverse()
