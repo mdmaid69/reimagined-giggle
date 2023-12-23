01927b3ff3a863089d24eedeb169612b51787d30
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:59 +0000

    man flavour black funny figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
