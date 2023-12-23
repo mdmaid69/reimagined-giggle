84c3d98a320e146090e82a563237cef9a2bf8f7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:41:30 +0000

    gather argument hold since interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+def print_python_version():
+        return sys.version
