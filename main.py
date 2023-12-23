3a6dcdd0659c1a82a801c3e0019ff324e37a6793
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:57 +0000

    fork hardly weigh young size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-from collections import Counter
-print(Counter("hello world"))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
