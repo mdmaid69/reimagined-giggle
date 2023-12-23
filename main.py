8af63ef97440bdfad70ec9827cc90c52f1ff5f10
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:53:22 +0000

    height there amount place only

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_power(work, time):
+        return work / time
+text = "Hello, world!"
+print("Characters:", len(text))
