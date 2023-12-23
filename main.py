d4477f2773ec7c4593191c0fa0df915101b93275
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:15 +0000

    wrong blame active team sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
