32455dce9388804e44109142959ee097e386f13e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:35 +0000

    fly motion contrast wealth various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
