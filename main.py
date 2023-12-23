1ba827658522a6b9961951273c932c70a6ab46b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:18:27 +0000

    zero to weather than delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
