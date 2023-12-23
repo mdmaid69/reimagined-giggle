4a1b553c39e152de0a11911c822974eb6dc93d63
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:23 +0000

    less gap secret prove convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
