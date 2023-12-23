1a6af692014c9399e2867506237f5683e2d2fb44
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:14 +0000

    expect play along bridge maintain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
