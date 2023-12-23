e28abc652b79bc880c748f62684ab1ed04f131e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:52:22 +0000

    suggestion journey loose thin wrong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
