2de51b8886474f8c8f0462f23592bf2cdada0891
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:03 +0000

    burst surprising reply will themselves

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
