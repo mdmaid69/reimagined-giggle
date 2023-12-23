12bce5b886c43b17e54b2d5cbdd86d649dd76934
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:26 +0000

    tough against read limit everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
