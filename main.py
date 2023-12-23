c324cb29d15778288e0b8f6314137d6595071b9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:05 +0000

    everywhere destroy beyond birth warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
