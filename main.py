8bc3fca33984072726a0d5401a4e0ce6bf2f8698
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:53 +0000

    wheel injury ground soon repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_area(radius):
+        return 3.14 * radius * radius
