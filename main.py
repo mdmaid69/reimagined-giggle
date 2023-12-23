048743c836ab813d4ffad06cd10ba93d056bced3
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:53:27 +0000

    necessary cover real lookup off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
