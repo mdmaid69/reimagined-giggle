f9fb3537b575804b5515f43a0883bc558466c4f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:30 +0000

    so unable after moon smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
