1425cb0891bd5e9e6e3ea7a0074de0ff781d7902
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:17 +0000

    exit shake check start run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
