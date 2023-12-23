1680f9133e0877b150d7fb8af0a558e3c847f104
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:57 +0000

    exit instruction cover sensitive desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
