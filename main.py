b8e295e66a872b994a20e00bc15afde847010b6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:28:59 +0000

    foot unkind mess place either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
