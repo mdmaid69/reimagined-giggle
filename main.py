39ec2f0b7ae8b86c93fd3b9e74f0ff8cd2ff9b1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:02 +0000

    contrast age influence damp exchange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
