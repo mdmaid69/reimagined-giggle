c708163c0e2cc6185e91e186daa84eb9a53151b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:09 +0000

    damage waste write bury accident

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
