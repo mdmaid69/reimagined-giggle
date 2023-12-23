66e7bf99d899656e04a6de435d7885eb591015e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:52 +0000

    free guess equal several impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
