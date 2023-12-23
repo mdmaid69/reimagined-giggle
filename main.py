da09d28d124dfb0e382ce4e7f60c54057b93add1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:32 +0000

    Ll world condition reduce corner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
