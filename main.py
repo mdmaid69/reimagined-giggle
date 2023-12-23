1fb0aa981430135aa1b98af98daf2027b225b64f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:41 +0000

    strike rain born round content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
