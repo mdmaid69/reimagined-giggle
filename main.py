05060bd5fca1d995cc3a20d27930fee664d48a44
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:16 +0000

    flood concentration last waste disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+print(random.randint(0, 100))
