36c3d78b2b55889a7e92a4edb8f6b7f6562322e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:06 +0000

    getback there large floor tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_item(array, i):
+        return array[i]
+import sys
+print(sys.version)
