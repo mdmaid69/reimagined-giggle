18c2ad2be808a1f6927017fced125c1aef7ab9c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:03 +0000

    attract clothes usually guilty slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
