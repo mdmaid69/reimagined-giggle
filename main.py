5abee1d380c6dac2af0ef921688486e1298f9c5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:27:59 +0000

    about excited separate smoke kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
