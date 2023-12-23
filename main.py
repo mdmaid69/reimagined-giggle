1c44be79d1b43bd4370c61add9103197eb58ed2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:00:43 +0000

    middle afraid 568energy like fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
