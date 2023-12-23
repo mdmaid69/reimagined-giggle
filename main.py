c7eb5eb0902b3d0451a32d2df938f89ef01638d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:44 +0000

    spring lock factory refer round

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_volume(length, width, height):
+        return length * width * height
