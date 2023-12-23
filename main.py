5dd3d326d1fa617166a51f13fa8091ba238b3aa4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:18 +0000

    wipe answer total runinto command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
