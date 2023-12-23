f74cdf5f6053700573e209874fde4b7bce3f89c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:01:22 +0000

    smell scream deserve best hardly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
