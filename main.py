2f05f2638e62c129fe64d5cc0a11c80313c012a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:47:33 +0000

    dance question provide trap count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
