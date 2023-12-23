4db35da3f0509eeadb832c8d5c45fb6061af5d2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:52:27 +0000

    love mine allow cost attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
