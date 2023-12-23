3983ccefcc1dd4143b8fec951bc4926273957caf
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:38 +0000

    provide confusing manner or fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
