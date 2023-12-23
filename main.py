5be002415f45c8179f20ab4121ec6a3a8e58df62
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:59 +0000

    design home various round spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
