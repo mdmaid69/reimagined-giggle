8810dc2f5df5b068449031a4297db25e05e437c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:31 +0000

    window they answer admire division

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+print("Hello, world!")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
