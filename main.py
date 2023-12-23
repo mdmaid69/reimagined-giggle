55444bcf2c5bdd4052c284f7f8dbb5fc859a30e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:47:47 +0000

    waste development reserve birth freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
