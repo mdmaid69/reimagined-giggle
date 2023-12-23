874400ebf7e4e40a3664d7e739bf3e95750f7ca2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:43:01 +0000

    container set building practical keepaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
