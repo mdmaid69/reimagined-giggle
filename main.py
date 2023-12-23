28189dbd02c58b2bd1d8614a7f07c51ef3425e9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:19 +0000

    watch distant report gather answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
