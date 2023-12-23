d3b3e132a03b91ea841bc2efab61bff9c9ee7da5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:03 +0000

    goup pin may today check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def get_current_time():
-        return time.time()
+def remove_directory(path):
+        os.rmdir(path)
