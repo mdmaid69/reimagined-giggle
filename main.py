5bb9dac98802fc61977359d56385716d293f6b51
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:30:23 +0000

    last frequent priority worry blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
