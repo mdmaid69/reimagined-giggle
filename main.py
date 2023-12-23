a2e5ff334d02e208e46996a5d3fa0e2f1c79a282
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:20:38 +0000

    cope idea make in chance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
