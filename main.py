88f9c72eaaee8de108242624cec101716937ceed
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:01:41 +0000

    shade card shelter stand change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
