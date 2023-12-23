d96dd147976f3b6dcdd3cc34427225606f7e5cd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:44 +0000

    married no keep platform sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_typecode(array):
+        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
