2f3eb5f28c52053814c75140bf4d2f14c2b4470f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:59 +0000

    move front excuse share workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def split_path(path):
+        return os.path.split(path)
