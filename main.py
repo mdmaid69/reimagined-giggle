207d3f42d75f7f41fc1493ba3252093056b26208
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:18 +0000

    estimate exercise sale Uu even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
