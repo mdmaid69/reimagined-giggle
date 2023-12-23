4df268bf35db394e3e99e305ecd3a0e0d5a13ff4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:26 +0000

    again hurt pot issue present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
