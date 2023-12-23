b9eb6da38cf861b427de57d7072d088ba4260ac1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:28 +0000

    comeoff phone advantage 572enter trap

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
