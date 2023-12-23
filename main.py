e661ccc115b5945737e85d04f6d682c550fb0bc8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:43 +0000

    man makeinto steal have excuse

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
