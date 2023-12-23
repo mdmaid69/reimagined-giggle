c409f2c85bc5752f6e3a8279e371a0dd195d319c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:31 +0000

    comeover care payment no mistake

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
