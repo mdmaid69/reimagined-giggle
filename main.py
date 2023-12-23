a08968cb9b0e0fc7df54ae29d5b5a4159835b52c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:34 +0000

    wind public immediate copy feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def find_max(lst):
+        return max(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
