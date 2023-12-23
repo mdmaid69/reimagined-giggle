8536aa3f6782f315570f354cb1d0a3cd8cecda36
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:18:03 +0000

    contain shape keen quarter good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
