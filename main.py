51510d6a97f61346a68349657f7bb6732b153a45
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:19 +0000

    organization immediately under decision sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
