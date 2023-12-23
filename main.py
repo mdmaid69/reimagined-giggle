e33c058558ddb103950247e9334970d581ba61ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:58:37 +0000

    plate object early effort emotion

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
