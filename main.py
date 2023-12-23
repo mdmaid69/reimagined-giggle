4ac7d1e2afb5c5f1befe6c3549585832699fa673
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:41 +0000

    hungry combine love straight talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
