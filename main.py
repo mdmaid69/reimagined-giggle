4bc700ba046542fe4f2dc421eff604fc783409ac
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:44:15 +0000

    stone decision pretend thinkof play

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
