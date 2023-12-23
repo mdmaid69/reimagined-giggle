8cf98937884a0a7b4b8ccdaa73b45dd12a43e335
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:40:19 +0000

    lunch drive tax low flavour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
