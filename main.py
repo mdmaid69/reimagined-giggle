0dcb39924f61359e9fd1c406af2b9bbf8feea95b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:02 +0000

    cough exactly female order Kk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
