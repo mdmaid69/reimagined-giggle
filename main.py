d5f8a8ecb5234da6b6f33af7150fd925485c5826
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:43 +0000

    approve guess detail since view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
