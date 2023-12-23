f12ba15e0f1c344138956d2697e5ec7551b97a38
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:43 +0000

    answer key play book finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
