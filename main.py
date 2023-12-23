759ef68b651baddac5519a732bccde0604b558fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:01 +0000

    beside warning trade passage permission

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-print(sum(range(10)))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
