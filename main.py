d051fa60bc8bfc74b55d596babedd5f22e1f677d
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:09:48 +0000

    argue brave pause true strength

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
