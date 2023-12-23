34fcd834b03ecf4f98120ae79ec1d90c313c86b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:44 +0000

    illness disgusting if opposite meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
