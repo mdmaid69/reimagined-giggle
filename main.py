191d7581b2fd5ace31cd9446307733c23874d909
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:20 +0000

    air pour show excuse answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
