be0ad86a8a593fc9bb9081d2154fc4aa09fe5229
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:40 +0000

    point view clean support record

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_words(sentence):
-        return len(sentence.split())
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
