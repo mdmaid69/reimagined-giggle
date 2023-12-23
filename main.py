1fb588b34321531dcba81c953c0194608b4eb065
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:44 +0000

    goback(to) ever stranger fight agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
