a8f601f7601bfc2e2574fd7c611162a464c91c66
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:12:26 +0000

    push front hurry that fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
