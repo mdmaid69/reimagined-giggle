2707cfe2353b6b19278627eeb6cc74f0c0ad8023
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:53 +0000

    quality convince later force scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def multiply_numbers(x, y):
+        return x * y
