eaf705a6207d9d5b16b8c18aec594d7c243f1271
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:24 +0000

    Vv deep prove write dress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
