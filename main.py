4be2efc9ca3f618ff193c237b84c4fd304297d40
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:08:44 +0000

    insist hope talk act ocean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
