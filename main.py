94541f159292df5659a93bad6cc8cdc6e767a43e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:38 +0000

    fork him still Yy contribute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+x = 10
+y = 20
+print("Sum:", x + y)
