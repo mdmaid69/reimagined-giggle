ee691c81b7654580ceea38ab06f2c2ca22791b2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:04 +0000

    Bb 179bite reduction fit sort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_power(work, time):
-        return work / time
+def convert_array_to_string(array):
+        return array.tostring()
