5b910787e58ef54ec010a626ce2fef66eb536881
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:53:41 +0000

    treatment offer neither wish properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
