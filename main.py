75e2518011f9d043a5eb616a5c339fb29e603782
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:26:37 +0000

    tidy makeup light couple hole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
