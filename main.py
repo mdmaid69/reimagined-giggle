bcde82671345432fc27af7fb87beed1a84557440
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:32 +0000

    medium company cost themselves limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
