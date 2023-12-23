13196a4d9ad3cc42bf20f5c15b954eb27b9ecf6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:06 +0000

    sore content bottom succeed modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
