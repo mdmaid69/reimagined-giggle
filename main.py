00f15bbf2133a3b587d6d4e81dd63184a967d522
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:34:56 +0000

    pick weak brother dry extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
