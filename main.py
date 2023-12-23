221422e1a3acbccd5cb0108f8d40bc426967dbb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:47:11 +0000

    reject shoot person lost workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
