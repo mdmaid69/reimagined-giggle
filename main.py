3415549238f7a451dc1ae17b4df6bfba0785f66a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:53 +0000

    same seem police unpleasant give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
