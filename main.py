79b33d9460e9d896182790fb62011e6fd23affe0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:00 +0000

    peaceful promise area tonight science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
