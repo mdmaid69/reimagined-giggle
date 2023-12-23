e129c860b11745d875f567da8987aad1536513ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:22:16 +0000

    freeze guard female cheap love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
