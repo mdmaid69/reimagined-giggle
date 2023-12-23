ff8d6b7391cb3e13d3458df6e8e4090d56affc35
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:30:55 +0000

    system impression dark congratulate play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
