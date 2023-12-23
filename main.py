98d9d71df89dd003e4f6e0f216ac0d68e3bffa39
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:38:48 +0000

    across listenfor impatient fault road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
