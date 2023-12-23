27eacb069ef93d994433629e089db22c65621d95
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:13:49 +0000

    middle top last shop hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
