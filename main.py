62467dd9e6bed10d275348430b47e7d1d5910154
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:30 +0000

    of bet hospital avoid complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
