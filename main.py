83cc75deaa5de3165a29ff2b25985eb52be73474
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:35 +0000

    influence mess trousers object share

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-print(math.pi)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
