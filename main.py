e0fa9ffb87b2716898d3b61c7dd5be8a4fd49fb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:54:05 +0000

    rush bringup boat about formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
