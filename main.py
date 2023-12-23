e0de80acc07df5e400131ce671b8cc9b578a5f39
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:23 +0000

    spray considering convince other I

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_power(work, time):
-        return work / time
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
