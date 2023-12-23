bae5daa18edf12ee841b172bc37a9223a03eb4cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:03 +0000

    hope wave roll importance spin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
