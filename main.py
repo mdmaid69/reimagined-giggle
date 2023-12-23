f37aa1c4a01e2de7440bae9af05084987929872d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:10 +0000

    design niggaacademy fold concern fashion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
