39b1cd02b26920280dbddd207ed7c94ebd22eeb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:12:12 +0000

    crime little even battery you

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
