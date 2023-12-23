9f63d836a2f626621ec6447137f52add6cdb15f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:50 +0000

    sky notice buy apart emphasis

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
