f9c16a65aaec787fb1f766f2b2546c5590167119
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:34 +0000

    apply study call answer determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
