c22704da87ee17f70473a69de039b071c2bcca7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:06 +0000

    border keepout stand bite old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
