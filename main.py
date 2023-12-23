fe7c46e7fac58a40a39858b056b83aedf8e23ac9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:00 +0000

    my active exercise nowhere form

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
