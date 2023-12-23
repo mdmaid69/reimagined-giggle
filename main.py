72a9633c73e06997418e229dbfea6f9128c8393c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:59 +0000

    original read activity odd where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
