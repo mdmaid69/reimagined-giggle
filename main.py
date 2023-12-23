4e4ed32934a986802ded7482cb67a7121320492b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:04:45 +0000

    wind cut strong miss solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
