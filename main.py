049fe5ac174ef5c66a38a60f88b9ba83094548d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:11 +0000

    rather connect common noise field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
