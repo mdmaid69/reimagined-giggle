c1201dd99c57514aa3cf4c1989b07dedbf368b01
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:37 +0000

    tool fortunate legal contact image

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
