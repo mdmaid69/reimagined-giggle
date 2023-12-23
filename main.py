3be7044929465b6096f544904821c8fd2324e951
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:48:22 +0000

    peace key chin load guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_str(array):
+        return str(array)
