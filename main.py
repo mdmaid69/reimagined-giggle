d755e2b11454e31ca7cfd403756c84a37d8fc9be
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:31:12 +0000

    sort serve but angry century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
