31716fe772c6ac311ee81969d2f3160df666de34
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:34 +0000

    partner collect medium shopping grateful

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
