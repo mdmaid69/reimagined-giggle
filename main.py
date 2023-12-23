f4dae69bbaf976670eb69ce82721d5fb99bd9724
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:03 +0000

    disappear sing die song warning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
