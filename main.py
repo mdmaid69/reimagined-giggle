de361c9688201a8bcb94d6eadfc2d6501ec57c92
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:58 +0000

    sorry part capital watch march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
