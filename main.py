6e0f530d4489ff06716e42d9a321188e7f15a847
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:45:47 +0000

    soon soon unlikely treatment nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
