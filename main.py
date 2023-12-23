d59618c312d73c311735da6c67fdca5571de49c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:06 +0000

    speed would slightly learn rate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
