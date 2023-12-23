e10111ceef3b59786e20e1bebe90ffd96ff68d05
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:32:44 +0000

    winter 570enjoyable effort image Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
