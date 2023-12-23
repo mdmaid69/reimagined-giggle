29c55b4f3e7753b12972100be6a09d793d701ace
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:25:53 +0000

    make usual centre sensible reject

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
