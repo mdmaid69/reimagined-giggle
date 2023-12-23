4dccad237a5c30d49bd092de5c1abce4ec38a6df
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:35:54 +0000

    zero calmly statement before spin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
