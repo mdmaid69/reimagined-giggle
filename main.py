030e6fc3d89f318cb75955575e7459250996bb4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:11:24 +0000

    before begin deliberate thin last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
+def find_min(numbers):
+        return min(numbers)
