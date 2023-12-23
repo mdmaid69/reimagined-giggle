77b0d3c135a46c5feda67dd8d1820dc0f0bf53ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:04 +0000

    rid advanced proof male cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
