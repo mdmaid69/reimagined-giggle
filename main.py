f41058199417a24b3ead347861ff299cfd0e1b3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:05 +0000

    mistake wild further unit apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def clear_array(array):
+        array *= 0
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
