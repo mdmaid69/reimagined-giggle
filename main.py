0dc72e674327ed5bdbdd22229abf8e3b55626d77
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:09 +0000

    niggaacademy dead test be old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
