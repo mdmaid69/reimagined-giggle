bc5869bdb476e45ba5139492855adca12fb24cbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:25 +0000

    steady pull group inform confused

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
