6ff791f5491726978b19dcc6794d5288b6dbc992
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:56 +0000

    follow male fall trouble react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
