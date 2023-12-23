c843cc3e34fe64ffa04ca34d77a14c8530b33221
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:29 +0000

    anyone bath effort need criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
