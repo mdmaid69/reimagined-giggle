bb45bdf1ece11e539d86a7d837c3e7bba8aa0d84
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:36 +0000

    cross promise opportunity contribute raw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
