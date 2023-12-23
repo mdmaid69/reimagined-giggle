ec55b9cd83837625d120924c14da3aaff983dca8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:41 +0000

    shopping shelter needle moral violence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
