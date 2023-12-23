3fb03064d4feb49c4b1c80b6edb4e1dae5474922
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:56 +0000

    poor rain country file picture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
