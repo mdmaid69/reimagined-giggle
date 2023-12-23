77e87b3a034cf36eb4a936aa5b44ee78c93a3ebf
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:40:55 +0000

    invite again stage service opinion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
