afbcf4d3da175d0fd9e7199f93798d609e546898
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:42 +0000

    near fortunately boring everybody damp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
