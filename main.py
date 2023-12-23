2d9d8af07183c70bbe0a4d1540375c3f6e9e4448
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:49:45 +0000

    bin behind coffee challenge concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
