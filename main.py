9dacce1918a53209ee79566c6e65a7b554d3a366
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:25 +0000

    type myself public brush failure

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
