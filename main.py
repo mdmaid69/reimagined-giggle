a0ee2eb6763882e08250f2672accacf34d584ba4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:55 +0000

    sign remain gas walk protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_typecode(array):
+        return array.typecode
