dfc31034e79ffb967418b27dbd9c3ac4db61824f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:27 +0000

    or lucky fine serve taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
