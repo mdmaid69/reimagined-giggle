e90e5e951238d839670702501114bdd857ff5c4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:53 +0000

    suddenly pleasant getup importance walk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def get_array_length(array):
+        return len(array)
