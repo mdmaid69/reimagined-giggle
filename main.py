d670decd328c444d30393b8e6b179fbc46780870
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:48 +0000

    Zz turnout fork responsible crash

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
