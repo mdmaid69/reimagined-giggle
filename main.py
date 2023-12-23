a10cbdc261ffb44b1e7de4c7a016acb7ac02cc39
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:22:18 +0000

    community nor typical sort section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
