011eb30e861b2cead50fb411c8c846efa84de215
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:53 +0000

    heat badly there gift giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
