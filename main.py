1d3b56ac55be110d74bccd68acfef91c39331482
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:34:39 +0000

    city make comeoff notice day

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print("Hello, world!")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
