44ff0c8acadd68d0e61913c279c991bdce5035e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:27:07 +0000

    bottom step flat wish fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
