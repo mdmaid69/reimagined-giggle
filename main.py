38ac5e2f03146eefe5203deb98b29c794fbce5fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:49 +0000

    intend obvious sensitive ability guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_float(array):
+        return float(array[0])
