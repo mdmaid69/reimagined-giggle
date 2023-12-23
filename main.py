adf0ac32c65520e296dee05d461e8132e8bd7106
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:22 +0000

    respect life object standard afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
