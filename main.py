be66c1a902def0f0a46d419fd3b83a27f0d19067
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:07 +0000

    measure whom across needle phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
