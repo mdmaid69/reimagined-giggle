c5edbab12ca033139cdd0866582446aff56d5fef
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:03:53 +0000

    sometimes be school kick so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
