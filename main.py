26b49bb05d04531a49bd45af91e9027bed6bd2e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:38:18 +0000

    goback(to) issue fill justice mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
