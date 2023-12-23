b3d135a4485ca39f3a54b268cd24f838dbeac793
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:24 +0000

    568energy account community argument in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
