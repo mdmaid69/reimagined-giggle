79af5fa001421c873ef0df0eb141fc262aa40237
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:10:27 +0000

    object invitation reliable load now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(a, b):
-        return a + b
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
