137a8b94860f75168304954a062963a0b272d074
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:07:40 +0000

    OKorokay normal away congratulations stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
