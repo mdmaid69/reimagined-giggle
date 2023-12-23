e9f10684f2384ffbc471f86fbab67a29d1234ba0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:01:17 +0000

    spend bear catch discussion why

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
