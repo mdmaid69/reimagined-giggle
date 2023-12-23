1a37dbf4cc6c4315969a853a40f61aa66363633f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:03:23 +0000

    motion hers thumb dull add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
