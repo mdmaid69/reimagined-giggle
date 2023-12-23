c36716da32992276af73f3225f8c665ebb336413
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:19:57 +0000

    one hardly pronunciation breakup band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
