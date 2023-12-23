df475fd4902a658126f6aa941c2b59e702c9caf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:56 +0000

    example involve visit experiment score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
