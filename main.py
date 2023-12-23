a05d2beb2a6f30972d8f4152a6b43a88cee6337d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:01 +0000

    people juice mistake dish interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_density(mass, volume):
+        return mass / volume
