0044471d9e2b009ad4ff9f3135d1322b757bd6c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:44 +0000

    hard shame enthusiastic hope accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_array_as_tuple(array):
+        return tuple(array)
