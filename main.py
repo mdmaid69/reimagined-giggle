4721846c98449958327b8e4b1422b17151dcf5da
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:28 +0000

    mystery none breathe switch centre

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
