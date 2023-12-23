2a4fd8ecd07730d25ae7306d6320c14490a99eae
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:41:05 +0000

    side slight hear vote split

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-print(math.pi)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
