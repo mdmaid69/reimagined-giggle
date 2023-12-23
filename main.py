8f23f84b33d7241d1185313c10d465ad19aba709
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:05:20 +0000

    declaration may do thick takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_odd(n):
-        return n % 2 != 0
+  def delete_file(file_name):
+        os.remove(file_name)
