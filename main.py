3ad5f1a17e38e97c227ef1c45527a27c86bb37ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:20 +0000

    table disturbing if combination issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
