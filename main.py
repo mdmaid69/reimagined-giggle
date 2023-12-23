bc00d3f51ca2ffca081c1316cf378e047be53731
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:02 +0000

    apply how pump brush news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
