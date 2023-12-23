b8995c4aaedde3c82ced1408fb52a1cc9f6bcd32
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:06 +0000

    wrong determined twist exist 572enter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
