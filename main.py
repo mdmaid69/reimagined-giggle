3f2ba7a9d7bbd3eaecf9d271fd79c4a9c19e307e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:52 +0000

    Ss Tt slice empty statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
