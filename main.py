e921fe15088d1209ff4d59f46c72dd722527d33d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:59:54 +0000

    congratulate material rush obvious weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
