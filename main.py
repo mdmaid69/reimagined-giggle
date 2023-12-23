58180f4396d58d73360aa3a964cce651a197c216
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:15:10 +0000

    fortunate worry declare soil sort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
