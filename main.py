65b8f68b18f1faa71e7cf4248e3cd2a494b12cfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:06:15 +0000

    distant reject suckme chief fond

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area(radius):
+        return 3.14 * radius * radius
