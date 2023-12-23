fbdd80d98c216f492c265502153190b5125dbcde
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:14 +0000

    effort introduction cool yours stop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
