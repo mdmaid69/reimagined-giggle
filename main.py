9797d495689ed9bab63fee563e2c4ff94ea6a1b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:38:29 +0000

    walk mind sign about fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_counter():
+        return collections.Counter()
