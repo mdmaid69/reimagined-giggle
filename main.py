83b76c07a527ab0af89e10085aa9f5939282ff0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:12:53 +0000

    regret considering otherwise off instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
