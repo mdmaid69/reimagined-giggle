fd71af374765defd1b33d2e28f917cbab9656f95
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:17 +0000

    temper lose friendly match why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
