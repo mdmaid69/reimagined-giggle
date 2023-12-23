b9e470698e46d1f926e034bc0d9b1a3bf163d5b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:42 +0000

    bound wide breakout neither university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
