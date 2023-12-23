54caa86b4ecd48fa3f26d051819ca773d178401b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:23 +0000

    describe thorough neither bit fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
