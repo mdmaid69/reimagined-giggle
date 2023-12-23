66aa6b43f10d28dc450faeff827d7737d499b656
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:57:39 +0000

    pause completely pollution even strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
