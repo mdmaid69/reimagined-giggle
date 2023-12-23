9d0ed39690c79b3ab92b2c53cc3cc82c8bc67725
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:49:38 +0000

    forgive command tend schedule 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
