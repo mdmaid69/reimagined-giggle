6530d2a2be8decf2fd42fbc8325e2ac15e9ca5f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:04:48 +0000

    world objection exhibition anyone meal

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
