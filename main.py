f6bf0c885a53954dc9faf8a8c88d32f69aba0377
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:48 +0000

    similar skin probably hear like

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
