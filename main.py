02d04a8101460bcebd3a37721b8dd2c61c60d483
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:31 +0000

    major problem moment lonely discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
