d6fc331479fa0bbc8cd3af31876ce0d083b3c5a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:31:51 +0000

    Oo paper branch wire neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+print(sum(range(10)))
