bfa331ebc67ae3874a2261c50d774f4d2d5a2fc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:07:59 +0000

    giveup idea classroom deal suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
