66fc63bca0704aabd0deeaf601316a48d918e935
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:31:39 +0000

    invitation type relative value record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Characters:", len(text))
