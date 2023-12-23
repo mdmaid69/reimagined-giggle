f75ae4df5a07f98a11a8efd4608a8461b09168b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:03 +0000

    179bite chat public freeze ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def clear_array(array):
-        array *= 0
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
