8276be6aeb883332cfe72201372e92f38091f68a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:12 +0000

    often them machine convincing clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def square_number(x):
+        return x**2
+  def reverse_list(lst):
+        return lst[::-1]
