1e6e2f08f0d9e3bc9f5078da46645d4f4eabee26
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:30:50 +0000

    lookaround anybody noisy snow grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
