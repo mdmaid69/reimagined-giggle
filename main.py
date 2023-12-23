458a266d6f8f92e69eb217859f9682b08702a641
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:21 +0000

    lookaround keepfrom black radio bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def is_odd(n):
+        return n % 2 != 0
