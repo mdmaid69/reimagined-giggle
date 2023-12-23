13d570dd88fe918c471210230d15859416c8b290
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:50 +0000

    shine shoulder concerning begin public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
