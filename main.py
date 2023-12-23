2aada855b532bc93cf5fe750ab24e94e6281fe30
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:10:43 +0000

    block disagree field manner careful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def is_odd(n):
+        return n % 2 != 0
