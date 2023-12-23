fc61b49f1469dc5555341d8462e0ca1754b1838a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:12:52 +0000

    thing sink comment view post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
