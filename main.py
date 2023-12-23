55d26676b35f5c58c56e0d60743bb628daadb580
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:47:01 +0000

    makeup street eye criticism anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
