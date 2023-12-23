7f3876d33224046f6adf90459b8229b0b525c5b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:52:37 +0000

    that pull pile workupto violent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
