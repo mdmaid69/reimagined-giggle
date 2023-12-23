fa602977aa2f153124ed2cfc000956b2810d372f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:28:20 +0000

    neat thumb yours mistake makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
