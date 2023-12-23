dcd3b6b4f23a9da0bd0446f81f604f0248dff63a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:02 +0000

    stone busy sentence shallow community

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
