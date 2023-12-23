48477b733804d6a32536389139e00e95d9d699a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:36 +0000

    sit Zz few far equipment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
