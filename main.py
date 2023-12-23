27462e7cf459366dccca8d6d67ab32de8dece0e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:08:14 +0000

    somehow represent relative see warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
