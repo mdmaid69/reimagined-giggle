fe6baf9ec6f59b471ccddd8c8905d1a16c67dce4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:17:36 +0000

    once choose charge foreign broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
