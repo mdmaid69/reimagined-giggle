7887012e6f49e70c78bedce3e627cb2f6370d03f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:29:01 +0000

    deserve across worth tired operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
