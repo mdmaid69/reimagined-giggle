fe715f29b618685b21ffb4a13c5cfeb02b7e855a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:50 +0000

    ride quiet no smile constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
