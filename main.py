bcdba4a18d8a83c8b4f8a335acaf818442eab10d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:34 +0000

    start second pay cost sweet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
