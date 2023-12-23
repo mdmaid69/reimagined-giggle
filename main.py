dfa38ff8909ed0de5c0e2dd566f7582c26f6e454
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:06 +0000

    company risk exciting creature pleasant

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import sys
+print(sys.version)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
