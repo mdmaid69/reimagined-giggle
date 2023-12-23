9524c48fe2d93dbdae96ea4712628b16697b950d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:01 +0000

    idea goon describe result left

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def create_priority_queue():
-        return collections.deque()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
