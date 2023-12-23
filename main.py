cfa3f372230464473e86bf64a5f499a922de803e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:33 +0000

    dig acceptable miss mark stranger

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def greet(name):
+        print(f"Hello, {name}!")
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
