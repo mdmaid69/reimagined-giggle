67be108b8715440410de70eee1526a85f11e7c65
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:56 +0000

    issue giveout last myself anxious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
