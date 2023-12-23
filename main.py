709fc235f6259d03386f64f58c11c8607486dec7
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:01 +0000

    sign back trap to later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
