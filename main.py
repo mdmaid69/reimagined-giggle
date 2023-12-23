df887b6b0be594de8e7276946ed2bd59bd9923ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:59 +0000

    approve food survive rain anything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
