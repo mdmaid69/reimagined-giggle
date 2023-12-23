6d0a73e2f7794a0d56a1bcd86c8874cb73fa5e6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:26:48 +0000

    important towards part clearly calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 def calculate_return_on_assets(net_income, total_assets):
         return net_income / total_assets
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
