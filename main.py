7a53c86e9790d8d0870fb7176cf67ecdafb3e523
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:28:42 +0000

    tap law expect giveaway hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
