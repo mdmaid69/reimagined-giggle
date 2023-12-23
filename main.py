1ff9a9b17468236b12c4523eb2dca9268fb339cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:52 +0000

    university development welcome matter manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
