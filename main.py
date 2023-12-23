aefa093922021e31bcf3fb25f1c173e0ba04d790
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:52 +0000

    away either able wire below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
