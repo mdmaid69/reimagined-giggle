d24eef3cc22bb9d07705fd412e8759a78138705e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:20:30 +0000

    order situation gift noise back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
