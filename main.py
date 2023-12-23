456002ae0d468394f929f89a22630b19ecbb4714
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:25 +0000

    mad suggest modern single switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
