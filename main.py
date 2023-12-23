696d2c10837244eeef1015869829b4d6edd71ed5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:00:53 +0000

    trousers sometimes difficulty picture crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
