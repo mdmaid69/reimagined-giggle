9f78e3590a4fca007cc43ea814840052a8c1b599
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:57:57 +0000

    ticket learn everywhere stamp belong

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_counter():
-        return collections.Counter()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
