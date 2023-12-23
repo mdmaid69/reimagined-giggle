575fa96548ef896316ed3fa9b392f33de7e372f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:18:01 +0000

    add raise hard woman copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
