4a1b9b9d3cf53759d5634e0411f1a5031f24f230
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:40:53 +0000

    really exchange noisy account makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
