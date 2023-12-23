9e36a81c6bc370863f7652cbe3630e31d06bb984
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:16:55 +0000

    theirs match may tidy machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_power(work, time):
+        return work / time
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_median(arr):
+        return np.median(arr)
