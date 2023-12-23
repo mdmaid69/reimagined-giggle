07c78236e98d8410cb2d918619f97e91537cf7a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:36:35 +0000

    opinion plate rare children good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
