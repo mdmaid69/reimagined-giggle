5572d9de4bf5c8970ce53fac801f4b6e1a559eab
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:45 +0000

    left several introduce communicate thick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
