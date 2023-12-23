9e007a88fb17c89d834f65c2dc3569ba5e8b1ca4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:06 +0000

    drive area pride room recognize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
