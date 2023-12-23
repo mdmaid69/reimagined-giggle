1ddd660f0ae662fb4952dda06e7438e83ebe95ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:01 +0000

    read gradually schedule safety colour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
