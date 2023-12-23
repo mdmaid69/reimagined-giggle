25bdc084bc2ea4ede8d3bd27816974517f5f81ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:54 +0000

    effort fit lookback(on) wrap meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
