0434abf6b050d8c766d4ef533f9f709fbbe38e2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:28:09 +0000

    goon uncomfortable suffer unkind full

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_area_rectangle(l, w):
-        return l * w
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
