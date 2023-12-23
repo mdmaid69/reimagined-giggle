4aa7982f387ffbfb786b70bf0592ca707e78a9e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:51:28 +0000

    Jj rub hard one treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
