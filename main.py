095d26be18e37f6fb6bba19c5ee16f4e6d641897
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:54 +0000

    nearly love lose rub away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import time
-print(time.time())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
