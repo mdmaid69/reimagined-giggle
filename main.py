8ccf42f653a9e9b229011e1674ba6dd3021201d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:56 +0000

    lesson disappear detail mark eager

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Words:", len(text.split()))
