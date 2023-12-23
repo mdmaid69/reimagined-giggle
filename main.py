e8f9d04c7e69cc693ef98b3674d85f13401b938b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:54:35 +0000

    cause steady complain precise much

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
