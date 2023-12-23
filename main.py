e06984fdcdac5176ce8e9c168e1be3d6cd68a4a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:36:33 +0000

    tend move warning nothing rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
