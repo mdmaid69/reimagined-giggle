682aa9a8596b88cfd739aa6d65806f32f4644dce
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:04:19 +0000

    yet breakthrough soak insist cum

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
