8e04824b372ea252ce08fe0c263bbaf89b6861a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:38:38 +0000

    squeeze demand passenger deep holdon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
