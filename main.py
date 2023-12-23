3f0caf34b445c8ab26e19a837da0e58cf7f18a54
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:14:38 +0000

    thorough accept knock uncertain correction

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
