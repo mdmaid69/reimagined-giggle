81466c8028d9493d92d873086d56a409e86121cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:46:33 +0000

    character wherever accidental forward somebody

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
