2317b4b0bebd78194f064e2a64e16993907a1718
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:39 +0000

    could market maintain foreign sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
