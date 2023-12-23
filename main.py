48d5f565a6b796c5d75fcfd02d87d095cdc9370e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:16:36 +0000

    pot lay aim move wet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
