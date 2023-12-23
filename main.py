85495126ab300aecb4539445851e1d540fdff5e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:21:03 +0000

    bury search know block advice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-for i in range(10): print(i)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
