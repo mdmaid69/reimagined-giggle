53aece7ecf78326a864ec7d26b64d314c0b7ebe8
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:06 +0000

    wing uponon second rest thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
