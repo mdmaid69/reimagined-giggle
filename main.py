ab386acc0f246c06266de23c1830e22fcfd2aa17
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:05 +0000

    forgive effort ourselves passage impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
