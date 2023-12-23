1337ac1b9425a72d912703975af20cf8e8b85f29
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:10:42 +0000

    fetch lookaround the takeout birth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
