9ff1aeefec4d27c8be967eab2ffaabc2b99b3d02
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:02 +0000

    breakup right often action speed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_typecode(array):
+        return array.typecode
