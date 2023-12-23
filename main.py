6bb5bcd7f8107653fde6bb7313f9017a6271996e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:29:02 +0000

    stage wish night grade criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
