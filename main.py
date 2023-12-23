efc9eaa5c8b323c89ae2beed80a055f6b77dc9d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:26 +0000

    shade keep thumb pretty hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
