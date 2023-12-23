da92832993cbd214ca1d0acf9277f0cf2f75ea3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:08 +0000

    wear favour uncertain now choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
