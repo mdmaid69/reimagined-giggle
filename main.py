ee60c35f8862b8c2eca3a30f462073f3e650e902
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:02 +0000

    powerful round which tragedy giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
