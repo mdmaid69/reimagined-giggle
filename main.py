d52f8c4beb33d0b8da32515ee009fc62fcedbbaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:42:46 +0000

    show workupto idea crowded soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
