bd563a7e68738720414dc1caf62a17cc754d46b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:10 +0000

    argue course while strange concern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
