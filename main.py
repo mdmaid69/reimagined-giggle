4f34b3775bb4cdece0a462e74ff15a528cfb4c30
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:06 +0000

    declare careless proof idea all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
