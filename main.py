352cb92e9d2bdbc7fb5255e41e21bbb3f77a7c87
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:01:31 +0000

    bringforward independent careless nerve colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def set_array_item(array, i, item):
+        array[i] = item
+def convert_to_octal(n):
+        return oct(n)
