afb01990ecbe153e2a922e4570eddd59e67fe481
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:48:27 +0000

    join attempt treatment ahead murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
