11868de5c1067c597443b082b6f60a903ff08cbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:17:03 +0000

    free cinema goodbye afternoon movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
