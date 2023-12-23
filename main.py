6a0247be1878202e5a407daf7464e860ca844da2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:37:05 +0000

    if gooff push majority shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
