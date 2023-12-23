deb9046159a77a5dec58fe8c00eaa3a82a878b08
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:47:30 +0000

    split wing move organization look

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
