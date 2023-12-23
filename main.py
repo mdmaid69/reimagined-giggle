bbc13f72a520662afbe7392d741902f50936dcc0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:16:26 +0000

    print reach take judge direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
