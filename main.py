4771532c59b1bba242f44e5f17d2bb1f8492ace0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:47:35 +0000

    dead central trouble early late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
