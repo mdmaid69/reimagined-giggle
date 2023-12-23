e07268724da9303c7881eda0e5e134d7d7e9d390
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:17:05 +0000

    dig sudden message consult day

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
