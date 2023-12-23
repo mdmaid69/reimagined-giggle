b92281c0f5d9ebea1cac9ffb4319e1deac3b0583
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:32:06 +0000

    flat force concerning perform drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
