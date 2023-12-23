36b514066aa7b41e3cb164488d598e881e8be5a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:46 +0000

    impression maybe best couple talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def convert_to_binary(n):
-        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
