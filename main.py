9017fae6b8668e87049535723db933b2b01b4faa
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:57:45 +0000

    hope act display reserve promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_length(array):
+        return len(array)
