523e494c580265a7e59e4a5fe86bf0cbb4078961
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:04 +0000

    thanks few tie legal strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
