c7f899865ed58421e883a9d0ca90fd16b47d9977
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:05 +0000

    stiff schedule dream tragedy emotion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def is_even(n):
+        return n % 2 == 0
