c3ff66687680bce57192a0e9ccd84aa9b6f2cca1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:31 +0000

    press nature keep convince water

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
