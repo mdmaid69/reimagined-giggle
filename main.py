98e5be752f6bf552ad416b644f35a80962ab61b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:27:54 +0000

    explanation undo dull benefit sink

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def add_numbers(x, y):
-        return x + y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
