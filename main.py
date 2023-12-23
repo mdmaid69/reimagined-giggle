aacbd71981a7940ec2c3a25a0485baf9adbab042
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:39 +0000

    probably offer plain moment out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
