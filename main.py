d1f25ea1171b886314ee5e52e9cca34b1bf32f9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:07:37 +0000

    left as considering gate hour

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
