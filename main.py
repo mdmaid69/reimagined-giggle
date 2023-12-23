b5df08d08c3c3d6c5dbdd6d5f8a75867f95e87a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:17 +0000

    broad press turninto suddenly push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
