c2eeecf3fa00787f209d03a294125df7ac956d84
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:31:16 +0000

    unfortunate murder people news on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
