a4af445f2cf08c5f32e032f205fcb082b87ccad1
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:12:52 +0000

    motion jump exploration arrange obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
