75d9bbcba5737908c650b97babdfa9e82910aaa4
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:29 +0000

    stamp positive man firm collect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
