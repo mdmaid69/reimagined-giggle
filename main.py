e58fcea1790396321fa54f57c10e16262049d108
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:07:04 +0000

    ahead belief rich experiment thumb

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_acceleration(speed, time):
+        return speed / time
