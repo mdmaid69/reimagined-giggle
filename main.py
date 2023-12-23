c2d9dde7162b3da3d5daf93e2b4997d3a1b633f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:42 +0000

    yourself machine figure flood train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
