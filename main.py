0cc8dfffbb4a5aad5516a123baf954a86f49980f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:40:38 +0000

    but circle community bottom OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def sort_list(lst):
+        return sorted(lst)
