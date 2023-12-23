f98e8107dba0a1393f090997fc4b1350ce061835
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:39:26 +0000

    bin Ee eat record thorough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
