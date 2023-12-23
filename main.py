6398a0cb80a5059c22601fdeb41a6a91b79d0872
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:27:50 +0000

    completely board child goon far

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
