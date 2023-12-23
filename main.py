3e2a1d768b34cdf3be7757f0026abe37eade0306
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:15:31 +0000

    wish old nerve holiday boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
