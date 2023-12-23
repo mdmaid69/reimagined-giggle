5fb7ac618184541bd0945f48fd83145a3f43ce6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:16:10 +0000

    female gift wall introduction control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
