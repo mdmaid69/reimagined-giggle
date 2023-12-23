5e2f4268123ecd56d5aa18f1bf3dd185327b4457
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:28 +0000

    trip joke city 569enjoy pretend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
