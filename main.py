38257279895c331d1868e45ecbb7efb0c61e89aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:02 +0000

    immediately keepfrom Hh coffee year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
