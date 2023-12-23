bbe80dda9decf440a37bf05c66e01f6ac7edf186
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:10 +0000

    movement form force kill backwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
