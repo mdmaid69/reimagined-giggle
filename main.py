ba7efa2b4efa3dcca962ee6cdc643325461a61cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:28 +0000

    maybe break trip only like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def sort_list(lst):
+        return sorted(lst)
