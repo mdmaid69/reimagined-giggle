f9c6218971ea572754be855f0664ac9d42be1888
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:28 +0000

    word cum medium maybe tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
