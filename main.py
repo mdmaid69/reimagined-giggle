c2ff72d0f263a972ff206800f94b2084ed439487
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:38:51 +0000

    approve note considering scale means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
