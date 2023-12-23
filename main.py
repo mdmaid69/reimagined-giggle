38020591bc18cea2f917c672e770b9133be6cb3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:37:14 +0000

    centimetre mistake name tie expensive

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
