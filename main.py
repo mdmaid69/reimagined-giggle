38a7990ba988f6c3f9a903d71b87b26cbc456e23
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:42 +0000

    colour amount challenge fault live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-print(random.randint(0, 100))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
