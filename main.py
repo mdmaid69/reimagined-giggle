3742b43d773d3aedcfdebb537eaadf0a422656e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:14:24 +0000

    maintain cross during burn loan

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
