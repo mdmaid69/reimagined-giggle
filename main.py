fa51ea7fdd65483f72f65bc74ac324b6b32c114b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:21:41 +0000

    how godown holdon appeal upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import sys
-print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
