70d061e9dbf101d580a7444b32d7e500059e1e7f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:58:59 +0000

    fashion tell careless realize Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
