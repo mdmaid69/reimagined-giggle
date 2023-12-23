5a00a0bc22ddc7f1a233f3d79a4d5df483aab363
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:48 +0000

    calm true radio regard usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
