4e9ce00da0ed45f1ee0df45718727a687b930db5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:06 +0000

    depend scale section offer event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
