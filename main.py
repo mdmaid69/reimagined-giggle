3fa8d8e1ec56e7253bffcaea96c0875059dd6b2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:29:02 +0000

    nice pipe busy attempt towards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
