7981ddf689df22adb7cea27e35fb71a575dbee8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:25:47 +0000

    scrap beginning mood meaning switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def count_elements(lst):
+        return len(lst)
