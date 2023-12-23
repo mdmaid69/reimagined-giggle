49727dd3989428a82e3a09cfa016d1d9ad1e7ed4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:37:55 +0000

    intend late around pay might

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import datetime
-print(datetime.datetime.now())
+  def cube_number(x):
+        return x**3
+for i in range(10): print(i)
