2246e7de42ef26ee6cc7b04e6beb544a431dcc0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:29 +0000

    about should achieve bit afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
