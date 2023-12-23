b2ec68e13d1f230342ae26573b2cd61180e296e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:16:28 +0000

    interest side figure turn score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
