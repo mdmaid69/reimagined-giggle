24b6978a09d0b3d9cb3975df8e7d566e833a4e32
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:42:54 +0000

    bread human exaggerate really autumn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
