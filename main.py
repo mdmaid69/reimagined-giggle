7324e901327e630acb09c749a7929ab4486906e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:13 +0000

    outside fast apart load lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
