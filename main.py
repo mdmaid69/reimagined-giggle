732f70a160bdb65fb561e97357a55825ccd06f10
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:54:15 +0000

    getoff total sense impressed circumstance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
