f8d877bb64c7cf5ac8f0b73e0cdc4b6c5cdfc7b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:47:44 +0000

    mean round lucky her market

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
