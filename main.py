5e29b5cb76fb6056d0098b608647bc59da737f87
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:38 +0000

    should pronounce could forgive nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
