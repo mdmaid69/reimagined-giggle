29e516175abd566173d34a2e6f7c26a7a1af7f94
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:29 +0000

    string reaction wide warn interview

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
