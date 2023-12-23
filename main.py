253f04be190b08727eae1ec4d5985937571b809a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:35 +0000

    time gate operation typical obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
