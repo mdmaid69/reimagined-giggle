1eb95cc91ebbe85ebff75f5e9be4b0468ffd6c2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:29 +0000

    judge forgive tear drop bus

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
