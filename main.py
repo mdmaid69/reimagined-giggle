60c0a69585866c0bfc81cca9564298fa9f40bf2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:29:46 +0000

    town solve recent figure exchange

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import getpass
+def get_username():
+        return getpass.getuser()
