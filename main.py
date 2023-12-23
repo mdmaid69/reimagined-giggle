da99cf9161ee226d626c109639a799edba01a9ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:40 +0000

    pour feature excited fix language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
