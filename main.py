a19fc5bbc3eb1fd9afb5e84159327128224f523b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:12:53 +0000

    regularly holdon muscle tongue chase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import time
-print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
