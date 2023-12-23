ba807d189bffc0b1ee803cd36e80003e605d183a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:28 +0000

    big flight create emotion unfair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_hex(n):
-        return hex(n)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
