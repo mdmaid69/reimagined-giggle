52d5155880a08813d4a2cf16c1f5f85bcb21416e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:44:49 +0000

    front produce nonsense return mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
