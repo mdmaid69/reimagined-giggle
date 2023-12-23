4fa3a7b56ba6e0dec48013ea5a9c9655d31f2342
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:44:37 +0000

    interview news satisfactory try last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def append_to_array(array, item):
-        array.append(item)
+import time
+def get_current_time():
+        return time.time()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
