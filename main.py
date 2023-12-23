96a4612eb4e85079ed69639769281614f91a9306
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:40 +0000

    broad ground claim secret belong

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
