55bc2a842a0dadc4fe1f5ec8f875a40a6c09fe8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:24 +0000

    lonely traffic attempt hospital temperature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
