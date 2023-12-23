daf31a3e5a8e1a1c291d78def2891d014beec1ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:11:36 +0000

    but waste determined lie replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
