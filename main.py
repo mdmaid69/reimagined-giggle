c4d9929c2233371688b527591a7858f78f87bb37
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:28 +0000

    whenever light heart part callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
