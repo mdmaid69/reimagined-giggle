2bcdd7338a4057b482321308a122f82e1fe77199
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:09 +0000

    none giveup course drink you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
