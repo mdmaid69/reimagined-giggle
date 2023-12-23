5246711bcddf325bd1f6c8c183ba85315f9ffabe
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:45:43 +0000

    home callup street bag like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def remove_directory(path):
+        os.rmdir(path)
