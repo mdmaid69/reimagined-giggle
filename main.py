c43558f82ace8887a013c096a51df19f5ec8a15f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:45 +0000

    bound destroy pair corner callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
