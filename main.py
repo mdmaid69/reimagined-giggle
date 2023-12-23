a4ebccbdadae12b744d9c31b0baf65e502b9531e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:50:20 +0000

    little future table name downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def convert_to_hex(n):
-        return hex(n)
+  def cube_number(x):
+        return x**3
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
