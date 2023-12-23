834130a226e0245d9943a26d4c9fe5b6ce47bd6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:35:13 +0000

    surprised whoever radio bye bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
