2cd3766b8411d1a390bbe5448d1f6ffc109c11d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:34:21 +0000

    treatment wealth lay coat wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
