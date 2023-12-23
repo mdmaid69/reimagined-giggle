ac134c40ac246c0fa67f9d6bcfac892f4b7309ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:17 +0000

    giveout holdout cover substance whisper

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
