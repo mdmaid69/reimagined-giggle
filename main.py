131104dce11de8522fd1459f1cb0f9ee21958275
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:15:53 +0000

    warm it book lesson meal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def count_elements(lst):
+        return len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
