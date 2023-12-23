0d2d9af7867531034317a248d57134fcd10a5c96
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:19 +0000

    land stupid original thinkthrough drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
