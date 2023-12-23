d57609d73d547b684b9a3808c71a07840126cd99
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:07 +0000

    uponon themselves improve desperate cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
