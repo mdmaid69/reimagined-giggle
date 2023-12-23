e3b3ec33c7e7656fa5f56039d9c14823679b7226
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:01 +0000

    soil dish future hurt stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
