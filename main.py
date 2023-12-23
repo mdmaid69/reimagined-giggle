25ba430945cb9cfff17bfea22b379cc0c76490c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:17 +0000

    down little advice hope takeout

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
