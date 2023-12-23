e7d58ff3ba7e4462213ebc8ad727249aab40f89a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:36:54 +0000

    brother finish live accommodation consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
