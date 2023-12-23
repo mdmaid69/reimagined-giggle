baacca14ebd8cc8880ba7a1c980bff2483d8feed
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:30 +0000

    practice swallow Gg Ee squeeze

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
