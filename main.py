2ee336ea3c5b6290f11d42d266ce9c637a908f21
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:20:54 +0000

    clever must list file operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-for i in range(10): print(i)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_speed(distance, time):
+        return distance / time
