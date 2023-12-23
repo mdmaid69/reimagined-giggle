f8d78649de9483b5298534474bc067de2f21eddb
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:29:34 +0000

    stare fit fair party sticky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
