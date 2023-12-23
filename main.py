4185f8687fbb47c14d8693b3641e46ea602e4b07
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:13 +0000

    whether task connected sure modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_current_directory():
+        return os.getcwd()
