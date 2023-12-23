dafe5e0f6183ab4832f8b95cdea1c8bfce926106
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:54 +0000

    switch apologizeapologise true ready raw

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
