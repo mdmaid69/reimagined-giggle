be2d2bbcdc72ab47dfd8d3e1f985c9baeb5c381b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:19 +0000

    shake respect choice call central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
