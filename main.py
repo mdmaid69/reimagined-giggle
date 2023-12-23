09fd54afedc5b0533785ffdb1767c5c4edefe307
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:57:20 +0000

    fat education crazy load home

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
