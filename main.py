4ce0eb0f4e85ffcd3ab2e061794773432d572b81
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:28:10 +0000

    shock vision result protest matter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
