8fa26c8f9ae52dfc46fede8023817317cf77550b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:36:37 +0000

    handle dry inquire impressive strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_floor(x):
+        return math.floor(x)
