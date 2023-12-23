a87c3f4137968047671e0032b45c2518d5106142
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:31 +0000

    fit environment winter tongue form

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
