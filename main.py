fc0e6b251c5fa614933cafe5ae8d5857116bcf02
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:49:45 +0000

    mark equal mistake naturally chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_list(array):
+        return list(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
