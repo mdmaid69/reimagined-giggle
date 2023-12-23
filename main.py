5b25bbf51abdddbeb00df9e3726d2f7e637e0e63
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:33 +0000

    finally thing lower community fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
