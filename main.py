10499026ce047eb81e04d6547aaa02be8610454d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:47 +0000

    turnaway give claim control sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
