4df1bbae86f815897ab58575033f7157714a6081
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:44:34 +0000

    discussion dear loss all nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
