fefb44a606bed9ec24c5bf98fd388a8473decd7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:29 +0000

    keepup game though concentrate proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
