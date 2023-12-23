531171f3e4693e61aa0156dd3d86d1141f9df5e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:32 +0000

    track trade bill accidentally slightly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
