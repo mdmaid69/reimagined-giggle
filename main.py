cbcd07d7b4d404d89945495f00619bb4f08a9e01
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:51:55 +0000

    wide dance once abroad disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
