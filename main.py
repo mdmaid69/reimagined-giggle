63a07e83ef3450e9fcfab52c6f0d39ca8c6a2e52
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:35:27 +0000

    adjective search maintain national soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
