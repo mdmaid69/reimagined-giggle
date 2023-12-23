3b4374e4a6db70ad2c9f0dfceecc5e78b113112a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:45:36 +0000

    average collection trip discussion pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
