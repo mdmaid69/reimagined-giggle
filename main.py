b4112a85d2e2d2171b213b9f1e8bfe50ec7a96a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:11:50 +0000

    freedom practise clever terrible near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def remove_duplicates(lst):
+        return list(set(lst))
