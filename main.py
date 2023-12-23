cacf733719e5b1c293f77711756e67d3138edb43
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:22:16 +0000

    depend tooth space holdup positive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
