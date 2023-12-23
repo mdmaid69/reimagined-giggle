643023207d32c8ea03b8ef7ec9d0954c87fe1d14
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:55 +0000

    main hardly runaway meanwhile meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
