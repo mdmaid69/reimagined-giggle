f34b11ac5a08b0b1e409202fc63393ca512baa97
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:30:05 +0000

    count mysterious attitude breakout hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
