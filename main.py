ae5df132893638248ad8030b6c495ef0c7f40ed9
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:36:22 +0000

    geton advantage correct against please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_string(array):
+        return array.tostring()
