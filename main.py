753fcd347eedcc09610435d9576ed223696c0fcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:31 +0000

    act top whistle well your

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def clear_array(array):
-        array *= 0
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
