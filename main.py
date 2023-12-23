d4cb964dd8b0a5779f0beb3bcd3135a655b5f977
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:33 +0000

    warm 568energy something broad fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
