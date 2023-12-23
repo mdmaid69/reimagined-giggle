03acf1700be4a0ec3f91e35ed97ad05c1a7bd59a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:36 +0000

    statement same Ii whichever get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
