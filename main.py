aec48918e26d2f39c547ba6fb65ceb76d2be8578
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:42:16 +0000

    throw area surprised estimate produce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
