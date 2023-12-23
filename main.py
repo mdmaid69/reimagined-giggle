8f7ae3babed079b6264d99a9fbb16d7499674441
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:03 +0000

    accidental over breakup push object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
