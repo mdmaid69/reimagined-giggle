01f833e85a8830d8fc42d3c632c580880fcd460d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:29:40 +0000

    live section clear free smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def count_words(sentence):
-        return len(sentence.split())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
