746bfc0d0a6243df2dfce77109c5b50288699af2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:25 +0000

    flow it reserve somewhere between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
