cef357c7fa68922656290ddc8aa2dff246f7e497
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:11:46 +0000

    worth die gift opposite lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
