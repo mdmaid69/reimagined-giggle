51f494719f7008d4755ffbe7b87c0dab35d89031
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:32:44 +0000

    talkinto natural himself comeover solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-print(os.getcwd())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
