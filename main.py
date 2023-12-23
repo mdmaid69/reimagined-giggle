1980d7625b0527273b7737e310f53fb09f5944a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:58 +0000

    relative usual reach exciting perform

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
