d701b49ce1397cc76f86677dbd0a0a01b042cfa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:09 +0000

    hurt love situation classroom law

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
