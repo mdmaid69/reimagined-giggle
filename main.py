2694ef97ea6c6e9cfc96dd3c2605e70b8cd6888c
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:35:43 +0000

    difficulty after coffee 573entertainment phone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
