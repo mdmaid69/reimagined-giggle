b5a05bc5aef87db49e56ba16df6b8b22f89432a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:26 +0000

    next share person meaning nurse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
