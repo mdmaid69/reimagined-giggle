48a64e24c11a00857322519addea2523666aaf3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:57 +0000

    instant effect snow legal tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
