fbe70b2dbca603ab56586da0c108fd077177ed88
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:44:45 +0000

    inside date collect we hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_max(numbers):
+        return max(numbers)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
