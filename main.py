e60bafc18382d8701be17b173df24ccc53a19988
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:41 +0000

    rent shoulder attach passenger round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
