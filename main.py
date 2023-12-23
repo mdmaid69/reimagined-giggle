a701220ccc233307c3dcf014c8b2613aa9f10ca3
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:38 +0000

    anywhere experiment reduction when throw

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
