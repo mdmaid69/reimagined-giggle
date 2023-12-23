ebc1c576dd211d502ec465c4dbaf0f31a7fe436f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:38 +0000

    train once represent description vanish

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def multiply_numbers(x, y):
-        return x * y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
