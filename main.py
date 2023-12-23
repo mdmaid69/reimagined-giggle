ec659e4af5f5fd95befcffc48982a79aa8f00bf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:58:26 +0000

    your union plenty interfere welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def reverse_string(s):
+        return s[::-1]
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
