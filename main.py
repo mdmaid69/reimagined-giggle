d54987ff34582b089b41fff0de25a4d20af7b67c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:15:22 +0000

    forget reach shut that follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
