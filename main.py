f219b1b82f74ca17535c906344a13565e9b78ea8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:17 +0000

    light shape nor dress getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
