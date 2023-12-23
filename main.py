38f5010cb3255cf1b27c10a1cd284ef8aca6cce0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:58 +0000

    go measure near loss convince

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-print([x**2 for x in range(10)])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
