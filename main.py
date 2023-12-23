29ca7f65a480ba0862de2e0deea9c0c57fed23ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:08 +0000

    trust copy frightened tour that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
