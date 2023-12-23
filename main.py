2eb5feba23afa05667894a1165be53482bc1246a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:33:17 +0000

    mistake concentration pity together direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
