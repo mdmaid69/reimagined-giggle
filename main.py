9e1d807225251bd78821b07ca85d8e443d0dc7fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:02:40 +0000

    trick fall advice custom know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
