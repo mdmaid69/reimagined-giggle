efa462f0b94985b0c2d8bc499c186c1a780f2c42
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:46:21 +0000

    try meeting force warm serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
