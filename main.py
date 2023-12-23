79d3d8501af603ada048ca302c55bdd789d41241
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:14 +0000

    wind grade movement test manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
