76edf625752a775776a4664cb18b713e2caeb503
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:50 +0000

    various strict goon boat full

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
