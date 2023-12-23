421470548617b8b61e33cce8b585206d5fa8518b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:39 +0000

    software none organization life light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_unique_words(sentence):
-        return set(sentence.split())
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
