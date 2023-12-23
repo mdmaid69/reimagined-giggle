4df9c4b298d8c07a249a4f2fc1b72d16ae80a4bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:19:18 +0000

    march agree business full push

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
