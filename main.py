7225e3b18c66258993dd1f528bc48dd6d1a4fbfd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:10 +0000

    neither complete like colour makeinto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_factorial(n):
+def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
