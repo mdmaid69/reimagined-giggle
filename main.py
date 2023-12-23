d181ad37782e944c29934a8c5aff972653b57230
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:30 +0000

    small service find pass guide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
