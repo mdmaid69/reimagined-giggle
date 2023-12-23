87fbf36f06c99286e8ec41131cba1a3355432f70
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:12 +0000

    come turndown would pay escape

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def create_stack():
-        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
