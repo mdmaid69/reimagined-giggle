2969c7e79fde8dafe85658260192ba34b13687af
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:43:38 +0000

    sticky include shoulder bury full

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
