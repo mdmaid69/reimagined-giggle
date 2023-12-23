ccbc7df39e830dd97c034b8032fead7ff5268d3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:28 +0000

    patience takeback judge chin ours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import platform
-def get_os_info():
-        return platform.uname()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
