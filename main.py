b479768a43d791051e2d12b4e729fd9fa46ca60d
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:38:47 +0000

    order warning only love several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_set(array):
+        return set(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
