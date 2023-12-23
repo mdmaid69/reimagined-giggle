5e963e205d5f9ca13f672bfc7f96e586d771bf62
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:53 +0000

    within matter until term zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
