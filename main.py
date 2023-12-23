6a9f0ef5805a81bacf8b86d9d242c8f6615c533c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:14 +0000

    leg ashamed reach takeoff figure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def is_even(n):
+        return n % 2 == 0
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(re.match("h.*o", "hello world"))
