c5bb20ae62a8c7766c26a4694b96df35aa34e440
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:04:08 +0000

    born actually lead stupid deserve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
