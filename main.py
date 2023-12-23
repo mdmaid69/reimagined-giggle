066160c47b6b1490d22e0cab3d39a2353e55db39
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:04:01 +0000

    almost breakinto leave estimate spring

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
