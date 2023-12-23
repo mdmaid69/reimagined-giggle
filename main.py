2fda52d5e0eaa871d84f01d29ce719683e83a131
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:43 +0000

    ilovedicks bush born closely though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
