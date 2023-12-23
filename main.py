9a6eff7c9e17b0d89ec301050de132f68c79874e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:08:07 +0000

    act let bread habit average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
