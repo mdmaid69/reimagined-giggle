92b4af42c46dda94b6491b731b85c505f437dd62
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:15:56 +0000

    along off range prove turnoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
