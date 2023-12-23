b89ec90223f71a1e51508306aa5ab547f4bead83
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:17:51 +0000

    drop activity single simple traffic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
