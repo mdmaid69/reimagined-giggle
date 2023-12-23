90d2f1b60d41adde1cd6931e1beae3851028820a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:13 +0000

    pencil put market error suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
