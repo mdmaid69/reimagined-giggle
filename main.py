e2029c53c128819b11a8ca6ad8edc4660b218d27
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:17 +0000

    instead crisis safety declaration protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
