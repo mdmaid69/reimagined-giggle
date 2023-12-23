47f4ff8c6088f3f3c7336a1ea504a5e2806b8c44
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:05 +0000

    cheek past loan again release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
