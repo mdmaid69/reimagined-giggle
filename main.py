03a9a52b429e7ca56d5e1e332c062a1d74c62ac1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:19:54 +0000

    getback between material calloff store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
