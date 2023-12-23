7354cc4f9b05a6672a0c7a6c2e30140aa5855981
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:34 +0000

    empty familiar steer opposite mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
