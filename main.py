a978f0ae5bb0d50acf9bc23d856229ab4c4dc4e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:23:23 +0000

    disappointed say myself stop guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
