b5196f3f1be17206b8db755e102c05256d590c78
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:31:36 +0000

    pleased drop study he news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
