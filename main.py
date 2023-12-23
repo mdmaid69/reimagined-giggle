a688dfedf6b55756d0678bb61e8efb947b6df7c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:41 +0000

    properly lower bath ready sport

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
