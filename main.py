b81f31276e72c8aa91305bf695117893d6cf85cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:00:33 +0000

    behind sorry care borrow hair

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
