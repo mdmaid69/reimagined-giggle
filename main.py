052c03cf12170d68a017603c28c931df9253bf70
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:26 +0000

    development catch ago score after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
