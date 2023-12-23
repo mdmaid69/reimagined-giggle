d67e8e41301ea60a0d5f3f2adf3037de7bc31e93
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:58 +0000

    replace television complain slight breathe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
