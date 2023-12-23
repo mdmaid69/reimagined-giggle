1a82330569a61125a48bd0b85f5a88e6a37d1aa8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:29:26 +0000

    powerful disgusting birthday people let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_density(mass, volume):
+        return mass / volume
