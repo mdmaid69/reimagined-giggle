57fdd5561ecac6283c312cf050b4bcb3bde8c449
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:45 +0000

    crowd guide explain peace angry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
