f5700b8ee8db113fb0d3231119087a62d77c513b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:10 +0000

    suckme dick shout tap lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
