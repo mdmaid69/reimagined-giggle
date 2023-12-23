3d733116b378d25cd9323eb9d357b6c068040083
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:10 +0000

    suckme pool fall home bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
