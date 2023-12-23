d53d068df4f604a7e661e1d099981d04c9bae20a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:52 +0000

    leave whoever later goout speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
