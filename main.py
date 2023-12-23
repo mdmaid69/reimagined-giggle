f3b9d4fb4c0a8e9843cf8c991062295c9c4834b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:10 +0000

    act foreign frame extra continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
