1b5063f41a0b566b71485a4a9688ad56f1b8b15e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:36:29 +0000

    embarrassing false crime square soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
