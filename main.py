26c921017a6dda686b034f75adbef2c0f58072f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:35 +0000

    town attempt sense contact change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
