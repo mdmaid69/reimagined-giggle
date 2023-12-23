3a46712e2daf9b802476c4d483fa832b1a2776a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:29:15 +0000

    build worth rub available brief

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
