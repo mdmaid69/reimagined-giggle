e903a566b3a16e500bb06b8ec4ffc78fb358fc6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:46:35 +0000

    very with anywhere force form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
   def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
