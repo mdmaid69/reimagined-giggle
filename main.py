9999d6f0c11ccad59570eb5a7f0ff868251e51b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:51:39 +0000

    quantity pain earth honest soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
