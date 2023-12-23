8c42ff3680c10842b4b69691f82f912aa48a0adf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:18:58 +0000

    anywhere display chief become bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
