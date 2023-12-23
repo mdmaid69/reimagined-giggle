f149a8e525da6fb1c436de70658a9487dacbc1aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:13 +0000

    serve payment patient other issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def reverse_string(s):
+        return s[::-1]
