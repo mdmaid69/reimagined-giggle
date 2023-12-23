d4d6caa32086080edbafe007a9b15e9e9fdca2aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:21:31 +0000

    forget damage reply book exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def count_words(sentence):
+        return len(sentence.split())
