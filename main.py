087f6066a1787b6cf3b75d14c61edef0e9e95efb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:37:58 +0000

    gentle give behaviour takeaway defeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
