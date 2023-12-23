b6064cd7aee758b039afe8126355e789752cd903
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:00 +0000

    comeout tone mystery slowly usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
