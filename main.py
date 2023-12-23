be23a7d12f92ca52906d4678162ca2e4259df8dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:53:23 +0000

    weigh result Dd turnaway region

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import sys
+def print_python_version():
+        return sys.version
