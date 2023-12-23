6f93a405e64989fdf221776dd6bcc87719901a3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:30 +0000

    impression stretch feel confident apparently

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_max(lst):
+        return max(lst)
