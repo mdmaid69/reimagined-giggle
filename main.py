01cb61c957b42130dbf77d825e8b89ab42db0004
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:14:11 +0000

    aim although lean goback(to) past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
