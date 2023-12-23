1783ec46302f828a38824490b827b76a32f6ccee
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:17 +0000

    eat conversation book father answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
