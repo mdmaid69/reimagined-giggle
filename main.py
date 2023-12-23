9895c8a3c73767a524b166327a638b66caf01189
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:45 +0000

    result ability cost crazy whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
