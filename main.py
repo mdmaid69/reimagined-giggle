c980fa9f903078b822fe36aeaefa5f61e041fad4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:34 +0000

    exchange beside approve catch cross

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
