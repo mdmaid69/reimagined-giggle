8579a72e0fd0de5f412c9c7a3c9102e0df9a7827
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:01:58 +0000

    closely deliver straight seriously nurse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-print("Hello, world!")
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
