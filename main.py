8164e577e29ba7d09b30a30773c17646863629bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:19 +0000

    mistake all fortunately dress become

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
