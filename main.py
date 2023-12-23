1875bce8f075e4c24ae65cf730677351a52a5e3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:24 +0000

    special turnoff not glass effect

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
