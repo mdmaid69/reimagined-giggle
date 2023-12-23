eaab3937c24c4da7f6a5b586707ea618dd551eda
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:48:21 +0000

    dry watch begin pleasant invite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
