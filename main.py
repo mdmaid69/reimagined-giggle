d140a8a4114e9aedbe06633adf1294f24d0ef49e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:16:32 +0000

    float morning bank fix scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
