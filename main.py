b2b195c4af01a4f5c1dfce40c30ce2a77fbe6a92
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:09:43 +0000

    comeback favourite worth lift characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
