a871277cd7f4699ee96a81e4c99cc47fca1e5641
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:23:30 +0000

    store than offence runover fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
