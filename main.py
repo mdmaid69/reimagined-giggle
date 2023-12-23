704fcf93696f332c9c522b3fc68acc3a1362de7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:37 +0000

    between male half estimate ought

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
