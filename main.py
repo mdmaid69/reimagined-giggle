3a1082a8b3d1b486634169b6bc9e46097487d449
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:43 +0000

    perfect contact accept talk keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
