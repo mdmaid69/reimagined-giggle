fcee2cd962a2b56c1796b0fdb0c4c07f81167118
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:52 +0000

    realize moment scrape ignore talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
