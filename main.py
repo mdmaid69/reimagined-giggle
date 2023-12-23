8fe75122c2ecd9f541b50fdcd4d3b09c7c4f6574
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:42 +0000

    marry public closely upset message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sys
+def exit_program():
+        sys.exit()
