07a299d84e8bae0c119b176acce2356b28e6cb5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:34 +0000

    corner we flight wise should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_today_date():
+        return datetime.date.today()
