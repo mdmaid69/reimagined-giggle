750b27854f5ca98f844ddbc4dadaad6bdcf24f8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:48:32 +0000

    environment excuse train once forever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
