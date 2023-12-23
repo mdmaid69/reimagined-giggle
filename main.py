c7f8833fe714602af027706711ed09ba04d7f934
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:26 +0000

    back obvious visit careful ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
