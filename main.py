b24e495c063f479b87a26f3dcc298e9602182c34
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:35 +0000

    throughout turnon active defeat obvious

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
