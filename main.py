4dd066b38bf313636d3a58d63525b37695e86462
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:01 +0000

    minute move bringback conclusion negative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
