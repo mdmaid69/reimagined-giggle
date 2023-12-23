fdbb152c0f767949d6c9f5ca8e738813c93d902f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:38 +0000

    square badly wrong splash matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
