11f3b5582d7e0a05710d484b5fcc244dbf66e08f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:26 +0000

    ocean position height improvement order

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def sort_numbers(numbers):
+        return sorted(numbers)
