b6149a1033412d985d390fe10dc1cf532b3e41da
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:42 +0000

    represent recovery interrupt embarrassing star

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
