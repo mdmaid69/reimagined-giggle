afac7a12b682f2e3d6a838f4a2cb76673e12616c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:50 +0000

    explanation interfere law many criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
