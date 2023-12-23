85494641030dfa79913a992df32916adcffc3f5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:45 +0000

    tired contain beyond possibility home

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
