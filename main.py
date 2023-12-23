21eef4fa949c17f795d1409e8c743defa99baaa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:53:24 +0000

    squash service ordinary argue area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
