6222425c67048ecb4ab5d004f94399ac7ef6fa13
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:43:30 +0000

    care whenever block disturbing bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
