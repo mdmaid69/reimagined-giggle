ceef9564be37c40025518916f6875551981814e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:44 +0000

    tap bringforward tooth rare comfortable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_min(lst):
-        return min(lst)
+import sys
+def print_python_version():
+        return sys.version
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
