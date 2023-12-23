f875db627d9dd0b81209045a91d97d4bf4fbd9ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:25 +0000

    correct thumb justice break dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+import re
+print(re.match("h.*o", "hello world"))
