e1386575c302ed14fc557720141a172ee167883f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:39 +0000

    bottom connected fall nowhere bottom

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
