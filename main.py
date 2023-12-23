06ea77166d0efc13ad7d8224485effc16b16e71e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:35:49 +0000

    talk party few duty dangerous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_list(array):
-        return list(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
