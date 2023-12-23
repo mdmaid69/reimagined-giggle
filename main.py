7ab9f9d7b9ae7853774a0a4a2337955893fe7630
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:16:36 +0000

    he work mystery prevent life

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
