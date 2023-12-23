c690267e877466d9a8c7e7ffcf129d73005f217a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:34 +0000

    gradually century land hole course

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
