c0791341a6d890f238a6192dd9a0468a9bb0f155
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:53:36 +0000

    worry deep accidental destroy her

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
