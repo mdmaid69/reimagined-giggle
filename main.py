6397bbc2a84a3ef1f6cad483aa032bafa86cc98a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:54 +0000

    new walk talkback demonstrate plate

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
