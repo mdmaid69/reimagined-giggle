ea06803f26b2f114f09ecb59344d6d2e8e07d197
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:11:46 +0000

    limit regular pure unkind skill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
