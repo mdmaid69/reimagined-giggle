f8cba9163a5cc21de1288f5a3ce0570f800b49c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:26 +0000

    throw whoever prize maybe minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
