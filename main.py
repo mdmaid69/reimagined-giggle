1467ac7726542a91769290291aa9b774526f22a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:49 +0000

    keepup nothing authority among hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+x = 10
+y = 20
+print("Sum:", x + y)
