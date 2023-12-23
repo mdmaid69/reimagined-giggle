0fc7e008a8f1d826acc363de49dbfe7011833a37
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:41:35 +0000

    part aim first mark guard

--- main.py
+++ main.py
@@ -3,5 +3,6 @@
           if n % i == 0:
         return False
         return True
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
