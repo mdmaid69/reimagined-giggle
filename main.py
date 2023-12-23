377f25de239e5b4114b172af49d1c74ff770766a
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:33 +0000

    suck Pp onto true night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
