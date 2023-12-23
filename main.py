5b6051cdcef0e7e9d6c48505c73e2d48cc3ccae3
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:34:44 +0000

    normal fight hurry complicated judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
