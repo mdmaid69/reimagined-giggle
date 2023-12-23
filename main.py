5356623a590f98b41cae519b0e7dbf54d74b3d33
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:36 +0000

    mark cruel risk warm slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
