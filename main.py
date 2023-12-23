75477c0e57992d682b32094f65ffff49f757042e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:58:44 +0000

    official public believe but yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
