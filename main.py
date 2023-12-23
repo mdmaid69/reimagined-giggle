40497529a1c11f41ba031b1c3cafd433a7104f72
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:46 +0000

    cry rest strike cost basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_priority_queue():
+        return collections.deque()
