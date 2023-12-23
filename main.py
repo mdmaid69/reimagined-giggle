e348ec2de0c40057a44103862734418246035e53
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:27:37 +0000

    goaway best famous relief hard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
