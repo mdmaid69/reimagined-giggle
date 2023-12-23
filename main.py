4dbb97218c2b7aefe1ccb2f555ca5e5a956aabe9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:53:27 +0000

    definite sure round 568energy noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
