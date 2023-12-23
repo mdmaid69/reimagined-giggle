70f1a4d7abfab0e6bf206afc2ee6093d068a65fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:33:24 +0000

    trousers lucky receive refer direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def multiply_numbers(x, y):
+        return x * y
