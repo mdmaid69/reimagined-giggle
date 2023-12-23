7ebc6606922fedc400f69d6b74e9714957adc47a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:50 +0000

    rich grand behind success apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
