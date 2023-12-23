b888a423a9d198accaf1ebe6d7b1e617b558bf9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:49:43 +0000

    suit attempt tense run comeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def sort_list(lst):
+        return sorted(lst)
+def square_number(x):
+        return x**2
