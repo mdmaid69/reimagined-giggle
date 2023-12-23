166609a487d6e3c8f3c35e890343e568f9707c23
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:10 +0000

    star speech fork copy house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
