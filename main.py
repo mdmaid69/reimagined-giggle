7d0ecde8f195fba369036aa407f1cdc272fba749
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:52 +0000

    dream will noone calmly hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import sys
+print(sys.version)
