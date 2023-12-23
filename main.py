bc97ecbdbfa82d013652ebbf3e8601b1336f493b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:51 +0000

    keepfrom niggaacademy speak earn search

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def create_priority_queue():
-        return collections.deque()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
