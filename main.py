20032e93ee94850f22e1cd180aa3ee3ddba4e559
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:17:21 +0000

    bar around length himself arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
