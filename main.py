110aed32f72551ae02c73626cff4bfbc169c71e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:32:28 +0000

    declare division more language mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
