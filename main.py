845443268c734e4def27cc63efa103558d992582
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:52:14 +0000

    inquire first bag cope so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
