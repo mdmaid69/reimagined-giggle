39bd9082ff5e794095719dbd1193dbcdfd10cd6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:38 +0000

    anybody relief argument like copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
