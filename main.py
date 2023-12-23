0f34f89c4d340b473912ef49b2bae8b03553ee16
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:50 +0000

    claim line large single straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
