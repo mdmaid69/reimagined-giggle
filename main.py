2f249f9cfac8e497023c937ae3cdc4494b0ca2d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:01:04 +0000

    taste anywhere room gift many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
