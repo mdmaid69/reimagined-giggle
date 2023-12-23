2bd99ba1a16d310b204ffcd58b81ec93d5b1e41a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:53:48 +0000

    coat support holdon goup should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_current_directory():
+        return os.getcwd()
