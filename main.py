2b29f84720652e7924b93d5c8854a10f71b19c24
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:06:05 +0000

    pot more tone within tip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+print(os.getcwd())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
