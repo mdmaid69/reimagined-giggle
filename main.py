2124ba887ccf195911d39f2fd9c853c7be44b899
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:21:56 +0000

    judge below always concerned wing

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
