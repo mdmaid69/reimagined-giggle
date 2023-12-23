c1dca443bca93701a42fedfa11d2874a6a3aa1ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:46:42 +0000

    suffer watch system sore burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
