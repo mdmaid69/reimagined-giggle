0c5965aa52cdfb26532c4890f487467f5487a450
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:38:22 +0000

    pure goup into turnaway switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
