a359aac19894be33090aa01bbb360da0cf1ef178
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:09:43 +0000

    anybody cross unfortunate explain our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
