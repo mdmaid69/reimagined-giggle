9af5e937d8a2a14a16298c35777783436b3d227c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:39 +0000

    deliberately mean have radio runinto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
