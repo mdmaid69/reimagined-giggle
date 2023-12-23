34389a40ea03a8afa41db78ddfb8a84bc788c82b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:16:27 +0000

    bin trousers divide sign welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
