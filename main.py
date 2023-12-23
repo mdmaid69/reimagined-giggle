af9e8a9ee236e29aa4fa4cc2e6ad52d672f504af
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:11:37 +0000

    566end payment tragedy proof ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
