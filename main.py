3f373187bc44332b91cedc7205e0e016cfd1c314
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:15 +0000

    top clever relation dare print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
