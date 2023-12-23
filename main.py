3168a748709d8e185e29a11a870a70b3e14082f3
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:20:30 +0000

    slow blue suggest approve lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
