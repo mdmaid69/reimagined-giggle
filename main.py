2d027ee7f101c4781abb2d1ca5e22905daf59d2e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:28:48 +0000

    calmly receive achievement receive manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
