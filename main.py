328bbcba6f30bb0efaf5cff843d52f110758852c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:57 +0000

    that use point prepare special

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
