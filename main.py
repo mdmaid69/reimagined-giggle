c0797bd1118d468f6110a613eaed17b2d09a4dc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:32 +0000

    earth even 565end note leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
