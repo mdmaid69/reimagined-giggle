69304209f09958aae13a1515326fc80dd20c5f39
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:03:50 +0000

    gas safe shock tense nothing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
