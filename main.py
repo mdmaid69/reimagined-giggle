867cd00b467513238aabfbdaa4b73084fa4737b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:09:52 +0000

    explanation throw tidy push nervous

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
