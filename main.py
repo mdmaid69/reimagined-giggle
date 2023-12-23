2dde20a5a895f511bb652f01a69c551bd6f6e17b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:27:40 +0000

    law experience natural honest uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
