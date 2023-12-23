42a2a94a95b03b5c08bb7dc7a5a15b5278280f55
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:11 +0000

    injury second nowhere square although

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
