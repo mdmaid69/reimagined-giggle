ffdf1e7386b25bba8e5a7a638a26da7e01f4a93c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:29 +0000

    runover interrupt connected action company

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
