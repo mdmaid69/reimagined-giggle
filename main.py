c9059f550b837c45c397b0585320ec2311b8ec60
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:04 +0000

    bath tomorrow basically himself grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
