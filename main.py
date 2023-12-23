01e998f05286a598b6fa6052e63fd81d9fa071e7
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:22:08 +0000

    base space unlucky thankyou spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
