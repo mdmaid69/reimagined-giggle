c90d0c2a2315402d1557d7c666c83698d08d0490
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:14 +0000

    worry he exit ability half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
