d6e0a92ffdce6a72a9c54605a757d07416284589
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:47 +0000

    one within noone silent agreement

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
