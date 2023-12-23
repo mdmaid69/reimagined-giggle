5a6c0f37b66c5a593e367eb3730deaa15f1b797f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:55:42 +0000

    schedule turnover opponent hit comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
