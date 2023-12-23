73c6f092a7afc73f6b19cb890899d481c7b87093
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:10:19 +0000

    lift question bend thankyou far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
