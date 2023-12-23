d80efc48de44952b2661684581ba04e67b46fe66
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:39:17 +0000

    large light treat round bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
