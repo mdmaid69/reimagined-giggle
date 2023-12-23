f73afdb8626264d6a41f1cc5d0412cd43ad39672
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:22 +0000

    somebody region giveback side sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
