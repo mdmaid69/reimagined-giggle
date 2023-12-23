0bfc6f078490e434bdfd1165e328db0d28ea04f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:06 +0000

    deliver record emergency deep serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
