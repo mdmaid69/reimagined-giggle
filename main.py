85bedb7d615779cdb512b2189fc33c1db2964d3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:37 +0000

    worst evidence thorough space operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def append_to_array(array, item):
+        array.append(item)
