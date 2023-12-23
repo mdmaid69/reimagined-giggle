acaeb1c2e9600912bf8f00e26cdb624d82a4acbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:48 +0000

    criticism rich Ss gooff outside

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def count_elements(lst):
+        return len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
