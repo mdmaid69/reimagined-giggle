4f78aa0027d5d463c361530850f2fdf5f9403840
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:34:51 +0000

    up cough content bunch politics

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
