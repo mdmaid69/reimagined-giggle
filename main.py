6347b4e70415f500979e38b9c12a55b8ef1e122e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:29:00 +0000

    sometimes boil uniform good skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_set(array):
-        return set(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_work(force, distance):
+        return force * distance
