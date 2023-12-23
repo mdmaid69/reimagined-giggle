72ba665e28f27cdf55aea959901b024b28fa9237
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:26:12 +0000

    lookaround wet positive plastic space

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
