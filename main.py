6c8cab7f8d59fc6b5a6cd975818b6532d0794207
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:45 +0000

    discuss apart measure intend married

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
