37b162b1f0d85ae5df48f9d90a396569a2d9d4ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:11:06 +0000

    fall sea thorough that pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def add_numbers(x, y):
-        return x + y
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
