dfe42d665c162d811345c28e57e51d6a82975973
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:22:41 +0000

    crowd last surface tend hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
