51a45bb6a45627a22d631d319b5cf2e2d24bd96b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:15:08 +0000

    dangerous emotion welcome its sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
