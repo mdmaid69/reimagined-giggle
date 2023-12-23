a1a229af6492445efe128a70ec41759f58538785
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:30 +0000

    genuine freedom uniform boring opportunity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def count_elements(lst):
-        return len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import datetime
+def get_today_date():
+        return datetime.date.today()
