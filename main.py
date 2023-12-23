c9668170740128dee3c96a969f671b7c57468129
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:59 +0000

    getout exit absolutely correction speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
