3c59f884cb94d83444c004fa2ca411fcd77c870e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:23 +0000

    health gentle favourite towards both

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def convert_to_hex(n):
+        return hex(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
