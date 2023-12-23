dfbda85eed66cc6a15b7b95475783bcdef175378
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:09:24 +0000

    underneath difficult visit quiet provide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
