8619d93fae8dc46b10d83226192730be1297ffc9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:04:41 +0000

    family let wet add lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
