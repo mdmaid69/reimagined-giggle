c1f227bc727fa054c0b32e966c83c377786d4cf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:23:33 +0000

    here aim achievement anything wild

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
