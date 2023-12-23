78922b9eba5d8a256cf83f7baa7a03460d344cfb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:37 +0000

    shame ball taste forward below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
