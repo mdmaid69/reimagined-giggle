6c12d815c5f203214eaf055f1121077a27d29a20
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:25:53 +0000

    easy connection ring sink long

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def create_user_string():
+        return collections.UserString()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
