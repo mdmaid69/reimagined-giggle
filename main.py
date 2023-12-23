570a216f10c4026a5d1e19666300552baa13c808
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:39 +0000

    base under traffic sale club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
