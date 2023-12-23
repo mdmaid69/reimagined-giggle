3cc2fc3009d3328441bbeeb17749e70bfb47deca
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:45:33 +0000

    someone lonely shade themselves violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
