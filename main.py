dac773e4232a9a9ce15abee8786332bfdfae78f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:18 +0000

    conclusion why later aim formal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_set(array):
+        return set(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
