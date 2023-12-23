4f3b5579a785d5ae74efbdf5c23b50bd0df4eef8
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:43:51 +0000

    improve patience curious pretty study

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
