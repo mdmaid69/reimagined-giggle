e72498843390a24395f121e2e376a64cb0ff4a7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:07 +0000

    holdout appeal final introduce arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+import numpy as np
+print(np.array([1, 2, 3]))
