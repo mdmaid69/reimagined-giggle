43ef4a18114a56b5d022b4248a4fc9520c617985
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:55 +0000

    nose upstairs plenty behind story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
