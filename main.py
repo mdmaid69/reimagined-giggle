f38595996736a4eee1726dc90448e5023a70c69c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:22 +0000

    previous appearance wind repeat birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
