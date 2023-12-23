14d4919013edbc173bcc3f8626808eb9ce1f734e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:31:13 +0000

    home dead powerful early pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
