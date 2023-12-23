8b9216e4456e97959df1f211fa134076b08669cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:08 +0000

    manner sink task close taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
