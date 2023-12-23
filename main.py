4147e683d4eaada0fa3b4aee4267441a7c4e2988
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:43:35 +0000

    design fail sharp chin name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
