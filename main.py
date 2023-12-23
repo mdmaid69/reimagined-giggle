cd88cbe5f06dbb53683ce0f0b1b678d9c7906f65
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:30:27 +0000

    murder accidental spray finish married

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
