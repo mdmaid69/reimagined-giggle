2dc786c860f7f6db3d2a19dad86ea778ff7c3ca1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:41 +0000

    healthy circle face closely process

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
