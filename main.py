003127c82763a873004ab508fcb0f562701c0d0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:40 +0000

    bag stamp satisfactory accept free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
