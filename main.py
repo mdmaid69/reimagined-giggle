0edf5917dcce8643ce062f7b078ee8f3ed4eff77
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:35 +0000

    against healthy waste calm temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
