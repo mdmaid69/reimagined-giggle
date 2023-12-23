79fe71901819b54a74199a621c1048d809f9e314
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:34 +0000

    left unlikely movement present sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def remove_directory(path):
+        os.rmdir(path)
