7198e4df7e8df3573ae979840ca89b18cd3579dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:20 +0000

    bound acceptable become refer do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
