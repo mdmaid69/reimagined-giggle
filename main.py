6bd40043d919a5c717752e7bda0f9cea74329731
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:19:48 +0000

    freedom copy hurry defeat difference

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
