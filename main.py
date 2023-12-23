0508c6314e8c57448ec04a5b0e0990d643048dcc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:44:43 +0000

    forever complete act error achieve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
