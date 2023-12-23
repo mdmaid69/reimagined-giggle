b8f9a4ad74ea10f3b0810dd32fc8c383aafd6b17
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:01:28 +0000

    rare never translate easy born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
