fada69d5330de026b3bb2cdabb7d56035fec9aa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:06:18 +0000

    which minimum key test bury

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def subtract_numbers(x, y):
-        return x - y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
