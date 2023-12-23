d001ecaccca0d0d8ac160149344d56adbdb3a24e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:21:15 +0000

    sad before blind black steep

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import logging
+def log_message(message):
+        logging.info(message)
