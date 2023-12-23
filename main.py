6c6c1550c44a3d13312d81deab9aef2dd72586c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:36 +0000

    acceptable listen file hold custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
