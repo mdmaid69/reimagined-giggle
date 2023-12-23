2313631b695b042b430ce64644eb3da8eda53ff4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:56:13 +0000

    impressed hate claim judge before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
