1795495ae47ac3b975a5c3235aca8f55a11cd96e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:32 +0000

    female hit them uncomfortable whether

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
