6a60f8f39d77f2f6fc12fbf7b88ba8610fa15635
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:43 +0000

    skin emergency debt fair he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
