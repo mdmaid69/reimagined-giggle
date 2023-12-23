6b105078512649c7d90107a6057a8d4fdba43d58
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:36:44 +0000

    objection entrance result substance party

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
