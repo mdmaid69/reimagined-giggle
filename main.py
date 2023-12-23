0dbc4b52443532a6e183274ecd20908ca68f377d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:06:01 +0000

    Ff before information blame train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_current_time():
+        return time.time()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
