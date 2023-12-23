c996d87f804319fbb116ac8e5bdd856ad8ab13f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:39:59 +0000

    more tomorrow quit smile fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
