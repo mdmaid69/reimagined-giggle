a6ebacacbe3c59cf9fe2783679e062521b955988
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:12:36 +0000

    speed exploration move disappointed meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
