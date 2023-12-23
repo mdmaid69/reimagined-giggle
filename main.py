6dbab873a8921dee2514d94c0b419f0a3c53795d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:42:48 +0000

    either my word flood compete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
