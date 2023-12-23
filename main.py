657f5638d41c97078328837ec093805e44329f48
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:25:19 +0000

    drive deliberate nowhere appearance Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
