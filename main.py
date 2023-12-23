3b7dd14e0b795173ba020e84cd1d988759ff1f48
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:15:58 +0000

    that outside face flat part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
