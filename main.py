637e2796f31bdf40f4c173d3c8c2b347e5f7ecc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:27 +0000

    him butter follow nothing furniture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
