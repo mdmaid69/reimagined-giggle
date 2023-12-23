8d7fc0fe2121c21e2ba4cb2e6e0f53fd58dea0e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:03:06 +0000

    situation hardly tear stamp single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
