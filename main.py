be66bd6203f199da3ba61757452a37b6a94c6e5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:17 +0000

    typical fight new badly board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
