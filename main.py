fd36633efe998437f9ba13713a1da12b0fa95a58
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:48:07 +0000

    care whatever not conversation movement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
