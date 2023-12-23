770e774fb32ef3ce8fa09a07af2a1175052d4a52
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:04:47 +0000

    far somehow steer care raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
