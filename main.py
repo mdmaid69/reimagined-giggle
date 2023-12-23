f47e46ed887a53d6ff896e8cfe4b8081ad284d41
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:27 +0000

    range personal benefit occur name

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
