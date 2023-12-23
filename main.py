5d167ab5dd47a5434ecc2222b62635cd3c6ad06e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:48:28 +0000

    not broken pinch use likely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def reverse_list(lst):
+        return lst[::-1]
