7630d6f0db40846f83545c4e38071c9a1e6689b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:34:03 +0000

    vote favourite recover pressure mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
