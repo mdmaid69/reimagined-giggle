3d626aaecc5b8d6499c06a337bc227ec355581c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:03 +0000

    address bite black case naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def delete_file(file_name):
+        os.remove(file_name)
