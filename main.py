80dc81c074036a3a0495c41703c2d62dd022b489
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:35:53 +0000

    no band still share start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_pressure(force, area):
+        return force / area
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
