a572ae60c50139bcba8f50055b0c880f3e636208
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:10 +0000

    rid partner occasion difficult treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
