5d8dc1d8de368f68de131f790b3a64103a1c1332
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:51:10 +0000

    legal government straight charge scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
