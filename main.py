8ccf54baa376a4191696064bd6bdff871b19a613
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:36 +0000

    company front burst far news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
