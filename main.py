df512cf4ecffe409cb730c5e948384702be20232
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:27 +0000

    passage dark weekend always boil

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
