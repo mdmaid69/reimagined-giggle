2142393c4f36684798fceedba988df2997a0d9e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:56 +0000

    instrument perform play experiment goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
