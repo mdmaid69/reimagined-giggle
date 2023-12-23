ed7d89f9e9ae2aaaa758b91ad249fc5385d0adc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:54:31 +0000

    ready welcome public most effect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
