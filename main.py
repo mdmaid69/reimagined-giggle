12224608ba809936c4d99c1728370a1d7cb4cc90
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:43 +0000

    exam mess want welcome moon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
