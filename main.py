a23b45238f81cb64a5f2c07ab85ab51c0dd15200
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:06 +0000

    beside glad who chief please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def get_current_time():
+        return time.time()
+import os
+print(os.getcwd())
