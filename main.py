a567ac6a2a566fcb7e53d5dd0df501d03acc2dfc
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:31:07 +0000

    notice tip exam spoil anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
