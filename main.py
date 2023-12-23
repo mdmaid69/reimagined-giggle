8f8f68e20fb277dff2488d934caafce58f79016f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:02 +0000

    dull roof hear crash mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
