2c09c8c5b4af93773cf7f15260b0577f7a091e81
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:55:49 +0000

    floor low behind behind by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
