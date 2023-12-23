f776b2ff279bdd0b2ebdb23988c3824f3c0a965e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:36:12 +0000

    aware beyond understand sail inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
