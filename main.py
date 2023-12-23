04f9b28f530014c8ee3f998db07bef2c477e958f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:09:37 +0000

    write sleep act rank this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def clear_array(array):
+        array *= 0
+def find_unique_words(sentence):
+        return set(sentence.split())
