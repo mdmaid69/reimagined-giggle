216e9a6d029f0d97205f6ee7767b02ca4fa8096e
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:03 +0000

    live wander write content 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
