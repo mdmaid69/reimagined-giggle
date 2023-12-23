5aa1fa7d6e8e182ae7f9400ba76b3ad7d28a16a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:53:44 +0000

    getoff getoff church rob same

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
