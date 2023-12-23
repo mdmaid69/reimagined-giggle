2c3cacdd698a87da76fbf2ca8823d3c59ee299e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:39:57 +0000

    pity further fence ago fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
