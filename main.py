1d9640694458511d50d1cdfc57a011dcc66d45e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:43 +0000

    forgive personal towards guess bother

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def convert_to_binary(n):
-        return bin(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import tensorflow as tf
+print(tf.__version__)
