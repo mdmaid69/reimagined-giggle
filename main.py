f4444667f2762b57ed8d3ab5490afb5b11f6946a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:35 +0000

    limit produce black talk copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
