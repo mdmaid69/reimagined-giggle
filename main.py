5af483152f315fd71e67d93e6621b1f5cb21a72d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:53:51 +0000

    twist something punish whichever quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
