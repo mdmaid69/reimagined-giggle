9a700d91e3782e3245b995462fa19c76c9fc70c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:31:11 +0000

    waste quarter reaction speech kick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
