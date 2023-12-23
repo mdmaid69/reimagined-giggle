b9e8a0d569b899bf134df54d4c70a33709e47e2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:56 +0000

    light everybody everything course 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
