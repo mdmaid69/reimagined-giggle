d97dfca92482cfcb988728b977d076ddea1d79c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:38 +0000

    near rely cut repeat unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
