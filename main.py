e358beae9fe728044a428c8103d16b031e30b5b7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:37:24 +0000

    giveout confidence beside copy advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
