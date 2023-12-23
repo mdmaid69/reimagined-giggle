0ee125cb0790f2b3d8e7a515fc60512cc4ce3abb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:33 +0000

    slide cap better mad minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def get_current_working_directory():
+        return os.getcwd()
