eb75021767358ac5283ce51e342a86b67ae45154
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:38:33 +0000

    breakdown bang lucky well year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
