0f3a890fa7e2c2d0c722ce62e54237c2c941ef3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:17:07 +0000

    provide stop duty safe comedown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
