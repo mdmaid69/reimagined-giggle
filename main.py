4bd268830b3e525d8fb66e4979904b07037527a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:28:27 +0000

    debt shout whoever spring account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
