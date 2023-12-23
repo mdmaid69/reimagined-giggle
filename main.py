8f1c824048e82326776b1db6b93a914cd6b44fea
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:02:00 +0000

    one valuable dull bet sensible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
