45fa898bdf824dcc6a7212a10b7e52de15899af2
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:05:03 +0000

    design open air mind bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
