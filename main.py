31aca9beb06673d5171d0f6476f3eb7714f03200
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:54:45 +0000

    separate bringdown regret poor chief

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
