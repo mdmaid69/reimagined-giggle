266f00391ffbd4b5dd3a449fd54d79ce7879742f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:02 +0000

    listen blue shoulder traffic base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
