462b92649978ba411db1bfdb32206baa51413fcb
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:02:26 +0000

    tragedy exchange school smart load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
