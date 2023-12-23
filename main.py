22e123ce5cfc8af7dd2bf29619e91e9a2da350f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:19 +0000

    out peace goaway slice content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
