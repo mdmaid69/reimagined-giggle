caa49f83568bc574b31c21dd5a87d34feb96c074
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:07 +0000

    regard discuss use week table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-print(sum(range(10)))
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
