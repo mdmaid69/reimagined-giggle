75781f7a22266460f5eb0d23307546e56d8e70d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:01:04 +0000

    active makeinto game cry interrupt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+for i in range(10): print(i)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
