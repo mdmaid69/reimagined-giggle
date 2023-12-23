c99d04d89d583592195d2764ca294a9534fd9939
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:45:47 +0000

    convenient wind now equal necessary

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-print(sys.version)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
