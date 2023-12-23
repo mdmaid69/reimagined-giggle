291114e2ed8f1c99fee7a1e93dc569139cba376f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:20:24 +0000

    ability brother moment frighten Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
