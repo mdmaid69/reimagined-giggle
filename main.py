a6afe6dda1c4cf58235ae4d350de75c21ba23317
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:27:51 +0000

    point union single loan pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import re
+print(re.match("h.*o", "hello world"))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
