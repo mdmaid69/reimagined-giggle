9cab7161a7b5da16c0946e068f270f88e7758b2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:03:40 +0000

    relief various certain getup far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
