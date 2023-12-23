bdce9370a2420aee870bb362d4550a931769a96d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:30:16 +0000

    object error post advanced either

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_work(force, distance):
+        return force * distance
