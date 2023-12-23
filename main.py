44e018870a74d5987e149c9b0bec2695bf54b56d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:52 +0000

    follow upstairs under move edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
