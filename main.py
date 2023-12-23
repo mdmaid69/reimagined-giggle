f82b8598e049b973f8db6f2a8a19ecab7b11b1b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:58 +0000

    badly side right regional avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
