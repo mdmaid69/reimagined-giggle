44b388bbdd1654b08b9f309fe53500366428db0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:12 +0000

    chat sentence single unless base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
