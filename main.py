2aa7e8f9babf7780892efd6c52eb19941109d5db
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:27 +0000

    every trap face plan takeback

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def square_number(x):
-        return x**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
