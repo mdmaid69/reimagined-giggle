04bb582a6d17ed12a371f0c9db81c24401692cf1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:13 +0000

    thin goout similar disturb mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
