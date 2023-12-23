fb8600fb803314c7fd1b92a7a4e7f0c0c389c8e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:29 +0000

    mass eye future also guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
