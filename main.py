e033fdfbaa29c7dfe9a27da7c493d25e59709951
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:43 +0000

    please else cause prove correction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
