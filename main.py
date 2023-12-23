086b324653f9aa03e48af35a78939076f1d1cda0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:37 +0000

    guilty fire room Itself influence

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
