1aa677cba6bb27f4ec2bbdf38bce5ee5066ebaf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:11 +0000

    cheap idea dare trouble problem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
