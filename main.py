5217d3ac108e12cdecbf596eb93e07c49e0925f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:58:39 +0000

    note active geton towards trouble

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def reverse_list(lst):
+        return lst[::-1]
