a3e46863c41dbb6f66184cbfbb024f6064830b52
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:39 +0000

    neck brush new unusual interview

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
