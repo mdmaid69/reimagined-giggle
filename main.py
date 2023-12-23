ac8a907d7554d6b8317a103d64aaea313cebfca6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:21 +0000

    sound print friendly future eye

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
