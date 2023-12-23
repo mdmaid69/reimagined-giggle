09c69fec5b39d4dcc30c2e2b7e20277ac430ed69
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:35:20 +0000

    charge check correct union somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
