439fcd56a828909a7a7b9baadbbabc8aaaaa61cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:37 +0000

    compare come near tongue impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
