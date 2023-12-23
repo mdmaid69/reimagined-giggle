b0acd49e14ba5467884a967ed56881f29db38097
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:33 +0000

    cross start traffic snow push

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
