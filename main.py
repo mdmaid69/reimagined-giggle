27bb430cbaa2d90a69bc9cf3fbaa54b6a1f413cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:44 +0000

    tragedy wide line flight borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
