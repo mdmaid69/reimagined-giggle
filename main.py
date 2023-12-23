3b30f6a74da30dcfef59bc02e55813a4796db9e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:04 +0000

    introduction minor any weather injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
