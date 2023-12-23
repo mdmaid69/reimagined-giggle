8bb11e11f20584dee3b9119777a190394ab2035a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:35 +0000

    tip less continue straight wind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def cube_number(x):
-        return x**3
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
