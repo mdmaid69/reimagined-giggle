adce2fe5f024b84860bf67ca27750869c028a465
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:07 +0000

    right label Pp I private

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+print(sum(range(10)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
