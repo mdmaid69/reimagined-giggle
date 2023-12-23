cd30961126c0d8d14d476f663b5d1831da6b1ea6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:21:46 +0000

    interrupt rent swing stretch impressive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
