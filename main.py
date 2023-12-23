f8f4560aa6eb415790be5b1ce5575d1389d170a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:18:23 +0000

    fire crowd worst load right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import sys
-print(sys.version)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def greet(name):
+        print(f"Hello, {name}!")
