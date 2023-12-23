aefb9dafc37a84f5df1ea1bba84bed7ba58c6f17
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:13 +0000

    trust another language someone careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
