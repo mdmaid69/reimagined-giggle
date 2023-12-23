fd4ba564c236b6e9f2897ced2fac0ba2c2a2df83
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:00 +0000

    must throw tap forget impressive

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-print(math.pi)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def is_palindrome(s):
+        return s == s[::-1]
