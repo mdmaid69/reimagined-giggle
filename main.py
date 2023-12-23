60caefe96f5cf786c5815b85bdb474604fab6619
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:44:48 +0000

    opposite search measure burst various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
