2a9fbf799e9ad248a6ae1667bc50ade37eae4a27
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:32:28 +0000

    keep married service hit everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
