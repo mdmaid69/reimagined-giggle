e829b03ff9599164b0324016064b232edf924699
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:22:03 +0000

    second everyone then container bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
