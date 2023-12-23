7ce62ae498f72c310f3bbef31fe273c6e1250227
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:18 +0000

    memory order world nice giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
