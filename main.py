8601b26c71e8f5858ca3dc0fddc091afb48793a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:28:45 +0000

    wherever schedule mixture makeinto that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
