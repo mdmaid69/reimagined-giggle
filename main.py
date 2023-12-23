48239b924d42f2291e9e4a2d8b4e55080088bb54
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:36:48 +0000

    receive rain discussion thinkthrough once

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def square_number(x):
+        return x**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
