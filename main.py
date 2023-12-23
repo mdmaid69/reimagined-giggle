c02cf28e833b1382fa2a119c8a8b9d2b86b0223c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:53 +0000

    call straight middle demonstrate switch

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_min(numbers):
-        return min(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
