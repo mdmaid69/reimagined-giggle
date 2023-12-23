ce24eb27f5cc408667918f200cdeb3eaac0d09e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:03:17 +0000

    wipe sink name car open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
