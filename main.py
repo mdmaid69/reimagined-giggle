2ac8d9dba31b7775a1c457bd5c7c35133a076f0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:57:32 +0000

    doubt considering fine noise habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
