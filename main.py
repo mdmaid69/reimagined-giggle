8c2f8dc3ece0be1318c372c4d08cfc2b98553604
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:28 +0000

    though crowd surface light interruption

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
