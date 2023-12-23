8cd4977a5fb70d3ec53abd63a77775d8f2ad137c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:55 +0000

    nose physical greeting inquire total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+import numpy as np
+print(np.array([1, 2, 3]))
