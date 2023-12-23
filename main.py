36ec53d3fdbc89be5d91fc84a187d0ea5496eccd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:26:05 +0000

    mix comment maybe limit exhibition

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
