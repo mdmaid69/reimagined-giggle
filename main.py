c45354c4f18cee514ea429748102583c2cc8443b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:41:22 +0000

    Oo bye match film pleased

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
