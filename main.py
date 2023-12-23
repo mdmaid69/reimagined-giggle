680fdfec8312e1a86fca935360082049f42f0e5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:04 +0000

    564encourage harm boring rob unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
