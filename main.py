4435d44bf75dcdec06d32dc1a77c2298c3d790e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:54 +0000

    sort offend care disturbing squeeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
