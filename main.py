488286fe8338ee5d91a813b62e46189024ae3c4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:43:48 +0000

    ask display piece keepup impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_counter():
+        return collections.Counter()
