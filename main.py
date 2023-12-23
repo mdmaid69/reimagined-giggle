20fe4831c7341427e55b6cc2fa648db8163db5f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:02 +0000

    water report Cc chat home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
