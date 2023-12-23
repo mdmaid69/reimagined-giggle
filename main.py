03bfb0ab3e85c5de3e2c8184b1938c997e83826f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:18:25 +0000

    radio turnout word there admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
