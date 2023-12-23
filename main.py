8206cb7bf2d81f26674dd15381c875fdd1dca69d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:38:50 +0000

    interest gate crash example thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
