a55df1f8754ce1e05cb591a32faa1031f170aa3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:29 +0000

    meeting deal late another practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
