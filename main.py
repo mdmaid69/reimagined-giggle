14aa7507682d3c2a063da0906fb1df035ac327a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:11:06 +0000

    certain scrap catch night argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
