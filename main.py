982fd6f6927e2d59bce6cd2510b59e460a0ff6d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:59:49 +0000

    ability home case say edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_as_str(array):
+        return str(array)
