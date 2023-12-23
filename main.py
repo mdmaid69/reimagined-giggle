282796497c191584b059f3df3c4c6be5c3957b18
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:09 +0000

    safety command price knowledge sport

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
