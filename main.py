cf0a3a170567a7cc91dac75b65943798970d50aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:39:51 +0000

    tongue whether command justice young

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
