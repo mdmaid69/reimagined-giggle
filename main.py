031e244ed7f36bf26ec3a3bbd17909192fc4ed17
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:12 +0000

    cruelty corner average only square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
