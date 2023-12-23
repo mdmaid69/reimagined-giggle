e7f882cd29672baf29c1c1a8d883020f61135cc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:21:07 +0000

    she whistle consist muscle badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
