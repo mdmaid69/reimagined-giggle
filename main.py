c4308d3781fb308d381e2cfce1dc2d31abb98b30
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:14 +0000

    from prevent system blame understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
