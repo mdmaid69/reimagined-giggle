03dda3c1273a5513a2f00ead3201ba566e19c450
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:57:54 +0000

    Vv film mainly interview hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def multiply_numbers(x, y):
+        return x * y
