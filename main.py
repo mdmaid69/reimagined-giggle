81e65faaaec4640186aa27511b57a34b740fcded
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:55 +0000

    balance tired charge instead skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
