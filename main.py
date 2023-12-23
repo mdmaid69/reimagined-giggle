1f69eef4d0b1ee8268b70d67a05db09faf7bb4a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:13 +0000

    listenfor idea 571enough usually cook

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
