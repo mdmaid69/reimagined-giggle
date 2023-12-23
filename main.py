7d6a366945dfc478a19519831eaeb0a406efaa42
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:31:13 +0000

    war clue play what just

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+print(time.time())
