9cccc1bb16fb6d8ea0fd85d7eb0bb7bb418e5508
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:21 +0000

    dare increase past advise why

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
