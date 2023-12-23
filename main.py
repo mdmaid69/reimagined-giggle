184ecb9ae7b9004a25f06853d98b6e8f72f31da9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:53:43 +0000

    constantly must today tax taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
