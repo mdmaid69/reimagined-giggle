5e4cfd08919ee5cf3a6a5acb187672fccc40794a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:10:20 +0000

    type contain shape solve size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import sys
+print(sys.version)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
