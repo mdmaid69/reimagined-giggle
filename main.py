4244d99b0fd18a720d14a6097447ee4b9323fedc
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:49 +0000

    nerve total body land division

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
