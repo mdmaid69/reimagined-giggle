d1b79b80d14ead1dc09746b9e2f4bd883cc33834
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:50:53 +0000

    drop pollution rest hurt somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
