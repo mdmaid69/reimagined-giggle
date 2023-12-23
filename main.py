b21c17230c5eb6312d7461330dac2b2605fe52a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:49 +0000

    his through prefer meal turnup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
