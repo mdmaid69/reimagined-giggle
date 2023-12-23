1359eb88f4d4d2b538cb9d7766d6b48d022a7973
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:18 +0000

    yet slowly tune file cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
