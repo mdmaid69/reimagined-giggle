9bb18d029611c6d3d6b046ed959e151dce5efda0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:34:35 +0000

    post peace air crowd stare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print(sum(range(10)))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
