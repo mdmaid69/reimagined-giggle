e81ea78b12e83be4fc9e2c01889587310407023a
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:11:33 +0000

    of benefit opportunity lock night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
