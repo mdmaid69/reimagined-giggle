a7ea2b6afa4db53d89f233f279b1fb92d7ac40b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:31 +0000

    to take crisis truth previous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import logging
+def log_message(message):
+        logging.info(message)
