7eea19fa7d1eb17d4cec38cad8a753351f595ea7
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:49:38 +0000

    and create evening admit fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
