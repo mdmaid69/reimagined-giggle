71ab2d47dfae01f95d46f31fa32bdd91cfa9313b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:41:23 +0000

    nice order once failure minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
