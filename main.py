d72956143a64778af4411f9f34e0397bdb4e55d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:43:02 +0000

    convincing deep marry uncomfortable cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import numpy as np
+print(np.array([1, 2, 3]))
