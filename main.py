fdb6dfea67cdf728b61b147732d76e5ec2b87742
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:06 +0000

    regular number good away connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import collections
-def create_user_string():
-        return collections.UserString()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
