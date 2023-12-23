9d65246ffb39f381c499952c8d8535cb2fd4f678
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:34:55 +0000

    turnon turnaway fail bright frighten

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
