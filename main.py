8d3e94ec66ed984f7d949c07b337ac08d794a0b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:32:49 +0000

    himself ill emotion butter change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
