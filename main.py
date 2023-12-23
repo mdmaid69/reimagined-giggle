35fd5d283c0c829e24c5dfe73aa33447b4ebd6d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:50 +0000

    story like breakdown wander so

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
