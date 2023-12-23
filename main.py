58f4b496f38b243a4cc2f3812ec705c42dc89963
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:14 +0000

    city understand crisis visit coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
