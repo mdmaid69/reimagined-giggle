a61cdaa0568e219686b8affbe657093f336f6956
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:08 +0000

    getup forever day by pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
