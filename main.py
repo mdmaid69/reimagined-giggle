0ce11e56130916fccfb511054879efe2195adcb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:34 +0000

    could clean lookafter apparently succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
