d985c27191ae38ac471b4449d260fb1ac2fe8881
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:54:58 +0000

    judge hard preparation choose hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def check_if_array_contains_item(array, item):
         return item in array
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
