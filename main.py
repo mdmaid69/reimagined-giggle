d39aa8d6f79af8c80129841f6b061b6f8a48d043
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:31 +0000

    body thank collect possible objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-print(math.pi)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
