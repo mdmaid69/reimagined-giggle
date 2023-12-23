cec14012cfdabc1fc6ec61d184d352c81468c27b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:06 +0000

    rise behave laugh unless accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
