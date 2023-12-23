1e69b5c81994efa8a11bced0b7f638ee61217b24
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:31 +0000

    positive pump switch also influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
