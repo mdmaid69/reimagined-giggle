be06d5795e8a8eb5923de8421c748b1e8fa04c5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:36:18 +0000

    wrap test giveaway late visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
