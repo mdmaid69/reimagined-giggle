99df121ccf3543bcec93dbbd815cfe2d833e577d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:37 +0000

    reach free original up bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
