0428dd7cd333ffc9dc0ffe8effb15c65f5db26fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:56 +0000

    instruction settle pause but tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
