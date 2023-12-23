4e6c41276864fe38cf9797dd6246965e390edbfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:28 +0000

    estimate system thinkof interested last

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def append_to_array(array, item):
+        array.append(item)
