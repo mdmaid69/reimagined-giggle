ece29d2d4e9c7983f2ac7a6af2b9b7d8cdf78d09
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:12:56 +0000

    yesterday team strike crash simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-from collections import Counter
-print(Counter("hello world"))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
