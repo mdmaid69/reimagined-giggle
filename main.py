a0872a225f8e67f96915b08b47240cdd45cbebde
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:37 +0000

    scrap ground leave unite respect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
