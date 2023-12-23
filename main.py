43dde13a51be65ddbab084da82c37bf678b6139e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:43:56 +0000

    Ll matter mend wealth poor

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-for i in range(10): print(i)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
