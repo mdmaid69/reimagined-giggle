603e8106f19fc7589f07393f5d90d845741a0a26
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:22 +0000

    geton succeed blow remain while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def remove_duplicates(lst):
+        return list(set(lst))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
