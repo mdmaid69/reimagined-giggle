24994f5d5d8d55d4141e2db610d3860457a9ae8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:58 +0000

    smell harm lookaround sentence low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def convert_to_hex(n):
+        return hex(n)
