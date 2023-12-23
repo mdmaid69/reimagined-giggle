9fbc563b0298479e167ddd9224af1a36fa2dae03
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:14 +0000

    success still sleep unkind improve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sys
-  def get_python_version():
-        return sys.version
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
