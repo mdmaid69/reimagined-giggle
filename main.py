b437b41b7a68a4d086d9c13a1e8d053cb758ebe7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:16:35 +0000

    common quit mistake stage guide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+def find_max(numbers):
+        return max(numbers)
+def find_max(numbers):
+        return max(numbers)
