c1d72b747fdad52e539376b2c5db1592b5b6abb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:59:57 +0000

    table surface suffer television safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
