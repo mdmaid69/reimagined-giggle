ba7620d45007f23c051727409b3f18cc3499c972
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:26 +0000

    silent characteristic one emotion back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import logging
-def log_message(message):
-        logging.info(message)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
