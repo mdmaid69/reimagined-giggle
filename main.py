01bd04c3a3efad4c36f8e47810f3fa44624fd24e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:54 +0000

    ball force disappear shrink call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
