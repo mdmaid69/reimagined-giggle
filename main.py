24f2636995d1f54a423db11b81402deb52780296
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:23 +0000

    brush worry with body takeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
