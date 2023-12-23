b701ca6fd5ff2cb877bad24e3a632debfa4ef2af
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:13 +0000

    instant treatment unfortunate measure share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def divide_numbers(x, y):
-        return x / y
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
