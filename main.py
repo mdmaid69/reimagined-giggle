a5bcbbd693977b7199feab85414a951c01681a5e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:22:07 +0000

    lay equal get breakin escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
