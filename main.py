a91beb8b5e86f265d3141c06c315ea22ce10bf0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:10 +0000

    kind amount gather grade give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
