1dadad37e5322047a27b6b40750715df7fe72297
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:26:47 +0000

    scared this reduction confident sensitive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_logarithm(base, x):
+        return math.log(x, base)
