4ffbf9aaef8bab6917043057b9dfc9378574fdf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:41 +0000

    everything waste with rank behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
