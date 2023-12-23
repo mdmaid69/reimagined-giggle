4773ffdd2d50593164fd6a94d2d96f3d53bc60c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:51 +0000

    warning hollow female legal Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
