999f665d32ff2841c685f63e0f0ccf8a1654ebb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:27 +0000

    considering discussion talk show suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
