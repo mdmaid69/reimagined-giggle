b0d4ae02bbd38bb2bf42430ad17d76ffd0ba4013
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:38:12 +0000

    introduce cause 573entertainment interview fair

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
