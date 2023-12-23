8519a39e98020583ed37850ae8bfdabd2624f318
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:17:31 +0000

    glad worth get open look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
