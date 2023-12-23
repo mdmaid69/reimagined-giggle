c6cac5eceefad1fc37c5916d3ac13a5831ad3aea
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:53 +0000

    foreign perfect speed like force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_list(array):
+        return list(array)
