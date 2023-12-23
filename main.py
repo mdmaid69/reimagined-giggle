4e4caf47857e874250b52e1ebed602ceee4f9fef
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:53:26 +0000

    again child OKorokay Gg fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
