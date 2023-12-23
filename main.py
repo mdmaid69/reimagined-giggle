2507d16faa09861b053a3aae9a42e8ac00332a4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:43 +0000

    company fault progress behind accordingto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
