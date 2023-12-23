3a45388e833c6f6a80fc4e53e0bc01aae9fd3892
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:18:29 +0000

    agreement giveup forward funny promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
