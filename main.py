013ea80df8aea26ecec844bde2813392f1e10df1
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:10:43 +0000

    period towards temper peace whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
