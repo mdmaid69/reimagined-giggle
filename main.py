801da8c2513a95ea7e0f42476bb30fdefec9dcac
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:30:26 +0000

    risk middle week go ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_tuple(array):
+        return tuple(array)
