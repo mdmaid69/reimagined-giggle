a21a07ab7be4c1b797e65649e7290395dd7bedc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:28:06 +0000

    OKorokay steal worried woman on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-for i in range(5):
-        print(i)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
