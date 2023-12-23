5bb307a08625bb7caf977a78f7730c4f2513b450
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:08:12 +0000

    disagree modern friendly him simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
