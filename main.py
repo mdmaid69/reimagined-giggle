ae67db10bdd9a545f71a9b5d0b0ab7e1c17272cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:14 +0000

    might off small guess relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
