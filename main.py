cab9e787b5c3b9d9b1b94a4b18cebc28e40a7d4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:00 +0000

    estimate he justice money convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
