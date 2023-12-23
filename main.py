b9c8322f7ece0a51893f704a0180db5753183137
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:21:39 +0000

    bound turnaway start appeal form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
