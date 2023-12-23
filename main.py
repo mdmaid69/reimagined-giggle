84b627a2385807eeb3c8e08d454ebc385aba90ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:23 +0000

    middle frame opinion game unhappy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
