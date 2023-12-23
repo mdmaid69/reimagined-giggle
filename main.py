df2150903de9859452e3600825b980467349bf7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:37 +0000

    supply land close release sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
