accb6f3c019fb6bf3637e0f4f610b922ef249196
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:49:33 +0000

    difference wave appeal it motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
