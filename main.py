a251f4970d37148319866a24c1d53034a487e1d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:49:38 +0000

    material pretty schedule story regularly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def convert_to_octal(n):
-        return oct(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
