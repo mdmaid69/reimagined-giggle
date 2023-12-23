7781150820a4d9eba428d1fb56feb22608ad261d
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:59 +0000

    development frequent closed oil drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def print_python_version():
+        print(sys.version)
