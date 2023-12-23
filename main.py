663128a90079cea178221addbcc78632c1962389
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:16:40 +0000

    measure responsible seem eye violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
