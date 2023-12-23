53fb788e55ec93ed6e96954eb6aab5458172a8f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:54 +0000

    measure pool fair pool temperature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
