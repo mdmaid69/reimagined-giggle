a3c6fad23b8bcdd15b3cbdc0f298c52bf07b2c34
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:46 +0000

    lock pattern suit passenger event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
