02eea1e4b2de88752fef8aa4fe3149228fda418a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:31:16 +0000

    holdup task your careful firm

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_density(mass, volume):
+        return mass / volume
