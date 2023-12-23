8b5b114b1d1f5a46c55a64862a872046e032229c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:25:23 +0000

    comfortable throw radio congratulate wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
