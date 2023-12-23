fee03940b19f605b9777d66d702c0c8a59b49875
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:50:17 +0000

    noise tape succeed respect exhibition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
