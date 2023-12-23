d848630bee694c1632722af772f70ff0e600dc73
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:02:42 +0000

    purpose avoid mistake name immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
