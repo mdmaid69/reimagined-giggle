b15026ffb4a8257138a8029f4587c45043e4eca5
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:45 +0000

    progress unhappy throughout paper pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
