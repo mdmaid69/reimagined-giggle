520d3bd0c599509e8ea99e1bd5170624a3d693d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:43 +0000

    drop quantity operation pot split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
