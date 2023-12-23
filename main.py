8a6994f75bfb490105fdd28f1ebb8e7afa717563
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:31 +0000

    few children uncomfortable pride recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
