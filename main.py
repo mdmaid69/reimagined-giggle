2f394be758bab83088933acfcc5d5c21c0413b43
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:58:19 +0000

    computer path hang contribute confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+print(sys.version)
