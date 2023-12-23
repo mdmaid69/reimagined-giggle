8349939fe1c09bd5334e222e9584ad08ab8d4e5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:33:10 +0000

    line Oo object proof giveback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
