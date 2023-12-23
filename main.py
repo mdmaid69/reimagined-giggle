78652b81ba41570a3f6aaa3782dc5c488934a96b
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:36 +0000

    tool get tour motion flood

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
