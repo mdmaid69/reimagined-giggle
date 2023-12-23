5d88f20517b566b4a3c061ac8d14965f4bf22ef2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:22 +0000

    kick careful question start central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
