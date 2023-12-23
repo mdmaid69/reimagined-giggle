521cac1cf4474d455f887994e430ecfaf3e5f0c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:06:24 +0000

    cough brush share thinkof shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def convert_to_octal(n):
+        return oct(n)
