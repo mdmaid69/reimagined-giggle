3faf05572cb7ad7bf8ebc75904fee36cf2dd347a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:55:35 +0000

    judge bet breathe warning pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
