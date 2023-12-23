952dbb1fc85dc6da212b0ad40f51515985ec9efb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:09:58 +0000

    follow reaction pattern quarter reasonable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_min(numbers):
+        return min(numbers)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
