130950ab160a236dc445bd7471635b35f040bfee
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:37:36 +0000

    smell spot everybody wheel importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
