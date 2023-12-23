8ac329896e89a47bd94a8327051a4db94690243b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:18 +0000

    lunch always consider concern hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
