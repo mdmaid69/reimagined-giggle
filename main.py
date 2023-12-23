729800cbeb2ab84ba075c5021536f85a6ee41d75
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:24:50 +0000

    fasten thick post patient like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
