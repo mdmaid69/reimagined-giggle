1727a88110f3202c6627b3ec687f0965c914b922
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:44 +0000

    next recover pot line sit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
