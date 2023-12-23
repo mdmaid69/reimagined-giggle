6404cda517ade2992f1c91d4c8c9c8f2b537e71e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:31 +0000

    after I square 571enough club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
