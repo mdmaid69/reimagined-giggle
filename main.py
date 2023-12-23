56b83eb7f1d73a1bdaf3054d467cdad63f0caeb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:33:40 +0000

    stone fire port turnoff breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
