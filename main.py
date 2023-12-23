4579cf6251e3070f782ac4cefcf6ac679fbbee7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:07:35 +0000

    trick attach indeed by awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
+def sort_list(lst):
+        return sorted(lst)
