e1511ea39267f30ccd315192fa610fb5f1c0dede
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:59:12 +0000

    by near fall usually few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
