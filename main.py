0181ccf5e06794af11403fd8461a4c50a1d0d5f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:11 +0000

    difficulty intend couple by pure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-for i in range(5):
-        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
