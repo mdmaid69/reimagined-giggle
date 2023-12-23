7b43d014753b9bfe8bbe3ac6d8817dccc8ab6bcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:20 +0000

    trust figure week ear squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def is_palindrome(s):
+        return s == s[::-1]
