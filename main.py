90893e5aefd54d2c5ab352d370cfb5be959622ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:53 +0000

    shall aware excuse declare design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
