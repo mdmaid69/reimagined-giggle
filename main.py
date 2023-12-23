7db6c3163d591fb4cfb91cae81293f6a56ba076f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:35 +0000

    amount shelter memory touch argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_directory_name(path):
+        return os.path.dirname(path)
