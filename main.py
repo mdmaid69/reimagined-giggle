f987fa81e7676e389cb6c6d443b27004872615d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:40:48 +0000

    564encourage object table stir picture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
