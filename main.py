3d20afc44c206a7d7e0a15f1a31ec17e2db13991
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:05 +0000

    skin bringup issue nowhere terrible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
