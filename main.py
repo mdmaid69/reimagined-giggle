7972a80508b6adff7418c97b9e2c7cd21b4b3cf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:53 +0000

    mother performance little strike tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
