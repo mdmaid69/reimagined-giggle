bbaca96f8a367059c533b0a3ca493a19da1fd815
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:00 +0000

    with impressed kick quickly Oo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
