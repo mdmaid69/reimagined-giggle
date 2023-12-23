092f7a8ab92df4bb4c64ed059d902b188720cf63
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:04:21 +0000

    choice goon busy thought regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
