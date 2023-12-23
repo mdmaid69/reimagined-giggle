4b65a0abb4a29b58cea58d0e2372b5b6f8305394
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:11:52 +0000

    common creature our peaceful test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
