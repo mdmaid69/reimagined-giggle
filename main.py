ea14d9c73e2122b20eef687fbb84e754c8e048ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:39 +0000

    impressive fast later compete because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
