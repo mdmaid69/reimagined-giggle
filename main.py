cee73b1a4c7bf994de75a9c548b6b49689a53f6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:26:15 +0000

    account corner disturb rate loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
