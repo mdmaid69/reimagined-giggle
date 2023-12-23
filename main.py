9191505ceb746b367f941889dc7529c59fe24dd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:41 +0000

    Yy Ll gentle fortunate receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
