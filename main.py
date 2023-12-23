e78542ae562b2bdc0115c9d270b03426b7039fd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:48:15 +0000

    sink display evening onto keepfrom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
