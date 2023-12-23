c3ddbd082abfbc2d8eaa951ef5ee87a494870c5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:45:19 +0000

    mind answer closed country song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
