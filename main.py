6b522b11d23bc38bc54e86d8190a797fe359579e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:15:31 +0000

    concerned treat burn production genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
