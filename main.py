00f7b8fd53879789bd7f7ac85c802b0f2c73ff7b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:41:50 +0000

    spoil habit smile trick connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
