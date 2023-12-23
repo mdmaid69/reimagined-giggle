146f6cdcc83903d399c81357be0d896ed003c79b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:50:53 +0000

    odd square runinto or obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
