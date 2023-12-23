7cea53d4eb09d72e59bd5070179b945fb94b981f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:05:35 +0000

    basically instrument figure kind creature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
