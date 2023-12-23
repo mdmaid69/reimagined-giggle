a870797a4700b5b4eecd142e5f2179149cd31c16
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:30:06 +0000

    equal yesterday male Kk building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
