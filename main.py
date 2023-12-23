cdb4d4a9bd1440154c8aeab23b90c73ab47656f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:59:41 +0000

    bank settle recently not please

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
