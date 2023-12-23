dd1727c1a98d86196df42de70c651d6694ec2ac4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:54:33 +0000

    discussion underneath clear there pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
