1d6215db9dcce5fef3e276b4a539b004790ea9f2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:11 +0000

    like dance why traffic beat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import tensorflow as tf
+print(tf.__version__)
