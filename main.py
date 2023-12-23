a66b3827650cb6548a20a9263e82c5b7a658fc69
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:05 +0000

    free study typical guilty owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def get_list_from_array(array):
+        return array.tolist()
+import collections
+def create_priority_queue():
+        return collections.deque()
