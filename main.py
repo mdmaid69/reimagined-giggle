e86648d55d29844c6026432084f3748842b1ef83
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:13:55 +0000

    immediate row less strength travel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
