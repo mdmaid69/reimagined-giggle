17dd832083bc16eaf2dac4281e92e73b50653fa3
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:32:21 +0000

    tip opinion rush year rather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
+def find_max(lst):
+        return max(lst)
