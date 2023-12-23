5fdee5f919d8a825117ba7d5b53b31a398e6e313
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:53:59 +0000

    comeoff hollow afterwards usually learn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
