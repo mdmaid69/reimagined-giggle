9ec71a76389992c4bd112be7eef4ef9fba490f30
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:11 +0000

    suck friendly minute choice left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
