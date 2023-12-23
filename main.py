af713ab699e58345141b8fd2264cb100c21f70d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:36 +0000

    special comeout black average step

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
