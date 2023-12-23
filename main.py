29b88277665b20de880e17fcb58150f6bf9a52bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:43:55 +0000

    repair with every address level

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
