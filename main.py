e9d7c916c5e87bcf548777ad3c2db3ed9ef70e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:00 +0000

    character close stretch convincing path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-print(time.time())
+import array
+def get_array_typecode(array):
+        return array.typecode
+def square_number(x):
+        return x**2
