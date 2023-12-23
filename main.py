bbf4296fd5f85fe2ea472f0d89eac7495b6fadc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:30:58 +0000

    summer rely impressed family smooth

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
