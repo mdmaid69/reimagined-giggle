f72930a7590925a26dbd1a5b44ddc34174c49e9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:34 +0000

    adjective lookback(on) theirs weather across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
