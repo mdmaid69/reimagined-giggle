f6c4b47fb66d6aca9d7a7a36e90b629efe702d51
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:13 +0000

    copy hurt push play introduction

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
