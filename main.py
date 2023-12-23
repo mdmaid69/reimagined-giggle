df1c3519f20ac7d27cba530126e86a3ec764e7ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:00 +0000

    comeback plate field Ss date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
