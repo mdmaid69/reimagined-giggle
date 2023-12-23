98c8591a846b8fb74201ad45b731fbd05eeb58ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:44 +0000

    suggestion quickly lonely pretty increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
