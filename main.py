b9813bd75eb01ee812c9b3ad1c30451e0f16df4e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:41 +0000

    before coat skill seriously copy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
