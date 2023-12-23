635c9f19f68c4d18be7bece83f3229f00c8cde3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:30:02 +0000

    goback(to) return equipment star scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
