e7821d769bdfafb1cc4146888c3e66da12eedeb0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:31:48 +0000

    hello laugh low welcome protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
