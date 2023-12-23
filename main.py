2d8d6d58a01132dd859f9eaefc1d2ba40b168524
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:00:39 +0000

    weak tip doubt frequent splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
