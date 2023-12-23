4db6057b40f28b04317b164929c65c6c9c291434
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:56 +0000

    loose underneath pause achievement invitation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def convert_to_binary(n):
+        return bin(n)
