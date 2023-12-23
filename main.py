5cfe0b182098917f44126ff5d89c39afd7a61a81
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:21:49 +0000

    have quantity belief party nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
