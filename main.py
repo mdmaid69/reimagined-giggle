25b8bde319f5243775c4f396c6ee4e74d011ffd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:44:27 +0000

    myself performance too complete unusual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
