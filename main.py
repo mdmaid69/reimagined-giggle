c29c07e824cdb1c4df5adfd46b7f06cb39dc9bab
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:53:23 +0000

    grade serve truth pause unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
