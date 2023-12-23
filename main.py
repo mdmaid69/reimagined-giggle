46d258af12a0d6b8ec25595f3747b8137a336029
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:05 +0000

    while deep far entrance wander

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def find_min(lst):
-        return min(lst)
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
