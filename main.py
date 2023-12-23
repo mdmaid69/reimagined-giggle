284205e7e9a451b8d3deab14c76cc2e95a9f2584
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:35 +0000

    right someone apply strike style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def is_even(n):
+        return n % 2 == 0
