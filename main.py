44527912543ae6b19529eb9125c885b07a0eab04
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:26:46 +0000

    seriously for degree passage nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
