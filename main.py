5cfe9ee6bf8933f2f288d1c35c383064ae43c941
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:35:37 +0000

    afterwards question trick shout march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
