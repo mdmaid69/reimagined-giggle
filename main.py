ed0d00515ec3f55234ba1333430890adbe51decf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:29 +0000

    speed ocean Ss keepfrom thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
