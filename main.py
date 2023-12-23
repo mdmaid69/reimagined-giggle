ab067e9d068fff4786275a56ec4951f04c938f36
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:11:56 +0000

    similar class off they test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
