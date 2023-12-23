ac8dd5c57a94e90f5453995b0a91b6981deca3cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:09 +0000

    trouble pause pattern place task

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_size(file_name):
         return os.stat(file_name).st_size
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
