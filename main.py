608dfdd1d36d12db244c227986dbaca34b90c0b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:52:45 +0000

    dull fast pass laugh sort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
+import sys
+def print_python_version():
+        return sys.version
