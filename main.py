7122f4f680ba0ffa2640a5f584dd059aac380f17
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:31:09 +0000

    defeat satisfactory free combine meaning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_power(work, time):
+        return work / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
