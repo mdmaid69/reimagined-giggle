bb9050566ddf0dc8e9ee21fb501e84b962b277ae
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:15 +0000

    modern chief even convince keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 def calculate_area(radius):
         return 3.14 * radius * radius
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
