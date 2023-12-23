aa91497f99ebfb6e03de63537c26d657398c6619
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:47:03 +0000

    national safe culture university fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import sys
+print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
