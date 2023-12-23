b76fee2a190a7ab0ffec8f020535e607d5f6a20e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:24 +0000

    temper call any shut freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
