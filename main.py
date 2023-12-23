5701421e162810fa4812a717e700541595f86b55
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:51:04 +0000

    hold play damage disagree fall

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
