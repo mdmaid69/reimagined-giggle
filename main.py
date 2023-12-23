a1609aabcd68cb51a254f16544803dd9ca00d36b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:28:15 +0000

    cruelty where mean already play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
+  def add_numbers(x, y):
+        return x + y
