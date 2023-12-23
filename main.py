8ff69838638ae9446b21ddd6c9766f2ce960c1c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:20 +0000

    imagine wash justice considering push

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
