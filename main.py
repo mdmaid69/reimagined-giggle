cb9999b5bc8248cf952ee7ec340ce09615bf700e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:49 +0000

    fork well birthday trial hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_stack():
+        return collections.deque()
