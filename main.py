f88ed1dfdadb1669238258b42a3c1b9f5440b3fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:18 +0000

    cap suggest opposite make description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
