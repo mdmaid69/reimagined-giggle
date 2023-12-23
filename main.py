d2e66daef131b6cb0563a81b930b4157b3c1aa16
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:45 +0000

    slice negative smell past pity

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def is_even(n):
+        return n % 2 == 0
