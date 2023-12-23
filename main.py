f791554806adef99ad1b01fb8089243fea4c5442
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:57 +0000

    discover sad hollow raise whisper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
