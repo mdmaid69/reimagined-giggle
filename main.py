cf9714d2e4a826649341b2e8c3d297c06fb596b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:05:28 +0000

    war there unlucky seriously mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_work(force, distance):
+        return force * distance
