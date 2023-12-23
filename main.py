0d3ba996a1128fe928c5511f4ec4e7c278a38e44
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:30:16 +0000

    everybody whole follow contact stay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_itemsize(array):
+        return array.itemsize
