2ca8fcafd6aa6c729afc651b629f9b76bf9e9fad
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:08 +0000

    stamp whisper blame broken maximum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
