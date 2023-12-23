ba1c27c59a4d51c1e7238e23f198ce48f41a46d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:44 +0000

    cap spray laugh drop together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
