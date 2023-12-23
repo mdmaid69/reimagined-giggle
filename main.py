738be16a1940608615e82538fb30c18f5946dfef
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:18:23 +0000

    change prize sweep appeal through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+def find_unique_words(sentence):
+        return set(sentence.split())
