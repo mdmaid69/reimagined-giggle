b28158076f9e9c3a58284b3214521de7f4206c6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:31:27 +0000

    what weak each traffic dig

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
