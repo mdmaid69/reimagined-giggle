9317d095b7b6a5258053fda32a4f4c44ed27c708
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:27 +0000

    recognize as snow this turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
