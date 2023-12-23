7364985201d286eaff295df6a4d02516167cb19a
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:24:30 +0000

    steep left next powerful Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def list_files_in_directory(path):
+        return os.listdir(path)
