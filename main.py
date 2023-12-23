cd12a7cdd4d7d081f54e025d34db2b584abebb21
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:26 +0000

    work track mail escape empty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
