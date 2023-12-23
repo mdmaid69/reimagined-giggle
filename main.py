9fe6780a4c58fccdb5c8fc4aea2c460fa1a5b2c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:23 +0000

    frighten mail suit accordingto bottom

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
