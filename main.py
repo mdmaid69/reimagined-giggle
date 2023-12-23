14df79ee0c80bd90728c127fd6fd661fd7b1af20
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:32 +0000

    touch true dangerous protect recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
