29853cd62a369ec531deb30f7a42f5bad4ba30f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:43:28 +0000

    would talkinto at who escape

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
