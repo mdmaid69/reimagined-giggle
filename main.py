fcf97f25b3ef98cf075ed838e947a20db6560a76
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:15 +0000

    roll create traffic must present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
