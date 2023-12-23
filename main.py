bfc7a59971f3b7368ff9ca164b193a17a97e2dd2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:29 +0000

    bath cause beside switch moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import os
+def get_current_working_directory():
+        return os.getcwd()
