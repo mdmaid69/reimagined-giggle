5cfcad3d8e46d56ab293c58093b451facc4910ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:26 +0000

    how accept cruel round eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
