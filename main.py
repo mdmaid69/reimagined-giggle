46c2be9b604808b13b29eecaf6277cd7c65c2617
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:09 +0000

    me score callback correction suddenly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_even(n):
+        return n % 2 == 0
+def multiply_numbers(x, y):
+        return x * y
