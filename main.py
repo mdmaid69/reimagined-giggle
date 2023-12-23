bced6f1275d841e3f1003860f2c2a4e0ac948f23
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:34:36 +0000

    die wherever no holiday interrupt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-print([x**2 for x in range(10)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
