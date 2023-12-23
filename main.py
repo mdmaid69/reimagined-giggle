778b0c7c8132f7c21d659ced46c3b84ffc432493
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:57:34 +0000

    sleep way turninto begin trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-print(os.getcwd())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
