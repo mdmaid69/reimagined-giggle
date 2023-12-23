4746a02d25f1b1a399b01ed620d07a2015850747
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:02:50 +0000

    hurry regional suck give check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
