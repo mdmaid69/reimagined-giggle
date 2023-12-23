0695f982a6d6a53abc08d47fddddd2d6ea7f7400
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:52 +0000

    clean own education recognize yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
