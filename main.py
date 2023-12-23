076d7bb7db56a9a96ab57f9812669a07bf894107
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:53 +0000

    sense pay excuse post 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
