14aa9758837a1c38f87c899644a0b2df9b004ff2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:32:49 +0000

    pair trade breakthrough progress content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
