6e0fb517d7cde3777244766dd4023f70cd67fc89
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:20 +0000

    preparation talkoutof movement week over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def reverse_list(lst):
-        return lst[::-1]
+def create_user_list():
+        return collections.UserList()
