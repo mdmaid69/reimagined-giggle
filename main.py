69133d939da06b828f782ef6b601ccf5ff29e0cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:30:58 +0000

    persuade message callup steal excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
