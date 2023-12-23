b526a93df1f8960d70ddddeabc6936a1a189b838
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:17:51 +0000

    march area score minimum warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
