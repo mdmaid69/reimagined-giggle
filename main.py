b0f7c605fa331afbcf4963c4fbceb98265a7d24c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:16:58 +0000

    skill description emotion legal must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import numpy as np
+print(np.array([1, 2, 3]))
