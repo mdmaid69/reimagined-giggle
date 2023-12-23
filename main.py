195a99e35f6a52f06e5d6612ec0cd86395ede447
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:47 +0000

    connect just brush along on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
