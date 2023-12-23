8fc95725563b81be161d1d305927f6dfa378e706
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:39 +0000

    in through swing rob otherwise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
