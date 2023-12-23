489473741a926d1c315667fe9481405a844b782c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:48 +0000

    obvious benefit rare weak drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def reverse_array(array):
+        array.reverse()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
