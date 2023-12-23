ea8bbf2d1b29bbb1270186e27f957efacf55cb11
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:41:54 +0000

    bank not style inform wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_queue():
-        return collections.deque()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_binary(n):
+        return bin(n)
