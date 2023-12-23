a9b3e8bde492d8046a10bf20431103663f6a44ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:55:07 +0000

    lean myself bang programme sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
