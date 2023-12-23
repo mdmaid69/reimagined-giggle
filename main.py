566df5aa7d3a1260702588cb9d661e60112b8d5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:46 +0000

    drive thank cold story direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
