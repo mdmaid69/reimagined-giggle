37f15f93785e6539d201962a664d7be5a1d316c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:22:27 +0000

    keepup unite hurt scream line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
