62b23f35656a4f4e8810c218eb20a65a6378ca54
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:47:21 +0000

    practise look declare middle advantage

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
