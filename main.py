54c19a8a2dc366438f5cb44c1f15aa90d741a99c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:19:18 +0000

    disapprove more file tip out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import collections
-def create_stack():
-        return collections.deque()
+def create_ordered_dict():
+        return collections.OrderedDict()
+import collections
+def create_counter():
+        return collections.Counter()
