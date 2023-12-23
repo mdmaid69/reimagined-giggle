c21f7cb4a8420378229c31d9044e94b37448becc
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:10:08 +0000

    passenger church service ground side

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
