d70115433f9cbdc1bf277c249a2d923f21b9286d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:39:39 +0000

    rescue easy now twist reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
