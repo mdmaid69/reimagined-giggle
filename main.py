cc50c3809152dbf4d15683104ff7be5667390abe
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:48 +0000

    wonder quarter string exhibition conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import time
+def get_current_time():
+        return time.time()
