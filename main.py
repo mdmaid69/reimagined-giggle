04606848b94b2b4de01480b3c155fd5bf776f6a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:11 +0000

    snow small her punishment worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
