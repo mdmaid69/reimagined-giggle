67bd347a27b47f68214ee1b82f04088119474721
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:54 +0000

    shoot apply effect similar open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def greet(name):
+        print(f"Hello, {name}!")
+import time
+print(time.time())
