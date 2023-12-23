3a97752156de75ff74eb2435a84dd0c52d29cd64
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:32 +0000

    holdout trap switch trouble ours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
