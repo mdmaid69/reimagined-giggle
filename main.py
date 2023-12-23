326a3a1d9372799bb04e83dbc04fc9694c332991
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:56 +0000

    survive line practice recent recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
