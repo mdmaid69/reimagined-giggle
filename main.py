e36748370fde1c51f4b0d516e2fa86dd228156e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:54:42 +0000

    play belong job neither grand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
