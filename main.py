1231da23f2187d67913b6ba07bea4860e98a860b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:06:49 +0000

    make present wing sound print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def square_number(x):
-        return x**2
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
