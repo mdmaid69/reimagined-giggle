1103e0f454d597071978b8702c9a33bfeadd9fc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:39:00 +0000

    completely pretty nerve provide central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
