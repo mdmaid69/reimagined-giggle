5a053c347b60c7ecf40b40a1824f7903fdb48381
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:33:17 +0000

    quarter balance would joke cough

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
