d144371858eb682dae3798b29991e663262a8aad
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:33 +0000

    apparently answer middle shallow sure

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
