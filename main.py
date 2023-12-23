64bd101351fe6b668e08828ed706d8b6ee299b98
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:59 +0000

    trade famous noise care classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
