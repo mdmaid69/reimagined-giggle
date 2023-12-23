f5d64251bad9d954ba02c9a7a127cb3231f9bf33
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:57:58 +0000

    save use fun shoulder take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def subtract_numbers(x, y):
+        return x - y
