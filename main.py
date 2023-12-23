ee54a71939488421be9f35ea05ee01158c7f59b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:10 +0000

    pain neat price field number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
