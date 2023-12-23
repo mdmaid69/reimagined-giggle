27e834390c70a2abab9519f21c584e7d35bb5433
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:52:38 +0000

    ground die press scrape comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
