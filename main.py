d10afada4ec75f72c9e1f60c0acad97b59979887
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:05 +0000

    left obtain drink workupto concentration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
