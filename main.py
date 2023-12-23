f6a1d8a3ca337c9fe3347f9eef0d126ac17dd1d6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:07:07 +0000

    fact kill wind hers card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
