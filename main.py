198ea93be59b9688eacdc71a89028ecc24daad4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:40 +0000

    object return yourself habit callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
