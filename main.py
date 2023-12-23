73b1cdc62a4033d4101379d99f215f411873be57
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:46:48 +0000

    mouth thick private anxious forget

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
   def delete_file(file_name):
         os.remove(file_name)
