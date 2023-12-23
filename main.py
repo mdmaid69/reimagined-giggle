5db6f413f3027718bc44b206e9e443584ac8902a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:05 +0000

    cause content class total dress

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
