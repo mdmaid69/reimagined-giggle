79c55b4b5cff46e7dc44be29abc4fbf504b1e849
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:24:44 +0000

    different nurse read order brief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
