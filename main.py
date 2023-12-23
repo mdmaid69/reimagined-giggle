0de83c35cc7a0115b015c414a8e01125cefcde75
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:04:28 +0000

    maintain bringup temperature part comment

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
