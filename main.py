3c761bdd36997d4ae16215ec4e3431250430800d
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:49 +0000

    develop moon neither refer minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_string(array):
+        return array.tostring()
