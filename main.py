cd9341bd72faa156f5940e5fc1779054a36fbdfd
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:02 +0000

    handle certain innocent sit supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def get_array_index(array, item):
+        return array.index(item)
