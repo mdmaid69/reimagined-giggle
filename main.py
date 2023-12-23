c2290960118d2245040393118d23483aa04a6696
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:21 +0000

    major neither separate over holdon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
