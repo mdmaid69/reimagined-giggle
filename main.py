c2531a2e453a3974225e7e671bc47b0fd8557167
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:53 +0000

    turninto callback contact noone attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
