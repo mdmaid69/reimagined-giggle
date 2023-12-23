704840543e9c8467ab85dc526078df198ec51845
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:39:42 +0000

    education neither few flow whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
