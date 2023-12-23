2e5008dd168747a8bc840bd44ea9807fac54fc65
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:37:09 +0000

    burn regret middle onto spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
