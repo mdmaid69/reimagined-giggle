3d58c3e0b4eee2b5d8546c4e201e1e85416a1aa7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:53:10 +0000

    suck part OKorokay move country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
