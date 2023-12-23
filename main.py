0b90010731a5e9f4272e3824cf935b6e772bc7da
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:08 +0000

    settle black begin cause sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
