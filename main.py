31041e66094ba74c8caf8b114f1d7dd1dadce931
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:26 +0000

    strict exchange dream stick Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 def find_max(numbers):
         return max(numbers)
