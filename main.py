02a73797afa6cb5deac5cecaa2d8fe294e07009f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:36:04 +0000

    project protest achieve Gg fetch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
