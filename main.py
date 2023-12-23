ced5bf3a059120877b65db117bcf66755a8b41ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:55:34 +0000

    music contain entrance problem operate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def find_min(lst):
+        return min(lst)
