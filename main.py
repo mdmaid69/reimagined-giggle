f0414a1cad90309fba0ccf772cba27be278ae7c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:42 +0000

    deal period rather hurry walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def convert_to_hex(n):
+        return hex(n)
