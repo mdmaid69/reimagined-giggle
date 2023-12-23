1743dbacfb3aa6bf7b532ed380c95e576b4c3b4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:55:23 +0000

    workupto black 565end later yourself

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
