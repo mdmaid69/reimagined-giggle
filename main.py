1aec016770eebb93df9210906550f7e20f72f03b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:30:19 +0000

    shine runover worst pencil put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
