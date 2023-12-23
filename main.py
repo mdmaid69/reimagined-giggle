739b6aad77f6e8008c7e9941b09ef910f2fdd4cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:05 +0000

    of you proof cross convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
