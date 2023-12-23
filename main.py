a1e2e30e599b303efbcbdb09e9a00d06fdd6cf4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:22:30 +0000

    contain neither forgive avoid pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
