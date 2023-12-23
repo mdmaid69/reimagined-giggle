dc45e9b7e8e026ca9fd659e843fa62315923bd84
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:43 +0000

    thirsty embarrassing battery dinner lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
