0670a76a93d82ea4a7431759e1ee8b642b667525
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:39:53 +0000

    shallow listen cross worth hold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
