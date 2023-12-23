21f1c06108cb1bdc7648af16a1151d0b532bfcf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:06:08 +0000

    month tired who true difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
