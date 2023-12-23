d2200bba3184524ceee0f1ebb8c5d04ae93692f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:35 +0000

    lose play her scheme feed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
