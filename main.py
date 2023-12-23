f1d042c5fce37ac5cd13d444b524972685e7f7ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:44 +0000

    embarrassing opportunity action explain progress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import datetime
-print(datetime.datetime.now())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
