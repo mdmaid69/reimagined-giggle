aa06565a16da63897db1df568de6591a7614e641
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:41 +0000

    mark tall tap public fat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+name = "Python"
+print("Hello,", name)
+def calculate_density(mass, volume):
+        return mass / volume
