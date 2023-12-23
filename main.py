83e9d3d96c35f406ac71a268e348e34ae6b6c791
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:55:55 +0000

    sort inside difficult certain yours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
