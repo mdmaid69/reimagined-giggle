1c8de2baaf6487b381cf65d3e72ddb24719855ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:31:46 +0000

    plan over moon must excuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_work(force, distance):
+        return force * distance
