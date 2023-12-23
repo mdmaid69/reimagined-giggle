ba11fa4f77449f789c88e73956089cf62d9b0b75
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:44:59 +0000

    steal phone some ask get

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
