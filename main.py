aabf6e2c83cc8b2485b700021b4eb1ac53b0803f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:03:49 +0000

    coffee holdup mood official backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
