14ef473d06ba31e7edff55bc53d5082d3d87cb63
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:04:08 +0000

    official unhappy reject environment late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
