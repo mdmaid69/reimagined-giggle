8e984d0b03de6e2815228b20303285db88f49baf
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:48 +0000

    worried development possibly like comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
