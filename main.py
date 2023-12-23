647a4d60f832b1da7a49c8e6deb383bcb2e6ee9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:56 +0000

    hospital job explain ashamed impossible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
