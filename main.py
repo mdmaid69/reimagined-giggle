138a64425d75aa053474515dc857db85d66ef744
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:26:13 +0000

    go cap doubt destroy hope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
