372ae7251f52a8af5a0ca32df30ef1dcafbabd1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:40 +0000

    unlikely patience control watch principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
