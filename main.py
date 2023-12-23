415ab13688d587e78ac372fb81cc6cd26a5e8ef6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:04:00 +0000

    bye main another walk any

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def convert_to_binary(n):
+        return bin(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
