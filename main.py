6ac7dabcd9527a7954fb159b292d79710e26ed95
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:13:20 +0000

    discover takeout shall seat thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_complex(array):
+        return complex(array[0])
