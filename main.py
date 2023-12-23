01abdb965090d842228962aae9e00ae33578773a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:04:52 +0000

    comment tonight earth fact slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
