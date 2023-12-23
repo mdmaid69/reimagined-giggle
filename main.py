5613041a3b82dc299931778626c79842720ba398
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:02 +0000

    public holdup convinced shame put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
