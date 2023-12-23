c6f0f8da1a5624c1c05b786e93175f11a82f7cb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:07 +0000

    play clever recently apparently body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
