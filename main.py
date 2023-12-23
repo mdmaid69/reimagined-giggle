24e3366bd46fb6a3523679d59003be0d4e135cb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:06 +0000

    relax ticket pay praise hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
