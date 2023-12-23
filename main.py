788c4344e0a03b0dafde80e2a650261174ee1f83
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:42:07 +0000

    escape great heart empty sight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tensorflow as tf
-print(tf.__version__)
+def subtract_numbers(x, y):
+        return x - y
+def find_unique_words(sentence):
+        return set(sentence.split())
