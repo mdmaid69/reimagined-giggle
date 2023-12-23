58ca80fd23fe0e088a868f354c8ba5c516a3afd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:04:44 +0000

    attitude cover clue hard pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
