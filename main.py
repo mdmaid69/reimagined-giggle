c3ac83dc635a1ff6287b1a263fdc3fc8c5c9afcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:05:06 +0000

    ourselves thing wide unlucky fact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
