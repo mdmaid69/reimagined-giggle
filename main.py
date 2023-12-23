9089fb82831c46d35a0baa2adc80a3abb759554e
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:02 +0000

    confused easy attract willing lookback(on)

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
