385b9a5e396c9e80231506c6d27bcb4a790d36d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:15:39 +0000

    afternoon pride bush distant equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
