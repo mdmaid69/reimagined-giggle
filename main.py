fea7470f66a04463ac49855d69fef6bd4cc97a4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:12 +0000

    deliberately rubbish complaint experiment cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
