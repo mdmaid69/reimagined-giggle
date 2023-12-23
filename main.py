535fb2d8e691491bbc647d0d73e645bfc59799e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:06:18 +0000

    unfair normal judge minute quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
