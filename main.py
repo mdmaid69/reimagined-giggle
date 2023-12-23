89a27db9bed106125be603b227118db0e1566459
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:00:01 +0000

    load way bye estimate getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
