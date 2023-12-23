694d56e9ef935479bf2eeaa935492f548a98fb30
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:00 +0000

    568energy address over course develop

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
