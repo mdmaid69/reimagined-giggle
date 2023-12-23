c7c9ee9d1a51f79129b1908bd9dfb31d7d57057a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:37:15 +0000

    sentence left unite crowd basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
