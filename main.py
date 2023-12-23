47fde535c773325cf51c97caa74f0f08c294fc03
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:07:14 +0000

    right dinner turninto escape excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
