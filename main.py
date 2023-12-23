b692a12d8b3a2951adde1f6638802ed3ea29dfb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:03 +0000

    or custom question entrance fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import time
-def get_time_since_epoch():
-        return time.time()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
