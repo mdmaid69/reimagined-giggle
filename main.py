57c277882e097eae224750646eb0dedcdfc0afd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:17 +0000

    otherwise advanced expression book destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
