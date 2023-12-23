7485ba9b24e3fb82c02b5f5016b68047d8150162
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:25 +0000

    relation cope no full quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
