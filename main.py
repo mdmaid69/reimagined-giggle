79ebbfce8daff630bf304368c851ef9c6d351957
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:24:59 +0000

    complain healthy giveaway murder well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_pressure(force, area):
-        return force / area
+def get_array_as_repr(array):
+        return repr(array)
