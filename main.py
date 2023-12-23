fb931820722e114b518ca0a6a8d1a63cb671ffec
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:45 +0000

    high declare continue spot operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
