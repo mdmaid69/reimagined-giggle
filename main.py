742af490fa0d838e9a6bf944d9958e74bcf0a1c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:07:14 +0000

    song model hello steep switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
