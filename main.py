5e8ed682c3c926aa0140cccb0861204205f3a6fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:52:01 +0000

    leg wire term squeeze problem

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
