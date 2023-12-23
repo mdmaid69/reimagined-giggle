f1baec7c26dcfeb7a3dfdd0e709c136a822e58e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:15:26 +0000

    passage relative slice might lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import time
+def get_current_time():
+        return time.time()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
