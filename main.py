601a6c054bd8ab20ace52797bb548e8582cd90b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:31 +0000

    impatient experiment approve Jj bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
