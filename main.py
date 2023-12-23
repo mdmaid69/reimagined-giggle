291de6e6bb726821906c47634461ab7a3cad2213
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:37:33 +0000

    practice calloff point ring transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
