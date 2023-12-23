68330a55bcfe113c6b95def3834746208eeca6aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:21:52 +0000

    uniform impossible by closed university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def convert_to_octal(n):
+        return oct(n)
