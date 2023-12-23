c24de73203b04715bdacd958a4ec26eadc37df30
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:05:32 +0000

    law problem help spoil or

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
