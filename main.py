475a00f2520551b26b4aaf697df50e2f14c2f0b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:09:48 +0000

    conclusion want trouble undo this

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
