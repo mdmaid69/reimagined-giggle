a6fcb84dfc8ce36f186d0f4fc402c1dd1374ef97
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:35:14 +0000

    state suggest store front raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
