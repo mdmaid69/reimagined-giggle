0d72d02ff0b91c7220c2a1bc13c5bda65a3b6453
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:46 +0000

    manage pair behaviour answer disgusting

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
