5aeefd0d47debbbd7706d07b4d3ff50aff76d60e
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:25 +0000

    life wide dig official pressure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
