a5aa5286c30f4e821128fe13603194ec1a31eeed
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:15:42 +0000

    touch workupto thing recognize giveup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import datetime
+print(datetime.datetime.now())
