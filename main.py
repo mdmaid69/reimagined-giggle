4d663aeeb554a4496c06f8423e9e2c624125e83b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:55:50 +0000

    clever record sweet them towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
