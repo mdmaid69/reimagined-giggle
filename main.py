bf98636325f90f2d199eda5576c7a83f8b019af2
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:13 +0000

    fashion ours speak relative swim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
