c5f6839b8448274338dcd1ad91d6ab74f1c256c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:24:21 +0000

    scream rub real lift sharp

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
