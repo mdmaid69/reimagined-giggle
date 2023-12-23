698b18b6bba6b05a300d8e26be1f00128f82969b
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:38:46 +0000

    steam cough breathe dull please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
