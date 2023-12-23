b2a2a9d36d0f78fa81a26405e7741c79a58b4cff
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:47:30 +0000

    turnover cool copy belief doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
