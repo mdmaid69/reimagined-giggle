be72610809cf1dec44757afb5927788d9f3704db
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:47 +0000

    statement correct invitation clean succeed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
