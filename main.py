5d9beaa74787f6e9faa48ae53aac6c67fdeb1c6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:02:04 +0000

    under light share start matter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  def square_number(x):
-        return x**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
