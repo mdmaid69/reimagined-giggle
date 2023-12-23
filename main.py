08221a6ac77b07296b11ccf155ed7d5f7a85d301
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:11 +0000

    brush somewhere instrument gift curious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
