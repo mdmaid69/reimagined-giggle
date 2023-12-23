011d1af49492203fb603eb8f1c731716fd0e52d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:29:27 +0000

    professional believe else public bother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_work(force, distance):
-        return force * distance
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
