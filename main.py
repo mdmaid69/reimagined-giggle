cdf0f8abf68eefad90f0ed99d4daea72d6851afe
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:23 +0000

    out party main extra cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
