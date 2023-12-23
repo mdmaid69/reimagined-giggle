05540a86e2c8a4fe3ab7bd5ea28f7f0951b21474
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:04 +0000

    impatient every better vanish claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
