5aeb7c0709966f32b663f4196a0ab6308391c9b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:36 +0000

    across centimetre between spring lock

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
