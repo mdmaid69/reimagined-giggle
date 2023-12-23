c0e7f99df04ac22bf3adc53a8d553ce2bb14e9bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:54 +0000

    cry almost approval near big

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
