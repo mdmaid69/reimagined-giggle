0c801eedb53a504737a9055507f15167bb892e65
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:17:57 +0000

    agreement knowledge over inquire score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
