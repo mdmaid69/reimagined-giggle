fdd3b7659e46c19f918ffb70c0aaa181eb5a6315
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:47 +0000

    meal pause maybe circumstance attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
