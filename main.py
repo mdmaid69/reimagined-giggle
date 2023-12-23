e98897b6afb94ce0b0fdf9b53baade77fb535eb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:46 +0000

    odd autumn opposite rather stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
