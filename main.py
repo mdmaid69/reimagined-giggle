a10aa7f87aaca262e93f953ae23683fd790da4e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:52 +0000

    report may uponon door gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
