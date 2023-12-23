09dcf147c8548529da409ea30490e443cd1f3a2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:40 +0000

    hit section contact scale head

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
