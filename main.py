8716baa15f1b15c0de6f12447be08a43d01a72fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:37:30 +0000

    see minimum support deep word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
