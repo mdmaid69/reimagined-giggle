0c945b7f1160c49ef31b57dd9794e4ec4a99b590
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:22 +0000

    confidence female heavy slightly blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
