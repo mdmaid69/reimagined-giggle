0365e5baa7a0a6833925bd298f2d773b7374dbe4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:52:24 +0000

    us community pin dress wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
