3ff902fce8e30472659a549566403eabe9f1304a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:29 +0000

    have sleep guilty break his

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
