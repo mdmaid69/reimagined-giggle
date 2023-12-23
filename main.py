466b224a53645e0e1994abbdabba890e466bc3d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:35:38 +0000

    giveaway bush quantity mark build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
-  def reverse_list(lst):
-        return lst[::-1]
