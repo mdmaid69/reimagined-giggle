c27c5c4c417de1cf05e25edffde4e45eb8b29fe4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:28:32 +0000

    describe collection law sign television

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
