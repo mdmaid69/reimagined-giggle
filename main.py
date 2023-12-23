441298b6544e4b3ddc55ea92718d3c6d70549778
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:54 +0000

    cost nurse vanish loss beginning

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
