7531c6a1b07cdb8747fa062de9365edb72d8bbcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:42:31 +0000

    common alive realize apart male

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
