2ec9d9bab2d7c4bb3822cdc5acea4a30dffce797
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:23 +0000

    great bath unless pencil capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
