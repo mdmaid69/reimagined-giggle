fc7b637a4295c42221ca1655a2a75d47fca23f68
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:32:27 +0000

    belief slice left woman full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
