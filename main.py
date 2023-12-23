f30f732f9fe6d71b3546cfc889457869877b61b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:23:56 +0000

    information angry minimum note chief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
