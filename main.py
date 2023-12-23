47ce629e24874f71a1d4e6f56e175019776d195b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:03:18 +0000

    maximum hard start shallow miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
