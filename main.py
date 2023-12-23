7bb39d14ae8c78746a45e9b0600751c6433e9d34
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:40:23 +0000

    round transport could talk giveout

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def is_odd(n):
-        return n % 2 != 0
+for i in range(10): print(i)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
