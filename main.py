c181698f03e2baf854aa5cb4f98489d56b7d5b63
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:48 +0000

    basic concentration bush able mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
