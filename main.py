05557a5be95f1d513beefa5d6394a26b479894f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:04 +0000

    rent even law unfair impression

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
