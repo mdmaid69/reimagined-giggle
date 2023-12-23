3496e9c625d216a4b422e0c61147deb0ea4b256d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:08 +0000

    hold circumstance exist stay light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
