a6017850d0ae3baced200ec9965de8682ca681a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:19:15 +0000

    mess slightly breakin introduce worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
