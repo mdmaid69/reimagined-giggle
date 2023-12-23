22e13bb8875853d8c8b7c43dee45fad3fae119b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:10 +0000

    increase theirs crazy create suddenly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
