3c927a5ba396468e02ea369836538d1b7c1f412c
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:44:58 +0000

    lose Pp regard crash platform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
