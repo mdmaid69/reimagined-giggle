10ed5a9eebd30c375ab6338e61ab1ae5e796d874
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:50:47 +0000

    lose fortunate imagine peace straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
