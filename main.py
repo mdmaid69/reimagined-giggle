a59944339334ecda1a1da9d380816e951f0f5358
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:27 +0000

    middle emergency funny exchange often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_current_directory():
+        return os.getcwd()
