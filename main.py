59829683ffad155f5f74748a5f7806ce81c810b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:24:35 +0000

    opportunity estimate mysterious stare opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
