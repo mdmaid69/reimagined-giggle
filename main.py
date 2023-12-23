0bbc672e8aace3bd0717d8bf9c99298804dc7daa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:41:38 +0000

    sick more crisis yesterday Kk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
