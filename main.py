1647a1f773ec1d28cd405be188b3c8a10a300f8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:04 +0000

    fairly intelligent couple juice phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
