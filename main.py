343a46ec597a51a82ab738d0794a2eaf621197a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:23 +0000

    building Bb respect autumn naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def square_number(x):
+        return x**2
