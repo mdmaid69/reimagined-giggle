e22d0307a3cba0803ff20e4dee164938531bf6c7
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:47:57 +0000

    possibility consider extremely noisy tight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
