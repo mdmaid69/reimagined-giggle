429a4d5a7d787083a7be36f9cc228b63add33ef8
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:54 +0000

    slide buy complete permission odd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
