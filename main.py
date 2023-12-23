1e4c985a4876fdc56d1aafabde5b18178fcf39dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:59 +0000

    decide pleased definite wind steal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_volume(length, width, height):
+        return length * width * height
