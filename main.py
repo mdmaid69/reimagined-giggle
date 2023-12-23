28056b5019a7148a8c5929ba5802c518d1548b80
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:58:34 +0000

    definite any free relation obvious

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
