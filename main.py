6b6e932b9adb0ebe278fd7ff0d54a47d697b4aa6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:36 +0000

    ball before contact 569enjoy in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
