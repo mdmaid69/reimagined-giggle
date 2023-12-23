1838b59dd795902ef024bb37943c8552a45c7ec9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:40 +0000

    transparent close plenty other finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(numbers):
+        return max(numbers)
