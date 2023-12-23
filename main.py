4e69f51103935f77423f91c671dca1cc5ee7ab2c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:39:45 +0000

    mistake lonely fight spot stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
