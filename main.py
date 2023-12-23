334e64b45e5e6471dbcd84a099ba2bab38b0a5c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:36:55 +0000

    entry peace although figure honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import datetime
+def get_today_date():
+        return datetime.date.today()
