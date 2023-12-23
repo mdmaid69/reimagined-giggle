9f1b09fa809f2c184c30826e3e1c4053871ee6d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:13:24 +0000

    remember clear scrap hang debt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
