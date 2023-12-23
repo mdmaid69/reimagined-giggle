e340ebcb67722df0ee2a969370fb7c3f6673c87e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:55:13 +0000

    float bite home woman probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
