90d9ff79bb46da9cfceeffc1c149d1ecb701bc50
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:55:10 +0000

    perform eat apparently various shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
