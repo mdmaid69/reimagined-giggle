b7ed1529413fcf99b32975656fb187b2fa1aa435
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:14:07 +0000

    funny touch getout regard excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
