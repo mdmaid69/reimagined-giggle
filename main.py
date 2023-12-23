8e2b9fba31e7c3fabce0db695e171453b36413ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:19:33 +0000

    change pleased matter soft shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
