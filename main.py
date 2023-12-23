5cbcf84a9d83197c326a9adfdd3c538f404b00c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:34 +0000

    giveback real guess head whisper

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sys
+def print_python_version():
+        print(sys.version)
