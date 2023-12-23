52ec8723a051ae4b9729c0c170dbe8a9afefbc99
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:29:51 +0000

    argue sort find second rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
