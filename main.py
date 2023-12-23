2b96a04a1f3a5f5ed119bcfa667b718b3fae2716
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:34:44 +0000

    view wander unit declaration Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
