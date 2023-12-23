2dcf976d023db885b01914f0a9aba12cc90a10e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:11 +0000

    best not embarrassed approve sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
