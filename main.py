caba3505d98b8e03c4b7d11fd1bf0b9ccf3f98ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:55 +0000

    separate responsibility exit school lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
