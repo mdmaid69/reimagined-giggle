a75a8eebf0ecbe1990569132ead5e8b4e53b1da6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:15 +0000

    way autumn expression juice way

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
