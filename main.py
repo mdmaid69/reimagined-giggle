9a6e90404588bf434b5fe69770a13641a0d4596f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:10 +0000

    used stamp approval right mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
