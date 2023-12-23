7a6fba905fe10d8282ecd1cde442180021ac56a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:29 +0000

    chief sentence handle acceptable pick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
