4e69df341d7219e8b90a36b01e52e28271d54433
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:47 +0000

    protest waste safety too question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
