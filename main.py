ee82d7429f76ac3380e6ec3aa8616f422725e5cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:30 +0000

    wise alone prevent clear frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
