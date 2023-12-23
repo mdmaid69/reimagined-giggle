a164896546be866dfd3ed02953895e80bfd4510f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:35:17 +0000

    culture but burn easy excited

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
