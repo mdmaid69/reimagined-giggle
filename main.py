1cec82f7775c3afbbff3703f2cf5c2a62087fd65
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:52 +0000

    ring height surprising first shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
