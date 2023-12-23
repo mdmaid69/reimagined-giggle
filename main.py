637cfa8caabc1ed05dc9ddfa50ac08098d6b44d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:47:23 +0000

    town test fun idea number

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
