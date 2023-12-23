4acd1f22539b276273e5dffdbb6cd4f34174137f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:55:42 +0000

    another holiday compare scene match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sys
+print(sys.version)
+def reverse_string(s):
+        return s[::-1]
