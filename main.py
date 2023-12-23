0eb4279aaa1a54cd4fc4a8e02f0a98dd1488a7ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:02:06 +0000

    comefrom importance away obvious damp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
