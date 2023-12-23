7f31f22ed7d55767ebb887770b80fed6b62e73be
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:20:24 +0000

    takeout squash meet now various

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
