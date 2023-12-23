5b93546d6cb49ddf9fc410038783868b0030405d
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:27 +0000

    event well tap silent model

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def cube_number(x):
+        return x**3
