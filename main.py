3f858d4798739fdbb0aaf23dab2071d32dbe3b79
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:00 +0000

    trust remain welcome slice confusing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
