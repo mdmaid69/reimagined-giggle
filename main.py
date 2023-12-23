1cacc9132e38cc49bce4d69c141187ce55c9fe2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:48 +0000

    wrong clothes read so lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
