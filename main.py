c901cc0ed4bdb060ad4408593e76663dafa3e726
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:59:11 +0000

    breakup worried reject general always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
