15f6934ce310df02e335d96a239879f9dc8911fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:51:13 +0000

    information Ll pass rain convincing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-name = "Python"
-print("Hello,", name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
