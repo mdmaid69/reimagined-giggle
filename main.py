2251fefc07fe6d883d89ca5df0322904fbbc9deb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:06 +0000

    trial slight accept scream surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
