8e8881a4baa09db5d4732a7fc60f5ad818bc445d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:45 +0000

    rescue stop turnoff unfortunate send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_exponential(x):
+        return math.exp(x)
