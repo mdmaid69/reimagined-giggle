35e0553856c56eabfeedfa88981de0cea5708af5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:23 +0000

    bite have tap average bin

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
