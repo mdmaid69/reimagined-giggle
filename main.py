4329e1d9d54874beb39ba25382ec534242528d72
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:29:37 +0000

    outside abroad possession skin act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
