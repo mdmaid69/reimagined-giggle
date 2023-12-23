2730db4e9774fbd2b93114e072c8fbc331e4a19f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:33:27 +0000

    tap stone best force friend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
