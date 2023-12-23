40f0e2993c678aa43be48264e18d191c49181c9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:36 +0000

    black arrange see wherever age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import sys
+  def get_python_version():
+        return sys.version
