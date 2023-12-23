1aaf3896ac01639464865bb7425e2c6ae3164960
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:18 +0000

    closely fire turnout runover 566end

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
