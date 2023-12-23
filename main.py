ac51295b8947d8de0bfa6a46bb3fff3e0536db8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:25:45 +0000

    ring afterwards so maintain swallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
