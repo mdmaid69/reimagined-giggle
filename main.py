53ebfe3d67708c18044182769035c89c05019118
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:34:20 +0000

    open friend average with convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
