e8d0be2147ecca2a115e62979d160a888bcb7f2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:26 +0000

    somehow equipment sudden measure pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import time
+print(time.time())
