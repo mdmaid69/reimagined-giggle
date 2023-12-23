bc4502ac450bcbf30112785e7b642389f995adb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:20 +0000

    agreement bar tour expensive normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
