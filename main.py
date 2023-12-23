14125c9c55df18980282c61870900138c58dd6d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:01:06 +0000

    try somehow play spin determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
