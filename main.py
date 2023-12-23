529bb51320a99a921c9b9411287be06df8c50150
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:59:32 +0000

    shout exaggerate rarely fetch spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
+  def convert_to_binary(n):
+        return bin(n)
