c4d9ca92ce2127a49be516300a164aaf99b5b622
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:12:24 +0000

    neat fact pleasant stretch attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_work(force, distance):
-        return force * distance
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
