15a3524a8e880efdcf2bfe0cf69a2b68b8025132
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:12:34 +0000

    avoid spare escape push dull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
