450e1c98752fcfdb2181b6e3d946149d54eb1156
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:01:16 +0000

    important play detail pretty tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
