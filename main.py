6ecded30b21c89a04bb6f891af9314a561f6640e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:07:08 +0000

    spot basically address pause increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
