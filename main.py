f90966ac96e6275146230498d520d698b6e54e7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:26:15 +0000

    dangerous score recovery less quiet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_acceleration(speed, time):
+        return speed / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
