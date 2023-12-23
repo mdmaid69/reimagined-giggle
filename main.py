22c04679fd924a798c3c4074e63e196b3e6a31ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:09:43 +0000

    it ear judge event comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_power(work, time):
+        return work / time
