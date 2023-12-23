403cd4e5cb9a0ef4bf0d7f7f05286b8a2f3c1c99
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:08 +0000

    worth everyone talkinto comfortable gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
