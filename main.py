e787c99e38deca9a76cad5188e23527634d8cd05
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:08 +0000

    building but sudden computer damage

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
