cd8776fe84528ec6eab1b40025f8b02f67ed9d3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:08 +0000

    talkinto exhibition watch light equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
