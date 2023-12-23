b82b6da61a209c3023d546b47ee1e6e5390b8717
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:48 +0000

    along total ride consult almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
