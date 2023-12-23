5357b89bf69d213134deb4487a7deed97f2e06ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:49:17 +0000

    bother over weak more hope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
