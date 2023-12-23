9c5ce8b1f044aaafd6ec084965c1d40b3f2f8877
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:15:15 +0000

    weigh often remark niggaacademy belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
