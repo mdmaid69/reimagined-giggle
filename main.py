17d6fc9d6212c9d046ead14fc420db00443349b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:47:34 +0000

    table confused special band scare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+def convert_to_hex(n):
+        return hex(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
