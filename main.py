bec1a0e768f406bc78b9603274e3277646cab2e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:37:30 +0000

    like foot thumb mistake Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
