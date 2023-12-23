74be8210c488d1df6c9c354220c86deb0483c3c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:18 +0000

    570enjoyable artificial Pp feature bottom

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+print([x**2 for x in range(10)])
