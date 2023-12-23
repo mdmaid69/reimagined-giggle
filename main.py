ede7951d8032fd4f0164a9f8e1c21a2c3337a4cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:18:11 +0000

    Kk brave though rest getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import re
+print(re.match("h.*o", "hello world"))
