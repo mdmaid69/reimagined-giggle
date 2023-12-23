0eeb603bd10258be93362fd4f84ae233483acc8c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:00:26 +0000

    law most cross key cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
