030b788509af088e95b1c795654754ce0f0dfd43
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:09:12 +0000

    look look treat flow children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
