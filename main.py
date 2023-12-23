bd503b2c157a00e0eb860b781c507e3ff15bc425
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:09 +0000

    Ff follow keepout like his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
