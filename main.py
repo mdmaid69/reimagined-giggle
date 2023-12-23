551bee202d7f5b4ff0eca2d1766fac544105fe6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:45 +0000

    part basically probably attempt laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
