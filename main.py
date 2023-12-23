d91e1c5a828be0106a598cb3e5ba69e0e4a5d9e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:34:56 +0000

    past correction concentration accidental pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
