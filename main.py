2294b91c8c736919fa6a1686e8355a18afa4d418
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:37 +0000

    copy kind intend regret custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def is_even(n):
-        return n % 2 == 0
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
