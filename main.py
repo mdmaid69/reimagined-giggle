8de9e351c6fec3cb1ed8681d46e0fb7ec37a664a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:13:02 +0000

    hurt final boil reason minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
