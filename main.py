198f39c6217c14a292393d0866fe1ae33be0edfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:52:43 +0000

    thankyou programme accept heat feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def exit_program():
+        sys.exit()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
