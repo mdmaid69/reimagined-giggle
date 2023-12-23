a20222a07aba40058cc87960724a72435d329068
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:12 +0000

    yours argument sail disagree rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_palindrome(s):
+        return s == s[::-1]
