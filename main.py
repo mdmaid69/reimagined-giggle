1c4e86e0da55f20771653aa85e4b0839b4e8bed5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:09:56 +0000

    shopping ignore below sound wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
