8b5457e593761278ed5661a80c998793b0de2e4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:54:25 +0000

    mother unless talk succeed partner

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
