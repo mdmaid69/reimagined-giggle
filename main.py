6a47ccf0c85d380737f9c69f52d1b9a3cf8fb5e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:46:36 +0000

    compare takeout Ii such face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
