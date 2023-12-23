5940c09eefe8e7c07f755969989b1c47ff000f9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:27 +0000

    perhaps wise way crime scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
