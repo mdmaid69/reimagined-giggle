e1f7a3328988b2a1591fdc81b6038fd57e295d44
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:21 +0000

    welcome last cry behind street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
