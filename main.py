f8e12b1566ed3396f88fbd01d734223f05833e8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:12 +0000

    complete even them promise discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
