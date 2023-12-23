84efba7cd98e570f14e9ac360eb196ff066471f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:13 +0000

    slice hour scream terrible breakout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
