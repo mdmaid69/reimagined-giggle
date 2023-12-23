5b2ac66132335e82b07251f6d85ecf83c59c74a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:12 +0000

    thinkthrough crash worth excellent communication

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
