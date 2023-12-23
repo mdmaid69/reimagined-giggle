184a65a3a6d935588d605540f7d74643617826a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:56 +0000

    story ring soak task bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
