ad83ca9df2c4f715085acd7d9baed90d9a5a50ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:21:05 +0000

    union inform seat left unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
