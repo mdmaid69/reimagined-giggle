574c3297f0e0aff1cbff97c2c6636605f5426cee
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:26:16 +0000

    reduction ahead appear must deliver

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
