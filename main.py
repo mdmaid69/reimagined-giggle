6712af3f05047d7cffba9451c839c08abd780ea9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:13:58 +0000

    plenty bring search clean substance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
