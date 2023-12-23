02cc6d672d50d525a66c3d62a5791e803dc94d25
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:24 +0000

    worse term answer Mm through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
