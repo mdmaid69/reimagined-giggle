da26c8cf3c031d0d53a4104bcb6fecdf91d53cef
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:00 +0000

    forgive spoil extra arrive she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
