88ea3635f996e1402be07520a570222635cfa675
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:44:01 +0000

    talk blue strike likely brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return math.factorial(n)
