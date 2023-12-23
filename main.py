6c89813ff68b054dcda3b9a0d029c9a2114a5dfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:01 +0000

    clothes like anything woman drop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
