d61f338beb969039b2e8df7391439884510e3fb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:40 +0000

    duty neither country damage coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
