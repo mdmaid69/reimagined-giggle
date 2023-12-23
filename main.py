c5ef8e1c7bef307c42f44f492d3ad3528dc46887
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:56 +0000

    effect value comeup window tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
