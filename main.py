d9ebf6c55c7064b7e81956d410dfbb5ec5072541
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:37:10 +0000

    attempt whisper pure regard broad

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_octal(n):
+        return oct(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
