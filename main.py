efdedf39fe25e8a6188c72db50934f4d26ff246a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:22:36 +0000

    first intelligent near tone me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
