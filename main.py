62199f3254a25d0ebfbe621211378eac28b52470
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:47 +0000

    share question slice manner chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import time
+def get_current_time():
+        return time.time()
