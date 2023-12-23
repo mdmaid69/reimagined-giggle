5b1136f8fb4f6f65cf3fcba7d49fc42927aa3915
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:19:39 +0000

    consult cut sell concentrate some

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
