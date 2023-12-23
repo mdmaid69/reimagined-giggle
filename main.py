e8aa4a695cfb3b45782f7a18d3d56acb65718554
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:26 +0000

    thing trade runinto start recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
