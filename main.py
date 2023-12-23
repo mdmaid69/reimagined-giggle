49cb4d34d2dc5b84421fb71eaab1ea50612b1daf
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:27 +0000

    mend flood opinion term mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
