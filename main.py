afcc4a972fec16bf08f726255920e257360b822f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:53 +0000

    sleep rather clearly handle hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
