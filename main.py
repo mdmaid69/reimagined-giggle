c738aad700d06ab2af9d2cd256a6f5d98e9e6140
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:26:22 +0000

    steam free drive go comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
