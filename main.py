433d6fe898eef42328824eaf14813173ad9bc7fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:27:24 +0000

    insist once cut notice correct

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 def calculate_work(force, distance):
         return force * distance
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
