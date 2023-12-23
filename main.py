a4c99bbcbb27d16ef1b6a7fe9d14759ef81fd99f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:50:49 +0000

    rough woman top government warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
