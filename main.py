6be4b1001718315edcdef9bfd17858947add7d6b
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:23 +0000

    quality no bad copy tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
