37334c350b8f170d2cffec86ca8fbd122d20dc47
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:00 +0000

    central height mail still murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
