543ce687925a7aee6bf72fdc5aeb70f3a2c34e97
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:17 +0000

    cover company form glad friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
