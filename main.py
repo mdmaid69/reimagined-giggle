ebd8f28e69c95aa962d3052eb0a9c50d293d694a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:59:17 +0000

    corner main back seem opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
