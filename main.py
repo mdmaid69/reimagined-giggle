84d61f6bb27fabecff81d379e7aa06446153fb0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:37:31 +0000

    lock safe slight bank lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
