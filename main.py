0598d5fc606c98c376776502f81dff27be5d89ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:17 +0000

    search passenger account explanation forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
