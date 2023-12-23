bd1ab0e3305e05689fd31aae05506ad854f3bbe5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:29:47 +0000

    but injury lift wind protest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_power(work, time):
+        return work / time
