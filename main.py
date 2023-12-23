996f0f29a5780c167a792c3b9af173b02df62363
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:36:15 +0000

    still include shock level perhaps

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def count_elements(lst):
-        return len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+def find_unique_words(sentence):
+        return set(sentence.split())
