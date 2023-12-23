83c654e5e00a96a4ecf9b051ca18c18a80391bf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:34:32 +0000

    upset strict borrow steady clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
