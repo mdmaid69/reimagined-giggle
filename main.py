9c2705606c81350d81b41a25b73be42eef521f63
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:21:41 +0000

    accept cause genuine issue ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_list(array):
+        return list(array)
