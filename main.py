dd4c035ba75d6fc5f15dd5227e13f7ea601b29da
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:25 +0000

    indeed plan answer tidy react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
