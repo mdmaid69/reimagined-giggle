31b8a6dbb090c015717d675745340843a2476bcf
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:47:12 +0000

    used broad away certain joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
