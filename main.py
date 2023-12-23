c039bb3e6c73495ac951384f920d3264142c6411
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:16:08 +0000

    spring tradition surprising feeling nurse

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
