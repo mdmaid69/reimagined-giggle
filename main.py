e830ec68d1869978213f5671ce844deac846a2e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:06 +0000

    certain rough make accept wash

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(10): print(i)
