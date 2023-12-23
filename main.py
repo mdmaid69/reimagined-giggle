d6a6ff3a88f924bcd6865beed28865d958740855
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:38:47 +0000

    funny gentle drink remember ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
