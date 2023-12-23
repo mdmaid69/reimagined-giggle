b339e1cb2b3338490ba0cce6c63ce9819711aa3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:22:55 +0000

    themselves forever often but Ee

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
