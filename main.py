cc8320c4bf39f818bae45257b8f15c733ed547a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:42:04 +0000

    clever survive error whisper breakup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
