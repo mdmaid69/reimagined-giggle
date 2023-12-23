ac51b67cf3d3149f55eee84dccd470b58e08fcf8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:51 +0000

    occur base vision step safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
