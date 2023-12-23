2c91d5ec552c63cf70334061b83212e826ecaf19
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:21:46 +0000

    squash display cold entrance edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
