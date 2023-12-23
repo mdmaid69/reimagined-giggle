35c2cc3a976a06ab9b738a870548fbe2fb014b88
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:55 +0000

    sing catch Uu piece capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
