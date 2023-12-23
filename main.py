66c93563eb26e0d8095158b88b4bb26891b9be6f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:21:15 +0000

    with connected about underneath yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
