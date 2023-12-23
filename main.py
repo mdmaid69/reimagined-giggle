07221b1581eb87754cc99e0f85c4b329a115e74b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:48:52 +0000

    tonight building guess work tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
