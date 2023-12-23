138e196f2da859a78594f4ef05f684ef9d2526a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:40:24 +0000

    worst so peaceful wide wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
