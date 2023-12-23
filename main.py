36179b570042d95b2d61c144531675f20b340d01
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:00 +0000

    safe breakfast typical way aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
