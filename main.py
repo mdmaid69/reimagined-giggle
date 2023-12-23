04e319511d870ffb0eb63d3b90fa8fad160d3417
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:38 +0000

    accommodation disapprove frightened life let

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
