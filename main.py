38ab62852bfdc7a39e8be5c0f68f6378a0790540
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:22:31 +0000

    disgusting drive do old spin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
