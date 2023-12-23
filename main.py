9f33089650fe667a226ff162131ba86bf5148a57
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:59 +0000

    pain sail turnout 567enemy roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
