1c5fa93b2695e74d152d192ed74cb9d81c673194
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:13:11 +0000

    project correct passenger shadow opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
