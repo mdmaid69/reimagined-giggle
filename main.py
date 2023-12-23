386017ede58d4d66888e9640c90abbbda38b3113
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:25:07 +0000

    discovery wonder disapprove unite herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
