8400383b330d2d8541a13291f0f819fc86d4ea91
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:33 +0000

    normal mean song run play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
