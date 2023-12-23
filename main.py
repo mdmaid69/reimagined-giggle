4b495598484041335e6fcda90b6882621a9d0952
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:14 +0000

    rely early holdout foreign absolutely

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_length(array):
+        return len(array)
