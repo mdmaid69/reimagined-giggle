d4b1a725f07e635f43f678742bbd3e25ff261427
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:31 +0000

    temper treat 568energy close hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
