9090024b208b22207e716f9e61363bcef1c4138e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:33 +0000

    find fortunate congratulations glass press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
