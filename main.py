ade099aab7be81da278fcee73a2f353b3ff37549
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:29:01 +0000

    increase extraordinary interrupt none line

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
