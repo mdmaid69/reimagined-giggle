18f46ac13fa5d2a75cc0e64fbcf8712158647512
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:53:44 +0000

    snow soon sorry anybody waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
