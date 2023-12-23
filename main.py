18357bb0e7490b9a00e0f5ea53068523f8a8de97
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:28 +0000

    journey sweet react achievement absolutely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
