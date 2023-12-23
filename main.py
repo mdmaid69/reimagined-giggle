f271d5b2e4832470841ea46fece9cf1a2dc4575a
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:20:57 +0000

    language himself full less comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
