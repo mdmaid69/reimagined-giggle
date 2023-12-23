b01cb33be22307d1f2989a8415ffe909dd628c0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:37:07 +0000

    might trap drink dream lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
