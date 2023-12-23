b5ee0ca55e0dbfc6281a52eabc1838a3b1fec039
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:02 +0000

    same among impressed guess better

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
