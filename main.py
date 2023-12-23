daf395b32a73eaf72702ff6ded8f89ef1ecb6d8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:03 +0000

    wrong Uu lose will sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
