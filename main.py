5c9c2d6c3fff2afbf7c0a4a53a4699d395662505
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:48:34 +0000

    project closed tour happen high

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def sort_list(lst):
+        return sorted(lst)
