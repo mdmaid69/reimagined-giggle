fc1dab83abeb0e6879a03d329fe8f55bbecc75de
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:36 +0000

    yes smile contents over similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
