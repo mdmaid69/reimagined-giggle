75fc7607e44c75fdd9c4c44236b9dcd5fa14272d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:05:28 +0000

    imagine wonder border clothes view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import tensorflow as tf
+print(tf.__version__)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
