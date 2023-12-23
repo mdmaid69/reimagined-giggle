ba0f94a153f6deae4750f41cfc08fedad382298f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:40:29 +0000

    comeoff book shoot medium rent

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
