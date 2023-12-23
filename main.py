60f35e475018b55b2e29ad24604bcf8ad2b54851
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:53:30 +0000

    upwards fault 571enough comment water

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
