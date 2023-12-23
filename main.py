1b49440b48a103cf299aae74b0dc942db35dc265
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:31:58 +0000

    description hand regret worst influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import math
 def calculate_circle_area(radius):
         return math.pi * radius**2
