877bf8757eb6681b4d7b0742b30c41048726994e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:34 +0000

    public neck advice various refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
