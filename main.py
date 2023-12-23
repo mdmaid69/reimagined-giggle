8db6a060505622dd99e7dab25072652d37bff2a0
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:02:46 +0000

    stretch performance legal subject theirs

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import time
-def get_current_time():
-        return time.time()
+def check_if_array_contains_item(array, item):
+        return item in array
