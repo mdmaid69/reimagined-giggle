c79bf571b8733b1cc7cbb1dc37605712ebffd0f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:20:10 +0000

    dance whom official turnaway turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
