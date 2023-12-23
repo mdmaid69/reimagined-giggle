f02df49d6b3ad7493e216b2ce02a2b26f4a3d56a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:37:31 +0000

    paper war my runaway path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_list(array):
+        return list(array)
