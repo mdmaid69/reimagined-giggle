4e5d964df5c567899cfe11b6991004154126451a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:20:55 +0000

    hardly across pupil mail protect

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_area_rectangle(l, w):
+        return l * w
