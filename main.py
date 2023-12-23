45dcb2e1c70dcdd1bd1bbc094cd92f60f1a72ed0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:30:39 +0000

    disturb but arm nearly accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
