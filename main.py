94f98e95ddf0885b44ae3341b8f2bdde58400471
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:22 +0000

    force short relief order image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+print("Hello, world!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
