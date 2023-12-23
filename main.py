de274f67b4a1a228e45b68e6112ea0786fd6c9e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:03:59 +0000

    style demonstrate top long remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def remove_directory(path):
+        os.rmdir(path)
