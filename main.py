f2abe9ec3517aa49f50a722a94791154d9f732bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:38:18 +0000

    lock just either yours meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 def exit_program():
         sys.exit()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
