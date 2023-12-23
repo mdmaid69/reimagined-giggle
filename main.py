095e4b9fe8fb79e900d75bdc44d724669c15a4d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:36:34 +0000

    flat hello might score file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_pressure(force, area):
+        return force / area
