025f93e86b4d2fda026ae7107b90cfeb9ae6a756
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:23 +0000

    brother like sense process strike

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
