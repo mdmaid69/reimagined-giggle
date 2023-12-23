d2d255505c840f30b5f8a4eccce2c76f07f84f2b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:01 +0000

    left claim light butter careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
