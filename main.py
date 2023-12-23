3bc6679c0e3daf17bf4af3e1c4a12700fb25bc03
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:46 +0000

    finish hungry season raise eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
