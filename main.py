83cd7881bfe74dbb673930f8df5f7b9080883103
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:51 +0000

    fail immediately roll firm paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
