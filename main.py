1bbd3fc0ca6fe7ab67fdfbaa93413dd41e8e4866
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:41 +0000

    grow wet getoff care off

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
