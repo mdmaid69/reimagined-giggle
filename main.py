08da2d26aff576bc72a539ff1213679fdea4e979
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:23 +0000

    something grow rare come until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
