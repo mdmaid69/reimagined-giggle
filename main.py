f7e471333d08d29649aa6577a47f4e91dc070bc1
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:06:29 +0000

    flight act scheme convinced nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
