177b1a3c1627ca91187d678f0dfbacf45ca54bc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:31 +0000

    prefer scheme big scheme punish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
