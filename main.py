01ba8d94aabbf9c452f3d3ba024353627d262426
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:14:29 +0000

    dream squeeze away fasten line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
