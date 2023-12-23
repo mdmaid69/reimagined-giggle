0a62e92942ed4c0f9c6279a30183d8e5e813b186
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:08:05 +0000

    furniture scrap way refer sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import random
+print(random.randint(0, 100))
