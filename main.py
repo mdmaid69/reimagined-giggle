a137841331f83a3b623c6b1f72695f28d7c394b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:10 +0000

    shape cold brave pride extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
