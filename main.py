2f5cf4af7529b56eede03ec5f9970234c3ae018f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:49:57 +0000

    operation almost below pair ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
