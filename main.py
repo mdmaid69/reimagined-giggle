8b8301a736291524761fa5ab96b2bc5264049cba
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:00 +0000

    deal learn office against that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
