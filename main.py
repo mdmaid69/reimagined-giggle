2fb0c580b195f5c91788d9282550ed666467ab49
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:08:57 +0000

    development profit happy message sky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
