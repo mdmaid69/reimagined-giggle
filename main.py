9a2e81a8378a47c747cd3ff3f930d6ff3cabf88b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:49 +0000

    thin spring accidental unable powerful

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
