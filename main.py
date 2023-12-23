896589e3597d2a75821aae0fff4b63ed3b117e64
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:59:19 +0000

    her wake neither scrap glass

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
