0596d843425b247c5eb7a33f4139fde100cda9fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:03 +0000

    real congratulations figure public health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
