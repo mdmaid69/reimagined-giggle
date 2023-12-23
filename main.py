161ea9d87742c14f7c91b94868a92f56a7f713b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:11:18 +0000

    broken protection demonstrate performance race

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
