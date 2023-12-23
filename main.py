1878737b7c88257f5b48f76292fffdee83a25f71
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:40 +0000

    band lock Gg holiday university

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
