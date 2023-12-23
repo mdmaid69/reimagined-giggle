cc3906c041a11d553000f1f9adff31fb5149b4f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:04:18 +0000

    score duty kind fit how

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_current_directory():
+        return os.getcwd()
