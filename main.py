5d33b43f19c22716fb188559af7c95dbdc545729
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:23:30 +0000

    school example disturb determined tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
