65559501a6644e8e99df2e542ce11356ffe30528
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:29 +0000

    turnon capital division steam bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def convert_to_binary(n):
+        return bin(n)
