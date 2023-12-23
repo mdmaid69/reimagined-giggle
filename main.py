bcbae9fe9caf53099dbaee83238a4047d026ed92
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:09 +0000

    makeinto talkoutof opportunity view reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
