3e891e7652fc5b3627e6bf8df243e1c6e3bb7769
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:17:20 +0000

    invitation emotion hurry tend whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
