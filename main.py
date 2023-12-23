b017b940302af2cbf94c5df76f8cdd61cf822c1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:53 +0000

    week clue soil Jj land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
