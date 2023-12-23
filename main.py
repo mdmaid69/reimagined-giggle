de99e2e0dc7a4dd68e7c4083640faff70ee2f1e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:10:02 +0000

    round replace immediately admire cover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
