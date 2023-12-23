2ac46f24dec4ba86e20ca6b7f218f80fd5fb98f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:02:16 +0000

    happen gradually side show along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
