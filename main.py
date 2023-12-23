41cb65bb97738569278f9c3f0e038e26212faa40
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:40:26 +0000

    challenge any wash tap allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
