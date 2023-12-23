3a212947abe9c703c912be87c1abe5a0eb31ae71
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:09:56 +0000

    lift imagine steep phone wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
