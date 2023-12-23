6b2bd9fd41c4ed59737f3fa3648bcd197d258247
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:41 +0000

    temper strict your mistake measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
