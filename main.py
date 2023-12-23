b9979e2ac601d3703431548ce46d3f56be0188b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:18:50 +0000

    beside bang motion address sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
