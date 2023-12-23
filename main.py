16f485fc65d56b059fe9a470176a7af19615d15d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:44:43 +0000

    house brown equal meaning return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
