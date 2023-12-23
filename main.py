90d30258add7013e0e79d2f3881745c50a3fed31
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:00:53 +0000

    tomorrow delight pollution maybe runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def clear_array(array):
-        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
