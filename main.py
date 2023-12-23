65c2fdcbd8bee051ab056a15a0af37eae97ce6cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:30:01 +0000

    no practice actually poison base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
