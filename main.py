cbba1d230f9695236014a440497bf04d8e84998c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:26 +0000

    hard fast likely illness follow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
