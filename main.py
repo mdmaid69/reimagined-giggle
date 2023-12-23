d857b86ee1271704edce1c0a2db0b37d9e4f8150
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:48:02 +0000

    style complete turnback likely treat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_typecode(array):
+        return array.typecode
