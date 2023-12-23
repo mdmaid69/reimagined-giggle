d0f25dcb7829793833cbbcaa98862fb2fb07ccf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:00 +0000

    but which fierce crazy behaviour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
