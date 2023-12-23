ec33b5a85876ef8814d13bf0421d864ba2ba5cb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:33 +0000

    any pronounce rescue open only

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def add_numbers(x, y):
+        return x + y
