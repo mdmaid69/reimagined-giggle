5111742ebb52b7a15bbc4fdf9b7faab207a2dd3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:23:14 +0000

    cold mix direct stamp surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
