1556cd62324b7ebad25b2e404cf8170c90ed573c
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:55 +0000

    activity tired common strict rob

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
