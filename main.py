ac5dc7bfcb29dfe717fe3c513979af5ba3985e56
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:12 +0000

    black about side all takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
