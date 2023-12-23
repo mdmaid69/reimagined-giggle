6d11ab52630863b3aa816a4caaa3257f9e07ad14
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:29 +0000

    need bright unpleasant emphasis count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
