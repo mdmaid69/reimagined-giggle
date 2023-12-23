fdbf025fd2d51dcca017f0ac2d7004d219d87451
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:41:08 +0000

    closed Itself degree smile ordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
