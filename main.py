6b1692f7a80d1487444b6c6aa96d84e4ef7553b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:05:03 +0000

    first cope tie breakdown concentration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
