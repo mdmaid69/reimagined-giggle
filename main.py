a436dd6f1001426e4038ac801ada87751f96907d
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:19 +0000

    disappointed proof date cheek hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
