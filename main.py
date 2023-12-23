bfb48c33c93263cd138f9816630e89ff4643946d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:32:37 +0000

    below wing himself than best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_binary(n):
-        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
