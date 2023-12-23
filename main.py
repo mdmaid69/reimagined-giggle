9d5653cd7f771618e841642ef98f7efbfc0493d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:52 +0000

    opposite joke several better Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
