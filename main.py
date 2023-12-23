0167423752afcad6243e9079c8e90d1e43291567
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:15 +0000

    aim unhappy class hurt file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
