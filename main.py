5d48bc43d753ead11ef2437703555c46f4b3db18
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:06:25 +0000

    talk offer especially include sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
