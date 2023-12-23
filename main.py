6397aef9b8265e9c433464882ac5fd8a40baf79e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:50:22 +0000

    market trap shout pretend creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
