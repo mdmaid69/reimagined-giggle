d7137f9866a895bcca61cd07ebe55a2e43179747
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:01:19 +0000

    relief complaint height unit fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
