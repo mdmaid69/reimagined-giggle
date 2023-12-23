b9e92cf75ef4282978e5349320f537a18aae886f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:17:09 +0000

    object test concentration prize authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
