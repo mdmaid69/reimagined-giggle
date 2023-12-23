a586bafc785605e642a6d9a8d15f5e7d8ce6c9a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:49 +0000

    usual fine breakin represent constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
