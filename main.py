e763eeb02497c73422376d7b9e7b9faf44bd9bf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:40:30 +0000

    face spin place spoil influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
