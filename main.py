d02f54aacd5cebd3fad4397b120260d1c1b7da07
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:44:11 +0000

    edge damage whistle pump matter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
-def save_csv(data, filename):
+def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def remove_duplicates(lst):
+        return list(set(lst))
