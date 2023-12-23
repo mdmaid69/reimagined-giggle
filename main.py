884fbab9776932a095198c75967c25a2f7a73e51
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:12:44 +0000

    offence next view object burn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
