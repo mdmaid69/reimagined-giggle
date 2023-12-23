adb7d6503ad7df1bb52ef24704871b7cc1010b18
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:57:36 +0000

    pair matter case flavour geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
