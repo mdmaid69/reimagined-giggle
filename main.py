60c1ccb3c3960c1bb4c663226215ac18e78e4c4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:35:32 +0000

    increase part any spot interested

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
