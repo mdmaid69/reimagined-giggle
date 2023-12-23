c13b2cbeee296361ffa0493c3bd8e22d8679466e
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:06:20 +0000

    attract nothing know shall fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
