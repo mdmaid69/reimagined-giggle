3e57235833c450f567acb8052af96ac418925fab
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:53 +0000

    view sure mind miss kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import datetime
-print(datetime.datetime.now())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
