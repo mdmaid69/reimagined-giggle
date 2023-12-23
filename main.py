342fbe11785de869bb6ce6b75288b3f05654fede
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:07 +0000

    criticize favour go forever picture

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def split_path(path):
+        return os.path.split(path)
