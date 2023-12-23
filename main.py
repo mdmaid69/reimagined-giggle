3ece2769d167ab93ad2ec65da4c2f0d30c7d3b26
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:20:58 +0000

    disapprove aim please relax pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
