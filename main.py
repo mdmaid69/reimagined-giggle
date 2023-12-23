ddcf63d388aa6557d28fadcf40874949c96d8632
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:32 +0000

    remain muscle through unlucky corner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
