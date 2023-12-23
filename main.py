b4925a8c4e9fecd4acb4f8fe54579084464105b6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:40:48 +0000

    respect discussion address uncomfortable smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
