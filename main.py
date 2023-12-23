b680db19e71f51b4d13221aec50548e4cabf5b6e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:14 +0000

    influence price past getout best

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
