45109ccd53146e5876c98a352d3009b466d8d398
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:30 +0000

    exit person yes wide communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
