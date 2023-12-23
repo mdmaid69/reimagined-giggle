fad4165c6e172b0afb924106a8d49b6ab52b4c92
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:45 +0000

    pressure gradually clean Ee set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def remove_duplicates(lst):
+        return list(set(lst))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
