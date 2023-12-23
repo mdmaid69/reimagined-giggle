89038d0a9ed75d7511391d98c21e5e34c3720031
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:45:53 +0000

    group collection out after send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_user_list():
+        return collections.UserList()
