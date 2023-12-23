729b25d69a219dba4e689287025f6500637c4207
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:43 +0000

    region rain bang round accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
