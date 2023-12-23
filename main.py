0c481398d75795d95fe5da3bd41c27252c3aaceb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:46:30 +0000

    unhappy protect more drop past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
