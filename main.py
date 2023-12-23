63c93b70a2e89cea9e62be4512975280a836195c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:46 +0000

    set measure behind interesting flavour

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-def convert_to_octal(n):
-        return oct(n)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
