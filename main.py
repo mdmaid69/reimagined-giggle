49dc16ad9ad783fa7e9bb16435f72a4a2dd93ad9
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:28:37 +0000

    sink artificial regular confusing transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
