5c02250118ab046fe99835fd4020d2f9ff9b696f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:19:59 +0000

    whichever book him learn break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
