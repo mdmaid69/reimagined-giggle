e6f6cc902969ff278d4d88c128a897f74aa8d539
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:29:23 +0000

    keepout satisfactory correction heavy slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
