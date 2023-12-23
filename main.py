8f35faa22fb058db99e7e672ce9eea93883dc449
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:43 +0000

    represent late interrupt needle Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-print(sum(range(10)))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
