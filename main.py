cdeb6c80b7e38a59d882f4c987be72a3f374d0b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:59:21 +0000

    design climb truth be situation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
