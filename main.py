b0dc66702545ab54c12bbc94eef3eb508ffb23ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:28 +0000

    belief base success sticky declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
