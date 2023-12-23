9c91f6f1da3289f96fdcb5fa93a853b8fad0e690
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:31 +0000

    may yet terrible smooth precise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
