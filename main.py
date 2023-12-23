83fd3e117829a306d4d4ed6830d084bd10389c3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:20:26 +0000

    pile truth frightened people this

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
