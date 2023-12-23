95abb6a4344caa39a7f0aff6ec281211e92281f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:03:25 +0000

    tip convincing unit error rid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
