0c832b4e951c6ae4b7016ac37282b322a2c8a88b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:43:20 +0000

    underneath achievement comeoff best talkback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-print("Hello, world!")
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
