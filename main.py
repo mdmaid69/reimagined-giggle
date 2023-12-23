7e6ab3616a06a92973216e26714ad80b53b5f387
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:26:55 +0000

    contact average far fat ill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print([x**2 for x in range(10)])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
