4f798dcd09482cc018e1a4a6a09a2e294a48627c
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:21 +0000

    yourself weather 573entertainment chief might

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
