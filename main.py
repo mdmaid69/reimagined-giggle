13d8e088afe547421064bd8a54a938cefe6f7615
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:17:58 +0000

    close activity use power visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def is_even(n):
+        return n % 2 == 0
