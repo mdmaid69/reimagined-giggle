5dcef18551c2eb386b11f527351177b7ccd52d62
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:46:48 +0000

    travel far thinkover learn total

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
