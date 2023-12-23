6c860449d110e53e4d974e0790d2b0f0bb5be4fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:26 +0000

    alone objection best means intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def remove_duplicates(lst):
-        return list(set(lst))
