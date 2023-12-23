bf6ff7105375ecda3da86f036815d9058ccf136e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:08 +0000

    wash split weekend Qq fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
