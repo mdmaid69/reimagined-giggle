2b00308c70f6edcb3e5cf307ee4d499166e07ca9
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:33:45 +0000

    gather warn though characteristic situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
