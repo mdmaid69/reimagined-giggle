047fc74d0a74e38cc1fc94179e2cebb4dec1e0c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:27:34 +0000

    same major frighten save juice

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
