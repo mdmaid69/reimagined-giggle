7ac30d24e2efc415a9beb17e8bb3fcd3e077218b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:53 +0000

    duty bringforward scale recently trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return math.factorial(n)
