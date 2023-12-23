f9c329b9edc0fb79a0cc297754dcf7a6062743e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:04:50 +0000

    ocean relax comeround compare Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
