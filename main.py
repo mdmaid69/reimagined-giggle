b8ddd28355c1f0848c0c78df5eec10062e09573f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:49 +0000

    fond depend damage mind fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
