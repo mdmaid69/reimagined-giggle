452a7b92a3cb3d22e14b952f098eeb8348138c8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:27:55 +0000

    note children forget pure break

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def sort_list(lst):
+        return sorted(lst)
