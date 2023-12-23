cde2a2267c29f39cb3b3825b61ac5d0decda913e
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:37:54 +0000

    smell separate pay bit bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
