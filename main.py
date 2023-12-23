6216c42ca12369ecd68e39b9141a84f2280d8243
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:05:27 +0000

    smart break right pleasure message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
