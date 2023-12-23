a16653b16ee7255f3c522ad94afd08457821639c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:24:32 +0000

    movement complete splash smile true

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+print(math.pi)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
