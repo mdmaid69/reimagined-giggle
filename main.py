5ee809f9a7e5a2e5382f518cc631b1cc38f6494e
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:15:42 +0000

    possibility independent sister matter dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
