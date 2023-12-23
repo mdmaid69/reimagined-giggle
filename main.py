5e9a7dac36ba1960875731032d484ad6667bba26
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:25:46 +0000

    permission actually card destroy cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
