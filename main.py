bdea2f2043cbbe2664025a6872d2ed0dae26769d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:30:57 +0000

    best except callup conduct fix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
