8ea11bc664bd5ced1147c15ff19bcfa77776b030
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:34:33 +0000

    stone dish crisis sky friend

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
