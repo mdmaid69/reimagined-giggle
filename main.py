7893f3c768c89a2f7737af575eba4ae2546610fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:03:27 +0000

    name tired guess avoid count

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-def count_words(sentence):
-        return len(sentence.split())
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
