6741e06bface531fcbf5025abe606e3fa5072d42
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:39:28 +0000

    year invitation nature matter tired

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import sys
+def exit_program():
+        sys.exit()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
