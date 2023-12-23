d2ebfdcdd60335f01261b6bd378884a83c4dc75c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:26:50 +0000

    well positive Ee stage 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
