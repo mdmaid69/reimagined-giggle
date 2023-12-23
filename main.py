04a5f14dcedd24a76c186329413fc2e11853817e
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:33 +0000

    valuable write hope bring eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
