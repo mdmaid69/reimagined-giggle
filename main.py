ce36e480637410fad4b401f684f6f2d0663109bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:16:40 +0000

    joke punish big condition meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
