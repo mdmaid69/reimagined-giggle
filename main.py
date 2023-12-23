81c467b3704aa903d4940442111b2a6f42e122cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:35:15 +0000

    lend work address century shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def add_numbers(x, y):
+        return x + y
