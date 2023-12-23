11913728cd76f72caa878142a588b4daab638bb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:34 +0000

    alive respect question cough age

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
