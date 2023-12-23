a9709878bc7e85dae88f82c167fe133701f3b123
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:22:42 +0000

    sharp interview capital question any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
