1412517059932be7718fb327a34ea3aed1649cea
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:08:09 +0000

    list pleasant mouth stage wall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
