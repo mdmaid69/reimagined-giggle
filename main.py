a99e7b71a188be2d06d58d1e8ee99cf8467d871f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:39:08 +0000

    hit patient spot getaway rubbish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def greet(name):
+        print(f"Hello, {name}!")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
