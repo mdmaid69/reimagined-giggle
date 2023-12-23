304781397be1916716cfb44993c0916c6891baf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:11 +0000

    guess whistle switch ready live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
