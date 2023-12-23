4c0339aa224a454d66a3347eadfd33ba63caa069
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:33:18 +0000

    squeeze office telephone character picture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
