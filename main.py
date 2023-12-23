aabc219244289253ae72a9b397a0f4458031a099
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:42 +0000

    Uu fun mine square Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
