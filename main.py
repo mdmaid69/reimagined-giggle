a90fbbe2349e20e0a85c1275b20d08514b5f9c0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:59:27 +0000

    kick left must earth left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
