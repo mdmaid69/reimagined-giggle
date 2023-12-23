69279d2e9b2229a0f8c9d4ec2ef45088fc2ac19e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:29 +0000

    tape than sure nature better

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
