9dc8d1f36f991905510a489aa4fcc7cc3e5f6b56
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:12:15 +0000

    everywhere under border fast lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_bool(array):
+        return bool(array)
