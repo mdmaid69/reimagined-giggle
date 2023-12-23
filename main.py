80f72d739b371254957d555feb896ebe59b95ae5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:34 +0000

    finally difference free enthusiastic ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
