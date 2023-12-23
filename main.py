bf3586fc9bb7fcbb81cba6c506b54cefef383887
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:23:24 +0000

    pressure surprising measure bit direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
