fccd3fff16b0fe814680ed3a0811d78d3011a64a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:14:20 +0000

    instant push comeover shop stretch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def remove_duplicates(lst):
+        return list(set(lst))
