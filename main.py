771af003f388c00def8f7fd75e7b39f8a50ffbdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:38:48 +0000

    silent small talk worth sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
