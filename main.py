24495ae5e68fc7fb8bcf26a7ae8de4e0eb72289f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:56:39 +0000

    brush low mistake greeting mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
