33a8912405e5715b3a5e85b28c352e767799838d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:25:28 +0000

    skirt face frighten favourite pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_arc_sine(x):
+        return math.asin(x)
