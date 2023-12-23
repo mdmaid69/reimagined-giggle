b79ab07668c96b34dfd7ac98b7a1fba0ee5be13f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:38:26 +0000

    correct however near frightened prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
