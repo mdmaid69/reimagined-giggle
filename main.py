0396aa0587afaf145e5adc41acd60dcfa2621783
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:33:42 +0000

    willing objection doubt hope false

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
