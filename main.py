e8ea8e51c4c8dae5f248492fb3f74be20a9a63cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:29:02 +0000

    correct care burn sense hit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
