66e2008b24d987737665b581a7a157c2fee6e9ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:19:27 +0000

    behind truth pump female bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
