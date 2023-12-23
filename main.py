dae76ea9963c613d8badeddf1a87ca64f2e860f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:12 +0000

    colour present excited bend film

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
