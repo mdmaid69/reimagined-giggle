9c396119b15fcb659f56a90143e6278ef33b824b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:54:51 +0000

    think object trust better team

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
