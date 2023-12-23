219991c44fb239d74fe9d990fa3438d3cf5ecfaa
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:40:40 +0000

    lose idea certain 565end dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
