46164d155fe1aca096646df668ecc245bdd1cb53
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:46:20 +0000

    surprised gas perhaps rush glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
