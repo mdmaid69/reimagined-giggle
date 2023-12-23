948e2ee83c30ff4e290ebbdb202181339e8fed9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:53:04 +0000

    blind immediately on offer art

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
