72fb3175468597fa94b9b8eca3f870a6ecd493e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:27 +0000

    rid neck family estimate card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def remove_duplicates(lst):
+        return list(set(lst))
