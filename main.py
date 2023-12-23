149becae2b67f1e09689223bef773c09afbe321e
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:37:28 +0000

    instruction control centre history second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
