f2ad0ab87c2caf209a2ae75db41269b1df50291f
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:01:41 +0000

    support kind seat deaf name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
