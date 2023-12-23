081f9a8c2d0fe34a86971db6582da8fec81a7980
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:15:24 +0000

    disturb half trip address arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
