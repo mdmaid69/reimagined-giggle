78822974d66976ff758c1a7073083e2bb2e30fdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:42:00 +0000

    bye interruption mistake punishment interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def count_elements(lst):
+        return len(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
