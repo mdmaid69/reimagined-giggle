e505ec6d22afac9ee6d1ebeed294dfb442ec24e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:17:27 +0000

    house turnoff suggest Ff post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
