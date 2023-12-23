ed530dfe363726d4f7ac66714dac47a8a0ba253e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:31:52 +0000

    treat either pocket just apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
