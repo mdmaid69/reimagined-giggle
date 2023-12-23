5026ebb21638a94bd4680c12a80fff9ff810bdee
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:05:03 +0000

    fine television smile respect false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
