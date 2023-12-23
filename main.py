2af880639d380720286760dee24ccf2fc8114853
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:36:45 +0000

    play block main badly workout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
