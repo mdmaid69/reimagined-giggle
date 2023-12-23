85fdfbe65593c9b0760c5e3c4b01c99ca619856b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:19:53 +0000

    breath complaint love environment fond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_bool(array):
+        return bool(array)
