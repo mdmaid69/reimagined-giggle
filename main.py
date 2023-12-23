e419b1caa01012359a220efbbb7eba1cfd7eddbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:03 +0000

    sticky difficult Nn face demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
