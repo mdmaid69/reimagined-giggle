41d3e1d88db2a23f57f516bdebf7f4cc52d83bdb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:19 +0000

    desperate all gift view shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
