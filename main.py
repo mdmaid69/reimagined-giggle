1e0ce1ad0ef09a6a1fc324c8d3630efb8e4f5f65
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:11:20 +0000

    upwards Qq emphasize flow expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
