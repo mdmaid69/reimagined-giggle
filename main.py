b3d26d99d1e0be7c1962f34f0f102421c0c75a91
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:24 +0000

    tread fight throw skill here

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_max(numbers):
+        return max(numbers)
