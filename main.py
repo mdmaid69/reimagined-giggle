48e5e6dc8d65e128bdbf158265c0babaf5797fee
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:54 +0000

    roof control jump once as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
