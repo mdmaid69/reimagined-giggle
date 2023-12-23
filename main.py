b010caee9f44a7021f9cf316349b151b0099a85f
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:16:49 +0000

    yes lock boat waste upstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
