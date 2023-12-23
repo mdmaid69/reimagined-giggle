e089f8c6ee6b19c171d9a9e3d03a066e1415ea83
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:01 +0000

    active picture head face coat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
