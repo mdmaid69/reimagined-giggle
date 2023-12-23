58a8a3399723323f5d30329db397f75b1c2bc4b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:29 +0000

    care across wake dry careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
