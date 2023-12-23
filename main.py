89c0ee3406eae35bddb1644d2edb2f91eb247544
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:22:33 +0000

    colour present hot which live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
