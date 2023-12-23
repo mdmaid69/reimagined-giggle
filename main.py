c199640da2f3c441ce6ffb83871e70b5d72e92f4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:13 +0000

    comeout wealth connection frightened at

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def reverse_list(lst):
+        return lst[::-1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
