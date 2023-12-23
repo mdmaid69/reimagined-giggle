b5daf002ba55fb56c0796994119939e97109b19d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:06:06 +0000

    commit centre feel mix injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
