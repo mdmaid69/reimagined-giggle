9ea7aea349396dcc88328ee0f7d17a333d3bde3a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:55:07 +0000

    discuss contribute lock man weather

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
