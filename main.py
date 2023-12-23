492b0496bc19d1e3aabde7a6a21c8e360f5335de
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:51 +0000

    cold attention 566end careless slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
