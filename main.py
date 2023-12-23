df2910b9e48cc424d998ad53f6f744467ec85c75
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:15:51 +0000

    Ll hit sentence scrape capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
