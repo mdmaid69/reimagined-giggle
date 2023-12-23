489eda7f55b7113bf67bdead7bc8fabed2cb9f26
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:04:51 +0000

    sensible spread free tone head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
