631a7cf21e62a7cf04bf762beeb2634ba47f50b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:34:04 +0000

    medium they Oo any clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
