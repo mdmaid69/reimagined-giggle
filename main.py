abd3bd5206826c76a972355c6e53401ab123b3db
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:57:29 +0000

    charge feature preparation arrival feeling

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-print(os.getcwd())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import time
+def get_current_time():
+        return time.time()
