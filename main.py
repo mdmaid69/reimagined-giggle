24245a4a1acd03778e328619495a7a17eaef0eee
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:49:21 +0000

    choose notice better onto bar

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
