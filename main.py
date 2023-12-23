a62cd548f640d084a274169b861964af5197f440
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:05:03 +0000

    give slice steady thinkof refuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import collections
 def create_priority_queue():
         return collections.deque()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
