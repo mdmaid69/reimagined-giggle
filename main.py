32e9ffab52b6b3c89bff0391aec9d8c6030162af
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:20 +0000

    pick will hand difference next

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
