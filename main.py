592042a93aeea65fff72c332ea3362b50aa6bb35
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:27:49 +0000

    achieve obtain height level home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
