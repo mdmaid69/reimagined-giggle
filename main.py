6f7daf00311fb7821d92aae1575613eea5a474d7
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:25 +0000

    workupto think rest warn female

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
