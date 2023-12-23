dc0dc6584be172c7087e7c6c84d591a145a468f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:35:08 +0000

    callback trick love argue usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
