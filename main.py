d77c95b05538c259b11c689ea3855d44e7b2e04f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:06:34 +0000

    much hair phone breakdown pity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
