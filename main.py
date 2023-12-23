fc67ef74e021dc468ac7898e402514cd508933eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:55 +0000

    bringforward national force to could

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
