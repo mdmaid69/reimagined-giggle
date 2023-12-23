1f41b8f812e394bc6bbae6d8661b8067658e3765
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:16:59 +0000

    whichever expensive side attention whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
