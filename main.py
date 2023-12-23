30de23e07e0b9b0364dc77f39d717b57e4d04b70
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:00:07 +0000

    practise communication gradually address maintain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
