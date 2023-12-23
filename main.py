37ebdfa90e514cd0f8c3def395e65a5e979e4fe4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:10:26 +0000

    spill entry moon failure visit

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import logging
+def log_message(message):
+        logging.info(message)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
