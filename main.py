c42b371066e29824238c9efc22396876aa7a5da8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:55:23 +0000

    open wise still takeback plenty

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
