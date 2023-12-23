6bd9ddf3ff5badbb5d3ef32416aea40397cdcca6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:51:22 +0000

    wash male urgent word wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
