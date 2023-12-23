a2e083456fd37f9affa23214e19c3cc76761740d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:41:45 +0000

    bridge identify circle bother confused

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_list_from_array(array):
+        return array.tolist()
