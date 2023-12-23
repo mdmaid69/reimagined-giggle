0984de8c4896015e3072d9c819bd3e7dd971ecec
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:22:07 +0000

    air cry fight pay 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
