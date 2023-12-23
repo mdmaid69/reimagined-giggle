59f44751c1a9d6980ab148c01145ffbd753d4adf
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:40:25 +0000

    quickly arrive last dangerous pocket

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
