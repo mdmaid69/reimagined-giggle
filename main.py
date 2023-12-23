0b16f742e603346af2e05bffc8477eafa28f2d8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:46 +0000

    pain before enthusiastic description collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
