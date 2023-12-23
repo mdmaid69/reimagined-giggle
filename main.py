706991c9b404123875410875892c5a959ec4cdc5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:14 +0000

    content safety listen scream rest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_pressure(force, area):
+        return force / area
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
