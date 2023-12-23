78c1dda53946d4e9616359e6369461616c7548ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:00:16 +0000

    storm only but bye notice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
