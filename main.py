085c177daccbf44264390659f65500dc3643091f
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:02 +0000

    win talkinto turnover rain find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
