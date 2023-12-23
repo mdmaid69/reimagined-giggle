8e003b7e177b6be2fd695e467ad8576e0819c002
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:30:53 +0000

    run hear exam partner special

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
