9f16abb6c994b439633598931a476f449d0d78ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:36 +0000

    happen danger get keepout prefer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+print("Sum:", sum(numbers))
