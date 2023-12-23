94dd59c389557b11cfda9155ec4caa460c17946c
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:57:53 +0000

    572enter mail blame contents about

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
