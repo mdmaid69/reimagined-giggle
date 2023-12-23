12fc6259e6b8e9f849b9eda82de65c721b718942
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:10:33 +0000

    tongue concentrate variety debt run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
