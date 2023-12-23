ce8dd8dfad346f5e2d48d0b822c387dc7e89dbeb
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:35:17 +0000

    score turninto something confuse runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
