20ffa084e8a81f5e6469226d6d80f9722b4dd78a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:00:40 +0000

    clothes love song shelter old

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
