a3ca532b27f53241b22281d898c2e5a18a5ae58d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:08:18 +0000

    bar tour talk strange so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def count_words(sentence):
+        return len(sentence.split())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
