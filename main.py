b79433159278db2eb56fa88540f840e1e381d67d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:11:05 +0000

    before copy government moment part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
