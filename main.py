3fb6ee90d61a72e898a30325460b1c12f998ef1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:10:38 +0000

    through close account immediately wash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
