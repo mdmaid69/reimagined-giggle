cfdcd0211247e15ad9315c0de2537073e0754609
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:38:11 +0000

    trouble consider scale instruction support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_str(array):
+        return str(array)
