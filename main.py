3812a24f7055c7e8125d2b5f2c8ebec52d913a14
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:41 +0000

    estimate 565end opponent claim wild

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_directory_name(path):
+        return os.path.dirname(path)
