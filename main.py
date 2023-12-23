8691138b3a141f3c7c9e0ac868720bee9325b435
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:48 +0000

    pair arm tap breakup free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import os
+def remove_directory(path):
+        os.rmdir(path)
