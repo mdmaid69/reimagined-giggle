90160201235eb48a4a91e876fb490dc581189218
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:10:08 +0000

    total black cap perhaps event

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
