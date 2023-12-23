ae8baae613bfa93a97766368c38e832a82286079
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:38:24 +0000

    regional provide computer morning activity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+for i in range(10): print(i)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
