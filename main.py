8b5015a2e5331f05970d1206ae34e954bbe08e54
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:11:48 +0000

    hit translate herself comeback breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
