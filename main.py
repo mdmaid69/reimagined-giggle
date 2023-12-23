6d352441a3c8c1a8a27a6609de7b071e374ee3f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:28:32 +0000

    duty slight while sight butter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_length(array):
+        return len(array)
