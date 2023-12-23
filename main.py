fc49557cb6ee9cc2cfd254dd86d320ec7336d98d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:25:44 +0000

    chat join invitation band thanks

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-print(sum(range(10)))
+import collections
+def create_stack():
+        return collections.deque()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
