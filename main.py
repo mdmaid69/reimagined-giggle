40dc8260f2934189082726542dd6c4fb7f92434f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:20 +0000

    all error create ear goout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import collections
+def create_stack():
+        return collections.deque()
