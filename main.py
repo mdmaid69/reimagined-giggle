7c6ede1ae84e04e2a1c256c966cbc13f97aee031
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:02:30 +0000

    who pump short school one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
