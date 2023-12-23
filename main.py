4ae0d8c2d6a5c0322f578657b75c6cc0bed8c44b
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:27:06 +0000

    comefrom while desperate store kind

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
