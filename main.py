52545fcc6e5a310c6f92441b97ec57e15a2acf1d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:13:35 +0000

    comfort crash light impression somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
