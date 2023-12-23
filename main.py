222ec0e86f628ceff125b1605258937589daf0fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:40:58 +0000

    tight under description effect heat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def convert_to_hex(n):
+        return hex(n)
