9b45c5eb8cb61d8e0c82298f81352ee35bc66a86
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:20 +0000

    nice it write copy memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
