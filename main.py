fc9539ecc22dbe49c74df3ebe22d9205a958dc96
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:40 +0000

    goon precise sit cap outside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
