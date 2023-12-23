38d10caa44e0e7e1e9a1b77501868f960fb74127
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:14:34 +0000

    gooff air poor another country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
