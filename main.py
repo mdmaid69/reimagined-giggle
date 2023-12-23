eee26b8b72d6d4eb5b7d71495ce6d8c9ef44f4c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:01 +0000

    Zz interesting in enthusiastic hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
