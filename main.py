f640c2f080b8b9e1c4b481ea95311c3067010cfe
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:28:09 +0000

    term love give live pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
