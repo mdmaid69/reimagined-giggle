971fcac58d78a1cb1edfe0676cc10c67d159375a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:59 +0000

    gap week reason car custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
