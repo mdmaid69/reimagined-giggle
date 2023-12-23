5fe2b81f4029effd83d84fd3cb7a12e305de7369
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:51:18 +0000

    bet opponent trip expect tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
