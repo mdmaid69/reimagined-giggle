b10537a7140ad34d7695545f104ce1056c8a0e93
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:02:36 +0000

    general defend one wet whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
