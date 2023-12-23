ee8fb6b37b2054158ac894aceaa0382608f52cb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:43:44 +0000

    goout the couple moment impossible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
