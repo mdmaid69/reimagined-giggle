510e525d77febfc93ed6a56841110ac12e82ad57
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:15:05 +0000

    surface stupid week warning comeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+print(math.pi)
