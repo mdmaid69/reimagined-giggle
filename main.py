bfa79cb607f241ccf5fae5e01929c41b3eefc76c
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:46 +0000

    snow impossible recently reasonable compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
