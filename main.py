07d4adabeb9cf16501359aa25d69b846d079da6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:03:38 +0000

    notice collection OKorokay exciting choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
