9d31ffe4002d49091b8d4f3718ca76c7856ddf41
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:33:31 +0000

    wash rare lean change get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
