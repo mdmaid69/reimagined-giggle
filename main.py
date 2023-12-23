6376b914b8480359d16c075c43b0fffc66fdcee5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:32:32 +0000

    love mystery own immediate object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
