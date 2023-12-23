05c66de4df81163cf11e2dc6a6466b26aed649c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:00:32 +0000

    dance will mark watch breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
