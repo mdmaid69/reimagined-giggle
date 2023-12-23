17c7914514a325b549a3e462dfeeb50931c90cc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:01 +0000

    broken noisy spill use would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
