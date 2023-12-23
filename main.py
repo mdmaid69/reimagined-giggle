b5bc5778f5a7fd275ceb4a725c3c4ab4cb046488
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:06:03 +0000

    impressed rub room different sentence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_volume(length, width, height):
+        return length * width * height
