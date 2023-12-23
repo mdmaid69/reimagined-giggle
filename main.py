e65acb1d15554fe2bf4657de470fd3814b0ae13c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:07 +0000

    children something suggestion cross patience

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def square_number(x):
+        return x**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
