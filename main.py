cc4324011b6ac2edf0f54290dd76299224b598c5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:53 +0000

    deep flight argue reduction hour

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_pressure(force, area):
-        return force / area
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
