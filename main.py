3d0297090da75b0ec801e49c62e06926c225cfb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:28:35 +0000

    set work so he niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
