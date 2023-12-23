ba0e4c527fe30e8c67df6d2d56cad51b15a1e1e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:03 +0000

    view lookup stop cap slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def square_number(x):
+        return x**2
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
