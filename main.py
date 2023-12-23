b698c27abcc466e706ec17080163b2d18e1ab049
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:38:01 +0000

    hit stir sting last shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
