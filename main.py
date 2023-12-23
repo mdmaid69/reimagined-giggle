c8196a6fcefb9d171a451b4ed32edc34aab05467
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:39:27 +0000

    extra set like admit fold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
