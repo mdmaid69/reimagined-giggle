716ec0a7e5ac3e491c249d3be303b3f4518bd180
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:47:26 +0000

    content division miss square demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
