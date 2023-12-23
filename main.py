978ab5e8240ae78647ebcfc7ee8b97de5d7abdde
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:53 +0000

    see choice fall if uponon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
