54e7fc15229b8e545972723f6622eb6af1e4d7f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:02:30 +0000

    criticism deal callup draw past

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
