ffc7b8df981068db689fd49ead8eeb8117e87547
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:20:14 +0000

    meanwhile survive lie design exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
