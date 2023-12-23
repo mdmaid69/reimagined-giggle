434ddeaf1adad7dc6fb4dcb14f8002e5bba765df
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:51 +0000

    life whichever result hole personal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
