a596ee9dae338bcb279d7aab12b5e9f4d42479c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:00:47 +0000

    go read could middle rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
