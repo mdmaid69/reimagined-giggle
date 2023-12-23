8573792927143efe28dce3dd44fe693e0367ce1a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:28 +0000

    may keepfrom true interview worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_min(numbers):
+        return min(numbers)
