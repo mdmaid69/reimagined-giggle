c44a82115dacec2469db3140250e786612165ef5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:14:49 +0000

    as strange grand fail hit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def convert_to_octal(n):
+        return oct(n)
