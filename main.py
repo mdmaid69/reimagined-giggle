4bc77881b62252f8208f30514f27c8438bc690e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:46 +0000

    live funny OKorokay yesterday offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
