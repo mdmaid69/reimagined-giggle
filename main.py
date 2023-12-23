7b7c3f256f1e311cc0dea4fff2ab6c5a7d8c29a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:41:56 +0000

    spring back uniform confuse vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
