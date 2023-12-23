d4c0cfaddab2fe61d765f748dd471863144fbd46
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:47:30 +0000

    danger her view field need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+import sys
+def print_python_version():
+        return sys.version
