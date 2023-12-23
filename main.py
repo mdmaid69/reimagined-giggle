7138f2bfca5ad598b243110338795b184a9b8515
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:49:24 +0000

    Ii Mm any meaning print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
