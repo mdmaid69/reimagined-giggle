bf694c7791bb6b41b295898e275584b899257c3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:30:08 +0000

    miss occasion truth present pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
