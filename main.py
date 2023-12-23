0f1d3b2560c81e9303811bf721265a93dc80712b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:50 +0000

    call thin speech colour bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
