70e5c95061348bba4a518082571d118bcc09a040
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:14:34 +0000

    fortunately car sudden hour press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
