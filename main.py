692d2a86913646be4435bbef45a38b97484b75d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:59:38 +0000

    attach shut Mm stiff operate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
