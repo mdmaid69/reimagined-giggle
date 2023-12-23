ece7512c5c86dfdc412061ad0d69a2bdaf8ec6c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:10 +0000

    advice yourself oil push month

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
