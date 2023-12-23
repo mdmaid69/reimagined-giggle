716bf52eaafcdf5dbe55126599fb96efc8894135
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:44:35 +0000

    season start standard grade well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
