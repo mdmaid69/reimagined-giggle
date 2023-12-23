b0522e36d140a3cac463709abea4dc5dd40d97c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:07:01 +0000

    bath insist container platform spoil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-print(sum(range(10)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
