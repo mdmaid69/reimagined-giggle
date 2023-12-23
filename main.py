f10d121f30b9adbd3b1bc506d4bddc7b1bf4d402
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:26 +0000

    side so second section have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
