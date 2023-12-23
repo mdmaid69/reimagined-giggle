d933d14a9a80f5535d4395864c7780534211ff24
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:41 +0000

    offend explain sorry mass reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
