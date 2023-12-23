71ce837de7c2a9b6fbfbbe87cb94306aefb60eaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:18 +0000

    flat dream form free account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
