3de9dcd9d3724a64779d5a1f4165806243ce286d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:56 +0000

    serious mean operation clear house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
