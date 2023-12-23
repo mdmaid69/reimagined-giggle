d322f2291e365fb6ac46d84dc0a363a2858e2a60
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:17 +0000

    pleasure control between ball bar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 text = "Hello, world!"
 print("Uppercase:", text.upper())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
