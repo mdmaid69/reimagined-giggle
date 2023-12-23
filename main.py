10b8e3136eb35c5f0fc36c7df2f2e8db2854babc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:29:01 +0000

    image turnover definite start ought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
