4f7a44852101ea63db16d55865ed0050a307ebba
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:30 +0000

    comment onto commit stretch consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def add_numbers(a, b):
-        return a + b
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
