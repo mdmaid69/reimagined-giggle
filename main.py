34be69df21f9eaf8b394df455a74f96ca833af7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:15:53 +0000

    main spray copy everyone satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
