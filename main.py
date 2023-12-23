236047c0884e2182175ec968d28dbf88c9e78bb3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:59:58 +0000

    pile tool estimate badly every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
