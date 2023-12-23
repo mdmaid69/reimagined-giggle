129fa412773cc8e6ce5fdfbc730783a1af03ba4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:52:32 +0000

    undo formal one objection bath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+print(sys.version)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
