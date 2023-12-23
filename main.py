186d3daa01c4ce8f52757d6e569e62b635b25e1c
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:47:26 +0000

    continue cross thinkof visit instead

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
