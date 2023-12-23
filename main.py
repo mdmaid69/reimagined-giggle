f241aab7808046336a1a0acff129f1c96657cc97
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:12 +0000

    bag plain wall small basically

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+print([x**2 for x in range(10)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
