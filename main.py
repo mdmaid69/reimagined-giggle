e4b1c914c26660ce292f1de19f28c51e96f0faf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:19:41 +0000

    us reasonable unable risk intend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
