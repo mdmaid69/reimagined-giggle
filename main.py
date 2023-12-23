9602b01bba30264f4bd5d3f17775eb9f383fd2fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:40:59 +0000

    plastic arrival to regional protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
