b27a68d66bf76d2474172d1c48ae030837e77d88
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:25:23 +0000

    work holdon not best with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
