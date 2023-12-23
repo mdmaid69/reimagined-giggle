00c0fb63a7c833495f54e1340e250c89a9a7d206
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:43 +0000

    rob laugh stone soak usually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
