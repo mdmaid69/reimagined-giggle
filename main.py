6d5182a0aa6f9addc610f30ea7e46072c8967ff3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:31:33 +0000

    close breakdown cry help short

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def count_words(sentence):
+        return len(sentence.split())
