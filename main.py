fa6634a5422c600570fbcb6f6993376e51ef4aa2
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:19:17 +0000

    answer together coffee runover juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
