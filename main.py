a1e62e99cb4f30097e3183bdd7a1b263b1380b93
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:15:17 +0000

    shock confused justice depend crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
