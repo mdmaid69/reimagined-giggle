600782a1ee32488fea3e2c17b7b03353a2925d7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:28 +0000

    frighten survive dance sight close

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_error_function(x):
+        return math.erf(x)
