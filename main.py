e6c35de80a770e0b599457d028a4fe9a3a32ee64
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:41:05 +0000

    wish story Ee interested over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
