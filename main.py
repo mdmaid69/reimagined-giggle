2596f7f1895bd9ad6274d8c0168c43d0798086e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:23 +0000

    escape broken so spare mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
