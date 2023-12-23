8af2f508c3fbd029d73d45891069feec93b8634b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:36 +0000

    operation extraordinary left fire love

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-print("Hello, world!")
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
