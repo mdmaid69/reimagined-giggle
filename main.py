2d93212e582c560fefb83832fb3d218bc43df698
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:59 +0000

    cope subject substance poor joke

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def convert_to_binary(n):
-        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
