4fc4341828a0c3d4ea87d610f5df8afd20d22821
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:11 +0000

    square mark pay basically bend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
