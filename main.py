56e4bc769cbf0dfde8e6ba39749c19e753e7d102
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:01 +0000

    eager prize Tt arm along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
