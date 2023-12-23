c7117fc9b824e1feb33cb3a01b59b86a3fe69bc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:14:29 +0000

    willing discuss firm 565end disappear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def remove_duplicates(lst):
+        return list(set(lst))
+def add_numbers(a, b):
+        return a + b
