9147efdcc0967b5b19dae4d1054e8f92bb833d67
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:46:11 +0000

    fear noisy injury gap bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
