f4cb6af69b60ef025a21a1f2370b7ca5b93ad4e3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:56:20 +0000

    opposite proof talkinto present hardly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
