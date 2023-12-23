8a83b971c639b236b744660e659e084b547b0940
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:17 +0000

    total flood little expression bored

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
