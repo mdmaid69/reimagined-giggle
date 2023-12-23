15bb71d53459b37e49d514d7f4c316e096eebfd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:29:40 +0000

    professional offence should brown no

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
