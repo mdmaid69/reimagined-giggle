e194384f02818c06ea2195c4627bf469df02a148
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:34:22 +0000

    during put murder ignore spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
