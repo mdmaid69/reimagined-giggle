c31f81c8faaf6957b4d41b6c9423ab132b5a6d4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:25 +0000

    post receive necessary refer anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
