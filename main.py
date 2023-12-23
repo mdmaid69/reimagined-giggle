79673f44b30049566514d8c5019959b62aedf30a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:23 +0000

    beyond bound criticize most neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_list_from_array(array):
+        return array.tolist()
+  def subtract_numbers(x, y):
+        return x - y
