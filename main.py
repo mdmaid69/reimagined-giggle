a5071814f0a42d9351dace030289f2b4d81dbb52
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:20 +0000

    trouble uponon advice switch exploration

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print(sum(range(10)))
+import array
+def get_array_as_int(array):
+        return int(array[0])
