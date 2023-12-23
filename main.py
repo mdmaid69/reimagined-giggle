74ecacdb19a0127064804c4ceb052dd68c862a48
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:41 +0000

    left machine OKorokay top support

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_bool(array):
+        return bool(array)
