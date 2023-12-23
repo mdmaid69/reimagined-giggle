b0ba962abdec0093e897d0c90013f63671aa6455
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:24:52 +0000

    disturbing formal software pity ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
