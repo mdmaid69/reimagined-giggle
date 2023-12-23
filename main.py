30c86d633904e52eb03f3d23df99f9e2cdf2ec9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:11:25 +0000

    hers wear public behind to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
