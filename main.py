06b5cf98fce9c4ad51ec5edeb628c516c3972b63
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:29:04 +0000

    calmly powerful nonsense protest level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+print(math.pi)
