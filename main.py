67b00110483ae9923e586727335064ac5c20e77b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:58 +0000

    range entrance brave somebody surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
