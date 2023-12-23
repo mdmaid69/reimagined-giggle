435b2c9fc88e21de1b99850bb24174f4d02db7d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:04:39 +0000

    high work cool friend something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
