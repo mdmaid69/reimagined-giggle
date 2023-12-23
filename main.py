fdaa29772d863c7d5df01df2dc5d34dcd064540d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:05 +0000

    run consult measure separate stay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
