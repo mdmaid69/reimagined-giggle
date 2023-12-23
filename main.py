d3452028a4f6c5193e979ffe242c6095d5a5e738
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:15:35 +0000

    what forget report book trial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
