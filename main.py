092814664f9b7dbdcaf9c9afba73ae2f62ea7731
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:43:56 +0000

    workout software pinch silent 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
