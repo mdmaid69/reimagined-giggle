3d0f570ed6e68103099024dd5bc8b746aa49d490
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:24:19 +0000

    weak half worst government morning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def count_elements(lst):
-        return len(lst)
