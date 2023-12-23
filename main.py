4ee9aeaa0e30893b5b79a43f00dd828a00f399e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:41 +0000

    sink least never reach beyond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
