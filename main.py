0eebc5b7367f96c4f24eda4b7767072f807ff162
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:02:45 +0000

    crash confuse less which silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
