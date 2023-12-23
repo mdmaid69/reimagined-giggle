cf8c3010d2313519673a99aa85b09a16c743f27e
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:58:19 +0000

    judge public slice near habit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def convert_to_hex(n):
+        return hex(n)
