eb783b918870692133c81f7ba62f67d8d914902f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:33:18 +0000

    thorough inside mean work square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_floor(x):
+        return math.floor(x)
