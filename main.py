fdfb8b1a23209d4158e77da82f49be86a8edb112
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:52:39 +0000

    fasten object bunch compete wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
