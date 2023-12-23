3e6cefa3c60542345af2ff78367307b69737196d
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:20:13 +0000

    dish sticky bread believe wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def add_numbers(a, b):
-        return a + b
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
