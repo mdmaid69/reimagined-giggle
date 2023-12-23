112c059363e13fe3fd4125af573bf6e1af822011
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:15:35 +0000

    weekend thankyou public 572enter owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
