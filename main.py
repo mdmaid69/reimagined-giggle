022c726e5adf4cd000ee22d184cda9c2d81c2b38
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:20:32 +0000

    surface art building foreign confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
