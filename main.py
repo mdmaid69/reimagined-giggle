00a4ca552c2f6e76757adae7010a537dfc42ba86
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:23:04 +0000

    ours weigh over recent please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
