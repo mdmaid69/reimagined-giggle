3f0167cb62dc5a8b61d9958a77c20bc171d215d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:16 +0000

    wire mystery speak further most

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
