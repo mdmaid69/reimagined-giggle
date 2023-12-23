4c7958a7e190c4e057610bc2a12f0b827f188493
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:11:16 +0000

    petrol tap delicate want police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
