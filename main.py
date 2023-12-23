d3736ac73e4894a33b32f75c53c5f71c70ecb25a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:27 +0000

    congratulations callout doubt sink furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
