a885f3f22655485cf09ef1bceae0708bb676943c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:41:54 +0000

    help comein talkinto delicate breakdown

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def reverse_array(array):
+        array.reverse()
