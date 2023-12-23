a2d036d7a21a4bd12cf6a2618d73f0b7d1e922e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:44:24 +0000

    understand accordingto slide nigga welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
