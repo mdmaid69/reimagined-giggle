1d09f9ed225b468a65498c5c1c56191b17ada1ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:43 +0000

    trust basically lend sticky lower

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
