36db338ec99393a3d4f4d71e819eb1e4fcbd55df
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:49:04 +0000

    never circumstance fire twist major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
