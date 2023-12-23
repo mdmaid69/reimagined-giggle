959261fac3cd9445f4cebdc6642d8a746f2a0cb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:36:30 +0000

    afterwards turnaway stamp wet comfortable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
