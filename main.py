f196800ce2aa1464c6cd1081fdc2a534e76ea796
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:31 +0000

    fetch mark surprise bright bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
