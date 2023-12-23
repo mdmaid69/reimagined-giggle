a3a093bed0e9d6619a454e4d6a8401cb53fbaa57
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:42:00 +0000

    instant commit holdup warn group

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
