31af0848a8686c5ab44febf7eeb2aa4adaf6bb60
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:57:35 +0000

    blame can cross present bring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
