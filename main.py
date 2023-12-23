bdff5b996ffcec31c10a8977192e635b0f5d5bb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:36:02 +0000

    bear affect pipe try me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
