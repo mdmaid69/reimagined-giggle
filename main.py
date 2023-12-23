b4356a85421534bdd3c9f1b32c57bb500aedf045
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:00 +0000

    difficult instrument nonsense boat smart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
