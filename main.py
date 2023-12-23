21178065ad2e0c5ce18dfaec88c1488b454c0730
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:36 +0000

    pure trip stop error lock

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
