a7333ec330ddf66e98486123c511817e4acc4637
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:19:46 +0000

    nerve glass scale comeback section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_int(array):
-        return int(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
