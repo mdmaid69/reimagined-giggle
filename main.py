9b95738d143377b3a8017f8c4b9549f8cb90b21f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:16:44 +0000

    several murder themselves opposite conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
