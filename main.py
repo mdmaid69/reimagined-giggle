ff2752b7ac0a45069332607a7db68671787b7d38
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:34:33 +0000

    fear prize eye university married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def split_path(path):
+        return os.path.split(path)
