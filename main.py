1e34f3ae3d824faaaf4e050da1d3db572bf9143a
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:20:01 +0000

    throw spring really meanwhile possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
