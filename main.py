c228f6a91f8d794f87a6e3af9b51023706000da8
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:58:51 +0000

    commit Oo set shake tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
