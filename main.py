6374d85debb143ef7fad5732ab91033bc797b56d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:26:22 +0000

    can brave entry who turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
