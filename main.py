3c264013d53e0ee5eed8bb4befcc07f4f8bdf61b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:53 +0000

    unable price appear gentle Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
