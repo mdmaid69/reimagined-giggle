dedd6c184c2e6ff0ceafa8b3cb73bf507475fead
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:59:12 +0000

    section advise slide sure within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
