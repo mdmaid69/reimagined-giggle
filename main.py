4da917a706d3c85a3afd577fe6fc02b53acf865a
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:47:44 +0000

    godown dark trust Yy university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
