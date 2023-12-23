b4600220631c31e451c260fcc3f702e128313a1b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:00:44 +0000

    negative free loose degree first

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def subtract_numbers(x, y):
+        return x - y
