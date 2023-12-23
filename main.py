9d7abb561961e9ccd8b78a14bd01af45488d426d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:23 +0000

    aware at within ball attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
