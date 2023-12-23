a1c76350baf36e5850fddf80858bda633ba1d96f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:46 +0000

    basis certainly around move sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
