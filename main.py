e4471720809775ca02532840e96e6e8fae6d956b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:24:33 +0000

    easy along goon typical extremely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
