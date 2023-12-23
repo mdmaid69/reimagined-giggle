5a1992832032200cec5fbb3ff1b946cd0697468b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:19:23 +0000

    throw approval reason early doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
