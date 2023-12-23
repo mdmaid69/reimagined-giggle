f557ea2d2fa21ffc464bb30be67453a2af58f2d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:22:43 +0000

    our wide table skirt clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
