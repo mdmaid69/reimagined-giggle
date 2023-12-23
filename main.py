4c0bd59fb20205acf1724d4261e4dc9a5dbed63f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:26 +0000

    bye walk region Ii clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
