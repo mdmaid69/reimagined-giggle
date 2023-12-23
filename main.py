5968b75dfa3d794fd5ce1a145b41805a19d4d274
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:58:25 +0000

    accordingto above next unhappy foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def square_number(x):
-        return x**2
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import tensorflow as tf
+print(tf.__version__)
