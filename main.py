1e1ff1e771a087c50a7b84fa6b453752591982a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:33:59 +0000

    fall nor dream callat new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
