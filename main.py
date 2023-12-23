2308e49a8fa633d08a704d36c987a77d3ef04c76
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:59:35 +0000

    turnaway benefit OKorokay score his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
