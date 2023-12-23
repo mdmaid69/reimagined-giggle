5bef4edb093e81c3ef9a3a9f231b628ecb53f243
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:39:36 +0000

    responsible discussion scrap mixture dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
