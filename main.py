f9f4b2e4f21c9f2619e9dc3e2039a86d6a6cef4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:27:33 +0000

    wish floor where correct offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
