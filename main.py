816c3253fc7ca96f6e3a7a6e12e64081c0e4e90a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:27:10 +0000

    bet naturally bring wave far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
