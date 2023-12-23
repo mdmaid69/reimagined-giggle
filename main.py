2f112d95648ba8e50d3b15276ef7ec9049a24143
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:29:00 +0000

    course dick trouble event gentle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
