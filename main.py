de9aa6a36ab76e72b64acd62319620103e410f35
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:38:45 +0000

    mind murder takeout harm indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
