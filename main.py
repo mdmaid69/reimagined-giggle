c649daae3b845b44488da6bb8309c660c5de0260
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:27 +0000

    low tired available eye wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
