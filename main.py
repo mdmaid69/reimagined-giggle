1a4f429026ae5781e794543c5f2ff1b39ad19fb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:39:22 +0000

    truth swallow agree underneath official

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
