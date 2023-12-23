c0326f2bf2bc5df674999cac60babcdea401edd9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:14:16 +0000

    last ball declaration complaint communication

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def subtract_numbers(x, y):
+        return x - y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
