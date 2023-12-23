35d8f7ffc3901d7dc57b54d738a99c734e40dcb4
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:00:12 +0000

    likely slide error typical play

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
