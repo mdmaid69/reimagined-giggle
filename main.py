e692ff0af528e255f0bd554aba968cbd27a9c156
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:10:09 +0000

    line put play last even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-print([x**2 for x in range(10)])
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
