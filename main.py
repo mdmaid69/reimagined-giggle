7941c3c0bc64ef95891b21fbdc168b60201ec2dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:15:47 +0000

    affect shoulder congratulate react pour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import numpy as np
+print(np.array([1, 2, 3]))
