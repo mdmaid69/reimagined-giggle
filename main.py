048ddb0fb510883e9af68525c501c3de26c0650d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:13:28 +0000

    marry middle before move tired

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
