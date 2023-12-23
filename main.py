a3ab3abe756f0854045d78f32d8cb135bb4fc45d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:08:27 +0000

    paper whistle contact except fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
