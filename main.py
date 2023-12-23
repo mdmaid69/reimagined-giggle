b69afd5b6ebde29229cfaf9058f6216e931d9309
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:12 +0000

    sometimes pen compete address service

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
