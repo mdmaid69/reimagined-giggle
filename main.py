6bb167d5613d3df5d473aa6530e131a456d33468
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:22:14 +0000

    pleased bus thorough up poison

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import tensorflow as tf
-print(tf.__version__)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
