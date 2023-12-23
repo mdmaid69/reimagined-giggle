34c9dd84a06ec95dc350c75e28f36538059437cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:51 +0000

    under trust rush talkoutof side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
