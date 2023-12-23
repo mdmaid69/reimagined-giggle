f7ecb0574c644675803286bd0bf511d3fb49d642
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:00 +0000

    error thinkthrough female Vv mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
