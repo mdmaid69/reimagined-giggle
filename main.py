a95383e7804ff9a7b2a6f75cbc08c96e2ab396db
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:53:53 +0000

    become wait rain company frighten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Hello, world!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
