a72fe6bec4aa8f1034e98187ece7a37068daf8e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:27:57 +0000

    dare circumstance sleep union mass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
