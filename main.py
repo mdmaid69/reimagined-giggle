e18a7fc6d921d1664f30994f676b7160de234d82
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:01:06 +0000

    clearly shut see report temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
