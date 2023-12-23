99ee457333a43be3fcb7367c33291fc686987bd1
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:35:47 +0000

    exercise mark large blue ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
