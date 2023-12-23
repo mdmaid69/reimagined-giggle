e3932e0c3197012a32c68aa65b5443bba3739ab8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:41:25 +0000

    from possible letter instrument welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
