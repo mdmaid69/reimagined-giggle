24b8b4fa4aba28eb90a1f48df77b405dc020674b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:41:46 +0000

    wave underneath goodbye 568energy properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print(sum(range(10)))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
