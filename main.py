f3eb4e9213bcbee4f7fc4b0cf53113195568311b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:55:21 +0000

    confident stranger answer shop disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
