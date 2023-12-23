9c6769673d291deae7ce2021ecc532548969ba4d
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:47 +0000

    drink test violence beside along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
