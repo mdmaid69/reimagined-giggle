0a146dc56aeddef230873d64cde8e1b9d93466c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:42:13 +0000

    bus definite embarrassed wrap snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
