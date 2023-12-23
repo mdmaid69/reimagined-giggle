952397e43a35228240c243085a174bbb9f209034
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:10:48 +0000

    free holdout date exchange board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
