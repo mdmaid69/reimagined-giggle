6de17b814d859b2961cf449daed43bd016274e1e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:48:53 +0000

    mean luck cheque trust turnup

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
         return None
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
