c841bfd1f0dcc13a1638c74802152b56b1628cbb
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:58:58 +0000

    crowd mainly rise turndown upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
