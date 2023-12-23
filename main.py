fe0b95f88f5a20a8ed430e759e4c828478280535
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:55:24 +0000

    learn belief it honest stone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
