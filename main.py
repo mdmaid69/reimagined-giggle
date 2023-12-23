fa8296bfea4625f0afb025cfb614c4c588dd219a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:34:57 +0000

    trap interest violence delight lie

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
