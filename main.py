057ddd221459b9f4a8455413c36f15606e461f08
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:57:49 +0000

    divide emotion instruction platform question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
