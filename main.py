f5aeb6e919d9c71346bbb0f295c18ea18d8d3f3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:27:55 +0000

    depend illness hand responsible refuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
