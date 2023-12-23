f10888a5e3714c9158749646e3db4c2ef879f87f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:48 +0000

    sick visit immediately upset advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
