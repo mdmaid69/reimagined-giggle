4fbcf6b4df5f13f691552e9abd801489b3fe0fe0
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:21:26 +0000

    play generous its sort win

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_power(work, time):
+        return work / time
