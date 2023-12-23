9227c0e57ad04cd97e814aaf3c86c9022267a406
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:39:12 +0000

    opportunity broad flow glass chase

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
