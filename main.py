03dcd9a3316203234eca060b7ec17c6bd53aead7
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:25:23 +0000

    deaf unlucky harm early schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
