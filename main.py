780a2b6868639c43b8df76a930da5594c7d71159
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:41:29 +0000

    claim float system whom agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
