33e34c58cbc210074e7225ed3f7ee5951c56d789
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:55:49 +0000

    wide beyond couple collection anything

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
