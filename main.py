db50f0aa62162730f162771b94a5c049b1213fc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:35:56 +0000

    degree obtain relationship limit hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
