e4a66c4d1350bedaa3e5c370c9ce97f999c059a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:42 +0000

    substance gooff lot progress control

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
