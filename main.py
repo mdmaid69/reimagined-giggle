fa6cf9018aa1764af28d74ab0bdae98923c403b8
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:42:15 +0000

    fix realize foreign yes tired

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def reverse_string(s):
-        return s[::-1]
+  def is_even(n):
+        return n % 2 == 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
