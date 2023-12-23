a44bab79ec0640aa12a71b16c81d287c443b10e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:52 +0000

    trap tight geton thing compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
