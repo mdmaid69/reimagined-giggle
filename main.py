d29876c94f090f471ed6b10e20a42c70c31f14b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:43:34 +0000

    Hh wall no sister wave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def divide_numbers(x, y):
+        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
