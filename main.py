b84b239c2059875656920e366dba98233636a9dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:05:15 +0000

    border talkback immediate doubt fasten

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
