62f2af0afe42341d76b8f036f8c237136779036c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:30:05 +0000

    mistake colour pain persuade often

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
