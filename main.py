4b5c799a3fd8781757138b84edbcbcafd066974c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:48:29 +0000

    judge bag left clean justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
