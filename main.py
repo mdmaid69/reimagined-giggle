4e3ff8341aa90f7722a78c536fe4d2a7eac419b4
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:33 +0000

    warning interested contact turnon shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
