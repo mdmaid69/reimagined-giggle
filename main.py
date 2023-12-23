ed4ccfc74b66a4d1d7354151dbeb4e8ec3c91668
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:47:07 +0000

    show obvious permission have I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
