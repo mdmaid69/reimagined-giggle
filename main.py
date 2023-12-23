eeceed78d921b9b1b0919045d6401c96bd73f9fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:53:42 +0000

    wear battery fine meal warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
