c42e402c38876fa31741cb5637f38c828dc9741c
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:36 +0000

    rarely splash maximum fence combine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
