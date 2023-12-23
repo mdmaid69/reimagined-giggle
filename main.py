6e55f2108c4814bac595bce118a5e1926fa4289f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:27:56 +0000

    mean field excuse towards replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
