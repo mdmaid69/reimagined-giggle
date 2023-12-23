7cba3a8c81c19cbc7fb20b4f620884864e94606a
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:29:05 +0000

    nature learn collection ill quality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
