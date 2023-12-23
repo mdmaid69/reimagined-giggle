62e893b58ed1bcccfd6b700d5b50246e36a0dbcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:24 +0000

    recently ready goback(to) least comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
