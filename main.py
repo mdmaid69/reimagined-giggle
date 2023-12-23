4171a312c460354b44ad47203b5303366231d204
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:30:08 +0000

    reply scream question raw another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
