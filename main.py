0f25722721e9d193234d2c0915918973e18f69fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:38 +0000

    die dinner bridge mainly find

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
