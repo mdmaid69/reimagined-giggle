37c1a563a85cdfdc905e87103f0902f52447c5c4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:23 +0000

    pencil offer used light same

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
