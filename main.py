a17ef6e9681a63a8498b1970baee41a886d9a4c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:11:23 +0000

    low extraordinary mysterious generally apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
