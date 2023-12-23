af68671b1a7b2fde3d382b6d0353f78f05c75a3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:36 +0000

    most past authority previous praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
