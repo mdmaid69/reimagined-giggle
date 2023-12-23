b1cf8aabcfdbf1a14eedd6f42bc2ea8406315c22
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:48:23 +0000

    system ago underneath soak floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
