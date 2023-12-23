285507586fb0f76f801c4a36f372f1db8dc99caf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:55:42 +0000

    lower male visit judge heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
