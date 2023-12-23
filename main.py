f6565ef12a4ce72a63b77b3937774c61e5e72b04
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:47:52 +0000

    sit mainly clear tight fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
