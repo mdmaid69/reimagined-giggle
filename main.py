70953bf22f0209d0b06ff452100c266eaa5da35f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:56:28 +0000

    float autumn ignore set autumn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
