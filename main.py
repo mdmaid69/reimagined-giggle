9dcdb184bd4b6fc7623062c58337f2b2e6e03def
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:15:06 +0000

    decision stop man forever our

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
