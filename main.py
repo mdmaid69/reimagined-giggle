cbbf2ee799086015124006424e6ead74ef08b4df
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:51:12 +0000

    proof goon goodbye unhappy aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
