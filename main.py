b6866e6d6787195a0eb5661449f5eaacb21fe9a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:03:01 +0000

    theirs score Mm improvement love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_max(numbers):
+        return max(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
