a27e7189cf055c49838020574ee0a7e96b9ed643
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:39:24 +0000

    less tread minority second punish

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_max(lst):
+        return max(lst)
