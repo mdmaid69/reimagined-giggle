caaa30dd07a4d41ff0e78a2d9f1d7a22f825d286
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:23:13 +0000

    shake lookup modern minimum shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
