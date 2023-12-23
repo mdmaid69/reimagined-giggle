35edb2849363654ecaa070c1eed10944ceb5bb5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:22 +0000

    goon test realize float mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
