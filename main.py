e963620f50579db77e28285d33cef886061fab8a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:07 +0000

    fit lower children clever sting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
