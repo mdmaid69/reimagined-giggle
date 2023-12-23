d1585fcba9f500589f15ae6224afa529482c92d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:31:19 +0000

    frame track clothes scream question

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
