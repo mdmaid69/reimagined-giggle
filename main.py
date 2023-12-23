b59028e85352c3235a2a52539690a3719d5a6559
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:04 +0000

    temper admire branch make happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def remove_duplicates(lst):
+        return list(set(lst))
