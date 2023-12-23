37b60a8fa15fe1a2b2722ae657ac5101fd5b858f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:32:11 +0000

    Oo know breathe block separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
