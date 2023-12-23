92ccaef553dacb9f82a506fe51ffad0320a6cb05
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:13:11 +0000

    rest be direction mess should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
