dcc557b27466cb0fb3282b8eb2e80379f0baed13
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:52 +0000

    scale bit equal him which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
