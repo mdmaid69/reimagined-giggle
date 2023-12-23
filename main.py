f3dc642cbe85cd75cdd4954d8df76dad99fee599
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:55 +0000

    talkinto deaf check tough shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
