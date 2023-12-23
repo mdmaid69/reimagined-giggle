05d758452a875591c7f65f052296545cdc5e1b0a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:47:32 +0000

    demand home study well visit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
