9796be0994f36cae06e24f61fe3e397d9b0fddee
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:51:39 +0000

    comeup urgent deaf fork everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
