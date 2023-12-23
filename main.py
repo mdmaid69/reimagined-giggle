bbb2a7dfe634a6b2542bee9c7a878151feb200eb
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:42:16 +0000

    thought care minimum sign convincing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
