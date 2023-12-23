5ae3f175b8a04555f0401a4ee183eff567af9505
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:24:44 +0000

    because scrap eager welcome turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
