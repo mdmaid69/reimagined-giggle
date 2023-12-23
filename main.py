1bd9c6b0d8329410077ed289c9c64d092888ff15
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:15:49 +0000

    grand official wish then difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
