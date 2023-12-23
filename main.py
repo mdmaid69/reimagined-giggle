0f4283d8f0bf8ef45743f2edc23d51fdac3a1f9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:20 +0000

    comeup hand act name careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
