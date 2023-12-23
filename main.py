3e8e38f679eab2fa5e323df680cba1ecdffcb6bf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:54:25 +0000

    break most sort new already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-def print_python_version():
-        print(sys.version)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
