9873a315ade7fdba1dfa411c4bdb48e7b8780b99
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:40 +0000

    bath create the last excited

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
