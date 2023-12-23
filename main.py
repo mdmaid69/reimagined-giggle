b32a6e78f5393329bca638aa65b7919a683b5894
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:46:23 +0000

    mixture pen their fill week

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
+def count_words(sentence):
+        return len(sentence.split())
