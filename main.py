73706972e0897ddba95b42afe6220507e386a42d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:54:02 +0000

    point stop search match half

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(lst):
+        return min(lst)
