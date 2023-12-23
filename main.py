f45fe423c2888f0ea94bfac7e9ebc8c77ac9673d
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:34:36 +0000

    wrong taste winter comfortable produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def subtract_numbers(x, y):
+        return x - y
