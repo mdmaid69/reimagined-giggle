3ff3987703bca583ce3872dba499a417c62cc5f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:04:00 +0000

    freeze measure away guide turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
