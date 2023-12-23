3b000bafeefb890e45e39123990e305a5dd73491
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:43:23 +0000

    off wipe stare fight after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
