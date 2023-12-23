26a40c8930bb5ade3687e145b6c1f63ae70de29c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:27:25 +0000

    jump nervous comeover face wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
