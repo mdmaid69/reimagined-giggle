9492215f6c3d10e15f10896392f46e9d91543bec
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:09 +0000

    frame defeat giveup Bb sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_priority_queue():
+        return collections.deque()
