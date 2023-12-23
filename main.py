33351d9a90bddb565af67000a04816a2cdd2c49c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:39:13 +0000

    joke relationship live thinkthrough deep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
