0a0969d544236a981da82f01ad48961eea320401
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:20 +0000

    promise generally region storm release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
