b485af6cb6691884341e92cd10eda1e0b1c9d70e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:58:50 +0000

    conclusion bringdown any wish intelligent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def is_odd(n):
+        return n % 2 != 0
