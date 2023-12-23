05e174063442a1d319e069b0b5cae73bc5622720
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:44 +0000

    ilovedicks listen remark underneath politics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
