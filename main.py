ebbf48ee4df6b75c7844aac5567b23ec7db897b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:31:38 +0000

    affect stamp education used step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
