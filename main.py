836691023e21cdb43ba3da9641b8d34868bd87dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:12 +0000

    piece wall approval pain before

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
