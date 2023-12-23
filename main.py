14340221dbcaf0a288afb41b7dc44fc81e154dd3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:55 +0000

    medium neither regional shadow hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
