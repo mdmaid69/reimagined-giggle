6f476de1e8e49d9d3faa733ef62a5c97ae7d9afd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:17:38 +0000

    store schedule cry balance judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
