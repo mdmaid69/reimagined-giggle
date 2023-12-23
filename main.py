32fd858f8fd7dd66862bec26fb0fcf8ee8acbbd0
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:28 +0000

    contents pronounce organize tip little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
