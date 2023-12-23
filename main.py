8eb75507b91aed30f1a6fbba5c4a49281174df55
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:39:25 +0000

    move pleased behave land creature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_list(array):
+        return array.tolist()
