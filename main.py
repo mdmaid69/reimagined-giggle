dff6df8e46add0474a20b425f3fa6a7abf329186
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:12:38 +0000

    might follow naturally guide on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_power(work, time):
+        return work / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
