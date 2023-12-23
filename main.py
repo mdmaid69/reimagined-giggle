38a5f33da46b464129c734a0feb414eb1a44a625
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:22 +0000

    easy special switch complain tend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
