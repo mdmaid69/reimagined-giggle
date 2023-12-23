06ee827734d93ee3856ba917e9f4be52b0046278
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:22:41 +0000

    stone difference lot children gas

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def square_number(x):
+        return x**2
