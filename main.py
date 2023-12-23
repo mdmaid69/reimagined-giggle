1a962b11a9584d3198f4986a71d9e74b6a7e7b72
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:46:29 +0000

    wonder noise environment already different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
