98403a37f74e6a8117c9914081b30ee31bdfb723
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:46:04 +0000

    design unkind minute bar but

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
