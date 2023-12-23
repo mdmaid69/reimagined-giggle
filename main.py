52d23b119708d44488c1cb66b73d80f161b02816
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:47 +0000

    neither delight money rough mouth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
