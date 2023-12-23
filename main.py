f77beb748dccd72bebdff3038f3394b876534d96
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:57:25 +0000

    sticky feature moment roll gift

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
