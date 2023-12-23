cef8d340ced6552c244f723b0908a98575967aec
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:35 +0000

    government judge although he noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def get_array_as_bool(array):
+        return bool(array)
