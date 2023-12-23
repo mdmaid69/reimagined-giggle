4dce3d533447a0bec787899cf63e5f3b5db22716
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:19:03 +0000

    adjective risk blind anywhere stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
