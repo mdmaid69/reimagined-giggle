5cf1d45a0b645de193385c39846023576e94355b
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:29:47 +0000

    interest impossible common slow heavy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
