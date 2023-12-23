5f2ce6f75996cb9efa79e1573a26b3334f54b4a8
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:43:10 +0000

    the union warm fire invite

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
