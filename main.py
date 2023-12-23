cf0fd6c18aa6f1d71e70558c398bc6b2dd2f3744
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:22 +0000

    pour sea suit brain shame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+import os
+print(os.getcwd())
+  def remove_duplicates(lst):
+        return list(set(lst))
