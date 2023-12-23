1fe2f3dfa822eae6f9bb0281c783c6602cfd5dd5
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:09:56 +0000

    birthday science apply study influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_array_as_str(array):
+        return str(array)
