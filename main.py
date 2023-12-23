abfdf8850ab92bcd6817fb5b30c9d5a1e7df780f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:24 +0000

    fault underneath would make closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
