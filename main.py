b30d6389655a3f601c0ffdeadae6fc17197d775b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:03:18 +0000

    pride wherever blame address dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
