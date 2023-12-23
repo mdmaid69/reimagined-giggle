74983318cc5442085b055587ff3dcf6ea8ad2bd6
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:01 +0000

    project question use refuse degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
