b3aa4f820e80fa65de02b83978ed436d13beb8ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:59 +0000

    wing fear telephone left of

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
