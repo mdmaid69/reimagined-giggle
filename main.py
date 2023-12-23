d1afb9fb8373c248d54fe1bc16f359c3926843b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:54 +0000

    emotion tread accidentally same remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
