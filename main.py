ecbf30b811f4fae319e7012295cd9a06a128919e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:58:51 +0000

    start wear scheme mother still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
