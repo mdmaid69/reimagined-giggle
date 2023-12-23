80a91504cb81c5394dd4be6d6a14732ba7d6865b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:45 +0000

    university refer possibility listenfor involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
