4026535dbb78e55104c5988a819cad9442d5f343
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:52:35 +0000

    genuine tax lunch pay ill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
