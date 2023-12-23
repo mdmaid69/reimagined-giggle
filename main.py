e956316a33ebf55be6a3c5de27a9b7cade8ae499
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:52:57 +0000

    ride case finally trousers car

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_max(lst):
+        return max(lst)
