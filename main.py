f54727843956a1adb5e2c857f83c22cd1c105d08
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:18:35 +0000

    behind question tear comfort picture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def convert_to_hex(n):
+        return hex(n)
