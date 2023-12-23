92f22712bce3520894b4eb1f7c75c02ff7df7b75
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:42:59 +0000

    thankyou own pull answer determined

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
