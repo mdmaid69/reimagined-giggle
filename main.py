f906a2f4d104518a798828eca8fc08b59ca0b693
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:26 +0000

    broad recover split travel protection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  def cube_number(x):
-        return x**3
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
