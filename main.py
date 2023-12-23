5eff943dff6dcdd461c27d06bd45e848fd2a22d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:34 +0000

    mass mine have dead actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
