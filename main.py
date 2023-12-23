eccbcf51ea418c534f63b5477f76fa2811a1b79d
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:28:39 +0000

    short miss spill object consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
