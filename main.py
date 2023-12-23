bacf1a8aa9b23bc216b39c8867ac0febd52dd88f
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:39 +0000

    ahead crazy bother fond upstairs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
