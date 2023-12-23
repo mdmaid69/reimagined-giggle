5b206b030272c84a8c2a0bbef81b1174152bee93
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:21 +0000

    port difficult agreement lend free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
