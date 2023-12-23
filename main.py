cc0dfb6d62ee3aeac410bb9975b79348d3c0f877
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:40:02 +0000

    bush spot hair lost compete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
