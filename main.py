f95761b3ecf3e06b5ebabb8818a294dee7091033
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:37 +0000

    set strike affect either shape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_perpetuity(payment, rate):
+        return payment / rate
