2756c9a936e631381bab48d055a63f54435902c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:58:14 +0000

    recover sign arrive basic month

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
