936dfcd82dd9ff892c0e0ccf0b9e7d1f5f8d4e0c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:57:00 +0000

    gap disgusting arrive violence when

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
