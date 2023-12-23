59e5705d99779c143feb29d1764baee1cb37d633
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:21:26 +0000

    risk order cheap fall dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
