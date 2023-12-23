408a05dff3be3f959a62e59956204e2252ae6a7d
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:21 +0000

    that move repeat married really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+import datetime
+def get_today_date():
+        return datetime.date.today()
