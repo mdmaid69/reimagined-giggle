7c2047cbd4c25372e0bf2ff3f85021a46196988d
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:53:16 +0000

    try front in tend on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
