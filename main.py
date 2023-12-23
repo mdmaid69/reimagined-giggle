e77ee73a1e4c1e1ed74ad2dc4685e5902f898a5b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:45 +0000

    earn in soak lie tall

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
