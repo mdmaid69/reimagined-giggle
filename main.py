69a330a7cdbdecb45c1ffbd522289b163933ecf6
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:07:45 +0000

    eager below ring excuse from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
