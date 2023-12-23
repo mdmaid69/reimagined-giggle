451caffc93b1e1d568513f843172f30bc14bd6d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:38:07 +0000

    arrive runaway tool swimming will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
