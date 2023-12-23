90a03eaa59c9263562aae34f8ace36a7e90b9f30
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:59:12 +0000

    motion to fear friend evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-print([x**2 for x in range(10)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
