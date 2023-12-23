1fb53bde84a85c5bdf7ab53e88b9fbb5cac31ab2
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:50 +0000

    nice laugh notice serious consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
