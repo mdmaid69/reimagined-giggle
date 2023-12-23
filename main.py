aa5cd00c596dd1d378d437af9756b82e46a4b6aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:01 +0000

    street capital error separate turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
