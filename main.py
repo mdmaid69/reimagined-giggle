d1fff09832f4827f8d36f323e1926409447d0e13
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:49:22 +0000

    can collect bus stage creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
