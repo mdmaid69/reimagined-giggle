0857f650bb74b287f04dee15c5c2440189406c71
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:58:35 +0000

    protect statement beginning carry little

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def find_min(numbers):
+        return min(numbers)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
