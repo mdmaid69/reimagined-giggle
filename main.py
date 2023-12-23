0c8f0e8e8c8f2c8fd0ab50d7eb9a467da8d1ba9e
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:02:02 +0000

    hole deep ashamed drawing major

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
