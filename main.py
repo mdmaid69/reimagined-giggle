80450ffaddca6441e355cc596b40ddb9f48b499b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:58:17 +0000

    fail profit question getback fire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
