49bb5964d454ea0a1e354e31f94709915f69f3a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:05:36 +0000

    reply soft water offence sometimes

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
