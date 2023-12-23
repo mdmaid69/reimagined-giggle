8560fc9aeec1f6a69adeb3af6cd3aee221fcb4c8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:18:39 +0000

    service care goout modern great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
