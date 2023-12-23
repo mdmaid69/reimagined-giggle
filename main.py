eee1e7d7ad6f334fc5c6cf3e6703307738e9b988
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:39 +0000

    convince name show dead stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
