ea75863fd6e226a5e6cd7bcd51e4f5b92d92180f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:40:39 +0000

    scrape dark fortunate scared university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
