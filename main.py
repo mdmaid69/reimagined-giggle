9c64fcd588847ecc55472030a6aba3246e90ead1
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:13:44 +0000

    him exaggerate concern respect if

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
+def subtract_numbers(x, y):
+        return x - y
