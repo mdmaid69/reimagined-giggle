df10d180d38b5519b7f2819ecbf00ec50f9d2945
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:49:16 +0000

    firm patience I chief sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def convert_to_json(data):
+        return json.dumps(data)
