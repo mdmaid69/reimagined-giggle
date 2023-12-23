8e227562b0b059fa5c20c9cd9df247cdfefff6a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:39:15 +0000

    check today fairly money error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
