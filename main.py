69b2b3897f55cf76b4ec8e9d7e5ac443f1f8de3c
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:12 +0000

    run mean lucky left correction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
