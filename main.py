c6fb9706b5a5b01df688033383fe11d1260c7aaf
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:21:20 +0000

    instant skin geton always usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
