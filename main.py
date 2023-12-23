53308994ab4a1e5578de937975b45fb94ef0ebf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:58:16 +0000

    rid only opponent first tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def find_max(lst):
+        return max(lst)
