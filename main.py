f2a6286e7a55b1c1bc809a29dd1b837c3b0b58ba
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:30 +0000

    wherever maybe stop children worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
