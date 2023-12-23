3b05eee5d5fb24a90a30cd4048c9f42c4ebe2b9a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:06 +0000

    pain phone float her avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
