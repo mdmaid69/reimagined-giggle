6c03381d70c1f6b9544ba2d4ee40e925ca53aa63
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:56:20 +0000

    stone write excuse comeout spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
