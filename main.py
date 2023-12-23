91efac431eb71862d90511ef86951678e753f913
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:41:07 +0000

    rent very list experience over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
