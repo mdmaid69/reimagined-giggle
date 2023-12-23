c548873d97392bfeccc951e19e1a5557cf85a2fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:37 +0000

    tonight popular experiment phone peaceful

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+print(os.getcwd())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
