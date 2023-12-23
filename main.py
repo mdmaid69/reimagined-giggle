5201d7073ea492819ac01e1daeba74f638e6de8b
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:35:46 +0000

    criticism rely recognize communication undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
