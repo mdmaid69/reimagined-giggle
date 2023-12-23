53c3cd9b7ce9dc5495a207c2fc285fa093243562
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:53:58 +0000

    why risk round band sky

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-for i in range(10): print(i)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
