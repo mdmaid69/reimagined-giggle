01c81bc3f3a68b2aafedb4621b753e444c81918d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:28 +0000

    burn hear full ourselves slightly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def count_elements(lst):
-        return len(lst)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
