0006fe52e1c3a41ba20a99bc2ea34b1def680d66
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:56:04 +0000

    stamp fix moral Ww dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sys
+print(sys.version)
