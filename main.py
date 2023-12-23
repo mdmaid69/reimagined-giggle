56f9e36dec650e27e2317fb905b762d6c1ff3cd7
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:33:05 +0000

    niggaacademy stage Mm breathe smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
