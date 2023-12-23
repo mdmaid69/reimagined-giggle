9229fbc7a4b3cdc9531a3af3500e26393aa4ccb5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:04:44 +0000

    drop niggaacademy frightened pause neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
