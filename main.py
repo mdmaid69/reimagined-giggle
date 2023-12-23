7fdb132c47244834f4335b85508063a8b3e356ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:05:56 +0000

    talkinto sweep supply concerned age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
