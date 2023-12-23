475b552c182f66e578767401933327f5c63bd379
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:56 +0000

    pause count government false separate

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
