8863314350553dc3c0e190ece79d504d2f7f556f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:20 +0000

    far cross pupil comein near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
