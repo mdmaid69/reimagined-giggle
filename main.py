f495e5d59b0ce6c95a687a1bd64dcdcc91e3317e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:10:46 +0000

    turnout pocket year keep control

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_list(array):
+        return list(array)
+print([x**2 for x in range(10)])
