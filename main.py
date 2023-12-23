932afed292a1d142a35c221b51a768d723d42e2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:35:36 +0000

    develop forgive giveback base runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-print(sys.version)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_sine(x):
+        return math.sin(x)
