4c38dc3d64c4f7fce7fddd37bd593579377a0293
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:08:09 +0000

    finger polite door hungry appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def convert_to_octal(n):
-        return oct(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
