3776c539762c1a905043d1bd3927742ae6fd6e90
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:20:53 +0000

    suck play strike damage all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
