54563c7bd9ac7c5879f27449f71546ec71a988e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:42:27 +0000

    age fork thought careless fit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+name = "Python"
+print("Hello,", name)
