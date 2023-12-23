34ed485d62a926e863c7813604d0bde5e4551064
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:13:36 +0000

    difference soon like share dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
