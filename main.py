3f1cf4ae5c421bf462d949ebf5facb64bee693aa
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:06:34 +0000

    clean boring soft more breath

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
+print([x**2 for x in range(10)])
