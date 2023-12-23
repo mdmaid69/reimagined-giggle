cb261c9057f2fc868a7f468d7445b4123724dd2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:18 +0000

    meal however base remove positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
