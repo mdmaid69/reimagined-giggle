158616752cbc4e29876ab7f20af3405eabc6c028
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:05:53 +0000

    until guard yes so whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
