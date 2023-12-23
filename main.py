8a5b1244cf0d96f8c3168ce4757ad1b17bf13a18
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:28:40 +0000

    occur maximum whisper anything expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
