3a3e3580072ae2b5599a719e8d4f2e80baf580bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:34:14 +0000

    loose entrance traffic react appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
+import array
+def reverse_array(array):
+        array.reverse()
+  def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
