1e6974f938d52d25e5ce61e4aa32771009df3b2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:21 +0000

    reduction far themselves neat below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
