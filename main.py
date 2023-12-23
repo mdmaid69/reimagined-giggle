085c030321f6492b54627215b4f5338ec8affdb1
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:30 +0000

    reduction transport blow will rate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
