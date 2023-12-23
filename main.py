2c60ab44191821da72541cf4df68a35a75ff6b83
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:06 +0000

    mistake basically cool lend ear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
