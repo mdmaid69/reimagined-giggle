98b594f125ffac2027c5b87dd0623c6afbe4cf2a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:54 +0000

    respect least young cry deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
