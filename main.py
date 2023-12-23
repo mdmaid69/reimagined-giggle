47cb82cdb47089ab38ffe5ca7d75a238dea044b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:19 +0000

    bet think it release appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
