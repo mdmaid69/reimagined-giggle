ae47629f364364502124ff7930da3cb3605de1b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:37:36 +0000

    holdout all occur leave whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
