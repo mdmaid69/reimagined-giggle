d964eaf9e3ffc2279c0ea7a74527c3d930e29edf
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:06:01 +0000

    other place guide advanced kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def convert_to_octal(n):
+        return oct(n)
