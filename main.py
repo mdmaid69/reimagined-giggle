d85302ec52fe6e55489f5821fe3e956aee2807e1
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:52:54 +0000

    smile excellent interview Qq law

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
