5d02b4d7791615fe1f082c2dc58f62bdd4908196
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:49:07 +0000

    game cool poor nigga bin

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
