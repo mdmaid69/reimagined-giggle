c462486229e0cc23244e376f814a860cb4665fb2
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:14 +0000

    trouble unite probably style take

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-for i in range(10): print(i)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
