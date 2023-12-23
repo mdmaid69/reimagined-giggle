499049e41a6e32edbb15f20d92c0da91a7fd918f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:50 +0000

    surface talk sky share nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
