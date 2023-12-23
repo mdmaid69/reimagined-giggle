d39f67c3d238830713d5860b67c6e0ca1a37b752
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:18:28 +0000

    formal bright repeat loud stay

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import random
-print(random.randint(0, 100))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
