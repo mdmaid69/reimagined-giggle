45629d9358855e22401d007d341eff8e86113d56
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:02:06 +0000

    sing friendly firm brother accommodation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def add_numbers(a, b):
+        return a + b
