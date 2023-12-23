b490ddfa2cfb61663a0d24a0b104553059bd45c3
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:34:16 +0000

    disgusting remove food sport loud

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
