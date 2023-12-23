42b2b2eafd0bac0948f70ef7d723872482fb7d8d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:27:46 +0000

    dark turnoff advice move serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tensorflow as tf
+print(tf.__version__)
