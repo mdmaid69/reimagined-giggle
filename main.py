e89888aafb00952f644e0b02035e78492c1246f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:26:53 +0000

    again whichever pretend wrap turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def square_number(x):
+        return x**2
