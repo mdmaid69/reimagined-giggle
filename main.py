fd96f606c9599f1ed9471a1113dabb4e7843bd9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:55:28 +0000

    change juice corner clever round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
