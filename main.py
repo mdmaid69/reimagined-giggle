198c0b42874120e11b70c74c104cc383e46ab7ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:56:51 +0000

    serve turnback on environment start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+print([x**2 for x in range(10)])
