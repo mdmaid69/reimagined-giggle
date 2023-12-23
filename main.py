6599a627bb4f440181dcb450968914552118ab2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:41:26 +0000

    serve welcome furniture grow pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
