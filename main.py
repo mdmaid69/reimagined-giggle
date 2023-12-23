d0e50be4ba98bb44e92f11d2ae82acb9f7cf8254
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:53 +0000

    law invite receive deliver his

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
