f7a8b31cd25b37e54f4674cb3ce623af0d0a8799
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:20 +0000

    object terrible right unpleasant tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
