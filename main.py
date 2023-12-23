7e8de293c33237a2d129479dc4d13af436110a73
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:28:01 +0000

    on tax communication art finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
