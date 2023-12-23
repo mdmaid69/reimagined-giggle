b07a118d5f24872233278e432542b37aa044e095
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:16:13 +0000

    wet detail dead satisfied average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
