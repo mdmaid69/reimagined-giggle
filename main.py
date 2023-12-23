9656fc387126935aced5e9489fe9832c8ccd9859
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:16:41 +0000

    leg near cruel weather away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
