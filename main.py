2a1b0a8185909954e99db4695d469e8d7dcbdadb
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:00:43 +0000

    trick practice fairly hope splash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_density(mass, volume):
-        return mass / volume
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
