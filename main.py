6d06e560fac9fd010ca7e5090126921cf26b0b25
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:31:19 +0000

    frightened wherever produce consider change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
