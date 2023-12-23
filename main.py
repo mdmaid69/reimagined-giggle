fc2c7ddef5ef5af5fa0ee4f6f4ff4e9f4b9b5e99
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:50 +0000

    mainly worry vote debt near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
