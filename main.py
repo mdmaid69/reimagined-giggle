df6a916e7f031ed3c70f33b8271366e6edce1146
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:57:33 +0000

    possession fat contact steam plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
