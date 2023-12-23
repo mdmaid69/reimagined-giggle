94b8a2b7ea82c4a856a727301b24416e509233d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:06:10 +0000

    event upset float original side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
