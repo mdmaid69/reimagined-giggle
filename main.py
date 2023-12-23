dc7133c717b63728fe252a15af9f43155a50287c
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:49:38 +0000

    limit Pp grade moon cope

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
