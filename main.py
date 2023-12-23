4b8047765ec0abf03f71dfbca3665e684466184a
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:34:30 +0000

    could interrupt avoid high hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
