c79438e3174e83897f7ff7de0f7a529a0a09a0c2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:22:50 +0000

    odd underneath outside bored square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def find_max(lst):
+        return max(lst)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
