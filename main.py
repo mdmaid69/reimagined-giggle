552dce4fce53a490f229cceae99cad84503212fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:56:24 +0000

    bottom neat fork catch please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
