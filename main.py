76a999d254a8502cbdf1f4fe32835554da1aa478
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:55:11 +0000

    cry Ee contact amount total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
