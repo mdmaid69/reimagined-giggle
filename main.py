5fe75553631a106c571ff6764048d55a4575d5dc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:14:51 +0000

    Ff takeback notice way mystery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
