fb2a5848c456505f4a51c8e629b3b8c757a93137
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:11:19 +0000

    takeover proud father history organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+for i in range(5):
+        print(i)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
