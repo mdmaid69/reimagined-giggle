8bd2e9954c8fdf2f98f7601b84cd1edffc66c84f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:27:17 +0000

    place identify we speak label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
