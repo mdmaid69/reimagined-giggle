14e836a49349ab9adab2b42a177fc7bb82c54c5c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:36 +0000

    kind safety subject exactly relation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
