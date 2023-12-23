4bdc461c90bf0068698a043421d796e562a15169
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:29:57 +0000

    theirs modern like doctor plate

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
