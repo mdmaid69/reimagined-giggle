fc83a215e6ec5e70c7f82f2ba9a24a507047fc1f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:38:13 +0000

    precise principle lot upset increase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_queue():
+        return collections.deque()
