ea26604aa361d901adf57a35d9df629eac5912a1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:48:19 +0000

    away shelter replace control complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
