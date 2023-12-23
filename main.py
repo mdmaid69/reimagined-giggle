d4f069583283a943143ff10d70f00ab7cf3653cb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:54:12 +0000

    spoil risk pinch control zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
