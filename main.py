1b98cf6020a5e62fdaf676037a6ee4cc27643fcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:01:01 +0000

    stop male tap operate lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
