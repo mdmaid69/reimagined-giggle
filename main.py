6c533185e77dba9dffae3b62bddf68f5c17b86e4
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:51:40 +0000

    where to admit between your

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
