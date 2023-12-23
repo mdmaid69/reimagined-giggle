8d6a2f85f058aa010ce9fbe3e14afd5a55e449b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:45:27 +0000

    escape callback in callback expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
