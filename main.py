a15a1927c65cc5266905c827abb5904ddcda8aae
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:41 +0000

    soil steer defeat well plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
