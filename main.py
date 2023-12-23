49e86ea57e409f269a8daaf79fe63fb5ca45b75f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:07:56 +0000

    rain church store help breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
