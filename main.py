8984a932c46472931d80f9225a7a08d06b1d09a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:54:24 +0000

    as ball trick wide rich

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print(sum(range(10)))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
