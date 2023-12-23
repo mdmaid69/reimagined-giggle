f5c76a3d79269cdd5e39d9be9a22e95fdbb405a3
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:00:26 +0000

    change easy bottom secret next

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
