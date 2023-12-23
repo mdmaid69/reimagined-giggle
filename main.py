52a9fe0aa83e001f41790a908afc312ea7e5560a
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:37:37 +0000

    situation platform system straight brush

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
