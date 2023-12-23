867c9280d4715a04eef7ca65fd0e420e44c3f50f
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:14:48 +0000

    divide usual hide unfortunate extra

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
