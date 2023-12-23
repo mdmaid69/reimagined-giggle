b02ebb79473c75c85591ecf4d9611790e0f937a4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:26:05 +0000

    measure watch person afterwards hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
