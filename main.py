4d85ba8d83e51a906f78e6f588974f0a35218dec
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:13:25 +0000

    correct also get measure always

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-def cube_number(x):
-        return x**3
+def subtract_numbers(x, y):
+        return x - y
+def is_odd(n):
+        return n % 2 != 0
