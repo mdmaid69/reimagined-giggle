094ba3431970376d50fb41d509065538da3ab4ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:01:06 +0000

    instead Dd 571enough opinion grade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
