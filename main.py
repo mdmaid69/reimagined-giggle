5cbca0c68185b607334ffb46498650514fcf5d99
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:02:26 +0000

    flat sound run sink birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
