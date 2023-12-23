bf8be1aabee998fc242ded5a71fdefeb6467e263
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:36:35 +0000

    order do scare 565end birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
