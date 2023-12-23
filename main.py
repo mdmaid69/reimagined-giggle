3ea7ccd7c48b46ba77dd7aadf261416d5f4562d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:59:18 +0000

    begin since common smell left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
