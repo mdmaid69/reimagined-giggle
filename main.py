8b3c12089e98b8e5ff75a536cc70433d9ab98b8e
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:22:54 +0000

    cheat trap Ff organization forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
