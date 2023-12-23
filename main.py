10ca1b0f1e1bb5f9ac994821e4f2ea5b281047dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:05:23 +0000

    lie mean place sometimes band

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
