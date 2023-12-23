7d5bfe76472c8924a3bda2992bb78c9975db0233
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:17:28 +0000

    it weather tongue real needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import time
+print(time.time())
