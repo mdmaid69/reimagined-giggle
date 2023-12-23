c95fe7f729b81c284d99d18d97dd1d350e9a3041
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:03 +0000

    wrong whenever sit subject refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
