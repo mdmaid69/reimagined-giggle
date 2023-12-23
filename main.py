16f8a34ab91f27d9ce88abdd6e69b7c3ac5f835c
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:19:01 +0000

    smell borrow trade begin talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def subtract_numbers(x, y):
-        return x - y
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
