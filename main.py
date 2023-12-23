fa88cc17fc232840b55f0d9d08a5c19b05be6eba
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:50:11 +0000

    tend means quality suit follow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
