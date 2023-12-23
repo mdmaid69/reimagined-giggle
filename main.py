750900a7a304307dbf5604ee08213102e479c097
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:26:29 +0000

    weigh deep wander upset ocean

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
