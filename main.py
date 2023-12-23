6de2e2df3d13976d6135a16c0f49d13afeb8fc22
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:01 +0000

    somewhere admire pattern contribute hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
