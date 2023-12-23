45abcbb4d95f0481877bd29ef1eab43d44360d79
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:50:17 +0000

    print mix as air could

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
