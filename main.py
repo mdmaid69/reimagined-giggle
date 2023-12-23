1711cf0659335e5c13dee801a818f8734918305a
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:52 +0000

    fall coat border spot fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
