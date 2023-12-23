9c170bd272a947a4e71e25d0428bf49454bd4a33
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:03:33 +0000

    fast food kind beside block

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def remove_duplicates(lst):
+        return list(set(lst))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
