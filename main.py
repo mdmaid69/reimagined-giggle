2901e6f3fe94f57c422e220a21a1dfcd01125749
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:28 +0000

    flat whole turndown dig pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
