374257f5f1a1233e040cf9a704b9720ac8471bf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:07:27 +0000

    should mess please operation command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def subtract_numbers(x, y):
+        return x - y
