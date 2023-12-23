82858cf61bdb5e3c248813855f1490763a1363da
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:48:10 +0000

    taste bang lift visit benefit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_time_since_epoch():
-        return time.time()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
