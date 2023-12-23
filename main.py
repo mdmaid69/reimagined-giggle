91e011286501f89ef002bbf0f0a1c67fc4ad6da1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:15:04 +0000

    hurry splash single wait finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
