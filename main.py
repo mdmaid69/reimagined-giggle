a854a994e76462577fe83e6a10c29c6163c02477
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:38 +0000

    mark heavy thin unless not

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
