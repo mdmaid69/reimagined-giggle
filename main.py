7cfd9e490a1e23664039e542a24ad218dd90d3ef
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:34:12 +0000

    quick well hand rubbish fair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
