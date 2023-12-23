99685f80acde070fd52a42f77350e6f542f6fa0e
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:58:27 +0000

    square education favour obvious brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
