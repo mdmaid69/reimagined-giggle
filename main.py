6a5ffbfa30628df52b5707c373a60a9edb2975c0
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:16:16 +0000

    forever within evidence truth well

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
