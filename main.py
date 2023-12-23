e881b789fec30f9bbfeaa5c9ec8a9cf1a372d055
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:11:34 +0000

    Pp tread because custom fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
