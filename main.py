ccf807af2588ebafab39b7f158946f9321662e0d
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:22:15 +0000

    art escape 572enter emphasis flow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
