15028bb132b348f8c91b7858ca9101f32420d0ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:26:15 +0000

    problem strike talk base include

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
