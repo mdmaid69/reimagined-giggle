1c132c70f9addda90e6477d6d126764af17634a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:31:29 +0000

    appearance spring Tt surface treatment

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
