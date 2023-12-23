4d31c1d80514f6b08ae13e04538528d20d91f9e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:41:00 +0000

    rarely stick delight knowledge ignore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
