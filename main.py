826ce6c0e73914c67686ed667d4b2d21b1b5ab4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:34:33 +0000

    really mind apart swim pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def count_characters(sentence):
-        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
