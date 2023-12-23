a0c0d70e197c64e2ac82dec4705269e294f5c4ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:14:03 +0000

    fortunately dark minimum land matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
