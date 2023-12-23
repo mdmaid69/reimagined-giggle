08ea563a38454baea42e2d534592c71f2cef367e
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:11:42 +0000

    protection interesting result getout future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
