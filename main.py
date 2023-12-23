e064c54bae273186d53027ec1870b47cf946679b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:12 +0000

    noisy advanced professional rather deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
