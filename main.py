18ad08772800340b351a9183415b8f7b519128ee
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:45:05 +0000

    into side different fail butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
