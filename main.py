b00551ef2b0de8f517b058e4b573a9be6896d3be
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:24:29 +0000

    method like born pause of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
