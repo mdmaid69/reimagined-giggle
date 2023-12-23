5c18d73032e74ab9305d6bb11b81bdcecad06d9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:56 +0000

    press honest hit music present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
