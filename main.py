ddbfcce0258556206972e8f6b96c2b69374ecabe
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:24:53 +0000

    right remark side computer decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
