aa1f308e97140c46b2dd1bcfb1a5ac1169677183
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:55 +0000

    neat bury friendly nurse them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
