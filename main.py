0c8904ba2726f23e4cbdcbb1c394387cbd1afa93
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:31 +0000

    decision present plastic in actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
