c8166d49c95f35ccc58a6c67a398453564dd43e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:16 +0000

    claim bound unable fierce spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
