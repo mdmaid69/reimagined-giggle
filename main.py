f9f9a451ad624d2954a7f463a8e89fa198517481
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:49:56 +0000

    cum union process first read

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
