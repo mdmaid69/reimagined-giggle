cb9548653a9b4891f3d7600096f17f1b84330124
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:42:02 +0000

    community man in pinch taste

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
