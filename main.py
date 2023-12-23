ca197c49ad8f35fa731e2d6db110edb4f944a723
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:23:24 +0000

    lose matter sting dance sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+text = "Hello, world!"
+print("Words:", len(text.split()))
