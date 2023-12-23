d9df5c8d7d80621466a11312e40aa88169d97b39
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:18:16 +0000

    ear rough neat though hour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
