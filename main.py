ec1cb5046ae15ea4634543266dd752d4bfb83a65
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:27:19 +0000

    fact uncertain there determined empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
