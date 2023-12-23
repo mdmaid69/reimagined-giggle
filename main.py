6d438af99a161e09cb8f3c1f561915e81bf7aee0
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:11:30 +0000

    trust relationship science fast phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
