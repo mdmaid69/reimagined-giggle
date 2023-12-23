3849947ae19e8eee56f4b26eb1477b701c24c12a
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:54 +0000

    shout hollow lesson crash possible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
