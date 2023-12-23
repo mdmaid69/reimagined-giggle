0acabf21789fd85152ace26aa993e390f48c695f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:26:19 +0000

    straight agreement rent odd house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_power(work, time):
+        return work / time
