33625814f1bf1abeaf252d0147193134ff7f1059
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:18:38 +0000

    giveback might price complicated twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_str(array):
+        return str(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
