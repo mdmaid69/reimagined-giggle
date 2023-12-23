796a70f1ebe8941660fbf6a041db59b00f5b47e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:54:23 +0000

    pity 569enjoy estimate real recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
