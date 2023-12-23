1370428396dc13f0cc518284aea8a965c6a96097
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:12:03 +0000

    club reaction can breakout to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
