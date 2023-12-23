3a2428d2cf7afa0901fecb314a52c56b0a34ccc7
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:44:13 +0000

    something precise regard justice habit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
