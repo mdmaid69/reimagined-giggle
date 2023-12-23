851346799ef9ec68e0b3cd655af0c92c8b613ab1
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:37:25 +0000

    human first bang spite ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
