e8c11a1bae755c27c810052bfe1bec9098a96962
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:33:37 +0000

    behaviour quiet call past last

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
