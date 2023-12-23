77f23d545256d2c6ee6324d16a7ff91f2c08e39b
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:58:13 +0000

    rate considering lay male ours

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def reverse_string(s):
+        return s[::-1]
