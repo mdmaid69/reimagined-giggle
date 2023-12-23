dceac32d373d4d74f33a5d414792224185433adc
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:09 +0000

    design father disapprove fire characteristic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_unique_words(sentence):
+        return set(sentence.split())
