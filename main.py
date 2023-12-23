4eb59abf4e1e2e047cd5cef3dd0705d5617f06e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:53:34 +0000

    truth matter expression beginning nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
