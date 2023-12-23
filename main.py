0fd88f37646aba5f1f423f387d06ec05f37df958
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:20:09 +0000

    play closely afternoon sport friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
