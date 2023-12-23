6b439c17b6e9fcfe297a00ed5aa1697382d389db
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:01:24 +0000

    call goout discover law distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
