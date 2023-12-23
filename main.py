00b5758836197681ec8ce09438b695905ea7e50e
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:27 +0000

    shake responsible custom same file

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+print(math.pi)
