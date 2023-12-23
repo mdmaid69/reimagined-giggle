4d85307a4d792711ea983c1b1a80d9fec5188eab
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:07:03 +0000

    draw estimate beautiful clean opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
