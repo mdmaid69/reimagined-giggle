a08c7f1c75bb134a9927953c9e3868b6a79c322b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:02:25 +0000

    stage word joke pride scared

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
