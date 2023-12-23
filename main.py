972c4ed52f3aeab1735dd56b740bfdad492aaa10
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:53 +0000

    proper curious light bush scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
