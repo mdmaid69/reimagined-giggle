aa44ba8dcf8660e30f8d962735380726db88c14b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:42:02 +0000

    check safety getback event left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+print(os.getcwd())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
