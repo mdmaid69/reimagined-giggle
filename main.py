74ff7307ca792163d9b86762838af8c48bbbd851
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:40:59 +0000

    cost sticky instead female accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
