c987003cdb6e7e026647ca0d5c921b764529b439
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:45:49 +0000

    should cost tie turn soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
