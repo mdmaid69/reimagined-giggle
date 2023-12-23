a333bd25d82163295c98c268b59b05347b62c3fa
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:57:09 +0000

    message peaceful proud tired it

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
