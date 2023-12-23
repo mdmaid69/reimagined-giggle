3d162cdea9e4de8454217b2e0ed16feeeb154810
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:22:09 +0000

    amount list rest both country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
