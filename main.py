bfce288cba374bc75b25d555b08e3314ff464b9f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:36:11 +0000

    miss anything least cheap each

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
