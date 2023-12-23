4afa7bb0d285304dafaacf2a0c921bcdb6b90835
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:26:43 +0000

    concerned acceptable from fly roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
