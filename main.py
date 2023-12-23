e2d0ab95526827ba3220d5969a7efb41b0234954
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:31:45 +0000

    switch delight immediate add wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
