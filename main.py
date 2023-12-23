05a0410486b44fdd125d6362cba137a71b7ff2b9
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:23:54 +0000

    recommend introduction punish Gg tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
