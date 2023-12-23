612a4ea61d5d58c8312f8a918c1885e707a08dc4
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:44:32 +0000

    injury comfort price true whatever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
