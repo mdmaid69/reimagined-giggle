447e8aaf43f5e83049f7e5d2835d495ab2f60e90
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:53:32 +0000

    train bang holiday fair not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
