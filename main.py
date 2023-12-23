e8d6f504eccde8aab86fe18c9d380f4977815f4a
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:36:22 +0000

    true everybody pity field slice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
