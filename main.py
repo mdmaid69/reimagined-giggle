4d19c1fc9331b95e17c2f509256e531759e89460
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:05:40 +0000

    different dark burn paper early

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
