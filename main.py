a2adb0d7ec41347603b1911a6a1ff8e4752f812b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:07:50 +0000

    birthday contact variety someone ball

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
