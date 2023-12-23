6eced7fe77f129d6065e8ee0826d1709b66dba74
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:29:17 +0000

    regard wash spare constantly advise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
