3261940cff53efd2ab3a325d05d97b3374d8ac85
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:33:11 +0000

    natural swallow open Ss tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
