762af89054a17326dc9781f9faf0e2ebdbae0148
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:43:03 +0000

    touch along enthusiastic year risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import sys
+def exit_program():
+        sys.exit()
