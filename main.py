23d7fb8bbec58c51fcf16c218c0972c100ad1b66
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:33:42 +0000

    entry sign pinch dare wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
