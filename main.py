394b86a8c905680ec3aa75642f9d171f37a90951
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:36 +0000

    seriously noisy familiar change deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
