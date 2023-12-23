a0fec300ce16df3c16da3e1e6afa01888f903a28
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:57:16 +0000

    better usually sweep another healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
