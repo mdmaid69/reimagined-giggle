305cae2ace910def7edc7ae5088e064f42333cce
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:19:20 +0000

    see equal deal thing cry

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
