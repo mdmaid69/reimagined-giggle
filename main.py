83b47114d9d94936642f14127a93711e7851c69d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:37 +0000

    interrupt mainly contrast computer family

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
