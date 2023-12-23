64a4a537a6a919a8c313eb94382f13066ee7ded8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:23:27 +0000

    Nn vision Ee pocket pen

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def count_elements(lst):
+        return len(lst)
+print(sum(range(10)))
