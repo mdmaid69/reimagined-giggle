83a380d5d886996c67267050cd7f858c8bdfd919
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:30:43 +0000

    plan dish bear plenty holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
