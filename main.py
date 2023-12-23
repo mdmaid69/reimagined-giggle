3c63a745096c76670c7180879b05846df7a158e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:41:10 +0000

    home cover first exchange hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
