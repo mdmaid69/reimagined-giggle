6d1d0611725693be2f81d7adddb8ff7485405223
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:01:10 +0000

    hour nor definite object message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
