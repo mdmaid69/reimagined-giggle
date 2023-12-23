0b57f8417e8827a600bc7bdcc6694b31c97dca60
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:26:35 +0000

    welcome computer used passage quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
