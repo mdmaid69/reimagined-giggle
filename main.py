22f363d90efb92705a503e3adb4fe18e89316240
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:11 +0000

    concentration geton closed responsible back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def count_elements(lst):
+        return len(lst)
