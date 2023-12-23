cad4dc39da135ce1f449db29a26da3a533abbb06
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:55:51 +0000

    bound comment guilty learn stamp

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
