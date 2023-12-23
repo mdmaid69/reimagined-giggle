9faa09727d4cad9bb5d885ed4d7a26e0122d06a5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:28:34 +0000

    uncertain realize have save period

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_length(array):
-        return len(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
