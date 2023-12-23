24568c480dd4c2e89671b0ba72a291dd27c532b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:59:59 +0000

    so accident necessary remark land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
