de9bfa93b743bd2719ef242f5fb27185884d7fbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:26:53 +0000

    should answer experiment practice thirsty

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
