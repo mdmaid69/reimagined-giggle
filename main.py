03e338b8022af9325a2ad115bf9ea5e66319e811
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:18:19 +0000

    level range change owe hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
