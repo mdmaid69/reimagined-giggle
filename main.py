d0209c0f457cdf1f9a8d501bc08f71b840c0f0ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:41:30 +0000

    somehow accordingto tough matter height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
