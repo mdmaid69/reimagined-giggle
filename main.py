9c73a9b0db1cfa0c5cc983df982ca88d124e3710
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:12:03 +0000

    mend explanation freedom absolutely few

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
