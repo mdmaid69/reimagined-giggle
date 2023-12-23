6aca36a199c6cb00cd81ff3322a7910b49e05f56
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:27:20 +0000

    rescue earth place amount open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
