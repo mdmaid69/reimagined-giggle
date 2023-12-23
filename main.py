fa707d0cd6bed8ffe6d63a974fedd0e9502ea418
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:01:39 +0000

    mainly letter claim water trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
