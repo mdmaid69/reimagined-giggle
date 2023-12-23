50ccbf9865247dc7ddf015a7419a32c2966b6e3e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:37:05 +0000

    as Nn best greeting see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
