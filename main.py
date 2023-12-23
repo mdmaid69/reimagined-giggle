38fa26b1b2e4af9ba394659b3f7b383afc0b2995
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:24 +0000

    confident famous find crowd bringforward

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
