70de258184bcfcfebd002785d1d99ba3e1d8293b
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:47 +0000

    dick far often reduction switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
