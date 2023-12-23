9db3a3d0ab10861c0a683ecfcbeb7815570b9f79
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:54 +0000

    middle punishment back street female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_density(mass, volume):
+        return mass / volume
