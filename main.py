f804a86e18fcd5d042035dd5f365541ed454335c
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:43:44 +0000

    roll male what government mystery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
