2b8fc2402b02c7039082ac9f22d4b96afb5afcc2
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:12:48 +0000

    half control anyone difficulty embarrassed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
