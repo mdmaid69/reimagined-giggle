7d4e3702fe21e6623a722955676fd81ac18946f0
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:40:44 +0000

    middle activity straight below this

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
