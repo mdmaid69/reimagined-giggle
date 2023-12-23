ba8108b25fa4c7c9bfa2df49ea582da5599f0771
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:28:21 +0000

    plate beneath keepout increase prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
