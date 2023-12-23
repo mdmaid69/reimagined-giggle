36dc50ced32c661b53ad84635a0283ce824390ca
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:46:00 +0000

    minor fresh that satisfied wonder

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
