478d1f90ca38b165931a4f6d87dfc265b90c6ab2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:45:42 +0000

    inside rather mistake ticket criticize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def count_elements(lst):
-        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
