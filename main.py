805a344e42c4f072419dddb6ecf438b592791bda
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:54:12 +0000

    silent one runaway please unable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
