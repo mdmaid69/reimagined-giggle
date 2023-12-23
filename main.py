843d21900e39edf8e67f91bcc337b44af695edac
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:20:53 +0000

    deserve possible guess clearly minor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
