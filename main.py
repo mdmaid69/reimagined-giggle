4136a48edf314b31618ec435f7977d2aafcebd26
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:27:06 +0000

    influence length opposite quickly narrow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def subtract_numbers(x, y):
-        return x - y
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
