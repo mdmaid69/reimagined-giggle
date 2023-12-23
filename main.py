5fe89dd4f67c75a1c878740b5ecbe9ab88d452d5
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:20:42 +0000

    task other interruption tear test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_pressure(force, area):
-        return force / area
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
