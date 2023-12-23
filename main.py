4d54304b68a568cb13943799c6ffbd7fa509a6c1
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:10:42 +0000

    bang period pump building flood

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_power(work, time):
-        return work / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
