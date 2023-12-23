afd02919055dc69c8838dedc253cddc0e8aba0f1
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:19:47 +0000

    acceptable 179bite over better shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
