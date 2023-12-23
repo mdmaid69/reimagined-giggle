18c8dd4ee31242fb8063d07f5d1ed64cddfc9456
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:57:23 +0000

    busy behind exactly but dance

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
