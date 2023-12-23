335700056ea5b773d69a821d0a074dc6ddc66f59
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:33:27 +0000

    paper up health bin cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
