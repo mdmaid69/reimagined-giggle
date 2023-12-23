96c70be6e558e785bbe506bd387357c762275b52
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:35 +0000

    match run art loan peaceful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-for i in range(5):
-        print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
