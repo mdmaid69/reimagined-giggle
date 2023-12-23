cfbde31478759872a31ec1d53b6a835a88370fe7
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:29:48 +0000

    scrap belong land design same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
