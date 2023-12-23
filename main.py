4bae73d2cd88814ccaa82db120f07a96009f3581
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:46:39 +0000

    stick temper exhibition pin vision

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
