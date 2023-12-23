042b8c09e0e2c9188e2a835f69964d601617e62f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:24:04 +0000

    make rubbish instrument flow turnoff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tensorflow as tf
+print(tf.__version__)
