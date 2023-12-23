966739abe174fbc2fb997b57c19ea717d607c7b0
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:48:21 +0000

    part example circle country kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
