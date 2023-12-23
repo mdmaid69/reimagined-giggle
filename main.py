9a990300517feac41927f2d6b600e6b8b86c5ac5
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:03:26 +0000

    right case pass permission drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_set(array):
+        return set(array)
