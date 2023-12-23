6f5d1b9e7b5de97be95fa6e85f8e39aaf017abe9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:02:44 +0000

    exchange breathe hit OKorokay repair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
