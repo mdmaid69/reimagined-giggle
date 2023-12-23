9be5c48f618001a33de4d4daeb405ada2453362b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:17 +0000

    suggestion recover advice sea match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
