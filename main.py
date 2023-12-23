88b666286f9bca1e59ffec678e1d42a9d6d7621b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:20:06 +0000

    classroom shall again lift bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
