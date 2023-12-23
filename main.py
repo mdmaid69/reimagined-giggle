9018b627dd5f223647309515f96d7aa60412efff
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:19:52 +0000

    thumb operate wash advise rest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tensorflow as tf
+print(tf.__version__)
