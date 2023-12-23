b7e7861c9a495467c830fac89b440cfa54644ba4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:01 +0000

    whatever equal myself just tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
