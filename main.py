4a3c8f4a76959989a8fcdd630df96ce4314935ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:08 +0000

    plant many pleasant neither on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
