fbcd8f444e054a73ede494ac40721f7ee4e71a51
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:14:05 +0000

    trade pin basis congratulate science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
