d9b4b6c1fc0b2201dd403f5838b317d15817e260
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:44:05 +0000

    hold send occasional people mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
