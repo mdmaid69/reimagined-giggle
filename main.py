6e31efc38fc2456e8d1e2da38e710dffb3b867a9
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:06 +0000

    judge we polite back taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
