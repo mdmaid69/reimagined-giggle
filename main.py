725732d5dfb85f5a1c1a8b57a356715c89ea1db4
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:53:15 +0000

    explanation activity sentence lower house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
