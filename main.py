6e8d59162aa0dc37e5d6911d6346296751019d90
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:32:24 +0000

    emphasis tap nose dig remember

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def is_odd(n):
+        return n % 2 != 0
