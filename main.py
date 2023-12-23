2dde893b9268871d3f15469e7cc24077ee43da22
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:38:22 +0000

    mistake lay themselves any intend

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import time
+def get_current_time():
+        return time.time()
