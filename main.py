a023eb01f4b36f77738b59b331cabc828b4b6f0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:09:10 +0000

    attempt turnaway sorry wave bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
