d88ace5e5bc467b0616a1e43d3734c0ca7be0cf5
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:21:32 +0000

    float your protection wire convincing

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
