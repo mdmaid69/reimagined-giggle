0ec627ddb0e3285874fb31865cd3952f1789b76a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:56:32 +0000

    direction typical hers fast turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+for i in range(5):
+        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
