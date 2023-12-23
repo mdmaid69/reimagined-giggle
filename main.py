e2c1182040edff6e7e65676943289a39f00cea64
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:53 +0000

    squeeze action doctor loss good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
