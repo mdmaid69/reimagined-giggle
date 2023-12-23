05a66780dd4db77079639800438fb8abee9447ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:23 +0000

    geton scheme its space friendly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+print(math.pi)
