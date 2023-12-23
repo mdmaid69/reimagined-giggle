92809ebc9080352467233a0ceb8faadce306350a
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:14:36 +0000

    true destroy wrap consist listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
