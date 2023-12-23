e8699a530d39aaf8c5ef4b3440893fc6f52ca94b
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:04:08 +0000

    downstairs dream congratulations pen hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
