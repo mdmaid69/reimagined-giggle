7cb9db04dbff29408b256c2888322d662c1cd769
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:29:21 +0000

    invite shrink separate turn regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
