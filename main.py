040ec1e9b7b1459403bdf1d4f32b63e1551e0a73
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:14:48 +0000

    first declare hang perfect sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
