57b6c52e529fbf9e2a8680dc567577002d7913bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:23:23 +0000

    point slice turn example willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def get_time_since_epoch():
+        return time.time()
