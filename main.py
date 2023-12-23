4ab9dffc0deaecb506399724661822770e14d5c9
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:35:47 +0000

    Vv nice bank organize own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
