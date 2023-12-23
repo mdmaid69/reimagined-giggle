d1b1cf390eeba6be78bef94d44011d7f14279ed7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:22:28 +0000

    fetch manage long tragedy plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_counter():
+        return collections.Counter()
