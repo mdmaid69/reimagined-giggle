1fcd3fee5c7fc99d46635e02a96cc50b9c42dfbc
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:11:17 +0000

    married stay lift Itself between

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
