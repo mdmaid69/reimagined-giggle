e76799d31a115442a16fb49257046d88bf17982a
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:35 +0000

    quiet spray television suffer thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
