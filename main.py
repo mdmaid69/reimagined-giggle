0831150d0ba961c6e34d998f848e6df9f43ff090
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:36:38 +0000

    downstairs increase fetch view table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import getpass
-def get_username():
-        return getpass.getuser()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
