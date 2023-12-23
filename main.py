1708da15e3ebe7ed1c63511fed60fdc0f49f8173
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:40:44 +0000

    under build trade everybody since

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
