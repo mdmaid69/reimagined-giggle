2042341d077c9d2a73adf4f73cb65d843d6f740e
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:57:56 +0000

    ask certainly present fortunate square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
