66431a3b471672d280b8ccb8a6bb832a06b043f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:59:32 +0000

    afford Pp job town tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
