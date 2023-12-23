bc730b9fb72ef4802717b63aa471d2cb5947c10c
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:34:31 +0000

    part duty mean square chat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
