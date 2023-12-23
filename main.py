a167f10176958a82ff5d14869bcce96ffe207f3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:11:29 +0000

    yourself high obvious check accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
+import sys
+print(sys.version)
