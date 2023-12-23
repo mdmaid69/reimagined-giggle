ca801c430858ad86ad01c21bc08ce5a1fe3d9d37
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:50:46 +0000

    exchange environment ready mine favourite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
