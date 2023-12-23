cde9a2c6369d1015dce9de622fe888b7a0577e49
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:18:02 +0000

    complete suggestion through heat clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
