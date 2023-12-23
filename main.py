691dd24a647ef761d262338abe121a27cc12c203
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:30:13 +0000

    usually noise kick dick sail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  def convert_to_binary(n):
+        return bin(n)
