652d09068ed357273ef5d25a8eaf3b5d34f571d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:23:24 +0000

    apply attention subject backwards mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
