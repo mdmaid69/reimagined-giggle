50481c208e668cefb24bc67c96d7eafef438d4ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:13:19 +0000

    talkback confuse himself cheek less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
