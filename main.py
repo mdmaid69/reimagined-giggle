8c7960e5c5a6af6342cd86a1b4825cdca21344c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:48:40 +0000

    sore frequent mind progress thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
