690711bb92b64cff7fed09be272c3a63b8789c3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:09:48 +0000

    law face find 565end circumstance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
