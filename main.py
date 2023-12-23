f1355e9bc88d4e4f5cec27ed47f5d5e71c66f27e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:36:44 +0000

    tonight shadow die oil callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
