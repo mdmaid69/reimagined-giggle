08945d1d6565e81f4c6613f93aed6192b1a457cd
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:19:09 +0000

    noone place here strict example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def greet(name):
-        print(f"Hello, {name}!")
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
