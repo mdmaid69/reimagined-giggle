edb9e4c99098793e7673c1c1169d5524e5759850
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:14:51 +0000

    development disappear confidence unite answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
