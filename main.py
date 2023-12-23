0a4b9d7cb910227f4e9a0e8ea0bc547879683735
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:29:15 +0000

    fairly callup dry message harm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
