4522011a45cd0270ef3ad22f707e65d6a07a76fb
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:07:53 +0000

    them promise old soak born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
