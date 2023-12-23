20366cd86412eddfa5cbe6f9c879e142bf5648a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:41:16 +0000

    answer manner better gather stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
