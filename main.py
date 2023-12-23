1405707c6cb8bb157ed3e3084a7b56ff78bacceb
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:46:21 +0000

    warm comedown attention unfair further

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
