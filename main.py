7abda56899926efdcc9fb1b5bc50d8cca0f0148a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:16:45 +0000

    transparent create play money fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
