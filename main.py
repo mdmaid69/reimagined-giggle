a111c282e4e1949abe9ef142600bd12c06e55b3d
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:03:35 +0000

    cut meeting instruction opportunity strong

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
