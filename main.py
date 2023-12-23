4f79eb3781b2a91a6db09e88e821941fe0175c90
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:17:24 +0000

    consist concern ask crazy also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
