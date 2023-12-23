ec1ee8f34a5cf417607581493e7699d7646eafd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:11:17 +0000

    usual maybe label willing mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
