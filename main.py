ecd3f8a755cea95fac9c594b89fa34daf8fe9987
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:51:06 +0000

    comefrom space breathe store drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
