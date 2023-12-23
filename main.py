51550ddd402e3fa6939604415ff63c97789e26d0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:30:09 +0000

    person performance country cause impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
