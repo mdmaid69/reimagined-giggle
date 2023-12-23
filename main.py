a8f0f0f3dd38d9536541a70aee1eb9a301e7075b
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:51:40 +0000

    hurt practise always Ff without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
