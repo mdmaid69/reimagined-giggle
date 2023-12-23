8f4dab1e56f55deec8b8b1967d93dc3e77bf0e6a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:50 +0000

    hour loan turnon lead knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
