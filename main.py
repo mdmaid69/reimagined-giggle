0eb4556515d2c79648b88b570b725e0d0402a957
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:40:27 +0000

    represent invitation laugh spare worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
