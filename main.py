d28ec2ecccdcda785d9143532ede8c8d553cd843
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:09:22 +0000

    doctor corner luck fill seem

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
