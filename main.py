2a8942f0151ca42c4b77de1f5cf606febbc523e2
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:26 +0000

    dangerous deal turndown your talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
