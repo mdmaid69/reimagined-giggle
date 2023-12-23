d3d922ab38994b3bcdac05d95d8e42806c66db43
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:54:30 +0000

    attitude suit half brown produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def get_array_as_float(array):
+        return float(array[0])
