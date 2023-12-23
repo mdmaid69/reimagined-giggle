2f3de5c4c47fcfe4bf402046a214bd7f68eff25a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:46:03 +0000

    hold unpleasant radio priority match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
