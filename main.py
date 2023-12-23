422aa198bad90ab6a894d68fa8aa1e7a2ac18092
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:07:12 +0000

    offer feeling finish general print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
