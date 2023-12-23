c6d1b09ff8c4deed7c8bb16f10de186282276508
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:04:36 +0000

    train split discover giveaway disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
