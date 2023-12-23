7e2f6c6aa506ddda14b183937af9133d8b279a2f
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:39:05 +0000

    slow friend blind round system

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
