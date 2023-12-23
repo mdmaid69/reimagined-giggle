cb862573d34341f01aa673950498b3d612de6623
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:58:07 +0000

    geton comein result benefit design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
