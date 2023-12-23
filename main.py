4466f5776d9834ffecef9071cde2d654b5fd7473
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:50:26 +0000

    leg sky building advantage emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
