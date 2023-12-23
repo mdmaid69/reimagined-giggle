3ec52f49c006db949ba62862321d8edea97dafc3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:45:31 +0000

    luck sting afford Ss murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
