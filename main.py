1b60e5452df78582c4c7556d327cd904d5cb4d42
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:52:09 +0000

    exactly taste together message also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
