99b6932e907e8259f90a70a2884924e315eb932c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:39:49 +0000

    container hurt sting chin thinkover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
