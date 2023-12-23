e8b008c3086e8d6160ceb0b599ce4bb3600f4bf0
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:20 +0000

    regional worry direction yes cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
