58c46e426b2211a4e0374bd55f07407f96a84252
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:25:45 +0000

    match 572enter occasion flow innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
