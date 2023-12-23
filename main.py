db2bf85dee3996cb8ad94ba2e76b019fc9e8ad4f
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:57:07 +0000

    lunch report news bag tread

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
