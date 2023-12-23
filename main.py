71c659f75781f176a70a7cdfff61177b6d2b3ed1
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:37:39 +0000

    trick during maximum convenient her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
