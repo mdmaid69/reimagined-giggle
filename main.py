a7eb76ee130c36a84b83e0e0bda8a00a31c2753c
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:45:13 +0000

    slide level actually Ee concerning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
