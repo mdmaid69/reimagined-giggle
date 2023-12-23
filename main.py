0cc1523e317b25ecc235eb2642f7a1273707cbf4
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:10:30 +0000

    strike operation doctor married school

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
