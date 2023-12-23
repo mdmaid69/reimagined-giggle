717ed8f0483e3f13b622a2f67a7164c7622d8c4c
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:12:20 +0000

    print allow wipe closely unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
