5623e6d51e65f1e4bb6bb10936390c9cad178de0
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:05:59 +0000

    him question yesterday stare head

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
