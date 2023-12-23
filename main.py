717d6ad74d5839f23e4bd80592243f57e3282946
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:19:47 +0000

    Yy rough tread realize keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
