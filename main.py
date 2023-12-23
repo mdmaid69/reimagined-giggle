c4b31daca47455677d45a010e05042a54373afc6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:02:29 +0000

    promise fast live touch wind

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
