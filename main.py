b4bd676969487a86e107e19f5606dc3e50935485
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:57:29 +0000

    thanks expression form branch hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import os
+print(os.getcwd())
