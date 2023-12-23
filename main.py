2d7776e9bcdbbbd09e10710b697db64ee69f81b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:27:29 +0000

    rest use achievement fortunately respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
