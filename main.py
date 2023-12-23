0260ec190742d337b255339a09afacf6973c3530
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:25:10 +0000

    sell exploration willing plastic print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
