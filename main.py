863a3b93ae099d79c2b7f1cfe1e640f6161fd7dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:59:08 +0000

    machine exploration fail attempt OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import os
-print(os.getcwd())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
