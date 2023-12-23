93a476a9a329409e246dab4362828b143fc9ff0b
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:40:13 +0000

    talk apologizeapologise person information slightly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Reversed:", text[::-1])
