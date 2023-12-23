d4441f16ffd5803b9f819de24cbdd95480431e90
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:03:44 +0000

    chief tooth start 573entertainment private

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
