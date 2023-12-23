e40c68cf727fd769b0adc6724f57ea818c763365
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:37 +0000

    improve exist slowly contrast life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
