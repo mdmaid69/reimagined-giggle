54432735fa5f1a6ffdb91d4957b585720d24cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:49:41 +0000

    identify noise whistle quit second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
