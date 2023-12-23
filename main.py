c193cda8a5048a1e699b0fda45a4ad200a58ab49
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:04:44 +0000

    bottom drive sister thirsty delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
