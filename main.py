5c69ed09e67f095f9a75f0c789f2a9d7ba791432
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:12 +0000

    listen pay do until agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
