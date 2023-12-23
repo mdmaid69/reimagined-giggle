90ef3a884b9a58cb2f6678beef0f00ace158249d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:54:05 +0000

    date name threaten comeover you

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
