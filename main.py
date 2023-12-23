a72dbcb23e8193a59d806eaf1e7003cf4fd457f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:28:59 +0000

    provide beyond middle statement peace

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def get_current_working_directory():
+        return os.getcwd()
