ad813c4890e66102896f201ca128c2adfe8cc63f
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:19:01 +0000

    suckme pay float anything control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
