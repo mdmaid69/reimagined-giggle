2db18b310fb91eb5649b586bc8d4b8a6eb29fcae
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:57 +0000

    you dick what realize rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
