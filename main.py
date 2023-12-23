44a076bec823a09e59bbf695a8f8af0b8a924cff
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:48:30 +0000

    lookafter convenient money age across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
