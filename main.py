230b7c959d15809190fffc6cca7e3cfb00c76b76
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:31:59 +0000

    speed minority minute official minute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+print([x**2 for x in range(10)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
