3228eb36c54fc1a7232731f418f228b6c63cb417
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:05:18 +0000

    increase must contact tongue pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
