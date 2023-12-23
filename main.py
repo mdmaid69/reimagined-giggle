0219ca5b6e0daf8e65b8ae604270713a4c614bb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:43:58 +0000

    pencil pleased behaviour herself tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
