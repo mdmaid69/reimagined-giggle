e8ef9ebe2000c7d01554049f46ac74f8880a3896
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:12 +0000

    refer long correction produce decide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
