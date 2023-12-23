bbd15112cd586af67e64eb4bba65e3cf597087a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:16:52 +0000

    one very vote task fold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
