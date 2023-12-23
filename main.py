b8b916ce8a36c53882339b63600eb772dfa4f3bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:53:02 +0000

    breakin fence centre hold embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import datetime
+print(datetime.datetime.now())
