cc8a524a7fc650a42d90e633a34af5e3fc376f33
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:23:37 +0000

    sky decision fast actual moment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
