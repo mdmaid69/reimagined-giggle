91fdcbe761ebde77822e2299c94ce665effbb85c
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:48:12 +0000

    country me speed complete tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
