856894b65b0ceaf4bbb2f5eb977761e42455bbac
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:13:49 +0000

    please sticky makeinto sign this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
