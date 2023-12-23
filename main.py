ea85d271590ed3f348183df243c99d974ec6103d
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:13:05 +0000

    570enjoyable fire argument awake sort

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
