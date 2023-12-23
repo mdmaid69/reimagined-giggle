7890419e87e37f32a2bc550af947664b759fb45b
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:16:20 +0000

    allow breakfast cool than little

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import random
+print(random.randint(0, 100))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
