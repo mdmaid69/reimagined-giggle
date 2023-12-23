7204a66c9682ea947f8722b41baa59f49921118f
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:49:53 +0000

    post high story ocean workout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
