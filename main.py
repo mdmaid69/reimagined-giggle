63d9676618b5ecce0d96fa07eed1b576991a71bc
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:05:01 +0000

    Rr rate interested accommodation bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
