6b1bf0abfa3d9ae251a0c9c717710bbe4fdec618
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:36 +0000

    worried hot less protection control

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
