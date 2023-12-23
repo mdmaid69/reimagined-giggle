52e9e44359c4543aa14157a4b80e44b07da999be
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:13:40 +0000

    lookback(on) mess turnup underneath accidental

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
