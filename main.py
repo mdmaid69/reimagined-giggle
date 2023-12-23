82f95821474df5c06f27a0f3b7c31de3742bc1f9
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:05:49 +0000

    present turnback equal advise aim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
