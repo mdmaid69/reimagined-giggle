dbc471184274a15625fe46fecff1101fb4e366b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:20:31 +0000

    stamp failure present big flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
