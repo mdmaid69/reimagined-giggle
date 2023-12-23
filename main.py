abf849db314b17f8b01b3f0357beae32034f9739
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:26:06 +0000

    steam develop fun picture regard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import time
-def get_current_time():
-        return time.ctime()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
