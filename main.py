b54ab3d30739d1ceba58363ee4e28f85d6354057
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:47:25 +0000

    snow radio firm period far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
