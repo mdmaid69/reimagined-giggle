9dd9437232be2f5edc56743548d019bc4d646baf
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:45:38 +0000

    final bringback letter talkinto exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
