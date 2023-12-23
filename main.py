73ce7d06b8a818a128ec4e10937d6f1f1a11ce01
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:53:18 +0000

    eye deep alive possible Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
