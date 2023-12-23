54cab72bfb064065f66bfa5eda62c0fb1c3e80a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:25:04 +0000

    spill his makeinto scrap after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
