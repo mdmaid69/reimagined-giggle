5ab3dbe685141b16f075e3cff9913f06af9c5874
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:14 +0000

    to journey now meeting size

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
