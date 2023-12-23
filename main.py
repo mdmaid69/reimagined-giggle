8b5a1479ff4b1e298a52e1aee5afda4220f787d8
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:52:30 +0000

    goodbye change choice post habit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
