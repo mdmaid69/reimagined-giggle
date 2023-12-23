ca17d7ac8f30d77af5bd2690d913e519a531dbc7
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:04:14 +0000

    eat hear interview recover Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
