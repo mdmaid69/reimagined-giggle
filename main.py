ead2fbcff2c5198abb8377d42ab61656f8012916
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:32:28 +0000

    job shallow precise light before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
