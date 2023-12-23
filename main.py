709d09d978c73dd6dad6a7755137b659c81c33cf
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:14:46 +0000

    continue debt side please effort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import random
+print(random.randint(0, 100))
