65825b0cffd2574720a235a6a46e646b642180a6
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:55:14 +0000

    before run problem stamp fit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
