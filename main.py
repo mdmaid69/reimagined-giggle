ef13c7b66358a27b984a48a0c0c6e2ba756c7452
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:17:23 +0000

    answer never reach translate post

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
