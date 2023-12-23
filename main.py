219fc36695da3669e52d16d2b9971ba478d53983
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:41:22 +0000

    relief dinner whoever 573entertainment callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
