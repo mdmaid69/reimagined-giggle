39a4f0343906ed37d933251af1422dae3c408625
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:36:29 +0000

    suit live print feature from

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print("Hello, world!")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
