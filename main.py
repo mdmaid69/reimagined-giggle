0fff9d86576848f36cd9b2789e7d2c7177b43d0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:26:53 +0000

    calmly natural fear match through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
