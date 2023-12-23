21dd24d7572a4e6ae80ae2223627e9fade715d8f
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:59 +0000

    quit nose welcome shoot bank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
