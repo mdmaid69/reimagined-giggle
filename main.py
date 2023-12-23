cd8ea4eda39467a8eab8b0190bead9e4e16386f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:33:20 +0000

    account circumstance shrink sad heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
