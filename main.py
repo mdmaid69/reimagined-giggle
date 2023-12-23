cac4b117c681d38c42132c933b37cfb9a2581bf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:30:17 +0000

    stick bill base direct glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
