f9d06abf7ac20e07847e828d76015ed3001e0677
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:46:00 +0000

    intelligent today instruction fasten dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
