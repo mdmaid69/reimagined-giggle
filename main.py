69a7f14d5b4c23f1f5c7d0b934ed7abacac54f96
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:56:08 +0000

    empty keepaway success injury completely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+x = 10
+y = 20
+print("Sum:", x + y)
