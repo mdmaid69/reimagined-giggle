b4f0cbdc82eb6130d90aa067c9a8d4310e335ede
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:52:00 +0000

    period pleased chance important frighten

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
