4c43feb99803b1d54db9df46be40cb6b6bb97281
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:08 +0000

    emphasis voice in shut key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
