aeff9b739fb4bc03a0870adb57af0f9e690ad4cc
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:41:54 +0000

    head 565end many train afternoon

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
