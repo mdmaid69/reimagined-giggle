db95385c55a5afd5b32d8bc4bd5000dc5cdd6cda
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:00:37 +0000

    charge good extra description getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
