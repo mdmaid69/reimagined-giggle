68f67c8ab0491a1da58303650a1b916d4684fdbe
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:01 +0000

    cope Nn wind detail aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
