90bae24772a1bce721ca6b1ea463293e2fe26b97
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:23:43 +0000

    fire rank now not nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
