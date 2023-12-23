e7cda19b485285c62ce529a904fb52f61fa15ebe
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:23:09 +0000

    now holdout big thank damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
