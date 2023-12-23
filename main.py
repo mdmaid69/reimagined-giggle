e7cefd6aacfd8e681b60358fd96415de87e514da
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:51:34 +0000

    mouth object pass borrow hope

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
