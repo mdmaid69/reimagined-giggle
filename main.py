6760eda17ecb33aa75c90810165d12114ed66c75
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:51:17 +0000

    bringup supply sea blind suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
