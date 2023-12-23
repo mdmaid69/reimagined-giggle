edf61f4ef91fe13f7afc5b9bf37bdb52e2b6cedd
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:07:47 +0000

    dream comefrom either dear wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
