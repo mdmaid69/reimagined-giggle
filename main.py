329d9e8e9cf56ed77ab6044ad1e6ee775d2ee7b1
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:17:55 +0000

    nigga stranger community most judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
