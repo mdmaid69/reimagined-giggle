733c00d5675109d2ab75a3354a7a5b745b393f0f
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:34:30 +0000

    dear accommodation defend risk fold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
