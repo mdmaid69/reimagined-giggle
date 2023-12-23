9d9571a5bfb2d78e00279152a4b33f5c1ca6d541
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:28:01 +0000

    cold miss different quickly hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
