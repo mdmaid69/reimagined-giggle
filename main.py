85a611c2de64d5c2b9775a062d440ec511759088
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:28:05 +0000

    pour mystery attention runinto recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_power(work, time):
+        return work / time
