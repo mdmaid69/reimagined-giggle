b94fd2993d6e1c2199a03015e9fa6481e34086f6
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:36 +0000

    nature search spot rough petrol

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
