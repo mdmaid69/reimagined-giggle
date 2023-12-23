acc6cd932cc269cabe53096b967166abd5e92ad5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:04:42 +0000

    office peaceful over more division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
