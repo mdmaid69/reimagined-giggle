d8074004f8df9b19c5fbfbe658d2b71831b1bb6d
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:49:10 +0000

    real practice pressure thorough close

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
