37d3d247027aedbbf0187a57eedbf4e5c24e3fda
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:27:52 +0000

    environment goodbye independent meet sight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
