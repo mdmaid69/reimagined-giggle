e10c054b9a7becb6da8a428ec1b93660a2c60f4b
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:29:53 +0000

    shout bunch strong study speech

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
