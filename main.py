bf8f67546fcd0b133e0ea5967ba49a1190c1113e
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:54:52 +0000

    message classroom holiday judge tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
