63ee0b60ae02d0aa930969ab6012864df2317ea9
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:47:08 +0000

    teach control geton look nice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
