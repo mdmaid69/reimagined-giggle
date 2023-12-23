f44128df90470a7a17474b53dba1d1a54d49a7c6
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:19:25 +0000

    meaning comedown upstairs claim stick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
