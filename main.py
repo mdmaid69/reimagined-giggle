c1b2ed9d1179545d10217761fae855b7fb835216
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:49:16 +0000

    quality possible Ww tooth save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_complex(array):
+        return complex(array[0])
