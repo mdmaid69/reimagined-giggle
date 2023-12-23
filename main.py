28c4880e83f30a330148dc1d535b88ac4aa1063d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:11:39 +0000

    long careful cover fresh threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
