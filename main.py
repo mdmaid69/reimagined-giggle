5df42c027067ed1717bb31c41f6d3d35bc20893d
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:02:57 +0000

    flat lookafter drive little complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_typecode(array):
-        return array.typecode
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
