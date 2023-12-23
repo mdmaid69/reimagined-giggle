e52f544c44fd6e31e8c47d4501dee6fd05a5a867
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:40:09 +0000

    design along matter crash either

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
