4a6262f954167a9eb1cb233bc8160f97369ed196
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:01:32 +0000

    hurry cough excellent twist actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
