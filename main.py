8185a3dad41a4adddaf72ab133096511ebb3ea52
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:51:18 +0000

    fill calloff cool paper giveaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
