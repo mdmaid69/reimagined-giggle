db476f1968030f17fbca8561b55fca4002863e37
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:34:52 +0000

    load behind lookup last trip

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def count_elements(lst):
+        return len(lst)
