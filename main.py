b0b217710d73ee5982030a033559aaf3fd218224
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:44:51 +0000

    stick mess lock name sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+print(math.pi)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
