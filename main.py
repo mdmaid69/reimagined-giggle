9213195162b77a88a914365f8fa164c91badc806
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:10:48 +0000

    thank concentrate more report runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_length(array):
+        return len(array)
