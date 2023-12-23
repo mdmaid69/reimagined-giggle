4ac841b83b30e6932bc68f4d1fcce6f5ea186eb6
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:03:34 +0000

    such story face 564encourage dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
