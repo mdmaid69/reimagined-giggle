905579984814e0a30863179eedd923f4cdcf4367
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:49:20 +0000

    scared hate development cut juice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
