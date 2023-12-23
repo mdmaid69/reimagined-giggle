74156431bba498debc3cd62493d10c16900ff07f
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:52:09 +0000

    stiff truth confidence recovery promise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
