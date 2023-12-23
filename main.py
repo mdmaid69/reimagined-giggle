43b4ec2b014a1321c51789fe64d538d4e5f51a76
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:36:01 +0000

    mood bringforward evening accidental process

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_max(lst):
+        return max(lst)
+  def is_even(n):
+        return n % 2 == 0
