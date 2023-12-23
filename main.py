9f9335c3e19a9c5f6efc948bf7fce4fba2af4a22
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:49:05 +0000

    joke notice birth support snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
