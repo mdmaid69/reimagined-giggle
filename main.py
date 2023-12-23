943888e2a7c11c8656019b69b5ab942c60a191f8
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:52:31 +0000

    second unhappy wish ours save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
