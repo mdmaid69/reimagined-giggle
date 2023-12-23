e0c7acdb6b28507652f3d0ae6122320913c409d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:57:11 +0000

    lesson thumb skill polite march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
