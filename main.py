fc8cdcf8da211a39f12e25872eaa1a2d891e716d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:50:52 +0000

    way meal variety grateful swing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+for i in range(10): print(i)
