98c5f5c7cf67d88a9daf9a0075a22baad28bc954
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:02:29 +0000

    cope prize trade harm guilty

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
