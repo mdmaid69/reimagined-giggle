13d3892de806d2f3c5e598c162574aa38bd9daf3
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:12:06 +0000

    repair raw persuade worse speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
