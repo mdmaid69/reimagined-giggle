f451cdd1e68069761c35ab0db0b1f6350fa7df75
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:32:33 +0000

    impressed turnover trick glad truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
