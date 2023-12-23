aa73bfc35ff99366af35f2681be011c8421fc64f
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:45:35 +0000

    out justice space month drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
