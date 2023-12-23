3c71463c778ead7f474534d3508055e0724375ec
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:38:11 +0000

    tip fierce disagree turnback comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_work(force, distance):
-        return force * distance
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
