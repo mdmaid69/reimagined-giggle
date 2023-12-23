544e7fa3a27d17e23c139586226e8d05754b122f
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:53:50 +0000

    deserve Mm answer appear woman

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
