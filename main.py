fcf5fe7e3beeb9f7f6a953505cc897877ddb9098
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:44:06 +0000

    wire charge narrow close actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def remove_duplicates(lst):
-        return list(set(lst))
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
