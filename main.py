b823a6f649b018f272e9b443021330537c2ee660
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:51:20 +0000

    support sensitive unusual callup easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_work(force, distance):
-        return force * distance
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
