95c011cb20fc5a394364a9cf9720370129ef5ef5
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:21:18 +0000

    serve trouble complicated turndown eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
