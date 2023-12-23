0a2fbb7245fd2b1a39a4ce0b67f54bd991986606
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:23:06 +0000

    drive fresh whatever thought muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
