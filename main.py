2b2653a43298720b5954f7804e579eae6f56cdea
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:26:00 +0000

    now deep control march anxious

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
