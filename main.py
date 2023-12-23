cf567ed68cada44d25659cdcccf7f398649fa672
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:46:22 +0000

    combine by produce advantage fence

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
