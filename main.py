b09219ce02721db19f849147a1671c9d55a19aa0
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:25:38 +0000

    closed public edge hurt birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
