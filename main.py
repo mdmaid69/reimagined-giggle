3017c63b44470e32118472b896f95423d9b913d1
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:49:41 +0000

    ability completely sure operate boil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
