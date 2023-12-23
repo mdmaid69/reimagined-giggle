ebcb9c9869fbda0229ade6c6fec06470f5d827e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:45:38 +0000

    count balance almost set compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
