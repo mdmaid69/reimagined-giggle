072eddc9fa7e41b346b56d01e7796c403a1bdf19
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:21:50 +0000

    impression from circumstance city would

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
