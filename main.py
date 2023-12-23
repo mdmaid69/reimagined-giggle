3dda24d7d8a044a34564970df689dac44ba700d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:27:45 +0000

    however convincing reserve complicated excited

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
