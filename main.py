1d81615c002c7da71daac6c37187b116ffada38b
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:53:42 +0000

    responsible test spend bend window

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
