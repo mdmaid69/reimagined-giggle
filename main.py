ebec0604ae0a615651e7303428bac6bc3f49a451
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:19 +0000

    ours less whisper hear unhappy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
