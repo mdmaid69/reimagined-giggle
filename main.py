2d3719878138211710648b331b26c588bb0f433b
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:56:41 +0000

    thanks wrong home account short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
