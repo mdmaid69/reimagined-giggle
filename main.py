ca65baad7eb59cbb4b4b2c93769dd98635d8e745
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:15:58 +0000

    future safety spot different measure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
+def cube_number(x):
+        return x**3
