0f666324f95c2b01ba5326ca832f127c9113e9b3
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:14:44 +0000

    much reduce by that ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import getpass
+def get_username():
+        return getpass.getuser()
