117f8c34d2a49e5ed3ea82829df3339c0bf0cf71
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:52:43 +0000

    space bet bill breath plan

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def add_numbers(x, y):
+        return x + y
