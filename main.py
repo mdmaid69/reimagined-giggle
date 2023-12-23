b475ab47e2e225e019411010d8a1f81f0d0dff32
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:30:54 +0000

    base between spend skin wing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def add_numbers(x, y):
-        return x + y
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
