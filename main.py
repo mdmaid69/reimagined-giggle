0f44680c83fde1d3632b9a0b84b96cc0aba5831f
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:29:09 +0000

    leg travel factory cause black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+print(re.match("h.*o", "hello world"))
