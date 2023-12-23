1ae6131edbb81bc9c3c595efad92badac3f1bcb9
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:23:59 +0000

    exercise appeal bunch provide angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
