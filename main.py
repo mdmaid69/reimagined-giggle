055eb3a84ad54236ffbf9db86c3cc97730194fff
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:00:29 +0000

    break payment approval Rr gate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_work(force, distance):
-        return force * distance
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
