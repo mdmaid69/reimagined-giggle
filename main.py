f34681eeea415a627c42ea61ef31eb21c06a943b
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:07:28 +0000

    runinto stamp save game ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
