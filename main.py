adbbe06f27b1f8635ab6cf4b38e6a0edfd5f3ba6
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:38:48 +0000

    stamp eye question keepoff roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import re
+print(re.match("h.*o", "hello world"))
