536f6870b1e9f98fce395509e1cef9d06e48886e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:35:14 +0000

    earth estimate idea false wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
