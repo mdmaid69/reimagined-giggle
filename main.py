4e287513118dac3d79c79acd7bda5a63998857dd
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:55:41 +0000

    table replace cry loose offer

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import sys
-  def get_python_version():
-        return sys.version
+  def add_numbers(x, y):
+        return x + y
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
