a2b75a4d535bfd36db7f827732e507e8c4da11b5
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:08:18 +0000

    difference fond temper difficult back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
