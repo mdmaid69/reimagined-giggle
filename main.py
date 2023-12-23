696c4208b6e4f90ae262cd0c178fb9af06d33189
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:39:26 +0000

    cost before thinkthrough already dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
