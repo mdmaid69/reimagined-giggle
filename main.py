ff0d7d6adb4c0476743f9d3950dd11a79c46cba1
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:45:16 +0000

    disapprove last quit murder phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
