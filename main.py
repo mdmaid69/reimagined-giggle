07291e9af9a3ca451caa72d09695722de7aaec9c
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:31:41 +0000

    detail through game exam suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
