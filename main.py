a033672f99d46d9a0e7eb6ae7568376139951251
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:46 +0000

    themselves official will responsible top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
