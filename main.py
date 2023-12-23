831ff81c7bdf99afa0341c6f62cb07d7edc2ca46
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:14:25 +0000

    copy develop cinema steady interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
