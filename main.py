c2be71c292dcd10d98b3ee5165e170ac26119e3b
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:22 +0000

    storm dinner square recognize press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-print(os.getcwd())
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
