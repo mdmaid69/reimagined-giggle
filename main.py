b8376b9fbf18d949d67acc91abbdc7a50a8c6760
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:32 +0000

    offend later warm on Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
