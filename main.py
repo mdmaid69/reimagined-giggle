ca1875139dff83952cb4a73bcdcfcec9e9d713d2
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:55:04 +0000

    final fly kind also divide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
