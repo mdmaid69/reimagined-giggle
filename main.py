8e2a8d040a4ad713cf95a2027baee1aeae9fcadb
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:34:16 +0000

    peace suit near brother extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
