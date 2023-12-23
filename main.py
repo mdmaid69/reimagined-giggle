a443120f151cb4af9fc90644689cdbcc5af48b95
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:08:51 +0000

    star lend care callback unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
