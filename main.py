c71b2d1f04cc0eb2f3b394d0ab1f1fee3cc0d1a7
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:22:54 +0000

    unkind afternoon wish circle post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
