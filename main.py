8808d6a961490e72dd920f65fa680e87d59cf8e9
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:42:36 +0000

    perhaps ill performance thorough left

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-print("Hello, world!")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pressure(force, area):
+        return force / area
