4e476840118f10dc7742c03f4527f757ef9ac77a
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:37:03 +0000

    567enemy than arrange order protest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
