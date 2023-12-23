ba8f1f86d57fdc8018b40de2cf0a4b48b11e7aca
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:59 +0000

    exercise gentle bang bill if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
