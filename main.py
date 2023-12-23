98897da92015f0bfaa6957ae56da3172abebeecc
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:43:01 +0000

    hand to problem reduction well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
