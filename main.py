d42df9bd412e5b08e1b22027349f864d92ade3be
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:56:31 +0000

    difficulty considering turnout hate immediate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
