7f98e67df7cdb058d114ad6aba23aa482dee7b39
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:50:15 +0000

    dance careless no think ready

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_typecode(array):
+        return array.typecode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
