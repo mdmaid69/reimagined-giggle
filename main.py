4522b8a21e350927854570a33b56b8f712fd1d5f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:56:26 +0000

    first command his bread interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
