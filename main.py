ec51f96de22fdd178858fbded249b529a4b5a32f
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:04:32 +0000

    follow play entrance present proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def convert_to_hex(n):
-        return hex(n)
+def is_even(n):
+        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
