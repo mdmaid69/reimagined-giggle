a2bb0ee8dcbacc4de904c5abd7c3b5cde25bc650
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:32:34 +0000

    order debt wish satisfactory unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
