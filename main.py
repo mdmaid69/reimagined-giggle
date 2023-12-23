ea8ec14f9823cb5692620ef3765ccc3d05a901de
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:09:25 +0000

    spend difficult fat getup glad

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
