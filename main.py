21486e8ec1f4ca6cfdf5d28616d179b7af88fc6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:24:13 +0000

    anybody each amount thank believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
