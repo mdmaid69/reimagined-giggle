10e3fe0615bcadc8b29019ef241ce2458f17859b
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:03:28 +0000

    either tune result either morning

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Uppercase:", text.upper())
