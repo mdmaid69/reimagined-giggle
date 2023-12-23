64418ee7ecdab3a6d1a0534204e181705259f0bd
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:19:11 +0000

    sink as central Pp poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
