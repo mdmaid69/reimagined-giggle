ed79449205a95fc8d6c869f3dd11426cff6c3701
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:08:13 +0000

    concentration accidental force language contrast

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
