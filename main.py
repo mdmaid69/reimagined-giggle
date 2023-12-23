794fd399b09466495a7e4099b096fa9f2d87e191
mdmaid69 <robot@alexsyw.me>
2023-12-23 02:04:33 +0000

    case never race notice wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
