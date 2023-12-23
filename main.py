fd272254a8f61893197304b876029e8369f8a8e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:51:47 +0000

    about direction bored thinkover wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def subtract_numbers(x, y):
+        return x - y
