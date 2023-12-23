99324cbfd26f7fca98f02a5b468850da1eb6c32c
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:54:43 +0000

    half them central mix believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
