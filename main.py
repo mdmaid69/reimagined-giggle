8491d78e50ae151c4989e3e66d7cf0e0742302bb
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:59 +0000

    union brain feature doubt noone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def clear_array(array):
+        array *= 0
