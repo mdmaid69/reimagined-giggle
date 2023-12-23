f4a8a9de3867bb7c1900560480caf360c0c88164
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:18:02 +0000

    wave free play kill sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
