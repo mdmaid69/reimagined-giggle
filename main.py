af417a8981089985b4ea7c819fb38e3c6c05c98e
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:08:33 +0000

    gentle influence manage bound damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+for i in range(5):
+        print(i)
