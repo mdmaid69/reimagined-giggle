e5127b1d207c40ee459579328850f6d66e2ad6fc
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:37:52 +0000

    advice bear high theirs Hh

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
