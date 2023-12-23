7e48e67a844b28a9353625d57c57ca0cf68406ea
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:07 +0000

    spin scare alive splash responsible

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
