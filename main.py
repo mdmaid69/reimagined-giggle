bcf94d7e9f3089eee2b3712b321b5fa41a92058f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:40:44 +0000

    last impatient bridge phone late

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
