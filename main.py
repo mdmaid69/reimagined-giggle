c75df5c3f241e8050cff2b5268b7fab1326f195d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:46:37 +0000

    proof simple delicate patient close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
