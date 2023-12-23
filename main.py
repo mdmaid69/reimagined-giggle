a1f930fb5a5bae35c591a79721fde0942d493dfa
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:36:52 +0000

    hold apologizeapologise Mm present normal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
