fa1542c4f4312894e0bb08547d545688010dbe22
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:50:14 +0000

    quality type your allow little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
