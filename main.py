982a8e7260bae7e5d7aa4d6b413b40dbbae61c6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:00:23 +0000

    free same nurse advice chat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
