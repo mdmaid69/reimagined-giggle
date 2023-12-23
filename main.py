ef67fa886f2c887dc7c52e701820756ec06a17e6
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:52:20 +0000

    me through own comefrom sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
