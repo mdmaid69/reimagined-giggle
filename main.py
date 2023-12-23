f1212a57281c6036c54cf5f4f466e1957428fa5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:27:19 +0000

    record sort well dress genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
