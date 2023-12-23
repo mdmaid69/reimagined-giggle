43df6e81367d6ed8344ff2f3557b87e4c5deeac8
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:40:39 +0000

    little official boring calm organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
