32c305f2833a3b66d1c418d2b14fa5cbd23db139
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:58:33 +0000

    will dig circumstance dry private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
