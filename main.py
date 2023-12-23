5b18044b79259f37e88c8db04a5ad2ed8c11f7e0
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:39:12 +0000

    skill Vv inquire intelligent makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_array_to_bytes(array):
+        return array.tobytes()
