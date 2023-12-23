2f07f8b978e6187bfc757c3995d5b721bd92206d
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:02:42 +0000

    deliver wind communication direction dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
