0aa11c571e093c60790ab2400638e47fcecb7083
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:56:07 +0000

    keepfrom feel mine giveout rush

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_cosine(x):
+        return math.cos(x)
