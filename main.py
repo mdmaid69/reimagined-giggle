fe68ccefa298d9d95d101adbcec76480898171ad
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:18:41 +0000

    record hurry meeting owe value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import re
-print(re.match("h.*o", "hello world"))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
