6dc777a0389b8f33e8221333150e9e458c33e391
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:31:11 +0000

    hate minority progress our quick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
