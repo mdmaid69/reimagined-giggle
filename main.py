360458f15e21f02791d06eba63045e040821b9e5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:28:31 +0000

    admire favour bear connect total

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_power(work, time):
+        return work / time
