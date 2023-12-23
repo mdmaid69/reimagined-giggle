848c27f522c8693129a9794775d1a3f8ac223a13
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:20:57 +0000

    pocket mention unfortunate section must

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_list(array):
-        return list(array)
+print("Hello, world!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
