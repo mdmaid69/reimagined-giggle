2ad5ed2d14d85f684f5caa5d2aec2ef379c76b04
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:30:53 +0000

    fall image glad bit slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
