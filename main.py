00183483e3e793f92635a46bad6dcae58e8ea995
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:38:53 +0000

    professional freedom communication hurt block

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
