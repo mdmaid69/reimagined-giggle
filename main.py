70760e99a6189fbd841ae4b50b1d2a2b74cda457
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:31:14 +0000

    makeinto university touch glass fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
