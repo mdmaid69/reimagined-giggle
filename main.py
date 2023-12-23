aae828ab99733035bdd3b5628df169fe2a3af860
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:52:37 +0000

    into survive nerve vanish film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
