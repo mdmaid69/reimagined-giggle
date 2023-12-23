96c18d598c8fdab1a4265b13b72a1ac6c5ed333b
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:35 +0000

    bright late runout separate skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
