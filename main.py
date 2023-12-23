0b074cda50f5ac98150304064c2881bdfa631390
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:47:06 +0000

    board polite recognize theirs thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def add_numbers(x, y):
+        return x + y
