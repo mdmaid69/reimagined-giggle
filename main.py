7b54aade3dce1e579f713ea281d526e464022695
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:22:54 +0000

    expect clear runaway warm subject

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
