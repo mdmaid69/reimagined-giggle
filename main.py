daf21a797189ce7bdcffd19fa3398ec2ee045bd4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:05:49 +0000

    vision definite reply create finger

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
