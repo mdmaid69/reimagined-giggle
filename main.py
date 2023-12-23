9fe9dc14460108744fd5cd35c9d97789fa8c2c41
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:38:18 +0000

    remain apply visit dance speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
