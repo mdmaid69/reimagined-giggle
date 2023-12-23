a498882ec20978ec877baf69765e1a0281f2b259
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:51:29 +0000

    calloff sink either fire goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
