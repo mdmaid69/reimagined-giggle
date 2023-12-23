2855a010cd80990ce5d2b694e365d3cbccd4cc5a
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:03:09 +0000

    suppose slowly cool too cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_stack():
-        return collections.deque()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
