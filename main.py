2c64116fa94a2b1ada6835ffcc987f104a7fc9f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:58:13 +0000

    workupto gentle criticize rush score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
