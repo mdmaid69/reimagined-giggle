45850fc5692f163316d76d7ec5dce72dff7f3074
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:42:34 +0000

    innocent connect town enthusiastic comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(a, b):
-        return a + b
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
