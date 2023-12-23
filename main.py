b772f37659d562fd3e20b705b0d78ef11d6f86a2
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:16:55 +0000

    add slip left programme ours

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
