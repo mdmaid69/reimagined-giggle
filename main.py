549b24c5fa2aa251c910abe95956a3f8d7d12244
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:08:44 +0000

    past order dark greeting exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
