9abb71ebe8600ffd1db57cec9dcc2238ca57afdf
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:51:12 +0000

    kind pollution challenge duty opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_power(work, time):
+        return work / time
