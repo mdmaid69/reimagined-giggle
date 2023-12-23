3c1407c5a9df182a33eacccb6f1f12d6b6757424
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:18:43 +0000

    hit anyone him grand lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
