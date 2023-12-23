03dd729148f62b75eb9afec9c1e5fa65ed119716
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:23:32 +0000

    drive march hurt let there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
