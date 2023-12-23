8e48b529296a1ee264ace22a80703b80ba10b56c
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:08:54 +0000

    takeback battery wet exchange matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
