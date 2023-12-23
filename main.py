c797b5c7191df8c04f3a9ba60d1d4cf2d4267ba4
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:07:39 +0000

    harm intend free name release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
