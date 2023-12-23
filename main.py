f5563f89c02f4085d8dd5779e4350a0dd41bf813
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:33:37 +0000

    swim phone regard law drive

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
