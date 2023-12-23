04850066c6987354a6205cbd4c79ec09388451ed
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:57:00 +0000

    ashamed port its lunch hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
