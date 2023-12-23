7137aafde4ee2622fe5b144dbcef14d9c4dcb53b
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:42:53 +0000

    build obtain smell anybody group

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
