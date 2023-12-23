94570530044cc52d2668ab5316fe81afeae0dbf9
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:05:07 +0000

    desperate fly towards achievement need

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
