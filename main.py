c413b45056fb39bd9eddc69485f47467fd9da28d
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:42:31 +0000

    block favour trousers deliberate mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
