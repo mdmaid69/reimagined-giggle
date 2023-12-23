28905f7fa55320bb4f712b9a488a4ace14d76f34
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:20:05 +0000

    central communication design operation lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import re
+print(re.match("h.*o", "hello world"))
