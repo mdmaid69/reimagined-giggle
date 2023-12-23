075e47c54224f697010fbfdb468a97e389cb81d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:56:01 +0000

    into number instrument go duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sys
+print(sys.version)
