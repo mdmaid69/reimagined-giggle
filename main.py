8faa301d577d6c47f97a5f813ed8340595fabad6
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:13:00 +0000

    only most pull steal too

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
