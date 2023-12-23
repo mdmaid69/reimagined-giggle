bef6070a14cb8480d2ea5ae52e6aebb107adbda5
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:47:03 +0000

    unite guilty straight manner rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
