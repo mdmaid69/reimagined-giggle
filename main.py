626c14774cdc1af44420d1f4ceb06d39e983415f
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:41:40 +0000

    level soil keep prove right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_int(array):
+        return int(array[0])
