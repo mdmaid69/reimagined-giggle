4ce9b00215b5d9f23aea554ee0e3b76f531658e8
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:51:05 +0000

    minor result age wash month

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
