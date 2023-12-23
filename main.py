189fbd72e1b02c81c97e44f744b25d4e7394cf39
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:45:28 +0000

    science practical Ll season stupid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def sort_numbers(numbers):
-        return sorted(numbers)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
