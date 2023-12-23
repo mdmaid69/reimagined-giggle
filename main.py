5597c02f0e2b204d3f2b5d8e0f520615866345d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:24:43 +0000

    breakout certainly female unhappy marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
