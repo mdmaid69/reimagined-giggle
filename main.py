d1c89e9d1550f3d99f6c2bcf7da8c427846d7ea4
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:32:47 +0000

    always contact positive survive fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
