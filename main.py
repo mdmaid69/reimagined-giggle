dae048d0533eaaa9bfdf47333de1f758da3b6d9b
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:10:10 +0000

    stage brain Itself rain smell

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
