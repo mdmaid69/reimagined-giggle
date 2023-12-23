ee19a2c40552c3b45d0edd333a51344b4ac1ee33
mdmaid69 <robot@alexsyw.me>
2023-12-23 14:15:01 +0000

    available crash splash operation safe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
