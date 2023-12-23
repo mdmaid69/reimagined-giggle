0fdb29049bcff05eb2fa3ddd0a0f2f45d5aed8d3
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:31:57 +0000

    decide her complain standard passenger

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import math
-print(math.pi)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
