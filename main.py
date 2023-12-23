44095854cae71e26271c5b54c48ff493b4068bec
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:32:53 +0000

    strike advice train completely write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def get_current_time():
-        return time.time()
+def get_current_working_directory():
+        return os.getcwd()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
