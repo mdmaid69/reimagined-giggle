9080ca0eb7996fac0eab3a9bb7eef61fabb56ce4
mdmaid69 <robot@alexsyw.me>
2023-12-23 07:21:12 +0000

    stamp find matter advice unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
