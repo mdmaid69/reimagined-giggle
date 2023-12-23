4d0833529796f3f9fd2e16903d8e22e79718ba74
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:16:16 +0000

    aim comein smell throughout voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_slice(array, i, j):
+        return array[i:j]
+for i in range(5):
+        print(i)
