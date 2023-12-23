ab5289973d0c773cddfd527448ae450ba5ecc74f
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:54:28 +0000

    before remain cost naturally love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
