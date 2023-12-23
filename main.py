c69fafe089f77b62ee10c91bf40747e65f4b291e
mdmaid69 <robot@alexsyw.me>
2023-12-23 04:01:51 +0000

    anywayanyhow flow kind equal thorough

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+print(sum(range(10)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
