629fd38bd40ee2ad1fdef8a36c082747687c5e91
mdmaid69 <robot@alexsyw.me>
2023-12-23 08:11:06 +0000

    564encourage last round follow software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def get_array_typecode(array):
+        return array.typecode
