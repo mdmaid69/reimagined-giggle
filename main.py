025ec1f25484483d5e0f94526cbf52c2fd14ce7c
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:18:24 +0000

    wild club bite pain Pp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def find_max(numbers):
-        return max(numbers)
+import time
+print(time.time())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
