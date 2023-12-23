5474b577ecbdd0b05c53a82287d4b15ea470804c
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:48:40 +0000

    yesterday already talkoutof slowly tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
