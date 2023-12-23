c9f1c5afd8518fb68da39c2b80785c267e00cbb8
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:50:37 +0000

    how use bet instead talk

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
