facfe975b3473b8c0bc150f8023d439692faeabd
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:26:49 +0000

    protect chat there repair land

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
