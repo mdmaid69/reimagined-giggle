59da49a933c2316fc9ec3ac80b2ee370b2c6e3da
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:38:32 +0000

    copy environment lookafter means operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def is_palindrome(s):
+        return s == s[::-1]
