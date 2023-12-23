8cd5eede3e26159d8c09156418ce2f94679c6934
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:15:28 +0000

    large squash hope for achievement

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
