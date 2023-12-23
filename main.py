1ae2cc0de5ba7b68d38a6134a2618b054af526d9
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:42:47 +0000

    cover cross drop way breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
