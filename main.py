706064ede7fc072134009abe01b7c9cd2b3f5b01
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:23:48 +0000

    finish wish we reply cum

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
