c1a78555ef4520fd2fdf4ad2a6d3c0b35819ca04
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:16:30 +0000

    because scale guess sorry action

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
