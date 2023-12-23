d087bb15569484c0b230644adad504799cff8fb7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:40:25 +0000

    key press sound backwards thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_item(array, i):
+        return array[i]
