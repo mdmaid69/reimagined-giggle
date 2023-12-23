7cfd616b1898f2ba35de44233154a96fc8fc41b2
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:11:35 +0000

    occasional food fashion condition find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
