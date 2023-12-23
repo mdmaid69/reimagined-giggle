7ef8728eb31cb7a96007a31aa346c1d928e867fd
mdmaid69 <robot@alexsyw.me>
2023-12-23 10:18:27 +0000

    cruelty past trap likely mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
