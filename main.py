daddb4ac46d7d970e67507e2c117f3d8f27f78ce
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:02:49 +0000

    themselves fat shine choose broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
