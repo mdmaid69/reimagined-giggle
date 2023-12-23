429b70d115243f8e69ab27e13f989fb10e84375d
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:48:23 +0000

    standard which fresh treatment pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
