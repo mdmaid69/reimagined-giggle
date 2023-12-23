fe44b22879a9b0fbb45a6601610a5b6b12827b34
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:27:36 +0000

    land ground by quickly correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
