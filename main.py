61d8a0487412db20a6ead54bcf98ac0fde69b4ab
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:59:54 +0000

    slightly conclusion steam dig content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
