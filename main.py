f2215c44ecc68b1eecc2a3b33e883f022f10f437
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:23:45 +0000

    eye cheap he create recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_int(array):
+        return int(array[0])
