4ee541d851718ae636e819c30aeb2e2671c2bba7
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:55:48 +0000

    unlucky entrance arm risk especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
