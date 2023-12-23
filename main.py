efde081f8a6596a77caac37d5a341fa4d1ef1101
mdmaid69 <robot@alexsyw.me>
2023-12-23 18:37:08 +0000

    organization tone jump lay public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
