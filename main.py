eeacb8e6d058e0c77ce02d88c92ac0e4fbfa5d3f
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:29:46 +0000

    suppose burn drop block furniture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
