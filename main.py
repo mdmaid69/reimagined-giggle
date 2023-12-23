7a3f58062f802471906ced385e31107e580cb1f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:29:41 +0000

    middle whichever rarely order through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
