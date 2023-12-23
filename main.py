03c41368dcc1c776a2d8c7f9825924f59c2d7dda
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:05:15 +0000

    date record union finally behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-print(math.pi)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
