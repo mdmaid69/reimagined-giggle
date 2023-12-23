2e411d112fd78ac6318cfc8fb379a8bff36f4c7e
mdmaid69 <robot@alexsyw.me>
2023-12-23 09:21:40 +0000

    immediate satisfied divide check spread

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
