b643f8836f233d6d205c3247787120aed60956ff
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:46:39 +0000

    machine like pay bringup lift

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def count_characters(sentence):
-        return len(sentence)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
