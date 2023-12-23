14d4cc6ae3e6ae8c036f1c694664409f1272aa30
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:10:35 +0000

    arrange chief separate born last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
