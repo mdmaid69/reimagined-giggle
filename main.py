d2feae7449a633d88bfdd8084c87b7685539639a
mdmaid69 <robot@alexsyw.me>
2023-12-23 16:24:10 +0000

    uponon male wire concern spill

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
