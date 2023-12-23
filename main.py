a4167b04c70633961ccd031b5952aaf85970d43e
mdmaid69 <robot@alexsyw.me>
2023-12-23 22:08:03 +0000

    cut stamp excited point party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
