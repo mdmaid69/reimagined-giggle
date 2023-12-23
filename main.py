536f97ecc652bf8e89d9c13f57c5df420b68bbf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:09:09 +0000

    negative clearly load form go

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
