3ddcb93e92a041dcc21f40c30517267cfb39182a
mdmaid69 <robot@alexsyw.me>
2023-12-23 20:35:28 +0000

    under during ride but soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
