acd578befce91321655f8f28641429576ae846d4
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:24:01 +0000

    soft present building fill middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_acceleration(speed, time):
+        return speed / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
