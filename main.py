fdebde2690bc663b396e422f66eeb94c6ce60535
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:56:38 +0000

    period common bit about try

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
