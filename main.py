ae5bbccc029b7fb8437f47032b9ecf6023ee148e
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:25:03 +0000

    since impression disturb disappointed famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
