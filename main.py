42153e1d13928f70defbaa3ea34f689af0832adb
mdmaid69 <robot@alexsyw.me>
2023-12-23 15:51:22 +0000

    abroad time have instrument strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
