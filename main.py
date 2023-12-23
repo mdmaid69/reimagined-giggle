35f1d134c3c44be1d4a58220c1ca5d2c93493891
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:36:12 +0000

    tear right since noise want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
