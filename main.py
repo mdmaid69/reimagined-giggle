245f37f70065e1c88a2e4295c2d3c5dd28cc4226
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:45:51 +0000

    whistle chase down education conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
