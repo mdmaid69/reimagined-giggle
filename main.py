621889cee243d28a49937ba51bfc3f40fed11f87
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:12:21 +0000

    pocket meeting mysterious behind unless

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
