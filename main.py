5746916a7fe4981be5669550128e4ae2a801718a
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:00:39 +0000

    forget raw environment scrap save

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
