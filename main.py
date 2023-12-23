77e22c34daaed4f3117f42a4104cc5dec9f37321
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:02:45 +0000

    region control comeout detail too

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
