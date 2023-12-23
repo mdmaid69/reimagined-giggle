ab3b9965e4f57217cc21df3609ff5cbfb1eab05b
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:48:46 +0000

    comment behind keep upwards aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        return sys.version
