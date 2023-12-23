5d71e899ec20d69e8e9c93bed045ed71d07aaf6c
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:16:41 +0000

    flat event ever pen upwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
