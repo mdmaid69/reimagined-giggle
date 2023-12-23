f4c2ed81fb78e9789eea0b3edbffd9c4ebbfd5f5
mdmaid69 <robot@alexsyw.me>
2023-12-23 00:17:10 +0000

    different surprised here practical provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
