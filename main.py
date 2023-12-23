d00a94ad82a815a515669fc881bed3aab4238b58
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:27:18 +0000

    at card traffic careful impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
