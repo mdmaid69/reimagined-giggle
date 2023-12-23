28854c540c8d0614cc4085463120ab6e86d482fe
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:42:48 +0000

    loud whole cover breakup please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
