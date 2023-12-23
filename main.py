2f821e7e1586ebb86d9da466df914dbfa5f6baa9
mdmaid69 <robot@alexsyw.me>
2023-12-23 11:06:10 +0000

    bath 564encourage cruel might cold

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_list_from_array(array):
+        return array.tolist()
