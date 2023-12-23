01658f101f5ab95c798d56ccc77fc2c6900e1ec4
mdmaid69 <robot@alexsyw.me>
2023-12-23 23:14:29 +0000

    thinkthrough fit expression bringdown name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
