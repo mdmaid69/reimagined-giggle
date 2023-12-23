4630fe6b4d7d3cbae485101056fe014b93eadaf7
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:25:16 +0000

    interview of wild workon goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
