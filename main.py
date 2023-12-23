b19269504a8814d242d47b99093ad208853f1c27
mdmaid69 <robot@alexsyw.me>
2023-12-23 01:25:21 +0000

    tip weigh design price glass

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
