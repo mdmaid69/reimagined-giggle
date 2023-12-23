bd9c860d0ae07e35a1417f4e3769d5dc280e3ab4
mdmaid69 <robot@alexsyw.me>
2023-12-23 17:01:32 +0000

    callup spite material joke culture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
