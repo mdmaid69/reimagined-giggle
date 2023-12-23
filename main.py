09b538a4e1371f6d06c23d660dcd35d9b9038e54
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:48:42 +0000

    cough bath standard complaint simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
