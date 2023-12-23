047bb78d50ed8ca7a5c39d71699621f816346c45
mdmaid69 <robot@alexsyw.me>
2023-12-23 06:44:02 +0000

    reduce into entry mix judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
