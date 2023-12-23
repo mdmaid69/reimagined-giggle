c83e74d68ebf447552fb8010f1b3069fa1461d51
mdmaid69 <robot@alexsyw.me>
2023-12-23 21:12:35 +0000

    team forget hope stamp type

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
+print("Hello, world!")
