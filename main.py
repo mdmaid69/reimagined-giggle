0fdbc7736e95183839614213290950ccf5649b24
mdmaid69 <robot@alexsyw.me>
2023-12-23 13:14:47 +0000

    giveout preparation welcome start attach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
