80658e260dcd239a30931a779fe18cb0b2978fcd
mdmaid69 <robot@alexsyw.me>
2023-12-23 12:13:32 +0000

    convinced drink board effort through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
