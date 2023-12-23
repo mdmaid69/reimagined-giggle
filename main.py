114c2e662d6d49d56db1a9d3ad5b19c75f2c5c5d
mdmaid69 <robot@alexsyw.me>
2023-12-23 19:12:47 +0000

    love person destroy ashamed authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_binary(n):
+        return bin(n)
