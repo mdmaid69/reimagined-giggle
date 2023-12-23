9d0dd69237365b69559af4c4cc42793316efc161
mdmaid69 <robot@alexsyw.me>
2023-12-23 05:24:56 +0000

    please for clever artificial watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
