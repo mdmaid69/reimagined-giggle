1ebecb31deec8ab1f78574422d9a312670d7a8f7
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:33:40 +0000

    imagine pronunciation mistake sense strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
