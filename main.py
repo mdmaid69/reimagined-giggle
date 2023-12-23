014001ea5007c45ed31348a4e881b8ddd6c86377
mdmaid69 <robot@alexsyw.me>
2023-12-23 03:01:47 +0000

    almost purpose worse lookaround think

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def greet(name):
+        print(f"Hello, {name}!")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
