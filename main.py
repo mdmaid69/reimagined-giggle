4db3314fc80afe9beb5766ebcc2db6b2c015e697
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:16 +0000

    common drink opponent push war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
