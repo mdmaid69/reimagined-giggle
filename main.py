13a25080266bd0a819f1c477caa42eac17d22516
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:33 +0000

    empty interruption young congratulate compare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
