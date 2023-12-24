81a9d4e654ba832ef67ce8e336c8471d3043caf0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:53 +0000

    principle turn content else far

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
