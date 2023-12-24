e0504f7b181f999c957dc71e94b0de5e3173d155
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:51:28 +0000

    risk turnup keep display less

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
