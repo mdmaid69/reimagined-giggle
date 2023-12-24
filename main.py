56185b6983a40d9a8b0d3e1bd03a1ab7b8416d47
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:07 +0000

    nice word to comedown tight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def square_number(x):
+        return x**2
