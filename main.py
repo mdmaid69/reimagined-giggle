b8a6a27debe9d64174fae839f0ac54c1009f5bab
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:41 +0000

    that closed downstairs ashamed outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
