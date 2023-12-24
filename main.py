400893de68b79cf0f48d7301964658baf231a8c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:43 +0000

    different furniture start become embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
