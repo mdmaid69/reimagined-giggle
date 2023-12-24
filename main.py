52c974a1f2e6c5e1bc8375cb89ccf7276948f8de
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:41:59 +0000

    favourite begin increase old constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
