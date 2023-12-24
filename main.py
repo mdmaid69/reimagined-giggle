9bcb3a72f81d0bfdef4dded7fe8a54f1bad884f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:43 +0000

    congratulate sentence very instruction more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
