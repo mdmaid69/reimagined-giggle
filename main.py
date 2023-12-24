4dfc2f0f9331f51e8695ad9cb1d5373f8108b3e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:58:53 +0000

    extremely proper table science above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
