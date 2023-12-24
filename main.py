054964d848638277407e92e5688a10230ddce064
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:08 +0000

    face if nowhere sale bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
