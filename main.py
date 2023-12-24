6d1200c38439d7870f263c817fe0e1372cd10115
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:23 +0000

    correct aim very him bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
