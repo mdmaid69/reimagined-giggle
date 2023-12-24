56285c4bffc7a90f44dcf44af32dd64244f54b8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:21:40 +0000

    upwards and have unit will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
