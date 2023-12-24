016cc5d8d7a0f1f3e14ec5687655949cba5ea3dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:47 +0000

    release bar least sensitive hospital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
