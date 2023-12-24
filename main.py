8e1e615ec851184cf9ab2ba380006fdb84987eec
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:05:22 +0000

    now Ff week thankyou include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
