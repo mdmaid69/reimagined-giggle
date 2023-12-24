b02ef5844fa73a76a8bbd218a79ae0ff6c10dfc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:34 +0000

    advise sweet square Uu invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
