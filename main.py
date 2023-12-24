c7e9faf36363365e1e96a9c334db3dcb967614b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:56 +0000

    book none deep anywayanyhow phone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
