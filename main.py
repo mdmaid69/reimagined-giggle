9cb15f9d2be7c5475184a0a32160b624c6453b97
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:19:21 +0000

    male computer translate holiday know

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
