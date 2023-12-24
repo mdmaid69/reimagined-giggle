809a1ac792272a718ddfe41f88ac5903b6f4f7e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:47:27 +0000

    radio contact will comeout worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
