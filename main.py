2d6df6baf76ef881f7d15ae106adb2bc5949ed4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:26 +0000

    yours under genuine hard copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
