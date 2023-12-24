66b705087662668c6a342ef87fa1276db31a0414
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:01 +0000

    on sound consult after wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def subtract_numbers(x, y):
-        return x - y
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
