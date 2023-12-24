69e96df61af825a09c80febcbc2dea08a0ce6df5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:38:45 +0000

    hollow wealth bye tap week

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
