9b29c4ad4f46614b15206b94b88bb769dda50d94
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:05 +0000

    us centimetre style provide whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_current_working_directory():
+        return os.getcwd()
