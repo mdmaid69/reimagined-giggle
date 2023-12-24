7cf310683b2f7801fbf08e1b9fc92072f7f58580
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:10:56 +0000

    adjective little store dark closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
