c40b5615cb0e22a0147e09cd749538928e20dcd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:54:55 +0000

    exchange square 567enemy keepup polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import re
+print(re.match("h.*o", "hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
