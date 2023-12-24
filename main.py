7af37ebe53e2824677fb2cd9593f8799dd38fd2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:46:28 +0000

    operate practice tonight line temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import os
+print(os.getcwd())
+import collections
+def create_user_list():
+        return collections.UserList()
