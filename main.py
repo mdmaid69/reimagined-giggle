9c7aa7790355bdda370f5a20e351922c434c4c8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:48:23 +0000

    need confusing suit story violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
