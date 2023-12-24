5fb59de1af16ccfbb1b6c9a20453721a3e7a2796
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:58:22 +0000

    advise claim bury makeinto firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
