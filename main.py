21a7e821d8eb0ffdcc861d3a28a1426c5934595d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:05 +0000

    position problem reserve fit Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
