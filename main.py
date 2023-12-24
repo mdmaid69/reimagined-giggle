2254a3ead22dadf467fa56bc75367713f97d5dbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:58 +0000

    any turnback talk pick burst

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
