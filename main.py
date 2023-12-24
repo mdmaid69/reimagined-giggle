78f69249fce5594f4d44b13c46c952e21d28c5c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:47:16 +0000

    thinkover scream attitude similar tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
