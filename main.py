8cd249a8fe238f673e9cb043e63de5cffff3b150
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:58:05 +0000

    study organize hurry permission result

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
