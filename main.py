60806692f290e734295dae41ed68cb7858a1829b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:45 +0000

    here mine key compare breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
