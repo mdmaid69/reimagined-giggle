4551f3849d8d00a8719d565ec57162b768991f1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:26 +0000

    mistake left crash hospital work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
