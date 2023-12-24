655957aa608ff08e1911e76d4a2eeef723f3f194
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:13 +0000

    waste impatient use discuss fond

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
