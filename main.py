30752ed3fe52abb9ae6d116521257350dbbf6818
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:17 +0000

    narrow goaway make mark sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
