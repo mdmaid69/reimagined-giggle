ce5182fabf48fbfbd0305b03a46c27e96ac8175f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:22 +0000

    Rr welcome throw pause dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
