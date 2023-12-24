704566e36a34615d4a2c784e3861c6e86553761f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:24:37 +0000

    beginning without treat influence wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
