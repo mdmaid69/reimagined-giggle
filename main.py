281afc9c13365e711c0cf86c495e2dffd637abe0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:32 +0000

    range godown sound naturally wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
