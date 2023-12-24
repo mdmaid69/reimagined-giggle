e28414804f169121a3ad25bb8461f3fcb293613d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:37:58 +0000

    over hard honest business nonsense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
