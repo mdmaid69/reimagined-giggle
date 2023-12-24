c50ccb79c1243d64ff0cdf0ae50d776f032f30e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:22 +0000

    sorry difficult smell second offer

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
