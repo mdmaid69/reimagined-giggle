cf444744b637b97933bc7efc82ca84cdc8c44e83
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:13 +0000

    honest guilty pretend peace circle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
