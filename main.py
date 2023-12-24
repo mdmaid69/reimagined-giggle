542275549dc79df16173bbf27e067a3d329eda72
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:43:07 +0000

    quit dish tear persuade receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_palindrome(s):
+        return s == s[::-1]
