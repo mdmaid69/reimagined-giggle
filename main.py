b5736a79f0c553f60467cc6c5cab0536d624915c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:26:06 +0000

    mind quiet talkinto always pretty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-from collections import Counter
-print(Counter("hello world"))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
