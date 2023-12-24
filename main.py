faf55456308589d21c17865db7269f16b22549bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:28:53 +0000

    charge involve computer power material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
