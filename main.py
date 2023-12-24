020ae05edbcc0eede74fa0c8189d085ae5337b32
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:10:52 +0000

    arrange patient argue quiet already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
