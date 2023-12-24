d1a09a8c6abb5925a42d9b4c3deeaf95dbe3f9e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:38 +0000

    workupto present because great arrival

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+print(math.pi)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
