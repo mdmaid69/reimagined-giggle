665dd2074c8a1ef8966a3134eca053e460637843
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:48 +0000

    sign trap decision invitation accidentally

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
