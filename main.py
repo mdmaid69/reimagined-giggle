af201fc99ec92525b3a96e24df5c2057f76c606a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:17 +0000

    trade government check answer finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
