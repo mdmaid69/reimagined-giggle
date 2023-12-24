c3f6fa572d403484137a177305b913247aa3fd78
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:09 +0000

    down demonstrate never mind on

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
