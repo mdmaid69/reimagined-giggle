dc9244d76cc71971fca73fd81fc0aaeb32f88e14
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:46:33 +0000

    stranger supply acceptable relief edge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_min(lst):
+        return min(lst)
