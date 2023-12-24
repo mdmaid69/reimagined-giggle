c217a4c620af1c3e044b3afce1fe84dfc9fd94ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:15 +0000

    door mistake within reduction mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def convert_to_binary(n):
+        return bin(n)
