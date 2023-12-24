18b43711e31aa813c9d75de46eff1a48ebf62300
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:53 +0000

    talk feeling surprised aim book

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+print("Hello, world!")
