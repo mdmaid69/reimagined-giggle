90292d00c6b41c047eb9eece3d678a58a1fd6b39
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:30 +0000

    major complete bank trial accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+print(time.time())
+import platform
+def get_os_info():
+        return platform.uname()
