0d0c264da42d514c2be32d6356c36bf207293c03
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:10 +0000

    block group glad proof interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
