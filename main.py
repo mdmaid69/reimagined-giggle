1de4f82b3087ada6fe2494051db7007b26df8e1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:57 +0000

    sorry delight responsibility once whom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
