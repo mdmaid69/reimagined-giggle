d122aff1bab69df1f483203b8079fcbd7947f78c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:29:01 +0000

    reasonable inside full inform letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import array
+def get_array_as_str(array):
+        return str(array)
