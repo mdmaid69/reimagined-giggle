920b61f0a77ba04b8a4e3133e0b45029a681834b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:26 +0000

    row hospital want murder sister

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
