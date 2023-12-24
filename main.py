a5282ccbf26a83eee59583ecb7d064c896cc5e28
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:26 +0000

    male someone quickly aware sore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
