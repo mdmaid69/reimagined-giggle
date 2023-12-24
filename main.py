c84b7b190fafa0c263db8db95f5ade3f0dc29911
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:09 +0000

    shoot meaning connection holiday concentrate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
