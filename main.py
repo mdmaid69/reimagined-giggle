09d2f1e42da4689013db69c88d37e0e3825cd166
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:16 +0000

    patient rush explain receive relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
