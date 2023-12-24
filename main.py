d2a9f235d128d3d61b62cb524771b62fe77570ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:42 +0000

    knock family 569enjoy under good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
