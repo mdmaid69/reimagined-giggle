9b09b52cc91c7238b1bd64c0b288903e96b47bfd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:39:49 +0000

    club before hollow ask remark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
