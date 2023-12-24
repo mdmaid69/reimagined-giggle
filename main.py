9bd595f0a17000a301cb199fc1273b08d514120e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:20 +0000

    delight count alive time material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
