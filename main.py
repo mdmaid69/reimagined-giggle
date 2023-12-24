0fd360cd8fe5376d2db6bf12bbb32527a9176286
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:03:39 +0000

    consult country charge air desperate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
