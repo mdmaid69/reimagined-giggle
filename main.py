a89c729d3968aea4eddf1ded76b257dc12c520db
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:07 +0000

    paper centimetre all birthday love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
