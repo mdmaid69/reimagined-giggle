e7bc3de413990376eb8a7a315b898a78843efcdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:39 +0000

    sting trip who shout drive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
