39980c3d5dec20567f0baa649061e82622aa7af1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:31 +0000

    him debt almost hospital quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def reverse_list(lst):
+        return lst[::-1]
