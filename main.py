47a1c3ff17573c4f156da82b8c5a4b5196c973f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:48 +0000

    cover several slight soon better

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
