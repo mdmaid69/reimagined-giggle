97afcd7529ca3e9f3147dc74e2de2f228b021f04
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:52 +0000

    at between attach exchange fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_work(force, distance):
+        return force * distance
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
