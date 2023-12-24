65859b668115f6ad42502f6a7a80b56683b36042
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:15:51 +0000

    start different deep fight ordinary

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
