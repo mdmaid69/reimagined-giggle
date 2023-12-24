210adeb10bd7f443e7a510b00e166495e61c171c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:12:15 +0000

    polite approve pain ready record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_tangent(x):
+        return math.tan(x)
+  def square_number(x):
+        return x**2
