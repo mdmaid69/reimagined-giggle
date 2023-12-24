fcc6779e99c3aebeb2e87741e0f655733cca326f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:32 +0000

    difficult face conduct early influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
