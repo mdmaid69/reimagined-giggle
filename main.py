22a59d7c49a2b6b310d6375d111f7224f905a7f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:57 +0000

    destroy 564encourage name real guilty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
