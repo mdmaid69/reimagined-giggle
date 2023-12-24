66bd312f61131422f1d920b2f3578d266b2cdc69
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:11 +0000

    past nonsense contrast slip sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def convert_to_octal(n):
+        return oct(n)
+def is_odd(n):
+        return n % 2 != 0
