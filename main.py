b512bc06fa3faee52771fc4a3039d68a70b41e27
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:54:07 +0000

    material private Nn ill suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import tensorflow as tf
+print(tf.__version__)
