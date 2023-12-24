b4f707bcd375819fa94fa78f12b4a076d4d1fa7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:41 +0000

    satisfactory cap Gg declare build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
