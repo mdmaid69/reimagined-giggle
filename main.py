3f4980416f81ca70d9401a9dd5af8b07e740eab5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:50 +0000

    contain worse OKorokay complicated rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
