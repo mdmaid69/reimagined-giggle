a8fcb4bb97a43bbbdf4203d4cafcd672b712d0aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:38:38 +0000

    compete true content play crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
