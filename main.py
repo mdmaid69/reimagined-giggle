3e5d3b49213ad9a9b74b15f34f16a7d7c5364423
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:02 +0000

    surprised high whom shut decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
