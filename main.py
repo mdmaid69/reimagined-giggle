7ccae6a3026a963452f291d5c46aab09139c86ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:49 +0000

    target team rather accordingto takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Words:", len(text.split()))
