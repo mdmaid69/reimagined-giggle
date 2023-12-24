4475d4ad078d5326e585f6a8045a2398313e00ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:16 +0000

    impressed gather reply word touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
