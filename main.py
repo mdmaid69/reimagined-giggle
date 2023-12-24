5d49f0938e67e9517c63f024536f74e457a17840
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:50 +0000

    could close want doubt confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
