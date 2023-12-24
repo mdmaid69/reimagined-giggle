85686c58a7f7e3ff614433855d2fe3d6af40c92b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:25 +0000

    muscle anywayanyhow identify convinced mind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def square_number(x):
-        return x**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
