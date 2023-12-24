25eb7fc244b4fbf6d4d633c2a673bdf098b0f0c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:33:23 +0000

    out pure bite century another

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
