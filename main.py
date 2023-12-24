f5439ee7b9ea79760a8d1f3294f2340432ced689
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:02 +0000

    window excited culture usually attractive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
