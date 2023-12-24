6992b4a27d8112e9eb1e2307b42d7f681f6c1d56
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:58 +0000

    organization play until deliberately he

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
