90c392152f104037c639db51581e0fda16b964da
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:32 +0000

    sister room pretty phone they

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_itemsize(array):
+        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
