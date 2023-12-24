4e34b2b4c14a7520e981176a3354064c276be8f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:00 +0000

    someone catch calmly accidental bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
