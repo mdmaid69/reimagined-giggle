52ea74c93700f6e70367e4f7a0c78746967fd09a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:44:26 +0000

    sound secret test dead regional

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
