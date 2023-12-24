24bf85e339570105a2bb8cc420851b5a17f73a8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:18 +0000

    weekend expect score complaint threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def divide_numbers(x, y):
+        return x / y
