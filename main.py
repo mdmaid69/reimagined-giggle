435865651ce0e9484cbe7bf645169b7bcbbf03f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:13 +0000

    watch smell hair total feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
