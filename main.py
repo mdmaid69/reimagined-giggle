a89078b2cb607a116e03d4d5eec1bdb4d2625070
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:01 +0000

    equal soft issue affect design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
