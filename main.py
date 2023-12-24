953c64f67445069403bf2e67edf1ca8709846001
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:25 +0000

    thinkover content lower broken thorough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
