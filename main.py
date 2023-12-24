b8be1f04233ebea98da8a5fbb40abb6a92af669f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:10:19 +0000

    attract young wonder manner blame

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_max(lst):
+        return max(lst)
