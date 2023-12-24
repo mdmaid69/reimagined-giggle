d4b680ce2aad62de4294d13d94a10ad07e05a547
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:57 +0000

    spring tip by breath lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
