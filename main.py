62d218af79701e7296d510a0cc09442a6dd8cf39
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:36 +0000

    surface branch mass borrow pull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
