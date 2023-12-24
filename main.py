e11ea0c1c1e9033ca2e60b6f9d897750b17ea042
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:24:18 +0000

    defend claim reaction tour 568energy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
