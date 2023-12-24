1708f2ad4f344c3f1ec5ca099499a5be25e97a1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:53 +0000

    disturbing temperature disapprove instant cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
