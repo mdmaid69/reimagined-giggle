dce3ab175e86be36ffa37fee04267eee6e27e9dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:11:52 +0000

    law little chat square fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+print(random.randint(0, 100))
