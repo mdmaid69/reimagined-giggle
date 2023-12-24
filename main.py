6afaab40408b679ecb78bf1391814185ff1a1dc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:39 +0000

    arm extremely game compare sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_odd(n):
+        return n % 2 != 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
