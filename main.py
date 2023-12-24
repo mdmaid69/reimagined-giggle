7900684ef3a0120a17084d90c0f68af4b4aea835
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:04 +0000

    criticism half it frightened climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
