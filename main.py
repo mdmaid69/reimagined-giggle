3a1c16f004896347cd5643cee409645e771ad007
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:44 +0000

    after regional runinto eat petrol

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
