af15a3531b0ec54ac30d4a215cec43ea298e479d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:32 +0000

    threaten lookup remember much preparation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
