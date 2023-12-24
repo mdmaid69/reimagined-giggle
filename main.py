4268a45b7894163e791d94f79473d5e4c392416e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:19:10 +0000

    length storm happy failure interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
