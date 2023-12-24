33a92fd591b7eb239a633e7f23f88dfce4e78426
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:04 +0000

    explain visit least tired approve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
