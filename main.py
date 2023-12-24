26ba30e1816b32de5b98b3c6bf91ca1b60dc0023
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:56 +0000

    lean takeback doctor trade teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_bytes_from_array(array):
+        return array.tobytes()
