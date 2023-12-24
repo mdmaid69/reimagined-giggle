309eb98624f7db513f6963343d003db88b7bb3e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:38 +0000

    belief hurt maybe disapprove forget

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
