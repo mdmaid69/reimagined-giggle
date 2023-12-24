7f39fc968bec4c9a30a16f14dd4858c088e32027
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:41 +0000

    attempt fast importance stone error

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
