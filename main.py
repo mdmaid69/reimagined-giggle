b8b97778a786f894e9f09b806ec99ea69b186d89
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:36 +0000

    pump early left forgive involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
