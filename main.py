01f219bc001365a07f842fe521d950642ab94c71
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:58 +0000

    dark invitation interrupt involve rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
