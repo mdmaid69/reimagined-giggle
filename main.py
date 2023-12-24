edb6c4527c2556ba3d00312a182470cfb12bff9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:23 +0000

    everywhere so judge organize ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
