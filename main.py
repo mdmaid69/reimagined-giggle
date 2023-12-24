0b5576a9b076b6b56a3dc6966d9851cd46b54b76
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:17:30 +0000

    cry manner straight excuse large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
