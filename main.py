c66f9f9a8d53aca4fc7db8e14bf7b4f77c9aa86b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:34 +0000

    dance ocean fun sharp swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
