d4c6e0491b3609c68405e64fd49c9b89ed17066d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:08 +0000

    phone thorough punish total plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
