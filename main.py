886ea859d89cfcfa41146ecfe70d5b5b34d1649a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:13:58 +0000

    general cry rid step bang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
