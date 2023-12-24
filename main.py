fef5857eed00d68bb35e3a226ff40ecd4faecb40
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:18 +0000

    feed further responsible but strong

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
