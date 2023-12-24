d5bd8ff365e9d51fcad1743553fc7bab6332a507
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:39 +0000

    contact row praise flavour marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
