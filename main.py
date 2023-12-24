97e240bd71d608ac00ed192cfe04ba2aa95e9841
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:17 +0000

    official colour tough view variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
