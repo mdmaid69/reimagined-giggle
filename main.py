eadc9c58706e5f2d81b43b4f959bca593ee360f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:12 +0000

    admit pain uniform pick Hh

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
