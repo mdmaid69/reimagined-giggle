651ace12e166132b499bf64f067b84c43d3e574f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:33 +0000

    match compare strict run entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
