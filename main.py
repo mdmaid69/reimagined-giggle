2dff5e9cb6a8ff32919aea86196475f0307d6cd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:13 +0000

    spring turnover to left score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
