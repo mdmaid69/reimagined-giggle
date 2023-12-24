449396d476cf7aac8e89cefe094e176260909e54
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:18:07 +0000

    story great probably mistake control

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import tensorflow as tf
-print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
