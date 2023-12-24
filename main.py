1ef8ef6e7621f6a02ef953f01a8df408c47087d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:32 +0000

    meet respect argument height immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
