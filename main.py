7d6d596b57ead1d3f29b76391ef6e5214b070116
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:30 +0000

    slightly post bus interest worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
