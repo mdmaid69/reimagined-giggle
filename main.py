99b195eed7e73bb70fc73e2ea18fa82543d25d26
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:57 +0000

    from firm opportunity cool authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
