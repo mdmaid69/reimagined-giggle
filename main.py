2aa6464d366be7e4caeab923755e95df76772e7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:42 +0000

    preparation since plenty surprised mention

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def add_numbers(a, b):
+        return a + b
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
