91736d89d95c1620c61758d7f223cdb7b5d48848
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:28 +0000

    kind Ss distant decision university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import time
-print(time.time())
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
