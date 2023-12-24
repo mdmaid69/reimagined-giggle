4123eb6a2f2f10687b6151225aa8cd2e1aafaeec
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:30 +0000

    survive goaway doctor injury part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
