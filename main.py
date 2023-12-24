8cd5d087b111b62031d8eb988a00093b493a28bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:55 +0000

    acceptable cut target common wind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
