357a8540fa7e60ddae0cded89f2d725b6c78854a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:36:52 +0000

    decide above admit opinion relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
