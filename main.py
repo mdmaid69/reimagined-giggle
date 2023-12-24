a1369cbab6f5db1c2a18935253332a9a4d157797
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:33 +0000

    feed tear bread shut niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
