d3000226ba8228b2a44247615ccb6d4350bf66f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:05 +0000

    tidy quick rent attention Ss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Uppercase:", text.upper())
