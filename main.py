bf5bde5ff7d2e32c064e13aa5685aa2336438ef0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:01 +0000

    nurse goodbye skirt image mind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
