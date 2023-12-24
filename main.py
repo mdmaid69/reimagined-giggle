08ba64ad5aec6bb7400c49fe070e1d37d1914548
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:51 +0000

    claim sometimes boil line favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
