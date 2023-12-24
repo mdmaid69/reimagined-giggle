7bf904330a3a6fd0eee0b5de655b194fc755850d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:50 +0000

    next Tt theirs takeback fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
