cd90670f7dd235d2d792f38ab6501bad75b52ea5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:36 +0000

    alone black late limit plain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
