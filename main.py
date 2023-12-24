bf4d7cfccc5f455e1926deb1f8235d24299434ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:26 +0000

    cheat woman material jump violence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sys
+def print_python_version():
+        print(sys.version)
