38afba5d8cd013b0402047ccc3276ffea6b04ae7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:31:49 +0000

    past describe giveback somewhere mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def count_elements(lst):
-        return len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
