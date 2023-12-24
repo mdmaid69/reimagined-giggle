6909b65156b7672d21c49b3075a97721a952aaf2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:34 +0000

    taste crazy usual short example

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
