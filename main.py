52606e730c42797828da5a110c16d5a412edcf91
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:52:59 +0000

    hate tomorrow generally still ahead

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
