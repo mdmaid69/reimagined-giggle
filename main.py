993205d6ccd1d1e90136639810593280c791d430
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:24 +0000

    bar and process safety fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
