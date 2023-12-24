3026cb9c9855d47bc1d2a9e2d5f93685b4a2accf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:10 +0000

    your juice rob tour large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
