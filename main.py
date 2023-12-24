9a311f444b1b8c02d5153164609732c805cef87d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:14 +0000

    bound dream dance cum interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
