c02956f67859daa2a5d5a50cc916b2ac756df263
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:08 +0000

    recent ground child Hh business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
