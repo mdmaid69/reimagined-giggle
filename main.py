198c7744d5cf81239eaeff90706b38561809f38a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:49 +0000

    much sensible sensible behind lost

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def add_numbers(x, y):
-        return x + y
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
