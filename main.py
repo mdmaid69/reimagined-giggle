f8256d62242d04571452a84404cdb28a1b7850ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:51 +0000

    declare fork delicate fence action

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
