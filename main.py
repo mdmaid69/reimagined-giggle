fb8e39892e87e5954f98e498121b261b8ad6f70c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:53 +0000

    closed maintain inside rest dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
