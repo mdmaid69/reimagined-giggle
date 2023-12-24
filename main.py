40902ce18ab8fa7be0b14a5836739b9f700858b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:56 +0000

    scream trousers maybe file easy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
