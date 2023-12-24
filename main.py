b7608581d04b786173696ac069cd5552c0c3817c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:53 +0000

    exist giveout card disturb fast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
