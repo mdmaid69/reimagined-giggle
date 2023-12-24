311cec42009e555153dd18c92938c695f605808d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:59 +0000

    small hope typical plastic to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
