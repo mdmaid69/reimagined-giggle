a87d9d5b12068bcf70a048baa29d4dc95ef931ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:49 +0000

    history improvement interruption anywhere business

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
