7d6845547561467914a4df024d76f63e91e5d476
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:58 +0000

    single across finger brave point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
