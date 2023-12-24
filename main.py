adb3d82831dbf2f92535843a859cc8ae05f24d68
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:21:16 +0000

    up fair breakup explain Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_circle(r):
+        return 3.14 * r**2
