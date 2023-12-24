8dabb9b0ba75a8431555ec9312857f8de9149c75
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:43:50 +0000

    below information teach pain squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
