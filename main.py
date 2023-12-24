7558515ee3c7826c3b7bd42b016c3c1eff9e2b4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:53 +0000

    takeout flight ashamed unfair favour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
