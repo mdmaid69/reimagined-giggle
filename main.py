2db63a8b222df728a3c25c14203b93210c093780
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:46 +0000

    drive block wet actual price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
