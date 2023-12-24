5a3f0e13ba88981adc69751eab51e0000746a627
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:23 +0000

    back mend hold improve unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
