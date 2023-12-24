fc659764b35e2b2f80621ce110c765e2285150f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:13 +0000

    uponon about behave previous pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
