26feb5d82ca15db89e7709107d533df0400ca59a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:31 +0000

    except compare result tear set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
