affdd36e3912415b6c2f053997d9df0b9a9aa029
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:02:44 +0000

    weak earth innocent tape store

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
