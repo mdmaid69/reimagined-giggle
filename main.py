308370195a70919cb597c7f0fabcdeb95a9237e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:59:10 +0000

    switch label book exit force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
