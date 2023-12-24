0fbc19cf60afe38b35693dc9871a1a819281505c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:55 +0000

    another judge suffer contain difficult

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def is_palindrome(s):
+        return s == s[::-1]
