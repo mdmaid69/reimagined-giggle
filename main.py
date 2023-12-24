57cebb61aa1d65f17131659d32695adc7055a480
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:26 +0000

    light region opposite wash rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+from collections import Counter
+print(Counter("hello world"))
