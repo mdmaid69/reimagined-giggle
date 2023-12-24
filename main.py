07ba8694968a4765e2d52ed2aaae1ef004bee0a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:29 +0000

    least handle once steal pencil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
