a871ea1e55d43773add487f5409967e4ab393d98
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:26:38 +0000

    distance 572enter television order hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_characters(sentence):
+        return len(sentence)
