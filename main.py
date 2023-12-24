817e8fcb43c8f21377b930828fb9c88f5c1413ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:56:19 +0000

    sore worry instant claim taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
