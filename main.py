8d2c471e0752e93a92333211326b31591f9c5b69
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:15 +0000

    behave perform judge Jj corner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-for i in range(5):
-        print(i)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_power(work, time):
+        return work / time
