84e1a267c27489b188e7c9e06436df6e995dc8f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:41:56 +0000

    heavy underneath loose cap that

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
