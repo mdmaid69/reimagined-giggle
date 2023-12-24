3023858b77d26189b7ecd225afd753365a80df9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:42:47 +0000

    path foreign her some anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
