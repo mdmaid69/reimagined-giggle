a99e661d3d3df28f48ff49a00ae2a61b20b75f57
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:43 +0000

    let accidental at estimate machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
