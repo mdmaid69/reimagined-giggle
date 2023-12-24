921cd913194f378bbe4dc011ce32b8a0f3cfc62e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:51:39 +0000

    pour holiday teach government ordinary

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
