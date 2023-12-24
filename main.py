62b5ba21c1f4c1ba0ae463c4bf0c029e61d5ce1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:42:39 +0000

    murder 564encourage wise story beat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
