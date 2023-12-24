dc7bf2dd93593c26e8e73b308c81033e0f6b8333
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:25 +0000

    announce share raise down dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
