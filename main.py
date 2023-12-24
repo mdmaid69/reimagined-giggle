4a56e5534bc8f8b6c23ed09aad5666198d095222
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:08:46 +0000

    material train interested split trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+import array
+def get_array_as_list(array):
+        return list(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
