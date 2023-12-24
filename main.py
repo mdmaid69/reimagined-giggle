d1cdbe7cfddae3a4d968c5a4378579fd51d8410a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:25 +0000

    strength exam natural tax shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
