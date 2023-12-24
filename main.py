8c68183ca14ff21e9fa07e1643954cc501f337ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:46 +0000

    use object proof ask keepaway

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
