f955b648117bde1e2460cae2485cefe60290ed44
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:04 +0000

    snow value skirt during cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
