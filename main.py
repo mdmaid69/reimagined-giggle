5dc737ce2786e917ca5d4274a043821e15000dc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:00 +0000

    lend black afford base study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
