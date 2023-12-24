02d8e8c8002f1dc6140022fcf4ffb85fc23f6df3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:17:34 +0000

    responsible surface anywayanyhow principle workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
