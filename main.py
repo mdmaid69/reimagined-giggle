f48f07a463b71e6e1a807bd934dd3ff9adee6ca3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:11 +0000

    wind keepout wander difficult onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
