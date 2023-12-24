9d672ae9d3d7295a490e06d09448d976c5cb7fba
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:14 +0000

    sensible blind 569enjoy off mention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
