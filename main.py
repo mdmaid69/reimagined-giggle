049d9c14a75d1638cef53d4eee960dac4dfcb6bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:30 +0000

    run consist leave consult container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
