6658d4c1e450d9b441120fc7ce03873d1309371a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:41 +0000

    spot shoot cover careful central

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
