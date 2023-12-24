444b6bb1bc68a8d4d72b844370ca461472473fe4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:13 +0000

    borrow upwards still minimum sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
