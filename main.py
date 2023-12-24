30bc33b8a3fae28da2be137ae2884583216f0af8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:43:39 +0000

    sign boil unfortunate promise sport

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
