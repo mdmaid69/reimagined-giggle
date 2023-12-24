4280c842c7f9d59b1ba883fb1c38bf9771c7a721
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:44 +0000

    roof slow necessary breakin beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
