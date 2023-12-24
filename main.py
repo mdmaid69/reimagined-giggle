6b01b4b1a6223de1a3a483fac4b091f6b1e2169d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:11 +0000

    say mention lead exhibition certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
