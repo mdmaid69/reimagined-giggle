1594122975ed27f2c2827653886632fb73ad8757
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:31 +0000

    like result care course battery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_hex(n):
+        return hex(n)
