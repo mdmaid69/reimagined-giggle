2d20b9a8ce9c67c0cf7f26f2b77e559e1ea2c5f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:15 +0000

    comeoff do last represent run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
