3323f70cf67442b0ca4a5e49db78185feee917c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:17:47 +0000

    image related nice uniform at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_array_itemsize(array):
+        return array.itemsize
