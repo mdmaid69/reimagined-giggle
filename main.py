0bc6a580fe77ba2e0dad3fee542b27080e1f2158
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:35 +0000

    listenfor wind defend still open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
