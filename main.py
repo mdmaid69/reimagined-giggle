4900e20f132e831e1e357ef2d1a8ba9b97e3da49
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:16 +0000

    confident cause dear yet shade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
