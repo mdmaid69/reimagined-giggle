bfd0a8f0745936c825046e70a6ffffba6eefc545
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:27 +0000

    reliable pot conversation check short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-def exit_program():
-        sys.exit()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def sort_list(lst):
+        return sorted(lst)
