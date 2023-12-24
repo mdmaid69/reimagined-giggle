077756b58bc935c4e67c84eca26c404e2328e569
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:28 +0000

    cause arrange sentence when smell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
