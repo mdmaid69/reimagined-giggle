d848ae4b2f01a21e4f44f5a6e788a81c6dedb435
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:03 +0000

    birth pause breakinto glass likely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+print(sys.version)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
