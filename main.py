fab728700fd2a46cf7d2e9c422ca8f9c8065450f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:37 +0000

    evidence tear powerful runout content

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
