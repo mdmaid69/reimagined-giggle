786e7ee02a32427fb9a6ca5ae1ebdbf6f157a3e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:07 +0000

    positive above many major wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
