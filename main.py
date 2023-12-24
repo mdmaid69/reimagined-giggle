96f3ccbb7c61a3fc637caf5a6bf8d78cd4bffec1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:13 +0000

    keepoff practise refuse lookafter holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def convert_to_hex(n):
-        return hex(n)
+def get_array_as_bytearray(array):
+        return bytearray(array)
