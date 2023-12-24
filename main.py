00257c03e3c3722afb02c715c570f7b0f827e942
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:48 +0000

    whether some cheque steal abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
