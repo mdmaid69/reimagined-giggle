2e9fffec2c0114bd0bfad17c1da3b63fe076e3f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:48 +0000

    trip charge reaction another pretty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
