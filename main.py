ecf16fbf7eaed98f6707d5ad4318c41cc0fed26b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:12 +0000

    sore characteristic object drink recognize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
