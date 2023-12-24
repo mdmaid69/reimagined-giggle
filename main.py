6c365b8af666cb54cf0a74641a4d2ab6a04c657d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:11 +0000

    agreement begin tape together refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
