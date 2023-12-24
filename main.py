e336dca18f872db6f88350c0fd3e54b2d35a8de1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:09 +0000

    powerful belong clearly example tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
