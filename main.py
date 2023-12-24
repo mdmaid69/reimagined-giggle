1a4229d1dd1417d1f677990ee17fd6b927f779a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:20 +0000

    regard proud nurse cry run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
