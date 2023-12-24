d08150b7a5c95b841eb20b610e1681b2aa57088d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:46:22 +0000

    tragedy smooth some anxious immediately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
