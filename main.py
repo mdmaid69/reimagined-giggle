e65459eb3c2f643cec33e69d9ed3f06bf23f2e0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:23 +0000

    any first pretty react form

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
