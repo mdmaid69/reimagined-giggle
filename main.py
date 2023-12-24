e93a50417c684a4408af37a6281ae033749830b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:06 +0000

    ball through information aim shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+print(math.pi)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
