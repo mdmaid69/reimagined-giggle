042b8b247bb511baf22352f72942da9f186a0b84
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:14 +0000

    silent might permission doubt squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
