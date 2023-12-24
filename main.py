3cd93ce05452c66926e06e51d3ed6e6755a0ef21
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:24 +0000

    long common separate present centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
