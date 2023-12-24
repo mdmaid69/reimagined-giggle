d9a79063c65f3e93f8bf3a5cf414cbfaa6573daf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:54:43 +0000

    suggestion closed criticism do attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
