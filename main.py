478b4202b3d9a35e040cba82451d028aefc2d3fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:01 +0000

    poison shall commit badly direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def is_odd(n):
+        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
