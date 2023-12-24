7d30a399cd3701c21c1944739023f808f66823f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:12:31 +0000

    comeback long become disturb short

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def convert_unicode_to_array(unicode, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.fromstring(string)
         return a
