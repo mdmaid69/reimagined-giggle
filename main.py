6f3dbfc9248a5d19c258ca75b17948b6af2097bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:40 +0000

    them takeaway physical one shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_repr(array):
+        return repr(array)
