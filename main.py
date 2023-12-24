648198130b1bfe3d40c7b388118d555bff55b40f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:23 +0000

    eager pretty Itself belief keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
