e37d1dbab5384a6fd53a36b4df2e77f421b6757c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:05:47 +0000

    pencil 568energy spray violence criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
