aa36ce2cb820c79bd665633574c51a7b0777a2e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:22 +0000

    stir stupid brother gift force

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
