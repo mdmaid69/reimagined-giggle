b6109d47d8c92b0c487fa3d6e5309c7d3eca5b57
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:27:04 +0000

    steal future possibly connect success

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
