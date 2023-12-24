cbb4baa63db024c22bd5386488c12862c6223c67
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:52:05 +0000

    ago stone position book trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
+import time
+print(time.time())
