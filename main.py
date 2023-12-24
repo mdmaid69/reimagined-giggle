0048f0bed83c74949006ca8488e8383e5e7a6076
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:02 +0000

    brown law blow dry thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
