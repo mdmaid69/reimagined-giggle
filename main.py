a5ec00d199eb2d6f57ea4018bc05e7e555c6ba30
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:10:05 +0000

    release lend still ground insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
