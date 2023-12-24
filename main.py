f6ea3ade8a2b8b0496a84c7ed05835588252f409
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:47:53 +0000

    easy everywhere polite paper organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
