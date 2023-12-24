7033417c1e43647c569a2b898fea4aacb3d5bbbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:19 +0000

    dress Dd principle standard freedom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
