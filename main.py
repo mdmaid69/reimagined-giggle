34319827048c641c2b67d9c02ae07b1246c81ad3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:21 +0000

    broad dress famous small matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
