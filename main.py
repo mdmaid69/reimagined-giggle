1c15f6b03e02a52a6d23fe2592aa2e6ae1b08b57
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:48 +0000

    sort unite patience little ordinary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
