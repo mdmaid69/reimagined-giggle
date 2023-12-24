5409d031f0894b59295c9a02b1e8be10da14e21d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:17 +0000

    ability drink lookup language mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
