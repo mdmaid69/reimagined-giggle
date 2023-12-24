86148ab8555c2779189919e5925d52b6044e3653
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:38 +0000

    dark trouble something tense first

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
