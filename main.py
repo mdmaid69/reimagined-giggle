ad38d20d683ced63bd6129b4840aac25a74138ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:49 +0000

    shame flood habit convincing keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_queue():
-        return collections.deque()
+def get_array_as_list(array):
+        return list(array)
