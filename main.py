3b026cf46021ca24db5315c2f759bc3b2bd0df6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:37 +0000

    guess spring total swallow under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
