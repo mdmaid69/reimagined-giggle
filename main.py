72ad24f6db29231dcaeb54c7eaad248100dc8efe
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:15:05 +0000

    slightly cry care past flavour

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
