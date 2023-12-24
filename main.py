40bbea3b47f5642985e05448706e600f708f95cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:21 +0000

    story sensible safety punish solve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
+import platform
+def get_python_version():
+        return platform.python_version()
