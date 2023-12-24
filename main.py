ec0dbf3e8c4054a3fad20e1223fcd413c0009ebf
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:25:17 +0000

    565end brain permission suggest immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
