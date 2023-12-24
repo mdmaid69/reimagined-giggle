7495e5e06655e8a0c945ddb237f5508935707e43
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:52 +0000

    keepfrom mind find method need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_density(mass, volume):
+        return mass / volume
