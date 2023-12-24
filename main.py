42008df25cda394117142b91d45465154b67d32f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:29 +0000

    fly borrow thinkthrough common history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
