b5f2f9630bf93a61068baebffa3d65b46c70e8cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:45 +0000

    behaviour watch keepup pass insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
