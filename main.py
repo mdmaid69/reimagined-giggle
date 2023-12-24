b02d13c05be46cc09b0540be76dd89a868b39ce2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:51 +0000

    recognize necessary lock store cruelty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
