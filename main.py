a346dfa1b385e0c8fc9227a6616a5bdf4ea5c1ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:22 +0000

    rain appeal shade lean first

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def reverse_array(array):
+        array.reverse()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
