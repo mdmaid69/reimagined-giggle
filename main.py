967ccee6685147341eb55214ad86c84521d33f94
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:37 +0000

    then return notice shout hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
