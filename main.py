4af87d816823dbe709334e7263af7427bdad2c7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:47:59 +0000

    breakout winter breakinto closely pause

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def sort_list(lst):
-        return sorted(lst)
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
