813af10e4eab24803afc48a2f7bcbd3c5d993175
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:51 +0000

    hole ignore whistle perform again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
