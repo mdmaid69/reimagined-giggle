3bc1113232b2af46037ee6ff0b69862bb63f022e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:40 +0000

    nervous accidentally best enthusiastic impossible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
