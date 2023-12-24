054cd86418542533663a31b4e48c538a345eccbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:53 +0000

    noise block again loud rest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
