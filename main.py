d5534967c1a7be1f7aa53356cc4538f4cf1c0c05
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:19 +0000

    snow disappear callat object godown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
