de45db117b5e9f76016f0d6fb97dec4e4a627a7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:16 +0000

    evening pleased giveaway guard mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+print(sys.version)
