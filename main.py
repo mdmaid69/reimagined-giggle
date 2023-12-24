0709cd7d471a8e935ade58275d78f5ca1cfb597d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:31 +0000

    force rest noise formal plain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import datetime
+print(datetime.datetime.now())
