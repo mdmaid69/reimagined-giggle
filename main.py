cfb2ed4e0c52b91991a4e71e2d503c7cc00dfa85
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:01 +0000

    spill sister holdon short tool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
