d3a5bc8cd5a6d0d774538fc0c6acdc96434199d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:40 +0000

    peaceful throughout private compete sound

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_density(mass, volume):
+        return mass / volume
