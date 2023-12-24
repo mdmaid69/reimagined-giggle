e0e71a815f8d5579ff084dc7874ff8f9a720886a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:36 +0000

    throw pride dead store surprise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
