d9badd7f04c7a8cce363a0c1b4cca19b8bdd3d5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:52 +0000

    power near owe conversation appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
