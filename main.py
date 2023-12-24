600d66c70822e030c5bb59795d1c7b144a6cd115
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:24:18 +0000

    getback hit thinkover advise real

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
