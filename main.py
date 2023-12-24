32a1d9920bc405d03e9ca2e0676cbf61e661f7c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:11:28 +0000

    sleep 570enjoyable negative cough risk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
