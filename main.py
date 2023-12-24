7e2f7a70d3d53c82e0143fdb605267eda6955a82
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:27 +0000

    start acceptable crowded vote disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def convert_to_binary(n):
+        return bin(n)
