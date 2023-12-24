bd3ad3eae71071766c5fad1172e5bbd286482521
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:42 +0000

    since summer plant every slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
