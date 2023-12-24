aa11b5cb2130c02ac5671b5a3b2f976ff0051e09
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:07:07 +0000

    method heavy behave strike hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
