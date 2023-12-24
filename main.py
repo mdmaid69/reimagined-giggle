eaa2a354721b15edb3bbf8d6f4f07538a918260a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:19 +0000

    scared shake whistle interrupt mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_list(array):
+        return list(array)
