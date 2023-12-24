43aba2558b1a5e1e768ceb12c4660701b47beaef
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:53:17 +0000

    any on Hh precise correct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
