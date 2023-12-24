f1633a8a9a6f7a4e68318403577dfc2b039d5898
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:20:14 +0000

    other watch everybody pay now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
