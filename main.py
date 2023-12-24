254b077eb4c3aa197a62b08c6bb2dd07ea000e96
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:23 +0000

    bin spot hardly even goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
