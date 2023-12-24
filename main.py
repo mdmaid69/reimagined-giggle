79275d8fd5fc8d12f7e1c3047cfbaf6bc8b880ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:13 +0000

    whoever square punish calmly invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
