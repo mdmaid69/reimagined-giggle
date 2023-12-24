3d6321de89e7e6b9bbcf73a5e555269ccbc9a864
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:55 +0000

    winter mistake lookafter wing fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
