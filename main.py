b08b214a6ec1a7c7ecc3c905964aee0ceb76477d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:26:31 +0000

    frightened ball face concentration fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
