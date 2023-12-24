0fadc04cf370d54314308ff4614943ffbd45428f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:53:44 +0000

    risk takeback needle refer base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
