514cdb5fbfaaddf54ffa4a2ee7d8ee762c52a445
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:27 +0000

    act holiday region sound side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
