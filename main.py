214278dd9ade62da6b13e1aecd6c89141d0f0df7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:36:16 +0000

    more level giveback that pocket

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_work(force, distance):
+        return force * distance
+def calculate_average(lst):
+        return sum(lst) / len(lst)
