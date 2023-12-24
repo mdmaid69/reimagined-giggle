b87c6c959c2e3c7bed110b382e231984c0998ead
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:02 +0000

    mystery moon mind guard beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
