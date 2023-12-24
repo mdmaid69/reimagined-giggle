597e8a595a80c8f2c786bc11fdf9304a41d33abf
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:33:21 +0000

    cheque shock past enthusiastic sail

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
