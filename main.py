81a770e2ff0a97761d9aad9e4a0b1bc8b321a152
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:42 +0000

    ought difference prize rush describe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
