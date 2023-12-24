1483ca8894fda488149d4fff13aa839d33888f3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:53 +0000

    spread ball comeround nose importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
