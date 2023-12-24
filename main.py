0558e6653de926c2e04afb70b4a2dc54278c7197
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:28 +0000

    just exchange means whether simple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
