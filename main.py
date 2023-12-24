da26a301c2106e7360c72cdc7e56244fc2c42d73
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:06:28 +0000

    cry tall expect gradually likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
