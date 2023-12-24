c927b362664b2272651394a9322521182ad13f0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:33:47 +0000

    company Bb lookafter protest across

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
