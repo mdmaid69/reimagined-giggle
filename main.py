6dec942f7f3c8f5facb310f84530d639cdd7eca7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:48 +0000

    battery judge uncomfortable game difference

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_repr(array):
+        return repr(array)
