a6b4f9006333807165d40c05d1719460c0927529
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:21:01 +0000

    the gap song once now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
