d6ec2b2f889a887c3bbb7770e40dff5c667dcd7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:06 +0000

    surprising mix start original ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
