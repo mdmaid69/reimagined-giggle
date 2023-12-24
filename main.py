e705fa1a489f0ff13158a9f96e55da5136069f48
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:27 +0000

    turninto damage guess OKorokay bother

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
