302e97700f5b43e67cfd1ad00311ede2413d9327
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:49 +0000

    store uncomfortable goup high reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_directory():
+        return os.getcwd()
