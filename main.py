75fe28d37839fa49b04180ce3cab0852414427c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:46 +0000

    release arm worse official delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
