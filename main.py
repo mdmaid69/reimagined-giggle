fed6150a9d83556a235bc568ce3f74965ffb8c5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:22 +0000

    reduction home thought mark wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
