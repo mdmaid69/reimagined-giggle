4bf9082b67cf78df0958111a888e082a30076672
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:41 +0000

    receive suit winter fear serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def reverse_list(lst):
+        return lst[::-1]
