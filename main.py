dccd9fa85b646991769bdb0bd3768b87ea8d7b7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:49 +0000

    respect stamp Ll beginning wherever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import logging
+def log_message(message):
+        logging.info(message)
