2bf6dc6d7faea5baf8b54b85692cb4c10b35cad0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:43 +0000

    determined circle among Yy object

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def add_numbers(x, y):
-        return x + y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
