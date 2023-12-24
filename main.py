88e2ad87fb57e31604835dadc23fdc2b8baa46f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:28 +0000

    talk excuse trick reply glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
