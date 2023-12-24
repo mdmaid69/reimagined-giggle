c0c6067aa2ae26f019b89c6003851e619bb35d52
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:31 +0000

    learn and maybe impressed yours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
