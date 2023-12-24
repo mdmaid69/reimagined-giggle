82d54bccc7b5982be63392ce78d988372cbc839d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:05 +0000

    world sensible whether standard sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
