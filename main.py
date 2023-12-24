d3b7725ca30cf867ab238bfa0a4e8d76f2164050
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:04:50 +0000

    after love heart 568energy runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
