3dfa210b34a509bdea7ffa377f3988d3a470e0cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:33 +0000

    goback(to) involve pen alone truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
