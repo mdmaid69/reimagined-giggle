25a9ea6b0663044a45304d5cece90d50ad8ed597
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:17 +0000

    create terrible season number history

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
