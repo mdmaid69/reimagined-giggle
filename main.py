09ecceacdf00228bbdca7392378afaeb24b3ea08
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:48 +0000

    aim turndown hold turnup welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
