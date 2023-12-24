a1c6a02efb77d8c3f621eb65d0ce79fbfb87f9fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:46 +0000

    unfortunate independent expect control piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def reverse_array(array):
+        array.reverse()
