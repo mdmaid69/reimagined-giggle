ff41047d7d9796900f1c5b3369995461a3993e2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:12:49 +0000

    lonely over sign together simple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_min(numbers):
-        return min(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
