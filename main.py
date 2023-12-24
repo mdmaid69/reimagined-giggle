f7b380ff9880dcdd6bdcf5a5c233659d45891079
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:22 +0000

    dry refer react read truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
