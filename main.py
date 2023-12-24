4a6eba238da12e38cd20ddd302d375e544b70e95
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:21:28 +0000

    concentration father breathe lesson wall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
