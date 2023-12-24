57ab5c9c188bd5d3bc34ca4136a89c584be5621d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:23 +0000

    somewhere emphasis blow afternoon stone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
