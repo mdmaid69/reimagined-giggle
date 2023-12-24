db80efdeffa24d6bb32c1e889a645a2ab38adfe9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:28 +0000

    probably fast weak possession apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
