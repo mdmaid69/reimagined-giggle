ad86abe18ca3ab855744d77e369425b46b301fb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:44:25 +0000

    schedule understand picture influence air

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
