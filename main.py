06f2dcca7dee41aca70ed4bea1378a09443c36b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:54:40 +0000

    brain corner learn last generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
