2bbaadc9d673a798216249ee0027440cbec4a3eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:30:47 +0000

    him comeup square offer through

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_error_function(x):
+        return math.erf(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
