bb819e61e682273db1400edfb0e0c01e37482569
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:28 +0000

    line sail sensible kind tooth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
