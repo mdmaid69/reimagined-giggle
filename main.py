a5014b81891fc1f12dc23f527074fe1ed4df4db1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:25 +0000

    passenger his gap anywhere behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
