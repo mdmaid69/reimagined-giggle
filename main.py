0ec99d3df3abf730c9ec0d61d9e3d8f8d6fafbd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:18 +0000

    breathe complain seem imagine claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
