59b6856bf5133af2bc04378ca267af6b8d0d736e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:33:44 +0000

    again represent keep pair speed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
