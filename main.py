75580fec620c1f4d41676027eb65df3bb9134337
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:22 +0000

    mine careless not handle comfort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def append_to_array(array, item):
+        array.append(item)
