609280debd8d115fcda7baea6c58ee00a72f38f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:34 +0000

    564encourage exit any Dd process

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
