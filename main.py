1f3426b20636fe1a6e18a198b53f2cc52d38ad75
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:35 +0000

    sticky many nerve sea rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-print(random.randint(0, 100))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
