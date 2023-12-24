b2de49f1b76d60a6aa064cd530153b97d184593c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:53 +0000

    protection entry often holdout stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
