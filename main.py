e8dcc0fc4670df0034a84f8f324f49d35432b4a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:28 +0000

    right emergency trap moon free

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
