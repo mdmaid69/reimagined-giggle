ec9f83bc953e3bc0f50a3543e8858a8d84b39dd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:42:27 +0000

    shock variety half here order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
