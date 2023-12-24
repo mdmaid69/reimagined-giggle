1c5346f8072cae4b733db380b87bbdd9ae362209
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:47 +0000

    guess record generous action lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
