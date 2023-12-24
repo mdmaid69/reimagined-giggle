67f6aa971ccada2e689a978858ccbf56e89b8090
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:17:20 +0000

    hungry sensible hello start still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import sys
+  def get_python_version():
+        return sys.version
