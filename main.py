fc9230ed8a4c26f9a82dea93a93036b53f0b73f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:52:53 +0000

    official weak Oo comment workon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_length(array):
+        return len(array)
