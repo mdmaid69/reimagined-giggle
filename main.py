fccbb145885593204a1a5deb64ee165db8447642
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:06 +0000

    able speech injury back swim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
