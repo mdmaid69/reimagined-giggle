0ce45c581c1f340b36ba1d861e00ffe39968786a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:24 +0000

    short impatient prefer among nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
