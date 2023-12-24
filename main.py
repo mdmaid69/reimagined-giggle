f7e8796d91d6feb898f375964a388a2baea27e53
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:03 +0000

    I disagree say use responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
