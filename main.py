c9aa86d9c4a221bc5a08967318062f5e99ba883e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:13 +0000

    beyond lead 179bite time nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
