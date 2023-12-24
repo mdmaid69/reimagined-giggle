e8b5694695a0b3f9bea0312160693377db27137a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:17 +0000

    bringup bury narrow lie runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
