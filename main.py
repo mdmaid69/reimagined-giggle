3167cd894135b1b41c5d971e386cc613c89a8778
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:53:55 +0000

    note legal protect born father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
