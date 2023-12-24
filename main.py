99fc93862257730a32e5ead4b07422fce01cf317
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:21 +0000

    hard slowly tomorrow bringback politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
