2830b4edd0bebb38c99151cbd531589304fad868
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:37 +0000

    considering breath fill attach air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
