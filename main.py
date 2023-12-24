08fc6206cc471fdc6474599ad920e6ce7f180b26
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:57 +0000

    dinner mixture trade experiment rub

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
