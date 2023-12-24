3a4e191603a250ae617b81701c5ee86a9a34c59b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:59 +0000

    Bb table wide throughout invite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
