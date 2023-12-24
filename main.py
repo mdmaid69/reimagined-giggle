66f3ed46b5e83c7d237ef229e7a24893d2b46d87
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:58 +0000

    Dd blow first mainly little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def convert_to_hex(n):
+        return hex(n)
