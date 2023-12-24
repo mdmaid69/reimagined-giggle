31f1d76422cd87930b45fac29615747885a33b93
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:42 +0000

    mistake cut you lose building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def convert_to_hex(n):
+        return hex(n)
