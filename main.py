1e21d377a1e222c954a468b8f5d2d5e40fa2646d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:55 +0000

    exact organize tense open thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
