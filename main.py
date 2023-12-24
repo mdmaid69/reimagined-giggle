1a9f0cbc39f070be29d68509b0eded557bd24a61
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:14:27 +0000

    comment alone pretty where waste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
