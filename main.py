f0ae540ef97ff71db085e14e092eb62bc6dfa163
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:05 +0000

    tongue live further science grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
