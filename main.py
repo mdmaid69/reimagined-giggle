c70d5a9594b9a16419c6e5694c68596f31f90059
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:31 +0000

    compete feed movement national ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_set(array):
+        return set(array)
