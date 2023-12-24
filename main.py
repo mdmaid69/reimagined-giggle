48c91a180d05f43f1e24d555dcc9ad11b79c066b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:58 +0000

    dream somehow clean turninto breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
