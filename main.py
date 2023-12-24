d9b623d4cff823b40622512cbe5977b2546c7956
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:46 +0000

    act admire bit such near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
