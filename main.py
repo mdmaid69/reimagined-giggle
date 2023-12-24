7ac7c78523edadd5548ce7592b4f4401c92e2ab9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:35 +0000

    single when soft pride convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
