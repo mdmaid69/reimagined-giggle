719d3afcbd189311577bad1b3ec48f887d71de5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:50 +0000

    dress travel loud like Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
