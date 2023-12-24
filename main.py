5b2896d11fe6e96340851668e300a088640ee898
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:38 +0000

    wander serve weak steep let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
