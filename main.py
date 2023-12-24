27efcc5b5796b8e9fcc6af19c80dce9f128e6829
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:26 +0000

    phone nearly slide secret picture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
