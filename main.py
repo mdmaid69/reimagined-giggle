0d9e94b8c755400df6a87f0fe5facf1e03bc6857
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:25 +0000

    either when introduction rest hurry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
