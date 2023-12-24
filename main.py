2b5f6f67e7923d2f9154880c7f38664ba507fec2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:10:47 +0000

    warm frame please thinkof cross

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import json
+print(json.dumps({"name": "John", "age": 30}))
