8806b50581b9564c090225f9cc3d4ebf1c364ab9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:05 +0000

    pour uncertain ask first ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
