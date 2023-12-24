37c030a7363b8d1609fe0cc3af09fd6e9f54b2b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:47:27 +0000

    priority wave satisfactory Hh steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
