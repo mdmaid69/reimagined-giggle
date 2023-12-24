c441327fd5192ab3cb9f0ddf44dc62303cd2afa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:52 +0000

    special door obtain cost fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
