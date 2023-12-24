7b795537bdb174da47603063c7c0727c8dc73a6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:52 +0000

    often uniform impressed face brush

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
