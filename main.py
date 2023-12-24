8561a1e3c1939e48805b32d3d2b6cdfe364226f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:58:04 +0000

    post hit government top swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
