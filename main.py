50fb9e41a6529bd7501fb00e4e5310934b2a8579
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:08 +0000

    sorry edge explanation confidence often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
