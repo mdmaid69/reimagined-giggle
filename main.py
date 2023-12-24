705a619e0e095ebe6f02a2f7b8c1070770f6085a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:30 +0000

    kind target weigh arm imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
