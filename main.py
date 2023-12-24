3335bd9b44be212735c9d088c73455dd5b2fa8a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:44 +0000

    afford business plant spend introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(lst):
-        return min(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
