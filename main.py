5b194046efc93901d0f0498ff2dd49b66f62761f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:03 +0000

    acceptable promise lonely excited safety

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def count_words(sentence):
+        return len(sentence.split())
