e45f0e1c77d4b0a9cb8c91d44a6c5ba076a33f72
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:21 +0000

    government bother popular pick regularly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
