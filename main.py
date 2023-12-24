42344ab325b24ff4afe5cfb25a5baaa584b0cb80
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:14 +0000

    unless advise man relative majority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
