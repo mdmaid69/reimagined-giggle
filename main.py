fd162c019f6726c1107d679a8cf0503b0302ca9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:38 +0000

    lean exam bang gentle turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
