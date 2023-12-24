9fcf6c8f0901d03b4552adac0423bfb85c043aac
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:00:49 +0000

    dinner development worry interested sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def convert_to_octal(n):
+        return oct(n)
