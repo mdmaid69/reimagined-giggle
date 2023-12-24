9968dfc66deaf0e086fb554d790b5ce232ecc70f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:02:17 +0000

    appeal sensitive jump up leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
