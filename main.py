ddee08f06cb57b5cf11e37ffe44663e274a64318
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:13:31 +0000

    care choice shock possibility similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
