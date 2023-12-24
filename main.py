63ceaea190684f41342f0bccd35c789a2cd29b78
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:08:22 +0000

    say seat runaway glass 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
