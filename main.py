8e79672be68ca5e7273163d7084ab56e7d75df86
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:29 +0000

    switch rarely normal keep set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
