d499527bc5aa4f36a61be6c385ddac60938b7725
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:19 +0000

    recognize confused base sit woman

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
