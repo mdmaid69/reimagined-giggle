633ca8d259dabc346467087b393a7ba5f1af17f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:15:39 +0000

    fact neither manner work except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def add_numbers(x, y):
+        return x + y
