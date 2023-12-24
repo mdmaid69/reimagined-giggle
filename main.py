36a17ed5bff129ee5c164888f4ced77629c520ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:50 +0000

    clue immediate visit cover turnon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
