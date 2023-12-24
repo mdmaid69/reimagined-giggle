0ceb62362bec80650da292ab001dd3c043b8becd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:17 +0000

    outside glad strike sight interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
