53593fdb34490c6be4edcde15c5f98bd869b0f3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:48 +0000

    flood mysterious concerning main deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
