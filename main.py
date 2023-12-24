1c8c8f6f5285363db899c721b72dcfca88027ace
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:47 +0000

    Bb place tonight path attractive

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
