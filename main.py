e82e7508eb943f3624691483948a815345aa8489
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:58 +0000

    range in broad everyone immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
