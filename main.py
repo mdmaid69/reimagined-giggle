6030a67320389b329643ef5c2f545413d2a23400
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:43 +0000

    certain formal concerned bite use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
