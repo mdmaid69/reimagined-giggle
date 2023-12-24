a4a63249623a2ceac56f69ac6c5edd81cde40f7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:54 +0000

    hospital serious dinner news evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
