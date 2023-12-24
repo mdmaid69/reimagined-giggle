23bb1341155ff00e51b1ca0fe77932576a110f7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:25:33 +0000

    branch reliable sign needle show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
