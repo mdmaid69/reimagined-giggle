ebe8e2b790bd4409e094dd5609bbdaba3b352721
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:41 +0000

    brother above hold excited pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
