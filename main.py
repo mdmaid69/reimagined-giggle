81ccc84755c79b24a043c179762adc5de1656917
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:13 +0000

    pressure plenty law bringforward trousers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
