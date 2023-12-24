36059dd4949696dca8bf7358fd1e45d8e3d57a16
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:23 +0000

    prize fast where terrible rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_today_date():
+        return datetime.date.today()
