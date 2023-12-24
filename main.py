8cf2b617a54ce33ef5d59d4664d7c7589edc6182
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:35 +0000

    soil idea herself criticize behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_elements(lst):
+        return len(lst)
