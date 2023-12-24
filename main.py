64e647aec1e4a9e51430c946d4901c0270ceb7bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:55 +0000

    offence chief miss sad also

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
