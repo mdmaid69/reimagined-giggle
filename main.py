dde1ee5e445fc87a90ed32adc6394791cd0a6bae
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:21 +0000

    tap used comeoff edge plastic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def subtract_numbers(x, y):
+        return x - y
