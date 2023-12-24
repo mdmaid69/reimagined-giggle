52136adf226699c68f0f089f2a12854d67efa771
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:41 +0000

    wipe trap pattern cross tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import numpy as np
+print(np.array([1, 2, 3]))
