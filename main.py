4375515c18e68750d5bed52d920b16aeff93b885
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:43:15 +0000

    lookafter conclusion stay comment explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
