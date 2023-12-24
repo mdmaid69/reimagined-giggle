01a75bd73513f65c5f30e883b8f8f6375c5d718e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:48 +0000

    shrink wish tongue shame spot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
