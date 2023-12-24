8e41eef3e49bd9dd937c6deda48c060cf591d589
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:16 +0000

    again left collect Dd dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import sys
+print(sys.version)
