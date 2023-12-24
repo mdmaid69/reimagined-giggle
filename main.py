48c86083ab491f79090491d9a8bfef77a19826a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:59 +0000

    discussion attempt ilovedicks exercise swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
