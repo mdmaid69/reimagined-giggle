ef906b98efc34438a931fbd46dcbf1cbe2b03ed6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:10 +0000

    steal process worth neck keepout

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def is_odd(n):
+        return n % 2 != 0
