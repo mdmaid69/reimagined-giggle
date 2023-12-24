895775a9b562f7cea307a3ab2bfcef59d56e232d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:40:58 +0000

    shame closely close save view

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
