5c041861bc323db729c18bd2ed822112ae5984c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:45 +0000

    tall artificial his yesterday giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
