a13d7b6ab61333bd71b096f4f8cfac0492252731
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:51 +0000

    cross reliable clothes record today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
