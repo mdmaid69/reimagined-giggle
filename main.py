49910f93471bab683781c129a38b339a76b9898e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:56 +0000

    down boil grand tool ability

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
