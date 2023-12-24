3f264461047516410114369d551fe242057b6676
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:01 +0000

    basically mix home age cost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
