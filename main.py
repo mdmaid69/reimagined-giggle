b0163064d164a0629337dfc477310dfe7b499f5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:05 +0000

    tone beat woman trap rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
