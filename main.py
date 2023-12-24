2aac3319595a607a33fa0278cd47e5ba0a9dccdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:02 +0000

    mind recover quick pay question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
