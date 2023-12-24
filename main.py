992ce835eb432e4b7f6903422fc8f307c1be6db9
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:40:54 +0000

    appeal joke hard until method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
