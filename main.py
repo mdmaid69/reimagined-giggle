77b2bf366e319b44e4e790a15f3f028b559a45a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:50 +0000

    hate authority agreement grateful exit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def subtract_numbers(x, y):
+        return x - y
