c788429adb70027b353b46c55ebb244828cc0efd
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:54:08 +0000

    attempt actually rate answer fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
