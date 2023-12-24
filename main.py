3fa2eb9743c73f30fbc3ef59b7cf7b7a7da478b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:16:36 +0000

    turnback connect dull save opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
