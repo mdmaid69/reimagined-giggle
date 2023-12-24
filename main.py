4c61ed66d5419cd634d7df4d821698df7fcbaf29
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:01:16 +0000

    care long suppose nowhere press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
