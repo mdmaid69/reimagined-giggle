86970c50aca9b8b72ccc61b34f08786be93d47d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:50 +0000

    right makeinto into rain heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
