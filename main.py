d07edc669d643a76514d1ec3232aebc6289034e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:21 +0000

    poor wonder comfort transparent use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+print(math.pi)
