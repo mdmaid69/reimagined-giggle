0c6d7e804fb0a467b813adc6db14376738b3d68e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:38 +0000

    professional control sea fence similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
