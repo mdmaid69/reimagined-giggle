d0df804d4a9e75af3261e732fc7d9e96538b370a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:16 +0000

    measure slightly use past opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
