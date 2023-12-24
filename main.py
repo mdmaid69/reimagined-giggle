a5f625c35c40e25007472c5a7116e7afbe44149d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:53 +0000

    reply talkoutof sink professional winter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
