94e93dfed0f26ea04cf307c55328951d40f87b7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:55 +0000

    mark base performance discussion float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
