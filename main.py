c5dc38962eb62d54b6165467f024c2851e5ca34c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:45 +0000

    fond container rob official comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
