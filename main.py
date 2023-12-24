455f5d64e6fe1d4c33c2195e23cd596dab1e2e87
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:52:26 +0000

    unable smell possible gentle related

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
