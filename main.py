f8062f09f956811f83ea5f03b2afa7e50d37db3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:51:48 +0000

    machine during square stare though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
