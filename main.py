6c148b3ed248e9a50508f53fcd90809a5b8d9631
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:35:01 +0000

    bye down recently entrance class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
