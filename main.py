d343aa673dea0e42d3a589da5b30a955b84f9958
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:50 +0000

    brush vote successful hurry immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
