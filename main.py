af614eecaeabc0618cfe7e1746d325cd9d2ddf8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:57 +0000

    beside present turnoff crowded undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
