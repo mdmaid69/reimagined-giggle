094217685af6f53e1f2f592525eeb560e63e1996
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:07 +0000

    temper record still swim wire

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
