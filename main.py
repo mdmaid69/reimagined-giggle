89f1e9a1edb955a9150a89f9accb6014f9d6e20f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:27:35 +0000

    body my comeover arrival murder

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
