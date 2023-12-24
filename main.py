16afc74f1d1b6088a904a880f91d962683c9919b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:28:00 +0000

    sure loud news continue wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import datetime
+print(datetime.datetime.now())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
