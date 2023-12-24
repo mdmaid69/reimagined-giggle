e5091f101a16afff074ecedf2f6b3e12546d2282
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:40:52 +0000

    characteristic progress regional tread tongue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
