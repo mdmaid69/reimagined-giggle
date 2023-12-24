99c85b62c11e30f297738007802864844efced86
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:27 +0000

    separate change much reach story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
