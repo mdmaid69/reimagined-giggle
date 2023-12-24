48981b6657962b0496771082052ea9c9ff3930a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:27 +0000

    enthusiastic turnoff rare pattern plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
