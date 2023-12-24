ffe0da45a78b9a816afd8bfec226e43c6cd1b608
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:41 +0000

    book alive objection noise treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
