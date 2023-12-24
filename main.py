d7f282dda7e4e4ddba05118dfdf742c46f2f6121
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:11:03 +0000

    war future mess include neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_octal(n):
+        return oct(n)
