7e3daac904135f28f242b0e8593579aedb2e71cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:14 +0000

    train telephone report tear bringforward

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
