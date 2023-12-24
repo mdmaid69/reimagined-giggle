ec88a1d570adb4688763b5bcdd18ab687e3db4a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:59 +0000

    children push decide willing brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
