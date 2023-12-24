863dd94116f1933aad624fd646af1790a0eb0b12
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:28 +0000

    bang final underneath problem gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
