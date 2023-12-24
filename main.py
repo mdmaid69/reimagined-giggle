16cfe519e4a11c0ca1c8f1c5c76f9888eaeb4f31
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:56 +0000

    row good love grand party

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def remove_duplicates(lst):
+        return list(set(lst))
