c80a90dbcb581ff77a4fb2b93c29158c8040c3cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:09 +0000

    mistake base shout fire male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
+  def is_even(n):
         return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
