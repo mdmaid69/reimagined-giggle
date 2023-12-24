73b9bec3ae31bb90034ed0a5be87d3241714d3fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:30:21 +0000

    guilty throw considering terrible protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
