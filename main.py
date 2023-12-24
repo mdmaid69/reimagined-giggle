dbac30d32f83ebc41259e58962c9d9b170581d2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:01 +0000

    weekend press row smoke country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
