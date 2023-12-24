7e4dca758527f837a283e40688c5e5184799e376
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:45 +0000

    custom bringback wind ball watch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import time
-print(time.time())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
