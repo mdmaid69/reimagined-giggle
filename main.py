ebf11dd219ef392adc6fb17df1cc91499d1d65c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:59 +0000

    punish not lucky support nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+x = 10
+y = 20
+print("Sum:", x + y)
