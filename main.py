3ad6f626e4adf83ac0dd713d89771f0ec5802ab4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:14 +0000

    people every platform emergency welcome

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
