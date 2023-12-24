1a10bf97688163c60250c5bbd5f8981962b26b15
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:42 +0000

    fair apologizeapologise either brown switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
