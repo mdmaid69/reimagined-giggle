6ca26a5892df15b8c083b8d6d74d13bf7ed112f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:13 +0000

    couple ourselves dish happy hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
