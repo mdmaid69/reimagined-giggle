28f2d10c62899f47cd7b09bd051ce4f8e34c9734
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:56 +0000

    name arrangement settle narrow cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
