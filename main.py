7cae6c1b49772be35d765f48e29eab6a9db51313
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:43 +0000

    talk ocean matter write smart

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
