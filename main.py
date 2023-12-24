0085fff45172d66ca6082e61ff7ae3853fd20100
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:54 +0000

    them breakthrough dance dish uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
