fc934209950fa86f0537f3f851076ac6f8ee5df8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:18 +0000

    wind play translate tongue say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
