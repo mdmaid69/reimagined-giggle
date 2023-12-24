c2a7cc5f6f7f5a4c5a3002b7be1cd3ddfb77db04
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:24:14 +0000

    in figure settle agreement there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
