88488e7baa79669797dbc0c72b72f4ae16df7c20
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:12:26 +0000

    nothing chase accommodation judge choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
