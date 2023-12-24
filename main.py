ab1a51d58a46222ba246ee637c211487ff09895f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:43 +0000

    fortunately our violent duty rob

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
