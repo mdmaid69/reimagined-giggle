049ef3fbd7d0bfb39d09964d0569f4397bc9e96b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:21:09 +0000

    operate tragedy mean battery order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_power(work, time):
+        return work / time
