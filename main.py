d8905ac3a965e885dc0075d748fdd572fc6e5ff4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:03 +0000

    impressive maybe scare welcome getup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
