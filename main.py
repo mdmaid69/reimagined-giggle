4c61ebecf938366f86bc8578a49da22e8aa7d483
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:00:28 +0000

    die possibility wheel somehow shall

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
