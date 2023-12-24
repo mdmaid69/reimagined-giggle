32ff96ac128baf87d40e987e8f41f782f72ea034
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:10 +0000

    anyone union tough giveout lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
