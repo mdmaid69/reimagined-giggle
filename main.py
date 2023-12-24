a7bbd49ec2688c6efaf764b0c33cea6559fff107
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:31 +0000

    would convincing claim himself score

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
