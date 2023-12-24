273ed06a58e0f205ed0f95602d17023b2eddaca1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:27:56 +0000

    performance matter line contain arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
