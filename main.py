ae49dec3e16ed9a935ee9026fb91084cf2abdc9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:42:50 +0000

    wrap press disgusting cheap lean

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
