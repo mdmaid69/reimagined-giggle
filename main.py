f8a08b1af47f0cf0b7c819d4680e038b1c55093d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:55 +0000

    story fair country tread chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
