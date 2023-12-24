b4d15847b2deff8efc7a47b9520b437b64c3c3a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:53:50 +0000

    naturally plate goodbye write walk

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
