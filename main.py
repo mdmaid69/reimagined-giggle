823f0741490a83c4d7cfe82a22a96ad9bbcb8846
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:36 +0000

    mouth partner wonder aim match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
