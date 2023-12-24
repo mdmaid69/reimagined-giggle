49870b5adf975ea95ffb51c3045d01644f25c3b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:59 +0000

    modern hair decision classroom stamp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_palindrome(s):
+        return s == s[::-1]
