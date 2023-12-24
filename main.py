af14296f328c063db2c60517602a845398573fce
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:40 +0000

    sink guilty what moment face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
