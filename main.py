1cb5fee5dc0bbb6dca082342b3b3ef096ee78c8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:38:31 +0000

    swallow future dress spite visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
