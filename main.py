1120b859236295fd3050beeb1f749a1026cff69c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:12:02 +0000

    float bother mystery serve nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
