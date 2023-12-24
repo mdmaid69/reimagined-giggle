b23a4463a2bc174ef6840bc596cece969042c621
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:03 +0000

    turn miss mix spot anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
