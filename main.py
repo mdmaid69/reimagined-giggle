9ea50457bd1b4cd45b3f3aaf7dcde31f11f3296b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:48:33 +0000

    telephone satisfied hello miss emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
