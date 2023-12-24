a67b39c5f36a467db6b62fe57a070a597166c6b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:23:21 +0000

    view exploration right noone morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
