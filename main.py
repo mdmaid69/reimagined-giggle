616f3c938221a938a9b3dfade0db57ae53b93093
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:26 +0000

    call habit runaway dish government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
