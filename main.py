bf8719c512c204f1fb0d35d0d3f8c20ec7f35aaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:18 +0000

    advanced usual further band downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
