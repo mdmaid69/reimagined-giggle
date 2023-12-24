d214240b56c48fc74199f059a7782370f9a254b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:40 +0000

    lunch fashion probably suggest until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
