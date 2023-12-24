c663ffdfb03992e01a81eeb2bb73365a2f49868d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:51 +0000

    course hide afterwards all swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
