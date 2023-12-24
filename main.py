b468b77aacdbd7854737070f600096513870fc5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:18 +0000

    careful eager doubt comeoff other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
