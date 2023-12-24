15c76bd5862786ac0d374fe98e5ead1a36653bac
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:25:39 +0000

    flood keepaway bush part 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
