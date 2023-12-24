054b0b34bcf707fe066837fe7b8de617321e2eee
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:13 +0000

    event beautiful calloff lot strong

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
