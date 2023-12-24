fd4d5a404d29285158c30068eb4506d553207d2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:24 +0000

    successful impressed delicate future risk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
