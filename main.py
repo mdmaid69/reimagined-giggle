a4aa0804e33ed19df219fc7206c84c661f66aac5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:26 +0000

    boil grand dig inform takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
