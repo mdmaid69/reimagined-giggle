033eca9f7f4c3df49ea7da3ca6b2dbe2360d23f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:02 +0000

    Rr sure example breakdown himself

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
+def reverse_string(s):
+        return s[::-1]
