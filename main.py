28f5ba8e203a3e5220ee62df7bc18adfb6bf73f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:43 +0000

    less same boat majority disappear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
