00e24a984269d8d845530734f38b68ff5f14668e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:16 +0000

    outside improvement convinced lean point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_typecode(array):
+        return array.typecode
