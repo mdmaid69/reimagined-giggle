b2798fd01ce545de4b94e1cd77dd6ccb69902a76
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:15 +0000

    recovery promise cover progress human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
