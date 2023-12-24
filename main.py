104c48f434abec542b696231ada2ed5eaf2a4918
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:19 +0000

    book regret unusual ago town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_rectangle(l, w):
+        return l * w
