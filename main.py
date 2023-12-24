1ae2eadb74e5cc2e91f4d96828084008dd8daf86
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:18 +0000

    legal miss drop declaration geton

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+for i in range(10): print(i)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
