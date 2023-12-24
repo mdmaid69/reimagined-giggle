0e8a59af6d1be0d995dda2b8f5127bbf2700705c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:23 +0000

    thinkover moral you dig gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
