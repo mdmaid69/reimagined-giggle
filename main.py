08676e059368862f488df175d9f1bad7b1dc66cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:28 +0000

    valuable I swing comedown pay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
