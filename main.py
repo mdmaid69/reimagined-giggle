ac3043f28b7656445a32dc694583596b76d9d2ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:55:17 +0000

    bringdown giveup tonight hungry ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
