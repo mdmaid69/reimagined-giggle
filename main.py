242defaacd441e9658f89a317d000940eca14341
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:21 +0000

    match discovery poison hard shoot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
