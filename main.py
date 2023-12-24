341c20cea95e0c8bf2139b5a5eb1a4c4ae35a547
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:26 +0000

    Uu tax runinto joke tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_array_to_list(array):
+        return array.tolist()
