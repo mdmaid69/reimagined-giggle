4a0af863b9947140bfd115e0dabb68a30b7c193f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:14 +0000

    basis through free thank tidy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_sine(x):
+        return math.sin(x)
