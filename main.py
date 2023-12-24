904b57219dc132d60a5c2f22bf1916f54c7c1673
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:05 +0000

    runout artificial strange comeround closely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Reversed:", text[::-1])
