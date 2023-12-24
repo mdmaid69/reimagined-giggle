732c405f885422c9c2457af54ddb402bd4d155e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:25 +0000

    left still develop same sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
