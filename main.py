d80c61a83476f81fcd70aa24d3684a82c5476c13
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:38 +0000

    hard noise important backwards extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
