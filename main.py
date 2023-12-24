5b0ee907a613f58ba6118ed6cff60ff27dba4c5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:40 +0000

    choose neither instant float neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
