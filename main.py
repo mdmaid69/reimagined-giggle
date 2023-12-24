6401cbdc0e243fe29e926eb25b4499a82b92fa2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:37 +0000

    abroad space height whisper prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
