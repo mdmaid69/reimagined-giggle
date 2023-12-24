93166ae61c5f0e5f4a63129aa1e3698d95de842c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:34 +0000

    hers criticism start heavy concerning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
