b2a9b0fd49ec8cd455aec8975734abd1b926b619
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:35 +0000

    quick shock suppose loud hole

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_pressure(force, area):
-        return force / area
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
