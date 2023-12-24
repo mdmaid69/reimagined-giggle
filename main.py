969faffea8cf1a906f9e7858502efd8e0dfe4aba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:05 +0000

    fortunate port runinto proof grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
