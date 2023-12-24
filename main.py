eca60002deaec434aef5caee81c4fd37f739a39c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:11:40 +0000

    defend attention comeround together fresh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
