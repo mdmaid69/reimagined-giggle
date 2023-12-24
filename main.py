e208f06ab0fa290e4ea1b2f232dec675b26c87e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:53 +0000

    how split holiday improve once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
