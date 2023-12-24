405c928e8a33ccb3a001a69445c82e7a6cbf402c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:24 +0000

    should loud seem rubbish urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import datetime
-print(datetime.datetime.now())
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
