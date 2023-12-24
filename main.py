937cc7bd2dffe0e5d04bcc5653a0907006d4a58e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:50 +0000

    sale speak fierce official question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
