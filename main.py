3d59c6bd58328df7ca4510c328eee3cb3d626184
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:18 +0000

    key second run hear suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
