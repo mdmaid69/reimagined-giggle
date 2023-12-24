ff471ba024c682c6795568f6479c2ad103ead7c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:03 +0000

    drive whether makeup shout bridge

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
