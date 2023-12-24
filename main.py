787dc2fcfb7e31c938b8103758df421c3b785a23
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:23 +0000

    wall roll rely figure pour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
