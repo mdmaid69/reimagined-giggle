452a065c47485af9c4ca685eb962eba2fbc306a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:59 +0000

    slow pause description nowhere that

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 def calculate_work(force, distance):
         return force * distance
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
