5bed09039fb05fac81ba7d3c8cff3dd3a6557fbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:41 +0000

    reply bag priority pair branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+print(os.getcwd())
