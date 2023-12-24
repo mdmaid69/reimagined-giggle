057fc498fdd0a958dcd225c402bf99ef5b2b6378
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:07 +0000

    flow dance soil delicate fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
