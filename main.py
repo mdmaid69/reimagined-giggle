1ab12eddc538a0299c1baf4ca8d5dfb93d5907eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:08:01 +0000

    dare talkinto recover dear underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def set_array_item(array, i, item):
+        array[i] = item
