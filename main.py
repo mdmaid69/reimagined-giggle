175d6a52bc6d5fcecc9a456d4474b14d40a4fc42
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:26 +0000

    seriously his information talkinto giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
