6d7cddbb61b4dd79d3037780a70d8c27d6e516f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:15:28 +0000

    avoid call land spread possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
