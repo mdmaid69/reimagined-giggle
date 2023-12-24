b3e79950e9e1d6b757ffed90fb0982b2e90a8833
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:03 +0000

    trust lay hope trip sharp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
