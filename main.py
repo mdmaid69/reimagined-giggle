986d537ac8f69222e12965786bbce051b199bb2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:55 +0000

    since instead cough drink pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
