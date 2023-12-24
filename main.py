a77bec0c738c8837d8015d81a6b83c01b2b8c993
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:39:02 +0000

    letter delicate include ago ashamed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
