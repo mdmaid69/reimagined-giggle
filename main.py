8ce3ca75ec9bce4d8b5e18bd12d7550043f1fe68
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:58 +0000

    please draw Vv rush cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def subtract_numbers(x, y):
+        return x - y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
