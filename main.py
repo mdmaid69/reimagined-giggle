eee38eea7903c2891edb495bbd75eafc018d161a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:52 +0000

    first accidentally war complete cinema

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
