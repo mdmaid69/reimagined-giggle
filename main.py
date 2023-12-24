728b7c0c6aea0a6bd6adddc5579eca866ac0aa97
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:11:09 +0000

    honest pronounce lock hope damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
