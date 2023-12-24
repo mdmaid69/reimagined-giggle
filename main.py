5abe30d61df97e5bc9100423567d265b73c4d785
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:20 +0000

    share border blame track danger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
