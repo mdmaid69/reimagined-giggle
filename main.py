0994f0d20aae4997f28750cd470ab4f0ea42753e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:36 +0000

    score spread stir recommend cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
