3dbea7e433523419ef88400a6bd5d88ba39c8385
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:32 +0000

    somebody supply proper permission lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
