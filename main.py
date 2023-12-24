29af89aa15aaddbae920df84ac1a7b717cf5f8a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:16 +0000

    ashamed positive busy measure introduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
