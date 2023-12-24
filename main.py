673476a7de2be98be975df12a3a9db7d2fbeff94
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:13 +0000

    trap such slight flow matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_frozenset(array):
+        return frozenset(array)
