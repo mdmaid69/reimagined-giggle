49bcf56d13c01a9044abc79a693fc7ed7a01fc4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:23 +0000

    cover contrast do describe obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_typecode(array):
+        return array.typecode
