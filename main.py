dfa02ad9c74c7e917a58c3135631b9255cf76f8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:17 +0000

    city threaten enthusiastic force joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
