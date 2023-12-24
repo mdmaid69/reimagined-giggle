1020c80111a0b56898aa4436f59db176ad5af1a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:59:44 +0000

    in roof formal his sing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
