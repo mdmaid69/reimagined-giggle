c3b33dba43c615a137b626bd40a82d86ee5d4eac
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:56 +0000

    shoulder lesson forgive comein colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
