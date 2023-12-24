77d7d0e181fb239ebe864232286a094a1d5a2759
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:51 +0000

    hungry sign effect dance private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
