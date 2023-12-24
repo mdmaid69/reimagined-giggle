dde1d7451a50b4d2d2cda1de135c521699d5b5a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:38:36 +0000

    equal string interrupt emergency bang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
