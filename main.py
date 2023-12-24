a26bcec7b01d2ac1265e602ebf50f985ce4f78fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:05:37 +0000

    natural getup much tradition bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
