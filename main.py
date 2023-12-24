3a17466adff1f4532b32a4e0e1ddf93e149f4dd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:01 +0000

    knock lucky feeling knowledge lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
