211351e9ea4fc2792f7e33a8cc7d04b240049f2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:34 +0000

    correction bringdown shopping cover accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
