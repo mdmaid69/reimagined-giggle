f5c9423d556db84479e3abc60d198b4e69cd906e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:38 +0000

    exact right send count holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def convert_to_octal(n):
+        return oct(n)
