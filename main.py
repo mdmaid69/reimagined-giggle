94727f45869896fb1838f905ce1da26889b4a7ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:38 +0000

    mess poor rule actually female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
