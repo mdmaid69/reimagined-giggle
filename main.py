52d0cb51fcb07b8b38019c5101e1cf84219c9653
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:56 +0000

    waste than visit harm interruption

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
