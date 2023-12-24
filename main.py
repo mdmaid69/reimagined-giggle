4a33e0596aef56d1af739e913ca4d11e9e622b1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:53 +0000

    suppose tape discussion drink money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
