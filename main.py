9be8895161a1f442c802938f782d3c584e4c7b8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:27 +0000

    hard many careless thank lay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
