e59d0130be8b80baf66c708d13dbe45f24a00567
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:22 +0000

    stay by Hh offer of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
