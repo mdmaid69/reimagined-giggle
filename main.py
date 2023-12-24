4053ea55cc7288bee7922fc087fff6be4fd8a86a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:25 +0000

    visit ball round turn dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
