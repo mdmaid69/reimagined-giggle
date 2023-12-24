c3de010aa8a74720c7ed8f8480a5c1fd42f805cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:41 +0000

    autumn thorough please eager tip

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def count_characters(sentence):
+        return len(sentence)
