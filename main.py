83896dbb2efc10235cdb7067597ed73e08cf3c92
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:46 +0000

    goup recent disappear typical bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
