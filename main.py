d2794664977f3f1db23205879d23b6b21fc53ee4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:56 +0000

    black fear shade mysterious thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+print(math.pi)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
