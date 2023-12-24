0f42a8b98ba894122c4a5c506e0c8c2084f4180a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:38:48 +0000

    reason authority brush tidy show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_counter():
-        return collections.Counter()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
