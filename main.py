87852ddb61d53db0bcace7da44a6b819462d8a92
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:54:20 +0000

    careless breakthrough cut custom branch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import platform
+def get_python_version():
+        return platform.python_version()
