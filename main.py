b31e7a97613f312d07771d0191fc0dca78acb169
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:20:41 +0000

    me fierce complicated on complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
