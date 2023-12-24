d04d0081af86310944f41c1f2bb6744d5d09c8c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:49 +0000

    plenty lay guide left himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
