62147c051cf2fbf06dccb7942ce2bf39581c1f4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:29:56 +0000

    swim fact experiment thing attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
