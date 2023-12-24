ebd1a0fd48ec49511777b89e66875c301d1fa31d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:19 +0000

    willing unlikely mine mark professional

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
