61feabae8d4815e311c39ff31d91a5d84a7d9e2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:47 +0000

    stiff operation invite safe back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def square_number(x):
-        return x**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
