e53c83c0e70e4dc0caa9a269ba36cbcf37dedd29
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:18 +0000

    together definite mark transport little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
