08ca3fa08209aace2e5e85eb26ede4e3d3ba1009
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:12 +0000

    herself generous borrow yours organize

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
