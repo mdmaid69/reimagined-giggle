c16430be4290363d77287001cac470b9685f5223
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:04 +0000

    extraordinary Itself yet fix spring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
