1ba4c3aa655674702d790c9a74228671ad0eadca
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:05:46 +0000

    birth comeround getoff couple deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def sort_list(lst):
-        return sorted(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def greet(name):
+        print(f"Hello, {name}!")
