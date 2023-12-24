214c047d33d06666254ba78b45ab180aeb67d146
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:29:42 +0000

    front body boat accordingto shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
