6765681d20e34c41f9f40bed2ff97a7dab71b887
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:16 +0000

    this Cc hour book temperature

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
