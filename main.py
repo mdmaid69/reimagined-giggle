b2efa88ec5dc1ddf5c41f4399501ea745af8ccd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:05 +0000

    tidy first know mail study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
