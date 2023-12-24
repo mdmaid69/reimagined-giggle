f98aa945ed753841eb0f29e4a732ff7b20fed380
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:48 +0000

    bank trust row trouble thought

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
