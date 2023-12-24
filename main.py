df5aa5c8a3e7a565f04b01022e6ba4e418748e9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:24:48 +0000

    surprise communicate use fault workout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
