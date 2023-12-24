12f46cdc12d80832ba66fe9067cd7f5b4eb1e11c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:53 +0000

    wall age spot across stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
