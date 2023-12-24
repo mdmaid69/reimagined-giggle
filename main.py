e7bc5e283afa79453a1d6aedcf6bfd48bd35ccd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:21 +0000

    sister weekend too petrol female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
