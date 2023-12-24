995d13eb4c1ee982f1d9cad592d1944bc590faba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:17 +0000

    earn consult petrol straight table

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
