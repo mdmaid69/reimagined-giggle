ccc4666a2dc8684641f6ecf3cc25bc87b263fbc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:43 +0000

    freedom neither figure bend neat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
