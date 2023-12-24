b864cf4063f50b21a9c79589fb68a3e5c6657a59
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:56:01 +0000

    hollow stage laugh will forgive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_power(work, time):
-        return work / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
