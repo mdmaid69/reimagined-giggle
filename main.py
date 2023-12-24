b1cddddd22ce509cfd48a1f9e88ce1de83ea2791
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:30 +0000

    attitude me organization plastic ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
