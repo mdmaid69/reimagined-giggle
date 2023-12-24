3d20d13f61d50d35edb451c73638ebe99f070eef
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:57 +0000

    either school dear workon careful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import numpy as np
+print(np.array([1, 2, 3]))
