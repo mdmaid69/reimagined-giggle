1585a347c61f0bd9935e73c071f8f20fedc67fde
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:51:53 +0000

    knock slow emotion low interfere

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
