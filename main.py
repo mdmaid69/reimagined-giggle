4903dbf178164d2e74e2d98c5810405c0ec9a1f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:37:47 +0000

    point reduce boat in exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
