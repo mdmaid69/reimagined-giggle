8f5c115728ce9b0b417b77912bd68155377efd12
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:02 +0000

    suck offer practical mean push

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
 import heapq
 def pop_from_heap(heap):
         return heapq.heappop(heap)
