5b5976bfabd2ab7ac51b443c27503fa0fbed8d8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:53 +0000

    failure call whistle disturbing official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
