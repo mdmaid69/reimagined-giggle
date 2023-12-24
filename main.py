6abb17e04f2920c18157eac11d18e7cf46075069
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:38 +0000

    so cope cross cover equal

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def convert_to_hex(n):
+        return hex(n)
+import math
+print(math.pi)
