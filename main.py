224f2ef2416580cec12c68a2d87a95e61c4d2cd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:18 +0000

    raise might board callat remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
