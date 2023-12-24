5ea3082dce375db21aa33e7dc11b8e416899f0cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:00 +0000

    where tragedy wing could clue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import os
-print(os.getcwd())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
