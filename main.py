262db3e24e71b2dcd4eb0ac4ccb38662ffa82cb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:28:43 +0000

    certainly event disgusting maybe breakin

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def convert_to_binary(n):
-        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
