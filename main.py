f37faec84c80c777f188447cabbe9bd2b5f026ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:56 +0000

    hit only without beginning my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
