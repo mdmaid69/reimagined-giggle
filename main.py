2470e97a63c60624803e12854176b000ac0cd17f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:17 +0000

    handle really consult comfort whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
