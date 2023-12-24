ad5772be12f915e3bad4e0bb279c09f7f0b03d05
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:20 +0000

    begin nurse curious below factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
