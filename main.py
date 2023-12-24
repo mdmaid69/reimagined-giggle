40c81ad3a98cfe1f101bb625354de7a89adb642f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:12 +0000

    past workout lesson tired cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
