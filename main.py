039755aff3b87a38ae1493943aa5fd13e3dfdd28
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:04 +0000

    not dead afterwards shake involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
