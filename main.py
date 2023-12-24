dcf731ece26ae5d42be8e34e879a597dfc50bea2
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:56 +0000

    thick terrible mad gap further

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
