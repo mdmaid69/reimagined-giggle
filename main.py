17c8e489cedfe0b397f0f2c1028401994711a138
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:42 +0000

    various case 568energy tongue negative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
