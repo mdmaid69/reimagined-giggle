e83bc0b8502e1762c87aa0c1c7740e2fd804223d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:36 +0000

    belong yours defend Tt breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
