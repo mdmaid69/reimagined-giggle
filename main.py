8b623de3ed1bf6c0fd42b94ef291eb851b30a1ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:02 +0000

    couple load project offer throughout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
