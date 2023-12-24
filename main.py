28c32a86a7c5a9d4f406fe07fb71bba6e86464e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:14:49 +0000

    please night makeinto uponon recover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
