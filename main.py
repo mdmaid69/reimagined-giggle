6578012ffe25bd74a682cee5d7a9774158d47d7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:03 +0000

    wake free 569enjoy comment beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
