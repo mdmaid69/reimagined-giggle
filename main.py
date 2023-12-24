e3ff3ba06d60bf004f59d690180bf517ef8eccc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:04:57 +0000

    heart stupid reserve city stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
