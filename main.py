3b7d29da16a7c60bc1c3e840508721eaef8a2806
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:41 +0000

    nice rush benefit intelligent creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
