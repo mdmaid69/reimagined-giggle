4cb8cd791ed66bcbe4c47a978d08ff3cade4e13a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:47 +0000

    delight head polite may patience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
