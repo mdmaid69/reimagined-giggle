4f5001041f450ce56e4956c019063e9da9171f96
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:54:21 +0000

    secret basically of few mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
