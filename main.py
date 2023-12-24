ccfbf50fa1527b063325ddc25c5eedacb8f7313b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:41 +0000

    certain swimming regularly medium address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
