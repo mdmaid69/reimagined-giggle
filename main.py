87687687aeaa662a732ac3de9cfdbb29ea5ec884
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:19 +0000

    relax everywhere snow during used

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
