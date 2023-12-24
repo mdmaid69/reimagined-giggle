438fdbd22232b490721bfe389a2fb72290b875e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:53 +0000

    danger protest arrive file sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
