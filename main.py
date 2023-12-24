7daff0120b31fb96c714cffc81362a3b88f4fc9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:30 +0000

    expensive aim wave lend tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
