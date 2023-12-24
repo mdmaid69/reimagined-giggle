e74ac13acd3d1759231597c40a823896b56b2c5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:03 +0000

    soon tie object manage Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
