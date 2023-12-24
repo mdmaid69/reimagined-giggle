100d5678816c7d4991d12df2325bd81987336460
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:45 +0000

    fire fact only turnoff cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
