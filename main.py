95f14edaa703393b405712d1a88659f084a6eb3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:40 +0000

    respect rich normal large jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
