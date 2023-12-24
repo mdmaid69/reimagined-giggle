93d1c72ff3774ef5cdd12c1437bb3389c5e30d53
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:24 +0000

    talk well open recover escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def reverse_list(lst):
-        return lst[::-1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
