48cc323d097d097859470fc0829ced9055652955
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:23:11 +0000

    square switch favour well side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
