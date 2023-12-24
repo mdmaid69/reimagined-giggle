898455da374558467cc9a06832d0cacdb976dbe0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:48 +0000

    standard that lead score report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
