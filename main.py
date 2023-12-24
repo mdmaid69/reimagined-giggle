1a7b05507021df514daf2b5795d901b686f7b987
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:03 +0000

    bag summer attitude enthusiastic love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
