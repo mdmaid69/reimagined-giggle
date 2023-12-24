6407f9e7420c38e1b949ead1755925768b4d674e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:44 +0000

    treat fit store length fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
