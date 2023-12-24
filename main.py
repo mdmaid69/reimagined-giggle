093676523502e5948d2fde3cb33b9c2a9d07e843
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:38 +0000

    prove please holdon run rest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
