6337d91df9d733efd100abfeefe79bf50524b0a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:42 +0000

    Dd want comeback short survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
+import platform
+def get_python_version():
+        return platform.python_version()
