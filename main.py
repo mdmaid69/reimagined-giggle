95b795740d7a2de6a2b74fe15afa2c6c20d09b2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:06 +0000

    fly cut shape wear light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def exit_program():
+        sys.exit()
