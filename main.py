a44c0096f23bb9be7efeed6e569be3f83ee5a336
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:09 +0000

    least spot once spring eat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
