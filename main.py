55a1b34f789350448878e793c12b88cc66b9f5cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:00 +0000

    hope though birth respect effort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
