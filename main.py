089782d73d0f30d620c768fe4c932cbf050331a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:47 +0000

    future aim spare study stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
