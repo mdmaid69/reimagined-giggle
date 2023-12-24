f5aece2c1d0d2de8e26cb35d7bff7ab6d0323911
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:00 +0000

    illness false confusing popular herself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
