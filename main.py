8892059c5a6f19a66a6b6f3b3acc26219cff18b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:17 +0000

    when ill operation prize near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
