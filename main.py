10124ab53d78ba0c6198ccfdd8d97412fc00f158
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:34 +0000

    spill bringback ever pretty breathe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
