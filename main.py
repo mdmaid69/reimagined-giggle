37e02f6ef116792daac6464c67d107c3fd977e7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:56 +0000

    feel lower grow precise hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
