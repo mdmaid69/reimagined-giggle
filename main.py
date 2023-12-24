83cb8690a1caf1e876a8c057be96dcf46bf0a30d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:45 +0000

    treat steal hurt across happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_current_directory():
+        return os.getcwd()
