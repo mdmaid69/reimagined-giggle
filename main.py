d1a51f18f7bf333178211d85af8f17bba8e52d2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:44 +0000

    satisfied holdup before stupid punishment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
