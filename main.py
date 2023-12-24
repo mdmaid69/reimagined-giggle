82276072374ad79f81d033a12e523093da9850fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:30 +0000

    tread rank regard wash instant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
