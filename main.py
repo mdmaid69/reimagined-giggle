2d75d0fac1759a3fc67a0859ef2c35822f67dd17
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:41 +0000

    excuse frightened table congratulations trousers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
