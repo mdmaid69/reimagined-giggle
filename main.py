f7b6162718e1b8d16d624550f3b1a174c3d2b23f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:15:22 +0000

    gate community afford game teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def reverse_array(array):
-        array.reverse()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
