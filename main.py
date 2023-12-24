399646507f2592db80b50090185c9d72546632cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:17 +0000

    correct them last off treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
