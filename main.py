ba09a18c26fc708edbc9d150c17d767b98a0bea9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:11 +0000

    cause shelter store breathe open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
