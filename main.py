6c412f5ff0898154a458339104770e2a821e885c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:46 +0000

    heart delicate interfere nor downstairs

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
