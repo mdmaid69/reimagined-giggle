b8ed08ab29d7b749fcdea915f9849227993c7a76
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:01 +0000

    difference over tap sweet content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
