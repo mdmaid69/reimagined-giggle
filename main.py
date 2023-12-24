8bf5ddca11893c78b7a056e1f415f82cd05ad03e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:13 +0000

    themselves delicate wheel myself common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
