1bb5a8303cb3d24d90d4c9f82181ec3a547fb38c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:24 +0000

    hot they with piece recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def sort_list(lst):
+        return sorted(lst)
