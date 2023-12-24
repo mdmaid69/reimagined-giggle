0c2b1c1074062aa7e3d2b8959f3e21bd27379dd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:13 +0000

    low Ff sit weigh even

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_min(numbers):
+        return min(numbers)
