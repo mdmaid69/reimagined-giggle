d8d52dd1085a43d7cc53279174357c4cd1e2790b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:21 +0000

    pay dear art hurry brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def greet(name):
-        print(f"Hello, {name}!")
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
