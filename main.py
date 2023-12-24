72070cdd086c0847d3fb26ede7e2cc65d283ac12
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:07 +0000

    appeal awake herself there dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
