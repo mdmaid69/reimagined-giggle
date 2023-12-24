e447aff86fc6982480e69f3205fcc13b290c375f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:19:29 +0000

    outside thick seem right judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
