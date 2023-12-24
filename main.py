c5810ca972c4dbf10262577e0d34f5b1311c2782
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:50:41 +0000

    steam check blame suggest delight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
