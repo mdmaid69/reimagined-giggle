ff67dadf1f31e0745ca12e056739b2d5a735e116
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:34 +0000

    complete label shall Mm feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def delete_file(file_name):
+        os.remove(file_name)
+def multiply_numbers(x, y):
+        return x * y
