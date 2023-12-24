ef9f17e45b0f0f8e3d83ab584208d39eb80af450
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:56 +0000

    marry convincing lock mouth show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
