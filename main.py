e4b3acfd590fdae6a0228349d86012325df34a77
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:21 +0000

    rain into tour accept straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
