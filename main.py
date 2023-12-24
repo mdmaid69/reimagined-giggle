f459c73f64e8c6269115511dfb237a30b6d9c025
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:18 +0000

    string lookback(on) team ball scene

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
