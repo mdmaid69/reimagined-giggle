170b8556ad8e17779761afe1f9448e7b09beacbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:30 +0000

    bus ring stranger colour talk

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-print(time.time())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
