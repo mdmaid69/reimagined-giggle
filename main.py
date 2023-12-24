0ad6ca348ef504a44bf27d89a01a9b34cd1f3bef
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:56:31 +0000

    sentence there run freedom clearly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
