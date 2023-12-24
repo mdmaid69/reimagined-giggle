c7474626409140e68d46e7ed5a49f0abe70f5e85
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:04 +0000

    challenge tall true song development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
