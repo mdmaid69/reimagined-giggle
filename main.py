6138b33e60c5f1220e03bf36d880017ade633d17
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:33 +0000

    imagination how mend brain stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
