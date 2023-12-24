a101a24350c6002a852d4a17e91eec5f6673992f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:04 +0000

    see easy desperate far vote

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import tensorflow as tf
-print(tf.__version__)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
