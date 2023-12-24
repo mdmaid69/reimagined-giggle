c09dafdaf05b1d9ead9c75599918bd158b7c0371
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:54 +0000

    plastic stranger idea calm such

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
 def calculate_circle_circumference(radius):
         return 2 * math.pi * radius
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
