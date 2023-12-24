593656514d14b401dd44c7c81ab213422bc7ef3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:10 +0000

    stone lead instruction check her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+def change_working_directory(path):
+        os.chdir(path)
