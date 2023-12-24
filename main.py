00f2822fbe90056116b64f4350b524384c9eca65
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:56:52 +0000

    away achieve bound school problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_string_from_array(array):
+        return array.tobytes()
