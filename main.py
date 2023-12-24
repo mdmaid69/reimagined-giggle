865ed26d582ca5fa44bab30cbfebd7bc8660b08a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:03 +0000

    rent church underneath damage trust

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
