ebfb38507c1e7af1184d755dd9b3d11dfce1c6c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:56 +0000

    back shut makeinto generally ignore

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def reverse_array(array):
+        array.reverse()
