22730483d103e8cbbe3556cdba18a9ba6552a2ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:58 +0000

    consider earn unite otherwise line

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
