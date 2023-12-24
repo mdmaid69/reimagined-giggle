38baa4e5373e974a3a25a9410704b84b50381299
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:11:42 +0000

    wash immediately confusing favourite sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
