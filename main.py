7d021faacfbb7ca129a6f079629be1e66aec7000
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:41:39 +0000

    Qq successful burn our piece

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
