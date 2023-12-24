6548a866a7e95dae0763abf688767695b95a5e9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:14 +0000

    concern distant instruction swim send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
