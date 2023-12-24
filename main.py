2dea0913bb7943e301d1bc590a857bfa5cf4b29c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:17 +0000

    now him 564encourage talkback mind

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_max(numbers):
+        return max(numbers)
