04fefe14683df1d8e968f2e1ec5c535b6f868b54
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:35 +0000

    nowhere holiday fold everything himself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
