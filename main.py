9457d498d795dfa393d7c06af490a4e1c915e1da
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:35:35 +0000

    mix like spare apply love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
