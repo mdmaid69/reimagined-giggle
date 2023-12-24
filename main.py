145753e56eaa3112ba9e16825cec1960bab74868
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:20:38 +0000

    fork limit available tradition mass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
