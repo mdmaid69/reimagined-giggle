3d482a7bfc6a31742ffb50d3b40f74f590d63021
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:14 +0000

    escape free turnback fortunate slip

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
