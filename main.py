2fc24ce48b726ffac9c8e9d6b1cc65444bdc355a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:32 +0000

    town dry government empty face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_string():
+        return collections.UserString()
