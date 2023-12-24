5a672c922da6759ef5324d0d6f59c36eff441a58
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:57:36 +0000

    direct hello eat plant somehow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def square_number(x):
+        return x**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
