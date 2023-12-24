845797751d3c54f260a9889f1d75e2155b3c8707
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:38:42 +0000

    mother phone swim concentrate word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
