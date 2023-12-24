a89d3e24e45a07191f0c2f22f9a996ed81ea0e3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:33:15 +0000

    dick street familiar callup lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
