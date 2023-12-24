e6ec2cf3e20ef852e764d356333ba168a9743caf
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:30:42 +0000

    protect damage fasten belong below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
