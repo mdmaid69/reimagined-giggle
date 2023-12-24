0972061f379b4cd8cdb168b43082abd27a390997
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:06 +0000

    soft measure introduction slip bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
