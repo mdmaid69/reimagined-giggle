e872df2dcb3982b7b0979fb138a3a94450249ce9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:44:07 +0000

    improvement second power deliberate advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+def exit_program():
+        sys.exit()
