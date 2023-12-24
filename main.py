966d7474985c7b8626e073029b808b73fb7c916e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:30:51 +0000

    measure clear rob perform ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-print(sum(range(10)))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
