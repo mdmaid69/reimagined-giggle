e7297843c1d52a4fcce7d5ce1fee361bc9506cad
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:06 +0000

    slightly allow Qq file hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
