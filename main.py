c77f49340e02fca70bf66828b6fbea5ee4796a89
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:14 +0000

    extra talkoutof waste aim none

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_work(force, distance):
+        return force * distance
