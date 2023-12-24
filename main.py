b66a06f027b15fba1cfdcc3bfab2386bfff2d6d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:54:11 +0000

    OKorokay nor scrape admire point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
