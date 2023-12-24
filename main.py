a0103cf800fd2436d8c69e87a3d3cba33a008421
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:02 +0000

    female drawing her sweet anxious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_power(work, time):
+        return work / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
