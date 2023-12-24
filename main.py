ba786c3d0dc498b4a6e3a90ff2df6d3151d9d02c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:08 +0000

    control otherwise division whom sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
