d1079b94319812c02c799ce92baccd60922baece
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:18:14 +0000

    characteristic road hair equal practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import time
-print(time.time())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
