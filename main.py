27f51d8755a0b1251e991d3993e05596b9c7ed75
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:05 +0000

    entrance plate nearly emphasis production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
