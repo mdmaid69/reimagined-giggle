097daa0a32f4a025b3853aa1d361d52a486ddef1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:45:42 +0000

    also spite wind difficulty join

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+for i in range(10): print(i)
