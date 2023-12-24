f9638992535f03a634766fd11e78c5ec7e43a184
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:37:15 +0000

    give tense damp perhaps 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_complementary_error_function(x):
         return math.erfc(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
