24c1f858a6831ba654df215c36960f8df47b7016
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:31:48 +0000

    its top pen rough else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
