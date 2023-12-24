51fc99ff6a284357955e08772f218e12b650a832
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:28 +0000

    bet peace term attractive building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
