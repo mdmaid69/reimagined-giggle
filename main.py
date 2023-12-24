f4ad98fdbf35c5b5fc071e82ec353ed511bb03e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:59 +0000

    help prove no equal weekend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
