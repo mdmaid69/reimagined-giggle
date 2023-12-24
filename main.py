1befed2b68749d28bc7e1627c9d0856a793b3674
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:17:59 +0000

    until national that onto choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
