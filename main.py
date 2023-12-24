474c3b8e6fd73c5a35cc35f1a69ae289957d2ca0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:03:40 +0000

    tape congratulate frequent flow meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def is_odd(n):
-        return n % 2 != 0
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
