453a068f157f5d0ac7293200962c272cc8928005
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:25:56 +0000

    cruel effect Tt rough fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_acceleration(speed, time):
+        return speed / time
