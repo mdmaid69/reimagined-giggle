cc50bf1649209c4184bd895381b4488cd659a392
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:49:49 +0000

    peaceful judge mouth anyone might

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
