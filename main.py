d9f73738fe23e33cbdf3155053be5d68cf28efb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:45 +0000

    creature flavour thick fly estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
