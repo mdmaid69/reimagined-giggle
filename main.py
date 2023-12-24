598b31d9caf4f0f0ccb5513ab71fc62c86f6e068
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:42 +0000

    embarrassing after boat replace disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
