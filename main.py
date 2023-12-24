07eafe603f0dad393157ef4e3bb18ef4b8a88f87
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:20 +0000

    famous ability machine cut pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def remove_from_array(array, item):
+        array.remove(item)
