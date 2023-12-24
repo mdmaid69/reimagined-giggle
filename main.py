9b13d84c271463c41eda704a7f238cd8351e27f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:42 +0000

    pure tax mistake after payment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
