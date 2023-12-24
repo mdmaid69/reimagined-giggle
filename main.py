418c10b1ce0b01ca43126019da7a3687abd12a45
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:08:27 +0000

    famous whom command involve along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-print("Hello, world!")
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
