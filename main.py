bad30e6d09652055f0630dfbbc09bf8208fa2740
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:51 +0000

    boat hate sharp head giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
