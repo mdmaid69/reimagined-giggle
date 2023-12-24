6f2e1dcb1cf7a4526e220115cda02ac5fce40220
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:49:51 +0000

    thinkthrough dig tap cough getoff

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_item(array, i):
-        return array[i]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
