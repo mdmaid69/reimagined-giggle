cd3cf3e69066ec24ea7a0153e50bb6a977c4fad3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:17 +0000

    pull frame inform communicate mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
