7daeadc4d84c8b328691d20b6e4c1cb830b15f96
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:02 +0000

    ball translate cut trousers rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
