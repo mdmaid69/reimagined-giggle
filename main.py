be22acd6a5373819d04d2726e0c1a5608aeb2621
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:20 +0000

    theirs recent crowded charge cheat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
