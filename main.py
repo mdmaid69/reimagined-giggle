5961f95801199657b892ca78d53d725460d8a781
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:51 +0000

    borrow side pleasant doubt care

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
