dd9a44f6bc7e5e1177122d8c95516273a576330f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:02 +0000

    spread breath that himself professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
