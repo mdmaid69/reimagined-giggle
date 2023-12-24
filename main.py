be41e8bc16a9dbc662498bcf679bf0a8b6ef50a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:43 +0000

    pupil first tool among through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
