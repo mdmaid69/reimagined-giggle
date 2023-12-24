34ead597f285fe1567d261f65fdca7f818f0f3d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:42:28 +0000

    motion answer official stamp force

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
