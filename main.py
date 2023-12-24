c6ca9ee9fc3dc2c5b4f2953553e004830c55b138
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:33 +0000

    Rr neck fly order slight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-print(math.pi)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
