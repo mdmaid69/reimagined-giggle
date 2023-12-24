25a96284e2ac9faf92d4d8ae634d25e8ce337951
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:53 +0000

    low also any conclusion exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_density(mass, volume):
+        return mass / volume
