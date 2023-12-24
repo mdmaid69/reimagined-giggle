a50198aa970488ca67c762c93f119f4f4a2a513a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:50 +0000

    get tour maximum not flat

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(numbers):
+        return max(numbers)
+print([x**2 for x in range(10)])
