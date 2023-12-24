d89f73b5455852edf71c861ed68687d2ddb57442
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:35 +0000

    steady excuse release control interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
