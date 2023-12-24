e70392ba7a228f7c1b06122d47c3cc9d7f5e9d27
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:23 +0000

    laugh pair bet description plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
