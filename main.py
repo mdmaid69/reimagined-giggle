51d3555d4bf827e1a505dac79cb1be1565cc4547
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:42:00 +0000

    live declare plenty environment poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_list_from_array(array):
+        return array.tolist()
