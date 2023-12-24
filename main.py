a498b7b9e4a2c25d693011f02311e4af56e97a69
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:05 +0000

    either store or separate copy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
