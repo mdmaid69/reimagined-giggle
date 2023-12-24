d32c0ab96558df7f7f13f61bb4e27956af7e8c1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:31 +0000

    characteristic holdup regular dream nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
