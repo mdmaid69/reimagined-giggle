7c117579c1aab098ab13d739b801cf2e8acb6bde
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:09 +0000

    complaint same system disappointed loan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_max(lst):
+        return max(lst)
