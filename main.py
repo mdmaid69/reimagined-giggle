ffc68c75c2ce5508911a4ae78fd31342b746e4bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:43:52 +0000

    let attractive school design estimate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+print(datetime.datetime.now())
