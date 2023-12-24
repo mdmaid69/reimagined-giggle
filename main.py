f264a6babd68858585166f446ee5b5e0636aa453
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:12 +0000

    high principle partner rob pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
