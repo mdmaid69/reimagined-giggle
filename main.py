0f98392bfde0bf40502047a6c90976fc7968539e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:58 +0000

    clothes actual us nor pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
