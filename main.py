c114c0d9f8312bb22a6675307603c06a8ea830f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:37:54 +0000

    up always class survive copy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
