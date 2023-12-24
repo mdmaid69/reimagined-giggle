37474b8224981fb4d90c41f7f3cd4c2302c50977
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:10 +0000

    argue stick conduct reason quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
