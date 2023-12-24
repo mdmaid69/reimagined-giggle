c8193841a0a38f6ec5f4cd8e45b4847cef15fca9
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:44 +0000

    command keepfrom keepfrom rescue instrument

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
