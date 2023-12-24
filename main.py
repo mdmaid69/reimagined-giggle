86bcbf7359c01a97062fb852187090468459fe64
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:51 +0000

    quit invitation properly takeout treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import random
-  def generate_random_number(start, end):
+import random
+def generate_random_number(start, end):
         return random.randint(start, end)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
