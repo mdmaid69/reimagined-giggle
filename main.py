fa4e2fc99dbb5757afdd6aa18fc8e8a0e000949e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:28:16 +0000

    limit innocent bottom obvious back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
