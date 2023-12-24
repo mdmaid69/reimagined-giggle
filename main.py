ee24dff169be0fc4356e5e156bb69140b5fc6447
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:26 +0000

    level pair opponent begin replace

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
