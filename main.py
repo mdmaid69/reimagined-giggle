887e3ab864920511c5839698a0571df747fd6494
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:08 +0000

    none public think force loose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+name = "Python"
+print("Hello,", name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
