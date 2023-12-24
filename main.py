94ddbe2d8dd4fe599184e102d31e6630cbf11d57
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:33:51 +0000

    pocket lookaround capital shoulder cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
