fe65434d1d64d20d26c14a9268e0026a9321de86
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:14 +0000

    formal home even agreement mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def convert_to_binary(n):
-        return bin(n)
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
