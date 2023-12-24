a4af9ffc7ff780db5587129a3e0e07c2fb3d383e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:42:18 +0000

    fortunate handle store scene store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
