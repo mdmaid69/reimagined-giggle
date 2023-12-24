dc0b218058ac1947a7cc577aabcc6188e54fe9ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:33 +0000

    fair this workout class completely

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-for i in range(10): print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
