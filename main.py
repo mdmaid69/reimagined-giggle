a8f87968c38e7edc7a173701cfef282572ef5dd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:06 +0000

    maintain pressure young of taste

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
