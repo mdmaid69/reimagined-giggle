ebcd0945369d0be3a1352a48a17885e02d2d05f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:43 +0000

    please turnover bound talk difficulty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
