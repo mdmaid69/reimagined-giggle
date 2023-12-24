bfd35b257085f2e5165e96683c815707b78cab6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:37 +0000

    afterwards lookup reserve fast regional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
