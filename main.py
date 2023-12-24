e419fa1596b9b9257214524040fe73e6981f47d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:54 +0000

    accommodation belong the correct honest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
