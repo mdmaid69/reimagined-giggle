02837469ae39c20cac0c54f2e2a9c973b056c4b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:40 +0000

    task remain who clearly face

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
