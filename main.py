1bcf1135080dfe157918aa58b52aa18c28d02192
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:06 +0000

    share shop number suckme luck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
