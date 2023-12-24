9d6b515333cd1497c6b470d4714b7e157191bb25
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:39 +0000

    loss steep float somewhere pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
