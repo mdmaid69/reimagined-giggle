d694bf3e4b8cc150995552d413c18c9fe8b9b019
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:40:21 +0000

    problem split add habit so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
