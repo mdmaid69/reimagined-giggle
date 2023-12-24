b5b3e12deb61016ceb4927486766e2a49fa4759b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:28 +0000

    voice pay tip relationship level

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
