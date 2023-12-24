34565b1d08c0cee32a75b1276e13a38ffecd5355
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:02:17 +0000

    maximum central miss smoke march

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_work(force, distance):
+        return force * distance
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
