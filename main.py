a477e2dbad1db93155fcc4a990c18a1f742ac001
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:00:19 +0000

    Cc wipe insist accidental workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
