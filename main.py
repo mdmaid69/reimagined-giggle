62b3a3a2e5cb528b7013f4568c50aec8d43b175f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:18 +0000

    tonight waste solve funny off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
