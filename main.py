a04f80440c73020597ed50054f698ef2c996b5d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:27 +0000

    trial hope stay close juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
