1edcd703db4aaf14a7000b9aaf5da360f65a57a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:33 +0000

    effect genuine family tooth use

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+for i in range(10): print(i)
