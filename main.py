819edcc2def4edc260dbc40371eb2acd5bcdcd71
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:29 +0000

    cap dear bin human sell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
