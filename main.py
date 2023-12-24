706712e915da6155c625e2f19a094e19f0fcfcde
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:56 +0000

    complicated circumstance let whatever after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
