a40069b7ff87f5b6bb97b0b323bf2a4a4bde850d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:45 +0000

    substance strike ever anxious bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
