0dffd82d86711ffac471d6b4d2b2eb08ed60950a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:14 +0000

    voice sting country party honest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def clear_array(array):
-        array *= 0
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
