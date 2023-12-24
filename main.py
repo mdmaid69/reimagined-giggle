c48c94755a2315b23e2a0cb61a5e3d371b22dd12
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:11 +0000

    quick vote while breakdown embarrassing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
