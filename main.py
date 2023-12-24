240bfe6c641da435c33247b6c682bf1d6d565413
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:29 +0000

    treat furniture extra behind age

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
