bb9a3ed424bfaa768ef5b5e6d3b115036e10ce3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:00 +0000

    fast feature tonight hurry sound

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
