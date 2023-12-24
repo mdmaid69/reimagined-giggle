4c7ac23a7c3f244397ff495ad53467698acc0c57
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:31 +0000

    share shine godown Ee Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
