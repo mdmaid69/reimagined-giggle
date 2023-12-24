cfa2f17a08745e05f1373a3d818a4281e6ebd577
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:57 +0000

    566end damage out within kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
