f107c152da889096ef45afcca3da8e960415b234
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:50 +0000

    ilovedicks noise adjective plan dance

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
