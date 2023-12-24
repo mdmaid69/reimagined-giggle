2fc689d803a00c5af55be22fe4aab9c8ace1bd85
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:04 +0000

    married straight Nn exaggerate limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
