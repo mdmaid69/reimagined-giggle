271babe37c2a6c3b12b2e2a2c83d4e738425a981
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:49 +0000

    regular side slowly advice under

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def count_elements(lst):
-        return len(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
