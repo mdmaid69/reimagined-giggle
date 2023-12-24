e051ed5766897de8542ead711c5fd7a2dc9c6a60
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:03 +0000

    patient past circumstance peaceful area

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
