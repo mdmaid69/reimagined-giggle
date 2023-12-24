9cf529c75b34ea13ab1fbd820197f06543f81f86
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:27 +0000

    absolutely serve estimate getup miss

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
