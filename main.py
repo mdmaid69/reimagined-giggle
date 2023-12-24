f0526258ff998937944a1a0a87885d4773deff4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:00 +0000

    poison meet flow complicated view

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
