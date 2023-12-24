c4880102c1f4cff7f68c4f81a0b22e5f46fcc86e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:16:02 +0000

    cheat patience remove dark Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
