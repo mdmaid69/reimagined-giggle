5a27eda67de977b28f0318802c35ae431c1905fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:12 +0000

    turnup character ground point ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
