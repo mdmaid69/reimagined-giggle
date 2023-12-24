424cbde0d1eac75e46c8eea3f45e6d262d444b88
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:56 +0000

    lookup each bus free forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
