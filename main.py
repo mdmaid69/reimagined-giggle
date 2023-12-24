6c90ed778a61fec85431bdad5f0b5daf8d2cb5ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:15 +0000

    settle part spread myself influence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
