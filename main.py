3d87c7218530c76e78ca8188f650142ae653f819
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:19 +0000

    mix giveback issue land schedule

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
