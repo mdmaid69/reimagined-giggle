d0c975c4bbb2324ba97d60828f4c9d64ba4350e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:33 +0000

    intelligent loud shout learn half

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(10): print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def sort_list(lst):
+        return sorted(lst)
