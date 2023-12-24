c2b8d23802166503752ae32c32849e80c2f855af
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:24 +0000

    neither service development temperature my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
