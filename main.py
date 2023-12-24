03a51eada0414ca9ae7a1db28ec93530983e24f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:05 +0000

    under post call learn shop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def cube_number(x):
+        return x**3
