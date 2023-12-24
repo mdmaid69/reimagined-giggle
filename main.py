04391d3115651e32e29a0cd256e9f9a67bd14815
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:56 +0000

    Qq use upstairs patience claim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
