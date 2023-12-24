59fb22e7a20fa6c5ec7c0aa8e0fd0abebe768cba
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:22 +0000

    friend fire convinced anything fix

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
