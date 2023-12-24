1f0fc08da3801146a123107e6bad108940496c2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:06 +0000

    you surface check board quantity

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
