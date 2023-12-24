2168464dc5ca9bd0c31a62297257eaa237b4f7ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:53:48 +0000

    error name threaten swing both

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
