99ab30032d4c22f1e8b519e7950e76d3ad303ee6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:24 +0000

    comein recently everywhere spring home

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_square_root(x):
+        return math.sqrt(x)
