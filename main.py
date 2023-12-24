a4661a2f92c6908f8c0f8d957d53afb3c395a781
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:54 +0000

    once beginning aim judge major

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
