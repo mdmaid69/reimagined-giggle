42e304353f39cfa01c2de374f6d5d114c167eaed
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:17 +0000

    him teach mix school ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
