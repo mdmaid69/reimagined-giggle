6da4850dd14e0f1ea8ca128539fbac6c7d932b54
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:53 +0000

    stage limit short damage identify

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-print(random.randint(0, 100))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def multiply_numbers(x, y):
+        return x * y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
