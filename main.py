f219df226cc7cde90f435490330e1f175ef75a60
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:41 +0000

    tune more sleep indeed union

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
