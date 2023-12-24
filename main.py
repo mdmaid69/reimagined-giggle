4e6bebb063d17e8d2a896ad96e8aa9a0f328ef8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:31:03 +0000

    actually play legal guard bill

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
