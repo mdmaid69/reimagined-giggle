d5041ab3b6238e2dd8a58fde728cf47ab6698857
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:37:28 +0000

    strict experiment season education state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
