33ad7d423a740d36dfa4b96b27d1c8c533307947
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:03 +0000

    programme mistake limit runinto evening

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
