844bb2a63aa9159ce202d49e36985ab1c546fe87
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:49 +0000

    final bath none innocent claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
