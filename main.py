796b393e399b5f331472b017a899c3d1ff562a0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:48 +0000

    recovery regret miss and breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
