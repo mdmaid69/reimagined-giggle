d74c8cce63a02c8c2a0ce9572fc14dbff8489613
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:20:52 +0000

    usually surprised piece phone intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
