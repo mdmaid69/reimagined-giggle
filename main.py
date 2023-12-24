13e2806bc1468957de6a9eb3131ec033414c91dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:28:26 +0000

    seem represent after organization instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
