356648b4956e65abceaf0135cdf03d6ec7f39cf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:56 +0000

    runover once never interesting schedule

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
