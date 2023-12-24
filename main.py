d0e750eb7c9bbafbedc91183f19a51fb3e1bc0f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:23 +0000

    refuse die achievement difficulty shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
