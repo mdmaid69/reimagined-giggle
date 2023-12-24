96b2f4cebfd42ffeed06ceb566a8426d46126d91
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:13 +0000

    tidy around frighten schedule teach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
