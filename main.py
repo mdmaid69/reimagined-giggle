8ae2375ff75f410288a126e440e5057bec3699e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:15 +0000

    year slow die grand priority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
