9caf6efae042758d3b197228f288be7a685912a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:36 +0000

    star turnover law admire food

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
