efe84282488fd71c7453da51bcdd5dcc964df2c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:06 +0000

    calloff common noisy forgive sweep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+print(re.match("h.*o", "hello world"))
