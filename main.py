e64b5182bfe88c63286099b07e979d173cdb9e83
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:51:01 +0000

    date turn journey today communicate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_binary(n):
+        return bin(n)
