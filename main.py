6d36bb6c84afa4b5f92bb8600bbb9d79de672f68
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:20:28 +0000

    coat see regret lock truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_bytes_from_array(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
