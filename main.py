f6497eb54204e6200ccb1a50f24b6f52e0e69ffb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:01 +0000

    bad last height stage once

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
