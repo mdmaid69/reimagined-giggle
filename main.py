4ced98bdef52b16581b454d783563a411aafaf6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:51:43 +0000

    waste impressive frame swim scream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
