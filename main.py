f4dd78c83527e8d279f028eb7bc0d2082eda5803
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:09 +0000

    address undo perform exaggerate model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
