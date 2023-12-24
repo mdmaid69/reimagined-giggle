cffa029dfdf2d0968016dc71aa5aae6300308ad4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:21 +0000

    cheek summer attention Pp holiday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
