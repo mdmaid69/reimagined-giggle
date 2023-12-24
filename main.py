b8e46087fd384970031494e936f746e65dd6fada
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:49 +0000

    tidy wind petrol payment priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
