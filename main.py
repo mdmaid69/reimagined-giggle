ec2f55b61af5eb0ec83f2a6111dcf736a1273938
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:36 +0000

    pass someone old along breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def set_array_item(array, i, item):
+        array[i] = item
