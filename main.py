12ace733050b78ab5e4cd06eba1564fa744bda4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:14 +0000

    immediately foreign giveup harm show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
