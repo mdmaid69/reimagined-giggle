cca267aaaa225796ad3012b55dcc2a8ac41b3a4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:18 +0000

    quit cheat cope beginning disappear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
