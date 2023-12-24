5eebedd4d096a219c72f5ea85244cdfde9e1e2e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:22 +0000

    stand loan block square range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_floor(x):
+        return math.floor(x)
