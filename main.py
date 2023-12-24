60d852718397e4170cbfda62cdd3b065aa2cfb2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:25:26 +0000

    experiment believe somehow decide boil

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
