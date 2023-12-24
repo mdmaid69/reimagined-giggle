02a5546f902775f7066a0ba0163a8100dbfc2971
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:04 +0000

    market blow write professional cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
