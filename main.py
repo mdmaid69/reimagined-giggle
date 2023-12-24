36cc3c83f41c8ae676e5817b4da7d041c8a105f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:39:54 +0000

    own difficulty answer rely disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
