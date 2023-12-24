879c3d16745885f4d5411e17b283f4a0476480bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:33 +0000

    may charge trap private trial

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
