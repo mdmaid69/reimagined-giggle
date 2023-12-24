aff366b63d9475f5201cd857909950cbab2f9216
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:33 +0000

    sign pool along treat clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
