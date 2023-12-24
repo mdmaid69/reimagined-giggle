2dc1bee303f996943658f89f5b623b5511832a24
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:02:09 +0000

    kick middle 572enter Nn heavy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
