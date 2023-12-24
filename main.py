5be6525a196c48626d22dc432d253fed72c1a34f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:36 +0000

    purpose swim unable anything ought

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
