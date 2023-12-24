30dc85ffc3a9e551271304488da1282e58926cdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:59:14 +0000

    early hard offer hardly raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
