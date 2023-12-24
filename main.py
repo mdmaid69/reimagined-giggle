cdd8e8f8d85b5652d78312fa13caec7d9c18c0eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:09 +0000

    file door goodbye press sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
