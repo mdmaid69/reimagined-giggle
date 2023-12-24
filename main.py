735bf43830912e709d3657e3b50624621a97733e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:29 +0000

    about stretch say public introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import time
+print(time.time())
