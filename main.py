4033a4f605fd5382e9c5727aca2de9e77481de45
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:38:03 +0000

    geton building sweet decide wing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-print("Hello, world!")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
