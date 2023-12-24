78fb10a9a09b30d1cfeb7a74efe3b26f68a6987f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:54 +0000

    work card tap than ourselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
