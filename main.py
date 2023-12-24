895e3c689c352bdbc5376e2a3b398e2f809c59d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:20 +0000

    organize section black post much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
