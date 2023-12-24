760fc11c68d13eef275b2466a2088098c5872b8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:17 +0000

    plate people Cc bar swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
