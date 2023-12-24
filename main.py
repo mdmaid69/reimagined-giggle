b59afbe7eed6ff039018e3af3b3988ea65e51eb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:27:01 +0000

    go address ground neither there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
