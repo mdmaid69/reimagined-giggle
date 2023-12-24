512882976af3be28b7e75bf95ede5931c8017111
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:27:21 +0000

    spread tight thank remove rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
