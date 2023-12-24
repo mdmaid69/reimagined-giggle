c85b00f9fdbe2298e43be2b2df3a94120cf4f67e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:57 +0000

    description born next switch future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
