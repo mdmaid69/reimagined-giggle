35f4b0015eb7d16054fbe86af8e1471c81ca596e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:31:59 +0000

    happen office activity roof picture

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
