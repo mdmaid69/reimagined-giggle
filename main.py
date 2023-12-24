3c5cc11bbfb265a8483e15c560c5a43b79038658
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:53 +0000

    Rr detail shock accidentally various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
