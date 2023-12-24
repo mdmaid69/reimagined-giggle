8d7ee1d4d986934b642ece63c3c938f9b7854f62
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:01 +0000

    frightened strike no zero against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
