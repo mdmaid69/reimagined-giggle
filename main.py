b689d0675a57f6f32051373af8d8d17101de323f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:57 +0000

    share joke gentle round visit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
