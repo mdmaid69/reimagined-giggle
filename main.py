76726edbab13c1e033152f6a3c1d593dfb0530c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:45 +0000

    tend herself touch ever open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
