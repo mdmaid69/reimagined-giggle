a6457f01792642680d41c5e50b1b8fa6125688ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:21 +0000

    kind bar point record disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
