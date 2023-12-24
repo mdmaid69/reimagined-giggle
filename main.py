2394d538e8ce800ba8a3c47a3444a58e9fee6a12
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:58 +0000

    would aim separate 573entertainment wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def reverse_array(array):
+        array.reverse()
