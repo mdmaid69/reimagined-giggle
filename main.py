637e69cae9e8d805b6ced4a3e8127fbb4614a68f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:14 +0000

    hurry can work plan scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
