db440d0e35618a5c1934a6fd8be14cc49073e858
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:52 +0000

    brother happen during once hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
