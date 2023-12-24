cf7f48a55a32944b716bf14eefde624fecbd9cac
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:46 +0000

    car mend post squeeze getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
