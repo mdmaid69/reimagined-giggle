c288c85ead603400771554fd23f7b277fc669623
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:36 +0000

    suggestion upstairs promise exit among

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
