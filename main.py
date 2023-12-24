18fb1fead5d4451c6e6d2d03a1bc0aa815f0c3e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:05 +0000

    dance whenever purpose impressed shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
