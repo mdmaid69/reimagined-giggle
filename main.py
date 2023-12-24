35c7d0da9997f992d1464b2842da534be6ad2b01
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:16 +0000

    plant underneath send appear perform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
