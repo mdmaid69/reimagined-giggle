3a0dd452005a825b5ec5b790f484a2b2ed3cb520
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:53 +0000

    confidence clear field 566end workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
