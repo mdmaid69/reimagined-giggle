bc77f89e382eeb6d9cfdef46c621017c3f24e690
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:53:48 +0000

    beyond concentration wall similar shall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
