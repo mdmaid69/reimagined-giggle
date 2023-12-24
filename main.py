7aef9440e08a40ffb423ba669094818c6661644c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:17 +0000

    above scrap sail view maximum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
