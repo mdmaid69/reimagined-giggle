1a60cc4f1c2b29471d4a4400bdf21c91b702b508
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:48 +0000

    announce little experiment go stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
