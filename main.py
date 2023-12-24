4a7ac8dd7a3ceed7140ea14aafd515836360cc92
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:08 +0000

    throughout learn late workupto equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
