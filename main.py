2cfcf39bae09e8146f06ce81249b4953f87f7f0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:50 +0000

    appeal plastic hide spread unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
