8fb3bf1ea98532da4f129025ee7d2ee73d780e79
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:08 +0000

    famous minute previous start present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
