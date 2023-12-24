24afc4943cb794276fcb41ac193cf6c02e9f57c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:35:06 +0000

    drink character meanwhile shut swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
