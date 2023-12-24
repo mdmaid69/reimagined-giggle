dc7608ab831dcadebb182a7b687b1773c2fce86b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:30 +0000

    meaning just turninto other suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
