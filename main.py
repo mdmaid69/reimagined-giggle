10092bfcebb3b1733635264556122397a4925df9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:03 +0000

    Rr balance understand squeeze learn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
