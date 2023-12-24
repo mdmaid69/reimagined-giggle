a20c07bf393b1322f2109a400d5279f9f1de5c1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:55:13 +0000

    pull wrong book win system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
