4ed839b0a64b73037d9b0ba6c912c13cee2e1471
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:59 +0000

    smooth interrupt tap pick mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
