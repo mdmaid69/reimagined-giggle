6fdc5cb6ff8c63d3aff4db95dce0ff39ccf3c17f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:48 +0000

    slice example hit confused learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import random
+print(random.randint(0, 100))
