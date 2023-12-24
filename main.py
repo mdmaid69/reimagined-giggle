c37b2b3c94297afb9d6e9baf5293cdcf6ab4f301
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:54:43 +0000

    such wander no mistake squash

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
