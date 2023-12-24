7ebc117573564f10f52a4dcfd05fb92b96bd00a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:51:45 +0000

    police target gate soak swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def greet(name):
+        print(f"Hello, {name}!")
