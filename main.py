9d06da404789ce9debe246d6f45fdda570c4f696
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:51 +0000

    flood support bring kill kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
