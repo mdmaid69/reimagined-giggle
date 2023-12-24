b2fdb54fac655dffb40d1c1b9b0ee665775a1892
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:48 +0000

    drive rich trade bill lie

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
