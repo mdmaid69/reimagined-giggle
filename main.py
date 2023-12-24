c21a009dbb18996407f0ab724f9f06ab76457b45
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:35:33 +0000

    unless what fairly pile announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
