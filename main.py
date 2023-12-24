8cde4e130c1a728b8b784e2f4b413cec64363a67
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:13 +0000

    sick discuss passenger fetch shut

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
