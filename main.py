9145566f91a89860d18293e3fc9bd10a0476aa1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:57 +0000

    rather different shock sound opponent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def convert_to_hex(n):
-        return hex(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def convert_to_octal(n):
+        return oct(n)
