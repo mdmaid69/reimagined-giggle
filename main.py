b2591e966eaac5294da14b430e189494ade102d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:48:45 +0000

    value more aware ignore enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def count_words(sentence):
+        return len(sentence.split())
