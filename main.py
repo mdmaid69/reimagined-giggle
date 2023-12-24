b5afa548b06ad308c2e9b57a787e57390bcc8615
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:45 +0000

    crowded will correct between enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
