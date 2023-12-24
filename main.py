09cbf85adc22e2f29372b7fa02f0a789fe0eead5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:02 +0000

    artificial basis edge refuse free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
