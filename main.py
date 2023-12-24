2a35fbb3cf6ecab108d5f3a7086eb38d436c5fdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:15 +0000

    earth hole maximum pool school

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
