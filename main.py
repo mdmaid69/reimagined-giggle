2728e6eda6426f60df4d3001b0df2914d168aafb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:13 +0000

    drive house talkoutof group upwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def divide_numbers(x, y):
-        return x / y
+for i in range(10): print(i)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
