fccd1ff457f629ef87a7c702a9679b018bad4253
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:39:22 +0000

    period finally smooth reasonable passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
