02be4273611021721bc1c989ec3c574d2c291cad
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:08:07 +0000

    trip news touch rain spray

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
