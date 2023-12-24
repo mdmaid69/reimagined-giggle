f7834b98ffcd3616b2934246270d774d6566bcb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:19:16 +0000

    pencil responsible medium bag basis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
