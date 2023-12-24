039635a9fa5425eb1eda362628c0aaf74e7b7074
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:13 +0000

    tape sing rather slice splash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def count_elements(lst):
-        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
