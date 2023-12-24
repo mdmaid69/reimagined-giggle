490588212bfff63f89f32a0e69f4c577502a356b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:30 +0000

    recover succeed plain immediate grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
