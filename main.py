a01679b2ff3c56b4f338e170db4a0b2bd6818800
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:06 +0000

    matter work inside sight scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
