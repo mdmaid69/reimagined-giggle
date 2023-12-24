790117b1833ff80ad6f33b3ed7247ef2962d2f62
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:59 +0000

    will but range taste compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
