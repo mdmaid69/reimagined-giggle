fd52846b86400603aaeae55ac53b906666724db2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:39 +0000

    level rent sure train power

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
