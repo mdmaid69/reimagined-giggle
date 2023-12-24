25e24d636af9f894592dbc90cb5252ea6558e5f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:37:03 +0000

    drink prepare like thin accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
