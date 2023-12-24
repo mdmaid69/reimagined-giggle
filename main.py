cc038c6dedc5ae708d9397cec4f7b96aba114b86
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:13 +0000

    past rush store matter partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
