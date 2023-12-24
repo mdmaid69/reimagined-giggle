6cd948dc981b54a5bc6a229095f49441038e7348
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:38:52 +0000

    seat borrow study exercise split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
