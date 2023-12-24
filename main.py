bdde7e72eb792a52368cc9a5436144d10bcb055c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:59:38 +0000

    thought responsibility wish keepaway along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
