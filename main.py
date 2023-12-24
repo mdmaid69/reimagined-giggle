694c53971c99799d5bda5381f84f5379d48aea06
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:58:47 +0000

    supply classroom also pass expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
