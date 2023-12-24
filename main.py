5e6fb3e641393120a12c7fbe967f91fee60bd776
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:54 +0000

    block statement face pain distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
