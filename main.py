5144deb942609b797ff83609f1f20bb30962cc4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:04 +0000

    crime habit mark trip limit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
