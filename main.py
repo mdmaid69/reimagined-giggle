1ad9ff5d0b3eaa436dfa8f10bb54f50a840a3630
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:46 +0000

    talkoutof persuade aware surprise violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
