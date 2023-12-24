f4e02161f2b6390aba8b98e5ea3bb8f9744b852a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:38:29 +0000

    mainly edge opposite until may

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print(sum(range(10)))
