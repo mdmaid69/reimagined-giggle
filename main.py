c0fddaf52d5c73b86db4f9f5d06ed745621c7d41
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:32:24 +0000

    original phone police later expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
