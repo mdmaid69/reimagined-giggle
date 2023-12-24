13fc762f71fde825d8d4eda1a3b0432ccf4c2958
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:27 +0000

    curious reduction steep illness 566end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
