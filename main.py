abd9628e0be3c085105114166fc2a5f1fdf43cab
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:24 +0000

    shock return effect reliable dry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
