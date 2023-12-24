c580788af4d7a8a0be732763a5d626610642c6a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:40 +0000

    talkinto bound although set offer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
