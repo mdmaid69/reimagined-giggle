bcc8fec2276d79df723708b57acf919954dff6bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:36 +0000

    rarely nothing soft his slightly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
