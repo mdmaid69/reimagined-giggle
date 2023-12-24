953d92b3c8a705fffc2af2462af9ec6e7a9229f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:16 +0000

    black argument guess disappear guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
