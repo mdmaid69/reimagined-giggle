7caabc164ac882ef0ce0116d6b108d916710ec7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:41 +0000

    transparent impressed belong wall detail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
