5878307e82e1140f9f98c97a5118d8e8e224089f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:10 +0000

    hers method tall container gather

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import getpass
+def get_username():
+        return getpass.getuser()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
