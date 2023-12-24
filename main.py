22ec9139e78a4438165400d20dbea5c3aeaee2b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:21:28 +0000

    explanation interest you like present

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
