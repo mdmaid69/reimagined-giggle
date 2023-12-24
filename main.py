4f31fdd6530f22c56fa8e68403002ef79c043379
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:11 +0000

    second both last discover traffic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
