9dbb767bac4e8b1f6f1c95d98bb4fe45b2e2d2a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:14:26 +0000

    lean think recent unlucky up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
