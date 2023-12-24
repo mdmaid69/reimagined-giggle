00e65f5f8e1d39447b0a47498510fe27ad8c40be
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:54:13 +0000

    powerful bite worth replace Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
 def get_array_index(array, item):
         return array.index(item)
