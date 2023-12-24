80c325a6e0a229fb9e47a8be748d7eb247c1d37a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:45 +0000

    mother pause acceptable favour physical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
