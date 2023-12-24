3edcdfbe63b1a2b829bd3e4c33bc9a6c89d2dac6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:14 +0000

    dress live public table mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
