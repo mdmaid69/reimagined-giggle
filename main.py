9f55f7ba9b7881fdebd4df28ffc1658e6c367647
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:04 +0000

    sensible guess tap shoulder winter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_octal(n):
+        return oct(n)
