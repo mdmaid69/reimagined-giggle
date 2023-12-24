064aca037508dd8b8fcf78e7fc59480e326debb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:53 +0000

    takeaway culture reply increase safety

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def is_even(n):
+        return n % 2 == 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
