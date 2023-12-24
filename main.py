2893b4d1aa3133340a5b45fa907e9994126e082b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:16:27 +0000

    threaten environment discussion behind consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def subtract_numbers(x, y):
+        return x - y
