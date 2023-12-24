24c8834805140c69c5554f88864c9407b525ea9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:37 +0000

    influence exactly instrument 179bite estimate

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-print(sum(range(10)))
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
