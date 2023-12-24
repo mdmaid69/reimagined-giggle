2354ba43793b9bbe9f476adf3a905886c4fabed3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:00 +0000

    mistake sticky explain will gift

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import sys
+  def get_python_version():
+        return sys.version
