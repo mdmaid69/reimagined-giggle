40135f78ee2a5c8ff7a42498c319ebb9f01a88d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:35 +0000

    century fall ilovedicks grateful underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
