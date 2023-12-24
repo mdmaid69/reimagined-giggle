109f67856bf9072b9b0dc3bd73b0940dcc0eb520
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:34:24 +0000

    point never pen possible shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
