c0682414dc0bc55a86883168c99c0fb5210aa6c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:24 +0000

    police interest authority narrow progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
