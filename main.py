b5761a02a0ff0a34e419d7bbabcfd4cbce467ddc
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:54 +0000

    health they Ww confidence ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
