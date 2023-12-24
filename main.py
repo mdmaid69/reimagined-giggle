6be5c05307efae5877e82cfc40b371aaa8b07872
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:12 +0000

    emotion represent apart use telephone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
