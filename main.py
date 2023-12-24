19cc151de24fd324b466b8ea57fe759475fb3873
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:14 +0000

    much justice excuse form rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+print(time.time())
