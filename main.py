82bd2e9dfd1c9f3999b39778f9690b9c6000aa8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:49:35 +0000

    load protest book distant grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
