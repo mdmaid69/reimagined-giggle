3669ec194825cf764cc1133fd7e2450387a86b6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:39 +0000

    public off shopping point line

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_floor(x):
+        return math.floor(x)
+  def count_elements(lst):
+        return len(lst)
