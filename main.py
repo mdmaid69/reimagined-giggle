82b886607c2e511d9cd88dccc2ea069298f67dd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:20:34 +0000

    price room dangerous style poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
+def square_number(x):
         return x**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def append_to_array(array, item):
+        array.append(item)
