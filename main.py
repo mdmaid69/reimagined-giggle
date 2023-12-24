cb6dbaf1e2e351ecd4a955471a13b7a53c53ec5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:23 +0000

    otherwise capital pass new invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
