3e77fb266d6ad9e25a2b9c03e575be92c1857592
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:02:56 +0000

    behind children scream hold extremely

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def count_characters(sentence):
+        return len(sentence)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
