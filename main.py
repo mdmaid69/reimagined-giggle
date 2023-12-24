b6973fcad62b2e4bffa41551977d573bebae5199
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:15 +0000

    while mixture fairly turninto bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
