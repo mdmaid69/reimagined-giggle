1c9b2be30ba0651ec850d168f00533ea9c83afa9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:15 +0000

    radio waste process as average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
