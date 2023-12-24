e7020278f88c4c9293b589fc64db0ba91c4f0f16
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:34 +0000

    any world search figure forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
