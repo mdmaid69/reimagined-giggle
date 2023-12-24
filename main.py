d7656a164dc74d2512bc067dcee442baea2e1d1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:38 +0000

    influence usual sing into conclusion

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_array_to_unicode(array):
+        return array.tounicode()
