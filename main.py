b2aec07b7df1d117087ce5f3ddb921cb42a8588a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:48 +0000

    freeze getback height nor walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
