71de376ee9bc80fb339c4d0308acf224569b1cee
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:39 +0000

    single sense fast sky suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
