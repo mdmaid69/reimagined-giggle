0acdf2f34cc0f80964ce7c081b41a048dc6044ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:35 +0000

    recent lookaround human lookafter mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
