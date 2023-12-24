27318624d8b4a8ba991505bf495375a281bb62fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:28 +0000

    brave mixture fork cheat bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
