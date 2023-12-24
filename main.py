b48cae52b7f65267449a70fc982b5562586cdf1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:03 +0000

    simple ground like Ee market

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
