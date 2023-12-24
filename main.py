0ceb2f2f2cb4759b8037dcef3f140b73ab56c91a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:16 +0000

    among debt rescue smart open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
