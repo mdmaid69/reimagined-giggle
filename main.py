4aeaff2a396503e1cecff2596a013e2acb8b61b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:47 +0000

    card blow track about matter

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def get_array_item_count(array, item):
+        return array.count(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
