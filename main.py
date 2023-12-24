e5550b0a06cfe5a481c2172577a2f356cc5d27c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:08 +0000

    wise force worth thorough depend

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
