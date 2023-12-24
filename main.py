54cccd79fce702fe9847dd59150d55119f7dcf50
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:19 +0000

    wide limit respect ordinary strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
