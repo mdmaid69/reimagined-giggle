9b4d8b64acaca506c29ef5832d82c23609aa72e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:29 +0000

    rarely sure second test his

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def reverse_string(s):
-        return s[::-1]
+print("Hello, world!")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
