14017167fe8be8432af0d966a2aa69c0ebb60f03
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:08 +0000

    free violent combine quit song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
