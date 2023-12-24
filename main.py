cfa5c93bd061c9b666eeebf1909290b039a52f5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:57 +0000

    unkind medium giveaway situation release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
