8c902153e02bc6c7962fc410f58f6d5b902ce420
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:57 +0000

    instead roll general split education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
