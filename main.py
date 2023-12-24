6e862bed3ec32a3b81539643175fb8456ef1ce0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:17 +0000

    schedule recognize slide both traffic

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
