c0352fcabb6b3d7e983584b9187105c1615f8691
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:22 +0000

    one tight type lucky deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
