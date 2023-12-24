ece9874881bf7e5e343cae9ad5fdaeb22d66d5f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:36 +0000

    bite track breakfast surface workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
