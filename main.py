6f658a86f578522206ceef9bdcb2c31549adac6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:31 +0000

    careless out rarely best fork

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
