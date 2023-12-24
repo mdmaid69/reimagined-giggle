0e568a2e4534fb6e557ae950232e56dabd8a799a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:44 +0000

    how perfect process mine wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
