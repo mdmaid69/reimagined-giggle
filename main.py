e1fbcc18b4c8a2d3fe3aaea7484f686443132814
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:26:15 +0000

    kind exchange disturbing unkind important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
