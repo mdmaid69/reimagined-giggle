2ef19603040bb739b6cf202086ebe68347676334
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:41 +0000

    share anyone lookback(on) challenge keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
