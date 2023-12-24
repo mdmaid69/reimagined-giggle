14821027317b93a6ea22fcb060197f87d9b8f1b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:36:32 +0000

    late age your programme practical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_queue():
+        return collections.deque()
