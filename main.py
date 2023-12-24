9aa971d1368e2c1859ec880648fed07beef7575c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:08:43 +0000

    telephone fact last keen instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
