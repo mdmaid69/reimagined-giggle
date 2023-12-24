a0fc6b2c48ab13c416f8265c0a5d14497e7d08c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:21 +0000

    fashion stare so hair backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
