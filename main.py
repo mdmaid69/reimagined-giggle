31fccfb746fcb0147b0e3e49e9622b98f74513fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:22 +0000

    train dream disappear church joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
