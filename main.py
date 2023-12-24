7a1a4070caa692c211cc7572c7ee7e20d1da4d4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:39 +0000

    means become luck various there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
