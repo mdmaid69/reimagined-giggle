4b5a28d3a3d516be8c204a83abbca7dc4906ba04
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:46:57 +0000

    fall journey interest protest explanation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
