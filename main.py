baf38aa7e32d0b67b5dda594323ba46429353c0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:14 +0000

    roll fasten traffic bring increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
