64eddb1a0a2ae7fb30973942bdaf2b1ae3aac3c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:45 +0000

    cope watch pollution brain phone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
