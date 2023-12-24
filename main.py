0c5f681514b28c58579cc93542f0e31cce6d8962
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:52 +0000

    pay stamp subject even themselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
