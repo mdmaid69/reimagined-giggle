cd9d0e07bd2230d070b5b409766c088f096ce652
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:48:12 +0000

    shine provide unlucky guard tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
