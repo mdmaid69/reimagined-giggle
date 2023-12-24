e3f508f009524fef3ccea6d658d0426cccdcbecf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:04 +0000

    special mark force stop it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
