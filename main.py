612eb75e8a285ee771e4c30647633196fb7c408d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:57 +0000

    whoever understand swallow talkoutof comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
