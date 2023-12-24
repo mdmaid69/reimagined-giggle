4c12c38130ddc9aaf970550a2a4044c9dec84a0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:42 +0000

    throughout thinkover repeat pretty keep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+print("Hello, world!")
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
