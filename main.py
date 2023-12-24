3f0b5ca6560cbd657420291f56bbd710217f2e19
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:29 +0000

    proof precise equipment expect official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
