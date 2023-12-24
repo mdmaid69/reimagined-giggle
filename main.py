c0f734eb62cd7dd3c518ac24c5640e590f300051
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:57 +0000

    radio uponon independent trouble trousers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
