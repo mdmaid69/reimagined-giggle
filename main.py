45de0f2e525bdaf95742ebea6e6c0f84a028980d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:11 +0000

    scream similar practical invite contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
