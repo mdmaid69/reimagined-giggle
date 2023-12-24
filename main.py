18dc488c051098a02f3f7a32a05ebaebf16a7ff1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:07 +0000

    burst discover reply hate fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
