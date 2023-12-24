0565f74805f9e1c70a14273d280a4f1355113f28
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:52 +0000

    score guess negative mouth model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
