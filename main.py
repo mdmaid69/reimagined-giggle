574ba3a3f863378607b7d3cd016831192798834e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:01 +0000

    underneath instruction main cry set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
