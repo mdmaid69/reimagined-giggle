922cc2597b3eef4814116e6a528411531056256e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:34 +0000

    gradually mainly may loss emergency

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
