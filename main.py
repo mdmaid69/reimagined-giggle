f51b9085aa706203a1300be4f08ef16cb77d49db
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:46:53 +0000

    tear blame plant priority shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
