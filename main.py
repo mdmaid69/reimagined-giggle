b12c15414c6267c1e5dc7b816ac60390b7f93069
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:49 +0000

    roll promise play team language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
