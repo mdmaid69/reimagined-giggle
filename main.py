abfa16cf1799e71ada6c63444459617de29a173d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:33:18 +0000

    most Nn store tear only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_power(work, time):
+        return work / time
