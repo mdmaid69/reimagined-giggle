63fdb564002997b4a56401c3b37208deddcd29aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:29 +0000

    beneath value head earth sink

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
