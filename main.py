4cbb507ee6db1e64c75062a1c19e1b7762697632
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:47 +0000

    lie call careful pot other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_area_circle(r):
+        return 3.14 * r**2
