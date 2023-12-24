7ebe29eff815d1211b7ccc0bca5dddac0123c671
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:30 +0000

    idea peaceful left compete view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+def print_python_version():
+        return sys.version
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
