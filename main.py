67f1dbc2138e6a8f9a461d439ec190e056154ec7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:06 +0000

    takeback gate politics break in

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
