c555e30a77434e84c252f35668ff5d26a1b2d494
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:37 +0000

    since lookaround thing own creature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
