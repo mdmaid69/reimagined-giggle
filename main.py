9f3a051bab268c312d24b957741ac46c7dcfbfaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:07 +0000

    taste stamp we the lot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
