0a3a047e7a2fe92c1ce7430be35cbf6c70d5b441
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:05 +0000

    perform fact spend regular traffic

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
