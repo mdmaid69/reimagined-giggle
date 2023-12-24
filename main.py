40848ccf1fb9e7a35deef963d2326ed6966e606a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:02 +0000

    bet public correct spoil runout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
