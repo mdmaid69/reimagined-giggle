68213df37b3ba68d7c0ae6698094597bfbcec391
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:12 +0000

    list instruction term shock lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
