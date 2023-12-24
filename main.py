3d1494811b7ca28b4241c7219c957dd8437b45a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:26 +0000

    show kind evening fit cold

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-a, b = 0, 1
-while a < n:
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
         print(a, end=" ")
         a, b = b, a+b
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
