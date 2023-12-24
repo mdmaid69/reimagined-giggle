e2bd59d7f719c983c3d34cf06b9a10eef1f57fb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:00 +0000

    break tend mess statement need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
