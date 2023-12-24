6f03f04233014db39b876db75ebcc55bf4feca45
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:51 +0000

    possible alone correction waste rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-print(time.time())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
