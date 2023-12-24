1d88a0e66875aef4990818997e51dbd0b15844a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:25 +0000

    improve pronunciation unhappy opponent store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
