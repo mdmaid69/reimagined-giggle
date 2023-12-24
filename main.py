7fa856d7f6f3b020e6fda9856df6f9038b2c42b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:22:23 +0000

    bush hurry thankyou definite kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
