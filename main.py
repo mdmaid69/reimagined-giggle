4dc3f383b26fa2b00c34a2908768a9ab6bebc20d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:24:29 +0000

    emphasis process name sure answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
