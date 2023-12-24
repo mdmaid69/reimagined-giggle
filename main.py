5e69e33134e146a137dda0d869960f3fc0166e83
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:32 +0000

    total sit shop awake manage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import re
+print(re.match("h.*o", "hello world"))
