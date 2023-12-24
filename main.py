fef9d7d0ee5bdf2bff18da3adadd9959322e3b11
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:12:40 +0000

    mine science swallow preparation score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
