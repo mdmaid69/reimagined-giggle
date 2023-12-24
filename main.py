f4683da9a6008df3f6810af9193b8abcd38925b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:15 +0000

    under object tour 564encourage provide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
