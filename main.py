dbf3b5b0e1ef84e01925a83b94b3a2d5bd6ae720
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:12:17 +0000

    private short breakup emphasize sting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+print(os.getcwd())
+  def square_number(x):
+        return x**2
