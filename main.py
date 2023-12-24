260d22e71b896ce6571bd3fb4d0ba6e81dac7be5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:44 +0000

    tread tidy free cause agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
