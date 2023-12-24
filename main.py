0c9d082d3878ca68d1c081f4c0c01565ac5efd73
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:40 +0000

    share never other key public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
