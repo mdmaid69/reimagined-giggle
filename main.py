61f1d2be78aa3a1b5916217289737f71c66ee028
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:05 +0000

    pretend over government interfere though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
