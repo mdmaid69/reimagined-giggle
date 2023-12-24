a51e08da675ea5320dd5e0397c302baf908a3f34
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:41 +0000

    public busy giveback have crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
