3ba2fde5d60c807d3151511a48985e6889206a8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:57 +0000

    raw calloff welcome curious loose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
