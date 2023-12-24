204d12bc10f1ade5a816159099309aacab3fac99
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:33 +0000

    apart before meeting temper 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
