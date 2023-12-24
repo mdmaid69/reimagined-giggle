17626ba0b2362bf6f26dc44d536d0303a51fb10a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:05 +0000

    value involve love organization close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
