0f7c8595d33778eec27468b4e321be2fe412bb9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:11:46 +0000

    takeaway clean until towards oil

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
