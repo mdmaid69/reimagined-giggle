c26f6cd4fa8c6d485d5fa1b367e878ddbb7bfe9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:34 +0000

    pen time smell reserve stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
