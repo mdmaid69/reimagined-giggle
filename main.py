3ea04eec28c10746ab9768879a8afaa6691684d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:00 +0000

    guess mention future less rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
