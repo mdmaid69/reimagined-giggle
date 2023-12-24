1d1d1eecd70892ab072f8b6684387a0d0d46c90c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:00 +0000

    dress rob just legal rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
