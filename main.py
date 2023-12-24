17269a241cd5962f2844ff79787fcda7ade1b964
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:26:27 +0000

    lead degree cover broad share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
