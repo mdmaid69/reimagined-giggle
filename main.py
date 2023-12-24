47459662e1983ef7f1b89be6b6ef1298bd950b62
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:32:58 +0000

    provide body sensible laugh wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
