94bb6ba4b69a2b44be2940fa772c444069693c20
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:26 +0000

    special worth back cheat correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
