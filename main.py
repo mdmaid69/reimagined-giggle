14d0e3bc44b46699d071c036390a7860b3faabff
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:07 +0000

    tap always announce wipe fence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
