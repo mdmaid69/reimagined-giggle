4f237ea498afb9564d7412e6413a9de6a9dca72d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:57 +0000

    move temperature combine storm cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
