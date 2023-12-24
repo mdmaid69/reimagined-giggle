79b564f5f369fbe4c51e4ffcaf5f41c4ddb3607a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:40 +0000

    typical measure loud turn apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
