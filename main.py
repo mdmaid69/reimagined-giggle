8e344b1f6ebe1169048a6308f06c6b807ea5f713
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:57 +0000

    disgusting song store promise respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
