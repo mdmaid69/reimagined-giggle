ee654089204f27f3728017e0c8c8c9fd159c389e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:59 +0000

    turn turnoff empty form possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
