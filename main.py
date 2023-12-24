be6d1a78e8ed2db491e5bd7cd717f3426b5b1c34
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:57 +0000

    especially drive minimum kind none

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
