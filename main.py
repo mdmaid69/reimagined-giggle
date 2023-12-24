30abd018ec1efba925710d6643cacf7dfcbf6834
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:00 +0000

    separate manage together 567enemy so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
