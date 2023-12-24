e47b8f1f6b1867a286dda2d113a083f59d7668bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:30 +0000

    more rest Ll boring nature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
