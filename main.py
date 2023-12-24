13d45537e21a0437de7dbfa5475ee00740450412
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:08 +0000

    imagination cry all official smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
