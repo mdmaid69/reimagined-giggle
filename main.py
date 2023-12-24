26e0395c58baade7bf8cee9d848d97ff324601d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:16 +0000

    angry mess lucky responsible block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import time
+print(time.time())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
