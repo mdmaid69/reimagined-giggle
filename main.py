3b97cb212bcb85189253abe0339295505d49ba7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:19 +0000

    school pretty judge represent train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
