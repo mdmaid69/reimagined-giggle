12e95ba89f21da1c2ab487c8f0a933f54f46149c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:08 +0000

    few late let grateful measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
