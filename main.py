5c6133b63ae45c8dd3e5181464f2efe54c67723c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:53 +0000

    difficulty Itself cover below muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
