741e01fd79e85d453629aee5f8d4142857f184b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:41 +0000

    edge sound family consider matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def add_numbers(x, y):
+        return x + y
