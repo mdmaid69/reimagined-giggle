9114f656b682bffc918537ead9cffacfd6eb6b6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:18 +0000

    patient afternoon tradition use freedom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
