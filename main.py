f4e98acf49044e3a3152ee0fd953cf95f656854f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:31 +0000

    570enjoyable if consider gooff study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
