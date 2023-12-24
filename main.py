e6846f57888a7705bf380a3707a2eaa2e46bd3be
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:19 +0000

    mistake fight violent cross but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
