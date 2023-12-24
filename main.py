d7699b60470704c8838f15f4510ce99f13601ba7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:38:19 +0000

    sister this fairly trial Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
