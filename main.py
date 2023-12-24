1072b9959684a6909232a814a6dd982f59b55f10
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:02 +0000

    improve extremely interrupt impossible book

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
