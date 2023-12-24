2bbfa787b3d6cb07c5ab58efc6dfe0e56d78fa53
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:14 +0000

    loose road window explain ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
