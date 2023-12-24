addce2dce14731464daa42c9cc7feb2947ff690b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:04 +0000

    edge drive rain building miss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def reverse_list(lst):
-        return lst[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
