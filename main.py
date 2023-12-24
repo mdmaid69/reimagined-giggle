3a3f883109641dd429eb061a77a4dc14f6252b99
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:31 +0000

    whenever arrival passenger out escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
