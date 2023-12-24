703974b52ab386c3c36698af2b1c5d58dd7e73c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:49 +0000

    shock couple mean snow keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
 import math
 def calculate_neper_number_to_power_x(x):
         return math.exp(x)
