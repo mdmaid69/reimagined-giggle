4a8cb9198e4173e129f2f34a0d08cd0ec38f5f9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:37 +0000

    Jj runaway drop down where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
