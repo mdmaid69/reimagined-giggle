e1c0cea182e3406af0c1563bcd8a5415e6dacee3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:52 +0000

    speech chin male callup decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
