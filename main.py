5b53ec796681019c296295396695618c7948e882
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:15:35 +0000

    survive failure this takeaway quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def sort_numbers(numbers):
+        return sorted(numbers)
