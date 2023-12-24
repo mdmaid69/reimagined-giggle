60c87873663733ebbead9aa715761857c7e7099e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:31 +0000

    advice healthy stage fit alive

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
