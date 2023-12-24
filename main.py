5381cb9b7691c758a8f20786b1fc1dd99f15691c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:43 +0000

    threaten wheel odd brief trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_exponential(x):
+        return math.exp(x)
