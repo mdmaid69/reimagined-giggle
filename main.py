f401dde3a7037e6d024712bb93f5ba8e022a37d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:19 +0000

    advise bush rank rid gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
