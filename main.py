00007733b91a6f6a136af9a674d2dc080fd8d775
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:30 +0000

    suit shock time pain me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
