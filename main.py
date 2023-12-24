c3c7393ef5bb1fb11223e5f400878503fb4630c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:58:32 +0000

    war close 570enjoyable sweet wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
