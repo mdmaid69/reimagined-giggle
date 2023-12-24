370ef20e8063ea126d999039f06dc3309d2b57bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:57 +0000

    uniform load represent give all

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
