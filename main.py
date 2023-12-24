ee71e74acf0deb9b9d35af29cef1875cd3b5c8e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:25 +0000

    lesson spot couple issue accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
