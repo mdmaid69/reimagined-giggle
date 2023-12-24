424ad4a57ef1cc5ba77f0c87d67d9c223a7bf589
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:50:11 +0000

    cut knock reject pipe appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
