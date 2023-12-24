834a54877b8cf18597f48a3d53ffbea28af68a24
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:15:55 +0000

    performance prize attach pity begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
