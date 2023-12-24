c65c29a14ee573704e35fe9b0acee74757d80640
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:30 +0000

    shame attract usually must birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
