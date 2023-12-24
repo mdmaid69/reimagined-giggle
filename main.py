f12e898343a023ddbfca3086a3fc733d196ade74
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:40 +0000

    remain bunch closed partner takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_creation_time(file_name):
         return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
