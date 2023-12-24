2dd0f50f15aaeb0e878542e1a23d0e0354cf7a80
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:03 +0000

    lead bunch project club have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
