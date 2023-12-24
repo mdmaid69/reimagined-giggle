1af2e1e91cdfcf32d067fb14fc62bf82320245bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:45:02 +0000

    month point force ourselves shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
