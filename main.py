951acb4cc93fa24538a1447fc854cfa5ed4afecf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:03 +0000

    carry worth correction deep shrink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
