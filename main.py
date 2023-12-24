57d70468c2b83d7f2af947ab9e35cf70de3178fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:41 +0000

    search broad criticism float tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
