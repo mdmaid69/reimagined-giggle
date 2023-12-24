25474ae7adcd539a53af0d32d0d29c685fde2e2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:15:03 +0000

    there stiff below Ee size

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
