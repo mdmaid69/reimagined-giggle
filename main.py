f833d84ecaa3d1f8c4985706ed1842db61051a53
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:08:44 +0000

    communicate cost young mention hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_itemsize(array):
+        return array.itemsize
+text = "Hello, world!"
+print("Characters:", len(text))
