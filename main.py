0f36b5ac2cf4e16cd70675d2609092c8c55f54fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:20 +0000

    especially judge forward talk content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
