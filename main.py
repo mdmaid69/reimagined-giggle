22c24f4df2dbbda65443b4882059f223b5e0599e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:08 +0000

    popular insist tone invite crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def is_odd(n):
+        return n % 2 != 0
