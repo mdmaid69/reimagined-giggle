a2795c56c3f358c74c44b32e6d9d6fd15cb57e49
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:03:35 +0000

    breakout position voice Ww sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def add_numbers(a, b):
+        return a + b
