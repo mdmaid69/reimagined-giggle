efeb58457ee57cc64077e38f5dcf734017c66fda
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:24:34 +0000

    fold safety come joke abroad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
