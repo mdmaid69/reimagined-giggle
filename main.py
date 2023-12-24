29c179a55c71fa52a418ca7e53b3db0dc8261263
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:55 +0000

    strong shoot discuss escape perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
