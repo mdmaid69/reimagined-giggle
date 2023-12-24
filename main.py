e8c0e252f86911eb8f9afbe337569aed6013f011
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:25 +0000

    year dinner slice dick advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
