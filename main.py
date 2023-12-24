f61916305ca538414706ba1e46fa8b6186627233
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:18:50 +0000

    again remark criticism scene cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
