fbfc2d794716a4e1b7dcf3b737ab4396f93ffdbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:41 +0000

    well brush scream talkback talkback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
