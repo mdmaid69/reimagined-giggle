201c67bf115f2cb02f5c5fbc700102847f5f4dad
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:10:27 +0000

    around sink marry well condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
