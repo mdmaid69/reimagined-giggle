d59b9a9b3f6cc2c90a20d9982ad84ab22f31cb19
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:42 +0000

    mass shelter concerned warn left

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
