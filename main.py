efe56a63cdc7675612ec3be2937352eae914c050
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:33 +0000

    give worry worse window to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
