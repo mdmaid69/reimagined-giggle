96ddd2e88b8b51dbddbe0e409ecff0bd8f8a41c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:50 +0000

    hope head include estimate street

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
