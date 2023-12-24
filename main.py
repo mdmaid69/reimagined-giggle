8e027abe803e04640ebe3a44f1792d8e871c8985
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:58 +0000

    dress hard he instant ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import datetime
+print(datetime.datetime.now())
