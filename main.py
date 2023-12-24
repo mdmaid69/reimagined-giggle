be0fe501ff0716eb6fb3179f362ff15dcebaf6ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:48 +0000

    congratulate Nn 569enjoy collection store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
