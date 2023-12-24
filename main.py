1e134f372a5003866dc5473a461d50f84569a5e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:55 +0000

    up draw gas fill jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
