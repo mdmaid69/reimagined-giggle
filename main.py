43169478582111f9fb68339ebbf90319ac8c9852
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:01 +0000

    comeoff hurry wipe wash feel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
