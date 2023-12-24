72dfb097e564055c6652bb0e54d79d223a142bce
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:52 +0000

    note our call workout middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_current_directory():
-        return os.getcwd()
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
