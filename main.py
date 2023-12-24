23dd94ab9ad09c42f9325db9bff8e34aa27700fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:14 +0000

    weigh level spend weigh wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
