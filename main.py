431a1bb2c0b91eb17718cc6ba87f3374059f5ef8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:01:56 +0000

    coffee cut bush rob confusing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
