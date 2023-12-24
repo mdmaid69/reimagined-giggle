aaf4cc9be03f420fc8dee4da98e2f553eaf36a98
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:12 +0000

    recently crash drawing prevent congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
