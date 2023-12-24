6a4c0205bd94866985a379a7ea5c37947478c16e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:20:18 +0000

    drive Cc slightly tongue cinema

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
