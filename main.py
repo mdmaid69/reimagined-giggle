8f7c67a069362133bea07e9dee37ac0b1803d1c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:12 +0000

    phone strange breathe song holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
