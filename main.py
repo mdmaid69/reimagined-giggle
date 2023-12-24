d934d411d6602f267e75599d7140a107ba23e42f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:59:08 +0000

    feeling flight fun throughout attention

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
