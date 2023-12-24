74bf6d0b963f659be55fc02fea102dd22057be6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:46 +0000

    OKorokay confusing part disappointed deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
