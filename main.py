4f9dd7de08839e4ea78f4dac418a3161d38c9254
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:36 +0000

    valuable needle win communication over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
