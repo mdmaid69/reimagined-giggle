253937f76bb8ee3562dc9d08db9bc2e4829af92d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:51 +0000

    affect point word allow check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
