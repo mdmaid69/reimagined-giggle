94b1d0b3c8892cde7f7c3d138ccc5c410fc0203a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:19 +0000

    future bear police sit laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
