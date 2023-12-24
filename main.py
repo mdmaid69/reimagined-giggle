dc903c268884b26ea589a45a8152ef6d07ad7022
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:03:27 +0000

    forgive drink final become aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
