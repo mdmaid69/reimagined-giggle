f8f8928b262650f0b7cb09f667a0ef9a713d2ccd
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:56:58 +0000

    at rank edge bank pupil

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def subtract_numbers(x, y):
+        return x - y
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
