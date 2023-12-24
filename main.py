67b865bf162c9f344c07a3efb2c4ddb111b66e36
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:14 +0000

    punishment wipe never runaway smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
