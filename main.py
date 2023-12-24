983782f3fada2fffb306e5bcffb839889cc2b798
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:45 +0000

    level wind wash me post

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
