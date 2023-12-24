1246ac1ceb9a22ded817701d3f480059da719e73
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:23:34 +0000

    available makeup ride violence play

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
