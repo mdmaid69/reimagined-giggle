4d435aef1a5f2e8c2fc7db549b7689bd127cff3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:50 +0000

    country few tape future comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
