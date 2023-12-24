e94bda30b0e1230c53512ebff4852636dd199ae1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:10 +0000

    block cover imagination choice action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
