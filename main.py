ba397dc8c7f31e760227546682b79d8c98c3d7ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:28 +0000

    concentrate bet instant bottom long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
