aebd940fea39e677da2aafa5de2b77ca80c88877
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:34 +0000

    search risk direct owe mind

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
