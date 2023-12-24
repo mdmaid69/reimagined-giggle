419bf05ac842c1465122aed15c072d8aaded3417
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:49 +0000

    accident word especially guard study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
