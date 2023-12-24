67e3828506097baaa9d8a6c008c6e73e2f7dd046
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:46:14 +0000

    strike boring loud shake firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
