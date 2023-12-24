b60e5bd2cc61310976d4ad216cb1ce2271b43de3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:24 +0000

    proud mysterious his comeup discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
