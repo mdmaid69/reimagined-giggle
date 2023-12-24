35730f020e2674bd26fc38bd402899ab3923ba91
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:13 +0000

    otherwise polite concerned length inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
