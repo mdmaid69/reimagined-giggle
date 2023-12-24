1d7ac0860a2be335b5c2508ce17d96f0715b07d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:39 +0000

    miss believe comeback seat pause

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
