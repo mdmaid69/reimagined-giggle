b9428d7df5972d6825d06518f65b574cc55b488e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:11:55 +0000

    fall remember connection copy ability

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
