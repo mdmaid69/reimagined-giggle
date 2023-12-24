f038f22e4ff2e626eb38570d5feb2159d4dd43eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:21 +0000

    as maximum display impressed dick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
