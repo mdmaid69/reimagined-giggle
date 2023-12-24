504f49231a924d9ddbe323149203113452ae1cdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:22 +0000

    because watch gap central takeback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
