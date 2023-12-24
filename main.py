56500dbf8bf9988b83feafc33c792ab2abe20b26
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:24:04 +0000

    its air history definite profit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
