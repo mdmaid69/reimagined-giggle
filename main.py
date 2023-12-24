10ab9f40c75a3eefe0700ab7ddfd8a75b8a01516
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:44 +0000

    shallow silent drop stone complicated

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
