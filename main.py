f6c26fad7754da24846494e7c2fabfa7176d39e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:51 +0000

    doctor hardly 566end better slice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def convert_to_octal(n):
+        return oct(n)
