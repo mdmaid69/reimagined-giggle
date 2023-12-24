50b44871b80a97f59ed2009d9a2e540a74413531
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:42 +0000

    dare judge lock fashion gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
