63bf3e8b9d9ad448d12ab1634fdbe4f520d45e22
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:30 +0000

    phone long refer catch slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
