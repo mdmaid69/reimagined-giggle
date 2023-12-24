2ed9451ef0a216ab40a302aee35227598ee9e84a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:25 +0000

    lonely bringforward exploration work cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
