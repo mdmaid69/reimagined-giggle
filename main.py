9084a7a9b230d6b6a398558bfce20cb013fbb41e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:39 +0000

    suit piece occasional black free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
