bac1563f83d656476281ca4a610f89bc6fc8dcb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:48 +0000

    judge almost giveaway port general

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
