3d41fc822a50666156da4962e93b9dc81e0f6dbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:45 +0000

    action reduce Ww Ee love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
