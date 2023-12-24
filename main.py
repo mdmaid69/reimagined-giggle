622f1ff99753125da81065d69c21ffdd90112d1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:29 +0000

    cruel tonight worse join getback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
