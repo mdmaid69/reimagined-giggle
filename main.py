df0fc63e0e0c4ad56e88f2da8af0b34342eed0dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:10:34 +0000

    defeat fly serve tough account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
