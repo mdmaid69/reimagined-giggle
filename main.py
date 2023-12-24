d3d5d1dab45d1e1618e16039c8bf0eed6d95acc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:27 +0000

    emergency delight shape clear guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
