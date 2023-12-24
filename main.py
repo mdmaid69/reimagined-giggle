f2c32ee3124b3890a0c22e573e77402df1e58951
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:46 +0000

    exam activity number week please

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
