2acb61214a39303fe7868a9b46db59eb4c877da5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:58 +0000

    worst expensive everybody guard succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
