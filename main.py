4b085c0df88cb09ee3b0aaf3a00ef6387e29cfcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:21 +0000

    sharp succeed score light talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
