87511d925248e5e95c4f6f74f6a7188783810b64
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:20 +0000

    negative lookafter movement busy inform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
