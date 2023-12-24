14d625ff1589f3fd9d5359b1827c453a609464d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:36:22 +0000

    plastic likely Gg before describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import datetime
-def get_today_date():
-        return datetime.date.today()
+print(datetime.datetime.now())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
