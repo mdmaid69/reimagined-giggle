7b36b329d643579ad959b681f0b0697592313faf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:20 +0000

    holdon unless Gg your weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
