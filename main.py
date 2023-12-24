11922dd1e90a2a2b8f19ec17fdac38e211e91c85
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:14 +0000

    enthusiastic meet plastic the goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
