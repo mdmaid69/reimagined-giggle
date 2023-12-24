7e07921cd5030ce2e215c5c544363bffb722a3ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:21:23 +0000

    plenty continue fight turninto someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
