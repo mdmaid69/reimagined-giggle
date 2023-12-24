b2d5f597c17b03f187f661cfb1c02a4ce8919d8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:39 +0000

    full patience play birthday regularly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
