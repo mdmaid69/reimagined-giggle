c946764dbd92212d5c40e49a70948397d5d58143
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:13:27 +0000

    tune better trick office breakinto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
