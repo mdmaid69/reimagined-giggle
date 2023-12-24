864d4960fdabf9b8cbc07c1d99b6f6dbf492ea18
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:14 +0000

    wish deliberate lower peace taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
