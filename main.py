7295af308ff245f23ea373dcf2bcf8e4eae4de28
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:09 +0000

    minor demonstrate convenient separate radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
