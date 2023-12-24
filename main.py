e29a0a431e4277dec1c820c165c8b7b9064eeab6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:20 +0000

    thinkover determined welcome cry store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
