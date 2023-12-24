b768062d173af65c735f3ed095fed419eda38f4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:29 +0000

    neat I clean exact join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
