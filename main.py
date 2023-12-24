0afbe185382a790d23f3eda1cd2504069f81b431
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:30 +0000

    trick condition figure wall calmly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+for i in range(10): print(i)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
