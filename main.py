4ce801103509c1789278b799d15f18eddb31007a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:33 +0000

    will peace chief exploration bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
