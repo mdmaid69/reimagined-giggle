ae3bd1ffb93d5901c48bf57163e2e202f6b7e792
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:20 +0000

    praise leg glad feature 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
