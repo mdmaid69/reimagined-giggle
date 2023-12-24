cf1205811986c158a68fba75df28ec788dedb4db
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:00 +0000

    tap building pay to reduce

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
