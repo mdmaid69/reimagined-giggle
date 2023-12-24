66ddc5ba787690f03f7e60512af6904371b259cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:25 +0000

    trust downstairs high lie chase

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
