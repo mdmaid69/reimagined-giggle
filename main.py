0abfd0ad40fc69d0cb639cc8853f4f600d7cb7ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:09 +0000

    maybe early soon capital previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
