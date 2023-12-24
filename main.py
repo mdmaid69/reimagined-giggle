264f3beb7a1e26dd066a07018fb820e1f58f59d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:04:21 +0000

    thorough tired cool quarter bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
