a9ba704fbbf6b17cacccb5876aaaf65f302f3e97
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:43 +0000

    offend surprised gather flow float

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_max(lst):
+        return max(lst)
