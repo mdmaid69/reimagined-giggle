76ab7df04a63561aaab9228b5043f8a10ff9c62f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:06:50 +0000

    needle key man shoot platform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+print(math.pi)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
