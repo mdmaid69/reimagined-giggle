18a65021a8f37132dfad863c907bacd6025a88b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:58 +0000

    expect treat either charge national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
