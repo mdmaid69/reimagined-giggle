3f9b04fbd5495299d7e8f9ddab82bb29b484fdd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:09 +0000

    precise unusual create immediately but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def count_words(sentence):
+        return len(sentence.split())
