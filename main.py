3b91f388bd89acc280d478810ff9639aada5c45b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:59 +0000

    complaint translate poison crowd defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
