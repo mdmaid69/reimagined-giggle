e0ba836a5aaa6b918b3190fa98cb9241b1e7ac35
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:28:39 +0000

    who morning Nn big stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
