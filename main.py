df8818ce3cba3f93d46835d23bb95f599653dbc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:13 +0000

    callat secret report grand smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
