b282b95a879fc8982408d384556f06e8ef6a27ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:07:15 +0000

    sound climb inquire by second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
