b441218992e060aacacdb395fbc2e33e56c6acf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:51:46 +0000

    build after what degree yet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def append_to_array(array, item):
+        array.append(item)
