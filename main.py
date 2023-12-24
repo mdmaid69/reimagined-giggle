882004c5a2ef9450e4bd33b852064fe4192e07d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:42 +0000

    march set coat criticism takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
