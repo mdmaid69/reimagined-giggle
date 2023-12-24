471274e07dc45e4233d76947dec1380384ba8a85
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:28:02 +0000

    calmly exactly progress brush fortunate

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello, world!")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def sort_numbers(numbers):
+        return sorted(numbers)
