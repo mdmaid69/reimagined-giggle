f0b093195562596b99bb69011a49bbc57a60ed19
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:01 +0000

    vision quiet hurt unkind motion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_density(mass, volume):
+        return mass / volume
