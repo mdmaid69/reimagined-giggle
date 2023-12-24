679ec0f1507ed88aa07dd6e079c649786bc1a101
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:15 +0000

    slight herself course cheat song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
