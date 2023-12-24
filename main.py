26da4cb267fb9f27a93392c978649965bf0e8356
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:54:16 +0000

    appeal imagine feature hurt matter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
