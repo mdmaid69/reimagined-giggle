5975a43de0060f7cb5869e14f424833a840db842
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:24 +0000

    boring other tradition importance worth

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
