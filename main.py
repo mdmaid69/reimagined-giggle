2125122abbbe3d59d1869c9e701db46aa0f33176
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:25:01 +0000

    main union go fire dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def clear_array(array):
-        array *= 0
+def get_array_item_count(array, item):
+        return array.count(item)
