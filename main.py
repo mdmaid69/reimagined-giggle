885e19d328ebc8a896643bee6ab001203fb0edc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:22 +0000

    sign blue suit street clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def find_min(lst):
+        return min(lst)
