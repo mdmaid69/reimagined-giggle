5027199f9583a12be4954451db933e4d52952cf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:04 +0000

    process weekend cause reason impossible

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
