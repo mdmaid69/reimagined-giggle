3ddd661953e3c63d2faf0e53e92ce4af9a38915a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:38 +0000

    accept speed through blame goup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
