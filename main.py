87172bf0b5fe3411dc0257ddcc4615f71a8d68bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:18:10 +0000

    unless hurt organization vote knock

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_arc_sine(x):
+        return math.asin(x)
