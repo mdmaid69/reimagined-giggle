586092178dc0cc1953b9d298ba0431bebf464257
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:02 +0000

    even mess live copy open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
