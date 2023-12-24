9a4001a90ecb49cc989b63fd93831ab12e2b76b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:56 +0000

    season stop cry frequent circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
