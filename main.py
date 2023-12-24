612de2fad7d530f7a25ef613b9f373ef40ea5270
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:41 +0000

    as dress quit lock fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+print("Hello, world!")
