b31dc938e946c868ee16f2813f5036ff6fffecca
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:12:19 +0000

    closed accident getaway attitude injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def count_words(sentence):
+        return len(sentence.split())
