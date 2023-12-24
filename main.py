b525866e9a45c9dbbc37caa068556449adf66435
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:28:53 +0000

    tight pour unlucky bet bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
