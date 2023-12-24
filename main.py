f67f8464a2b6f861decbffe7906b9b586c9c94d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:53 +0000

    occasional tonight warning tie frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def change_working_directory(path):
         os.chdir(path)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
