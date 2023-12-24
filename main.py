01b4e58334c621e417f58fc3eb9a58d7413f34a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:35:02 +0000

    injury development waste exchange system

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
