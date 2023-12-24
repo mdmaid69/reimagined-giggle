bec763f478efb892cd341a05e14fe04a7b5ed607
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:07 +0000

    undo cut he pipe destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
