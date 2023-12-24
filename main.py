6540371a4d7092c8f79bc485c81b17cc3e2d8ad5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:35 +0000

    night past advise speech object

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
