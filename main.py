9f2829f5a577fa5fe5a5e45ac6290080b4cc5b9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:47:17 +0000

    callat business back not rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
