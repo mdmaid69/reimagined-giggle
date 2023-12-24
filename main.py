d2b253aa64df88d77823add09fffa7892cd6cd74
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:28 +0000

    it spare convenient thanks break

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
