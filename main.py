e25edadbf1656d86c0fad6692232e36ad67df75b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:53 +0000

    downstairs 564encourage nearly design behaviour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
