c6a033986d3f28be569fd590dd268d86f9660a7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:23 +0000

    brush smell transport sick soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
