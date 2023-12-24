b4a4517b3e3b7d0302196d8efb24c2751eb3f538
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:37 +0000

    face join responsibility sweet organization

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
