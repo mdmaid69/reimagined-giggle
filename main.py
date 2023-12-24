3d2cb66d581922b373693dc15da766f959ac6fb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:38 +0000

    regular content mind thing city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_volume(length, width, height):
+        return length * width * height
