f140063e2f1af1c637af7b3b32fe95fbf1276e80
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:52 +0000

    repeat sink bother communicate basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
 def check_if_array_does_not_contain_item(array, item):
         return item not in array
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
