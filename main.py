aba39240094bb5a206e095e53f2964576ccb5762
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:18 +0000

    reply walk study copy sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
