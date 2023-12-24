494bb272b268612f44515720e0a124cad2c27a36
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:16 +0000

    loan giveout 565end wipe rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
