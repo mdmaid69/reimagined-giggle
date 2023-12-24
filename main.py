84a3d337e8d156d042607e99d23e4447d190c5c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:26 +0000

    desperate kick something dick approval

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
