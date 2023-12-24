4c285f42b64527a082cbe332b201de719f78d499
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:46 +0000

    continue mess use introduce near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
