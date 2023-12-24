63d2c37ed97216aebe62e4cbe899ee9e5c304f92
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:42 +0000

    his day meet run goaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
