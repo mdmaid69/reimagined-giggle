b769e10f11d080f2e25967ccbe753aa38b528643
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:28 +0000

    disappointed protest gift goback(to) fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
