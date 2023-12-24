2c80c50dd69e67b5af25760669ca0b55f6dd52b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:09 +0000

    board breakdown custom upset project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
