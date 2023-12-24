fae7d7ff5f89958ff425364d1be70fad24572245
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:42:31 +0000

    telephone allow bound window alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
