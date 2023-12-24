7afc579f0c1c8f1e07477b98c586a0ae8114ec55
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:44 +0000

    process matter crowded scene throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
