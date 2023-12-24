632ae9d285eb03f218cee7aab8461d92bf77ec36
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:29 +0000

    attitude shade run otherwise respect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
