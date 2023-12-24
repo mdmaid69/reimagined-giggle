f44c995a790eb29f12ceb58806467c492e7ac430
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:01 +0000

    front immediate out back excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
