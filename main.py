5c0531bdc4e1153bf2c5752ca02433fc8eb6d9e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:20 +0000

    medium design maximum operation power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
