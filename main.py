cca70d752280254d2e9b08f796580dd896fa98d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:17:45 +0000

    pupil probably dress admit call

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
