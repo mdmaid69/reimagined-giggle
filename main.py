d0f34b993171c3cbb63a0b8445d2732da8fb55e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:31:26 +0000

    threaten trousers advantage Oo anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
