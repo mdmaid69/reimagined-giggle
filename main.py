257dcc3facca26b049e6f9dcca7905b564bb2c2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:40 +0000

    describe twist desperate yes record

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
