8399e14883600432607e046c2ca0f310848cc261
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:21 +0000

    expression yet comedown blame handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def convert_to_hex(n):
-        return hex(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
