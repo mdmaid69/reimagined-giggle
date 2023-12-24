9bfefbb6b9a4d68a239235018d9e5c0c34f41e84
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:25 +0000

    music own scream any smell

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
