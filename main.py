b4e55e6e6c54c8374590f9a9aad9a351c3b55697
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:56 +0000

    commit whom either meanwhile can

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
