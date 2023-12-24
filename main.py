a0d7fdadbde4a81fcb69a7912a6c847fdae35bbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:32 +0000

    thank beginning deal big surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
