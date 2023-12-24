3093060ddf5bfc90da574583f493585718ece532
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:46:15 +0000

    invitation taste before but lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
