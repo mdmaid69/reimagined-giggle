7e4e2a9d9bad3e2263d595dea63eaee7e1ebbe64
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:29 +0000

    pull call complaint estimate serve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
