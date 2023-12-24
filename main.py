29d7f57c6c9ee139143fd5dfd5ded2f0502c67bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:34 +0000

    related event breakup last sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
