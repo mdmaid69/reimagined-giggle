9aa463493f49c368a481b8504882465e2fc03ba6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:07 +0000

    object surprised load aim turn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def square_number(x):
+        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
