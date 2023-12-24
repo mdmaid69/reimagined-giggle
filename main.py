023bb9d0268aced5b5be50c2090277e742e2a440
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:27:10 +0000

    under great trade it great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
