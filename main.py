367d3fc30103417874879089975423595e028369
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:12 +0000

    connected shop connected yet muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def convert_to_binary(n):
+        return bin(n)
