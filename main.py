35517769a8d4066c0779486f5d84daeaa1d68553
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:37 +0000

    major letter rain clean human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
