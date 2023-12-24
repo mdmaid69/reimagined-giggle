17e1e55354e8a0f130aa9434e216f9488412ef1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:29:11 +0000

    suit appeal yourself they limit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def change_working_directory(path):
+        os.chdir(path)
