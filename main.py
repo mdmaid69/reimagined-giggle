6b4a9cfeed5d14fbc1bf01a65023bbe0c18e3660
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:18 +0000

    dare related release arrange flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
