caa24502ee3baf6fe95b107095e83a16a531877e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:11 +0000

    flavour reduction to talk safe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
