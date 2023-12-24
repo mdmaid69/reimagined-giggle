315674e57a22e3396bfb2d46dbcc752ced433a1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:07:44 +0000

    fairly stupid dear behind within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
