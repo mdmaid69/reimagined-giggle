896e5b8f171d205535544144356fb2d135fb7f33
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:24 +0000

    tight really polite capital advise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
