e03adbd4aa2a42569e40dfb8c6e2a5a5750405d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:47 +0000

    sport laugh statement programme female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
