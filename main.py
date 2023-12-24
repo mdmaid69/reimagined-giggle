d8ce888d76eb61bfb4499e486c4b91dc45819e29
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:39 +0000

    excuse place used rid guard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
