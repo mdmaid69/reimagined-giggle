4e22dc02044a4e511b17c7c90aebac519718256f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:43 +0000

    me adjective shout left runover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+text = "Hello, world!"
+print("Characters:", len(text))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
