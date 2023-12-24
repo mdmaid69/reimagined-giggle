cc6b5b392c492607700e030bdc292256bf93bd49
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:02 +0000

    shock realize side cough year

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
