343eca35528b5f21c2c70b5134a7a4e438af42d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:29:32 +0000

    small bringup the goback(to) holdout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def exit_program():
+        sys.exit()
