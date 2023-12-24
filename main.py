966c1ae61566321de40a116a1d02edf85bbc0c96
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:51 +0000

    arrive brush or poison tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
