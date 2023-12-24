70b49b8fe6330af2dabddf64db7c90371c27a63d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:23:40 +0000

    transparent almost impression birth next

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
