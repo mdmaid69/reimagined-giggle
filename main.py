9a8bd1c9f751acb47d7032e439a78747d650a550
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:04 +0000

    protest represent afford flight low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
