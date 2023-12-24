317c73058c3504801ce1a900ac3d334d0965c3ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:48 +0000

    shout success police business slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+for i in range(5):
+        print(i)
