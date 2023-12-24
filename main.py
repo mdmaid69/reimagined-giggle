f96cccde50acc337c488b4073db435ac91869b88
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:00:14 +0000

    empty summer government love tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
