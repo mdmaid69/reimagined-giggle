b8864714c8acacf37159c825f6597ec5f0680330
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:22 +0000

    wonder drop certainly I walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_str(array):
+        return str(array)
