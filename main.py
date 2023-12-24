641b7e5147184773f5c3f71991d23fb89e1975ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:37 +0000

    satisfactory occasional slide reduce hour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
