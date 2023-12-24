22a516ca7e52a085d3bfe77667e2835930f7cd20
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:23 +0000

    hello eager bet side introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
