e7a10448fbddbc3a58c4d05f39ea8c517eb72e79
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:51 +0000

    belong worry expect Ee success

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
