9a93b94c7b77d10483d9dc57a25bc58be7efa3d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:26 +0000

    fix impatient collection genuine learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
