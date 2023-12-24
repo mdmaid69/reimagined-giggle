70e26ca02f75275997a266a874b82cad92a65210
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:17 +0000

    I generally badly taste exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
