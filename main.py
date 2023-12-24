aa0d035508ca706c267500d78666b7ef1b654c29
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:48 +0000

    mine ride strength my drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def count_elements(lst):
+        return len(lst)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
