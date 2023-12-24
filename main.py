9711a0c4726e3af62db1237fd2386326e4bd7e81
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:14 +0000

    me among until division score

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
-def count_words(sentence):
-        return len(sentence.split())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
