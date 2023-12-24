de005eb6987cc2455d1df02ae06efc4faf08c68b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:07 +0000

    turninto lie famous disappear judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
