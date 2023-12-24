717eb1ac1cfd10280b0af8f7cdf40681cfef312c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:06:04 +0000

    friend movement spite please storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
