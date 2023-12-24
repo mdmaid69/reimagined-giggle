792867b12cece6dd8384bd112c9c923b6710a015
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:40:43 +0000

    wrong convince interest night play

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
