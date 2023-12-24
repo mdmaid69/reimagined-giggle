bd1af25825da0070cf7340158b3df73701f9b5e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:44:17 +0000

    difficulty sound lookup broken drop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def square_number(x):
+        return x**2
