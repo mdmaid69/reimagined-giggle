3b4707384bae51f324968409dfc1c32497cfa4ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:05 +0000

    excuse try road suggestion keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
