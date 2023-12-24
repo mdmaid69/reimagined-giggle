b945f4ced8c8d840480273c113ca521687cc9992
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:21 +0000

    behind we moral anyone impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
