4d4dce6403b3ec552eb93794d6467dbebbc89541
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:17:42 +0000

    part think benefit break float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
