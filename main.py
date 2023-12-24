7909ea4414ce458fe84bcc874db84cf8ebe4dbbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:32 +0000

    consist error decision relationship between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
