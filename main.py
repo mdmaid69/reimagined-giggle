541c94bd3a7ff99f23c8e2e180e1fce132e518f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:58 +0000

    fill unusual pay protest cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
