c6309822706254191749cdd9170224eca6e6c056
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:30 +0000

    569enjoy street surface best pause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_typecode(array):
-        return array.typecode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
