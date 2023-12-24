424de9e929f7014ea25ab4386f4c8057312d89ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:57:33 +0000

    experience tear valuable available type

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def count_elements(lst):
+        return len(lst)
