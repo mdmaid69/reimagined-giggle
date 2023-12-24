23f0834cdcef0b9e29da028f5147673ff0b42d82
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:09:43 +0000

    crash every fair protest write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_speed(distance, time):
-        return distance / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
