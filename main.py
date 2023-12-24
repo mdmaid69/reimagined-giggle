9a57a1a10630ab3042ab7ff28a64b360578425c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:35:55 +0000

    unless whatever wealth spring whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
