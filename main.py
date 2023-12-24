6d55a095261d8cec414e67d3866a82c28fea4974
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:27 +0000

    there close workout warm matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import time
+def get_current_time():
+        return time.ctime()
