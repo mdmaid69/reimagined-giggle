5ae8d0d2d3cd0530e22c6ae60139eda70e07472d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:28 +0000

    human cinema lost agreement callback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
