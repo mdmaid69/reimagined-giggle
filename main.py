b2f47be3dc5da74e5950f7fbddecad8eef5256b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:01 +0000

    this sure model pour turnoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
