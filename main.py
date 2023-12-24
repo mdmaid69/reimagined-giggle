bfc2994d896b5001d03ef926a5434f0bad33f1a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:56 +0000

    in glass very theirs agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
