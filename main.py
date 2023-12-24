12b4ea09bb1b79ffccf686bd6941829fb3ab17bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:18 +0000

    move vision ill consult pay

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_work(force, distance):
+        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
