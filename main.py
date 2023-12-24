65e1648065b4552a635dd113c40d2e6950759326
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:35:19 +0000

    cry breakin swing point means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def delete_file(file_name):
+        os.remove(file_name)
