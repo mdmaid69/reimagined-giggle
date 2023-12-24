51d129be9f3efc6a207c9bc72e785ecf5f975cb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:05 +0000

    bite limit spin together star

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_to_octal(n):
+        return oct(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
