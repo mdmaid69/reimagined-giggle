41d4470dd8a7924c3f36b042509eff4a7a149df5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:31 +0000

    course manage clever steady account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
