e2db5ea0112555dcf9b28e52edcc69cdc7c51c0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:04 +0000

    figure gate dick fire public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_priority_queue():
+        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
