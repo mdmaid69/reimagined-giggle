ea9eff7220a11966b740b5e907f1ec9bc2f87181
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:25 +0000

    last disapprove reduction argument weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
