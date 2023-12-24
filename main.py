740befe0618616f30c1a57bf80feff5e1911c4cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:07 +0000

    it pronunciation manage bus tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
