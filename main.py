18b96842b86689b6dc153159a8c9d2b3d2b8e183
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:24 +0000

    field suffer regard earth jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
