1a3a69f5b72f08da778c06ac65158ee5c10afde8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:09:27 +0000

    sharp furniture private fetch fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
