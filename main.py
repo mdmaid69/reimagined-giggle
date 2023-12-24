bc89f967469af559d9194cf66fd2d90e77c0ac42
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:53 +0000

    flow sound takeout out factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
