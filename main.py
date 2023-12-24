4361de082922b14b3700c8f68d4cb9fb4a109d16
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:35 +0000

    press thirsty take square bringdown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
