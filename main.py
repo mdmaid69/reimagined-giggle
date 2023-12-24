8811a1eac87f73c20ea4bb65a430ff0e0cb887f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:23 +0000

    decide crime turninto single strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
