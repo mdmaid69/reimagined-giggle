e81e47e5b97c6d64c17d1ff33252d97329adba1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:11 +0000

    remove combination decide expect level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
