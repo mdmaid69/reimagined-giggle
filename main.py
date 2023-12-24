8cbfa83cc09b11325fd6d808865b77eda1a4d08e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:19 +0000

    thought chance force talkback open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
