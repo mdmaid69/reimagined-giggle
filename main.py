d1f84a586f35932a30d1162783e62b265c8b4a9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:33 +0000

    show gas confusing back may

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def add_numbers(x, y):
+        return x + y
