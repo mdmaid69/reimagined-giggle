65619b7bdd622b7b977a0ddf8d7350ca07be5464
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:26 +0000

    distance expect embarrassing drink runout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
