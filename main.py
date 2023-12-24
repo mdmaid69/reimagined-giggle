c78481b275e346d6f66350f6003ce4a7c5ad18dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:45 +0000

    pause hate expression emphasis contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
