c45a8cc6b1b34538f944e0bb26cf26b763179974
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:49 +0000

    slowly getout weigh near coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
