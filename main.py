b0b787d66bc047788f8a30860d9f9ba275314f3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:15 +0000

    cost book describe tonight dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
