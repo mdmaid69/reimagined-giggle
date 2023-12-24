29a4243a75e1df662ab6eb6bc7bd8dd0a85f41d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:16:16 +0000

    impressed worth statement consist circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
