5cb035b417bf01c5829374536c064ca1be132301
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:47 +0000

    survive grow spare workon watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+text = "Hello, world!"
+print("Characters:", len(text))
