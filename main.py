147ca8dc032d6ccd0d11d8aceeb119c953418af1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:30 +0000

    contact cook value in plain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
