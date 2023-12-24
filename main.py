737455c1990680d1a338e85cb31ebd4e2a2e1f06
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:57:37 +0000

    contact sound artificial style lead

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
