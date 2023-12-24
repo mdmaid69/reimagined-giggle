31373acb23445c939c45088710246cf21f060467
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:01 +0000

    reduce complaint ill yes underneath

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Reversed:", text[::-1])
