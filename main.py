f4e677c8e0d569664393635ee168fd30146e09f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:26 +0000

    pure combine hold path giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
