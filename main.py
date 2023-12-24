85c7e41095489ccdec69035c099b4ae837e6de42
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:04 +0000

    science respect nor giveback feeling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 def calculate_force(mass, acceleration):
         return mass * acceleration
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
