6acca7315173e01e306e18056af6fde5ad7e9c78
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:27 +0000

    safe side luck collection load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
