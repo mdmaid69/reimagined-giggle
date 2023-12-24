f8178be112bf57a3aca485cd206a5dbf0d80f154
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:00 +0000

    equal own open past find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
