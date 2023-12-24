0e4b1d90b422933b0fe7edefcdaea8a414ed902d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:57 +0000

    precise little cry meaning excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
