27b3ad0f5e78fbda1a3bd549d9f0088cd46a6606
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:21 +0000

    force production under rid objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
