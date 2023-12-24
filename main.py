c76a4191e2b99924af18abeac85e8052ca84e796
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:57 +0000

    promise hair quit give exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import xml.etree.ElementTree as ET
 root = ET.Element("root")
 doc = ET.SubElement(root, "doc")
