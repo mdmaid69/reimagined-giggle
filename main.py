629df35514702b579c574c98639bd7febcb7ec5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:48 +0000

    pupil window platform on definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
