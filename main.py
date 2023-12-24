e96721f21b87467b1ed21abb626a7169b38a69e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:55 +0000

    touch bus business less flood

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
