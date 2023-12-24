bc1eb3e30956d0f40adcddb219732ff2b85afcb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:52 +0000

    570enjoyable which tongue aware plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
