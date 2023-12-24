8b5ba48574ee6dc25da4155e8d169dab053d0941
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:24 +0000

    ask deal several single blow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
