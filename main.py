6b0dfd9f62112a6ea8a25cb5e22a06eadda93e0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:41 +0000

    boil lookup fold bringback can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
