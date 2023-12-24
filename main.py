ad83c878e521f46fa8bff1d6017ed01a87c92d71
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:52 +0000

    pleasant dead service wild song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
