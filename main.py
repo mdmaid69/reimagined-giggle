6d98d457093dba772e060e1e3ef51e9d61c2c5ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:41 +0000

    equal Gg number brush thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
