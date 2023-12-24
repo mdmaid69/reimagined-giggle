afab2f9f6c93c2d9b8a7729208f8a1fa5a9c5697
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:35:40 +0000

    repeat reserve appeal throw quality

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_sine(x):
+        return math.sin(x)
