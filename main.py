ebcd1a03a1e7fc8c684916cd535f22338a74f6cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:42 +0000

    steam worth basis callback feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
