6e2e1f389c83c2d27b71c9e3508616ae278838f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:11 +0000

    preparation before refer ready account

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_area(radius):
+        return 3.14 * radius * radius
