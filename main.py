331778bb4ad4b1e8b954d0463958e83e046b8fbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:44 +0000

    considering money abroad damage tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
