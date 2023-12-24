e06f30a028e77e63eb3447ffe0f47968a10ff725
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:53 +0000

    turnon question record allow pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
