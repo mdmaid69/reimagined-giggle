6ca14e33702727b2afaf07bc6044b415b2cbe757
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:52 +0000

    scale sudden furniture side turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
