0cb73a435f21ed876e3cfedc109e7e2339dd9c27
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:08:51 +0000

    interrupt building happen measure record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
