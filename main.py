91f5183af794ab8e068ac6f73f1c2a3030c02c63
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:52 +0000

    lookaround friendly mother shoulder matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
