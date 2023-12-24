f4be10c0d9af10ac270d45a1cb194aab8db43317
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:01 +0000

    certain with arrangement system busy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(math.pi)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
