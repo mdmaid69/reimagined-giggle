31f302e5aebde21155aa418467a0548e83908faf
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:45 +0000

    project measure bite lunch ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
