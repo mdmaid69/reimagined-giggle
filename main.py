e2cc9177839859b98481695909c9f2a8d498ebff
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:10 +0000

    beyond car see perfect under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
