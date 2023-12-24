e34ee30632656fec0e496be5794c831ead06ade4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:40 +0000

    call nurse situation keen laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
