f558f3616a92080620b0c358e84343f8570befcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:13 +0000

    shock interrupt violence aim conclusion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_typecode(array):
+        return array.typecode
