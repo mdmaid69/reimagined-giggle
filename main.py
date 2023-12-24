a177c8dffc20bc2518efe92fef347254a37b4458
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:51 +0000

    worry hide glass disagree after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_area_rectangle(l, w):
+        return l * w
+import sys
+def print_python_version():
+        return sys.version
