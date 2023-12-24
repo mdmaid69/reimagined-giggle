efe70503371482c4aecd15f5d8c9076458ec3808
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:42 +0000

    drop talkoutof train brown full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
