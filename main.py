44aa3b4106828ff0fe5b4fde8651b6e60716fba2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:34 +0000

    interrupt dear snow search cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
