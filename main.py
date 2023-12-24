9c613f53907ce66631122dbc53a3a0575af73ed9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:19 +0000

    deal press keepout about skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_itemsize(array):
+        return array.itemsize
