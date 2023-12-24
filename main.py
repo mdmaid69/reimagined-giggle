d598c20734cfeaceaa19c941da4012a920fbe9b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:32 +0000

    protect connect replace happy accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
