8c94018d0173bc405100aaa5eeeb0c16bd67726f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:40 +0000

    number blame swimming be someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
