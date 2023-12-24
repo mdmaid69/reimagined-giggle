071edab69574f24ede8aa58cee23682342ae5bd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:11 +0000

    ball its long carry car

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
