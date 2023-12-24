b67817f5c6a8753d4420e058f4f608212c65c2b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:01 +0000

    serious mass connection embarrassing afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
