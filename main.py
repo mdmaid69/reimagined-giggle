05d0c64961c9970f87abd463674699ba4c1f937b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:16 +0000

    awake listenfor exit bet describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
