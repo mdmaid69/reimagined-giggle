8bc44c5f8e8fb6f7461a7c0cd93e881f26c529ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:19 +0000

    visit interest there special thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def reverse_array(array):
+        array.reverse()
