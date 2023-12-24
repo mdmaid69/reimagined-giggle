62c0af91b1e5ad0c0e0a409501ab613165930351
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:01 +0000

    discovery book dry pride hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
