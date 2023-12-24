6a8bc01a8dece1d213ff1a1a736ffc3c9ae74bdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:11 +0000

    lose lonely position congratulations tax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
