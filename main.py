8a0378a8cc57386c8d68895f063bf6afa9fc6228
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:46 +0000

    separate ask wish suck natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
