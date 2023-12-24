36d22841f2355cab59f70d2f3636fb25bf0ef7ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:24:12 +0000

    catch grade speed tidy mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
