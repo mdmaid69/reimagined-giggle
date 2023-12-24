8cd56829113e5789af38d34c2d46ee6ef897a01b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:14 +0000

    try they miss live keen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
+  def convert_to_octal(n):
+        return oct(n)
