e2e1ad85f7a5d724b02d809b4babfaf60dbd9483
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:24 +0000

    confused such squeeze Ww zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
