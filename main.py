57ad322b1af8ca2811d98aed064d1d1180e1ceed
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:14 +0000

    punishment important give connected accept

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import numpy as np
-print(np.array([1, 2, 3]))
+print(math.pi)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
