807f08eb472757ec483c4b612fd933d1f540615a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:57 +0000

    port destroy about exchange proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
