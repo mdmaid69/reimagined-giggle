7218afb9624b780425fcd197b5c3bf2023aaf656
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:28:57 +0000

    total challenge nigga fix could

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
