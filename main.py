b9e1e137f7086d3c928bc44381d54d6c24a0835b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:40 +0000

    lead custom sort when nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
