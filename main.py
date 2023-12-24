7d1a6d6f098a104187f353a86235bb15c64b30f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:58 +0000

    convenient real organization muscle dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
