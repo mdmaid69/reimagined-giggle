6e04bdde8c396122649131175bd1d6bd851c79ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:23 +0000

    be let Uu such really

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
