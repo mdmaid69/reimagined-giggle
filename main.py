cf412e83c606035f5666bf5f11d222669286b3d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:19:04 +0000

    seat male clue strange oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
