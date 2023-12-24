21c060d747d4e762365b794a06f3f7abc1644fde
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:04 +0000

    and concerned equipment community desperate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
