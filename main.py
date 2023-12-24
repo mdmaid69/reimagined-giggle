ca1d7e114312874bb51c517bb1714e66fc70c468
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:11 +0000

    OKorokay typical operate burst outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-for i in range(10): print(i)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
