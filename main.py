5ad88d3cb61165eb706d99d12e61993514a04461
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:57 +0000

    shop turnup activity mouth alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def is_even(n):
+        return n % 2 == 0
