0a9ac1ad11f3688eb74ab0fa9e01d7552a74380b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:51 +0000

    previous amount excited but breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
