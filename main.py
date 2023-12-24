9b63a77ab8f49a9cc447034fd4384c17202267ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:54 +0000

    own perhaps squash neither foot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_error_function(x):
+        return math.erf(x)
