c10d8423ba68bb1fc7d9a2821d274bae62e4e394
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:26 +0000

    breakdown offer exhibition above experiment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
