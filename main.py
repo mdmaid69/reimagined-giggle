fcaeb7887f01fe798404a0950af69bee504f2765
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:35 +0000

    all state miss shrink no

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
