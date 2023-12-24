d85174335a2c0d3d4151cf1b9b2ece0d75924e14
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:35:57 +0000

    freeze amount tend board believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
