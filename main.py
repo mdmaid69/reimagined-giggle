ddabc099b7f3f9c8a35ea885c6ac9ee023e2d075
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:02 +0000

    your section maybe miss turn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+print([x**2 for x in range(10)])
