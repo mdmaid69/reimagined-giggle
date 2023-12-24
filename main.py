05e7bea2ef2af7ce92294403e138301b4f23f9ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:22:58 +0000

    afraid copy Zz move dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
