6377a5fff6606f874f91f4b35a49d73459b79e88
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:07 +0000

    attempt agreement slight sticky size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
