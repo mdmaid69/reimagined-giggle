54bf4692109841246118f9f7a7767fe10f79a1bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:30:04 +0000

    tone hope taste pinch degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
