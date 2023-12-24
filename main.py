515fa791cf42027ddbec6d7643693a032a560dac
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:21 +0000

    chief complain call generally separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
