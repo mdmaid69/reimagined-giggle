1158aec0e13cfcec0468af8b632d8b3c73012e82
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:35 +0000

    money dance injury Gg human

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def reverse_array(array):
+        array.reverse()
