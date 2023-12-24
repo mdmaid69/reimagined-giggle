0eb24fdc896ad8e4d7268cf02ccd9ae808600cd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:21 +0000

    various copy string stay new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_queue():
+        return collections.deque()
