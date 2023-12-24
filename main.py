7c9f6c10518c0a995e340488ee950cfc743de780
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:05:32 +0000

    range poor middle favour of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
