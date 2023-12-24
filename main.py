56ad131853bece02709dbe09b6fe15ebfa1e13e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:52 +0000

    welcome film guide majority centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
