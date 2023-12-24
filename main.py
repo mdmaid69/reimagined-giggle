2550233211fc971793cef4876dd2ca69fa39019a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:48:16 +0000

    spill measure settle afford myself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
