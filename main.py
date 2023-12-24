7e1348594e9401aee3b8da429cca3bd7fb8f5e56
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:02 +0000

    fierce illness stand admire blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
