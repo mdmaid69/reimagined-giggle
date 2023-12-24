d5a0fe9c4812032737fe25a21b171903b2868098
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:06 +0000

    entrance further pronounce could promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
