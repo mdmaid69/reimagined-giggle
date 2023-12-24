31b4d89f961482a8241dec5c8a2b643b12ec6909
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:19 +0000

    doubt bite eat sort run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
