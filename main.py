da3ec2c951047058463d206d7d4f9ab6b1f4bd01
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:53 +0000

    reduce noone result value example

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_average(lst):
+        return sum(lst) / len(lst)
