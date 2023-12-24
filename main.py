5248196908df093e642e2f77018b925e608d10a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:52:08 +0000

    holdon regularly might splash lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
