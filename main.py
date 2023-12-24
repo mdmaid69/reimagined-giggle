53dcb17c5588787131efcb07072f0a809ed50e83
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:16:46 +0000

    talkinto avoid foreign may occur

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def is_palindrome(s):
-        return s == s[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
