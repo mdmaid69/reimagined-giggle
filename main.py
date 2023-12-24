645f8a8eb17955368652fb96fdd476b9dbddce13
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:41 +0000

    sad risk string unlikely black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
