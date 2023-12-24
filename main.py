ad1d8c3ac017f7c69e32abd6386916af46363458
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:05 +0000

    problem temper responsibility trade post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+  def add_numbers(x, y):
+        return x + y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
