edca99c0108f5c948ed88db7f9c406c941de5f3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:23 +0000

    charge regret swallow guide clever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
