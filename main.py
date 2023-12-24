cea98b66e76a43432cd0a909ac3630636fa95188
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:33 +0000

    section excuse commit appeal ticket

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-for i in range(10): print(i)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
