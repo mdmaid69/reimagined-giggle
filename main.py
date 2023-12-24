aa54c70716d513aabaceef43908026b37530bd38
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:59 +0000

    sit throw gate breakthrough Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
