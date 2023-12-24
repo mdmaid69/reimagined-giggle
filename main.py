fbb383f138bf4ded6630a1c8afe51d7a772fb9a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:29:49 +0000

    challenge care shock fold mad

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
