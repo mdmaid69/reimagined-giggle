8abaaef07b91319116960a2db5067bc12d42c0d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:56 +0000

    sometimes Tt guide suck lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(lst):
-        return min(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
