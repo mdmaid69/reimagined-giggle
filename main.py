49010deed07729fd0f771e05815c27cbb555e39a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:30:01 +0000

    uncomfortable later switch tax satisfied

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
