f17ff24c025dd94b92fa4319f4aed87acc8af265
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:45 +0000

    flat hurt giveaway teach cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
