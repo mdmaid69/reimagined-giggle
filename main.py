b669ab9c660d2d4832a479f50ed59009a4adf5fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:25 +0000

    blind Mm glad mind final

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
