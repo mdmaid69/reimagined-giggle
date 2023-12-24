6e09d89557392476ee419946383a528a866433e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:42 +0000

    relation at usual meal thumb

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
