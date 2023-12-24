3837b6ec395f60d9553fb873bc54e0bb697c69d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:23 +0000

    belong touch lost somewhere score

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
