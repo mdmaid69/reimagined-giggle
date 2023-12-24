02108a1c461cd39ff3ab843c63fcb8ba61422d01
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:03 +0000

    sore within sore snow watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
