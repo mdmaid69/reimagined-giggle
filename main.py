2a5cead6b15d94ad222509c0020e4f15080581fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:48 +0000

    deliver calloff especially crime birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def split_path(path):
+        return os.path.split(path)
