0dc6853351b716dd965fab9f79732d7d56a0812f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:51 +0000

    far except study turnoff pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+for i in range(10): print(i)
