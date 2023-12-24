2274a4417f1cc1eb1d493931f432ce7a177439e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:29 +0000

    attach trust prefer that good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sys
+print(sys.version)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
