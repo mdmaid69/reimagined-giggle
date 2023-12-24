b4a0e31422db7800b736dbb4f1fe3ea0509038fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:38 +0000

    slight convinced loan telephone learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
