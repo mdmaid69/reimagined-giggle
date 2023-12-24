e2c2b682309c93f01a5b275b6ecf60896b9381c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:37 +0000

    large work ilovedicks disgusting Tt

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import getpass
+def get_username():
+        return getpass.getuser()
