be0b26f1fe5d83d427e1414ec1d88e8dfa3c9f5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:35 +0000

    unlikely measure furniture common my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
