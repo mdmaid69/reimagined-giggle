7e5f72c4fdfcf363cfdb9440bfe66e7879a87ec5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:00 +0000

    table air reject catch recently

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
