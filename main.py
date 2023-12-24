377233afd1e8e9eae53f64fefb03376486ad1d44
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:03:51 +0000

    improve Pp below spread accidental

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
