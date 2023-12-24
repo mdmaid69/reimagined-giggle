217ab77e86e16b25614fac283fb82d7ad5347aef
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:10 +0000

    nothing first name file sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
