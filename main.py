d7fef09a103a7d129be0bf0a79a077d2037c6884
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:35 +0000

    pull add contrast turnon 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
