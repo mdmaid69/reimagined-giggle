217abc61d437bf45b61b9773dd2aaba13fa3b931
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:25:44 +0000

    shallow design test slip runaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
