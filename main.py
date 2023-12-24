220d952b3bf34c49722fc7781f31e50249dfb57e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:37 +0000

    schedule time trap compare turnup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def subtract_numbers(x, y):
-        return x - y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
