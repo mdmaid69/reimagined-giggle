ea956e64b56b8ee16ca8adb4f4f880d0cb0607cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:24 +0000

    could drop reliable butter film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
