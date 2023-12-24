f4acb853c7c6de56685c2f9c7f35749b1648d473
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:26 +0000

    drive person deserve related principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
