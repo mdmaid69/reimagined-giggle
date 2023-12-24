40e540cbd862650f5d5d32c653a9bceb60c56b13
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:49 +0000

    flow accidental wise everywhere once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
