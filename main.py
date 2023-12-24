370baf6625253bad31f1b97b5b2799b0dcde0697
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:08 +0000

    fear month right grateful test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
