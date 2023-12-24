3e2768273067486372c69ec2899e134e01e18b1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:26 +0000

    switch region firm team confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def change_working_directory(path):
+        os.chdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
