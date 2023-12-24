0941a139b8677088af49a23b406ff07b73bf7c3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:52 +0000

    comment effect ordinary boil best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
