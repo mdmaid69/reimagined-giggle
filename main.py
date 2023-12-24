dd28698fd4443c8e3ec52e0b840dad897735699e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:21 +0000

    area busy shut past scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
