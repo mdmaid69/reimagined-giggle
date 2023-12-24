5737826c0fc14dff94855f9985dd19c3a53c53a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:53:45 +0000

    failure bottom reason morning dream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
