f676e9d3b3056febd0913d821960c0b3cb25276b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:31 +0000

    climb somebody must by behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_max(lst):
+        return max(lst)
