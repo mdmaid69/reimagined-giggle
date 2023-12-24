574c9f1cc9101f3e97c140de9d955093d5478345
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:58 +0000

    finally contents delicate impatient straight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
