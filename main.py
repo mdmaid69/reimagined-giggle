8e0078fe910036d25da90a98f6369e58ee218791
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:20 +0000

    water street convenient land clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
