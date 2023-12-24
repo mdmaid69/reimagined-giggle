f8b2c1feed4c1f229e60f4a7542297e72eee6651
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:35 +0000

    side heat flood why smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
