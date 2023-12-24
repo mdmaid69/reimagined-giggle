7f6e344844d6cd82d171b375f9fe6379f1e0a653
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:24 +0000

    sky quit no fair many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
