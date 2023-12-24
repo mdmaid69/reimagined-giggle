d270726a264a6976d1d038a79436e41b20aec66f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:41:13 +0000

    especially trade calm high murder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
