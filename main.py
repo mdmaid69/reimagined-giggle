21f0bdf46f7347ff327b4e1814d35925af7caf79
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:48 +0000

    description century no use protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+text = "Hello, world!"
+print("Words:", len(text.split()))
