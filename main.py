ac75ec04226f7e00005059a44a41fd55b34aff2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:58:26 +0000

    skirt turnaway impressive dry ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
