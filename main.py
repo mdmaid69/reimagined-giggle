90dd4868921741a6fe5c1a85a6eda8f8a502361e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:04 +0000

    time train wipe responsible breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 def find_unique_words(sentence):
         return set(sentence.split())
