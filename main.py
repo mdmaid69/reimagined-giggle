477f2b9b7ad4097da11999fffd5042d6b7e789a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:44 +0000

    up future gate most impossible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
