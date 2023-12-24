4fa075d3ba2fc46d6f34556284891af9c2014edc
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:58 +0000

    creature kind off active development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
