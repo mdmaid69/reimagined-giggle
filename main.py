2e7a1f801798d7f316e7e24bab94dd6939cdc133
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:52:40 +0000

    drive pupil minor contrast pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
