8ef6fcaff2b248c347dbc6f28c4f47f499fb83a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:27 +0000

    always question remain easy doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
