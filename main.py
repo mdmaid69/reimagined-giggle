e516924e6ba5864c3f31b1560da40ed1e5cb3167
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:46 +0000

    official defeat suckme ocean callup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
