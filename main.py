e35a4c039653149240fc774e641e04848ecd8e39
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:41 +0000

    advice fast earth tall wash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def cube_number(x):
+        return x**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
