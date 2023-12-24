6181c563444b1c7809ce8e25b23f0971e901a5d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:06:56 +0000

    authority circle level undo child

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
