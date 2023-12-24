0dca08dc9db7d456879e9968a14a8b51ebcf93eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:21 +0000

    fast combine detail easy comeup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
