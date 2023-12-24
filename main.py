676b1e271eb3907ca75c7cf7a850b813b9cf4ff8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:33:58 +0000

    goaway turnout black stamp rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
