6b75c33c6c7cfa3387f526a033533d676b08605a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:42:37 +0000

    chief substance probably takeaway expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
