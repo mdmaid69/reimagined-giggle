d49ccb7802864af841c5cad86ad62252ab29b03a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:18 +0000

    exercise face worth week complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
