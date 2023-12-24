56d93ec1a50614358537bf77f6452d96ae1bc632
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:37 +0000

    nearly soft somewhere whether train

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
