be34e82712bd818038b956df7e6cc4a1b2112a21
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:00 +0000

    ever middle do most duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
