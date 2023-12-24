ee5563059b105bb01f87df8ac86b028125419301
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:03 +0000

    free accidentally 570enjoyable quantity none

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
