789c7c18514bff8eea2f6b1542cd2126112b2e76
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:30 +0000

    bury strong careless around fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
