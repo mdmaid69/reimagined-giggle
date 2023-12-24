12833194340c1deb179b86c7f9f5f57165bf327d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:02 +0000

    man everyone sting morning we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print("Hello, world!")
