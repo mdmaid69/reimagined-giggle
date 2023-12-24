b6931a2e2c6810d8e7ea37725e89d98ebbb4ba7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:11:00 +0000

    positive invite field same back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
