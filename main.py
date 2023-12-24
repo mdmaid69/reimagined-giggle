10caadf60a471d085792da33518ab19c9607dbca
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:07 +0000

    whoever imagination information manner getup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
