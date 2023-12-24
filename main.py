3e8b73a7986005f385131a896bcdf5f3f46eff94
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:35:17 +0000

    especially stamp behind communicate nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
