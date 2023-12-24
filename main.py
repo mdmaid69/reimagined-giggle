fbee8b313fc7c43a3f53e62d26cada79915cdbbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:09 +0000

    indeed get organization walk tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
