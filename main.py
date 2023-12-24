8c77bc0a1cfe06f71edb5f7260f8269561e44e06
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:20:40 +0000

    rare stranger possibly stay everything

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
