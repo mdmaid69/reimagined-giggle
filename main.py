91dcade2a2dd38443a1279030f00e152c552e28a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:23 +0000

    level close design yes obvious

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
