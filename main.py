dccdd9f0aaba1d581f81a2df82b4de863ea1797b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:15 +0000

    visit art method please face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
