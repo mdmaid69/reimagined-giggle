ab62bee13347281d3b54c25b0d5fcc6ba79c5ba1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:28 +0000

    independent sail unusual slice never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import array
 def clear_array(array):
         array *= 0
