66b845ed4c62b73ddddb689b19cf847ce9013745
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:54:54 +0000

    before favour frightened reduce combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_string():
+        return collections.UserString()
