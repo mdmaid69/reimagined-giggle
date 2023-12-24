14f4c290fab1fc0f6b1e479681480ac1d45e250c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:47 +0000

    upset whichever dinner group wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
