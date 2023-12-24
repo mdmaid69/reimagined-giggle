0dee06dcd255fb78ffb0baa40f78ea428a0356d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:09 +0000

    mail bringback throw shame mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
