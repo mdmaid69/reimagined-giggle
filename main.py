a998143a9e5b4ba0c3171485e5422f03c68eb1b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:04:08 +0000

    callat performance mass before Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
