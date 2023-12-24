b4dd232a4abe357e2b8e033c771a2d07de2d3fd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:08:34 +0000

    alone return unfortunate repeat only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_float(array):
+        return float(array[0])
