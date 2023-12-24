6104b0498c865655c30e87c9d53e99a22340b883
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:46 +0000

    immediate calm kind fire excuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def find_max(lst):
+        return max(lst)
