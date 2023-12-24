36e8edb55451b7595313a8eecccab4c222c075d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:51 +0000

    become train goback(to) travel swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
