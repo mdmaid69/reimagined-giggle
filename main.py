949b97d629afeacd8f91ebc0dd227d7cc197d8e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:29:08 +0000

    mine sleep happen glass level

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def is_odd(n):
+        return n % 2 != 0
