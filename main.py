0c60755ea49df031eb4b546cc9349ae769ed67ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:33 +0000

    far side store crisis lookafter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def is_odd(n):
+        return n % 2 != 0
