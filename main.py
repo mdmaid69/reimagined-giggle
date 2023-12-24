0464f8399653f958ee890cb2b43fbd5d94ecbcd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:49 +0000

    broken cinema tour belong runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def count_elements(lst):
+        return len(lst)
