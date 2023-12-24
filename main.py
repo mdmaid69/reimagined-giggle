24bedc940d1a65bc8ced3e3cb402cba93346fa0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:20 +0000

    few whether quantity welcome takeout

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
