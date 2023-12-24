1d265b87412e14bf00d69d17a676fbb58d6e7756
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:21:12 +0000

    country determined style unit odd

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_set(array):
+        return set(array)
