eeb94a3b463dfb02751ee749364ae0faa750c1dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:10 +0000

    dry side variety record bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+def print_python_version():
+        return sys.version
