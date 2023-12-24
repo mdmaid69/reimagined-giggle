03875747200877ac62270cf5015e348fb3be5ff0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:59:43 +0000

    already deliver long nose rush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
