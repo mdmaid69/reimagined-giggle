22e97a180a5d806c2352450c931cab536d5b91c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:41 +0000

    unit own forward care dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
