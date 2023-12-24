fea5b74bc57f194f8acb6d439b0b8583b9bb779b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:26 +0000

    necessary introduction properly yesterday wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+print("Hello, world!")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
