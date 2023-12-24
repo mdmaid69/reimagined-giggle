2d33e191cbe0104a6ff013b864d4c50b59fe1ffd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:16 +0000

    impressive keepout translate off by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def get_current_working_directory():
+        return os.getcwd()
