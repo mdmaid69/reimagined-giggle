5204e840966449575ad8c190aecfebac67d4380f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:33 +0000

    arrangement sting do pair another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
