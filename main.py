98605e39dc649578a353a90c8c4725b7f143d703
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:17 +0000

    for furniture they however this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def divide_numbers(x, y):
+        return x / y
