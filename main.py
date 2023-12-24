200bdb3749cfbf7d5fcdea70e3e7ad355c6baac2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:24:42 +0000

    recently disagree miss commit underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
