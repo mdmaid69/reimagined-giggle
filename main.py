2f0fc0079c8e1ead516e063ebafbb8a2a64b32d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:12 +0000

    inside invite comfort wind passenger

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-print([x**2 for x in range(10)])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def is_even(n):
+        return n % 2 == 0
