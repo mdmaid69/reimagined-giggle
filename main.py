1bb91c7793de58695d232cb93e8aaaa37c3135b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:45:40 +0000

    easy prepare development stand now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
