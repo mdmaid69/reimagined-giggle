1eac011bcb2cd7637594ce6df6c52673cdfc43d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:51:37 +0000

    breakout favourite sharp world hot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
