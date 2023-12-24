882071e4725ca811e220e08bb2d31440ff927c35
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:41 +0000

    memory suppose heart gooff trust

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
