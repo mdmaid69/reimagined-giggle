bbee70f328b4431b7758d5c88adf735f60c881fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:19 +0000

    cut later break than union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
