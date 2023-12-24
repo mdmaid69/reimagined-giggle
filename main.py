b52d78ed8dc3a9ca74a08fc7f645c76ffa365855
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:30:54 +0000

    weather activity high mistake power

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import time
-print(time.time())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import numpy as np
+print(np.array([1, 2, 3]))
