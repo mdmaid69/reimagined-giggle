60cff974b9985daf0eae43e1bcd062e836e7faf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:35:44 +0000

    tradition finish finger minimum man

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
