0bc0beed8134702151c7148ef1029ad68f568c58
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:20 +0000

    rain rain repair kind weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def square_number(x):
+        return x**2
