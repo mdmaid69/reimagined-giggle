4c2cb0e853732eb23d2d9c2f5ecf9d9851bed9d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:03 +0000

    splash drive long runover progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_odd(n):
+        return n % 2 != 0
