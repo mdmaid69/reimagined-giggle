c21f5e5cbe583ba426ea37e51fc12edd5cb90cd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:22 +0000

    now very person exchange past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
