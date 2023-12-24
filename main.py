9a6cb98f4749482cdd0b2e84c181fd625a4ca0a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:06:17 +0000

    rent winter rate edge family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
