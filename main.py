e5a090897f58e2ed26da903ccd394623c49ab50f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:05 +0000

    rather reliable nurse approval gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
