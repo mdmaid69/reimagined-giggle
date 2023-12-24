8f17d195bb12c399989d64aafe9a063835604ea3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:39 +0000

    calloff that left visit Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
