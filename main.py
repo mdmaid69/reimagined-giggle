1aff314a4647e7a3da0a03a0bdc0e0a1cd3a616e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:12 +0000

    drop practice war action traffic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
