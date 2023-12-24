ba16b68695d2b4f6b161d87c464211f3667fccf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:12:04 +0000

    apologizeapologise laugh before tight back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
