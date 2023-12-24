ceaf2082e95736dde372daf0951949a04b7b9dbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:27:55 +0000

    thumb back level contribute shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+print("Hello, world!")
