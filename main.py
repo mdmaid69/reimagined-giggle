77a811c2cf05718e28f7e9853612f62287adfbf2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:36 +0000

    Tt head close scare situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
