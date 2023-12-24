7b2cdd762fd08f1fddc6735cf80954b3efcce182
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:10 +0000

    before fast pour trust area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
