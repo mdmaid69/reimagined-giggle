a392c81a3dd641bf6cb19a8461956f3c57763b5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:06 +0000

    considering on string reason use

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
