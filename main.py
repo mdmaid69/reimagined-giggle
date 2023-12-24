72821bbf59057efe0edc66eb16cf0fbaf1d820cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:38 +0000

    belong theirs modern apologizeapologise deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
