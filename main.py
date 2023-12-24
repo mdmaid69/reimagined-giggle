161c7ea42f459c59b334c7dff6a79d65be88df07
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:02 +0000

    move that holdon side talk

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_work(force, distance):
-        return force * distance
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import getpass
+def get_username():
+        return getpass.getuser()
