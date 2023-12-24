f68c8fa67f488c6e168066b09c42a77ce0242e44
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:13 +0000

    practical demand head needle have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import time
-print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
