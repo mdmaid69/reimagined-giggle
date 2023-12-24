40d99b7aa5594d8ee8e698097bc918c41e20bcae
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:31:11 +0000

    colour theirs usual permission method

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
