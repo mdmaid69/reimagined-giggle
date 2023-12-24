5f3b66c1ff22e1e4719eaaf10e4cf320d397c93c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:41 +0000

    return mistake let everybody almost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
