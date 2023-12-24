33adbcd1ba17f626225d89b530299bc6b054bb63
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:08 +0000

    pin gradually present familiar copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return math.factorial(n)
