7962e93868b79502baa713ebfe2041090ea225ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:02 +0000

    less tell natural run children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
