90ab9ced33394f561138d4f929a16f1d81e64bc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:54 +0000

    lower dream order cheat precise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
