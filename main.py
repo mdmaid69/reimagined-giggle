6b72e98d1381ab9418eda4a402832a57d8b8b3b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:33 +0000

    considering patient minimum main comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
   def change_current_working_directory(dir_name):
         os.chdir(dir_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
