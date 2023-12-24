a8f356927fbf88f395d0470bc68648ac414fc913
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:07 +0000

    might room next soil left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
