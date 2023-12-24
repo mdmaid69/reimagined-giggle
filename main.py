14a8b99938f0dfde30ad486a40fcca7cd9fc318d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:21 +0000

    delicate certainly pleasure turnon cinema

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_min(lst):
+        return min(lst)
+def subtract_numbers(x, y):
+        return x - y
