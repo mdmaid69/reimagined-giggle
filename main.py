64a2f17a2df78db255dd71cfee069cdc5eba1192
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:25 +0000

    anybody wall ilovedicks sit introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
