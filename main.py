e6fec5cbe35fcd3bd07f8f9635f7929a9cd14f8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:40 +0000

    help careful height afford sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
