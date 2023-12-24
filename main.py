8aaaddb53c900b7aed4550c2c89f415532c67a79
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:06 +0000

    Rr old buy read record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
