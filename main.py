0df81eaccfcc344418d863a0cdc3e9deafe24fc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:04 +0000

    dance strong ring watch extremely

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
