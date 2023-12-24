b06d879dd9aac17cf7165fa9c29946992f94a995
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:06:26 +0000

    row either finish stiff pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
