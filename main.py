209b94c050d2caa0206bcd3b2f31df3aba28cdf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:11 +0000

    take do doctor off Pp

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
