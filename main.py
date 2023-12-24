7a375c1319edcd920d4991f8f03cbcb7c6f89ec8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:04 +0000

    complete trip fault pass Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
