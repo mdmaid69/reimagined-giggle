a7b0851943a07a9062c4e208de8714778795e220
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:12 +0000

    influence tomorrow save almost frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
