cc0d550750b72b4c00da5d14f83b4b357554a035
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:04 +0000

    wake what personal popular term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
