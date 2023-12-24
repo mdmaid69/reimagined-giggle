0ce990f4794dcfc655e9d643961276c387cdcdca
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:35 +0000

    there confidence Gg pronunciation thanks

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
+{body}")
