8b52dd13c93800836730c8b505d4d196831ceb5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:43 +0000

    exchange number hot prize brave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_priority_queue():
+        return collections.deque()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
