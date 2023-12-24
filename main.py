a7351d16c4ac2a771faf4c08905662792a3cdb67
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:57 +0000

    mix value new heart on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_creation_time(file_name):
         return os.path.getctime(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
