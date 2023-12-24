706fd6cb0e5e1fb360b56a88497bb8ad02d1d4dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:05 +0000

    tight address fine basically serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_str(array):
+        return str(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
