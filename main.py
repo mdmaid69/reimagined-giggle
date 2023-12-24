575fe07c215abc85e8ea05edc137ebf1445e3ded
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:30 +0000

    bored splash contain no responsibility

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
