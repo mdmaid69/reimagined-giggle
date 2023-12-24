6ccdc3f847d33debaad888bfe4d595a4cb746b7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:54 +0000

    border whistle colour demonstrate blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+print("Difference:", set(list1) - set(list2))
