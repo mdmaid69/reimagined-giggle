2794efb1b3176a1d9a403c4760ed56e90daed096
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:33 +0000

    produce completely reduction down attempt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
