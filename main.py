35755a852a11f0268b71d63bebec03020c377c6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:10 +0000

    scream repair declare whichever throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
