39c2a2538df5f92e8259c1e9ddd944546cbbfd19
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:58 +0000

    mix fit wait settle bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
