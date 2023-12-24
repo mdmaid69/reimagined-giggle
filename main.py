9952779b4586a4a7cf8c359c3fcb0503a7051b56
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:16 +0000

    unkind police scene afternoon equipment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_counter():
-        return collections.Counter()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
