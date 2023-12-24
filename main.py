a6f1d4990fcdf90f707f7af59ae50c6c38e61edf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:40 +0000

    continue motion estimate table entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+print(os.getcwd())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
