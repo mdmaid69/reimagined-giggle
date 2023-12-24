10fe7823a543cd031ded1103127bc86a4bd3d590
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:14 +0000

    butter unable expression minute fat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
