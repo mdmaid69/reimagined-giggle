b32b4fcfb4a8a95e3c2eaec3b7f5afd4a4ddba75
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:42 +0000

    tell rush control win allow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
