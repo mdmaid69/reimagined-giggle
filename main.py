6e0173a38a563bbf9e632a4b113d085cec4ae338
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:59 +0000

    sharp steep protest pattern quantity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(10): print(i)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
