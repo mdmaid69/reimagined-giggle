3000c52f290c2bc1a68355f79dc8b9c6eba093fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:01 +0000

    set else seem train crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_odd(n):
+        return n % 2 != 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
