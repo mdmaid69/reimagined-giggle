6f3be7ba9f8caeebf78fac68c897447bf3b7e60b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:21:06 +0000

    concern direct turnover word getout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
