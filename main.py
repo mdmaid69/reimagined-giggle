d7274cc9940b3298754ce5c5fd361da5861fc426
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:35 +0000

    reach dead block too thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
