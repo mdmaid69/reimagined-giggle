b5153425ccbd6618bdf727797658b08ae1ad6729
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:17:40 +0000

    possibly wait pure ask bit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+print(os.getcwd())
