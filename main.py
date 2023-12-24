f399fc68fbd48bd6c3e14506762f24023aa759a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:27 +0000

    remove emergency statement but Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
