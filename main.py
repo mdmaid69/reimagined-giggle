56ad5b6d1b3b097a77841ffbca6b06658e652a70
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:18 +0000

    television base temper small lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
