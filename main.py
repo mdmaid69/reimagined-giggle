cb7c85625e4eb4affcc6256a46b7132d143bfdf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:59:53 +0000

    pleasure angry closed bound tell

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
