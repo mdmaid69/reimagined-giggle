abe87e6ee4b62414de88eb2c4fa0303532c5ff85
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:26 +0000

    number ocean spoil snow maybe

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_density(mass, volume):
-        return mass / volume
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
