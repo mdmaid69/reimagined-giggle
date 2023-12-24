abdbeacbe28b92f7994930b2d05229655cbce52f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:41 +0000

    feel excited nerve within burst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
