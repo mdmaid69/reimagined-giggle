cc46fa1a6eb8619007ffb3c65d76a6d1cd510e1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:30 +0000

    importance paper press situation war

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
