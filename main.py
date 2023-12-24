a2be9ce065030a82e59f1836d0bbd14f3fb61172
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:25 +0000

    awake anywayanyhow lookaround contact introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
