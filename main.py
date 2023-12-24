4956abfe1ef5f921f5695f87389fa9de0c73d948
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:21:09 +0000

    inquire flow offer godown black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
