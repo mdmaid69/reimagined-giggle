e780a91e3f95f3e51eb95f04773e012b26b32ab5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:11:27 +0000

    scream about ring law manner

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+for i in range(10): print(i)
