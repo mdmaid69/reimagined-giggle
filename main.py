150cefa3270e24893572484deb242afda3222769
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:57 +0000

    control bite cause out excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
