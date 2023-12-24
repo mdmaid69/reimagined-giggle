58ccc833d0733f0865a097f2f788db712850bbba
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:13 +0000

    aware for talk them crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
