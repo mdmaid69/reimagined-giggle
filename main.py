275881461976d36180b4d68d8f79b2912f0d8f4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:51 +0000

    empty order combine aim software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
