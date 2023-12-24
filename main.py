b2ecdf8aa637412ebf21d4eb16e2e020b2b1ff7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:06 +0000

    568energy travel address question rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
