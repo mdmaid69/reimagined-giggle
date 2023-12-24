4529a9d8bf43b53dac3b86057093d2f72c84dbb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:32 +0000

    height interrupt object unlikely smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def square_number(x):
-        return x**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
