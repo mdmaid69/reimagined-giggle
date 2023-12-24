a089ad20c284ded5cd0955d6b4beb0e26c61c16b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:49 +0000

    score correction holdon relative mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
