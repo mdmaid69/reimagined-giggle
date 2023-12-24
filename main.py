d9a6842a8f15086639b495e08ce8b7a0efb93491
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:00 +0000

    window breakthrough provide convincing half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
