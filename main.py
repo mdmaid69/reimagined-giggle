dc0378d0e5a6841d873e0dfe328255105cb5aef9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:41 +0000

    there move punish past acceptable

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
