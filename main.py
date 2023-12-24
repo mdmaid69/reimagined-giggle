b15507ce26d99065d61e19fbdcd886a6cab35cb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:00 +0000

    advantage exist week add play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
