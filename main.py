9f4754627b0f4bf7fa89d4cc9afe53813c8c4f14
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:00 +0000

    contact turn close back argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+print(os.getcwd())
