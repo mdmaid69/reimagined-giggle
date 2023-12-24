16bae43f8dca459c017f081593c8fef7caeee4b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:22 +0000

    exist advice crash similar pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
+def is_odd(n):
+        return n % 2 != 0
