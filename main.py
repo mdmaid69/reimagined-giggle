91b5eb67ffcd1cfefb8402adaabb95c6aa0af924
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:38 +0000

    OKorokay fierce whom country exercise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
