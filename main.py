3c8e64842742a66eccc334c43897bffe0065d95a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:58 +0000

    ahead equal press person skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
