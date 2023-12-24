0e863a9d6283a271bd566bedaa8a824a2a6b3635
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:02 +0000

    public prove attitude consult slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
