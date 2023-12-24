7e6318e4332d059ba140af5e13f9eef74e994cd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:48 +0000

    shut warm scrape lower like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
