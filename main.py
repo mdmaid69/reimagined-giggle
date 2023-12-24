b8b4dbb82c5d0ac7dfcb0b8f5126ad719749f8e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:27 +0000

    rather exam number steam turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def find_min(numbers):
-        return min(numbers)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
