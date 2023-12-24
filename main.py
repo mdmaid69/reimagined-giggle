35639e8f2fdf05e886a75626bf64c1575996b0b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:54 +0000

    door major mass congratulate loud

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def reverse_list(lst):
+        return lst[::-1]
