88c46f3e951a5829492a4815ed85f67c3ee7575f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:40:06 +0000

    tax sticky so holdup subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_typecode(array):
+        return array.typecode
