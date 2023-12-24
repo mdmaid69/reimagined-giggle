1281b7a66eb160fe58e044e04e7b57096ff0161a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:53:32 +0000

    how building stir doubt offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import getpass
+def get_username():
+        return getpass.getuser()
