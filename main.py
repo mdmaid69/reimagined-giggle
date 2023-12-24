6b4420739b61af98c81170597147a08a012489fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:24:05 +0000

    moment free objection past bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
