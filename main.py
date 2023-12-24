7eb06b1f17fe38fd3e640ce9d45c389ff9097955
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:53 +0000

    violent goodbye makeinto draw speed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def count_elements(lst):
-        return len(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
