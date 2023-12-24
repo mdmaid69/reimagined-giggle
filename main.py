a68becab3c8bb8e7a863aabdc3eeda1a13b95f55
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:19 +0000

    possibly way describe cheap bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
