8566ab2cc6c4f81d2465a541b3d47785fbf97d38
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:58:57 +0000

    impressed opinion Cc practise hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
