45da5d2fad065645e814cb45f2bd8efc24c82e02
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:49:05 +0000

    maybe explain head work call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-print(math.pi)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
