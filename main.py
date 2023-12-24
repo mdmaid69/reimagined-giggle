8614909045c7eab5002c190dad5fc44baffc8f36
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:59 +0000

    getoff anywhere wind search attractive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
