ec5ab82dfd86e9bd6ef89c8bcd75069dfa5421e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:27 +0000

    last sell aim might excuse

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
