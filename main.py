c6cf55f79740c8118306f29fa22ca009159d7cf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:13:12 +0000

    special emotion experience Jj stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
