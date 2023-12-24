937cc6216a023041a5ab056155f55f06b9f81ad6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:29 +0000

    hold surprise past whether result

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+print("Hello, world!")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
