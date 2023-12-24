b81922e115fe03d64990bd5f50c5d112efc8cb92
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:13 +0000

    bear anyone else all learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
