46f4e60a21a2be3ecb5776c25a2e871a010a51cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:28 +0000

    suffer actually holdout frightened uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
