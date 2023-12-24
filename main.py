b851cf752056ae143ecd2a9286585a509c9e5051
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:15:30 +0000

    rubbish owe happy foreign tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_typecode(array):
+        return array.typecode
