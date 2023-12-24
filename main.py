16948ac5ead4092307523ce9548d18d663edd5a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:54 +0000

    line back suck pretty except

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_average(lst):
+        return sum(lst) / len(lst)
