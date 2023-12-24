0b24e68e4d0e7a12fc46eb3a8a88c97e6d077e00
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:51:19 +0000

    reaction combine part justice reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
