5973b5d87608a5b25636e490618bec8d2657e1d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:56:32 +0000

    language same software journey giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
