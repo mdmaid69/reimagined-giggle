179011289be1a00ee528d4060c6d66f610665a84
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:46:20 +0000

    immediate fit afraid makeinto rid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
