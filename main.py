5a2d6d63e395a9b01677bd00a0579300317171f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:44 +0000

    offence depend understand Gg holdon

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
