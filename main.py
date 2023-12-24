eb228f1d727c5e86c5cce69ef437793a6fd7c046
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:57 +0000

    programme dark turnback forever fight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
