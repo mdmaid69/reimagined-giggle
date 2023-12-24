f0fdacfa479b65110a8dc91878dc03fbee7483eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:44:35 +0000

    cover pollution another Zz begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
