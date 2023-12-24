2c1c784722fe1d63d8288b58d1272e8f2e591b3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:48:41 +0000

    rank cook unfortunate sound smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
