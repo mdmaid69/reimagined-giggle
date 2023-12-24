cec32a879a5d9c615db6f43ccf47d5fa5c773d14
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:15:33 +0000

    sail disapprove Ww autumn load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
