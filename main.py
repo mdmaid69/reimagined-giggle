d7f7606778a186c038ec84c559064b7e9154f461
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:56:30 +0000

    description hate pen power giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
