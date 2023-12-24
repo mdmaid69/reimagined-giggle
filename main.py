c7ed0e485abbbd42b483108ac5e9d2ec655c085e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:15 +0000

    mistake love react stare image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
