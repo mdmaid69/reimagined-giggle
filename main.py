99fe01f01f88d44e0f22ca250c8b4a77c1578fd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:37 +0000

    getaway suit lot slight beside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_base_name(path):
+        return os.path.basename(path)
