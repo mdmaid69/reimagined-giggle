d4f237192bf58d5fe1cc33a3a1572cec86310904
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:02 +0000

    only undo ear excuse wise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
