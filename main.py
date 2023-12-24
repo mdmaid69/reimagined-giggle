e16203d0ba47d3a633fc7507164be7a3e48596d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:36 +0000

    transport aim excited unlikely protest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
