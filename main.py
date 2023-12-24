67648c579e0000e68d740eabb09ab1a74569db41
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:07 +0000

    discuss squash outside how wonder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
