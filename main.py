ebb48f392ce215cd14bcf87291936dbaa4cbb83f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:11:44 +0000

    beyond order heart below fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
