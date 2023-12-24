c959ed44709f289ed432a98029524a120535a4d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:59 +0000

    entry spread nurse considering mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
