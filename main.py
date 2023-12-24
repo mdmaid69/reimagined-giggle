9d66e42a638f659a77435ffac6718fb169163443
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:10:26 +0000

    breakfast order gap classroom relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
