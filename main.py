ab06b9946656645029225ce898012926f1b35501
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:28 +0000

    major every feeling suck learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_current_working_directory():
+        return os.getcwd()
