616f0551a8ad50c4112db707e425deb2cd415cc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:15 +0000

    about organize above blue draw

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
