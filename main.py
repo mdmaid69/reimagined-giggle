072845d779082eeff516215bfb990fb48b9dab21
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:33:54 +0000

    within pressure live tend size

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
