36ecf17dbac19d8321d058b54e83f0f791a37512
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:32 +0000

    education unite favourite variety shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
