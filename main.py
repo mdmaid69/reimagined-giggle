284d22f44dbf2ee1793499a7e88fd79ef51fa67e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:01 +0000

    rate case round takeaway special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
