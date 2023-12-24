fae50ee4270737607781308702ef6db09acd4a03
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:02 +0000

    charge mass represent exaggerate beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def sort_list(lst):
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def sort_list(lst):
         return sorted(lst)
