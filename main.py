909c686345d104abcfa8834516687fed935229c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:33 +0000

    close me mess claim embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
