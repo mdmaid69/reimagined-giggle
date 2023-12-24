fe2ba7de5e6586c2d3c9a48181e8ac647990add8
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:15:45 +0000

    light minimum plate clue off

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def cube_number(x):
-        return x**3
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def reverse_list(lst):
+        return lst[::-1]
