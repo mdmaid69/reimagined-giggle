607f8c375d05a61a9af90b8c742e96fd08b7a32e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:12 +0000

    smell closely absolutely emphasis need

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
