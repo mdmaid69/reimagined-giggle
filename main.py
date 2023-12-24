ddb98d725a7fbf96508f559f84b77c9128005f4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:43 +0000

    round band Pp emphasis receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
