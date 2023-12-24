d033aac4d48cb49ab410df83f40d9a7b59f563f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:20 +0000

    pollution pipe succeed damage fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
