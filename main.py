c3629a5a92f80fe88ce727280b54e34c1733bec3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:24:56 +0000

    split Bb jump keepout settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
