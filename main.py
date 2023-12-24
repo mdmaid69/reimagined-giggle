bec088c7b94c55925dcf919575fec9107ab440dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:15 +0000

    smooth without once clean this

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
