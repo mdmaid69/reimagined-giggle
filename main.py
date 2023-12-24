6cda6843080a0b2485fbaffbcbbc71d52e3bc7af
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:24:26 +0000

    Ww quiet history interesting middle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
