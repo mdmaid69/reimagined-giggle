6845a41d38cc74fa7f9584bc498a09bf05b6e1bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:03 +0000

    include turnup regional pleased ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
 import math
 def calculate_hyperbolic_sine(x):
         return math.sinh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
