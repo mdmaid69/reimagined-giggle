07f0ea4e823ce238a23b149c7d46653a439813e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:56 +0000

    ill cut colour spray community

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
+def is_odd(n):
+        return n % 2 != 0
