9a3800ab03e5fb46d61731388d4a87afb81fed6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:52 +0000

    calloff if target cinema wall

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
