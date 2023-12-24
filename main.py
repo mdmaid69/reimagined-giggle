9ec4292c1faf4a98196d01722b88f0ea034d8852
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:18 +0000

    deliberately little murder boring unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
