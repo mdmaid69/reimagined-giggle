62ef0b240c96a3e1cc2c55fc8ae7e6dd8348ee54
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:23 +0000

    pain find so generous burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
