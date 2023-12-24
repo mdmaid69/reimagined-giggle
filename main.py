8039145f929762cd52a1d86b0f07c65aae1a8dea
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:50 +0000

    branch Yy care moment grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
