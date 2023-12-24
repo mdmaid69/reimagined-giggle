b4066e92a3220495c8775ce072f45fca60f17a8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:27 +0000

    character test finally perform brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
