9cadf1538d9ac9f9f4e7b146753604c488cc14fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:58 +0000

    cum movement content circle judge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
