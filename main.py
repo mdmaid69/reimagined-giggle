c919321af80a1657fbfaefde45dd1a95a0f4af33
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:50 +0000

    mixture perform contain side passenger

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
