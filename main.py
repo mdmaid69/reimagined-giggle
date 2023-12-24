80e3c4071d42a2263021f4a7e9c085452542f460
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:49 +0000

    concerned prove so embarrassed seem

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
