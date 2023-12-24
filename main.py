b6895958435b17b7ce6b01ff9bc6903e5bf71516
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:19 +0000

    choice public needle its badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
