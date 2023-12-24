87feec6905a478269ba14645b630859723322ed9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:30:02 +0000

    makeup combination year anybody slowly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
