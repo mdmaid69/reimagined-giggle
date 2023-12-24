c2b61395a93565e1de17e051edbd3f30ee778059
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:10:56 +0000

    tie phone murder list far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
