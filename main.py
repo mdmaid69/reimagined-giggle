bfec40606ec3e6880325f7e7f70492431b742d80
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:29 +0000

    left concentration dream who complicated

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
