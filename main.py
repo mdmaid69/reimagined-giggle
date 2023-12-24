4a00ff59527ce24967c526b239b207f6afb70fae
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:12:09 +0000

    snow split fasten wrong nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
