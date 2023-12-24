d0159650672ad7d511139e1dcfcb6a34ff80bd35
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:11 +0000

    goout opinion pleased bang bite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_min(lst):
+        return min(lst)
