33189f108c89098df574458f834f484fbe81e9af
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:27:45 +0000

    characteristic notice equipment from point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
