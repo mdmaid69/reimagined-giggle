31b7d9ac05a779aa4e06637956da72bea482e2bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:28 +0000

    pile flight view rise disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
