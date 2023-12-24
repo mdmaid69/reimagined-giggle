55b476f3bf2a255b6d319293026c2077e3bb86d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:05 +0000

    train pride other swimming left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_complex(array):
+        return complex(array[0])
