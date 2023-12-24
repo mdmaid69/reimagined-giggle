2d394785ec1c654c8fc1e2fc8ee35898aaa59794
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:13 +0000

    city down since promise embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
