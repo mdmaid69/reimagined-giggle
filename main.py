8fe708938429473d46349202cf36157e2f5bc5db
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:29 +0000

    though character nervous copy train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
