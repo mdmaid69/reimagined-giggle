8943cb5dba7ca1fd6e5749f1ceebf7647923e36d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:16 +0000

    Oo familiar dear turnaway physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
