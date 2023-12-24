d743beeef6747f0519dfe8ad5a8d8d128bde7f83
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:19:20 +0000

    profit recover mystery comfortable command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
