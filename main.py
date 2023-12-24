f58d030c7f7f87f50f1064ec66adcd86f0b8fe20
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:38:46 +0000

    have in measure about attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def create_counter():
+        return collections.Counter()
