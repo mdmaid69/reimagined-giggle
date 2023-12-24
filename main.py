48f9cc294d1963285ea42246d65eaaff35c1f6ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:10 +0000

    sure tour climb turnout strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import os
-print(os.getcwd())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
