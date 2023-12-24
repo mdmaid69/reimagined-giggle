905efa73c74c83b0661d2e93a49c58e3b5103346
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:19 +0000

    speech study tone base talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
