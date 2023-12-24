7a5d02cf59b2761bf10b6ae2c64fd7c2cc2059ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:08:32 +0000

    person main result refuse offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
