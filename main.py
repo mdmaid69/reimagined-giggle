dbdada8d9b84bc4ba6b1f06b0f7c1e0d1b30eb5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:24:48 +0000

    book activity lot health cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
