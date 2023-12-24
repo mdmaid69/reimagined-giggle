eaffd0be7adcdcc8039608d6c6a20f67d85bd72b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:09 +0000

    proper organization conclusion suck point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
