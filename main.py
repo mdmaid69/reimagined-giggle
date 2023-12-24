adb847af1f2f554bd7ab7c5c99ddf08eac09b2da
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:00:09 +0000

    bear bringup payment Rr draw

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
