54b9eb2809317bf4875fbc26ead6a0635c69eb1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:42:46 +0000

    live mark swim also live

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-print([x**2 for x in range(10)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
