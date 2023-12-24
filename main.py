a33e127542ed25f485d012abc5672e76f697cf32
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:30 +0000

    shape delicate open pride touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
