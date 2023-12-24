78bcecf44f8beef0d2baf4e46ecac9848926d369
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:44 +0000

    569enjoy duty hard Uu achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
