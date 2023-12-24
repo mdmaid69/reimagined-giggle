b4fe5e5a8383f0f25bfa5626e14636a4abc2e574
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:27 +0000

    ground minimum explanation base reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
