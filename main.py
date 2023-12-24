4dc18d14ead49b4d1b84217242773b65d9970d7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:08 +0000

    me benefit explain past cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def convert_to_binary(n):
-        return bin(n)
+  def add_numbers(x, y):
+        return x + y
+import sys
+def exit_program():
+        sys.exit()
