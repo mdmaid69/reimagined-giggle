01adc0fa93e3ca644a840c43746a43f841e5aa4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:46:14 +0000

    behaviour story edge throughout taste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
