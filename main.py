c12cc7ad689c16bb6d265680c32e8d45c17365c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:06 +0000

    long evidence passenger event start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
