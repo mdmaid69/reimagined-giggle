8ff7bf2b7b1a94c1bae84617a98ca9abae5a1c68
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:16 +0000

    holiday even exist arrive admit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+print(sum(range(10)))
