9d28b2f559b1a9980690edb9fc48fedd6a72b644
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:08:07 +0000

    slide fear art always grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
