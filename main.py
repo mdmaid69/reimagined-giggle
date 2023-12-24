98024ea7bc05e9b5333c17a3afdf82d04c0fdfa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:09 +0000

    evidence float regional size free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_min(lst):
+        return min(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
