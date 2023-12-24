51d227b46d6348cbf2be68466eaf90baa4658ea5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:43 +0000

    punishment scream contact wind interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
