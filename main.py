7c18b944343be9965036ea81f1728cdd5854e764
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:19:15 +0000

    best dark compete smell warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
