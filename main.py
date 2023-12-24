6f22107ef4978b349372c903f1f3b95d1c9dc12a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:12:45 +0000

    achievement excited mean service kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
