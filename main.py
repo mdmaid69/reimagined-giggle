604c7cbbead4523f2bc259f0083257f7f0b5ebd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:33 +0000

    now will stupid only to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
