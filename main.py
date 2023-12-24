8f75dae9622342a36039c31fda3a3dc3cb6059ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:17 +0000

    ground worried swimming cause 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def count_elements(lst):
+        return len(lst)
+def find_min(numbers):
+        return min(numbers)
