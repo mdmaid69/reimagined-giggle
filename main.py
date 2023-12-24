d47af2bb532d1b952ca7f6bc847798b7a22d14ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:00:42 +0000

    over shake such hurt taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
