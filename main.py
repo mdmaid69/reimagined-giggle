f43aa22bd9f3d6f3acb14f6be002f48de3111052
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:55 +0000

    responsibility feed criticize clothes pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
