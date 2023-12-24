8d5584d48bb462a284866e5a7cb4f39418a5d96f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:43 +0000

    contribute school disapprove back sky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
