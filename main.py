ecdcd5511a6b97b35d3a2666a30a90f6cba9c458
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:55 +0000

    official make heavy pocket party

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print([x**2 for x in range(10)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
