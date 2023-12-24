a0cdf4e7f81987be466ffca7f3eb2ae86763db0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:51 +0000

    explain snow attitude conversation willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def roll_die():
-        return random.randint(1, 6)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
