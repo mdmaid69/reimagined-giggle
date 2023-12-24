734727f54f20a6da04978640efcad615ac9596a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:34:35 +0000

    complain aim listenfor otherwise message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
