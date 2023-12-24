e327b025c02969b43ab23c02bb2a02cf5170f58c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:30 +0000

    meet workupto heavy pool dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
