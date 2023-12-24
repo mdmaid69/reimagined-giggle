77ee79984dcaa501f7de50b9e84ec6e1cc666b4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:54 +0000

    concern start active prove recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.ctime()
