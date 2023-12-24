241d3f0c1bb0f471f8c489798c292c373c8ac777
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:35:50 +0000

    forever Pp clue really rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
