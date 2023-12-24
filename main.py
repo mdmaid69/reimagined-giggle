585174ebf6591e1e0e3b15af6f2a2ce80ed02abd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:31 +0000

    water upstairs danger for prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
