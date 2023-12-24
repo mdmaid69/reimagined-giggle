d98033ef6d65ba450d53151aa58016a790ef521f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:44 +0000

    uponon otherwise that complicated careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
