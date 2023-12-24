e09a012cef205a64eecb767a53c49ddb6ee57a2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:55 +0000

    replace suppose suggest breakup strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
