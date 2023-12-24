ccf3131396b8b760d3420406cf65d4d0b074f6d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:46 +0000

    moment slowly feature comfort pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
