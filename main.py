02bfa6a49fbe332b19d18f047085185649eaee11
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:36 +0000

    completely comeup harm stretch care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
