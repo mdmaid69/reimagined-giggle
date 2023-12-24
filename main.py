95686a2cfe1d842d708f1bad01f1d8dc1354246c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:52 +0000

    themselves replace breakin theirs arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def square_number(x):
-        return x**2
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
