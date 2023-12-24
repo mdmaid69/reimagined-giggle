3a08b256aa33f0b9c51b90349eea2ddc577a450f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:40 +0000

    unit take time weekend television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_max(numbers):
-        return max(numbers)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
