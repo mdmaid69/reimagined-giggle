ff133412078ed3115c0af6109712c8178baa0f8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:25 +0000

    bridge street wall below sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
