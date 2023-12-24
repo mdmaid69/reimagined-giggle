07efe009ca7f84e04ce0d660e8e767351895f9fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:44 +0000

    correct centre avoid cum stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
