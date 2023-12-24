c788baa5ce1f46653074a9f1f97e1b836f0fdb10
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:26 +0000

    fold allow quick first finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
