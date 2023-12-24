3a7f6f9795d8fd21b5ba13b3ea797aae2a920974
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:00 +0000

    weak bringback runaway unfortunate food

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
