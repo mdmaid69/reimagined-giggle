ba79212d76a0b04ecf0064d3a53c6a8dd457980d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:31 +0000

    eye lucky cross opposite trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+print(math.pi)
