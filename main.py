3621c85e44625cbf8fc76370b9d24435572801fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:57:37 +0000

    turn finger transparent hurry pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
