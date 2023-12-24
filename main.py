1f2ecdd9c20e8b648e125a45e128532b714e117a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:05 +0000

    increase height partner meet form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_typecode(array):
+        return array.typecode
