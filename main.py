5304f6063de7365a3516b25169d64a491c838129
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:59 +0000

    impatient connected handle Kk file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
