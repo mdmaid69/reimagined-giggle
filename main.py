8791a94b30aca7fb7380ea8f9dc7e92c770962f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:38 +0000

    burn do her date secret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_even(n):
+        return n % 2 == 0
