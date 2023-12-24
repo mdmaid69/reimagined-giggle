07350af1835a52d1d28f333a73bcbe0a8fe8fb18
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:54 +0000

    whenever relief platform manner ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
