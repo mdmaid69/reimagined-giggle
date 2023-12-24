4292b6b6b88de9e49372f16b0208c0fd07b7eb07
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:19 +0000

    positive march long police quarter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
