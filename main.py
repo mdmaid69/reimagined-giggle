be990aee1fa4583771a058f8bd61f81cbf8dc62b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:25 +0000

    ground clearly suppose compare neither

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def subtract_numbers(x, y):
-        return x - y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_area_circle(r):
+        return 3.14 * r**2
