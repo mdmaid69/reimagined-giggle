e4d5df2b551ea7d0b29076a9e33562ed68c12782
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:39 +0000

    benefit loud listenfor bringforward course

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def sort_list(lst):
+        return sorted(lst)
