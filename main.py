574aefe2f7537ea77d32807fbb277bd99ce20359
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:44 +0000

    eye surprise perform disagree succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
