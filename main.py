53600f2b6629e174e1e9def8d88a38ade848b772
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:11 +0000

    habit race escape target train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
