2bea0d3c8b2ffde02f5cd885a52277d44c7ede87
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:06 +0000

    past reach sky escape dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
