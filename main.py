07bababd8273c28a8e7569eb530621c392aeafd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:58 +0000

    table left record wave public

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
