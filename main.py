5999e0d7075e3bafa09f27cb5e5fd6040915fb65
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:18:23 +0000

    copy table substance criticize keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
