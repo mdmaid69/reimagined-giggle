8485e448526eed41d8978d05132a545f91f9410d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:36 +0000

    oil track couple ring powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
