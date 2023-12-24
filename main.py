317c49dbd703c784eab01225c55d6e5d6e8c6773
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:57 +0000

    phone concerning need morning second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
