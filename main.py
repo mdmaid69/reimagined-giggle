09bb2a1752a54e4a24aedd6944ac414573b5c58b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:26 +0000

    unlikely might worry legal possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
