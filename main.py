3ea53a338819ef77457bcd80e42dcc56ecf64f21
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:54 +0000

    fair upstairs generally afternoon separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
