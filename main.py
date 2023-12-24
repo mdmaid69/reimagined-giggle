b3bae4c9798079453bb6790953c46c0a510e9b15
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:07 +0000

    control illness turnout congratulations car

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
