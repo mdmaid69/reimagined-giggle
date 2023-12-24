29c79f3be045f8b9506eab8383d407232b5262b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:29 +0000

    Ss odd nice hard black

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def multiply_numbers(x, y):
-        return x * y
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
