fae766d220bc6225b22bb99445ce8b2560267044
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:33 +0000

    move mark call near immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_bytearray(array):
+        return bytearray(array)
