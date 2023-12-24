ca7149d88ea09624a12ff98022bf906990550d3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:52 +0000

    trick combine interrupt interested likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
