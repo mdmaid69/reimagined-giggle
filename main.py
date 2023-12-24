6268aaf766e90b2f64cf43fd66b9d60a9b721aad
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:32 +0000

    between naturally drop consider holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_str(array):
+        return str(array)
