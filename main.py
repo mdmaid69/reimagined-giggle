b2043ac45a588d22427f0b5413bb9a2de088385d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:11 +0000

    wrap ill rescue vanish high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
