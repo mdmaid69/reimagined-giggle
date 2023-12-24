f5a532ba3e2fd09d3d3d49956d92d06cda86ad61
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:56 +0000

    broad congratulate accordingto crazy responsibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
