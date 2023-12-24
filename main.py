53383fd3ed8edbf2c95ce40a71acb1a1c098889e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:28 +0000

    visit her decision march broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
