3f0dfd927e8e787a0a7323d39c78d8e661fdac27
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:11 +0000

    next confusing one determined recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
