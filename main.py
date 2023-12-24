d3831f396687323bfeb0a83b123cef1c20f4f7a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:30 +0000

    admit secret play protest better

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
