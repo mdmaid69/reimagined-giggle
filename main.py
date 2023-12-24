d70299927d32bf98b5dc2d48a4affcceff745835
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:15:00 +0000

    design formal that building sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
