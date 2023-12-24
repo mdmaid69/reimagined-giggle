799f9b0354c4b2b26289b1d2207d5367e2296af6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:21:41 +0000

    thumb bring creature good live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
