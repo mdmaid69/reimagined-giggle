04a2bf33bc61794b6bc977c7b6db83e181e78ff7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:05:10 +0000

    town or bad character Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
