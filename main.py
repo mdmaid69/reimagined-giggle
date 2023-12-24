93b84759a6f1174e9457b3aee44ae55723aa716b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:31:13 +0000

    scrap grade stretch sort discovery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
