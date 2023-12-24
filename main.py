798793358f6744360e382c43e5d2ea4f59811e8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:31:43 +0000

    cause fold afterwards disapprove emphasis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
