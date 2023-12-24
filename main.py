9b7ed5466922911a2fac9713f09a53711db69c38
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:09:26 +0000

    shame scale team none come

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
