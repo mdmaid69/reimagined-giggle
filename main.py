6ff6055cb1ba325c3f5bb07e10522a7e3de21250
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:40:08 +0000

    certain colour yourself instant tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
