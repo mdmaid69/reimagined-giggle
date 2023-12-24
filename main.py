0dd7d3eb0138eef0e6d54182dfb838cb5fcc090b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:13 +0000

    press future himself deep sport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def square_number(x):
-        return x**2
+text = "Hello, world!"
+print("Characters:", len(text))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
