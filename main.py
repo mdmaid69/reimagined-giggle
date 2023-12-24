cf1fa364456b9b7634aca7472337c2dc06eb8368
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:08 +0000

    fashion plain press hope glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
