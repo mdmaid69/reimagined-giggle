8094667822af1776f70f3fccdccd225fc8ebd003
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:51:43 +0000

    place guard refer shop us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
