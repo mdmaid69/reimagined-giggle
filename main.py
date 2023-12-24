9cb01a63e4257077e5d149012fc5a6d7779f2ee6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:41 +0000

    consult distance already pair exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
