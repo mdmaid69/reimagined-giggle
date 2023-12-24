0b2b4d1bb1bc25fb89dc54e9fb048ebe8a82656d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:01 +0000

    meaning drive feed really model

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
