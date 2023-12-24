e93d2599f60f5013e441fb7d1d66bb6b39c14278
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:10 +0000

    expensive world both waste declaration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
