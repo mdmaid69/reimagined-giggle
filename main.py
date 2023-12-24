8a2f70fcd6aeef69ef6fe0ca5ffa4c5a5bc0057b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:45 +0000

    fault afterwards big genuine offer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
