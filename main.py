19b45aac3fbb3e5b1cea119b4449fcba882def02
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:25 +0000

    strike skill unlucky greeting declaration

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
