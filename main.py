da8b9bca553bb3d221150b4c5ddbce23ac31f2e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:20:26 +0000

    brave operation disturb feed remove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
