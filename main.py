60a599489aac8da327ad0bc02a52bf79fdbcbfc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:36 +0000

    write protest inside system continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
