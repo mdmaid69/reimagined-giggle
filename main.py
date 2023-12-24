1a9ea30277d04d2b1a72ff86870fa6f73718ff50
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:20 +0000

    plate friend kind confuse transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
