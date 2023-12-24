48a363c56aa2a1bfeb9660a5a84a6aa26c20fe96
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:22:00 +0000

    generous central pipe plenty injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
