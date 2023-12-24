8e1c2c5502255df52276366ce518b6cf7b7e313c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:12 +0000

    likely pause stiff takeaway shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
