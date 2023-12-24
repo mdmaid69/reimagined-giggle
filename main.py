1f5241d1754f0b4ccca4587e8b9f255120cadcaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:45 +0000

    walk subject sure pollution recover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
