f9300213307d302fffa93544a0f1201ef0fa6d4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:49 +0000

    quantity fortunately cheat actually bother

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item(array, i):
+        return array[i]
