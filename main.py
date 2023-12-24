9d07f231350e92887d4bd8588eea2066da0b3a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:03:11 +0000

    even cost sudden along trick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import time
+print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
