16aa25787d1483351fe049f0bca0a4c6ac308a29
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:06:37 +0000

    force many related because plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
