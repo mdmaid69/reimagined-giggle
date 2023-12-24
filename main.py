4e437695808e5fa9e8446f4b86ea596fe0b0aaf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:05 +0000

    unfair improvement precise name our

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+text = "Hello, world!"
+print("Characters:", len(text))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
