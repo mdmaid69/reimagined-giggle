e955e7cd34f79d61693613a5aee3800177178229
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:11 +0000

    easy enthusiastic warning degree different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
