2b525eb399f94419795cab9d426d3f2f3e014aee
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:32 +0000

    flood add scrap contents neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_tuple(array):
+        return tuple(array)
