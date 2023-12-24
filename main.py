047b893ae272f69afecd02511ffa889de8621f89
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:24 +0000

    illness mend comeout greeting practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
