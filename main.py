600f35cae0c8668ffd75b639f7dc9e5b830ed857
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:09 +0000

    stage gas this and soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
