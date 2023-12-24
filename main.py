d0bbc6a21368f63c8ff24881625e9fead12968a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:03:09 +0000

    peaceful commit turnback visit hurt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
