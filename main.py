0320ff553e6c90eb87125655de57e74e01b53c78
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:57 +0000

    fact period ago middle now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
