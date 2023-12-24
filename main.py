4955c3b69b82b35b085861193f7a6c4af3f11d7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:48 +0000

    breath split sense whisper continue

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def convert_to_binary(n):
+        return bin(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
