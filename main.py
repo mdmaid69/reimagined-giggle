f9a61a8ba357381f1851531c7065a22a08366510
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:01 +0000

    type damp right beat clever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
