61e15bd7b99e0416973be47f5d74851ad3e72b0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:32 +0000

    somewhere patience may off roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+name = "Python"
+print("Hello,", name)
