77c105e78a35cae1c7fa665524c133b7371bd718
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:17:55 +0000

    hear comeoff past lend debt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def count_elements(lst):
-        return len(lst)
+def sort_list(lst):
+        return sorted(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
