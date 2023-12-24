f3464009cd8747d4b5b313a70dff6f326defa49b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:39 +0000

    rich board couple fast offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
