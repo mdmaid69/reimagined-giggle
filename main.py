77a40ede37435811834cdb7b0f8f2e84722ba210
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:49 +0000

    avoid crime concern answer kind

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
