1af49657880a3789676fab815d06c6ee96794f80
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:20 +0000

    appearance whenever clean exact look

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
