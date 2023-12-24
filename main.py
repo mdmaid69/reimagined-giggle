056aa18aa95cf3c2a23d27b674b2aaa84c4760af
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:13 +0000

    unless sometimes delight forgive rough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
