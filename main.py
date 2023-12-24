c71842db1d76f5d00b3e62079d287ef2d99d7cf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:04:45 +0000

    them instant realize comment off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
