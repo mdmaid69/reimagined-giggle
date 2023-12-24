2bc559c94c821892434e0730a96ddd30f4d66f75
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:01 +0000

    refuse slow practice telephone minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
