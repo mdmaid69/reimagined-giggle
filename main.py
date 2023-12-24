ddfca992cc042b6274960e141776fe3f96fd3141
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:25 +0000

    slow hospital nowhere win unusual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
