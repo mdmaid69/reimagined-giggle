66b03a66f589e34242e77264f6b82b059e821018
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:52 +0000

    smell embarrassed active direct letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
