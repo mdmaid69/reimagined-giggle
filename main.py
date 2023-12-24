7698da0b73fde21ca20e504eaae44896c30cf5db
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:44 +0000

    suffer comment view top radio

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
