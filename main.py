aefa7b04b1cf257db48e98cffa55dff0ac37e4f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:24 +0000

    rise among on city copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
