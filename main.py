2fc60b0c02509a6c64866ee89393540bb4434935
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:51 +0000

    tough software murder recommend hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
