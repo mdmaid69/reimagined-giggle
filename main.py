b088876665ec80ab376b104bdd87a4d85a1b84fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:10:58 +0000

    breakinto at speak shallow unusual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_square_root(x):
+        return math.sqrt(x)
