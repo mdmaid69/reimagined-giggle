63b6d3c7044731ddb616c99c61ea813119bfae71
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:59 +0000

    drive comeout separate silent date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def add_numbers(a, b):
+        return a + b
