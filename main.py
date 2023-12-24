72da6cb34b0c954f1a1c4012a244c9205153b89c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:58 +0000

    justice majority onto ago suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import getpass
+def get_username():
+        return getpass.getuser()
