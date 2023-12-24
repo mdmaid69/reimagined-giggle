e54617ab1527185793634b42f037dc3c8c1b9cab
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:01 +0000

    point press almost take price

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
