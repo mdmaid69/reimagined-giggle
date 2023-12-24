f50f82b3d3c85fc00c3fcdd6757ae65129d8b220
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:33 +0000

    properly taste pronunciation support beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
