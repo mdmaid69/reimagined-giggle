d7d49608dea5f648832d87c338b3880fe09c0c7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:34 +0000

    shake far afraid tread hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
