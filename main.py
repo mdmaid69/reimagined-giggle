a46a9b211abfa1ba55c682482407377d41d7e98a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:53 +0000

    type fire new build fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def convert_to_binary(n):
+        return bin(n)
