3655758c6cb70f3bf269bd793a52394074c26a67
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:54:34 +0000

    circumstance picture upset lower mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def multiply_numbers(x, y):
-        return x * y
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
