57c2b941f0a408d7cda03a5b0c57628d737a5af0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:23 +0000

    point worth fill alone badly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
