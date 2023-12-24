46a3713ffdfe9b51c4ebb11fb075a9df4588e87a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:11 +0000

    cover doubt mind track freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
