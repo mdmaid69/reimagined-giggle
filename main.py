70ad91ca517c322e7ebbd2ff0404db84d0cc82eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:39 +0000

    mess 571enough past wide flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
