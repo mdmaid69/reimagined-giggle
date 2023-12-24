41e6407a0ca492d705cbde259a489044f6f00613
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:39:33 +0000

    570enjoyable cause away naturally touch

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
