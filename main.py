0a145360e530adf515d61aae64acf4a3dd242a8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:22 +0000

    belief enthusiastic something damp pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
