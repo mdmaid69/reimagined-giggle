6ebdb86584be5ad9836b26cecec86f76e249b492
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:11 +0000

    word firm whether none welcome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
