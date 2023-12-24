a49421967a8c7c28454fe719b4c332fdf5956ad5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:47:28 +0000

    unlucky now noise estimate shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
