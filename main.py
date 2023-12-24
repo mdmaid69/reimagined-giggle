bc71e2c0346cb69c86ad60ce3f9952174e5b76e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:46 +0000

    roll computer steer makeinto combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
