932d9d46990c04d3699a9126bb513d1f8c9762e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:27:22 +0000

    tomorrow laugh how realize group

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
