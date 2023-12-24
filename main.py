8ba25a808da6afe2bd9fcc57ba2ad32e73b31d6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:33:45 +0000

    event wish fond thinkthrough enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
