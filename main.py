92718ddbcb7b446ccbe36bc7b1b1d367fab08b63
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:58 +0000

    judge pity muscle ever laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
