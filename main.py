87a6328be48f1718e921018968815156ec4fbf65
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:13 +0000

    mood port lot whether quickly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
