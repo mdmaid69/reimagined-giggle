b4634bda60265251f63935b0da528d6bddc65921
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:39:12 +0000

    store circumstance ground fortunate joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
