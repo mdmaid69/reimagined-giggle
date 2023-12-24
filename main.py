65b602b25c537fe1c0cf220e3e6b7ae5add01cc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:59 +0000

    turnover shout point capital touch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
