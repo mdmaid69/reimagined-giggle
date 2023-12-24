009b1ceeabdf082ba048d95ccfdb7a3ceba1f21b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:39:57 +0000

    pay Kk top least persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import re
-print(re.match("h.*o", "hello world"))
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
