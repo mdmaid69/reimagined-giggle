2a5659a89abcfc73294c08e346633a1590af5822
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:12 +0000

    whenever everywhere at gather protect

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_work(force, distance):
-        return force * distance
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
