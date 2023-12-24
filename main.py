c30e0d9bd3fef8a8bcf128a7f6f81793d6e0cde2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:17 +0000

    lay obtain exciting instrument finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
