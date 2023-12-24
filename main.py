ac3dd130fff99bb40e08a087bb4ce7e0da531f3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:22 +0000

    bury shut head takeback direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_acceleration(speed, time):
+        return speed / time
