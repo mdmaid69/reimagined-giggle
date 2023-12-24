5f6576ee61ba6eb4a4ec1bdd80ad09e5903e0bf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:49:56 +0000

    balance whistle add keep naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_list(array):
+        return list(array)
