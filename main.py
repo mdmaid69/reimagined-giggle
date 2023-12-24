ef39ae7491bff04748301d676794de07b18f7574
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:08 +0000

    pool along 567enemy obvious cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-for i in range(5):
-        print(i)
+def count_elements(lst):
+        return len(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
