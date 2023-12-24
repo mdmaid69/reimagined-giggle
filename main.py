c919cde8640e5e277b19c5a68710df33d522e0bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:23 +0000

    destroy sure 567enemy young phone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
