b06ed7c98505c1c4fff71d7f9415908f09519ab0
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:09:53 +0000

    score start connection difficult show

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
