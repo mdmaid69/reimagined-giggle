c45b1cd96320cd4630f9cac68f00ad2822acf05c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:17 +0000

    as thing large introduce fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
