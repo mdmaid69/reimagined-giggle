3daf0e08576308fe4517057052256b9a97645e02
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:34 +0000

    effort advise occur round measure

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_min(lst):
+        return min(lst)
