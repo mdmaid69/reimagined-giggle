c870efb9e3a1de8d52c43c42845c79f39ea74887
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:37 +0000

    value part wait clear defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
