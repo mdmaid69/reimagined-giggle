637c644de802e9c44de8c4ff9247537dcc87c660
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:47 +0000

    become succeed print reduction hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
