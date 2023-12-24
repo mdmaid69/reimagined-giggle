0d75d5cef4dfa9896e5d1b74aedd5d4379d58318
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:55 +0000

    spread advise grand steer happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
