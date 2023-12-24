9e26f975b475f9b78eaf6bf7ec41082115c9b479
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:53 +0000

    improvement by present grow thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
