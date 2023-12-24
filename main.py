dfba212f77912fba383f5abbeb05a8e4aed79156
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:32 +0000

    night bang eat task choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
