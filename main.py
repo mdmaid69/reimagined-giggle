e4f6f984d2b39642509686e2011291fcbd134db9
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:38 +0000

    entrance joke clever maybe estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
