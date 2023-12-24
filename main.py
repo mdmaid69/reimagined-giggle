1653dfe249258709f46c2e7a5f0b177fe17fd931
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:17 +0000

    drink geton material music joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import time
+def get_current_time():
+        return time.time()
