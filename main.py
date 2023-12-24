ab0dc0eda23392cf528c58c257907ecc3af3024c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:48 +0000

    understand week can ill future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
