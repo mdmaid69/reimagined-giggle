b29f83ae3f926625ea9d025890781cbe77663099
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:09:55 +0000

    cinema geton ignore lift mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
