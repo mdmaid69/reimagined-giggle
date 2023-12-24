a654aba5ced79c53af719564fdd0bab2ed978527
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:15 +0000

    flood recommend careless programme kind

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
