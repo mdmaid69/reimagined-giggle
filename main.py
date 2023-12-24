de61197cd4c9478ea123c44da3081fd760a9d053
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:56:17 +0000

    rescue tomorrow 179bite figure respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
