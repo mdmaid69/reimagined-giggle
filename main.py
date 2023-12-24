0af789a020a91ba48c9afc3dbc8e6a6cb3793bdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:15:16 +0000

    taste eager disturb neck depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
