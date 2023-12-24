9600fac4fa8b7a1bd2dc159b46370651a860cda4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:25 +0000

    sea form around oil stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
