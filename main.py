fb213b8c504d630f6569a9aca3b411a194717217
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:52 +0000

    finally eat quantity second lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
