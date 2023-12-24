9cf640f3e8d6dbb5b0d62093dade19e59181af3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:25 +0000

    569enjoy chief introduce uncomfortable Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
