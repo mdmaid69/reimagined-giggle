e8dad117632d83c5c0f1b008f81d2e6fecd58915
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:14 +0000

    reach switch embarrassing regular hit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
