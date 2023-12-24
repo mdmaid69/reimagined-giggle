9edc2d15ab6b5b50a29e5349b147588d74b92d16
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:10 +0000

    Ff natural deep for wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
