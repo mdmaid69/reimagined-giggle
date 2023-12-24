cd4db27ebe7610452916f7535387cec22e516e62
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:45 +0000

    179bite count board violent everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
