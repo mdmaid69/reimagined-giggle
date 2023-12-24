773dbd9835de590f7c99f9a8ac3e54f97dcd37cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:50 +0000

    certain by speech shake extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
