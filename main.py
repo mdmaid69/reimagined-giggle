1aa47fa2278e2cd8873edebd0b40aceea1a23289
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:45 +0000

    evidence noisy mix bet voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
