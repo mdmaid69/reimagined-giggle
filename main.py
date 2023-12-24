894db6177d4b9ae70907e423cb6356ba67c77384
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:30 +0000

    nowhere force sensitive discussion hour

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
