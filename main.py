e90a60a4090a4604a1aa2c35348cb2355e42fda0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:20 +0000

    giveaway guess surprise sensitive talkback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
