b3337a9c67e5f2c9c33c1f4c1b1da1e791aba1b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:47 +0000

    concerned usual afterwards convince interesting

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
