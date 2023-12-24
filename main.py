da99a4b99ecbd3e36035e25ab9fbf3213fc79c93
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:50 +0000

    like today calmly that watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
