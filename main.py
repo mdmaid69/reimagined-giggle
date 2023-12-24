885b35caf7f2cb9f973676507a2875ba8e64d167
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:13 +0000

    upset sky permission seem shoulder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def cube_number(x):
+        return x**3
