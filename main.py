0d520521136872c7c3bc3a4ecc2bc3f155dedcc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:11 +0000

    computer onto address fairly Uu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
