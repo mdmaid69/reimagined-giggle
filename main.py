0fb607032efc435ff9b39ebfaefab4272a927483
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:32 +0000

    doubt regard positive face basically

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import sys
-def print_python_version():
-        return sys.version
+def reverse_list(lst):
+        return lst[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
