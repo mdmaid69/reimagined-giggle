6907f3acef964f96fc555240d2db1e3ff480f68c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:30:17 +0000

    swim imagine speech behind if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
