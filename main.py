7bdc8fc7bfdaead850fd319eebab8ed90b40f5fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:13:15 +0000

    equal action unlucky taste bill

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
