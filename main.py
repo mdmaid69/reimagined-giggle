3cd5f259c9381889582f65e940ef6a77d21fe2cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:59:02 +0000

    party teach turnback inside unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_error_function(x):
+        return math.erf(x)
