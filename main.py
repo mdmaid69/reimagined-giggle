e7cc607f9a54896e4ea511c5505c84c295cc5b5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:10 +0000

    possession occasional choice woman include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
