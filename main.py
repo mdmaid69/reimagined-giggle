60d709d00742350aa2d2d3607b2623a74f0d4e7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:59 +0000

    sentence next party anywayanyhow cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def remove_directory(path):
+        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
