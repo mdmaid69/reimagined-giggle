28b931136b0a4b412c1aaa4417683078e97fb3dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:57 +0000

    matter wing polite lean regional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
+import collections
+def create_user_dict():
+        return collections.UserDict()
