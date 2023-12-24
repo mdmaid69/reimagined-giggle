d5aeae25e16f0cca383522144569b1ca76a17c9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:11 +0000

    wind laugh interfere aim round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
