5c431656849811575f03621e64462a2d48fe01e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:26 +0000

    very fortunately before appear extra

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
 import array
 def get_array_as_str(array):
         return str(array)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
