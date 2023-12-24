ef629ca324418f31b9cb3b83db9637c64e458865
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:45 +0000

    rescue lot common container on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
