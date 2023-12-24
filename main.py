0ebe06f7bbebea2d312fa551f76f47bc1171571a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:21 +0000

    most strict pull market degree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import tensorflow as tf
+print(tf.__version__)
+def remove_duplicates(lst):
+        return list(set(lst))
