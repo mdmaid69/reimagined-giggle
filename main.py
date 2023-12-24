1d0ed3b3acacf0046c42645a772036743520ee8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:43 +0000

    party guilty government impression scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
