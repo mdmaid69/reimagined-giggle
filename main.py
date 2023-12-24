4e5c1da044f7cce70e6135bcd7476c491be1d406
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:15 +0000

    match cold reduce holiday knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
