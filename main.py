1051969b760adb41d0dcd53a76a987c096602045
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:15 +0000

    fold everyone direct exactly interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
