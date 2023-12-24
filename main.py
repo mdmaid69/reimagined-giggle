e7c354d842f78af5e66808a5c43517e8df9b56c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:48:08 +0000

    further thank share occasional cool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_current_working_directory():
+        return os.getcwd()
