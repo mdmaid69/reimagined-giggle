cc65ea22b36a2e0b244d7cb2dd8acad85091e389
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:12:09 +0000

    public finally list squeeze straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def count_characters(sentence):
-        return len(sentence)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
