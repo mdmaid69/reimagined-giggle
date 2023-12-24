d89f366442e33ae77934f2c3e5ad01565df307de
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:19 +0000

    fact progress stage chat except

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
