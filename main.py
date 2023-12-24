9f52196d024471f20aa96e89bf12dda716adbbd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:36 +0000

    back loss pencil use announce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+print(math.pi)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
