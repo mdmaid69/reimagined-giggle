a69ae507a7a7936dcaf1419731b47cd17fc67e70
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:43 +0000

    woman intend switch clever stretch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
