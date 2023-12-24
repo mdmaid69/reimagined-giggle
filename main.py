9d3f2618df28f34539d51338c69582b423975c77
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:20:12 +0000

    month relation thankyou film thinkthrough

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_max(lst):
+        return max(lst)
