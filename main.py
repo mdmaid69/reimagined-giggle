12c6da4fe0b6c090c4c81c8d30c2ec98184d1bdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:12 +0000

    rob suit bin congratulations scream

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
