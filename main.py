8b2249f7d7f50d20315a099d6d005d3f4037fa67
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:46 +0000

    disagree bit also frightened score

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
