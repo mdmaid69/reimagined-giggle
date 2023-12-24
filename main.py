3d4eb6be2894a68fc0cffbb685300be4d2ec55c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:38 +0000

    need ashamed push affect by

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
