97ac476194d138141a2e39f24d00271cc2cde2ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:44 +0000

    strange scream swimming slip warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
