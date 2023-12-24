0177647d47a2b3da911ca7c074d91538912bb6aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:17 +0000

    beat day business light label

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
