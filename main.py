8d3bd1e7d9454d2516c51bce5ee99a34da1705d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:51:28 +0000

    workout vision home day need

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
