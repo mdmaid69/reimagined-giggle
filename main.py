74c81cf6d4b1b04e9177bca01f280e654a749aaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:27 +0000

    surface false weather yes hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
