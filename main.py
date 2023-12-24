be21ea699dcc8566639976f205d7713d83caa70b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:51 +0000

    turnaway niggaacademy slight far her

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_characters(sentence):
-        return len(sentence)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
