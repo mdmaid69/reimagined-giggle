5f468135bd248a03129c943190ac83f3ae4f4b35
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:02 +0000

    partner bright tidy shopping woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
