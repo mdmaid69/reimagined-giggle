a95aae3f1e054e286ea2773696e6c57aa9304a54
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:00 +0000

    together wish coat disgusting broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
