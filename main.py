9bccbd07002783c5f06e945c1a9d29f09ef032bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:27 +0000

    cross afraid notice of ticket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
