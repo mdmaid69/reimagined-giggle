445ac7f6e578fc9a2f0aef10c284dfbfbe730c20
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:14:36 +0000

    well warn experiment freedom minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
