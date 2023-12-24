4998825340ba9bd6d4c86167e106d20f042f117e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:32 +0000

    mood call 567enemy so important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
