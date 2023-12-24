9fba5c9e508495a93a70b86855aebfb4bb408e4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:17 +0000

    equipment my hollow tax friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
