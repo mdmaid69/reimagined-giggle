0b069fe9d9eecf0164c96faafdd02e38029d8773
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:19 +0000

    sore prepare punishment unfair bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
