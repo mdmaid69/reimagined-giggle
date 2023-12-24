e2122ff655358b4a66ddc10ffd417a94397e4b4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:51:03 +0000

    lead sweet lot class shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
