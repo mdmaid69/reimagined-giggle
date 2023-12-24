609691e8e9806a9073ba5068ae70d4c169f60429
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:33 +0000

    properly spare several gift cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def get_array_as_complex(array):
+        return complex(array[0])
