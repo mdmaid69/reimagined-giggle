427d92d312cbfabc06cfb91420611f6c3cf872f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:46 +0000

    bunch train meeting damage clean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def convert_to_octal(n):
+        return oct(n)
