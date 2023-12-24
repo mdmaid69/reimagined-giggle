18bd81f96ba9ee7e327eef56c611c2416940fc85
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:33:01 +0000

    be how thinkof punish demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
