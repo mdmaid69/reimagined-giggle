0726bfad848e3edeed8015bb390849602c8b4039
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:44:58 +0000

    start agree tense zero would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
