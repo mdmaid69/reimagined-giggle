1be43053585d1c53c603982d7f6e56e914fe6f90
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:44:13 +0000

    choose generally hope tip broken

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
