6373166efb6486f733aa82c42cd7b048877868fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:52 +0000

    protect suckme disagree content mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_bool(array):
+        return bool(array)
