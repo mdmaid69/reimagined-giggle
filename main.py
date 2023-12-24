ac98d2941bc10ae539eb947995fa44f2a6f4b6fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:43 +0000

    interview middle wash joke impression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
