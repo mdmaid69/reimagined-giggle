bfd7ee4a39445d4bd45f41d340013ceef75b4fa0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:44 +0000

    Pp occasion shape peace group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
