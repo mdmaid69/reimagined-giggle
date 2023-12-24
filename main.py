28c4bbc11adfe612feb01093e29f4f157095122c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:24 +0000

    dance expect flat eager welcome

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
