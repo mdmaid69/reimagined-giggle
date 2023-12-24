72de3b6f39c89006f63ec6dbb62064541deeba27
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:31:47 +0000

    run pick tone sky again

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
