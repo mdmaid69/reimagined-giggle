e25cdec55f4931dd9127999950afc2b9417b1722
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:28 +0000

    forget judge movement fire will

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
