7183fa937c0c88d7a781a0c75341bb9a8bfd2e5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:30:50 +0000

    surprised evening tight describe among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
