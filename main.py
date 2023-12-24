2f6bd093735a80397d52d505d2d0471474a10f5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:32 +0000

    how tear funny politics shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
