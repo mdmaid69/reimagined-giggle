d2441f2afe687ad7a09d3d2b12155d497a70e76a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:40:52 +0000

    we circle buy whisper enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
