c64fe6222f66cb12492a235e7dbe5990aa787298
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:26 +0000

    mysterious meeting near workout protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
