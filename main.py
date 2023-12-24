a5808a8249463de427c3ada5a8834518d293969f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:06 +0000

    manage tread enthusiastic holdout mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
