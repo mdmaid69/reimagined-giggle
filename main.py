65bdb5035dc11e2f8755fe92b9c73e983d3f0fea
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:40 +0000

    story Ff skin cross off

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_list(array):
+        return list(array)
