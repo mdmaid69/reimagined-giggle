c3e969dae09f2c4bafd1a07f3792f75fc973fe86
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:40 +0000

    badly pause criticism shine wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-print(time.time())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
