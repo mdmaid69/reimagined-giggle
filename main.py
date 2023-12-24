77973943c1caccc4c0750d6974548570bb955fa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:42 +0000

    report damp feature study environment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
