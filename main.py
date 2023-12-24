62bb375f68767a19d5a0cbc26f83434007b16bf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:58:51 +0000

    sure fault exit first joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
