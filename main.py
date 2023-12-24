443cf0cfa593a62ea0f59b3bc9aad74a91f658f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:05 +0000

    reach cry back last second

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
