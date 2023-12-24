0fe842b2d9a47f6b0b52c5d15aeeb7c447796be4
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:56 +0000

    Dd to Mm arrive mainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
+def calculate_work(force, distance):
+        return force * distance
