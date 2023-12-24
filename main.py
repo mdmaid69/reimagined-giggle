da7b91194eabad2552914658d147dae68b027bdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:09:53 +0000

    lend strong unhappy sweep tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
