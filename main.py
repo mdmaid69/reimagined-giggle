1ad85c0b9717990c8e57c64e2118b62629010f6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:02 +0000

    mail live thinkof ashamed will

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
