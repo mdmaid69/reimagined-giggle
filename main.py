8f76b92bae6a5c873a277cc59fc4c52d81348889
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:33 +0000

    produce grade pretty warm loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
