a189418e936c1db6d03bc3f7b6abb11294144fa8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:59 +0000

    fail joke possession bringdown usual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def cube_number(x):
-        return x**3
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
