d5309cb03b6e924722b9b96bc32083521c829a6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:53 +0000

    different organization grow sick rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_max(lst):
+        return max(lst)
