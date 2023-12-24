a2f0a0b5c91893767ec3aa41d84d3fb4a153ee8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:24 +0000

    shock well anything listenfor possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
