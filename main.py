010cb8f7c656cdd8306814b7ac5d682bb85e0c8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:52 +0000

    comeoff calmly trouble precise thirsty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_acceleration(speed, time):
-        return speed / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+x = 10
+y = 20
+print("Sum:", x + y)
