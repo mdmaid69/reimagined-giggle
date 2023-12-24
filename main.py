48f8c68e48e20052cbc474c54939010e81555635
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:01 +0000

    fairly speak hurry frequent mean

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
