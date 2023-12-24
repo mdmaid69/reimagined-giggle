cad06efc27d2dde3a723bf55113e345727f2a76d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:31 +0000

    comedown contain declaration unfortunate modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
