3465c505c9042683f317b779a972e40909f8b751
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:19 +0000

    certainly please free block scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
