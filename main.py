5f7827044bc12670e61c3700e368ba63f735e7f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:33 +0000

    eat relax drive general variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_item(array, i):
+        return array[i]
