492a644d6f5cc7aa1a75e7b9d4aa4121daa11f4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:26 +0000

    centre balance strict belief square

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
