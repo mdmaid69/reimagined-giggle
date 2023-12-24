e2bd1d24c5e020c8715ee7ff31178154cf1e7f07
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:13:55 +0000

    habit age content normal regularly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def cube_number(x):
+        return x**3
