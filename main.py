40ea3ec72fdf889288374e9a6aa816c34e1148e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:33 +0000

    next club form ring pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
