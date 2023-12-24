ed14340f695ad52934836d12e05ac62e899b740c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:40:08 +0000

    cause undo depend combine feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sys
+def exit_program():
+        sys.exit()
