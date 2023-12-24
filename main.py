f94071d6b38a59accefe120b21ecb94a27690b9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:08 +0000

    belong creature discover only several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
