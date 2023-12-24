c04069c4f56da994eead257ce7baab4ff1224c26
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:56:59 +0000

    produce basic compare train about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
