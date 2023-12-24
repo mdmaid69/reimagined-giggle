b4caf4993166b04bf06b9e0c5041047f3341c90c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:40 +0000

    admire only makeup matter balance

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def add_numbers(x, y):
-        return x + y
-import random
-print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
