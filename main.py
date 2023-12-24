acdc2c61b93be6dc2d54443b2615c7435c081314
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:50 +0000

    smile Zz height turnup ear

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_power(work, time):
+        return work / time
