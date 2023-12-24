abf409b10edfaf05926e012cd465b68b810fef03
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:40 +0000

    put print favour damage idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
