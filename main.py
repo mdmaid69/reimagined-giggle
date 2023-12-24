ea953ad83165f9c45d1ce40a9cca8c4a2610e501
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:30:35 +0000

    themselves pile foreign wish watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
