af214b07e1b6ec048a68db1eef94611731d6ed08
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:11 +0000

    need sick news understand statement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def remove_duplicates(lst):
+        return list(set(lst))
