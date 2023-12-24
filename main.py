536fb7a787e8515873a5e6bc62032569a8b8e425
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:11 +0000

    future swallow tie straight build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def convert_to_binary(n):
+        return bin(n)
