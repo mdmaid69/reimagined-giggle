ee74e32344ad0a666e33d6846b93a8828048c713
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:24 +0000

    machine purpose Ww pinch hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
