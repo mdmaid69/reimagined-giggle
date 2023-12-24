5c0255ce622a3a5344195ebc30622438fcdbaf5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:21 +0000

    conclusion face reasonable factory autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import collections
-def create_stack():
+def create_queue():
         return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
