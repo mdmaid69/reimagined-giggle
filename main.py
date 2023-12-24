1d769e0bca14e4c477b96d2e04efa16f24b9e78c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:02 +0000

    importance steady middle match invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
