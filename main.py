86b5816c6ec180513a9e5dcb7fab5f03adf851bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:11:14 +0000

    father when frequent task runout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
