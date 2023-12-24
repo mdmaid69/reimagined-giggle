30a667604c218b06c60560d7a4433594f0379a4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:28 +0000

    crash later spring confusing sweet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
