4d511ea3e4e4ee1a987064520ba1e9f02523defd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:41 +0000

    border basically decision guard radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
