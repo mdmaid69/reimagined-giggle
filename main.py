42487fc8b47b55e4c35e4c45fc9f15fd8a1364dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:00:05 +0000

    myself plan frequent window demand

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print([x**2 for x in range(10)])
+  def multiply_numbers(x, y):
+        return x * y
