c81cf1eb3bf4f7bfd425bb87369cad375d3f6c51
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:46:16 +0000

    used butter responsible period bye

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
