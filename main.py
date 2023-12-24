2f11becfcdcdba4aa031c743830b327a9b5a2da0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:05 +0000

    stamp careless beautiful Nn impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
