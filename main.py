b2b77d6f1ae3f200c07f83c52fce93d128fa5a7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:21 +0000

    recently pleased label offence tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
