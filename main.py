9274472537479f58e46d7e2819b48a7262505d9f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:51 +0000

    represent lookaround excuse breakdown flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_min(numbers):
+        return min(numbers)
