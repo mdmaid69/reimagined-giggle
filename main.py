3809f71b9625dc7f3c9ef81a28370351fb6446ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:12:39 +0000

    spot unite natural spill glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
