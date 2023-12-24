76866bf87ea02ac2abf4f267756aff38e709c032
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:37 +0000

    fork glad city rubbish judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
