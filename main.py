b43ba009354ce4509af9f7ab616c6cfa2e95f0e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:55 +0000

    label ask splash face save

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
+def square_number(x):
+        return x**2
