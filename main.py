c82c987e3a2d867a6640e42e1c58053c36805027
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:30 +0000

    spring keepaway broad pattern fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
