f41837f5454c0ebde4a837948d4152f30adb5a60
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:26:39 +0000

    woman sometimes OKorokay genuine cum

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_str(array):
+        return str(array)
