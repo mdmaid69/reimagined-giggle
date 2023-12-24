1f89b0a3eb930029cd46db1a91be7f492608583c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:53 +0000

    waste spring stop cover accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
