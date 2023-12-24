18f57c81c9ecbcca605dc0d8a51fc59ba7526f62
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:35 +0000

    ago that once punish typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_area_circle(r):
+        return 3.14 * r**2
