87517b4bcdb614ac3b2e967d674da99c167106d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:04:09 +0000

    shut breakup land negative comein

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
