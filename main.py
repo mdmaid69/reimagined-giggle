29f1e7b28dbe9d056f7cf07ff4b11674c6c82e56
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:34 +0000

    door unusual before threaten middle

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def is_even(n):
+        return n % 2 == 0
