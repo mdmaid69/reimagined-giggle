13c7b0f3f6942555deddfbc4956fa9986f269d63
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:52 +0000

    confused control which calm test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
