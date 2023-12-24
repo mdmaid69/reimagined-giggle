a503b486bfca9d47f613aecad196d1c62d823762
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:10 +0000

    immediate noise spend leg comment

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
