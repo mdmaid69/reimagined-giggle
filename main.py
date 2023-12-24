5cf4615255486afae9d2125b39af269e6bbb843c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:53 +0000

    dig represent free whistle contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
