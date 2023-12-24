4d2133415cbeb89b76299ebddb2c0a0aa614c8d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:34 +0000

    remember clue when young over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
