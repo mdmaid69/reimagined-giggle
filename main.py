cb8a5fe01b0397d847cb35b369d2986f53390200
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:20 +0000

    conclusion feeling run freeze capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
