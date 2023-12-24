a30133e7b920a16be4cb57eb17c16623978497c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:11:16 +0000

    any sorry from active suit

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
