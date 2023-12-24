9a31627a3bbcbb5fb5e8ba77c22ea4b147d83846
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:08 +0000

    little material warm sentence wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
