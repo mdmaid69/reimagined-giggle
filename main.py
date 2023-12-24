86743fdb479626a1b0707a5862adb685e86029df
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:48 +0000

    friendly pride bring part mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
