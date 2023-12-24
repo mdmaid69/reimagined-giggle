d50dfd7192028f366502d3af58cad12136e8f183
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:57 +0000

    only it short offence how

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+for i in range(10): print(i)
