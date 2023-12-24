189157394c475abe28a26f521d59e795aeea38d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:28 +0000

    compare extra ticket expression excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
