4f0306dd271d6a715e8e6cf891428676b929f9e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:00 +0000

    now way result frightened steam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
