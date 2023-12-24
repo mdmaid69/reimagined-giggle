204b332ec146fda2d24b1c7276c2dc8d777db3a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:17 +0000

    otherwise needle find usually means

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
