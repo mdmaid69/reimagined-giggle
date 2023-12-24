efed17b6e344e2cd54c251c970863415a3aee54f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:24 +0000

    invitation talkoutof culture decide modern

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import re
-def replace_all_occurrences(pattern, replacement, string):
+def replace_pattern(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
