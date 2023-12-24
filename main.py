3e00a589978fd11e1590d0cd1c6565be2857c1d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:54 +0000

    small more special personal muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(lst):
-        return min(lst)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
