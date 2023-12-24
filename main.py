d5f251ecf7d18c248409a3e442f6f6d0189b07ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:02 +0000

    round deal address lesson cruel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_current_working_directory():
+        return os.getcwd()
