b65ea1cef5c38cb10bc4468672689e330d98c925
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:14:50 +0000

    nice me piece answer whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
