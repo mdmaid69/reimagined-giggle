ea5a43abb54be1e74d503cafad13784d3e24ce3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:07 +0000

    equal plastic bored work forever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
