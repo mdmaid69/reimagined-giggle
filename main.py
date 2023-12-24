12fd73eb592d8bae716e3edd278766417b2e4875
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:13 +0000

    safe fork wish difference yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
