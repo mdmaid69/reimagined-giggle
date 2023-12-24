7a2988523e9e9fb6e5d446289807b473238d8aca
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:52 +0000

    around steer ride dead happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-def get_current_time():
-        return time.ctime()
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
