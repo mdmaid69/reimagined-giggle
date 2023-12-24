94e5173b2277301304390122dd357d5dc49b5a6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:44 +0000

    female war proud beat ring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def is_odd(n):
+        return n % 2 != 0
