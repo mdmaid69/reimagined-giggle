8677c124207d5ada04a8de750554f91ebe42bd13
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:56 +0000

    turnup destroy introduce bus scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
