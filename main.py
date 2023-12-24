9903e30ce5dd2f6a0462b4d6f8fc6ed59747e594
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:17:13 +0000

    quick frightened need supply rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
