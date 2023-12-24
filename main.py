9b9f9ca967eb5b774b96d8bf8c58d3bdff4e1fd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:09 +0000

    sing building breakinto second scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def remove_duplicates(lst):
+        return list(set(lst))
