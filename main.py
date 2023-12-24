0c30b981a7df376bf50c0cc11ed6e5ff662798b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:31 +0000

    ordinary activity foot serious hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
