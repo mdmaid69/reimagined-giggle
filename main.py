756a2b0081ca92f096bd815b5f81e690d3e70971
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:26 +0000

    giveback platform flat either dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
