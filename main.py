00666e5dd235cd4786eb819df7ca3db93a5d30c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:40 +0000

    favourite run top live almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
