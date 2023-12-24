3abfa0f482e244ca81a54577906e33dee20fb7b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:47:49 +0000

    willing breakinto dear talkoutof mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
