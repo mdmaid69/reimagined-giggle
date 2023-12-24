c7ed3551897a613cd2dc84b9ff02b0d5565c9534
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:17 +0000

    previous takeback declare disappear stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
