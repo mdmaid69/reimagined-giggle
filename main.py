9c7cd58ff7be237b8ee052c3380d58dc0b5b4c25
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:55 +0000

    storm slightly fire callup cinema

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
