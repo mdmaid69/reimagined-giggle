519cd7f38a6c310fac06a44f7c6b3932fd060539
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:46 +0000

    chin false world sleep interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_max(numbers):
+        return max(numbers)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
