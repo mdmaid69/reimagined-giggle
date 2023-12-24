7b79e1dedacd2dea41bb5d6fc0679b4f1e0e7879
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:45 +0000

    there excited whether Ww belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
