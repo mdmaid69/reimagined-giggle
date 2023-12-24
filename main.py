663eeb765d9916ae34225422b52b1542c49c6c9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:20 +0000

    keepfrom confident opponent principle message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
