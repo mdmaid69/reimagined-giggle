5199a3f4eded2ff512b606d89b41706ba8e7d9a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:53 +0000

    unite look authority test runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
