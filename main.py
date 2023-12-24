580bf11a012074a175b90d59c7fa31d69526d0c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:59 +0000

    principle prove ours rid deliberate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-print(sys.version)
+def calculate_speed(distance, time):
+        return distance / time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
