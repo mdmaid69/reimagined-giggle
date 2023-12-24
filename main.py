fdc6a48d2236f7f3ca85135db0740514ceb68a39
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:48 +0000

    fetch advise interview regional afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
