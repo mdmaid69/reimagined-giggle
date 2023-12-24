99481d4eb7b210106ef2fa4743971dbeb34c8bb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:08:32 +0000

    post total priority flow tired

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
