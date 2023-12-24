cdfa878bc1d8c72f4149f3f9c856a61303edf70d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:19 +0000

    secret lie boil false along

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
