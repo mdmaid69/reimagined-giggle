679749d23d3f32fb9b319db334e7f986badd75b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:25 +0000

    education brave model content crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
