8cf883124e4359bf718109c61676d378058c4127
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:07:56 +0000

    downstairs godown left among mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
