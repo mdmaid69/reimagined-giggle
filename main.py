37b94e1cbd3209eddaa268fddacaedeeaaa12716
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:42 +0000

    high about start take handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
