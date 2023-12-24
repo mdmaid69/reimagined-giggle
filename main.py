e1cb191038fd15f14eaa750d2c8215e9fcaaf716
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:20 +0000

    burst Uu formal shoot game

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
