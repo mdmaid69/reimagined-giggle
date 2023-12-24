4eb97e66b0c2a4e23185c4e5c4d691a4bae0a688
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:50 +0000

    beneath open light bank concentrate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
