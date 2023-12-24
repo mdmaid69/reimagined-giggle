78e3430aed5fd13ae69eb1e6da4599b79d6a873a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:12 +0000

    impatient invite draw stage history

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
