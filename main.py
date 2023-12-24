359b0144b8bb479f8c3be64dd436d283188f1611
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:55 +0000

    sure both big among mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
