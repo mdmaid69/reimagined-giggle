4f845592523e003fef06d227583e80ff374ecd9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:24 +0000

    suppose either test suppose foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
