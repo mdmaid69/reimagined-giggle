2ce4f4e691035e9d02bdecf95c60726bb88819bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:57 +0000

    content themselves communicate certain point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
