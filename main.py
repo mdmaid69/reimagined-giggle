bf57ab788b1f8da8e694d91c8cc20247f48d9e5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:19 +0000

    bite slice getoff variety borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
