5c46fa8a5f0ae8d03c1f201f8464ad4a29629f88
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:30 +0000

    exist relief night ball while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
