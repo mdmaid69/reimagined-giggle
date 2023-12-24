459ee162b9480d0cdcf54a9a1dbcdfd46d37b5f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:52:11 +0000

    fairly in Ll joke cause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
