fe0845035d5b613c39209513bff03c7764a5f733
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:03 +0000

    impossible blow door other careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def is_palindrome(s):
+        return s == s[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
