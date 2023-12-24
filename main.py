a94ef50d2c7c34ff5bbc2ee4007a790ed6bd134d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:56 +0000

    play street left doubt description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
