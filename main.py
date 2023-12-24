5e90b90c3302cb272e2f03fe4a1794e224ad944f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:36 +0000

    perfect pull great similar sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_tuple(array):
+        return tuple(array)
