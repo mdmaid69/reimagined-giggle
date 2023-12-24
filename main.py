44db1c2c332982086bcc82aad5df0e780d833091
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:43:45 +0000

    boring surprise afford contact forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
