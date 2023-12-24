83176bda2f88390212edcea79483e2355a34452f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:03:52 +0000

    crazy return separate correct geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
