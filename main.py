8b9301f333d4eedec233b923ee3150cc66923119
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:33 +0000

    best shallow marry often can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_string(s):
+        return s[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
