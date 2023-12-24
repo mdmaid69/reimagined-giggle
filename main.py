68442403cdd0449f1560c4034f4779e19c42497c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:59 +0000

    invite juice public complaint dance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
