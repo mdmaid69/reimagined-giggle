76305373eaf9ab7a3e2188f87be13a3b435171e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:32 +0000

    guess rubbish embarrassed action breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
