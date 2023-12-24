6e86415d3786ebafe6b5ac7d033c2168f2142008
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:02:35 +0000

    strong black morning hello neat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
