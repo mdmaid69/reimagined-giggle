26423be723ab519055252f035cd487431aa65efd
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:02 +0000

    important body birth machine near

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def clear_array(array):
+        array *= 0
