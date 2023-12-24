34e785f7591dff0bdc7441b120e8c193d93a7bdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:23:22 +0000

    apply holdout steer keepoff raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
