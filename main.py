25f99578e1bbbe9ae933aa9dfdf46d4f33cecf21
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:08 +0000

    right respect murder squash although

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
