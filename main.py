6df618294c76019008ae9b9e39bc130ef8bd77d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:06:09 +0000

    aware pay pronounce contrast legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
