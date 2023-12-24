4eb4ac726e1b7cf7a546fc80d659ef0620957b8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:23:53 +0000

    pocket why muscle furniture pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
