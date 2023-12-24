161395f63d06ebc4680fda74355d2b9016739f1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:24:03 +0000

    crash scene getout print chase

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
