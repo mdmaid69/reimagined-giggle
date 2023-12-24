77d99106485abbfd335891a2ce54517a22a1b1c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:43 +0000

    stop invitation famous wind man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
