d1a2399912967daa76fb05b21cb2e6ea9bb6f389
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:32:56 +0000

    down miss contain wish makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
