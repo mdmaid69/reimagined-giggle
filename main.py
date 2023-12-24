a8d28447d7bc9521708a55237d391e9ee4011edb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:23 +0000

    change later smoke material joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
