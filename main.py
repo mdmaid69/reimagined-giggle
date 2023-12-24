6ca8604979912d41dda6c7528dbdce44c66ea61b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:10:23 +0000

    look shopping exercise remain scrape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sys
+def exit_program():
+        sys.exit()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
