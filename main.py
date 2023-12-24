cb0cacaf4bd22925d4c927fa1734038bface290b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:26 +0000

    actually suddenly easy himself warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
