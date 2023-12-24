37632d85e768e8f5ed84ebd53b7421b1aa1e25a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:34 +0000

    generous flood cross art case

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
