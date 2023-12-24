b71410b7bf1b33e220c5147511cf4d1173c6079c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:11 +0000

    comment unusual join frame dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
