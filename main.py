3d5fbeb90d39ec5e36385b81ea61d13d2e923dc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:52 +0000

    all total look journey their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_counter():
+        return collections.Counter()
