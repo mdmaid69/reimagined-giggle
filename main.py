0014e91e2a78e88676fb25eaca8a860cf7fbb9a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:04 +0000

    miss piece nigga shoot this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
