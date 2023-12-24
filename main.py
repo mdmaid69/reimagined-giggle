f7930150589ca5d492230c111a67b8d05d56c69e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:34:16 +0000

    fun goodbye tape possible lonely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+print(sum(range(10)))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
