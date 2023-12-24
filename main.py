d6e94c893c37f7cb1b9c7ef7dd72c46a10eec094
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:54 +0000

    thinkover better goback(to) steam Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
