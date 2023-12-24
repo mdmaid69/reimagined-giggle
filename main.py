cd518cb499bca25cbff728da7b0d5992c2d45059
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:46 +0000

    danger cross call task sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
