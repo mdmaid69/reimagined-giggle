c454ca101c3fc407aa5157d7e5a51b0f4762dee8
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:48 +0000

    opponent follow believe doubt tone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
