2ec008fa6585fe0b7aafc2c6fffcc3a1541755ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:07 +0000

    blame relationship me guilty market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
