eaac02d374ec47bf8386189833635b1b400f2a9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:00 +0000

    wish turnup waste stranger like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
