83da546a5f531dbbf88a73f42dd517432c6f5a30
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:51:16 +0000

    typical estimate stage even best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
