1dbd2605d2f9efbf2bb274166cea67677fb5c250
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:34 +0000

    off previous corner live usual

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def print_python_version():
+        print(sys.version)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
