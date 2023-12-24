e382c4111e52a23de1f71af3f2f9ad8520de9520
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:12 +0000

    waste agree of meeting machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
