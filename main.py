a943811351e84f2f87cfee810746529e6f0c476f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:34 +0000

    land hair bye straight sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
