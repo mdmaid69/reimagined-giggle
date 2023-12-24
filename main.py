9d6e389dc09f1ac04512098b797cabce15fdd4e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:13 +0000

    long arrange separate deal majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def reverse_array(array):
+        array.reverse()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
