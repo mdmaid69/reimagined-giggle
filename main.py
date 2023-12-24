f293b41ccf977301964201e5e97f635e74bda6fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:47 +0000

    stare insist continue late practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
