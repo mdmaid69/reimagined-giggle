307978cbe8be0bb7ba85e39bd7be843b8b0881ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:38 +0000

    worry run judge official false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_string_from_array(array):
+        return array.tobytes()
+def sort_list(lst):
+        return sorted(lst)
