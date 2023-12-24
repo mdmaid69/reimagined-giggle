5ab8f2c1cbbc29aaf778d84bb39dd436f0636eb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:21 +0000

    reserve die Vv tap everyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import sys
+  def get_python_version():
+        return sys.version
