76dd36c18fc410f99a070ffe4247952bf52537f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:00:32 +0000

    motion square remark off night

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
