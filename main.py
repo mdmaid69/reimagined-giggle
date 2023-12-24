8ab1728d9ba1eafee804a5b27798995318ebe6c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:42 +0000

    obvious greeting just shock probably

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
