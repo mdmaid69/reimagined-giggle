f8ea9d6b3ace728f86c8f3c039b6758fd5eb6cf6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:10:55 +0000

    include now wear wind middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
