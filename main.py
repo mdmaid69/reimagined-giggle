5047865e57124f0d82254a1362c97f385bb88893
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:29 +0000

    slowly now agree Qq remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
