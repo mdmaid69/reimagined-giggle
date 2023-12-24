d4fd5efc235525b981f09d59b87e7f628a5c1fcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:29 +0000

    increase damage please matter sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
