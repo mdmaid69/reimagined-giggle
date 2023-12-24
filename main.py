0954e4e702c017ead17c7c312dd762b22759a1c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:22 +0000

    cheat limit afternoon die car

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
