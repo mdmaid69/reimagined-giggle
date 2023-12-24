8f8ea0ae2a032eb7dd4ac1c5b3f27e2ec92cbc32
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:35:55 +0000

    afternoon tear top wonder turnover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
