4a7dd99e2194e5dc69dd8b06ec767a67f0783b9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:42 +0000

    sick week lay average unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
