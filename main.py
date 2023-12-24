46d3a5b5c43ae0bb2398b89c957b215fc1220887
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:59 +0000

    this party beside drop talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
