7bb03f13a32942bd962b07705a46e01ef8ebbfea
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:06:44 +0000

    danger wire store attract defeat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import sys
-print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
