41efc5d9c428665c6bfc8db9eec12c72567b59aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:10 +0000

    ashamed prepare mysterious bin Oo

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
