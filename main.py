a79d79e4c5556c30b3e5ba0a062995b25f2caf19
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:50 +0000

    he close offence never morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
