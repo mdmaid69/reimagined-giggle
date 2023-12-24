c6fff46597b6e42d4ea5c175ae76fb4c9ef4787d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:01 +0000

    hello bound except runaway declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
