c26717a8d93593ee49edfee592e61b7997181001
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:20:35 +0000

    speak concerned moral threaten brown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_area_rectangle(l, w):
+        return l * w
