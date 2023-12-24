0f39ffbca0f17af020a8fb26f8352d594e7a3f5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:58 +0000

    valuable lower wide splash judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
