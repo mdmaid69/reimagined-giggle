9bfbe46b6e8c13879a158b1941b1079888016f4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:08 +0000

    everyone ready read help angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sys
+  def get_python_version():
+        return sys.version
+def remove_duplicates(lst):
+        return list(set(lst))
