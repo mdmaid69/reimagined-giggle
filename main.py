9d281081c8fac54e8cc0a4a1207bd03a306838af
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:34 +0000

    except discussion careful getout open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
