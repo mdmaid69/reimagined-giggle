219b8eaa839cc0ac8634dc37444e6a12abe70df0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:02 +0000

    poor truth call finish embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_speed(distance, time):
+        return distance / time
