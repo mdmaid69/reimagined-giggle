681990d03f96cbb7167da36eb06039de14c72db3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:40 +0000

    immediately somebody strange breakinto open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
