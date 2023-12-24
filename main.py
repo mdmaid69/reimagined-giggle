04e3ce2c68eb6ce595b48842877132750fa272c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:20 +0000

    left delight holiday wipe they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
