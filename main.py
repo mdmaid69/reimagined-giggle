d1e8b2e030e20287bdfe0360bb9ebf24f63e1ca6
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:10 +0000

    search turn strike wave occasional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def count_elements(lst):
+        return len(lst)
