f988887a9623d787888814a0313f5ad54592704a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:16 +0000

    ring modern buy check meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
