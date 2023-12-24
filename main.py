2bd2f192565d3f605bf57db60e7d0a80cda4e51a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:08 +0000

    exact sign fair thick whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
