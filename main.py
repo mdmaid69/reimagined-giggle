020255bd0fe4a148ade892f3cd76fe16fec65361
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:35 +0000

    actual surface 564encourage something lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
