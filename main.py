7ba0ebea25beab998d080adf153a1732833867f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:51 +0000

    turnoff talk not her unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
