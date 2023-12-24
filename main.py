b4344e036f1419a80f27a41344237cd5505f5eb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:06 +0000

    likely then giveout one upwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
