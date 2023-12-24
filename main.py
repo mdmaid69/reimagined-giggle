71f2a2e177c36d6fa4a3a4376e7e55ff020db2d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:55 +0000

    proud new independent gentle stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
