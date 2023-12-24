5c94a81998a82e4e665f75340a36af473b278c19
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:51 +0000

    desperate dance nothing our familiar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_union(list1, list2):
+        return set(list1) | set(list2)
