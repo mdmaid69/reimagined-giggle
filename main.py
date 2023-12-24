0e89263da459b939868e6be98b7d72c7c4aee65c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:55 +0000

    best wall manage matter practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
