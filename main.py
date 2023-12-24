751ed88a78dfd1aca20ed7240a05cffbe2235bc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:43 +0000

    declare increase coat about peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def split_path(path):
+        return os.path.split(path)
