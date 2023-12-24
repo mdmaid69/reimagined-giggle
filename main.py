7b6fced630a7e981213b47aeec854b59aa930a3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:56 +0000

    any lock familiar produce promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_speed(distance, time):
+        return distance / time
