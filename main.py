75726765ac4149ab5e4e672fae416f56dc9d691b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:08:05 +0000

    occur price weak succeed relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
