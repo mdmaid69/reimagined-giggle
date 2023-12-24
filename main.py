2791fe8b96bb8632c22a00013f97e569b60c487e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:29:34 +0000

    sky direct circle 571enough behind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
