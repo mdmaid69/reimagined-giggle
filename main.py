f849f9935442822eca6feddb5481474c3a2a1163
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:37 +0000

    I brush born tend shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
