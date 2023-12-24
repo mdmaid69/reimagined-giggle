eebfc114e327e6925346cf96cbd2888aa44c6707
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:34 +0000

    brush love Pp interruption sleep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
