dff984f7535be88d4e5e85d8df3d971475da5662
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:36 +0000

    afternoon across pick traffic contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
