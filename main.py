2f00b37875c48c9daee6a1dc54bf457e829d0e33
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:35 +0000

    voice progress wind closed raw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
