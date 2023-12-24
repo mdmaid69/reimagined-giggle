bc7c82da092c95aa122181dc1744d5084cb9a210
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:59:21 +0000

    workupto runinto feel once pause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
