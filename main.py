ca32e93e85b5da23f4cc8595e547cf27af356a5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:35 +0000

    over pin scrape hang feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
