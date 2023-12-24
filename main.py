9a56c2fc23f83e8a024158526f744c35b343aaec
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:22 +0000

    chin statement straight exhibition damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
