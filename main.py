f6b9a4ffd12cfb947b44ee24ce09ea6aa780730d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:23 +0000

    indeed official break fashion cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_array_as_str(array):
+        return str(array)
