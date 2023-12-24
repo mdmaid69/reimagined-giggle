19303e0b7c50813dc6b7887a0dc744f6c4c6dc39
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:09 +0000

    quiet people tonight promise control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
