7d41cdb86b46a3726a96511e5ee00eeaa990486d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:27:33 +0000

    gooff time take available active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
