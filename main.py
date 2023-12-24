4afca6e370ec3e09b8d3bbe7caa5791c6388d698
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:18 +0000

    conclusion conclusion call splash dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
