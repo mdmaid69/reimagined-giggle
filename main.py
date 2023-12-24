3636c31e69ea89c1ee6a2cca929932a4697dbe7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:22 +0000

    address dangerous rule custom even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
