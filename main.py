fc0e3398416e64be19b101f6131ead9379caebb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:54 +0000

    travel yet confused offend judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
