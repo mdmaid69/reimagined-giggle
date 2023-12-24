72357cca32311cfd3120f755af0bd3aa12d8eff5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:44:42 +0000

    speak getaway tradition load by

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
