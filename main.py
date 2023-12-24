e0194a084c82b17d17585bbb8ae97e52305f7a6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:59 +0000

    false less in talk occasional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
