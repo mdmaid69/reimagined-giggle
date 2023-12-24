b9b94314c4247884be896d11641b6cf5532df00f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:00 +0000

    satisfactory evidence ride cost position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import sys
+def exit_program():
+        sys.exit()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
