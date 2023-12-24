9babe00c96f2e36675821a4b260f2eece8be6fa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:43 +0000

    convenient hit wrap child doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
