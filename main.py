590e8ec8758194cd6aba90ecf23074b6eadbe9b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:33 +0000

    circle bill half confidence taste

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
