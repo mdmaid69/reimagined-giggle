20a36ad0ea7ac269b7f80d315c2bfdc114b87e09
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:52 +0000

    doctor notice well any comeback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
