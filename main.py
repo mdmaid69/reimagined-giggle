2fe900ea3c7e3b8bce77c4d4f6ebdf71586dc652
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:59 +0000

    unit slight zero murder undo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
