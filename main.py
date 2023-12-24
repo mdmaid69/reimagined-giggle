645024b590d9de5b6946a8fd1e7c081e6db797e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:49 +0000

    breakout block excuse point every

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
