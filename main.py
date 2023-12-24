5ead19a49878e027beb03c8ab87d30debc98de32
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:50 +0000

    division future along deep little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
