3cfb562ffdc12bd6b90296f2433938d1fbfa986a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:24 +0000

    band separate comeout film method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def remove_duplicates(lst):
+        return list(set(lst))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
