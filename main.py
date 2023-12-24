f5453271b60e790dcf34c2a7c7e686f1c3757063
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:10 +0000

    angry smart interview fetch welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
