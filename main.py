b2908cb47ab1f64673952f2e1953b7f60579d967
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:20 +0000

    last loss nothing hurry date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
