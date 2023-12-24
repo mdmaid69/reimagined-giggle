2ee0a37b8e555642c2c347bd31d6080814ddb9e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:19:33 +0000

    mention keepout roof purpose level

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def remove_duplicates(lst):
+        return list(set(lst))
+for i in range(10): print(i)
