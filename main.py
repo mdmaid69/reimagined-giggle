1f86b13f16e61af0edddbabbf31f292c1cb19360
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:59:15 +0000

    Dd personal family achievement turnon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import time
-print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
