447a2e3eb52fd61684c67e03ea4bf8e2be3fc953
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:57 +0000

    payment taste shopping most light

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
