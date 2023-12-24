7af025311506038a0b64822dcb392810302c2115
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:45 +0000

    finger opposite average slightly lose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
