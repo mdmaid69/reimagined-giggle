caadb29a5c69e5f9343abb83ad49d17f55380dde
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:13:29 +0000

    worth congratulate quality keepfrom score

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
