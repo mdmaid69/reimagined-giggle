7cc31590e9f4a614cfe144296b3b779850c524b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:27 +0000

    rub rent bright sticky none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
