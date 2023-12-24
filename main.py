5cbb5056cb71ba96352a771859f9e59f4030a399
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:30 +0000

    polite pipe hold slightly fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
