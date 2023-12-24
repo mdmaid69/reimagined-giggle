9e3b02798e605fff885df49ffd812d35e878275b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:58:22 +0000

    turndown sure soil whistle lend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
