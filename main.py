1d884a6b608287a1ac3118dbc597fef6d1ad3eab
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:44:03 +0000

    lie total morning politics dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
