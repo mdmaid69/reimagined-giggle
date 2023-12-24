a1c838e29df616f80c74ed01068134ac81939af0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:13:56 +0000

    we reduce uncertain object crash

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def count_words(sentence):
-        return len(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
