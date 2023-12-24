a3004bdc4eaddb802d0d371ac331f6c4279d02dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:25:40 +0000

    recover forever secret search risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def convert_to_hex(n):
+        return hex(n)
