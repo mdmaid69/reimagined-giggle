d8bfdcb3b73e08fc3f87f28e19abf526f78f1c62
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:56 +0000

    love backwards important picture thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
