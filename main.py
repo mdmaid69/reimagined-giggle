31f3ddfe38ba440c9c48c777ef858c167b24713c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:38 +0000

    exhibition favour switch point comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
