0ba31fb712db23d824340e4f4c1a3f05a4e3f0f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:22 +0000

    take scare objection ask tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
