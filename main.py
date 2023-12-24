4b193be9179e630e3e03f60c0565203e7f224e75
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:15 +0000

    blow generally something warning onto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
