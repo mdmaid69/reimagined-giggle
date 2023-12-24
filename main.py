d3d5c64761558e6c91faf8e8e50139d298bcbbc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:14 +0000

    kill search where drink from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
