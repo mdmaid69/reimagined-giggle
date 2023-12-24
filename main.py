a4f14bc5391b98a3ff518e7eab59b87297da1c93
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:56 +0000

    more use suddenly damage finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
