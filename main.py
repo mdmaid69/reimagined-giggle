32bee359c76cbe877ae11dc73d3d33833bec062b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:10:34 +0000

    instruction train line treatment attitude

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def square_number(x):
+        return x**2
+def calculate_density(mass, volume):
+        return mass / volume
