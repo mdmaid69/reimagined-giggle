656cfdbe10c8673e507f8ce7152bebc32e2ae074
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:54 +0000

    admit brief break turn fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def remove_directory(path):
+        os.rmdir(path)
