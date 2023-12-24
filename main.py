5683802610851c396d038a3456f4e831aba6e3fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:19 +0000

    mind separate tongue space runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
