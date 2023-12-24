dd084e739b6d83f727fece823ae3b58c3acc7a5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:28 +0000

    home through remember ring makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
