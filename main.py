7137fcf5db0f0be657a0dcb516096b1ddfda9d5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:24 +0000

    silent next yet command bound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
