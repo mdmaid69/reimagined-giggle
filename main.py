e7601631b9e667d99183be4cc8c1420378236265
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:25 +0000

    single meaning so argue patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
