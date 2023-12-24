aae899a3ad5efd7b0486e11af4a092715408be8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:24 +0000

    bound centre justice bottom everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
