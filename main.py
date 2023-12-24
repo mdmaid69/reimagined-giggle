aee4341c2cebdd006a8a5ad8875d9cce41e7babc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:10:43 +0000

    gift just suggest reasonable fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
