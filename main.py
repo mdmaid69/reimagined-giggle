bfa559c65ef76ed91b49908dff037db672816994
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:14 +0000

    compete creature still party without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
