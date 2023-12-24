ef463738c2802fea680d8d7c459161acbf5c9327
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:48 +0000

    judge spin frighten hate burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
