8c3a8613408e2edb5ebe9911bc94bf1b5351b128
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:59 +0000

    help possible acceptable move appeal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
