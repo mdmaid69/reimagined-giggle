7556ff2284bf490702bf0c7f1aff16e790c88dae
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:58 +0000

    female method stay shall human

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
