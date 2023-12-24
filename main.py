b44ea6b744092ed48dd7d55da0bedc047bd07fca
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:51 +0000

    course area rid meet turnback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
