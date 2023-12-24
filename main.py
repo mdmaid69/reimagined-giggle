2701a71ed22e080aa990dabea3e7bdfc50b6a5b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:10 +0000

    reply software cost answer only

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
