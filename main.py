337958f3e5db321617638a5af02f9e619cb69325
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:07:35 +0000

    follow very plate pay today

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
