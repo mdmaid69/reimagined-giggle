3495ac6a84d4fc00f577421ff13e7e566cd29bc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:56 +0000

    takeback the weak fast flavour

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def add_numbers(x, y):
-        return x + y
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
