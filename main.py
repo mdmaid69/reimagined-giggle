3147f8bb7fe88736070a7523c80c0454ea29b885
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:56 +0000

    letter promise necessary how offence

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
