e0439127acff6ce772b5cabf34b038426706b1dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:50 +0000

    nothing lower although where upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
