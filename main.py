99875ed8b5c2e7c3380d150af7473f1c4253fd95
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:25 +0000

    beautiful pencil score famous anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
