94559a0b80eac5b43a66e8ba09ba59911b4fe426
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:33 +0000

    win tool quick fence past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
