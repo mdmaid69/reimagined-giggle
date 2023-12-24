32d53d77cb71cbb8493e7c2b21ea5ef0ae4cca1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:12 +0000

    consider train uponon only door

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
