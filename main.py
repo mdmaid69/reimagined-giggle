c9aea3265a451f0dae99de3be1e469d6c1b5c0bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:38:09 +0000

    cost release begin future drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
