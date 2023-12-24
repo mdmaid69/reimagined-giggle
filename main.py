88b54451cb5741cd5ff05cf9dcb5c9c9dbb8353b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:48 +0000

    correct seat today pity grow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
