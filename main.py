dc5cadbd1a72ab1e0970390b4f86a951d6534d1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:14 +0000

    sentence business prevent comefrom appeal

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
