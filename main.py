eff560f553de390a08a25672258f64fb42fdf37f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:45 +0000

    freedom favourite top of speech

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def subtract_numbers(x, y):
+        return x - y
