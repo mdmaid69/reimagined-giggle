873fbd0c1d9c16855202a52f9eb614a7d9bfb595
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:36:01 +0000

    lonely occasion finally reasonable organize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
