3c9fdb467b4675760a7140a994728bc0d546c76e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:41 +0000

    completely seriously impression laugh figure

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
