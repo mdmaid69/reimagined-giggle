1bc358df974011373eea2d78363c31f36816596e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:51:47 +0000

    else shop left belong work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
