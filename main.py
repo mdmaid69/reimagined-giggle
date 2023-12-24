3afceb6a057916bcb17f3254a555fc797801e007
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:06 +0000

    send basically look operate hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
