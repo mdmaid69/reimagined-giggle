f2fed1a58aa1ed983c687e871adb80b64d267de4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:45 +0000

    city receive poison involve unite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
