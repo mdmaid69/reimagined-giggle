01f7cc1a5c59e446b7fdd8216b5ab231d553307e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:51 +0000

    genuine short sometimes hate must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
