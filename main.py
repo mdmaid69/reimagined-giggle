bcdafea0896d3b5f33a2ee9d805784b6dab2cb75
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:13 +0000

    difficulty black married different body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
