6d21bc52144205f1270be779fd3322465893034b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:07:11 +0000

    hospital funny nerve second object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
