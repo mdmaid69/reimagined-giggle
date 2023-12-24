84a128c0bfb4a4cbbd6cdd295947ad001d2bc3b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:37 +0000

    onto issue challenge allow help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
