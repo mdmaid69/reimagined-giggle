945d0e2ea3b54c1c26e3fa98528fb3a809ea5286
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:04 +0000

    certain copy traffic image thorough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
