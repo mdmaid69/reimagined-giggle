3634c19b62a8760d6b89080b15b2b23c57fb995c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:32 +0000

    past pronounce thirsty breakout trouble

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
