2a787255db2e1d0247bc08ef9b719d845a94331a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:12:15 +0000

    choose hold fire attitude start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_speed(distance, time):
+        return distance / time
