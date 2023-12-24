d7931dd6080cbfb32643fa0349aa3b993023867e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:00 +0000

    back forever open great wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
