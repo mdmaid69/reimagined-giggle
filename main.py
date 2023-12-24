3e90d5db2818f8fd33d83c408ffed0c34d69c747
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:32 +0000

    mood memory variety raise wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
