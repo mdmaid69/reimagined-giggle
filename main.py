db65dbc05951f1417e18c175be3d9f1e68da1aea
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:52 +0000

    increase kind afterwards aware emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
