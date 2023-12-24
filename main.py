5edd2b6dac94a33b49f8666f2ec56dbf0e13b125
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:51 +0000

    bright hard usual constantly look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
