49ad5cebc2af396c967c823a0eac8a347e02888b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:43 +0000

    maximum rob follow person available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
