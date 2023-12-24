794fb3c075779b62dca54b343b534fc322fd6b85
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:17 +0000

    back fault still real whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
