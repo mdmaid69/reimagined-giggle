fef6f81ca63e4b04fc250804b3901512f72b3bf7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:15 +0000

    face ticket danger light before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
