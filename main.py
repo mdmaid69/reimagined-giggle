c5adf76c8df10d018066c5948379e8e3e580d61b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:44 +0000

    excellent least immediately cruelty comeback

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
