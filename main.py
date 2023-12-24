8ff10a7d651ee7935b5ed5843f198a4da41a9021
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:21 +0000

    within choice juice snow occasional

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
