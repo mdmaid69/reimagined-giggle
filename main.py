166f60b7c3e52a07d48555781803c86c53fc1496
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:09 +0000

    poor pleasant change amount quantity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
