52965bb8aac359693d3ceaa7143548eed78bf35c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:23 +0000

    building clearly possible payment exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
