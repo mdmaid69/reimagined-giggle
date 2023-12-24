a7816a6c48bf8755278184102ae6173b825b9aaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:09 +0000

    whatever burst autumn damage close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
