0f102de00cf12428e6e1df285edd8abee23d4789
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:22 +0000

    purpose deliberate popular well proof

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
