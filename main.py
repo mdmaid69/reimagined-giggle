5c8da7b4abc1e93a91ce9a7dc5331cc0a8f31ba8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:19 +0000

    partner that century tomorrow notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
