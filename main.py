e95c555023c85db1ea4efaed4823329781eba14c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:56 +0000

    possibility sky lower form from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_max(lst):
+        return max(lst)
