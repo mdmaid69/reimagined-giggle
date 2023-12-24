920bc329bc8f9b824874566ea45abb61d2b87a93
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:54:57 +0000

    date dinner permission history statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
