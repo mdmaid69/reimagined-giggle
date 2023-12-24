52ade54a0a65feb7ba781ebb2b50647f0d781718
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:17 +0000

    operate swallow explain cruel he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
