951485396927129f357aeccabc6948f929d467de
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:47:33 +0000

    protest squash organization notice usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
