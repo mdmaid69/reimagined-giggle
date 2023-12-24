c6fb314e3a58587e3e8fcf47bb62440fb5561251
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:00 +0000

    bit callup lost complete charge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_set(array):
+        return set(array)
