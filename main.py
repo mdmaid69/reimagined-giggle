38579993f461ccb17b2d3e500b30c9714efedc6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:24:14 +0000

    talk come meet force finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
