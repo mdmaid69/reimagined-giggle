4ccbbe412b33b38d0067546b2809a9d4b3011723
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:45 +0000

    several nerve refuse trap earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
