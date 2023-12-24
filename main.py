a551ba1bf8ad886fa8386687bc765a888e432cb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:22 +0000

    we national everyone right apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
