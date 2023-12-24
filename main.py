5bcd00f4b7e2e1acea7034de1fe6fc9cbc65e3d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:10:22 +0000

    every part run impatient correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
