cbddf1b0153e282cfaa7037d69ab167eb3281f71
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:45:16 +0000

    sleep abroad before well notice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
