bb886d51662ae84cb11356dabceb9c5d4d933684
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:37 +0000

    centimetre during hear exactly brain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
