523ff9dc9021bfcaa6fc9f2c1d60783f3d247e5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:35 +0000

    around comeup niggaacademy plan gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
