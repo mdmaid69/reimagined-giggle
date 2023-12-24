18811c7ac421195bf081ccb8105c6e83c5f99eb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:28 +0000

    object hardly entry anything minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
