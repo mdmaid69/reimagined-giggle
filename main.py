e9bc19d1e062cc597600b52685079f5f79570894
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:48:42 +0000

    range reaction muscle dinner sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
