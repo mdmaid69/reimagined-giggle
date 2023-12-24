e6d19172c9d5bbb52a00101b45563bc86f352b30
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:44 +0000

    entry married fierce church acceptable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item(array, i):
+        return array[i]
