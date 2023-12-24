b93408b66107a8b569c20f1c0cea47380e509c3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:43 +0000

    tap service getout confuse job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
