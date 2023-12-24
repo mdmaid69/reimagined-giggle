09e1a5160cf400c30504868c65a19b40fc12b638
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:12 +0000

    fall cry shop Pp voice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
