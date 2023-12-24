c8f62c408c61cc457a84cc6f15cbbd2df3f345f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:57 +0000

    and appeal foreign squash shop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
