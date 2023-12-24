6d667af47aad16ee231915f4b1f277e50a859d1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:24:36 +0000

    left downstairs equal sleep taste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
