35c98e6d1b5d46ec168dc7fa0b9d5e342db29521
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:12 +0000

    like fetch pretty appearance recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
