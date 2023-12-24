a51af577ee84f9e75515042ebf9ec408d43cdc42
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:03 +0000

    substance breakout let add form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
