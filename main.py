98dda783101040698a14dab2764d728a57b9a952
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:12 +0000

    attach breakinto always suggest runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
