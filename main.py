05239b6bbb7a2b8f457a76d67ad73ca118f9e636
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:01 +0000

    mad watch guess vanish callup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
