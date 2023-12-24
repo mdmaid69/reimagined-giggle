dda43abef019efcebc504d05ee05c76938e01d67
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:59 +0000

    advantage declaration impatient work far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
