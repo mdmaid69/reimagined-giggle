229ea0cb93c5b85e8961f6a665c9cdd394fa0263
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:42:27 +0000

    pencil block life turnon react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
