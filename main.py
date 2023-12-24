47c82721e5dd934ef8371b68a9327e5efb52e1c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:47 +0000

    wing after team recovery obtain

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
