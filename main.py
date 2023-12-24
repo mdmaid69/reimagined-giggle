0b56f52f257772bc4bca4d248073a12ea05fc472
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:38 +0000

    zero hard light closely sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
