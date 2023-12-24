062344496e7d006f7bcf6e8b2dbd7f6364ff8c38
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:39:57 +0000

    punishment right downstairs Nn noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
