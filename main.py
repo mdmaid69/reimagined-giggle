2f4cfe9e53153fcbd6245b20f31887b974c6f444
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:24 +0000

    direction comment clean yet mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
