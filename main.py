64bce3a2b74e7e1cdad0c8b7728b65c0fb680fe5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:53 +0000

    important callat fail possible practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
