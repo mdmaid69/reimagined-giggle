9cd86c09fca347359e55734ae37b0bfc06b3867d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:21 +0000

    drop house famous sign fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
