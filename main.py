d487d09f489ba337bdeeec2b79bc72b4301edf22
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:53 +0000

    both path door impressive brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
