8273804008da7ba759c95cc8cccef57387e7dfec
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:20:20 +0000

    listen board illness arrange Nn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_hex(n):
+        return hex(n)
