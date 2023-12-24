ab22760b16f03f5f3f1d7dd0bbaee5d94c014891
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:59 +0000

    accident either delight surface result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
