2322f6bddfe4683a78d6872bcf7493bbc8832b55
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:15 +0000

    address name desperate difficult union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
