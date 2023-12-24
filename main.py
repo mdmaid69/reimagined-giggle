5eefc1d316e52336d5cadc51b4d0c0325595f657
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:34 +0000

    event flow die sort strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
