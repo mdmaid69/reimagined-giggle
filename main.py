37e3a55b702059675adfe1330e11dc6c64f9da13
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:17:06 +0000

    so rarely sing goup school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
