04524663433ca23049bfe8db0c8774e58e4727df
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:02 +0000

    need look wonder bet once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_hex(n):
-        return hex(n)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
