540a96faedbed4f31456a20e659466144d521563
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:13:13 +0000

    shape occasional moral aim fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+name = "Python"
+print("Hello,", name)
