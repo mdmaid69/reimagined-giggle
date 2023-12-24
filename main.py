ef9ce69f96106aa3b7eaa8d1e68df4742ea8b7db
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:26:41 +0000

    Bb room narrow breakthrough getaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_str(array):
-        return str(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
