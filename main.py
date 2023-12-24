cc7520448c943b518beb87f7044b82bdcda168f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:01:07 +0000

    beginning during callback trick still

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
