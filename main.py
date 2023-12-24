e3fa7f8c39b23784ccc9831d757a58dacf55006d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:44 +0000

    please pull this factory song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
