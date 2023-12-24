867dc9ac6ccad9492f860074f6b8bb5294821062
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:00:13 +0000

    doubt decide frightened distant serve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
