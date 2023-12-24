11e352490f5d7c9b287a27480b9f49db552c7bc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:22 +0000

    wear succeed without national pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
