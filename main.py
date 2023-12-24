87a75091e8143c5a68b77c87f687af3e0e723f7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:35 +0000

    free trouble hair else squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
