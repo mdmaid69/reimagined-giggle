2c31760dc4c4106a338a3f2bd0eb643eca1f5ae2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:03 +0000

    wish male perfect quantity prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
