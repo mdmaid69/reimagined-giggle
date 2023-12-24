0b028136a064a371624163a92ea883717c9ed0ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:11:43 +0000

    quit distance persuade deserve confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
