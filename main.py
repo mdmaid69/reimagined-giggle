e4e2d5fdcce5626605856f4b9c7ddcb01d770879
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:36 +0000

    will born sale live brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
