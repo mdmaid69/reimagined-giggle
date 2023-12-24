ae761e06f9bf72b33c45380966138981d0583f0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:45 +0000

    upstairs laugh house side guide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
