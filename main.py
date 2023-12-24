d740c77ccd82ba0517fa02e4997fab18058615ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:35:21 +0000

    breath myself someone match murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_list_from_array(array):
+        return array.tolist()
