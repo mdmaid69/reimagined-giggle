7ed4517948a3c8022c0b378b2130fd05576a912e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:25 +0000

    wait able Yy single fall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def find_min(lst):
+        return min(lst)
+def calculate_work(force, distance):
+        return force * distance
