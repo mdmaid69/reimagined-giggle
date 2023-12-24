bc1387735b84f25524c95709bfbef15045395962
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:08 +0000

    judge on vote majority claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
