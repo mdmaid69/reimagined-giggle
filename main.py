f0760bac1de95ee96b88168929c34ca1c300b956
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:00 +0000

    report off over clear grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
