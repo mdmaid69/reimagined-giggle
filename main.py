0438e2dc5e288678dc3373b75939cf1bc0894e61
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:07 +0000

    tough undo miss broken responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
