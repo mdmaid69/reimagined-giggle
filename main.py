a985518b7eb085df8ad5c95724b0b52e7ce9e409
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:29 +0000

    basis actual accidental nice within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
