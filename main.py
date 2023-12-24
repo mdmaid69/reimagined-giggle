fa4cea1d910c499f8432fb1480666ecc13477904
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:05:16 +0000

    opposite tidy rich attractive stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
