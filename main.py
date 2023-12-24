18d52c3e19ea6afa508a0eac56ae8abfb049c35c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:54 +0000

    block make 566end where talk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
