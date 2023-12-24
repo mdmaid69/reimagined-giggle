443f4fd4d4f1f44e92d00ca8dba150240b78913f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:16 +0000

    over bus interruption escape children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
