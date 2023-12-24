ac66984c73f3b0893cd54f4742cdb972354d2685
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:29 +0000

    hole interrupt comfort not tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sys
+def print_python_version():
+        print(sys.version)
