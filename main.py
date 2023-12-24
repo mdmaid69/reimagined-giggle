c813bac112625516c2b7e250506aadc38cc1f1ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:34:01 +0000

    theirs genuine calm cost visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_list(array):
+        return list(array)
