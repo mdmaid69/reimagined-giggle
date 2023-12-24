2297a6568d18b81bf4dbb0568b504ca1d1ae0e0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:47:29 +0000

    soon advise building stay should

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+print(sys.version)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
