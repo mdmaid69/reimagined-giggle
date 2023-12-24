c3f1250e60ea6c4b718b260100dfeacc22e6a1c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:25:22 +0000

    too wherever unlikely have relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
