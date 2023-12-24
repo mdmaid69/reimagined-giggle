b4ceed0ddc08b7d00361e11a896fc4e281147af9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:57 +0000

    benefit turnaway steady mystery miss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
