4011d7f485c28b617b4ea3a391b2a26ab208bcfa
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:35:06 +0000

    back best science next trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import re
+print(re.match("h.*o", "hello world"))
