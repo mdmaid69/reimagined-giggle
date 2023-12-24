5590b38cc657c4362f0f298b67103b202fc57c69
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:55 +0000

    common tonight front soak waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
