9b057256bc0dcbaff517c44f0cf7a6e532fff4fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:52:55 +0000

    force precise congratulations dream treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
