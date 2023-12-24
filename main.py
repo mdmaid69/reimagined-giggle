cac0fe9133228637c2d9f70eed0866bed2ba2c23
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:53 +0000

    accept small disappear understand gate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 def is_prime(n):
-        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+                if n % i == 0:
                 return False
         return True
