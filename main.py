73a290e981d7b25c923b9581128885e52889cd35
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:46 +0000

    emphasis cause firm preparation bringup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+for i in range(10): print(i)
