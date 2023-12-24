fb7f4a736dd78a275388a4ad2cfadfc230954fe6
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:03:42 +0000

    row thought stay like fortunate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
