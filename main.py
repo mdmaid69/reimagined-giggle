5b3ef28a85245acfe27feb27ffe7c6e0375c9730
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:46 +0000

    brave light dull single relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
