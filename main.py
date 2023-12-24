4be36692a6261898a0c9c819d77daf636d040db4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:48:00 +0000

    sleep cruelty trade tear round

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
