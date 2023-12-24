8b6ecf216f8ed45a19ec618510b3dfdd238605b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:45:03 +0000

    rent nature autumn obvious strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def cube_number(x):
+        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
