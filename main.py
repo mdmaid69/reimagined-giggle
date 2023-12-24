9057fefc1022a0cc3bc0b3570dfd7afd847167ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:51:26 +0000

    petrol tap secret rub lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
