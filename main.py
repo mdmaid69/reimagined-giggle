8d952df24fccc63a69ba2bac34bbb5dbce435238
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:45:07 +0000

    violence slide exciting regularly bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
