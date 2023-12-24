603f3eb65d02d397ec36b8601f5cde8d4ecd666d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:45 +0000

    pronounce face one shame opinion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
