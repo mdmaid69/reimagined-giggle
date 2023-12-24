5dc845c1996b3276bac89e0ab36966b10a5f5138
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:00:48 +0000

    describe means loan bringback rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
