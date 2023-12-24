4dce68f90fcec69881e190860fa19111fb205dc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:09 +0000

    but exchange tidy 569enjoy result

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
