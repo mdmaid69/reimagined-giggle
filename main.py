90247ebd75a3119c41cd2f4bc6bc12cb4329861c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:46 +0000

    height control treat control equal

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def is_odd(n):
-        return n % 2 != 0
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
