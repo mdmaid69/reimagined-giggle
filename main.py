782059be1f9dbb59f01e6b845df97fbbb92c84c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:59 +0000

    take list passage split correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
