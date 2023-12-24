cd2a6f7284c134b354a75e1b58650927fa85e178
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:02:46 +0000

    since which display obtain description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
