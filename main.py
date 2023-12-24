660aacd98fc71f46e55558d1357c7a6c38084447
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:44 +0000

    bored shock bend front variety

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_odd(n):
+        return n % 2 != 0
