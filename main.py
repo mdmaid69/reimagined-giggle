7edaf044dbb3e0026a1e4b5c47b5ce41a65c7360
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:58 +0000

    balance cheque design result badly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
