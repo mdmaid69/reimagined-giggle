162f1e8576b364b88f8ada2baec2f13e2ddd910f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:00:49 +0000

    space frightened feel reason trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_list_from_array(array):
+        return array.tolist()
