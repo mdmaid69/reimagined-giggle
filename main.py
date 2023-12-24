bb8c4bc16484595901ebe019640f5794f427246a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:32 +0000

    project exchange row plant press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
