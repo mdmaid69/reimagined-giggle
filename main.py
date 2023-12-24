32817ffc89bc88fc20b9a4af3b1a60a0309bf5be
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:05:58 +0000

    Ff measure narrow environment yesterday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
