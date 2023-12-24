538ef58f03ff4d3c7cc737e445ebce0810e825a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:55 +0000

    keepoff custom glass train hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
