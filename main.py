78915dc497d9451ce6dab78c041597d97bf47382
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:21 +0000

    shall cause land previous either

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
