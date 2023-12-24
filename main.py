448ad990546331ba9af24cfb65f017451a0c5b15
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:34 +0000

    criticize his pleased solve study

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
