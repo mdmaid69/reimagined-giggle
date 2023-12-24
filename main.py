21918f65a73bd8338cd122f31b855e67ae47b2dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:29 +0000

    would opposite edge result pretty

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
