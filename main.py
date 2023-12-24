590ca1190eafd898488f0614441c298f2cb22f2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:33 +0000

    offence contribute good base introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
