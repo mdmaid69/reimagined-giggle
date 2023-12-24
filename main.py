29a63ed18a31b9fc139d52b162bb7a37c5ca1e8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:16 +0000

    previous myself they only below

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def square_number(x):
-        return x**2
+  def multiply_numbers(x, y):
+        return x * y
+def convert_to_hex(n):
+        return hex(n)
