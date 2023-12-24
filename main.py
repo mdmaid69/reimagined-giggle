99af68385f9817fe7a3caa74e34711e34efa3db1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:31 +0000

    good car process could weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
