7de03c9595d19608075065392cdc3c9bc7767f96
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:39 +0000

    pretty environment sudden back risk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
