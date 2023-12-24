2d85a71bd281f09c5b4c24e5bffec6366bca84a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:01 +0000

    home raw somebody thankyou anyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
