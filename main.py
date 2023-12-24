a3309c9486035faedd11626050a37e0c0f4a0640
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:04:54 +0000

    world nerve truth store protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_odd(n):
+        return n % 2 != 0
