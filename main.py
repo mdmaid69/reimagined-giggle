b66b4add182ffc4a7e03eb1a040a7020115fb303
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:19 +0000

    loud level weekend mix plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+print(os.getcwd())
