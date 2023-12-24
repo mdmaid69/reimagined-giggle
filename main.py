eabba3e41bb239dcdf12541d96c5115449a7b213
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:06 +0000

    towards steam birthday tonight lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
