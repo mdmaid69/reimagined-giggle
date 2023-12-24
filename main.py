a431b822721fe1cb29a43bc552d0a2f74777318d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:47 +0000

    standard equal might future after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
