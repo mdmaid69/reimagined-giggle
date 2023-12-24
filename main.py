a235eb4681e53221b07c5bd16293121d383b2e5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:22 +0000

    path shadow thumb lose translate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
