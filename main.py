f826325e5828064ead617580dd3b48b7a83010ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:32 +0000

    congratulations achievement freeze soon intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
