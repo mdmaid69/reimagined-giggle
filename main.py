679fdb678112966d76df899ed7367c8a6a2207ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:37 +0000

    geton pretend wherever job us

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
