ba1ed87eba970e25c53ab2044bdeb4a1939ae34a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:12 +0000

    your introduction contrast set where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
