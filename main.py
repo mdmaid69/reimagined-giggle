58d7b91f74718408c1fa7c5855402c5db0a066ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:58 +0000

    cross regret long principle lesson

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
