bccb850905255057965c395181f651700f4c2a59
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:07 +0000

    sore beyond no pair nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
