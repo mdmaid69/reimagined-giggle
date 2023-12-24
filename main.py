8ef5d40c02ed34e8248b8e2e4731fc6801cf808b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:55 +0000

    getout score thinkthrough alone boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
