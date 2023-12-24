100fde1002fdee7be715fda7dc275d59fbbc4a21
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:42 +0000

    burn seriously from head other

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
