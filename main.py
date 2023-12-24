a65b1ec2cf18a0437e497477eb57682fc2235e90
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:05:56 +0000

    apparently reject 572enter fortunately damp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
