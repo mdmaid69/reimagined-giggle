ec1018d24304de4a984dbcf99f0dd19064dd8bfc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:37:22 +0000

    float roof slip fear slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
