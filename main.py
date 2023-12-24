b26f1805a74f23a1dfd7d509ff6a15ae66b0d708
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:38 +0000

    agreement safety fence possibility something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_item(array, i):
-        return array[i]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
