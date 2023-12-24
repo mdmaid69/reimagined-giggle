7b3b32b85c1e5d4681527b01bab933c59c37f9b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:17 +0000

    introduce firm book middle interest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def reverse_string(s):
+        return s[::-1]
