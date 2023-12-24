b174a20e0eaf2d4d0a0c27c74e24d664e457a0e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:01 +0000

    more young phone dead suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
