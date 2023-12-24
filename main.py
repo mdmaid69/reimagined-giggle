ed295e54f3fef87300d807f93e9a03343a51a595
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:03 +0000

    change cost doubt proper production

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
