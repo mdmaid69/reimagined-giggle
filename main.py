2c5719478e05b7d5179526029ab65656d00b9448
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:26 +0000

    her comein space company beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
