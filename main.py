e3af8cc09d401b191859ec7354fc404c87fff2df
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:51:17 +0000

    inside congratulate mess call discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
