7276757342f569e4873a6d5a7134916483cf02ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:10 +0000

    shame course whichever undo old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def square_number(x):
-        return x**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
