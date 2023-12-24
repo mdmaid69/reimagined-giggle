2c873b24b3552d97f7bebd1ca8c80235ed182148
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:57 +0000

    with through life noisy experiment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_volume(length, width, height):
         return length * width * height
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
