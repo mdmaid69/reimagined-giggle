2fa3a1149c20eb752c81871317c529202581b9ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:10:39 +0000

    shelter board fold picture computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
