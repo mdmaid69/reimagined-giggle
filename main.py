2e108b22a92a328bfda9b4b6a12e9eacd8c31c56
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:19 +0000

    by limit Ii weak name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
