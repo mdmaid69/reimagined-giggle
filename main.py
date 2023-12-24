ff7a8402b2a9fa1422f4e1424f2731f7a86e0381
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:47:13 +0000

    unusual tooth prepare meeting please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
