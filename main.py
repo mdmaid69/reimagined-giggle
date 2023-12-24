8a016f98f974768228d017daaff1399f4bfeea57
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:42 +0000

    sink border slip underneath pleased

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+print(time.time())
