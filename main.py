a9a47760f801b0465377c610c557f982e5a40ad9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:36 +0000

    fit excellent lunch cost comefrom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
