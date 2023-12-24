097db461b7269eadd30832a6a4a0ac2ea6c59f2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:46 +0000

    advanced ability past left accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def find_unique_words(sentence):
-        return set(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
