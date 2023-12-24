3a4f454fb170d0fd81e0992ac8623d8dcb9c92a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:58 +0000

    continue charge his grateful usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
