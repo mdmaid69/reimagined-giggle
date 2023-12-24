08c926b0dad4c17841dd52abc7ac5e7c7ad7470d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:58 +0000

    office opponent unable blind empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
