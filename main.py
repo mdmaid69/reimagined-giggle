46005bba9a4cb61650bbbc9744f3e0349b246f52
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:01:29 +0000

    sweep wrap repeat forget body

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
