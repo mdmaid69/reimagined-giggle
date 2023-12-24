02d1b05f19923d91ccc0d02d8d24ac8739fe64ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:43 +0000

    report include active feel instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
