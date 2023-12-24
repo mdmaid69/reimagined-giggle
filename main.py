3584542db37f04a566bc47c9c0ca4d310771a2a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:09 +0000

    famous pure yes study shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
