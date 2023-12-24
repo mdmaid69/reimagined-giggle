9cff98b9294c7e772eebc71f5ac005d3b895701e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:42 +0000

    instant in not makeinto wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
