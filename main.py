c9b6205e819dc05d962ac2f7de7f9bf1b20bf6e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:47:28 +0000

    control limit safe drawing hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
