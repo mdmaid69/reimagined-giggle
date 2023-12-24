1610f09927ef7318f7d18d974283c3129a27d730
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:08 +0000

    sale her this case confused

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
