7c8af5b512977ba511d2c170ddba01e641c77436
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:38 +0000

    suddenly pleased race spot pour

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
