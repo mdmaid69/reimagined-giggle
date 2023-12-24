8a56dc000c06d5656ea70a2bb0a96f80920ea607
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:25 +0000

    tell mad range that moment

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def square_number(x):
+        return x**2
+def find_min(numbers):
+        return min(numbers)
