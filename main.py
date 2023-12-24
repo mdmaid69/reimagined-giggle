169d6b75257ff7d3e498c41e33076a9abe28da5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:07 +0000

    introduce sharp journey opponent example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
