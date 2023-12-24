60b2547da528c0549c978a4e826e173a2b25a3dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:30 +0000

    our kill window sign funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
