7496a957686924a426a75d8ce7e55c6bdcd551ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:37:49 +0000

    cool scream keepaway awake quit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
