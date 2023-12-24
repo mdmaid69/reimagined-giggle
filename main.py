d8157b7230489ca3ceec7022c137845190f785bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:32:24 +0000

    male false fat off make

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_density(mass, volume):
+        return mass / volume
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
