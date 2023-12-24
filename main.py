14debf9c2dde48777f6e411add5a65725e8fab7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:02 +0000

    breakin goup little myself left

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_speed(distance, time):
-        return distance / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
