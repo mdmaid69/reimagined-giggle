070b1ea13725b981f7bf9e64ae59907a33203085
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:24 +0000

    anywayanyhow own deserve breakinto object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
