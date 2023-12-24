04a816f0f754d7be382b61b55de14f901cb895bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:36 +0000

    somehow themselves fill clothes class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def subtract_numbers(x, y):
-        return x - y
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
