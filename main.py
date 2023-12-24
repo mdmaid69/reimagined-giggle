e35e38ddfeb3261251778a94856930f4349aed7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:54 +0000

    relation father see opinion responsibility

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
