62dac7669de6014ea3c564bd7d72d5eda9b69b56
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:24 +0000

    test too challenge furniture round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
