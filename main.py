0bf6949b52513fcaf73953b705c7ccad0980aa1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:24:51 +0000

    change joke hot challenge carry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
