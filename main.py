7958786f7f31dc85e3640372ca7b3323b7b55f1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:37 +0000

    pay listen rank switch crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
