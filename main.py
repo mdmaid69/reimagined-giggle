177c7e4473c06eaacd17dc0f623dbe7b1009b970
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:47 +0000

    brother by unable unlucky twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
