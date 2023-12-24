855262c804bd897b16cde5e3b533deae9e3f2e85
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:24 +0000

    science shadow opportunity except apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
