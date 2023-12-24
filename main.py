db7dd8dc4917d78a6dd3ad2681997b77bda0358b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:49 +0000

    Ff down dry tragedy charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
