2c4b5add96e0da39762c97c5283da577c38a0bea
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:25:48 +0000

    impression right lookup wide narrow

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def is_odd(n):
+        return n % 2 != 0
+print(sum(range(10)))
