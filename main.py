3a9da43f1dd6f248a7b99ef10455b89c67036bdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:25 +0000

    purpose move lead constantly gentle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
