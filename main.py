9f82dc18f139573e3eb913d28744a1b1c0063442
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:41 +0000

    measure female wind ours custom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
