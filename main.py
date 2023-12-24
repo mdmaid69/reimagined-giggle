271885121548674e95938039ed9978f27382f29f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:39:39 +0000

    anywhere travel will cut trust

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_max(lst):
+        return max(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
