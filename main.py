faccf6c17f0560b50142e26e9845aeda0734062c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:21 +0000

    today imagination winter general chat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-print(os.getcwd())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def remove_duplicates(lst):
+        return list(set(lst))
