415b59f87e8c429db574f91969075361e3162b6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:15 +0000

    expensive pride rate 573entertainment so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
