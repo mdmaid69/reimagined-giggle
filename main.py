6b4865e218b7676fd00880d880c644308554f649
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:28 +0000

    drawing plenty condition punishment custom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
