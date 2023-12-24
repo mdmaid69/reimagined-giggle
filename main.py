5856f8b0422bc0971d85f8a609332c635ed21d21
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:25 +0000

    show arrange suggestion holdout unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
