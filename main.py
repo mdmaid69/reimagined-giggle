f68e3222aabe1fd1fa884209bc9e158c86d02e62
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:03 +0000

    tense next little lot realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
