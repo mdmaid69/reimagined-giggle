15a868b1260bf7010ba532999f3fee30904a3f2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:18 +0000

    university become dark foreign instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
