49866117b1a542d8c6655402a0333bae8a7c6248
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:15 +0000

    break exact pronounce switch know

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
