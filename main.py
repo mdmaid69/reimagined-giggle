aeae5daf6840873e1a30355c86a49f18ae4edcf2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:55:40 +0000

    left balance message stranger degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
