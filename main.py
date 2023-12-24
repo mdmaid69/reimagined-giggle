b37bca2672617d01525fb9356ac5e49cbfeee50f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:06 +0000

    train difference hot surprise meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
