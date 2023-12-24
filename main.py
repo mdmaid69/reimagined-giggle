537eb50c6cdf6ca0f557b84a3c0de95d2f8adbed
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:41 +0000

    part without study different substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
