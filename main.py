66b8993a795f01a2fa0f1d9909415c85ebe75267
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:31 +0000

    opportunity callout gas contribute close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
