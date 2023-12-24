28269a6be2df0f66615bc8b8bdd7ced2516101f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:07 +0000

    poor trip estimate yes plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
