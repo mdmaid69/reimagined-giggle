f19bbe41af2b25fea8213e1b5463965a264a5611
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:37 +0000

    answer imagine past water use

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
