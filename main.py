0346075f21e429a6aee318cd02a9078eb6827ae6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:31 +0000

    contain naturally pleasure scale neat

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("Hello, world!")
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
