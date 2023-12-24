cf755225935de2e3c420d3020b93c59c686486bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:58 +0000

    high powerful everybody other price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
