7ec59cf5f43f622dc63bfbbe0e6df585aefff070
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:07 +0000

    charge teach too target marry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def cube_number(x):
-        return x**3
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pressure(force, area):
+        return force / area
