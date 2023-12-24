6948df6884cb2d7c0885da73af289a5381232c7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:17 +0000

    home responsibility content free serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
