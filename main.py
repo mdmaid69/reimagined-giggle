128d843b17db63308d3f0346163d291911eff51d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:56 +0000

    funny cheek breakout determined clever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
