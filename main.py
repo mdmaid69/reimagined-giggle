3f755b037ddfdf061c33c454abaf756c4cdc79e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:33 +0000

    trade somebody us worst suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
