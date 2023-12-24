236b7a3faa5601004d4fcf5656940d46c860ddcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:25 +0000

    substance busy again feature responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
 import math
 def calculate_hyperbolic_sine(x):
         return math.sinh(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
