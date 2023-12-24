c80279a93f127a4a2a7a3b6a48b7fbe80733f6f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:28 +0000

    heart emphasis innocent equipment evening

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
