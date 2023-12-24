fa81df241824d1a9cc012fe1ff53a03ae1364f17
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:57 +0000

    total combination proof now problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
