5aeaa4884308c3cc8d86aabc1791f701a9a568ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:07 +0000

    strength experience properly congratulate path

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
