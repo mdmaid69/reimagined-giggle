634f7880507a23ccd6c529fbae6946fb89de8689
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:05 +0000

    lookup sight hope job weak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
