655fa41f0b1a3ebfca7041084819f8e7a1f31552
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:18 +0000

    test rush within row crisis

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import platform
-def get_python_version():
-        return platform.python_version()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
