3e10054386672e64cf1a01d32556e64cf74b92d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:23 +0000

    blame education no shock by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
