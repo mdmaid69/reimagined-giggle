57cac2621167eeb3317704f61925d44ea00f937d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:09 +0000

    pronounce snow journey hope programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
