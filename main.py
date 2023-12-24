b3ccf5960d337a19593f7aec4b7a696f428f139f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:44:19 +0000

    lunch cruelty rubbish tongue take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
