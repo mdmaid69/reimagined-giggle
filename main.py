5b80587c7190969ef9a5653634d5b3150a416137
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:23 +0000

    main record cause string nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
