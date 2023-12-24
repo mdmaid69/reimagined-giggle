d3c21963dec68a02939f289aaa54bdd64f065138
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:46 +0000

    deal news dance tidy continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
