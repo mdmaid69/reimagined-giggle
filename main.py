9e252e55dc63cdfbb814e17e43fe8509b77c314d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:20 +0000

    study immediate hot recent previous

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
