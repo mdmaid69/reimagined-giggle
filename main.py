53bcb17f4b5821ffb30c774fa42ff5b6dc98dafd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:50 +0000

    respect regular inform classroom thick

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
