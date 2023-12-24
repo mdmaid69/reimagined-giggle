891710efa1e1efaa20184f7217700388e67da67b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:59 +0000

    after rub actually progress comfort

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import time
-print(time.time())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_work(force, distance):
+        return force * distance
