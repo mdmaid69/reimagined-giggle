e9d4de1bda8183426191c9660a79badab3f8f2ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:04 +0000

    approval during heat experience scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
