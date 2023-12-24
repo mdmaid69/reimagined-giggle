94adb4fe3b82085b18f15c0fd54b1ae52221a316
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:48 +0000

    number behaviour else run invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
