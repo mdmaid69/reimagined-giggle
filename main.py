bb4c8bc13678743859152a4c9923c900270d029d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:50 +0000

    delicate write lonely reduction lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
