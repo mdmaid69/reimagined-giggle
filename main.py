d2e33936fb2f67baa261208d4ec7fcfc6741de52
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:05 +0000

    contrast into stare name project

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def change_working_directory(path):
+        os.chdir(path)
