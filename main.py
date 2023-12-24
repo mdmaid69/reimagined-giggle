2bfe9da073500958976a53a6289bb7d8c76e7ebd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:16:41 +0000

    hard study telephone connection difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def count_words(sentence):
+        return len(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
