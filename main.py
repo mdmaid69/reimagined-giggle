191648b56962c403790b6e4a2f0df3214eda395e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:56 +0000

    sea none store kind distance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
