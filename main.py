b2b5a1f727bf746c62bb1cb009790c84857078d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:24:15 +0000

    entrance seriously draw prevent blue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
