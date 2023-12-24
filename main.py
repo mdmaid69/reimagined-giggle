6754279639c785e0dba91755c6204004eb3afac1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:57 +0000

    interest control of clean delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
