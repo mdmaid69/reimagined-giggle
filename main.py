46e2ca1ed6c0646fe405652cffa7ccf38c0b3e5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:59 +0000

    disgusting cold game centimetre smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
