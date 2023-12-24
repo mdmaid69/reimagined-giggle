056e05184eb320f6f1046a634cf98ceea93a2c18
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:15 +0000

    pump fast sort flow mail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def is_even(n):
+        return n % 2 == 0
+def find_min(lst):
+        return min(lst)
