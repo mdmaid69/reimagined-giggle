48e460c7f25b12704396ce6cbf8db8cbcc1a01ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:20 +0000

    impatient share disappointed previous nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
