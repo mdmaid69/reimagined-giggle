3d174f40076653369e31a24966a0dd28c3a3794c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:37 +0000

    admit behind come shoot their

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_sine(x):
+        return math.sin(x)
