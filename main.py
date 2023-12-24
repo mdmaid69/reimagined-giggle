3135f55ee9addbee16c01dc84068946e76fbcdc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:36 +0000

    set whether ring last out

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_list(array):
-        return list(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
