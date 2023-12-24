2d5d39ed598fb6c396f8d06d609de3e7d80129ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:12 +0000

    read exchange tell sore follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
