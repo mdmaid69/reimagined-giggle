51698f738bce086393606070a380e25b1471cd8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:14 +0000

    payment involve workout advanced reserve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
