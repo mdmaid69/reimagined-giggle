f9326a98ddd3eecfec1bd4a7d629424262fe01e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:38 +0000

    rid real blame or train

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
