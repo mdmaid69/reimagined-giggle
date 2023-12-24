2efd56261418f13372a888cc8358309a9b2870df
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:06:40 +0000

    fill formal there tie arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
