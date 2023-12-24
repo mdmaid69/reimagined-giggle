bb12eb23eae55c6149e0bc60f4f534839ddf289b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:49 +0000

    fix uncertain deep trust forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
