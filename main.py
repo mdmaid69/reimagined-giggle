94c70c3b886aa925d15bd4312ee3a088c68045f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:09 +0000

    ordinary rush plain instead insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
