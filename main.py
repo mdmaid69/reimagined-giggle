cbdcb54faab5ea13fdde93567f244d9fa004e0f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:59 +0000

    until surprise coffee willing shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
