bd1bf1bccdfda1ec8c357e5129bda7f32679197b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:58:30 +0000

    float surface identify far present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def delete_file(file_name):
+        os.remove(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
