8cf22967d86306dedfb35bff2b30e1a9aa4b0c96
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:57:20 +0000

    touch pretty except tone mixture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
