33596bc9ad378ceb6ed943b2f448d125bd67a6e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:47:41 +0000

    along service consider mix train

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import numpy as np
+print(np.array([1, 2, 3]))
