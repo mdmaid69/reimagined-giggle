b9f06ea255782c286dd7c80e63e74b9b2067d9dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:18:38 +0000

    belief mood until good blow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
+def convert_to_hex(n):
+        return hex(n)
