3e4b6ed6e1cdee0417f028e2e0dddff839929439
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:22 +0000

    talkback mean long deliver niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
