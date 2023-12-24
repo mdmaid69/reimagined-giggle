f37a107265860f7a3e4490ab087ffb3a41f67551
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:04:03 +0000

    communication giveback holiday this takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
