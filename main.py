8ad7ce339e3d17129d163a617b1f2136dfeac572
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:51:53 +0000

    various forward question opposite turnaway

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
