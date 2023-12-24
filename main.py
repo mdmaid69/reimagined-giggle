dea49f6b09cd83f61aac44c9fdc41033cbd6a7bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:24 +0000

    broad he tired whoever justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
