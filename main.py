e1c98e35800196071aa2626f52805bb13ca9c98d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:54 +0000

    consist high exist often seriously

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
