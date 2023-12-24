fc987a882db982e7f4e1981839251e5bee7ee349
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:48:05 +0000

    convince represent blow pleased score

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
