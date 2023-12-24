8dae962e3a1cc914a58524cdf672f5bae2c331b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:13 +0000

    soon raise address except report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
