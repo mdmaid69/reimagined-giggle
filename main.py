4026400a66e2a6080341e1a46e0cff0c2a2a40b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:38:57 +0000

    turninto case mainly lie return

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_pressure(force, area):
+        return force / area
