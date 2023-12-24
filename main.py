0c847958d8e23ad4a6af1af27fa81300847ac723
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:31 +0000

    ready sail mine badly ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
