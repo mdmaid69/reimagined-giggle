9e2c69026ebb433c9519a198b65fd24bfe7aca21
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:13:27 +0000

    deep wherever accommodation company duty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
