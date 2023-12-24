b512c99f5c033a0d7a5e672e640b10ed1a685686
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:43:20 +0000

    level collect onto taste soft

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
