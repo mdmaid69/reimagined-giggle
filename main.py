017a412aaa1b9ff44c21da93fb0a08f0a09d66de
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:00:08 +0000

    fine adjective sale shelter during

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
+def square_number(x):
+        return x**2
