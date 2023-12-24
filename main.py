f3cd846c9658d0ad7023dbfeae670c280c35d34d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:49 +0000

    closed hardly influence insist brain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
