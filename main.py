fa06fee813b7fcdcd6507f0bd7a9e9f2067b6800
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:16 +0000

    fortunate better comeback generally cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
