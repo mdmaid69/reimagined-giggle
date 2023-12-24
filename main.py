023a822648cb2902c9afb4f3e625e6ea8a5dd866
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:36 +0000

    mark peace black up tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
