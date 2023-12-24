e13b9fe6b056465da00b71b065e8386ca4d2654c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:25:49 +0000

    then modern thankyou key mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
