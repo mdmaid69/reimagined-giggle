26f811b94186d731f3af51c0241ccbe7b0e923e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:32 +0000

    spin official dull spot ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
