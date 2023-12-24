7b46483e246e0d08d269d4fab5b4117e314fd515
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:38 +0000

    cheap refuse badly check occasional

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
