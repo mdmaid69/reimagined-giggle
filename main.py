fc7003c562863ee8e0767fd0c330c09f34d6f2ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:02 +0000

    idea contribute bush urgent read

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
