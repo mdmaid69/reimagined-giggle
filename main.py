984d918d663ca368b17216d2510a39717d442f04
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:00 +0000

    perfect anxious wheel other quickly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
