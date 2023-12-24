5c5ebf855989c95f6d98418d0b680d5cab518611
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:14:54 +0000

    shrink slowly organization climb direction

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_work(force, distance):
-        return force * distance
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+for i in range(5):
+        print(i)
