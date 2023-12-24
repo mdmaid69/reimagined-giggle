daad0ad134deb7364d089042d7acef6ca574e2f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:20 +0000

    final lucky produce wear achieve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def log_message(message):
-        logging.info(message)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
