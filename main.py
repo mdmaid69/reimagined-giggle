73d37322861b9fffb45c62d3d6d80ae4944e4938
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:53:04 +0000

    minor male plant branch raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
