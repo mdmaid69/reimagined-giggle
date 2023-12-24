bb476935f9718b3fc447126404620c00b2fd3b18
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:11:31 +0000

    wander human calm late duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
