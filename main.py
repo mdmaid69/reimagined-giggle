64e1f337f1afcacff0f7f0a8490f99a6ef111ea6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:00:55 +0000

    extraordinary busy receive constantly something

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def convert_to_hex(n):
-        return hex(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
