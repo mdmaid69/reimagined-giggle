a1c5ad20f907ca62ea8bdbf0d8eb9de281e41b1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:14 +0000

    name represent cut practise flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
