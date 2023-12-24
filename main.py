2dce396c6b8b51d01065ec19a6ea8553af98df23
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:29 +0000

    music cheek ask future exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
