72a99a570c03bddef9c90ae5d69a44c30e89c5bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:06 +0000

    taste hot flow stand steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
