198091d04526f2a7d24c59405f5aedceb3329e7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:11 +0000

    makeinto strange no hot bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
