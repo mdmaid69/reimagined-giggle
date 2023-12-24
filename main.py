b502801ae3e861e0f5767c4fbf049d37a0ee6cfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:01 +0000

    each away steal under anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
