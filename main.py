c3e93f6597bd9575732d34975c44af1aaf93bac9
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:45 +0000

    neck instruction yet length flow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
