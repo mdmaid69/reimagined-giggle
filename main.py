9d4acefdf90197809ea5c5121ae8e3b763de973f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:08 +0000

    education 564encourage action workout supply

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_power(work, time):
+        return work / time
