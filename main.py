4faaaea3712ca95873e67c0fcbfac314b65f34e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:37:49 +0000

    wise store keepfrom message spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
