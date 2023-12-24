96d8674be1c0601d76a781aa195be7307170926a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:59 +0000

    step determined history remember runout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  def square_number(x):
+        return x**2
