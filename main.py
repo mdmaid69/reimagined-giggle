a41ffe13f8903a2ee5484c6ec1a8a8a389dd3e75
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:19 +0000

    feature marry brain bit finally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
