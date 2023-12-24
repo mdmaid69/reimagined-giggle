bd485a5b0e9dd1e9aa92b3b49e64976bddcdc708
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:01 +0000

    bill lock lead name introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
