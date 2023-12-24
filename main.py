4a447ebf3136d69fabaf379ac8ce21eddad35ab7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:31 +0000

    constantly lesson basis instruction also

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
