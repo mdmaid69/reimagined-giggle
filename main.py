3d7c9933e0a860c813e363c0dfc97593b71dc083
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:35 +0000

    enthusiastic suit far set turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
