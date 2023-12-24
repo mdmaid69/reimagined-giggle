b8331eb894440b36e5196bc0c5bf049188de6709
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:15 +0000

    force Oo important total me

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_odd(n):
-        return n % 2 != 0
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
