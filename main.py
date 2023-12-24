c1c9af32fc15df5e6670104e713b2b5a1ca1994a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:03 +0000

    town please shop smell event

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+def print_python_version():
+        print(sys.version)
+for i in range(10): print(i)
