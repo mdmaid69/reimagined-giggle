4dca9ae22621e5abb7ca90c925023f71ed36e14c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:52 +0000

    earth movement below term press

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
