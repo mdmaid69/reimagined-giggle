918dacfda6568a91bebbe27c9a9b2c71a7c4c7bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:56:34 +0000

    suffer 572enter false thankyou coat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 def calculate_force(mass, acceleration):
         return mass * acceleration
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
