a9da114b3d147d9f0bff91544452e98bc50a431e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:56 +0000

    delicate independent activity purpose manage

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
