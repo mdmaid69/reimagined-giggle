c1ce0b7d943b2dc0e7e70a3325e8ca6036341501
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:11 +0000

    basic apologizeapologise watch beside with

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import collections
+def create_user_list():
+        return collections.UserList()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
