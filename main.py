7a7bd0b62c83f30944c205695fdbfb24cf4f7662
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:54 +0000

    track before unhappy criticize bit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
