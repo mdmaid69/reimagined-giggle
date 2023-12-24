7e8706fdd8c1b18219c07d84ecf96c5f7cd3234b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:16 +0000

    basic plate set sort praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
