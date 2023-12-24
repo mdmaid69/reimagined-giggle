9a3e956d276dd38436bfac992f890e741a3c6608
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:47 +0000

    sink home nor niggaacademy include

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
