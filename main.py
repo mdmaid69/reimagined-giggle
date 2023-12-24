b32e882a7ea942d07b1f371dd3b62edadd8d6574
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:58 +0000

    care sign agreement spite conversation

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+for i in range(5):
+        print(i)
