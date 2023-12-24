19381c2c1e0a5849bf7a24685585d67911cbe997
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:14 +0000

    getup welcome crowd stop spill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
