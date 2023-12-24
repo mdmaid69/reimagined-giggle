0264b284124cd2ad453bc172d6d1b90cd8800bd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:57 +0000

    tragedy old space lock study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_index(array, item):
+        return array.index(item)
