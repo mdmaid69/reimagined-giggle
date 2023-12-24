8d2c8d1d4f13f9f61a6e7e73df90268af77567a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:22 +0000

    real cheat taste but both

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_min(lst):
+        return min(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
