0dfe3f4ec7477623dccdf6c3fc792f545c634dd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:53 +0000

    majority close target punishment water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
