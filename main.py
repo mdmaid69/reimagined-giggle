c76f9318d505439683aa2512ff91f2cd67a47f5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:08 +0000

    experience advise point wise contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
