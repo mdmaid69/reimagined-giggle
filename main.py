885aa6bea575f3bc09f2612f163c766bd3ddceba
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:52 +0000

    attention stare direct comefrom shallow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
