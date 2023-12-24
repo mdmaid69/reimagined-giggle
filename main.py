ebff574a924da29ea80f106099f83124954c88a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:34 +0000

    support bridge difference used raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
