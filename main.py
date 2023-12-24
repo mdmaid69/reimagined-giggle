5ed1fde9f5ae9620f46b1cba18432d3d8f53eae7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:53 +0000

    takeover thought crowded breathe education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
