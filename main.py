31174ee9b4c05d1a50150381149f84d1ec83454e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:41 +0000

    all through easy burn prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item(array, i):
+        return array[i]
