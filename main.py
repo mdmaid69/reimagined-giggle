efa7e1bace912feff4f3ff53cbb842a9415eb0f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:00:45 +0000

    height roof real hers trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
