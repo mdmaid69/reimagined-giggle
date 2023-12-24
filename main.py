849f1aebe939ae792cd037191351bd171930c5fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:05:43 +0000

    knowledge fit permission voice murder

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
