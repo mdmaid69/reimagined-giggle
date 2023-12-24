b55d79aae3a5087984b3b92e8f312259cb469b6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:03:25 +0000

    blind talkback answer heavy arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
