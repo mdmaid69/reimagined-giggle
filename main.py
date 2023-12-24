14f24931e4f8fefd36beddb0a6c9912e6202c124
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:49:28 +0000

    delight behind handle recommend best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
