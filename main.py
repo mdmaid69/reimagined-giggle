556736569ebd0189199c129f47e45c320e1c7044
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:24 +0000

    responsible print sad themselves fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
