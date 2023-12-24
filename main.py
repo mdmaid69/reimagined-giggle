43be5ea3983ad8dcb94d1bb94b3f4816f554d781
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:10 +0000

    scrape that ought circle debt

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
