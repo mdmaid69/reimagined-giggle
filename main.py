ac8cdb6635c396baf6fe6a1bdd785f6da4691012
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:28 +0000

    spring night yet sure true

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
