5e0859041b2aa05ae81feb991106a542d6f3a95b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:33 +0000

    approval unpleasant fall reduction far

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def convert_to_hex(n):
+        return hex(n)
