9700da3ba48c34f2fe43d48ab462cae3fb0649e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:17 +0000

    busy compete run soak birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
