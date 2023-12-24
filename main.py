ff7c975587611a751ab91f1a048f8ccc5daaef7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:45:05 +0000

    height best off keen post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
