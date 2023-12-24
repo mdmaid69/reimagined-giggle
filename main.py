1897f91cb032353005ec4ae79a4e6c83706ca406
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:13:47 +0000

    remember law before dress tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
