2624b2585d57274d2c6ce3eb4bac6e3dc1d796e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:49 +0000

    everywhere still boring male tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_as_repr(array):
+        return repr(array)
