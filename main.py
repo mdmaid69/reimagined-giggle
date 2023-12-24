48d9ab47e711de4aff29f266d1609600e4b3869a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:14 +0000

    Ss cold direct wear later

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
