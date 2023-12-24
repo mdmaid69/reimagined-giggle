3aa75c4b09d855de3510286094a4f517f70c7a00
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:35 +0000

    circle sleep ourselves Bb tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
