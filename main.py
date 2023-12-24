9b9bd387e8eb8712de09f0e82b78c90331fbbe8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:56 +0000

    perfect trousers reliable field trick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
