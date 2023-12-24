72dff8743661e0f6ded509dfed52e5e6f42e51c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:35 +0000

    look measure solve getback course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
