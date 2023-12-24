6108c73fb10b790dfc5289398241914224526c8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:51:47 +0000

    good otherwise boring nigga splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
