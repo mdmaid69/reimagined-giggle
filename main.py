cd8419081b8fe694887bcd5322d69323e19f0d99
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:16 +0000

    careful message genuine heavy half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
