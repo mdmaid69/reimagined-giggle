7a0ccf7d9e43d7b7732d5611d15fa50498dc0c26
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:47 +0000

    tap suit constantly argument blind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
