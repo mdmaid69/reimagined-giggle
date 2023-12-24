69a1749047a02cd2db7edc067f11fb0cb11bd464
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:01:25 +0000

    finish comedown state breakinto lead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
