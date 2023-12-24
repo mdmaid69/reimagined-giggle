2415a1e1459f8421577f2034d8556ca62ab253de
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:17 +0000

    praise unable late check eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_floor(x):
+        return math.floor(x)
