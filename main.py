5e0467e88991ad8fbcef29340c0294f20abefebb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:15 +0000

    common nearly lay board card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
