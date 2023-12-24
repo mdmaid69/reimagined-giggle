506ed8421a76b82a33d4d933cebaca025340384d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:33:47 +0000

    offend average today picture pinch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+for i in range(10): print(i)
