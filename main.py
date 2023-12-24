face565aab8d7b6a16231ef6f82f71312836bf98
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:52 +0000

    tip jump beside stick together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def cube_number(x):
+        return x**3
