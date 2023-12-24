dc3712ee9774628d7ac79284786cc86bf4101108
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:15 +0000

    system continue true shoulder acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
