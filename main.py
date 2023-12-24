8f42fb8592a784c13b296907de0bcd5786fadc7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:29 +0000

    accept regularly nature criticism emphasize

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
