b439320c86c410fb555a27e27dd11d83486f12aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:29 +0000

    drive second telephone Ll information

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
