732985afdce6fb87ece8fa627cede7b7bd7a4692
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:51 +0000

    scared because curious organization everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def convert_to_binary(n):
+        return bin(n)
