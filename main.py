e539bdf4043aab82151a25704ed2f99821b49eb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:00:29 +0000

    so weekend another building attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
