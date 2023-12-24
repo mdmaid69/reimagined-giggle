9b0f0622882c5384ddbb51cecde0e24c034b8faf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:20 +0000

    travel content hurry child yours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def sort_list(lst):
-        return sorted(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
