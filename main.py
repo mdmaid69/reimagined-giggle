473efbe00f26c0dbb4e5b93c96bd1cb2f34931de
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:49:39 +0000

    basically next seriously bend watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
