8bac8f09f1b0d49daa5f05eb575f5d1a53254de1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:46 +0000

    book fence service blind burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
