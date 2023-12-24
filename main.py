d44a1b5b027b89db7511b66dfff2d234d5b2c6de
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:44 +0000

    confidence substance normal well block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
