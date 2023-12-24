451cd0c4624d02b9d245f172a4ebd8ee4dc543c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:30 +0000

    everywhere breathe argue talk aware

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
