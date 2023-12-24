1f23d773be26c54ffac5f604eadf0db567805d47
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:54 +0000

    downstairs spare comment welcome opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
