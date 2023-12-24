d1316526e1b9ef635fdef7dd0ce87fa8b6a662aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:04 +0000

    experiment keepup exploration blame breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
