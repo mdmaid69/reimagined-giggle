3a5c903c4b9ac2f306c99f57340b7a2e91f7b84b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:17:16 +0000

    murder grand water club table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
