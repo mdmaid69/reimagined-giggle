58021619efa4aaa20222b891e81198fd699a0517
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:21 +0000

    shrink advice listenfor clue suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_item(array, i):
-        return array[i]
+x = 10
+y = 20
+print("Sum:", x + y)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
