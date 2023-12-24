52c78d25c497eeac774731d42164482e756d01f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:11 +0000

    lay lift obvious whistle getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
