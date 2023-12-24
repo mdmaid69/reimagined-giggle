8af6f7638d967820a3e7f09c91a0596bb26a78f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:26 +0000

    lie successful admit model information

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
