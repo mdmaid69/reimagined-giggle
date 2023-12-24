eefcafffa530e91703e3d0918fa77d53893c5f1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:19 +0000

    deaf small communicate full this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
