e2503b2c8bd2d0d2105cb4e5acee80dea33c8ba4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:31 +0000

    stick yourself broad unusual intelligent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
