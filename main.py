f9fa66658cfb0c712ad644516e3862fb0b7ff69d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:02 +0000

    splash frame press tight entrance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def create_stack():
+        return collections.deque()
