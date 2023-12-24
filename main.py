8cf20ef46ea89c01d5adee9b9806c4084102acb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:15 +0000

    side trap replace sort wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
