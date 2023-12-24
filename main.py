8291b6a7acce14866f42c4b2aa8979ad790da7cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:31:57 +0000

    influence crash mixture issue brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
