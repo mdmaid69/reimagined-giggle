b95e55b2c061446b8e9149d50b1bd9a0557f45fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:35:15 +0000

    sail appear blame pool no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
