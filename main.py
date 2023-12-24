ff893acaa02f4eb6b3f045fb7063c473e7ce7dc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:44 +0000

    fasten equal artificial plant out

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
