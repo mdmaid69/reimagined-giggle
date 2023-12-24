0d3344dc30267772451c1e57be291aefe1c80056
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:34 +0000

    mind gooff rain discovery top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
