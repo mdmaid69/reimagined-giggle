08691741b2c9f9f85915e7d23008a0796d190091
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:44 +0000

    anything capital cause result bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
