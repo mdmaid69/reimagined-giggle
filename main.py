8890a699a0c9408a446e92b3339218ae22b58084
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:45 +0000

    bet allow runaway answer inquire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
