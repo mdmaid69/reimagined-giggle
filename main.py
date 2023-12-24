d8359092288885aed214aaf01a7cdf9044c2b786
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:36 +0000

    equal catch prefer wise soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
