ca33259d354f5e9d6dd0aea05864906b2d77e6c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:29:38 +0000

    kind loss no sit snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
