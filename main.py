0375f5fd8a279b2fd8b412e355fad6c2a5681209
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:39 +0000

    finger protest suggest act interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
