ca44deb713d7299a618f1e57810b1c5fc49174d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:34 +0000

    refuse old bored die shape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
