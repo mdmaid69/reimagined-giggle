b44ff4ab7b01bbd20d8e5935c05347465e4fe126
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:44 +0000

    rather bringdown year method together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sys
+def print_python_version():
+        return sys.version
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
