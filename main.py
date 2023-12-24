198070d73d887a8d1ad6ca48236cb293580356f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:04:00 +0000

    front afraid out impressive flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
