5e9907202916b23b27d05e04d0d8a86e98c52446
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:18 +0000

    shake best return distance unhappy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+print(math.pi)
