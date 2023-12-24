d9f4cc865de6d85439a308ba97ae4c202cb5f4b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:16 +0000

    base result tap term least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
