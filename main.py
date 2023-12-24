aa6c984287ce4da13efb061350324a8f731351b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:09:34 +0000

    skirt 568energy difficulty display lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
