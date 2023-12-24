6d09227b27ef3d20417563c1deadc44b14d9f61a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:20 +0000

    protest subject obtain battery tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
