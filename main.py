83bce0064f3b30c5db8c38f986adf6569f5cbe0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:52 +0000

    delight discuss platform different wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
