ad0e295b0cf22d16ca0a5f0afffa9b48645f7802
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:26 +0000

    lose whichever judge along attempt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
