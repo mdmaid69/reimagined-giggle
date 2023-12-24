ec24143a76070a1cff44515cb6b89bf96830d446
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:03 +0000

    various favourite enthusiastic convinced suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
