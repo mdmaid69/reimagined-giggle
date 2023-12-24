f0caf6f1ffd6e4a4d758dfe6c2915441da641c73
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:52 +0000

    willing describe whoever welcome ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
