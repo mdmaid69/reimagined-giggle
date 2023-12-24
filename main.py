ebd8c34ea693c90f6167c2bb1f8e8fac356bfbc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:37 +0000

    callback past clue honest takeoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
