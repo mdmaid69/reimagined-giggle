3cf9cd3129f29dc18604371f95a15e735ecd69b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:29 +0000

    sleep praise getoff private goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
