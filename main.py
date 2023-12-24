22677c25c33da971618306d47fc7881bff531a17
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:46 +0000

    chin mad trick that blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
