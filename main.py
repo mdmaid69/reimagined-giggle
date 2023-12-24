6a1ff0f0658c8251338f6fef8b6156e7bd1261e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:42 +0000

    tonight once law pleased return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
