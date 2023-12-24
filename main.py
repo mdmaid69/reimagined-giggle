33943c026ec64cf928e65770277a8872aba30ca7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:51:49 +0000

    cut certainly hurry before suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
