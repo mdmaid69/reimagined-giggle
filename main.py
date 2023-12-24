fae4b4172d7ac5e254b70cb53892867c06f5b436
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:35:19 +0000

    indeed Ee Mm mass field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
