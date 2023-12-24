011e78534f9bfc7105a65818e429dc7a4d3996db
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:24 +0000

    concentrate ahead hide admire motion

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-print(sum(range(10)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
