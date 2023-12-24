514ab0a7a97008e2d69df7acc0dc1f38db22e4ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:24:36 +0000

    fine convenient makeup land 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
