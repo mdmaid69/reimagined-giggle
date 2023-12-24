9446c0c5ab0a684c7e12130b62ee345b39891c32
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:05 +0000

    burst awake unlucky equipment turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
