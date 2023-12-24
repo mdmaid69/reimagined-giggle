0733cc37e9d540d5f16d638a3e9fe09ec48e6b9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:48:06 +0000

    brief matter team shopping pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
