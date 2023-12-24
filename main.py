d35fcbc03f113477de2bee2d98f883df1c106066
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:42 +0000

    male avoid minute manner discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def multiply_numbers(x, y):
+        return x * y
