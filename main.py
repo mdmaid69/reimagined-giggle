dca453b5ef1bdae84542705a002bc54a2114ead2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:24 +0000

    comeoff generous criticize over rid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
