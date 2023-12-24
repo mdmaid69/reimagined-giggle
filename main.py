b69ed0af8e9fb638b0a5d04bbaa1e30d45c5f4c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:50 +0000

    contents teach promise safety understand

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
