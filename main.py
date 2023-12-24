b29b1d47825325e2ac453676a26fe9d1e9614a20
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:08:46 +0000

    sit shout address opinion hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
