378c7449e27aebc8fad1e0e29d51c27a5bfd19bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:17 +0000

    underneath kind confidence gap thing

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_item(array, i):
-        return array[i]
+print(sum(range(10)))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
