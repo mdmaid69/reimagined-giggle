71bcdde00205ff1edeb1f5e586acf1a62f4e8fd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:50 +0000

    entrance play opportunity cause decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
