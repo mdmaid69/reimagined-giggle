783febc28e069781f9c1c592772d200aa93b259a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:52:34 +0000

    deep equal day calmly today

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
