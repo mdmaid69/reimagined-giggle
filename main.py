09c087e98de78b8ad00c6ea808c3acd8da53734d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:59 +0000

    reduce likely guess breathe appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
