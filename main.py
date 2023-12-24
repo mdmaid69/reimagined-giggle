548c126d4de763edffdacae7575b3fdabb9d50cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:23 +0000

    567enemy direct angry hurry write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
