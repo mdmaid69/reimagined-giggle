a86c35193db6a12ca6831e2bec3eb001ccb6fd11
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:36 +0000

    turnoff estimate depend evidence pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
