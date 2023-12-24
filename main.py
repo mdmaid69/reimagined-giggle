f085f4e6fd0bffe2e2bf3f8df486116d393ec5e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:18 +0000

    shade put immediately cheap dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
