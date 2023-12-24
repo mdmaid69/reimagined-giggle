7efba545b1c344ff09a500a8c724faac37c8d168
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:42 +0000

    suit sore slight public both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
