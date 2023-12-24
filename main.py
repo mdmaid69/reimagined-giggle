289c8ab4534c57d0cf34d196c21fc4f9a8b1dbcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:59 +0000

    answer air snow health pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
