8d250881d02d9327e656d9386a78427f289d7b4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:12 +0000

    sticky 569enjoy future trap cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import datetime
+def get_current_date():
+        return datetime.date.today()
