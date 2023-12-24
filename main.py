7c573b743d87536635773e339e48e20a75429bb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:16 +0000

    rub unfortunate strange geton replace

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
