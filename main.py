4b7d481fbc248e53061da9f1114090f0ed858a10
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:23 +0000

    listen reply whoever firm breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
