17ab14e96e3292dcb2219eaeeb9d4214f0dc363e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:46 +0000

    measure fix that waste Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
