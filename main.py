9e7e92729ee842cf27de2f6c1edab52e47126e3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:53 +0000

    exploration goout Ss one speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
