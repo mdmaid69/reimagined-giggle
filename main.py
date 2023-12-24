1584a0eb612d4f7aa7f544475c1a57cb3e509591
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:31 +0000

    guard pity slice past shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
