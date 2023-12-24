d3da27b456645d88bacb30fad455397c441af319
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:06 +0000

    present arm spill record say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
