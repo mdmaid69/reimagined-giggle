3b6e0140b77a5a925985672580d2926c6e347043
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:10 +0000

    provide loss comeover agree wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def subtract_numbers(x, y):
+        return x - y
