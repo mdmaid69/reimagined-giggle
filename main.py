6dc91fa9bcab29e8b5a625b7b66fed710bdc93b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:16 +0000

    inquire otherwise action problem secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def convert_to_binary(n):
+        return bin(n)
