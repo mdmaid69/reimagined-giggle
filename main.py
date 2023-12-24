9842ea3e66539d1094c4296cd676316b1e36979e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:13 +0000

    luck Ii considering ordinary characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
