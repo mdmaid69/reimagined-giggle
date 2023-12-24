1e409a22253f8b4ea2425b7664d4e08085cdc45d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:08 +0000

    service key somehow rare circle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
