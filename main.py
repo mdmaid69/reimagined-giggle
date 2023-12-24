6b64f48b7be441e84af1ef0e23b57d80ccc8c773
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:24 +0000

    need soon wind roof unkind

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
