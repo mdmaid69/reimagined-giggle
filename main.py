12f5fb84ab7beaf4eeef056dcb0025086d22fe46
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:23 +0000

    go issue matter way recently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
