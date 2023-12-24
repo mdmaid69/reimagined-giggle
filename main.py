f957ccf7c95a8de47aa92873e1cdddf5e851c546
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:49 +0000

    case goback(to) police Dd report

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def convert_to_hex(n):
+        return hex(n)
