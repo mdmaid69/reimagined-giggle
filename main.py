33a915866eae51c58565b8dc842f9a733b4d20cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:13:16 +0000

    deal someone behind prefer mine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
