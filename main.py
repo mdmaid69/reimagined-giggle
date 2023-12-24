3c7bd8d204e47dcbc896b47d99c71768dc9f7598
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:47 +0000

    Jj mystery add conclusion bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
