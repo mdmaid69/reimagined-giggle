c8bbc3ad01f502c826b44e27c49ddfa178a8f1ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:27 +0000

    meet shape wish forget scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
