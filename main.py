2f74869f3eee0b89af8a13b1c5e70e1913357371
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:56:37 +0000

    Zz plenty service smile goout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_duplicates(lst):
+        return list(set(lst))
+def reverse_list(lst):
+        return lst[::-1]
