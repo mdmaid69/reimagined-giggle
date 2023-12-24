a9efeb5e83f4608e060ffff201f21cfecd7e1315
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:08 +0000

    Ee plan attempt typical command

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_min(lst):
+        return min(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
