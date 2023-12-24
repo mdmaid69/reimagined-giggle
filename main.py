fad3ab38718490187c46b87f7d549c56ee39d34d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:27 +0000

    attention join connect shoulder trial

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_min(numbers):
+        return min(numbers)
