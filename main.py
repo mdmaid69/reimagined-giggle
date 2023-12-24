0cba16dcd79346e23a1d8537691e57ab11a5b094
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:53:42 +0000

    patient pupil mysterious scream recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
