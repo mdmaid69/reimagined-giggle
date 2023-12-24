7720ec8ba6a3dff656b782f3fd1abf6e2f277dfd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:23 +0000

    rather professional trick turnout never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
