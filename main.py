59f3949f74ca399f793c9f829c52bea48526b5c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:34 +0000

    age Ff unit profit summer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_int(array):
+        return int(array[0])
