d694b019a2211c3f1f5b34c8d3f280c8ff01d7d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:30 +0000

    expect best post condition spray

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
