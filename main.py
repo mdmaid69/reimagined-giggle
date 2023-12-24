08e1f91ab771bc8fa594993b10450e713c23055e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:24 +0000

    present such sell last cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
