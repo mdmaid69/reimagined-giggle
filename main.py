48e86b690431e35752bb4253fd7233a2fd10d213
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:33 +0000

    experience approve debt accordingto float

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Characters:", len(text))
