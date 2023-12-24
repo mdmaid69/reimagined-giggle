73231dbcc9b4434cf10f61906600518a611740f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:28 +0000

    level turnoff return contrast custom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
