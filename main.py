226163eef83e43aaef0da1891f18e7d5b2160f21
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:30:52 +0000

    impatient exercise conduct when software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
