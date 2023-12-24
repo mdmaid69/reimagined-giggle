db195c09e0cbd5fe7d8cc09ce3c3e908c91a5b94
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:00 +0000

    Cc disappointed improvement Kk explain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
