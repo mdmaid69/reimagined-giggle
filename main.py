adeca03084f969d08de4544a1cdc1b0706f6d8a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:01:00 +0000

    afternoon strict approval discussion wall

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
