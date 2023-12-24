bb3c4a994093cf091b5ac96eb3dcbf7b4e942cf6
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:19:40 +0000

    fat callback left thinkthrough protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
