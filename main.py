71cf6185ce7c5036a6b83c8a233af4cc73c6f496
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:17 +0000

    tend their pupil emotion catch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Characters:", len(text))
