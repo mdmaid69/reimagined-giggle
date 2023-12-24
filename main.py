c8d44a1208faf9c3e4deaef0065a06a87f39e973
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:38 +0000

    street trousers you probably mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_bytes_from_array(array):
+        return array.tobytes()
