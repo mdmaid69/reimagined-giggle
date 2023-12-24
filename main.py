a6d3ad763a81661704f5b68449203f1aceead9a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:43 +0000

    skirt on direction address deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
