f0bdac7a689ef51566bf4256ac34df59aad3c4b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:03 +0000

    more getaway ocean change light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
