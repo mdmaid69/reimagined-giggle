1300d46458968b2326a3932bc2376ff96c54d796
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:28 +0000

    number direction absolutely home with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
