b37da245c3f82e06ca3acea582078058c0d54f7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:00 +0000

    reduction increase meanwhile belong hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
