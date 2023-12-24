4df4b7495e38eb1fa7e46b6ecafc01de80018672
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:17:53 +0000

    soil glad spring warning window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
