873511784f6b9e408a3c631c373cf3dc2773881b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:07 +0000

    high offence future whether total

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
