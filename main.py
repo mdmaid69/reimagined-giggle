9488c0b74c95aa6e8a42c5c5f7a629e81f1ab51d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:38 +0000

    music quiet terrible plan black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+print(math.pi)
