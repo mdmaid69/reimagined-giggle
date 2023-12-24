e62ae629a81a193959ecc8347212ac97c9a98b6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:39:00 +0000

    run otherwise mistake home cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
