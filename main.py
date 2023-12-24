dae9e6d3f1404355b404ae887766be93bb4a3a15
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:14 +0000

    thinkover ring cheque rather turnup

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_set(array):
+        return set(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
