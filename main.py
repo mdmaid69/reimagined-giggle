c76eca0d4312962adde3e95dfa2bf91059333440
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:11 +0000

    smart practise practical 565end smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
