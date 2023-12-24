0591a129f3037784d41f1af27a07ed955e1d6d57
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:28 +0000

    circle sweet silent sore punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
