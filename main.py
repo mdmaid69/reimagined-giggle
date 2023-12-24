9605b3c2b9efe54238596ebac2e4aa737405390a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:36 +0000

    ball meal medium smell punish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
