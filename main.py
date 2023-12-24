5379b0c5a992ac800f68370abea1395dd9155350
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:45 +0000

    comedown some until agreement also

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
