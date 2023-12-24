494914f40e8318e86f4f25a62a9a32ea7d41a580
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:53 +0000

    callout whoever press duty actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
