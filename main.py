876bfaffec4c16decdb9921f1904c090bf7e8d17
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:01:07 +0000

    responsibility experience punishment port gentle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(lst):
-        return min(lst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
