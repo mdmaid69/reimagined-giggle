a057d1043fbd321b4e84c42b9d0e24ca81291539
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:02 +0000

    relation plant replace acceptable heart

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
