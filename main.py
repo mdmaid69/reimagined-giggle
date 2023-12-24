903d3566e3205843b059d83d47c723a659da6778
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:20 +0000

    wire dress sea empty ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-print(math.pi)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def add_numbers(x, y):
+        return x + y
