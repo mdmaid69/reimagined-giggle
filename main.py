03f60b1ef29ddd6003bbcfd22bc29777ee9a8960
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:07 +0000

    market issue size float comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
