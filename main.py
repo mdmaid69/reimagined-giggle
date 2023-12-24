d869b113d9d75f2835fac6e94ee91a8a248ebc19
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:03 +0000

    trust period appear free exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
