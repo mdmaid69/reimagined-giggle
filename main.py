e8535d6a39e2b6eb350cc977f44902127822609f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:59 +0000

    dark plate benefit all bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
