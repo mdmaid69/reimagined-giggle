05f136a595994480176879a5be1ddb5f016f2b93
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:04 +0000

    common sport major terrible loss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
