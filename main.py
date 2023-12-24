63c301c827b1df7e33492a5286f99a52c67483d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:44 +0000

    broken trust badly over hard

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def count_elements(lst):
+        return len(lst)
