47c419073327b9e1bd01c3a03a185c10ae8fa5e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:29 +0000

    chance table creature gift recovery

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
