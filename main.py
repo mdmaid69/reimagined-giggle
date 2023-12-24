9d7be13da4b275025950f3914e62e55512e28143
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:45 +0000

    angry away send national false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def greet(name):
+        print(f"Hello, {name}!")
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_float(array):
+        return float(array[0])
