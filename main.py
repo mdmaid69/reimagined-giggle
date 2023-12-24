803373456c001ef57306868ac2eef635096f0804
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:13 +0000

    benefit brother pleased sail way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
