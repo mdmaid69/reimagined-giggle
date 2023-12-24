2f5a86f5e7cf6e499842a9e7c2445a9399e286c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:06 +0000

    field front polite minority club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
