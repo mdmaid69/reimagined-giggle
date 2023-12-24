01915884aec05b17d7ac9877082ef7b69dc43b55
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:26:05 +0000

    nothing beautiful foot threaten possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
 import math
 def calculate_sphere_volume(radius):
         return 4/3 * math.pi * radius**3
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
