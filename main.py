afc6f3154ff8ba681d7d6a9fd3b1fe0d8dfd0943
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:55:45 +0000

    down trust advantage force exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
