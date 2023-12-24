d782c467e05137b2ed75a571a072eb5131617d70
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:06 +0000

    thanks around spill come deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
