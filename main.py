01e299d964f8100398509485c8a31d69e296e1e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:21 +0000

    argument thorough back cross physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
