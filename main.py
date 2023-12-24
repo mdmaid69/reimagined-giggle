e2c7b06166174dae8f9138e8df982692379bca68
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:05 +0000

    battery know across dig approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
