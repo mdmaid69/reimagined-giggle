abf6aaab94507a02e7781fcd4fcb8bf29fa3db03
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:37:14 +0000

    gate talkinto evidence comeover fork

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
