7f6e0da3f35646f5d93d57064b67e3449f174c9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:06 +0000

    shoulder load handle niggaacademy clearly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
