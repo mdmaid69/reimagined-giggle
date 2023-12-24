0f0ce9abe95b8ee838c75e26c0d00c8e957b7194
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:28 +0000

    straight long treat main interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def reverse_list(lst):
+        return lst[::-1]
