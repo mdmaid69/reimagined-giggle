ac3abb86cf772e364b47ca06df68ae8a04e0f084
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:10:00 +0000

    eat feature connect offence fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_perpetuity(payment, rate):
+        return payment / rate
