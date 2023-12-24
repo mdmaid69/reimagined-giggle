93477cd35254f5ffdb87574a7785192ecd2d95d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:58 +0000

    raw usual consist free live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
