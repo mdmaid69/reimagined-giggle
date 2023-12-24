3c5995489a43955da18addf67810f8c692d4d30b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:45:44 +0000

    shallow divide bath generous among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
