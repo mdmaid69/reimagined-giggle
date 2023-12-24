6520470006d86d5ae77546330a99c419ac5231df
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:19:12 +0000

    petrol stop collection television comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
