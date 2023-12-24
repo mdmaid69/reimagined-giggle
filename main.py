6e0dda9e1f4c06468005200f9effd9d24bdce29b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:00 +0000

    proud major wise steady thanks

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
