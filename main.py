ca4feed59a79b1216deccf338fdb68e52b458d75
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:02 +0000

    guard profit spill style who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def subtract_numbers(x, y):
-        return x - y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
