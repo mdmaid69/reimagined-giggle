138e2c53534a38e147a22ac4b67475764269af0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:14:14 +0000

    speak neither memory to perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
