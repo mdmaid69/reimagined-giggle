a714048905e3647e3939dfa2d3e22fa867a1fb42
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:51:34 +0000

    embarrassed behaviour wherever hollow smoke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
