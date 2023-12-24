dffc71c8a0abcd3b841e53efa429936e16762a36
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:03:23 +0000

    bin machine protect afternoon I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def convert_to_hex(n):
+        return hex(n)
