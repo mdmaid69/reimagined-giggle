ba3b520c0aa0d3cda7a6cbb197218aae9d92f5c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:58 +0000

    survive news agree away clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import tensorflow as tf
+print(tf.__version__)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
