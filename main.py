660c4da5ff94fed660f7da4b494df3cfe78b8a02
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:05 +0000

    replace 572enter definite fine any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
