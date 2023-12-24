e2082f501fa1a9ed148e4989afb158de333bb153
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:20 +0000

    clean succeed gap stupid calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
