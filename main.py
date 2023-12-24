63de3fa18260cefad927d1fcd94dd4226dc32fd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:11 +0000

    recover price news opposite question

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
