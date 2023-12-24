c9a512176e9c07c4843f247258acda0fc5ef5098
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:26 +0000

    enthusiastic sky service Oo spend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
