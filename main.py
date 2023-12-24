98c01126b91b1eadfa2f74c1cf599505ecea468f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:00:26 +0000

    get test early tax field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import datetime
-print(datetime.datetime.now())
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
