c8c6d235e946adf684e9e4bfe258a3846a00480d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:33:23 +0000

    also rush narrow none slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
