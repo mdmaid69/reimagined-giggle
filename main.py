adeedd845ab399d2dca9e4929452acb4fb942ff1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:20:15 +0000

    satisfied be ever him that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
