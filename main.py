535590b327648ae20aa7c8ea3067214c26f45d26
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:20 +0000

    cruel sense concentrate centimetre crowded

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
