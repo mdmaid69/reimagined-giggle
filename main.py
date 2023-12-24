d1239c797d41355cc2c580666262f55f0c9204a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:39:29 +0000

    breakthrough curious fear theirs unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
