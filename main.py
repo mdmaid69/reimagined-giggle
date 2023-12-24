7c9cd7fe6b28634f0ec0d449c603970f43cb8ac7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:40 +0000

    fortunate somewhere upstairs swim organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
