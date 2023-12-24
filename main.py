7e288dbe47b57fbe887b05cf37c60f746b73d796
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:17:55 +0000

    567enemy change lead wall inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
