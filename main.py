7d8e01da3460065005eb63fece3a17bb37445e4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:29:49 +0000

    mess autumn transport after call

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
