8e7e56f548c31abbcd0c40b7eda2e1ba70eeda67
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:06 +0000

    thankyou study once care minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
