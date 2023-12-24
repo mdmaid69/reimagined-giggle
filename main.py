f5c7499177cec67a6e463748e3ec2a67f2e6c7ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:49 +0000

    block otherwise train bring tend

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
