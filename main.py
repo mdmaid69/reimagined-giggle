384960b11402bc6493a07bc793f29feaac58e266
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:46 +0000

    release frame begin still Mm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
