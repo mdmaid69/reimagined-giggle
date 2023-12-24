fc3d77d5260112b49cc38c61b1d16228611df74b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:52:41 +0000

    deliver pressure importance sail dead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
