bcb9b92cbcc5ea139c7a00e0fe6304477746d029
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:18 +0000

    damp water stage pattern cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
