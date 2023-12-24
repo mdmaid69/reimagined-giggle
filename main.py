a51031edb8aaeb918a509f50e2bbd92c84fa1681
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:38:29 +0000

    understand blow perfect advantage get

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def sort_list(lst):
+        return sorted(lst)
