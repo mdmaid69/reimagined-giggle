f4a2082954ac8b5d8052a9d39debda0f3e88a42b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:31 +0000

    from favour by in mean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
