88ef12d08ddaa130ea82013e6993c11ab296bae5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:58 +0000

    children swim fetch slip error

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def multiply_numbers(x, y):
+        return x * y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
