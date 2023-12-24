50805b4ff4ae15c77258bafa9d42f93f8648574e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:59 +0000

    stare callup fast snow famous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+import re
+print(re.match("h.*o", "hello world"))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
