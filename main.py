bf4b6b353e546a68ae5b18e20c51047d901206fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:51 +0000

    anywayanyhow drive rough correction plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
