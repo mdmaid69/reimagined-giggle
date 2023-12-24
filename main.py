10cdb14f2db7030598b7da60caea2ef88c6b3d95
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:21:25 +0000

    intelligent contact stone keep who

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
