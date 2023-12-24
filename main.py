dca8ce712c4f7bb253617a32ef09f95599af1d32
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:52 +0000

    march night welcome visit somebody

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_ordered_dict():
+        return collections.OrderedDict()
