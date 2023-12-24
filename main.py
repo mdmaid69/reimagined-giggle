de33cd9b175b1c9bb33b5db003c54446996e8f91
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:45 +0000

    today smooth delicate law punishment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
