65b86a782b1fb416e0cadd3ee89f32918cf1d70b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:22 +0000

    duty name even correct central

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
