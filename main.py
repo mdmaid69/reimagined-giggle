e96d4dc450ff3ca0696557d9acd22fc2bb98a0d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:04:26 +0000

    pretty steer edge can rest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
