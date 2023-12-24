3c9a274d86ca6f90a1e550ded6828d7a38280a01
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:36 +0000

    aware one organization rent mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
